a8be6fbe30260c555d9f9a18d0ca6df755e018b3
Ross Mason <ross@mulesoft.com>
2005-02-23 06:28:53 +0000

    Added codec transformers
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@86 bf997673-6b11-0410-b953-e057580c5b09

---
+++ src/test/java/org/mule/test/transformers/codec/UCEncodingTransformersTestCase.java
@@ -0,0 +1,66 @@
+/*
+ * $Header:
+ * /cvsroot/mule/mule/src/test/org/mule/test/transformers/DefaultTransformersTestCase.java,v
+ * 1.3 2003/11/26 06:45:02 rossmason Exp $ $Revision$ $Date: 2003/11/26
+ * 06:45:02 $
+ * ------------------------------------------------------------------------------------------------------
+ * 
+ * Copyright (c) Cubis Limited. All rights reserved. http://www.cubis.co.uk
+ * 
+ * The software in this package is published under the terms of the BSD style
+ * license a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ *  
+ */
+
+package org.mule.test.transformers.codec;
+
+import org.mule.tck.AbstractTransformerTestCase;
+import org.mule.umo.transformer.UMOTransformer;
+import org.mule.test.transformers.ReverseCompressionTransformer;
+import org.mule.transformers.codec.Base64Encoder;
+import org.mule.transformers.codec.Base64Decoder;
+import org.mule.transformers.codec.UUEncoder;
+import org.mule.transformers.codec.UUDecoder;
+import org.mule.transformers.codec.UCEncoder;
+import org.mule.transformers.codec.UCDecoder;
+
+/**
+ * @author <a href="mailto:ross.mason@cubis.co.uk">Ross Mason</a>
+ * @version $Revision$
+ */
+public class UCEncodingTransformersTestCase extends AbstractTransformerTestCase
+{
+    /* (non-Javadoc)
+     * @see org.mule.tck.AbstractTransformerTestCase#getResultData()
+     */
+    public Object getResultData()
+    {
+        return new sun.misc.UCEncoder().encode(getTestData().toString().getBytes());
+    }
+
+    /* (non-Javadoc)
+     * @see org.mule.tck.AbstractTransformerTestCase#getTestData()
+     */
+    public Object getTestData()
+    {
+        return "the quick brown fox jumped over the lazy dog";
+    }
+
+    /* (non-Javadoc)
+     * @see org.mule.tck.AbstractTransformerTestCase#getTransformers()
+     */
+    public UMOTransformer getTransformer()
+    {
+        return new UCEncoder();
+    }
+
+    /* (non-Javadoc)
+     * @see org.mule.tck.AbstractTransformerTestCase#getRoundTripTransformer()
+     */
+    public UMOTransformer getRoundTripTransformer()
+    {
+        return new UCDecoder();
+    }
+
+}
