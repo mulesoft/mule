4bc66d6812532c9e9c762a306e4617c4206ebd14
Ross Mason <ross@mulesoft.com>
2005-02-23 11:51:41 +0000

    Added encryption transformers
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@87 bf997673-6b11-0410-b953-e057580c5b09

---
+++ src/java/org/mule/transformers/encryption/EncryptionTransformer.java
@@ -0,0 +1,32 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) Cubis Limited. All rights reserved.
+ * http://www.cubis.co.uk
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.mule.transformers.encryption;
+
+import org.mule.umo.security.CryptoFailureException;
+
+/**
+ * <code>EncryptionTransformer</code> will transform an array of bytes or string
+ * into an encrypted array of bytes
+ *
+ *
+ * @author <a href="mailto:ross.mason@cubis.co.uk">Ross Mason</a>
+ * @version $Revision$
+ */
+public class EncryptionTransformer extends AbstractEncryptionTransformer
+{
+    protected byte[] getTransformedBytes(byte[] buffer) throws CryptoFailureException
+    {
+        return getStrategy().encrypt(buffer);
+    }
+}
