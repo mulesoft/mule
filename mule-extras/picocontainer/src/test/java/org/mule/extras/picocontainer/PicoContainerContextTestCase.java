4fb8a5f3132999eb2bed9e85fde9143d23cc02a1
Ross Mason <ross@mulesoft.com>
2005-02-25 02:10:55 +0000

    *** empty log message ***
    git-svn-id: https://svn.codehaus.org/mule/trunk@106 bf997673-6b11-0410-b953-e057580c5b09

---
+++ mule-extras/picocontainer/src/test/java/org/mule/extras/picocontainer/PicoContainerContextTestCase.java
@@ -0,0 +1,47 @@
+/* 
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ * 
+ * Copyright (c) Cubis Limited. All rights reserved.
+ * http://www.cubis.co.uk 
+ * 
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file. 
+ *
+ */
+
+package org.mule.extras.picocontainer;
+
+import org.mule.tck.model.AbstractComponentResolverTestCase;
+import org.mule.umo.model.UMOContainerContext;
+
+/**
+ * @author <a href="mailto:ross.mason@cubis.co.uk">Ross Mason</a>
+ * @version $Revision$
+ */
+public class PicoContainerContextTestCase extends AbstractComponentResolverTestCase
+{
+
+    PicoContainerContext context;
+
+    /* (non-Javadoc)
+     * @see org.mule.tck.model.AbstractComponentResolverTestCase#getConfiguredResolver()
+     */
+    public UMOContainerContext getContainerContext()
+    {
+        return context;
+    }
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        context = new PicoContainerContext();
+        context.setConfigFile("test-pico-config.xml");
+    }
+}
