35198b6511ea15a395334639f3edf466031b7036
Ross Mason <ross@mulesoft.com>
2005-02-27 21:29:53 +0000

    fixed testcase for Spring container context
    git-svn-id: https://svn.codehaus.org/mule/trunk@122 bf997673-6b11-0410-b953-e057580c5b09

--- mule-extras/spring/src/test/java/org/mule/extras/spring/SpringContainerContextTestCase.java
+++ mule-extras/spring/src/test/java/org/mule/extras/spring/SpringContainerContextTestCase.java
@@ -16,7 +16,9 @@
 package org.mule.extras.spring;
 
 import org.mule.tck.model.AbstractComponentResolverTestCase;
+import org.mule.tck.testmodels.fruit.Apple;
 import org.mule.umo.model.UMOContainerContext;
+import org.mule.umo.model.ComponentNotFoundException;
 
 /**
  * @author <a href="mailto:ross.mason@cubis.co.uk">Ross Mason</a>
@@ -42,6 +44,44 @@ public class SpringContainerContextTestCase extends AbstractComponentResolverTes
         super.setUp();
         context = new SpringContainerContext();
         context.setConfigFile("test-application-context.xml");
+    }
+
+    public void testContainerContext()
+    {
+        UMOContainerContext container = getContainerContext();
+
+        assertNotNull(container);
+
+        Object result = null;
+
+        try
+        {
+            result = container.getComponent(null);
+            fail("Should throw ComponentNotFoundException for null key");
+        }
+        catch (ComponentNotFoundException e)
+        {
+            // expected
+        }
+
+        try
+        {
+            result = container.getComponent("abcdefg123456!£$%^n");
+            fail("Should throw ComponentNotFoundException for a key that doesn't exist");
+        }
+        catch (ComponentNotFoundException e)
+        {
+            // expected
+        }
+
+        try
+        {
+            result = container.getComponent(Apple.class.getName());
+            assertNotNull("Component should exist in container", result);
+        } catch (ComponentNotFoundException e)
+        {
+            fail("Component should exist in the container");
+        }
     }
 
 }
