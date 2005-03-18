bcf2d26f55ef25556e45c545e93f5aa15f761f62
gnt <gnt>
2005-03-18 18:56:10 +0000

    Jira: MULE-195: The default http response transformers fails to handle binary data
    git-svn-id: https://svn.codehaus.org/mule/trunk@222 bf997673-6b11-0410-b953-e057580c5b09

--- providers/http/src/java/org/mule/providers/http/transformers/UMOMessageToResponseString.java
+++ providers/http/src/java/org/mule/providers/http/transformers/UMOMessageToResponseString.java
@@ -13,6 +13,17 @@
  */
 package org.mule.providers.http.transformers;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import org.mule.MuleManager;
 import org.mule.config.MuleProperties;
 import org.mule.providers.http.HttpConnector;
@@ -21,14 +32,6 @@ import org.mule.transformers.AbstractEventAwareTransformer;
 import org.mule.umo.UMOEventContext;
 import org.mule.umo.UMOMessage;
 import org.mule.umo.transformer.TransformerException;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
 /**
  * <code>UMOMessageToResponseString</code> TODO
@@ -59,79 +62,91 @@ public class UMOMessageToResponseString extends AbstractEventAwareTransformer
         String version = (String)context.getProperty(HttpConnector.HTTP_VERSION_PROPERTY, HttpConstants.HTTP11);
         String date = format.format(new Date());
 
-        String response = src.toString();
-
-
-        StringBuffer httpMessage = new StringBuffer();
-
-        httpMessage.append(version).append(" ");
-        httpMessage.append(status).append(HttpConstants.CRLF);
-        httpMessage.append(HttpConstants.HEADER_DATE);
-        httpMessage.append(": ").append(date).append(HttpConstants.CRLF);
-        httpMessage.append(HttpConstants.HEADER_SERVER);
-        httpMessage.append(": ").append(server).append(HttpConstants.CRLF);
-        if(context.getProperty(HttpConstants.HEADER_EXPIRES)==null) {
-            httpMessage.append(HttpConstants.HEADER_EXPIRES);
-            httpMessage.append(": ").append(date).append(HttpConstants.CRLF);
-        }
-
-        httpMessage.append(HttpConstants.HEADER_CONTENT_TYPE);
-        String contentType = (String)context.getProperty(HttpConstants.HEADER_CONTENT_TYPE);
-        if(contentType==null) {
-            httpMessage.append(": ").append("text/xml").append(HttpConstants.CRLF);
+        byte[] response;
+        if (src instanceof byte[]) {
+        	response = (byte[]) src;
         } else {
-            httpMessage.append(": ").append(contentType).append(HttpConstants.CRLF);
+        	response = src.toString().getBytes();
         }
 
-        httpMessage.append(HttpConstants.HEADER_CONTENT_LENGTH);
-        httpMessage.append(": ").append(response.length()).append(HttpConstants.CRLF);
-
-        String headerName;
-        String value;
-        for (Iterator iterator = headers.iterator(); iterator.hasNext();)
-        {
-            headerName = (String) iterator.next();
-            value = (String)context.getProperty(headerName);
-            if(value!=null) {
-                httpMessage.append(headerName).append(": ").append(value);
-                httpMessage.append(HttpConstants.CRLF);
-            }
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        OutputStreamWriter httpMessage = new OutputStreamWriter(baos);
+        
+        try {
+	        httpMessage.append(version).append(" ");
+	        httpMessage.append(Integer.toString(status));
+	        httpMessage.append(HttpConstants.CRLF);
+	        httpMessage.append(HttpConstants.HEADER_DATE);
+	        httpMessage.append(": ").append(date).append(HttpConstants.CRLF);
+	        httpMessage.append(HttpConstants.HEADER_SERVER);
+	        httpMessage.append(": ").append(server).append(HttpConstants.CRLF);
+	        if(context.getProperty(HttpConstants.HEADER_EXPIRES)==null) {
+	            httpMessage.append(HttpConstants.HEADER_EXPIRES);
+	            httpMessage.append(": ").append(date).append(HttpConstants.CRLF);
+	        }
+	
+	        httpMessage.append(HttpConstants.HEADER_CONTENT_TYPE);
+	        String contentType = (String)context.getProperty(HttpConstants.HEADER_CONTENT_TYPE);
+	        if(contentType==null) {
+	            httpMessage.append(": ").append("text/xml").append(HttpConstants.CRLF);
+	        } else {
+	            httpMessage.append(": ").append(contentType).append(HttpConstants.CRLF);
+	        }
+	
+	        httpMessage.append(HttpConstants.HEADER_CONTENT_LENGTH);
+	        httpMessage.append(": ").append(Integer.toString(response.length)).append(HttpConstants.CRLF);
+	
+	        String headerName;
+	        String value;
+	        for (Iterator iterator = headers.iterator(); iterator.hasNext();)
+	        {
+	            headerName = (String) iterator.next();
+	            value = (String)context.getProperty(headerName);
+	            if(value!=null) {
+	                httpMessage.append(headerName).append(": ").append(value);
+	                httpMessage.append(HttpConstants.CRLF);
+	            }
+	        }
+	        //Custom headers
+	        Map customHeaders = (Map)context.getProperty(HttpConnector.HTTP_CUSTOM_HEADERS_MAP_PROPERTY);
+	        if(customHeaders!=null) {
+	            Map.Entry entry;
+	            for (Iterator iterator = customHeaders.entrySet().iterator(); iterator.hasNext();)
+	            {
+	                entry =  (Map.Entry)iterator.next();
+	                httpMessage.append(entry.getKey().toString()).append(": ").append(entry.getValue().toString());
+	                httpMessage.append(HttpConstants.CRLF);
+	            }
+	        }
+	
+	        //Mule properties
+	        UMOMessage m = context.getMessage();
+	        String user = (String)m.getProperty(MuleProperties.MULE_USER_PROPERTY);
+	        if(user!=null) {
+	            httpMessage.append("X-" + MuleProperties.MULE_USER_PROPERTY).append(": ").append(user);
+	            httpMessage.append(HttpConstants.CRLF);
+	        }
+	        if(m.getCorrelationId()!=null) {
+	            httpMessage.append("X-" + MuleProperties.MULE_CORRELATION_ID_PROPERTY).append(": ").append(m.getCorrelationId());
+	            httpMessage.append(HttpConstants.CRLF);
+	            httpMessage.append("X-" + MuleProperties.MULE_CORRELATION_GROUP_SIZE_PROPERTY).append(": ").append(Integer.toString(m.getCorrelationGroupSize()));
+	            httpMessage.append(HttpConstants.CRLF);
+	            httpMessage.append("X-" + MuleProperties.MULE_CORRELATION_SEQUENCE_PROPERTY).append(": ").append(Integer.toString(m.getCorrelationSequence()));
+	            httpMessage.append(HttpConstants.CRLF);
+	        }
+	        if(m.getReplyTo()!=null) {
+	            httpMessage.append("X-" + MuleProperties.MULE_REPLY_TO_PROPERTY).append(": ").append(m.getReplyTo().toString());
+	            httpMessage.append(HttpConstants.CRLF);
+	        }
+	
+	        //End header
+	        httpMessage.append(HttpConstants.CRLF);
+	        httpMessage.close();
+	        baos.write(response);
+	        baos.close();
+        } catch (IOException e) {
+        	throw new TransformerException("IO error", e);
         }
-        //Custom headers
-        Map customHeaders = (Map)context.getProperty(HttpConnector.HTTP_CUSTOM_HEADERS_MAP_PROPERTY);
-        if(customHeaders!=null) {
-            Map.Entry entry;
-            for (Iterator iterator = customHeaders.entrySet().iterator(); iterator.hasNext();)
-            {
-                entry =  (Map.Entry)iterator.next();
-                httpMessage.append(entry.getKey()).append(": ").append(entry.getValue());
-                httpMessage.append(HttpConstants.CRLF);
-            }
-        }
-
-        //Mule properties
-        UMOMessage m = context.getMessage();
-        String user = (String)m.getProperty(MuleProperties.MULE_USER_PROPERTY);
-        if(user!=null) {
-            httpMessage.append("X-" + MuleProperties.MULE_USER_PROPERTY).append(": ").append(user);
-            httpMessage.append(HttpConstants.CRLF);
-        }
-        if(m.getCorrelationId()!=null) {
-            httpMessage.append("X-" + MuleProperties.MULE_CORRELATION_ID_PROPERTY).append(": ").append(m.getCorrelationId());
-            httpMessage.append(HttpConstants.CRLF);
-            httpMessage.append("X-" + MuleProperties.MULE_CORRELATION_GROUP_SIZE_PROPERTY).append(": ").append(m.getCorrelationGroupSize());
-            httpMessage.append(HttpConstants.CRLF);
-            httpMessage.append("X-" + MuleProperties.MULE_CORRELATION_SEQUENCE_PROPERTY).append(": ").append(m.getCorrelationSequence());
-            httpMessage.append(HttpConstants.CRLF);
-        }
-        if(m.getReplyTo()!=null) {
-            httpMessage.append("X-" + MuleProperties.MULE_REPLY_TO_PROPERTY).append(": ").append(m.getReplyTo().toString());
-            httpMessage.append(HttpConstants.CRLF);
-        }
-
-        //End header
-        httpMessage.append(HttpConstants.CRLF);
-        httpMessage.append(response);
-        return httpMessage.toString();
+        return baos.toByteArray();
     }
 }
