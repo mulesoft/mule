3ca6412808e0207c086e24c4448d728f827da3da
Ross Mason <ross@mulesoft.com>
2005-04-15 01:00:41 +0000

    pgp test updates from Alex
    git-svn-id: https://svn.codehaus.org/mule/trunk@312 bf997673-6b11-0410-b953-e057580c5b09

---
+++ mule-extras/pgp/src/test/java/org/mule/extras/pgp/EchoMsg.java
@@ -0,0 +1,25 @@
+/*
+ * Project: mule-extras-pgp
+ * Author : ariva
+ * Created on 14-apr-2005
+ *
+ */
+package org.mule.extras.pgp;
+
+import org.mule.umo.UMOEventContext;
+import org.mule.umo.lifecycle.Callable;
+
+/**
+ * @author ariva
+ *
+ */
+public class EchoMsg implements Callable {
+
+    /* (non-Javadoc)
+     * @see org.mule.umo.lifecycle.Callable#onCall(org.mule.umo.UMOEventContext)
+     */
+    public Object onCall(UMOEventContext eventContext) throws Exception {
+        return eventContext.getMessageAsString();
+    }
+
+}
