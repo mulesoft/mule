4da188fc3267d283f043e3cce93f1c27af81319e
Ross Mason <ross@mulesoft.com>
2005-04-16 18:05:12 +0000

    fixed npe and version info
    git-svn-id: https://svn.codehaus.org/mule/trunk@315 bf997673-6b11-0410-b953-e057580c5b09

--- mule/src/java/org/mule/impl/DefaultExceptionStrategy.java
+++ mule/src/java/org/mule/impl/DefaultExceptionStrategy.java
@@ -53,6 +53,8 @@ public class DefaultExceptionStrategy extends AbstractExceptionListener
     
     protected void defaultHandler(Throwable t) {
         logException(t);
-        RequestContext.setExceptionPayload(new ExceptionPayload(t));
+        if(RequestContext.getEvent()!=null) {
+            RequestContext.setExceptionPayload(new ExceptionPayload(t));
+        }
     }
 }
