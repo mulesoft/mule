4e7f4c544f910dc9c5c20ddd574503511f63119d
Ross Mason <ross@mulesoft.com>
2005-04-25 23:06:50 +0000

    Added integration tests for exception strategies and the Axis connector for using different transports other than http
    git-svn-id: https://svn.codehaus.org/mule/trunk@328 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/integration/exceptions/ExceptionListenerTestCase.java
@@ -0,0 +1,33 @@
+package org.mule.test.integration.exceptions;
+
+import org.mule.MuleManager;
+import org.mule.config.builders.MuleXmlConfigurationBuilder;
+import org.mule.extras.client.MuleClient;
+import org.mule.impl.message.ExceptionMessage;
+import org.mule.tck.NamedTestCase;
+import org.mule.umo.UMOMessage;
+
+public class ExceptionListenerTestCase extends NamedTestCase
+{
+     public void setUp() throws Exception
+    {
+        if(MuleManager.isInstanciated()) MuleManager.getInstance().dispose();
+
+        MuleXmlConfigurationBuilder builder = new MuleXmlConfigurationBuilder();
+        builder.configure("org/mule/test/integration/exceptions/exception-listener-config.xml");
+    }
+
+    public void testExceptionStrategy() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        client.send("vm://mycomponent", "test", null);
+
+        UMOMessage message = client.receive("vm:/mycomponent.out", 2000);
+        assertNull(message);
+
+        message = client.receive("vm:/error.queue", 2000);
+        assertNotNull(message);
+        Object payload = message.getPayload();
+        assertTrue(payload instanceof ExceptionMessage);
+    }
+}
---
+++ tests/integration/src/test/java/org/mule/test/integration/providers/soap/AxisConnectorVMFunctionalTestCase.java
@@ -0,0 +1,145 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.mule.test.integration.providers.soap;
+
+import org.apache.axis.client.Call;
+import org.apache.axis.client.Service;
+import org.mule.MuleManager;
+import org.mule.config.ConfigurationBuilder;
+import org.mule.config.builders.MuleXmlConfigurationBuilder;
+import org.mule.extras.client.MuleClient;
+import org.mule.impl.endpoint.MuleEndpoint;
+import org.mule.impl.endpoint.MuleEndpointURI;
+import org.mule.providers.service.ConnectorFactory;
+import org.mule.providers.soap.Person;
+import org.mule.tck.AbstractMuleTestCase;
+import org.mule.umo.UMOEvent;
+import org.mule.umo.UMOMessage;
+import org.mule.umo.endpoint.UMOEndpoint;
+import org.mule.umo.provider.UMOConnector;
+import org.mule.umo.provider.UMOMessageDispatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
+ * @version $Revision$
+ */
+public class AxisConnectorVMFunctionalTestCase extends AbstractMuleTestCase
+{
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        if (MuleManager.isInstanciated()) MuleManager.getInstance().dispose();
+        ConfigurationBuilder configBuilder = new MuleXmlConfigurationBuilder();
+        configBuilder.configure("org/mule/test/integration/soap/axis-test-vm-mule-config.xml");
+    }
+
+    public void testRequestResponse() throws Throwable
+    {
+        MuleClient client = new MuleClient();
+        Service service = new Service();
+        Call call = (Call) service.createCall();
+
+//        String endpoint = "http://localhost:38009/mule/mycomponent";
+//        call.setTargetEndpointAddress(endpoint);
+//        call.setOperationName("echo");
+//        call.setSOAPActionURI(endpoint + "?method=echo");
+        List results = new ArrayList();
+        for(int i = 0;i < 100;i++) {
+            results.add(client.send("vm://mule/mycomponent?method=echo", "Message " + i, null).getPayload());
+        }
+
+        assertEquals(100, results.size());
+        for(int i = 0;i < 100;i++) {
+            assertEquals("Message " + i, results.get(i).toString());
+        }
+    }
+
+    public void testReceive() throws Throwable
+    {
+        UMOConnector c = ConnectorFactory.getConnectorByProtocol("axis");
+        assertNotNull(c);
+        UMOMessageDispatcher dispatcher = c.getDispatcher("ANY");
+        UMOMessage result = dispatcher.receive(new MuleEndpointURI("http://localhost:38009/axis/services/mycomponent2?method=getDate"), 0);
+        assertNotNull(result);
+        assertNotNull(result.getPayload());
+        assertTrue(result.getPayload().toString().length() > 0);
+    }
+
+    public void testReceiveComplex() throws Throwable
+    {
+        UMOConnector c = ConnectorFactory.getConnectorByProtocol("axis");
+        assertNotNull(c);
+        UMOMessageDispatcher dispatcher = c.getDispatcher("ANY");
+        UMOMessage result = dispatcher.receive(new MuleEndpointURI("http://localhost:38009/mycomponent3?method=getPerson&param=Fred"), 0);
+        assertNotNull(result);
+        assertTrue(result.getPayload() instanceof Person);
+        assertEquals("Fred", ((Person)result.getPayload()).getFirstName());
+        assertEquals("Flintstone", ((Person)result.getPayload()).getLastName());
+    }
+
+    public void testSendComplex() throws Throwable
+    {
+        UMOConnector c = ConnectorFactory.getConnectorByProtocol("axis");
+        assertNotNull(c);
+        UMOMessageDispatcher dispatcher = c.getDispatcher("ANY");
+        UMOEndpoint endpoint = new MuleEndpoint("test",
+                new MuleEndpointURI("http://localhost:38009/mycomponent3?method=addPerson"),
+                        c, null, UMOEndpoint.ENDPOINT_TYPE_SENDER, 0, null);
+        UMOEvent event = getTestEvent(new Person("Ross", "Mason"), endpoint);
+
+        UMOMessage result = dispatcher.send(event);
+        assertNull(result);
+
+        //lets get our newly added person
+        result = dispatcher.receive(new MuleEndpointURI("http://localhost:38009/mycomponent3?method=getPerson&param=Ross"), 0);
+        assertNotNull(result);
+        assertTrue(result.getPayload() instanceof Person);
+        assertEquals("Ross", ((Person)result.getPayload()).getFirstName());
+        assertEquals("Mason", ((Person)result.getPayload()).getLastName());
+    }
+
+    public void testReceiveComplexCollection() throws Throwable
+    {
+        UMOConnector c = ConnectorFactory.getConnectorByProtocol("axis");
+        assertNotNull(c);
+        UMOMessageDispatcher dispatcher = c.getDispatcher("ANY");
+        UMOMessage result = dispatcher.receive(new MuleEndpointURI("http://localhost:38009/mycomponent3?method=getPeople"), 0);
+        assertNotNull(result);
+        assertTrue(result.getPayload() instanceof Person[]);
+        assertEquals(3, ((Person[])result.getPayload()).length);
+    }
+
+    public void testDispatchAsyncComplex() throws Throwable
+    {
+        UMOConnector c = ConnectorFactory.getConnectorByProtocol("axis");
+        assertNotNull(c);
+        UMOMessageDispatcher dispatcher = c.getDispatcher("ANY");
+        UMOEndpoint endpoint = new MuleEndpoint("test",
+                new MuleEndpointURI("http://localhost:38010/mycomponent4?method=addPerson"),
+                        c, null, UMOEndpoint.ENDPOINT_TYPE_SENDER, 0, null);
+        UMOEvent event = getTestEvent(new Person("Rossco", "Pico"), endpoint);
+
+        dispatcher.dispatch(event);
+        Thread.sleep(2000);
+        //lets get our newly added person
+        UMOMessage result = dispatcher.receive(new MuleEndpointURI("http://localhost:38009/mycomponent3?method=getPerson&param=Rossco"), 0);
+        assertNotNull(result);
+        assertTrue(result.getPayload() instanceof Person);
+        assertEquals("Rossco", ((Person)result.getPayload()).getFirstName());
+        assertEquals("Pico", ((Person)result.getPayload()).getLastName());
+    }
+}
