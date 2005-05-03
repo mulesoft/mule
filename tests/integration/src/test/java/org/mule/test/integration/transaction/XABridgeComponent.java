44f26adffe7836f5b19880745216469efbbd93c7
gnt <gnt>
2005-05-03 00:50:34 +0000

    Update licence
    git-svn-id: https://svn.codehaus.org/mule/trunk@346 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/transaction/XABridgeComponent.java
+++ tests/integration/src/test/java/org/mule/test/integration/transaction/XABridgeComponent.java
@@ -1,29 +1,40 @@
 /*
- * Created on 7 mars 2005
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * Copyright (c) Cubis Limited. All rights reserved.
+ * http://www.cubis.co.uk
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
  */
 package org.mule.test.integration.transaction;
 
 import java.util.Map;
 
-import javax.jms.Message;
-import javax.jms.TextMessage;
-
 import org.mule.transaction.TransactionCoordination;
 import org.mule.umo.UMOTransaction;
 
 /**
- * @author gnt
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * Simple component that receives messages from jdbc or jms and just forward 
+ * the interesting part.
+ * 
+ * @author <a href="mailto:gnt@codehaus.org">Guillaume Nodet</a>
+ * $Revision$
  */
 public class XABridgeComponent {
 	
 	public static boolean mayRollback = false;
 
+	/**
+	 * If <code>mayRollback</code> has been set to true,
+	 * the component will mark the current transaction as rollback only
+	 * on a 30 percent basis.
+	 * @throws Exception
+	 */
 	protected void mayRollback() throws Exception {
 		if (mayRollback) {
 			UMOTransaction tx = TransactionCoordination.getInstance().getTransaction();
@@ -36,11 +47,25 @@ public class XABridgeComponent {
 		}
 	}
 	
+	/**
+	 * Receive the jdbc message and forward the <code>data</code> part.
+	 * May mark the current transaction as rollback only.
+	 * @param msg
+	 * @return
+	 * @throws Exception
+	 */
 	public Object onJdbcMessage(Map msg) throws Exception {
 		mayRollback();
 		return msg.get("data").toString();
 	}
 	
+	/**
+	 * Receive the content of the jms message and forward it.
+	 * May mark the current transaction as rollback only.
+	 * @param msg
+	 * @return
+	 * @throws Exception
+	 */
 	public Object onJmsMessage(String msg) throws Exception {
 		mayRollback();
 		return msg;
