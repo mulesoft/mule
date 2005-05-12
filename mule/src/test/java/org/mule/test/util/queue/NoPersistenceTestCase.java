21cb18aeda89cf08a74fc63cad456f565cce9c64
gnt <gnt>
2005-05-12 08:25:32 +0000

    Add tests for queue transactions
    git-svn-id: https://svn.codehaus.org/mule/trunk@370 bf997673-6b11-0410-b953-e057580c5b09

---
+++ mule/src/test/java/org/mule/test/util/queue/NoPersistenceTestCase.java
@@ -0,0 +1,51 @@
+/* 
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ * 
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ * 
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file. 
+ *
+ */
+package org.mule.test.util.queue;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mule.util.queue.MemoryPersistenceStrategy;
+import org.mule.util.queue.TransactionalQueueManager;
+
+/**
+ * @author <a href="mailto:gnt@codehaus.org">Guillaume Nodet</a>
+ * @version $Revision$
+ */
+public class NoPersistenceTestCase extends
+		AbstractTransactionQueueManagerTestCase {
+
+	private static final Log logger = LogFactory.getLog(FilePersistenceTestCase.class);
+	
+	protected Log getLogger() {
+		return logger;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.mule.transaction.xa.queue.AbstractTransactionQueueManagerTestCase#createQueueManager()
+	 */
+	protected TransactionalQueueManager createQueueManager() throws Exception {
+		TransactionalQueueManager mgr = new TransactionalQueueManager();
+		mgr.setPersistenceStrategy(new MemoryPersistenceStrategy());
+		return mgr;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.mule.transaction.xa.queue.AbstractTransactionQueueManagerTestCase#isPersistent()
+	 */
+	protected boolean isPersistent() {
+		return false;
+	}
+
+}
