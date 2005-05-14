31aaef45c3c1383205d7868e566715135fa901ef
gnt <gnt>
2005-05-14 15:38:43 +0000

    Add a use case for http post with the payload as the parameter
    git-svn-id: https://svn.codehaus.org/mule/trunk@379 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/usecases/src/test/java/org/mule/test/usecases/http/HttpPostTestCase.java
@@ -0,0 +1,48 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+
+package org.mule.test.usecases.http;
+
+import org.mule.MuleManager;
+import org.mule.config.builders.MuleXmlConfigurationBuilder;
+import org.mule.extras.client.MuleClient;
+import org.mule.tck.NamedTestCase;
+import org.mule.umo.UMOMessage;
+
+/**
+ * TODO: document this class 
+ *
+ * @author <a href="mailto:gnt@codehaus.org">Guillaume Nodet</a>
+ * @version $Revision$
+ */
+public class HttpPostTestCase extends NamedTestCase {
+
+    protected void setUp() throws Exception
+    {
+        if(MuleManager.isInstanciated()) MuleManager.getInstance().dispose();
+        MuleXmlConfigurationBuilder builder = new MuleXmlConfigurationBuilder();
+        builder.configure("org/mule/test/usecases/http/http-post.xml");
+    }
+	
+    public void testPost() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        UMOMessage message = client.send("httpRequest", "payload", null);
+        assertNotNull(message);
+        assertNotNull(message.getPayloadAsString());
+        assertEquals("IncidentData=payload", message.getPayloadAsString());
+    }
+	
+	
+}
