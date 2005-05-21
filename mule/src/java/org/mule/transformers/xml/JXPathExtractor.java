c47d94185074a99defdecd7de036a5f9403256bd
gnt <gnt>
2005-05-21 14:05:47 +0000

    Fix MULE-260 : add an xpath extractor transformer
    git-svn-id: https://svn.codehaus.org/mule/trunk@407 bf997673-6b11-0410-b953-e057580c5b09

---
+++ mule/src/java/org/mule/transformers/xml/JXPathExtractor.java
@@ -0,0 +1,73 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.mule.transformers.xml;
+
+import org.apache.commons.jxpath.JXPathContext;
+import org.apache.log4j.Logger;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.mule.routing.filters.xml.JXPathFilter;
+import org.mule.transformers.AbstractTransformer;
+import org.mule.umo.transformer.TransformerException;
+
+/**
+ * The JXPathExtractor is a simple transformer that
+ * evaluates an xpath expression against the given bean
+ * and that returns the result.
+ * 
+ * @author <a href="mailto:gnt@codehaus.org">Guillaume Nodet</a>
+ * @version $Revision$
+ */
+public class JXPathExtractor extends AbstractTransformer {
+
+	private static final Logger logger = Logger.getLogger(JXPathFilter.class);
+
+	private String expression;
+
+	/**
+	 * Evaluate the expression in the context of the given object and
+	 * returns the result.
+	 * If the given object is a string, it assumes it is an valid xml
+	 * and parses it before evaluating the xpath expression. 
+	 */
+	public Object doTransform(Object src) throws TransformerException {
+		try {
+			Object o = null;
+			if (src instanceof String) {
+				Document doc = DocumentHelper.parseText((String) src);
+				o = doc.valueOf(expression);
+			} else {
+				JXPathContext context = JXPathContext.newContext(src);
+				o = context.getValue(expression);
+			}
+			return o;
+		} catch (Exception e) {
+			throw new TransformerException(this, e);
+		}
+	}
+
+	/**
+	 * @return Returns the expression.
+	 */
+	public String getExpression() {
+		return expression;
+	}
+
+	/**
+	 * @param expression The expression to set.
+	 */
+	public void setExpression(String expression) {
+		this.expression = expression;
+	}
+}
