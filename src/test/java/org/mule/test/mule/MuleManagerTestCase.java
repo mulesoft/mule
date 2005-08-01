b07366f3b8543f66f1eaa5bcec2fa55dfd6fd28b
aperepel@gmail.com <Andrew Perepelytsya>
2005-08-01 13:22:51 +0000

    Fix for MULE-348 Mule does not honour agent order declared in XML config. Also refactored the test case.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@558 bf997673-6b11-0410-b953-e057580c5b09

--- src/test/java/org/mule/test/mule/MuleManagerTestCase.java
+++ src/test/java/org/mule/test/mule/MuleManagerTestCase.java
@@ -13,9 +13,9 @@
  */
 package org.mule.test.mule;
 
-import org.mule.MuleManager;
-import org.mule.config.builders.MuleXmlConfigurationBuilder;
-import org.mule.tck.AbstractUMOManagerTestCase;
+import org.mule.management.agents.JmxAgent;
+import org.mule.management.agents.Mx4jAgent;
+import org.mule.tck.AbstractMuleTestCase;
 import org.mule.umo.manager.UMOManager;
 
 /**
@@ -24,23 +24,45 @@ import org.mule.umo.manager.UMOManager;
  * @version $Revision$
  */
 
-public class MuleManagerTestCase extends AbstractUMOManagerTestCase
+public class MuleManagerTestCase extends AbstractMuleTestCase
 {
-    public UMOManager getUMOManager() throws Exception
+
+    private UMOManager manager;
+
+    /**
+     * Print the name of this test to standard output
+     */
+    protected void setUp() throws Exception
     {
-        if (MuleManager.isInstanciated())
-            MuleManager.getInstance().dispose();
-        MuleXmlConfigurationBuilder builder = new MuleXmlConfigurationBuilder();
-        UMOManager manager = builder.configure("test-config-for-manager.xml");
-        return manager;
+        super.setUp();
+        manager = getManager();
     }
 
     public void testRemoveNonExistentAgent() throws Exception
     {
-        UMOManager manager = this.getUMOManager();
         manager.removeAgent("DOES_NOT_EXIST");
 
         // should not throw NPE
+    }
+
+    public void testAgentsRegistrationOrder() throws Exception
+    {
+        JmxAgent agentFirst = new JmxAgent();
+        // If you specified "JmxAgent", it was the first one in the map,
+        // but for "jmxAgent" the order was not preserved.
+        // MX4JAgent depends on JmxAgent having finished initilisation
+        // before proceeding, otherwise it is not able to find any
+        // MBeanServer.
+        agentFirst.setName("jmxAgent");
+        manager.registerAgent(agentFirst);
+
+        Mx4jAgent agentSecond = new Mx4jAgent();
+        agentSecond.setName("mx4jAgent");
+        manager.registerAgent(agentSecond);
+
+        manager.start();
+
+        // should not throw an exception
     }
 
 }
