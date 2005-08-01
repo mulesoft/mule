4e01f132b6457229e22cf3e425a14bcb6487ea6a
aperepel@gmail.com <Andrew Perepelytsya>
2005-08-01 16:50:14 +0000

    Missing license terms, imports cleanup.
    git-svn-id: https://svn.codehaus.org/mule/trunk@560 bf997673-6b11-0410-b953-e057580c5b09

--- mule/src/java/org/mule/management/mbeans/ConnectorService.java
+++ mule/src/java/org/mule/management/mbeans/ConnectorService.java
@@ -1,17 +1,29 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
 package org.mule.management.mbeans;
 
-import org.mule.umo.MessagingException;
 import org.mule.umo.UMOException;
 import org.mule.umo.lifecycle.InitialisationException;
 import org.mule.umo.lifecycle.RecoverableException;
 import org.mule.umo.provider.UMOConnector;
-import org.mule.umo.provider.UMOMessageAdapter;
-import org.mule.umo.provider.UMOMessageDispatcher;
 import org.mule.umo.provider.UMOMessageDispatcherFactory;
 
 import java.beans.ExceptionListener;
 
 /**
+ * @author <a href="mailto:aperepel@itci.com">Andrew Perepelytsya</a>
+ * 
  * $Id$
  */
 public class ConnectorService implements ConnectorServiceMBean
--- mule/src/java/org/mule/management/mbeans/ConnectorServiceMBean.java
+++ mule/src/java/org/mule/management/mbeans/ConnectorServiceMBean.java
@@ -1,3 +1,16 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
 package org.mule.management.mbeans;
 
 import org.mule.umo.UMOException;
@@ -8,6 +21,8 @@ import org.mule.umo.provider.UMOMessageDispatcherFactory;
 import java.beans.ExceptionListener;
 
 /**
+ * @author <a href="mailto:aperepel@itci.com">Andrew Perepelytsya</a>
+ * 
  * $Id$
  */
 public interface ConnectorServiceMBean
