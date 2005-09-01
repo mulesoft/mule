d44224aba1b337b0fa97ed95582768549b699e0d
Ross Mason <ross@mulesoft.com>
2005-09-01 18:13:44 +0000

    Added Bridge compoennt for bridging from one endpoint to another with the need for having a component inbetween
    git-svn-id: https://svn.codehaus.org/mule/trunk@609 bf997673-6b11-0410-b953-e057580c5b09

---
+++ mule/src/java/org/mule/components/simple/BridgeComponent.java
@@ -0,0 +1,44 @@
+/* 
+* $Header$
+* $Revision$
+* $Date$
+* ------------------------------------------------------------------------------------------------------
+* 
+* Copyright (c) SymphonySoft Limited. All rights reserved.
+* http://www.symphonysoft.com
+* 
+* The software in this package is published under the terms of the BSD
+* style license a copy of which has been included with this distribution in
+* the LICENSE.txt file. 
+*
+*/
+package org.mule.components.simple;
+
+import org.mule.impl.UMODescriptorAware;
+import org.mule.routing.inbound.ForwardingConsumer;
+import org.mule.umo.UMODescriptor;
+import org.mule.umo.UMOEventContext;
+import org.mule.umo.lifecycle.Callable;
+
+/**
+ * Han be used to bridge inbound requests to an outbound router without any processing
+ * done inbetween.
+ *
+ * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
+ * @version $Revision$
+ */
+public class BridgeComponent implements UMODescriptorAware, Callable {
+
+    public void setDescriptor(UMODescriptor descriptor) {
+        //Adding a forwarding consumer will cause the inbound routing to
+        //directly invoke the outbound router, bypassing the component
+        descriptor.getInboundRouter().addRouter(new ForwardingConsumer());
+        //Make sure if other routers on the inbound router are honoured
+        descriptor.getInboundRouter().setMatchAll(true);
+    }
+
+    public Object onCall(UMOEventContext context) throws Exception {
+        throw new UnsupportedOperationException("A bridge should not ever receive an event an  event instead the event should be directly dispatched from the inbound endpoint to outbound router. Component is: "
+                + context.getComponentDescriptor().getName());
+    }
+}
