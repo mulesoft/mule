460ea5f4e7cd1ff3eceeb4ffda4a0e2cca339090
lajos <lajos>
2005-09-26 15:35:58 +0000

    Perhaps redundant, but nonetheless useful for some situations. PassThroughComponent just returns the transformed message like EchoComponent but without the logging.
    git-svn-id: https://svn.codehaus.org/mule/trunk@721 bf997673-6b11-0410-b953-e057580c5b09

---
+++ mule/src/java/org/mule/components/simple/PassThroughComponent.java
@@ -0,0 +1,33 @@
+/*
+* $Header$
+* $Revision$
+* $Date$
+* ------------------------------------------------------------------------------------------------------
+* 
+ * Copyright (c) Lajos Moczar. All rights reserved.
+ * http://www.galatea.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.mule.components.simple;
+
+import org.mule.umo.UMOEventContext;
+import org.mule.umo.lifecycle.Callable;
+
+/**
+ * <code>PassThroughComponent</code> will simply return the payload back
+ * as the result
+ * 
+ * @author <a href="mailto:lajos@galatea.com">Lajos Moczar</a>
+ * @version $Revision$
+ */
+public class PassThroughComponent implements Callable
+{
+    public Object onCall(UMOEventContext context) throws Exception
+    {
+        return context.getTransformedMessageAsString();
+    }
+
+}
