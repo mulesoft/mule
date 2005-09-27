089d836447d0b8a3f6738a9eb7f8b724ed6176ca
holger <holger>
2005-09-27 13:40:51 +0000

    migration classes for (CountDown)Latch
    git-svn-id: https://svn.codehaus.org/mule/trunk@741 bf997673-6b11-0410-b953-e057580c5b09

---
+++ mule/src/java/org/mule/util/concurrent/CountDownLatch.java
@@ -0,0 +1,71 @@
+/* 
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ * 
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ * 
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file. 
+ *
+ */
+package org.mule.util.concurrent;
+
+import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
+import edu.emory.mathcs.backport.java.util.concurrent.locks.Condition;
+import edu.emory.mathcs.backport.java.util.concurrent.locks.Lock;
+
+/**
+ * @author Holger Hoffstaette
+ */
+
+public class CountDownLatch extends edu.emory.mathcs.backport.java.util.concurrent.CountDownLatch
+	implements Lock
+{
+
+	public CountDownLatch(int count)
+	{
+		super(count);
+	}
+
+	public void lock()
+	{
+		try
+		{
+			super.await();
+		}
+		catch (InterruptedException ex)
+		{
+			// ignore
+		}
+	}
+
+	public void lockInterruptibly() throws InterruptedException
+	{
+		super.await();
+	}
+
+	public boolean tryLock()
+	{
+		return (super.getCount() == 0);
+	}
+
+	public boolean tryLock(long time, TimeUnit unit) throws InterruptedException
+	{
+		return super.await(time, unit);
+	}
+
+	public void unlock()
+	{
+		super.countDown();
+	}
+
+	public Condition newCondition()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+}
