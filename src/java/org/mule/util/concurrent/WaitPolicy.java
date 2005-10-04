67f2238e00485ee1e5c270fec6eafd77f5129343
holger <holger>
2005-10-04 14:20:20 +0000

    replacement policy for PooledExecutor.waitWhenBlocked()
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@775 bf997673-6b11-0410-b953-e057580c5b09

---
+++ src/java/org/mule/util/concurrent/WaitPolicy.java
@@ -0,0 +1,58 @@
+/* 
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ * 
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ * 
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file. 
+ *
+ */
+package org.mule.util.concurrent;
+
+import edu.emory.mathcs.backport.java.util.concurrent.RejectedExecutionException;
+import edu.emory.mathcs.backport.java.util.concurrent.RejectedExecutionHandler;
+import edu.emory.mathcs.backport.java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author Holger Hoffstaette
+ */
+
+/**
+ * A handler for unexecutable tasks that waits until the task can be
+ * submitted for execution.
+ * 
+ * Generously snipped from the jsr166 repository at:
+ * http://gee.cs.oswego.edu/cgi-bin/viewcvs.cgi/jsr166/src/main/java/util/concurrent/ThreadPoolExecutor.java
+ * 
+ */
+public class WaitPolicy implements RejectedExecutionHandler
+{
+	/**
+	 * Constructs a <tt>WaitPolicy</tt>.
+	 */
+	public WaitPolicy()
+	{
+	}
+
+	public void rejectedExecution(Runnable r, ThreadPoolExecutor e)
+	{
+		if (!e.isShutdown())
+		{
+			try
+			{
+				e.getQueue().put(r);
+			}
+			catch (InterruptedException ie)
+			{
+				Thread.currentThread().interrupt();
+				throw new RejectedExecutionException(ie);
+			}
+		}
+	}
+}
+
