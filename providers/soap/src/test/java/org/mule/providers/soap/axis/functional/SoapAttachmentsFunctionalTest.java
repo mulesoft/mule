1f81b9ad6cdcda2fd3e0dac229d6341acff6839c
rsears <rsears>
2005-10-11 14:41:57 +0000

    Adding test case for streaming Soap message with attachments support. Updated soap project file to add dependency for javamail for executing Soap attachments test.
    git-svn-id: https://svn.codehaus.org/mule/trunk@839 bf997673-6b11-0410-b953-e057580c5b09

---
+++ providers/soap/src/test/java/org/mule/providers/soap/axis/functional/SoapAttachmentsFunctionalTest.java
@@ -0,0 +1,26 @@
+/*
+ * $Header$ 
+ * $Revision$ 
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ * 
+ * Copyright (c) SymphonySoft Limited. All rights reserved. http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD style
+ * license a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ *  
+ */
+
+package org.mule.providers.soap.axis.functional;
+
+import javax.activation.DataHandler;
+
+/**
+ * @author <a href="mailto:risears@gmail.com">Rick Sears</a>
+ * @version $Revision$
+ */
+public interface SoapAttachmentsFunctionalTest
+{
+	public String receiveMessageWithAttachments(String payload, DataHandler[] attachments);
+}
---
+++ providers/soap/src/test/java/org/mule/providers/soap/axis/functional/SoapAttachmentsFunctionalTestCase.java
@@ -0,0 +1,121 @@
+/*
+ * $Header$ 
+ * $Revision$ 
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ * 
+ * Copyright (c) SymphonySoft Limited. All rights reserved. http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD style
+ * license a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ *  
+ */
+
+package org.mule.providers.soap.axis.functional;
+
+import org.mule.MuleManager;
+import org.mule.config.PoolingProfile;
+import org.mule.impl.MuleEvent;
+import org.mule.impl.MuleMessage;
+import org.mule.impl.endpoint.MuleEndpoint;
+import org.mule.impl.endpoint.MuleEndpointURI;
+import org.mule.impl.model.seda.SedaModel;
+import org.mule.providers.soap.axis.AxisConnector;
+import org.mule.providers.soap.axis.AxisMessageDispatcher;
+import org.mule.tck.functional.AbstractProviderFunctionalTestCase;
+import org.mule.umo.UMOMessage;
+import org.mule.umo.UMOTransactionConfig;
+import org.mule.umo.endpoint.MalformedEndpointException;
+import org.mule.umo.endpoint.UMOEndpointURI;
+import org.mule.umo.provider.UMOConnector;
+
+import java.io.File;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+
+/**
+ * @author <a href="mailto:risears@gmail.com">Rick Sears</a>
+ * @version $Revision$
+ */
+public class SoapAttachmentsFunctionalTestCase extends AbstractProviderFunctionalTestCase
+{
+    protected void doSetUp() throws Exception
+    {
+        manager = MuleManager.getInstance();
+        // Make sure we are running synchronously
+        MuleManager.getConfiguration().setSynchronous(true);
+        MuleManager.getConfiguration()
+                   .getPoolingProfile()
+                   .setInitialisationPolicy(PoolingProfile.POOL_INITIALISE_ONE_COMPONENT);
+
+        manager.setModel(new SedaModel());
+        callbackCalled = false;
+        callbackCount = 0;
+        connector = createConnector();
+    }
+	
+    protected UMOEndpointURI getInDest()
+    {
+        try {
+            return new MuleEndpointURI("axis:http://localhost:60198/mule/services");
+        } catch (MalformedEndpointException e) {
+            fail(e.getMessage());
+            return null;
+        }
+    }
+
+    protected UMOEndpointURI getOutDest()
+    {
+        return getInDest();
+    }
+	
+    protected UMOConnector createConnector() throws Exception
+    {
+        AxisConnector connector = new AxisConnector();
+        connector.setName("testAxis");
+        connector.getDispatcherThreadingProfile().setDoThreading(false);
+        return connector;
+    }
+
+    public void testSend() throws Exception
+    {
+        descriptor = getTestDescriptor("testComponent", SoapAttachmentsFunctionalTestComponent.class.getName());
+
+        initialiseComponent(descriptor,
+                            UMOTransactionConfig.ACTION_NONE,
+                            UMOTransactionConfig.ACTION_NONE,
+                            null);
+        // Start the server
+        MuleManager.getInstance().start();
+
+        sendTestData(5);
+
+        afterInitialise();
+
+        receiveAndTestResults();
+    }
+
+    protected void sendTestData(int iterations) throws Exception
+    {
+    	AxisMessageDispatcher client = new AxisMessageDispatcher((AxisConnector) connector);
+    	for(int i = 0; i < iterations; i++) {
+    		UMOMessage msg = new MuleMessage("testPayload");
+    		File tempFile = File.createTempFile("test", ".att");
+    		tempFile.deleteOnExit();
+	    	msg.addAttachment("testAttachment", new DataHandler(new FileDataSource(tempFile)));
+	    	MuleEvent event = new MuleEvent(msg, new MuleEndpoint("axis:http://localhost:60198/mule/services/testComponent?method=receiveMessageWithAttachments", false), null, true);
+	    	UMOMessage result = client.send(event);
+	    	assertNotNull(result);
+	    	assertNotNull(result.getPayload());
+	    	assertEquals(result.getPayloadAsString(), "Done");
+	    	callbackCount++;
+    	}
+    }
+
+    protected void receiveAndTestResults() throws Exception
+    {
+        assertEquals(5, callbackCount);
+    }
+}
