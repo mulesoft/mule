be12a3e5bc2fd29502338f034ac1f4f1cbd76ec2
Ross Mason <ross@mulesoft.com>
2005-10-13 20:16:12 +0000

    Added Soap proxy test case
    git-svn-id: https://svn.codehaus.org/mule/trunk@849 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/integration/providers/soap/VmToAxisProxyTestCase.java
@@ -0,0 +1,38 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.mule.test.integration.providers.soap;
+
+import org.mule.tck.FunctionalTestCase;
+import org.mule.extras.client.MuleClient;
+import org.mule.umo.UMOMessage;
+
+/**
+ *
+ * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
+ * @version $Revision$
+ */
+public class VmToAxisProxyTestCase  extends FunctionalTestCase {
+
+    protected String getConfigResources() {
+        return "org/mule/test/integration/providers/soap/vm-to-axis-proxy-mule-config.xml";
+    }
+
+
+    public void testWSProxy() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        UMOMessage result = client.send("vm://proxy", "ibm", null);
+        assertNotNull(result);
+    }
+}
