cf96b7dc7d6346c98830f5a1cead5d61a46a88b3
Ross Mason <ross@mulesoft.com>
2005-10-24 10:24:27 +0000

    Set the default Imap port.  This is not strictly necessary, just in keeping with the other mail connectors
    git-svn-id: https://svn.codehaus.org/mule/trunk@926 bf997673-6b11-0410-b953-e057580c5b09

--- providers/email/src/java/org/mule/providers/email/ImapConnector.java
+++ providers/email/src/java/org/mule/providers/email/ImapConnector.java
@@ -21,6 +21,12 @@ package org.mule.providers.email;
  */
 public class ImapConnector extends Pop3Connector {
 
+    public static final int DEFAULT_IMAP_PORT = 143;
+
+    public ImapConnector() {
+        setPort(DEFAULT_IMAP_PORT);
+    }
+
     public String getProtocol()
     {
         return "imap";
