04eab50e36ed744fe49e8df700855808bc700f9e
Ross Mason <ross@mulesoft.com>
2005-10-26 17:00:33 +0000

    Entropy in the Glue functional test fixed
    git-svn-id: https://svn.codehaus.org/mule/trunk@960 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/providers/soap/GlueConnectorHttpFunctionalTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/providers/soap/GlueConnectorHttpFunctionalTestCase.java
@@ -35,4 +35,43 @@ public class GlueConnectorHttpFunctionalTestCase extends AxisConnectorHttpFuncti
     public String getConfigResources() {
         return "org/mule/test/integration/providers/soap/glue-http-mule-config.xml";
     }
+
+    protected String getComponentWithoutInterfacesEndpoint() {
+        return "glue:http://localhost:38011/mule/test";
+    }
+    protected String getRequestResponseEndpoint() {
+        return "glue:http://localhost:38008/mule/mycomponent?method=echo";
+    }
+
+    protected String getReceiveEndpoint() {
+        return "glue:http://localhost:38009/mule/services/mycomponent2?method=getDate";
+    }
+
+    protected String getReceiveComplexEndpoint() {
+        return "glue:http://localhost:38009/mycomponent3?method=getPerson&param=Fred";
+    }
+
+    protected String getSendReceiveComplexEndpoint1() {
+        return "glue:http://localhost:38009/mycomponent3?method=addPerson";
+    }
+
+    protected String getSendReceiveComplexEndpoint2() {
+        return "glue:http://localhost:38009/mycomponent3?method=getPerson&param=Ross";
+    }
+
+    protected String getReceiveComplexCollectionEndpoint() {
+        return "glue:http://localhost:38009/mycomponent3?method=getPeople";
+    }
+
+    protected String getDispatchAsyncComplexEndpoint1() {
+        return "axis:http://localhost:38010/mycomponent4?method=addPerson";
+    }
+
+    protected String getDispatchAsyncComplexEndpoint2() {
+        return "glue:http://localhost:38009/mycomponent3?method=getPerson&param=Joe";
+    }
+
+    protected String getTestExceptionEndpoint() {
+        return "glue:http://localhost:38009/mycomponent3?method=addPerson";
+    }
 }
