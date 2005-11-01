ce9c8fade656bf9f37443d241fdd9915cd9fa8ca
Ross Mason <ross@mulesoft.com>
2005-11-01 16:58:52 +0000

    fixed regression in Ejb dispatcher. Introduced methods disregarded the behaviour of the pervious version.  Need more testcases for this
    git-svn-id: https://svn.codehaus.org/mule/trunk@1015 bf997673-6b11-0410-b953-e057580c5b09

--- providers/ejb/src/java/org/mule/providers/ejb/EjbMessageDispatcher.java
+++ providers/ejb/src/java/org/mule/providers/ejb/EjbMessageDispatcher.java
@@ -16,19 +16,28 @@ package org.mule.providers.ejb;
 import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicBoolean;
 import org.mule.impl.MuleMessage;
 import org.mule.providers.AbstractMessageDispatcher;
+import org.mule.providers.rmi.RmiConnector;
 import org.mule.umo.UMOEvent;
 import org.mule.umo.UMOException;
 import org.mule.umo.UMOMessage;
 import org.mule.umo.endpoint.UMOEndpointURI;
 import org.mule.umo.lifecycle.InitialisationException;
+import org.mule.umo.provider.DispatchException;
 import org.mule.umo.transformer.TransformerException;
+import org.mule.util.ClassHelper;
+import org.mule.util.PropertiesHelper;
 
 import javax.ejb.EJBObject;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.rmi.NotBoundException;
 import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -70,14 +79,93 @@ public class EjbMessageDispatcher extends AbstractMessageDispatcher
             if (System.getSecurityManager() == null)
                 System.setSecurityManager(new RMISecurityManager());
 
-            remoteObject = EjbConnectorUtil.getRemoteObject(event.getEndpoint(), connector);
+            remoteObject = getRemoteObject(event);
 
-            invokedMethod = EjbConnectorUtil.getMethodObject(event.getEndpoint(), remoteObject, connector, this.getClass());
+            try {
+                invokedMethod = getMethodObject(event, remoteObject);
+            } catch (UMOException e) {
+                throw new InitialisationException(e, this);
+            }
 
             initialised.set(true);
         }
     }
 
+    private EJBObject getRemoteObject(UMOEvent event) throws RemoteException, UnknownHostException {
+        EJBObject remoteObj = null;
+
+        UMOEndpointURI endpointUri = event.getEndpoint().getEndpointURI();
+
+        int port = endpointUri.getPort();
+
+        if (port < 1) {
+            port = RmiConnector.DEFAULT_RMI_REGISTRY_PORT;
+        }
+
+        InetAddress inetAddress = InetAddress.getByName(endpointUri.getHost());
+
+        String serviceName = endpointUri.getPath();
+
+        try {
+            Object ref = connector.getJndiContext(inetAddress.getHostAddress() + ":" + port).lookup(serviceName);
+
+            Method method = ClassHelper.getMethod("create", ref.getClass());
+
+            remoteObj = (EJBObject) method.invoke(ref, ClassHelper.NO_ARGS);
+        } catch (Exception e) {
+            throw new RemoteException("Remote EJBObject lookup failed for '" + inetAddress.getHostAddress() + ":" + port + serviceName + "'", e);
+        }
+
+        return (remoteObj);
+    }
+
+    private Method getMethodObject(UMOEvent event, EJBObject remoteObject) throws UMOException, NoSuchMethodException, ClassNotFoundException {
+        UMOEndpointURI endpointUri = event.getEndpoint().getEndpointURI();
+
+        String methodName = PropertiesHelper.getStringProperty(endpointUri.getParams(),
+                RmiConnector.PARAM_SERVICE_METHOD,
+                null);
+
+        if (null == methodName) {
+            methodName = (String) event.getEndpoint()
+                    .getProperties()
+                    .get(RmiConnector.PARAM_SERVICE_METHOD);
+
+            if (null == methodName) {
+                throw new DispatchException(new org.mule.config.i18n.Message("ejb",
+                        RmiConnector.MSG_PARAM_SERVICE_METHOD_NOT_SET),
+                        event.getMessage(),
+                        event.getEndpoint());
+            }
+        }
+
+        // Parse method args
+        String arguments = (String) event.getEndpoint()
+                .getProperties().get(RmiConnector.PROPERTY_SERVICE_METHOD_PARAM_TYPES);
+
+        List methodArgumentTypes = new ArrayList();
+        if (null != arguments) {
+            String[] split = arguments.split(",");
+
+            for (int i = 0; i < split.length; i++) {
+                methodArgumentTypes.add(ClassHelper.loadClass(split[i].trim(), getClass()));
+
+            }
+        } else if (event.getTransformedMessage().getClass().isArray()) {
+            Object args[] = (Object[]) event.getTransformedMessage();
+            for (int i = 0; i < args.length; i++) {
+                methodArgumentTypes.add(args[i].getClass());
+            }
+        } else {
+            methodArgumentTypes.add(event.getTransformedMessage().getClass());
+        }
+
+        Class types[] = new Class[methodArgumentTypes.size()];
+        types = (Class[]) methodArgumentTypes.toArray(types);
+
+        // Returns possible method
+        return (remoteObject.getClass().getMethod(methodName, types));
+    }
 
     public UMOMessage receive(UMOEndpointURI endpointUri, long timeout) throws Exception
     {
