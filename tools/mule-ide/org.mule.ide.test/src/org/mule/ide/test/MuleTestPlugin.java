0750996ba71c187fd6aa9643a92aad871c58e1e4
dadams <dadams>
2006-01-11 20:22:23 +0000

    Initial add of JUnit tests.
    git-svn-id: https://svn.codehaus.org/mule/trunk@1246 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tools/mule-ide/org.mule.ide.test/src/org/mule/ide/test/MuleTestPlugin.java
@@ -0,0 +1,43 @@
+package org.mule.ide.test;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class MuleTestPlugin extends Plugin {
+
+	//The shared instance.
+	private static MuleTestPlugin plugin;
+	
+	/**
+	 * The constructor.
+	 */
+	public MuleTestPlugin() {
+		plugin = this;
+	}
+
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static MuleTestPlugin getDefault() {
+		return plugin;
+	}
+
+}
