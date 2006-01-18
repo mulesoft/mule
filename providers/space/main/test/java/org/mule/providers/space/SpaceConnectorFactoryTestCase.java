ff85ddd8bff55f03664283fbc6af3a82137c80db
Ross Mason <ross@mulesoft.com>
2006-01-18 22:42:00 +0000

    Added Space transport
    git-svn-id: https://svn.codehaus.org/mule/trunk@1283 bf997673-6b11-0410-b953-e057580c5b09

---
+++ providers/space/main/test/java/org/mule/providers/space/SpaceConnectorFactoryTestCase.java
@@ -0,0 +1,40 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.mule.providers.space;
+
+import org.mule.impl.endpoint.MuleEndpointURI;
+import org.mule.providers.service.ConnectorFactory;
+import org.mule.tck.NamedTestCase;
+import org.mule.umo.endpoint.UMOEndpoint;
+
+/**
+ * @version $Revision$
+ */
+
+public class SpaceConnectorFactoryTestCase extends NamedTestCase
+{
+    public void testCreateFromFactory() throws Exception
+    {
+        MuleEndpointURI url = new MuleEndpointURI(getEndpointURI());
+        UMOEndpoint endpoint = ConnectorFactory.createEndpoint(url, UMOEndpoint.ENDPOINT_TYPE_RECEIVER);
+        assertNotNull(endpoint);
+        assertNotNull(endpoint.getConnector());
+        assertTrue(endpoint.getConnector() instanceof SpaceConnector);
+        assertTrue(getEndpointURI().endsWith(endpoint.getEndpointURI().toString()));
+    }
+    
+    public String getEndpointURI() {
+    	return "space:/test.space";
+    }
+}
