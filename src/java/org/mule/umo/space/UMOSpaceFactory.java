70a34a2e0b39d3fdb562cb8a24f7ddfb5e0adf26
Ross Mason <ross@mulesoft.com>
2006-01-20 16:59:52 +0000

    Added create method with a UMOEndpoint as the endpoint can provide much more information about the space configuration. Though makes the Space API less portable
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@1297 bf997673-6b11-0410-b953-e057580c5b09

--- src/java/org/mule/umo/space/UMOSpaceFactory.java
+++ src/java/org/mule/umo/space/UMOSpaceFactory.java
@@ -13,13 +13,30 @@
  */
 package org.mule.umo.space;
 
+import org.mule.umo.endpoint.UMOEndpoint;
+
 /**
- * todo document
+ * A factory for creating Mule Space facade objects
  *
  * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
  * @version $Revision$
  */
 public interface UMOSpaceFactory {
 
+    /**
+     * Creates a space based on the endpoint URI and can use additional properties, transaction info,
+     * security info and filters associated with the endpoint
+     * @param endpoint the endpoint from which to construct the space
+     * @return an new Space object
+     * @throws UMOSpaceException
+     */
+    public UMOSpace create(UMOEndpoint endpoint) throws UMOSpaceException;
+
+    /**
+     * Creates a space based on a URI identifier for the space
+     * @param spaceIdentifier a URI from which to construct the space
+     * @return an new Space object
+     * @throws UMOSpaceException
+     */
     public UMOSpace create(String spaceIdentifier) throws UMOSpaceException;
 }
