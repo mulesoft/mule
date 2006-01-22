501a19692b7c2aabd0846d571fc02aa6c5f26e52
Ross Mason <ross@mulesoft.com>
2006-01-22 13:44:15 +0000

    NPE
    git-svn-id: https://svn.codehaus.org/mule/trunk@1316 bf997673-6b11-0410-b953-e057580c5b09

--- mule/src/java/org/mule/util/queue/FilePersistenceStrategy.java
+++ mule/src/java/org/mule/util/queue/FilePersistenceStrategy.java
@@ -135,6 +135,8 @@ public class FilePersistenceStrategy implements QueuePersistenceStrategy
     protected void restoreFiles(File dir, List msgs) throws IOException, ClassNotFoundException
     {
         File[] files = dir.listFiles();
+        if(files==null) return;
+
         for (int i = 0; i < files.length; i++) {
             if (files[i].isDirectory()) {
                 restoreFiles(files[i], msgs);
