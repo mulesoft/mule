3f643fcf0ecba8001f1be1488958fc7af261c181
dadams <dadams>
2006-02-10 23:52:50 +0000

    Initial commit of plugin that provides sample projects for Mule IDE
    git-svn-id: https://svn.codehaus.org/mule/trunk@1356 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tools/mule-ide/org.mule.ide.samples/hello/src/org/mule/samples/hello/HttpRequestToString.java
@@ -0,0 +1,61 @@
+/* 
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ * 
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ * 
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file. 
+ *
+ */
+
+package org.mule.samples.hello;
+
+import org.mule.config.i18n.Message;
+import org.mule.transformers.NoActionTransformer;
+import org.mule.transformers.AbstractTransformer;
+import org.mule.umo.transformer.TransformerException;
+
+/**
+ * <code>NameStringToChatString</code> This is test class only for use with the Hello world
+ * test application.
+ *
+ * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
+ * @version $Revision$
+ */
+public class HttpRequestToString extends AbstractTransformer
+{
+    /**
+     *
+     */
+    public HttpRequestToString()
+    {
+        super();
+        this.registerSourceType(String.class);
+        this.registerSourceType(byte[].class);
+    }
+
+    /* (non-Javadoc)
+     * @see org.mule.transformers.AbstractTransformer#doTransform(java.lang.Object)
+     */
+    public Object doTransform(Object src) throws TransformerException
+    {
+    	String param;
+    	if (src instanceof byte[]) {
+    		param = new String((byte[]) src);
+    	} else {
+        	param = src.toString();
+    	}
+        int equals = param.indexOf("=");
+        if(equals > -1)
+        {
+            return param.substring(equals + 1);
+        } else {
+            throw new TransformerException(Message.createStaticMessage("Failed to parse param string: " + param), this);
+        }
+    }
+}
---
+++ tools/mule-ide/org.mule.ide.samples/hello/src/org/mule/samples/hello/StringToNameString.java
@@ -0,0 +1,51 @@
+/* 
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ * 
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ * 
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file. 
+ *
+ */
+ 
+package org.mule.samples.hello;
+
+import org.mule.transformers.NoActionTransformer;
+import org.mule.transformers.AbstractTransformer;
+import org.mule.umo.transformer.TransformerException;
+
+/**
+ *  <code>StringToNameString</code> converts from a String to a NameString object
+ *
+ * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
+ * @version $Revision$
+ */
+public class StringToNameString extends AbstractTransformer
+{
+    /**
+     * 
+     */
+    public StringToNameString()
+    {
+        super();
+        this.registerSourceType(String.class);
+    }
+    
+
+    /* (non-Javadoc)
+     * @see org.mule.transformers.AbstractTransformer#doTransform(java.lang.Object)
+     */
+    public Object doTransform(Object src) throws TransformerException
+    {
+        String name = (String)src;
+        int i = name.indexOf("\r");
+        if(i > -1) name = name.substring(0, i);
+        return new NameString(name);
+    }
+
+}
