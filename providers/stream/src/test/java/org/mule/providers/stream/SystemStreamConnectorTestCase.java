e45566c514dbab28dc8bc35eb0fcb0f89b2dde5b
Ross Mason <ross@mulesoft.com>
2006-02-20 12:52:36 +0000

    removed mock test in favour of functional test
    git-svn-id: https://svn.codehaus.org/mule/trunk@1400 bf997673-6b11-0410-b953-e057580c5b09

--- providers/stream/src/test/java/org/mule/providers/stream/SystemStreamConnectorTestCase.java
+++ providers/stream/src/test/java/org/mule/providers/stream/SystemStreamConnectorTestCase.java
@@ -12,17 +12,8 @@
 
 package org.mule.providers.stream;
 
-import com.mockobjects.dynamic.C;
-import com.mockobjects.dynamic.Mock;
-import org.mule.tck.MuleTestUtils;
 import org.mule.tck.providers.AbstractConnectorTestCase;
-import org.mule.umo.UMOComponent;
-import org.mule.umo.UMOEvent;
-import org.mule.umo.endpoint.UMOEndpoint;
-import org.mule.umo.endpoint.UMOImmutableEndpoint;
 import org.mule.umo.provider.UMOConnector;
-
-import java.util.HashMap;
 
 /**
  * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
@@ -48,64 +39,6 @@ public class SystemStreamConnectorTestCase extends AbstractConnectorTestCase
         return "stream://System.out";
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.mule.tck.providers.AbstractConnectorTestCase#testDispatch()
-     */
-    public void testDispatch() throws Exception
-    {
-        UMOConnector connector = createConnector();
-
-        UMOEndpoint endpoint = getTestEndpoint("test", UMOImmutableEndpoint.ENDPOINT_TYPE_SENDER);
-        UMOComponent component = getTestComponent(descriptor);
-        Mock event = MuleTestUtils.getMockEvent();
-
-        // event.expectAndReturn("getMessage", new MuleMessage("Hello", null));
-        event.expect("setSynchronous", C.IS_FALSE);
-        event.expectAndReturn("isSynchronous", false);
-        event.expectAndReturn("getEndpoint", endpoint);
-        event.expect("setProperty", C.ANY_ARGS);
-        event.expectAndReturn("getProperties", new HashMap());
-        event.expectAndReturn("getEndpoint", getTestEndpoint("test", UMOEndpoint.ENDPOINT_TYPE_SENDER));
-        connector.registerListener(component, endpoint);
-        connector.startConnector();
-        connector.getDispatcher("dummy").dispatch((UMOEvent) event.proxy());
-        // running async we need to wait for everything to load
-        Thread.sleep(1000);
-        event.verify();
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.mule.tck.providers.AbstractConnectorTestCase#testSend()
-     */
-    // public void testSend() throws Exception
-    // {
-    // UMOConnector connector = createConnector();
-    //
-    // UMOComponent component = getTestComponent(descriptor);
-    // UMOEndpoint endpoint = getTestEndpoint("test",
-    // UMOImmutableEndpoint.ENDPOINT_TYPE_SENDER);
-    // Mock event = getMockEvent();
-    //
-    // event.expectAndReturn("getMessage", new MuleMessage("Hello", null));
-    //
-    // event.expectAndReturn("getTransformedMessageAsBytes",
-    // "Hello".getBytes());
-    // event.expect("setSynchronous", C.IS_TRUE);
-    // event.expectAndReturn("getEndpoint", endpoint);
-    // event.expect("setProperty", C.ANY_ARGS);
-    // System.out.println("@@@ before listener");
-    // connector.registerListener(component, endpoint);
-    // System.out.println("@@@ before start");
-    // connector.start();
-    // System.out.println("@@@ before dispatch");
-    // connector.getDispatcher("ANY").send((UMOEvent) event.proxy());
-    //
-    // event.verify();
-    // }
     public void testGetStreams() throws Exception
     {
         SystemStreamConnector connector = (SystemStreamConnector) createConnector();
