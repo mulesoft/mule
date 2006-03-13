5b3f0d1bd983832e20b0219d74c531d7f32aa257
tcarlson <tcarlson>
2006-03-13 13:45:01 +0000

    MULE-700 (org.mule.ra.MuleConnectionFactory should implement javax.resource.Referenceable) Reported by Hrishikesh Barua
    git-svn-id: https://svn.codehaus.org/mule/trunk@1607 bf997673-6b11-0410-b953-e057580c5b09

--- mule-extras/ra/src/java/org/mule/ra/MuleConnectionFactory.java
+++ mule-extras/ra/src/java/org/mule/ra/MuleConnectionFactory.java
@@ -13,7 +13,7 @@
  */
 package org.mule.ra;
 
-import javax.naming.Referenceable;
+import javax.resource.Referenceable;
 import javax.resource.ResourceException;
 import javax.resource.spi.ConnectionManager;
 
@@ -22,7 +22,7 @@ import java.io.Serializable;
 /**
  * <code>MuleConnectionFactory</code> defines the connection factory interface
  * that the RA clients will obtain a reference to.
- * 
+ *
  * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
  * @version $Revision$
  */
