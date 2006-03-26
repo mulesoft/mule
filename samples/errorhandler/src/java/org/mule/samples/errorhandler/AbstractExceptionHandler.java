a2971497fac85880cfc087d59549ae02784e8f7b
aperepel@gmail.com <Andrew Perepelytsya>
2006-03-26 06:06:30 +0000

    Replaced tabs with spaces.
    git-svn-id: https://svn.codehaus.org/mule/trunk@1684 bf997673-6b11-0410-b953-e057580c5b09

--- samples/errorhandler/src/java/org/mule/samples/errorhandler/AbstractExceptionHandler.java
+++ samples/errorhandler/src/java/org/mule/samples/errorhandler/AbstractExceptionHandler.java
@@ -29,62 +29,62 @@ import java.util.Iterator;
  * @version $Revision$
  */
 public abstract class AbstractExceptionHandler implements ExceptionHandler {
-	
-	/** logger used by this class */
-	private static transient Log logger =
-		LogFactory.getLog(AbstractExceptionHandler.class);
-	
-	protected HashMap registry = new HashMap();
+
+    /** logger used by this class */
+    private static transient Log logger =
+        LogFactory.getLog(AbstractExceptionHandler.class);
+
+    protected HashMap registry = new HashMap();
     
     private String endpointName;
-	
-	protected ErrorManager errorManager = null;
 
-	public void registerException(Class exceptionClass) {
-		
-		registry.put(exceptionClass, exceptionClass);
+    protected ErrorManager errorManager = null;
 
-	}
-	
-	public Iterator getRegisteredClasses() {
-		return registry.keySet().iterator();	
-	}
+    public void registerException(Class exceptionClass) {
 
-	public void unRegisterException(Class exceptionClass) {
-		registry.remove(exceptionClass);
+        registry.put(exceptionClass, exceptionClass);
 
-	}
+    }
 
-	public boolean isRegisteredFor(Class exceptionClass) {
-		Class aClass=null;
-		for(Iterator i = getRegisteredClasses(); i.hasNext(); ) {
-			aClass = (Class)i.next();
-			if(aClass.isAssignableFrom(exceptionClass)){
-				return true;	
-			}
-		}
-		return false;
-	}
+    public Iterator getRegisteredClasses() {
+        return registry.keySet().iterator();
+    }
 
-	public void onException(ErrorMessage message) throws HandlerException {
-		
-		Throwable t = null;
+    public void unRegisterException(Class exceptionClass) {
+        registry.remove(exceptionClass);
+
+    }
+
+    public boolean isRegisteredFor(Class exceptionClass) {
+        Class aClass=null;
+        for(Iterator i = getRegisteredClasses(); i.hasNext(); ) {
+            aClass = (Class)i.next();
+            if(aClass.isAssignableFrom(exceptionClass)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void onException(ErrorMessage message) throws HandlerException {
+
+        Throwable t = null;
 
         try {
-			t = message.getException().toException();
-		} catch (Exception e) {
-			throw new HandlerException("Failed to retreive exception from exception message: " + e, e);
-		}
-		
-		if(!isRegisteredFor(t.getClass())) {
-			throw new HandlerException("Exception: " + t.getClass().getName() + 
-			" was recieved by Exception behaviour: " + getClass().getName() + 
-			", but the exception is not registered to be handled by this behaviour");
-		}
-		processException(message, t);
-	}
-	
-	protected abstract void processException(ErrorMessage message, Throwable t) throws HandlerException;
+            t = message.getException().toException();
+        } catch (Exception e) {
+            throw new HandlerException("Failed to retreive exception from exception message: " + e, e);
+        }
+
+        if(!isRegisteredFor(t.getClass())) {
+            throw new HandlerException("Exception: " + t.getClass().getName() +
+            " was recieved by Exception behaviour: " + getClass().getName() +
+            ", but the exception is not registered to be handled by this behaviour");
+        }
+        processException(message, t);
+    }
+
+    protected abstract void processException(ErrorMessage message, Throwable t) throws HandlerException;
 
     /**
      * @return Returns the errorManager.
