7a688aa994ae9dd98060c6ba617b23d608010fbb
tcarlson <tcarlson>
2006-03-27 20:31:46 +0000

    Propagating changes from $MULE/bin
    git-svn-id: https://svn.codehaus.org/mule/trunk@1703 bf997673-6b11-0410-b953-e057580c5b09

--- resources/bin/mule.bat
+++ resources/bin/mule.bat
@@ -13,7 +13,7 @@ REM      http://www.apache.org/licenses/LICENSE-2.0
 REM
 REM  Unless required by applicable law or agreed to in writing, software
 REM  distributed under the License is distributed on an "AS IS" BASIS,
-REM  WITHOUT WARRMULEIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 REM  See the License for the specific language governing permissions and
 REM  limitations under the License.
 
