a36880566a97b4aa57cf6eff332d0d8de641ec5c
aperepel@gmail.com <Andrew Perepelytsya>
2006-04-02 19:20:45 +0000

    Replaced tabs with spaces.
    git-svn-id: https://svn.codehaus.org/mule/trunk@1714 bf997673-6b11-0410-b953-e057580c5b09

--- providers/oaq/src/test/java/org/mule/providers/oracle/jms/OracleJmsConnectorTestCase.java
+++ providers/oaq/src/test/java/org/mule/providers/oracle/jms/OracleJmsConnectorTestCase.java
@@ -29,63 +29,63 @@ import java.util.Enumeration;
  * @author <a href="mailto:carlson@hotpop.com">Travis Carlson </a>
  */
 public class OracleJmsConnectorTestCase extends AbstractConnectorTestCase {
-	
-	private OracleJmsConnector connector;
 
-	public UMOConnector getConnector() throws Exception {
-		if (connector == null) {
-			connector = new OracleJmsConnector();
-			connector.setName("TestConnector");
-			connector.setUrl("jdbc:oracle:oci:@TEST_DB");
-			connector.setUsername("scott");
-			connector.setPassword("tiger");
+    private OracleJmsConnector connector;
 
-			Mock connectionFactory = new Mock(ConnectionFactory.class);
-			Mock connection = new Mock(Connection.class);
-			connectionFactory.expectAndReturn("createConnection", connection.proxy());
-			connection.expect("close");
-			connection.expect("start");
-			connection.expect("stop");
-			connection.expect("stop");
-			connection.expect("setClientID", "mule.TestConnector");
-			connector.setConnectionFactory((ConnectionFactory) connectionFactory.proxy());
-			connector.initialise();
-		}
-		return connector;
-	}
+    public UMOConnector getConnector() throws Exception {
+        if (connector == null) {
+            connector = new OracleJmsConnector();
+            connector.setName("TestConnector");
+            connector.setUrl("jdbc:oracle:oci:@TEST_DB");
+            connector.setUsername("scott");
+            connector.setPassword("tiger");
 
-	public String getTestEndpointURI() {
-		return "jms://TEST_QUEUE";
-	}
+            Mock connectionFactory = new Mock(ConnectionFactory.class);
+            Mock connection = new Mock(Connection.class);
+            connectionFactory.expectAndReturn("createConnection", connection.proxy());
+            connection.expect("close");
+            connection.expect("start");
+            connection.expect("stop");
+            connection.expect("stop");
+            connection.expect("setClientID", "mule.TestConnector");
+            connector.setConnectionFactory((ConnectionFactory) connectionFactory.proxy());
+            connector.initialise();
+        }
+        return connector;
+    }
 
-	public Object getValidMessage() throws Exception {
-		return getMessage();
-	}
+    public String getTestEndpointURI() {
+        return "jms://TEST_QUEUE";
+    }
 
-	public static Object getMessage() throws Exception {
-		Mock message = new Mock(TextMessage.class);
-		message.expectAndReturn("getText", "Test JMS Message");
-		message.expectAndReturn("getText", "Test JMS Message");
-		message.expectAndReturn("getJMSCorrelationID", null);
-		message.expectAndReturn("getJMSMessageID", "1234567890");
-		message.expectAndReturn("getJMSDeliveryMode", new Integer(1));
-		message.expectAndReturn("getJMSDestination", null);
-		message.expectAndReturn("getJMSPriority", new Integer(4));
-		message.expectAndReturn("getJMSRedelivered", Boolean.FALSE);
-		message.expectAndReturn("getJMSReplyTo", null);
-		message.expectAndReturn("getJMSExpiration", new Long(0));
-		message.expectAndReturn("getJMSTimestamp", new Long(0));
-		message.expectAndReturn("getJMSType", null);
-		message.expectAndReturn("getPropertyNames", new Enumeration() {
+    public Object getValidMessage() throws Exception {
+        return getMessage();
+    }
 
-			public boolean hasMoreElements() {
-				return false;
-			}
+    public static Object getMessage() throws Exception {
+        Mock message = new Mock(TextMessage.class);
+        message.expectAndReturn("getText", "Test JMS Message");
+        message.expectAndReturn("getText", "Test JMS Message");
+        message.expectAndReturn("getJMSCorrelationID", null);
+        message.expectAndReturn("getJMSMessageID", "1234567890");
+        message.expectAndReturn("getJMSDeliveryMode", new Integer(1));
+        message.expectAndReturn("getJMSDestination", null);
+        message.expectAndReturn("getJMSPriority", new Integer(4));
+        message.expectAndReturn("getJMSRedelivered", Boolean.FALSE);
+        message.expectAndReturn("getJMSReplyTo", null);
+        message.expectAndReturn("getJMSExpiration", new Long(0));
+        message.expectAndReturn("getJMSTimestamp", new Long(0));
+        message.expectAndReturn("getJMSType", null);
+        message.expectAndReturn("getPropertyNames", new Enumeration() {
 
-			public Object nextElement() {
-				return null;
-			}
-		});
-		return message.proxy();
-	}
+            public boolean hasMoreElements() {
+                return false;
+            }
+
+            public Object nextElement() {
+                return null;
+            }
+        });
+        return message.proxy();
+    }
 }
