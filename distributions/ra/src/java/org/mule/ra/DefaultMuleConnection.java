e3f2646bdffa52e334750bd0b234c78edac48699
tcarlson <tcarlson>
2006-04-03 22:30:57 +0000

    Propagating changes from mule-extras/ra
    git-svn-id: https://svn.codehaus.org/mule/trunk@1719 bf997673-6b11-0410-b953-e057580c5b09

--- distributions/ra/src/java/org/mule/ra/DefaultMuleConnection.java
+++ distributions/ra/src/java/org/mule/ra/DefaultMuleConnection.java
@@ -34,6 +34,7 @@ import org.mule.umo.provider.DispatchException;
 import org.mule.umo.provider.ReceiveException;
 
 import javax.resource.ResourceException;
+
 import java.util.Map;
 
 /**
@@ -102,11 +103,10 @@ public class DefaultMuleConnection implements MuleConnection
 
         UMOEndpoint endpoint = MuleEndpoint.getOrCreateEndpointForUri(muleEndpoint, UMOEndpoint.ENDPOINT_TYPE_SENDER);
         try {
-            UMOMessage message = endpoint.getConnector().getDispatcher(muleEndpoint.getAddress()).receive(muleEndpoint,
-                                                                                                          timeout);
+            UMOMessage message = endpoint.getConnector().getDispatcher(endpoint).receive(endpoint, timeout);
             return message;
         } catch (Exception e) {
-            throw new ReceiveException(muleEndpoint, timeout, e);
+            throw new ReceiveException(endpoint, timeout, e);
         }
     }
 
@@ -159,8 +159,9 @@ public class DefaultMuleConnection implements MuleConnection
      */
     public void close() throws ResourceException
     {
-        if (managedConnection == null)
+        if (managedConnection == null) {
             return; // connection is already closed
+        }
         managedConnection.removeConnection(this);
 
         // Send a close event to the App Server
