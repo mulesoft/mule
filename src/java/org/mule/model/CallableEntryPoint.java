3d5f65215871df374571f3679e1594ef59c9c8fd
Ross Mason <ross@mulesoft.com>
2006-04-06 20:55:20 +0000

    Added a Callable entry point resolver
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@1748 bf997673-6b11-0410-b953-e057580c5b09

---
+++ src/java/org/mule/model/CallableEntryPoint.java
@@ -0,0 +1,48 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.mule.model;
+
+import org.mule.umo.model.UMOEntryPoint;
+import org.mule.umo.UMOEventContext;
+import org.mule.umo.lifecycle.Callable;
+
+/**
+ * A simple Entrypoint for the callable interface
+ *
+ * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
+ * @version $Revision$
+ */
+public class CallableEntryPoint implements UMOEntryPoint
+ {
+    public Class[] getParameterTypes() {
+        return Callable.class.getMethods()[0].getParameterTypes();
+    }
+
+    public Object invoke(Object component, UMOEventContext context) throws Exception
+    {
+        if(component instanceof Callable) {
+            return ((Callable)component).onCall(context);
+        } else {
+            throw new NoSatisfiableMethodsException(component);
+        }
+    }
+
+    public boolean isVoid() {
+        return false;
+    }
+
+    public String getMethodName() {
+        return Callable.class.getMethods()[0].getName();
+    }
+}
