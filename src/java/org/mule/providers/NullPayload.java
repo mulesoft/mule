c505732a9dff75287e3cd5cacd1bd37e1a59b494
holger <holger>
2006-04-10 19:54:51 +0000

    more descriptive toString()
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@1770 bf997673-6b11-0410-b953-e057580c5b09

--- src/java/org/mule/providers/NullPayload.java
+++ src/java/org/mule/providers/NullPayload.java
@@ -11,20 +11,29 @@
  * style license a copy of which has been included with this distribution in
  * the LICENSE.txt file.
  */
+
 package org.mule.providers;
 
 import java.io.Serializable;
 
 /**
  * <code>NullPayload</code> represents a null event payload
- *
+ * 
  * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
  * @version $Revision$
  */
 
-public class NullPayload implements Serializable {
+public class NullPayload implements Serializable
+{
 
-    public boolean equals(Object obj) {
+    public boolean equals(Object obj)
+    {
         return obj instanceof NullPayload;
     }
+
+    public String toString()
+    {
+        return "{NullPayload}";
+    }
+
 }
