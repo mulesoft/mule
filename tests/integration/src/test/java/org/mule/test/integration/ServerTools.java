8d55568b0983348bf871dd42eddfba42256cf412
holger <holger>
2006-04-23 13:25:08 +0000

    don't fail silently
    git-svn-id: https://svn.codehaus.org/mule/trunk@1892 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/ServerTools.java
+++ tests/integration/src/test/java/org/mule/test/integration/ServerTools.java
@@ -11,6 +11,7 @@
  * style license a copy of which has been included with this distribution in
  * the LICENSE.txt file.
  */
+
 package org.mule.test.integration;
 
 import org.activemq.ActiveMQConnection;
@@ -31,7 +32,7 @@ import java.io.File;
 
 /**
  * Will start external test servers needed for the integration tests
- *
+ * 
  * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
  * @author <a href="mailto:gnt@codehaus.org">Guillaume Nodet</a>
  * @version $Revision$
@@ -44,31 +45,40 @@ public class ServerTools
     private static KillableWatchdog activemq;
     private static ActiveMQConnectionFactory embeddedFactory = null;
 
-    public static void launchActiveMq()  {
+    public static void launchActiveMq()
+    {
         launchActiveMq(ActiveMQConnection.DEFAULT_BROKER_URL);
     }
 
-    public static ActiveMQConnectionFactory launchEmbeddedActiveMq() throws JMSException {
+    public static ActiveMQConnectionFactory launchEmbeddedActiveMq() throws JMSException
+    {
         embeddedFactory = new ActiveMQConnectionFactory();
         embeddedFactory.setUseEmbeddedBroker(true);
         embeddedFactory.start();
         return embeddedFactory;
     }
 
-    public static void killEmbeddedActiveMq()  {
-        if(embeddedFactory != null) {
+    public static void killEmbeddedActiveMq()
+    {
+        if (embeddedFactory != null) {
             try {
                 embeddedFactory.stop();
-            } catch (JMSException e) {
-                
+            }
+            catch (JMSException e) {
+                throw new RuntimeException("Could not stop embedded ActiveMQ!", e);
             }
             embeddedFactory = null;
         }
     }
-    public static void launchActiveMq(String brokerUrl) {
+
+    public static void launchActiveMq(String brokerUrl)
+    {
         String activeMqHome = System.getProperty(ACTIVEMQ_HOME);
-        if(activeMqHome == null) {
-            throw new NullPointerException("You must set the " + ACTIVEMQ_HOME + " system property to the root path of an ActiveMq distribution (v3.0 and greater) before running these tests");
+        if (activeMqHome == null) {
+            throw new NullPointerException(
+                    "You must set the "
+                            + ACTIVEMQ_HOME
+                            + " system property to the root path of an ActiveMq distribution (v3.0 and greater) before running these tests");
         }
         Project project = new Project();
         DefaultLogger consoleLogger = new DefaultLogger();
@@ -89,39 +99,48 @@ public class ServerTools
         final JavaTask java = new JavaTask();
         java.setProject(project);
         java.setClasspath(path);
-        if(activeMqHome.indexOf("4.") > -1) {
+        if (activeMqHome.indexOf("4.") > -1) {
             java.setClassname("org.apache.activemq.broker.Main");
-        } else {
+        }
+        else {
             java.setClassname("org.activemq.broker.impl.Main");
         }
         java.setArgs(brokerUrl);
         java.setFork(true);
         java.setDir(new File(activeMqHome));
         java.addSysproperty(createVar("activemq.home", new File(activeMqHome).getAbsolutePath()));
-        java.addSysproperty(createVar("derby.system.home", new File(activeMqHome, "\\var").getAbsolutePath()));
+        java.addSysproperty(createVar("derby.system.home", new File(activeMqHome, "\\var")
+                .getAbsolutePath()));
         java.createWatchdog();
         new Thread() {
-            public void run() {
+            public void run()
+            {
                 java.execute();
             }
         }.start();
         activemq = java.watchDog;
     }
 
-    public static void killActiveMq() {
-        if (activemq != null ) {
+    public static void killActiveMq()
+    {
+        if (activemq != null) {
             activemq.kill();
         }
     }
 
-    static class JavaTask extends Java {
+    static class JavaTask extends Java
+    {
         public KillableWatchdog watchDog;
         private Long timeout = new Long(Long.MAX_VALUE);
-        public void setTimeout(Long value) {
+
+        public void setTimeout(Long value)
+        {
             this.timeout = value;
             super.setTimeout(value);
         }
-        protected ExecuteWatchdog createWatchdog() throws BuildException {
+
+        protected ExecuteWatchdog createWatchdog() throws BuildException
+        {
             if (watchDog == null) {
                 watchDog = new KillableWatchdog(timeout != null ? timeout.longValue() : 0);
             }
@@ -130,24 +149,35 @@ public class ServerTools
 
     }
 
-    static class KillableWatchdog extends ExecuteWatchdog {
-        public KillableWatchdog(long timeout) {
+    static class KillableWatchdog extends ExecuteWatchdog
+    {
+        public KillableWatchdog(long timeout)
+        {
             super(timeout);
         }
-        public void timeoutOccured(Watchdog w) {
+
+        public void timeoutOccured(Watchdog w)
+        {
+            // ignore
         }
-        public synchronized void start(Process process) {
+
+        public synchronized void start(Process process)
+        {
             super.start(process);
         }
-        public void kill() {
+
+        public void kill()
+        {
             super.timeoutOccured(null);
         }
     }
 
-    static Environment.Variable createVar(String name, String value) {
+    static Environment.Variable createVar(String name, String value)
+    {
         Environment.Variable var = new Environment.Variable();
         var.setKey(name);
         var.setValue(value);
         return var;
     }
+
 }
