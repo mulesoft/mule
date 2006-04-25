6c9953ee6afa8e2aed3eb5a67281861a6928b395
holger <holger>
2006-04-25 10:49:30 +0000

    hopefully fixing MULE-785 - please review
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@1911 bf997673-6b11-0410-b953-e057580c5b09

--- src/java/org/mule/providers/SimpleRetryConnectionStrategy.java
+++ src/java/org/mule/providers/SimpleRetryConnectionStrategy.java
@@ -14,6 +14,8 @@
  */
 package org.mule.providers;
 
+import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicInteger;
+
 import org.mule.config.ExceptionHelper;
 import org.mule.config.i18n.Message;
 import org.mule.config.i18n.Messages;
@@ -31,15 +33,23 @@ import org.mule.umo.provider.UMOConnectable;
 public class SimpleRetryConnectionStrategy extends AbstractConnectionStrategy
 {
 
+    private static ThreadLocal count = new ThreadLocal()
+    {
+        protected Object initialValue()
+        {
+            return new AtomicInteger(0);
+        }
+    };
+
     private int retryCount = 2;
     private long frequency = 2000;
-    private int count = 0;
 
     public void doConnect(UMOConnectable connectable) throws FatalConnectException
     {
         while (true) {
+            int currentCount = ((AtomicInteger)count.get()).incrementAndGet();
+
             try {
-                count++;
                 connectable.connect();
                 if (logger.isDebugEnabled()) {
                 	logger.debug("Successfully connected to " + getDescription(connectable));
@@ -51,21 +61,24 @@ public class SimpleRetryConnectionStrategy extends AbstractConnectionStrategy
                                                                 getClass().getName(),
                                                                 getDescription(connectable)), ie, connectable);
             } catch (Exception e) {
-                if (count == retryCount) {
+                if (currentCount == retryCount) {
                     throw new FatalConnectException(new Message(Messages.RECONNECT_STRATEGY_X_FAILED_ENDPOINT_X,
                                                                 getClass().getName(),
                                                                 getDescription(connectable)), e, connectable);
                 }
+
                 if(logger.isErrorEnabled()) {
-                    StringBuffer msg = new StringBuffer();
+                    StringBuffer msg = new StringBuffer(512);
                     msg.append("Failed to connect/reconnect on endpoint: ").append(getDescription(connectable));
                     Throwable t = ExceptionHelper.getRootException(e);
                     msg.append(". Root Exception was: ").append(ExceptionHelper.writeException(t));
                     logger.error(msg.toString(), e);
                 }
+
                 if (logger.isInfoEnabled()) {
-                	logger.info("Waiting for " + frequency + "ms before reconnecting. Failed attempt " + count + " of " + retryCount);
+                	logger.info("Waiting for " + frequency + "ms before reconnecting. Failed attempt " + currentCount + " of " + retryCount);
                 }
+
                 try {
                     Thread.sleep(frequency);
                 } catch (InterruptedException e1) {
@@ -81,7 +94,7 @@ public class SimpleRetryConnectionStrategy extends AbstractConnectionStrategy
      * Resets any state stored in the retry strategy
      */
     public synchronized void resetState() {
-        count=0;
+        ((AtomicInteger)count.get()).set(0);
     }
 
     public int getRetryCount()
