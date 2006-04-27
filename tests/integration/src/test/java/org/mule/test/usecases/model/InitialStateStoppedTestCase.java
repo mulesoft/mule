c77f8024a183a90e29e3c3b31104224e078c8b69
Ross Mason <ross@mulesoft.com>
2006-04-27 09:29:06 +0000

    Fixed MULE-794 (swallowed exception)
    git-svn-id: https://svn.codehaus.org/mule/trunk@1938 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/usecases/model/InitialStateStoppedTestCase.java
+++ tests/integration/src/test/java/org/mule/test/usecases/model/InitialStateStoppedTestCase.java
@@ -1,11 +1,14 @@
 package org.mule.test.usecases.model;
 
 
-import org.mule.MuleManager;
 import org.mule.config.builders.MuleXmlConfigurationBuilder;
 import org.mule.providers.AbstractConnector;
 import org.mule.tck.AbstractMuleTestCase;
+import org.mule.umo.UMOComponent;
 import org.mule.umo.manager.UMOManager;
+import org.mule.umo.provider.UMOMessageReceiver;
+
+import java.util.Iterator;
 
 /**
  * Makes sure endpoints are not started if a component's initialState is set to "stopped".
@@ -13,37 +16,37 @@ import org.mule.umo.manager.UMOManager;
  * @author <a href="mailto:carlson@hotpop.com">Travis Carlson</a>
  */
 public class InitialStateStoppedTestCase extends AbstractMuleTestCase {
-	
-	private UMOManager manager;
-    
-	public void testInitialStateStopped() throws Exception {    
-        if (MuleManager.isInstanciated()) {
-            MuleManager.getInstance().dispose();
-        }
+
+    public void testInitialStateStopped() throws Exception {
 
         // "TestComponent" is initially stopped (initialState="stopped").
-		manager = new MuleXmlConfigurationBuilder().configure("org/mule/test/usecases/model/initial-state-stopped-config.xml");
-		manager.start();
-		
-		// The connector should be started, but with no listeners registered.
-		AbstractConnector connector = 
-			(AbstractConnector) manager.lookupConnector("TestConnector");
-		assertTrue(connector.isStarted());
-        assertTrue(connector.getReceivers().isEmpty());   
+        UMOManager manager = new MuleXmlConfigurationBuilder().configure("org/mule/test/usecases/model/initial-state-stopped-config.xml");
+        UMOComponent c = manager.getModel().getComponent("TestComponent");
+
+        assertNotNull(c);
+        assertFalse(c.isStarted());
+        manager.start();
+
+        assertFalse(c.isStarted());
+
+        // The connector should be started, but with no listeners registered.
+        AbstractConnector connector =
+            (AbstractConnector) manager.lookupConnector("TestConnector");
+        assertTrue(connector.isStarted());
+        assertTrue(connector.getReceivers().isEmpty());
 
         // Start the component.
         manager.getModel().startComponent("TestComponent");
 
-		// The listeners should now be registered and started.
-		assertTrue(connector.isStarted());
-        assertFalse(connector.getReceivers().isEmpty());   
+        // The listeners should now be registered and started.
+        assertTrue(connector.isStarted());
+        assertFalse(connector.getReceivers().isEmpty());
 
-        // TODO The receivers should all be connected now, but the mock objects
-        // don't actually set the connected property.  
-//        Iterator it = connector.getReceivers().values().iterator();
-//        while (it.hasNext()) {
-//        	assertTrue(((UMOMessageReceiver) it.next()).isConnected());
-//        }        
-	}
+        assertTrue(c.isStarted());
+        Iterator it = connector.getReceivers().values().iterator();
+        while (it.hasNext()) {
+            assertTrue(((UMOMessageReceiver) it.next()).isConnected());
+        }
+    }
 
 }
