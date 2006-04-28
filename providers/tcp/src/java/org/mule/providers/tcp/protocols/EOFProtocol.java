858972eab3f08dffff2173a244f4583d7eadc3f1
Ross Mason <ross@mulesoft.com>
2006-04-28 11:04:15 +0000

    Added this protocol for client connections where the stream is closed to signify there is no more data to read.
    git-svn-id: https://svn.codehaus.org/mule/trunk@1963 bf997673-6b11-0410-b953-e057580c5b09

---
+++ providers/tcp/src/java/org/mule/providers/tcp/protocols/EOFProtocol.java
@@ -0,0 +1,77 @@
+/*
+ * $Header: $
+ * $Revision: $
+ * $Date: $
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.mule.providers.tcp.protocols;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mule.providers.tcp.TcpProtocol;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+
+/**
+ * The EOFProtocol class is an application level tcp protocol that does
+ * nothing. Reading is terminated by the stream being closed by the client
+ *
+ * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
+ * @version $Revision: 1767 $
+ */
+public class EOFProtocol implements TcpProtocol
+{
+
+    private static final int BUFFER_SIZE = 8192;
+
+    private static final Log logger = LogFactory.getLog(EOFProtocol.class);
+
+    public byte[] read(InputStream is) throws IOException
+    {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(EOFProtocol.BUFFER_SIZE);
+
+        byte[] buffer = new byte[EOFProtocol.BUFFER_SIZE];
+        int len = 0;
+        try {
+            while ((len = is.read(buffer)) == 0) {
+                // wait
+            }
+        } catch (SocketException e) {
+            // do not pollute the log with a stacktrace, log only the message
+            EOFProtocol.logger.debug("Socket exception occured: " + e.getMessage());
+            return null;
+        } catch (SocketTimeoutException e) {
+            EOFProtocol.logger.debug("Socket timeout, returning null.");
+            return null;
+        }
+        if (len == -1) {
+            return null;
+        } else {
+            do {
+                baos.write(buffer, 0, len);
+            } while ((len = is.read(buffer)) >= 0);
+
+            baos.flush();
+            baos.close();
+            return baos.toByteArray();
+        }
+    }
+
+    public void write(OutputStream os, byte[] data) throws IOException
+    {
+        os.write(data);
+    }
+
+}
