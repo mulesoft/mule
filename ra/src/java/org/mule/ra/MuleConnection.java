073439aaa44e850e02bd330ae3e0c8ed29494bfd
holger <holger>
2006-04-29 16:03:08 +0000

    merge changes from mule-extras/ra (deployment in BEA Weblogic)
    git-svn-id: https://svn.codehaus.org/mule/trunk@1992 bf997673-6b11-0410-b953-e057580c5b09

---
+++ ra/src/java/org/mule/ra/MuleConnection.java
@@ -0,0 +1,41 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.mule.ra;
+
+import org.mule.umo.UMOException;
+import org.mule.umo.UMOMessage;
+
+import javax.resource.ResourceException;
+
+import java.util.Map;
+
+/**
+ * <code>MuleConnection</code> defines the client connection methods for the
+ * JCA cci contract
+ * 
+ * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
+ * @version $Revision$
+ */
+public interface MuleConnection
+{
+    void dispatch(String url, Object payload, Map messageProperties) throws UMOException;
+
+    UMOMessage receive(String url, long timeout) throws UMOException;
+
+    MuleManagedConnection getManagedConnection();
+
+    void close() throws ResourceException;
+
+    void associateConnection(MuleManagedConnection newMc) throws ResourceException;
+}
