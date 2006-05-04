2d0c972b508ac57751beeaba86a75076b1916355
jmoller <jmoller>
2006-05-04 23:29:58 +0000

    Tiny enhancement, allow the use of XSLT document templates as "data" elements right in the Mule XML, like for scripts and StaticComponent
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2036 bf997673-6b11-0410-b953-e057580c5b09

---
+++ src/test/java/org/mule/test/transformers/InlineXsltTransformerTestCase.java
@@ -0,0 +1,71 @@
+/*
+ * $Header$
+ * $Revision: 1983 $
+ * $Date: 2006-04-29 18:35:25 +0200 (lÃ¸, 29 apr 2006) $
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.mule.test.transformers;
+
+import org.mule.transformers.xml.XsltTransformer;
+import org.mule.umo.transformer.UMOTransformer;
+import org.mule.util.Utility;
+
+/**
+ * @author <a href="mailto:jesper@selskabet.org">Jesper S. Møller</a>
+ * @version $Revision: 1983 $
+ */
+public class InlineXsltTransformerTestCase extends AbstractXmlTransformerTestCase
+{
+
+    private String srcData;
+    private String resultData;
+
+    protected void doSetUp() throws Exception
+    {
+        srcData = Utility.loadResourceAsString("simple.xml", getClass());
+        resultData = Utility.loadResourceAsString("simple-out.xml", getClass());
+    }
+
+    public UMOTransformer getTransformer() throws Exception
+    {
+        XsltTransformer transformer = new XsltTransformer();
+        transformer.setXslt("<?xml version='1.0'?>\n" +
+            "<xsl:stylesheet version='1.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform'>\n"+
+            "<xsl:output method='xml'/>\n"+
+            "<xsl:template match='/'>\n"+
+            "  <some-xml>\n"+
+            "    <xsl:copy-of select='.'/>\n"+
+            "  </some-xml>\n"+
+            "</xsl:template>\n"+
+            "</xsl:stylesheet>");
+        transformer.initialise();
+        return transformer;
+    }
+
+    public UMOTransformer getRoundTripTransformer() throws Exception
+    {
+        return null;
+    }
+
+    public void testRoundtripTransform() throws Exception
+    {
+        // disable this test
+    }
+
+    public Object getTestData()
+    {
+        return srcData;
+    }
+
+    public Object getResultData()
+    {
+        return resultData;
+    }
+}
