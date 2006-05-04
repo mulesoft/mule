e589fe47ca74f1794fe19bc0efa303bb5826a205
aperepel@gmail.com <Andrew Perepelytsya>
2006-05-04 21:03:18 +0000

    Fixed MULE-816 SystemStreamConnector hangs when disposing
    git-svn-id: https://svn.codehaus.org/mule/trunk@2033 bf997673-6b11-0410-b953-e057580c5b09

--- providers/stream/src/java/org/mule/providers/stream/SystemStreamConnector.java
+++ providers/stream/src/java/org/mule/providers/stream/SystemStreamConnector.java
@@ -3,13 +3,13 @@
  * $Revision$
  * $Date$
  * ------------------------------------------------------------------------------------------------------
- * 
+ *
  * Copyright (c) SymphonySoft Limited. All rights reserved.
  * http://www.symphonysoft.com
- * 
+ *
  * The software in this package is published under the terms of the BSD
  * style license a copy of which has been included with this distribution in
- * the LICENSE.txt file. 
+ * the LICENSE.txt file.
  *
  */
 package org.mule.providers.stream;
@@ -26,7 +26,7 @@ import java.io.OutputStream;
 /**
  * <code>SystemStreamConnector</code> Connects to the System streams in and out by default and add some basic fuctionality for
  * Writing out prompt messages
- * 
+ *
  * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
  * @version $Revision$
  */
@@ -49,7 +49,7 @@ public class SystemStreamConnector extends StreamConnector
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see org.mule.providers.stream.StreamConnector#getInputStream()
      */
     public InputStream getInputStream()
@@ -59,7 +59,7 @@ public class SystemStreamConnector extends StreamConnector
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see org.mule.providers.AbstractConnector#doStart()
      */
     public void doStart()
@@ -69,7 +69,7 @@ public class SystemStreamConnector extends StreamConnector
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see org.mule.providers.stream.StreamConnector#getOutputStream()
      */
     public OutputStream getOutputStream()
@@ -95,7 +95,7 @@ public class SystemStreamConnector extends StreamConnector
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see org.mule.umo.provider.UMOMessageDispatcher#getConnector()
      */
     public UMOConnector getConnector()
@@ -105,7 +105,7 @@ public class SystemStreamConnector extends StreamConnector
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see org.mule.umo.provider.UMOMessageDispatcher#getDelegateSession()
      */
     public Object getDelegateSession() throws UMOException
@@ -133,5 +133,25 @@ public class SystemStreamConnector extends StreamConnector
 
     public void setMessageDelayTime(long messageDelayTime) {
         this.messageDelayTime = messageDelayTime;
+    }
+
+
+    protected void doDispose () {
+        // Override as a no-op.
+        // The reason is System.in/out shouldn't be closed.
+        // It is valid for them to remain open (consider, e.g. tail -F).
+        // Trying to close System.in will result in I/O block, and
+        // available() will always return 0 bytes for System.in.
+
+        // There is a scheme to get a ref to System.in via NIO,
+        // e.g. :
+        // FileInputStream fis = new FileInputStream(FileDescriptor.in);
+        // InputStream is = Channels.newInputStream(fis.getChannel);
+        //
+        // It is then possible to register a watchdog thread for the caller
+        // which will interrupt this (now wrapped with NIO) read() call.
+
+        // Well, it isn't absolutely required for the reasons stated above,
+        // just following the KISS principle.
     }
 }
