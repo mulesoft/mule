ce52d8ca48bf553140afea089f34adb0f192d520
holger <holger>
2006-05-22 12:12:11 +0000

    re-add some files from ross' last commit
    git-svn-id: https://svn.codehaus.org/mule/trunk@2074 bf997673-6b11-0410-b953-e057580c5b09

---
+++ mule/src/java/org/mule/routing/outbound/MessageChunkingRouter.java
@@ -0,0 +1,100 @@
+/*
+ * $Header: $
+ * $Revision: $
+ * $Date: $
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.mule.routing.outbound;
+
+import org.mule.config.i18n.Message;
+import org.mule.config.i18n.Messages;
+import org.mule.impl.MuleMessage;
+import org.mule.umo.UMOMessage;
+import org.mule.umo.UMOSession;
+import org.mule.umo.routing.RoutingException;
+
+/**
+ * A router that breaks up the current message onto smaller parts and sends them
+ * to the same destination. The Destination component needs to have a
+ * MessageChunkingAggregator inbound router in order to rebuild the message at the other end.
+ *
+ * @see org.mule.routing.inbound.MessageChunkingAggregator
+ * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
+ * @version $Revision$
+ */
+public class MessageChunkingRouter extends FilteringOutboundRouter
+{
+    private int chunkSize = 0;
+    private int numberOfMessages = 1;
+
+    public int getChunkSize() {
+        return chunkSize;
+    }
+
+    public void setChunkSize(int chunkSize) {
+        this.chunkSize = chunkSize;
+    }
+
+    public int getNumberOfMessages() {
+        return numberOfMessages;
+    }
+
+    public void setNumberOfMessages(int numberOfMessages) {
+        this.numberOfMessages = numberOfMessages;
+    }
+
+    public UMOMessage route(UMOMessage message, UMOSession session, boolean synchronous) throws RoutingException {
+        if (chunkSize == 0 && numberOfMessages < 2) {
+            return super.route(message, session, synchronous);
+        } else if (chunkSize > 0) {
+            byte[] data = new byte[0];
+            try {
+                data = message.getPayloadAsBytes();
+            } catch (Exception e) {
+                throw new RoutingException(new Message(Messages.FAILED_TO_READ_PAYLOAD), message, getEndpoint(0, message), e);
+            }
+
+            int parts = data.length / chunkSize;
+            if ((parts * chunkSize) < data.length) {
+                parts++;
+            }
+            int len = chunkSize;
+            UMOMessage part = null;
+            int count = 0;
+            int pos = 0;
+            byte[] buffer = null;
+            try {
+                for (; count < parts; count++) {
+                    if ((pos + len) > data.length) {
+                        len = data.length - pos;
+                    }
+                    buffer = new byte[len];
+                    System.arraycopy(data, pos, buffer, 0, buffer.length);
+                    pos += len;
+                    part = new MuleMessage(buffer, message);
+                    part.setCorrelationId(message.getUniqueId());
+                    part.setCorrelationGroupSize(parts);
+                    part.setCorrelationSequence(count);
+                    super.route(part, session, synchronous);
+                }
+
+            } catch (RoutingException e) {
+                //we'll want to send the whole message to the Exception handler
+                e = new RoutingException(e.getI18nMessage(), e.getUmoMessage(), e.getEndpoint(), e.getCause());
+//                e.addInfo("chunking", "true");
+//                buffer = new byte[data.length - len];
+//                System.arraycopy(data, len, buffer, 0, buffer.length);
+//                e.addInfo("remaining data", buffer);
+                throw e;
+            }
+        }
+        return message;
+    }
+}
---
+++ mule/src/java/org/mule/umo/manager/DefaultWorkListener.java
@@ -0,0 +1,64 @@
+/*
+ * $Header: $
+ * $Revision: $
+ * $Date: $
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.mule.umo.manager;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.resource.spi.work.WorkEvent;
+import javax.resource.spi.work.WorkListener;
+
+/**
+ * Default exception Handler used when executing work in the work manager
+ *
+ * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
+ * @version $Revision: $
+ */
+public class DefaultWorkListener implements WorkListener {
+
+    /**
+     * logger used by this class
+     */
+    protected transient Log logger = LogFactory.getLog(getClass());
+
+    public void workAccepted(WorkEvent event) {
+        handleWorkException(event, "workAccepted");
+    }
+
+    public void workRejected(WorkEvent event) {
+        handleWorkException(event, "workRejected");
+    }
+
+    public void workStarted(WorkEvent event) {
+        handleWorkException(event, "workStarted");
+    }
+
+    public void workCompleted(WorkEvent event) {
+        handleWorkException(event, "workCompleted");
+    }
+
+     protected void handleWorkException(WorkEvent event, String type) {
+        Throwable e = null;
+        if(event!=null && event.getException()!=null) {
+            e = event.getException();
+        } else {
+            return;
+        }
+        if(event.getException().getCause()!=null) {
+            e = (Throwable)event.getException().getCause();
+        }
+        logger.error("Work caused exception on '" + type + "'. Work being executed was: " + event.getWork().toString());
+        logger.error(e);
+    }
+}
