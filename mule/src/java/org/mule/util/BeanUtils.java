74215183f1494c49dcce85883d2375e119f3c08e
holger <holger>
2006-05-23 09:33:49 +0000

    fix (C) headers
    git-svn-id: https://svn.codehaus.org/mule/trunk@2083 bf997673-6b11-0410-b953-e057580c5b09

--- mule/src/java/org/mule/util/BeanUtils.java
+++ mule/src/java/org/mule/util/BeanUtils.java
@@ -11,6 +11,7 @@
  * style license a copy of which has been included with this distribution in
  * the LICENSE.txt file.
  */
+
 package org.mule.util;
 
 import org.apache.commons.logging.Log;
--- mule/src/java/org/mule/util/ClassHelper.java
+++ mule/src/java/org/mule/util/ClassHelper.java
@@ -1,15 +1,16 @@
-/*
- * $Header: /cvsroot/mule/mule/src/java/org/mule/util/ClassHelper.java,v
- * 1.1 2003/11/02 03:01:59 rossmason Exp $ $Revision$ $Date: 2003/11/02
- * 03:01:59 $
+/* 
+ * $Header$
+ * $Revision:$
+ * $Date:$
  * ------------------------------------------------------------------------------------------------------
  * 
- * Copyright (c) SymphonySoft Limited. All rights reserved. http://www.symphonysoft.com
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
  * 
- * The software in this package is published under the terms of the BSD style
- * license a copy of which has been included with this distribution in the
- * LICENSE.txt file.
- *  
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file. 
+ *
  */
 
 package org.mule.util;
