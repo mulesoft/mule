bd3eaed85972a02b97678beb7e0cd491a20db529
holger <holger>
2006-05-23 09:33:49 +0000

    fix (C) headers
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2083 bf997673-6b11-0410-b953-e057580c5b09

--- src/java/org/mule/util/Utility.java
+++ src/java/org/mule/util/Utility.java
@@ -1,15 +1,16 @@
-/*
- * $Header: /cvsroot/mule/mule/src/java/org/mule/util/Utility.java,v 1.13
- * 2004/01/14 09:34:02 rossmason Exp $ $Revision$ $Date: 2004/01/14
- * 09:34:02 $
+/* 
+ * $Header$
+ * $Revision$
+ * $Date$
  * ------------------------------------------------------------------------------------------------------
  * 
- * Copyright (c) SymphonySoft Limited. All rights reserved. http://www.symphonysoft.com
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
  * 
- * The software in this package is published under the terms of the BSD style
- * license a copy of which has been included with this distribution in the
- * LICENSE.txt file.
- *  
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file. 
+ *
  */
 
 package org.mule.util;
