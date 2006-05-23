80ad10e0aac3339f748e40eb1b1458eba737b8c7
holger <holger>
2006-05-23 21:18:08 +0000

    fix typo that prevents the filter from being configured in xml
    git-svn-id: https://svn.codehaus.org/mule/trunk@2087 bf997673-6b11-0410-b953-e057580c5b09

--- providers/gs/src/main/java/org/mule/providers/gs/filters/PojoTypeTemplateFilter.java
+++ providers/gs/src/main/java/org/mule/providers/gs/filters/PojoTypeTemplateFilter.java
@@ -11,48 +11,59 @@
  * style license a copy of which has been included with this distribution in
  * the LICENSE.txt file.
  */
+
 package org.mule.providers.gs.filters;
 
 import net.jini.core.entry.Entry;
+
 import org.mule.providers.gs.JiniMessage;
 import org.mule.umo.UMOMessage;
 
 import java.lang.reflect.InvocationTargetException;
 
 /**
- * todo document
- *
+ * TODO document
+ * 
  * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
  * @version $Revision: $
  */
-public class PojoTypeTemplateFilter implements JavaSpaceFilter {
+public class PojoTypeTemplateFilter implements JavaSpaceFilter
+{
+    private String expectedType;
 
-    private String expectType;
-
-    public Entry getEntry() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException, InstantiationException, ClassNotFoundException {
-        if(expectType==null) {
+    public Entry getEntry() throws IllegalAccessException, NoSuchMethodException,
+            InvocationTargetException, InstantiationException, ClassNotFoundException
+    {
+        if (expectedType == null)
+        {
             return null;
         }
+
         JiniMessage entry = new JiniMessage();
-        entry.setPayloadType(expectType);
+        entry.setPayloadType(expectedType);
         return entry;
     }
 
     /**
      * Check a given message against this filter.
-     *
-     * @param message a non null message to filter.
+     * 
+     * @param message
+     *            a non null message to filter.
      * @return <code>true</code> if the message matches the filter
      */
-    public boolean accept(UMOMessage message) {
+    public boolean accept(UMOMessage message)
+    {
         return true;
     }
 
-    public String getExpectType() {
-        return expectType;
+    public String getExpectedType()
+    {
+        return expectedType;
     }
 
-    public void setExpectType(String expectType) {
-        this.expectType = expectType;
+    public void setExpectedType(String expectedType)
+    {
+        this.expectedType = expectedType;
     }
+
 }
