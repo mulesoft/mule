a8ae03f48b3c7f3fd157e92acb7f8b928e8cf6e0
holger <holger>
2006-05-24 18:55:54 +0000

    fixed (C) headers
    git-svn-id: https://svn.codehaus.org/mule/trunk@2095 bf997673-6b11-0410-b953-e057580c5b09

--- mule/src/java/org/mule/providers/AbstractConnector.java
+++ mule/src/java/org/mule/providers/AbstractConnector.java
@@ -1,12 +1,15 @@
-/*
- * $Header: /cvsroot/mule/mule/src/java/org/mule/providers/AbstractConnector.java,v 1.16 2003/12/11
- * 13:32:40 rossmason Exp $ $Revision$ $Date$
+/* 
+ * $Header$
+ * $Revision$
+ * $Date$
  * ------------------------------------------------------------------------------------------------------
- *
- * Copyright (c) SymphonySoft Limited. All rights reserved. http://www.symphonysoft.com
- *
- * The software in this package is published under the terms of the BSD style license a copy of
- * which has been included with this distribution in the LICENSE.txt file.
+ * 
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ * 
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file. 
  *
  */
 
