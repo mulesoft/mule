d4db74717b7bd53dbf37d89efb1d5095af730292
Ross Mason <ross@mulesoft.com>
2006-06-08 21:38:35 +0000

    restructured to M2 layout
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2225 bf997673-6b11-0410-b953-e057580c5b09

---
+++ mule-extras/jotm/src/main/java/org/mule/extras/jotm/JotmTransactionManagerFactory.java
@@ -0,0 +1,59 @@
+/*
+ * $Id: JotmTransactionManagerFactory.java 2179 2006-06-04 22:51:52Z holger $
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ *
+ */
+
+package org.mule.extras.jotm;
+
+import org.mule.umo.manager.UMOTransactionManagerFactory;
+import org.objectweb.jotm.Current;
+import org.objectweb.jotm.Jotm;
+
+import javax.transaction.TransactionManager;
+
+/**
+ * This factory retrieves the transaction manager for <a
+ * href="http://jotm.objectweb.org">JOTM </a>. If an existing JOTM instance
+ * exists (for example if running on JOnAS) it will retrieve it, else if will
+ * create a new local JOTM instance.
+ * 
+ * @author Guillaume Nodet
+ * @version $Revision: 2179 $
+ */
+public class JotmTransactionManagerFactory implements UMOTransactionManagerFactory
+{
+    private TransactionManager jotmCurrent;
+
+    public JotmTransactionManagerFactory()
+    {
+        super();
+    }
+
+    /**
+     * Retrieves the JOTM Current object that implements the TransactionManager
+     * interface.
+     * 
+     * @see org.mule.umo.manager.UMOTransactionManagerFactory#create()
+     */
+    public TransactionManager create() throws Exception
+    {
+        if (jotmCurrent == null) {
+            // check for already active JOTM instance
+            jotmCurrent = Current.getCurrent();
+            // if none found, create new local JOTM instance
+            if (jotmCurrent == null) {
+                jotmCurrent = new Jotm(true, false).getTransactionManager();
+            }
+        }
+        return jotmCurrent;
+    }
+
+}
