71ca425efb2df893745390234a194139e914f8cd
holger <holger>
2006-06-09 18:25:00 +0000

    clean copy.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2298 bf997673-6b11-0410-b953-e057580c5b09

---
+++ ra/src/java/org/mule/ra/MuleManagedConnectionMetaData.java
@@ -0,0 +1,59 @@
+/*
+ * $Id$
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.mule.ra;
+
+import org.mule.MuleManager;
+import org.mule.config.i18n.Message;
+import org.mule.config.i18n.Messages;
+
+import javax.resource.ResourceException;
+import javax.resource.spi.ManagedConnectionMetaData;
+
+/**
+ * <code>MuleManagedConnectionMetaData</code> TODO
+ * 
+ * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
+ * @version $Revision$
+ */
+public class MuleManagedConnectionMetaData implements ManagedConnectionMetaData
+{
+    private MuleManagedConnection managedConnection;
+
+    public MuleManagedConnectionMetaData(MuleManagedConnection mc)
+    {
+        this.managedConnection = mc;
+    }
+
+    public String getEISProductName() throws ResourceException
+    {
+        return MuleManager.getConfiguration().getVendorName();
+    }
+
+    public String getEISProductVersion() throws ResourceException
+    {
+        return MuleManager.getConfiguration().getProductVersion();
+    }
+
+    // TODO
+    public int getMaxConnections() throws ResourceException
+    {
+        return 0;
+    }
+
+    public String getUserName() throws ResourceException
+    {
+        if (managedConnection.isDestroyed()) {
+            throw new IllegalStateException(new Message(Messages.X_IS_DISPOSED, managedConnection).toString());
+        }
+        return managedConnection.getUsername();
+    }
+}
