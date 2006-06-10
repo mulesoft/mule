892a788e63da23ce1ab3611a91fefff746fccaf7
holger <holger>
2006-06-10 21:58:45 +0000

    be threadafe
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2322 bf997673-6b11-0410-b953-e057580c5b09

--- mule-extras/client/src/test/java/org/mule/extras/client/TestReceiver.java
+++ mule-extras/client/src/test/java/org/mule/extras/client/TestReceiver.java
@@ -11,13 +11,15 @@
  */
 package org.mule.extras.client;
 
+import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
  * @version $Revision$
  */
 public class TestReceiver
 {
-    private int count = 0;
+    private AtomicInteger count = new AtomicInteger(0);
 
     public String receive(String message) throws Exception
     {
@@ -28,7 +30,6 @@ public class TestReceiver
 
     protected int inc()
     {
-        count++;
-        return count;
+        return count.incrementAndGet();
     }
 }
