a1e309ce9cbe34c3b6561139994a6e8225fa81a9
aperepel@gmail.com <Andrew Perepelytsya>
2006-06-15 13:42:28 +0000

    Corrected JavaDoc errors.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2359 bf997673-6b11-0410-b953-e057580c5b09

--- mule/src/main/java/org/mule/providers/streaming/OutputHandler.java
+++ mule/src/main/java/org/mule/providers/streaming/OutputHandler.java
@@ -18,11 +18,12 @@ import java.io.OutputStream;
 import java.util.Map;
 
 /**
- * The OutputHandler is a strategy class that is set on the StreamingMessageAdapter to defer the writing
- * o fthe message payload until there is a stream available to write it to.
+ * The OutputHandler is a strategy class that is set on the StreamMessageAdapter to defer the writing
+ * of the message payload until there is a stream available to write it to.
  *
  * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
  * @version $Revision$
+ * @see StreamMessageAdapter
  */
 public interface OutputHandler {
 
@@ -30,14 +31,14 @@ public interface OutputHandler {
      * Write the event payload to the stream. Depending on the underlying transport, attachements and message
      * properties may be written to the stream here too.
      * @param event the current event
-     * @param out the output strewam to write to
+     * @param out the output stream to write to
      * @throws IOException
      */
     public void write(UMOEvent event, OutputStream out) throws IOException;
 
     /**
      * Used to obtain a set of headers that will be sent with this stream payload.  Headers are typically
-     * Set independently from a stream payload.
+     * set independently from a stream payload.
      * @param event the current event
      * @return a Map of headers or an empty map if there are no headers
      */
