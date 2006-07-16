d48eb04aa9a3f1475aa0b9da01fa6db9bc63ff38
Ross Mason <ross@mulesoft.com>
2006-07-16 15:51:30 +0000

    Added POMs and cleaned up the samples a bit
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2476 bf997673-6b11-0410-b953-e057580c5b09

--- samples/echo/bin/mule-echo.bat
+++ samples/echo/bin/mule-echo.bat
@@ -5,6 +5,6 @@ if "%MULE_HOME%" == "" SET MULE_HOME=..\..\..
 REM Set your application specific classpath like this
 SET CLASSPATH=%MULE_HOME%\samples\echo\conf
 
-call %MULE_HOME%\bin\mule.bat -config ../conf/echo-mule-config.xml
+call %MULE_HOME%\bin\mule.bat -config echo-mule-config.xml
 
 SET CLASSPATH=
--- samples/echo/bin/mule-echo.sh
+++ samples/echo/bin/mule-echo.sh
@@ -8,6 +8,6 @@ fi
 # Set your application specific classpath like this
 export CLASSPATH=$MULE_HOME/samples/echo/conf
 
-$MULE_HOME/bin/mule -config ../conf/echo-mule-config.xml
+$MULE_HOME/bin/mule -config echo-mule-config.xml
 
 export CLASSPATH=
