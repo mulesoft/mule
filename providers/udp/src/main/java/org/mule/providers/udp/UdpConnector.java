6ffce5815781379f98559da331d16f30e194cfdb
holger <holger>
2006-07-18 12:49:21 +0000

    MULE-919: remove unused backlog property (UDP backlog is handled via bufferSize)
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2495 bf997673-6b11-0410-b953-e057580c5b09

--- providers/udp/src/main/java/org/mule/providers/udp/UdpConnector.java
+++ providers/udp/src/main/java/org/mule/providers/udp/UdpConnector.java
@@ -26,13 +26,9 @@ public class UdpConnector extends AbstractServiceEnabledConnector
 
     public static final int DEFAULT_BUFFER_SIZE = 64 * 1024;
 
-    public static final int DEFAULT_BACKLOG = 256;
-
     private int timeout = DEFAULT_SOCKET_TIMEOUT;
 
     private int bufferSize = DEFAULT_BUFFER_SIZE;
-
-    private int backlog = DEFAULT_BACKLOG;
 
     public String getProtocol()
     {
@@ -65,13 +61,4 @@ public class UdpConnector extends AbstractServiceEnabledConnector
         this.bufferSize = bufferSize;
     }
 
-    public int getBacklog()
-    {
-        return backlog;
-    }
-
-    public void setBacklog(int backlog)
-    {
-        this.backlog = backlog;
-    }
 }
