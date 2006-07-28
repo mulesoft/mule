d4f648f67349508339dd17be7da6de47a2277874
stephenfenech <stephenfenech>
2006-07-28 13:52:32 +0000

    Test Cases for the outputAppend change
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2551 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/integration/providers/file/FileAppendConnectorTestCase.java
@@ -0,0 +1,46 @@
+package org.mule.test.integration.providers.file;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.mule.extras.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+
+/**
+ * 
+ * @author <a href="mailto:stephen.fenech@symphonysoft.com">Stephen Fenech</a>
+ *
+ */
+public class FileAppendConnectorTestCase extends FunctionalTestCase{
+	
+	public void testBasic() throws Exception
+	{
+		MuleClient client=new MuleClient();
+		(new File("myout/out.txt")).delete();
+		client.send("vm://fileappend", "Hello1", null);
+		client.send("vm://fileappend", "Hello2", null);
+		assertEquals("Hello1Hello2\r\n",this.readFile("myout/out.txt"));			
+	}
+	
+	private String readFile(String path) throws IOException
+	{
+		FileReader fr = new FileReader(path);
+		BufferedReader br=new BufferedReader(fr);
+		
+		String tmp;
+		String email="";
+		while((tmp=br.readLine())!=null)
+		{
+			email=email.concat(tmp)+"\r\n";
+		}
+		
+		return email;
+	}
+
+
+	protected String getConfigResources() {
+		return "mule-fileappend-endpoint-config.xml";
+	}		
+}
