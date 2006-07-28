f29c88f56901374d3d0466fd904ad6bcbe8c05b8
alan.cassar <alan.cassar>
2006-07-28 13:37:34 +0000

    Changed XFire client from being created with our custom transport. When the custom transport was being used, the SOAP request generated was leaving namespaces empty
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2547 bf997673-6b11-0410-b953-e057580c5b09

--- providers/xfire/src/main/java/org/mule/providers/soap/xfire/wsdl/XFireWsdlClientPoolFactory.java
+++ providers/xfire/src/main/java/org/mule/providers/soap/xfire/wsdl/XFireWsdlClientPoolFactory.java
@@ -12,11 +12,12 @@
 
 package org.mule.providers.soap.xfire.wsdl;
 
+import java.net.URL;
+
 import org.codehaus.xfire.XFire;
 import org.codehaus.xfire.client.Client;
 import org.codehaus.xfire.service.Service;
 import org.mule.providers.soap.xfire.XFireClientPoolFactory;
-import org.mule.providers.soap.xfire.transport.MuleUniversalTransport;
 import org.mule.umo.endpoint.UMOImmutableEndpoint;
 
 public class XFireWsdlClientPoolFactory extends XFireClientPoolFactory
@@ -29,9 +30,13 @@ public class XFireWsdlClientPoolFactory extends XFireClientPoolFactory
         super(endpoint, service, xfire);
     }
 
+    
+    
     public Object makeObject() throws Exception
     {
-        Client client = new Client(new MuleUniversalTransport(), service, uri.getAddress());
+    	//We are assuming here that we are not going to use any 
+    	//other transport other than http.
+        Client client = new Client(new URL(uri.getAddress()));
         client.setXFire(xfire);
         client.setEndpointUri(uri.toString());
         return client;
