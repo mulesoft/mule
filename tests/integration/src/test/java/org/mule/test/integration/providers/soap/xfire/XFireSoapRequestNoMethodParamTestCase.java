3c943d930e17484b32ea0c2f5a48fec581a2c07f
alan.cassar <alan.cassar>
2006-08-02 09:29:51 +0000

    Added a new test for xfire soap request without any method params
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2606 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/integration/providers/soap/xfire/XFireSoapRequestNoMethodParamTestCase.java
@@ -0,0 +1,25 @@
+package org.mule.test.integration.providers.soap.xfire;
+
+import org.mule.extras.client.MuleClient;
+import org.mule.impl.MuleMessage;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.umo.UMOMessage;
+
+public class XFireSoapRequestNoMethodParamTestCase extends FunctionalTestCase{
+
+	public void testXfireSoapRequest() throws Exception
+	{
+		MuleClient client = new MuleClient();
+		
+		UMOMessage msg = client.send("http://localhost:81/services/TestComponent", new MuleMessage("<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soap:Body><receive xmlns=\"http://www.muleumo.org\"><src xmlns=\"http://www.muleumo.org\">Test String</src></receive></soap:Body></soap:Envelope>"));
+		
+		assertNotNull(msg);
+		assertNotNull(msg.getPayload());
+		assertEquals(msg.getPayloadAsString(),"<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soap:Body><receiveResponse xmlns=\"http://www.muleumo.org\"><out xmlns=\"http://www.muleumo.org\">Received: Test String</out></receiveResponse></soap:Body></soap:Envelope>");
+	}
+	
+	protected String getConfigResources() {
+		return "org/mule/test/integration/providers/soap/xfire/mule-xfire-soap-request.xml";
+	}
+
+}
