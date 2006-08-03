9c27b74b020563579bc3bf522ec9e4d98f39e6ef
Ross Mason <ross@mulesoft.com>
2006-08-03 08:10:20 +0000

    Moved remaining script dependencies from the integration tests
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2617 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/scripting/src/test/java/org/mule/components/script/GroovyMessageBuilderTestCase.java
@@ -0,0 +1,37 @@
+/*
+ * $Id: $
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) SymphonySoft Limited. All rights reserved.
+ * http://www.symphonysoft.com
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+
+package org.mule.components.script;
+
+import org.mule.extras.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.umo.UMOMessage;
+
+/**
+ * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
+ * @version $Revision: 2181 $
+ */
+public class GroovyMessageBuilderTestCase extends FunctionalTestCase
+{
+    protected String getConfigResources() {
+        return "groovy-builder-config.xml";
+    }
+
+    public void testFunctionBehaviour() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        UMOMessage m = client.send("vm://groovy.1", "Test:", null);
+        assertNotNull(m);
+        assertEquals("Test: A Received B Received", m.getPayloadAsString());
+    }
+}
+
