f89e2fd6c1c1c3d3ccadfa659b0b24ca369be6f6
alan.cassar <alan.cassar>
2006-08-08 13:26:46 +0000

    New test which tests the custom http headers over xfire
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2636 bf997673-6b11-0410-b953-e057580c5b09

---
+++ providers/xfire/src/test/java/org/mule/providers/soap/xfire/XfireCustomHttpHeaderTestCase.java
@@ -0,0 +1,63 @@
+package org.mule.providers.soap.xfire;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Vector;
+
+import org.mule.MuleManager;
+import org.mule.config.MuleProperties;
+import org.mule.extras.client.MuleClient;
+import org.mule.impl.MuleMessage;
+import org.mule.impl.internal.notifications.MessageNotification;
+import org.mule.impl.internal.notifications.MessageNotificationListener;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.umo.UMOMessage;
+import org.mule.umo.manager.UMOServerNotification;
+
+public class XfireCustomHttpHeaderTestCase extends FunctionalTestCase implements MessageNotificationListener{
+
+	private List notifications;
+	private final String myProperty = new String("myProperty");
+	
+	public void testXfire() throws Exception
+	{
+		notifications = new Vector(5);
+		
+		MuleManager.getInstance().registerListener(this);
+		MuleClient client = new MuleClient();
+		HashMap props = new HashMap();
+		props.put(MuleProperties.MULE_USER_PROPERTY, "alan");
+		props.put(MuleProperties.MULE_METHOD_PROPERTY, "sayHello");
+		props.put(myProperty, myProperty);
+		
+		UMOMessage reply = client.send("xfire:http://localhost:81/services/TestComponent?method=onReceive", new MuleMessage(new Object[] {"Test String"}) , props);
+		assertNotNull(reply);
+		assertNotNull(reply.getPayload());
+		assertEquals(reply.getPayloadAsString(), "Test String Received");
+		
+		//should have received all notifications before the reply arrives back to the client
+		UMOMessage msgNotification = (UMOMessage)notifications.get(0);
+		
+		//MULE_USER	should  be allowed in
+		assertEquals(msgNotification.getProperty(MuleProperties.MULE_USER_PROPERTY), "alan");
+		
+		//mule properties should be removed
+		assertNull(msgNotification.getProperty(MuleProperties.MULE_METHOD_PROPERTY));
+		
+		//custom properties should be allowed in
+		assertEquals(msgNotification.getProperty(myProperty),myProperty);
+	}
+	
+	public synchronized void onNotification(UMOServerNotification notification)
+	{
+		MessageNotification msgNotification = (MessageNotification)notification;
+		UMOMessage msg = (UMOMessage)msgNotification.getSource();
+		notifications.add(msg);
+	}
+	
+	protected String getConfigResources() {
+		
+		return "mule-xfire-conf.xml";
+	}
+
+}
