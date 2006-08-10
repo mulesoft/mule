d3b7a291064f8c84a38d0b314034e8d3524bdd0f
holger <holger>
2006-08-10 03:34:44 +0000

    MULE-958 - license header fun :(
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2668 bf997673-6b11-0410-b953-e057580c5b09

--- tools/benchmark/src/java/org/mule/tools/benchmark/BenchmarkComponent.java
+++ tools/benchmark/src/java/org/mule/tools/benchmark/BenchmarkComponent.java
@@ -1,13 +1,11 @@
 /*
  * $Id$
- * ------------------------------------------------------------------------------------------------------
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
  *
- * Copyright (c) Cubis Limited. All rights reserved.
- * http://www.cubis.co.uk
- *
- * The software in this package is published under the terms of the BSD
- * style license a copy of which has been included with this distribution in
- * the LICENSE.txt file.
+ * The software in this package is published under the terms of the BSD style
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
  */
 package org.mule.tools.benchmark;
 
