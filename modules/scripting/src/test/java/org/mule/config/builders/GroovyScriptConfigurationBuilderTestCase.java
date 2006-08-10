d251076f8b163126740b223d1775a0ab124e77e6
holger <holger>
2006-08-10 02:48:54 +0000

    fix breakage from previous license massacre.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2659 bf997673-6b11-0410-b953-e057580c5b09

--- modules/scripting/src/test/java/org/mule/config/builders/GroovyScriptConfigurationBuilderTestCase.java
+++ modules/scripting/src/test/java/org/mule/config/builders/GroovyScriptConfigurationBuilderTestCase.java
@@ -7,7 +7,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-*/
+
 package org.mule.config.builders;
 
 import org.mule.config.ConfigurationBuilder;
