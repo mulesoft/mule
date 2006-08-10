03feeec479957b5cc737df8be78e1248a1b2129e
holger <holger>
2006-08-10 02:48:48 +0000

    fix breakage from previous license massacre.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2658 bf997673-6b11-0410-b953-e057580c5b09

--- modules/picocontainer/src/main/java/org/mule/extras/picocontainer/PicoContainerContext.java
+++ modules/picocontainer/src/main/java/org/mule/extras/picocontainer/PicoContainerContext.java
@@ -7,7 +7,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
- */
+
 package org.mule.extras.picocontainer;
 
 import org.mule.config.i18n.Message;
