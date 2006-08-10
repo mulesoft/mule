3991191ff46608710e6fd4e76673608652692a88
aperepel@gmail.com <Andrew Perepelytsya>
2006-08-10 03:11:33 +0000

    Headers and tabs.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2665 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/providers/file/FileAppendConnectorTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/providers/file/FileAppendConnectorTestCase.java
@@ -1,3 +1,13 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the BSD style
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.test.integration.providers.file;
 
 import java.io.BufferedReader;
@@ -14,33 +24,33 @@ import org.mule.tck.FunctionalTestCase;
  *
  */
 public class FileAppendConnectorTestCase extends FunctionalTestCase{
-	
-	public void testBasic() throws Exception
-	{
-		MuleClient client=new MuleClient();
-		(new File("myout/out.txt")).delete();
-		client.send("vm://fileappend", "Hello1", null);
-		client.send("vm://fileappend", "Hello2", null);
-		assertEquals("Hello1Hello2\r\n",this.readFile("myout/out.txt"));			
-	}
-	
-	private String readFile(String path) throws IOException
-	{
-		FileReader fr = new FileReader(path);
-		BufferedReader br=new BufferedReader(fr);
-		
-		String tmp;
-		String email="";
-		while((tmp=br.readLine())!=null)
-		{
-			email=email.concat(tmp)+"\r\n";
-		}
-		
-		return email;
-	}
+    
+    public void testBasic() throws Exception
+    {
+        MuleClient client=new MuleClient();
+        (new File("myout/out.txt")).delete();
+        client.send("vm://fileappend", "Hello1", null);
+        client.send("vm://fileappend", "Hello2", null);
+        assertEquals("Hello1Hello2\r\n",this.readFile("myout/out.txt"));
+    }
+
+    private String readFile(String path) throws IOException
+    {
+        FileReader fr = new FileReader(path);
+        BufferedReader br=new BufferedReader(fr);
+
+        String tmp;
+        String email="";
+        while((tmp=br.readLine())!=null)
+        {
+            email=email.concat(tmp)+"\r\n";
+        }
+
+        return email;
+    }
 
 
-	protected String getConfigResources() {
-		return "mule-fileappend-endpoint-config.xml";
-	}		
+    protected String getConfigResources() {
+        return "mule-fileappend-endpoint-config.xml";
+    }
 }
