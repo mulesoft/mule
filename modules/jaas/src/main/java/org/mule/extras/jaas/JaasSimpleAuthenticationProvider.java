c1e2cb90404c96a2233692106627dcdb05546dce
holger <holger>
2006-08-21 14:16:05 +0000

    fix header
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2743 bf997673-6b11-0410-b953-e057580c5b09

--- modules/jaas/src/main/java/org/mule/extras/jaas/JaasSimpleAuthenticationProvider.java
+++ modules/jaas/src/main/java/org/mule/extras/jaas/JaasSimpleAuthenticationProvider.java
@@ -1,23 +1,11 @@
 /*
-
  * $Id$
-
- * ------------------------------------------------------------------------------------------------------
-
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
  *
-
- * Copyright (c) SymphonySoft Limited. All rights reserved.
-
- * http://www.symphonysoft.com
-
- *
-
- * The software in this package is published under the terms of the BSD
-
- * style license a copy of which has been included with this distribution in
-
- * the LICENSE.txt file.
-
+ * The software in this package is published under the terms of the BSD style
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
  */
 
 package org.mule.extras.jaas;
@@ -33,7 +21,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.mule.config.i18n.Messages;
-import org.mule.extras.jaas.JaasSecurityContextFactory;
 import org.mule.impl.security.MuleAuthentication;
 import org.mule.umo.lifecycle.InitialisationException;
 import org.mule.umo.lifecycle.RecoverableException;
