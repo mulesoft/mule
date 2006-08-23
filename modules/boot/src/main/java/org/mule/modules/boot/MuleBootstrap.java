3af39f24fd2347bc77fcf62d3e2d6ab5422089cc
tcarlson <tcarlson>
2006-08-23 22:44:01 +0000

    Use org.mule.modules.boot.MuleBootstrap to bootup Mule (this just handles the -main parameter in case we're not going to use org.mule.MuleServer as main, and then hands control over to the Java Service Wrapper).
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2765 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/boot/src/main/java/org/mule/modules/boot/MuleBootstrap.java
@@ -0,0 +1,40 @@
+/*
+ * $Id
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the BSD style
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.modules.boot;
+
+import org.mule.MuleServer;
+import org.mule.util.SystemUtils;
+import org.tanukisoftware.wrapper.WrapperSimpleApp;
+
+/**
+ * Determine which is the main class to run and delegate control to the
+ * Java Service Wrapper.
+ *
+ * @author Travis Carlson
+ */
+public class MuleBootstrap {
+
+    public static void main(String args[]) {
+
+        // See if a main class was given, otherwise use MuleServer.
+        String mainClassName = SystemUtils.getCommandLineOption("-main", args);
+        if (mainClassName == null) {
+            mainClassName = MuleServer.class.getName();
+        }
+
+        // Add the main class name as the first argument to the Wrapper.
+        String[] appArgs = new String[args.length + 1];
+        appArgs[0] = mainClassName;
+        System.arraycopy(args, 0, appArgs, 1, args.length);
+
+        // Call the wrapper
+        WrapperSimpleApp.main(appArgs);
+    }
+}
