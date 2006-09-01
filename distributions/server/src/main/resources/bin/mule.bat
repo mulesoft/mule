e51c106a7c5a3680f4f43b95ba0b55199b7cbc29
tcarlson <tcarlson>
2006-09-01 18:16:26 +0000

    Upgrade Java Service Wrapper to 3.2.1 Properly set the "wrapper.working.dir" property (which in turn sets the Java "user.dir" property). Use the command-based Windows script (accepts similar arguments as the Unix script: console, start, stop, etc.) Customization of both scripts to pass command-line arguments through to the MuleServer ("-config, -builder, etc.")
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2901 bf997673-6b11-0410-b953-e057580c5b09

--- distributions/server/src/main/resources/bin/mule.bat
+++ distributions/server/src/main/resources/bin/mule.bat
@@ -3,14 +3,9 @@ setlocal
 
 rem Copyright (c) 1999, 2006 Tanuki Software Inc.
 rem
-rem Java Service Wrapper general startup script
+rem Java Service Wrapper command based script
 rem
 
-rem
-rem Resolve the real path of the wrapper.exe
-rem  For non NT systems, the _REALPATH and _WRAPPER_CONF values
-rem  can be hard-coded below and the following test removed.
-rem
 if "%OS%"=="Windows_NT" goto nt
 echo This script only works with NT-based versions of Windows.
 goto :eof
@@ -20,16 +15,20 @@ rem
 rem Find the application home.
 rem
 rem %~dp0 is location of current script under NT
-set _REALPATH=%~dp0..\sbin\
+set _REALPATH=%~dp0
 
 rem Decide on the wrapper binary.
-set _WRAPPER_BASE=wrapper
+rem ###############################################################
+rem Customized for Mule
+rem ###############################################################
+set _WRAPPER_BASE=..\sbin\wrapper
+rem ###############################################################
 set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-x86-32.exe
-if exist %_WRAPPER_EXE% goto conf
+if exist "%_WRAPPER_EXE%" goto validate
 set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-x86-64.exe
-if exist %_WRAPPER_EXE% goto conf
+if exist "%_WRAPPER_EXE%" goto validate
 set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%.exe
-if exist %_WRAPPER_EXE% goto conf
+if exist "%_WRAPPER_EXE%" goto validate
 echo Unable to locate a Wrapper executable using any of the following names:
 echo %_REALPATH%%_WRAPPER_BASE%-windows-x86-32.exe
 echo %_REALPATH%%_WRAPPER_BASE%-windows-x86-64.exe
@@ -37,31 +36,88 @@ echo %_REALPATH%%_WRAPPER_BASE%.exe
 pause
 goto :eof
 
+:validate
+rem Find the requested command.
+for /F %%v in ('echo %1^|findstr "^console$ ^start$ ^pause$ ^resume$ ^stop$ ^restart$ ^install$ ^remove"') do call :exec set COMMAND=%%v
+
+if "%COMMAND%" == "" (
+    echo Usage: %0 { console : start : pause : resume : stop : restart : install : remove }
+	rem ###############################################################
+	rem Customized for Mule
+	rem ###############################################################
+    echo No command specified, running in console/foreground mode by default, use Ctrl-C to exit...
+    set COMMAND=:console
+    rem pause
+    rem goto :eof
+	rem ###############################################################
+) else (
+    shift
+)
+
 rem
 rem Find the wrapper.conf
 rem
 :conf
-rem set _WRAPPER_CONF="%~f1"
-rem if not %_WRAPPER_CONF%=="" goto startup
 set _WRAPPER_CONF="%_REALPATH%..\conf\wrapper.conf"
 
-if not "%1"=="" set MULE_ARG1="%1"
-if not "%2"=="" set MULE_ARG2="%2"
-if not "%3"=="" set MULE_ARG3="%3"
-if not "%4"=="" set MULE_ARG4="%4"
-if not "%5"=="" set MULE_ARG5="%5"
-if not "%6"=="" set MULE_ARG6="%6"
-if not "%7"=="" set MULE_ARG7="%7"
-if not "%8"=="" set MULE_ARG8="%8"
-if not "%9"=="" set MULE_ARG9="%9"
-if not "%10"=="" set MULE_ARG10="%10"
+rem ###############################################################
+rem Customized for Mule
+rem ###############################################################
+rem Export the location of this script.  This will be used in wrapper.conf
+set MULE_EXE=%_REALPATH%
 
+rem Mule options: Set the working directory to the current one and pass all command-line
+rem options (-config, -builder, etc.) straight through to the main() method.
+set MULE_OPTS=wrapper.working.dir="%CD%" wrapper.app.parameter.1=%1 wrapper.app.parameter.2=%2  wrapper.app.parameter.3=%3  wrapper.app.parameter.4=%4  wrapper.app.parameter.5=%5  wrapper.app.parameter.6=%6  wrapper.app.parameter.7=%7  wrapper.app.parameter.8=%8 wrapper.app.parameter.9=%9
+rem ###############################################################
 
 rem
-rem Start the Wrapper
+rem Run the application.
+rem At runtime, the current directory will be that of wrapper.exe
 rem
-:startup
-"%_WRAPPER_EXE%" -c %_WRAPPER_CONF%
-if not errorlevel 1 goto :eof
-pause
+call :%COMMAND%
+if errorlevel 1 pause
+goto :eof
 
+rem ###############################################################
+rem Customized for Mule
+rem ###############################################################
+
+:console
+"%_WRAPPER_EXE%" -c %_WRAPPER_CONF% %MULE_OPTS%
+goto :eof
+
+:start
+"%_WRAPPER_EXE%" -t %_WRAPPER_CONF% %MULE_OPTS%
+goto :eof
+
+:pause
+"%_WRAPPER_EXE%" -a %_WRAPPER_CONF% %MULE_OPTS%
+goto :eof
+
+:resume
+"%_WRAPPER_EXE%" -e %_WRAPPER_CONF% %MULE_OPTS%
+goto :eof
+
+:stop
+"%_WRAPPER_EXE%" -p %_WRAPPER_CONF% %MULE_OPTS%
+goto :eof
+
+:install
+"%_WRAPPER_EXE%" -i %_WRAPPER_CONF% %MULE_OPTS%
+goto :eof
+
+:remove
+"%_WRAPPER_EXE%" -r %_WRAPPER_CONF% %MULE_OPTS%
+goto :eof
+
+rem ###############################################################
+
+:restart
+call :stop
+call :start
+goto :eof
+
+:exec
+%*
+goto :eof
