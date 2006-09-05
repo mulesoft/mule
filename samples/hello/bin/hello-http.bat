2574698ac4e9c1048effacb5dac6d90b4dbfdb2d
aperepel@gmail.com <Andrew Perepelytsya>
2006-09-05 01:59:10 +0000

    Fix Win scripts.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2937 bf997673-6b11-0410-b953-e057580c5b09

--- samples/hello/bin/hello-http.bat
+++ samples/hello/bin/hello-http.bat
@@ -2,9 +2,6 @@
 REM There is no need to call this if you set the MULE_HOME in your environment properties
 if "%MULE_HOME%" == "" SET MULE_HOME=..\..\..
 
-REM Set your application specific classpath like this
-SET CLASSPATH=%MULE_HOME%\samples\hello\conf;%MULE_HOME%\samples\hello\classes
+SET MULE_LIB=%~dp0..\classes
 
-call %MULE_HOME%\bin\mule.bat -config ../conf/hello-http-mule-config.xml
-
-SET CLASSPATH=
+call %MULE_HOME%\bin\mule.bat -config ../conf/hello-http-config.xml
