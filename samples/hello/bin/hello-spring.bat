f79f1f839041b85b2e5c84b3cfd4174a9feecade
aperepel@gmail.com <Andrew Perepelytsya>
2006-09-05 01:45:21 +0000

    Resurrecting the bin folder, there is still value in it, though slightly modified.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2936 bf997673-6b11-0410-b953-e057580c5b09

---
+++ samples/hello/bin/hello-spring.bat
@@ -0,0 +1,10 @@
+@echo off
+REM There is no need to call this if you set the MULE_HOME in your environment properties
+if "%MULE_HOME%" == "" SET MULE_HOME=..\..\..
+
+REM Set your application specific classpath like this
+SET CLASSPATH=%MULE_HOME%\samples\hello\conf;%MULE_HOME%\samples\hello\classes
+
+call %MULE_HOME%\bin\mule.bat -config ../conf/hello-spring-config.xml -builder org.mule.extras.spring.config.SpringConfigurationBuilder
+
+SET CLASSPATH=
