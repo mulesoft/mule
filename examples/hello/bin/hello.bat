53ec0022941e214fbf566b8b9db935d9928a81bc
aperepel@gmail.com <Andrew Perepelytsya>
2006-09-08 02:13:42 +0000

    Update unix startup scripts, make them shell-friendly (for better Solaris compatibility), minor updates in Win scripts.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@2981 bf997673-6b11-0410-b953-e057580c5b09

--- examples/hello/bin/hello.bat
+++ examples/hello/bin/hello.bat
@@ -2,6 +2,7 @@
 REM There is no need to call this if you set the MULE_HOME in your environment properties
 if "%MULE_HOME%" == "" SET MULE_HOME=..\..\..
 
-SET MULE_LIB=%~dp0..\classes
+REM Set your application specific classpath like this
+SET MULE_LIB=%MULE_HOME%\examples\hello\classes
 
 call %MULE_HOME%\bin\mule.bat -config ../conf/hello-config.xml
