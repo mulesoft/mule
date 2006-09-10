12cc0b065c74c2bea8b9b81ac792eb4e437a530a
aperepel@gmail.com <Andrew Perepelytsya>
2006-09-10 14:03:36 +0000

    Fixed MULE-1016 Mule 1.3-rc5 fails to run on Windows2000.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3000 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/boot/src/main/java/org/mule/modules/boot/DefaultMuleClassPathConfig.java
@@ -0,0 +1,143 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the BSD style
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.modules.boot;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Constructs a default set of JAR Urls located under
+ * Mule home folder.
+ */
+public class DefaultMuleClassPathConfig
+{
+    protected static final String FOLDER_MULE = "/lib/mule";
+    protected static final String FOLDER_OPT = "/lib/opt";
+    protected static final String FOLDER_USER = "/lib/user";
+
+    private List urls = new LinkedList();
+
+    private String muleHome;
+
+    /**
+     * Constructs a new DefaultMuleClassPathConfig.
+     */
+    public DefaultMuleClassPathConfig()
+    {
+        muleHome = System.getProperty("mule.home");
+        if (muleHome == null || muleHome.trim().length() == 0)
+        {
+            throw new IllegalArgumentException("Either MULE_HOME is not set or mule.home system property is missing.");
+        }
+
+        try
+        {
+            // if trailing slash is specified, the folder will be added (e.g. for properties files)
+            addURL(new URL("file:///" + muleHome + FOLDER_USER + "/"));
+            addURL(new URL("file:///" + muleHome + FOLDER_MULE + "/"));
+            addURL(new URL("file:///" + muleHome + FOLDER_OPT + "/"));
+
+            File[] muleJars = listJars(FOLDER_USER);
+            for (int i = 0; i < muleJars.length; i++)
+            {
+                File jar = muleJars[i];
+                addURL(jar.toURL());
+            }
+
+            muleJars = listJars(FOLDER_MULE);
+            for (int i = 0; i < muleJars.length; i++)
+            {
+                File jar = muleJars[i];
+                addURL(jar.toURL());
+            }
+
+            muleJars = listJars(FOLDER_OPT);
+            for (int i = 0; i < muleJars.length; i++)
+            {
+                File jar = muleJars[i];
+                addURL(jar.toURL());
+            }
+
+        }
+        catch (MalformedURLException e)
+        {
+            throw new RuntimeException("Failed to construct a classpath URL", e);
+        }
+
+    }
+
+
+    /**
+     * Getter for property 'urls'.
+     *
+     * @return A copy of 'urls'. Items are java.net.URL
+     */
+    public List getURLs()
+    {
+        return new ArrayList(this.urls);
+    }
+
+    /**
+     * Setter for property 'urls'.
+     *
+     * @param urls Value to set for property 'urls'.
+     */
+    public void addURLs(List urls)
+    {
+        if (urls != null && !urls.isEmpty())
+        {
+            this.urls.addAll(urls);
+        }
+    }
+
+    /**
+     * Add a URL to Mule's classpath.
+     * @param url folder (should end with a slash) or jar path
+     */
+    public void addURL(URL url)
+    {
+        this.urls.add(url);
+    }
+
+    /**
+     * Find and if necessary filter the jars for classpath.
+     * @param muleSubfolder folder under Mule home to list
+     * @return a list
+     */
+    protected File[] listJars(String muleSubfolder)
+    {
+        String fullPath = muleHome + muleSubfolder;
+        File path = new File(fullPath);
+
+        File[] jars = path.listFiles(new FileFilter()
+        {
+            public boolean accept(File pathname)
+            {
+                try
+                {
+                    return pathname.getCanonicalPath().endsWith(".jar");
+                }
+                catch (IOException e)
+                {
+                    throw new RuntimeException(e.getMessage());
+                }
+            }
+        });
+
+        return jars == null ? new File[0] : jars;
+    }
+}
+
--- modules/boot/src/main/java/org/mule/modules/boot/MuleBootstrap.java
+++ modules/boot/src/main/java/org/mule/modules/boot/MuleBootstrap.java
@@ -9,24 +9,93 @@
  */
 package org.mule.modules.boot;
 
-import org.mule.MuleServer;
 import org.mule.util.SystemUtils;
 import org.tanukisoftware.wrapper.WrapperSimpleApp;
+
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Determine which is the main class to run and delegate control to the
  * Java Service Wrapper.
- *
- * @author Travis Carlson
+ * <p/>
+ * MuleBootstrap class is responsible for constructing Mule's classpath from the Mule home
+ * folder.
  */
-public class MuleBootstrap {
+public class MuleBootstrap
+{
 
-    public static void main(String args[]) {
 
-        // See if a main class was given, otherwise use MuleServer.
+    /**
+     * Do not instantiate MuleBootstrap.
+     */
+    private MuleBootstrap()
+    {
+    }
+
+    /**
+     * Entry point.
+     * @param args command-line arguments
+     * @throws Exception in case of any fatal problem
+     */
+    public static void main(String args[]) throws Exception
+    {
+
+        ClassLoader sys = ClassLoader.getSystemClassLoader();
+        if (!(sys instanceof URLClassLoader))
+        {
+            throw new IllegalArgumentException(
+                    "PANIC: Mule has been started with an unsupported classloader: "
+                            + sys.getClass().getName() + ". "
+                            + "Please report this error to user<at>mule<dot>codehaus<dot>org");
+        }
+
+        // system classloader is in this case the one that launched the application,
+        // which is usually something like a JDK-vendor proprietary AppClassLoader
+        URLClassLoader sysCl = (URLClassLoader) ClassLoader.getSystemClassLoader();
+
+        /*
+          IMPORTANT NOTE:
+
+          The more 'natural' way would be to create a custom URLClassLoader and configure it,
+          but then there's a chicken-and-egg problem, as all classes MuleBootstrap depends on
+          would have been loaded by a parent classloader, and not ours. There's no straightforward
+          way to change this, and is documented in a Sun's classloader guide. The solution
+          would've involved overriding the ClassLoader.findClass() method and modifying the semantics
+          to be child-first, but that way we are calling for trouble.
+
+          Hacking the primordial classloader is a bit brutal, but works perfectly in case of
+          running from the command-line as a standalone app. All Mule embedding options then
+          delegate the classpath config to the embedder (a developer embedding Mule in the app),
+          thus classloaders are not modified in those scenarios.
+        */
+
+
+        DefaultMuleClassPathConfig classPath = new DefaultMuleClassPathConfig();
+        // get mule classpath urls pointing to jars and folders
+        List urls = classPath.getURLs();
+        // get a Method ref from the normal class, but invoke on a proprietary parent object,
+        // as this method is usually protected in those classloaders
+        Class refClass = URLClassLoader.class;
+        Method methodAddUrl = refClass.getDeclaredMethod("addURL", new Class[]{URL.class});
+        methodAddUrl.setAccessible(true);
+        for (Iterator it = urls.iterator(); it.hasNext();)
+        {
+            URL url = (URL) it.next();
+            // System.out.println("Adding: " + url.toExternalForm());
+            methodAddUrl.invoke(sysCl, new Object[]{url});
+        }
+
+        // the core jar has been added dynamically, this construct will run with
+        // a new Mule classpath now
         String mainClassName = SystemUtils.getCommandLineOption("-main", args);
-        if (mainClassName == null) {
-            mainClassName = MuleServer.class.getName();
+
+        if (mainClassName == null)
+        {
+            mainClassName = "org.mule.MuleServer";
         }
 
         // Add the main class name as the first argument to the Wrapper.
