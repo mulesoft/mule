4e3525d3ccde4001d9cd83e7e40edc0e220c5956
aperepel@gmail.com <Andrew Perepelytsya>
2006-09-12 02:40:57 +0000

    Added startup scripts for the errorhandler example.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3028 bf997673-6b11-0410-b953-e057580c5b09

---
+++ examples/errorhandler/bin/errorhandler.bat
@@ -0,0 +1,8 @@
+@echo off
+REM There is no need to call this if you set the MULE_HOME in your environment properties
+if "%MULE_HOME%" == "" SET MULE_HOME=..\..\..
+
+REM Set your application specific classpath like this
+SET MULE_LIB=%MULE_HOME%\examples\errorhandler\classes
+
+call %MULE_HOME%\bin\mule.bat -config ../conf/error-config.xml
---
+++ examples/errorhandler/bin/errorhandler.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+# There is no need to call this if you set the MULE_HOME in your environment
+if [ -z "$MULE_HOME" ] ; then
+  MULE_HOME=../../..
+  export MULE_HOME
+fi
+
+# Set your application specific classpath like this
+MULE_LIB=$MULE_HOME/examples/errorhandler/classes
+export MULE_LIB
+
+$MULE_HOME/bin/mule -config ../conf/error-config.xml
+
