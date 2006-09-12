9338c32875e7ac0fbd067f9b87314f89dac2dc11
aperepel@gmail.com <Andrew Perepelytsya>
2006-09-12 16:24:00 +0000

    Startup scripts for the stockquote example. Don't bundle stockquote example in the distro.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3033 bf997673-6b11-0410-b953-e057580c5b09

---
+++ examples/stockquote/bin/sq-rest.bat
@@ -0,0 +1,8 @@
+@echo off
+REM There is no need to call this if you set the MULE_HOME in your environment properties
+if "%MULE_HOME%" == "" SET MULE_HOME=..\..\..
+
+REM Set your application specific classpath like this
+SET MULE_LIB=%MULE_HOME%\examples\stockquote\classes;%MULE_HOME%\examples\stockquote\conf
+
+call %MULE_HOME%\bin\mule.bat -config ../conf/rest-config.xml
---
+++ examples/stockquote/bin/sq-rest.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+# There is no need to call this if you set the MULE_HOME in your environment
+if [ -z "$MULE_HOME" ] ; then
+  MULE_HOME=../../..
+  export MULE_HOME
+fi
+
+# Set your application specific classpath like this
+MULE_LIB=$MULE_HOME/examples/stockquote/classes:$MULE_HOME/examples/stockquote/conf
+export MULE_LIB
+
+$MULE_HOME/bin/mule -config ../conf/rest-config.xml
+
