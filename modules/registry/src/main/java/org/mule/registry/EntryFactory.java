ad018e146996d841a521bbeed59f6dc39d8dbfdb
tcarlson <tcarlson>
2006-09-18 23:12:35 +0000

    Move Registry classes out of mule-core
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3083 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/registry/src/main/java/org/mule/registry/EntryFactory.java
@@ -0,0 +1,26 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the BSD style
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.registry;
+
+/**
+ * TODO document
+ *
+ * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
+ * @version $Revision$
+ */
+public interface EntryFactory {
+
+    public RegistryComponent createComponent(String name, ComponentType type, Registry registry);
+
+    public RegistryDescriptor createDescriptor();
+
+    public Assembly createAssembly();
+}
---
+++ modules/registry/src/main/java/org/mule/registry/RegistryStore.java
@@ -0,0 +1,26 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the BSD style
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.registry;
+
+/**
+ * TODO document
+ *
+ * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
+ * @version $Revision$
+ */
+public interface RegistryStore {
+
+    public Registry create(String location, RegistryFactory factory) throws RegistryException;
+
+    public Registry load(String location) throws RegistryException;
+
+    public void save(Registry registry) throws RegistryException;
+}
