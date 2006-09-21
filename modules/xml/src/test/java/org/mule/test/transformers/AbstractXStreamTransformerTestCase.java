ce6228f73a9ae500e4e0a73c2309269bacc4db17
holger <holger>
2006-09-21 17:11:02 +0000

    everybody loves testcases
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3150 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/xml/src/test/java/org/mule/test/transformers/AbstractXStreamTransformerTestCase.java
@@ -0,0 +1,85 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the BSD style
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.transformers;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+import junit.framework.TestCase;
+
+import org.mule.transformers.xml.XmlToObject;
+import org.mule.umo.transformer.TransformerException;
+import org.mule.util.XStreamFactory;
+
+/**
+ * Tests configuration and creation of XStream-based transformers
+ */
+public class AbstractXStreamTransformerTestCase extends TestCase
+{
+    public static boolean MyDriverDidInitialize;
+
+    public void setUp()
+    {
+        MyDriverDidInitialize = true;
+    }
+
+    public void tearDown()
+    {
+        MyDriverDidInitialize = false;
+    }
+
+    public void testDefaultDriver() throws Exception
+    {
+        XmlToObject transformer = new XmlToObject();
+        // check for XStream's default
+        assertEquals(XStreamFactory.XSTREAM_XPP_DRIVER, transformer.getDriverClassName());
+    }
+
+    public void testCustomDriver() throws Exception
+    {
+        XmlToObject transformer = new XmlToObject();
+        // set custom driver
+        transformer.setDriverClassName(MyDOMDriver.class.getName());
+        XStream xs = transformer.getXStream();
+
+        assertNotNull(xs);
+        assertSame(xs, transformer.getXStream());
+        assertTrue(MyDriverDidInitialize);
+    }
+
+    public void testBadDriver() throws Exception
+    {
+        XmlToObject transformer = new XmlToObject();
+        // set nonexisting driver class
+        transformer.setDriverClassName("DudeWhereIsMyDriver");
+
+        try
+        {
+            assertNotNull(transformer.getXStream());
+            fail();
+        }
+        catch (TransformerException tex)
+        {
+            // OK
+            assertTrue(tex.getCause() instanceof ClassNotFoundException);
+        }
+    }
+
+    protected static class MyDOMDriver extends DomDriver
+    {
+        public MyDOMDriver()
+        {
+            super();
+            AbstractXStreamTransformerTestCase.MyDriverDidInitialize = true;
+        }
+    }
+
+}
