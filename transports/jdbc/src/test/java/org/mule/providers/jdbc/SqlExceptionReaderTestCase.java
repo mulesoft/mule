01337fd688c65aebe49d97dcd5937e7dfea50fd1
holger <holger>
2006-09-21 14:57:05 +0000

    don't print a highly confusing exception when it was already tested
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3149 bf997673-6b11-0410-b953-e057580c5b09

--- transports/jdbc/src/test/java/org/mule/providers/jdbc/SqlExceptionReaderTestCase.java
+++ transports/jdbc/src/test/java/org/mule/providers/jdbc/SqlExceptionReaderTestCase.java
@@ -7,31 +7,35 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
+
 package org.mule.providers.jdbc;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 import org.mule.MuleException;
 import org.mule.config.ExceptionHelper;
 import org.mule.config.i18n.Message;
 import org.mule.tck.AbstractMuleTestCase;
 
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
-
 /**
  * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
  * @version $Revision$
  */
-public class SqlExceptionReaderTestCase extends AbstractMuleTestCase {
+public class SqlExceptionReaderTestCase extends AbstractMuleTestCase
+{
 
     /**
      * Print the name of this test to standard output
      */
-    protected void doSetUp() throws Exception {
+    protected void doSetUp() throws Exception
+    {
         ExceptionHelper.registerExceptionReader(new SQLExceptionReader());
     }
 
-    public void testNestedExceptionRetreval() throws Exception {
+    public void testNestedExceptionRetreval() throws Exception
+    {
         Exception testException = getException();
         Throwable t = ExceptionHelper.getRootException(testException);
         assertNotNull(t);
@@ -52,16 +56,15 @@ public class SqlExceptionReaderTestCase extends AbstractMuleTestCase {
         assertNotNull(info.get("JavaDoc"));
         assertEquals("1234", info.get("SQL Code"));
         assertEquals("bad SQL state", info.get("SQL State"));
-
-        System.out.println(ExceptionHelper.getRootMuleException(testException).getDetailedMessage());
     }
 
-    private Exception getException() {
+    private Exception getException()
+    {
 
         SQLException e = new SQLException("SQL error", "bad SQL state", 1234);
         e.setNextException(new SQLException("blah"));
 
-        return new MuleException(Message.createStaticMessage("foo"),
-                new MuleException(Message.createStaticMessage("bar"), e));
+        return new MuleException(Message.createStaticMessage("foo"), new MuleException(
+            Message.createStaticMessage("bar"), e));
     }
 }
