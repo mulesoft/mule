e624b987a617c1ef5df8194ff366402f5b3f6913
Ross Mason <ross@mulesoft.com>
2006-09-22 16:26:33 +0000

    fixed up the webapp example. The layout now mirrors the other examples
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3170 bf997673-6b11-0410-b953-e057580c5b09

---
+++ examples/webapp/src/main/java/org/mule/samples/rest/RestTester.java
@@ -0,0 +1,21 @@
+/*
+ * $$Id: $$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the BSD style
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.samples.rest;
+
+import org.mule.components.simple.StaticComponent;
+
+/**
+ * TODO This is a bogus class that has been put here so that the Corbertura plugin does not
+ * barf, we need to find a way to get this plug in to ignore certain modules
+ */
+public class RestTester extends StaticComponent
+{
+
+}
