b11710d76b7bca2ef373fa9ac2b37f86549f0a2e
holger <holger>
2006-09-23 09:55:31 +0000

    MULE-1035/1036
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3185 bf997673-6b11-0410-b953-e057580c5b09

--- modules/xml/src/main/java/org/mule/routing/filters/xml/JXPathFilter.java
+++ modules/xml/src/main/java/org/mule/routing/filters/xml/JXPathFilter.java
@@ -20,6 +20,7 @@ import org.apache.commons.logging.LogFactory;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
+import org.dom4j.XPath;
 import org.mule.umo.UMOFilter;
 import org.mule.umo.UMOMessage;
 import org.mule.util.StringMessageUtils;
@@ -27,63 +28,66 @@ import org.mule.util.StringMessageUtils;
 /**
  * <code>JXPathFilter</code> evaluates an XPath expression against a W3C Document,
  * XML string, or Java bean and returns true if the result is as expected.
- *
- * @author <a href="mailto:S.Vanmeerhaege@gfdi.be">Vanmeerhaeghe St?phane</a>
- * @author <a href="mailto:carlson@hotpop.com">Travis Carlson</a>
- * @version $Revision$
  */
-public class JXPathFilter implements UMOFilter {
+public class JXPathFilter implements UMOFilter
+{
 
     protected transient Log logger = LogFactory.getLog(getClass());
 
     private String expression;
-
     private String expectedValue;
-
     private Map namespaces = null;
-
     private Map contextProperties = null;
-
     private AbstractFactory factory;
-
     private boolean lenient = true;
 
-    public JXPathFilter() {
+    public JXPathFilter()
+    {
         super();
     }
 
-    public JXPathFilter(String expression) {
+    public JXPathFilter(String expression)
+    {
         this.expression = expression;
     }
 
-    public JXPathFilter(String expression, String expectedValue) {
+    public JXPathFilter(String expression, String expectedValue)
+    {
         this.expression = expression;
         this.expectedValue = expectedValue;
     }
 
-    public boolean accept(UMOMessage obj) {
+    public boolean accept(UMOMessage obj)
+    {
         return accept(obj.getPayload());
     }
 
-    private boolean accept(Object obj) {
-
-        if (obj == null) {
+    private boolean accept(Object obj)
+    {
+        if (obj == null)
+        {
             logger.warn("Applying JXPathFilter to null object.");
             return false;
         }
-        if (expression == null) {
+        if (expression == null)
+        {
             logger.warn("Expression for JXPathFilter is not set.");
             return false;
         }
-        if (expectedValue == null) {
-
+        if (expectedValue == null)
+        {
             // Handle the special case where the expected value really is null.
-            if (expression.endsWith("= null") || expression.endsWith("=null")) {
+            if (expression.endsWith("= null") || expression.endsWith("=null"))
+            {
                 expectedValue = "null";
                 expression = expression.substring(0, expression.lastIndexOf("="));
             }
-            else {
-                logger.info("Expected value for JXPathFilter is not set, using 'true' by default");
+            else
+            {
+                if (logger.isInfoEnabled())
+                {
+                    logger.info("Expected value for JXPathFilter is not set, using 'true' by default");
+                }
                 expectedValue = Boolean.TRUE.toString();
             }
         }
@@ -92,103 +96,155 @@ public class JXPathFilter implements UMOFilter {
         boolean accept = false;
 
         // Payload is a DOM Document
-        if (obj instanceof Document) {
-            xpathResult = ((Document) obj).valueOf(expression);
+        if (obj instanceof Document)
+        {
+            if (namespaces == null)
+            {
+                // no namespace defined, let's perform a direct evaluation
+                xpathResult = ((Document)obj).valueOf(expression);
+            }
+            else
+            {
+                // create an xpath expression with namespaces and evaluate it
+                XPath xpath = DocumentHelper.createXPath(expression);
+                xpath.setNamespaceURIs(namespaces);
+                xpathResult = xpath.valueOf(obj);
+            }
+
         }
         // Payload is a String of XML
-        else if (obj instanceof String) {
-            try {
-                Document doc = DocumentHelper.parseText((String) obj);
-                xpathResult = doc.valueOf(expression);
-            } catch (DocumentException e) {
+        else if (obj instanceof String)
+        {
+            try
+            {
+                return accept(DocumentHelper.parseText((String)obj));
+            }
+            catch (DocumentException e)
+            {
                 logger.warn("JXPathFilter unable to parse XML document: " + e.getMessage(), e);
-                logger.debug("XML = " + StringMessageUtils.truncate((String) obj, 200, false));
+                if (logger.isDebugEnabled())
+                    logger.debug("XML = " + StringMessageUtils.truncate((String)obj, 200, false));
                 return false;
             }
         }
         // Payload is a Java object
-        else {
-            logger.debug("Passing object of type " + obj.getClass().getName() + " to JXPathContext");
+        else
+        {
+            if (logger.isDebugEnabled())
+            {
+                logger.debug("Passing object of type " + obj.getClass().getName() + " to JXPathContext");
+            }
             JXPathContext context = JXPathContext.newContext(obj);
             initialise(context);
             xpathResult = context.getValue(expression);
         }
 
-        logger.debug("JXPathFilter Expression result = '" + xpathResult
-                        + "' -  Expected value = '" + expectedValue + "'");
-
+        if (logger.isDebugEnabled())
+        {
+            logger.debug("JXPathFilter Expression result = '" + xpathResult + "' -  Expected value = '"
+                         + expectedValue + "'");
+        }
         // Compare the XPath result with the expected result.
-        if (xpathResult != null) {
+        if (xpathResult != null)
+        {
             accept = xpathResult.toString().equals(expectedValue);
         }
-        else {
+        else
+        {
             // A null result was actually expected.
-            if (expectedValue.equals("null")) {
+            if (expectedValue.equals("null"))
+            {
                 accept = true;
             }
             // A null result was not expected, something probably went wrong.
-            else {
+            else
+            {
                 logger.warn("JXPathFilter expression evaluates to null: " + expression);
             }
         }
-        logger.debug("JXPathFilter accept object  : " + accept);
+
+        if (logger.isDebugEnabled())
+        {
+            logger.debug("JXPathFilter accept object  : " + accept);
+        }
+
         return accept;
     }
 
     /**
      * Initializes the JXPathContext based on any relevant properties set for the
      * filter.
+     * 
      * @param the JXPathContext to initialize
      */
-    protected void initialise(JXPathContext context) {
+    protected void initialise(JXPathContext context)
+    {
         Map.Entry entry = null;
-        if (namespaces != null) {
-            for (Iterator iterator = namespaces.entrySet().iterator(); iterator.hasNext();) {
-                entry = (Map.Entry) iterator.next();
-                context.registerNamespace(entry.getKey().toString(), entry
-                        .getValue().toString());
+        if (namespaces != null)
+        {
+            if (logger.isDebugEnabled())
+            {
+                logger.debug("Initializing JXPathContext with namespaces: " + namespaces);
+            }
+
+            for (Iterator iterator = namespaces.entrySet().iterator(); iterator.hasNext();)
+            {
+                entry = (Map.Entry)iterator.next();
+                context.registerNamespace(entry.getKey().toString(), entry.getValue().toString());
             }
         }
 
-        if (contextProperties != null) {
-            for (Iterator iterator = namespaces.entrySet().iterator(); iterator.hasNext();) {
-                entry = (Map.Entry) iterator.next();
+        if (contextProperties != null)
+        {
+            if (logger.isDebugEnabled())
+            {
+                logger.debug("Initializing JXPathContext with properties: " + contextProperties);
+            }
+
+            for (Iterator iterator = contextProperties.entrySet().iterator(); iterator.hasNext();)
+            {
+                entry = (Map.Entry)iterator.next();
                 context.setValue(entry.getKey().toString(), entry.getValue());
             }
         }
 
-        if (factory != null) {
+        if (factory != null)
+        {
             context.setFactory(factory);
         }
+
         context.setLenient(lenient);
     }
 
     /**
      * @return XPath expression
      */
-    public String getExpression() {
+    public String getExpression()
+    {
         return expression;
     }
 
     /**
-     * @param expression
-     *            The XPath expression
+     * @param expression The XPath expression
      */
-    public void setExpression(String expression) {
+    public void setExpression(String expression)
+    {
         this.expression = expression;
     }
 
     /**
      * @return The expected result value of the XPath expression
      */
-    public String getExpectedValue() {
+    public String getExpectedValue()
+    {
         return expectedValue;
     }
 
     /**
      * Sets the expected result value of the XPath expression
      */
-    public void setExpectedValue(String expectedValue) {
+    public void setExpectedValue(String expectedValue)
+    {
         this.expectedValue = expectedValue;
     }
 
@@ -196,47 +252,58 @@ public class JXPathFilter implements UMOFilter {
      * @return The expected result value of the XPath expression
      * @deprecated Use <code>getExpectedValue()</code>.
      */
-    public String getValue() {
+    public String getValue()
+    {
         return getExpectedValue();
     }
 
     /**
      * Sets the expected result value of the XPath expression
+     * 
      * @deprecated Use <code>setExpectedValue(String expectedValue)</code>.
      */
-    public void setValue(String value) {
+    public void setValue(String value)
+    {
         setExpectedValue(value);
     }
 
-    public Map getNamespaces() {
+    public Map getNamespaces()
+    {
         return namespaces;
     }
 
-    public void setNamespaces(Map namespaces) {
+    public void setNamespaces(Map namespaces)
+    {
         this.namespaces = namespaces;
     }
 
-    public Map getContextProperties() {
+    public Map getContextProperties()
+    {
         return contextProperties;
     }
 
-    public void setContextProperties(Map contextProperties) {
+    public void setContextProperties(Map contextProperties)
+    {
         this.contextProperties = contextProperties;
     }
 
-    public AbstractFactory getFactory() {
+    public AbstractFactory getFactory()
+    {
         return factory;
     }
 
-    public void setFactory(AbstractFactory factory) {
+    public void setFactory(AbstractFactory factory)
+    {
         this.factory = factory;
     }
 
-    public boolean isLenient() {
+    public boolean isLenient()
+    {
         return lenient;
     }
 
-    public void setLenient(boolean lenient) {
+    public void setLenient(boolean lenient)
+    {
         this.lenient = lenient;
     }
 }
--- modules/xml/src/test/java/org/mule/test/filters/xml/JXPathFilterTestCase.java
+++ modules/xml/src/test/java/org/mule/test/filters/xml/JXPathFilterTestCase.java
@@ -11,64 +11,212 @@
 package org.mule.test.filters.xml;
 
 import java.io.InputStream;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.commons.io.IOUtils;
+import org.dom4j.DocumentHelper;
 import org.dom4j.InvalidXPathException;
 import org.mule.impl.MuleMessage;
 import org.mule.routing.filters.xml.JXPathFilter;
 import org.mule.tck.AbstractMuleTestCase;
-import org.mule.util.IOUtils;
+import org.xml.sax.InputSource;
 
-/**
- * @author <a href="mailto:S.Vanmeerhaege@gfdi.be">Vanmeerhaeghe St?phane</a>
- * @author <a href="mailto:carlson@hotpop.com">Travis Carlson</a>
- * @version $Revision$
- */
-public class JXPathFilterTestCase extends AbstractMuleTestCase {
-    private String xmlData = null;
+public class JXPathFilterTestCase extends AbstractMuleTestCase
+{
+    private String xmlStringInput = null;
+    private String xmlStringInputNS = null;
+    private org.dom4j.Document dom4jDocumentInput = null;
+    private org.dom4j.Document dom4jDocumentInputNS = null;
+    private org.w3c.dom.Document w3cDocumentInput = null;
+    private org.w3c.dom.Document w3cDocumentInputNS = null;
+    private JXPathFilter simpleFilter = null;
+    private JXPathFilter nsAwareFilter = null;
 
-    private JXPathFilter myFilter = null;
+    // @SuppressWarnings("unchecked")
+    protected void doSetUp() throws Exception
+    {
+        final ClassLoader currentClassLoader = Thread.currentThread().getContextClassLoader();
 
-    protected void doSetUp() throws Exception {
-        // Read Xml file
-        InputStream is = IOUtils.getResourceAsStream("cdcatalog.xml", getClass());
-        assertNotNull("Test resource not found.", is);
-        xmlData = IOUtils.toString(is);
+        // Read No-Namespace Xml file
+        InputStream is = currentClassLoader.getResourceAsStream("cdcatalog.xml");
+        assertNotNull("Test resource 'cdcatalog.xml' not found.", is);
+        xmlStringInput = IOUtils.toString(is);
+        dom4jDocumentInput = DocumentHelper.parseText(xmlStringInput);
+        w3cDocumentInput = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(
+            new InputSource(new StringReader(xmlStringInput)));
+        simpleFilter = new JXPathFilter();
 
-        myFilter = new JXPathFilter();
+        // Read Namespace Xml file
+        is = currentClassLoader.getResourceAsStream("cdcatalogNS.xml");
+        assertNotNull("Test resource 'cdcatalogNS.xml' not found.", is);
+        xmlStringInputNS = IOUtils.toString(is);
+        dom4jDocumentInputNS = DocumentHelper.parseText(xmlStringInputNS);
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        w3cDocumentInputNS = dbf.newDocumentBuilder().parse(
+            new InputSource(new StringReader(xmlStringInputNS)));
+        nsAwareFilter = new JXPathFilter();
+        Map namespaces = new HashMap();
+        namespaces.put("nsone", "http://one.org");
+        namespaces.put("nstwo", "http://two.org");
+        nsAwareFilter.setNamespaces(namespaces);
     }
 
-    public void testFilter1() throws Exception {
-        myFilter.setExpression("catalog/cd[3]/title");
-        myFilter.setExpectedValue("Greatest Hits");
-        assertTrue(myFilter.accept(new MuleMessage(xmlData)));
+    public void testBogusExpression() throws Exception
+    {
+        try
+        {
+            JXPathFilter myFilter = new JXPathFilter();
+            myFilter.setExpression("foo/bar/");
+            myFilter.accept(new MuleMessage(xmlStringInput));
+            fail("Invalid XPath should have thrown an exception");
+        }
+        catch (InvalidXPathException e)
+        {
+            // expected
+        }
     }
 
-    public void testFilter2() throws Exception {
-        myFilter.setExpression("(catalog/cd[3]/title) ='Greatest Hits'");
-        assertTrue(myFilter.accept(new MuleMessage(xmlData)));
+    private void doTestExpectedValueFilter(Object xmlData) throws Exception
+    {
+        simpleFilter.setExpression("catalog/cd[3]/title");
+        simpleFilter.setExpectedValue("Greatest Hits");
+        assertTrue(simpleFilter.accept(new MuleMessage(xmlData)));
     }
 
-    public void testFilter3() throws Exception {
-        myFilter.setExpression("count(catalog/cd) = 26");
-        assertTrue(myFilter.accept(new MuleMessage(xmlData)));
+    private void doTestBooleanFilter1(Object xmlData) throws Exception
+    {
+        simpleFilter.setExpression("(catalog/cd[3]/title) ='Greatest Hits'");
+        assertTrue(simpleFilter.accept(new MuleMessage(xmlData)));
     }
 
-    public void testFilter4() throws Exception {
+    private void doTestBooleanFilter2(Object xmlData) throws Exception
+    {
+        simpleFilter.setExpression("count(catalog/cd) = 26");
+        assertTrue(simpleFilter.accept(new MuleMessage(xmlData)));
+    }
+
+    private void doTestExpectedValueFilterNS(Object xmlData) throws Exception
+    {
+        nsAwareFilter.setExpression("nsone:catalog/nstwo:cd[3]/title");
+        nsAwareFilter.setExpectedValue("Greatest Hits");
+        assertTrue(nsAwareFilter.accept(new MuleMessage(xmlData)));
+    }
+
+    private void doTestBooleanFilter1NS(Object xmlData) throws Exception
+    {
+        nsAwareFilter.setExpression("(nsone:catalog/nstwo:cd[3]/title) ='Greatest Hits'");
+        assertTrue(nsAwareFilter.accept(new MuleMessage(xmlData)));
+    }
+
+    private void doTestBooleanFilter2NS(Object xmlData) throws Exception
+    {
+        nsAwareFilter.setExpression("count(nsone:catalog/nstwo:cd) = 26");
+        assertTrue(nsAwareFilter.accept(new MuleMessage(xmlData)));
+    }
+
+    public void testFilterOnObject() throws Exception
+    {
         Dummy d = new Dummy();
         d.setId(10);
         d.setContent("hello");
 
-        myFilter.setExpression("id=10 and content='hello'");
-        assertTrue(myFilter.accept(new MuleMessage(d)));
+        simpleFilter.setExpression("id=10 and content='hello'");
+        assertTrue(simpleFilter.accept(new MuleMessage(d)));
     }
 
-    public void testBogusExpression() throws Exception {
-        try {
-            myFilter.setExpression("catalog/cd[3]/banana");
-            myFilter.accept(new MuleMessage(xmlData));
-            // fail("Invalid XPath should have thrown an exception");
-        } catch (InvalidXPathException e) {
-            // expected
-        }
+    public void testExpectedValueFilterXmlString() throws Exception
+    {
+        doTestExpectedValueFilter(xmlStringInput);
     }
+
+    public void testBooleanFilter1XmlString() throws Exception
+    {
+        doTestBooleanFilter1(xmlStringInput);
+    }
+
+    public void testBooleanFilter2XmlString() throws Exception
+    {
+        doTestBooleanFilter2(xmlStringInput);
+    }
+
+    public void testExpectedValueFilterDom4JDocument() throws Exception
+    {
+        doTestExpectedValueFilter(dom4jDocumentInput);
+    }
+
+    public void testBooleanFilter1Dom4JDocument() throws Exception
+    {
+        doTestBooleanFilter1(dom4jDocumentInput);
+    }
+
+    public void testBooleanFilter2Dom4JDocument() throws Exception
+    {
+        doTestBooleanFilter2(dom4jDocumentInput);
+    }
+
+    public void testExpectedValueFilterW3cDocument() throws Exception
+    {
+        doTestExpectedValueFilter(w3cDocumentInput);
+    }
+
+    public void testBooleanFilter1W3cDocument() throws Exception
+    {
+        doTestBooleanFilter1(w3cDocumentInput);
+    }
+
+    public void testBooleanFilter2W3cDocument() throws Exception
+    {
+        doTestBooleanFilter2(w3cDocumentInput);
+    }
+
+    public void testExpectedValueFilterXmlStringNS() throws Exception
+    {
+        doTestExpectedValueFilterNS(xmlStringInputNS);
+    }
+
+    public void testBooleanFilter1XmlStringNS() throws Exception
+    {
+        doTestBooleanFilter1NS(xmlStringInputNS);
+    }
+
+    public void testBooleanFilter2XmlStringNS() throws Exception
+    {
+        doTestBooleanFilter2NS(xmlStringInputNS);
+    }
+
+    public void testExpectedValueFilterDom4JDocumentNS() throws Exception
+    {
+        doTestExpectedValueFilterNS(dom4jDocumentInputNS);
+    }
+
+    public void testBooleanFilter1Dom4JDocumentNS() throws Exception
+    {
+        doTestBooleanFilter1NS(dom4jDocumentInputNS);
+    }
+
+    public void testBooleanFilter2Dom4JDocumentNS() throws Exception
+    {
+        doTestBooleanFilter2NS(dom4jDocumentInputNS);
+    }
+
+    public void testExpectedValueFilterW3cDocumentNS() throws Exception
+    {
+        doTestExpectedValueFilterNS(w3cDocumentInputNS);
+    }
+
+    public void testBooleanFilter1W3cDocumentNS() throws Exception
+    {
+        doTestBooleanFilter1NS(w3cDocumentInputNS);
+    }
+
+    public void testBooleanFilter2W3cDocumentNS() throws Exception
+    {
+        doTestBooleanFilter2NS(w3cDocumentInputNS);
+    }
+
 }
