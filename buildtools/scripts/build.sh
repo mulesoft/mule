eb18bfe2b65a05fc27f34124dc15690ae7359ed1
tcarlson <tcarlson>
2006-09-26 23:18:38 +0000

    Add a nightly build script.  Rearrange existing stuff.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3227 bf997673-6b11-0410-b953-e057580c5b09

---
+++ buildtools/scripts/build.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+##############################################
+# Full nightly build for Mule
+##############################################
+
+# Purge old Mule artifacts from repository
+rm -rf $M2_REPO/org/mule/
+
+# Check-out project if necessary, otherwise update.
+if [ ! -f "mule" ]
+then
+    svn co http://svn.codehaus.org/mule/trunk/mule mule
+    cd mule
+else
+    cd mule
+    svn update
+fi
+
+# Clean everything
+mvn -Ptests,distributions clean
+
+# Build all modules
+mvn -Dmaven.test.skip=true -Ptests install
+
+# Run unit tests only
+mvn -Dmaven.test.failure.ignore=true test
+
+# Run unit and integration tests
+# mvn -Dmaven.test.failure.ignore=true -Ptests test
+
+# Generate HTML reports
+# mvn -Dmaven.test.skip=true -Ptests site:site
+
+# Generate javadocs
+mvn -Dmaven.test.skip=true -Ptests -Daggregate=true javadoc:javadoc
+
+# Generate source bundles
+mvn -Dmaven.test.skip=true -Ptests source:jar
+
+# Generate all distributions
+mvn -Dmaven.test.skip=true -Pdistributions install
