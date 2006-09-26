55e7ad0bd529b8dd94ea9dc1171390d84710abd9
holger <holger>
2006-09-26 09:46:42 +0000

    yummy testcase
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3213 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/core/src/test/java/org/mule/test/util/XStreamFactoryTestCase.java
@@ -0,0 +1,45 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.util;
+
+import com.thoughtworks.xstream.XStream;
+
+import edu.emory.mathcs.backport.java.util.concurrent.ConcurrentHashMap;
+
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+import org.mule.util.StringUtils;
+import org.mule.util.XStreamFactory;
+
+public class XStreamFactoryTestCase extends TestCase
+{
+
+    public void testConcurrentHashMapConverter()
+        throws ClassNotFoundException, IllegalAccessException, InstantiationException
+    {
+        ConcurrentHashMap map = new ConcurrentHashMap();
+        map.put("foo", "bar");
+
+        XStream xstream = new XStreamFactory().getInstance();
+        String mapXML = xstream.toXML(map);
+        assertNotNull(mapXML);
+        assertTrue(StringUtils.isNotEmpty(mapXML));
+
+        Object newMap = xstream.fromXML(mapXML);
+        assertNotNull(newMap);
+        assertTrue(newMap instanceof ConcurrentHashMap);
+        assertEquals(1, ((Map)newMap).size());
+        assertEquals("bar", ((Map)newMap).get("foo"));
+    }
+
+}
