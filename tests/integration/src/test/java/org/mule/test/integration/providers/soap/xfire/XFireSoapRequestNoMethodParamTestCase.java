d9a582fdebb15b3d600f4f999f57e12dda883ba3
Ross Mason <ross@mulesoft.com>
2006-09-27 13:13:41 +0000

    Use higher range port. And Xfire latatest seems to remove uncessary namespace declarations.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3236 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/providers/soap/xfire/XFireSoapRequestNoMethodParamTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/providers/soap/xfire/XFireSoapRequestNoMethodParamTestCase.java
@@ -21,11 +21,11 @@ public class XFireSoapRequestNoMethodParamTestCase extends FunctionalTestCase{
 	{
 		MuleClient client = new MuleClient();
 		
-		UMOMessage msg = client.send("http://localhost:81/services/TestComponent", new MuleMessage("<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soap:Body><receive xmlns=\"http://www.muleumo.org\"><src xmlns=\"http://www.muleumo.org\">Test String</src></receive></soap:Body></soap:Envelope>"));
+		UMOMessage msg = client.send("http://localhost:33381/services/TestComponent", new MuleMessage("<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soap:Body><receive xmlns=\"http://www.muleumo.org\"><src xmlns=\"http://www.muleumo.org\">Test String</src></receive></soap:Body></soap:Envelope>"));
 		
 		assertNotNull(msg);
 		assertNotNull(msg.getPayload());
-		assertEquals(msg.getPayloadAsString(),"<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soap:Body><receiveResponse xmlns=\"http://www.muleumo.org\"><out xmlns=\"http://www.muleumo.org\">Received: Test String</out></receiveResponse></soap:Body></soap:Envelope>");
+		assertEquals(msg.getPayloadAsString(),"<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soap:Body><receiveResponse xmlns=\"http://www.muleumo.org\"><out>Received: Test String</out></receiveResponse></soap:Body></soap:Envelope>");
 	}
 	
 	protected String getConfigResources() {
