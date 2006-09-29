7328f1c0c12d1e25981962c4459c3a83627e58be
holger <holger>
2006-09-29 18:12:42 +0000

    fix line endings and svn properties
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3316 bf997673-6b11-0410-b953-e057580c5b09

--- modules/spring/src/main/java/org/mule/extras/spring/config/MuleResourceLoader.java
+++ modules/spring/src/main/java/org/mule/extras/spring/config/MuleResourceLoader.java
@@ -7,6 +7,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
+
 package org.mule.extras.spring.config;
 
 import java.io.IOException;
@@ -23,30 +24,35 @@ import org.springframework.core.io.support.ResourcePatternResolver;
 /**
  * <code>MuleResourceLoader</code> is a custom Spring resource loader that calls
  * standard Mule methods for loading resource files.
- *
- * @author Travis Carlson
  */
 public class MuleResourceLoader extends DefaultResourceLoader implements ResourcePatternResolver
 {
     protected transient Log logger = LogFactory.getLog(MuleResourceLoader.class);
 
-    public Resource getResource(String rsc) {
+    public Resource getResource(String rsc)
+    {
         return getResourceByPath(rsc);
     }
 
     protected Resource getResourceByPath(String path)
     {
         InputStream is = null;
-        try {
+        try
+        {
             is = IOUtils.getResourceAsStream(path, getClass());
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             logger.error("Unable to load Spring resource " + path + " : " + e.getMessage());
             return null;
         }
 
-        if (is != null) {
+        if (is != null)
+        {
             return new InputStreamResource(is);
-        } else {
+        }
+        else
+        {
             logger.error("Unable to locate Spring resource " + path);
             return null;
         }
@@ -54,13 +60,15 @@ public class MuleResourceLoader extends DefaultResourceLoader implements Resourc
 
     public Resource[] getResources(String rsc) throws IOException
     {
-        if (rsc == null) {
+        if (rsc == null)
+        {
             throw new IOException("No resources to read");
         }
-        String[] resourcesNames =
-            org.springframework.util.StringUtils.tokenizeToStringArray(rsc, ",;", true, true);
+        String[] resourcesNames = org.springframework.util.StringUtils.tokenizeToStringArray(rsc, ",;", true,
+            true);
         Resource[] resources = new Resource[resourcesNames.length];
-        for (int i = 0; i < resourcesNames.length; ++i) {
+        for (int i = 0; i < resourcesNames.length; ++i)
+        {
             resources[i] = getResourceByPath(resourcesNames[i]);
         }
         return resources;
