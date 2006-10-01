a27681089bd565d89dca8541ab123e7cbf036605
Ross Mason <ross@mulesoft.com>
2006-10-01 10:51:19 +0000

    Useful test componet with two service methods with matching signatures
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3334 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/core/src/main/java/org/mule/tck/services/MatchingMethodsComponent.java
@@ -0,0 +1,28 @@
+/*
+ * $Id: $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.tck.services;
+
+import org.mule.util.StringUtils;
+
+import java.rmi.Remote;
+
+/**
+ * A test component that has two service methods with matching signature
+ */
+public class MatchingMethodsComponent implements Remote
+{
+    public String reverseString(String string) {
+        return StringUtils.reverse(string);
+    }
+
+    public String upperCaseString(String string) {
+        return string.toUpperCase();
+    }
+}
