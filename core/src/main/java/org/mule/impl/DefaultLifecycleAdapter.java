97450e5007bc2f59edf952c00a8a73b0e48ba722
Ross Mason <ross@mulesoft.com>
2006-10-02 11:09:53 +0000

    use instance variable
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3362 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/impl/DefaultLifecycleAdapter.java
+++ core/src/main/java/org/mule/impl/DefaultLifecycleAdapter.java
@@ -20,6 +20,7 @@ import org.mule.model.DynamicEntryPointResolver;
 import org.mule.umo.ComponentException;
 import org.mule.umo.Invocation;
 import org.mule.umo.UMODescriptor;
+import org.mule.umo.UMOEvent;
 import org.mule.umo.UMOException;
 import org.mule.umo.UMOMessage;
 import org.mule.umo.lifecycle.Disposable;
@@ -180,24 +181,26 @@ public class DefaultLifecycleAdapter implements UMOLifecycleAdapter
     public UMOMessage intercept(Invocation invocation) throws UMOException
     {
         // Invoke method
-        Object result = null;
+        Object result;
+        UMOEvent event = RequestContext.getEvent();
 
         try {
             result = entryPoint.invoke(component, RequestContext.getEventContext());
         } catch (Exception e) {
             //should all Exceptions caught here be a ComponentException?!?
-            throw new ComponentException(new Message(Messages.FAILED_TO_INVOKE_X, component.getClass().getName()),invocation.getMessage(),RequestContext.getEvent().getComponent(),e);
+            throw new ComponentException(new Message(Messages.FAILED_TO_INVOKE_X, component.getClass().getName()),
+                    invocation.getMessage(), event.getComponent() ,e);
         }
 
         UMOMessage resultMessage = null;
         if (result == null && entryPoint.isVoid()) {
-            resultMessage = new MuleMessage(RequestContext.getEventContext().getTransformedMessage(),
+            resultMessage = new MuleMessage(event.getTransformedMessage(),
                     RequestContext.getEventContext().getMessage());
         } else if (result != null) {
             if (result instanceof UMOMessage) {
                 resultMessage = (UMOMessage) result;
             } else {
-                resultMessage = new MuleMessage(result, RequestContext.getEventContext().getMessage());
+                resultMessage = new MuleMessage(result, event.getMessage());
             }
         }
         return resultMessage;
