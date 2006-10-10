73489267e8fc70e69b584f8a8bfd93cece0fb474
holger <holger>
2006-10-10 23:42:21 +0000

    cleanups, svn props, use a tearDown method that is actually invoked ;)
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3499 bf997673-6b11-0410-b953-e057580c5b09

--- modules/acegi/src/test/java/org/mule/extras/acegi/AuthenticationWithComponentSecurityAsynch.java
+++ modules/acegi/src/test/java/org/mule/extras/acegi/AuthenticationWithComponentSecurityAsynch.java
@@ -12,6 +12,7 @@ package org.mule.extras.acegi;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import org.acegisecurity.context.SecurityContextHolder;
 import org.acegisecurity.context.SecurityContextImpl;
 import org.mule.MuleManager;
@@ -22,51 +23,63 @@ import org.mule.tck.FunctionalTestCase;
 import org.mule.umo.UMOEncryptionStrategy;
 import org.mule.umo.UMOMessage;
 
-public class AuthenticationWithComponentSecurityAsynch extends FunctionalTestCase 
-{       
-        // Clear the security context after each test.
-        public void teardown() {
-            SecurityContextHolder.setContext(new SecurityContextImpl());
-        }
-        
-        public void testCaseGoodAuthenticationGoodAuthorisation() throws Exception{  
-            MuleClient client = new MuleClient();       
-            Map props = new HashMap();
-            
-            UMOEncryptionStrategy strategy = MuleManager.getInstance().getSecurityManager().getEncryptionStrategy("PBE");
-            String header=  MuleCredentials.createHeader("marie", "marie", "PBE", strategy);
-            props.put(MuleProperties.MULE_USER_PROPERTY, header);
-            client.dispatch("vm://test", "Marie",props);
-            UMOMessage m = client.receive("vm://output", 3000);
-            assertNotNull(m);
-            assertEquals((String) m.getPayload(),"Marie");
-        }
-        
-        public void testCaseGoodAuthenticationBadAuthorisation() throws Exception{  
-            MuleClient client = new MuleClient();       
-            Map props = new HashMap();
-            
-            UMOEncryptionStrategy strategy = MuleManager.getInstance().getSecurityManager().getEncryptionStrategy("PBE");
-            String header=  MuleCredentials.createHeader("anon", "anon", "PBE", strategy);
-            props.put(MuleProperties.MULE_USER_PROPERTY, header);
-            client.dispatch("vm://test", "Marie",props);
-            UMOMessage m = client.receive("vm://output", 3000);
-            assertNull(m);
-       }
-        
-        public void testCaseBadAuthentication() throws Exception{  
-            MuleClient client = new MuleClient();       
-            Map props = new HashMap();
-            
-            UMOEncryptionStrategy strategy = MuleManager.getInstance().getSecurityManager().getEncryptionStrategy("PBE");
-            String header=  MuleCredentials.createHeader("anonX", "anonX", "PBE", strategy);
-            props.put(MuleProperties.MULE_USER_PROPERTY, header);
-            client.dispatch("vm://test", "USD,MTL",props);
-            UMOMessage m = client.receive("vm://output", 3000);
-            assertNull(m);
-        }
-                            
-        protected String getConfigResources() {
-            return "test-mule-component-security-asynchronous-config.xml";
-        } 
+public class AuthenticationWithComponentSecurityAsynch extends FunctionalTestCase
+{
+
+    // Clear the security context after each test.
+    public void doFunctionalTearDown()
+    {
+        SecurityContextHolder.setContext(new SecurityContextImpl());
     }
+
+    public void testCaseGoodAuthenticationGoodAuthorisation() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        Map props = new HashMap();
+
+        UMOEncryptionStrategy strategy = MuleManager.getInstance()
+            .getSecurityManager()
+            .getEncryptionStrategy("PBE");
+        String header = MuleCredentials.createHeader("marie", "marie", "PBE", strategy);
+        props.put(MuleProperties.MULE_USER_PROPERTY, header);
+        client.dispatch("vm://test", "Marie", props);
+        UMOMessage m = client.receive("vm://output", 3000);
+        assertNotNull(m);
+        assertEquals((String)m.getPayload(), "Marie");
+    }
+
+    public void testCaseGoodAuthenticationBadAuthorisation() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        Map props = new HashMap();
+
+        UMOEncryptionStrategy strategy = MuleManager.getInstance()
+            .getSecurityManager()
+            .getEncryptionStrategy("PBE");
+        String header = MuleCredentials.createHeader("anon", "anon", "PBE", strategy);
+        props.put(MuleProperties.MULE_USER_PROPERTY, header);
+        client.dispatch("vm://test", "Marie", props);
+        UMOMessage m = client.receive("vm://output", 3000);
+        assertNull(m);
+    }
+
+    public void testCaseBadAuthentication() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        Map props = new HashMap();
+
+        UMOEncryptionStrategy strategy = MuleManager.getInstance()
+            .getSecurityManager()
+            .getEncryptionStrategy("PBE");
+        String header = MuleCredentials.createHeader("anonX", "anonX", "PBE", strategy);
+        props.put(MuleProperties.MULE_USER_PROPERTY, header);
+        client.dispatch("vm://test", "USD,MTL", props);
+        UMOMessage m = client.receive("vm://output", 3000);
+        assertNull(m);
+    }
+
+    protected String getConfigResources()
+    {
+        return "test-mule-component-security-asynchronous-config.xml";
+    }
+}
