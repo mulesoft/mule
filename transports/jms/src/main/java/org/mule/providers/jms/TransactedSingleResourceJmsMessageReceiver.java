2e543e738c59ce9ff1fb41066025bda13f883d2c
stephenfenech <stephenfenech>
2006-10-11 16:33:08 +0000

    Moved some stuff around, so that i don't have access to the actual workers.
    Now i have ONE jms consumer, which once a message is received will create a new Worker & schedules the work.
    
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3516 bf997673-6b11-0410-b953-e057580c5b09

--- transports/jms/src/main/java/org/mule/providers/jms/TransactedSingleResourceJmsMessageReceiver.java
+++ transports/jms/src/main/java/org/mule/providers/jms/TransactedSingleResourceJmsMessageReceiver.java
@@ -3,17 +3,13 @@
  * --------------------------------------------------------------------------------------
  * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
  *
- * The software in this package is published under the terms of the BSD style
+ * The software in this package is published under the terms of the MPL style
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
 package org.mule.providers.jms;
 
-import java.util.List;
-import java.util.Vector;
-
 import org.apache.commons.collections.MapUtils;
-import org.mule.config.ThreadingProfile;
 import org.mule.impl.MuleMessage;
 import org.mule.providers.AbstractMessageReceiver;
 import org.mule.providers.ConnectException;
@@ -27,7 +23,6 @@ import org.mule.umo.UMOTransaction;
 import org.mule.umo.endpoint.UMOEndpoint;
 import org.mule.umo.lifecycle.InitialisationException;
 import org.mule.umo.lifecycle.LifecycleException;
-import org.mule.umo.manager.UMOWorkManager;
 import org.mule.umo.provider.UMOConnector;
 import org.mule.umo.provider.UMOMessageAdapter;
 
@@ -39,37 +34,22 @@ import javax.jms.MessageListener;
 import javax.jms.Session;
 import javax.jms.Topic;
 import javax.resource.spi.work.Work;
-import javax.resource.spi.work.WorkException;
 
-/**
- *
- * Registers a single JmsMessage listener but uses a thread pool to process incoming messages
- * 
- * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
- * @version $Revision: 2180 $
- */
-public class TransactedSingleResourceJmsMessageReceiver extends AbstractMessageReceiver
+public class TransactedSingleResourceJmsMessageReceiver extends AbstractMessageReceiver implements MessageListener
 {
-	//SingleJmsReceiver properties
 	 protected JmsConnector connector;
 	 protected RedeliveryHandler redeliveryHandler;
-	 /*protected MessageConsumer consumer;
-	 protected Session session;*/
+     protected MessageConsumer consumer;
+     protected Session session;
 	 protected boolean startOnConnect = false;
-	 //end of SingleJmsReceiver properties
 	 
-	 //TransactedPollingMessageReceiver properties
+
 	 /** determines whether messages will be received in a transaction template */
 	 protected boolean receiveMessagesInTransaction = true;
 
 	 /** determines whether Multiple receivers are created to improve throughput */
 	 protected boolean useMultipleReceivers = true;
-	//end of TransactedPollingMessageReceiver properties
-	 
-	 //my Properties
-	 List workers=null;
-	 //end of my Properties
-	 
+
 	/**
 	 * @param connector
 	 * @param component
@@ -90,201 +70,122 @@ public class TransactedSingleResourceJmsMessageReceiver extends AbstractMessageR
         }
 	}
 
-	/* (non-Javadoc)
-	 * @see org.mule.providers.AbstractMessageReceiver#doConnect()
-	 */
-	public void doConnect() throws Exception {
-		int numberOfWorkers;
-		MessageReceiverWorker currentWorker=null;
-		
-		//		Connector property overrides any implied value
-        useMultipleReceivers = connector.isCreateMultipleTransactedReceivers();
-        ThreadingProfile tp = connector.getReceiverThreadingProfile();
-        
-        //create Workers
-        if (useMultipleReceivers && receiveMessagesInTransaction && tp.isDoThreading()) {
-        	numberOfWorkers=tp.getMaxThreadsActive();
-            workers=new Vector(numberOfWorkers);
-            for (int i = 0; i < numberOfWorkers; i++) {
-                workers.add(new MessageReceiverWorker(this.connector));
+    public void doConnect() throws Exception
+    {
+        try {
+            JmsSupport jmsSupport = this.connector.getJmsSupport();
+            // Create session if none exists
+            if (session == null) {
+                session = this.connector.getSession(endpoint);
             }
-        } else {
-            workers=new Vector(0);
-            workers.add(new MessageReceiverWorker(this.connector));
-        }
-        
-        //call the doConnect of the Workers
-        for(int i=0;i<workers.size();i++)
-        {
-        	currentWorker=(MessageReceiverWorker)workers.get(i);
-        	currentWorker.doConnect();
-        }   
-	}
-	
-	public void doStart() throws UMOException
-	{
-		UMOWorkManager workManager=getWorkManager();
-		for(int i=0;i<workers.size();i++)
-        {
-			try
-			{
-				workManager.scheduleWork((Work) this.workers.get(i));
-			}catch(WorkException e)
-			{
-				throw new LifecycleException(e, this);
-			}
-        }
-	}
 
-	public void doStop() throws UMOException
-	{
-		for(int i=0;i<workers.size();i++)
-        {
-        	((MessageReceiverWorker)workers.get(i)).doStop();        	
+            // Create destination
+            String resourceInfo = endpoint.getEndpointURI().getResourceInfo();
+            boolean topic = (resourceInfo != null && JmsConstants.TOPIC_PROPERTY.equalsIgnoreCase(resourceInfo));
+
+            //todo MULE20 remove resource Info support
+            if(!topic) {
+                topic = MapUtils.getBooleanValue(endpoint.getProperties(),
+                        JmsConstants.TOPIC_PROPERTY, false);
+            }
+
+            Destination dest = jmsSupport.createDestination(session, endpoint.getEndpointURI().getAddress(), topic);
+
+            // Extract jms selector
+            String selector = null;
+            if (endpoint.getFilter() != null && endpoint.getFilter() instanceof JmsSelectorFilter) {
+                selector = ((JmsSelectorFilter) endpoint.getFilter()).getExpression();
+            } else if (endpoint.getProperties() != null) {
+                // still allow the selector to be set as a property on the endpoint
+                // to be backward compatable
+                selector = (String) endpoint.getProperties().get(JmsConstants.JMS_SELECTOR_PROPERTY);
+            }
+            String tempDurable = (String) endpoint.getProperties().get(JmsConstants.DURABLE_PROPERTY);
+            boolean durable = connector.isDurable();
+            if (tempDurable != null) {
+                durable = Boolean.valueOf(tempDurable).booleanValue();
+            }
+
+            // Get the durable subscriber name if there is one
+            String durableName = (String) endpoint.getProperties().get(JmsConstants.DURABLE_NAME_PROPERTY);
+            if (durableName == null && durable && dest instanceof Topic) {
+                durableName = "mule." + connector.getName() + "." + endpoint.getEndpointURI().getAddress();
+                logger.debug("Jms Connector for this receiver is durable but no durable name has been specified. Defaulting to: "
+                        + durableName);
+            }
+
+            // Create consumer
+            consumer = jmsSupport.createConsumer(session, dest, selector, connector.isNoLocal(), durableName, topic);
+        } catch (JMSException e) {
+            throw new ConnectException(e, this);
         }
-	}
+    }
+
+    public void onMessage(Message message)
+    {
+        try {
+            getWorkManager().scheduleWork(new MessageReceiverWorker(message));
+        } catch (Exception e) {
+            handleException(e);
+        }
+    }
+
 	
-	/* (non-Javadoc)
-	 * @see org.mule.providers.AbstractMessageReceiver#doDisconnect()
-	 */
-	public void doDisconnect() throws Exception {
-		for(int i=0;i<workers.size();i++)
-        {
-        	((MessageReceiverWorker)workers.get(i)).doDisconnect();
-        }		
-	}
+    public void doStart() throws UMOException {
+        try {
+            //We ned to register the listener when start is called in order to only start receiving messages after
+            //start/
+            //If the consumer is null it means that the connection strategy is being run in a separate thread
+            //And hasn't managed to connect yet.
+            if(consumer==null)  {
+                startOnConnect=true;
+            } else {
+                startOnConnect=false;
+                
+                this.consumer.setMessageListener(this);
+            }
+        } catch (JMSException e) {
+            throw new LifecycleException(e, this);
+        }
+    }
+
+    public void doStop() throws UMOException {
+        try {
+            if(consumer!=null) {
+                consumer.setMessageListener(null);
+            }
+        } catch (JMSException e) {
+            throw new LifecycleException(e, this);
+        }
+    }
+	
+    public void doDisconnect() throws Exception
+    {
+        closeConsumer();
+    }
+    
+    protected void closeConsumer()
+    {
+        connector.closeQuietly(consumer);
+        consumer = null;
+        connector.closeQuietly(session);
+        session = null;
+    }
 	
 	
 	
-	protected class MessageReceiverWorker implements Work,MessageListener
+	protected class MessageReceiverWorker implements Work
 	{
-		//SingleJmsMessageReceiver
-		protected JmsConnector connector;
-	    protected MessageConsumer consumer;
-	    protected Session session;
+        Message message;
+		
 	    
-	    public MessageReceiverWorker(JmsConnector connector)
+	    public MessageReceiverWorker(Message message)
 	    {
-	    	this.connector=connector;
+	    	this.message=message;
 	    }
 
-		/* (non-Javadoc)
-		 * @see javax.resource.spi.work.Work#release()
-		 */
-		public void release() {
-			// TODO Auto-generated method stub
-			
-		}
 
-		/* (non-Javadoc)
-		 * @see java.lang.Runnable#run()
-		 */
-		public void run(){
-			try
-			{
-				this.doStart();
-			}catch(Exception e)
-			{
-				//TODO do something with the exception!
-			}
-		}
-		
-		public void doConnect() throws Exception
-		{
-			try {
-	            JmsSupport jmsSupport = this.connector.getJmsSupport();
-	            // Create session if none exists
-	            if (session == null) {
-	                session = this.connector.getSession(endpoint);
-	            }
-
-	            // Create destination
-	            String resourceInfo = endpoint.getEndpointURI().getResourceInfo();
-	            boolean topic = (resourceInfo != null && JmsConstants.TOPIC_PROPERTY.equalsIgnoreCase(resourceInfo));
-
-	            //todo MULE20 remove resource Info support
-	            if(!topic) {
-	                topic = MapUtils.getBooleanValue(endpoint.getProperties(),
-	                        JmsConstants.TOPIC_PROPERTY, false);
-	            }
-
-	            Destination dest = jmsSupport.createDestination(session, endpoint.getEndpointURI().getAddress(), topic);
-
-	            // Extract jms selector
-	            String selector = null;
-	            if (endpoint.getFilter() != null && endpoint.getFilter() instanceof JmsSelectorFilter) {
-	                selector = ((JmsSelectorFilter) endpoint.getFilter()).getExpression();
-	            } else if (endpoint.getProperties() != null) {
-	                // still allow the selector to be set as a property on the endpoint
-	                // to be backward compatable
-	                selector = (String) endpoint.getProperties().get(JmsConstants.JMS_SELECTOR_PROPERTY);
-	            }
-	            String tempDurable = (String) endpoint.getProperties().get(JmsConstants.DURABLE_PROPERTY);
-	            boolean durable = connector.isDurable();
-	            if (tempDurable != null) {
-	                durable = Boolean.valueOf(tempDurable).booleanValue();
-	            }
-
-	            // Get the durable subscriber name if there is one
-	            String durableName = (String) endpoint.getProperties().get(JmsConstants.DURABLE_NAME_PROPERTY);
-	            if (durableName == null && durable && dest instanceof Topic) {
-	                durableName = "mule." + connector.getName() + "." + endpoint.getEndpointURI().getAddress();
-	                logger.debug("Jms Connector for this receiver is durable but no durable name has been specified. Defaulting to: "
-	                        + durableName);
-	            }
-
-	            // Create consumer
-	            consumer = jmsSupport.createConsumer(session, dest, selector, connector.isNoLocal(), durableName, topic);
-	        } catch (JMSException e) {
-	            throw new ConnectException(e, this);
-	        }
-		}
-		
-		public void doStart() throws UMOException {
-	        try {
-	            //We ned to register the listener when start is called in order to only start receiving messages after
-	            //start/
-	            //If the consumer is null it means that the connection strategy is being run in a separate thread
-	            //And hasn't managed to connect yet.
-	            if(consumer==null)  {
-	                startOnConnect=true;
-	            } else {
-	                startOnConnect=false;
-	                
-	                this.consumer.setMessageListener(this);
-	            }
-	        } catch (JMSException e) {
-	            throw new LifecycleException(e, this);
-	        }
-	    }
-
-		public void doStop() throws UMOException {
-	        try {
-	            if(consumer!=null) {
-	                consumer.setMessageListener(null);
-	            }
-	        } catch (JMSException e) {
-	            throw new LifecycleException(e, this);
-	        }
-	    }
-		
-		public void doDisconnect() throws Exception
-	    {
-	        closeConsumer();
-	    }
-		
-		protected void closeConsumer()
-	    {
-	        connector.closeQuietly(consumer);
-	        consumer = null;
-	        connector.closeQuietly(session);
-	        session = null;
-	    }
-		
-		/* (non-Javadoc)
-		 * @see javax.jms.MessageListener#onMessage(javax.jms.Message)
-		 */
-		public void onMessage(Message message) {
+		public void run() {
 			try {
             	TransactionTemplate tt = new TransactionTemplate(endpoint.getTransactionConfig(),
                         connector.getExceptionListener());
@@ -341,6 +242,12 @@ public class TransactedSingleResourceJmsMessageReceiver extends AbstractMessageR
             }
 			
 		}
+
+        public void release()
+        {
+            // Nothing to release.
+            
+        }
 		
 	}
 	
