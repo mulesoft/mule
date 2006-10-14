42ff308fa06c467cb6208e3f22066d6ec6e7c1db
holger <holger>
2006-10-14 17:30:35 +0000

    MULE-1127: JMS transformers are now stateless (no more stale Session state). The getSession() method is now used every time a Session is required, which is effectively what happened before anyway. Also the requirement to pass a Serssion into transform(..) was removed; tests can now either use Mocks or override getSession() to return the desired JMS test session of choice when no endpoint is set on the transformer. AbstractJmsTransformer.encodeHeader(String) now creates new Strings only when necessary. All generic JMS, ActiveMQ and Spring-JMS tests now pass again.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3553 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/providers/jms/activemq/JmsTransformersTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/providers/jms/activemq/JmsTransformersTestCase.java
@@ -10,148 +10,150 @@
 
 package org.mule.test.integration.providers.jms.activemq;
 
+import java.io.File;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.jms.BytesMessage;
+import javax.jms.MapMessage;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
 import org.activemq.ActiveMQConnectionFactory;
 import org.activemq.broker.impl.BrokerContainerFactoryImpl;
 import org.activemq.store.vm.VMPersistenceAdapter;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.mule.impl.RequestContext;
 import org.mule.providers.jms.transformers.AbstractJmsTransformer;
 import org.mule.providers.jms.transformers.JMSMessageToObject;
 import org.mule.providers.jms.transformers.ObjectToJMSMessage;
 import org.mule.tck.AbstractMuleTestCase;
 
-import javax.jms.BytesMessage;
-import javax.jms.JMSException;
-import javax.jms.MapMessage;
-import javax.jms.ObjectMessage;
-import javax.jms.Session;
-import javax.jms.TextMessage;
-
-import java.io.File;
-import java.util.Map;
-import java.util.Properties;
-
 /**
- * <code>JmsTransformersTestCase</code> Tests the JMS transformer impls.
- * 
- * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
- * @version $Revision$
+ * <code>JmsTransformersTestCase</code> Tests the JMS transformer implementations.
  */
 
 public class JmsTransformersTestCase extends AbstractMuleTestCase
 {
-
     private static Session session = null;
-    /**
-     * logger used by this class
-     */
-    private static transient Log logger = LogFactory.getLog(JmsTransformersTestCase.class);
 
-    protected ActiveMQConnectionFactory factory = null;
+    protected void suitePreSetUp() throws Exception
+    {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
+        factory.setBrokerContainerFactory(new BrokerContainerFactoryImpl(new VMPersistenceAdapter()));
+        factory.setUseEmbeddedBroker(true);
+        factory.setBrokerURL("vm://localhost");
+        factory.start();
 
+        session = factory.createConnection().createSession(false, Session.AUTO_ACKNOWLEDGE);
+    }
 
+    protected void doTearDown() throws Exception
+    {
+        RequestContext.setEvent(null);
+    }
 
-        protected void suitePreSetUp() throws Exception {
-            factory = new ActiveMQConnectionFactory();
-            factory.setBrokerContainerFactory(new BrokerContainerFactoryImpl(new VMPersistenceAdapter()));
-            factory.setUseEmbeddedBroker(true);
-            factory.setBrokerURL("vm://localhost");
-            factory.start();
-
-            session = factory.createConnection().createSession(false, Session.AUTO_ACKNOWLEDGE);
-        }
-
-        protected void doTearDown() throws Exception {
-            RequestContext.setEvent(null);
-        }
-
-     protected void suitePostTearDown() throws Exception {
-            try {
-                session.close();
-            } catch (JMSException e) {
-                // TODO shoudln't this be handled?
-            }
-        }
+    protected void suitePostTearDown() throws Exception
+    {
+        session.close();
+        session = null;
+    }
 
     public void testTransObjectMessage() throws Exception
     {
         RequestContext.setEvent(getTestEvent("test"));
-        logger.debug("testTransObjectMessage()");
+
         ObjectMessage oMsg = session.createObjectMessage();
         File f = new File("C:/testdata/tests.txt");
         oMsg.setObject(f);
-        logger.debug("created object Message");
         AbstractJmsTransformer trans = new JMSMessageToObject();
-        Object result = trans.transform(oMsg, session);
-        logger.debug("transformed object is of type " + result.getClass().getName());
+        Object result = trans.transform(oMsg);
         assertTrue("Transformed object should be a file", result.getClass().equals(File.class));
-        AbstractJmsTransformer trans2 = new ObjectToJMSMessage();
+
+        AbstractJmsTransformer trans2 = new SessionEnabledObjectToJMSMessage(session);
         trans2.setReturnClass(ObjectMessage.class);
-        Object result2 = trans2.transform(f, session);
-        logger.debug("transformed object is of type " + result2.getClass().getName());
+        Object result2 = trans2.transform(f);
         assertTrue("Transformed object should be an object message", result2 instanceof ObjectMessage);
     }
 
     public void testTransTextMessage() throws Exception
     {
         RequestContext.setEvent(getTestEvent("test"));
+
         String text = "This is a tests Text Message";
-        logger.debug("testTransTextMessage()");
         TextMessage tMsg = session.createTextMessage();
         tMsg.setText(text);
-        logger.debug("created Text Message: " + tMsg);
+
         AbstractJmsTransformer trans = new JMSMessageToObject();
-        Object result = trans.transform(tMsg, session);
-        logger.debug("transformed object is of type " + result.getClass().getName());
+        Object result = trans.transform(tMsg);
         assertTrue("Transformed object should be a string", text.equals(result.toString()));
-        AbstractJmsTransformer trans2 = new ObjectToJMSMessage();
+
+        AbstractJmsTransformer trans2 = new SessionEnabledObjectToJMSMessage(session);
         trans2.setReturnClass(TextMessage.class);
-        Object result2 = trans2.transform(text, session);
-        logger.debug("transformed object is of type " + result2.getClass().getName());
+        Object result2 = trans2.transform(text);
         assertTrue("Transformed object should be an Text message", result2 instanceof TextMessage);
     }
 
     public void testTransMapMessage() throws Exception
     {
         RequestContext.setEvent(getTestEvent("test"));
-        logger.debug("testTransMApMessage()");
+
         Properties p = new Properties();
         p.setProperty("Key1", "Value1");
         p.setProperty("Key2", "Value2");
         p.setProperty("Key3", "Value3");
-        AbstractJmsTransformer trans = new ObjectToJMSMessage();
+
+        AbstractJmsTransformer trans = new SessionEnabledObjectToJMSMessage(session);
         trans.setReturnClass(MapMessage.class);
-        Object result2 = trans.transform(p, session);
-        logger.debug("transformed object is of type " + result2.getClass().getName());
+        Object result2 = trans.transform(p);
         assertTrue("Transformed object should be a Map message", result2 instanceof MapMessage);
-        MapMessage mMsg = (MapMessage) result2;
-        logger.debug("created Map Message: " + mMsg);
+
+        MapMessage mMsg = (MapMessage)result2;
         AbstractJmsTransformer trans2 = new JMSMessageToObject();
         trans2.setReturnClass(Map.class);
-        Object result = trans2.transform(mMsg, session);
-        logger.debug("transformed object is of type " + result.getClass().getName());
-        assertTrue("Transformed object should be a MAP", result instanceof Map);
+        Object result = trans2.transform(mMsg);
+        assertTrue("Transformed object should be a Map", result instanceof Map);
     }
 
     public void testTransByteMessage() throws Exception
     {
         RequestContext.setEvent(getTestEvent("test"));
-        logger.debug("testTransByteMessage()");
-        String text = "This is a tests Byte Message";
-        AbstractJmsTransformer trans = new ObjectToJMSMessage();
+
+        AbstractJmsTransformer trans = new SessionEnabledObjectToJMSMessage(session);
         trans.setReturnClass(BytesMessage.class);
-        Object result2 = trans.transform(text.getBytes(), session);
-        logger.debug("transformed object is of type " + result2.getClass().getName());
+        String text = "This is a tests Byte Message";
+        Object result2 = trans.transform(text.getBytes());
         assertTrue("Transformed object should be a Bytes message", result2 instanceof BytesMessage);
-        BytesMessage bMsg = (BytesMessage) result2;
+
         AbstractJmsTransformer trans2 = new JMSMessageToObject();
         trans2.setReturnClass(byte[].class);
-        Object result = trans2.transform(bMsg, session);
-        logger.debug("transformed object is of type " + result.getClass().getName());
+        BytesMessage bMsg = (BytesMessage)result2;
+        Object result = trans2.transform(bMsg);
         assertTrue("Transformed object should be a byte[]", result instanceof byte[]);
-        String res = new String((byte[]) result);
-        logger.debug("result message is " + res);
+        String res = new String((byte[])result);
         assertTrue("source and result messages should be the same", text.equals(res));
     }
+
+    /*
+     * This class overrides getSession() to return the specified test Session;
+     * otherwise we would need a full-fledged JMS connector with dispatchers etc.
+     */
+    public static class SessionEnabledObjectToJMSMessage extends ObjectToJMSMessage
+    {
+        private static final long serialVersionUID = -440672187466417761L;
+        private final Session _session;
+
+        public SessionEnabledObjectToJMSMessage(Session session)
+        {
+            super();
+            _session = session;
+        }
+
+        // @Override
+        protected Session getSession()
+        {
+            return _session;
+        }
+    }
+
 }
