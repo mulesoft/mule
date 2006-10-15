f58e8aa31fa08e9de4e08299fe49e74b8c70e12f
holger <holger>
2006-10-15 17:31:14 +0000

    delete dead comment
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3563 bf997673-6b11-0410-b953-e057580c5b09

--- transports/oaq/src/main/java/org/mule/providers/oracle/jms/OracleJmsMessageAdapter.java
+++ transports/oaq/src/main/java/org/mule/providers/oracle/jms/OracleJmsMessageAdapter.java
@@ -17,73 +17,78 @@ import org.mule.providers.jms.JmsMessageAdapter;
 import org.mule.umo.MessagingException;
 
 /**
- * If the message payload is XML, returns the XML as a string.
- * If the message payload is an ADT, simply returns {@code Object.toString()} in
- * order to avoid a null pointer exception.
- * Any other message is handled by the standard {@code JmsMessageAdapter}
- *
- * @author <a href="mailto:carlson@hotpop.com">Travis Carlson</a>
+ * If the message payload is XML, returns the XML as a string. If the message payload
+ * is an ADT, simply returns {@code Object.toString()} in order to avoid a null
+ * pointer exception. Any other message is handled by the standard
+ * {@code JmsMessageAdapter}.
  */
-public class OracleJmsMessageAdapter extends JmsMessageAdapter {
+public class OracleJmsMessageAdapter extends JmsMessageAdapter
+{
 
     /**
      * Serial version
      */
     private static final long serialVersionUID = -5304537031626649816L;
 
-    public OracleJmsMessageAdapter(Object message) throws MessagingException {
+    public OracleJmsMessageAdapter(Object message) throws MessagingException
+    {
         super(message);
     }
 
-    /** If the message payload is XML, returns the XML as an array of bytes.
-     * If the message payload is an ADT, simply returns {@code Object.toString().getBytes()}
-     * in order to avoid a null pointer exception.
-     * Any other message is handled by the standard {@code JmsMessageAdapter}
-     *
-     * @see JmsMessageAdapter#getPayloadAsBytes */
-    public byte[] getPayloadAsBytes() throws Exception {
+    /**
+     * If the message payload is XML, returns the XML as an array of bytes. If the
+     * message payload is an ADT, simply returns {@code Object.toString().getBytes()}
+     * in order to avoid a null pointer exception. Any other message is handled by
+     * the standard {@code JmsMessageAdapter}
+     * 
+     * @see JmsMessageAdapter#getPayloadAsBytes
+     */
+    public byte[] getPayloadAsBytes() throws Exception
+    {
         Object jmsMessage = getPayload();
-        if (jmsMessage instanceof AdtMessage) {
-            Object adtMessage = ((AdtMessage) jmsMessage).getAdtPayload();
-            if (adtMessage instanceof XMLType) {
-                return ((XMLType) adtMessage).getBytesValue();
+        if (jmsMessage instanceof AdtMessage)
+        {
+            Object adtMessage = ((AdtMessage)jmsMessage).getAdtPayload();
+            if (adtMessage instanceof XMLType)
+            {
+                return ((XMLType)adtMessage).getBytesValue();
             }
-            else {
+            else
+            {
                 return adtMessage.toString().getBytes(getEncoding());
             }
         }
-        else {
+        else
+        {
             return super.getPayloadAsBytes();
         }
     }
 
-    /** If the message payload is XML, returns the XML as a string.
-     * If the message payload is an ADT, simply returns {@code Object.toString()} in
-     * order to avoid a null pointer exception.
-     * Any other message is handled by the standard {@code JmsMessageAdapter}
-     *
-     * @see JmsMessageAdapter#getPayloadAsString */
-
     /**
      * Converts the message implementation into a String representation
-     *
-     * @param encoding The encoding to use when transforming the message (if necessary). The parameter is
-     *                 used when converting from a byte array
+     * 
+     * @param encoding The encoding to use when transforming the message (if
+     *            necessary). The parameter is used when converting from a byte array
      * @return String representation of the message payload
      * @throws Exception Implementation may throw an endpoint specific exception
      */
-    public String getPayloadAsString(String encoding) throws Exception {
+    public String getPayloadAsString(String encoding) throws Exception
+    {
         Object jmsMessage = getPayload();
-        if (jmsMessage instanceof AdtMessage) {
-            Object adtMessage = ((AdtMessage) jmsMessage).getAdtPayload();
-            if (adtMessage instanceof XMLType) {
-                return ((XMLType) adtMessage).getStringVal();
+        if (jmsMessage instanceof AdtMessage)
+        {
+            Object adtMessage = ((AdtMessage)jmsMessage).getAdtPayload();
+            if (adtMessage instanceof XMLType)
+            {
+                return ((XMLType)adtMessage).getStringVal();
             }
-            else {
+            else
+            {
                 return adtMessage.toString();
             }
         }
-        else {
+        else
+        {
             return super.getPayloadAsString(encoding);
         }
     }
