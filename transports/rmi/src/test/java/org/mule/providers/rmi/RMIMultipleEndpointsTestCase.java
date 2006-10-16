ecdd9d8b2199deee9b1532cb47c0466f7b5b03a0
aperepel@gmail.com <Andrew Perepelytsya>
2006-10-16 15:56:11 +0000

    Must compile under JDK 1.4. The test case still fails with ClassNotFoundException: org.mule.factory.rmi.MuleRMIFactory
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3587 bf997673-6b11-0410-b953-e057580c5b09

--- transports/rmi/src/test/java/org/mule/providers/rmi/RMIMultipleEndpointsTestCase.java
+++ transports/rmi/src/test/java/org/mule/providers/rmi/RMIMultipleEndpointsTestCase.java
@@ -6,31 +6,32 @@ import org.mule.tck.FunctionalTestCase;
 import org.mule.umo.UMOMessage;
 
 public class RMIMultipleEndpointsTestCase extends FunctionalTestCase{
-        
-    public void testCase() throws Exception{        
-        MuleClient client = new MuleClient();   
-        
+
+    public void testCase() throws Exception
+    {
+        MuleClient client = new MuleClient();
+
         //send Echo String
-        UMOMessage message = client.send("vm://testin",Integer.valueOf(12),null);
+        UMOMessage message = client.send("vm://testin", new Integer(12),null);
         assertNotNull(message);
-        Integer payload = (Integer)message.getPayload();
-        assertEquals(payload,Integer.valueOf(22));
-                
+        Integer payload = (Integer) message.getPayload();
+        assertEquals(payload, new Integer(22));
+
         //send String
-        message = client.send("vm://testin","test matching component first time",null);
+        message = client.send("vm://testin", "test matching component first time",null);
         assertNotNull(message);
-        assertEquals((String)message.getPayload(),"emit tsrif tnenopmoc gnihctam tset");
-        
+        assertEquals((String) message.getPayload(), "emit tsrif tnenopmoc gnihctam tset");
+
         //send String
-        message = client.send("vm://testin","test mathching component second time",null);
+        message = client.send("vm://testin", "test mathching component second time",null);
         assertNotNull(message);
-        assertEquals((String)message.getPayload(),"emit dnoces tnenopmoc gnihchtam tset");
-        
+        assertEquals((String) message.getPayload(), "emit dnoces tnenopmoc gnihchtam tset");
+
         //send Integer
-        message = client.send("vm://testin",Integer.valueOf(15),null);
+        message = client.send("vm://testin", new Integer(15),null);
         assertNotNull(message);
-        payload = (Integer)message.getPayload();
-        assertEquals(payload,Integer.valueOf(25));        
+        payload = (Integer) message.getPayload();
+        assertEquals(payload, new Integer(25));
     }
 
     protected String getConfigResources() {
