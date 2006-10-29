4320d9157bff030f0cfeb8a8b90e994a3854d02e
aperepel@gmail.com <Andrew Perepelytsya>
2006-10-29 17:13:25 +0000

    Re-enable the test, update it.
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3736 bf997673-6b11-0410-b953-e057580c5b09

--- modules/management/src/test/java/org/mule/management/agents/Mx4jAgentTestCase.java
+++ modules/management/src/test/java/org/mule/management/agents/Mx4jAgentTestCase.java
@@ -12,19 +12,21 @@ package org.mule.management.agents;
 
 import mx4j.tools.adaptor.http.HttpAdaptor;
 import org.mule.management.AbstractMuleJmxTestCase;
+import org.mule.management.support.JmxModernSupport;
+import org.mule.management.support.JmxSupport;
 
 import javax.management.ObjectName;
 
 /**
- * Test that the HttpAdaptor lifecycle is properly managed
+ * Test that the HttpAdaptor lifecycle is properly managed.
  */
 public class Mx4jAgentTestCase extends AbstractMuleJmxTestCase
 {
     public void testRedeploy() throws Exception
     {
-
-        mBeanServer.registerMBean(new HttpAdaptor(),
-            ObjectName.getInstance(Mx4jAgent.HTTP_ADAPTER_OBJECT_NAME));
+        final String name = JmxSupport.DEFAULT_JMX_DOMAIN_PREFIX +
+                                  ".Mx4jAgentTest:" + Mx4jAgent.HTTP_ADAPTER_OBJECT_NAME;
+        mBeanServer.registerMBean(new HttpAdaptor(), ObjectName.getInstance(name));
 
         Mx4jAgent agent = new Mx4jAgent();
         agent.initialise();
