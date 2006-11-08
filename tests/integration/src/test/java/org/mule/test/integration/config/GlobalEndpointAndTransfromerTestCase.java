7a3e736fdf66de7b352e274e016c2f1d196e1d54
stephenfenech <stephenfenech>
2006-11-08 10:24:52 +0000

    Test to Show that MULE-610 is not an issue anymore
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3841 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/integration/config/GlobalEndpointAndTransfromerTestCase.java
@@ -0,0 +1,38 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the BSD style
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.integration.config;
+
+import org.mule.extras.client.MuleClient;
+import org.mule.impl.MuleMessage;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.umo.UMOException;
+import org.mule.umo.UMOMessage;
+
+/*
+ * This test has been added due to MULE-610
+ */
+public class GlobalEndpointAndTransfromerTestCase extends FunctionalTestCase
+{
+    
+    public void testNormal() throws UMOException
+    {
+        MuleClient client=new MuleClient();
+        UMOMessage msg=client.send("vm://in",new MuleMessage("HELLO!"));
+        assertTrue(msg.getPayload() instanceof byte[]);        
+    }
+
+    protected String getConfigResources()
+    {        
+        return "org/mule/test/integration/config/globalendpointandtransformer-mule-config.xml";
+    }
+}
+
+
