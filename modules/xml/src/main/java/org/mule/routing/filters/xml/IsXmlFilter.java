99b1d1c8ada4046d4370ff0323d54aee34bb2066
holger <holger>
2006-11-19 16:13:19 +0000

    MULE-1055: XML payload validation with StAX (up to 10 times faster :-)
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3930 bf997673-6b11-0410-b953-e057580c5b09

--- modules/xml/src/main/java/org/mule/routing/filters/xml/IsXmlFilter.java
+++ modules/xml/src/main/java/org/mule/routing/filters/xml/IsXmlFilter.java
@@ -13,18 +13,24 @@ package org.mule.routing.filters.xml;
 import java.io.ByteArrayInputStream;
 import java.io.StringReader;
 
-import org.dom4j.DocumentException;
-import org.dom4j.io.SAXReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
 import org.mule.umo.UMOFilter;
 import org.mule.umo.UMOMessage;
-import org.xml.sax.InputSource;
 
 /**
  * <code>IsXmlFilter</code> accepts a String or byte[] if its contents are valid
  * (well-formed) XML.
  */
+// @ThreadSafe
 public class IsXmlFilter implements UMOFilter
 {
+    private final XMLInputFactory factory = XMLInputFactory.newInstance();
+
+    // TODO: add validation against a DTD, see MULE-1055
 
     public IsXmlFilter()
     {
@@ -38,26 +44,48 @@ public class IsXmlFilter implements UMOFilter
 
     private boolean accept(Object obj)
     {
+        XMLStreamReader parser = null;
+
         try
         {
             if (obj instanceof String)
             {
-                new SAXReader().read(new StringReader((String)obj));
+                parser = factory.createXMLStreamReader(new StringReader((String)obj));
             }
             else if (obj instanceof byte[])
             {
-                new SAXReader().read(new InputSource(new ByteArrayInputStream((byte[])obj)));
+                parser = factory.createXMLStreamReader(new ByteArrayInputStream((byte[])obj));
             }
             else
             {
-                throw new DocumentException("Object must be a String or byte array");
+                // neither String nor byte[]
+                return false;
+            }
+
+            while (parser.next() != XMLStreamConstants.END_DOCUMENT)
+            {
+                // meep meep!
             }
 
             return true;
         }
-        catch (DocumentException e)
+        catch (XMLStreamException ex)
         {
             return false;
+        }
+        finally
+        {
+            if (parser != null)
+            {
+                try
+                {
+                    parser.close();
+                }
+                catch (XMLStreamException ignored)
+                {
+                    // bummer
+                }
+            }
         }
     }
 
