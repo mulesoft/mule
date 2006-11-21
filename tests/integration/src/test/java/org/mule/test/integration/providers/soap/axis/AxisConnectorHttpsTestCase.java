42a1326b49adc8ee9b8b52eb0ee7826a58ebce4d
Marie.Rizzo <Marie.Rizzo>
2006-11-21 15:18:00 +0000

    Updated test
    git-svn-id: https://svn.codehaus.org/mule/trunk/mule@3966 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/providers/soap/axis/AxisConnectorHttpsTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/providers/soap/axis/AxisConnectorHttpsTestCase.java
@@ -12,7 +12,10 @@ package org.mule.test.integration.providers.soap.axis;
 
 import org.mule.extras.client.MuleClient;
 import org.mule.impl.MuleMessage;
+import org.mule.impl.RequestContext;
+import org.mule.providers.http.HttpsConnector;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.umo.UMOEvent;
 import org.mule.umo.UMOMessage;
 
 public class AxisConnectorHttpsTestCase extends FunctionalTestCase
@@ -22,6 +25,11 @@ public class AxisConnectorHttpsTestCase extends FunctionalTestCase
         MuleClient client = new MuleClient();
         UMOMessage m = client.send("axis:https://localhost:82/TestUMO?method=echo",new MuleMessage("hello"));
         assertNotNull(m);
+        
+        // check that our https connector is being used
+        UMOEvent event = RequestContext.getEvent();
+        assertTrue (event.getEndpoint().getConnector() instanceof HttpsConnector);
+        assertTrue(event.getEndpoint().getConnector().getName().equals("myHttpsConnector"));
     }
     
     protected String getConfigResources()
