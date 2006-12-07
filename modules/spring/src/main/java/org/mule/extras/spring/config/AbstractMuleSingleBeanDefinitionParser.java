e80213e9a4700c5b9db17684435acb384e35f913
Ross Mason <ross@mulesoft.com>
2006-12-07 16:35:36 +0000

    Work in progress for the namespace handlers
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@4166 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/spring/src/main/java/org/mule/extras/spring/config/AbstractMuleSingleBeanDefinitionParser.java
@@ -0,0 +1,53 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extras.spring.config;
+
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.beans.factory.xml.AbstractSimpleBeanDefinitionParser;
+import org.w3c.dom.Element;
+
+import java.util.Properties;
+
+/**
+ * todo document
+ *
+ */
+public abstract class AbstractMuleSingleBeanDefinitionParser extends AbstractSimpleBeanDefinitionParser
+{
+
+    //public static final String ID_ATTRIBUTE = "id";
+    //public static final String LOCAL_NAMESPACE = "http://mule.mulesource.org/schema/";
+
+    //Make the registry available to the parsers
+    protected BeanDefinitionRegistry registry;
+    protected Properties attributeMappings = new Properties();
+
+
+    protected String extractPropertyName(String attributeName)
+    {
+        attributeName = getAttributeMapping(attributeName);
+        return super.extractPropertyName(attributeName);
+    }
+
+    protected abstract Class getBeanClass(Element element);
+
+    protected void postProcess(RootBeanDefinition beanDefinition, Element element) {
+
+    }
+
+    protected void registerAttributeMapping(String alias, String propertyName) {
+        attributeMappings.put(alias, propertyName);
+    }
+
+    protected String getAttributeMapping(String alias) {
+        return attributeMappings.getProperty(alias, alias);
+    }
+}
