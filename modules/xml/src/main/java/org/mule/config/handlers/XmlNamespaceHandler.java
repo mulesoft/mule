8877ef145a59fea1ea6af68edf4fcbbfe119138d
aperepel@gmail.com <Andrew Perepelytsya>
2007-01-20 04:09:22 +0000

    Align with the root package hierarchy for handlers and parsers.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@4788 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/xml/src/main/java/org/mule/config/handlers/XmlNamespaceHandler.java
@@ -0,0 +1,28 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.config.handlers;
+
+import org.mule.config.handlers.AbstractHierarchicalNamespaceHandler;
+import org.mule.config.parsers.JXPathFilterParser;
+import org.mule.config.parsers.NamespaceDefinitionParser;
+import org.mule.routing.filters.xml.JXPathFilter;
+
+/**
+ * TODO
+ */
+public class XmlNamespaceHandler extends AbstractHierarchicalNamespaceHandler
+{
+
+    public void init()
+    {
+        registerBeanDefinitionParser("jxpath-filter", new JXPathFilterParser("filter", JXPathFilter.class));
+        registerBeanDefinitionParser("namespace", new NamespaceDefinitionParser());
+    }
+}
