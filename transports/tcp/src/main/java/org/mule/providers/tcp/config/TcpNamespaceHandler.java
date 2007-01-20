415c081913c225e624f8175ea00604121b966b14
aperepel@gmail.com <Andrew Perepelytsya>
2007-01-20 03:09:57 +0000

    Move to the main source tree, and under the config sub-package to be consistent with the rest.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@4782 bf997673-6b11-0410-b953-e057580c5b09

--- transports/tcp/src/main/java/org/mule/providers/tcp/config/TcpNamespaceHandler.java
+++ transports/tcp/src/main/java/org/mule/providers/tcp/config/TcpNamespaceHandler.java
@@ -7,11 +7,12 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.providers.tcp;
+package org.mule.providers.tcp.config;
 
 import org.w3c.dom.Element;
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
 import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
+import org.mule.providers.tcp.TcpConnector;
 
 /**
  * todo document
