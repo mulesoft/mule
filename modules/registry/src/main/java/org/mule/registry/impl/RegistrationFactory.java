51acc605da5615656bb4e45269bf99488cbdf371
Lajos <Lajos>
2007-01-23 22:43:57 +0000

    Maybe this would help ...
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@4861 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/registry/src/main/java/org/mule/registry/impl/RegistrationFactory.java
@@ -0,0 +1,52 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.registry.impl;
+
+import org.mule.registry.Registration;
+import org.mule.util.StringUtils;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+
+public class RegistrationFactory 
+{
+    public static String REF_MULE_COMPONENT = "mule-component";
+    public static String REF_OSGI_REGISTRATION = "osgi-registration";
+    private static String defaultReferenceType = REF_MULE_COMPONENT;
+
+    public RegistrationFactory()
+    {
+    }
+
+    public Registration getInstance()
+    {
+        return getInstance(defaultReferenceType);
+    }
+
+    public Registration getInstance(String referenceType)
+    {
+        if (StringUtils.isBlank(referenceType))
+        {
+            return new MuleRegistration();
+        }
+        else if (referenceType.equals(REF_MULE_COMPONENT))
+        {
+            return new MuleRegistration();
+        }
+        else if (referenceType.equals(REF_OSGI_REGISTRATION))
+        {
+            return new OSGIServiceRegistration();
+        }
+        else
+        {
+            return new MuleRegistration();
+        }
+    }
+}
