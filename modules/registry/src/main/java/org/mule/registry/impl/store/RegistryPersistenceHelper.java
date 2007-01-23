91177715362ae55e7b5cfb20dc856cf7a05417bb
Lajos <Lajos>
2007-01-23 17:08:34 +0000

    This PersistenceHelper, also an XStream Converter, uses the Metadata to translate registry Registration objects to Spring-friendly XML
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@4845 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/registry/src/main/java/org/mule/registry/impl/store/RegistryPersistenceHelper.java
@@ -0,0 +1,126 @@
+package org.mule.registry.impl.store;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.mule.persistence.PersistenceHelper;
+import org.mule.registry.Registration;
+import org.mule.registry.Registry;
+import org.mule.registry.impl.MuleRegistration;
+import org.mule.registry.metadata.MetadataStore;
+import org.mule.registry.metadata.ObjectMetadata;
+import org.mule.registry.metadata.PropertyMetadata;
+import org.mule.registry.metadata.MissingMetadataException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+
+public class RegistryPersistenceHelper implements PersistenceHelper, Converter 
+{
+    private Registry registry;
+    private boolean persistAll = false;
+
+    /**
+     * logger used by this class
+     */
+    private static transient Log logger = LogFactory.getLog(RegistryPersistenceHelper.class);
+
+    public RegistryPersistenceHelper(Registry registry) 
+    {
+        this.registry = registry;
+    }
+
+    public void setPersistAll(boolean persistAll)
+    {
+        this.persistAll = persistAll;
+    }
+
+    public boolean canConvert(Class clazz) 
+    {
+        return (clazz.equals(MuleRegistration.class));
+    }
+
+    public void marshal(Object value, HierarchicalStreamWriter writer, MarshallingContext context) 
+    {
+        MuleRegistration ref = (MuleRegistration) value;
+        String className = (String)ref.getProperty("sourceObjectClassName");
+        ObjectMetadata metadata = null;
+
+        try 
+        {
+            metadata = MetadataStore.getObjectMetadata(className);
+        }
+        catch (MissingMetadataException mme) 
+        {
+            logger.warn("No metadata found for " + className);
+        }
+
+        if (persistAll || (metadata != null && metadata.getIsPersistable())) 
+        {
+            writer.startNode("bean");
+            if (ref.getProperty("name") != null)
+                writer.addAttribute("id", (String)ref.getProperty("name"));
+            writer.addAttribute("class", className);
+
+            HashMap props = ref.getProperties();
+            Iterator iter = props.keySet().iterator();
+            while (iter.hasNext()) 
+            {
+                String key = (String)iter.next();
+                Object v = props.get(key);
+
+                if (metadata != null)
+                {
+                    PropertyMetadata p = metadata.getProperty(key);
+                    if (v != null && p != null && p.getIsPersistable()) 
+                    {
+                        writer.startNode("property");
+                        writer.addAttribute("name", key);
+                        writer.startNode("value");
+                        writer.setValue(v.toString());
+                        writer.endNode();
+                        writer.endNode();
+                    }
+                }
+                else
+                {
+                    if (v != null)
+                    {
+                        writer.startNode("property");
+                        writer.addAttribute("name", key);
+                        writer.startNode("value");
+                        writer.setValue(v.toString());
+                        writer.endNode();
+                        writer.endNode();
+                    }
+                }
+            }
+        }
+
+        HashMap children = ref.retrieveChildren();
+        Iterator iter = children.keySet().iterator();
+        while (iter.hasNext()) 
+        {
+            String id = (String)iter.next();
+            MuleRegistration child = (MuleRegistration)children.get(id);
+            context.convertAnother(child);
+        }
+
+        if (persistAll || (metadata != null && metadata.getIsPersistable())) 
+        {
+            writer.endNode();
+        }
+
+    }
+
+    public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) 
+    {
+        return null;
+    }
+
+}
