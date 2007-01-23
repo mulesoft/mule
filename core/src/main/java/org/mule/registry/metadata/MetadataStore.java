a4816f9e8e9f11105c6290149fdc285b3c5ec962
Lajos <Lajos>
2007-01-23 15:07:16 +0000

    Object to hold class metadata instances for use by registry
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@4834 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/main/java/org/mule/registry/metadata/MetadataStore.java
@@ -0,0 +1,44 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.registry.metadata;
+
+import java.util.HashMap;
+
+public class MetadataStore
+{
+    private static HashMap metadata = new HashMap();
+
+    public static void addObjectMetadata(ObjectMetadata om)
+    {
+        metadata.put(om.getClassName(), om);
+    }
+
+    /*
+     * Returns the Metadata object describing a Mule object
+     */
+    public static ObjectMetadata getObjectMetadata(String className) throws MissingMetadataException
+    {
+        if (!metadata.containsKey(className))
+            throw new MissingMetadataException(className);
+        return (ObjectMetadata)metadata.get(className);
+    }
+
+    /*
+     * Returns the Metadata object describing a property of a Mule object
+     */
+    public static PropertyMetadata getPropertyMetadata(String className, String propertyName) throws MissingMetadataException
+    {
+        if (!metadata.containsKey(className))
+            throw new MissingMetadataException(className);
+        if (!((ObjectMetadata)metadata.get(className)).getProperties().containsKey(propertyName))
+            throw new MissingMetadataException(className, propertyName);
+        return (PropertyMetadata)((Metadata)metadata.get(className)).getProperty(propertyName);
+    }
+}
