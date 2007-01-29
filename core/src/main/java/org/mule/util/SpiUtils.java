08929ebacb4340677d0f385d3ae226be8f481be6
tcarlson <tcarlson>
2007-01-29 11:50:24 +0000

    It turns out that commons-discovery wasn't even being used after all!
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@4913 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/util/SpiUtils.java
+++ core/src/main/java/org/mule/util/SpiUtils.java
@@ -12,18 +12,12 @@ package org.mule.util;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
-import java.util.Properties;
 
-import org.apache.commons.discovery.DiscoveryException;
-import org.apache.commons.discovery.resource.ClassLoaders;
-import org.apache.commons.discovery.tools.DiscoverClass;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * @deprecated The SPI discovery mechanism should probably use the OSGi Service Registry for Mule 2.x
+ * @deprecated Mule 2.x will use the OSGi Service Registry for locating services
  */
 // @ThreadSafe
 public class SpiUtils
@@ -32,116 +26,10 @@ public class SpiUtils
 
     public static final String SERVICE_ROOT = "META-INF/services/";
 
-    /**
-     * Find class implementing a specified SPI.
-     * 
-     * @param spi Service Provider Interface Class.
-     * @param propertyFileName is a location of a property file that contains the SPI
-     *            property value
-     * @param defaultImpl Default implementation class name.
-     * @param currentClass is used to include the classloader of the calling class in
-     *            the search. All system classloaders will be checked as well.
-     * @return Class implementing the SPI or null if a service was not found
-     */
-    public static Class findService(Class spi, String propertyFileName, String defaultImpl, Class currentClass)
-    {
-        ClassLoaders loaders = ClassLoaders.getAppLoaders(spi, currentClass, false);
-        DiscoverClass discover = new DiscoverClass(loaders);
-        try
-        {
-            return discover.find(spi, propertyFileName, defaultImpl);
-        }
-        catch (DiscoveryException e)
-        {
-            logger.warn("Failed to find service for spi: " + spi.getName());
-            return null;
-        }
-    }
-
-    /**
-     * Find class implementing a specified SPI. The system properties will be checked
-     * for an SPI property to use. this will be the fully qualified SPI class name.
-     * 
-     * @param spi Service Provider Interface Class.
-     * @param defaultImpl Default implementation class name.
-     * @param currentClass is used to include the classloader of the calling class in
-     *            the search. All system classloaders will be checked as well.
-     * @return Class implementing the SPI or the default implementation class if
-     *         nothing has been found
-     */
-    public static Class findService(final Class spi, final String defaultImpl, final Class currentClass)
-    {
-        ClassLoaders loaders = (ClassLoaders)AccessController.doPrivileged(new PrivilegedAction()
-        {
-            public Object run()
-            {
-                return ClassLoaders.getAppLoaders(spi, currentClass, false);
-            }
-        });
-        DiscoverClass discover = new DiscoverClass(loaders);
-        try
-        {
-            return discover.find(spi, System.getProperties(), defaultImpl);
-        }
-        catch (DiscoveryException e)
-        {
-            logger.warn("Failed to find service for spi: " + spi.getName());
-            return null;
-        }
-    }
-
-    /**
-     * Find class implementing a specified SPI. The system properties will be checked
-     * for an SPI property to use. this will be the fully qualified SPI class name.
-     * 
-     * @param spi Service Provider Interface Class.
-     * @param currentClass is used to include the classloader of the calling class in
-     *            the search. All system classloaders will be checked as well.
-     * @return Class implementing the SPI or null if a service was not found
-     */
-    public static Class findService(Class spi, Class currentClass)
-    {
-        ClassLoaders loaders = ClassLoaders.getAppLoaders(spi, currentClass, false);
-        DiscoverClass discover = new DiscoverClass(loaders);
-        try
-        {
-            return discover.find(spi, System.getProperties());
-        }
-        catch (DiscoveryException e)
-        {
-            logger.warn("Failed to find service for spi: " + spi.getName());
-            return null;
-        }
-    }
-
-    /**
-     * Find class implementing a specified SPI.
-     * 
-     * @param spi Service Provider Interface Class.
-     * @param currentClass is used to include the classloader of the calling class in
-     *            the search.
-     * @param props The properties will be checked for an SPI property to use. this
-     *            will be the fully qualified SPI class name. All system classloaders
-     *            will be checked as well.
-     * @return Class implementing the SPI or null if a service was not found
-     */
-    public static Class findService(Class spi, Properties props, Class currentClass)
-    {
-        ClassLoaders loaders = ClassLoaders.getAppLoaders(spi, currentClass, false);
-        DiscoverClass discover = new DiscoverClass(loaders);
-        try
-        {
-            return discover.find(spi, props);
-        }
-        catch (DiscoveryException e)
-        {
-            logger.warn("Failed to find service for spi: " + spi.getName());
-            return null;
-        }
-    }
-
     public static InputStream findServiceDescriptor(String path, String name, Class currentClass)
     {
+        name += ".properties";
+        
         if (path.startsWith("/"))
         {
             path = path.substring(1);
