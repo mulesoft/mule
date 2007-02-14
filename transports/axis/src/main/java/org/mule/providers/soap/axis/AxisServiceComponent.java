2673d97b565f34d4cb89c85ccd802f0934b8e2c0
tcarlson <tcarlson>
2007-02-14 12:26:22 +0000

    Leftovers from my last check-in (OSGi fixes)
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@5111 bf997673-6b11-0410-b953-e057580c5b09

--- transports/axis/src/main/java/org/mule/providers/soap/axis/AxisServiceComponent.java
+++ transports/axis/src/main/java/org/mule/providers/soap/axis/AxisServiceComponent.java
@@ -23,7 +23,7 @@ import org.mule.providers.soap.axis.extensions.MuleConfigProvider;
 import org.mule.umo.UMOEventContext;
 import org.mule.umo.UMOException;
 import org.mule.umo.UMOMessage;
-import org.mule.umo.endpoint.MalformedEndpointException;
+import org.mule.umo.endpoint.EndpointException;
 import org.mule.umo.endpoint.UMOEndpointURI;
 import org.mule.umo.lifecycle.Callable;
 import org.mule.umo.lifecycle.Initialisable;
@@ -657,7 +657,7 @@ public class AxisServiceComponent implements Initialisable, Callable
         }
     }
 
-    private UMOEndpointURI getEndpoint(UMOEventContext context) throws MalformedEndpointException
+    private UMOEndpointURI getEndpoint(UMOEventContext context) throws EndpointException
     {
         String endpoint = context.getEndpointURI().getAddress();
         String request = context.getMessage().getStringProperty(HttpConnector.HTTP_REQUEST_PROPERTY, null);
