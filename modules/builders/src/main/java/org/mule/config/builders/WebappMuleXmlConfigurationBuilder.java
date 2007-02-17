1b1b3430b13e0ae17099b8aec6208bbd0d83ef9d
tcarlson <tcarlson>
2007-02-17 21:31:11 +0000

    MuleXmlConfigurationBuilder is a synonym for SpringConfigurationBuilder.  Remove Commons-Digester stuff (and good riddance...)
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@5220 bf997673-6b11-0410-b953-e057580c5b09

--- modules/builders/src/main/java/org/mule/config/builders/WebappMuleXmlConfigurationBuilder.java
+++ modules/builders/src/main/java/org/mule/config/builders/WebappMuleXmlConfigurationBuilder.java
@@ -14,16 +14,12 @@ import org.mule.config.ConfigurationException;
 
 import javax.servlet.ServletContext;
 
-import java.io.InputStream;
-
 /**
  * <code>WebappMuleXmlConfigurationBuilder</code> will first try and load config
  * resources from the Servlet context. If this fails it fails back to the methods
  * used by the MuleXmlConfigurationBuilder.
  * 
- * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
- * @version $Revision$
- * @see org.mule.config.builders.MuleXmlConfigurationBuilder
+ * @deprecated Need to fix this up for Spring-based configs in 2.0
  */
 public class WebappMuleXmlConfigurationBuilder extends MuleXmlConfigurationBuilder
 {
@@ -33,25 +29,5 @@ public class WebappMuleXmlConfigurationBuilder extends MuleXmlConfigurationBuild
     {
         super();
         this.context = context;
-    }
-
-    /**
-     * ConfigResource can be a url, a path on the local file system or a resource
-     * name on the classpath Finds and loads the configuration resource by doing the
-     * following - 1. load it from the servelet context /WEB-INF 2. load it form the
-     * classpath 3. load it from from the local file system 4. load it as a url
-     * 
-     * @param configResource a single configuration resource
-     * @return an inputstream to the resource
-     * @throws org.mule.config.ConfigurationException
-     */
-    protected InputStream loadConfig(String configResource) throws ConfigurationException
-    {
-        InputStream is = context.getResourceAsStream(configResource);
-        if (is == null)
-        {
-            is = super.loadConfig(configResource);
-        }
-        return is;
     }
 }
