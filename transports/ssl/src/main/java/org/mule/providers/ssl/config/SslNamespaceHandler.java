b3eb76421372bab2e7b1668ca9a7f177c81d0564
Ross Mason <ross@mulesoft.com>
2007-04-17 09:52:45 +0000

    Added SSL Namespace handler
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@6049 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/ssl/src/main/java/org/mule/providers/ssl/config/SslNamespaceHandler.java
@@ -0,0 +1,29 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.providers.ssl.config;
+
+import org.mule.config.spring.parsers.CompoundElementDefinitionParser;
+import org.mule.config.spring.parsers.SingleElementDefinitionParser;
+import org.mule.providers.ssl.SslConnector;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+/**
+ * Reigsters a Bean Definition Parser for handling <code><ssl:connector></code> elements.
+ */
+public class SslNamespaceHandler extends NamespaceHandlerSupport
+{
+    public void init()
+    {
+        registerBeanDefinitionParser("connector", new SingleElementDefinitionParser(SslConnector.class, true));
+        registerBeanDefinitionParser("tls-configuration", new CompoundElementDefinitionParser());
+
+    }
+}
