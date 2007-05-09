020978365f34689ec85e63c3f2d7cff897bec737
acooke <acooke>
2007-05-09 21:14:53 +0000

    MULE-1708 - IMAP and IMAPS connectors.  Email done.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@6442 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/email/src/test/java/org/mule/providers/email/SmtpNamespaceHandlerTestCase.java
@@ -0,0 +1,87 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.providers.email;
+
+import org.mule.tck.FunctionalTestCase;
+
+import java.util.Properties;
+
+/**
+ * TODO
+ */
+public class SmtpNamespaceHandlerTestCase extends FunctionalTestCase
+{
+    protected String getConfigResources()
+    {
+        return "smtp-namespace-config.xml";
+    }
+
+    public void testConfig() throws Exception
+    {
+        SmtpConnector c = (SmtpConnector)managementContext.getRegistry().lookupConnector("smtpConnector");
+        assertNotNull(c);
+
+        assertEquals("abc@example.com", c.getBccAddresses());
+        assertEquals("bcd@example.com", c.getCcAddresses());
+        assertEquals("foo/bar", c.getContentType());
+        Properties headers = c.getCustomHeaders();
+        assertEquals(2, headers.size());
+        assertEquals("bar", headers.getProperty("foo"));
+        assertEquals("boz", headers.getProperty("baz"));
+        assertEquals("cde@example.com", c.getFromAddress());
+        assertEquals("example.com", c.getHost());
+        assertEquals("newMailbox", c.getMailboxFolder());
+        assertEquals("password", c.getPassword());
+        assertEquals(1234, c.getPort());
+        assertEquals("def@example.com", c.getReplyToAddresses());
+        assertEquals("subject", c.getSubject());
+        assertEquals("bob@example.com", c.getUsername());
+
+        // authenticator?
+
+        assertTrue(c.isConnected());
+        assertTrue(c.isStarted());
+    }
+
+    public void testSecureConfig() throws Exception
+    {
+        SmtpsConnector c = (SmtpsConnector)managementContext.getRegistry().lookupConnector("smtpsConnector");
+        assertNotNull(c);
+
+        assertEquals("abc@example.com", c.getBccAddresses());
+        assertEquals("bcd@example.com", c.getCcAddresses());
+        assertEquals("foo/bar", c.getContentType());
+        Properties headers = c.getCustomHeaders();
+        assertEquals(2, headers.size());
+        assertEquals("bar", headers.getProperty("foo"));
+        assertEquals("boz", headers.getProperty("baz"));
+        assertEquals("cde@example.com", c.getFromAddress());
+        assertEquals("example.com", c.getHost());
+        assertEquals("newMailbox", c.getMailboxFolder());
+        assertEquals("password", c.getPassword());
+        assertEquals(1234, c.getPort());
+        assertEquals("def@example.com", c.getReplyToAddresses());
+        assertEquals("subject", c.getSubject());
+        assertEquals("bob@example.com", c.getUsername());
+
+        // authenticator?
+
+        //The full path gets resolved, we're just checkng that the property got set
+        assertTrue(c.getClientKeyStore().endsWith("/greenmail-truststore"));
+        assertEquals("password", c.getClientKeyStorePassword());
+        //The full path gets resolved, we're just checkng that the property got set
+        assertTrue(c.getTrustStore().endsWith("/greenmail-truststore"));
+        assertEquals("password", c.getTrustStorePassword());
+
+        assertTrue(c.isConnected());
+        assertTrue(c.isStarted());
+    }
+
+}
