73487d85999320a84a649e632682e3b315b89bbf
Ross Mason <ross@mulesoft.com>
2007-05-12 13:03:49 +0000

    Fixed MULE-1737, missing parent exception
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@6505 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/config/spring/parsers/ThreadingProfileDefinitionParser.java
+++ core/src/main/java/org/mule/config/spring/parsers/ThreadingProfileDefinitionParser.java
@@ -9,6 +9,7 @@
  */
 package org.mule.config.spring.parsers;
 
+import org.mule.config.MuleProperties;
 import org.mule.config.ThreadingProfile;
 
 import org.w3c.dom.Element;
@@ -27,6 +28,20 @@ public class ThreadingProfileDefinitionParser extends AbstractChildBeanDefinitio
     protected Class getBeanClass(Element element)
     {
         return ThreadingProfile.class;
+    }
+
+    protected String getParentBeanName(Element element)
+    {
+        //The mule:configuration element is a fixed name element so we need to handle the
+        //special case here
+        if("configuration".equals(element.getParentNode().getLocalName()))
+        {
+            return MuleProperties.OBJECT_MULE_CONFIGURATION;
+        }
+        else
+        {
+            return super.getParentBeanName(element);            
+        }
     }
 
     public String getPropertyName(Element e)
