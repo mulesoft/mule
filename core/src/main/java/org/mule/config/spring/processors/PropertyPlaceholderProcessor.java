f05ed7cc7e53995656c5e6a58cd74c97e556379f
Ross Mason <ross@mulesoft.com>
2007-05-16 21:55:37 +0000

    Fixed MULE-1756. Added support for registering PropertyFactory objects with the PropertyPlaceHolderConfigurer. This then adds the properties factories to the map of placeholders
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@6587 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/config/spring/processors/PropertyPlaceholderProcessor.java
+++ core/src/main/java/org/mule/config/spring/processors/PropertyPlaceholderProcessor.java
@@ -10,8 +10,12 @@
 package org.mule.config.spring.processors;
 
 import org.mule.RegistryContext;
+import org.mule.config.PropertyFactory;
+import org.mule.config.i18n.CoreMessages;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
 
@@ -22,7 +26,7 @@ import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
  */
 public class PropertyPlaceholderProcessor extends PropertyPlaceholderConfigurer
 {
-
+    private Map factories = new HashMap();
 
     //@java.lang.Override
     protected Properties mergeProperties() throws IOException
@@ -33,6 +37,39 @@ public class PropertyPlaceholderProcessor extends PropertyPlaceholderConfigurer
         {
             props.putAll(p);
         }
+        if(factories!=null)
+        {
+            for (Iterator iterator = factories.entrySet().iterator(); iterator.hasNext();)
+            {
+                Map.Entry entry = (Map.Entry) iterator.next();
+                if(entry.getKey()==null)
+                {
+                    throw new NullPointerException(CoreMessages.objectIsNull("Factories.Key").getMessage());
+                }
+                if(entry.getValue()==null)
+                {
+                    throw new NullPointerException(CoreMessages.objectIsNull("Factories.Value").getMessage());
+                }
+                try
+                {
+                    props.put(entry.getKey(), ((PropertyFactory)entry.getValue()).create(props));
+                }
+                catch (Exception e)
+                {
+                    throw new IOException("Failed to invoke PropertyFactory: " + entry.getValue() + ". Error is: " + e.toString());
+                }
+            }
+        }
         return props;
+    }
+
+    public Map getFactories()
+    {
+        return factories;
+    }
+
+    public void setFactories(Map factories)
+    {
+        this.factories = factories;
     }
 }
