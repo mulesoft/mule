54b67d6f34dc6e2c420c802952b9840e172d3374
acooke <acooke>
2007-05-17 16:57:05 +0000

    MULE-1758: I think this is the same issue as seen in UDP and Multicast, reduced to the simplest form I can.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@6598 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/vm/src/test/java/org/mule/providers/vm/issues/ManySendsMule1758TestCase.java
@@ -0,0 +1,46 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.providers.vm.issues;
+
+import org.mule.tck.FunctionalTestCase;
+import org.mule.extras.client.MuleClient;
+import org.mule.umo.UMOMessage;
+
+public class ManySendsMule1758TestCase extends FunctionalTestCase
+{
+    private static int NUM_MESSAGES = 100;
+
+    protected String getConfigResources()
+    {
+        return "many-sends-test.xml";
+    }
+
+    public void testSingleSend() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        UMOMessage response = client.send("vm://s-in", "Marco", null);
+        assertNotNull("Response is null", response);
+        assertEquals("Polo", response.getPayload());
+    }
+
+    public void testManySends() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        for (int i = 0; i < NUM_MESSAGES; ++i)
+        {
+            logger.debug("Message " + i);
+            UMOMessage response = client.send("vm://s-in", "Marco", null);
+            assertNotNull("Response is null", response);
+            assertEquals("Polo", response.getPayload());
+        }
+    }
+
+}
