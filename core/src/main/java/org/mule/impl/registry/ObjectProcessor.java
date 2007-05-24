1d9bf0b441974d6a467ed20a3df052158b764a6b
Ross Mason <ross@mulesoft.com>
2007-05-24 00:07:09 +0000

    ignorred by IDEA...
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@6673 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/main/java/org/mule/impl/registry/ObjectProcessor.java
@@ -0,0 +1,19 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.impl.registry;
+
+/**
+ * TODO
+ */
+
+public interface ObjectProcessor
+{
+    public Object process(Object object);
+}
---
+++ core/src/main/java/org/mule/impl/registry/RegisteredServiceProcessor.java
@@ -0,0 +1,41 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.impl.registry;
+
+import org.mule.impl.model.ModelFactory;
+import org.mule.umo.UMODescriptor;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * TODO
+ */
+public class RegisteredServiceProcessor implements ObjectProcessor
+{
+    /**
+     * logger used by this class
+     */
+    protected transient final Log logger = LogFactory.getLog(RegisteredServiceProcessor.class);
+
+    public Object process(Object object)
+    {
+        if(object instanceof UMODescriptor)
+        {
+            UMODescriptor descriptor = (UMODescriptor)object;
+            if(descriptor.getModelName()==null)
+            {
+                logger.warn("modelName proeprty not set on service: " + descriptor.getName() + " trying default model name: " + ModelFactory.DEFAULT_MODEL_NAME);
+                descriptor.setModelName(ModelFactory.DEFAULT_MODEL_NAME);
+            }
+        }
+        return object;
+    }
+}
