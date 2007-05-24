74b4d3d320837f1cbf9a3ba47d1f88b609bec1fd
acooke <acooke>
2007-05-24 21:37:03 +0000

    MULE-1576: RMI test.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.0-spring@6696 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/rmi/src/test/java/org/mule/providers/rmi/RMIFunctionalTestCase.java
@@ -0,0 +1,52 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.providers.rmi;
+
+import org.mule.extras.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.umo.UMOMessage;
+
+public class RMIFunctionalTestCase extends FunctionalTestCase
+{
+
+    public void testCase() throws Exception
+    {
+        MuleClient client = new MuleClient();
+
+        // send Echo String
+        UMOMessage message = client.send("vm://testin", new Integer(12), null);
+        assertNotNull(message);
+        Integer payload = (Integer)message.getPayload();
+        assertEquals(payload, new Integer(22));
+
+        // send String
+        message = client.send("vm://testin", "test matching component first time", null);
+        assertNotNull(message);
+        assertEquals((String)message.getPayload(), "emit tsrif tnenopmoc gnihctam tset");
+
+        // send String
+        message = client.send("vm://testin", "test mathching component second time", null);
+        assertNotNull(message);
+        assertEquals((String)message.getPayload(), "emit dnoces tnenopmoc gnihchtam tset");
+
+        // send Integer
+        message = client.send("vm://testin", new Integer(15), null);
+        assertNotNull(message);
+        payload = (Integer)message.getPayload();
+        assertEquals(payload, new Integer(25));
+    }
+
+    protected String getConfigResources()
+    {
+        return "rmi-functional-test.xml";
+    }
+
+}
