a35ae6f78b5b4985f2f081efa5b692699117dfc5
Ross Mason <ross@mulesoft.com>
2007-06-02 19:45:41 +0000

    split the functions of the ResponseWriterCallback. Added better javadoc to the RWC.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@6831 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/functional/src/main/java/org/mule/tck/functional/CounterCallback.java
@@ -0,0 +1,35 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.tck.functional;
+
+import org.mule.umo.UMOEventContext;
+
+/**
+ * A test callback that counts the number of messages was received
+ */
+public class CounterCallback implements EventCallback
+{
+    private int callbackCount = 0;
+
+    public void eventReceived(UMOEventContext context, Object Component) throws Exception
+    {
+        incCallbackCount();
+    }
+
+    protected synchronized void incCallbackCount()
+    {
+        callbackCount = callbackCount + 1;
+    }
+
+    public int getCallbackCount()
+    {
+        return callbackCount;
+    }
+}
--- tests/functional/src/main/java/org/mule/tck/functional/ResponseWriterCallback.java
+++ tests/functional/src/main/java/org/mule/tck/functional/ResponseWriterCallback.java
@@ -14,33 +14,23 @@ import org.mule.umo.UMOEventContext;
 /**
  * A test callback that writes the results of a service invocation to the response output stream
  * of the event
+ * This should only be used when testing Asynchronous calls with the {@link FunctionalTestComponent} otherwise
+ * you will get duplicate messages, since both this class and the {@link FunctionalTestComponent} will write
+ * a return message back to the callee.
+ *
+ * @see org.mule.tck.functional.FunctionalTestComponent
  */
-public class ResponseWriterCallback implements EventCallback
+public class ResponseWriterCallback extends CounterCallback
 {
-    private int callbackCount = 0;
 
-    public void eventReceived(UMOEventContext context, Object Component) throws Exception
+    public void eventReceived(UMOEventContext context, Object component) throws Exception
     {
-        incCallbackCount();
+        super.eventReceived(context, component);
+
         String result = context.getMessageAsString() + " Received Async";
         assert (context.getOutputStream() != null);
 
         context.getOutputStream().write(result.getBytes());
         context.getOutputStream().flush();
-    }
-
-    protected synchronized void incCallbackCount()
-    {
-        callbackCount = callbackCount + 1;
-    }
-
-    public boolean isCallbackCalled()
-    {
-        return callbackCount > 0;
-    }
-
-    public int getCallbackCount()
-    {
-        return callbackCount;
     }
 }
