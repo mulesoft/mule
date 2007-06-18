efab7033a6b20eb776c25e7257e7fbdccd0d6d45
acooke <acooke>
2007-06-18 18:23:50 +0000

    MULE-1830: All Acegi tests OK (Acegi itself not yet complete - needs some docs).  More problems with IDEA + SVN.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@7143 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/acegi/src/test/java/org/mule/extras/acegi/PlainTextFunctionalTestCase.java
@@ -0,0 +1,53 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.extras.acegi;
+
+import org.mule.extras.client.MuleClient;
+import org.mule.providers.http.HttpConnector;
+import org.mule.providers.http.HttpConstants;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.umo.UMOMessage;
+
+public class PlainTextFunctionalTestCase extends FunctionalTestCase
+{
+    protected String getConfigResources()
+    {
+        return "encryption-test.xml";
+    }
+
+    public void testAuthenticationFailureNoContext() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        UMOMessage m = client.send("http://localhost:4567/index.html", "", null);
+        assertNotNull(m);
+        int status = m.getIntProperty(HttpConnector.HTTP_STATUS_PROPERTY, -1);
+        assertEquals(HttpConstants.SC_UNAUTHORIZED, status);
+    }
+
+    public void testAuthenticationFailureBadCredentials() throws Exception
+    {
+        MuleClient client = new MuleClient("anonX", "anonX");
+        UMOMessage m = client.send("http://localhost:4567/index.html", "", null);
+        assertNotNull(m);
+        int status = m.getIntProperty(HttpConnector.HTTP_STATUS_PROPERTY, -1);
+        assertEquals(HttpConstants.SC_UNAUTHORIZED, status);
+    }
+
+    public void testAuthenticationAuthorised() throws Exception
+    {
+        MuleClient client = new MuleClient("anon", "anon");
+        UMOMessage m = client.send("http://localhost:4567/index.html", "", null);
+        assertNotNull(m);
+        int status = m.getIntProperty(HttpConnector.HTTP_STATUS_PROPERTY, -1);
+        assertEquals(HttpConstants.SC_OK, status);
+    }
+
+}
