65668a12f94c0483a113dfd5c7fa9e42f023a7dd
acooke <acooke>
2007-06-19 19:57:59 +0000

    MULE-1921: Giving collection parsers a consistent set of names.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@7168 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/config/spring/parsers/collection/ChildMapDefinitionParser.java
+++ core/src/main/java/org/mule/config/spring/parsers/collection/ChildMapDefinitionParser.java
@@ -11,34 +11,42 @@ package org.mule.config.spring.parsers.collection;
 
 import org.mule.config.spring.parsers.generic.ChildDefinitionParser;
 
-import java.util.Properties;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.beans.factory.config.MapFactoryBean;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
 
 /**
- * Creates a single Properties object and processes standard Spring Properties sub elements
+ * Creates a single Map and processes standard Spring sub elements.  The map is injected
+ * into the parent object (the enclosing XML element). 
  */
-public class PropertiesBeanDefinitionParser extends ChildDefinitionParser
+public class ChildMapDefinitionParser extends ChildDefinitionParser
 {
 
-    public PropertiesBeanDefinitionParser(String setterMethod)
+    public ChildMapDefinitionParser(String setterMethod)
     {
-        super(setterMethod, /*clazz*/null);
+        super(setterMethod, HashMap.class);
     }
-    
+
+    public ChildMapDefinitionParser(String setterMethod, Class mapType)
+    {
+        super(setterMethod, mapType, Map.class);
+    }
+
     protected Class getBeanClass(Element element)
     {
-        return PropertiesFactoryBean.class;
+        return MapFactoryBean.class;
     }
 
     protected void parseChild(Element element, ParserContext parserContext, BeanDefinitionBuilder builder)
     {
         super.parseChild(element, parserContext, builder);
-        
-        Properties parsedProps = parserContext.getDelegate().parsePropsElement(element);
-        builder.addPropertyValue("properties", parsedProps);
+        Map parsedMap = parserContext.getDelegate().parseMapElement(element, builder.getRawBeanDefinition());
+        builder.addPropertyValue("sourceMap", parsedMap);
+        builder.addPropertyValue("targetMapClass", super.getBeanClass(element));
     }
+
 }
