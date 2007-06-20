18e182ea73fd7c772b7f7dab70a2b1e90976433d
acooke <acooke>
2007-06-20 21:28:20 +0000

    MULE-1921: Fixing lazy test naming - sorry!
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@7190 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/functional/src/test/java/org/mule/config/spring/parsers/CollectionTestCase.java
@@ -0,0 +1,45 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.config.spring.parsers;
+
+import java.util.Collection;
+
+public class CollectionTestCase extends AbstractNamespaceTestCase
+{
+
+    public CollectionTestCase()
+    {
+        super("org/mule/config/spring/parsers/collection-test.xml");
+    }
+
+    protected void testKids(int index)
+    {
+        OrphanBean orphan = (OrphanBean) beanExists("orphan" + index, OrphanBean.class);
+        Collection kids = (Collection) contentExists(orphan.getKids(), Collection.class);
+        assertEquals(index + 1, kids.size());
+    }
+
+    public void testNamed()
+    {
+        testKids(1);
+    }
+
+    public void testOrphan()
+    {
+        testKids(2);
+    }
+
+    public void testParent()
+    {
+        testKids(3);
+    }
+
+}
