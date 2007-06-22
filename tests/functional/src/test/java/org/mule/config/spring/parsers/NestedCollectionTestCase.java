c39bb5d89a435182e079702fd8482599262b0c7a
acooke <acooke>
2007-06-22 21:40:39 +0000

    MULE-1921: Another corner case.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@7234 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/functional/src/test/java/org/mule/config/spring/parsers/NestedCollectionTestCase.java
@@ -0,0 +1,30 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.config.spring.parsers;
+
+import java.util.Collection;
+
+public class NestedCollectionTestCase extends AbstractNamespaceTestCase
+{
+
+    protected String getConfigResources()
+    {
+        return "org/mule/config/spring/parsers/nested-collection-test.xml";
+    }
+
+    public void testAll()
+    {
+        OrphanBean orphan = (OrphanBean) assertBeanExists("orphan1", OrphanBean.class);
+        Collection kids = (Collection) assertContentExists(orphan.getKids(), Collection.class);
+        assertEquals(5, kids.size());
+    }
+
+}
