ee563ddc2200d5e647c21113ba918938f8fb23c8
Dirk Olmes <dirk.olmes@mulesoft.com>
2007-07-05 13:30:57 +0000

    MULE-1839 (Axis transport fails to pass tests in 2.0): converted AbstractProviderFunctionalTestCase subclass to FunctionalTestCase with config file
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@7358 bf997673-6b11-0410-b953-e057580c5b09

--- transports/axis/src/test/java/org/mule/providers/soap/axis/functional/SoapAttachmentsFunctionalTestCase.java
+++ transports/axis/src/test/java/org/mule/providers/soap/axis/functional/SoapAttachmentsFunctionalTestCase.java
@@ -10,102 +10,43 @@
 
 package org.mule.providers.soap.axis.functional;
 
-import org.mule.RegistryContext;
-import org.mule.config.PoolingProfile;
-import org.mule.impl.ImmutableMuleEndpoint;
 import org.mule.impl.MuleEvent;
 import org.mule.impl.MuleMessage;
 import org.mule.impl.MuleSession;
-import org.mule.impl.endpoint.MuleEndpointURI;
-import org.mule.impl.model.seda.SedaModel;
+import org.mule.impl.endpoint.MuleEndpoint;
 import org.mule.providers.AbstractConnector;
-import org.mule.providers.soap.axis.AxisConnector;
 import org.mule.providers.soap.axis.AxisMessageDispatcher;
-import org.mule.tck.functional.AbstractProviderFunctionalTestCase;
+import org.mule.tck.FunctionalTestCase;
 import org.mule.umo.UMOMessage;
-import org.mule.umo.endpoint.EndpointException;
-import org.mule.umo.endpoint.UMOEndpointURI;
-import org.mule.umo.endpoint.UMOImmutableEndpoint;
-import org.mule.umo.provider.UMOConnector;
 
 import java.io.File;
 
 import javax.activation.DataHandler;
 import javax.activation.FileDataSource;
 
-public class SoapAttachmentsFunctionalTestCase extends AbstractProviderFunctionalTestCase
+public class SoapAttachmentsFunctionalTestCase extends FunctionalTestCase
 {
-    // TODO DO: this config file will replace the functionality that's implemented here
-//    protected String getConfigResources()
-//    {
-//        return "axis-soap-attachments.xml";
-//    }
-
+    private static final int SEND_COUNT = 5;
     
-    // //@Override
-    protected void doSetUp() throws Exception
+    private int callbackCount = 0;
+
+    protected String getConfigResources()
     {
-        // Make sure we are running synchronously
-        RegistryContext.getConfiguration().setDefaultSynchronousEndpoints(true);
-
-        SedaModel model = new SedaModel();
-        model.getPoolingProfile().setInitialisationPolicy(PoolingProfile.INITIALISE_ONE);
-
-        model.setName("main");
-        managementContext.getRegistry().registerModel(model);
-        callbackCalled = false;
-        callbackCount = 0;
-        connector = createConnector();
+        return "axis-soap-attachments.xml";
     }
 
-    protected UMOEndpointURI getInDest()
-    {
-        try
-        {
-            return new MuleEndpointURI("axis:http://localhost:60198/mule/services");
-        }
-        catch (EndpointException e)
-        {
-            fail(e.getMessage());
-            return null;
-        }
-    }
-
-    protected UMOEndpointURI getOutDest()
-    {
-        return null;
-    }
-
-    protected UMOConnector createConnector() throws Exception
-    {
-        AxisConnector connector = new AxisConnector();
-        connector.setName("testAxis");
-        connector.getDispatcherThreadingProfile().setDoThreading(false);
-        return connector;
-    }
-
-    // //@Override
     public void testSend() throws Exception
     {
-        descriptor = getTestDescriptor("testComponent", SoapAttachmentsFunctionalTestComponent.class
-            .getName());
-
-        initialiseComponent(descriptor, null);
-        // Start the server
-        managementContext.start();
-
-        sendTestData(5);
-
-        afterInitialise();
-
-        receiveAndTestResults();
+        sendTestData(SEND_COUNT);
+        assertEquals(SEND_COUNT, callbackCount);
     }
 
     protected void sendTestData(int iterations) throws Exception
     {
-        UMOImmutableEndpoint ep = new ImmutableMuleEndpoint(
+        MuleEndpoint ep = new MuleEndpoint(
             "axis:http://localhost:60198/mule/services/testComponent?method=receiveMessageWithAttachments",
             false);
+        ep.setManagementContext(managementContext);
 
         AxisMessageDispatcher client = new AxisMessageDispatcher(ep);
         for (int i = 0; i < iterations; i++)
@@ -122,10 +63,5 @@ public class SoapAttachmentsFunctionalTestCase extends AbstractProviderFunctiona
             assertEquals(result.getPayloadAsString(), "Done");
             callbackCount++;
         }
-    }
-
-    protected void receiveAndTestResults() throws Exception
-    {
-        assertEquals(5, callbackCount);
     }
 }
