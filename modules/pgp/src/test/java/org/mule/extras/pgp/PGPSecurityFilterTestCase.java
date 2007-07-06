ed225223cdcde4ab8038f52970020575e39a118a
alan.cassar <alan.cassar>
2007-07-06 09:11:16 +0000

    PGP runnining fine and tests asserts the results not just sending
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@7368 bf997673-6b11-0410-b953-e057580c5b09

--- modules/pgp/src/test/java/org/mule/extras/pgp/PGPSecurityFilterTestCase.java
+++ modules/pgp/src/test/java/org/mule/extras/pgp/PGPSecurityFilterTestCase.java
@@ -11,15 +11,32 @@
 package org.mule.extras.pgp;
 
 import org.mule.extras.client.MuleClient;
+import org.mule.impl.DefaultExceptionStrategy;
+import org.mule.impl.internal.notifications.ExceptionNotification;
+import org.mule.impl.internal.notifications.ExceptionNotificationListener;
+import org.mule.impl.internal.notifications.SecurityNotificationListener;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.umo.UMOExceptionPayload;
+import org.mule.umo.UMOMessage;
+import org.mule.umo.manager.UMOServerNotification;
 import org.mule.umo.security.UnauthorisedException;
+
+import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PGPSecurityFilterTestCase extends FunctionalTestCase
 {
+    
+    protected static final String TARGET = "/encrypted.txt";
+    protected static final String DIRECTORY = "output";
+    protected static final String MESSAGE_EXCEPTION = "No signed message found. Message payload is of type: String";
 
     protected String getConfigResources()
     {
@@ -29,7 +46,8 @@ public class PGPSecurityFilterTestCase extends FunctionalTestCase
     public void testAuthenticationAuthorised() throws Exception
     {
         URL url = Thread.currentThread().getContextClassLoader().getResource("./encrypted-signed.asc");
-
+        
+        
         int length = (int)new File(url.getFile()).length();
         byte[] msg = new byte[length];
 
@@ -37,21 +55,37 @@ public class PGPSecurityFilterTestCase extends FunctionalTestCase
         in.read(msg);
         in.close();
 
+        Map props = new HashMap();
+        props.put("TARGET_FILE", TARGET);
         MuleClient client = new MuleClient();
-        client.send("vm://echo", new String(msg), null);
+        UMOMessage reply = client.send("vm://echo", new String(msg), props);
+        assertNull(reply.getExceptionPayload());
+        
+        try
+        {
+            //check if file exists
+            FileReader outputFile = new FileReader(DIRECTORY+TARGET);
+            outputFile.close();
+            
+            //delete file not to be confused with tests to be performed later
+            File f = new File(DIRECTORY+TARGET);
+            f.delete();
+        }
+        catch (FileNotFoundException fileNotFound)
+        {
+            fail("File not successfully created");
+        }
     }
 
     public void testAuthenticationNotAuthorised() throws Exception
     {
-        try
-        {
-            MuleClient client = new MuleClient();
-            client.send("vm://echo", new String("An unsigned message"), null);
-            fail("The request is not signed");
-        }
-        catch (UnauthorisedException e)
-        {
-            // ignore
-        }
+        MuleClient client = new MuleClient();
+
+        UMOMessage reply = client.send("vm://echo", new String("An unsigned message"), null);
+        
+        assertNotNull(reply.getExceptionPayload());
+        UMOExceptionPayload excPayload = reply.getExceptionPayload();
+        assertEquals(MESSAGE_EXCEPTION, excPayload.getMessage());
+
     }
 }
