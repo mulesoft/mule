063df150a7a225ef5e6c561ccc6b0e8ce9b528b2
tcarlson <tcarlson>
2007-07-10 11:20:27 +0000

    New callback for unit testing
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@7429 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/functional/src/main/java/org/mule/tck/functional/CountdownCallback.java
@@ -0,0 +1,59 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.tck.functional;
+
+import org.mule.config.i18n.MessageFactory;
+import org.mule.umo.UMOEventContext;
+import org.mule.umo.lifecycle.InitialisationException;
+
+import edu.emory.mathcs.backport.java.util.concurrent.CountDownLatch;
+import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
+
+import junit.framework.AssertionFailedError;
+
+public class CountdownCallback implements EventCallback
+{
+    private CountDownLatch countDown;
+
+    public CountdownCallback(int messagesExpected)
+    {
+        this.countDown = new CountDownLatch(messagesExpected);
+    }
+
+    public void eventReceived(UMOEventContext context, Object Component) throws Exception
+    {
+        if (countDown.getCount() > 0)
+        {
+            countDown.countDown();
+        }
+        else
+        {
+            throw new AssertionFailedError("Too many messages received");
+        }
+    }
+
+    public long getCount() throws InitialisationException
+    {
+        if (countDown != null)
+        {
+            return countDown.getCount();
+        }
+        else 
+        {
+            throw new InitialisationException(MessageFactory.createStaticMessage("CountDownLatch has not been initialized."), null);
+        }
+    }
+
+    public boolean await(long timeout) throws InterruptedException
+    {
+        return countDown.await(timeout, TimeUnit.MILLISECONDS);
+    }
+    
+}
