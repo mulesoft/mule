2360378c77a22fa6309e839b45cfd6d60f4832eb
alan.cassar <alan.cassar>
2007-08-03 09:28:28 +0000

    Added test case to scripting example
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@7738 bf997673-6b11-0410-b953-e057580c5b09

---
+++ examples/scripting/src/test/java/org/mule/examples/scripting/ScriptingExampleTestCase.java
@@ -0,0 +1,37 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.examples.scripting;
+
+import org.mule.extras.client.MuleClient;
+import org.mule.impl.MuleMessage;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.umo.UMOMessage;
+
+public class ScriptingExampleTestCase extends FunctionalTestCase
+{    
+    public void testScriptingExample() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        UMOMessage reply = client.send("vm://scripting", new MuleMessage("mule"));
+        
+//        assertNotNull(reply);
+//        assertNotNull(reply.getPayload());
+//        assertEquals("[Hello, :-)]", reply.getPayloadAsString());
+    }
+
+    protected String getConfigResources()
+    {
+        return "test-scripting-config.xml";
+    }
+
+}
+
+
