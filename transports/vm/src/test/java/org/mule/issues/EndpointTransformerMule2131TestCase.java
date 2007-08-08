5f7a5647586fffba5045ec14acba37775092ca98
acooke <acooke>
2007-08-08 19:43:14 +0000

    MULE-2131: Test for this
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@7805 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/vm/src/test/java/org/mule/issues/EndpointTransformerMule2131TestCase.java
@@ -0,0 +1,57 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.issues;
+
+import org.mule.extras.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.tck.functional.StringAppendTestTransformer;
+import org.mule.umo.UMOException;
+import org.mule.umo.UMOMessage;
+
+public class EndpointTransformerMule2131TestCase extends FunctionalTestCase
+{
+
+    public static final long TIMEOUT = 1000L;
+    public static final String MESSAGE = "a message";
+
+    protected String getConfigResources()
+    {
+        return "issues/endpoint-transformer-mule-2131-test.xml";
+    }
+
+    protected MuleClient send() throws UMOException
+    {
+        MuleClient client = new MuleClient();
+        client.dispatch("in", MESSAGE, null);
+        return client;
+    }
+
+    public void testDirect() throws Exception
+    {
+        String response = receive(send(), "direct");
+        assertEquals(MESSAGE, response);
+    }
+
+    public void testTransformed() throws Exception
+    {
+        String response = receive(send(), "transformed");
+        assertEquals(MESSAGE + StringAppendTestTransformer.DEFAULT_TEXT, response);
+    }
+
+    protected String receive(MuleClient client, String endpoint) throws Exception
+    {
+        UMOMessage message = client.receive(endpoint, TIMEOUT);
+        assertNotNull(message);
+        assertNotNull(message.getPayloadAsString());
+        return message.getPayloadAsString();
+    }
+
+}
