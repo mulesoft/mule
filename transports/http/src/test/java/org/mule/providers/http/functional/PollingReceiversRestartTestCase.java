e9d8008c65762fc371d2e9fd1834fa9f7ea7df5a
romikk <romikk>
2007-08-13 13:59:53 +0000

    rewrited the test to extend from FunctionalTestCase
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@7856 bf997673-6b11-0410-b953-e057580c5b09

--- transports/http/src/test/java/org/mule/providers/http/functional/PollingReceiversRestartTestCase.java
+++ transports/http/src/test/java/org/mule/providers/http/functional/PollingReceiversRestartTestCase.java
@@ -9,46 +9,35 @@
  */
 package org.mule.providers.http.functional;
 
-import org.mule.MuleServer;
-import org.mule.impl.model.MuleProxy;
-import org.mule.tck.AbstractMuleTestCase;
+import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicInteger;
+import org.mule.tck.FunctionalTestCase;
 import org.mule.tck.functional.CounterCallback;
 import org.mule.tck.functional.FunctionalTestComponent;
-import org.mule.tck.testmodels.mule.TestMuleProxy;
-import org.mule.tck.testmodels.mule.TestSedaComponent;
-import org.mule.umo.UMOComponent;
-import org.mule.umo.model.UMOModel;
 
-import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicInteger;
-
-public class PollingReceiversRestartTestCase extends AbstractMuleTestCase
+public class PollingReceiversRestartTestCase extends FunctionalTestCase
 {
     private static final int WAIT_TIME = 2500;
 
+    public PollingReceiversRestartTestCase()
+    {
+        setStartContext(false);
+    }
+
+    protected String getConfigResources()
+    {
+        return "polling-receivers-restart-test.xml";
+    }
+
     public void testPollingReceiversRestart() throws Exception
     {
-        // we are going to stop and start Mule in this thread, make it
-        // a daemon so the test can exit properly
-        MuleServer mule = new MuleServer("polling-receivers-restart-test.xml");
-        mule.start(true, false);
-
-        // well, no way to register notification on the MuleServer, only
-        // possible for MuleManager, so just sleep
-        Thread.sleep(WAIT_TIME);
-
-        UMOModel model = (UMOModel) managementContext.getRegistry().getModels().get("main");
-
-        UMOComponent c = model.getComponent("Test");
-        assertTrue("Component should be a TestSedaComponent", c instanceof TestSedaComponent);
-        MuleProxy proxy = ((TestSedaComponent) c).getProxy();
-        Object ftc = ((TestMuleProxy) proxy).getComponent();
-        assertNotNull("Functional Test Component not found in the model.", ftc);
-        assertTrue("Service should be a FunctionalTestComponent", ftc instanceof FunctionalTestComponent);
-
-        AtomicInteger pollCounter = new AtomicInteger(0);
-        ((FunctionalTestComponent) ftc).setEventCallback(new CounterCallback(pollCounter));
 
         managementContext.start();
+
+        FunctionalTestComponent ftc = lookupTestComponent("main","Test");
+
+        AtomicInteger pollCounter = new AtomicInteger(0);
+        ftc.setEventCallback(new CounterCallback(pollCounter));
+
         // should be enough to poll for 2 messages
         Thread.sleep(WAIT_TIME);
 
@@ -60,7 +49,7 @@ public class PollingReceiversRestartTestCase extends AbstractMuleTestCase
         managementContext.start();
 
         pollCounter.set(0);
-        ((FunctionalTestComponent) ftc).setEventCallback(new CounterCallback(pollCounter));
+        ftc.setEventCallback(new CounterCallback(pollCounter));
 
         Thread.sleep(WAIT_TIME);
         managementContext.dispose();
