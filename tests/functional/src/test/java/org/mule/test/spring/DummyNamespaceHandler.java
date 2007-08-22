754ed4b18866da0a9dd10c1cb322fca654b27143
acooke <acooke>
2007-08-22 20:49:47 +0000

    An example of extensible nested elements
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@8016 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/functional/src/test/java/org/mule/test/spring/DummyNamespaceHandler.java
@@ -0,0 +1,25 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.spring;
+
+import org.mule.config.spring.handlers.MuleNamespaceHandler;
+import org.mule.config.spring.parsers.generic.OrphanDefinitionParser;
+
+public class DummyNamespaceHandler extends MuleNamespaceHandler
+{
+
+    public void init()
+    {
+        registerBeanDefinitionParser("simple", new OrphanDefinitionParser(Nestable.class, false));
+        registerBeanDefinitionParser("subclass", new OrphanDefinitionParser(Nestable.class, false));
+        registerBeanDefinitionParser("random", new OrphanDefinitionParser(Nestable.class, false));
+    }
+}
