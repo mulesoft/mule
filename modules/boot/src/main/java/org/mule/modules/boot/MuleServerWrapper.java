98eb327722aec9f7e7f3ae4f10bd35362de93ccd
Dirk Olmes <dirk.olmes@mulesoft.com>
2007-08-27 14:18:46 +0000

    reformatting, no code changes
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@8072 bf997673-6b11-0410-b953-e057580c5b09

--- modules/boot/src/main/java/org/mule/modules/boot/MuleServerWrapper.java
+++ modules/boot/src/main/java/org/mule/modules/boot/MuleServerWrapper.java
@@ -24,76 +24,80 @@ public class MuleServerWrapper implements WrapperListener
      *-------------------------------------------------------------*/
     public MuleServerWrapper()
     {
+        super();
     }
 
     /*---------------------------------------------------------------
      * WrapperListener Methods
      *-------------------------------------------------------------*/
     /**
-     * The start method is called when the WrapperManager is signaled by the
-     *    native wrapper code that it can start its application.  This
-     *    method call is expected to return, so a new thread should be launched
-     *    if necessary.
-     *
+     * The start method is called when the WrapperManager is signaled by the native
+     * wrapper code that it can start its application. This method call is expected
+     * to return, so a new thread should be launched if necessary.
+     * 
      * @param args List of arguments used to initialize the application.
-     *
-     * @return Any error code if the application should exit on completion
-     *         of the start method.  If there were no problems then this
-     *         method should return null.
+     * @return Any error code if the application should exit on completion of the
+     *         start method. If there were no problems then this method should return
+     *         null.
      */
-    public Integer start( String[] args )
+    public Integer start(String[] args)
     {
-        try {
-            mule = new MuleServer( args );
+        try
+        {
+            mule = new MuleServer(args);
             mule.start(false, false);
             return null;
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
             return new Integer(1);
         }
     }
 
     /**
-     * Called when the application is shutting down.  The Wrapper assumes that
-     *  this method will return fairly quickly.  If the shutdown code code
-     *  could potentially take a long time, then WrapperManager.signalStopping()
-     *  should be called to extend the timeout period.  If for some reason,
-     *  the stop method can not return, then it must call
-     *  WrapperManager.stopped() to avoid warning messages from the Wrapper.
-     *
-     * @param exitCode The suggested exit code that will be returned to the OS
-     *                 when the JVM exits.
-     *
-     * @return The exit code to actually return to the OS.  In most cases, this
-     *         should just be the value of exitCode, however the user code has
-     *         the option of changing the exit code if there are any problems
-     *         during shutdown.
+     * Called when the application is shutting down. The Wrapper assumes that this
+     * method will return fairly quickly. If the shutdown code code could potentially
+     * take a long time, then WrapperManager.signalStopping() should be called to
+     * extend the timeout period. If for some reason, the stop method can not return,
+     * then it must call WrapperManager.stopped() to avoid warning messages from the
+     * Wrapper.
+     * 
+     * @param exitCode The suggested exit code that will be returned to the OS when
+     *            the JVM exits.
+     * @return The exit code to actually return to the OS. In most cases, this should
+     *         just be the value of exitCode, however the user code has the option of
+     *         changing the exit code if there are any problems during shutdown.
      */
-    public int stop( int exitCode )
+    public int stop(int exitCode)
     {
         mule.shutdown();
         return exitCode;
     }
 
     /**
-     * Called whenever the native wrapper code traps a system control signal
-     *  against the Java process.  It is up to the callback to take any actions
-     *  necessary.  Possible values are: WrapperManager.WRAPPER_CTRL_C_EVENT,
-     *    WRAPPER_CTRL_CLOSE_EVENT, WRAPPER_CTRL_LOGOFF_EVENT, or
-     *    WRAPPER_CTRL_SHUTDOWN_EVENT
-     *
+     * Called whenever the native wrapper code traps a system control signal against
+     * the Java process. It is up to the callback to take any actions necessary.
+     * Possible values are: WrapperManager.WRAPPER_CTRL_C_EVENT,
+     * WRAPPER_CTRL_CLOSE_EVENT, WRAPPER_CTRL_LOGOFF_EVENT, or
+     * WRAPPER_CTRL_SHUTDOWN_EVENT
+     * 
      * @param event The system control signal.
      */
-    public void controlEvent( int event )
+    public void controlEvent(int event)
     {
-        if (WrapperManager.isControlledByNativeWrapper()) {
+        if (WrapperManager.isControlledByNativeWrapper())
+        {
             // The Wrapper will take care of this event
-        } else {
+        }
+        else
+        {
             // We are not being controlled by the Wrapper, so
-            //  handle the event ourselves.
-            if ((event == WrapperManager.WRAPPER_CTRL_C_EVENT) ||
-                (event == WrapperManager.WRAPPER_CTRL_CLOSE_EVENT) ||
-                (event == WrapperManager.WRAPPER_CTRL_SHUTDOWN_EVENT)){
+            // handle the event ourselves.
+            if ((event == WrapperManager.WRAPPER_CTRL_C_EVENT)
+                || (event == WrapperManager.WRAPPER_CTRL_CLOSE_EVENT)
+                || (event == WrapperManager.WRAPPER_CTRL_SHUTDOWN_EVENT))
+            {
                 WrapperManager.stop(0);
             }
         }
