f4a56f486a49ad90d31c82d964e12c9aa4877e4b
aperepel@gmail.com <Andrew Perepelytsya>
2007-08-28 02:18:32 +0000

    Completing CPAL switch for Mule 2
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@8082 bf997673-6b11-0410-b953-e057580c5b09

--- transports/tcp/src/test/java/org/mule/providers/tcp/protocols/SafeProtocolTestCase.java
+++ transports/tcp/src/test/java/org/mule/providers/tcp/protocols/SafeProtocolTestCase.java
@@ -3,7 +3,7 @@
  * --------------------------------------------------------------------------------------
  * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
  *
- * The software in this package is published under the terms of the MuleSource MPL
+ * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
@@ -12,8 +12,8 @@ package org.mule.providers.tcp.protocols;
 
 import org.mule.extras.client.MuleClient;
 import org.mule.tck.FunctionalTestCase;
-import org.mule.umo.UMOMessage;
 import org.mule.umo.UMOException;
+import org.mule.umo.UMOMessage;
 
 public class SafeProtocolTestCase extends FunctionalTestCase
 {
