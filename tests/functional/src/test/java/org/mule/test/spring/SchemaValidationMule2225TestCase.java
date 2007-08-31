63bcac4ba888907fde3e0a76776cf4d7a6a00863
acooke <acooke>
2007-08-31 15:09:25 +0000

    MULE-2225: Removed ambiguity introduced by mule-unsafe.  Added test to catch this in future.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@8142 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/functional/src/test/java/org/mule/test/spring/SchemaValidationMule2225TestCase.java
@@ -0,0 +1,49 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.spring;
+
+import org.mule.tck.AbstractMuleTestCase;
+import org.mule.util.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+
+import org.xml.sax.SAXException;
+
+public class SchemaValidationMule2225TestCase extends AbstractMuleTestCase
+{
+
+    // not available in 1.4 constants?
+    public static final String XML_SCHEMA = "http://www.w3.org/2001/XMLSchema";
+
+    public void testValidation() throws SAXException, IOException
+    {
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XML_SCHEMA);
+        schemaFactory.setFeature("http://apache.org/xml/features/validation/schema-full-checking", true);
+        Source muleXsd = new StreamSource(load("META-INF/mule.xsd"));
+        Schema schema = schemaFactory.newSchema(muleXsd);
+        Source muleRootTestXml = new StreamSource(load("org/mule/test/spring/mule-root-test.xml"));
+        schema.newValidator().validate(muleRootTestXml);
+    }
+
+    protected InputStream load(String name) throws IOException
+    {
+        InputStream stream = IOUtils.getResourceAsStream(name, getClass());
+        assertNotNull("Cannot load " + name, stream);
+        return stream;
+    }
+
+}
