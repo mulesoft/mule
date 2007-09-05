c67a6829daa2e42b0761f1f29c81669229fe5875
holger <holger>
2007-09-05 14:41:08 +0000

    turns out Travis and Andrews tests disagree ;-)
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@8211 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/tck/providers/AbstractConnectorTestCase.java
+++ core/src/main/java/org/mule/tck/providers/AbstractConnectorTestCase.java
@@ -74,6 +74,8 @@ public abstract class AbstractConnectorTestCase extends AbstractMuleTestCase
         }
 
         managementContext.getRegistry().registerConnector(connector, managementContext);
+        // TODO MULE-1988
+        managementContext.start();
     }
 
     protected void doTearDown() throws Exception
