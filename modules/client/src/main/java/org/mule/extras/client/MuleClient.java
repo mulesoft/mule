b130704f3047b76b7ac406cd1d3183a8d68862dc
Daniel Feist <dfeist@gmail.com>
2007-09-06 19:20:29 +0000

    Temporary fix for MULE-2267
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@8238 bf997673-6b11-0410-b953-e057580c5b09

--- modules/client/src/main/java/org/mule/extras/client/MuleClient.java
+++ modules/client/src/main/java/org/mule/extras/client/MuleClient.java
@@ -755,6 +755,8 @@ public class MuleClient implements Disposable
     public UMOMessage receive(String url, long timeout) throws UMOException
     {
         UMOEndpoint endpoint = getEndpoint(url, UMOEndpoint.ENDPOINT_TYPE_RECEIVER);
+        //TODO DF: MULE-2267
+        endpoint.setType(UMOEndpoint.ENDPOINT_TYPE_RECEIVER);
         try
         {
             UMOMessage message = endpoint.receive(timeout);
@@ -831,6 +833,8 @@ public class MuleClient implements Disposable
         throws UMOException
     {
         UMOEndpoint endpoint = getEndpoint(uri, UMOEndpoint.ENDPOINT_TYPE_SENDER);
+        //TODO DF: MULE-2267
+        endpoint.setType(UMOEndpoint.ENDPOINT_TYPE_SENDER);
         if (!endpoint.getConnector().isStarted() &&managementContext.isStarted())
         {
             endpoint.getConnector().start();
