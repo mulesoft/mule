1c62b5cf6e3f184006abe06218bfac6933a808b7
romikk <romikk>
2007-09-07 14:14:25 +0000

    MULE-1687 TCP Streaming Doesn't Work for Synchronous Calls
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@8275 bf997673-6b11-0410-b953-e057580c5b09

--- transports/tcp/src/test/java/org/mule/providers/tcp/issues/SynchStreamingMule1687TestCase.java
+++ transports/tcp/src/test/java/org/mule/providers/tcp/issues/SynchStreamingMule1687TestCase.java
@@ -11,10 +11,11 @@
 package org.mule.providers.tcp.issues;
 
 import org.mule.extras.client.MuleClient;
+import org.mule.providers.streaming.StreamMessageAdapter;
 import org.mule.tck.FunctionalTestCase;
-import org.mule.umo.UMOMessage;
+import org.mule.umo.provider.UMOStreamMessageAdapter;
 
-import java.util.HashMap;
+import java.io.ByteArrayInputStream;
 
 
 public class SynchStreamingMule1687TestCase extends FunctionalTestCase
@@ -34,13 +35,14 @@ public class SynchStreamingMule1687TestCase extends FunctionalTestCase
 
     public void testSendAndReceive() throws Exception
     {
-        if (true)
-        {
-            throw new Exception("This test hangs - remove this exception when a fix has been added");
-        }
+//        if (true)
+//        {
+//            throw new Exception("This test hangs - remove this exception when a fix has been added");
+//        }
         MuleClient client = new MuleClient();
 
-        UMOMessage message = client.send("tcp://localhost:65432", TEST_MESSAGE, new HashMap());
+        UMOStreamMessageAdapter message = client.sendStream("tcp://localhost:65432", new StreamMessageAdapter(new ByteArrayInputStream(TEST_MESSAGE.getBytes())));
+//        UMOMessage message = client.send("tcp://localhost:65432", TEST_MESSAGE, new HashMap());
 
         assertNotNull(message);
     }
