2ead7ea5da6632397f2940669184231ed46b62f2
Daniel Feist <dfeist@gmail.com>
2007-09-10 22:42:33 +0000

    MULE-2268 Create EndpointFactory to replace/consolidate the many endpoint getOrCreate methods in registry.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@8322 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/main/java/org/mule/umo/endpoint/UMOEndpointFactory.java
@@ -0,0 +1,91 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.umo.endpoint;
+
+import org.mule.umo.UMOException;
+import org.mule.umo.UMOManagementContext;
+
+/**
+ * Endpoint factory creates or returns previously created immutable instances of {@link UMOImmutableEndpoint}.
+ * These endpoints may be <br/>
+ * <li> Endpoints created by uri string of the type requested.
+ * <li> Endpoints of the type requested created based on a given global endpoint identified by global endpoint
+ * name.
+ * <li> Endpoints of the type requested based on an existing configured endpoint instance that has been given
+ * a name in configuration. <br/> <br/> <b>NOTE: Currently this factory will always return new unique endpoint
+ * instances, due to some pending endpoint mutability issues.</b><br/> In the future this factory will no
+ * only use the registry to obtain endpoint configured in the xml confgiuration with identifiers (name's) but
+ * will also register newly created endpoints in the registry keyed by type and uri, and are subsequent
+ * request to this factory for the same uri/idendifier and type will thus be able to return the same immutable
+ * instance.
+ */
+public interface UMOEndpointFactory
+{
+
+    /**
+     * Creates or return and existing endpoint to be used in the "INBOUND" role. <br/><br/> The uri parameter
+     * can either be a uri, or a (global) endpoint identifier or name. <br/><br/> The
+     * {@link UMOImmutableEndpoint} interface is currently used as the return type but this will be replaces
+     * by and more specific interface. SEE MULE-2292
+     * 
+     * @param uri endpoint identifier or uri
+     * @param managementContext
+     * @return
+     * @throws UMOException
+     */
+    public abstract UMOImmutableEndpoint createInboundEndpoint(String uri,
+                                                               UMOManagementContext managementContext)
+        throws UMOException;
+
+    /**
+     * Creates or return and existing endpoint to be used in the "INBOUND" role. <br/><br/> The uri parameter
+     * can either be a uri, or a (global) endpoint identifier or name. <br/><br/> The
+     * {@link UMOImmutableEndpoint} interface is currently used as the return type but this will be replaces
+     * by and more specific interface. SEE MULE-2292
+     * 
+     * @param uri endpoint identifier or uri
+     * @param managementContext
+     * @return
+     * @throws UMOException
+     */
+    public abstract UMOImmutableEndpoint createOutboundEndpoint(String uri,
+                                                                UMOManagementContext managementContext)
+        throws UMOException;
+
+    /**
+     * Creates or return and existing endpoint to be used in the "INBOUND" role. <br/><br/> The uri parameter
+     * can either be a uri, or a (global) endpoint identifier or name. <br/><br/> The
+     * {@link UMOImmutableEndpoint} interface is currently used as the return type but this will be replaces
+     * by and more specific interface. SEE MULE-2292<br/><br/> Also see MULE-2293.
+     * 
+     * @param uri endpoint identifier or uri.<br/><br/>
+     * @param managementContext
+     * @return
+     * @throws UMOException
+     */
+    public abstract UMOImmutableEndpoint createResponseEndpoint(String uri,
+                                                                UMOManagementContext managementContext)
+        throws UMOException;
+
+    /**
+     * @param endpointUri
+     * @param endpointType
+     * @param managementContext
+     * @return
+     * @throws EndpointException
+     * @throws UMOException
+     */
+    public abstract UMOImmutableEndpoint createEndpoint(UMOEndpointURI endpointUri,
+                                                        String endpointType,
+                                                        UMOManagementContext managementContext)
+        throws EndpointException, UMOException;
+
+}
