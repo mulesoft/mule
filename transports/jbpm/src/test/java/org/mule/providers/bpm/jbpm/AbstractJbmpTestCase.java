a885ffa0c3cf446b51bfa1c7219ccd72bb05fcfa
aperepel@gmail.com <Andrew Perepelytsya>
2007-09-25 21:43:46 +0000

    Some JBpm build fixes
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@8586 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/jbpm/src/test/java/org/mule/providers/bpm/jbpm/AbstractJbmpTestCase.java
@@ -0,0 +1,33 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.providers.bpm.jbpm;
+
+import org.mule.providers.bpm.tests.AbstractBpmTestCase;
+import org.mule.util.MuleDerbyTestUtils;
+
+public abstract class AbstractJbmpTestCase extends AbstractBpmTestCase
+{
+    private static boolean derbySetupDone = false;
+
+    protected void suitePreSetUp() throws Exception
+    {
+        if (!derbySetupDone)
+        {
+            String dbName = MuleDerbyTestUtils.loadDatabaseName("src/test/resources/derby.properties", "database.name");
+            System.getProperties().put("hibernate.dbURL", "jdbc:derby:" + dbName + ";sql.enforce_strict_size=true");
+            MuleDerbyTestUtils.defaultDerbyCleanAndInit("src/test/resources/derby.properties", "database.name");
+            derbySetupDone = true;
+        }
+
+        super.suitePreSetUp();
+    }
+
+}
