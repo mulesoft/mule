a9adc07305d6081cccba755aac413106245a459a
holger <holger>
2007-10-01 12:13:13 +0000

    Merged revisions 8695 via svnmerge from https://svn.codehaus.org/mule/trunk/mule
    ........
      r8695 | holger | 2007-09-28 18:32:02 +0200 (Fr, 28 Sep 2007) | 2 lines
      
      The XSL like Yoda worked, so backwards the config was.
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@8729 bf997673-6b11-0410-b953-e057580c5b09

--- tests/functional/src/main/java/org/mule/tck/AbstractConfigBuilderTestCase.java
+++ tests/functional/src/main/java/org/mule/tck/AbstractConfigBuilderTestCase.java
@@ -241,7 +241,7 @@ public abstract class AbstractConfigBuilderTestCase extends AbstractScriptConfig
 //        assertEquals(5, pp.getMaxIdle());
 //        assertEquals(10001, pp.getMaxWait());
 //        assertEquals(ObjectPool.WHEN_EXHAUSTED_WAIT, pp.getExhaustedAction());
-//        assertEquals(PoolingProfiler.INITIALISE_ONE, pp.getInitialisationPolicy());
+//        assertEquals(PoolingProfile.INITIALISE_ONE, pp.getInitialisationPolicy());
 //        assertTrue(pp.getPoolFactory() instanceof CommonsPoolFactory);
 
         // test per-descriptor overrides
@@ -252,7 +252,7 @@ public abstract class AbstractConfigBuilderTestCase extends AbstractScriptConfig
 //        assertEquals(9, pp.getMaxActive());
 //        assertEquals(6, pp.getMaxIdle());
 //        assertEquals(4002, pp.getMaxWait());
-//        assertEquals(ObjectPool.WHEN_EXHAUSTED_GROW, pp.getExhaustedAction());
+//        assertEquals(ObjectPool.WHEN_EXHAUSTED_FAIL, pp.getExhaustedAction());
 //        assertEquals(PoolingProfile.INITIALISE_ALL, pp.getInitialisationPolicy());
     }
 
