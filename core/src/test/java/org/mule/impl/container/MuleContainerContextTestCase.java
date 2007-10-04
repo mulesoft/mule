f6a943690097cc2477290eae91d24e9b3affbe45
aperepel@gmail.com <Andrew Perepelytsya>
2007-10-04 22:17:03 +0000

    No response, rolling back r8839,8840,8841. Travis, you must have mixed private and mule-2.x branches.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@8847 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/test/java/org/mule/impl/container/MuleContainerContextTestCase.java
@@ -0,0 +1,114 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.impl.container;
+
+import org.mule.tck.model.AbstractContainerContextTestCase;
+import org.mule.umo.UMODescriptor;
+import org.mule.umo.manager.UMOContainerContext;
+
+public class MuleContainerContextTestCase extends AbstractContainerContextTestCase
+{
+
+    MuleContainerContext context;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.mule.tck.model.AbstractComponentResolverTestCase#getConfiguredResolver()
+     */
+    public UMOContainerContext getContainerContext()
+    {
+        return context;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void doSetUp() throws Exception
+    {
+        context = new MuleContainerContext();
+    }
+
+    // /* (non-Javadoc)
+    // * @see
+    // org.mule.tck.model.AbstractComponentResolverTestCase#testDescriptorExternalReferences()
+    // */
+    // public void testDescriptorExternalReferences() throws Exception
+    // {
+    // UMODescriptor d = getTestDescriptor("friutbowl",
+    // FruitBowl.class.getName());
+    // resolver.resolveComponents(new Object(), d.getObjectReferences());
+    //
+    // d.addObjectReference(new ObjectReference("foo", "bar", true));
+    // try
+    // {
+    // resolver.resolveComponents(new Object(), d.getObjectReferences());
+    // fail("Should barf if component isn't a class name");
+    // }
+    // catch (ContainerException e)
+    // {
+    // // expected
+    // }
+    // d.getObjectReferences().clearErrors();
+    // //Should work as the ref is not required
+    // d.addObjectReference(new ObjectReference("foo", "bar", false));
+    // resolver.resolveComponents(new Object(), d.getObjectReferences());
+    //
+    // d.getObjectReferences().clearErrors();
+    // FruitBowl fb = new FruitBowl();
+    // d.addObjectReference(new ObjectReference("banana",
+    // Banana.class.getName(), true));
+    // resolver.resolveComponents(fb, d.getObjectReferences());
+    // assertNotNull(fb.getBanana());
+    // assertNull(fb.getApple());
+    // }
+    //
+    // public void testDescriptorExternalReferenceAsFactory() throws Exception
+    // {
+    // UMODescriptor d = getTestDescriptor("friutbowl",
+    // FruitBowl.class.getName());
+    // resolver.resolveComponents(new Object(), d.getObjectReferences());
+    //
+    // FruitBowl fb = new FruitBowl();
+    //
+    // d.addObjectReference(new ObjectReference("banana",
+    // BananaFactory.class.getName(), true));
+    // resolver.resolveComponents(fb, d.getObjectReferences());
+    // assertNotNull(fb.getBanana());
+    // assertNull(fb.getApple());
+    // }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.mule.tck.model.AbstractComponentResolverTestCase#testExternalUMOReference()
+     */
+    // public void testExternalUMOReference() throws Exception
+    // {
+    // //The Mule component resolver doesn't support autowiring of components
+    // context.resolveComponents(null, null);
+    // }
+    public void testExternalUMOReference() throws Exception
+    {
+        UMOContainerContext ctx = getContainerContext();
+        assertNotNull(ctx);
+
+        UMODescriptor descriptor = getTestDescriptor("fruit Bowl", "org.mule.tck.testmodels.fruit.FruitBowl");
+        
+        fail("Need to figure out whether this test is relevant for Mule 2.0");
+        // TODO MULE-1908
+        //FruitBowl fruitBowl = (FruitBowl)ctx.getComponent(descriptor.getService());
+        //assertNotNull(fruitBowl);
+    }
+
+}
