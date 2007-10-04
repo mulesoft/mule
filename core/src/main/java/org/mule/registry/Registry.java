44a651563922a5e6a327fa9303f0635ed71133ea
aperepel@gmail.com <Andrew Perepelytsya>
2007-10-04 14:09:21 +0000

    JavaDoc syntax fixes and cleanup
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@8825 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/registry/Registry.java
+++ core/src/main/java/org/mule/registry/Registry.java
@@ -14,7 +14,6 @@ import org.mule.config.MuleConfiguration;
 import org.mule.umo.UMODescriptor;
 import org.mule.umo.UMOException;
 import org.mule.umo.UMOManagementContext;
-import org.mule.umo.endpoint.EndpointException;
 import org.mule.umo.endpoint.UMOEndpointBuilder;
 import org.mule.umo.endpoint.UMOEndpointURI;
 import org.mule.umo.endpoint.UMOImmutableEndpoint;
@@ -65,19 +64,14 @@ public interface Registry extends Initialisable, Disposable
      * <b>NOTE: This method does not create new endpoint instances, but rather returns existing endpoint
      * instances that have been registered. This lookup method should be avoided and the intelligent, role
      * specific endpoint lookup methods should be used instead.<br/><br/>
-     * {@link #lookupInboundEndpoint(String)} {@link #lookupOutboundEndpoint(String)}
-     * {@link #lookupResponseEndpoint(String)}
-     * 
      * @param name the idendtifer/name used to register endpoint in registry
-     * @return
+     * @see #lookupInboundEndpoint(String, org.mule.umo.UMOManagementContext)
+     * @see #lookupResponseEndpoint(String, org.mule.umo.UMOManagementContext)
      */
     UMOImmutableEndpoint lookupEndpoint(String name, UMOManagementContext managementContext);
 
     /**
      * Deprecated. Use {@link #lookupEndpoint(String, UMOManagementContext)}
-     * @param name
-     * @param managementContext
-     * @return
      * @deprecated
      */
     UMOImmutableEndpoint lookupEndpoint(String name);
@@ -87,8 +81,6 @@ public interface Registry extends Initialisable, Disposable
      * These endpoint builder are either global endpoints or they are builders used to create named
      * endpoints configured on routers and exception strategies.
      * 
-     * @param name
-     * @return
      */
     UMOEndpointBuilder lookupEndpointBuilder(String name);
     
@@ -102,8 +94,6 @@ public interface Registry extends Initialisable, Disposable
      * <br/><br/> The {@link UMOImmutableEndpoint} interface is currently used as the return type but this
      * will be replaces by and more specific interface. SEE MULE-2292
      * 
-     * @param uri
-     * @return
      */
     UMOImmutableEndpoint lookupInboundEndpoint(String uri, UMOManagementContext managementContext)
         throws UMOException;
@@ -118,8 +108,6 @@ public interface Registry extends Initialisable, Disposable
      * <br/><br/> The {@link UMOImmutableEndpoint} interface is currently used as the return type but this
      * will be replaces by and more specific interface. SEE MULE-2292
      * 
-     * @param uri
-     * @return
      */
     UMOImmutableEndpoint lookupOutboundEndpoint(String uri, UMOManagementContext managementContext)
         throws UMOException;
@@ -134,24 +122,16 @@ public interface Registry extends Initialisable, Disposable
      * <br/><br/> The {@link UMOImmutableEndpoint} interface is currently used as the return type but this
      * will be replaces by and more specific interface. SEE MULE-2292
      * 
-     * @param uri
-     * @return
      */
     UMOImmutableEndpoint lookupResponseEndpoint(String uri, UMOManagementContext managementContext)
         throws UMOException;
 
     /**
-     * @param endpointUri
-     * @param endpointType
-     * @param managementContext
-     * @return
-     * @throws EndpointException
-     * @throws UMOException
      */
     UMOImmutableEndpoint createEndpoint(UMOEndpointURI endpointUri,
                                         String endpointType,
                                         UMOManagementContext managementContext)
-        throws EndpointException, UMOException;
+        throws UMOException;
 
     UMOTransformer lookupTransformer(String name);
 
