f9446d3dc2d66e95dd9aa9c14b92a413431c1712
tcarlson <tcarlson>
2007-10-10 11:35:35 +0000

    Tests updated
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@9023 bf997673-6b11-0410-b953-e057580c5b09

--- tests/functional/src/test/java/org/mule/test/components/ServiceDescriptorTestCase.java
+++ tests/functional/src/test/java/org/mule/test/components/ServiceDescriptorTestCase.java
@@ -13,9 +13,7 @@ package org.mule.test.components;
 import org.mule.tck.FunctionalTestCase;
 import org.mule.tck.testmodels.fruit.Orange;
 import org.mule.umo.UMOComponent;
-import org.mule.umo.model.UMOModel;
 
-// TODO Update this test to use TestSedaModel so that we can have access to the underlying ServiceFactory.
 public class ServiceDescriptorTestCase extends FunctionalTestCase
 {
     protected String getConfigResources()
@@ -25,43 +23,41 @@ public class ServiceDescriptorTestCase extends FunctionalTestCase
 
     public void testGenericObjectFactory() throws Exception
     {
-//        UMOModel model = managementContext.getRegistry().lookupModel("main");
-//        UMOComponent c = model.getComponent("orange1");
-//        
-//        Object service = ComponentFactory.createService(c.getDescriptor());
-//        assertTrue("Service should be an Orange", service instanceof Orange);
-//        // Default values
-//        assertEquals(new Integer(10), ((Orange) service).getSegments());
+        UMOComponent c = managementContext.getRegistry().lookupComponent("orange1");
+        
+        Object service =  c.getServiceFactory().getOrCreate();
+        assertTrue("Service should be an Orange", service instanceof Orange);
+        // Default values
+        assertEquals(new Integer(10), ((Orange) service).getSegments());
     }
     
-//    public void testGenericObjectFactoryWithProperties() throws Exception
-//    {
-//        UMOModel model = managementContext.getRegistry().lookupModel("main");
-//        UMOComponent c = model.getComponent("orange2");
-//
-//        // Create an orange
-//        Object service = ComponentFactory.createService(c.getDescriptor());
-//        assertTrue("Service should be an Orange", service instanceof Orange);
-//        assertEquals(new Integer(8), ((Orange) service).getSegments());
-//        assertEquals("Florida Sunny", ((Orange) service).getBrand());
-//
-//        // Create another orange
-//        service = ComponentFactory.createService(c.getDescriptor());
-//        assertTrue("Service should be an Orange", service instanceof Orange);
-//        assertEquals(new Integer(8), ((Orange) service).getSegments());
-//        assertEquals("Florida Sunny", ((Orange) service).getBrand());
-//    }
-//    
-//    public void testSingletonObjectFactory() throws Exception
-//    {
-//        UMOModel model = managementContext.getRegistry().lookupModel("main");
-//        UMOComponent c = model.getComponent("orange3");
-//        Object service = ComponentFactory.createService(c.getDescriptor());
-//        assertTrue("Service should be an Orange", service instanceof Orange);
-//        // Default values
-//        assertEquals(new Integer(10), ((Orange) service).getSegments());
-//    }
-//    
+    public void testGenericObjectFactoryWithProperties() throws Exception
+    {
+        UMOComponent c = managementContext.getRegistry().lookupComponent("orange2");
+
+        // Create an orange
+        Object service =  c.getServiceFactory().getOrCreate();
+        assertTrue("Service should be an Orange", service instanceof Orange);
+        assertEquals(new Integer(8), ((Orange) service).getSegments());
+        assertEquals("Florida Sunny", ((Orange) service).getBrand());
+
+        // Create another orange
+        service =  c.getServiceFactory().getOrCreate();
+        assertTrue("Service should be an Orange", service instanceof Orange);
+        assertEquals(new Integer(8), ((Orange) service).getSegments());
+        assertEquals("Florida Sunny", ((Orange) service).getBrand());
+    }
+    
+    public void testSingletonObjectFactory() throws Exception
+    {
+        UMOComponent c = managementContext.getRegistry().lookupComponent("orange3");
+        Object service =  c.getServiceFactory().getOrCreate();
+        assertTrue("Service should be an Orange", service instanceof Orange);
+        // Default values
+        assertEquals(new Integer(10), ((Orange) service).getSegments());
+    }
+    
+      // TODO MULE-2060  
 //    public void testCustomFactory() throws Exception
 //    {
 //        UMOModel model = managementContext.getRegistry().lookupModel("main");
