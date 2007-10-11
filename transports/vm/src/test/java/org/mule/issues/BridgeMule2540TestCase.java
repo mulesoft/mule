3ebe2f02aad0b0bff3a3ea83fbd15238b93cf7a3
acooke <acooke>
2007-10-11 20:15:53 +0000

    MULE-2540, MULE-2541: Restoring old schema names, as far as I remember, and patching back in existing bridge service.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@9078 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/vm/src/test/java/org/mule/issues/BridgeMule2540TestCase.java
@@ -0,0 +1,45 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.issues;
+
+import org.mule.extras.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.umo.UMOMessage;
+
+public class BridgeMule2540TestCase extends FunctionalTestCase
+{
+    
+    private static final int RECEIVE_TIMEOUT = 5000;
+
+    protected String getConfigResources()
+    {
+        return "vm/persistent-vmqueue-test.xml";
+    }
+
+    public void testBridge() throws Exception
+    {
+        String input = "Test message";
+        String[] output = {"Test", "message"};
+        MuleClient client = new MuleClient();
+        client.dispatch("vm://receiver", input, null);
+        UMOMessage result = client.receive("vm://out", RECEIVE_TIMEOUT);
+        assertNotNull(result);
+        assertNotNull(result.getPayload());
+        assertNull(result.getExceptionPayload());
+        String[] payload = (String[]) result.getPayload();
+        assertEquals(output.length, payload.length);
+        for (int i = 0; i < output.length; i++)
+        {
+            assertEquals(output[i], payload[i]);
+        }
+    }
+
+}
