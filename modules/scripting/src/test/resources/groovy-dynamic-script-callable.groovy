24cead753ba6b60d8d6a205f9d8ffc09bfde1780
aperepel@gmail.com <Andrew Perepelytsya>
2007-11-07 20:41:27 +0000

    Reformatted to be actually readable.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@9652 bf997673-6b11-0410-b953-e057580c5b09

--- modules/scripting/src/test/resources/groovy-dynamic-script-callable.groovy
+++ modules/scripting/src/test/resources/groovy-dynamic-script-callable.groovy
@@ -1,1 +1,10 @@
-import org.mule.umo.UMOEventContext; import org.mule.umo.lifecycle.Callable; public class GroovyDynamicScript implements Callable { public Object onCall(UMOEventContext eventContext) throws Exception{ return eventContext.getMessage().getPayloadAsString() + " Received2"; }}
+import org.mule.umo.UMOEventContext
+import org.mule.umo.lifecycle.Callable
+
+public class GroovyDynamicScript implements Callable
+{
+    public Object onCall(UMOEventContext eventContext) throws Exception
+    {
+        return eventContext.getMessage().getPayloadAsString() + " Received2"
+    }
+}
