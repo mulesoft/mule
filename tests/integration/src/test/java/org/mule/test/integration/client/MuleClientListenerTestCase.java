00c2c7a15b5175ebc853de2744ec61eb76ba7633
tcarlson <tcarlson>
2007-11-09 16:46:53 +0000

    Convert test to be XML config-based and update for 2.x. The test has changed slightly because it used to programatically create a component, register and unregister it, which would implicitly start/stop the component in 1.x.  The XML-based component now has initialState="stopped", and the test explicitly starts and stops it.  Note that it is impossible to unregister a component from the SpringRegistry since it is read-only!
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@9674 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/client/MuleClientListenerTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/client/MuleClientListenerTestCase.java
@@ -12,17 +12,10 @@ package org.mule.test.integration.client;
 
 
 import org.mule.extras.client.MuleClient;
-import org.mule.impl.endpoint.EndpointURIEndpointBuilder;
-import org.mule.impl.model.seda.SedaComponent;
-import org.mule.routing.inbound.InboundRouterCollection;
 import org.mule.tck.FunctionalTestCase;
-import org.mule.test.integration.service.TestReceiver;
-import org.mule.transformers.simple.ByteArrayToString;
+import org.mule.umo.UMOComponent;
 import org.mule.umo.UMOMessage;
-import org.mule.umo.endpoint.UMOEndpointBuilder;
-import org.mule.umo.endpoint.UMOImmutableEndpoint;
 import org.mule.umo.provider.NoReceiverForEndpointException;
-import org.mule.util.object.SingletonObjectFactory;
 
 
 public class MuleClientListenerTestCase extends FunctionalTestCase
@@ -32,17 +25,15 @@ public class MuleClientListenerTestCase extends FunctionalTestCase
         return "org/mule/test/integration/client/mule-client-listener-config.xml";
     }
 
-    public void doTestRegisterListener(String urlString, boolean canSendWithoutReceiver) throws Exception
+    public void doTestRegisterListener(String component, String endpoint, boolean canSendWithoutReceiver) throws Exception
     {
         MuleClient client = new MuleClient();
-        client.getConfiguration().setDefaultSynchronousEndpoints(true);
-        client.getConfiguration().setDefaultRemoteSync(true);
 
         if (!canSendWithoutReceiver)
         {
             try
             {
-                client.send(urlString, "Test Client Send message", null);
+                client.send(endpoint, "Test Client Send message", null);
                 fail("There is no receiver for this endpointUri");
             }
             catch (Exception e)
@@ -50,39 +41,23 @@ public class MuleClientListenerTestCase extends FunctionalTestCase
                 assertTrue(e.getCause() instanceof NoReceiverForEndpointException);
             }
         }
+        
+        UMOComponent c = managementContext.getRegistry().lookupComponent(component);
+        c.start();
 
-        TestReceiver receiver = new TestReceiver();
-        // we need to code the component creation here, which isn't ideal, see
-        // MULE-1060
-        String name = "myComponent";
-        SedaComponent descriptor = new SedaComponent();
-        descriptor.setName(name);
-        descriptor.setServiceFactory(new SingletonObjectFactory(receiver));
-        managementContext.getRegistry().registerComponent(descriptor, managementContext);
-
-        UMOEndpointBuilder endpointBuilder = new EndpointURIEndpointBuilder(urlString, managementContext);
-        endpointBuilder.addTransformer(new ByteArrayToString());
-        UMOImmutableEndpoint endpoint = managementContext.getRegistry().lookupEndpointFactory().getInboundEndpoint(
-            endpointBuilder, managementContext);
-        descriptor.setInboundRouter(new InboundRouterCollection());
-        descriptor.getInboundRouter().addEndpoint(endpoint);
-
-        // No longer a valid assetion
-        //assertTrue(managementContext.getRegistry().lookupModel("main").isComponentRegistered(name));
-
-        UMOMessage message = client.send(urlString, "Test Client Send message", null);
+        UMOMessage message = client.send(endpoint, "Test Client Send message", null);
         assertNotNull(message);
         assertEquals("Received: Test Client Send message", message.getPayloadAsString());
-        client.unregisterComponent(name);
 
-        // No longer a valid assetion
-        // assertTrue(!managementContext.getRegistry().lookupModel("main").isComponentRegistered(name));
+        // The SpringRegistry is read-only so we can't unregister the component!
+        //managementContext.getRegistry().unregisterComponent("vmComponent");
+        c.stop();
 
         if (!canSendWithoutReceiver)
         {
             try
             {
-                message = client.send(urlString, "Test Client Send message", null);
+                message = client.send(endpoint, "Test Client Send message", null);
                 fail("There is no receiver for this endpointUri");
             }
             catch (Exception e)
@@ -94,12 +69,12 @@ public class MuleClientListenerTestCase extends FunctionalTestCase
 
     public void testRegisterListenerVm() throws Exception
     {
-        doTestRegisterListener("vm://test.queue", false);
+        doTestRegisterListener("vmComponent", "vm://test.queue", false);
     }
 
     public void testRegisterListenerTcp() throws Exception
     {
-        doTestRegisterListener("tcp://localhost:56324", true);
+        doTestRegisterListener("tcpComponent", "tcp://localhost:56324", true);
     }
 
 }
