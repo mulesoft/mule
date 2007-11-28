e9e6f65934605ee56b7a8970803e4f68978eb3b8
aperepel@gmail.com <Andrew Perepelytsya>
2007-11-28 13:55:28 +0000

    Minor review.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@9925 bf997673-6b11-0410-b953-e057580c5b09

--- transports/http/src/main/java/org/mule/providers/http/PollingHttpMessageReceiver.java
+++ transports/http/src/main/java/org/mule/providers/http/PollingHttpMessageReceiver.java
@@ -24,7 +24,7 @@ import org.mule.umo.lifecycle.CreateException;
 import org.mule.umo.provider.UMOConnector;
 import org.mule.util.MapUtils;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
 
 /** Will poll an http URL and use the response as the input for a service request. */
@@ -67,10 +67,9 @@ public class PollingHttpMessageReceiver extends AbstractPollingMessageReceiver
     public void poll() throws Exception
     {
         UMOMessage req = new MuleMessage(new DefaultMessageAdapter(""));
-        if (etag != null && checkEtag) 
+        if (etag != null && checkEtag)
         {
-            Map customHeaders = new HashMap();
-            customHeaders.put(HttpConstants.HEADER_IF_NONE_MATCH, etag);
+            Map customHeaders = Collections.singletonMap(HttpConstants.HEADER_IF_NONE_MATCH, etag);
             req.setProperty(HttpConnector.HTTP_CUSTOM_HEADERS_MAP_PROPERTY, customHeaders);
         }
         req.setProperty(HttpConnector.HTTP_METHOD_PROPERTY, "GET");
