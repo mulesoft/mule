1d150e099238ae978db19b5df52bf4425a25ea2e
acooke <acooke>
2007-11-28 17:30:59 +0000

    MULE-2582: Change this one too, in case we keep it.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@9933 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/oaq/src/main/java/org/mule/providers/oracle/jms/OracleJmsMessageRequester.java
@@ -0,0 +1,46 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.providers.oracle.jms;
+
+import org.mule.providers.jms.JmsConnector;
+import org.mule.providers.jms.JmsMessageRequester;
+import org.mule.umo.UMOMessage;
+import org.mule.umo.endpoint.UMOImmutableEndpoint;
+
+public class OracleJmsMessageRequester extends JmsMessageRequester
+{
+
+    public OracleJmsMessageRequester(UMOImmutableEndpoint endpoint)
+    {
+        super(endpoint);
+    }
+
+    /**
+     * Make a specific request to the underlying transport Save a copy of the
+     * endpoint's properties within the OracleJmsSupport object.
+     *
+     * @see org.mule.providers.oracle.jms.OracleJmsSupport#endpointProperties
+     * @param endpoint the endpoint to use when connecting to the resource
+     * @param timeout the maximum time the operation should block before returning.
+     *            The call should return immediately if there is data available. If
+     *            no data becomes available before the timeout elapses, null will be
+     *            returned
+     * @return the result of the request wrapped in a UMOMessage object. Null will be
+     *         returned if no data was avaialable
+     * @throws Exception if the call to the underlying protocal cuases an exception
+     */
+    protected UMOMessage doRequest(long timeout) throws Exception
+    {
+        ((OracleJmsSupport)((JmsConnector)getConnector()).getJmsSupport()).setEndpointProperties(endpoint.getEndpointURI()
+            .getParams());
+        return super.doRequest(timeout);
+    }
+}
