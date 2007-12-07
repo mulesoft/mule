ce08db9139edf839af942970e061fa86b11248c0
Daniel Feist <dfeist@gmail.com>
2007-12-07 13:20:38 +0000

    Failing test for MULE-2779 (Startup properties do not work yet with 2.x)
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@10031 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/functional/src/test/java/org/mule/test/config/StartupPropertiesTestCase.java
@@ -0,0 +1,42 @@
+package org.mule.test.config;
+
+import org.mule.tck.FunctionalTestCase;
+
+import java.util.Properties;
+
+public class StartupPropertiesTestCase extends FunctionalTestCase
+{
+    
+    private String STARTUP_PROPERTY_1_KEY = "startupProperty1";
+    private String STARTUP_PROPERTY_2_KEY = "startupProperty2";
+    private String STARTUP_PROPERTY_1_VALUE = "startupProperty1Value";
+    private String STARTUP_PROPERTY_2_VALUE = "startupProperty2Value";
+    
+    
+    protected String getConfigResources()
+    {
+        return "org/mule/test/config/startup-properties-test.xml";
+    }
+    
+    protected Properties getStartUpProperties()
+    {
+        Properties p = new Properties();
+        p.setProperty(STARTUP_PROPERTY_1_KEY, STARTUP_PROPERTY_1_VALUE);
+        p.setProperty(STARTUP_PROPERTY_2_KEY, STARTUP_PROPERTY_2_VALUE);
+        return p;
+    }
+    
+    public void testStartProperties(){
+        Object property1 = managementContext.getRegistry().lookupObject(STARTUP_PROPERTY_1_KEY);
+        Object property2 = managementContext.getRegistry().lookupObject(STARTUP_PROPERTY_2_KEY);
+        assertNotNull(property1);
+        assertNotNull(property2);
+        assertEquals(STARTUP_PROPERTY_1_VALUE, property1);
+        assertEquals(STARTUP_PROPERTY_2_VALUE, property2);
+
+        
+    }
+    
+    
+
+}
