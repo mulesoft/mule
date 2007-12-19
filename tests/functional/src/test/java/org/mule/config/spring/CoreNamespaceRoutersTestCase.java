29f4d2ba3d2c78ca96f4c5babe8e60fd76e66664
tcarlson <tcarlson>
2007-12-19 20:44:47 +0000

    Fix MULE-2602 and start on MULE-2758
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@10118 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/functional/src/test/java/org/mule/config/spring/CoreNamespaceRoutersTestCase.java
@@ -0,0 +1,63 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.config.spring;
+
+import org.mule.routing.inbound.ForwardingConsumer;
+import org.mule.routing.inbound.IdempotentSecureHashReceiver;
+import org.mule.routing.inbound.InboundPassThroughRouter;
+import org.mule.routing.inbound.SelectiveConsumer;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.umo.UMOComponent;
+import org.mule.umo.routing.UMORouter;
+
+import java.util.List;
+
+public class CoreNamespaceRoutersTestCase extends FunctionalTestCase
+{
+    public String getConfigResources()
+    {
+        return "core-namespace-routers.xml";
+    }
+
+    public void testForwardingRouter() throws Exception
+    {
+        UMORouter r = lookupInboundRouterFromService("ForwardingRouter");
+        assertTrue(r instanceof ForwardingConsumer);
+    }
+
+    public void testIdempotentSecureHashReceiverRouter() throws Exception
+    {
+        UMORouter r = lookupInboundRouterFromService("IdempotentSecureHashReceiverRouter");
+        assertTrue(r instanceof IdempotentSecureHashReceiver);
+    }
+
+    public void testInboundPassThroughRouter() throws Exception
+    {
+        UMORouter r = lookupInboundRouterFromService("InboundPassThroughRouter");
+        assertTrue(r instanceof InboundPassThroughRouter);
+    }
+
+    public void testSelectiveConsumerRouter() throws Exception
+    {
+        UMORouter r = lookupInboundRouterFromService("SelectiveConsumerRouter");
+        assertTrue(r instanceof SelectiveConsumer);
+        assertFalse(((SelectiveConsumer) r).isTransformFirst());
+    }
+
+    protected UMORouter lookupInboundRouterFromService(String serviceName) throws Exception
+    {
+        UMOComponent c = managementContext.getRegistry().lookupComponent(serviceName);
+        assertNotNull(c);
+        List routers = c.getInboundRouter().getRouters();
+        assertEquals(1, routers.size());
+        assertTrue(routers.get(0) instanceof UMORouter);
+        return (UMORouter) routers.get(0);
+    }
+}
