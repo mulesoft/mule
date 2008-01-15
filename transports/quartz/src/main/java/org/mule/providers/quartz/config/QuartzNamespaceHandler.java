6e63db3fa26ee651c9f7d2498b9d13b86dce6d08
acooke <acooke>
2008-01-15 16:48:20 +0000

    Zapping spring:entry by the guazillion.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@10332 bf997673-6b11-0410-b953-e057580c5b09

--- transports/quartz/src/main/java/org/mule/providers/quartz/config/QuartzNamespaceHandler.java
+++ transports/quartz/src/main/java/org/mule/providers/quartz/config/QuartzNamespaceHandler.java
@@ -11,7 +11,9 @@ package org.mule.providers.quartz.config;
 
 import org.mule.config.spring.handlers.AbstractMuleNamespaceHandler;
 import org.mule.config.spring.parsers.collection.ChildMapDefinitionParser;
+import org.mule.config.spring.parsers.collection.ChildSingletonMapDefinitionParser;
 import org.mule.config.spring.parsers.generic.MuleOrphanDefinitionParser;
+import org.mule.config.spring.parsers.specific.properties.NestedMapDefinitionParser;
 import org.mule.impl.endpoint.URIBuilder;
 import org.mule.providers.quartz.QuartzConnector;
 
@@ -25,7 +27,9 @@ public class QuartzNamespaceHandler extends AbstractMuleNamespaceHandler
     {
         registerStandardTransportEndpoints(QuartzConnector.QUARTZ, URIBuilder.PATH_ATTRIBUTES);
         registerMuleBeanDefinitionParser("connector", new MuleOrphanDefinitionParser(QuartzConnector.class, true)).addAlias("scheduler", "quartzScheduler");
-        registerBeanDefinitionParser("factory-properties", new ChildMapDefinitionParser("factoryProperties"));
+        // note that we use the singular (factoryProperty) for the setter so that we auto-detect a collection
+        registerBeanDefinitionParser("factory-property", new ChildSingletonMapDefinitionParser("factoryProperty"));
+        registerBeanDefinitionParser("factory-properties", new ChildMapDefinitionParser("factoryProperty"));
     }
 
 }
