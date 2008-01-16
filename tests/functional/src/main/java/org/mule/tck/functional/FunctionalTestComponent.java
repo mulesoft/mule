3a37bdb973ede6022f5cac0ca758ae6a40486c09
tcarlson <tcarlson>
2008-01-16 13:25:28 +0000

    Some additions to the TCK
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@10343 bf997673-6b11-0410-b953-e057580c5b09

--- tests/functional/src/main/java/org/mule/tck/functional/FunctionalTestComponent.java
+++ tests/functional/src/main/java/org/mule/tck/functional/FunctionalTestComponent.java
@@ -14,6 +14,7 @@ import org.mule.MuleException;
 import org.mule.MuleServer;
 import org.mule.config.i18n.MessageFactory;
 import org.mule.impl.RequestContext;
+import org.mule.tck.exceptions.FunctionalTestException;
 import org.mule.umo.UMOEventContext;
 import org.mule.umo.UMOManagementContext;
 import org.mule.umo.lifecycle.Callable;
@@ -126,7 +127,7 @@ public class FunctionalTestComponent implements Callable, Initialisable, Disposa
 
         if (throwException)
         {
-            throw new MuleException(MessageFactory.createStaticMessage("Functional Test Component Exception"));
+            throw new FunctionalTestException();
         }
 
         return replyMessage;
---
+++ tests/functional/src/main/java/org/mule/tck/functional/TransactionalFunctionalTestComponent.java
@@ -0,0 +1,73 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.tck.functional;
+
+import org.mule.config.i18n.MessageFactory;
+import org.mule.umo.TransactionException;
+import org.mule.umo.UMOEventContext;
+import org.mule.umo.UMOTransaction;
+
+/**
+ * This component is useful for unit tests involving transactionality because it
+ * will roll back the current transaction upon message arrival.  
+ */
+public class TransactionalFunctionalTestComponent extends FunctionalTestComponent
+{
+    private boolean expectTransaction = true;
+    private boolean rollback = true;
+
+    /** {@inheritDoc} */
+    public Object onCall(UMOEventContext context) throws Exception
+    {
+        Object replyMessage = super.onCall(context);
+
+        if (expectTransaction)
+        {
+            // Verify transaction has begun.
+            UMOTransaction currentTx = context.getCurrentTransaction();
+            if (currentTx == null || currentTx.isBegun() == false)
+            {    
+                context.setStopFurtherProcessing(true);
+                throw new TransactionException(MessageFactory.createStaticMessage("Trying to roll back transaction but no transaction is underway."));
+            }            
+
+            if (rollback)
+            {
+                // Mark the transaction for rollback.
+                logger.info("@@@@ Rolling back transaction @@@@");
+                currentTx.setRollbackOnly();
+            }        
+        }
+        
+        return replyMessage;
+    }
+
+    public boolean isRollback()
+    {
+        return rollback;
+    }
+
+    public void setRollback(boolean rollback)
+    {
+        this.rollback = rollback;
+    }
+
+    public boolean isExpectTransaction()
+    {
+        return expectTransaction;
+    }
+
+    public void setExpectTransaction(boolean expectTransaction)
+    {
+        this.expectTransaction = expectTransaction;
+    }
+}
+
