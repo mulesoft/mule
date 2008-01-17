af1d613dd81efb9d5d443fc762bca73be964796d
tcarlson <tcarlson>
2008-01-17 20:46:38 +0000

    Merged revisions 9655 via svnmerge from https://svn.codehaus.org/mule/trunk/mule
    ........
      r9655 | akuzmin | 2007-11-08 09:09:07 -0200 (Thu, 08 Nov 2007) | 1 line
      
      MULE-2011 Reimplement org.mule.test.integration.providers.jms to use real configs
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@10379 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/jms/src/test/java/org/mule/providers/jms/integration/JmsXATransactionComponentTestCase.java
@@ -0,0 +1,49 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.providers.jms.integration;
+
+import org.mule.extras.client.MuleClient;
+import org.mule.umo.UMOMessage;
+
+
+
+public class JmsXATransactionComponentTestCase extends AbstractJmsFunctionalTestCase
+{
+    public static final String CONNECTOR1_NAME = "jmsConnector";
+    
+    protected String getConfigResources()
+    {
+        return "providers/activemq/jms-xa-tx-component.xml";
+    }
+
+    public void testOneGlobalTx() throws Exception
+    {
+        UMOMessage result = null;
+        MuleClient client = new MuleClient();
+        client.dispatch("vm://in", DEFAULT_INPUT_MESSAGE, null);
+        result = client.request("vm://out", TIMEOUT);
+        assertNotNull(result);
+        result = client.request("vm://out", SMALL_TIMEOUT);
+        assertNull(result);
+
+        managementContext.getRegistry().lookupConnector(CONNECTOR1_NAME).stop();
+        assertEquals(managementContext.getRegistry().lookupConnector(CONNECTOR1_NAME).isStarted(), false);
+        logger.info(CONNECTOR1_NAME + " is stopped");
+        client.dispatch("vm://in", DEFAULT_INPUT_MESSAGE, null);
+        Thread.sleep(1000);
+        managementContext.getRegistry().lookupConnector(CONNECTOR1_NAME).start();
+        Thread.sleep(1000);
+        logger.info(CONNECTOR1_NAME + " is started");
+        result = client.request("vm://out", TIMEOUT);
+        assertNotNull(result);
+        result = client.request("vm://out", SMALL_TIMEOUT);
+        assertNull(result);
+    }
+}
