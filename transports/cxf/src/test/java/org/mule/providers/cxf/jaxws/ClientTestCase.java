e8be370244ce27dbcc372efdbcf3a02048d964e4
Dan Diephouse <dan@netzooid.com>
2008-01-22 23:32:56 +0000

    Bump up wait time for build server.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@10471 bf997673-6b11-0410-b953-e057580c5b09

--- transports/cxf/src/test/java/org/mule/providers/cxf/jaxws/ClientTestCase.java
+++ transports/cxf/src/test/java/org/mule/providers/cxf/jaxws/ClientTestCase.java
@@ -25,7 +25,7 @@ public class ClientTestCase extends FunctionalTestCase
     {
         GreeterImpl impl = getGreeter();
         
-        Thread.sleep(2000);
+        Thread.sleep(3000);
         
         assertEquals(1, impl.getInvocationCount());
     }
@@ -47,7 +47,7 @@ public class ClientTestCase extends FunctionalTestCase
         
         GreeterImpl impl = getGreeter();
         
-        Thread.sleep(2000);
+        Thread.sleep(3000);
         
         assertEquals(2, impl.getInvocationCount());
     }
