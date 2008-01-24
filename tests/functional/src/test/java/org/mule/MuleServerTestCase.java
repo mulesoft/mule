3cc971dd917dadabcd019644e83bc227b23f9a57
Daniel Feist <dfeist@gmail.com>
2008-01-24 16:47:03 +0000

    MULE-2936 Improve error/exception handling and reporting in MuleServer for ConfigBuilder reflection.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@10522 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/functional/src/test/java/org/mule/MuleServerTestCase.java
@@ -0,0 +1,73 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule;
+
+import org.mule.api.MuleContext;
+import org.mule.tck.AbstractMuleTestCase;
+import org.mule.util.ClassUtils;
+
+public class MuleServerTestCase extends AbstractMuleTestCase
+{
+
+    private MuleServer muleServer;
+
+    @Override
+    protected MuleContext createMuleContext() throws Exception
+    {
+        return null;
+    }
+
+    public void testMuleServer() throws Exception
+    {
+        muleServer = new MuleServer();
+        assertEquals(ClassUtils.getResource("mule-config.xml", MuleServer.class).toString(),
+            muleServer.getConfigurationResources());
+        assertEquals("org.mule.config.builders.AutoConfigurationBuilder", muleServer.getConfigBuilderClassName());
+        muleServer.initialize();
+    }
+
+    public void testMuleServerResource() throws Exception
+    {
+        muleServer = new MuleServer("org/mule/test/spring/config1/test-xml-mule2-config.xml");
+        assertEquals("org/mule/test/spring/config1/test-xml-mule2-config.xml", muleServer.getConfigurationResources());
+        assertEquals("org.mule.config.builders.AutoConfigurationBuilder", muleServer.getConfigBuilderClassName());
+        muleServer.initialize();
+    }
+
+    public void testMuleServerConfigArg() throws Exception
+    {
+        MuleServer muleServer = new MuleServer(new String[]{"-config",
+            "org/mule/test/spring/config1/test-xml-mule2-config.xml"});
+        assertEquals("org/mule/test/spring/config1/test-xml-mule2-config.xml", muleServer.getConfigurationResources());
+        assertEquals("org.mule.config.builders.AutoConfigurationBuilder", muleServer.getConfigBuilderClassName());
+        muleServer.initialize();
+    }
+
+    public void testMuleServerBuilerArg() throws Exception
+    {
+        MuleServer muleServer = new MuleServer(new String[]{"-builder",
+            "org.mule.config.spring.SpringXmlConfigurationBuilder"});
+        assertEquals(ClassUtils.getResource("mule-config.xml", MuleServer.class).toString(),
+            muleServer.getConfigurationResources());
+        assertEquals("org.mule.config.spring.SpringXmlConfigurationBuilder", muleServer.getConfigBuilderClassName());
+        muleServer.initialize();
+    }
+
+    public void testMuleServerSpringBuilerArg() throws Exception
+    {
+        MuleServer muleServer = new MuleServer(new String[]{"-builder", "spring"});
+        assertEquals(ClassUtils.getResource("mule-config.xml", MuleServer.class).toString(),
+            muleServer.getConfigurationResources());
+        assertEquals("org.mule.config.spring.SpringXmlConfigurationBuilder", muleServer.getConfigBuilderClassName());
+        muleServer.initialize();
+    }
+
+}
