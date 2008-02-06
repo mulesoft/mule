b6a225777d3dd381a35db6abd7abbefb122c8dbb
acooke <acooke>
2008-02-06 16:00:08 +0000

    MULE-2959: Second version.  This does still fail :o(
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@10727 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/ssl/src/test/java/org/mule/transport/ssl/SaveCertificatesCallback.java
@@ -0,0 +1,49 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.ssl;
+
+import org.mule.tck.functional.EventCallback;
+import org.mule.api.MuleEventContext;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.LinkedList;
+
+import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicReference;
+import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicBoolean;
+import org.apache.commons.collections.list.SynchronizedList;
+
+public class SaveCertificatesCallback implements EventCallback
+{
+
+    private List certificates;
+
+    public SaveCertificatesCallback()
+    {
+        clear();
+    }
+
+    public void eventReceived(MuleEventContext context, Object component) throws Exception
+    {
+        certificates.add(context.getMessage().getProperty(SslConnector.LOCAL_CERTIFICATES));
+    }
+
+    public void clear()
+    {
+        certificates = Collections.synchronizedList(new LinkedList());
+    }
+
+    public List getCertificates()
+    {
+        return certificates;
+    }
+
+}
---
+++ transports/ssl/src/test/java/org/mule/transport/ssl/SslCertificatesTestCase.java
@@ -0,0 +1,64 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.ssl;
+
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.tck.functional.FunctionalTestComponent;
+
+import java.util.Iterator;
+
+/**
+ * A different version of {@link org.mule.transport.ssl.SslCertificateTestCase} to see if we can get
+ * different timing.
+ */
+public class SslCertificatesTestCase extends FunctionalTestCase
+{
+
+    protected static String TEST_MESSAGE = "Test Request";
+    private static int NUM_MESSAGES = 1000;
+
+    protected String getConfigResources()
+    {
+        return "ssl-certificates-test.xml";
+    }
+
+    public void testOnce() throws Exception
+    {
+        doTests(1);
+    }
+
+    public void testMany() throws Exception
+    {
+        doTests(NUM_MESSAGES);
+    }
+
+    protected void doTests(int n) throws Exception
+    {
+        MuleClient client = new MuleClient();
+        for (int i = 0; i < n; ++i)
+        {
+            String msg = TEST_MESSAGE + n;
+            MuleMessage result = client.send("in", msg, null);
+            assertEquals(FunctionalTestComponent.received(msg), result.getPayloadAsString());
+        }
+        Iterator certificates =
+                ((SaveCertificatesCallback)
+                        muleContext.getRegistry().lookupObject("certificates")).getCertificates().iterator();
+        for (int i = 0; i < n; ++i)
+        {
+            assertTrue("No cert at " + i, certificates.hasNext());
+            assertNotNull("Null cert at " + i, certificates.next());
+        }
+    }
+
+}
