a2117ad63a9fde8cc888c458db92c11fab820444
tcarlson <tcarlson>
2008-02-25 20:41:37 +0000

    Resource handling without Spring-OSGi
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@11012 bf997673-6b11-0410-b953-e057580c5b09

--- modules/osgi/src/main/java/org/mule/module/osgi/MuleBundleListener.java
+++ modules/osgi/src/main/java/org/mule/module/osgi/MuleBundleListener.java
@@ -12,9 +12,13 @@ package org.mule.module.osgi;
 
 import org.mule.api.MuleContext;
 import org.mule.api.config.ConfigurationException;
+import org.mule.config.ConfigResource;
 import org.mule.config.spring.SpringXmlConfigurationBuilder;
+import org.mule.util.StringUtils;
 
+import java.net.URL;
 import java.util.Dictionary;
+import java.util.Enumeration;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -29,6 +33,8 @@ public class MuleBundleListener implements BundleActivator
 {
     /** Manifest entry name for listing Mule config files */
     public static final String MULE_CONFIG_HEADER = "Mule-Config";
+    
+    private BundleContext context;
     
     /** The id of the listener bundle itself */
     protected long bundleId;
@@ -55,29 +61,54 @@ public class MuleBundleListener implements BundleActivator
 
             switch (event.getType())
             {
-                case BundleEvent.STARTED :
+                case BundleEvent.INSTALLED :
                 {
-                    String configResources = getMuleConfigResources(bundle);
-                    if (configResources != null)
+                    Enumeration schemas = bundle.findEntries("/", "*.xsd", true);
+                    while (schemas.hasMoreElements())
                     {
-                        logger.info("New Mule configuration detected: " + configResources);
+                        URL url = (URL) schemas.nextElement();
+                        logger.debug("Schema found in bundle: " + url);
+                    }
+                }
+                case BundleEvent.STARTING :
+                {
+                    String configResourceList = getMuleConfigResources(bundle);
+                    if (configResourceList != null)
+                    {
+                        logger.info("Processing Mule configuration: " + configResourceList);
                         MuleContext muleContext = (MuleContext) muleContextRef.getService();
                         
                         // TODO This should be run in a separate non-blocking thread.
                         try
                         {
-                            new SpringXmlConfigurationBuilder(configResources).configure(muleContext);
+                            String[] configResources = StringUtils.splitAndTrim(configResourceList, ",; ");                            
+                            ConfigResource[] configResourceUrls = new ConfigResource[configResources.length];
+                            for (int i = 0; i < configResources.length; i++)
+                            {
+                                // Create a URL to the resource within the bundle jar (Resource files cannot be 
+                                // directly accessed across bundles through the classloader, unfortunately).
+                                configResourceUrls[i] = new ConfigResource(bundle.getEntry(configResources[i]));
+                            }
+                            
+                            SpringXmlConfigurationBuilder builder = new SpringXmlConfigurationBuilder(configResourceUrls);
+                            builder.setUseDefaultConfigResource(false);
+                            builder.configure(muleContext);
                         }
-                        catch (ConfigurationException e)
+                        catch (ConfigurationException ce)
                         {
-                            logger.error(e);
+                            logger.error(ce);
+                            
+                            //logger.info("Unable to process Mule configuration, stopping bundle...");
+                            // TODO How to abort bundle startup?  The following throws
+                            //      "BundleException: Bundle.start called from BundleActivator.stop"
+                            // bundle.stop();
                         }
                     }
                     break;
                 }
                 case BundleEvent.STOPPING :
                 {
-                    // TODO
+                    // TODO Tear down Mule config somehow
                     break;
                 }
                 default :
@@ -88,6 +119,7 @@ public class MuleBundleListener implements BundleActivator
 
     public void start(BundleContext context) throws Exception
     {
+        this.context = context;
         this.bundleId = context.getBundle().getBundleId();
         
         muleContextRef = new ServiceTracker(context, MuleContext.class.getName(), null);
@@ -95,7 +127,7 @@ public class MuleBundleListener implements BundleActivator
 
         muleConfigListener = new MuleConfigListener();
         // listen to any changes in bundles
-        context.addBundleListener(muleConfigListener);
+//        context.addBundleListener(muleConfigListener);
     }
 
     public void stop(BundleContext context) throws Exception
