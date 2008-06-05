4a3f6b90974c3c1e892eb5933073264e816a006e
Daniel Feist <dfeist@gmail.com>
2008-06-05 19:31:23 +0000

    Merged revisions 11451,11457-11478,11480-11488,11494-11499,11506,11509-11518,11520-11521 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.0.x
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@11964 bf997673-6b11-0410-b953-e057580c5b09

---
+++ server/src/main/resources/bin/launcher.bat
@@ -0,0 +1,3 @@
+@echo off
+set cp=.;%MULE_HOME%\conf;%MULE_HOME%\lib\opt\groovy-all-1.5.4.jar;%MULE_HOME%\lib\boot\commons-cli-1.0.jar
+java -Dmule.home=%MULE_HOME% -cp "%cp%" org.codehaus.groovy.tools.GroovyStarter --main groovy.ui.GroovyMain --conf %MULE_HOME%\bin\launcher.conf %*
