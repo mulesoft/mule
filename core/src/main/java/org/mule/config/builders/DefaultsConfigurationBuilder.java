694b4651748a349d7d83101b448c32410c354f94
tcarlson <tcarlson>
2008-07-30 15:09:34 +0000

    Merged revisions 10600-10760,10765-10769,10771-10773,10777-10781,10783-10786,10788,10792-10793,10795,10806,10814,10822-10831,10833-10834,10846,10848-10851,10853-10854,10857-10858,10860,10862-10864,10875-10883,10885-10887,10905,10910-10911,10916,10919-10920,10925,10942-10943,11006,11008-11022,11042,11044-11052,11055,11079,11081-11082,11088,11096-11097,11107-11109,11111,11116,11119,11150-11152,11167-11170,11188,11190-11194,11197,11209,11249,11279,11305-11309,11315,11321-11328,11330-11335,11345,11348,11350,11423,11427,11445,11447-11450,11452-11456,11479,11489-11493,11500-11505,11507-11508,11522,11525,11528,11540,11557-11558,11572-11574,11577,11579,11587-11589,11637,11639,11655-11656,11666,11668-11669,11671,11673-11674,11685,11687-11688,11690-11692,11702-11703,11712,11716,11720,11728,11730-11731,11734-11739,11741,11743,11749-11750,11752,11806-11810,11872-11873,11895-11897,11899-11902,11904,11906,11908-11911,11913-11914,11916,11918-11920,11922-11928,11930-11942,11958-11959,11964-11965,11967-11968,11981,11992-11997,12003-12005,12013-12016,12023-12024,12027-12030,12033,12035-12043,12048,12050-12057,12061,12063-12066,12068-12069,12071-12073,12084-12085,12088-12090,12092-12094,12097-12099,12104-12105,12108,12112,12114-12116,12119,12121-12123,12127,12129-12134,12136,12141-12148,12151-12159,12169-12170,12173-12174,12179,12184-12194,12206-12209,12246,12266-12267,12281,12301,12306,12335,12337-12339,12341,12343,12348,12352,12362-12364,12370,12372-12373,12375,12377,12379,12381-12385,12387-12388,12391,12393,12398,12406,12409-12411,12413,12418-12430,12432-12436 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.0.x
    ........
      r12435 | tcarlson | 2008-07-30 11:57:24 -0300 (Wed, 30 Jul 2008) | 1 line
      
      FilePersistenceStrategy is MuleContextAware but MuleContext was never getting injected into it
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@12437 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/config/builders/DefaultsConfigurationBuilder.java
+++ core/src/main/java/org/mule/config/builders/DefaultsConfigurationBuilder.java
@@ -11,7 +11,6 @@
 package org.mule.config.builders;
 
 import org.mule.api.MuleContext;
-import org.mule.api.MuleException;
 import org.mule.api.config.MuleProperties;
 import org.mule.api.config.ThreadingProfile;
 import org.mule.api.lifecycle.LifecycleManager;
@@ -48,20 +47,18 @@ import org.mule.util.queue.TransactionalQueueManager;
  */
 public class DefaultsConfigurationBuilder extends AbstractConfigurationBuilder
 {
-
     protected void doConfigure(MuleContext muleContext) throws Exception
     {
-        configureDefaults(muleContext.getRegistry());
-    }
-
-    protected void configureDefaults(MuleRegistry registry) throws MuleException
-    {
+        MuleRegistry registry = muleContext.getRegistry();
+        
         //registry.registerObject(MuleProperties.OBJECT_MULE_CONFIGURATION, new MuleConfiguration());
         registry.registerObject(MuleProperties.OBJECT_MULE_SIMPLE_REGISTRY_BOOTSTRAP,
             new SimpleRegistryBootstrap());
         
+        FilePersistenceStrategy ps = new FilePersistenceStrategy();
+        ps.setMuleContext(muleContext);
         QueueManager queueManager = new TransactionalQueueManager();
-        queueManager.setPersistenceStrategy(new FilePersistenceStrategy());
+        queueManager.setPersistenceStrategy(ps);
         registry.registerObject(MuleProperties.OBJECT_QUEUE_MANAGER, queueManager);
         
         registry.registerObject(MuleProperties.OBJECT_SECURITY_MANAGER, new MuleSecurityManager());
