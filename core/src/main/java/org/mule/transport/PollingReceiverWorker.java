fe493a7134be2519865da91d985b1f098f6073d1
tcarlson <tcarlson>
2008-09-29 21:03:43 +0000

    Fix MULE-3722 Regression in VM transport
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@12797 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/main/java/org/mule/transport/PollingReceiverWorker.java
@@ -0,0 +1,94 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport;
+
+import org.mule.api.retry.RetryCallback;
+import org.mule.api.retry.RetryContext;
+
+import javax.resource.spi.work.Work;
+
+public class PollingReceiverWorker implements Work
+{
+    protected final AbstractPollingMessageReceiver receiver;
+    protected volatile boolean running = false;
+
+    public PollingReceiverWorker(AbstractPollingMessageReceiver pollingMessageReceiver)
+    {
+        super();
+        receiver = pollingMessageReceiver;
+    }
+
+    public AbstractPollingMessageReceiver getReceiver()
+    {
+        return receiver;
+    }
+    
+    public boolean isRunning()
+    {
+        return running;
+    }
+
+    // the run() method will exit after each poll() since it will be invoked again
+    // by the scheduler
+    public void run()
+    {
+        if (!receiver.stopped.get())
+        {
+            try
+            {
+                running = true;
+                // make sure we are connected, wait if necessary
+                receiver.connected.whenTrue(null);
+
+                receiver.getConnector().getRetryPolicyTemplate().execute(new RetryCallback()
+                {
+                   public void doWork(RetryContext context) throws Exception
+                    {
+                       // Make sure we are connected
+                       receiver.connect();
+                       try
+                       {
+                           receiver.poll();
+                       }
+                       catch (InterruptedException e)
+                       {
+                           // stop polling
+                           receiver.stop();
+                       }
+                       catch (Exception e)
+                       {
+                           receiver.handleException(e);
+                       }
+                    }
+       
+                   public String getWorkDescription()
+                   {
+                       return receiver.getConnectionDescription();
+                   }
+               });
+            }
+            catch (Exception e)
+            {
+                receiver.handleException(e);
+            }
+            finally
+            {
+                running = false;
+            }
+        }
+    }
+
+    public void release()
+    {
+        // nop
+    }
+
+}
