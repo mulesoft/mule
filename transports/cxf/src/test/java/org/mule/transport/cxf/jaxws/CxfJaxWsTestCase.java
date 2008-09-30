021386213048b1c6836c1026078de08847f89598
Dan Diephouse <dan@netzooid.com>
2008-09-30 01:38:43 +0000

    Move QA test to CE build as it fits better here.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@12801 bf997673-6b11-0410-b953-e057580c5b09

--- transports/cxf/src/test/java/org/mule/transport/cxf/jaxws/CxfJaxWsTestCase.java
+++ transports/cxf/src/test/java/org/mule/transport/cxf/jaxws/CxfJaxWsTestCase.java
@@ -15,6 +15,10 @@ import org.mule.module.client.MuleClient;
 import org.mule.tck.FunctionalTestCase;
 import org.mule.transport.NullPayload;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+
 public class CxfJaxWsTestCase extends FunctionalTestCase
 {
     public void testEchoService() throws Exception
@@ -33,9 +37,31 @@ public class CxfJaxWsTestCase extends FunctionalTestCase
         assertEquals(NullPayload.getInstance(), result.getPayload());
     }
 
+    public void testHttpCall() throws Exception
+    {
+        HttpClient client =  new HttpClient();
+        // The format in which CXF processes the request in Http GET is:
+        // http://host/service/OPERATION/PARAM_NAME/PARAM_VALUE
+        // In this case: http://localhost:63081/EchoUMO/echo/text/hello
+        // (service: EchoUmo corresponds to the name in the mule config file: TC-HTTP-CALL.xml)
+        HttpMethod httpMethod = new GetMethod("http://localhost:63081/services/Echo/echo/text/hello");
+        // Http Status Code 200 means OK, the request has succeeded. (500 would indicate an error)
+        assertEquals(200, client.executeMethod(httpMethod));
+        // By default the class package - in its other way round - is used for the namespace:
+        // Here, EchoServiceImpl classpath is: com\mulesource\test\connectors\transport\cxf
+        // Therefore namespace should be: http://cxf.transport.connectors.test.mulesource.com
+        assertEquals(
+                "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+                    "<soap:Body>" +
+                        "<ns1:echoResponse xmlns:ns1=\"http://testmodels.cxf.transport.mule.org/\">" +                        
+                            "<text>hello</text>" +
+                        "</ns1:echoResponse>" +
+                    "</soap:Body>" +
+                "</soap:Envelope>", httpMethod.getResponseBodyAsString());
+    }
+
     protected String getConfigResources()
     {
         return "jaxws-conf.xml";
     }
-
 }
