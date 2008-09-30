801d2b790b0fbeed54dd7f17d8973583d179c70d
Dirk Olmes <dirk.olmes@mulesoft.com>
2008-09-30 08:34:02 +0000

    MULE-3714 (Review ListSchema.groovy)
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@12802 bf997673-6b11-0410-b953-e057580c5b09

--- buildtools/scripts/ListSchema.groovy
+++ buildtools/scripts/ListSchema.groovy
@@ -1,5 +1,4 @@
 /**
-  
   This should do the following:
   - scan the directory tree for all schema
   - infer the deploy location of the schema
@@ -24,6 +23,8 @@
 
   $Id$
 */
+
+import org.codehaus.groovy.runtime.InvokerHelper
 
 // Schema version
 def version = 2.0
@@ -54,6 +55,16 @@ def servicexsd = /.*(\/|\\)spring-config(\/|\\).*(\/|\\)mule-(.*)\.xsd/
 def testxsd = /.*(\/|\\)tests(\/|\\)([^\/]+)(\/|\\).*(\/|\\)mule-test\.xsd/
 def otherxsd = /.*(\/|\\)(transports|modules)(\/|\\)([^\/]+)(\/|\\).*(\/|\\)mule-(.*)\.xsd/
 
+def checkGroovyVersion = {
+    def groovyVersion = InvokerHelper.getVersion()
+    if (groovyVersion.indexOf("1.5") == -1) {
+        println ""
+        println "WARNING: this script needs at least Groovy 1.5.x to run"
+        println ""
+        System.exit(1)
+    }
+}
+
 def checkCurrentDirectory = {
   if (! (new File("").getCanonicalFile().getName() == "scripts")) {
     println ""
@@ -64,16 +75,17 @@ def checkCurrentDirectory = {
 }
 
 def scanForSchemaAndInferDestinations = {
-  println ""
-  println "scanning for schema"
-  println ""
-  for (f in new AntBuilder().fileScanner {
-                  fileset(dir:root) {
-                    include(name:"**/*.xsd")
-                    exclude(name:"**/src/test/**/*.xsd")
-                    exclude(name:"**/target/**/*.xsd")
-                  }
-                }) {
+	println ""
+	println "scanning for schema"
+	println ""
+	for (f in new AntBuilder().fileScanner {
+          fileset(dir: root) {
+            include(name:"**/*.xsd")
+            exclude(name:"**/src/test/**/*.xsd")
+            exclude(name:"**/target/**/*.xsd")
+          }
+        }) 
+	{
     if (f.absolutePath ==~ corexsd) {
       schemaNames << "core"
       schemaSources.put("core", f)
@@ -125,44 +137,45 @@ def checkSchema = {
 }
 
 def scanAndCheckConfigs = {
-  println ""
-  println "checking configurations"
-  println ""
-  for (f in new AntBuilder().fileScanner {
-                  fileset(dir:root) {
-                    include(name:"**/*.xml")
-                    exclude(name:"**/target/**/*.xml")
-                  }
-                }) {
-    try {
-      def parser = new XmlParser().parse(f)
-      for (key in parser?.attributes.keySet()) {
-        if (key instanceof groovy.xml.QName
-            && key.localPart == "schemaLocation") {
-          tokens = new StringTokenizer(parser.attributes[key])
-          while (tokens.hasMoreTokens()) {
-            id = tokens.nextToken()
-            uri = tokens.nextToken()
-            match = (id =~ /.*mulesource\.org.*mule\/([^\/]+)\/.*/)
-            if (match.matches()) {
-              name = match[0][1]
-              // we should really scan for test schema too, 
-              // but there's only one^H^H^H a few
-              if (name != "parsers-test" && ! name.startsWith("nest-example")) {
-                if (! schemaNames.contains(name)) {
-                  println "WARNING: missing schema for $name - see file $f"
-                } else if (uri != schemaDestinations[name]) {
-                  println "WARNING: URI for $name is ${schemaDestinations[name]} (not ${uri}) - see file $f"
-                }
-              }
-            }
-          }
+    println ""
+    println "checking configurations"
+    println ""
+    for (f in new AntBuilder().fileScanner {
+        fileset(dir: root) {
+            include(name: "**/*.xml")
+            exclude(name: "**/target/**/*.xml")
         }
-      }
-    } catch (e) {
-      println "WARNING: error parsing $f: $e"
+    })
+    {
+        try {
+            // println "parsing $f"
+            def parser = new XmlParser().parse(f)
+            for (key in parser?.attributes().keySet()) {
+                if (key instanceof groovy.xml.QName && key.localPart == "schemaLocation") {                    
+                    tokens = new StringTokenizer(parser.attributes()[key])
+                    while (tokens.hasMoreTokens()) {
+                        id = tokens.nextToken()
+                        uri = tokens.nextToken()
+                        match = (id =~ /.*mulesource\.org.*mule\/([^\/]+)\/.*/)
+                        if (match.matches()) {
+                            name = match[0][1]
+                            // we should really scan for test schema too, 
+                            // but there's only one^H^H^H a few
+                            if (name != "parsers-test" && ! name.startsWith("nest-example")) {
+                                if (! schemaNames.contains(name)) {
+                                    println "WARNING: missing schema for $name - see file $f"
+                                } else if (uri != schemaDestinations[name]) {
+                                    println "WARNING: URI for $name is ${schemaDestinations[name]} (not ${uri}) - see file $f"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (e) {
+            println "WARNING: error parsing $f: $e"
+        }
     }
-  }
 }
 
 def listSchema = {
@@ -188,6 +201,7 @@ def generateDeployCommand = {
   }
 }
 
+//checkGroovyVersion()
 checkCurrentDirectory()
 scanForSchemaAndInferDestinations()
 checkSchema()
