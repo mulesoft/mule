d6048d18774d9628e4e3d1f12f1e4bffb4fb0564
Ross Mason <ross@mulesoft.com>
2008-10-02 13:57:22 +0000

    Avoid duplicate class errors in IDEA
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@12845 bf997673-6b11-0410-b953-e057580c5b09

--- modules/scripting/src/test/resources/groovy-dynamic-script-bean.groovy
+++ modules/scripting/src/test/resources/groovy-dynamic-script-bean.groovy
@@ -1,7 +1,7 @@
 import org.mule.api.MuleEventContext
 import org.mule.api.lifecycle.Callable
 
-public class GroovyDynamicScript implements Callable
+public class GroovyDynamicScriptBean implements Callable
 {
     public Object onCall(MuleEventContext eventContext) throws Exception
     {
