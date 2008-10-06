c56eb3dbfe65ea2aa254f7c3eaa6cb6fcc4e8233
Dirk Olmes <dirk.olmes@mulesoft.com>
2008-10-06 12:21:44 +0000

    adjust the proper path to MULE_HOME
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@12914 bf997673-6b11-0410-b953-e057580c5b09

--- examples/bookstore/dist/bookstore.bat
+++ examples/bookstore/dist/bookstore.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 REM There is no need to call this if you set the MULE_HOME in your environment properties
-if "%MULE_HOME%" == "" SET MULE_HOME=%~dp0..\..\..
+if "%MULE_HOME%" == "" SET MULE_HOME=%~dp0..\..
 if "%MULE_BASE%" == "" SET MULE_BASE=%MULE_HOME%
 
 REM Any changes to the files in .\conf will take precedence over those deployed to %MULE_HOME%\lib\user
