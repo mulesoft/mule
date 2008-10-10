fe610bac196521eae2616d6747abe50f3a5be4a5
aperepel@gmail.com <Andrew Perepelytsya>
2008-10-10 15:34:47 +0000

    Updated SFP to support #[] and give warning on ${}. Updated JavaDocs and added deprecation info.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@13041 bf997673-6b11-0410-b953-e057580c5b09

--- transports/file/src/main/java/org/mule/transport/file/SimpleFilenameParser.java
+++ transports/file/src/main/java/org/mule/transport/file/SimpleFilenameParser.java
@@ -14,30 +14,40 @@ import org.mule.api.transport.MessageAdapter;
 import org.mule.util.DateUtils;
 import org.mule.util.TemplateParser;
 import org.mule.util.UUID;
+import org.mule.util.expression.ExpressionEvaluatorManager;
 
 import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicLong;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * <code>SimpleFilenameParser</code> understands a limited set of tokens, namely
  * <ul>
- * <li>${DATE} : the currrent date in the format dd-MM-yy_HH-mm-ss.SS</li>
- * <li>${DATE:yy-MM-dd} : the current date using the specified format</li>
- * <li>${SYSTIME} : The current system time milliseconds</li>
- * <li>${UUID} : A generated Universally unique id</li>
- * <li>${ORIGINALNAME} : The origial file name if the file being written was read
+ * <li>#[DATE] : the currrent date in the format dd-MM-yy_HH-mm-ss.SS</li>
+ * <li>#[DATE:yy-MM-dd] : the current date using the specified format</li>
+ * <li>#[SYSTIME] : The current system time milliseconds</li>
+ * <li>#[UUID] : A generated Universally unique id</li>
+ * <li>#[ORIGINALNAME] : The origial file name if the file being written was read
  * from another location</li>
- * <li>${COUNT} : An incremental counter</li>
- * <li>${<Message Property Name>} : A name of a property on the message</li>
+ * <li>#[COUNT] : An incremental counter</li>
+ * <li>#[&lt;Message Property Name>] : A name of a property on the message</li>
  * </ul>
  * Note that square brackets can be used instead of curl brackets, this is useful
  * when defining the file output pattern in a Mule Url endpointUri where the curl
  * bracket is an invalid character.
+ * <p/>
+ * Curly braces ${} are supported, but give a deprecation warning.
+ * @deprecated Superceded by {@link org.mule.transport.file.ExpressionFilenameParser}, which is now
+ * used by Mule implicitly.
  */
-
+@Deprecated
 public class SimpleFilenameParser implements FilenameParser
 {
+    protected transient Log logger = LogFactory.getLog(getClass());
+
     public static final String DEFAULT_DATE_FORMAT = "dd-MM-yy_HH-mm-ss.SSS";
 
+    private final TemplateParser wigglyMuleParser = TemplateParser.createMuleStyleParser();
     private final TemplateParser antParser = TemplateParser.createAntStyleParser();
     private final TemplateParser squareParser = TemplateParser.createSquareBracesStyleParser();
 
@@ -51,8 +61,14 @@ public class SimpleFilenameParser implements FilenameParser
         }
         else
         {
-            if (pattern.indexOf('{') > -1)
+            if (pattern.contains(ExpressionEvaluatorManager.DEFAULT_EXPRESSION_PREFIX))
             {
+                return getFilename(adapter, pattern, wigglyMuleParser);
+            }
+            else if (pattern.contains("{"))
+            {
+                logger.warn("Found " + pattern + ". Using ${} style has been deprecated for Mule placeholders and won't be supported in the future," +
+                            " please switch to #[] and ExpressionFilenameParser");
                 return getFilename(adapter, pattern, antParser);
             }
             else
--- transports/file/src/test/java/org/mule/transport/file/SimpleFilenameParserTestCase.java
+++ transports/file/src/test/java/org/mule/transport/file/SimpleFilenameParserTestCase.java
@@ -67,6 +67,37 @@ public class SimpleFilenameParserTestCase extends AbstractMuleTestCase
 
     }
 
+    public void testWigglyMuleStyleParsing()
+    {
+        String result = parser.getFilename(adapter, "Test1_#[COUNT].txt");
+        assertEquals("Test1_0.txt", result);
+
+        result = parser.getFilename(adapter, "Test2_#[DATE:yyMMdd].txt");
+        assertEquals("got result: " + result, 16, result.length());
+
+        result = parser.getFilename(adapter, "Test3_#[DATE].txt");
+        assertEquals("got result: '" + result, 31, result.length());
+
+        result = parser.getFilename(adapter, "Test4_#[SYSTIME].txt");
+        assertFalse(result.equals("Test4_#[SYSTIME].txt"));
+
+        result = parser.getFilename(adapter, "Test5_#[UUID].txt");
+        assertFalse(result.equals("Test5_#[UUID].txt"));
+
+        result = parser.getFilename(adapter, "Test6_#[COUNT].txt");
+        assertEquals("Test6_1.txt", result);
+
+        result = parser.getFilename(adapter, "Test7_#[ORIGINALNAME].txt");
+        assertEquals("Test7_originalName.txt", result);
+
+        result = parser.getFilename(adapter, "Test8_#[foo].txt");
+        assertEquals("Test8_bar.txt", result);
+
+        result = parser.getFilename(adapter, "Test9_#[xxx].txt");
+        assertEquals("Test9_#[xxx].txt", result);
+    }
+
+
     public void testSquareStyleParsing()
     {
         String result = parser.getFilename(adapter, "Test1_[COUNT].txt");
