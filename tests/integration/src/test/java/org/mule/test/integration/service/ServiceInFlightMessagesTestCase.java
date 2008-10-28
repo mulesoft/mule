79ea725987639a4731111ffa9ca684417897dccb
aperepel@gmail.com <Andrew Perepelytsya>
2008-10-28 18:07:13 +0000

    Aggressively expose a race condition in the test.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@13165 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/service/ServiceInFlightMessagesTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/service/ServiceInFlightMessagesTestCase.java
@@ -20,6 +20,9 @@ import org.mule.util.xa.ResourceManagerSystemException;
 public class ServiceInFlightMessagesTestCase extends FunctionalTestCase
 {
 
+    private static final int WAIT_TIME_MILLIS = 0;
+    private static final int NUM_MESSAGES = 50;
+
     protected String getConfigResources()
     {
         return "org/mule/test/integration/service/service-inflight-messages.xml";
@@ -28,7 +31,7 @@ public class ServiceInFlightMessagesTestCase extends FunctionalTestCase
     public void testInFlightMessages() throws Exception
     {
         Service service = muleContext.getRegistry().lookupService("TestService");
-        int numMessage = 500;
+        int numMessage = NUM_MESSAGES;
         for (int i = 0; i < numMessage; i++)
         {
             service.dispatchEvent(getTestEvent("test", service, getTestInboundEndpoint("test://test")));
@@ -44,13 +47,13 @@ public class ServiceInFlightMessagesTestCase extends FunctionalTestCase
     public void testInFlightMessagesPausedService() throws Exception
     {
         Service service = muleContext.getRegistry().lookupService("PausedTestService");
-        int numMessage = 500;
+        int numMessage = NUM_MESSAGES;
         for (int i = 0; i < numMessage; i++)
         {
             service.dispatchEvent(getTestEvent("test", service, getTestInboundEndpoint("test://test")));
         }
 
-        Thread.sleep(100);
+        //Thread.sleep(100);
 
         // Stop rather than dispose so we still have access to the connector for this
         // test.
@@ -65,7 +68,7 @@ public class ServiceInFlightMessagesTestCase extends FunctionalTestCase
     {
 
         Service service = muleContext.getRegistry().lookupService("TestPersistentQueueService");
-        int numMessage = 500;
+        int numMessage = NUM_MESSAGES;
         for (int i = 0; i < numMessage; i++)
         {
             service.dispatchEvent(getTestEvent("test", service, getTestInboundEndpoint("test://test")));
@@ -74,21 +77,21 @@ public class ServiceInFlightMessagesTestCase extends FunctionalTestCase
         // Stop service and give workers a chance to finnish up before insepcting
         // queues
         muleContext.stop();
-        Thread.sleep(200);
+        Thread.sleep(WAIT_TIME_MILLIS);
 
         assertQueues(numMessage, "TestPersistentQueueService");
 
         muleContext.start();
-        Thread.sleep(200);
+        Thread.sleep(WAIT_TIME_MILLIS);
         muleContext.stop();
-        Thread.sleep(200);
+        Thread.sleep(WAIT_TIME_MILLIS);
 
         assertQueues(numMessage, "TestPersistentQueueService");
 
         // Let mule finnish up with the rest of the messages until seda queue is
         // // empty
         muleContext.start();
-        Thread.sleep(3000);
+        //Thread.sleep(3000);
         muleContext.stop();
 
         assertQueues(numMessage, "TestPersistentQueueService");
@@ -99,7 +102,7 @@ public class ServiceInFlightMessagesTestCase extends FunctionalTestCase
     {
 
         Service service = muleContext.getRegistry().lookupService("PausedTestPersistentQueueService");
-        int numMessage = 500;
+        int numMessage = NUM_MESSAGES;
         for (int i = 0; i < numMessage; i++)
         {
             service.dispatchEvent(getTestEvent("test", service, getTestInboundEndpoint("test://test")));
@@ -108,23 +111,23 @@ public class ServiceInFlightMessagesTestCase extends FunctionalTestCase
         // Stop service and give workers a chance to finnish up before insepcting
         // queues
         muleContext.stop();
-        Thread.sleep(200);
+        Thread.sleep(WAIT_TIME_MILLIS);
 
         // Paused service does not process messages before or during stop().
         assertEquals(0, getTestQueueSession().getQueue("out").size());
         assertQueues(numMessage, "PausedTestPersistentQueueService");
 
         muleContext.start();
-        Thread.sleep(200);
+        Thread.sleep(WAIT_TIME_MILLIS);
         muleContext.stop();
-        Thread.sleep(200);
+        Thread.sleep(WAIT_TIME_MILLIS);
 
         // Paused service process messages before or during stop().
         assertTrue(getTestQueueSession().getQueue("out").size() > 1);
         assertQueues(numMessage, "PausedTestPersistentQueueService");
 
         service.resume();
-        Thread.sleep(100);
+        //Thread.sleep(100);
 
         // Paused service processes messages when resumed.
         assertTrue(getTestQueueSession().getQueue("out").size() > 0);
@@ -133,7 +136,7 @@ public class ServiceInFlightMessagesTestCase extends FunctionalTestCase
         // Let mule finnish up with the rest of the messages until seda queue is
         // // empty
         muleContext.start();
-        Thread.sleep(3000);
+        //Thread.sleep(3000);
         muleContext.stop();
 
         assertQueues(numMessage, "PausedTestPersistentQueueService");
@@ -143,7 +146,7 @@ public class ServiceInFlightMessagesTestCase extends FunctionalTestCase
     public void testInFlightDisposePersistentMessages() throws Exception
     {
         Service service = muleContext.getRegistry().lookupService("TestPersistentQueueService");
-        int numMessage = 500;
+        int numMessage = NUM_MESSAGES;
         for (int i = 0; i < numMessage; i++)
         {
             service.dispatchEvent(getTestEvent("test", service, getTestInboundEndpoint("test://test")));
@@ -153,26 +156,26 @@ public class ServiceInFlightMessagesTestCase extends FunctionalTestCase
         // inspecting
         // queues.
         muleContext.stop();
-        Thread.sleep(200);
+        Thread.sleep(WAIT_TIME_MILLIS);
         assertQueues(numMessage, "TestPersistentQueueService");
 
         // 3) Dispose and restart Mule and let it run for a short while
         muleContext.dispose();
         muleContext = createMuleContext();
         muleContext.start();
-        Thread.sleep(200);
+        Thread.sleep(WAIT_TIME_MILLIS);
 
         // Stop service and give workers a chance to finnish up before insepcting
         // queues
         muleContext.stop();
-        Thread.sleep(200);
+        Thread.sleep(WAIT_TIME_MILLIS);
 
         assertQueues(numMessage, "TestPersistentQueueService");
 
         // Let mule finnish up with the rest of the messages until seda queue is
         // empty
         muleContext.start();
-        Thread.sleep(3000);
+        //Thread.sleep(3000);
         muleContext.stop();
         assertQueues(numMessage, "TestPersistentQueueService");
     }
