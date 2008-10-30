2c2e886e78c4c56a10b4aba911167401dccd08c6
tcarlson <tcarlson>
2008-10-30 02:36:38 +0000

    Test cases for EE-979
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@13179 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/security/MultiuserSecurityTestCase.java
@@ -0,0 +1,82 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.security;
+
+import org.mule.MuleSessionHandler;
+import org.mule.api.EncryptionStrategy;
+import org.mule.api.MuleMessage;
+import org.mule.api.config.MuleProperties;
+import org.mule.api.transport.SessionHandler;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Tests multi-user security against a security provider which only authenticates 
+ * a single user at a time (i.e., authentication of a new user overwrites the 
+ * previous authentication).  
+ * 
+ * @see EE-979
+ */
+public class MultiuserSecurityTestCase extends FunctionalTestCase
+{
+    protected String getConfigResources()
+    {
+        return "multiuser-security-test.xml, singleuser-security-provider.xml";
+    }
+
+    public void testMultipleAuthentications() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        SessionHandler sessionHandler = new MuleSessionHandler();
+        MuleMessage reply;
+        Map props;
+
+        EncryptionStrategy strategy = muleContext.getSecurityManager().getEncryptionStrategy("PBE");
+        
+        props = new HashMap();
+        props.put(MuleProperties.MULE_USER_PROPERTY, MuleCredentials.createHeader("marie", "marie", "PBE", strategy));
+        reply = client.send("vm://test", "Data1", props);
+        assertNotNull(reply);
+        assertEquals("user = marie, logins = 1, color = bright red", reply.getPayload());
+
+        props = new HashMap();
+        props.put(MuleProperties.MULE_USER_PROPERTY, MuleCredentials.createHeader("stan", "stan", "PBE", strategy));
+        reply = client.send("vm://test", "Data2", props);
+        assertNotNull(reply);
+        assertEquals("user = stan, logins = 1, color = metallic blue", reply.getPayload());
+        
+        props = new HashMap();
+        props.put(MuleProperties.MULE_USER_PROPERTY, MuleCredentials.createHeader("cindy", "cindy", "PBE", strategy));
+        reply = client.send("vm://test", "Data3", props);
+        assertEquals("user = cindy, logins = 1, color = dark violet", reply.getPayload());
+
+        props = new HashMap();
+        props.put(MuleProperties.MULE_USER_PROPERTY, MuleCredentials.createHeader("marie", "marie", "PBE", strategy));
+        reply = client.send("vm://test", "Data4", props);
+        assertNotNull(reply);
+        assertEquals("user = marie, logins = 2, color = bright red", reply.getPayload());
+
+        props = new HashMap();
+        props.put(MuleProperties.MULE_USER_PROPERTY, MuleCredentials.createHeader("marie", "marie", "PBE", strategy));
+        reply = client.send("vm://test", "Data4", props);
+        assertNotNull(reply);
+        assertEquals("user = marie, logins = 3, color = bright red", reply.getPayload());
+
+        props = new HashMap();
+        props.put(MuleProperties.MULE_USER_PROPERTY, MuleCredentials.createHeader("stan", "stan", "PBE", strategy));
+        reply = client.send("vm://test", "Data2", props);
+        assertNotNull(reply);
+        assertEquals("user = stan, logins = 2, color = metallic blue", reply.getPayload());        
+    }
+}
---
+++ tests/integration/src/test/java/org/mule/security/MultiuserSecurityWithMultiuserProviderTestCase.java
@@ -0,0 +1,26 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.security;
+
+
+/**
+ * Tests multi-user security against a security provider which holds authentications 
+ * for multiple users concurrently.
+ * 
+ * @see EE-979
+ */
+public class MultiuserSecurityWithMultiuserProviderTestCase extends MultiuserSecurityTestCase
+{
+    protected String getConfigResources()
+    {
+        return "multiuser-security-test.xml, multiuser-security-provider.xml";
+    }
+}
