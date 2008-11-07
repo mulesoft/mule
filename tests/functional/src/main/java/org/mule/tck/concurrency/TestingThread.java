1ef40f117a9e7cee443e3d314ac8aae7765876c4
tcarlson <tcarlson>
2008-11-07 15:36:38 +0000

    New TCK class for making concurrent assertions in a TestCase
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@13258 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/functional/src/main/java/org/mule/tck/concurrency/TestingThread.java
@@ -0,0 +1,94 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.tck.concurrency;
+
+import org.mule.util.concurrent.Latch;
+
+/**
+ * This class is very useful for test cases which need to make assertions 
+ * concurrently in different threads.  Usage is as follows:
+ * <code>
+ *   public void testConcurrency() 
+ *   {                
+ *       TestingThread thread = new TestingThread()
+ *       {
+ *           protected void doRun() throws Throwable
+ *           {
+ *               // Wait a few seconds for somethingElse to begin
+ *               Thread.sleep(3000);
+ *               assertTrue(somethingElse.isRunning());
+ *               assertEquals(3, somethingElse.counter);
+ *           }
+ *       };
+ *       thread.start();
+ *
+ *       // This will block the main test thread
+ *       runSomething("big long task");
+ *       assertEquals("peachy", something.getResult());
+ *       assertFalse(something.isOutOfControl());
+ *       
+ *       // Verify that no exceptions occurred meanwhile in the TestingThread
+ *       thread.await();
+ *       if (thread.getException() != null)
+ *       {
+ *           fail(thread.getException().getMessage());
+ *       }
+ *   }
+ * </code>
+ *   Both the TestingThread and the main thread will run in parallel, 
+ *   therefore assertions can be made on "somethingElse" while the call 
+ *   to runSomething() is blocking the main thread of the TestCase.
+ */
+public abstract class TestingThread extends Thread implements Runnable
+{
+    private final Latch done = new Latch();
+    private volatile Throwable exception = null;        
+
+    /**
+     * Executes the doRun() method and stores any exception which occurred to be 
+     * returned at a later time.
+     */
+    @Override
+    public final void run() 
+    {
+        try
+        {
+            doRun();
+        }
+        catch (Throwable e)
+        {
+            exception = e;
+        }
+        finally
+        {
+            done.countDown();
+        }
+    }
+    
+    abstract protected void doRun() throws Throwable;
+
+    /**
+     * Block until the thread completes its doRun() method.
+     * @throws InterruptedException
+     */
+    public void await() throws InterruptedException
+    {
+        done.await();
+    }
+
+    /**
+     * @return any exception thrown by the doRun() method, including failed assertions
+     */
+    public Throwable getException()
+    {
+        return exception;
+    }
+}
