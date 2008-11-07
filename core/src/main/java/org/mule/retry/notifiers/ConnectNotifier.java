c765a012583383ce3ef370062154e6371050ceac
tcarlson <tcarlson>
2008-11-07 13:39:37 +0000

    Raise the log level of this message
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@13256 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/retry/notifiers/ConnectNotifier.java
+++ core/src/main/java/org/mule/retry/notifiers/ConnectNotifier.java
@@ -28,9 +28,9 @@ public class ConnectNotifier implements RetryNotifier
 
     public void sucess(RetryContext context)
     {
-        if (logger.isDebugEnabled())
+        if (logger.isInfoEnabled())
         {
-            logger.debug("Successfully connected to " + context.getDescription());
+            logger.info("Successfully connected to " + context.getDescription());
         }
 
         fireConnectNotification(ConnectionNotification.CONNECTION_CONNECTED, context.getDescription());
