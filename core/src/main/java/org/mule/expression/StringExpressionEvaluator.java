a20e447da7c0a46b5e5a4dcfa60e45fba9ec6e11
Ross Mason <ross@mulesoft.com>
2008-11-20 15:24:23 +0000

    MULE-3970. String expressions support
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@13393 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/main/java/org/mule/expression/StringExpressionEvaluator.java
@@ -0,0 +1,55 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.expression;
+
+import org.mule.api.MuleContext;
+import org.mule.api.MuleMessage;
+import org.mule.api.context.MuleContextAware;
+import org.mule.api.expression.ExpressionEvaluator;
+
+/**
+ * Will process an expression string that can contain other expressions
+ */
+public class StringExpressionEvaluator implements ExpressionEvaluator, MuleContextAware
+{
+    public static final String NAME = "string";
+
+    private MuleContext context;
+
+    public void setMuleContext(MuleContext context)
+    {
+        this.context = context;
+    }
+
+    public Object evaluate(String expression, MuleMessage message)
+    {
+        return context.getExpressionManager().parse(expression, message);
+    }
+
+    /**
+     * Gts the name of the object
+     *
+     * @return the name of the object
+     */
+    public String getName()
+    {
+        return NAME;
+    }
+
+    /**
+     * Sets the name of the object
+     *
+     * @param name the name of the object
+     */
+    public void setName(String name)
+    {
+        throw new UnsupportedOperationException("setName");
+    }
+}
---
+++ core/src/test/java/org/mule/expression/StringExpressionEvaluatorTestCase.java
@@ -0,0 +1,59 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.expression;
+
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleMessage;
+import org.mule.tck.AbstractMuleTestCase;
+import org.mule.tck.testmodels.fruit.Apple;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringExpressionEvaluatorTestCase extends AbstractMuleTestCase
+{
+    private Map props;
+
+        @Override
+        public void doSetUp()
+        {
+            props = new HashMap(3);
+            props.put("foo", "moo");
+            props.put("bar", "mar");
+            props.put("baz", "maz");
+        }
+
+    public void teststring() throws Exception
+    {
+        MuleMessage message = new DefaultMuleMessage(new Apple(), props);
+        StringExpressionEvaluator extractor = new StringExpressionEvaluator();
+        extractor.setMuleContext(muleContext);
+        Object o = extractor.evaluate("payload is #[function:shortPayloadClass] and has #[headers:count] headers", message);
+        assertNotNull(o);
+        assertEquals("payload is Apple and has 3 headers", o.toString());
+
+        o = extractor.evaluate("literal string", message);
+        assertNotNull(o);
+        assertEquals("literal string", o.toString());
+    }
+
+
+    public void testStringUsingManager() throws Exception
+    {
+        MuleMessage message = new DefaultMuleMessage(new Apple(), props);
+        Object o = muleContext.getExpressionManager().evaluate("#[string:payload is #[function:shortPayloadClass] and has #[headers:count] headers]", message);
+        assertNotNull(o);
+        assertEquals("payload is Apple and has 3 headers", o.toString());
+
+        o = muleContext.getExpressionManager().evaluate("#[string:literal string]", message);
+        assertNotNull(o);
+        assertEquals("literal string", o.toString());
+    }
+}
