e6c42affdcc9b793b2998b7bd4973f7faf7dfe38
Dan Diephouse <dan@netzooid.com>
2008-12-01 23:15:44 +0000

    Test jetty:// style endpoints
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@13484 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/cxf/src/test/java/org/mule/transport/cxf/jaxws/JettyTestCase.java
@@ -0,0 +1,60 @@
+/*
+ * $Id: ClientTestCase.java 11449 2008-03-20 12:27:50Z tcarlson $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.cxf.jaxws;
+
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.cxf.BusFactory;
+import org.apache.hello_world_soap_http.GreeterImpl;
+
+public class JettyTestCase extends FunctionalTestCase
+{
+
+    @Override
+    protected void suitePreSetUp() throws Exception
+    {
+        BusFactory.setDefaultBus(null);
+        super.suitePreSetUp();
+    }
+
+    private GreeterImpl getGreeter() throws Exception
+    {
+        Object instance = getComponent("greeterService");
+        
+        return (GreeterImpl) instance;
+    }
+
+    public void testClientWithMuleClient() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        Map<String, Object> props = new HashMap<String, Object>();
+        props.put("operation", "greetMe");
+        MuleMessage result = client.send("clientEndpoint", "Dan", props);
+        assertEquals("Hello Dan", result.getPayload());
+        
+        GreeterImpl impl = getGreeter();
+        
+        Thread.sleep(3000);
+        
+        assertEquals(1, impl.getInvocationCount());
+    }
+    
+    protected String getConfigResources()
+    {
+        return "jetty-conf.xml";
+    }
+
+}
