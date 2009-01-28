6b44a4eb91aa7b03b3c499cb44a7a3277b96efca
aperepel@gmail.com <Andrew Perepelytsya>
2009-01-28 21:09:12 +0000

    MULE-4121 (can't reproduce, but still committing a testcase) configuring xa-transaction on vm:inbound-endpoint with queueEvents="true" results in error
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@13871 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/integration/transaction/VmXaTransactionsPersistentQueueTestCase.java
@@ -0,0 +1,39 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.integration.transaction;
+
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+
+public class VmXaTransactionsPersistentQueueTestCase extends FunctionalTestCase
+{
+
+    private static final String TEST_MESSAGE = "TEST_MESSAGE";
+
+    private final long timeout = getTimeoutSecs() * 1000 / 30;
+
+    protected String getConfigResources()
+    {
+        return "org/mule/test/integration/transaction/vm-xa-transaction-persistent-queue.xml";
+    }
+
+    public void testOutboundRouterTransactions() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        MuleMessage msg = client.send("vm://in", TEST_MESSAGE, null, (int) timeout);
+
+        assertNotNull(msg);
+        assertNull(msg.getExceptionPayload());
+        assertEquals("Wrong message returned", TEST_MESSAGE + " Received", msg.getPayload());
+    }
+
+}
