aee4704db809e892e21ddd3ce6b9f57a09c1bfa7
aperepel@gmail.com <Andrew Perepelytsya>
2009-01-29 18:23:11 +0000

    MULE-4133 Provide auto-defaults for transaction manager XA node id
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@13884 bf997673-6b11-0410-b953-e057580c5b09

--- modules/jboss-transactions/src/main/java/org/mule/module/jboss/transaction/JBossArjunaTransactionManagerFactory.java
+++ modules/jboss-transactions/src/main/java/org/mule/module/jboss/transaction/JBossArjunaTransactionManagerFactory.java
@@ -16,15 +16,18 @@ import org.mule.api.transaction.TransactionManagerFactory;
 import com.arjuna.ats.arjuna.common.Environment;
 import com.arjuna.ats.arjuna.common.arjPropertyManager;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.MessageFormat;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 import javax.transaction.TransactionManager;
 
 public class JBossArjunaTransactionManagerFactory implements TransactionManagerFactory
 {
-    private Map properties = new HashMap();
+
+    private Map<String, String> properties = new HashMap<String, String>();
 
     //static
     //{
@@ -49,15 +52,31 @@ public class JBossArjunaTransactionManagerFactory implements TransactionManagerF
     {
         if (tm == null)
         {
+            // let the user override those in the config
             if (!properties.containsKey(Environment.OBJECTSTORE_DIR))
             {
                 final String muleInternalDir = MuleServer.getMuleContext().getConfiguration().getWorkingDirectory();
                 arjPropertyManager.propertyManager.setProperty(Environment.OBJECTSTORE_DIR, muleInternalDir + "/transaction-log");
             }
 
-            for (Iterator i = properties.entrySet().iterator(); i.hasNext();)
+            if (!properties.containsKey(Environment.XA_NODE_IDENTIFIER))
             {
-                Map.Entry entry = (Map.Entry) i.next();
+                try
+                {
+                    InetAddress address = InetAddress.getLocalHost();
+                    final String xaNodeId = MessageFormat.format("Mule[{0}/{1}]",
+                                                                 address.getHostName(), address.getHostAddress());
+                    properties.put(Environment.XA_NODE_IDENTIFIER, xaNodeId);
+                }
+                catch (UnknownHostException e)
+                {
+                    // ignore, let the defaults be generated
+                }
+            }
+
+
+            for (Map.Entry entry : properties.entrySet())
+            {
                 arjPropertyManager.propertyManager.setProperty((String) entry.getKey(), (String) entry.getValue());
             }
             tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
