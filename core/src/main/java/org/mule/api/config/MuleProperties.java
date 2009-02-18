7fd267d06b76c10e8430b55c91e58402813b0b37
aperepel@gmail.com <Andrew Perepelytsya>
2009-02-18 18:48:31 +0000

    Constant clases as interfaces smell - converted to classes
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@14067 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/api/config/MuleProperties.java
+++ core/src/main/java/org/mule/api/config/MuleProperties.java
@@ -14,115 +14,115 @@ package org.mule.api.config;
 /**
  * <code>MuleProperties</code> is a set of constants pertaining to Mule properties.
  */
-public interface MuleProperties
+public class MuleProperties
 {
     /**
      * The prefix for any Mule-specific properties set on an event
      */
-    String PROPERTY_PREFIX = "MULE_";
+    public static final String PROPERTY_PREFIX = "MULE_";
 
     // End System properties
 
     /********************************************************************************
      * MuleEvent Level properties
      *******************************************************************************/
-    String MULE_EVENT_PROPERTY = PROPERTY_PREFIX + "EVENT";
-    String MULE_EVENT_TIMEOUT_PROPERTY = PROPERTY_PREFIX + "EVENT_TIMEOUT";
-    String MULE_METHOD_PROPERTY = "method";
+    public static final String MULE_EVENT_PROPERTY = PROPERTY_PREFIX + "EVENT";
+    public static final String MULE_EVENT_TIMEOUT_PROPERTY = PROPERTY_PREFIX + "EVENT_TIMEOUT";
+    public static final String MULE_METHOD_PROPERTY = "method";
 
     // Deprecated. 'method' is now used consistently for all transports
-    // String MULE_METHOD_PROPERTY = PROPERTY_PREFIX + "SERVICE_METHOD";
-    String MULE_IGNORE_METHOD_PROPERTY = PROPERTY_PREFIX + "IGNORE_METHOD";
-    String MULE_ENDPOINT_PROPERTY = PROPERTY_PREFIX + "ENDPOINT";
-    String MULE_ORIGINATING_ENDPOINT_PROPERTY = PROPERTY_PREFIX + "ORIGINATING_ENDPOINT";
-    String MULE_ERROR_CODE_PROPERTY = PROPERTY_PREFIX + "ERROR_CODE";
-    String MULE_REPLY_TO_PROPERTY = PROPERTY_PREFIX + "REPLYTO";
+    // public static final String MULE_METHOD_PROPERTY = PROPERTY_PREFIX + "SERVICE_METHOD";
+    public static final String MULE_IGNORE_METHOD_PROPERTY = PROPERTY_PREFIX + "IGNORE_METHOD";
+    public static final String MULE_ENDPOINT_PROPERTY = PROPERTY_PREFIX + "ENDPOINT";
+    public static final String MULE_ORIGINATING_ENDPOINT_PROPERTY = PROPERTY_PREFIX + "ORIGINATING_ENDPOINT";
+    public static final String MULE_ERROR_CODE_PROPERTY = PROPERTY_PREFIX + "ERROR_CODE";
+    public static final String MULE_REPLY_TO_PROPERTY = PROPERTY_PREFIX + "REPLYTO";
     /**
      * Prevents processing of the ReplyTo property by the Service. This is useful
      * if you're component routed the message somewhere else which processed the
      * ReplyTo.
      */
-    String MULE_REPLY_TO_STOP_PROPERTY = PROPERTY_PREFIX + "REPLYTO_STOP";
-    String MULE_USER_PROPERTY = PROPERTY_PREFIX + "USER";
-    String MULE_ENCODING_PROPERTY = PROPERTY_PREFIX + "ENCODING";
-    String MULE_REPLY_TO_REQUESTOR_PROPERTY = PROPERTY_PREFIX + "REPLYTO_REQUESTOR";
-    String MULE_SESSION_ID_PROPERTY = PROPERTY_PREFIX + "SESSION_ID";
-    String MULE_SESSION_PROPERTY = PROPERTY_PREFIX + "SESSION";
-    String MULE_MESSAGE_ID_PROPERTY = PROPERTY_PREFIX + "MESSAGE_ID";
-    String MULE_CORRELATION_ID_PROPERTY = PROPERTY_PREFIX + "CORRELATION_ID";
-    String MULE_CORRELATION_GROUP_SIZE_PROPERTY = PROPERTY_PREFIX + "CORRELATION_GROUP_SIZE";
-    String MULE_CORRELATION_SEQUENCE_PROPERTY = PROPERTY_PREFIX + "CORRELATION_SEQUENCE";
-    String MULE_REMOTE_SYNC_PROPERTY = PROPERTY_PREFIX + "REMOTE_SYNC";
-    String MULE_REMOTE_CLIENT_ADDRESS = PROPERTY_PREFIX + "REMOTE_CLIENT_ADDRESS";
-    String MULE_SOAP_METHOD = PROPERTY_PREFIX + "SOAP_METHOD";
-    String MULE_JMS_SESSION = PROPERTY_PREFIX + "JMS_SESSION";
-    String MULE_MANAGEMENT_CONTEXT_PROPERTY = PROPERTY_PREFIX + "MANAGEMENT_CONTEXT";
+    public static final String MULE_REPLY_TO_STOP_PROPERTY = PROPERTY_PREFIX + "REPLYTO_STOP";
+    public static final String MULE_USER_PROPERTY = PROPERTY_PREFIX + "USER";
+    public static final String MULE_ENCODING_PROPERTY = PROPERTY_PREFIX + "ENCODING";
+    public static final String MULE_REPLY_TO_REQUESTOR_PROPERTY = PROPERTY_PREFIX + "REPLYTO_REQUESTOR";
+    public static final String MULE_SESSION_ID_PROPERTY = PROPERTY_PREFIX + "SESSION_ID";
+    public static final String MULE_SESSION_PROPERTY = PROPERTY_PREFIX + "SESSION";
+    public static final String MULE_MESSAGE_ID_PROPERTY = PROPERTY_PREFIX + "MESSAGE_ID";
+    public static final String MULE_CORRELATION_ID_PROPERTY = PROPERTY_PREFIX + "CORRELATION_ID";
+    public static final String MULE_CORRELATION_GROUP_SIZE_PROPERTY = PROPERTY_PREFIX + "CORRELATION_GROUP_SIZE";
+    public static final String MULE_CORRELATION_SEQUENCE_PROPERTY = PROPERTY_PREFIX + "CORRELATION_SEQUENCE";
+    public static final String MULE_REMOTE_SYNC_PROPERTY = PROPERTY_PREFIX + "REMOTE_SYNC";
+    public static final String MULE_REMOTE_CLIENT_ADDRESS = PROPERTY_PREFIX + "REMOTE_CLIENT_ADDRESS";
+    public static final String MULE_SOAP_METHOD = PROPERTY_PREFIX + "SOAP_METHOD";
+    public static final String MULE_JMS_SESSION = PROPERTY_PREFIX + "JMS_SESSION";
+    public static final String MULE_MANAGEMENT_CONTEXT_PROPERTY = PROPERTY_PREFIX + "MANAGEMENT_CONTEXT";
     // End MuleEvent Level properties
 
     /********************************************************************************
      * Generic Service descriptor properties
      *******************************************************************************/
-    String SERVICE_FINDER = "service.finder";
+    public static final String SERVICE_FINDER = "service.finder";
     
     /********************************************************************************
      * Model Service descriptor properties
      *******************************************************************************/
-    String MODEL_CLASS = "model";
+    public static final String MODEL_CLASS = "model";
 
     /********************************************************************************
      * Transport Service descriptor properties
      *******************************************************************************/
-    String CONNECTOR_CLASS = "connector";
-    String CONNECTOR_MESSAGE_RECEIVER_CLASS = "message.receiver";
-    String CONNECTOR_TRANSACTED_MESSAGE_RECEIVER_CLASS = "transacted.message.receiver";
-    String CONNECTOR_XA_TRANSACTED_MESSAGE_RECEIVER_CLASS = "xa.transacted.message.receiver";
-    String CONNECTOR_DISPATCHER_FACTORY = "dispatcher.factory";
-    String CONNECTOR_REQUESTER_FACTORY = "requester.factory";
-    String CONNECTOR_TRANSACTION_FACTORY = "transaction.factory";
-    String CONNECTOR_MESSAGE_ADAPTER = "message.adapter";
-    String CONNECTOR_INBOUND_TRANSFORMER = "inbound.transformer";
-    String CONNECTOR_OUTBOUND_TRANSFORMER = "outbound.transformer";
-    String CONNECTOR_RESPONSE_TRANSFORMER = "response.transformer";
-    String CONNECTOR_ENDPOINT_BUILDER = "endpoint.builder";
-    String CONNECTOR_SERVICE_FINDER = "service.finder";
-    String CONNECTOR_SERVICE_ERROR = "service.error";
-    String CONNECTOR_SESSION_HANDLER = "session.handler";
+    public static final String CONNECTOR_CLASS = "connector";
+    public static final String CONNECTOR_MESSAGE_RECEIVER_CLASS = "message.receiver";
+    public static final String CONNECTOR_TRANSACTED_MESSAGE_RECEIVER_CLASS = "transacted.message.receiver";
+    public static final String CONNECTOR_XA_TRANSACTED_MESSAGE_RECEIVER_CLASS = "xa.transacted.message.receiver";
+    public static final String CONNECTOR_DISPATCHER_FACTORY = "dispatcher.factory";
+    public static final String CONNECTOR_REQUESTER_FACTORY = "requester.factory";
+    public static final String CONNECTOR_TRANSACTION_FACTORY = "transaction.factory";
+    public static final String CONNECTOR_MESSAGE_ADAPTER = "message.adapter";
+    public static final String CONNECTOR_INBOUND_TRANSFORMER = "inbound.transformer";
+    public static final String CONNECTOR_OUTBOUND_TRANSFORMER = "outbound.transformer";
+    public static final String CONNECTOR_RESPONSE_TRANSFORMER = "response.transformer";
+    public static final String CONNECTOR_ENDPOINT_BUILDER = "endpoint.builder";
+    public static final String CONNECTOR_SERVICE_FINDER = "service.finder";
+    public static final String CONNECTOR_SERVICE_ERROR = "service.error";
+    public static final String CONNECTOR_SESSION_HANDLER = "session.handler";
     // End Connector Service descriptor properties
 
-    String MULE_WORKING_DIRECTORY_PROPERTY = "mule.working.dir";
-    String MULE_HOME_DIRECTORY_PROPERTY = "mule.home";
+    public static final String MULE_WORKING_DIRECTORY_PROPERTY = "mule.working.dir";
+    public static final String MULE_HOME_DIRECTORY_PROPERTY = "mule.home";
 
     // Object Name Keys
 
-    String OBJECT_SYSTEM_MODEL = "_muleSystemModel";
-    String OBJECT_MULE_CONTEXT_PROCESSOR = "_muleContextProcessor";
-    String OBJECT_PROPERTY_PLACEHOLDER_PROCESSOR = "_mulePropertyPlaceholderProcessor";
-    String OBJECT_OBJECT_NAME_PROCESSOR = "_muleObjectNameProcessor";
-    String OBJECT_LIFECYCLE_MANAGER = "_muleLifecycleManager";
-    String OBJECT_SECURITY_MANAGER = "_muleSecurityManager";
-    String OBJECT_TRANSACTION_MANAGER = "_muleTransactionManager";
-    String OBJECT_QUEUE_MANAGER = "_muleQueueManager";
-    String OBJECT_MULE_APPLICATION_PROPERTIES = "_muleProperties";
-    String OBJECT_MULE_ENDPOINT_FACTORY = "_muleEndpointFactory";
-    String OBJECT_MULE_STREAM_CLOSER_SERVICE = "_muleStreamCloserService";
-    String OBJECT_MULE_SIMPLE_REGISTRY_BOOTSTRAP = "_muleSimpleRegistryBootstrap";
-    String OBJECT_DEFAULT_THREADING_PROFILE = "_defaultThreadingProfile";
-    String OBJECT_DEFAULT_MESSAGE_DISPATCHER_THREADING_PROFILE = "_defaultMessageDispatcherThreadingProfile";
-    String OBJECT_DEFAULT_MESSAGE_REQUESTER_THREADING_PROFILE = "_defaultMessageRequesterThreadingProfile";
-    String OBJECT_DEFAULT_MESSAGE_RECEIVER_THREADING_PROFILE = "_defaultMessageReceiverThreadingProfile";
-    String OBJECT_DEFAULT_SERVICE_THREADING_PROFILE = "_defaultServiceThreadingProfile";
-    String OBJECT_DEFAULT_RETRY_POLICY_TEMPLATE = "_defaultRetryPolicyTemplate";
-    String OBJECT_MULE_CONFIGURATION = "_muleConfiguration";
-    String OBJECT_MULE_NAMESPACE_MANAGER = "_muleNamespaceManager";
+    public static final String OBJECT_SYSTEM_MODEL = "_muleSystemModel";
+    public static final String OBJECT_MULE_CONTEXT_PROCESSOR = "_muleContextProcessor";
+    public static final String OBJECT_PROPERTY_PLACEHOLDER_PROCESSOR = "_mulePropertyPlaceholderProcessor";
+    public static final String OBJECT_OBJECT_NAME_PROCESSOR = "_muleObjectNameProcessor";
+    public static final String OBJECT_LIFECYCLE_MANAGER = "_muleLifecycleManager";
+    public static final String OBJECT_SECURITY_MANAGER = "_muleSecurityManager";
+    public static final String OBJECT_TRANSACTION_MANAGER = "_muleTransactionManager";
+    public static final String OBJECT_QUEUE_MANAGER = "_muleQueueManager";
+    public static final String OBJECT_MULE_APPLICATION_PROPERTIES = "_muleProperties";
+    public static final String OBJECT_MULE_ENDPOINT_FACTORY = "_muleEndpointFactory";
+    public static final String OBJECT_MULE_STREAM_CLOSER_SERVICE = "_muleStreamCloserService";
+    public static final String OBJECT_MULE_SIMPLE_REGISTRY_BOOTSTRAP = "_muleSimpleRegistryBootstrap";
+    public static final String OBJECT_DEFAULT_THREADING_PROFILE = "_defaultThreadingProfile";
+    public static final String OBJECT_DEFAULT_MESSAGE_DISPATCHER_THREADING_PROFILE = "_defaultMessageDispatcherThreadingProfile";
+    public static final String OBJECT_DEFAULT_MESSAGE_REQUESTER_THREADING_PROFILE = "_defaultMessageRequesterThreadingProfile";
+    public static final String OBJECT_DEFAULT_MESSAGE_RECEIVER_THREADING_PROFILE = "_defaultMessageReceiverThreadingProfile";
+    public static final String OBJECT_DEFAULT_SERVICE_THREADING_PROFILE = "_defaultServiceThreadingProfile";
+    public static final String OBJECT_DEFAULT_RETRY_POLICY_TEMPLATE = "_defaultRetryPolicyTemplate";
+    public static final String OBJECT_MULE_CONFIGURATION = "_muleConfiguration";
+    public static final String OBJECT_MULE_NAMESPACE_MANAGER = "_muleNamespaceManager";
 
     // Not currently used as these need to be instance variables of the MuleContext.
-    String OBJECT_WORK_MANAGER = "_muleWorkManager";
-    String OBJECT_NOTIFICATION_MANAGER = "_muleNotificationManager";
+    public static final String OBJECT_WORK_MANAGER = "_muleWorkManager";
+    public static final String OBJECT_NOTIFICATION_MANAGER = "_muleNotificationManager";
 
     /**
     * Specifies whether mule should process messages sysnchonously, i.e. that a
     * mule-model can only process one message at a time, or asynchronously. The
     * default value is 'false'.
     */
-    String SYNCHRONOUS_PROPERTY = "synchronous";
+    public static final String SYNCHRONOUS_PROPERTY = "synchronous";
 }
--- transports/cxf/src/main/java/org/mule/transport/cxf/CxfConstants.java
+++ transports/cxf/src/main/java/org/mule/transport/cxf/CxfConstants.java
@@ -10,36 +10,35 @@
 
 package org.mule.transport.cxf;
 
-public interface CxfConstants 
+public class CxfConstants 
 {
-
-    String DATA_BINDING = "databinding";
-    String BINDING_ID = "bindingId";
-    String FRONTEND = "frontend";
-    String JAX_WS_FRONTEND = "jaxws";
-    String SIMPLE_FRONTEND = "simple";
-    String WSDL_LOCATION = "wsdlLocation";
-    String NAMESPACE = "namespace";
-    String SERVICE_NAME = "serviceName";
-    String CLIENT_CLASS = "clientClass";
-    String SERVICE_INTERFACE = "serviceInterface";
-    String CLIENT_PORT = "wsdlPort";
-    String OPERATION = "operation";
-    String PROXY = "proxy";
-    String SERVICE_CLASS = "serviceClass";
-    String FEATURES = "features";
-    String IN_INTERCEPTORS = "inInterceptors";
-    String IN_FAULT_INTERCEPTORS = "inFaultInterceptors";
-    String OUT_INTERCEPTORS = "outInterceptors";
-    String OUT_FAULT_INTERCEPTORS = "outFaultInterceptors";
-    String MTOM_ENABLED = "mtomEnabled";
-    String MULE_MESSAGE = "mule.message";
+    public static final String DATA_BINDING = "databinding";
+    public static final String BINDING_ID = "bindingId";
+    public static final String FRONTEND = "frontend";
+    public static final String JAX_WS_FRONTEND = "jaxws";
+    public static final String SIMPLE_FRONTEND = "simple";
+    public static final String WSDL_LOCATION = "wsdlLocation";
+    public static final String NAMESPACE = "namespace";
+    public static final String SERVICE_NAME = "serviceName";
+    public static final String CLIENT_CLASS = "clientClass";
+    public static final String SERVICE_INTERFACE = "serviceInterface";
+    public static final String CLIENT_PORT = "wsdlPort";
+    public static final String OPERATION = "operation";
+    public static final String PROXY = "proxy";
+    public static final String SERVICE_CLASS = "serviceClass";
+    public static final String FEATURES = "features";
+    public static final String IN_INTERCEPTORS = "inInterceptors";
+    public static final String IN_FAULT_INTERCEPTORS = "inFaultInterceptors";
+    public static final String OUT_INTERCEPTORS = "outInterceptors";
+    public static final String OUT_FAULT_INTERCEPTORS = "outFaultInterceptors";
+    public static final String MTOM_ENABLED = "mtomEnabled";
+    public static final String MULE_MESSAGE = "mule.message";
     
-    String APPLY_FILTERS_TO_PROTOCOL = "applyFiltersToProtocol";
-    String APPLY_TRANSFORMERS_TO_PROTOCOL = "applyTransformersToProtocol";
-    String APPLY_SECURITY_TO_PROTOCOL = "applySecurityToProtocol";
-    String PROTOCOL_CONNECTOR = "protocolConnector";
-    String ATTACHMENTS = "cxf_attachments";
-    String INBOUND_SERVICE= "cxf_service";
-    String INBOUND_OPERATION= "cxf_operation";
+    public static final String APPLY_FILTERS_TO_PROTOCOL = "applyFiltersToProtocol";
+    public static final String APPLY_TRANSFORMERS_TO_PROTOCOL = "applyTransformersToProtocol";
+    public static final String APPLY_SECURITY_TO_PROTOCOL = "applySecurityToProtocol";
+    public static final String PROTOCOL_CONNECTOR = "protocolConnector";
+    public static final String ATTACHMENTS = "cxf_attachments";
+    public static final String INBOUND_SERVICE= "cxf_service";
+    public static final String INBOUND_OPERATION= "cxf_operation";
 }
