c55bd118191d7246cdce66afc78a961fefd3b068
tcarlson <tcarlson>
2009-04-09 12:58:33 +0000

    Make this interface more flexible
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@14503 bf997673-6b11-0410-b953-e057580c5b09

--- transports/jms/src/test/java/org/mule/transport/jms/test/TestReconnectionConnectionFactoryWrapper.java
+++ transports/jms/src/test/java/org/mule/transport/jms/test/TestReconnectionConnectionFactoryWrapper.java
@@ -13,28 +13,16 @@ package org.mule.transport.jms.test;
 import java.lang.reflect.Method;
 import java.util.List;
 
-import javax.jms.JMSException;
-import javax.jms.QueueConnection;
-import javax.jms.TopicConnection;
+import javax.jms.ConnectionFactory;
 
 /**
  * Interface for testing JMS reconnections. Implementing classes should extend a
  * particular JMS providers ConnectionFactory and throw a JMS exception when
  * isEnabled() == false and the mule server is trying to connect to it.
  */
-public interface TestReconnectionConnectionFactoryWrapper
+public interface TestReconnectionConnectionFactoryWrapper extends ConnectionFactory
 {
     public abstract void init();
-
-    public abstract QueueConnection createQueueConnection() throws JMSException;
-
-    public abstract QueueConnection createQueueConnection(String userName, String password)
-        throws JMSException;
-
-    public abstract TopicConnection createTopicConnection() throws JMSException;
-
-    public abstract TopicConnection createTopicConnection(String userName, String password)
-        throws JMSException;
 
     // For InvocationHandler interface
     public abstract Object invoke(Object proxy, Method method, Object[] args) throws Throwable;
