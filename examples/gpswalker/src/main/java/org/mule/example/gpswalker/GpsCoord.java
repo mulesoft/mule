84e6ae47cbdeb7a4873b870d82a58d99d8055302
Ross Mason <ross@mulesoft.com>
2009-05-09 09:00:23 +0000

    MULE-4357: New Example: GPS Walker. Plots GPS coords generated by Mule on a map in the browser
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@14675 bf997673-6b11-0410-b953-e057580c5b09

---
+++ examples/gpswalker/src/main/java/org/mule/example/gpswalker/GpsCoord.java
@@ -0,0 +1,33 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.example.gpswalker;
+
+/**
+ * TODO
+ */
+public class GpsCoord {
+
+	private float latitude;
+	private float longitude;
+
+	public GpsCoord(float lat, float lng) {
+		latitude = lat;
+		longitude = lng;
+	}
+
+	public float getLatitude() {
+		return latitude;
+	}
+
+	public float getLongitude() {
+		return longitude;
+	}
+
+}
