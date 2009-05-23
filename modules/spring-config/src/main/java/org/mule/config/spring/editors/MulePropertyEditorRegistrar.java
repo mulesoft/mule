8db7b0434dd6416b6d155d9160f5ae157a075bce
Ross Mason <ross@mulesoft.com>
2009-05-23 15:18:32 +0000

    Needed change for Spring 3.0 support
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@14765 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/spring-config/src/main/java/org/mule/config/spring/editors/MulePropertyEditorRegistrar.java
@@ -0,0 +1,28 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.config.spring.editors;
+
+import org.mule.api.transport.Connector;
+import org.mule.endpoint.URIBuilder;
+
+import org.springframework.beans.PropertyEditorRegistrar;
+import org.springframework.beans.PropertyEditorRegistry;
+
+/**
+ * The preferred way to configure property editors in Spring 2/3 is to implement a registrar
+ */
+public class MulePropertyEditorRegistrar implements PropertyEditorRegistrar
+{
+    public void registerCustomEditors(PropertyEditorRegistry registry)
+    {
+        registry.registerCustomEditor(Connector.class, new ConnectorPropertyEditor());
+        registry.registerCustomEditor(URIBuilder.class, new URIBuilderPropertyEditor());
+    }
+}
