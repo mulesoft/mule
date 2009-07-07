f163758e7878cc963a14c2b3c0b1a646fb15aee3
Dirk Olmes <dirk.olmes@mulesoft.com>
2009-07-07 08:59:46 +0000

    Merged revisions 15158 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.1.x
    ........
      r15158 | dirk.olmes | 2009-07-07 10:47:02 +0200 (Tue, 07 Jul 2009) | 1 line
      
      MULE-4043 (CXF registers full URL as a service name, breaks JMX name conformance): Incorporated AP's feedback about changing service names. The name of the service includes the full (escaped) endpoint URL now which should make it unique within Mule (Mule cannot have two endpoints listening on the same port)
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@15160 bf997673-6b11-0410-b953-e057580c5b09

--- transports/cxf/src/main/java/org/mule/transport/cxf/CxfConnector.java
+++ transports/cxf/src/main/java/org/mule/transport/cxf/CxfConnector.java
@@ -43,6 +43,7 @@ import javax.xml.namespace.QName;
 import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.bus.spring.SpringBusFactory;
+import org.apache.cxf.endpoint.Endpoint;
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.transport.ConduitInitiatorManager;
 import org.apache.cxf.transport.DestinationFactoryManager;
@@ -203,7 +204,10 @@ public class CxfConnector extends AbstractConnector implements MuleContextNotifi
         // TODO MULE-2228 Simplify this API
         SedaService service = new SedaService();
         service.setMuleContext(muleContext);
-        service.setName(CXF_SERVICE_COMPONENT_NAME + server.getEndpoint().getService().getName().getLocalPart() + service.hashCode());
+
+        String uniqueServiceName = createServiceName(server.getEndpoint());
+        service.setName(uniqueServiceName);
+
         service.setModel(muleContext.getRegistry().lookupSystemModel());
 
         CxfServiceComponent svcComponent = new CxfServiceComponent(this, (CxfMessageReceiver) receiver);
@@ -312,6 +316,22 @@ public class CxfConnector extends AbstractConnector implements MuleContextNotifi
         service.setInboundRouter(new DefaultInboundRouterCollection());
         service.getInboundRouter().addEndpoint(protocolEndpoint);
         services.add(service);
+    }
+    
+    /**
+     * Build a unique name for the endpoint that is well suited for exposure by JMX.
+     */
+    protected String createServiceName(Endpoint endpoint)
+    {
+        StringBuilder name = new StringBuilder(CXF_SERVICE_COMPONENT_NAME);
+        name.append("{");
+        
+        String address = endpoint.getEndpointInfo().getAddress();
+        name.append(address.replace(":", "|"));
+        name.append("}");
+        name.append(endpoint.getService().getName().getLocalPart());
+
+        return name.toString();
     }
 
     /**
