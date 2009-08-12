979e093032263e4ac26b12c9f818c414401508d7
tcarlson <tcarlson>
2009-08-12 22:25:34 +0000

    Recorded merge of revisions 15425-15427,15429-15430,15433 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.2.x
    ........
      r15425 | tcarlson | 2009-08-11 17:55:20 -0700 (Tue, 11 Aug 2009) | 1 line
      
      Parameterize the test case for EE-1632
    ........
      r15433 | tcarlson | 2009-08-12 10:53:14 -0700 (Wed, 12 Aug 2009) | 1 line
      
      Make the test more generic so that it works with WMQ
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@15438 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/jms/src/test/java/org/mule/transport/jms/integration/JmsReplyToPropertyTestCase.java
@@ -0,0 +1,50 @@
+package org.mule.transport.jms.integration;
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+public class JmsReplyToPropertyTestCase extends AbstractJmsFunctionalTestCase
+{
+    public JmsReplyToPropertyTestCase(JmsVendorConfiguration config)
+    {
+        super(config);
+    }
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "integration/jms-replyto-property.xml";
+    }
+
+    @Test
+    public void testReplyTo() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        Map props = new HashMap();
+        props.put("JMSReplyTo", "middle");
+        client.dispatch("in", DEFAULT_INPUT_MESSAGE, props);
+
+        // Check that the property is still on the outbound message
+        MuleMessage output = client.request("out", 2000);
+        assertTrue(output.getProperty("JMSReplyTo").toString().contains("middle"));
+        
+        // Check that the reply message was generated
+        output = client.request("middle", 2000);
+        assertNotNull(output);
+        assertEquals(DEFAULT_OUTPUT_MESSAGE, output.getPayload());
+    }
+}
