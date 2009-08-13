ba3aa267af08b0c87cb9082723aa27fcb258dcd4
Dirk Olmes <dirk.olmes@mulesoft.com>
2009-08-13 12:39:12 +0000

    Merged revisions 15443 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.2.x
    ........
      r15443 | dirk.olmes | 2009-08-13 14:24:04 +0200 (Thu, 13 Aug 2009) | 2 lines
      
      fix license header
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@15444 bf997673-6b11-0410-b953-e057580c5b09

--- transports/jms/src/test/java/org/mule/transport/jms/integration/JmsReplyToPropertyTestCase.java
+++ transports/jms/src/test/java/org/mule/transport/jms/integration/JmsReplyToPropertyTestCase.java
@@ -1,12 +1,3 @@
-package org.mule.transport.jms.integration;
-import org.mule.api.MuleMessage;
-import org.mule.module.client.MuleClient;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.Test;
-
 /*
  * $Id$
  * --------------------------------------------------------------------------------------
@@ -16,6 +7,16 @@ import org.junit.Test;
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
+
+package org.mule.transport.jms.integration;
+
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
 
 public class JmsReplyToPropertyTestCase extends AbstractJmsFunctionalTestCase
 {
@@ -34,7 +35,7 @@ public class JmsReplyToPropertyTestCase extends AbstractJmsFunctionalTestCase
     public void testReplyTo() throws Exception
     {
         MuleClient client = new MuleClient();
-        Map props = new HashMap();
+        Map<String, String> props = new HashMap<String, String>();
         props.put("JMSReplyTo", "middle");
         client.dispatch("in", DEFAULT_INPUT_MESSAGE, props);
 
