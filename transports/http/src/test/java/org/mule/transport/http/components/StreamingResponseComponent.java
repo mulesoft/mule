75acc740064d5feddf7987a352ff8e88de2fa594
Dirk Olmes <dirk.olmes@mulesoft.com>
2009-08-18 12:59:44 +0000

    fix license header, checkstyle, tabs2spaces
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@15467 bf997673-6b11-0410-b953-e057580c5b09

--- transports/http/src/test/java/org/mule/transport/http/components/StreamingResponseComponent.java
+++ transports/http/src/test/java/org/mule/transport/http/components/StreamingResponseComponent.java
@@ -1,3 +1,13 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.transport.http.components;
 
 import java.io.ByteArrayInputStream;
@@ -7,13 +17,11 @@ import org.mule.api.lifecycle.Callable;
 
 /**
  * A simple component which returns a stream.
- * @author Dan
- *
  */
-public class StreamingResponseComponent implements Callable {
-
-	public Object onCall(MuleEventContext eventContext) throws Exception {
-		return new ByteArrayInputStream("hello".getBytes());
-	}
-
+public class StreamingResponseComponent implements Callable 
+{
+    public Object onCall(MuleEventContext eventContext) throws Exception 
+    {
+        return new ByteArrayInputStream("hello".getBytes());
+    }
 }
