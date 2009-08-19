b8de31bb2206e8c4ced953ae830634ccb5c2245c
Dirk Olmes <dirk.olmes@mulesoft.com>
2009-08-19 15:29:56 +0000

    license header, checkstyle, tabs2spaces
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@15474 bf997673-6b11-0410-b953-e057580c5b09

--- transports/http/src/test/java/org/mule/transport/http/functional/PartialReadComponent.java
+++ transports/http/src/test/java/org/mule/transport/http/functional/PartialReadComponent.java
@@ -1,3 +1,13 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.transport.http.functional;
 
 import org.mule.api.MuleEventContext;
@@ -5,16 +15,13 @@ import org.mule.api.lifecycle.Callable;
 
 import java.io.InputStream;
 
-/**
- * 
- */
-public class PartialReadComponent implements Callable {
+public class PartialReadComponent implements Callable 
+{
+    public Object onCall(MuleEventContext eventContext) throws Exception
+    {
+        InputStream stream = (InputStream) eventContext.getMessage().getPayload(InputStream.class);
 
-	public Object onCall(MuleEventContext eventContext) throws Exception {
-		InputStream stream = (InputStream) eventContext.getMessage().getPayload(InputStream.class);
-		
-		stream.read();
-		return "Hello";
-	}
-
+        stream.read();
+        return "Hello";
+    }
 }
