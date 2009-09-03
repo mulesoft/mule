846efaa1d9f9c5de3b0870133c229ace24f9a61d
aperepel@gmail.com <Andrew Perepelytsya>
2009-09-03 22:02:21 +0000

    This got re-applied again (and caused QA builds to lock forever). Rollback the default timeout value
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@15640 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/test/java/org/mule/tck/AbstractMuleTestCase.java
+++ core/src/test/java/org/mule/tck/AbstractMuleTestCase.java
@@ -84,7 +84,7 @@ public abstract class AbstractMuleTestCase extends TestCase implements TestCaseW
     /**
      * Default test watchdog timeout in seconds.
      */
-    public static final int DEFAULT_MULE_TEST_TIMEOUT_SECS = 6000;
+    public static final int DEFAULT_MULE_TEST_TIMEOUT_SECS = 60;
 
     protected static MuleContext muleContext;
 
