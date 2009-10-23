f32dd1cde86bb7236c2c6f3373d683d4a38cd465
Ross Mason <ross@mulesoft.com>
2009-10-23 15:55:18 +0000

    clean up, add license headers
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@15869 bf997673-6b11-0410-b953-e057580c5b09

--- modules/annotations/src/main/java/org/mule/config/annotations/expressions/Groovy.java
+++ modules/annotations/src/main/java/org/mule/config/annotations/expressions/Groovy.java
@@ -1,3 +1,12 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.config.annotations.expressions;
 
 import java.lang.annotation.Documented;
@@ -25,7 +34,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Evaluator("groovy")
-public @interface Groovy
+public @interface
+        Groovy
 {
     public abstract String value();
 
