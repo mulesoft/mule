b4bb59995d10516e236606f6b398217f7fc58efa
Dirk Olmes <dirk.olmes@mulesoft.com>
2009-12-17 02:57:11 +0000

    Merged revisions 16277 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.2.x
    ........
      r16277 | dirk.olmes | 2009-12-17 03:55:01 +0100 (Thu, 17 Dec 2009) | 1 line
      
      The root cause of this issue is fixed in MULE-4657. Re-enable the test on IBM JDK
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@16278 bf997673-6b11-0410-b953-e057580c5b09

--- transports/ssl/src/test/java/org/mule/transport/ssl/issues/AsynchronousSslMule1854TestCase.java
+++ transports/ssl/src/test/java/org/mule/transport/ssl/issues/AsynchronousSslMule1854TestCase.java
@@ -13,19 +13,9 @@ package org.mule.transport.ssl.issues;
 import org.mule.api.MuleMessage;
 import org.mule.module.client.MuleClient;
 import org.mule.tck.FunctionalTestCase;
-import org.mule.util.SystemUtils;
 
 public class AsynchronousSslMule1854TestCase extends FunctionalTestCase 
 {
-    protected static String TEST_MESSAGE = "Test Request";
-
-    @Override
-    protected boolean isDisabledInThisEnvironment()
-    {
-        // MULE-4664
-        return SystemUtils.isIbmJDK();
-    }
-
     protected String getConfigResources()
     {
         return "ssl-functional-test.xml";
@@ -35,7 +25,7 @@ public class AsynchronousSslMule1854TestCase extends FunctionalTestCase
     {
         MuleClient client = new MuleClient();
         client.dispatch("asyncEndpoint", TEST_MESSAGE, null);
-        // MULE-2754
+        // MULE-2757
         Thread.sleep(100);
         MuleMessage response = client.request("asyncEndpoint", 5000);
         assertNotNull("Response is null", response);
