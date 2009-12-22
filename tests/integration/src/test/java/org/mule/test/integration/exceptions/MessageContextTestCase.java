9ffd30eb2aa0149206b8d0aec8f2148d6166015e
Dirk Olmes <dirk.olmes@mulesoft.com>
2009-12-22 08:48:58 +0000

    License header, checkstyle
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@16315 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/exceptions/MessageContextTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/exceptions/MessageContextTestCase.java
@@ -1,3 +1,13 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.test.integration.exceptions;
 
 import org.mule.DefaultMuleMessage;
@@ -5,30 +15,26 @@ import org.mule.api.MuleMessage;
 import org.mule.module.client.MuleClient;
 import org.mule.tck.FunctionalTestCase;
 
-public class MessageContextTestCase extends FunctionalTestCase {
-
+public class MessageContextTestCase extends FunctionalTestCase 
+{
 	String request = "Hello World";
 
-	protected String getConfigResources() {
+	@Override
+    protected String getConfigResources() 
+	{
 		return "org/mule/test/integration/exceptions/message-context-test.xml";
 	}
 
 	/**
 	 * Test for MULE-4361
-	 * @throws Exception
 	 */
-	public void testAlternateExceptionStrategy() throws Exception {
+	public void testAlternateExceptionStrategy() throws Exception 
+	{
+	    MuleClient client = new MuleClient();
+	    DefaultMuleMessage msg = new DefaultMuleMessage(request, client.getMuleContext());
+	    MuleMessage response = client.send("testin", msg, 200000);
+	    assertNotNull(response);
+	    Thread.sleep(10000); // Wait for test to finish
 
-		try {
-			MuleClient client1 = new MuleClient();
-			DefaultMuleMessage msg1 = new DefaultMuleMessage(request, client1.getMuleContext());
-			MuleMessage response1 = client1.send("testin", msg1, 200000);
-			assertNotNull(response1);
-			Thread.sleep(10000); // Wait for test to finish
-		} catch (Throwable t) {
-			System.out.println(t.getMessage());
-			t.printStackTrace();
-		}
 	}
-
 }
