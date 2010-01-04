6044aa496257eb27f951d0c00b0a05dd18a7963f
Dirk Olmes <dirk.olmes@mulesoft.com>
2010-01-04 13:33:51 +0000

    MULE-4194 (Upgrade Groovy version to 1.6.x): thanks to Andrew P for reminding me of this dependency
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@16363 bf997673-6b11-0410-b953-e057580c5b09

--- distributions/standalone/src/main/resources/bin/launcher.bat
+++ distributions/standalone/src/main/resources/bin/launcher.bat
@@ -1,3 +1,3 @@
 @echo off
-set cp=.;%MULE_HOME%\conf;%MULE_HOME%\lib\opt\groovy-all-1.5.6-osgi.jar;%MULE_HOME%\lib\boot\commons-cli-1.0-osgi.jar
+set cp=.;%MULE_HOME%\conf;%MULE_HOME%\lib\opt\groovy-all-1.6.5.jar;%MULE_HOME%\lib\boot\commons-cli-1.0-osgi.jar
 java -Dmule.home=%MULE_HOME% -cp "%cp%" org.codehaus.groovy.tools.GroovyStarter --main groovy.ui.GroovyMain --conf %MULE_HOME%\bin\launcher.conf %*
