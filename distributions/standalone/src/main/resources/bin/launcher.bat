7f6349ab3d31d507bb55f9493d30bba0e3678cd8
Dirk Olmes <dirk.olmes@mulesoft.com>
2010-01-05 13:07:16 +0000

    MULE-4194 (Upgrade Groovy version to 1.6.x): back to Groovy 1.6.7 as Groovy 1.7.0 causes test failures and I don't have the time to debug now. Update the launcher scripts to make them independent of the jar versions we use so we don't have to update them each time we update the dependencies.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@16373 bf997673-6b11-0410-b953-e057580c5b09

--- distributions/standalone/src/main/resources/bin/launcher.bat
+++ distributions/standalone/src/main/resources/bin/launcher.bat
@@ -1,3 +1,13 @@
 @echo off
-set cp=.;%MULE_HOME%\conf;%MULE_HOME%\lib\opt\groovy-all-1.7.0.jar;%MULE_HOME%\lib\boot\commons-cli-1.2.jar
+
+rem dynamically evaluate the name of the groovy jar
+for /F %%v in ('dir /b lib\opt^| findstr groovy') do set GROOVY_JAR=%%v
+set GROOVY_PATH=%MULE_HOME%\lib\opt\%GROOVY_JAR%
+
+rem dynamically evaluate the name of the commons-cli jar
+for /F %%v in ('dir /b lib\boot^| findstr commons-cli') do set COMMONS_CLI_JAR=%%v
+set COMMONS_CLI_PATH=%MULE_HOME%\lib\boot\%COMMONS_CLI_JAR%
+
+set cp=.;%MULE_HOME%\conf
+set cp=%cp%;%GROOVY_PATH%;%COMMONS_CLI_PATH%
 java -Dmule.home=%MULE_HOME% -cp "%cp%" org.codehaus.groovy.tools.GroovyStarter --main groovy.ui.GroovyMain --conf %MULE_HOME%\bin\launcher.conf %*
