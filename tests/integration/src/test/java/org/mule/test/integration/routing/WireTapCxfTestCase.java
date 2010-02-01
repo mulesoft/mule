5113f6e2dcadea538652d4086a6c9403dc20b68d
Dirk Olmes <dirk.olmes@mulesoft.com>
2010-02-01 16:04:51 +0000

    Merged revisions 16579 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.2.x
    ........
      r16579 | dirk.olmes | 2010-02-01 16:22:53 +0100 (Mon, 01 Feb 2010) | 2 lines
      
      EE-1803 (Message access violation error while using wire-tap-router with cxf endpoint): add a test case
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@16580 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/integration/routing/WireTapCxfTestCase.java
@@ -0,0 +1,62 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.integration.routing;
+
+import org.mule.api.MuleMessage;
+import org.mule.api.context.notification.ServerNotification;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.tck.functional.FunctionalTestNotificationListener;
+import org.mule.util.concurrent.Latch;
+
+import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
+
+public class WireTapCxfTestCase extends FunctionalTestCase
+{
+    static final Latch tapLatch = new Latch();
+    
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        super.doSetUp();
+
+        muleContext.registerListener(new FunctionalTestNotificationListener()
+        {
+            public void onNotification(ServerNotification notification)
+            {
+                tapLatch.countDown();
+            }
+        });
+    }
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/test/integration/routing/wire-tap-cxf.xml";
+    }
+
+    public void testWireTap() throws Exception
+    {
+        String url = "http://localhost:65082/services/EchoUMO";
+        String msg = "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">"
+            + "<soap:Body><echo><text>foo</text></echo></soap:Body></soap:Envelope>";
+
+        MuleClient client = new MuleClient();
+        MuleMessage response = client.send(url, msg, null);
+        assertNotNull(response);
+        
+        String responseString = response.getPayloadAsString();
+        assertTrue(responseString.contains("echoResponse"));
+        assertFalse(responseString.contains("soap:Fault"));
+                
+        assertTrue(tapLatch.await(RECEIVE_TIMEOUT, TimeUnit.MILLISECONDS));
+    }
+}
