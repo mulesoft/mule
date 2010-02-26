0e728042f6be814af1175a72174a3c919cc70e34
Ross Mason <ross@mulesoft.com>
2010-02-26 16:11:52 +0000

    we do need to call dispose on the component object
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@16766 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/component/DefaultLifecycleAdapter.java
+++ core/src/main/java/org/mule/component/DefaultLifecycleAdapter.java
@@ -182,13 +182,19 @@ public class DefaultLifecycleAdapter implements LifecycleAdapter
                 // unregister a hard ref to the component object
                 muleContext.getRegistry().unregisterObject(createRegistryHardRefName(component));
 
+                //make sure we haven't lost the reference to the object
+                Object o = componentObject.get();
+                if(o!=null)
+                {
+                    ((Disposable) o).dispose();
+                }
+                
                 componentObject.clear();
                 componentObject.enqueue();
 
             }
             catch (Exception e)
             {
-                // TODO MULE-863: Handle or fail
                 logger.error("failed to dispose: " + component.getService().getName(), e);
             }
         }
@@ -227,8 +233,6 @@ public class DefaultLifecycleAdapter implements LifecycleAdapter
         }
         catch (Exception e)
         {
-            // should all Exceptions caught here be a ServiceException?!?
-            // TODO MULE-863: See above
             throw new ServiceException(RequestContext.getEventContext().getMessage(), component.getService(), e);
         }
 
