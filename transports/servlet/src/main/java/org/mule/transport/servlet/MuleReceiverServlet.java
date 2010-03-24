5270b40d94710ece1b5e3dd8ec9984293a121094
Daniel Feist <dfeist@gmail.com>
2010-03-24 04:56:41 +0000

    MULE-4806 REST functionality of MuleRESTReceiverServlet is non-functional
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@16906 bf997673-6b11-0410-b953-e057580c5b09

--- transports/servlet/src/main/java/org/mule/transport/servlet/MuleReceiverServlet.java
+++ transports/servlet/src/main/java/org/mule/transport/servlet/MuleReceiverServlet.java
@@ -154,9 +154,57 @@ public class MuleReceiverServlet extends AbstractReceiverServlet
     {
         // template method
     }
+    
+    // We cannot override the service method and maintain MuleRESTServletReceiver
+    // functionality. See MULE-4806.
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {
+        doAllMethods(req, resp);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {
+        doAllMethods(req, resp);
+    }
+    
+    @Override
+    protected void doHead(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {
+        doAllMethods(req, resp);
+    }
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {
+        doAllMethods(req, resp);
+    }
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {
+        doAllMethods(req, resp);
+    }
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {
+        doAllMethods(req, resp);
+    }
+    @Override
+    protected void doTrace(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {
+        doAllMethods(req, resp);
+    }
+
+    protected void doAllMethods(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         try
         {
