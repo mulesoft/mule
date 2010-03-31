e17c3c43f6837cfdd100253912dccd2add42beeb
Dirk Olmes <dirk.olmes@mulesoft.com>
2010-03-31 08:00:28 +0000

    MULE-4823 (MULE 3.0 M2 release is failing to launch mule as a windows service.)
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@16962 bf997673-6b11-0410-b953-e057580c5b09

--- distributions/standalone/src/main/resources/bin/launcher.bat
+++ distributions/standalone/src/main/resources/bin/launcher.bat
@@ -1,12 +1,12 @@
 @echo off
 
 rem dynamically evaluate the name of the groovy jar
-for /F %%v in ('dir /b lib\opt^| findstr groovy') do set GROOVY_JAR=%%v
-set GROOVY_PATH=%MULE_HOME%\lib\opt\%GROOVY_JAR%
+for /F %%v in ('dir /b %MULE_BASE%\lib\opt^| findstr groovy') do set GROOVY_JAR=%%v
+set GROOVY_PATH=%MULE_BASE%\lib\opt\%GROOVY_JAR%
 
 rem dynamically evaluate the name of the commons-cli jar
-for /F %%v in ('dir /b lib\boot^| findstr commons-cli') do set COMMONS_CLI_JAR=%%v
-set COMMONS_CLI_PATH=%MULE_HOME%\lib\boot\%COMMONS_CLI_JAR%
+for /F %%v in ('dir /b %MULE_BASE%\lib\boot^| findstr commons-cli') do set COMMONS_CLI_JAR=%%v
+set COMMONS_CLI_PATH=%MULE_BASE%\lib\boot\%COMMONS_CLI_JAR%
 
 set cp=.;%MULE_HOME%\conf
 set cp=%cp%;%GROOVY_PATH%;%COMMONS_CLI_PATH%
