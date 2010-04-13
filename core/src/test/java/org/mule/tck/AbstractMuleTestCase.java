0467f030305ce510a0c34116951d7a02c37f4273
epere4 <epere4>
2010-04-13 05:42:45 +0000

    Merged revisions 17028 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.2.x
    ........
      r17028 | epere4 | 2010-04-12 21:01:53 -0700 (Mon, 12 Apr 2010) | 2 lines
      
      This is an improvement on the handleTimeout() method. Now before killing the VM it attempts to interrupt the main thread of the test case. If the thread won't die then the VM is killed anyway, but if it does then the error reported by JUnit is much better. Without this fix maven would just report that there were test failures but you wouldn't know which test failed unless you scan the full stdio log.
      Full discussion in this thread: http://markmail.org/thread/xuuwzxroo63oyivx
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@17030 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/test/java/org/mule/tck/AbstractMuleTestCase.java
+++ core/src/test/java/org/mule/tck/AbstractMuleTestCase.java
@@ -174,6 +174,12 @@ public abstract class AbstractMuleTestCase extends TestCase implements TestCaseW
      */
     private boolean failOnTimeout = true;
 
+    /**
+     * This is just a reference to the main thread running the current thread. It is
+     * set in the {@link #setUp()} method.
+     */
+    private volatile Thread currentTestRunningThread;
+
     public AbstractMuleTestCase()
     {
         super();
@@ -350,16 +356,37 @@ public abstract class AbstractMuleTestCase extends TestCase implements TestCaseW
 
     public void handleTimeout(long timeout, TimeUnit unit)
     {
-        String msg = "Timeout of " + unit.toMillis(timeout) + "ms exceeded - exiting VM! (modify via -Dmule.test.timeoutSecs=XX)";
+        String msg = "Timeout of " + unit.toMillis(timeout) + "ms exceeded (modify via -Dmule.test.timeoutSecs=XX)";
 
         if (failOnTimeout)
         {
-            logger.fatal(msg);
-            Runtime.getRuntime().halt(1);
+            logger.fatal(msg + " - Attempting to interrupt thread for test " + this.getName());
+            if (currentTestRunningThread != null)
+            {
+                currentTestRunningThread.interrupt();
+            }
+            giveTheTestSomeTimeToCleanUpAndThenKillIt("Interrupting didn't work. Killing the VM!. Test "
+                                                      + this.getName() + " did not finish correctly.");
         }
         else
         {
             logger.warn(msg);
+        }
+    }
+
+    private void giveTheTestSomeTimeToCleanUpAndThenKillIt(String messageIfNeedToKill)
+    {
+        try
+        {
+            Thread.sleep(5000);
+            logger.fatal(messageIfNeedToKill);
+            Runtime.getRuntime().halt(1);
+        }
+        catch (InterruptedException e)
+        {
+            logger.info(
+                "someone interrupted the thread, probable the call to watchdog.cancel() in teardown method.",
+                e);
         }
     }
 
@@ -375,6 +402,8 @@ public abstract class AbstractMuleTestCase extends TestCase implements TestCaseW
         // start a watchdog thread
         watchdog = createWatchdog();
         watchdog.start();
+
+        currentTestRunningThread = Thread.currentThread();
 
         if (verbose)
         {
