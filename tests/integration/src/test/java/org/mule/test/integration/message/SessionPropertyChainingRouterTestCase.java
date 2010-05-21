88d2c9d4d4a6ec1bd35b858535c0181a4cfbdec9
aperepel@gmail.com <Andrew Perepelytsya>
2010-05-21 15:59:22 +0000

    Duplicate license header removed
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@17366 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/message/SessionPropertyChainingRouterTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/message/SessionPropertyChainingRouterTestCase.java
@@ -15,16 +15,6 @@ import org.mule.module.client.MuleClient;
 import org.mule.tck.FunctionalTestCase;
 import org.mule.tck.NonSerializableObject;
 
-/*
- * $Id$
- * --------------------------------------------------------------------------------------
- * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
- *
- * The software in this package is published under the terms of the CPAL v1.0
- * license, a copy of which has been included with this distribution in the
- * LICENSE.txt file.
- */
-
 /**
  * @see EE-1821
  */
