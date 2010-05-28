29c0f5f7d539f45b60573b3988c2d2c91af11eab
aperepel@gmail.com <Andrew Perepelytsya>
2010-05-28 18:48:07 +0000

    License headers
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@17419 bf997673-6b11-0410-b953-e057580c5b09

--- modules/launcher/src/main/java/org/mule/module/launcher/MultiApplication.java
+++ modules/launcher/src/main/java/org/mule/module/launcher/MultiApplication.java
@@ -1,3 +1,13 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.module.launcher;
 
 import org.mule.api.MuleContext;
@@ -6,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * TODO class probably should go
  * Deploys multiple applications.
  */
 public class MultiApplication implements Application
