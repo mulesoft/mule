67de27bd3d21030750031b866fb82fe0b65b7e64
tcarlson <tcarlson>
2010-06-23 13:39:51 +0000

    Unit tests for configuring MPs on endpoints (BL-23)
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@17699 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/test/java/org/mule/endpoint/inbound/InboundEndpointMessageProcessorsTestCase.java
@@ -0,0 +1,93 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.endpoint.inbound;
+
+import org.mule.DefaultMuleEvent;
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.api.MuleMessage;
+import org.mule.api.endpoint.ImmutableEndpoint;
+import org.mule.api.endpoint.InboundEndpoint;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.processor.builder.ChainMessageProcessorBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Unit test for configuring message processors on an inbound endpoint.
+ */
+public class InboundEndpointMessageProcessorsTestCase extends AbstractInboundMessageProcessorTestCase
+{
+    private static final String TEST_MESSAGE = "test";
+
+    private InboundEndpoint endpoint;
+    private MuleMessage inMessage;
+    private MuleEvent requestEvent;
+    private MuleEvent result;
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        super.doSetUp();
+        inMessage = createTestRequestMessage();
+        endpoint = createTestInboundEndpoint(null, null, null, null, true, null);
+        requestEvent = createTestRequestEvent(endpoint);
+    }
+
+    public void testProcessors() throws Exception
+    {
+        ChainMessageProcessorBuilder builder = new ChainMessageProcessorBuilder();
+        builder.chain(new LabelMessageProcessor("1"), new LabelMessageProcessor("2"), new LabelMessageProcessor("3"));
+        MessageProcessor mpChain = builder.build();
+        
+        result = mpChain.process(requestEvent);
+        assertEquals(TEST_MESSAGE + ":1:2:3", result.getMessage().getPayload());
+    }
+
+    public void testNoProcessors() throws Exception
+    {
+        ChainMessageProcessorBuilder builder = new ChainMessageProcessorBuilder();
+        MessageProcessor mpChain = builder.build();
+        
+        result = mpChain.process(requestEvent);
+        assertEquals(TEST_MESSAGE, result.getMessage().getPayload());
+    }
+
+    protected MuleMessage createTestRequestMessage()
+    {
+        Map<String, Object> props = new HashMap<String, Object>();
+        props.put("prop1", "value1");
+        return new DefaultMuleMessage(TEST_MESSAGE, props, muleContext);
+    }
+
+    protected MuleEvent createTestRequestEvent(ImmutableEndpoint endpoint) throws Exception
+    {
+        return new DefaultMuleEvent(inMessage, endpoint, getTestSession(getTestService(), muleContext), endpoint.isSynchronous());
+    }
+    
+    class LabelMessageProcessor implements MessageProcessor
+    {
+        String label;
+        
+        public LabelMessageProcessor(String label)
+        {
+            this.label = label;
+        }
+        
+        public MuleEvent process(MuleEvent event) throws MuleException
+        {
+            event.getMessage().setPayload(event.getMessage().getPayload() + ":" + label);
+            return event;
+        }
+    }
+}
---
+++ core/src/test/java/org/mule/endpoint/outbound/OutboundEndpointMessageProcessorsTestCase.java
@@ -0,0 +1,88 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.endpoint.outbound;
+
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.api.endpoint.OutboundEndpoint;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.api.routing.filter.Filter;
+import org.mule.api.security.EndpointSecurityFilter;
+import org.mule.api.transaction.TransactionConfig;
+import org.mule.api.transformer.Transformer;
+import org.mule.api.transport.Connector;
+import org.mule.processor.builder.ChainMessageProcessorBuilder;
+
+/**
+ * Unit test for configuring message processors on an outbound endpoint.
+ */
+public class OutboundEndpointMessageProcessorsTestCase extends AbstractOutboundMessageProcessorTestCase
+{
+    private MuleEvent testOutboundEvent;
+    private OutboundEndpoint endpoint;
+    private MuleEvent result;
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        super.doSetUp();
+        endpoint = createOutboundEndpoint(null, null, null, null, true, null);
+        testOutboundEvent = createTestOutboundEvent(endpoint, true);
+    }
+
+    public void testProcessors() throws Exception
+    {
+        ChainMessageProcessorBuilder builder = new ChainMessageProcessorBuilder();
+        builder.chain(new LabelMessageProcessor("1"), new LabelMessageProcessor("2"), new LabelMessageProcessor("3"));
+        MessageProcessor mpChain = builder.build();
+        
+        result = mpChain.process(testOutboundEvent);
+        assertEquals(TEST_MESSAGE + ":1:2:3", result.getMessage().getPayload());
+    }
+
+    public void testNoProcessors() throws Exception
+    {
+        ChainMessageProcessorBuilder builder = new ChainMessageProcessorBuilder();
+        MessageProcessor mpChain = builder.build();
+        
+        result = mpChain.process(testOutboundEvent);
+        assertEquals(TEST_MESSAGE, result.getMessage().getPayload());
+    }
+
+    protected OutboundEndpoint createOutboundEndpoint(Filter filter,
+                                                      EndpointSecurityFilter securityFilter,
+                                                      Transformer in,
+                                                      Transformer response,
+                                                      boolean sync,
+                                                      TransactionConfig txConfig) throws Exception
+    {
+        OutboundEndpoint endpoint = createTestOutboundEndpoint(filter, securityFilter, in, response, sync, txConfig);
+        Connector connector = endpoint.getConnector();
+        connector.start();
+        return endpoint;
+    }
+
+    class LabelMessageProcessor implements MessageProcessor
+    {
+        String label;
+        
+        public LabelMessageProcessor(String label)
+        {
+            this.label = label;
+        }
+        
+        public MuleEvent process(MuleEvent event) throws MuleException
+        {
+            event.getMessage().setPayload(event.getMessage().getPayload() + ":" + label);
+            return event;
+        }
+    }
+}
