a51daa93bbc136f8a086fa099e50838d0a07deb0
Pablo Kraan <pablo.kraan@gmail.com>
2010-06-29 10:55:51 +0000

    Merged revisions 17727 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.2.x
    ........
      r17727 | pablo.kraan | 2010-06-27 18:49:45 -0300 (Sun, 27 Jun 2010) | 1 line
      
      MULE-3478: sets encoding in the MuleMessage created in the FtpMessageReceiver
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@17745 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/ftp/src/test/java/org/mule/transport/ftp/FtpEncodingFunctionalTestCase.java
@@ -0,0 +1,68 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.ftp;
+
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.util.FileUtils;
+
+import java.io.File;
+
+public class FtpEncodingFunctionalTestCase extends AbstractFtpServerTestCase
+{
+
+    private static final String TEST_MESSAGE_EUC_JP_ENCODED = "\u3053";
+    private static final int FIVE_SECONDS_TIMEOUT = 5000;
+    private static final String ENCODING = "EUC-JP";
+    private static final int FTP_SERVER_PORT = 60200;
+
+    private File testDataFile;
+
+    public FtpEncodingFunctionalTestCase()
+    {
+        super(FTP_SERVER_PORT);
+    }
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "ftp-encoding-functional-config.xml";
+    }
+
+    public void testReadingFileWithEucJpEncodingGetsTheRightText() throws Exception
+    {
+        File tmpDir = new File(FTP_SERVER_BASE_DIR);
+        testDataFile = createDataFile(tmpDir, ENCODING, TEST_MESSAGE_EUC_JP_ENCODED);
+
+        MuleClient client = new MuleClient(muleContext);
+        MuleMessage message = client.request("vm://receive", FIVE_SECONDS_TIMEOUT);
+
+        assertNotNull(message);
+        assertEquals(ENCODING, message.getEncoding());
+        assertEquals(TEST_MESSAGE_EUC_JP_ENCODED, message.getPayloadAsString());
+    }
+
+    private File createDataFile(File folder, String encoding, final String testMessage) throws Exception
+    {
+        File target = File.createTempFile("mule-file-test-", ".txt", folder);
+        target.deleteOnExit();
+        FileUtils.writeStringToFile(target, testMessage, encoding);
+
+        return target;
+    }
+
+    @Override
+    protected void doTearDown() throws Exception
+    {
+        super.doTearDown();
+        FileUtils.deleteTree(testDataFile);
+    }
+}
