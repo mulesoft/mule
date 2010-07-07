a0d2803fb5f2d02cce028067d652dbd97d2045c7
tcarlson <tcarlson>
2010-07-07 15:34:32 +0000

    BL-116 Update for Hot Deployment
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@17948 bf997673-6b11-0410-b953-e057580c5b09

--- examples/stockquote/dist/stockquote.bat
+++ examples/stockquote/dist/stockquote.bat
@@ -3,7 +3,7 @@ setlocal
 if "%MULE_HOME%" == "" SET MULE_HOME=%~dp0..\..
 if "%MULE_BASE%" == "" SET MULE_BASE=%MULE_HOME%
 
-if exist "%MULE_BASE%\apps\mule-example-stockquote" goto :run
+if exist "%MULE_BASE%\apps\stockquote-rest" goto :run
 echo This example requires you to build it before running it.  Please follow the instructions in the README.txt file.
 goto :eof
 
@@ -16,6 +16,9 @@ ECHO   2. WSDL
 ECHO   3. SOAP
 SET /P Choice=Select the one you wish to execute and press Enter...
 
-IF '%Choice%'=='1' call "%MULE_BASE%\bin\mule.bat" -config "%MULE_BASE%\apps\mule-example-stockquote\stockquote-rest-config.xml,%MULE_BASE%\apps\mule-example-stockquote\stdio-config.xml"
-IF '%Choice%'=='2' call "%MULE_BASE%\bin\mule.bat" -config "%MULE_BASE%\apps\mule-example-stockquote\stockquote-wsdl-config.xml,%MULE_BASE%\apps\mule-example-stockquote\stdio-config.xml"
-IF '%Choice%'=='3' call "%MULE_BASE%\bin\mule.bat" -config "%MULE_BASE%\apps\mule-example-stockquote\stockquote-soap-config.xml,%MULE_BASE%\apps\mule-example-stockquote\stdio-config.xml"
+IF '%Choice%'=='1' call "%MULE_BASE%\bin\mule.bat" -config
+"%MULE_BASE%\bin\mule.bat" -app stockquote-rest
+IF '%Choice%'=='2' call "%MULE_BASE%\bin\mule.bat" -config 
+"%MULE_BASE%\bin\mule.bat" -app stockquote-wsdl
+IF '%Choice%'=='3' call "%MULE_BASE%\bin\mule.bat" -config 
+"%MULE_BASE%\bin\mule.bat" -app stockquote-soap
