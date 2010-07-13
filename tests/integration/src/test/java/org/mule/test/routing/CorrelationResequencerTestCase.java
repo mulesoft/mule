7f24d6753901711d0647d5379cafa09036d93e4f
Dirk Olmes <dirk.olmes@mulesoft.com>
2010-07-13 13:49:29 +0000

    rewrite the test to use a latch instead of waiting for incoming messages.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@18182 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/routing/CorrelationResequencerTestCase.java
+++ tests/integration/src/test/java/org/mule/test/routing/CorrelationResequencerTestCase.java
@@ -10,17 +10,37 @@
 
 package org.mule.test.routing;
 
+import org.mule.api.MuleEventContext;
 import org.mule.module.client.MuleClient;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.tck.functional.EventCallback;
 import org.mule.tck.functional.FunctionalTestComponent;
 
 import java.util.Arrays;
 
+import edu.emory.mathcs.backport.java.util.concurrent.CountDownLatch;
+import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
+
 public class CorrelationResequencerTestCase extends FunctionalTestCase
-{
+{    
+    CountDownLatch receiveLatch = new CountDownLatch(6);
+    
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        super.doSetUp();
+        
+        FunctionalTestComponent testComponent = getFunctionalTestComponent("test validator");
+        testComponent.setEventCallback(new EventCallback()
+        {
+            public void eventReceived(MuleEventContext context, Object component) throws Exception
+            {
+                receiveLatch.countDown();                
+            }
+        });
+    }
 
-    private int timeout = getTestTimeoutSecs() / 20 * 1000;
-
+    @Override
     protected String getConfigResources()
     {
         return "correlation-resequencer-test.xml";
@@ -29,11 +49,11 @@ public class CorrelationResequencerTestCase extends FunctionalTestCase
     public void testResequencer() throws Exception
     {
         MuleClient client = new MuleClient(muleContext);
-        client.send("vm://splitter", Arrays.asList("a", "b", "c", "d", "e", "f"), null);
+        client.dispatch("vm://splitter", Arrays.asList("a", "b", "c", "d", "e", "f"), null);
 
         FunctionalTestComponent resequencer = getFunctionalTestComponent("test validator");
 
-        Thread.sleep(timeout);
+        assertTrue(receiveLatch.await(30, TimeUnit.SECONDS));
 
         assertEquals("Wrong number of messages received.", 6, resequencer.getReceivedMessagesCount());
         assertEquals("Sequence wasn't reordered.", "a", resequencer.getReceivedMessage(1));
