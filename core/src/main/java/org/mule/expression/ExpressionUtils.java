fc20f6d3352962f04552a04e4f63dbd83378fb22
Dirk Olmes <dirk.olmes@mulesoft.com>
2010-07-14 07:54:28 +0000

    fix license header
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@18204 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/expression/ExpressionUtils.java
+++ core/src/main/java/org/mule/expression/ExpressionUtils.java
@@ -1,3 +1,13 @@
+/*
+ * $Id: FunctionExpressionEvaluator.java 18005 2010-07-09 16:47:06Z aperepel $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.expression;
 
 import org.mule.api.MuleMessage;
