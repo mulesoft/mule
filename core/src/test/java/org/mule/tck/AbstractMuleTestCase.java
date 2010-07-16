b38351a81ac1cf6e2da6b27872a9841dd1ea677e
Daniel Feist <dfeist@gmail.com>
2010-07-16 16:59:47 +0000

    Make client interface for MuleContext available in test cases
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@18248 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/test/java/org/mule/tck/AbstractMuleTestCase.java
+++ core/src/test/java/org/mule/tck/AbstractMuleTestCase.java
@@ -15,6 +15,7 @@ import org.mule.api.MuleContext;
 import org.mule.api.MuleEvent;
 import org.mule.api.MuleEventContext;
 import org.mule.api.MuleSession;
+import org.mule.api.client.LocalMuleClient;
 import org.mule.api.config.ConfigurationBuilder;
 import org.mule.api.context.MuleContextBuilder;
 import org.mule.api.context.MuleContextFactory;
@@ -99,6 +100,8 @@ public abstract class AbstractMuleTestCase extends TestCase implements TestCaseW
 
 
     protected static MuleContext muleContext;
+
+    protected static LocalMuleClient client;
 
     /**
      * This flag controls whether the text boxes will be logged when starting each test case.
@@ -435,6 +438,7 @@ public abstract class AbstractMuleTestCase extends TestCase implements TestCaseW
             {
                 muleContext.start();
             }
+            client = muleContext.getClientInterface();
 
             doSetUp();
         }
