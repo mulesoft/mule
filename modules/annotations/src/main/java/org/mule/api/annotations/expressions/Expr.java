d6dfed99d01c7562f2704eb5aa6fa50efbef844c
aperepel@gmail.com <Andrew Perepelytsya>
2010-07-21 19:15:38 +0000

    Missing license header
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@18452 bf997673-6b11-0410-b953-e057580c5b09

--- modules/annotations/src/main/java/org/mule/api/annotations/expressions/Expr.java
+++ modules/annotations/src/main/java/org/mule/api/annotations/expressions/Expr.java
@@ -7,6 +7,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
+
 package org.mule.api.annotations.expressions;
 
 import org.mule.api.annotations.meta.Evaluator;
