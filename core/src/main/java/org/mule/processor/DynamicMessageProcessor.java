0711eed798dfd0e87e33985fe7d470e794094237
Daniel Feist <dfeist@gmail.com>
2010-07-29 12:05:00 +0000

    Introduce DynamicMessageProcessor
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@18621 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/main/java/org/mule/processor/DynamicMessageProcessor.java
@@ -0,0 +1,38 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.processor;
+
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.api.processor.MessageProcessor;
+
+/**
+ * Implementation of {@link MessageProcessor} that dynamically chooses and uses
+ * another {@link MessageProcessor}
+ */
+public abstract class DynamicMessageProcessor implements MessageProcessor
+{
+
+    public MuleEvent process(MuleEvent event) throws MuleException
+    {
+        return resolveMessageProcessor(event).process(event);
+    }
+
+    /**
+     * Determines which MessageProcessor should be used. Implementations may choose
+     * to use a message property, configure this
+     * 
+     * @param event
+     * @return
+     */
+    protected abstract MessageProcessor resolveMessageProcessor(MuleEvent event);
+
+}
