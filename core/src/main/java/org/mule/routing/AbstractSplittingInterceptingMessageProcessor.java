d363b254a762971f2c11da20f826fab3facd3660
Daniel Feist <dfeist@gmail.com>
2010-08-02 17:06:55 +0000

    Add abstract implementation and unit test case for SimpleSplitter
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@18722 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/main/java/org/mule/routing/AbstractSplittingInterceptingMessageProcessor.java
@@ -0,0 +1,108 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.routing;
+
+import org.mule.DefaultMuleEvent;
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleContext;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.api.MuleMessage;
+import org.mule.api.context.MuleContextAware;
+import org.mule.api.routing.RouterResultsHandler;
+import org.mule.processor.AbstractInterceptingMessageProcessor;
+import org.mule.routing.outbound.DefaultRouterResultsHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractSplittingInterceptingMessageProcessor extends
+    AbstractInterceptingMessageProcessor implements MuleContextAware
+{
+
+    protected MuleContext muleContext;
+    protected RouterResultsHandler resultsHandler = new DefaultRouterResultsHandler();
+    protected CorrelationMode enableCorrelation = CorrelationMode.IF_NOT_SET;
+
+    public MuleEvent process(MuleEvent event) throws MuleException
+    {
+        Object result = splitMessage(event);
+        if (result instanceof List)
+        {
+            return resultsHandler.aggregateResults(processParts((List) result, event), event, muleContext);
+        }
+        else
+        {
+            logger.warn("Splitter only returned a single result. If this is not expected, please check your split expression");
+            MuleEvent resultEvent;
+            if (result instanceof MuleMessage)
+            {
+                resultEvent = new DefaultMuleEvent((MuleMessage) result, event);
+            }
+            else
+            {
+                resultEvent = new DefaultMuleEvent(new DefaultMuleMessage(result, muleContext), event);
+            }
+            return processNext(event);
+        }
+    }
+
+    protected abstract Object splitMessage(MuleEvent event);
+
+    protected List<MuleEvent> processParts(List parts, MuleEvent event) throws MuleException
+    {
+        String correlationId = event.getFlowConstruct().getMessageInfoMapping().getCorrelationId(
+            event.getMessage());
+        List<MuleEvent> resultEvents = new ArrayList<MuleEvent>();
+        int correlationSequence = 1;
+
+        for (Object part : parts)
+        {
+            MuleMessage message;
+            if (part instanceof MuleMessage)
+            {
+                message = (MuleMessage) part;
+            }
+            else
+            {
+                message = new DefaultMuleMessage(part, muleContext);
+            }
+
+            if (enableCorrelation != CorrelationMode.NEVER)
+            {
+                boolean correlationSet = message.getCorrelationId() != null;
+                if ((!correlationSet && (enableCorrelation == CorrelationMode.IF_NOT_SET))
+                    || (enableCorrelation == CorrelationMode.ALWAYS))
+                {
+                    message.setCorrelationId(correlationId);
+                }
+
+                // take correlation group size from the message properties, set by concrete
+                // message splitter implementations
+                message.setCorrelationGroupSize(parts.size());
+                message.setCorrelationSequence(correlationSequence++);
+            }
+            resultEvents.add(processNext(new DefaultMuleEvent(message, event)));
+        }
+        return resultEvents;
+    }
+
+    public void setEnableCorrelation(CorrelationMode enableCorrelation)
+    {
+        this.enableCorrelation = enableCorrelation;
+    }
+
+    public void setMuleContext(MuleContext context)
+    {
+        this.muleContext = context;
+    }
+
+}
---
+++ core/src/test/java/org/mule/routing/SplittingInterceptingMessageProcessorTestCase.java
@@ -0,0 +1,87 @@
+/*
+ * $Id: IdempotentMessageProcessorTestCase.java 17050 2010-04-20 02:52:45Z dfeist $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.routing;
+
+import org.mule.DefaultMessageCollection;
+import org.mule.DefaultMuleEvent;
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.api.MuleMessageCollection;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.tck.AbstractMuleTestCase;
+import org.mule.tck.testmodels.fruit.Apple;
+import org.mule.tck.testmodels.fruit.Banana;
+import org.mule.tck.testmodels.fruit.Fruit;
+import org.mule.tck.testmodels.fruit.FruitBowl;
+import org.mule.tck.testmodels.fruit.Orange;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SplittingInterceptingMessageProcessorTestCase extends AbstractMuleTestCase
+{
+
+    public void testSimpleSplitter() throws Exception
+    {
+        TestSplitter splitter = new TestSplitter();
+        MultipleEventSensingMessageProcessor listener = new MultipleEventSensingMessageProcessor();
+        splitter.setListener(listener);
+        splitter.setMuleContext(muleContext);
+
+        Apple apple = new Apple();
+        Banana banana = new Banana();
+        Orange orange = new Orange();
+        FruitBowl fruitBowl = new FruitBowl();
+        fruitBowl.addFruit(apple);
+        fruitBowl.addFruit(banana);
+        fruitBowl.addFruit(orange);
+
+        MuleEvent inEvent = new DefaultMuleEvent(new DefaultMuleMessage(fruitBowl, muleContext),
+            getTestEvent(""));
+
+        MuleEvent resultEvent = splitter.process(inEvent);
+
+        assertEquals(3, listener.events.size());
+        assertTrue(listener.events.get(0).getMessage().getPayload() instanceof Fruit);
+        assertTrue(listener.events.get(1).getMessage().getPayload() instanceof Fruit);
+        assertTrue(listener.events.get(2).getMessage().getPayload() instanceof Fruit);
+
+        assertEquals(DefaultMessageCollection.class, resultEvent.getMessage().getClass());
+        assertEquals(3, ((MuleMessageCollection) resultEvent.getMessage()).size());
+        assertTrue(((MuleMessageCollection) resultEvent.getMessage()).getMessage(0).getPayload() instanceof Fruit);
+        assertTrue(((MuleMessageCollection) resultEvent.getMessage()).getMessage(1).getPayload() instanceof Fruit);
+        assertTrue(((MuleMessageCollection) resultEvent.getMessage()).getMessage(2).getPayload() instanceof Fruit);
+
+    }
+
+    private static class MultipleEventSensingMessageProcessor implements MessageProcessor
+    {
+        List<MuleEvent> events = new ArrayList<MuleEvent>();
+
+        public MuleEvent process(MuleEvent event) throws MuleException
+        {
+            events.add(event);
+            return event;
+        }
+    }
+
+    private static class TestSplitter extends AbstractSplittingInterceptingMessageProcessor
+    {
+        @Override
+        protected Object splitMessage(MuleEvent event)
+        {
+            FruitBowl bowl = (FruitBowl) event.getMessage().getPayload();
+            return bowl.getFruit();
+        }
+    }
+
+}
