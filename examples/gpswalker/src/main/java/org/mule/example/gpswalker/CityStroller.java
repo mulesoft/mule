d714e4103934a83c44b168a1937cd388bbdc6458
Ross Mason <ross@mulesoft.com>
2010-08-04 23:05:27 +0000

    Update the gps walker to use new features. 1) no need to create a war, serve files directly from Mule. 2) Use Json binding support on the Coordinates object. 3) Use the @Schedule annotation on the CityStroller. 4) allow the example to be run straight from Maven. All goodness
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@18818 bf997673-6b11-0410-b953-e057580c5b09

--- examples/gpswalker/src/main/java/org/mule/example/gpswalker/CityStroller.java
+++ examples/gpswalker/src/main/java/org/mule/example/gpswalker/CityStroller.java
@@ -9,6 +9,8 @@
  */
 package org.mule.example.gpswalker;
 
+import org.mule.api.annotations.Schedule;
+
 import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -26,6 +28,7 @@ public class CityStroller
     private AtomicBoolean firstTime = new AtomicBoolean(true);
 
     //could use a better algorithm here or real test data for better results
+    @Schedule(interval = 3000, startDelay = 3000)
     public GpsCoord generateNextCoord()
     {
         if (firstTime.get())
--- examples/gpswalker/src/main/java/org/mule/example/gpswalker/GpsCoord.java
+++ examples/gpswalker/src/main/java/org/mule/example/gpswalker/GpsCoord.java
@@ -10,12 +10,18 @@
 
 package org.mule.example.gpswalker;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+
 /**
  * A bean that holds Geo Coordinates
+ * Note that this bean is annotated with Jackson Json annotations.  This allows Mule to automically
+ * serialise this object to and from JSON.
  */
 public class GpsCoord
 {
+    @JsonProperty
     private float latitude;
+    @JsonProperty
     private float longitude;
 
     public GpsCoord(float lat, float lng)
