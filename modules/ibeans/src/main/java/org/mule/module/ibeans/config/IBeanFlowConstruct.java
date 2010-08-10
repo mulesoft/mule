da22a95b7d4ec82157dca1193eed0fd793ffd349
David Dossot <david@dossot.net>
2010-08-10 00:10:06 +0000

    fixed compilation error
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@18906 bf997673-6b11-0410-b953-e057580c5b09

--- modules/ibeans/src/main/java/org/mule/module/ibeans/config/IBeanFlowConstruct.java
+++ modules/ibeans/src/main/java/org/mule/module/ibeans/config/IBeanFlowConstruct.java
@@ -7,19 +7,22 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
+
 package org.mule.module.ibeans.config;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.mule.api.MuleContext;
+import org.mule.api.processor.MessageProcessor;
 import org.mule.construct.SimpleFlowConstruct;
 
-import java.util.Collections;
-
 /**
- * This is an empty flow construct that is used to host an iBean as a component with one or more component bindings.
- *
- * Each method annotated with {@link org.ibeans.annotation.Call} or {@link org.ibeans.annotation.Template} has an associated
- * component binding associated with it.
- *
+ * This is an empty flow construct that is used to host an iBean as a component with
+ * one or more component bindings. Each method annotated with
+ * {@link org.ibeans.annotation.Call} or {@link org.ibeans.annotation.Template} has
+ * an associated component binding associated with it.
+ * 
  * @see org.mule.module.ibeans.config.CallInterfaceBinding
  */
 public class IBeanFlowConstruct extends SimpleFlowConstruct
@@ -27,6 +30,7 @@ public class IBeanFlowConstruct extends SimpleFlowConstruct
     public IBeanFlowConstruct(String name, MuleContext muleContext)
     {
         super(name, muleContext);
-        setMessageProcessors(Collections.emptyList());
+        final List<MessageProcessor> messageProcessors = Collections.emptyList();
+        setMessageProcessors(messageProcessors);
     }
 }
