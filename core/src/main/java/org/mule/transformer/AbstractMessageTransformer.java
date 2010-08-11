0231be0be707e9d5c1bb4bae15e8a97b6f222aa1
Ross Mason <ross@mulesoft.com>
2010-08-11 16:07:25 +0000

    correct company name
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@18952 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/transformer/AbstractMessageTransformer.java
+++ core/src/main/java/org/mule/transformer/AbstractMessageTransformer.java
@@ -1,7 +1,7 @@
 /*
  * $Id$
- * --------------------------------------------------------------------------------------
- * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ * -------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
  *
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
@@ -15,7 +15,6 @@ import org.mule.api.MuleEvent;
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.DataType;
 import org.mule.api.transformer.MessageTransformer;
-import org.mule.api.transformer.Transformer;
 import org.mule.api.transformer.TransformerException;
 import org.mule.api.transformer.TransformerMessagingException;
 import org.mule.config.i18n.CoreMessages;
