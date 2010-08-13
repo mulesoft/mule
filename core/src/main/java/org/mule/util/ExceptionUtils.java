aed5e91743f133cf1f7a14a27e1f18af879fbca0
tcarlson <tcarlson>
2010-08-13 14:39:04 +0000

    Merged revisions 17818,17820 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.2.x
    ........
      r17818 | epere4 | 2010-07-01 19:12:46 -0300 (Thu, 01 Jul 2010) | 3 lines
      
      MULE-4927: Exception strategy invoked both for connector *and* service exception strategies when exception on sync outbound endpoint
      Now, when the dispatchToOutboundRouter() method gets an exception from the outbound router and that exception is a DispatchException then we assume that it was handled by the connector-exception-strategy so we ignore it. Otherwise, we rethrow it.
      I had to add a method in ExceptionUtils to detect if the exception was caused by a DispatchException (because it is wrapped by other exceptions, so I cannot simply use instanceof).
    ........
      r17820 | epere4 | 2010-07-01 21:32:45 -0300 (Thu, 01 Jul 2010) | 2 lines
      
      MULE-4927: Exception strategy invoked both for connector *and* service exception strategies when exception on sync outbound endpoint
      The test was not extending AbstractMuleTestCase.
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@19000 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/util/ExceptionUtils.java
+++ core/src/main/java/org/mule/util/ExceptionUtils.java
@@ -15,7 +15,20 @@ package org.mule.util;
  */
 public class ExceptionUtils extends org.apache.commons.lang.exception.ExceptionUtils
 {
-    // nop
+
+    /**
+     * This method returns true if the throwable contains a {@link Throwable} that
+     * matches the specified class or subclass in the exception chain. Subclasses of
+     * the specified class do match.
+     * 
+     * @param throwable the throwable to inspect, may be null
+     * @param type the type to search for, subclasses match, null returns false
+     * @return the index into the throwable chain, false if no match or null input
+     */
+    public static boolean containsType(Throwable throwable, Class<?> type)
+    {
+        return indexOfType(throwable, type) > -1;
+    }
 }
 
 
---
+++ core/src/test/java/org/mule/util/ExceptionUtilsTestCase.java
@@ -0,0 +1,43 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.util;
+
+import static org.mule.util.ExceptionUtils.containsType;
+
+import org.mule.tck.AbstractMuleTestCase;
+
+import java.io.IOException;
+
+public class ExceptionUtilsTestCase extends AbstractMuleTestCase
+{
+
+    public void testContainsType()
+    {
+        assertTrue(containsType(new IllegalArgumentException(), IllegalArgumentException.class));
+
+        assertTrue(containsType(new Exception(new IllegalArgumentException()), IllegalArgumentException.class));
+
+        assertTrue(containsType(new Exception(new IllegalArgumentException(new NullPointerException())),
+            NullPointerException.class));
+
+        assertTrue(containsType(new Exception(new IllegalArgumentException(new NullPointerException())),
+            RuntimeException.class));
+
+        assertTrue(containsType(new Exception(new IllegalArgumentException(new NullPointerException())),
+            Exception.class));
+
+        assertFalse(containsType(new Exception(new IllegalArgumentException(new NullPointerException())),
+            IOException.class));
+    }
+
+}
+
+
