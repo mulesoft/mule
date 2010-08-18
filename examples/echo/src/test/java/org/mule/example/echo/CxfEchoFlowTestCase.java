e4ca261d0a281d85e00ebedf5e685b70a120b86d
Ross Mason <ross@mulesoft.com>
2010-08-18 18:34:12 +0000

    use MuleSoft
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@19093 bf997673-6b11-0410-b953-e057580c5b09

--- examples/echo/src/test/java/org/mule/example/echo/CxfEchoFlowTestCase.java
+++ examples/echo/src/test/java/org/mule/example/echo/CxfEchoFlowTestCase.java
@@ -1,7 +1,7 @@
 /*
- * $Id: CxfEchoTestCase.java 17313 2010-05-18 21:09:50Z aperepel $
- * --------------------------------------------------------------------------------------
- * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ * $Id:  $
+ * -------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
  *
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
