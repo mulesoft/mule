5bbe46ebc0e4804a20c997c24d97094f277ad50c
Daniel Feist <dfeist@gmail.com>
2010-08-18 21:21:39 +0000

    Minor modification to API of unused method
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@19107 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/api/processor/MessageRouter.java
+++ core/src/main/java/org/mule/api/processor/MessageRouter.java
@@ -31,6 +31,6 @@ public interface MessageRouter extends MessageProcessor
      * @param processor destination message processor to remove
      * @return true if the route was removed
      */
-    boolean removeRoute(MessageProcessor processor);
+    void removeRoute(MessageProcessor processor);
 
 }
