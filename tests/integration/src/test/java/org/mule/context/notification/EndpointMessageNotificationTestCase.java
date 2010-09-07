219d4ede178ab806e2c60579f0da5267c073e210
David Dossot <david@dossot.net>
2010-09-07 17:55:04 +0000

    fixed and reactivated EndpointMessageNotificationTestCase
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@19397 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/context/notification/EndpointMessageNotificationTestCase.java
+++ tests/integration/src/test/java/org/mule/context/notification/EndpointMessageNotificationTestCase.java
@@ -15,34 +15,49 @@ import org.mule.module.client.MuleClient;
 public class EndpointMessageNotificationTestCase extends AbstractNotificationTestCase
 {
 
-    public static final String SERVICE_1 = "service-1";
-    public static final String SERVICE_2 = "service-2";
-    public static final String CLIENT = null; // client id is null
+    public static final String NO_ID = null;
+    public static final String SERVICE_1_ID = "service-1";
+    public static final String SERVICE_2_ID = "service-2";
+    public static final String CLIENT_ID = "MuleClient";
 
+    @Override
     protected String getConfigResources()
     {
         return "org/mule/test/integration/notifications/endpoint-message-notification-test.xml";
     }
 
+    @Override
     public void doTest() throws Exception
     {
-        MuleClient client = new MuleClient(muleContext);
+        final MuleClient client = new MuleClient(muleContext);
         assertNotNull(client.send("vm://in-1?connector=direct", "hello sweet world", null));
         client.dispatch("vm://in-2?connector=direct", "goodbye cruel world", null);
         assertNotNull(client.request("vm://out-2?connector=queue", 5000));
     }
 
+    @Override
     public RestrictedNode getSpecification()
     {
-        return new Node()
-                .parallel(new Node(EndpointMessageNotification.class, EndpointMessageNotification.MESSAGE_SENT, CLIENT))
-                .parallel(new Node(EndpointMessageNotification.class, EndpointMessageNotification.MESSAGE_RECEIVED, SERVICE_1))
-                .parallel(new Node(EndpointMessageNotification.class, EndpointMessageNotification.MESSAGE_DISPATCHED, CLIENT))
-                .parallel(new Node(EndpointMessageNotification.class, EndpointMessageNotification.MESSAGE_RECEIVED, SERVICE_2))
-                .parallel(new Node(EndpointMessageNotification.class, EndpointMessageNotification.MESSAGE_DISPATCHED, SERVICE_2))
-                .parallel(new Node(EndpointMessageNotification.class, EndpointMessageNotification.MESSAGE_REQUESTED, CLIENT));
+        return new Node().parallel(
+            new Node(EndpointMessageNotification.class, EndpointMessageNotification.MESSAGE_SENT, CLIENT_ID))
+            .parallel(
+                new Node(EndpointMessageNotification.class, EndpointMessageNotification.MESSAGE_RECEIVED,
+                    SERVICE_1_ID))
+            .parallel(
+                new Node(EndpointMessageNotification.class, EndpointMessageNotification.MESSAGE_DISPATCHED,
+                    CLIENT_ID))
+            .parallel(
+                new Node(EndpointMessageNotification.class, EndpointMessageNotification.MESSAGE_RECEIVED,
+                    SERVICE_2_ID))
+            .parallel(
+                new Node(EndpointMessageNotification.class, EndpointMessageNotification.MESSAGE_DISPATCHED,
+                    SERVICE_2_ID))
+            .parallel(
+                new Node(EndpointMessageNotification.class, EndpointMessageNotification.MESSAGE_REQUESTED,
+                    NO_ID)); // a request notification bears no resource ID
     }
 
+    @Override
     public void validateSpecification(RestrictedNode spec) throws Exception
     {
         verifyAllNotifications(spec, EndpointMessageNotification.class,
