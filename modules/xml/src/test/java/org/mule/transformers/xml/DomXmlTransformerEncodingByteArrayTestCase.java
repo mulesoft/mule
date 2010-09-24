7d5001bbf92e56a0d5914eded925438f98b2b0f7
Dirk Olmes <dirk.olmes@mulesoft.com>
2010-09-24 14:08:46 +0000

    Merged revisions 19711 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.0.x
    ........
      r19711 | dirk.olmes | 2010-09-24 06:12:19 +0200 (Fri, 24 Sep 2010) | 1 line
      
      MULE-4957 (Patch for byte array transformation test cases)
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@19712 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/xml/src/test/java/org/mule/transformers/xml/DomXmlTransformerEncodingByteArrayTestCase.java
@@ -0,0 +1,80 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transformers.xml;
+
+import org.mule.api.transformer.Transformer;
+import org.mule.module.xml.transformer.XmlToDomDocument;
+import org.mule.transformer.types.DataTypeFactory;
+import org.mule.util.IOUtils;
+
+import java.io.InputStream;
+
+import org.dom4j.DocumentHelper;
+import org.dom4j.io.DOMWriter;
+
+public class DomXmlTransformerEncodingByteArrayTestCase extends DomXmlTransformerEncodingTestCase
+{
+    private byte[] srcData; // Parsed XML doc
+    private String resultData; // String as US-ASCII
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+    	InputStream resourceStream = IOUtils.getResourceAsStream("cdcatalog-utf-8.xml", getClass());
+        srcData = IOUtils.toString(resourceStream, "UTF-8").getBytes("UTF-8");
+        
+        resourceStream = IOUtils.getResourceAsStream("cdcatalog-us-ascii.xml", getClass());
+        resultData = IOUtils.toString(resourceStream, "US-ASCII");
+    }
+    
+    @Override
+    public Transformer getRoundTripTransformer() throws Exception
+    {
+        XmlToDomDocument trans = createObject(XmlToDomDocument.class); // encoding is not interesting
+        trans.setReturnDataType(DataTypeFactory.create(byte[].class));
+        return trans;
+    }
+
+    @Override
+    public Object getTestData()
+    {
+        return srcData;
+    }
+
+    @Override
+    public Object getResultData()
+    {
+        return resultData;
+    }
+
+    @Override
+    public boolean compareResults(Object expected, Object result)
+    {
+        try
+        {
+            // This is only used during roundtrip test, so it will always be byte[] instances
+            if (expected instanceof byte[])
+            {
+                org.dom4j.Document dom4jDoc = null;
+                dom4jDoc = DocumentHelper.parseText(new String((byte[])expected, "UTF-8"));
+                expected = new DOMWriter().write(dom4jDoc);
+                dom4jDoc = DocumentHelper.parseText(new String((byte[])result, "UTF-8"));
+                result = new DOMWriter().write(dom4jDoc);
+            }
+        }
+        catch (Exception ex)
+        {
+            // ignored.
+        }
+
+        return super.compareResults(expected, result);
+    }
+}
---
+++ modules/xml/src/test/java/org/mule/transformers/xml/xquery/InlineXQueryTransformerByteArrayTestCase.java
@@ -0,0 +1,45 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transformers.xml.xquery;
+
+import org.mule.util.IOUtils;
+
+import java.io.InputStream;
+
+import org.custommonkey.xmlunit.XMLUnit;
+
+public class InlineXQueryTransformerByteArrayTestCase extends InlineXQueryTransformerTestCase
+{
+    private byte[] srcData;
+    private String resultData;
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        XMLUnit.setIgnoreWhitespace(true);
+        srcData = IOUtils.toByteArray(IOUtils.getResourceAsStream("cdcatalog-utf-8.xml", getClass()));
+        
+        InputStream resourceStream = IOUtils.getResourceAsStream("cdcatalog-result-utf-8.xml", getClass());
+        resultData = new String(IOUtils.toByteArray(resourceStream), "UTF-8");
+    }
+
+    @Override
+    public Object getTestData()
+    {
+        return srcData;
+    }
+
+    @Override
+    public Object getResultData()
+    {
+        return resultData;
+    }
+}
