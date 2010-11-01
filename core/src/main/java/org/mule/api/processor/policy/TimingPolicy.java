79d6f8cb6f0a573606e4b22cc2580d9008a4bbb7
aperepel@gmail.com <Andrew Perepelytsya>
2010-11-01 14:51:26 +0000

    A rudimentary timing policy, mostly serving as an example (may remove from the code later)
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@20047 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/main/java/org/mule/api/processor/policy/TimingPolicy.java
@@ -0,0 +1,46 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.api.processor.policy;
+
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ *
+ */
+public class TimingPolicy implements AroundPolicy
+{
+
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    public String getName()
+    {
+        return "simple timing policy";
+    }
+
+    public MuleEvent invoke(PolicyInvocation invocation) throws MuleException
+    {
+        final MuleEvent invocationEvent = invocation.getEvent();
+        long startTime = System.currentTimeMillis();
+
+        final MuleEvent result = invocation.proceed();
+
+        long executionTime = System.currentTimeMillis() - startTime;
+        logger.info(String.format("%s took %dms to process event [%s]",
+                                      invocationEvent.getFlowConstruct().getName(),
+                                      executionTime, invocationEvent.getId()));
+
+        return result;
+    }
+}
