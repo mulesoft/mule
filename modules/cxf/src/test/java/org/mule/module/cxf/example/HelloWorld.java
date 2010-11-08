50aa82ea172597466d979afd8e419c41d193bac9
mike.schilling <mike.schilling>
2010-11-08 17:48:11 +0000

    Merged revisions 20119 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.0.x
    ........
      r20119 | mike.schilling | 2010-11-08 08:37:00 -0800 (Mon, 08 Nov 2010) | 1 line
      
      Add missing license headers
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@20122 bf997673-6b11-0410-b953-e057580c5b09

--- modules/cxf/src/test/java/org/mule/module/cxf/example/HelloWorld.java
+++ modules/cxf/src/test/java/org/mule/module/cxf/example/HelloWorld.java
@@ -1,8 +1,19 @@
+/*
+ * $Id: DefaultOutboundRouterCollection.java 19640 2010-09-13 22:00:05Z tcarlson $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.module.cxf.example;
 
 import javax.jws.WebService;
 
 @WebService
-public interface HelloWorld {
+public interface HelloWorld
+{
     String sayHi(String text);
 }
--- modules/cxf/src/test/java/org/mule/module/cxf/example/HelloWorldImpl.java
+++ modules/cxf/src/test/java/org/mule/module/cxf/example/HelloWorldImpl.java
@@ -1,12 +1,23 @@
+/*
+ * $Id: DefaultOutboundRouterCollection.java 19640 2010-09-13 22:00:05Z tcarlson $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.module.cxf.example;
 
 import javax.jws.WebService;
 
-@WebService(endpointInterface = "org.mule.module.cxf.example.HelloWorld",
-            serviceName = "HelloWorld")
-public class HelloWorldImpl implements HelloWorld {
+@WebService(endpointInterface = "org.mule.module.cxf.example.HelloWorld", serviceName = "HelloWorld")
+public class HelloWorldImpl implements HelloWorld
+{
 
-    public String sayHi(String text) {
+    public String sayHi(String text)
+    {
         return "Hello\u2297 " + text;
     }
 }
