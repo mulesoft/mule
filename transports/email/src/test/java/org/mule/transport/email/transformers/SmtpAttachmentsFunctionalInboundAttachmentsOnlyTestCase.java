1da25ebd45d638a968b5addc376e1932ff30855e
aperepel@gmail.com <Andrew Perepelytsya>
2010-11-12 14:07:04 +0000

    Fixed the license header to unblock the build
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@20185 bf997673-6b11-0410-b953-e057580c5b09

--- transports/email/src/test/java/org/mule/transport/email/transformers/SmtpAttachmentsFunctionalInboundAttachmentsOnlyTestCase.java
+++ transports/email/src/test/java/org/mule/transport/email/transformers/SmtpAttachmentsFunctionalInboundAttachmentsOnlyTestCase.java
@@ -1,9 +1,11 @@
-/**
- * Created by IntelliJ IDEA.
- * User: mike.schilling
- * Date: Oct 15, 2010
- * Time: 3:00:34 PM
- * To change this template use File | Settings | File Templates.
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
  */
 
 package org.mule.transport.email.transformers;
