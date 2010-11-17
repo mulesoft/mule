4c3d9db49de1b909e27d108f21c099c432531f07
aperepel@gmail.com <Andrew Perepelytsya>
2010-11-17 17:49:36 +0000

    More readable code
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@20220 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/util/ObjectUtils.java
+++ core/src/main/java/org/mule/util/ObjectUtils.java
@@ -339,7 +339,7 @@ public class ObjectUtils extends org.apache.commons.lang.ObjectUtils
         String str = obj.getClass().getName();
         if (obj instanceof NamedObject)
         {
-            str += " '" + ((NamedObject) obj).getName() + "'";
+            str += String.format(" '%s'", ((NamedObject) obj).getName());
         }
         return str;
     }
