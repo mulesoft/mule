a2278dbafe4855ce22a8e28f910f949b80b68b48
Daniel Feist <dfeist@gmail.com>
2010-11-23 13:26:57 +0000

    Add licence header exclusions for ClassReader and ParamReader
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@20304 bf997673-6b11-0410-b953-e057580c5b09

--- buildtools/scripts/ScanLicenseHeaders.groovy
+++ buildtools/scripts/ScanLicenseHeaders.groovy
@@ -16,10 +16,10 @@ public class ScanLicenseHeaders
     /**
      * These files are known to have invalid license headers and are the usual exception to the rule
      */
-    static List ignoredFiles = [ "BndMojo.java", "BobberArchetype.java", "BobberArchetypeMojo.java",
-        "ConfigurationPatternArchetypeMojo.java", "DummySSLServerSocketFactory.java", "ExampleArchetypeMojo.java", "ModuleArchetypeMojo.java",
-        "MultipartConfiguration.java", "Part.java", "ProjectArchetypeMojo.java",
-        "TransportArchetypeMojo.java", "XMLStreamReaderToContentHandler.java", "ClassReader.java", "ParamReader.java" ];
+    static List ignoredFiles = [ "BndMojo.java", "BobberArchetype.java", "BobberArchetypeMojo.java", "ClassReader.java",
+        "ConfigurationPatternArchetypeMojo.java", "DummySSLServerSocketFactory.java", "ExampleArchetypeMojo.java", 
+        "ModuleArchetypeMojo.java", "MultipartConfiguration.java", "ParamReader.java", "Part.java", 
+        "ProjectArchetypeMojo.java", "TransportArchetypeMojo.java", "XMLStreamReaderToContentHandler.java"];
 
     static void main(args)
     {
