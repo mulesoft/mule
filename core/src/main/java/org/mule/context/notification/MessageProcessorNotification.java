600b7d757da592cc92666d2b2920ef5bd04bc77f
aperepel@gmail.com <Andrew Perepelytsya>
2010-11-24 18:14:35 +0000

    Cleanup, reimplemented the name-obtaining part
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@20336 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/context/notification/MessageProcessorNotification.java
+++ core/src/main/java/org/mule/context/notification/MessageProcessorNotification.java
@@ -11,12 +11,13 @@
 package org.mule.context.notification;
 
 import org.mule.api.MuleEvent;
+import org.mule.api.MuleRuntimeException;
 import org.mule.api.context.notification.BlockingServerEvent;
 import org.mule.api.context.notification.ServerNotification;
 import org.mule.api.processor.MessageProcessor;
-import org.mule.util.BeanUtils;
+import org.mule.util.ObjectUtils;
 
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  *
@@ -29,6 +30,8 @@ public class MessageProcessorNotification extends ServerNotification implements 
     public static final int MESSAGE_PROCESSOR_PRE_INVOKE = MESSAGE_PROCESSOR_EVENT_ACTION_START_RANGE + 1;
     public static final int MESSAGE_PROCESSOR_POST_INVOKE = MESSAGE_PROCESSOR_EVENT_ACTION_START_RANGE + 2;
 
+    private transient MessageProcessor processor;
+
     static
     {
         registerAction("message processor pre invoke", MESSAGE_PROCESSOR_PRE_INVOKE);
@@ -38,26 +41,41 @@ public class MessageProcessorNotification extends ServerNotification implements 
 
     public MessageProcessorNotification(MuleEvent event, MessageProcessor processor, int action)
     {
-        super(event, action, event.getFlowConstruct().getName());
-        try {
-            final String name = BeanUtils.getProperty(processor, "name");
-            System.out.printf("++ %s > %s%n", event.getFlowConstruct().getName(), name != null ? name : processor);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        super(event, action, event.getFlowConstruct() != null ? event.getFlowConstruct().getName() : null);
+
+        try
+        {
+            String mpName;
+            // TODO would love to see MP.getName() in the API. Avoid BeanUtils.getProperty() overhead here
+            final Method method = processor.getClass().getMethod("getName");
+            if (method == null)
+        {
+            // no such method, fallback to MP class name + NamedObject
+            mpName = ObjectUtils.toString(processor);
+        }
+        else
+        {
+            // invoke existing getName(), but provide same fallback if it returned nothing
+            mpName = ObjectUtils.toString(method.invoke(processor), ObjectUtils.toString(processor));
+        }
+
+
+            System.out.printf("++ %s > %s%n", event.getFlowConstruct().getName(), mpName);
+        }
+        catch (Exception e)
+        {
+            throw new MuleRuntimeException(e);
         }
     }
 
-    public MessageProcessorNotification(MuleEvent event, MessageProcessor processor, int action, String resourceIdentifier)
+    @Override
+    public MuleEvent getSource()
     {
-        super(event, action, resourceIdentifier);
+        return (MuleEvent) super.getSource();
     }
 
-    @Override
-    public MuleEvent getSource() {
-        return (MuleEvent) super.getSource();
+    public MessageProcessor getProcessor()
+    {
+        return processor;
     }
 }
