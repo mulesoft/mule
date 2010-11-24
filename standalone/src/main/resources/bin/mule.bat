44f2ed57d50c34c792fc42e69e420bd7c4c5b3a3
tcarlson <tcarlson>
2010-11-24 16:17:18 +0000

    MULE-5152 Do not require MULE_HOME to be set for starting Mule
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@20323 bf997673-6b11-0410-b953-e057580c5b09

--- standalone/src/main/resources/bin/mule.bat
+++ standalone/src/main/resources/bin/mule.bat
@@ -86,17 +86,13 @@ set _WRAPPER_CONF="%_REALPATH%..\conf\wrapper.conf"
 rem ###############################################################
 rem Customized for Mule
 rem ###############################################################
-rem Export the location of this script.  This will be used in wrapper.conf
-rem ATTENTION: %_REALPATH% contains a trailing path delimiter that will cause the wrapper to fail. Do not use that
-rem (making MULE_EXE as absolute path to enable wrapper include additional configs correctly)
-for /f "tokens=* delims= " %%a in ('attrib %MULE_HOME%\bin') do set MULE_EXE=%%a
 
 rem add XML compatibility jars
 set MULE_ENDORSED=-M-Djava.endorsed.dirs="%MULE_HOME%\lib\endorsed"
 
 rem Mule options: Set the working directory to the current one and pass all command-line
 rem options (-config, -builder, etc.) straight through to the main() method.
-set MULE_OPTS=set.MULE_APP=%MULE_APP% set.MULE_APP_LONG=%MULE_APP_LONG% set.MULE_EXE="%MULE_EXE%" set.MULE_HOME=%MULE_HOME% set.MULE_LIB=%MULE_LIB% wrapper.working.dir="%CD%" wrapper.app.parameter.1=%1 wrapper.app.parameter.2=%2  wrapper.app.parameter.3=%3  wrapper.app.parameter.4=%4  wrapper.app.parameter.5=%5  wrapper.app.parameter.6=%6  wrapper.app.parameter.7=%7  wrapper.app.parameter.8=%8 wrapper.app.parameter.9=%9
+set MULE_OPTS=set.MULE_APP=%MULE_APP% set.MULE_APP_LONG=%MULE_APP_LONG% set.MULE_HOME=%MULE_HOME% set.MULE_LIB=%MULE_LIB% wrapper.working.dir="%CD%" wrapper.app.parameter.1=%1 wrapper.app.parameter.2=%2  wrapper.app.parameter.3=%3  wrapper.app.parameter.4=%4  wrapper.app.parameter.5=%5  wrapper.app.parameter.6=%6  wrapper.app.parameter.7=%7  wrapper.app.parameter.8=%8 wrapper.app.parameter.9=%9
 
 rem Adding additional jvm arguments to wrapper configuration if needed
 call %MULE_HOME%\bin\launcher.bat %MULE_HOME%\bin\additional.groovy %_WRAPPER_CONF% "%JPDA_OPTS%" "%MULE_ENDORSED%" %*
