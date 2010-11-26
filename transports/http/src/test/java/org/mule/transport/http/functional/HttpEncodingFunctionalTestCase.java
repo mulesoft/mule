1c32d142f29f6373faebbaf7dc20d904fb4e9506
Dirk Olmes <dirk.olmes@mulesoft.com>
2010-11-26 12:52:18 +0000

    MULE-3690 (Query string is always encoded as UTF-8 byte stream): apply the patch
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@20357 bf997673-6b11-0410-b953-e057580c5b09

--- transports/http/src/test/java/org/mule/transport/http/functional/HttpEncodingFunctionalTestCase.java
+++ transports/http/src/test/java/org/mule/transport/http/functional/HttpEncodingFunctionalTestCase.java
@@ -15,17 +15,89 @@ import org.mule.module.client.MuleClient;
 import org.mule.transport.http.HttpConnector;
 import org.mule.transport.http.HttpConstants;
 
+import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class HttpEncodingFunctionalTestCase extends HttpFunctionalTestCase
 {
     protected static String TEST_MESSAGE = "Test Http Request (R�dgr�d), 57 = \u06f7\u06f5 in Arabic";
-
+    private static String TEST_JAPANESE_MESSAGE = "\u3042";
+    
+    public HttpEncodingFunctionalTestCase()
+    {
+        super();
+        setDisposeManagerPerSuite(true);
+    }
+    
     @Override
     protected String getConfigResources()
     {
         return "http-encoding-test.xml";
+    }
+
+    // TODO MULE-3690 make me run green
+    public void _testPost() throws Exception
+    {
+        testPost(Locale.US, "US-ASCII", "A");
+        testPost(Locale.US, "UTF-8", "A");
+        
+        testPost(Locale.JAPAN, "UTF-8", TEST_JAPANESE_MESSAGE);
+        testPost(Locale.JAPAN, "Shift_JIS", TEST_JAPANESE_MESSAGE);
+        testPost(Locale.JAPAN, "Windows-31J", TEST_JAPANESE_MESSAGE);
+        testPost(Locale.JAPAN, "EUC-JP", TEST_JAPANESE_MESSAGE);
+    }
+    
+    private void testPost(Locale locale, String encoding, String payload) throws Exception 
+    {
+        MuleClient client = new MuleClient(muleContext);
+        
+        Map<String, Object> messageProperties = new HashMap<String, Object>();
+        
+        String contentType = "text/plain;charset=" + encoding;
+        messageProperties.put(HttpConstants.HEADER_CONTENT_TYPE, contentType);
+        messageProperties.put(HttpConnector.HTTP_METHOD_PROPERTY, "POST");
+     
+        String endpointUri = "clientEndpoint." + encoding;
+        MuleMessage reply = client.send(endpointUri, payload, messageProperties);
+        assertNotNull(reply);
+        assertEquals("200", reply.getInboundProperty(HttpConnector.HTTP_STATUS_PROPERTY));
+        assertEquals(payload + " Received", reply.getPayloadAsString());
+        assertEquals("text/plain;charset=" + encoding, reply.getInboundProperty(HttpConstants.HEADER_CONTENT_TYPE).toString());
+        assertEquals(encoding, reply.getEncoding());
+    }
+
+    // TODO MULE-3690 make me run green
+    public void _testGet() throws Exception 
+    {
+        testGet(Locale.US, "US-ASCII", "A");
+        testGet(Locale.US, "UTF-8", "A");
+
+        testGet(Locale.JAPAN, "UTF-8", TEST_JAPANESE_MESSAGE);
+        testGet(Locale.JAPAN, "Shift_JIS", TEST_JAPANESE_MESSAGE);
+        testGet(Locale.JAPAN, "Windows-31J", TEST_JAPANESE_MESSAGE);
+        testGet(Locale.JAPAN, "EUC-JP", TEST_JAPANESE_MESSAGE);
+    }
+    
+    private void testGet(Locale locale, String encoding, String payload) throws Exception 
+    {
+        MuleClient client = new MuleClient(muleContext);
+        
+        Map<String, Object> messageProperties = new HashMap<String, Object>();
+        String contentType = "text/plain;charset=" + encoding;
+        messageProperties.put(HttpConstants.HEADER_CONTENT_TYPE, contentType);
+        messageProperties.put(HttpConnector.HTTP_METHOD_PROPERTY, "GET");
+        
+        String endpointUri = "clientEndpoint." + encoding;
+        MuleMessage reply = client.send(endpointUri, payload, messageProperties);
+        String expectedReplyMessage = "/" + encoding + "?body=" + URLEncoder.encode(payload, encoding);
+
+        assertNotNull(reply);
+        assertEquals("200", reply.getInboundProperty(HttpConnector.HTTP_STATUS_PROPERTY));
+        assertEquals(expectedReplyMessage + " Received", reply.getPayloadAsString());
+        assertEquals("text/plain;charset=" + encoding, reply.getInboundProperty(HttpConstants.HEADER_CONTENT_TYPE).toString());
+        assertEquals(encoding, reply.getEncoding());
     }
 
     @Override
