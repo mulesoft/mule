8bf62d6c6657983e4a239207c1cc934326f9e650
Dan Diephouse <dan@netzooid.com>
2010-12-09 20:28:46 +0000

    MULE-5271: validation with payload=envelope wasn't working correctly because it would pass in a ReversibleXMLStreamReader instead of the root XMLStreamReader. Find the underlying Woodstox stream reader and use that instead.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@20573 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/cxf/src/main/java/org/mule/module/cxf/support/ProxySchemaValidationInInterceptor.java
@@ -0,0 +1,61 @@
+package org.mule.module.cxf.support;
+
+import org.mule.module.xml.stax.ReversibleXMLStreamReader;
+
+import java.util.logging.Logger;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.binding.soap.interceptor.ReadHeadersInterceptor;
+import org.apache.cxf.binding.soap.interceptor.StartBodyInterceptor;
+import org.apache.cxf.common.logging.LogUtils;
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.io.StaxValidationManager;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+import org.apache.cxf.service.model.ServiceInfo;
+
+public class ProxySchemaValidationInInterceptor extends AbstractPhaseInterceptor<Message> {
+    private static final Logger LOG = LogUtils.getL7dLogger(ProxySchemaValidationInInterceptor.class);
+    
+    private ServiceInfo service;
+    private Bus bus;
+    
+    public ProxySchemaValidationInInterceptor(Bus bus, ServiceInfo service) {
+        super(Phase.READ);
+        this.bus = bus;
+        this.service = service;
+        addBefore(StartBodyInterceptor.class.getName());
+        addAfter(ReadHeadersInterceptor.class.getName());
+    }
+
+
+    public void handleMessage(Message message) throws Fault {
+        XMLStreamReader xmlReader = message.getContent(XMLStreamReader.class);
+        
+        // if we're in proxy envelope mode, find the underlying stream reader before performing validation
+        if (xmlReader instanceof ReversibleXMLStreamReader) {
+            xmlReader = ((ReversibleXMLStreamReader) xmlReader).getDelegateReader();
+        }
+        
+        try {
+            setSchemaInMessage(message, xmlReader);
+        } catch (XMLStreamException e) {
+            throw new Fault(new org.apache.cxf.common.i18n.Message("SCHEMA_ERROR", LOG), 
+                            e);
+        }
+    }
+    
+    private void setSchemaInMessage(Message message, XMLStreamReader reader) throws XMLStreamException  {
+        Object en = message.getContextualProperty(org.apache.cxf.message.Message.SCHEMA_VALIDATION_ENABLED);
+        if (Boolean.TRUE.equals(en) || "true".equals(en)) {
+            StaxValidationManager mgr = bus.getExtension(StaxValidationManager.class);
+            if (mgr != null) {
+                mgr.setupValidation(reader, service);
+            }
+        }
+    }
+}
