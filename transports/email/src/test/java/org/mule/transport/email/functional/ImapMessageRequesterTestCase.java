28c75728a5e5ad3f328d275016e4c99be5d71bce
dzapata <dzapata>
2010-12-14 05:09:07 +0000

    ImapMessageRequesterTestCase was failing on a clean build machine when run in the email transport test suite; start the greenmail server before starting mule
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@20692 bf997673-6b11-0410-b953-e057580c5b09

--- transports/email/src/test/java/org/mule/transport/email/functional/ImapMessageRequesterTestCase.java
+++ transports/email/src/test/java/org/mule/transport/email/functional/ImapMessageRequesterTestCase.java
@@ -28,7 +28,13 @@ public class ImapMessageRequesterTestCase extends DynamicPortTestCase
     private static final String USER = "bob";
     
     private GreenMail server;
-    
+
+    public ImapMessageRequesterTestCase()
+    {
+        super();
+        setStartContext(false);
+    }
+
     @Override
     protected void doSetUp() throws Exception
     {
@@ -42,7 +48,6 @@ public class ImapMessageRequesterTestCase extends DynamicPortTestCase
         ServerSetup setup = new ServerSetup(PORT, null, ImapConnector.IMAP);
         server = new GreenMail(setup);
         server.start();
-
         GreenMailUtilities.storeEmail(server.getManagers().getUserManager(), EMAIL, USER, PASSWORD,
             GreenMailUtilities.toMessage(MESSAGE, EMAIL, null));
     }
@@ -62,6 +67,7 @@ public class ImapMessageRequesterTestCase extends DynamicPortTestCase
 
     public void testMessageRequester() throws Exception
     {
+        muleContext.start();
         String imapUri = String.format("imap://%1s:%2s@localhost:%3d/INBOX", USER, PASSWORD, PORT);
         
         MuleClient client = new MuleClient(muleContext);
