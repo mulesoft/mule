602eb36189ceaefc4d091c8e26fed08194ec3da6
Dirk Olmes <dirk.olmes@mulesoft.com>
2010-12-15 12:00:53 +0000

    bring back the relevant logging changes from r20739
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@20744 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/DefaultMuleEvent.java
+++ core/src/main/java/org/mule/DefaultMuleEvent.java
@@ -72,7 +72,7 @@ public class DefaultMuleEvent extends EventObject implements MuleEvent, ThreadSa
     /**
      * logger used by this class
      */
-    protected transient Log logger = LogFactory.getLog(getClass());
+    private static Log logger = LogFactory.getLog(DefaultMuleEvent.class);
 
     /**
      * The endpoint associated with the event
@@ -562,8 +562,6 @@ public class DefaultMuleEvent extends EventObject implements MuleEvent, ThreadSa
 
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException, MuleException
     {
-        logger = LogFactory.getLog(getClass());
-
         in.defaultReadObject();
         serializedData = new HashMap<String, Object>();
         serializedData.put("endpointHashcode", in.readInt());
