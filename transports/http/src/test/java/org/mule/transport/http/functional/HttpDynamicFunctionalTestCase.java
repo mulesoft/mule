6a5ce416f024b7fb47fcdc19d26001344c7b04d1
Dirk Olmes <dirk.olmes@mulesoft.com>
2010-12-16 12:30:24 +0000

    simplify the test
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@20780 bf997673-6b11-0410-b953-e057580c5b09

--- transports/http/src/test/java/org/mule/transport/http/functional/HttpDynamicFunctionalTestCase.java
+++ transports/http/src/test/java/org/mule/transport/http/functional/HttpDynamicFunctionalTestCase.java
@@ -10,23 +10,18 @@
 
 package org.mule.transport.http.functional;
 
-import org.mule.api.MuleEventContext;
 import org.mule.api.MuleMessage;
 import org.mule.module.client.MuleClient;
 import org.mule.tck.DynamicPortTestCase;
-import org.mule.tck.functional.EventCallback;
-import org.mule.tck.functional.FunctionalTestComponent;
-import org.mule.util.concurrent.Latch;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
-
 public class HttpDynamicFunctionalTestCase extends DynamicPortTestCase
 {
-    protected static String TEST_MESSAGE = "Test Http Request";
+    protected static String TEST_REQUEST = "Test Http Request";
 
+    @Override
     protected String getConfigResources()
     {
         return "http-dynamic-functional-test.xml";
@@ -34,40 +29,18 @@ public class HttpDynamicFunctionalTestCase extends DynamicPortTestCase
 
     public void testSend() throws Exception
     {
-        final Latch latch1 = new Latch();
-        final Latch latch2 = new Latch();
-        FunctionalTestComponent tc1 = getFunctionalTestComponent("testComponent1");
-        FunctionalTestComponent tc2 = getFunctionalTestComponent("testComponent2");
-        assertNotNull(tc1);
-        assertNotNull(tc2);
-
-            tc1.setEventCallback(new EventCallback() {
-                public void eventReceived(MuleEventContext context, Object component) throws Exception
-                {
-                    latch1.release();
-                }
-            });
-            tc2.setEventCallback(new EventCallback(){
-                public void eventReceived(MuleEventContext context, Object component) throws Exception
-                {
-                    latch2.release();
-                }
-            });
-
         MuleClient client = new MuleClient(muleContext);
+
         Map<String, Object> props = new HashMap<String, Object>();
         props.put("port", getPorts().get(0));
         props.put("path", "foo");
-        MuleMessage result = client.send("clientEndpoint", TEST_MESSAGE, props);
-        assertEquals(TEST_MESSAGE + " Received", result.getPayloadAsString());
 
-        assertTrue(latch1.await(3000, TimeUnit.MILLISECONDS));
+        MuleMessage result = client.send("clientEndpoint", TEST_REQUEST, props);
+        assertEquals(TEST_REQUEST + " Received 1", result.getPayloadAsString());
 
         props.put("port", getPorts().get(1));
-        result = client.send("clientEndpoint", TEST_MESSAGE, props);
-        assertEquals(TEST_MESSAGE + " Received", result.getPayloadAsString());
-
-        assertTrue(latch2.await(3000, TimeUnit.MILLISECONDS));
+        result = client.send("clientEndpoint", TEST_REQUEST, props);
+        assertEquals(TEST_REQUEST + " Received 2", result.getPayloadAsString());
     }
 
     @Override
