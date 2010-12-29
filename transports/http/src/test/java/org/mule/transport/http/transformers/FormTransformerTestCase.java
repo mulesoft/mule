e722f3b99bc38ddfd04b2b2ab6ccfbedf247af7e
Dan Diephouse <dan@netzooid.com>
2010-12-29 01:34:31 +0000

    Create a FormTransformer which can take URL encoded form parameters and convert them into a Map.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@20838 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/http/src/test/java/org/mule/transport/http/transformers/FormTransformerTestCase.java
@@ -0,0 +1,61 @@
+/*
+ * $Id: HttpResponseToStringTestCase.java 19250 2010-08-30 16:53:14Z dirk.olmes $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.http.transformers;
+
+import org.mule.DefaultMuleMessage;
+import org.mule.api.transformer.TransformerException;
+import org.mule.tck.AbstractMuleTestCase;
+
+import java.util.List;
+import java.util.Map;
+
+public class FormTransformerTestCase extends AbstractMuleTestCase
+{
+    private FormTransformer transformer;
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        super.doSetUp();
+
+        transformer = new FormTransformer();
+    }
+
+    public void testFormTransformer() throws TransformerException
+    {
+        DefaultMuleMessage msg = new DefaultMuleMessage("test1=value1&test2=value2&test3", muleContext);
+        Object result = transformer.transform(msg);
+        assertTrue(result instanceof Map);
+        
+        Map<String,String> m = (Map<String,String>) result;
+        assertEquals("value1", m.get("test1"));
+        assertEquals("value2", m.get("test2"));
+        assertNull(m.get("test3"));
+    }
+
+
+    public void testMultipleValues() throws TransformerException
+    {
+        DefaultMuleMessage msg = new DefaultMuleMessage("test1=value1&test1=value2", muleContext);
+        Object result = transformer.transform(msg);
+        assertTrue(result instanceof Map);
+        
+        Map<String,Object> m = (Map<String,Object>) result;
+        Object o = m.get("test1");
+        assertTrue(o instanceof List);
+
+        List list = (List) o;
+        assertTrue(list.contains("value1"));
+        assertTrue(list.contains("value2"));
+        
+    }
+
+}
