dee92a5c0616fde66ed8226c3e7bf85e813bcada
tcarlson <tcarlson>
2011-01-05 13:17:51 +0000

    MULE-1115 Example app to illustrate using a Rules Engine with Mule
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@20896 bf997673-6b11-0410-b953-e057580c5b09

---
+++ examples/cep/src/main/java/org/mule/example/cep/TickFeed.java
@@ -0,0 +1,71 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.example.cep;
+
+import org.mule.api.MuleEventContext;
+import org.mule.api.lifecycle.Callable;
+import org.mule.api.lifecycle.Initialisable;
+import org.mule.api.lifecycle.InitialisationException;
+import org.mule.transport.NullPayload;
+import org.mule.util.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.MessageFormat;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class TickFeed implements Initialisable, Callable
+{
+    public static final String DATA_FILE = "stocktickstream.dat";
+    private static final MessageFormat lineFormat = new MessageFormat("{0,number,0};{1};{2,number,currency}");
+    
+    private Iterator<String> lines;
+    
+    protected transient Log logger = LogFactory.getLog(getClass());
+    
+    public void initialise() throws InitialisationException
+    {
+        try
+        {
+            InputStream is = IOUtils.getResourceAsStream(DATA_FILE, TickFeed.class, false, false);
+            List<String> linesList = IOUtils.readLines(is);
+            logger.debug("Read data file, " + linesList.size() + " lines");
+            lines = linesList.iterator();
+        }
+        catch (IOException e)
+        {
+            throw new InitialisationException(e, this);
+        }
+    }
+
+    public Object onCall(MuleEventContext eventContext) throws Exception
+    {
+        if (lines.hasNext())
+        {
+            Object[] results = lineFormat.parse(lines.next());
+            StockTick tick = new StockTick((String)results[1],
+                                           ((Number)results[2]).doubleValue(),
+                                           ((Number)results[0]).longValue());
+            logger.info("New Stock Tick: " + tick);
+            return tick;
+        }
+        else
+        {
+            return NullPayload.getInstance();
+        }
+    }
+}
+
+
