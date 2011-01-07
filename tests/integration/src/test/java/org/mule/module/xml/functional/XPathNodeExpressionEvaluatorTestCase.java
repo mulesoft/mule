a8fab5786e693ed1885b448386c3b527cb2cf977
Pablo Kraan <pablo.kraan@gmail.com>
2011-01-07 16:53:23 +0000

    Merged revisions 20932 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.2.x
    ........
      r20932 | pablo.kraan | 2011-01-07 12:06:15 -0300 (Fri, 07 Jan 2011) | 1 line
      
      MULE-5332: return-argument in expression filter does not accept xpath-node
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@20934 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/module/xml/functional/XPathNodeExpressionEvaluatorTestCase.java
@@ -0,0 +1,49 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.xml.functional;
+
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+
+import org.custommonkey.xmlunit.XMLAssert;
+
+public class XPathNodeExpressionEvaluatorTestCase extends FunctionalTestCase
+{
+
+    private static final String SAMPLE_REQUEST =
+            "<root>" +
+            "<table>" +
+            "<name>African Coffee Table</name>" +
+            "<width>80</width>" +
+            "<length>120</length>" +
+            "</table>" +
+            "</root>";
+
+    private static final String EXPECTED_RESPONSE =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+            "<name>African Coffee Table</name>";
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/module/xml/xpath-node-config.xml";
+    }
+
+    public void testExpressionTransformerUsingXpathNode() throws Exception
+    {
+        MuleClient client = new MuleClient(muleContext);
+
+        MuleMessage message = client.send("vm://testInput", SAMPLE_REQUEST, null);
+
+        XMLAssert.assertXMLEqual(EXPECTED_RESPONSE, message.getPayloadAsString());
+    }
+}
