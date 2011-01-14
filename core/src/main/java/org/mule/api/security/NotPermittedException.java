968578df331559c04e9cb6380763750cf262f519
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-01-14 08:13:37 +0000

    fix svn properties
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@21020 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/api/security/NotPermittedException.java
+++ core/src/main/java/org/mule/api/security/NotPermittedException.java
@@ -1,5 +1,5 @@
 /*
- * $Id: UnauthorisedException.java 19739 2010-09-27 14:28:40Z tcarlson $
+ * $Id$
  * --------------------------------------------------------------------------------------
  * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
  *
