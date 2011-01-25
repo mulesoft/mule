1b377b72aa1a50983c7951bd560eb30d77fe5178
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-01-25 12:19:56 +0000

    expose the ObjectToFeed transformer in XML so it can be used idependently from the deprecated atom endpoint. Rename the namespace handler test case for this module and move it to the correct package.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@21098 bf997673-6b11-0410-b953-e057580c5b09

--- modules/atom/src/test/java/org/mule/module/atom/config/AtomNamespaceHandlerTestCase.java
+++ modules/atom/src/test/java/org/mule/module/atom/config/AtomNamespaceHandlerTestCase.java
@@ -7,24 +7,27 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.module.atom;
+package org.mule.module.atom.config;
 
 import org.mule.api.endpoint.InboundEndpoint;
 import org.mule.api.processor.MessageProcessor;
 import org.mule.api.service.Service;
+import org.mule.api.transformer.Transformer;
 import org.mule.construct.SimpleFlowConstruct;
 import org.mule.module.atom.endpoint.AtomInboundEndpoint;
 import org.mule.module.atom.routing.EntryLastUpdatedFilter;
 import org.mule.module.atom.routing.FeedLastUpdatedFilter;
 import org.mule.module.atom.routing.FeedSplitter;
+import org.mule.module.atom.transformers.ObjectToFeed;
 import org.mule.routing.MessageFilter;
 import org.mule.service.ServiceCompositeMessageSource;
 import org.mule.tck.FunctionalTestCase;
 
 import java.text.SimpleDateFormat;
 
-public class NamespaceTestCase extends FunctionalTestCase
+public class AtomNamespaceHandlerTestCase extends FunctionalTestCase
 {
+    @Override
     protected String getConfigResources()
     {
         return "namespace-config.xml";
@@ -60,7 +63,8 @@ public class NamespaceTestCase extends FunctionalTestCase
         assertEquals(sdf.parse("2009-10-01"), ((EntryLastUpdatedFilter)((MessageFilter)mp).getFilter()).getLastUpdate());
     }
 
-    public void testGlobalFilterConfig() throws Exception {
+    public void testGlobalFilterConfig() throws Exception 
+    {
         FeedLastUpdatedFilter filter = muleContext.getRegistry().lookupObject("feedFilter");
         assertNotNull(filter);
 
@@ -68,6 +72,12 @@ public class NamespaceTestCase extends FunctionalTestCase
 
         assertEquals(sdf.parse("2009-10-01 13:00:00"), filter.getLastUpdate());
         assertFalse(filter.isAcceptWithoutUpdateDate());
-
+    }
+    
+    public void testObjectToFeedTransformer() throws Exception
+    {
+        Transformer transformer = muleContext.getRegistry().lookupTransformer("ObjectToFeed");
+        assertNotNull(transformer);
+        assertTrue(transformer instanceof ObjectToFeed);
     }
 }
