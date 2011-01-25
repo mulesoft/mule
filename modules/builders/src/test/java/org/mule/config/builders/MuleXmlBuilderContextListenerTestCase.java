863f4c58f3f8f18282425353bd09ca635b23cb73
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-01-25 16:26:45 +0000

    our naming convention for tests is xxxTestCase
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@21103 bf997673-6b11-0410-b953-e057580c5b09

--- modules/builders/src/test/java/org/mule/config/builders/MuleXmlBuilderContextListenerTestCase.java
+++ modules/builders/src/test/java/org/mule/config/builders/MuleXmlBuilderContextListenerTestCase.java
@@ -23,7 +23,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public class MuleXmlBuilderContextListenerTest
+public class MuleXmlBuilderContextListenerTestCase
 {
     private MuleXmlBuilderContextListener listener;
     private ServletContext context;
