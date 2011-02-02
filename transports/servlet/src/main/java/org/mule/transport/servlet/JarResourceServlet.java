f5cc59b449f9b302f9b98e60d33da949bbffe803
Ross Mason <ross@mulesoft.com>
2011-02-02 19:04:38 +0000

    MULE-5386. Allow property placeholders in html static files
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@21175 bf997673-6b11-0410-b953-e057580c5b09

--- transports/servlet/src/main/java/org/mule/transport/servlet/JarResourceServlet.java
+++ transports/servlet/src/main/java/org/mule/transport/servlet/JarResourceServlet.java
@@ -9,11 +9,16 @@
  */
 package org.mule.transport.servlet;
 
+import org.mule.api.MuleContext;
+import org.mule.api.config.MuleProperties;
+import org.mule.registry.RegistryMap;
 import org.mule.util.IOUtils;
+import org.mule.util.TemplateParser;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -21,7 +26,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * A servlet for loading resources loaded in jar files
+ * A servlet for loading resources loaded in jar files. This allows javascript, htm and images to be bundled into a jar
+ * This servlet also supports property placeholders for html, xml and json files.  This allows for server configuration
+ * to be injected into static files.
  */
 public class JarResourceServlet extends HttpServlet
 {
@@ -33,12 +40,37 @@ public class JarResourceServlet extends HttpServlet
 
     private String basepath = DEFAULT_BASE_PATH;
 
+    private String[] templateExtensions = new String[]{"htm", "html", "xml", "json"};
+
+    private TemplateParser parser = TemplateParser.createAntStyleParser();
+
+    private MuleContext muleContext;
+
+    private Map props;
+
+    @Override
+    public void init() throws ServletException
+    {
+        muleContext = (MuleContext) getServletContext().getAttribute(MuleProperties.MULE_CONTEXT_PROPERTY);
+        //We need MuleContext for doing templating
+        if (muleContext == null)
+        {
+            throw new ServletException("Property " + MuleProperties.MULE_CONTEXT_PROPERTY + " not set on ServletContext");
+        }
+
+        props = new RegistryMap(muleContext.getRegistry());
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
         String file = getBasepath() + req.getPathInfo();
 
-        if(file.startsWith("/")) file = file.substring(1);
+        if (file.startsWith("/"))
+        {
+            file = file.substring(1);
+        }
+
         InputStream in = IOUtils.getResourceAsStream(file, getClass(), false, false);
         if (in == null)
         {
@@ -49,6 +81,8 @@ public class JarResourceServlet extends HttpServlet
         byte[] buffer;
         try
         {
+
+
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             IOUtils.copyLarge(in, baos);
 
@@ -67,6 +101,16 @@ public class JarResourceServlet extends HttpServlet
             resp.setContentType(mimetype);
             resp.setContentLength(buffer.length);
             resp.setHeader("Content-Disposition", "attachment; filename=\"" + req.getPathInfo() + "\"");
+
+            //We could wrap this parsing in a stream to make it more efficient
+            for (String extension : templateExtensions)
+            {
+                if(mimetype.endsWith(extension))
+                {
+                    buffer = parser.parse(props, new String(buffer)).getBytes();
+                    break;
+                }
+            }
             resp.getOutputStream().write(buffer);
         }
         finally
