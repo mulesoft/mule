73d8ba896e1a1c1dd762603e5576ffb76a28b622
dzapata <dzapata>
2011-02-03 21:49:03 +0000

    Merged revisions 21184 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.1.x
    ........
      r21184 | dzapata | 2011-02-03 13:36:34 -0800 (Thu, 03 Feb 2011) | 1 line
      
      use dynamic ports
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@21185 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/message/AbstractPropertyScopeTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/message/AbstractPropertyScopeTestCase.java
@@ -7,17 +7,18 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
+
 package org.mule.test.integration.message;
 
 import org.mule.DefaultMuleMessage;
 import org.mule.api.MuleMessage;
 import org.mule.api.client.LocalMuleClient;
-import org.mule.tck.FunctionalTestCase;
+import org.mule.tck.DynamicPortTestCase;
 
 /**
  * TODO
  */
-public abstract class AbstractPropertyScopeTestCase extends FunctionalTestCase
+public abstract class AbstractPropertyScopeTestCase extends DynamicPortTestCase
 {
     public void testRequestResponse() throws Exception
     {
@@ -27,7 +28,12 @@ public abstract class AbstractPropertyScopeTestCase extends FunctionalTestCase
 
         MuleMessage result = client.send("inbound", message);
         assertEquals("test bar", result.getPayloadAsString());
-        assertEquals("fooValue", result.<Object>getInboundProperty("foo"));
+        assertEquals("fooValue", result.<Object> getInboundProperty("foo"));
     }
 
+    @Override
+    protected int getNumPortsToFind()
+    {
+        return 1;
+    }
 }
