ce5031fc37ef5548dc1287498947c3210a8c6d73
Mariano Capurro <mcapurro@gmail.com>
2011-02-09 15:27:11 +0000

    Changes in this commit: - Added logic in mule_examples/mule_examples.bat scripts in order to obtain MULE_HOME (if called from outside mule/mule.bat script) - Fixed license comment in mule_examples.groovy - Force disconnect from HTTP connection in mule_examples.groovy script
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@21214 bf997673-6b11-0410-b953-e057580c5b09

--- standalone/src/main/resources/bin/mule_examples.bat
+++ standalone/src/main/resources/bin/mule_examples.bat
@@ -1,14 +1,31 @@
 @echo off
 setlocal
 
+rem ###############################################################
+rem Deploy examples and launch Mule ESB
+rem ###############################################################
+
 set URL=http://localhost:18082/examples/
 
-echo MULE_HOME is set to %MULE_HOME%
+if "%OS%"=="Windows_NT" goto nt
+echo This script only works with NT-based versions of Windows.
+goto :eof
+
+:nt
+
+rem ###############################################################
+rem Setting MULE_HOME if the script is called from outside mule.bat script
+rem ###############################################################
+if "%MULE_HOME%" == "" set MULE_HOME=%_REALPATH:~0,-5%
+
+rem ###############################################################
+rem Deploy examples and launch Mule ESB
+rem ###############################################################
 
 call %MULE_HOME%\bin\launcher.bat %MULE_HOME%\bin\mule_examples.groovy "%URL%"
 
 if not ERRORLEVEL 1 goto 
-echo "ERROR: %ERRORLEVEL%. Please check logs to see why Mule ESB is not starting"
+echo "ERROR: %ERRORLEVEL%. Please check log file (%MULE_HOME%\logs\mule.log) to see why Mule ESB is not starting"
 goto :eof
 
 :open_url
