b09011c90f8c8473a0c6cc5004a61ea0b6d3398a
Mariano Capurro <mcapurro@gmail.com>
2011-02-22 22:41:25 +0000

    Fixed example launcher script for Windows
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@21337 bf997673-6b11-0410-b953-e057580c5b09

--- distributions/standalone/src/main/resources/bin/mule_examples.bat
+++ distributions/standalone/src/main/resources/bin/mule_examples.bat
@@ -7,11 +7,23 @@ rem ###############################################################
 
 set URL=http://localhost:18082/examples/
 
+rem ###############################################################
+rem Deploy examples and launch Mule ESB
+rem ###############################################################
+
+set URL=http://localhost:18082/examples/
+
 if "%OS%"=="Windows_NT" goto nt
 echo This script only works with NT-based versions of Windows.
 goto :eof
 
 :nt
+
+rem
+rem Find the application home.
+rem
+rem %~dp0 is location of current script under NT
+set _REALPATH=%~dp0
 
 rem ###############################################################
 rem Setting MULE_HOME if the script is called from outside mule.bat script
@@ -24,12 +36,12 @@ rem ###############################################################
 
 call %MULE_HOME%\bin\launcher.bat %MULE_HOME%\bin\mule_examples.groovy "%URL%"
 
-if not ERRORLEVEL 1 goto 
+if not ERRORLEVEL 1 goto open_url
 echo "ERROR: %ERRORLEVEL%. Please check log file (%MULE_HOME%\logs\mule.log) to see why Mule ESB is not starting"
 goto :eof
 
 :open_url
-Start %URL%
+call start %URL%
 goto :eof
 
 :eof
