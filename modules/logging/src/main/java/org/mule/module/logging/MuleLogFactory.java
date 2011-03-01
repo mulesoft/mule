6b8c193376c849d7c3366a0000263c759b1f55da
aperepel@gmail.com <Andrew Perepelytsya>
2011-03-01 17:59:59 +0000

    MULE-5264 Per-app log files - More progress, need to hack log4j bridges now
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@21379 bf997673-6b11-0410-b953-e057580c5b09

--- modules/logging/src/main/java/org/mule/module/logging/MuleLogFactory.java
+++ modules/logging/src/main/java/org/mule/module/logging/MuleLogFactory.java
@@ -10,19 +10,60 @@
 
 package org.mule.module.logging;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogConfigurationException;
+import org.apache.commons.logging.impl.MuleLocationAwareLog;
+import org.apache.commons.logging.impl.MuleLog;
 import org.apache.commons.logging.impl.SLF4JLogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LocationAwareLogger;
 
 /**
- *
+ * TODO release/flush logger repositories on app removal
  */
 public class MuleLogFactory extends SLF4JLogFactory
 {
+    protected Map<ClassLoader, ConcurrentMap<String, Log>> repositories = new HashMap<ClassLoader, ConcurrentMap<String, Log>>();
 
-    @Override
-    public Log getInstance(Class clazz) throws LogConfigurationException
+    public Log getInstance(String name) throws LogConfigurationException
     {
-        return super.getInstance(clazz);
+        final ClassLoader ccl = Thread.currentThread().getContextClassLoader();
+        ConcurrentMap<String, Log> loggerMap = repositories.get(ccl);
+
+        if (loggerMap == null)
+        {
+            loggerMap = new ConcurrentHashMap<String, Log>();
+            // TODO rework to use ConcurrentHashMap
+            repositories.put(ccl, loggerMap);
+        }
+
+        Log instance = loggerMap.get(name);
+
+        if (instance == null)
+        {
+            Logger logger = LoggerFactory.getLogger(name);
+            if (logger instanceof LocationAwareLogger)
+            {
+                instance = new MuleLocationAwareLog((LocationAwareLogger) logger);
+            }
+            else
+            {
+                instance = new MuleLog(logger);
+            }
+            final Log previous = loggerMap.putIfAbsent(name, instance);
+            if (previous != null)
+            {
+                // someone got there before us
+                instance = previous;
+            }
+        }
+
+        return instance;
     }
 }
---
+++ modules/logging/src/main/java/org/mule/module/logging/TestLoggerFactory.java
@@ -0,0 +1,26 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.logging;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+
+/**
+ *
+ */
+public class TestLoggerFactory implements ILoggerFactory
+{
+
+    public Logger getLogger(String name)
+    {
+        return null;
+    }
+}
