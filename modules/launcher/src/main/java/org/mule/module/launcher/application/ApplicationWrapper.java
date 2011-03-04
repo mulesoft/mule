48777f069b9cf0a457b57caef0f20ebf3fb1cf9e
aperepel@gmail.com <Andrew Perepelytsya>
2011-03-04 16:27:36 +0000

    Re-arranged application dispose() logic, as proper dispose() was called on undeploy only. Now redeploy() does the same classloader cleanup as undeploy() as part of the process.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@21460 bf997673-6b11-0410-b953-e057580c5b09

--- modules/launcher/src/main/java/org/mule/module/launcher/application/ApplicationWrapper.java
+++ modules/launcher/src/main/java/org/mule/module/launcher/application/ApplicationWrapper.java
@@ -12,7 +12,6 @@ package org.mule.module.launcher.application;
 
 import org.mule.api.MuleContext;
 import org.mule.module.launcher.DeploymentStartException;
-import org.mule.module.launcher.GoodCitizenClassLoader;
 import org.mule.module.launcher.InstallException;
 import org.mule.module.launcher.descriptor.ApplicationDescriptor;
 
@@ -36,30 +35,9 @@ public class ApplicationWrapper implements Application
 
     public void dispose()
     {
-        final ClassLoader originalCl = Thread.currentThread().getContextClassLoader();
-        try
-        {
-            ClassLoader appCl = getDeploymentClassLoader();
-            // if not initialized yet, it can be null
-            if (appCl != null)
-            {
-                Thread.currentThread().setContextClassLoader(appCl);
-            }
-            delegate.dispose();
-            if (appCl != null)
-            {
-                // close classloader to release jar connections in lieu of Java 7's ClassLoader.close()
-                if (appCl instanceof GoodCitizenClassLoader)
-                {
-                    GoodCitizenClassLoader classLoader = (GoodCitizenClassLoader) appCl;
-                    classLoader.close();
-                }
-            }
-        }
-        finally
-        {
-            Thread.currentThread().setContextClassLoader(originalCl);
-        }
+        // moved wrapper logic into the actual implementation, as redeploy() invokes it directly, bypassing
+        // classloader cleanup
+        delegate.dispose();
     }
 
     public ClassLoader getDeploymentClassLoader()
