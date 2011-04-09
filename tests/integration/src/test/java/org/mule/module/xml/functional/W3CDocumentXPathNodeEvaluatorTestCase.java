f9ee92f7201fdc8691caa6347d1545ff7e6fa99b
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-04-09 17:28:36 +0000

    Merged revisions 21673 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.2.x
    ........
      r21673 | dirk.olmes | 2011-04-09 04:25:35 -0700 (Sat, 09 Apr 2011) | 2 lines
      
      MULE-5511 (xpath-node evaluator throws an exception if the input type is org.w3c.dom.Document)
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@21675 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/module/xml/functional/W3CDocumentXPathNodeEvaluatorTestCase.java
@@ -0,0 +1,47 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.xml.functional;
+
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleMessage;
+import org.mule.api.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+
+import org.w3c.dom.Node;
+
+public class W3CDocumentXPathNodeEvaluatorTestCase extends FunctionalTestCase
+{
+    private static final String XML_INPUT =
+        "<root>" +
+        "  <table>" +
+        "    <name>African Coffee Table</name>" +
+        "    <width>80</width>" +
+        "    <length>120</length>" +
+        "  </table>" +
+        "</root>";
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/module/xml/w3c-dom-xpath-node-config.xml";
+    }
+
+    public void testW3CDocument() throws Exception
+    {
+        MuleClient client = muleContext.getClient();
+
+        MuleMessage message = new DefaultMuleMessage(XML_INPUT, muleContext);
+        MuleMessage response = client.send("vm://test", message);
+        assertNotNull(response);
+        assertNotNull(response.getPayload());
+        assertTrue(response.getPayload() instanceof Node);
+    }
+}
