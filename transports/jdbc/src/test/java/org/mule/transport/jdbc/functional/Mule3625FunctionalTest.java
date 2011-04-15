d0c0842f584511ed52dd4f8ea36fa9a7b7ca196c
dzapata <dzapata>
2011-04-15 00:07:53 +0000

    Merged revisions 21681 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.1.x
    ........
      r21681 | dzapata | 2011-04-14 14:22:11 -0700 (Thu, 14 Apr 2011) | 1 line
      
      drop tables before and after to make sure other tests are affecting it
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@21686 bf997673-6b11-0410-b953-e057580c5b09

--- transports/jdbc/src/test/java/org/mule/transport/jdbc/functional/Mule3625FunctionalTest.java
+++ transports/jdbc/src/test/java/org/mule/transport/jdbc/functional/Mule3625FunctionalTest.java
@@ -39,6 +39,10 @@ public class Mule3625FunctionalTest extends FunctionalTestCase
         DataSource dataSource = (DataSource) muleContext.getRegistry().lookupObject("hsqldbDataSource");
         Connection connection = dataSource.getConnection();
         Statement statement = connection.createStatement();
+        
+        // make sure other tests didnt leave anything behind
+        statement.execute("DROP SCHEMA PUBLIC CASCADE");        
+        
         statement.executeUpdate("create table TABLE_A (value varchar(1))");
         statement.executeUpdate("create table TABLE_B (value varchar(1))");
         statement.executeUpdate("insert into TABLE_A(value) values('n')");
@@ -46,6 +50,11 @@ public class Mule3625FunctionalTest extends FunctionalTestCase
         ResultSet resultSet = statement.executeQuery("select count(*) from TABLE_B where value='y'");
         assertTrue(resultSet.next());
         assertEquals(1, resultSet.getLong(1));
+        
+        // clean up for later tests
+        statement.execute("DROP SCHEMA PUBLIC CASCADE");
+        resultSet.close();
+        connection.close();
     }
 
 }
