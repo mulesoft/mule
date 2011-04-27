8edb9d92b7efe354bb2c175a1478cc23dd48d123
Dan Diephouse <dan@netzooid.com>
2011-04-27 23:00:08 +0000

    MULE-4916: allow users to set properties on custom security filters.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@21741 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/integration/security/CustomSecurityFilterTestCase.java
@@ -0,0 +1,45 @@
+/*
+ * $Id: OutboundHttpEndpointAuthenticationTestCase.java 19191 2010-08-25 21:05:23Z tcarlson $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.integration.security;
+
+import org.mule.api.MuleMessage;
+import org.mule.client.DefaultLocalMuleClient;
+import org.mule.tck.FunctionalTestCase;
+
+import java.util.HashMap;
+
+/**
+ * See MULE-4916: spring beans inside a security filter
+ */
+public class CustomSecurityFilterTestCase extends FunctionalTestCase
+{
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/test/integration/security/custom-security-filter-test.xml";
+    }
+
+    public void testOutboundAutenticationSend() throws Exception
+    {
+        DefaultLocalMuleClient client = new DefaultLocalMuleClient(muleContext);
+        
+        HashMap<String, Object> props = new HashMap<String,Object>();
+        props.put("username", "ross");
+        props.put("pass", "ross");
+        MuleMessage result = client.send("vm://test", "hi", props);
+        assertNull(result.getExceptionPayload());
+        
+        props.put("pass", "badpass");
+        result = client.send("vm://test", "hi", props);
+        assertNotNull(result.getExceptionPayload());
+    }
+}
