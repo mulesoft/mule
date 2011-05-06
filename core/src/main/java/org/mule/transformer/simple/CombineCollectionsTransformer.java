5542479844d1c9cd57775229ef4968180588123a
Dan Diephouse <dan@netzooid.com>
2011-05-06 20:12:32 +0000

    Merged revisions 21826-21827,21829 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.x
    ........
      r21826 | dandiep | 2011-05-06 10:35:13 -0700 (Fri, 06 May 2011) | 1 line
      
      Formatting.
    ........
      r21827 | dandiep | 2011-05-06 10:35:25 -0700 (Fri, 06 May 2011) | 1 line
      
      Helper method to get at JSON arrays.
    ........
      r21829 | dandiep | 2011-05-06 10:37:03 -0700 (Fri, 06 May 2011) | 1 line
      
      Add transformer for combining collections of collections into a single collection.
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@21839 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/main/java/org/mule/transformer/simple/CombineCollectionsTransformer.java
@@ -0,0 +1,86 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transformer.simple;
+
+import org.mule.DefaultMuleEvent;
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.api.MuleMessage;
+import org.mule.api.MuleMessageCollection;
+import org.mule.api.processor.MessageProcessor;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * 
+ * Takes a payload which is a Collection of Collections and turns into a single List. For example, if the payload is a Collection
+ * which contains a Collection with elements A and B and another Collection with elements C and D, this will turn them into
+ * a single Collection with elements A, B, C and D.
+ *    
+ * This transformer will also work on MuleMessageCollections. In this case, it will take the individual Collection
+ * payloads of each MuleMessage and merge them into a single Collection on a new MuleMessage.
+ */
+public class CombineCollectionsTransformer implements MessageProcessor
+{
+
+    public MuleEvent process(MuleEvent event) throws MuleException
+    {
+        MuleMessage msg = event.getMessage();
+
+        List<Object> payload = new ArrayList<Object>();
+        if (msg instanceof MuleMessageCollection)
+        {
+            MuleMessageCollection collection = (MuleMessageCollection) msg;
+            collection.getPayload();
+            for (MuleMessage child : collection.getMessagesAsArray())
+            {
+                Object childPayload = child.getPayload();
+                if (childPayload instanceof Collection)
+                {
+                    payload.addAll((Collection) childPayload);
+                }
+                else
+                {
+                    payload.add(childPayload);
+                }
+            }
+        } 
+        else if (msg.getPayload() instanceof Collection)
+        {
+            add(payload, (Collection)msg.getPayload());
+        }
+        else
+        {
+            payload.add(msg.getPayload());
+        }
+
+        DefaultMuleMessage listMessage = new DefaultMuleMessage(payload, msg, msg.getMuleContext());
+        return new DefaultMuleEvent(listMessage, event);
+    }
+
+    private void add(List<Object> newPayload, Collection existingPayload)
+    {
+        for (Object o : existingPayload)
+        {
+            if (o instanceof Collection)
+            {
+                newPayload.addAll((Collection)o);
+            }
+            else 
+            {
+                newPayload.add(o);
+            }
+        }
+    }
+}
---
+++ core/src/test/java/org/mule/routing/CombineCollectionsTransformerTestCase.java
@@ -0,0 +1,72 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.routing;
+
+import org.mule.DefaultMessageCollection;
+import org.mule.DefaultMuleEvent;
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleMessageCollection;
+import org.mule.tck.AbstractMuleTestCase;
+import org.mule.transformer.simple.CombineCollectionsTransformer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombineCollectionsTransformerTestCase extends AbstractMuleTestCase
+{
+    private CombineCollectionsTransformer merger;
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        super.doSetUp();
+        
+        merger = new CombineCollectionsTransformer();
+    }
+
+    public void testMuleMessageCollectionMerge() throws Exception
+    {   
+        MuleEvent event = getTestEvent("hello");
+        
+        DefaultMessageCollection collection = new DefaultMessageCollection(muleContext);
+        collection.addMessage(new DefaultMuleMessage(Arrays.asList("1", "2", "3"), muleContext));
+        collection.addMessage(new DefaultMuleMessage("4", muleContext));
+        collection.addMessage(new DefaultMuleMessage(Arrays.asList("5", "6", "7"), muleContext));
+        
+        event = new DefaultMuleEvent(collection, event);
+        
+        MuleEvent response = merger.process(event);
+        
+        assertFalse(response.getMessage() instanceof MuleMessageCollection);
+        
+        assertTrue(response.getMessage().getPayload() instanceof List);
+        assertEquals(7, ((List)response.getMessage().getPayload()).size());
+    }
+    
+    public void testMuleMessageMerge() throws Exception
+    {
+        MuleEvent event = getTestEvent("hello");
+        
+        ArrayList<Object> payload = new ArrayList<Object>();
+        payload.add(Arrays.asList("1", "2", "3"));
+        payload.add("4");
+        payload.add(Arrays.asList("5", "6", "7"));
+        event.getMessage().setPayload(payload);
+        
+        MuleEvent response = merger.process(event);
+        
+        assertFalse(response.getMessage() instanceof MuleMessageCollection);
+        
+        assertTrue(response.getMessage().getPayload() instanceof List);
+        assertEquals(7, ((List)response.getMessage().getPayload()).size());
+    }
+}
