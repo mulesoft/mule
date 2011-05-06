dafd60391e385142d98f4f35a329fc249060c2bd
Pablo Kraan <pablo.kraan@gmail.com>
2011-05-06 19:39:12 +0000

    Merged revisions 21830 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.1.x
    ................
      r21830 | pablo.kraan | 2011-05-06 14:43:47 -0300 (Fri, 06 May 2011) | 13 lines
      
      Merged revisions 21813 via svnmerge from 
      https://svn.codehaus.org/mule/branches/mule-2.2.x
      
      ........
        r21813 | pablo.kraan | 2011-05-05 12:43:53 -0300 (Thu, 05 May 2011) | 6 lines
        
        MULE-5546: Add sequence router
        
        _ Extracted AbstractSequenceRouter from MulticastingRouter
        _ MulticastingRouter is now a subclass of AbstractSequenceRouter (I know that the multicasting concept does not implies a sequence, but the implementation used that way to send the messages)
        _ Added SequenceRouter and sequence-router tag 
      ........
    ................
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@21838 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/routing/outbound/MulticastingRouter.java
+++ core/src/main/java/org/mule/routing/outbound/MulticastingRouter.java
@@ -10,81 +10,24 @@
 
 package org.mule.routing.outbound;
 
-import org.mule.DefaultMuleMessage;
-import org.mule.api.MessagingException;
 import org.mule.api.MuleEvent;
 import org.mule.api.MuleException;
-import org.mule.api.MuleMessage;
-import org.mule.api.endpoint.OutboundEndpoint;
-import org.mule.api.processor.MessageProcessor;
-import org.mule.api.routing.CouldNotRouteOutboundMessageException;
-import org.mule.api.routing.RoutePathNotFoundException;
-import org.mule.api.routing.RoutingException;
-import org.mule.config.i18n.CoreMessages;
-import org.mule.routing.CorrelationMode;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * <code>MulticastingRouter</code> will broadcast the current message to every endpoint
- * registed with the router.
+ * registered with the router.
  */
 
-public class MulticastingRouter extends FilteringOutboundRouter
+public class MulticastingRouter extends AbstractSequenceRouter
 {
+
+    /**
+     * Indicates that this router always routes messages to all the configured
+     * endpoints no matters what a given response is.
+     */
     @Override
-    public MuleEvent route(MuleEvent event) throws RoutingException
+    protected boolean continueRoutingMessageAfter(MuleEvent response) throws MuleException
     {
-        MuleMessage message = event.getMessage();
-
-        if (routes == null || routes.size() == 0)
-        {
-            throw new RoutePathNotFoundException(CoreMessages.noEndpointsForRouter(), event, null);
-        }
-        if (enableCorrelation != CorrelationMode.NEVER)
-        {
-            boolean correlationSet = message.getCorrelationId() != null;
-            if (correlationSet && (enableCorrelation == CorrelationMode.IF_NOT_SET))
-            {
-                logger.debug("CorrelationId is already set, not setting Correlation group size");
-            }
-            else
-            {
-                // the correlationId will be set by the AbstractOutboundRouter
-                message.setCorrelationGroupSize(routes.size());
-            }
-        }
-
-        List<MuleEvent> results = new ArrayList<MuleEvent>(routes.size());
-        try
-        {
-            for (int i = 0; i < routes.size(); i++)
-            {
-                MessageProcessor mp = routes.get(i);
-                OutboundEndpoint endpoint = mp instanceof OutboundEndpoint ? (OutboundEndpoint)mp : null;
-                if (endpoint == null || endpoint.getFilter() == null || (endpoint.getFilter() != null && endpoint.getFilter().accept(message)))
-                {
-                    if (((DefaultMuleMessage) message).isConsumable())
-                    {
-                        throw new MessagingException(
-                            CoreMessages.cannotCopyStreamPayload(message.getPayload().getClass().getName()),
-                            event);
-                    }
-
-                    MuleMessage clonedMessage = cloneMessage(message);
-                    MuleEvent result = sendRequest(event, clonedMessage, mp, true);
-                    if (result != null)
-                    {
-                        results.add(result);
-                    }
-                }
-            }
-        }
-        catch (MuleException e)
-        {
-            throw new CouldNotRouteOutboundMessageException(event, routes.get(0), e);
-        }
-        return resultsHandler.aggregateResults(results, event, muleContext);
+        return true;
     }
 }
