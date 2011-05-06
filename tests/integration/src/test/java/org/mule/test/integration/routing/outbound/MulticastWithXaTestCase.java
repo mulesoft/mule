0f2ccecc86ea4404e736d7d94168f6276d7280ae
aperepel@gmail.com <Andrew Perepelytsya>
2011-05-06 18:05:02 +0000

    Testcase for MULE-5515
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@21834 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/integration/routing/outbound/MulticastWithXaTestCase.java
@@ -0,0 +1,38 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.integration.routing.outbound;
+
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+
+/**
+ * Make sure to run an external amq broker, otherwise the test isn't possible.
+ */
+public class MulticastWithXaTestCase extends FunctionalTestCase
+{
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/test/integration/routing/outbound/multicasting-router-xa-config.xml";
+    }
+
+    public void testName() throws Exception
+    {
+        MuleClient client = new MuleClient(muleContext);
+        MuleMessage msg = new DefaultMuleMessage("Hi", client.getMuleContext());
+        client.dispatch("jms://Myflow.input?connector=simpleJmsConnector", msg);
+        MuleMessage result = client.request("jms://Myflow.finishedOriginal?connector=simpleJmsConnector", 10000);
+        assertNotNull(result);
+    }
+}
