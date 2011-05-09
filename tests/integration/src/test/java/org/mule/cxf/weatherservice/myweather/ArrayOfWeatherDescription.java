fc5346215d7dbaaaf99c7d086ca22853188db095
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-05-09 09:59:16 +0000

    Merged revisions 21855 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.1.x
    ........
      r21855 | dirk.olmes | 2011-05-09 11:26:22 +0200 (Mon, 09 May 2011) | 2 lines
      
      add license header
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@21856 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/cxf/weatherservice/myweather/ArrayOfWeatherDescription.java
+++ tests/integration/src/test/java/org/mule/cxf/weatherservice/myweather/ArrayOfWeatherDescription.java
@@ -1,11 +1,22 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.cxf.weatherservice.myweather;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
--- tests/integration/src/test/java/org/mule/cxf/weatherservice/myweather/GetWeatherInformation.java
+++ tests/integration/src/test/java/org/mule/cxf/weatherservice/myweather/GetWeatherInformation.java
@@ -1,3 +1,13 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.cxf.weatherservice.myweather;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -25,7 +35,7 @@ import javax.xml.bind.annotation.XmlType;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "")
 @XmlRootElement(name = "GetWeatherInformation", namespace = "http://ws.cdyne.com/WeatherWS/")
-public class GetWeatherInformation {
-
-
+public class GetWeatherInformation 
+{
+    // no custom methods
 }
