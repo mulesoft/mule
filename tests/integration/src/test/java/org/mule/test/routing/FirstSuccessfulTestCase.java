e2a043e50eafb56ca3cfd5b155d0f681130f33e3
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-05-25 15:07:53 +0000

    MULE-5377 (First Successful message processor only usable with request-response endpoints): verify that errors during dispatch will cause trying the next endpoint
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@21972 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/routing/FirstSuccessfulTestCase.java
+++ tests/integration/src/test/java/org/mule/test/routing/FirstSuccessfulTestCase.java
@@ -13,7 +13,7 @@ package org.mule.test.routing;
 import org.mule.api.ExceptionPayload;
 import org.mule.api.MessagingException;
 import org.mule.api.MuleMessage;
-import org.mule.module.client.MuleClient;
+import org.mule.api.client.MuleClient;
 import org.mule.tck.FunctionalTestCase;
 
 public class FirstSuccessfulTestCase extends FunctionalTestCase
@@ -26,13 +26,16 @@ public class FirstSuccessfulTestCase extends FunctionalTestCase
 
     public void testFirstSuccessful() throws Exception
     {
-        MuleClient client = new MuleClient(muleContext);
+        MuleClient client = muleContext.getClient();
         MuleMessage response = client.send("vm://input", "XYZ", null);
         assertEquals("XYZ is a string", response.getPayloadAsString());
+
         response = client.send("vm://input", Integer.valueOf(9), null);
         assertEquals("9 is an integer", response.getPayloadAsString());
+
         response = client.send("vm://input", Long.valueOf(42), null);
         assertEquals("42 is a number", response.getPayloadAsString());
+
         response = client.send("vm://input", Boolean.TRUE, null);
         ExceptionPayload ep = response.getExceptionPayload();
         assertNotNull(ep);
@@ -42,14 +45,14 @@ public class FirstSuccessfulTestCase extends FunctionalTestCase
 
     public void testFirstSuccessfulWithExpression() throws Exception
     {
-        MuleClient client = new MuleClient(muleContext);
+        MuleClient client = muleContext.getClient();
         MuleMessage response = client.send("vm://input2", "XYZ", null);
         assertEquals("XYZ is a string", response.getPayloadAsString());
     }
 
     public void testFirstSuccessfulWithExpressionAllFail() throws Exception
     {
-        MuleClient client = new MuleClient(muleContext);
+        MuleClient client = muleContext.getClient();
         MuleMessage response = client.send("vm://input3", "XYZ", null);
         ExceptionPayload ep = response.getExceptionPayload();
         assertNotNull(ep);
@@ -57,18 +60,13 @@ public class FirstSuccessfulTestCase extends FunctionalTestCase
         assertTrue(ex instanceof MessagingException);
     }
 
-    /**
-     * Tests the First Successful message processor with one-way endpoints MULE-5377
-     * 
-     */
-    // public void testFirstSuccessfulWithOneWayEndpoints() throws Exception
-    // {
-    //     MuleClient client = new MuleClient(muleContext);
-    //     client.dispatch("vm://input4.in", "XYZ", null);
-    // 
-    //     MuleMessage response = client.request("vm://output4.out", RECEIVE_TIMEOUT);
-    //	   assertNotNull(response);
-    // 	   assertEquals(response.getPayload(), "XYZ");
-    // }
+    public void testFirstSuccessfulWithOneWayEndpoints() throws Exception
+    {
+        MuleClient client = muleContext.getClient();
+        client.dispatch("vm://input4.in", TEST_MESSAGE, null);
 
+        MuleMessage response = client.request("vm://output4.out", RECEIVE_TIMEOUT);
+        assertNotNull(response);
+        assertEquals(TEST_MESSAGE, response.getPayload());
+    }
 }
