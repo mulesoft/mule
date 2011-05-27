f25b33714d74d4115694f593aa8f9be928e5fd3a
Pablo Kraan <pablo.kraan@gmail.com>
2011-05-27 20:28:27 +0000

    Merged revisions 22014,22016 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.1.x
    ........
      r22014 | pablo.kraan | 2011-05-27 16:06:01 -0300 (Fri, 27 May 2011) | 1 line
      
      Moving to JUnit 4: adding base test classes
    ........
      r22016 | pablo.kraan | 2011-05-27 16:41:45 -0300 (Fri, 27 May 2011) | 1 line
      
      Moving to JUnit 4: adding base test classes (adding missing header)
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@22017 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/test/java/org/mule/tck/junit4/rule/WarnOnTimeout.java
@@ -0,0 +1,71 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.tck.junit4.rule;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.runners.model.Statement;
+
+/**
+ * Defines a {@link Statement} to execute a test with a given timeout.
+ * Differently from JUnit's {@link org.junit.internal.runners.statements.FailOnTimeout}
+ * this statement just prints a warning in the log, so the test will pass in
+ * case of timeout.
+ */
+public class WarnOnTimeout extends Statement
+{
+
+    private final Log logger = LogFactory.getLog(this.getClass());
+
+    private Statement next;
+    private final long timeout;
+    private boolean finished = false;
+    private Throwable thrown = null;
+
+    public WarnOnTimeout(Statement next, long timeout)
+    {
+        this.next = next;
+        this.timeout = timeout;
+    }
+
+    @Override
+    public void evaluate() throws Throwable
+    {
+        Thread thread = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    next.evaluate();
+                    finished = true;
+                }
+                catch (Throwable e)
+                {
+                    thrown = e;
+                }
+            }
+        };
+        thread.start();
+        thread.join(timeout);
+        if (finished)
+        {
+            return;
+        }
+        if (thrown != null)
+        {
+            throw thrown;
+        }
+
+        logger.warn("Timeout of " + timeout + "ms exceeded");
+    }
+}
