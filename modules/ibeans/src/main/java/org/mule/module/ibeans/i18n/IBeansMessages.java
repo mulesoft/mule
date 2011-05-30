f34fb9ddcb1b9dfb4d2cc8f033ae8c2af68fc808
Daniel Feist <dfeist@gmail.com>
2011-05-30 20:27:19 +0000

    Remove iBeans transport
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@22043 bf997673-6b11-0410-b953-e057580c5b09

--- modules/ibeans/src/main/java/org/mule/module/ibeans/i18n/IBeansMessages.java
+++ modules/ibeans/src/main/java/org/mule/module/ibeans/i18n/IBeansMessages.java
@@ -14,38 +14,12 @@ import org.mule.config.i18n.Message;
 import org.mule.config.i18n.MessageFactory;
 
 import java.lang.reflect.Method;
-import java.util.Arrays;
 
 public class IBeansMessages extends MessageFactory
 {
     private static final IBeansMessages FACTORY = new IBeansMessages();
 
     private static final String BUNDLE_PATH = getBundlePath("ibeans");
-
-    public static Message ibeanNotRegistered(String ibeanName)
-    {
-        return FACTORY.createMessage(BUNDLE_PATH, 1, ibeanName);
-    }
-
-    public static Message ibeanMethodFoundButNotValid(String ibeanName, String methodName)
-    {
-        return FACTORY.createMessage(BUNDLE_PATH, 2, ibeanName, methodName);
-    }
-
-    public static Message ibeanMethodNotFound(String ibeanName, String methodName)
-    {
-        return FACTORY.createMessage(BUNDLE_PATH, 3, ibeanName, methodName);
-    }
-
-    public static Message ibeanMethodNotFound(String ibeanName, String methodName, Class[] parameters)
-    {
-        return FACTORY.createMessage(BUNDLE_PATH, 4, methodName, Arrays.toString(parameters), ibeanName);
-    }
-
-    public static Message ibeanMethodParametersDoNotMatch(String iBeanName, String methodName, Class[] eventParams, Class[] methodParams)
-    {
-        return FACTORY.createMessage(BUNDLE_PATH, 5, methodName, Arrays.toString(eventParams), methodName, iBeanName, Arrays.toString(methodParams));
-    }
 
     public static Message illegalCallMethod(Method method)
     {
