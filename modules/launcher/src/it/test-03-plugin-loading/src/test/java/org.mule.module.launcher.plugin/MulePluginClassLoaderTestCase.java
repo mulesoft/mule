e35fa8c46b070d3139abbf2849b7d8fe7d648f49
aperepel@gmail.com <Andrew Perepelytsya>
2011-06-02 13:00:02 +0000

    Added a test when an override exists, but no child impl found
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@22073 bf997673-6b11-0410-b953-e057580c5b09

--- modules/launcher/src/it/test-03-plugin-loading/src/test/java/org.mule.module.launcher.plugin/MulePluginClassLoaderTestCase.java
+++ modules/launcher/src/it/test-03-plugin-loading/src/test/java/org.mule.module.launcher.plugin/MulePluginClassLoaderTestCase.java
@@ -52,7 +52,7 @@ public class MulePluginClassLoaderTestCase extends AbstractMuleTestCase
     /**
      * Child impl2 overrides parent and says 'Bye'.
      */
-    public void testChildPackageOverrides() throws Exception
+    public void testPackageOverrideChild() throws Exception
     {
 
         // load compiled classes from the previously built test modules
@@ -77,5 +77,29 @@ public class MulePluginClassLoaderTestCase extends AbstractMuleTestCase
         final Method methodHi = c.getMethod("hi");
         final Object result = methodHi.invoke(c.newInstance());
         assertEquals("Wrong implementation loaded", "Bye", result);
+    }
+
+    /**
+     * Child impl2 overrides parent, but no child impl available (fallback to parent)
+     */
+    public void testPackageOverrideChildClassMissing() throws Exception
+    {
+
+        // load compiled classes from the previously built test modules
+        final URL classPathRoot = ClassUtils.getClassPathRoot(getClass());
+        System.out.println("classPathRoot = " + classPathRoot);
+        File f = new File(classPathRoot.getPath(), "../../../test-01-plugin-impl-1/target/classes/");
+        System.out.println("f = " + f);
+        assertTrue("Dependent test classes not found, has required IT test modules been run before?", f.exists());
+        URL[] parentUrls = new URL[] {f.toURI().toURL()};
+        URLClassLoader parent = new URLClassLoader(parentUrls, Thread.currentThread().getContextClassLoader());
+
+        MulePluginClassLoader ext = new MulePluginClassLoader(new URL[0], parent);
+        // child will override all classes in 'mypackage'
+        ext.overrides = new String[] {"mypackage"};
+        Class c = ext.loadClass("mypackage.SneakyChatter");
+        final Method methodHi = c.getMethod("hi");
+        final Object result = methodHi.invoke(c.newInstance());
+        assertEquals("Wrong implementation loaded", "Hello", result);
     }
 }
