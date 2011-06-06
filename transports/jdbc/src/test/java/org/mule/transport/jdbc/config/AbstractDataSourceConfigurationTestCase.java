993def2c02362b1649463b79303113c9a6afba61
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-06-06 11:10:29 +0000

    add test for oracle DS that checks the attribute groups
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@22119 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/jdbc/src/test/java/org/mule/transport/jdbc/config/AbstractDataSourceConfigurationTestCase.java
@@ -0,0 +1,31 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.jdbc.config;
+
+import org.mule.api.MuleException;
+import org.mule.api.config.ConfigurationBuilder;
+import org.mule.api.context.MuleContextFactory;
+import org.mule.config.spring.SpringXmlConfigurationBuilder;
+import org.mule.context.DefaultMuleContextFactory;
+import org.mule.tck.junit4.AbstractMuleTestCase;
+
+import static org.junit.Assert.fail;
+
+public class AbstractDataSourceConfigurationTestCase extends AbstractMuleTestCase
+{
+    protected void tryBuildingMuleContextFromInvalidConfig(String config) throws MuleException
+    {
+        MuleContextFactory muleContextFactory = new DefaultMuleContextFactory();
+        ConfigurationBuilder builder = new SpringXmlConfigurationBuilder(config);
+        muleContextFactory.createMuleContext(builder);
+        fail();
+    }
+}
