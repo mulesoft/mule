4f375d5d37de598c5a4fc7e4016e52b166853cad
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-06-10 07:14:16 +0000

    give the test some love and use a Prober with a longer timeout
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@22167 bf997673-6b11-0410-b953-e057580c5b09

--- transports/email/src/test/java/org/mule/transport/email/functional/ImapFunctionalWithAttachmentsTestCase.java
+++ transports/email/src/test/java/org/mule/transport/email/functional/ImapFunctionalWithAttachmentsTestCase.java
@@ -10,8 +10,12 @@
 
 package org.mule.transport.email.functional;
 
+import org.mule.tck.probe.PollingProber;
+import org.mule.tck.probe.Probe;
+import org.mule.tck.probe.Prober;
 import org.mule.util.FileUtils;
 import org.mule.util.IOUtils;
+import org.mule.util.SystemUtils;
 
 import com.icegreen.greenmail.util.GreenMailUtil;
 
@@ -22,60 +26,89 @@ import javax.mail.Message;
 
 public class ImapFunctionalWithAttachmentsTestCase extends AbstractEmailFunctionalTestCase
 {
-    private static final String configFile = "email-attachment-save.xml";
+    private static final String CONFIG_FILE = "email-attachment-save.xml";
+    private File saveDir;
 
     public ImapFunctionalWithAttachmentsTestCase()
     {
-        super(true, "imap", configFile, true);
+        super(true, "imap", CONFIG_FILE, true);
         setStartContext(false);
+        
         // set up email properties for mule config
         System.setProperty("mail.user", DEFAULT_USER);
         System.setProperty("mail.password", DEFAULT_PASSWORD);
         System.setProperty("mail.host", "localhost");
-        System.setProperty("mail.save.dir", System.getProperty("java.io.tmpdir") + File.separatorChar
-                                            + this.getClass().getName());
+        
+        saveDir = new File(SystemUtils.JAVA_IO_TMPDIR, getClass().getName());
+        System.setProperty("mail.save.dir", saveDir.getAbsolutePath());
     }
 
     @Override
     protected void doSetUp() throws Exception
     {
         super.doSetUp();
-        // clear out destination directory
-        FileUtils.deleteTree(new File(System.getProperty("mail.save.dir")));
+    }
+    
+    @Override
+    protected void doTearDown() throws Exception
+    {
+        super.doTearDown();
+        clearAttachmentsSaveDirectory();
     }
 
+    private void clearAttachmentsSaveDirectory()
+    {
+        FileUtils.deleteTree(saveDir);
+    }
+    
     public void testRequest() throws Exception
     {
         // we do this since we need to set system mail properties before starting
         // mule since they are referenced in there
         muleContext.start();
+        
         // a test email gets sent by default
         assertEquals(1, server.getReceivedMessages().length);
 
-        InputStream inputstream = IOUtils.getResourceAsStream(configFile, getClass(), true, false);
+        InputStream inputstream = IOUtils.getResourceAsStream(CONFIG_FILE, getClass(), true, false);
         assertNotNull(inputstream);
         byte[] byteArray = IOUtils.toByteArray(inputstream);
 
         // send a file attachment email
         GreenMailUtil.sendAttachmentEmail(DEFAULT_EMAIL, "joe", "email subject with attachments", "",
-            byteArray, "text/xml", configFile, "description", smtpSetup);
+            byteArray, "text/xml", CONFIG_FILE, "description", smtpSetup);
         Message[] messages = server.getReceivedMessages();
         assertEquals(2, messages.length);
 
-        // need time for the email to process thru mule
-        Thread.sleep(5000);
+        assertAttachmentWasSaved();
+    }
+    
+    private void assertAttachmentWasSaved()
+    {
+        Prober prober = new PollingProber(10000, 100);
+        prober.check(new Probe()
+        {
+            public boolean isSatisfied()
+            {
+                // FIXME DZ: don't know why these are empty, so just compare the saved email
+                // file to the expected content
+                // assertEquals(messages[1].ATTACHMENT, messages[1].getDisposition());
+                // assertEquals("email-attachment-save.xml", messages[1].getFileName());
 
-        // FIXME DZ: don't know why these are empty, so just compare the saved email
-        // file to the expected content
-        // assertEquals(messages[1].ATTACHMENT, messages[1].getDisposition());
-        // assertEquals("email-attachment-save.xml", messages[1].getFileName());
-        File savedFile = new File(System.getProperty("mail.save.dir") + File.separatorChar + configFile);
-        assertTrue(savedFile.exists());
+                File savedFile = new File(saveDir, CONFIG_FILE);
+                return savedFile.exists();
 
-        // TODO DZ: compare the source and target files and makes sure they are the
-        // same
-        // FIXME DF: when you send an attachment email with text in the body, this
-        // mule config saves the body in it's own file; how do you just save the file
-        // attachment?
+                // TODO DZ: compare the source and target files and makes sure they are the
+                // same
+                // FIXME DF: when you send an attachment email with text in the body, this
+                // mule config saves the body in it's own file; how do you just save the file
+                // attachment?
+            }
+
+            public String describeFailure()
+            {
+                return "No attachments were saved";
+            }
+        });
     }
 }
