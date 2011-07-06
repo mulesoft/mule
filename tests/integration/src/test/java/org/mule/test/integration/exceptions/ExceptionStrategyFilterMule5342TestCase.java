2e0712023f73c145201838169e6e838aae977dfa
Evangelina Martinez Ruiz Moreno <evangelina.martinez@mulesource.com>
2011-07-06 15:32:08 +0000

    MULE-5342: message-filter throwOnUnaccepted=true not handled by default-exception-strategy
    This issue was fixed by changes on EE-2291 so I added a new junit to test that the functionality is working
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@22340 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/integration/exceptions/ExceptionStrategyFilterMule5342TestCase.java
@@ -0,0 +1,50 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.integration.exceptions;
+
+import org.mule.api.MuleMessage;
+import org.mule.api.client.MuleClient;
+import org.mule.api.routing.filter.Filter;
+import org.mule.message.ExceptionMessage;
+import org.mule.tck.FunctionalTestCase;
+
+import org.junit.Test;
+
+public class ExceptionStrategyFilterMule5342TestCase extends FunctionalTestCase
+{
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/test/integration/exceptions/exception-strategy-filter-mule-5342.xml";
+    }
+
+    @Test
+    public void testExceptionThrownFromMessageFilterIsHandledByExceptionHandler() throws Exception
+    {
+        MuleClient client = muleContext.getClient();
+        client.send("vm://in", TEST_MESSAGE, null);
+        MuleMessage handleException = client.request("vm://handleException", RECEIVE_TIMEOUT);
+        assertNotNull(handleException);
+        assertTrue(handleException.getPayload() instanceof ExceptionMessage);
+        assertEquals(TEST_MESSAGE, ((ExceptionMessage)handleException.getPayload()).getPayload());
+
+    }
+
+    public static class FalseFilter implements Filter
+    {
+        public boolean accept(MuleMessage message)
+        {
+            return false;
+        }
+    }
+
+
+}
