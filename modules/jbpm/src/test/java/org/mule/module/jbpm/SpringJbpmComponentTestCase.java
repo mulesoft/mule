ff4405a4a502bc2b1079ad28760173343a8350c0
justin.calleja <justin.calleja>
2011-07-20 09:28:00 +0000

    Added flow configuration; Changed test case to extend from AbstractServiceAndFlowTestCase; Changed test case to load the 2 configs.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@22470 bf997673-6b11-0410-b953-e057580c5b09

--- modules/jbpm/src/test/java/org/mule/module/jbpm/SpringJbpmComponentTestCase.java
+++ modules/jbpm/src/test/java/org/mule/module/jbpm/SpringJbpmComponentTestCase.java
@@ -10,32 +10,41 @@
 
 package org.mule.module.jbpm;
 
-import org.mule.api.MuleMessage;
-import org.mule.api.client.MuleClient;
-import org.mule.module.bpm.BPMS;
-import org.mule.module.bpm.Process;
-import org.mule.tck.junit4.FunctionalTestCase;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
+import org.mule.api.MuleMessage;
+import org.mule.api.client.MuleClient;
+import org.mule.module.bpm.BPMS;
+import org.mule.module.bpm.Process;
+import org.mule.tck.AbstractServiceAndFlowTestCase;
+
 /**
- * Tests jBPM component with a simple process.  The ProcessEngine is built by Spring and injected into the Mule wrapper.
+ * Tests jBPM component with a simple process. The ProcessEngine is built by Spring
+ * and injected into the Mule wrapper.
  */
-public class SpringJbpmComponentTestCase extends FunctionalTestCase
+public class SpringJbpmComponentTestCase extends AbstractServiceAndFlowTestCase
 {
 
-    @Override
-    protected String getConfigResources()
+    public SpringJbpmComponentTestCase(ConfigVariant variant, String configResources)
     {
-        return "spring-jbpm-component.xml";
+        super(variant, configResources);
+    }
+
+    @Parameters
+    public static Collection<Object[]> parameters()
+    {
+        return Arrays.asList(new Object[][]{{ConfigVariant.SERVICE, "spring-jbpm-component-service.xml"},
+            {ConfigVariant.FLOW, "spring-jbpm-component-flow.xml"}});
     }
 
     @Test
@@ -49,7 +58,7 @@ public class SpringJbpmComponentTestCase extends FunctionalTestCase
         MuleMessage response = client.send("vm://simple", "data", null);
         Object process = response.getPayload();
 
-        String processId = (String)bpms.getId(process);
+        String processId = (String) bpms.getId(process);
         // The process should be started and in a wait state.
         assertFalse(processId == null);
         assertEquals("dummyState", bpms.getState(process));
