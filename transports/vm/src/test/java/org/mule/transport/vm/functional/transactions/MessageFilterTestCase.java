5c1eda5edff191ba756015b3912a3fcf4fb45f79
justin.calleja <justin.calleja>
2011-07-22 11:42:50 +0000

    Added flow configuration; Changed test case to extend from AbstractServiceAndFlowTestCase; Changed test case to load the 2 configs.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@22527 bf997673-6b11-0410-b953-e057580c5b09

--- transports/vm/src/test/java/org/mule/transport/vm/functional/transactions/MessageFilterTestCase.java
+++ transports/vm/src/test/java/org/mule/transport/vm/functional/transactions/MessageFilterTestCase.java
@@ -10,37 +10,49 @@
 
 package org.mule.transport.vm.functional.transactions;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
 import org.mule.api.DefaultMuleException;
 import org.mule.api.MuleEvent;
 import org.mule.api.MuleException;
 import org.mule.api.MuleMessage;
 import org.mule.api.processor.MessageProcessor;
 import org.mule.module.client.MuleClient;
-import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.AbstractServiceAndFlowTestCase;
 import org.mule.transport.NullPayload;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
-/** Test transaction behavior when "joinExternal" is set to disallow joining external transactions
- * There is one test per legal transactional behavior (e.g. ALWAYS_BEGIN).
+/**
+ * Test transaction behavior when "joinExternal" is set to disallow joining external
+ * transactions There is one test per legal transactional behavior (e.g.
+ * ALWAYS_BEGIN).
  */
-public class MessageFilterTestCase extends FunctionalTestCase
+public class MessageFilterTestCase extends AbstractServiceAndFlowTestCase
 {
 
     protected static final Log logger = LogFactory.getLog(MessageFilterTestCase.class);
 
     private static String rejectMesage;
 
-    @Override
-    protected String getConfigResources()
+    public MessageFilterTestCase(ConfigVariant variant, String configResources)
     {
-        return "org/mule/test/config/message-filter-config.xml";
+        super(variant, configResources);
+    }
+
+    @Parameters
+    public static Collection<Object[]> parameters()
+    {
+        return Arrays.asList(new Object[][]{
+            {ConfigVariant.SERVICE, "org/mule/test/config/message-filter-config-service.xml"},
+            {ConfigVariant.FLOW, "org/mule/test/config/message-filter-config-flow.xml"}});
     }
 
     /** Check that the configuration specifies considers external transactions */
@@ -48,20 +60,20 @@ public class MessageFilterTestCase extends FunctionalTestCase
     public void testConfiguration() throws Exception
     {
         MuleClient client = new MuleClient(muleContext);
-        
+
         MuleMessage response = client.send("vm://order.validation", "OK", null);
         assertTrue(response.getPayload() instanceof NullPayload);
         assertEquals("OK(rejected!-1)", rejectMesage);
-        
+
         response = client.send("vm://order.validation", "OK-ABC", null);
         assertTrue(response.getPayload() instanceof NullPayload);
         assertEquals("OK-ABC(rejected!-2)", rejectMesage);
-        
+
         response = client.send("vm://order.validation", "OK-DEF", null);
         assertTrue(response.getPayload() instanceof NullPayload);
         assertEquals("OK-DEF(rejected!-1)", rejectMesage);
         rejectMesage = null;
-        
+
         response = client.send("vm://order.validation", "OK-ABC-DEF", null);
         assertEquals("OK-ABC-DEF(success)", response.getPayloadAsString());
         assertNull(rejectMesage);
@@ -89,11 +101,11 @@ public class MessageFilterTestCase extends FunctionalTestCase
         }
     }
 
-     public static class Reject2 implements MessageProcessor
-     {
-         public void setName(String name)
-         {
-         }
+    public static class Reject2 implements MessageProcessor
+    {
+        public void setName(String name)
+        {
+        }
 
         public MuleEvent process(MuleEvent event) throws MuleException
         {
