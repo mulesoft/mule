3d6aa24270ea2a32c2451c5abf803489eb18c208
justin.calleja <justin.calleja>
2011-08-17 11:44:57 +0000

    A test case to test flow-ref and show the possible bug present in the testFlowRef() test in the test case FlowConfigurationFunctionalTestCase. testFlowRef() expects "012xyzabc3" as a result, though because the config (flow.xml) references the flow which appends strings twice, "012xyzabc312xyzabc3" should be the result. It seems there are some problems when using flow-ref to reference a flow (as is done in flow.xml) instead of a sub-flow. Or maybe it's because of the use of the processor-chain instead of a sub-flow. Either way, the expected behavior is performed by the FlowRefTestCase in this commit by flow-ref to a sub-flow instead of a flow and using flow-ref to another sub-flow inside that sub-flow instead of using a processor ref to a processor-chain.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@22694 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/construct/FlowRefTestCase.java
@@ -0,0 +1,42 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.construct;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleMessage;
+import org.mule.api.client.MuleClient;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+public class FlowRefTestCase extends FunctionalTestCase
+{
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/test/construct/flow-ref.xml";
+    }   
+
+    @Test
+    public void testTwoFlowRefsToSubFlow() throws Exception
+    {
+        MuleClient client = muleContext.getClient();
+
+        MuleMessage msg = client.send("vm://two.flow.ref.to.sub.flow",
+            new DefaultMuleMessage("0", muleContext));
+        
+        assertEquals("012xyzabc312xyzabc3", msg.getPayloadAsString());        
+        
+    }
+
+}
