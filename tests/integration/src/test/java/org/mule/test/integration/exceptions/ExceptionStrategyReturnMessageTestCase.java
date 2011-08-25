6fa90a2c7f50779fc3d95582d4cc3a6bab62c57a
Daniel Feist <dfeist@gmail.com>
2011-08-25 22:03:54 +0000

    Merged revisions 22745 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.1.x
    ........
      r22745 | dfeist | 2011-08-25 18:58:18 -0300 (Thu, 25 Aug 2011) | 1 line
      
      Update test case to assert that the payload returned by DefaultExceptionStrategy in synchronous service is in fact 'NullPayload'
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@22747 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/exceptions/ExceptionStrategyReturnMessageTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/exceptions/ExceptionStrategyReturnMessageTestCase.java
@@ -10,17 +10,17 @@
 
 package org.mule.test.integration.exceptions;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import org.mule.api.MuleMessage;
 import org.mule.module.client.MuleClient;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.transport.NullPayload;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-//TODO This test case is illogical because if the request is end-to-end synchronous, no exception strategy
-//will be called, the exception is simple returned to the client.
 public class ExceptionStrategyReturnMessageTestCase extends FunctionalTestCase
 {
     @Override
@@ -30,10 +30,24 @@ public class ExceptionStrategyReturnMessageTestCase extends FunctionalTestCase
     }
 
     @Test
-    public void testExceptionMessage() throws Exception
+    public void testReturnPayloadDefaultStrategy() throws Exception
     {
         MuleClient client = new MuleClient(muleContext);
-        MuleMessage msg = client.send("vm://in", "Test Message", null);
+        MuleMessage msg = client.send("vm://in-default-strategy", "Test Message", null);
+
+        assertNotNull(msg);
+        assertNotNull(msg.getExceptionPayload());
+        assertEquals("Functional Test Service Exception", msg.getExceptionPayload().getMessage());
+
+        assertNotNull(msg.getPayload());
+        assertTrue(msg.getPayload() instanceof NullPayload);
+    }
+
+    @Test
+    public void testReturnPayloadCustomStrategy() throws Exception
+    {
+        MuleClient client = new MuleClient(muleContext);
+        MuleMessage msg = client.send("vm://in-custom-strategy", "Test Message", null);
 
         assertNotNull(msg);
         assertNotNull(msg.getExceptionPayload());
