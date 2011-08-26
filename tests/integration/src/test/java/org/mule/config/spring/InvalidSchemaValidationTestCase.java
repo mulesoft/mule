03a395dd2d037316d5be7652062a507265662dfd
Evangelina Martinez Ruiz Moreno <evangelina.martinez@mulesource.com>
2011-08-26 12:25:46 +0000

    Changes to mule schema to address issues:
    MULE-5651: No information displayed for some transformers due to type attribute missing from schema - XSLT not detecting inheritance
    MULE-5668: Schema inconsistencies - File connector contains transaction options because of schema inheritance
    MULE-5670: Schema issue - Incorrect Inheritance for Transformer Reference
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@22754 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/config/spring/InvalidSchemaValidationTestCase.java
@@ -0,0 +1,50 @@
+package org.mule.config.spring;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.After;
+import org.xml.sax.SAXException;
+import org.apache.commons.collections.map.HashedMap;
+
+public class InvalidSchemaValidationTestCase extends AbstractSchemaValidationTestCase
+{
+    @After
+    public void doCleanUp()
+    {
+        schemas = new HashedMap();
+    }
+
+    @Test(expected = SAXException.class)
+    public void testTransformerReference() throws SAXException, IOException
+    {
+        addSchema("http://www.mulesoft.org/schema/mule/vm","META-INF/mule-vm.xsd");
+        doTest("org/mule/config/spring/schema-validation-transformer-ref-test.xml");
+    }
+
+    @Test
+    public void testTransactedConnectors() throws SAXException, IOException
+    {
+        addSchema("http://www.mulesoft.org/schema/mule/vm","META-INF/mule-vm.xsd");
+        addSchema("http://www.mulesoft.org/schema/mule/jdbc","META-INF/mule-jdbc.xsd");
+        addSchema("http://www.springframework.org/schema/beans","http://www.springframework.org/schema/beans/spring-beans-3.0.xsd");
+        doTest("org/mule/config/spring/schema-validation-transacted-connectors-test.xml");
+    }
+
+    @Test(expected = SAXException.class)
+    public void testNotTransactedFileConnector() throws SAXException, IOException
+    {
+        addSchema("http://www.mulesoft.org/schema/mule/file", "META-INF/mule-file.xsd");
+        doTest("org/mule/config/spring/schema-validation-not-transacted-file-connector-test.xml");
+    }
+
+    @Test(expected = SAXException.class)
+    public void testNotTransactedFtpConnector() throws SAXException, IOException
+    {
+        addSchema("http://www.mulesoft.org/schema/mule/ftp", "META-INF/mule-ftp.xsd");
+        doTest("org/mule/config/spring/schema-validation-not-transacted-ftp-connector-test.xml");
+    }
+
+
+
+}
