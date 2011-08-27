09c28caabeab37a80f20c8a62e3d6dcdd584c0b8
Mariano Capurro <mcapurro@gmail.com>
2011-08-27 17:43:28 +0000

    Fixed format and checkstyle
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@22775 bf997673-6b11-0410-b953-e057580c5b09

--- examples/mule-example-launcher/src/main/java/org/mule/example/launcher/HTTPResponse.java
+++ examples/mule-example-launcher/src/main/java/org/mule/example/launcher/HTTPResponse.java
@@ -12,36 +12,32 @@ package org.mule.example.launcher;
 
 import org.codehaus.jackson.annotate.JsonProperty;
 
-/**
- * @author mariano
- *
- */
 public class HTTPResponse
 {
-	@JsonProperty
-	private String response;
+    @JsonProperty
+    private String response;
 
-	/**
+    /**
 	 *
 	 */
-	public HTTPResponse()
-	{
-		this(null);
-	}
+    public HTTPResponse()
+    {
+        this(null);
+    }
 
-	public HTTPResponse(String response)
-	{
-		super();
-		setResponse(response);
-	}
+    public HTTPResponse(String response)
+    {
+        super();
+        setResponse(response);
+    }
 
-	public String getResponse()
-	{
-		return response;
-	}
+    public String getResponse()
+    {
+        return response;
+    }
 
-	public void setResponse(String response)
-	{
-		this.response = response;
-	}
+    public void setResponse(String response)
+    {
+        this.response = response;
+    }
 }
--- examples/mule-example-launcher/src/main/java/org/mule/example/launcher/rss/EntryReceiver.java
+++ examples/mule-example-launcher/src/main/java/org/mule/example/launcher/rss/EntryReceiver.java
@@ -1,10 +1,14 @@
-/**
- * 
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
  */
 package org.mule.example.launcher.rss;
 
-//import org.apache.abdera.model.Entry;
-//import org.apache.abdera.model.Feed;
 import java.util.Iterator;
 import java.util.List;
 
@@ -20,68 +24,74 @@ import com.sun.syndication.feed.synd.SyndEnclosure;
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 
-/**
- * @author mariano
- *
- */
-public class EntryReceiver {
-	/**
+public class EntryReceiver
+{
+    /**
 	 * 
 	 */
-	public EntryReceiver() {
-	}
+    public EntryReceiver()
+    {
+    }
 
-	@SuppressWarnings("rawtypes")
-	public String processFeed(@Payload SyndFeed feed) throws Exception
-	{
-		StringBuilder feedXml = new StringBuilder();
-		feedXml.append("<muleforge-extensions>");
-		
-		for(Iterator it = feed.getEntries().iterator(); it.hasNext(); ) {
-			SyndEntry entry = (SyndEntry) it.next();
-			processItem(feedXml, entry);
-		}
-		feedXml.append("</muleforge-extensions>");
-		
-		return feedXml.toString();
-	}	
-	
-	private void processItem(StringBuilder feedXml, SyndEntry entry)
-	{
-		feedXml.append("<muleforge-extension>");
-		feedXml.append("<title>" + StringEscapeUtils.escapeXml(entry.getTitle()) + "</title>");
-		feedXml.append("<link>" + StringEscapeUtils.escapeXml(entry.getLink()) + "</link>");
-		if(!entry.getEnclosures().isEmpty())
-		{
-			feedXml.append("<image-url>" + StringEscapeUtils.escapeXml(((SyndEnclosure)entry.getEnclosures().get(0)).getUrl()) +"</image-url>");
-		}
-		parseDescription(feedXml, entry);
-		feedXml.append("</muleforge-extension>");
-	}
+    @SuppressWarnings("rawtypes")
+    public String processFeed(@Payload SyndFeed feed) throws Exception
+    {
+        StringBuilder feedXml = new StringBuilder();
+        feedXml.append("<muleforge-extensions>");
 
-	private void parseDescription(StringBuilder feedXml, SyndEntry entry)
-	{
-		Source source = new Source(entry.getDescription().getValue());
-		
-		List<Element> anchors = source.getAllElements(HTMLElementName.A);
-		if(anchors != null)
-		{
-			for(Element a : anchors)
-			{
-				if("documentation-url".equalsIgnoreCase(a.getAttributeValue("id")))
-				{
-					feedXml.append("<documentation-url>" + StringEscapeUtils.escapeXml(a.getAttributeValue("href")) +"</documentation-url>");
-				}
-				else if("source-url".equalsIgnoreCase(a.getAttributeValue("id")))
-				{
-					feedXml.append("<source-url>" + StringEscapeUtils.escapeXml(a.getAttributeValue("href")) +"</source-url>");
-				}
-				else if("download-url".equalsIgnoreCase(a.getAttributeValue("id")))
-				{
-					feedXml.append("<download-url>" + StringEscapeUtils.escapeXml(a.getAttributeValue("href")) +"</download-url>");
-				}
-			}
-		}
-		
-	}
+        for (Iterator it = feed.getEntries().iterator(); it.hasNext();)
+        {
+            SyndEntry entry = (SyndEntry) it.next();
+            processItem(feedXml, entry);
+        }
+        feedXml.append("</muleforge-extensions>");
+
+        return feedXml.toString();
+    }
+
+    private void processItem(StringBuilder feedXml, SyndEntry entry)
+    {
+        feedXml.append("<muleforge-extension>");
+        feedXml.append("<title>" + StringEscapeUtils.escapeXml(entry.getTitle()) + "</title>");
+        feedXml.append("<link>" + StringEscapeUtils.escapeXml(entry.getLink()) + "</link>");
+        if (!entry.getEnclosures().isEmpty())
+        {
+            feedXml.append("<image-url>"
+                           + StringEscapeUtils.escapeXml(((SyndEnclosure) entry.getEnclosures().get(0)).getUrl())
+                           + "</image-url>");
+        }
+        parseDescription(feedXml, entry);
+        feedXml.append("</muleforge-extension>");
+    }
+
+    private void parseDescription(StringBuilder feedXml, SyndEntry entry)
+    {
+        Source source = new Source(entry.getDescription().getValue());
+
+        List<Element> anchors = source.getAllElements(HTMLElementName.A);
+        if (anchors != null)
+        {
+            for (Element a : anchors)
+            {
+                if ("documentation-url".equalsIgnoreCase(a.getAttributeValue("id")))
+                {
+                    feedXml.append("<documentation-url>"
+                                   + StringEscapeUtils.escapeXml(a.getAttributeValue("href"))
+                                   + "</documentation-url>");
+                }
+                else if ("source-url".equalsIgnoreCase(a.getAttributeValue("id")))
+                {
+                    feedXml.append("<source-url>" + StringEscapeUtils.escapeXml(a.getAttributeValue("href"))
+                                   + "</source-url>");
+                }
+                else if ("download-url".equalsIgnoreCase(a.getAttributeValue("id")))
+                {
+                    feedXml.append("<download-url>"
+                                   + StringEscapeUtils.escapeXml(a.getAttributeValue("href"))
+                                   + "</download-url>");
+                }
+            }
+        }
+
+    }
 }
