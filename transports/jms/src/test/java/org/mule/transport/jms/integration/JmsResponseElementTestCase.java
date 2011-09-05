43042aba6995925275c68f80738540729476d179
Pablo La Greca <pablolagreca@hotmail.com>
2011-09-05 19:06:54 +0000

    Merged revisions 22596 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.1.x
    ........
      r22596 | pablo.lagreca | 2011-08-05 15:52:21 -0300 (Fri, 05 Aug 2011) | 1 line
      
      EE-2363 - Adding support for reseponse element in flow configuration for jms
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@22868 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/jms/src/test/java/org/mule/transport/jms/integration/JmsResponseElementTestCase.java
@@ -0,0 +1,41 @@
+/*
+ * $Id: ReplyToParameterProcessor.java 22578 2011-07-31 18:37:52Z mike.schilling $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.transport.jms.integration;
+
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsNull;
+import org.junit.Test;
+import org.mule.api.MuleMessage;
+import org.mule.api.transport.PropertyScope;
+import org.mule.module.client.MuleClient;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+    public class JmsResponseElementTestCase extends AbstractJmsFunctionalTestCase {
+
+    public static final String MESSAGE = "A Message";
+    public static final String EXPECTED_MODIFIED_MESSAGE = "A Message jms flow content";
+
+    @Override
+    protected String getConfigResources() {
+        return "integration/jms-response-element.xml";
+    }
+
+    @Test
+    public void testOutboundEndopintResponse() throws Exception {
+        MuleClient client = new MuleClient(muleContext);
+        MuleMessage response = client.send("vm://vminbound", "some message", null);
+        assertThat(response.getPayloadAsString(), is(EXPECTED_MODIFIED_MESSAGE));
+        assertThat(response.<String>getProperty("test", PropertyScope.INBOUND),Is.is("test"));
+        assertThat(response.getExceptionPayload(), IsNull.<Object>nullValue());
+    }
+
+}
