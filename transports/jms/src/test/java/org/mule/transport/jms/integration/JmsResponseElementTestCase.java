373cebc4a9cdbd978d6ec5d35e61e5a715f904c7
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-09-13 08:04:12 +0000

    fix svn properties
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.2.x@22925 bf997673-6b11-0410-b953-e057580c5b09

--- transports/jms/src/test/java/org/mule/transport/jms/integration/JmsResponseElementTestCase.java
+++ transports/jms/src/test/java/org/mule/transport/jms/integration/JmsResponseElementTestCase.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ReplyToParameterProcessor.java 22578 2011-07-31 18:37:52Z mike.schilling $
+ * $Id$
  * --------------------------------------------------------------------------------------
  * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
  *
