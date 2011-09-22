6d257b7d5b43dfaf1818ba7f7b11b0264001aa26
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-09-22 07:58:14 +0000

    Merged revisions 23006 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.2.x
    ................
      r23006 | dirk.olmes | 2011-09-22 07:51:15 +0200 (Thu, 22 Sep 2011) | 9 lines
      
      Merged revisions 23005 via svnmerge from 
      https://svn.codehaus.org/mule/branches/mule-3.1.x
      
      ........
        r23005 | dirk.olmes | 2011-09-22 05:36:08 +0200 (Thu, 22 Sep 2011) | 2 lines
        
        MULE-5744 (NPE using a filter-ref in a when element)
      ........
    ................
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@23008 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/routing/outbound/ChoiceRouterTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/routing/outbound/ChoiceRouterTestCase.java
@@ -10,33 +10,35 @@
 
 package org.mule.test.integration.routing.outbound;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
+import org.mule.api.MuleException;
 import org.mule.api.MuleMessage;
+import org.mule.api.construct.FlowConstruct;
+import org.mule.api.endpoint.OutboundEndpoint;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.api.processor.MessageProcessorChain;
 import org.mule.api.routing.RoutePathNotFoundException;
-import org.mule.module.client.MuleClient;
+import org.mule.api.routing.filter.Filter;
+import org.mule.construct.Flow;
+import org.mule.routing.ChoiceRouter;
+import org.mule.routing.MessageProcessorFilterPair;
+import org.mule.routing.filters.ExpressionFilter;
+import org.mule.routing.filters.RegExFilter;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class ChoiceRouterTestCase extends FunctionalTestCase
 {
     private static final String WITH_DEFAULT_ROUTE_CHANNEL = "vm://with-default-route.in";
     private static final String WITHOUT_DEFAULT_ROUTE_CHANNEL = "vm://without-default-route.in";
 
-    private MuleClient muleClient;
-    
     public ChoiceRouterTestCase()
     {
         setDisposeContextPerClass(true);
-    }
-
-    @Override
-    protected void doSetUp() throws Exception
-    {
-        super.doSetUp();
-        muleClient = new MuleClient(muleContext);
     }
 
     @Override
@@ -45,42 +47,132 @@ public class ChoiceRouterTestCase extends FunctionalTestCase
         return "org/mule/test/integration/routing/outbound/choice-router-test.xml";
     }
 
+    /**
+     * Check that the choice router was assembled correctly by the BDP machinery
+     */
     @Test
-    public void testNoRouteFound() throws Exception
+    public void filterReferenceShouldCreateFilterWithRegexFilterAndOutboundEndpointChain()
     {
-        MuleMessage message = muleClient.send(WITHOUT_DEFAULT_ROUTE_CHANNEL, "bad", null);
-        assertNotNull(message);
-        assertNotNull(message.getExceptionPayload());
-        assertEquals(RoutePathNotFoundException.class, message.getExceptionPayload()
-            .getRootException()
-            .getClass());
+        ChoiceRouter router = findChoiceRouterInFlow("without-default-route");
+        MessageProcessorFilterPair pair = router.getConditionalMessageProcessors().get(0);
+        assertIsRegExFilterWithPattern(pair.getFilter(), "apple");
+        assertMessageChainIsOutboundEndpoint(pair.getMessageProcessor(), "vm://fruit-channel.in");
+    }
+
+    /**
+     * Check that the choice router was assembled correctly by the BDP machinery
+     */
+    @Test
+    public void embeddedFilterShouldCreatePairWithFilterAndOtherConfiguredMPsAsChain()
+    {
+        ChoiceRouter router = findChoiceRouterInFlow("with-default-route");
+
+        MessageProcessorFilterPair firstPair = router.getConditionalMessageProcessors().get(0);
+        assertIsRegExFilterWithPattern(firstPair.getFilter(), "apple");
+        assertMessageChainIsOutboundEndpoint(firstPair.getMessageProcessor(), "vm://fruit-channel.in");
+
+        MessageProcessorFilterPair secondPair = router.getConditionalMessageProcessors().get(1);
+        assertIsRegExFilterWithPattern(secondPair.getFilter(), "turnip");
+        assertMessageChainIsOutboundEndpoint(secondPair.getMessageProcessor(), "vm://veggie-channel.in");
+
+        MessageProcessorFilterPair thirdPair = router.getConditionalMessageProcessors().get(2);
+        assertIsExpressionFilterWithExpressionAndEvaluator(thirdPair.getFilter(), ".*berry", "regex");
+        assertMessageChainIsOutboundEndpoint(thirdPair.getMessageProcessor(), "vm://fruit-channel.in");
     }
 
     @Test
-    public void testRoutesFound() throws Exception
+    public void sendToInvalidRouteWithoutDefaultRouteShouldThrowException() throws Exception
     {
-        String result = muleClient.send(WITHOUT_DEFAULT_ROUTE_CHANNEL, "apple", null).getPayloadAsString();
-        assertEquals("apple:fruit:fruit", result);
-
-        result = muleClient.send(WITH_DEFAULT_ROUTE_CHANNEL, "apple", null).getPayloadAsString();
-        assertEquals("apple:fruit:fruit", result);
-
-        result = muleClient.send(WITH_DEFAULT_ROUTE_CHANNEL, "turnip", null).getPayloadAsString();
-        assertEquals("turnip:veggie:veggie", result);
+        MuleMessage result = muleContext.getClient().send(WITHOUT_DEFAULT_ROUTE_CHANNEL, "bad", null);
+        assertNotNull(result);
+        assertNotNull("should have got a MuleException", result.getExceptionPayload());
+        assertNotNull(result.getExceptionPayload().getException() instanceof MuleException);
+        assertNotNull(result.getExceptionPayload().getRootException() instanceof RoutePathNotFoundException);
     }
 
     @Test
-    public void testWhenExpressionRouteFound() throws Exception
+    public void sendToValidRouteShouldReturnValidResult() throws Exception
     {
-        final String result = muleClient.send(WITH_DEFAULT_ROUTE_CHANNEL, "blueberry", null)
-            .getPayloadAsString();
-        assertEquals("blueberry:fruit:fruit", result);
+        MuleMessage result = muleContext.getClient().send(WITHOUT_DEFAULT_ROUTE_CHANNEL, "apple", null);
+        assertEquals("apple:fruit:fruit", result.getPayloadAsString());
     }
 
     @Test
-    public void testDefaultRoute() throws Exception
+    public void sendToAppleRouteShouldHitFruitService() throws Exception
     {
-        final String result = muleClient.send(WITH_DEFAULT_ROUTE_CHANNEL, "car", null).getPayloadAsString();
-        assertEquals("car:default:default", result);
+        MuleMessage result = muleContext.getClient().send(WITH_DEFAULT_ROUTE_CHANNEL, "apple", null);
+        assertEquals("apple:fruit:fruit", result.getPayloadAsString());
+    }
+
+    @Test
+    public void sendToTurnipRouteShouldHitVeggieService() throws Exception
+    {
+        MuleMessage result = muleContext.getClient().send(WITH_DEFAULT_ROUTE_CHANNEL, "turnip", null);
+        assertEquals("turnip:veggie:veggie", result.getPayloadAsString());
+    }
+
+    @Test
+    public void sendToBlueberryRouteShouldHitFruitService() throws Exception
+    {
+        MuleMessage result = muleContext.getClient().send(WITH_DEFAULT_ROUTE_CHANNEL, "blueberry", null);
+        assertEquals("blueberry:fruit:fruit", result.getPayloadAsString());
+    }
+
+    @Test
+    public void sendToInvalidRouteShouldHitDefaultRoute() throws Exception
+    {
+        MuleMessage result = muleContext.getClient().send(WITH_DEFAULT_ROUTE_CHANNEL, "car", null);
+        assertEquals("car:default:default", result.getPayloadAsString());
+    }
+
+    private ChoiceRouter findChoiceRouterInFlow(String flowName)
+    {
+        Flow flow = lookupFlow(flowName);
+        return getChoiceRouter(flow);
+    }
+
+    private Flow lookupFlow(String flowConstructName)
+    {
+        FlowConstruct flow = muleContext.getRegistry().lookupFlowConstruct(flowConstructName);
+        assertNotNull(flow);
+        assertTrue(flow instanceof Flow);
+        return (Flow) flow;
+    }
+
+    private ChoiceRouter getChoiceRouter(Flow flowConstruct)
+    {
+        MessageProcessor routerMessageProcessor = flowConstruct.getMessageProcessors().get(0);
+        assertTrue(routerMessageProcessor instanceof ChoiceRouter);
+        return (ChoiceRouter) routerMessageProcessor;
+    }
+
+    private void assertIsRegExFilterWithPattern(Filter filter, String pattern)
+    {
+        assertTrue(filter instanceof RegExFilter);
+
+        RegExFilter regExFilter = (RegExFilter) filter;
+        assertEquals(regExFilter.getPattern(), pattern);
+    }
+
+    private void assertIsExpressionFilterWithExpressionAndEvaluator(Filter filter, String expression, String evaluator)
+    {
+        assertTrue(filter instanceof ExpressionFilter);
+
+        ExpressionFilter expressionFilter = (ExpressionFilter) filter;
+        assertEquals(expression, expressionFilter.getExpression());
+        assertEquals(evaluator, expressionFilter.getEvaluator());
+    }
+
+    private void assertMessageChainIsOutboundEndpoint(MessageProcessor processor, String expectedAddress)
+    {
+        assertTrue(processor instanceof MessageProcessorChain);
+        MessageProcessorChain chain = (MessageProcessorChain) processor;
+
+        MessageProcessor firstInChain = chain.getMessageProcessors().get(0);
+        assertTrue(firstInChain instanceof OutboundEndpoint);
+        OutboundEndpoint endpoint = (OutboundEndpoint) firstInChain;
+
+        String address = endpoint.getAddress();
+        assertEquals(expectedAddress, address);
     }
 }
