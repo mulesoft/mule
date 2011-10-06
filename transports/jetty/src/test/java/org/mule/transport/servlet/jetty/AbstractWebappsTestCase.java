d28265301855bfe3b64c58f0f16923e02056f287
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-10-06 14:05:58 +0000

    Merged revisions 23116 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.2.x
    ................
      r23116 | dirk.olmes | 2011-10-06 15:55:56 +0200 (Thu, 06 Oct 2011) | 17 lines
      
      Merged revisions 23109-23110,23114 via svnmerge from 
      https://svn.codehaus.org/mule/branches/mule-3.1.x
      
      ........
        r23109 | dirk.olmes | 2011-10-06 03:34:33 +0200 (Thu, 06 Oct 2011) | 1 line
        
        pull common functionality into a superclass
      ........
        r23110 | dirk.olmes | 2011-10-06 03:43:49 +0200 (Thu, 06 Oct 2011) | 1 line
        
        do not (ab)use Mule as HTTP client to limit side effects.
      ........
        r23114 | dirk.olmes | 2011-10-06 04:27:57 +0200 (Thu, 06 Oct 2011) | 2 lines
        
        MULE-5811 Jetty SSL connector does not support the webapps element
      ........
    ................
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@23117 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/jetty/src/test/java/org/mule/transport/servlet/jetty/AbstractWebappsTestCase.java
@@ -0,0 +1,61 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.servlet.jetty;
+
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.util.ClassUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.io.FileUtils;
+
+import static org.junit.Assert.assertEquals;
+
+public abstract class AbstractWebappsTestCase extends FunctionalTestCase
+{
+    @Override
+    protected boolean isStartContext()
+    {
+        // prepare the test webapp before starting Mule
+        return false;
+    }
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        super.doSetUp();
+
+        final URL url = ClassUtils.getClassPathRoot(getClass());
+        File webapps = new File(url.getFile(), "../webapps");
+        FileUtils.deleteDirectory(webapps);
+        webapps.mkdir();
+
+        FileUtils.copyFile(new File(url.getFile(), "../../src/test/resources/test.war"),
+                           new File(webapps, "test.war"));
+
+        muleContext.start();
+    }
+
+    protected void sendRequestAndAssertCorrectResponse(String url) throws IOException
+    {
+        GetMethod method = new GetMethod(url);
+        int rc = new HttpClient().executeMethod(method);
+        assertEquals(HttpStatus.SC_OK, rc);
+        assertEquals("Hello", method.getResponseBodyAsString());
+    }
+}
+
+
