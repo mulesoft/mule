ec82ed3a0891dce08e442c2e75264b4ee75fc7a4
Pablo Kraan <pablo.kraan@gmail.com>
2011-10-16 00:50:47 +0000

    MULE-5830: Cached events are updated after being cached
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@23182 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/cache/responsegenerator/DefaultResponseGenerator.java
+++ core/src/main/java/org/mule/cache/responsegenerator/DefaultResponseGenerator.java
@@ -11,7 +11,9 @@
 package org.mule.cache.responsegenerator;
 
 import org.mule.DefaultMuleEvent;
+import org.mule.DefaultMuleMessage;
 import org.mule.api.MuleEvent;
+import org.mule.api.MuleMessage;
 
 /**
  * Implements {@link ResponseGenerator} creating a new {@link MuleEvent}
@@ -22,8 +24,9 @@ public class DefaultResponseGenerator implements ResponseGenerator
 
     public MuleEvent create(MuleEvent requestedEvent, MuleEvent cachedResponseEvent)
     {
-        return new DefaultMuleEvent(cachedResponseEvent.getMessage(), cachedResponseEvent.getEndpoint(),
-                                    requestedEvent, requestedEvent.getSession());
+        MuleMessage clonedMessage = new DefaultMuleMessage(cachedResponseEvent.getMessage().getPayload(), cachedResponseEvent.getMuleContext());
+
+        return new DefaultMuleEvent(clonedMessage, cachedResponseEvent.getEndpoint(), requestedEvent, requestedEvent.getSession());
     }
 }
 
