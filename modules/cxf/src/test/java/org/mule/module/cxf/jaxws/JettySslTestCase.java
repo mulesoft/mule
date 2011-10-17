74af0f8a67b2735bb708297f44e1b29185f27510
Evangelina Martinez Ruiz Moreno <evangelina.martinez@mulesource.com>
2011-10-17 18:24:17 +0000

    MULE-5612: NPE on initialise when using jetty-ssl with CXF jaxws-service
    CXF needs to map the jetty/jetty-ssl schemas to http/https to be able to work correctly. Added the mapping for jetty-ssl that was missing and also changed the previous mapping that included localhost
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@23194 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/cxf/src/test/java/org/mule/module/cxf/jaxws/JettySslTestCase.java
@@ -0,0 +1,65 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.cxf.jaxws;
+
+import org.mule.api.MuleMessage;
+import org.mule.api.client.MuleClient;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.cxf.BusFactory;
+import org.apache.hello_world_soap_http.GreeterImpl;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class JettySslTestCase extends FunctionalTestCase
+{
+    @Rule
+    public DynamicPort dynamicPort = new DynamicPort("port1");
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "jetty-ssl-conf.xml";
+    }
+
+    @BeforeClass
+    public static void setUpDefaultBus()
+    {
+        BusFactory.setDefaultBus(null);
+    }
+
+    @Test
+    public void testClientWithMuleClient() throws Exception
+    {
+        MuleClient client = muleContext.getClient();
+        Map<String, Object> props = new HashMap<String, Object>();
+        props.put("operation", "greetMe");
+        MuleMessage result = client.send("clientEndpoint", "Dan", props);
+        assertEquals("Hello Dan", result.getPayload());
+
+        GreeterImpl impl = getGreeter();
+        assertEquals(1, impl.getInvocationCount());
+    }
+
+    private GreeterImpl getGreeter() throws Exception
+    {
+        return (GreeterImpl) getComponent("greeterService");
+    }
+
+
+}
