be7e52e594dac764962c474150f69e639bd38d03
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-11-01 04:22:14 +0000

    Merged revisions 23285 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.2.x
    ................
      r23285 | dirk.olmes | 2011-11-01 05:03:22 +0100 (Tue, 01 Nov 2011) | 41 lines
      
      Merged revisions 23273-23281 via svnmerge from 
      https://svn.codehaus.org/mule/branches/mule-3.1.x
      
      ........
        r23273 | dirk.olmes | 2011-10-31 16:44:46 +0100 (Mon, 31 Oct 2011) | 1 line
        
        move all test cases for MessageHeaderExpressionEvaluator from HeadersExpressionEvaluatorTestCase into its own test case.
      ........
        r23274 | dirk.olmes | 2011-10-31 17:02:34 +0100 (Mon, 31 Oct 2011) | 1 line
        
        move all test cases for MessageHeadersExpressionEvaluator from HeadersExpressionEvaluatorTestCase into its own test case.
      ........
        r23275 | dirk.olmes | 2011-10-31 17:26:29 +0100 (Mon, 31 Oct 2011) | 1 line
        
        move all test cases for MessageHeadersListExpressionEvaluator from HeadersExpressionEvaluatorTestCase into its own test case.
      ........
        r23276 | dirk.olmes | 2011-10-31 17:32:54 +0100 (Mon, 31 Oct 2011) | 1 line
        
        move tests related to wildcards over to MessageHeadersExpressionEvaluatorTestCase
      ........
        r23277 | dirk.olmes | 2011-10-31 17:38:53 +0100 (Mon, 31 Oct 2011) | 1 line
        
        move tests using MessageHeaderExpressionEvaluator via the expression manager into MessageHeaderExpressionEvaluatorTestCase
      ........
        r23278 | dirk.olmes | 2011-10-31 17:45:08 +0100 (Mon, 31 Oct 2011) | 1 line
        
        move tests using MessageHeadersExpressionEvaluator via the expression manager into MessageHeadersExpressionEvaluatorTestCase
      ........
        r23279 | dirk.olmes | 2011-10-31 18:01:47 +0100 (Mon, 31 Oct 2011) | 1 line
        
        move tests that use the expression manager to the respective test cases
      ........
        r23280 | dirk.olmes | 2011-10-31 18:11:07 +0100 (Mon, 31 Oct 2011) | 1 line
        
        move the remaining tests into CorrelationPropertiesExpressionEvaluatorTestCase and kiss HeadersExpressionEvaluatorTestCase good bye
      ........
        r23281 | dirk.olmes | 2011-10-31 18:36:22 +0100 (Mon, 31 Oct 2011) | 1 line
        
        give the test some love and split out each test into its own method
      ........
    ................
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@23286 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/test/java/org/mule/expression/MessageHeadersExpressionEvaluatorTestCase.java
@@ -0,0 +1,272 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.expression;
+
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleMessage;
+import org.mule.api.expression.RequiredValueException;
+import org.mule.api.transport.PropertyScope;
+import org.mule.tck.junit4.AbstractMuleContextTestCase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class MessageHeadersExpressionEvaluatorTestCase extends AbstractMuleContextTestCase
+{
+    private HashMap<String, Object> messageProperties;
+    private MessageHeadersExpressionEvaluator evaluator = new MessageHeadersExpressionEvaluator();
+    private MuleMessage message;
+
+    public MessageHeadersExpressionEvaluatorTestCase()
+    {
+        super();
+        setDisposeContextPerClass(true);
+    }
+
+    @Override
+    public void doSetUp()
+    {
+        messageProperties = new HashMap<String, Object>(3);
+        messageProperties.put("foo", "foovalue");
+        messageProperties.put("bar", "barvalue");
+        messageProperties.put("baz", "bazvalue");
+
+        message = new DefaultMuleMessage(TEST_MESSAGE, messageProperties, muleContext);
+    }
+
+    @Test
+    public void requiredHeadersWithExitingValueShouldReturnValue()
+    {
+        Object result = evaluator.evaluate("foo, baz", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>)result;
+        assertEquals(2, map.size());
+        assertTrue(map.values().contains("foovalue"));
+        assertTrue(map.values().contains("bazvalue"));
+        assertFalse(map.values().contains("barvalue"));
+    }
+
+    @Test
+    public void requiredHeadersWithExistingValuesViaExpressionManagerShouldReturnValues()
+    {
+        Object result = muleContext.getExpressionManager().evaluate("#[headers:foo, baz]", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>)result;
+        assertEquals(2, map.size());
+        assertTrue(map.values().contains("foovalue"));
+        assertTrue(map.values().contains("bazvalue"));
+        assertFalse(map.values().contains("barvalue"));
+    }
+
+    @Test(expected = RequiredValueException.class)
+    public void requiredHeadersWithMissingValuesShouldFail()
+    {
+        evaluator.evaluate("OUTBOUND:foo, baz, faz", message);
+    }
+
+    @Test(expected = RequiredValueException.class)
+    public void requiredHeadersWithMissingValuesViaExpressionManagerShouldFail()
+    {
+        muleContext.getExpressionManager().evaluate("#[headers:nonexistent]", message);
+    }
+
+    @Test
+    public void optionalHeadersWithExistingValuesShouldReturnValues()
+    {
+        Object result = evaluator.evaluate("foo?, baz", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>)result;
+        assertEquals(2, map.size());
+        assertTrue(map.values().contains("foovalue"));
+        assertTrue(map.values().contains("bazvalue"));
+        assertFalse(map.values().contains("barvalue"));
+    }
+
+    @Test
+    public void optionalHeadersWithExistingValuesViaExpressionManagerShouldReturnValues()
+    {
+        Object result = muleContext.getExpressionManager().evaluate("#[headers:foo?, baz]", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>)result;
+        assertEquals(2, map.size());
+        assertTrue(map.values().contains("foovalue"));
+        assertTrue(map.values().contains("bazvalue"));
+        assertFalse(map.values().contains("barvalue"));
+    }
+
+    @Test
+    public void optionalHeadersWithMissingValuesShouldReturnEmptyMap()
+    {
+        Object result = evaluator.evaluate("fool?", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>)result;
+        assertEquals(0, map.size());
+    }
+
+    @Test
+    public void optionalHeadersWithMissingValuesViaExpressionManagerShouldReturnEmptyMap()
+    {
+        Object result = muleContext.getExpressionManager().evaluate("#[headers:nonexistent?]", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>)result;
+        assertEquals(0, map.size());
+    }
+
+    @Test
+    public void requiredHeadersWithExplicitScopeShouldReturnValues()
+    {
+        Object result = evaluator.evaluate("OUTBOUND:foo, OUTBOUND:baz", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>) result;
+        assertEquals(2, map.size());
+        assertTrue(map.values().contains("foovalue"));
+        assertTrue(map.values().contains("bazvalue"));
+    }
+
+    @Test
+    public void propertyScopeSpecifiedForOneKeyShouldSetScopeForAllOtherKeys()
+    {
+        // this is equivalent to OUTBOUND:foo, OUTBOUND:baz
+        Object result = evaluator.evaluate("OUTBOUND:foo, baz", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>) result;
+        assertEquals(2, map.size());
+        assertTrue(map.values().contains("foovalue"));
+        assertTrue(map.values().contains("bazvalue"));
+    }
+
+    @Test
+    public void propertiesFromDifferentScopesWithValuesShouldReturnValues()
+    {
+        message.setProperty("faz", "fazvalue", PropertyScope.INVOCATION);
+
+        Object result = evaluator.evaluate("OUTBOUND:foo, OUTBOUND:baz, INVOCATION:faz", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>) result;
+        assertEquals(3, map.size());
+        assertTrue(map.values().contains("foovalue"));
+        assertTrue(map.values().contains("bazvalue"));
+        assertTrue(map.values().contains("fazvalue"));
+    }
+
+    @Test
+    public void matchAllWildcardShouldReturnAllHeaderValues() throws Exception
+    {
+        Object result = evaluator.evaluate("*", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>)result;
+        assertEquals(3, map.size());
+        assertTrue(map.values().contains("foovalue"));
+        assertTrue(map.values().contains("bazvalue"));
+        assertTrue(map.values().contains("barvalue"));
+    }
+
+    @Test
+    public void matchAllWildcardViaExpressionManagerShouldReturnAllHeaderValues()
+    {
+        Object result = muleContext.getExpressionManager().evaluate("#[headers:*]", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>)result;
+        assertEquals(3, map.size());
+        assertTrue(map.values().contains("foovalue"));
+        assertTrue(map.values().contains("bazvalue"));
+        assertTrue(map.values().contains("barvalue"));
+    }
+
+    @Test
+    public void matchBeginningWildcardShouldReturnValues()
+    {
+        Object result = evaluator.evaluate("ba*", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>)result;
+        assertEquals(2, map.size());
+        assertFalse(map.values().contains("foovalue"));
+        assertTrue(map.values().contains("bazvalue"));
+        assertTrue(map.values().contains("barvalue"));
+    }
+
+    @Test
+    public void matchBeginningWildcardViaExpressionManagerShouldReturnValues()
+    {
+        Object result = muleContext.getExpressionManager().evaluate("#[headers:ba*]", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>)result;
+        assertEquals(2, map.size());
+        assertFalse(map.values().contains("foovalue"));
+        assertTrue(map.values().contains("bazvalue"));
+        assertTrue(map.values().contains("barvalue"));
+    }
+
+    @Test
+    public void wildcardWithNoMatchShouldReturnEmptyMap()
+    {
+        Object result = evaluator.evaluate("x*", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>)result;
+        assertEquals(0, map.size());
+    }
+
+    @Test
+    public void wildcardWithnoMatchViaExpressionManagerShouldReturnEmptyMap()
+    {
+        Object result = muleContext.getExpressionManager().evaluate("#[headers:x*]", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>)result;
+        assertEquals(0, map.size());
+    }
+
+    @Test
+    public void multipleWildcardsShouldReturnValues() throws Exception
+    {
+        Object result = evaluator.evaluate("ba*, f*", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>)result;
+        assertEquals(3, map.size());
+        assertTrue(map.values().contains("foovalue"));
+        assertTrue(map.values().contains("bazvalue"));
+        assertTrue(map.values().contains("barvalue"));
+    }
+
+    @Test
+    public void multipleWildcardsViaExpressionManagerShouldReturnValues()
+    {
+        Object result = muleContext.getExpressionManager().evaluate("#[headers:ba*, f*]", message);
+        assertTrue(result instanceof Map);
+
+        Map<?, ?> map = (Map<?, ?>)result;
+        assertEquals(3, map.size());
+        assertTrue(map.values().contains("foovalue"));
+        assertTrue(map.values().contains("bazvalue"));
+        assertTrue(map.values().contains("barvalue"));
+    }
+}
---
+++ core/src/test/java/org/mule/expression/MessageHeadersListExpressionEvaluatorTestCase.java
@@ -0,0 +1,275 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.expression;
+
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleMessage;
+import org.mule.api.expression.RequiredValueException;
+import org.mule.api.transport.PropertyScope;
+import org.mule.tck.junit4.AbstractMuleContextTestCase;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class MessageHeadersListExpressionEvaluatorTestCase extends AbstractMuleContextTestCase
+{
+    private HashMap<String, Object> messageProperties;
+    private MessageHeadersListExpressionEvaluator evaluator = new MessageHeadersListExpressionEvaluator();
+    private MuleMessage message;
+
+    public MessageHeadersListExpressionEvaluatorTestCase()
+    {
+        super();
+        setDisposeContextPerClass(true);
+    }
+
+    @Override
+    public void doSetUp()
+    {
+        messageProperties = new HashMap<String, Object>(3);
+        messageProperties.put("foo", "foovalue");
+        messageProperties.put("bar", "barvalue");
+        messageProperties.put("baz", "bazvalue");
+
+        message = new DefaultMuleMessage(TEST_MESSAGE, messageProperties, muleContext);
+    }
+
+    @Test
+    public void requiredHeadersWithExitingValueShouldReturnValue()
+    {
+        Object result = evaluator.evaluate("foo, baz", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(2, list.size());
+        assertTrue(list.contains("foovalue"));
+        assertTrue(list.contains("bazvalue"));
+        assertFalse(list.contains("barvalue"));
+    }
+
+    @Test
+    public void requiredHeadersWithExistingValuesViaExpressionManagerShouldReturnValue()
+    {
+        Object result = muleContext.getExpressionManager().evaluate("#[headers-list:foo, baz]", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(2, list.size());
+        assertTrue(list.contains("foovalue"));
+        assertTrue(list.contains("bazvalue"));
+        assertFalse(list.contains("barvalue"));
+    }
+
+    @Test(expected = RequiredValueException.class)
+    public void requiredHeadersWithMissingValuesShouldFail()
+    {
+        evaluator.evaluate("nonexistent", message);
+    }
+
+    @Test(expected = RequiredValueException.class)
+    public void requiredHeadersWithMissingValuesViaExpressionManagerShouldFail()
+    {
+        muleContext.getExpressionManager().evaluate("#[headers-list:nonexistent]", message);
+    }
+
+    @Test
+    public void optionalHeadersWithExistingValuesShouldReturnValues()
+    {
+        Object result = evaluator.evaluate("foo?, baz?", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(2, list.size());
+        assertTrue(list.contains("foovalue"));
+        assertTrue(list.contains("bazvalue"));
+        assertFalse(list.contains("barvalue"));
+    }
+
+    @Test
+    public void optionalHeadersWithExistingValuesViaExpressionManagerShouldReturnValues()
+    {
+        Object result = muleContext.getExpressionManager().evaluate("#[headers-list:foo?, baz]", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(2, list.size());
+        assertTrue(list.contains("foovalue"));
+        assertTrue(list.contains("bazvalue"));
+        assertFalse(list.contains("barvalue"));
+
+    }
+
+    @Test
+    public void optionalHeadersWithMissingValuesShouldReturnEmptyList() throws Exception
+    {
+        Object result = evaluator.evaluate("fool?", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void optionalHeadersWithMissingValuesViaExpressionManagerShouldReturnEmptyList()
+    {
+        Object result = muleContext.getExpressionManager().evaluate("#[headers-list:nonexistent?]", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void requiredHeadersWithExplicitScopeShouldReturnValues()
+    {
+        Object result = evaluator.evaluate("OUTBOUND:foo, OUTBOUND:baz", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(2, list.size());
+        assertTrue(list.contains("foovalue"));
+        assertTrue(list.contains("bazvalue"));
+        assertFalse(list.contains("barvalue"));
+    }
+
+    @Test
+    public void propertyScopeSpecifiedForOneKeyShouldSetScopeForAllOtherKeys()
+    {
+        // this is equivalent to OUTBOUND:foo, OUTBOUND,:baz
+        Object result = evaluator.evaluate("OUTBOUND:foo, baz", message);
+        assertNotNull(result);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(2, list.size());
+        assertTrue(list.contains("foovalue"));
+        assertTrue(list.contains("bazvalue"));
+    }
+
+    @Test
+    public void propertiesFromDifferentScopesWithValuesShouldReturnValues()
+    {
+        message.setProperty("faz", "fazvalue", PropertyScope.INVOCATION);
+
+        Object result = evaluator.evaluate("OUTBOUND:foo, OUTBOUND:baz, INVOCATION:faz", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(3, list.size());
+        assertTrue(list.contains("foovalue"));
+        assertTrue(list.contains("bazvalue"));
+        assertTrue(list.contains("fazvalue"));
+    }
+
+    @Test
+    public void matchAllWildcardShouldReturnAllHeaderValues()
+    {
+        Object result = evaluator.evaluate("*", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(3, list.size());
+        assertTrue(list.contains("foovalue"));
+        assertTrue(list.contains("bazvalue"));
+        assertTrue(list.contains("barvalue"));
+    }
+
+    @Test
+    public void matchAllWildcardViaExpressionManagerShouldReturnAllHeaderValues()
+    {
+        Object result = muleContext.getExpressionManager().evaluate("#[headers-list:*]", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(3, list.size());
+        assertTrue(list.contains("foovalue"));
+        assertTrue(list.contains("bazvalue"));
+        assertTrue(list.contains("barvalue"));
+    }
+
+    @Test
+    public void matchBeginningWildcardShouldReturnValues()
+    {
+        Object result = evaluator.evaluate("ba*", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(2, list.size());
+        assertTrue(list.contains("barvalue"));
+        assertTrue(list.contains("bazvalue"));
+    }
+
+    @Test
+    public void matchBeginningWildcardViaExpressionManagerShouldReturnValues()
+    {
+        Object result = muleContext.getExpressionManager().evaluate("#[headers-list:ba*]", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(2, list.size());
+        assertFalse(list.contains("foovalue"));
+        assertTrue(list.contains("bazvalue"));
+        assertTrue(list.contains("barvalue"));
+    }
+
+    @Test
+    public void wildcardWithNoMatchShouldReturnEmptyList()
+    {
+        Object result = evaluator.evaluate("x*", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void wildcardWithNoMatchViaExpressionManagerShouldReturnEmptyList()
+    {
+        Object result = muleContext.getExpressionManager().evaluate("#[headers-list:x*]", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void multipleWildcardsShouldReturnValues() throws Exception
+    {
+        Object result = evaluator.evaluate("ba*, f*", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(3, list.size());
+        assertTrue(list.contains("foovalue"));
+        assertTrue(list.contains("bazvalue"));
+        assertTrue(list.contains("barvalue"));
+    }
+
+    @Test
+    public void multipleWildcardsViaExpressionManagerShouldReturnValues()
+    {
+        Object result = muleContext.getExpressionManager().evaluate("#[headers-list:ba*, f*]", message);
+        assertTrue(result instanceof List);
+
+        List<?> list = (List<?>) result;
+        assertEquals(3, list.size());
+        assertTrue(list.contains("foovalue"));
+        assertTrue(list.contains("bazvalue"));
+        assertTrue(list.contains("barvalue"));
+    }
+}
