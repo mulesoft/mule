6dfc654b037e24fff0aafdc8b72936aa1b449615
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-11-03 02:54:29 +0000

    Merged revisions 23310 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.1.x
    ........
      r23310 | dirk.olmes | 2011-11-03 03:37:28 +0100 (Thu, 03 Nov 2011) | 1 line
      
      convert to JUnit 4
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.2.x@23311 bf997673-6b11-0410-b953-e057580c5b09

--- transports/http/src/test/java/org/mule/transport/http/functional/HttpExceptionStrategyTestCase.java
+++ transports/http/src/test/java/org/mule/transport/http/functional/HttpExceptionStrategyTestCase.java
@@ -9,28 +9,28 @@
  */
 package org.mule.transport.http.functional;
 
-import org.hamcrest.core.Is;
-import org.hamcrest.core.IsInstanceOf;
-import org.hamcrest.core.IsNot;
-import org.junit.Test;
-import org.mockito.internal.matchers.Not;
 import org.mule.api.ExceptionPayload;
 import org.mule.api.MuleMessage;
-import org.mule.module.client.MuleClient;
-import org.mule.tck.DynamicPortTestCase;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
 import org.mule.transport.NullPayload;
 
+import org.hamcrest.core.IsInstanceOf;
+import org.hamcrest.core.IsNot;
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
 
-public class HttpExceptionStrategyTestCase extends DynamicPortTestCase
+public class HttpExceptionStrategyTestCase extends FunctionalTestCase
 {
+    private static final int TIMEOUT = 3000;
 
-    public static final String MESSAGE = "some message";
-    public static final int TIMEOUT = 3000;
-    private MuleClient muleClient;
-
+    @Rule
+    public DynamicPort port1 = new DynamicPort("port1");
 
     @Override
     protected String getConfigResources()
@@ -38,28 +38,15 @@ public class HttpExceptionStrategyTestCase extends DynamicPortTestCase
         return "http-exception-strategy-config.xml";
     }
 
-    @Override
-    protected void doSetUp() throws Exception
-    {
-        muleClient = new MuleClient(muleContext);
-    }
-
     @Test
     public void testInExceptionDoRollbackHttpSync() throws Exception
     {
-        muleClient = new MuleClient(muleContext);
-        MuleMessage response = muleClient.send(String.format("http://localhost:%s/flowWithoutExceptionStrategySync", getPorts().get(0)), MESSAGE, null, TIMEOUT);
+        String url = String.format("http://localhost:%d/flowWithoutExceptionStrategySync", port1.getNumber());
+        MuleMessage response = muleContext.getClient().send(url, TEST_MESSAGE, null, TIMEOUT);
         assertThat(response, notNullValue());
         assertThat(response.getPayload(), IsNot.not(IsInstanceOf.instanceOf(NullPayload.class)));
-        assertThat(response.getPayloadAsString(), IsNot.not(MESSAGE));
+        assertThat(response.getPayloadAsString(), not(TEST_MESSAGE));
         assertThat(response.getExceptionPayload(), notNullValue()); //to be fixed
         assertThat(response.getExceptionPayload(), instanceOf(ExceptionPayload.class)); //to be review/fixed
     }
-
-    @Override
-    protected int getNumPortsToFind()
-    {
-        return 1;
-    }
 }
-
