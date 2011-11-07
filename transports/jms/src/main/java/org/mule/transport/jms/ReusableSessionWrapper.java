1d78e352be349baa5f5d685e7853cb6d89be1f90
Pablo La Greca <pablolagreca@hotmail.com>
2011-11-07 14:51:19 +0000

    Merged revisions 23338 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.2.x
    ........
      r23338 | pablo.lagreca | 2011-11-07 11:49:04 -0300 (Mon, 07 Nov 2011) | 1 line
      
      EE-2512 - improving ReusableSessionWrapper
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@23339 bf997673-6b11-0410-b953-e057580c5b09

--- transports/jms/src/main/java/org/mule/transport/jms/ReusableSessionWrapper.java
+++ transports/jms/src/main/java/org/mule/transport/jms/ReusableSessionWrapper.java
@@ -7,272 +7,170 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-
 package org.mule.transport.jms;
-
-import java.io.Serializable;
-
-import javax.jms.BytesMessage;
-import javax.jms.Destination;
-import javax.jms.JMSException;
-import javax.jms.MapMessage;
-import javax.jms.Message;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageListener;
-import javax.jms.MessageProducer;
-import javax.jms.ObjectMessage;
-import javax.jms.Queue;
-import javax.jms.QueueBrowser;
-import javax.jms.QueueReceiver;
-import javax.jms.QueueSender;
-import javax.jms.QueueSession;
-import javax.jms.Session;
-import javax.jms.StreamMessage;
-import javax.jms.TemporaryQueue;
-import javax.jms.TemporaryTopic;
-import javax.jms.TextMessage;
-import javax.jms.Topic;
-import javax.jms.TopicPublisher;
-import javax.jms.TopicSession;
-import javax.jms.TopicSubscriber;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-public class ReusableSessionWrapper implements QueueSession, TopicSession
+import javax.jms.*;
+import java.io.Serializable;
+
+public class ReusableSessionWrapper implements Session
 {
     protected transient Log logger = LogFactory.getLog(getClass());
 
     private Session delegateSession;
-    private QueueSession queueSession;
-    private TopicSession topicSession;
 
-    ReusableSessionWrapper(Session delegateSession)
+    public ReusableSessionWrapper(Session delegateSession)
     {
         this.delegateSession = delegateSession;
-        if (delegateSession instanceof QueueSession)
-        {
-            this.queueSession = (QueueSession) delegateSession;
-        }
-        if (delegateSession instanceof TopicSession)
-        {
-            this.topicSession = (TopicSession) delegateSession;
-        }
     }
 
-    @Override
     public BytesMessage createBytesMessage() throws JMSException
     {
         return delegateSession.createBytesMessage();
     }
 
-    @Override
     public MapMessage createMapMessage() throws JMSException
     {
         return delegateSession.createMapMessage();
     }
 
-    @Override
     public Message createMessage() throws JMSException
     {
         return delegateSession.createMessage();
     }
 
-    @Override
     public ObjectMessage createObjectMessage() throws JMSException
     {
         return delegateSession.createObjectMessage();
     }
 
-    @Override
     public ObjectMessage createObjectMessage(Serializable object) throws JMSException
     {
         return delegateSession.createObjectMessage(object);
     }
 
-    @Override
     public StreamMessage createStreamMessage() throws JMSException
     {
         return delegateSession.createStreamMessage();
     }
 
-    @Override
     public TextMessage createTextMessage() throws JMSException
     {
         return delegateSession.createTextMessage();
     }
 
-    @Override
     public TextMessage createTextMessage(String text) throws JMSException
     {
         return delegateSession.createTextMessage(text);
     }
 
-    @Override
     public boolean getTransacted() throws JMSException
     {
         return delegateSession.getTransacted();
     }
 
-    @Override
     public int getAcknowledgeMode() throws JMSException
     {
         return delegateSession.getAcknowledgeMode();
     }
 
-    @Override
     public void commit() throws JMSException
     {
         delegateSession.commit();
     }
 
-    @Override
     public void rollback() throws JMSException
     {
         delegateSession.rollback();
     }
 
-    @Override
     public void close() throws JMSException
     {
-        //Do Nothing
+        //Do nothing, reuse it
     }
 
-    @Override
     public void recover() throws JMSException
     {
         delegateSession.recover();
     }
 
-    @Override
     public MessageListener getMessageListener() throws JMSException
     {
         return delegateSession.getMessageListener();
     }
 
-    @Override
     public void setMessageListener(MessageListener listener) throws JMSException
     {
         delegateSession.setMessageListener(listener);
     }
 
-    @Override
     public void run()
     {
         delegateSession.run();
     }
 
-    @Override
     public MessageProducer createProducer(Destination destination) throws JMSException
     {
         return delegateSession.createProducer(destination);
     }
 
-    @Override
     public MessageConsumer createConsumer(Destination destination) throws JMSException
     {
         return delegateSession.createConsumer(destination);
     }
 
-    @Override
     public MessageConsumer createConsumer(Destination destination, String messageSelector) throws JMSException
     {
         return delegateSession.createConsumer(destination, messageSelector);
     }
 
-    @Override
     public MessageConsumer createConsumer(Destination destination, String messageSelector, boolean NoLocal) throws JMSException
     {
         return delegateSession.createConsumer(destination, messageSelector, NoLocal);
     }
 
-    @Override
     public Queue createQueue(String queueName) throws JMSException
     {
         return delegateSession.createQueue(queueName);
     }
 
-    @Override
-    public QueueReceiver createReceiver(Queue queue) throws JMSException
-    {
-        return queueSession.createReceiver(queue);
-    }
-
-    @Override
-    public QueueReceiver createReceiver(Queue queue, String messageSelector) throws JMSException
-    {
-        return queueSession.createReceiver(queue,messageSelector);
-    }
-
-    @Override
-    public QueueSender createSender(Queue queue) throws JMSException
-    {
-        return queueSession.createSender(queue);
-    }
-
-    @Override
     public Topic createTopic(String topicName) throws JMSException
     {
         return delegateSession.createTopic(topicName);
     }
 
-
-
-    @Override
     public TopicSubscriber createDurableSubscriber(Topic topic, String name) throws JMSException
     {
         return delegateSession.createDurableSubscriber(topic, name);
     }
 
-    @Override
     public TopicSubscriber createDurableSubscriber(Topic topic, String name, String messageSelector, boolean noLocal) throws JMSException
     {
         return delegateSession.createDurableSubscriber(topic, name, messageSelector, noLocal);
     }
 
-    @Override
-    public TopicSubscriber createSubscriber(Topic topic) throws JMSException
-    {
-        return topicSession.createSubscriber(topic);
-    }
-
-    @Override
-    public TopicSubscriber createSubscriber(Topic topic, String messageSelector, boolean noLocal) throws JMSException
-    {
-        return topicSession.createSubscriber(topic, messageSelector, noLocal);
-    }
-
-    @Override
-    public TopicPublisher createPublisher(Topic topic) throws JMSException
-    {
-        return topicSession.createPublisher(topic);
-    }
-
-    @Override
     public QueueBrowser createBrowser(Queue queue) throws JMSException
     {
         return delegateSession.createBrowser(queue);
     }
 
-    @Override
     public QueueBrowser createBrowser(Queue queue, String messageSelector) throws JMSException
     {
         return delegateSession.createBrowser(queue, messageSelector);
     }
 
-    @Override
     public TemporaryQueue createTemporaryQueue() throws JMSException
     {
         return delegateSession.createTemporaryQueue();
     }
 
-    @Override
     public TemporaryTopic createTemporaryTopic() throws JMSException
     {
         return delegateSession.createTemporaryTopic();
     }
 
-    @Override
     public void unsubscribe(String name) throws JMSException
     {
         delegateSession.unsubscribe(name);
