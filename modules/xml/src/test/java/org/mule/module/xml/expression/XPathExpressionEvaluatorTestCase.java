0736e9ec42954e196f4296f5411d6e0f4e39d667
Evangelina Martinez Ruiz Moreno <evangelina.martinez@mulesource.com>
2011-11-17 13:50:47 +0000

    MULE-5892: ClassCastException when choosing incorrect XPath Parser
     The key of the cache with which the XPath instances are being stored is not correct. It only considers the AbstractXPathExpressionEvaluator subclasses but each subclass can return a different XPath class. This is causing that for the same expression that is being evaluated on two different objects, the first XPath implementation from the cache is returned, resulting in the reported ClassCastException.
    
     The solution is to consider the correct xpath implementation instead of the evaluator class
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@23374 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/xml/src/test/java/org/mule/module/xml/expression/XPathExpressionEvaluatorTestCase.java
@@ -0,0 +1,43 @@
+/*
+ * $Id $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.xml.expression;
+
+import junit.framework.TestCase;
+import org.dom4j.dom.DOMDocument;
+import org.dom4j.tree.DefaultDocument;
+import org.jaxen.JaxenException;
+import org.jaxen.XPath;
+import org.jaxen.dom.DOMXPath;
+import org.jaxen.dom4j.Dom4jXPath;
+import org.junit.Test;
+
+public class XPathExpressionEvaluatorTestCase extends TestCase
+{
+    private static final String EXPRESSION = "//isTest[test() = 'true']";
+    private static final String OTHER_EXPRESSION = "//isNotTest[test = 'false']";
+
+
+    @Test
+    public void testXPathCache() throws JaxenException
+    {
+        XPathExpressionEvaluator xPathExpressionEvaluator = new XPathExpressionEvaluator();
+        XPath xPathDOM1 = xPathExpressionEvaluator.getXPath(EXPRESSION, new DOMDocument());
+        assertTrue(xPathDOM1 instanceof DOMXPath);
+        XPath xPathDOM2 = xPathExpressionEvaluator.getXPath(EXPRESSION, new DOMDocument());
+        assertTrue(xPathDOM2 == xPathDOM1);
+        XPath xPathDom4j3 = xPathExpressionEvaluator.getXPath(EXPRESSION, new DefaultDocument());
+        assertTrue(xPathDom4j3 instanceof Dom4jXPath);
+        assertTrue(xPathDOM1 != xPathDom4j3);
+        XPath xPathDOM3 = xPathExpressionEvaluator.getXPath(OTHER_EXPRESSION, new DOMDocument());
+        assertTrue(xPathDOM1 != xPathDOM3);
+    }
+
+}
