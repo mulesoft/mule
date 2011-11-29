ecd4866661ce7c3d280c281f8ca5f6d443777725
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-11-29 05:57:26 +0000

    Merged revisions 23409-23412 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.1.x
    ........
      r23409 | dirk.olmes | 2011-11-29 04:09:34 +0100 (Tue, 29 Nov 2011) | 1 line
      
      compile fix
    ........
      r23410 | dirk.olmes | 2011-11-29 05:19:42 +0100 (Tue, 29 Nov 2011) | 1 line
      
      MULE-5880 AbstractReceiverServlet not allowing to send multiple headers with the same header name
    ........
      r23411 | dirk.olmes | 2011-11-29 05:48:22 +0100 (Tue, 29 Nov 2011) | 1 line
      
      cleanup
    ........
      r23412 | dirk.olmes | 2011-11-29 06:04:22 +0100 (Tue, 29 Nov 2011) | 1 line
      
      fix a typo in comment
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.2.x@23413 bf997673-6b11-0410-b953-e057580c5b09

--- tools/schemadocs/src/test/java/org/mule/tools/schemadocs/PathTestCase.java
+++ tools/schemadocs/src/test/java/org/mule/tools/schemadocs/PathTestCase.java
@@ -14,9 +14,10 @@ import org.mule.tck.junit4.AbstractMuleTestCase;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PathTestCase extends AbstractMuleTestCase
 {
-
     @Test
     public void testTagFromFileName()
     {
@@ -25,5 +26,4 @@ public class PathTestCase extends AbstractMuleTestCase
         assertEquals("foo-bar",
                 SchemaDocsMain.tagFromFileName("/opt/j2ee/data/bamboo/xml-data/build-dir/MULE-MULEV20X/transports/quartz/target/mule-transport-quartz-2.0.0-RC3-SNAPSHOT.jar!/META-INF/mule-foo-bar.xsd"));
     }
-
 }
