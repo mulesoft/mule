2088924bb538b0b1d3e8d36d92d77957bdfdd245
Pablo Kraan <pablo.kraan@gmail.com>
2011-12-02 18:14:19 +0000

    Merged revisions 23436 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.1.x
    ................
      r23436 | pablo.kraan | 2011-12-02 09:24:27 -0300 (Fri, 02 Dec 2011) | 13 lines
      
      Merged revisions 23434 via svnmerge from 
      https://svn.codehaus.org/mule/branches/mule-2.2.x
      
      ........
        r23434 | pablo.kraan | 2011-12-01 12:43:10 -0300 (Thu, 01 Dec 2011) | 6 lines
        
        MULE-5943: maxRedelivery -1 not being taken into account by ActiveMQ Connector
        
        ActiveMQ uses the redelivery count in a wrong way, ActiveMQJmsConnector adjusts that
        value, but in an incorrect way that causes that "infinite redelivery" (-1) being
        changed to "no redelivery" (0)
      ........
    ................
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.2.x@23439 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/jms/src/test/java/org/mule/transport/jms/AbstractJmsRedeliveryTestCase.java
@@ -0,0 +1,142 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.jms;
+
+import org.mule.api.MuleEventContext;
+import org.mule.api.MuleException;
+import org.mule.api.MuleMessage;
+import org.mule.api.client.MuleClient;
+import org.mule.api.context.notification.ExceptionNotificationListener;
+import org.mule.context.notification.ExceptionNotification;
+import org.mule.context.notification.NotificationException;
+import org.mule.message.ExceptionMessage;
+import org.mule.tck.AbstractServiceAndFlowTestCase;
+import org.mule.tck.exceptions.FunctionalTestException;
+import org.mule.tck.functional.CounterCallback;
+import org.mule.tck.functional.FunctionalTestComponent;
+import org.mule.transport.jms.redelivery.MessageRedeliveredException;
+import org.mule.util.concurrent.Latch;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public abstract class AbstractJmsRedeliveryTestCase extends AbstractServiceAndFlowTestCase
+{
+
+    protected static final String JMS_INPUT_QUEUE = "jms://in";
+    protected static final String JMS_DEAD_LETTER = "jms://dead.letter";
+    protected final int timeout = getTestTimeoutSecs() * 1000 / 4;
+
+    protected MuleClient client;
+    protected Latch messageRedeliveryExceptionFired;
+    protected CounterCallback callback;
+
+    public AbstractJmsRedeliveryTestCase(ConfigVariant variant, String configResources)
+    {
+        super(variant, configResources);
+        System.setProperty("maxRedelivery", String.valueOf(getMaxRedelivery()));
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> parameters()
+    {
+        return Arrays.asList(new Object[][] {{ConfigVariant.SERVICE, "jms-redelivery-service.xml"},
+                {ConfigVariant.FLOW, "jms-redelivery-flow.xml"}});
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+        client = muleContext.getClient();
+        messageRedeliveryExceptionFired = new Latch();
+        registerEventListener(messageRedeliveryExceptionFired);
+        purgeQueue();
+        setupCallback();
+    }
+
+    protected void assertMessageInDlq() throws MuleException
+    {
+        MuleMessage dl = client.request(JMS_DEAD_LETTER, 1000);
+        assertNotNull(dl);
+        assertTrue(dl.getPayload() instanceof ExceptionMessage);
+        ExceptionMessage em = (ExceptionMessage) dl.getPayload();
+        assertNotNull(em.getException());
+        assertTrue(em.getException() instanceof MessageRedeliveredException);
+    }
+
+    protected void purgeQueue() throws MuleException
+    {
+        // required if broker is not restarted with the test - it tries to deliver those messages to the client
+        // purge the queue
+        while (client.request(JMS_INPUT_QUEUE, 1000) != null)
+        {
+            logger.warn("Destination " + JMS_INPUT_QUEUE + " isn't empty, draining it");
+        }
+    }
+
+    protected void setupCallback() throws Exception
+    {
+        callback = createCallback();
+
+        FunctionalTestComponent ftc = getFunctionalTestComponent("Bouncer");
+        ftc.setEventCallback(callback);
+    }
+
+    private CounterCallback createCallback()
+    {
+        // enhance the counter callback to count, then throw an exception
+        return new CounterCallback()
+        {
+            @Override
+            public void eventReceived(MuleEventContext context, Object Component) throws Exception
+            {
+                final int count = incCallbackCount();
+                logger.info("Message Delivery Count is: " + count);
+                throw new FunctionalTestException();
+            }
+        };
+    }
+
+    private void registerEventListener(final Latch messageRedeliveryExceptionFired) throws NotificationException
+    {
+        muleContext.registerListener(new ExceptionNotificationListener<ExceptionNotification>()
+        {
+            public void onNotification(ExceptionNotification notification)
+            {
+                if (notification.getException() instanceof MessageRedeliveredException)
+                {
+                    messageRedeliveryExceptionFired.countDown();
+                }
+            }
+        });
+    }
+
+    protected void assertNoMessageInDlq(String location) throws MuleException
+    {
+        assertNull(client.request(location, 1000));
+    }
+
+    @After
+    public void cleanUpMaxRedelivery()
+    {
+        System.clearProperty("maxRedelivery");
+    }
+
+    protected abstract int getMaxRedelivery();
+}
