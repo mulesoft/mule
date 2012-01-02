8e2f5b0fa1ebe1df5976a2cf283e96ea036de8ce
Dirk Olmes <dirk.olmes@mulesoft.com>
2012-01-02 05:16:59 +0000

    MULE-4193 HttpsPollingConnector is broken
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.2.x@23571 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/http/src/test/java/org/mule/transport/http/functional/HttpsPollingFunctionalTestCase.java
@@ -0,0 +1,43 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.http.functional;
+
+import org.mule.api.MuleMessage;
+import org.mule.api.client.MuleClient;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class HttpsPollingFunctionalTestCase extends FunctionalTestCase
+{
+    @Rule
+    public DynamicPort port1 = new DynamicPort("port1");
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "https-polling-config.xml";
+    }
+
+    @Test
+    public void httpsPolling() throws Exception
+    {
+        MuleClient client = muleContext.getClient();
+        MuleMessage message = client.request("vm://toTest", 60000);
+        assertNotNull(message);
+        assertEquals("/ received", message.getPayloadAsString());
+    }
+}
