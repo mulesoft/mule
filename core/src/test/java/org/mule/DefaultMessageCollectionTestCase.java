3a53bab698336429b3091356b916387b522c2a0d
Daniel Feist <dfeist@gmail.com>
2012-01-18 18:34:10 +0000

    Merged revisions 23637 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.2.x
    ................
      r23637 | dfeist | 2012-01-18 14:36:30 -0300 (Wed, 18 Jan 2012) | 13 lines
      
      Merged revisions 23628-23629 via svnmerge from 
      https://svn.codehaus.org/mule/branches/mule-3.1.x
      
      ........
        r23628 | dfeist | 2012-01-18 10:30:25 -0300 (Wed, 18 Jan 2012) | 1 line
        
        Add DefaultMessageCollectionTestCase for others to review
      ........
        r23629 | dfeist | 2012-01-18 10:56:27 -0300 (Wed, 18 Jan 2012) | 1 line
        
        Add a unit test for MuleMessageCollection, also expand testing of CollectionSplitter and CollectionAggregator
      ........
    ................
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@23642 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/test/java/org/mule/DefaultMessageCollectionTestCase.java
@@ -0,0 +1,117 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.mule.api.MuleContext;
+import org.mule.api.MuleMessage;
+import org.mule.api.MuleMessageCollection;
+import org.mule.tck.junit4.AbstractMuleTestCase;
+import org.mule.tck.testmodels.fruit.Apple;
+import org.mule.tck.testmodels.fruit.Banana;
+import org.mule.tck.testmodels.fruit.Fruit;
+import org.mule.tck.testmodels.fruit.GrapeFruit;
+import org.mule.tck.testmodels.fruit.Orange;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class DefaultMessageCollectionTestCase extends AbstractMuleTestCase
+{
+
+    Apple apple = new Apple();
+    Banana banana = new Banana();
+    GrapeFruit grapeFruit = new GrapeFruit();
+    Orange orange = new Orange();
+
+    MuleContext muleContext = Mockito.mock(MuleContext.class);
+
+    @Test
+    public void addMuleMessage()
+    {
+        MuleMessage message1 = new DefaultMuleMessage(apple, muleContext);
+        MuleMessage message2 = new DefaultMuleMessage(banana, muleContext);
+        MuleMessage message3 = new DefaultMuleMessage(orange, muleContext);
+
+        DefaultMessageCollection messageCollection = new DefaultMessageCollection(muleContext);
+        messageCollection.addMessage(message1);
+        messageCollection.addMessage(message2);
+        messageCollection.addMessage(message3);
+
+        assertEquals(3, messageCollection.getMessageList().size());
+        assertSame(message1, messageCollection.getMessage(0));
+        assertSame(message2, messageCollection.getMessage(1));
+        assertSame(message3, messageCollection.getMessage(2));
+
+        assertEquals(3, messageCollection.getPayloadList().size());
+        assertTrue(messageCollection.getPayload() instanceof List<?>);
+        assertSame(apple, ((List) messageCollection.getPayload()).get(0));
+        assertSame(banana, ((List) messageCollection.getPayload()).get(1));
+        assertSame(orange, ((List) messageCollection.getPayload()).get(2));
+    }
+
+    @Test
+    public void addMuleMessageCollection()
+    {
+        MuleMessageCollection messageCollection1 = Mockito.mock(MuleMessageCollection.class);
+        MuleMessageCollection messageCollection2 = Mockito.mock(MuleMessageCollection.class);
+        Mockito.when(messageCollection1.getPayload()).thenReturn(new Fruit[]{apple, banana});
+        Mockito.when(messageCollection2.getPayload()).thenReturn(new Fruit[]{grapeFruit, orange});
+
+        DefaultMessageCollection messageCollectionUnderTest = new DefaultMessageCollection(muleContext);
+        messageCollectionUnderTest.addMessage(messageCollection1);
+        messageCollectionUnderTest.addMessage(messageCollection2);
+
+        assertEquals(2, messageCollectionUnderTest.getMessageList().size());
+        assertSame(messageCollection1, messageCollectionUnderTest.getMessage(0));
+        assertSame(messageCollection2, messageCollectionUnderTest.getMessage(1));
+
+        assertEquals(2, messageCollectionUnderTest.getPayloadList().size());
+        assertTrue(messageCollectionUnderTest.getPayload() instanceof List<?>);
+        assertSame(apple, ((Fruit[]) ((List) messageCollectionUnderTest.getPayload()).get(0))[0]);
+        assertSame(banana, ((Fruit[]) ((List) messageCollectionUnderTest.getPayload()).get(0))[1]);
+        assertSame(grapeFruit, ((Fruit[]) ((List) messageCollectionUnderTest.getPayload()).get(1))[0]);
+        assertSame(orange, ((Fruit[]) ((List) messageCollectionUnderTest.getPayload()).get(1))[1]);
+    }
+
+    @Test
+    public void addMuleMessageAndMuleMessageCollection()
+    {
+        MuleMessage message3 = new DefaultMuleMessage(grapeFruit, muleContext);
+        MuleMessage message4 = new DefaultMuleMessage(orange, muleContext);
+
+        MuleMessageCollection messageCollection1 = Mockito.mock(MuleMessageCollection.class);
+        Mockito.when(messageCollection1.getPayload()).thenReturn(new Fruit[]{apple, banana});
+
+        DefaultMessageCollection messageCollectionUnderTest = new DefaultMessageCollection(muleContext);
+        messageCollectionUnderTest.addMessage(messageCollection1);
+        messageCollectionUnderTest.addMessage(message3);
+        messageCollectionUnderTest.addMessage(message4);
+
+        assertEquals(3, messageCollectionUnderTest.getMessageList().size());
+        assertSame(messageCollection1, messageCollectionUnderTest.getMessage(0));
+        assertSame(message3, messageCollectionUnderTest.getMessage(1));
+        assertSame(message4, messageCollectionUnderTest.getMessage(2));
+
+        assertEquals(3, messageCollectionUnderTest.getPayloadList().size());
+        assertTrue(messageCollectionUnderTest.getPayload() instanceof List<?>);
+        assertSame(apple, ((Fruit[]) ((List) messageCollectionUnderTest.getPayload()).get(0))[0]);
+        assertSame(banana, ((Fruit[]) ((List) messageCollectionUnderTest.getPayload()).get(0))[1]);
+        assertSame(grapeFruit, ((List) messageCollectionUnderTest.getPayload()).get(1));
+        assertSame(orange, ((List) messageCollectionUnderTest.getPayload()).get(2));
+    }
+
+}
--- core/src/test/java/org/mule/routing/SimpleCollectionAggregatorTestCase.java
+++ core/src/test/java/org/mule/routing/SimpleCollectionAggregatorTestCase.java
@@ -13,16 +13,20 @@ package org.mule.routing;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import org.mule.DefaultMessageCollection;
 import org.mule.DefaultMuleEvent;
 import org.mule.DefaultMuleMessage;
 import org.mule.MessageExchangePattern;
 import org.mule.api.MuleEvent;
 import org.mule.api.MuleMessage;
+import org.mule.api.MuleMessageCollection;
 import org.mule.api.MuleSession;
 import org.mule.api.endpoint.InboundEndpoint;
 import org.mule.api.service.Service;
 import org.mule.tck.MuleTestUtils;
+import org.mule.tck.SensingNullMessageProcessor;
 import org.mule.tck.junit4.AbstractMuleContextTestCase;
 import org.mule.tck.testmodels.fruit.Apple;
 
@@ -40,7 +44,7 @@ public class SimpleCollectionAggregatorTestCase extends AbstractMuleContextTestC
     }
 
     @Test
-    public void testMessageProcessor() throws Exception
+    public void testAggregateMultipleEvents() throws Exception
     {
         MuleSession session1 = getTestSession(getTestService(), muleContext);
         session1.setProperty("key1", "value1");
@@ -49,16 +53,17 @@ public class SimpleCollectionAggregatorTestCase extends AbstractMuleContextTestC
         session1.setProperty("key2", "value2");
         MuleSession session3 = getTestSession(getTestService(), muleContext);
         session1.setProperty("key3", "value3");
-        
-        
+
         Service testService = getTestService("test", Apple.class);
         assertNotNull(testService);
 
         SimpleCollectionAggregator router = new SimpleCollectionAggregator();
+        SensingNullMessageProcessor sensingMessageProcessor = getSensingNullMessageProcessor();
+        router.setListener(sensingMessageProcessor);
         router.setMuleContext(muleContext);
         router.setFlowConstruct(testService);
         router.initialise();
-        
+
         MuleMessage message1 = new DefaultMuleMessage("test event A", muleContext);
         MuleMessage message2 = new DefaultMuleMessage("test event B", muleContext);
         MuleMessage message3 = new DefaultMuleMessage("test event C", muleContext);
@@ -67,31 +72,120 @@ public class SimpleCollectionAggregatorTestCase extends AbstractMuleContextTestC
         message3.setCorrelationId(message1.getUniqueId());
         message1.setCorrelationGroupSize(3);
 
-        InboundEndpoint endpoint = MuleTestUtils.getTestInboundEndpoint(MessageExchangePattern.ONE_WAY, muleContext);
+        InboundEndpoint endpoint = MuleTestUtils.getTestInboundEndpoint(MessageExchangePattern.ONE_WAY,
+            muleContext);
         MuleEvent event1 = new DefaultMuleEvent(message1, endpoint, testService, session1);
         MuleEvent event2 = new DefaultMuleEvent(message2, endpoint, testService, session2);
         MuleEvent event3 = new DefaultMuleEvent(message3, endpoint, testService, session3);
 
         assertNull(router.process(event1));
         assertNull(router.process(event2));
-        MuleEvent resultEvent = router.process(event3);
-        assertNotNull(resultEvent);
-        MuleMessage resultMessage = resultEvent.getMessage();
-        assertNotNull(resultMessage);
-        List<String> payload = (List<String>)resultMessage.getPayload();
+        assertNull(router.process(event3));
+
+        assertNotNull(sensingMessageProcessor.event);
+        MuleMessage nextMessage = sensingMessageProcessor.event.getMessage();
+        assertNotNull(nextMessage);
+        assertTrue(nextMessage instanceof MuleMessageCollection);
+        assertTrue(nextMessage.getPayload() instanceof List<?>);
+        List<String> payload = (List<String>) nextMessage.getPayload();
         assertEquals(3, payload.size());
-        String[] results=new String[3];
-        results=payload.toArray(results);
+        String[] results = new String[3];
+        results = payload.toArray(results);
+        // Need to sort result because of MULE-5998
         Arrays.sort(results);
         assertEquals("test event A", results[0]);
         assertEquals("test event B", results[1]);
         assertEquals("test event C", results[2]);
-        
+
         // Assert that session was merged correctly
-        assertEquals(3, resultEvent.getSession().getProperties().size());
-        assertEquals("value1NEW", resultEvent.getSession().getProperty("key1"));
-        assertEquals("value2", resultEvent.getSession().getProperty("key2"));
-        assertEquals("value3", resultEvent.getSession().getProperty("key3"));
+        assertEquals(3, sensingMessageProcessor.event.getSession().getProperties().size());
+        assertEquals("value1NEW", sensingMessageProcessor.event.getSession().getProperty("key1"));
+        assertEquals("value2", sensingMessageProcessor.event.getSession().getProperty("key2"));
+        assertEquals("value3", sensingMessageProcessor.event.getSession().getProperty("key3"));
+    }
+
+    @Test
+    public void testAggregateSingleEvent() throws Exception
+    {
+        MuleSession session = getTestSession(getTestService(), muleContext);
+        Service testService = getTestService("test", Apple.class);
+        assertNotNull(testService);
+
+        SimpleCollectionAggregator router = new SimpleCollectionAggregator();
+        SensingNullMessageProcessor sensingMessageProcessor = getSensingNullMessageProcessor();
+        router.setListener(sensingMessageProcessor);
+        router.setMuleContext(muleContext);
+        router.setFlowConstruct(testService);
+        router.initialise();
+
+        MuleMessage message1 = new DefaultMuleMessage("test event A", muleContext);
+        message1.setCorrelationId(message1.getUniqueId());
+        message1.setCorrelationGroupSize(1);
+
+        InboundEndpoint endpoint = MuleTestUtils.getTestInboundEndpoint(MessageExchangePattern.ONE_WAY,
+            muleContext);
+        MuleEvent event1 = new DefaultMuleEvent(message1, endpoint, testService);
+
+        MuleEvent resultEvent = router.process(event1);
+        assertNull(resultEvent);
+
+        assertNotNull(sensingMessageProcessor.event);
+        MuleMessage nextMessage = sensingMessageProcessor.event.getMessage();
+        assertNotNull(nextMessage);
+        assertTrue(nextMessage instanceof MuleMessageCollection);
+        assertTrue(nextMessage.getPayload() instanceof List<?>);
+        List<String> payload = (List<String>) nextMessage.getPayload();
+        assertEquals(1, payload.size());
+        assertEquals("test event A", payload.get(0));
+    }
+
+    @Test
+    public void testAggregateMessageCollections() throws Exception
+    {
+        MuleSession session = getTestSession(getTestService(), muleContext);
+        Service testService = getTestService("test", Apple.class);
+        assertNotNull(testService);
+
+        SimpleCollectionAggregator router = new SimpleCollectionAggregator();
+        router.setMuleContext(muleContext);
+        router.setFlowConstruct(testService);
+        router.initialise();
+
+        MuleMessage message1 = new DefaultMuleMessage("test event A", muleContext);
+        MuleMessage message2 = new DefaultMuleMessage("test event B", muleContext);
+        MuleMessage message3 = new DefaultMuleMessage("test event C", muleContext);
+        MuleMessage message4 = new DefaultMuleMessage("test event D", muleContext);
+        MuleMessageCollection messageCollection1 = new DefaultMessageCollection(muleContext);
+        MuleMessageCollection messageCollection2 = new DefaultMessageCollection(muleContext);
+        messageCollection1.addMessage(message1);
+        messageCollection1.addMessage(message2);
+        messageCollection2.addMessage(message3);
+        messageCollection2.addMessage(message4);
+
+        messageCollection1.setCorrelationGroupSize(2);
+        messageCollection1.setCorrelationId(messageCollection1.getUniqueId());
+        messageCollection2.setCorrelationGroupSize(2);
+        messageCollection2.setCorrelationId(messageCollection1.getUniqueId());
+
+        InboundEndpoint endpoint = MuleTestUtils.getTestInboundEndpoint(MessageExchangePattern.ONE_WAY,
+            muleContext);
+        MuleEvent event1 = new DefaultMuleEvent(messageCollection1, endpoint, testService);
+        MuleEvent event2 = new DefaultMuleEvent(messageCollection2, endpoint, testService);
+
+        assertNull(router.process(event1));
+        MuleEvent resultEvent = router.process(event2);
+        assertNotNull(resultEvent);
+        MuleMessage resultMessage = resultEvent.getMessage();
+        assertNotNull(resultMessage);
+        List<List<String>> payload = (List<List<String>>) resultMessage.getPayload();
+        assertEquals(2, payload.size());
+
+        // Unable to assert results because of MULE-5998
+        // assertEquals("test event A", ((List)payload.get(0)).get(0));
+        // assertEquals("test event B", ((List)payload.get(0)).get(1));
+        // assertEquals("test event C", ((List)payload.get(1)).get(0));
+        // assertEquals("test event D", ((List)payload.get(1)).get(1));
+
     }
 
 }
