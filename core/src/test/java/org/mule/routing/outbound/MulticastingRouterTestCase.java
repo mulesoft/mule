4aec6c0e10571113b43bbfab54c9aaa0e3aaf1ed
stephen.fenech <stephen.fenech>
2012-01-31 04:57:13 +0000

    MULE-5838 : AbstractSequenceRouter uses the getRoute method instead of getting the route directly from the routes list in order for the propagation of "magic" properties.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.1.x@23761 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/test/java/org/mule/routing/outbound/MulticastingRouterTestCase.java
+++ core/src/test/java/org/mule/routing/outbound/MulticastingRouterTestCase.java
@@ -10,27 +10,31 @@
 
 package org.mule.routing.outbound;
 
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import org.mule.DefaultMuleMessage;
 import org.mule.api.MuleEvent;
 import org.mule.api.MuleMessage;
 import org.mule.api.MuleMessageCollection;
 import org.mule.api.MuleSession;
+import org.mule.api.config.MuleProperties;
 import org.mule.api.endpoint.OutboundEndpoint;
 import org.mule.api.processor.MessageProcessor;
+import org.mule.api.transport.PropertyScope;
+import org.mule.routing.CorrelationMode;
 import org.mule.routing.filters.RegExFilter;
 import org.mule.tck.MuleTestUtils;
 import org.mule.tck.junit4.AbstractMuleContextTestCase;
 
+import com.mockobjects.dynamic.ConstraintMatcher;
 import com.mockobjects.dynamic.Mock;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
-
-import static junit.framework.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 public class MulticastingRouterTestCase extends AbstractMuleContextTestCase
 {
@@ -157,6 +161,61 @@ public class MulticastingRouterTestCase extends AbstractMuleContextTestCase
         mockendpoint1.verify();
         mockendpoint2.verify();
     }
+    
+    @Test
+    public void testMulticastingRouterCorrelationIdPropagation() throws Exception
+    {
+        Mock session = MuleTestUtils.getMockSession();
+        session.matchAndReturn("getFlowConstruct", getTestService());
+        RegExFilter filter = new RegExFilter("(.*) Message");
+
+        OutboundEndpoint endpoint1 = getTestOutboundEndpoint("Test1Provider", "test://test1", null, filter, null);
+        assertNotNull(endpoint1);
+
+        OutboundEndpoint endpoint2 = getTestOutboundEndpoint("Test2Provider", "test://test2", null, filter, null);
+        assertNotNull(endpoint2);
+        Mock mockendpoint1 = RouterTestUtils.getMockEndpoint(endpoint1);
+        Mock mockendpoint2 = RouterTestUtils.getMockEndpoint(endpoint2);
+
+        MulticastingRouter router = createObject(MulticastingRouter.class);
+
+        List<MessageProcessor> endpoints = new ArrayList<MessageProcessor>();
+        endpoints.add((OutboundEndpoint) mockendpoint1.proxy());
+        endpoints.add((OutboundEndpoint) mockendpoint2.proxy());
+        router.setRoutes(endpoints);
+        router.setEnableCorrelation(CorrelationMode.NEVER);
+
+        MuleMessage message = new DefaultMuleMessage(TEST_MESSAGE, muleContext);
+        message.setProperty(MuleProperties.MULE_CORRELATION_ID_PROPERTY, "MyCustomCorrelationId", PropertyScope.INBOUND);
+
+        assertTrue(router.isMatch(message));
+        
+        ConstraintMatcher expectedCorrelationId = new ConstraintMatcher(){
+            public boolean matches(Object[] args)
+            {
+                boolean argsMatch=args.length == 1 && args[0] instanceof MuleEvent;
+                if(argsMatch)
+                {
+                    MuleEvent event=(MuleEvent) args[0];
+                    String correlationId=event.getMessage().getOutboundProperty(MuleProperties.MULE_CORRELATION_ID_PROPERTY);
+                    return correlationId!=null && correlationId.equals("MyCustomCorrelationId");
+                }
+                return false;
+            }
+
+            public Object[] getConstraints()
+            {
+                return new String[] {"Outbound Correlation ID property should be set."};
+            }
+        };
+        mockendpoint1.expect("process", expectedCorrelationId);
+        mockendpoint2.expect("process", expectedCorrelationId);
+        router.route(new OutboundRoutingTestEvent(message, (MuleSession)session.proxy()));
+        mockendpoint1.verify();
+        mockendpoint2.verify();
+
+    }
+
 
     private String getPayload(MuleMessage message) throws Exception
     {
