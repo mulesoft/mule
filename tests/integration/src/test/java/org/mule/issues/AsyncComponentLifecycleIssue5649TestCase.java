00f200cc36f137a91d82eaafb4d9443a05e6fcfa
mike.schilling <mike.schilling>
2012-02-04 00:04:59 +0000

    Merged revisions 23818 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.1.x
    ........
      r23818 | mike.schilling | 2012-02-03 15:42:40 -0800 (Fri, 03 Feb 2012) | 3 lines
      
      MULE-5649
      
      Propagate "Stop" the MPs is an async branch
    ........
    
    NOTE: There was no bug in this codeline.  This integrates only the test and test framework.
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.2.x@23819 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/issues/AsyncComponentLifecycleIssue5649TestCase.java
@@ -0,0 +1,61 @@
+package org.mule.issues;
+
+import static org.junit.Assert.assertEquals;
+
+import org.mule.api.MuleContext;
+import org.mule.api.lifecycle.Disposable;
+import org.mule.api.lifecycle.Initialisable;
+import org.mule.api.lifecycle.Startable;
+import org.mule.api.lifecycle.Stoppable;
+import org.mule.tck.functional.FunctionalTestComponent;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+
+public class AsyncComponentLifecycleIssue5649TestCase extends FunctionalTestCase implements FunctionalTestComponent.LifecycleCallback
+{
+    List<String> componentPhases = new ArrayList<String>();
+
+    @Override
+    protected MuleContext createMuleContext() throws Exception
+    {
+        componentPhases.clear();
+        FunctionalTestComponent.addLifecycleCallback(this);
+        return super.createMuleContext();    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    @Override
+    protected void doTearDown() throws Exception
+    {
+        FunctionalTestComponent.removeLifecycleCallback(this);
+    }
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/issues/async-component-lifecycle-5649.xml";
+    }
+
+    
+    @Test
+    public void testConfig() throws Exception
+    {
+        muleContext.stop();
+        muleContext.dispose();
+        FunctionalTestComponent.removeLifecycleCallback(this);
+        System.out.println(componentPhases);
+        assertEquals(4, componentPhases.size());
+        assertEquals(Initialisable.PHASE_NAME, componentPhases.get(0));
+        assertEquals(Startable.PHASE_NAME, componentPhases.get(1));
+        assertEquals(Stoppable.PHASE_NAME, componentPhases.get(2));
+        assertEquals(Disposable.PHASE_NAME, componentPhases.get(3));
+    }
+
+    public void onTransition(String name, String newPhase)
+    {
+        componentPhases.add(newPhase);
+    }
+
+}
