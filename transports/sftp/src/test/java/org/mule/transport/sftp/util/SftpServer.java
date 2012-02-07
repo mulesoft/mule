ed3e44668436ea69624caa554a057fbf9faa852e
Daniel Feist <dfeist@gmail.com>
2012-02-07 18:16:42 +0000

    Add missing license headers
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@23838 bf997673-6b11-0410-b953-e057580c5b09

--- transports/sftp/src/test/java/org/mule/transport/sftp/util/SftpServer.java
+++ transports/sftp/src/test/java/org/mule/transport/sftp/util/SftpServer.java
@@ -1,3 +1,13 @@
+/*
+ * $Id: ProcessingTime.java 23484 2011-12-14 01:26:29Z dfeist $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.transport.sftp.util;
 
 import java.io.IOException;
@@ -15,32 +25,33 @@ import org.apache.sshd.server.sftp.SftpSubsystem;
 import org.apache.sshd.server.shell.ProcessShellFactory;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
-public class SftpServer {
+public class SftpServer
+{
 
-	protected static final String USERNAME = "muletest1";
+    protected static final String USERNAME = "muletest1";
     protected static final String PASSWORD = "muletest1";
     private SshServer sshdServer;
     private Integer port;
-	
-    public SftpServer(int port) 
+
+    public SftpServer(int port)
     {
         this.port = port;
-	    configureSecurityProvider();		
-	    SftpSubsystem.Factory factory = createFtpSubsystemFactory();
-	    sshdServer = SshServer.setUpDefaultServer();
-		configureSshdServer(factory, passwordAuthenticator());
-	}
-
-    private void configureSshdServer(SftpSubsystem.Factory factory, PasswordAuthenticator passwordAuthenticator)
-    {
-        sshdServer.setPort(port);
-		sshdServer.setKeyPairProvider(new SimpleGeneratorHostKeyProvider("hostkey.ser"));
-		sshdServer.setSubsystemFactories(Arrays.<NamedFactory<Command>>asList(factory));
-		sshdServer.setCommandFactory(new ScpCommandFactory());
-		sshdServer.setShellFactory(new ProcessShellFactory());
-		sshdServer.setPasswordAuthenticator(passwordAuthenticator);
+        configureSecurityProvider();
+        SftpSubsystem.Factory factory = createFtpSubsystemFactory();
+        sshdServer = SshServer.setUpDefaultServer();
+        configureSshdServer(factory, passwordAuthenticator());
     }
 
+    private void configureSshdServer(SftpSubsystem.Factory factory,
+                                     PasswordAuthenticator passwordAuthenticator)
+    {
+        sshdServer.setPort(port);
+        sshdServer.setKeyPairProvider(new SimpleGeneratorHostKeyProvider("hostkey.ser"));
+        sshdServer.setSubsystemFactories(Arrays.<NamedFactory<Command>> asList(factory));
+        sshdServer.setCommandFactory(new ScpCommandFactory());
+        sshdServer.setShellFactory(new ProcessShellFactory());
+        sshdServer.setPasswordAuthenticator(passwordAuthenticator);
+    }
 
     private SftpSubsystem.Factory createFtpSubsystemFactory()
     {
@@ -48,39 +59,47 @@ public class SftpServer {
         return factory;
     }
 
-
     private void configureSecurityProvider()
     {
         Security.addProvider(new BouncyCastleProvider());
     }
 
-	private static PasswordAuthenticator passwordAuthenticator() {
-		return new PasswordAuthenticator() {
+    private static PasswordAuthenticator passwordAuthenticator()
+    {
+        return new PasswordAuthenticator()
+        {
 
-			@Override
-			public boolean authenticate(String arg0, String arg1, ServerSession arg2) {
-				return USERNAME.equals(arg0) && PASSWORD.equals(arg1);
-			}
-		};
-	}
+            @Override
+            public boolean authenticate(String arg0, String arg1, ServerSession arg2)
+            {
+                return USERNAME.equals(arg0) && PASSWORD.equals(arg1);
+            }
+        };
+    }
 
-	public void start()
-	{
-		try {
-			sshdServer.start();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void stop()
-	{
-		try {
-			sshdServer.stop();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		sshdServer = null;
-	}
-	
+    public void start()
+    {
+        try
+        {
+            sshdServer.start();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop()
+    {
+        try
+        {
+            sshdServer.stop();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        sshdServer = null;
+    }
+
 }
