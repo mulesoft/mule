7d6374ef64bafcb724b71cc43a81e668d44cf551
mike.schilling <mike.schilling>
2012-02-07 19:34:38 +0000

    Merged revisions 23834 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.2.x
    ........
      r23834 | dirk.olmes | 2012-02-07 10:33:38 -0300 (Tue, 07 Feb 2012) | 2 lines
      
      fix license header
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@23847 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/issues/AsyncComponentLifecycleIssue5649TestCase.java
+++ tests/integration/src/test/java/org/mule/issues/AsyncComponentLifecycleIssue5649TestCase.java
@@ -1,3 +1,13 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.issues;
 
 import static org.junit.Assert.assertEquals;
@@ -38,7 +48,6 @@ public class AsyncComponentLifecycleIssue5649TestCase extends FunctionalTestCase
         return "org/mule/issues/async-component-lifecycle-5649.xml";
     }
 
-    
     @Test
     public void testConfig() throws Exception
     {
@@ -57,5 +66,4 @@ public class AsyncComponentLifecycleIssue5649TestCase extends FunctionalTestCase
     {
         componentPhases.add(newPhase);
     }
-
 }
