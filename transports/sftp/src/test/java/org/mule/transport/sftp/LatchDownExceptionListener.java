61c5179d53a8b5afd7b0d5d708befa90a4ce8735
Daniel Feist <dfeist@gmail.com>
2012-02-07 18:17:36 +0000

    Add missing license headers
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@23839 bf997673-6b11-0410-b953-e057580c5b09

--- transports/sftp/src/test/java/org/mule/transport/sftp/LatchDownExceptionListener.java
+++ transports/sftp/src/test/java/org/mule/transport/sftp/LatchDownExceptionListener.java
@@ -1,6 +1,13 @@
-/**
- * 
+/*
+ * $Id: ProcessingTime.java 23484 2011-12-14 01:26:29Z dfeist $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
  */
+
 package org.mule.transport.sftp;
 
 import org.mule.api.context.notification.ExceptionNotificationListener;
@@ -9,8 +16,7 @@ import org.mule.api.context.notification.ServerNotification;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * @author alejandrosequeira
- * Exception listener that decreases a latch when notified.
+ * @author alejandrosequeira Exception listener that decreases a latch when notified.
  */
 @SuppressWarnings("rawtypes")
 public class LatchDownExceptionListener implements ExceptionNotificationListener
