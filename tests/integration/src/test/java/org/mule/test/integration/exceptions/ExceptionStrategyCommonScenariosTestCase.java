ae722f60f6ab7c895ca29b946b215d363c5b4a73
Pablo La Greca <pablolagreca@hotmail.com>
2012-02-15 21:17:17 +0000

    Merged revisions 23872 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.1.x
    ........
      r23872 | pablo.lagreca | 2012-02-15 17:28:55 -0300 (Wed, 15 Feb 2012) | 1 line
      
      EE-2636 - since mule 3.2.0 exception strategy started checking exception pattern in exception strategy against wrapper exceptions instead of wrapped one. That is wrong and behavior is restored
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.2.x@23873 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/exceptions/ExceptionStrategyCommonScenariosTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/exceptions/ExceptionStrategyCommonScenariosTestCase.java
@@ -78,7 +78,6 @@ public class ExceptionStrategyCommonScenariosTestCase extends AbstractServiceAnd
     public ExceptionStrategyCommonScenariosTestCase(AbstractServiceAndFlowTestCase.ConfigVariant variant, String configResources)
     {
         super(variant, configResources);
-        setDisposeContextPerClass(false);
     }
 
     @Parameterized.Parameters
