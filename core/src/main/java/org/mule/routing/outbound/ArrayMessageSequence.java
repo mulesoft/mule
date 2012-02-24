ac49e2d8dc3fa86cc5b41bd552e49135766cc465
Santiago Vacas <svacas@gmail.com>
2012-02-24 14:53:04 +0000

    MULE-6067: <collection-splitter> should handle arrays
    add missing class
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@23912 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/main/java/org/mule/routing/outbound/ArrayMessageSequence.java
@@ -0,0 +1,47 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.routing.outbound;
+
+import org.mule.routing.AbstractMessageSequence;
+
+public class ArrayMessageSequence extends AbstractMessageSequence<Object>
+{
+
+    private Object[] array;
+    private int idx;
+
+    public ArrayMessageSequence(Object[] array)
+    {
+        this.array = array;
+        idx = 0;
+    }
+
+    @Override
+    public int size()
+    {
+        return array.length - idx;
+    }
+
+    @Override
+    public boolean hasNext()
+    {
+        return idx < array.length;
+    }
+
+    @Override
+    public Object next()
+    {
+        return array[idx++];
+    }
+
+}
+
+
