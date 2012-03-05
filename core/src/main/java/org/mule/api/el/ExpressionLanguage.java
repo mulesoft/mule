23b402d5f1f5f307570b9b70c3eb255548728635
Daniel Feist <dfeist@gmail.com>
2012-03-05 17:43:51 +0000

    MULE-6103 Define ExpressionLanguage interface
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@23939 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/main/java/org/mule/api/el/ExpressionLanguage.java
@@ -0,0 +1,97 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.api.el;
+
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleMessage;
+import org.mule.api.expression.ExpressionRuntimeException;
+
+import java.util.Map;
+
+/**
+ * Allows for the execution of expressions within Mule using an expression language. Expression language
+ * implementations should not only wrap an expression language runtime but populate the context such that
+ * expression can be used to access, and optionally mutate, both Mule configuration and the current message
+ * being processed via expression variables. Runtime exceptions should be caught and wrapped in a Mule
+ * {@link ExpressionRuntimeException} before being re-thrown.
+ * 
+ * @since 3.3.0
+ */
+public interface ExpressionLanguage
+{
+
+    /**
+     * Execute the expression resulting the result. The expression will be executed without any event or
+     * message context.
+     * 
+     * @param <T> the return type expected
+     * @param expression the expression to be executed
+     * @return the result of execution of the expression.
+     */
+    <T> T execute(String expression);
+
+    /**
+     * Execute the expression resulting the result. The expression will be executed without any event or
+     * message context. A Map of variables can be provided that will be able to the expression when executed.
+     * Variable provided in the map will only be available if there are not conflicts with existing context
+     * variables provided by the expression language implementation.
+     * 
+     * @param <T> the return type expected
+     * @param expression the expression to be executed
+     * @param vars a map of expression variables
+     * @return the result of execution of the expression.
+     */
+    <T> T execute(String expression, Map<String, ? extends Object> vars);
+
+    /**
+     * Execute the expression resulting the result. The expression will be executed with MuleEvent context,
+     * meaning the expression language implementation should provided access to the message.
+     * 
+     * @param <T> the return type expected
+     * @param expression the expression to be executed
+     * @param event the current event being processed
+     * @return the result of execution of the expression.
+     */
+    <T> T execute(String expression, MuleEvent event);
+
+    /**
+     * Execute the expression resulting the result. The expression will be executed with MuleEvent context,
+     * meaning the expression language implementation should provided access to the message. A Map of
+     * variables can be provided that will be able to the expression when executed. Variable provided in the
+     * map will only be available if there are not conflicts with existing context variables provided by the
+     * expression language implementation.
+     * 
+     * @param <T> the return type expected
+     * @param expression the expression to be executed
+     * @param event the current event being processed
+     * @param vars a map of expression variables
+     * @return the result of execution of the expression.
+     */
+    <T> T execute(String expression, MuleEvent event, Map<String, ? extends Object> vars);
+
+    /**
+     * Validates the expression. All implementors should should validate expression syntactically. Semantic
+     * validation is optional.
+     * 
+     * @param expression
+     * @return
+     */
+    boolean isValid(String expression);
+
+    /**
+     * Required to provide gradual migration to use of MuleEvent signatures
+     * 
+     * @see ExpressionLanguage#execute(String, MuleEvent)
+     */
+    @Deprecated
+    <T> T execute(String expression, MuleMessage message);
+
+}
