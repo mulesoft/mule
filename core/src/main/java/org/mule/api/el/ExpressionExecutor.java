97b13af31b72aeccc631f8efd86cbd54e71b436d
Daniel Feist <dfeist@gmail.com>
2012-03-12 18:36:45 +0000

    Further work to improve API and extensibility, also added more tests (MULE-6116 Define and implement ExpressionLanguage extension mechanism)
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@24013 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/main/java/org/mule/api/el/ExpressionExecutor.java
@@ -0,0 +1,39 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.api.el;
+
+/**
+ * Wraps an expression language engine. Implementations should not wrap expression language engine exceptions,
+ * but rather the {@link ExpressionLanguage} implementation should handle them.
+ * 
+ * @since 3.3
+ */
+public interface ExpressionExecutor<T extends ExpressionLanguageContext>
+{
+
+    /**
+     * Execute an expression using using the provided context.
+     * 
+     * @param expression
+     * @param context
+     * @return
+     * @throws native expression language
+     */
+    public Object execute(String expression, T context);
+
+    /**
+     * Validate the expression
+     * 
+     * @param expression
+     */
+    public void validate(String expression);
+
+}
