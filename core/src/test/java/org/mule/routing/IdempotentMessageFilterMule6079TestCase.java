5027d08a50e0275286d9cb00ea0fd429ea8e3f7d
saveli.vassiliev <saveli.vassiliev>
2012-04-15 05:16:02 +0000

    Merged revisions 24237,24244,24250 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.2.x
    ........
      r24237 | saveli.vassiliev | 2012-04-04 11:54:15 -0300 (Wed, 04 Apr 2012) | 4 lines
      
      MULE-6079: IdempotentMessageFilter can process a given message multiple times
      
      Changes as suggested in issue description, no test case added.
    ........
      r24244 | saveli.vassiliev | 2012-04-11 09:18:02 -0300 (Wed, 11 Apr 2012) | 4 lines
      
      MULE-6079: IdempotentMessageFilter can process a given message multiple times
      
      Added test case
    ........
      r24250 | saveli.vassiliev | 2012-04-13 10:38:55 -0300 (Fri, 13 Apr 2012) | 4 lines
      
      MULE-6079: IdempotentMessageFilter can process a given message multiple times
      
      Made code look according to Mule Style. Removed useless methods for testing. Enforced to fail test if some of child threads fired an exception that was not excpected.  
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.3.x@24257 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/test/java/org/mule/routing/IdempotentMessageFilterMule6079TestCase.java
@@ -0,0 +1,201 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.routing;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.mule.DefaultMuleEvent;
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleMessage;
+import org.mule.api.MuleSession;
+import org.mule.api.endpoint.InboundEndpoint;
+import org.mule.api.service.Service;
+import org.mule.api.store.ObjectAlreadyExistsException;
+import org.mule.api.store.ObjectStore;
+import org.mule.api.store.ObjectStoreException;
+import org.mule.tck.MuleTestUtils;
+import org.mule.tck.junit4.AbstractMuleContextTestCase;
+
+import com.mockobjects.dynamic.Mock;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.Test;
+
+public class IdempotentMessageFilterMule6079TestCase extends AbstractMuleContextTestCase
+{
+
+    private Service service;
+    private InboundEndpoint inboundEndpoint;
+    private Mock session;
+    private ObjectStore<String> objectStore;
+    private IdempotentMessageFilter idempotentMessageFilter;
+    private Integer processedEvents = 0;
+    private Boolean errorHappenedInChildThreads = false;
+
+    /*
+     * This test admits two execution paths, note that the implementation of objectStore can lock on the await call of
+     * the latch, to avoid this a countDown call was added to contains method, since there is a trace that locks
+     * otherwise. See implementation of IdempotentMessageFilter.isNewMessage to understand the trace.
+     */
+    @Test
+    public void testRaceConditionOnAcceptAndProcess() throws Exception
+    {
+        inboundEndpoint = getTestInboundEndpoint("Test", "test://Test?exchangePattern=one-way");
+        session = MuleTestUtils.getMockSession();
+        service = getTestService();
+        session.matchAndReturn("getFlowConstruct", service);
+        CountDownLatch cdl = new CountDownLatch(2);
+
+        objectStore = new RaceConditionEnforcingObjectStore(cdl);
+        idempotentMessageFilter = new IdempotentMessageFilter();
+        idempotentMessageFilter.setIdExpression("#[header:id]");
+        idempotentMessageFilter.setFlowConstruct(service);
+        idempotentMessageFilter.setThrowOnUnaccepted(false);
+        idempotentMessageFilter.setStorePrefix("foo");
+        idempotentMessageFilter.setStore(objectStore);
+
+        Thread t1 = new Thread(new TestForRaceConditionRunnable(), "thread1");
+        Thread t2 = new Thread(new TestForRaceConditionRunnable(), "thread2");
+        t1.start();
+        t2.start();
+        t1.join(5000);
+        t2.join(5000);
+        assertFalse("Exception in child threads", errorHappenedInChildThreads);
+        assertEquals("None or more than one message was processed by IdempotentMessageFilter",
+                     new Integer(1), processedEvents);
+    }
+
+    private class TestForRaceConditionRunnable implements Runnable
+    {
+
+        public TestForRaceConditionRunnable()
+        {
+        }
+
+        @Override
+        public void run()
+        {
+            MuleMessage okMessage = new DefaultMuleMessage("OK", muleContext);
+            okMessage.setOutboundProperty("id", "1");
+            MuleEvent event = new DefaultMuleEvent(okMessage, inboundEndpoint, (MuleSession) session.proxy());
+
+            try
+            {
+                event = idempotentMessageFilter.process(event);
+            }
+            catch (Throwable e)
+            {
+                e.printStackTrace();
+                synchronized (errorHappenedInChildThreads)
+                {
+                    errorHappenedInChildThreads = true;
+                }
+            }
+
+            if (event != null)
+            {
+                synchronized (processedEvents)  // shared
+                {
+                    processedEvents++;
+                }
+            }
+        }
+    }
+
+    private class RaceConditionEnforcingObjectStore implements ObjectStore<String>
+    {
+
+        protected CountDownLatch barrier;
+        Map<Serializable, String> map = new TreeMap<Serializable, String>();
+
+        public RaceConditionEnforcingObjectStore(CountDownLatch latch)
+        {
+            barrier = latch;
+        }
+
+        @Override
+        public boolean contains(Serializable key) throws ObjectStoreException
+        {
+            if (key == null)
+            {
+                throw new ObjectStoreException();
+            }
+            boolean containsKey;
+            synchronized (this)
+            {
+                // avoiding deadlock with the latch (locks if the element was already added to map, see definition of
+                // IdempotentMessageFilter.isNewMessage definition, if the element is added, it wont enter the
+                // objectStore.store method, and will lock.
+                containsKey = map.containsKey(key);
+                if (containsKey)
+                {
+                    barrier.countDown();
+                }
+            }
+            return containsKey;
+        }
+
+        @Override
+        public void store(Serializable key, String value) throws ObjectStoreException
+        {
+            boolean wasAdded;
+            if (key == null)
+            {
+                throw new ObjectStoreException();
+            }
+            synchronized (map) // map is shared
+            {
+                wasAdded = map.containsKey(key);
+                map.put(key, value);
+            }
+            barrier.countDown();
+            try
+            {
+                barrier.await();
+            }
+            catch (Exception e)
+            {
+                synchronized (errorHappenedInChildThreads)
+                {
+                    errorHappenedInChildThreads = true;
+                }
+            }
+            if (wasAdded)
+            {
+                throw new ObjectAlreadyExistsException();
+            }
+        }
+
+        @Override
+        public String retrieve(Serializable key) throws ObjectStoreException
+        {
+            return null;
+        }
+
+        @Override
+        public String remove(Serializable key) throws ObjectStoreException
+        {
+            return null;
+        }
+
+        @Override
+        public boolean isPersistent()
+        {
+            return false;
+        }
+    }
+}
