124d4d5005f309cea58d43a493d291ce653b99b7
Daniel Feist <dfeist@gmail.com>
2012-04-16 20:04:08 +0000

    MULE-6147 The behavior of processors that have no return value are inconsistent
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.3.x@24284 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/main/java/org/mule/processor/chain/SimpleMessageProcessorChainBuilder.java
@@ -0,0 +1,43 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.processor.chain;
+
+import org.mule.api.construct.FlowConstruct;
+import org.mule.api.processor.MessageProcessor;
+
+import java.util.LinkedList;
+
+public class SimpleMessageProcessorChainBuilder extends DefaultMessageProcessorChainBuilder
+{
+
+    public SimpleMessageProcessorChainBuilder()
+    {
+        // empty
+    }
+
+    public SimpleMessageProcessorChainBuilder(FlowConstruct flowConstruct)
+    {
+        this.flowConstruct = flowConstruct;
+    }
+
+    @Override
+    protected DefaultMessageProcessorChain createInnerChain(LinkedList<MessageProcessor> tempList)
+    {
+        return new SimpleMessageProcessorChain(tempList);
+    }
+
+    @Override
+    protected DefaultMessageProcessorChain createOuterChain(LinkedList<MessageProcessor> tempList)
+    {
+        return new SimpleMessageProcessorChain(tempList);
+    }
+
+}
