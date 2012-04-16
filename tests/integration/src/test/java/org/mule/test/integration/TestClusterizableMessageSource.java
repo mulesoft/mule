1a514a672e64bb49fd1acee1ddbf058bb792a5d1
Pablo Kraan <pablo.kraan@gmail.com>
2012-04-16 12:54:28 +0000

    MULE-6164: Add support for Cluster aware MessageSource
    _ Adding functional test for ClusterizableMessageSource in flow 
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.3.x@24273 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/integration/TestClusterizableMessageSource.java
@@ -0,0 +1,57 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.integration;
+
+import org.mule.DefaultMuleEvent;
+import org.mule.DefaultMuleMessage;
+import org.mule.MessageExchangePattern;
+import org.mule.api.MuleContext;
+import org.mule.api.MuleException;
+import org.mule.api.construct.FlowConstruct;
+import org.mule.api.construct.FlowConstructAware;
+import org.mule.api.context.MuleContextAware;
+import org.mule.api.lifecycle.Startable;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.api.source.ClusterizableMessageSource;
+
+public class TestClusterizableMessageSource implements ClusterizableMessageSource, Startable, MuleContextAware, FlowConstructAware
+{
+
+    private MessageProcessor listener;
+    private MuleContext context;
+    private FlowConstruct flowConstruct;
+
+    @Override
+    public void start() throws MuleException
+    {
+        DefaultMuleMessage muleMessage = new DefaultMuleMessage("TEST", context);
+        DefaultMuleEvent defaultMuleEvent = new DefaultMuleEvent(muleMessage, MessageExchangePattern.ONE_WAY, flowConstruct);
+        listener.process(defaultMuleEvent);
+    }
+
+    @Override
+    public void setListener(MessageProcessor listener)
+    {
+        this.listener = listener;
+    }
+
+    @Override
+    public void setMuleContext(MuleContext context)
+    {
+        this.context = context;
+    }
+
+    @Override
+    public void setFlowConstruct(FlowConstruct flowConstruct)
+    {
+        this.flowConstruct = flowConstruct;
+    }
+}
