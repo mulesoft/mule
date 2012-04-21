940ddc2725c81744b98605b4fa14d94f6d76b55b
Pablo Kraan <pablo.kraan@gmail.com>
2012-04-21 14:46:34 +0000

    MULE-6176: Expression evaluators that can be used in filters only fail with obscure error messages if used in no valid places
    _ Adding beans for all the fake expression evaluators that can be used inside filter expressions only in order to obtain a cleaner error message
    _ Adding unit and functional tests
    _ Removing duplicated constants from ExpressionFilter
    _ Adding SystemProperty to manage System properties as external resources in tests 
    
    TODO:
    _ Refactoring DynamicPort to extend SystemProperty
    _ Replace usages of System.setProperty in tests with SystemProperty rule.
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.3.x@24316 bf997673-6b11-0410-b953-e057580c5b09

---
+++ core/src/main/java/org/mule/expression/IllegalExpressionEvaluator.java
@@ -0,0 +1,30 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.expression;
+
+import org.mule.api.MuleMessage;
+import org.mule.api.expression.ExpressionEvaluator;
+
+/**
+ * Defines an {@link ExpressionEvaluator} that can never be evaluated.
+ */
+public abstract class IllegalExpressionEvaluator implements ExpressionEvaluator
+{
+
+    public static final String ILLEGAL_EVALUATOR_MESSAGE = "Evaluator %s can not be used in this context";
+
+    @Override
+    public Object evaluate(String expression, MuleMessage message)
+    {
+        throw new UnsupportedOperationException(String.format(ILLEGAL_EVALUATOR_MESSAGE, getName()));
+    }
+
+}
