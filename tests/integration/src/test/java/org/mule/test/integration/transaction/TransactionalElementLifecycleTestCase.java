62c6df58dee1b8df5067a1a26e0e41fc38c252ad
Pablo La Greca <pablolagreca@hotmail.com>
2012-04-24 17:57:41 +0000

    Merged revisions 24341 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.3.x
    ........
      r24341 | pablo.lagreca | 2012-04-24 12:42:41 -0300 (Tue, 24 Apr 2012) | 1 line
      
      MULE-6184 - fixing lifecycle issue with TransactionalInterceptingMessageProcessor
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@24345 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/integration/transaction/TransactionalElementLifecycleTestCase.java
@@ -0,0 +1,52 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.integration.transaction;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+import org.mule.api.MuleEventContext;
+import org.mule.api.client.LocalMuleClient;
+import org.mule.tck.functional.EventCallback;
+import org.mule.tck.functional.FunctionalTestComponent;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.util.concurrent.Latch;
+
+import java.util.concurrent.TimeUnit;
+
+public class TransactionalElementLifecycleTestCase extends FunctionalTestCase
+{
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/test/integration/transaction/transactional-lifecycle-config.xml";
+    }
+
+
+    @Test
+    public void testInitializeIsCalledInInnerExceptionStrategy() throws Exception
+    {
+        final Latch endDlqFlowLatch = new Latch();
+        LocalMuleClient client = muleContext.getClient();
+        FunctionalTestComponent functionalTestComponent = getFunctionalTestComponent("dlq-out");
+        functionalTestComponent.setEventCallback(new EventCallback()
+        {
+            @Override
+            public void eventReceived(MuleEventContext context, Object component) throws Exception
+            {
+                endDlqFlowLatch.release();
+            }
+        });
+        client.send("vm://in","message",null,RECEIVE_TIMEOUT);
+        if (!endDlqFlowLatch.await(RECEIVE_TIMEOUT, TimeUnit.MILLISECONDS))
+        {
+            fail("message wasn't received by dlq flow");
+        }
+    }
+}
