678c6b49c2aa6625d616a91e3b22207137ab704d
Daniel Feist <dfeist@gmail.com>
2012-05-25 02:24:54 +0000

    Add missing javadoc in Mule Expression Language
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.3.x@24403 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/el/context/MessageContext.java
+++ core/src/main/java/org/mule/el/context/MessageContext.java
@@ -19,6 +19,23 @@ import java.util.Map;
 
 import javax.activation.DataHandler;
 
+/**
+ * Exposes information about the current Mule message:
+ * 
+ * <li> <b>id</b>                    <i>The unique message id</i>
+ * <li> <b>rootId</b>                <i>The root message id.  The id of the message before being split into parts. 
+ *                                      If was is not split then this value is the same as the id.</i>
+ * <li> <b>correlationId</b>         <i>The message correlationId.</i>
+ * <li> <b>correlationSequence</b>   <i>The message correlation sequence number.</i>
+ * <li> <b>correlationGroupSize</b>  <i>The message correlation group size.</i>
+ * <li> <b>dataType</b>              <i>The message data type (org.mule.api.transformer.DataType).</i>
+ * <li> <b>replyTo</b>               <i>The message reply to destination. (mutable)</i>
+ * <li> <b>payload</b>               <i>The message payload (mutable).  You can also use message.payloadAs(Class clazz).</i>
+ * <li> <b>inboundProperties</b>     <i>Map of inbound message properties (immutable).</i>
+ * <li> <b>outboundProperties</b>    <i>Map of outbound message properties.</i>
+ * <li> <b>inboundAttachements</b>   <i>Map of inbound message attachments (immutable).</i>
+ * <li> <b>outboundAttachements</b>  <i>Map of outbound message attachments.</i>
+ */
 public class MessageContext
 {
     protected MuleMessage message;
