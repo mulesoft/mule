dc5acb7ea00da93b88aeaa70f13b9fa0be00c66a
Santiago Vacas <svacas@gmail.com>
2012-05-28 23:11:32 +0000

    Merged revisions 24425-24427 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.3.x
    ................
      r24425 | svacas | 2012-05-28 02:18:01 -0300 (Mon, 28 May 2012) | 2 lines
      
      MULE-6250: Upgrade Spring to 3.1.1
    ................
      r24426 | svacas | 2012-05-28 02:56:04 -0300 (Mon, 28 May 2012) | 9 lines
      
      Merged revisions 24424 via svnmerge from 
      https://svn.codehaus.org/mule/branches/mule-3.2.x
      
      ........
        r24424 | svacas | 2012-05-28 00:37:55 -0300 (Mon, 28 May 2012) | 2 lines
        
        MULE-6249: Custom Exception Strategy class not getting being invoked in a Jetty Inbound Endpoint
      ........
    ................
      r24427 | svacas | 2012-05-28 03:44:57 -0300 (Mon, 28 May 2012) | 4 lines
      
      MULE-6249: Custom Exception Strategy class not getting being invoked in a Jetty Inbound Endpoint
      
      fix broken test class
    ................
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@24444 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/jetty/src/test/java/org/mule/transport/servlet/jetty/functional/JettyCustomExceptionStrategyTestCase.java
@@ -0,0 +1,44 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.servlet.jetty.functional;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.mule.api.MuleMessage;
+import org.mule.api.client.MuleClient;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.transport.http.HttpConnector;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class JettyCustomExceptionStrategyTestCase extends FunctionalTestCase
+{
+    @Rule
+    public DynamicPort dynamicPort1 = new DynamicPort("port1");
+
+    protected String getConfigResources()
+    {
+        return "jetty-custom-exception-strategy.xml";
+    }
+
+    @Test
+    public void customExceptionStrategy() throws Exception
+    {
+        MuleClient client = muleContext.getClient();
+        MuleMessage response = client.send("http://localhost:" + dynamicPort1.getNumber() + "/test", "test", null);
+        assertNotNull(response);
+        assertNotNull(response.getInboundProperty("CustomES"));
+        assertEquals(response.getInboundProperty(HttpConnector.HTTP_STATUS_PROPERTY), "400");
+    }
+}
---
+++ transports/jetty/src/test/java/org/mule/transport/servlet/jetty/functional/PreservePayloadExceptionStrategy.java
@@ -0,0 +1,41 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.servlet.jetty.functional;
+
+import org.mule.api.MuleContext;
+import org.mule.api.MuleEvent;
+import org.mule.api.exception.RollbackSourceCallback;
+import org.mule.exception.AbstractMessagingExceptionStrategy;
+import org.mule.transport.http.HttpConnector;
+
+public class PreservePayloadExceptionStrategy extends AbstractMessagingExceptionStrategy
+{
+    public PreservePayloadExceptionStrategy(MuleContext muleContext)
+    {
+        super(muleContext);
+    }
+
+    private MuleEvent processException(Exception e, MuleEvent event, RollbackSourceCallback rollbackCallback)
+    {
+        Object payloadBeforeException = event.getMessage().getPayload();
+        event.getMessage().setPayload(payloadBeforeException);
+        event.getMessage().setExceptionPayload(null);
+        event.getMessage().setOutboundProperty("CustomES", "CustomES");
+        event.getMessage().setOutboundProperty(HttpConnector.HTTP_STATUS_PROPERTY, "400");
+        return event;
+    }
+
+    @Override
+    public MuleEvent handleException(Exception e, MuleEvent event)
+    {
+        return processException(e, event, null);
+    }
+}
