f06d56f2d392786b756fe024694c1781160b6e7b
Pablo Kraan <pablo.kraan@gmail.com>
2012-06-08 00:20:39 +0000

    Merged revisions 24531 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.3.x
    ................
      r24531 | pablo.kraan | 2012-06-07 21:16:03 -0300 (Thu, 07 Jun 2012) | 16 lines
      
      Merged revisions 24530 via svnmerge from 
      https://svn.codehaus.org/mule/branches/mule-3.2.x
      
      ................
        r24530 | pablo.kraan | 2012-06-07 20:50:45 -0300 (Thu, 07 Jun 2012) | 9 lines
        
        Merged revisions 24529 via svnmerge from 
        https://svn.codehaus.org/mule/branches/mule-3.1.x
        
        ........
          r24529 | pablo.kraan | 2012-06-07 20:27:22 -0300 (Thu, 07 Jun 2012) | 2 lines
          
          MULE-6287: ClassCastException when binding interface returns null
        ........
      ................
    ................
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@24532 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/component/BindingInvocationHandler.java
+++ core/src/main/java/org/mule/component/BindingInvocationHandler.java
@@ -161,6 +161,10 @@ public class BindingInvocationHandler implements InvocationHandler
         {
             return reply;
         }
+        else if (reply.getPayload() == NullPayload.getInstance() && !bindingMethod.getReturnType().isInstance(reply.getPayload()))
+        {
+            return null;
+        }
         else
         {
             return reply.getPayload();
