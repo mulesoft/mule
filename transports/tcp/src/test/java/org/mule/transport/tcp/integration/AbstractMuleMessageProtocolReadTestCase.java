3f82fe86b39949fa56c08ccd8e21ff5e88a2692e
Pablo Kraan <pablo.kraan@gmail.com>
2012-06-29 17:56:16 +0000

    Merged revisions 24588 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.3.x
    ................
      r24588 | pablo.kraan | 2012-06-29 14:36:53 -0300 (Fri, 29 Jun 2012) | 21 lines
      
      Merged revisions 24587 via svnmerge from 
      https://svn.codehaus.org/mule/branches/mule-3.2.x
      
      ................
        r24587 | pablo.kraan | 2012-06-29 14:05:18 -0300 (Fri, 29 Jun 2012) | 14 lines
        
        Merged revisions 24586 via svnmerge from 
        https://svn.codehaus.org/mule/branches/mule-3.1.x
        
        ........
          r24586 | pablo.kraan | 2012-06-29 12:52:15 -0300 (Fri, 29 Jun 2012) | 7 lines
          
          MULE-6289: Mule Context missing when receiving a message on TCP inbound endpoint
          
          _ Updating MuleMessageWorker to be instantiable instead of an utility class
          _ Injecting muleContext in the MuleMessageWorker contructor
          _ Implementing MuleContextAware in the MuleMessageXProtocol classes in order to set the muleContext in the used wire format
          _ Adding functional test cases for all the updated protocols
        ........
      ................
    ................
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@24589 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/tcp/src/test/java/org/mule/transport/tcp/integration/AbstractMuleMessageProtocolReadTestCase.java
@@ -0,0 +1,65 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.tcp.integration;
+
+import static org.junit.Assert.assertEquals;
+
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleException;
+import org.mule.api.MuleMessage;
+import org.mule.api.client.LocalMuleClient;
+import org.mule.api.transformer.wire.WireFormat;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.transformer.wire.SerializedMuleMessageWireFormat;
+import org.mule.transport.tcp.TcpProtocol;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public abstract class AbstractMuleMessageProtocolReadTestCase extends FunctionalTestCase
+{
+
+    @Rule
+    public DynamicPort port = new DynamicPort("port");
+
+
+    @Test
+    public void testServer() throws Exception
+    {
+        LocalMuleClient client = muleContext.getClient();
+        safeProtocolSend("localhost", port.getNumber(), new DefaultMuleMessage(TEST_MESSAGE, muleContext));
+        MuleMessage response = client.request("vm://testOut", RECEIVE_TIMEOUT);
+        assertEquals(TEST_MESSAGE, response.getPayload());
+    }
+
+    private void safeProtocolSend(String host, int port, DefaultMuleMessage msg) throws IOException, MuleException
+    {
+        Socket clientSocket = new Socket(host, port);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        WireFormat wireFormat = new SerializedMuleMessageWireFormat();
+        wireFormat.setMuleContext(muleContext);
+        wireFormat.write(baos, msg, msg.getEncoding());
+        TcpProtocol delegate = createMuleMessageProtocol();
+        delegate.write(outToServer, baos.toByteArray());
+        clientSocket.close();
+    }
+
+    protected abstract TcpProtocol createMuleMessageProtocol();
+
+}
