6be2b8c9e856a580d2ad7402cb0fb3b9ca4313f9
Evangelina Martinez Ruiz Moreno <evangelina.martinez@mulesource.com>
2012-07-04 02:43:23 +0000

    Merged revisions 24597 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.3.x
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@24598 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/xml/src/main/java/org/mule/module/xml/stax/EndElementEventX.java
@@ -0,0 +1,36 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+
+package org.mule.module.xml.stax;
+
+import java.util.List;
+
+import javanet.staxutils.events.EndElementEvent;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.events.Namespace;
+
+public class EndElementEventX extends EndElementEvent
+{
+    private List<Namespace> namespaces2;
+
+    public EndElementEventX(QName name, List<Namespace> namespaces)
+    {
+        super(name, namespaces.iterator());
+        this.namespaces2 = namespaces;
+    }
+
+    public List<Namespace> getNamespaceList()
+    {
+        return namespaces2;
+    }
+
+}
