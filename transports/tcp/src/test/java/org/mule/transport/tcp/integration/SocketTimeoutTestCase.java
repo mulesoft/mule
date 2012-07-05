afb3a654961a477e4574c4e5fd2a4fe91ee24b31
Santiago Vacas <svacas@gmail.com>
2012-07-05 16:11:54 +0000

    Merged revisions 24604 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.3.x
    ................
      r24604 | svacas | 2012-07-05 11:58:48 -0300 (Thu, 05 Jul 2012) | 9 lines
      
      Merged revisions 24603 via svnmerge from 
      https://svn.codehaus.org/mule/branches/mule-3.2.x
      
      ........
        r24603 | svacas | 2012-07-05 11:20:22 -0300 (Thu, 05 Jul 2012) | 2 lines
        
        MULE-6326: TCP timeouts are not working
      ........
    ................
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@24605 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/tcp/src/test/java/org/mule/transport/tcp/integration/SocketTimeoutTestCase.java
@@ -0,0 +1,77 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.tcp.integration;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mule.api.FutureMessageResult;
+import org.mule.api.MuleEventContext;
+import org.mule.api.MuleException;
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.functional.EventCallback;
+import org.mule.tck.functional.FunctionalTestComponent;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.Assert.*;
+
+public class SocketTimeoutTestCase extends FunctionalTestCase
+{
+
+    @Rule
+    public DynamicPort dynamicPort = new DynamicPort("port1");
+
+    protected String getConfigResources()
+    {
+        return "tcp-outbound-timeout-config.xml";
+    }
+
+    @Test
+    public void socketReadWriteResponseTimeout() throws Exception
+    {
+        final MuleClient client = new MuleClient(muleContext);
+        FutureMessageResult result = client.sendAsync("vm://inboundTest1", "something", null);
+        MuleMessage message = null;
+        try
+        {
+            message = result.getMessage(1000);
+        }
+        catch (TimeoutException e)
+        {
+            fail("Response timeout not honored.");
+        }
+        assertNotNull(message);
+    }
+
+    @Test
+    public void socketConnectionResponseTimeout() throws Exception
+    {
+        final MuleClient client = new MuleClient(muleContext);
+        FutureMessageResult result = client.sendAsync("vm://inboundTest2", "something", null);
+        MuleMessage message = null;
+        try
+        {
+            message = result.getMessage(1000);
+        }
+        catch (TimeoutException e)
+        {
+            fail("Response timeout not honored.");
+        }
+        assertNotNull(message);
+    }
+
+}
