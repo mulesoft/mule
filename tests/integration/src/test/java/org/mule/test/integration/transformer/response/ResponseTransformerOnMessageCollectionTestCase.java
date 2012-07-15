27a4aabe2126f226254232536403ccf8912e6bbc
Pablo Kraan <pablo.kraan@gmail.com>
2012-07-15 11:19:56 +0000

    Merged revisions 24621 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.3.x
    ................
      r24621 | pablo.kraan | 2012-07-14 23:01:40 -0300 (Sat, 14 Jul 2012) | 31 lines
      
      Merged revisions 24620 via svnmerge from 
      https://svn.codehaus.org/mule/branches/mule-3.2.x
      
      ................
        r24620 | pablo.kraan | 2012-07-14 20:10:21 -0300 (Sat, 14 Jul 2012) | 24 lines
        
        Merged revisions 24619 via svnmerge from 
        https://svn.codehaus.org/mule/branches/mule-3.1.x
        
        ........
          r24619 | pablo.kraan | 2012-07-14 14:01:32 -0300 (Sat, 14 Jul 2012) | 17 lines
          
          MULE-5860: Inserting <collection-splitter /><collection-aggregator /> into a flow produces unexpected results
          MULE-4213: Response transformer results are discarded when using multicasting router
          MULE-5534: CLONE - Message modifications are discarded when using Collection Aggregator
          
          These issues are caused by the same problem: transformations on MessageCollection are "lost".
          The cause of the problem is that setting a payload on a DefaultMessageCollection during a transformation
          does not change the "message collection" nature of the instance, ie: it does not change the class of the 
          message to a simple DefaultMuleMessage. The reason is that the transformation is executed inside the message
          itself, so is imposible to change the instance class.
          This fix is intended to resolve the main problems in the most common scenarios and is not as nice as I would like.
          The idea is: a message collection is a valid MuleMessageCollection as long as the payload is not touched. 
          When that occurs, the message collection is "invalidated", meaning that all the message collection methods will throw
          an error in case they are invoked.
          Then when a transformer is invoked (it must extend from AbstractTransformer) there is a check that in case the message is a
          DefaultMuleMessage which has been invalidated, then the result of the transformation will be a new MuleMessage 
          containing the current payload of the invalidated collection.
        ........
      ................
    ................
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@24622 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/integration/transformer/response/ResponseTransformerOnMessageCollectionTestCase.java
@@ -0,0 +1,52 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.integration.transformer.response;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import org.mule.api.MuleMessage;
+import org.mule.api.MuleMessageCollection;
+import org.mule.api.client.LocalMuleClient;
+import org.mule.tck.AbstractServiceAndFlowTestCase;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+public class ResponseTransformerOnMessageCollectionTestCase extends AbstractServiceAndFlowTestCase
+{
+
+    public ResponseTransformerOnMessageCollectionTestCase(ConfigVariant variant, String configResources)
+    {
+        super(variant, configResources);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> parameters()
+    {
+        return Arrays.asList(new Object[][] {
+                {ConfigVariant.SERVICE, "org/mule/test/integration/transformer/response/response-transformer-on-message-collection-service-config.xml"},
+                {ConfigVariant.FLOW, "org/mule/test/integration/transformer/response/response-transformer-on-message-collection-flow-config.xml"}
+        });
+    }
+
+    @Test
+    public void transformedDataIsNotLost() throws Exception
+    {
+        LocalMuleClient client = muleContext.getClient();
+        MuleMessage response = client.send("vm://testInput", TEST_MESSAGE, null);
+
+        assertEquals("foo", response.getPayload());
+        assertFalse(response instanceof MuleMessageCollection);
+    }
+}
