ff20104365e91f33a4f3cf82c5a71803ae8954df
Pablo Kraan <pablo.kraan@gmail.com>
2012-07-27 19:37:37 +0000

    Merged revisions 24680 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.2.x
    ................
      r24680 | pablo.kraan | 2012-07-27 11:50:16 -0300 (Fri, 27 Jul 2012) | 22 lines
      
      Merged revisions 24677,24679 via svnmerge from 
      https://svn.codehaus.org/mule/branches/mule-3.1.x
      
      ........
        r24677 | pablo.kraan | 2012-07-26 16:51:21 -0300 (Thu, 26 Jul 2012) | 9 lines
        
        MULE-6173: Transformer weighting algorithm fails to determine transformer with higher priority
        
        Fixing transformer weighting algorithm.
        _ Adding support for primitive data types
        _ Calculating the right weight for classes and interfaces taking in account the distance from
          the original class
        _ Fixing check of interfaces: is only done when the dest class is an interface. If no interface
          matches, then continue the weighting with the super class instead of returning  x +2 as before.
      ........
        r24679 | pablo.kraan | 2012-07-27 10:17:57 -0300 (Fri, 27 Jul 2012) | 4 lines
        
        MULE-6173: Transformer weighting algorithm fails to determine transformer with higher priority
        
        _ Fixing weighting of Object class
      ........
    ................
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.3.x@24685 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/transformers/TransformerWeightingFunctionalTestCase.java
@@ -0,0 +1,50 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.transformers;
+
+import org.mule.api.MuleMessage;
+import org.mule.api.client.LocalMuleClient;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+import org.custommonkey.xmlunit.XMLAssert;
+import org.junit.Test;
+
+public class TransformerWeightingFunctionalTestCase extends FunctionalTestCase
+{
+
+    private static final String XML_REQUEST = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                                              "<catalog>\n" +
+                                              "    <cd>\n" +
+                                              "        <title>Empire Burlesque</title>\n" +
+                                              "        <artist>Bob Dylan</artist>\n" +
+                                              "        <country>USA</country>\n" +
+                                              "        <company>Columbia</company>\n" +
+                                              "        <price>10.90</price>\n" +
+                                              "        <year>1985</year>\n" +
+                                              "    </cd>\n" +
+                                              "</catalog>";
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/test/transformers/transformer-weighting-functional-config.xml";
+    }
+
+    @Test
+    public void findTwoTransformers() throws Exception
+    {
+        LocalMuleClient client = muleContext.getClient();
+
+        MuleMessage response = client.send("vm://testInput", XML_REQUEST, null);
+
+        XMLAssert.assertXMLEqual(XML_REQUEST, response.getPayloadAsString());
+    }
+}
