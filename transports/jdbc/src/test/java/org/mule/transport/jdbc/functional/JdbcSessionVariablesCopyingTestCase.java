b7bd497bd9a2462f3c5983986ad0550eb569eb00
Santiago Vacas <svacas@gmail.com>
2012-07-30 21:58:11 +0000

    Merged revisions 24684,24693 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.2.x
    ........
      r24684 | svacas | 2012-07-27 14:01:50 -0300 (Fri, 27 Jul 2012) | 2 lines
      
      MULE-6366: JDBC Outbound endpoint, loss of session properties when outbound endpoint is request response
    ........
      r24693 | svacas | 2012-07-28 12:28:34 -0300 (Sat, 28 Jul 2012) | 4 lines
      
      MULE-6366: JDBC Outbound endpoint, loss of session properties when outbound endpoint is request response
      
      use NullSessionHandler to preserve session properties
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.3.x@24699 bf997673-6b11-0410-b953-e057580c5b09

---
+++ transports/jdbc/src/test/java/org/mule/transport/jdbc/functional/JdbcSessionVariablesCopyingTestCase.java
@@ -0,0 +1,61 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.jdbc.functional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.transport.NullPayload;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
+
+public class JdbcSessionVariablesCopyingTestCase extends AbstractJdbcFunctionalTestCase
+{
+
+    public JdbcSessionVariablesCopyingTestCase(ConfigVariant variant, String configResources)
+    {
+        super(variant, configResources);
+    }
+
+    @Parameters
+    public static Collection<Object[]> parameters()
+    {
+        return Arrays.asList(new Object[][]{
+            {ConfigVariant.FLOW, "jdbc-session-variables-copying-flow.xml"}
+        });
+    }
+
+    @Test
+    public void testMessagePropertiesCopying() throws Exception
+    {
+        MuleClient client = new MuleClient(muleContext);
+        
+        MuleMessage message = new DefaultMuleMessage(TEST_MESSAGE, muleContext);
+        // provide a valid type header so the JDBC query actually returns something
+        message.setOutboundProperty("type", 1);
+
+        MuleMessage result = client.send("vm://in", message);
+        assertNotNull(result);
+        assertNull(result.getExceptionPayload());
+        assertFalse(result.getPayload() instanceof NullPayload);
+        assertEquals("test", result.getSessionProperty("test"));
+    }
+
+}
