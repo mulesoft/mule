110afd0b1cc4304c7fd99f42a9591bf5ad439541
Pablo Kraan <pablo.kraan@gmail.com>
2012-08-01 21:51:41 +0000

    Merged revisions 24706 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.2.x
    ................
      r24706 | pablo.kraan | 2012-08-01 17:28:40 -0300 (Wed, 01 Aug 2012) | 12 lines
      
      Merged revisions 24705 via svnmerge from 
      https://svn.codehaus.org/mule/branches/mule-3.1.x
      
      ........
        r24705 | pablo.kraan | 2012-08-01 14:57:50 -0300 (Wed, 01 Aug 2012) | 6 lines
        
        MULE-6374: Session properties lost in an Exception Strategy
        
        _ SimpleFlowConstruct was not updating the session in case of exception.
        _ EventGroup was creating the result message using the first message in the list, when it should be the last one
      ........
    ................
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.3.x@24707 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/SessionPropertiesInExceptionStrategyTestCase.java
@@ -0,0 +1,51 @@
+/*
+ * $Id$
+ * -------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import org.mule.api.MuleMessage;
+import org.mule.api.client.MuleClient;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.transport.NullPayload;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class SessionPropertiesInExceptionStrategyTestCase extends FunctionalTestCase
+{
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/properties/session-properties-in-exception-strategy-config.xml";
+    }
+
+    @Test
+    public void sessionPropertyIsNotLost() throws Exception
+    {
+        MuleClient client = muleContext.getClient();
+
+        List list = new ArrayList<String>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+
+        MuleMessage result = client.send("vm://testInput", list, null);
+
+        assertNull(result.getExceptionPayload());
+        assertFalse(result.getPayload() instanceof NullPayload);
+        assertEquals(list.size(), result.getSessionProperty("ErrorCount"));
+    }
+}
