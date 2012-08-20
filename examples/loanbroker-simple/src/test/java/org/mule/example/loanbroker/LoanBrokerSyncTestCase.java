c1e35ab2ff2575f160632151722d3d4403ee105c
Pablo Kraan <pablo.kraan@gmail.com>
2012-08-20 13:07:48 +0000

    Merged revisions 24773 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.3.x
    ........
      r24773 | pablo.kraan | 2012-08-19 10:08:12 -0300 (Sun, 19 Aug 2012) | 12 lines
      
      MULE-6267: Add Loanbroker example to distribution once it is compatible with Studio
      
      Adding feedback from DanF:
      _Moving XML comments to description elements
      _Making the logic used to create the requests more easy to understand
      _Refactoring flows to be more visually friendly
      _Chaging logs from WARN to INFO
      _Update foreach's inner enricher to use $ to access the current result
      _Chaging from expression-component to set-payload inside catch
      _Some naming improvements
      _Updating tests
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@24776 bf997673-6b11-0410-b953-e057580c5b09

--- examples/loanbroker-simple/src/test/java/org/mule/example/loanbroker/LoanBrokerSyncTestCase.java
+++ examples/loanbroker-simple/src/test/java/org/mule/example/loanbroker/LoanBrokerSyncTestCase.java
@@ -28,7 +28,7 @@ import org.junit.Test;
 
 public class LoanBrokerSyncTestCase extends FunctionalTestCase
 {
-    
+
     @Override
     protected String getConfigResources()
     {
@@ -41,11 +41,22 @@ public class LoanBrokerSyncTestCase extends FunctionalTestCase
         MuleClient client = new MuleClient(muleContext);
         @SuppressWarnings("unchecked")
         Map<String, String> props = new SingletonMap("http.method", HttpConstants.METHOD_GET);
-        MuleMessage result = client.send("http://localhost:11081", null, props);
+        MuleMessage result = client.send("http://localhost:11081?name=Muley&amount=20000&term=48&ssn=1234", null, props);
         assertNotNull("Result is null", result);
         assertFalse("Result is null", result.getPayload() instanceof NullPayload);
         assertNull(result.getExceptionPayload());
         assertTrue("Unexpected response string", result.getPayloadAsString().matches("Bank #\\d, rate: \\d\\.(\\d)*$"));
+    }
+
+    @Test
+    public void testIncompleteLoanBrokerRequest() throws Exception
+    {
+        MuleClient client = new MuleClient(muleContext);
+
+        @SuppressWarnings("unchecked")
+        Map<String, String> props = new SingletonMap("http.method", HttpConstants.METHOD_GET);
+        MuleMessage result = client.send("http://localhost:11081?amount=1234", null, props);
+        assertEquals("Error: incomplete request", result.getPayloadAsString());
     }
 
     @Test
@@ -55,10 +66,9 @@ public class LoanBrokerSyncTestCase extends FunctionalTestCase
 
         @SuppressWarnings("unchecked")
         Map<String, String> props = new SingletonMap("http.method", HttpConstants.METHOD_GET);
-        MuleMessage result = client.send("http://localhost:11081?amount=abcd", null, props);
+        MuleMessage result = client.send("http://localhost:11081?name=Muley&term=48&ssn=1234&amount=abcd", null, props);
         assertEquals("Error processing loan request", result.getPayloadAsString());
     }
-
 }
 
 
