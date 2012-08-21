2f4cc32b0f63fdfb3ab93ddcad1d59769384201c
Pablo La Greca <pablolagreca@hotmail.com>
2012-08-21 18:54:59 +0000

    Merged revisions 24719-24720 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.2.x
    ........
      r24719 | pablo.lagreca | 2012-08-08 19:21:54 -0300 (Wed, 08 Aug 2012) | 1 line
      
      EE-2661 - improving until-successful to call exception strategy in case that the retries is exhausted and no DLQ mp defined, or DLQ mp execution fails
    ........
      r24720 | pablo.lagreca | 2012-08-09 16:59:40 -0300 (Thu, 09 Aug 2012) | 1 line
      
      MULE-6387 - improving until-successful to call exception strategy in case that the retries is exhausted and no DLQ mp defined, or DLQ mp execution fails
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.3.x@24802 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/test/integration/routing/outbound/UntilSuccessfulRetryExhaustedTestCase.java
@@ -0,0 +1,49 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.integration.routing.outbound;
+
+import org.junit.Test;
+import org.mule.api.context.notification.ExceptionNotificationListener;
+import org.mule.construct.Flow;
+import org.mule.context.notification.ExceptionNotification;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.util.concurrent.Latch;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.fail;
+
+public class UntilSuccessfulRetryExhaustedTestCase extends FunctionalTestCase
+{
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/test/integration/routing/outbound/until-successful-retry-exhausted.xml";
+    }
+    
+    @Test
+    public void onRetryExhaustedCallExceptionStrategy() throws Exception
+    {
+        final Latch exceptionStrategyCalledLatch = new Latch();
+        muleContext.registerListener(new ExceptionNotificationListener<ExceptionNotification>() {
+            @Override
+            public void onNotification(ExceptionNotification notification)
+            {
+                exceptionStrategyCalledLatch.release();   
+            }
+        });
+        Flow flow = (Flow) getFlowConstruct("retryExhausted");
+        flow.process(getTestEvent("message"));
+        if (!exceptionStrategyCalledLatch.await(10000, TimeUnit.MILLISECONDS))
+        {
+            fail("exception strategy was not executed");
+        }
+    }
+}
