6c590892ccc637bdb65ebb145aa24d1caae7c99a
Pablo Kraan <pablo.kraan@gmail.com>
2012-11-07 21:47:35 +0000

    Merged revisions 24990 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.2.x
    ................
      r24990 | pablo.kraan | 2012-11-07 18:34:35 -0300 (Wed, 07 Nov 2012) | 21 lines
      
      Merged revisions 24988-24989 via svnmerge from 
      https://svn.codehaus.org/mule/branches/mule-3.1.x
      
      ........
        r24988 | pablo.kraan | 2012-11-07 15:40:45 -0300 (Wed, 07 Nov 2012) | 8 lines
        
        MULE-5232: xslt-transformer uriResolver setting does not work
        
        Problem is that uriResolver attribute is a string and cannot be converted
        to a URIResolver. The fix consist in declaring the attribute to be a reference
        to a bean instead of a plain String value.
        
        NOTE: there must be no problem renaming this attribute as it has never worked.
      ........
        r24989 | pablo.kraan | 2012-11-07 18:19:37 -0300 (Wed, 07 Nov 2012) | 4 lines
        
        MULE-5232: xslt-transformer uriResolver setting does not work
        
        Symplifying test case
      ........
    ................
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.3.x@24991 bf997673-6b11-0410-b953-e057580c5b09

---
+++ modules/xml/src/test/java/org/mule/transformers/xml/XsltTransformerWithUriResolverTestCase.java
@@ -0,0 +1,41 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transformers.xml;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.mule.api.transformer.Transformer;
+import org.mule.module.xml.transformer.XsltTransformer;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+import javax.xml.transform.URIResolver;
+
+import org.junit.Test;
+
+public class XsltTransformerWithUriResolverTestCase extends FunctionalTestCase
+{
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "xslt-transformer-wth-uri-resolver-config.xml";
+    }
+
+    @Test
+    public void configuresUriResolver() throws Exception  {
+        Transformer transformer = muleContext.getRegistry().lookupTransformer("testTransformer");
+        assertTrue(transformer instanceof XsltTransformer);
+        XsltTransformer xsltTransformer = (XsltTransformer) transformer;
+
+        URIResolver uriResolver = muleContext.getRegistry().lookupObject("testResolver");
+        assertEquals(uriResolver, xsltTransformer.getUriResolver());
+    }
+}
