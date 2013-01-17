55c80f11ad56ec2a92d72f51c58037dee6bcfef1
Chris Mordue <cmordue@gmail.com>
2013-01-17 00:47:41 +0000

    Added new integration tests. These can be used for documentation.
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@25217 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tests/integration/src/test/java/org/mule/module/xml/functional/SimpleXmlTransformerFunctionalTestCase.java
@@ -0,0 +1,119 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.xml.functional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.mule.api.MuleException;
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+import org.custommonkey.xmlunit.XMLAssert;
+import org.junit.Test;
+
+public class SimpleXmlTransformerFunctionalTestCase extends FunctionalTestCase
+{
+    public static final String SERIALIZED = "<org.mule.module.xml.functional.SimpleXmlTransformerFunctionalTestCase_-Parent>\n" +
+            "  <child>\n" +
+            "    <name>theChild</name>\n" +
+            "  </child>\n" + 
+            "</org.mule.module.xml.functional.SimpleXmlTransformerFunctionalTestCase_-Parent>";
+
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/module/xml/simple-xml-transformer-functional-test-flow.xml";
+    }
+
+    @Test
+    public void testXmlOut() throws Exception
+    {
+        MuleClient client = new MuleClient(muleContext);
+        client.dispatch("xml-in", SERIALIZED, null);
+        Parent parent = (Parent) request(client, "xml-object-out", Parent.class);
+        assertNotNull(parent);
+        assertNotNull(parent.getChild());
+        assertEquals("theChild", parent.getChild().getName());
+    }
+
+    @Test
+    public void testObjectXmlOut() throws Exception
+    {
+        MuleClient client = new MuleClient(muleContext);
+        client.dispatch("object-in", new Parent(new Child("theChild")), null);
+        String xml = (String) request(client, "object-xml-out", String.class);
+        XMLAssert.assertXMLEqual(SERIALIZED, xml);
+    }
+
+
+    protected Object request(MuleClient client, String endpoint, Class<?> clazz) throws MuleException
+    {
+        MuleMessage message = client.request(endpoint, 3000);
+        assertNotNull(message);
+        assertNotNull(message.getPayload());
+        assertTrue(message.getPayload().getClass().getName(), clazz.isAssignableFrom(message.getPayload().getClass()));
+        return message.getPayload();
+    }
+
+    public static class Parent
+    {
+        private Child child;
+
+        public Parent()
+        {
+            this(null);
+        }
+
+        public Parent(Child child)
+        {
+            setChild(child);
+        }
+
+        public Child getChild()
+        {
+            return child;
+        }
+
+        public void setChild(Child child)
+        {
+            this.child = child;
+        }
+    }
+
+    public static class Child
+    {
+        private String name;
+        
+        public Child()
+        {
+            this(null);
+        }
+        
+        public Child(String name)
+        {
+            this.name = name;
+        }
+        
+        public String getName()
+        {
+            return name;
+        }
+        
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ tests/integration/src/test/java/org/mule/test/integration/interceptor/MyCustomInterceptor.java
@@ -0,0 +1,31 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.integration.interceptor;
+
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.api.interceptor.Interceptor;
+import org.mule.processor.AbstractInterceptingMessageProcessor;
+
+public class MyCustomInterceptor extends AbstractInterceptingMessageProcessor implements Interceptor
+{
+    
+    @Override
+    public MuleEvent process(MuleEvent event) throws MuleException
+    {
+        String payload = (String)event.getMessage().getPayload();
+        event.getMessage().setPayload(payload + "!");
+        return processNext(event);
+    }
+
+}
+
+
