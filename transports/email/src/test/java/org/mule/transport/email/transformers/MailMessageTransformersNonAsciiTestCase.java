30d885c5a2a4e20004cb6b9add7c8ce035375ba8
damian.bakarcic <damian.bakarcic@mulesource.com>
2013-02-22 12:12:30 +0000

    Merged revisions 25294 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.1.x
    ........
      r25294 | damian.bakarcic | 2013-02-21 16:17:15 -0300 (Thu, 21 Feb 2013) | 5 lines
      
      MULE-6690: StringToEmailMessage doesn't encode subject correctly
      
      - Added the transformer output encoding to the message subject when the MimeMessage is being constructed inside the StringToEmailMessage transformer. 
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.2.x@25298 bf997673-6b11-0410-b953-e057580c5b09

--- transports/email/src/test/java/org/mule/transport/email/transformers/MailMessageTransformersNonAsciiTestCase.java
+++ transports/email/src/test/java/org/mule/transport/email/transformers/MailMessageTransformersNonAsciiTestCase.java
@@ -10,12 +10,27 @@
 
 package org.mule.transport.email.transformers;
 
-import org.mule.config.i18n.LocaleMessageHandler;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleMessage;
+import org.mule.api.transformer.Transformer;
+import org.mule.config.i18n.LocaleMessageHandler;
+import org.mule.transport.email.MailProperties;
+
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+
+import javax.mail.internet.MimeMessage;
+
+import org.junit.Test;
 
 public class MailMessageTransformersNonAsciiTestCase extends MailMessageTransformersTestCase
 {
+
     @Override
     protected String getContentType() 
     {
@@ -27,5 +42,28 @@ public class MailMessageTransformersNonAsciiTestCase extends MailMessageTransfor
     {
         return LocaleMessageHandler.getString("test-data", Locale.JAPAN,
             "MailMessageTransformersNonAsciiTestCase.getResultData", new Object[] {});
+    }
+
+    @Test
+    public void testNonAsciiSubjectEncodingInRoundtripTransformation() throws Exception
+    {
+        Transformer roundTripTransformer = super.getRoundTripTransformer();
+
+        // Build message setting the non-ascii message subject as a property for the transformer.
+        Map<String, Object> outboundProperties = new HashMap<String, Object>();
+        String testSubject = (String) getResultData();
+        outboundProperties.put(MailProperties.SUBJECT_PROPERTY, testSubject);
+        MuleMessage message = new DefaultMuleMessage(testSubject, outboundProperties, muleContext);
+
+        // Hack to set the default charset used by the javax.mail API to perform the mail subject encoding as iso-8859-1.
+        System.setProperty("mail.mime.charset", "iso-8859-1");
+
+        // Transform.
+        Object result = roundTripTransformer.transform(message, "iso-2022-jp");
+
+        assertNotNull("The result of the roundtrip transform shouldn't be null", result);
+        assertTrue(result instanceof MimeMessage);
+        // Assert that the mail subject has been correctly encoded in iso-2022-jp.
+        assertEquals(((MimeMessage) result).getSubject(), testSubject);
     }
 }
