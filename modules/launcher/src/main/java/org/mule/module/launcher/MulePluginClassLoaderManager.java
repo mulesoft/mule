f75665d7c4755d22df147cfad8640d7748ff332b
Pablo Kraan <pablo.kraan@gmail.com>
2013-03-12 07:26:27 -0300

    Adding plugin classLoader manager

---
+++ modules/launcher/src/main/java/org/mule/module/launcher/MulePluginClassLoaderManager.java
@@ -0,0 +1,39 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.launcher;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MulePluginClassLoaderManager implements PluginClassLoaderManager
+{
+
+    private final List<ClassLoader> classLoaders = new LinkedList<ClassLoader>();
+
+    @Override
+    public void addPluginClassLoader(ClassLoader classLoader)
+    {
+        synchronized (classLoaders)
+        {
+            classLoaders.add(classLoader);
+        }
+    }
+
+    @Override
+    public List<ClassLoader> getPluginClassLoaders()
+    {
+        synchronized (classLoaders)
+        {
+            return Collections.unmodifiableList(classLoaders);
+        }
+    }
+}
---
+++ modules/launcher/src/test/java/org/mule/module/launcher/MulePluginClassLoaderManagerTestCase.java
@@ -0,0 +1,42 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.launcher;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.internal.matchers.IsCollectionContaining.hasItem;
+import org.mule.tck.junit4.AbstractMuleTestCase;
+import org.mule.tck.size.SmallTest;
+
+import org.junit.Test;
+
+@SmallTest
+public class MulePluginClassLoaderManagerTestCase extends AbstractMuleTestCase
+{
+
+    private MulePluginClassLoaderManager pluginClassLoaderManager = new MulePluginClassLoaderManager();
+
+    @Test
+    public void isEmptyOnStartup() throws Exception
+    {
+        assertThat(pluginClassLoaderManager.getPluginClassLoaders().size(), equalTo(0));
+    }
+
+    @Test
+    public void addsPluginClassLoaders() throws Exception
+    {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        pluginClassLoaderManager.addPluginClassLoader(classLoader);
+
+        assertThat(pluginClassLoaderManager.getPluginClassLoaders().size(), equalTo(1));
+        assertThat(pluginClassLoaderManager.getPluginClassLoaders(), hasItem(classLoader));
+    }
+}
