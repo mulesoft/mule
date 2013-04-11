0b140391ca17788e4596c52c9f2f73fab879287c
Chris Mordue <cmordue@gmail.com>
2013-04-11 11:47:29 -0700

    MULE-6791: Jetty inbound endpoint configured with useContinuations="true" sets http.method as outbound rather than inbound

--- transports/jetty/src/test/java/org/mule/transport/servlet/jetty/functional/JettyFunctionalTestCase.java
+++ transports/jetty/src/test/java/org/mule/transport/servlet/jetty/functional/JettyFunctionalTestCase.java
@@ -13,17 +13,18 @@ package org.mule.transport.servlet.jetty.functional;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import org.mule.api.MuleException;
+import org.mule.api.MuleEventContext;
 import org.mule.api.MuleMessage;
-import org.mule.api.transformer.DataType;
 import org.mule.module.client.MuleClient;
+import org.mule.tck.functional.EventCallback;
+import org.mule.tck.functional.FunctionalTestComponent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.transport.http.HttpConnector;
+import org.mule.transport.http.HttpConstants;
 import org.mule.util.IOUtils;
 
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -46,6 +47,23 @@ public class JettyFunctionalTestCase extends FunctionalTestCase
     @Test
     public void testNormalExecutionFlow() throws Exception
     {
+        FunctionalTestComponent testComponent = getFunctionalTestComponent("normalExecutionFlow");
+        assertNotNull(testComponent);
+        
+        EventCallback callback = new EventCallback()
+        {
+            public void eventReceived(MuleEventContext context, Object component) throws Exception
+            {
+                MuleMessage msg = context.getMessage();
+                assertEquals(HttpConstants.METHOD_POST, msg.getInboundProperty(HttpConnector.HTTP_METHOD_PROPERTY));
+                assertEquals("/normal", msg.getInboundProperty(HttpConnector.HTTP_REQUEST_PROPERTY));
+                assertEquals("/normal", msg.getInboundProperty(HttpConnector.HTTP_REQUEST_PATH_PROPERTY));
+                assertEquals("/normal", msg.getInboundProperty(HttpConnector.HTTP_CONTEXT_PATH_PROPERTY));
+            }
+        };
+
+        testComponent.setEventCallback(callback);
+        
         MuleClient client = new MuleClient(muleContext);
         MuleMessage response = client.send("http://localhost:" + dynamicPort.getNumber() + "/normal", TEST_MESSAGE, null);
         assertEquals("200", response.getInboundProperty("http.status"));
