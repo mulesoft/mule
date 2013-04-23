dbca25fcb619ee2f46317e6937740f3b0ad855e1
Chris Mordue <cmordue@gmail.com>
2013-04-23 12:03:50 -0700

    MULE-6794: CXF WS-Security - SecurityContext is not set after successful authentication _ MuleSecurityManagerCallbackHandler now saves the authentication info in the session context (cherry picked from commit 51275dc4367ce8a1e18bc9131dc97e89e5caf4cf) (cherry picked from commit 50676ee2def47dbb6ee149bbc6937a78c83cf323) (cherry picked from commit e610f9e92fd66c50f40113c14d8d8ef938dfe2b7) (cherry picked from commit 7a1dce8a89d8c49ca3a1db4e7a74304217d9b964)

--- modules/cxf/src/main/java/org/mule/module/cxf/support/MuleSecurityManagerValidator.java
+++ modules/cxf/src/main/java/org/mule/module/cxf/support/MuleSecurityManagerValidator.java
@@ -10,10 +10,13 @@
 
 package org.mule.module.cxf.support;
 
+import org.mule.RequestContext;
 import org.mule.api.security.*;
 import org.mule.security.DefaultMuleAuthentication;
 import org.mule.security.MuleCredentials;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.ws.security.WSSecurityException;
 import org.apache.ws.security.handler.RequestData;
 import org.apache.ws.security.validate.Credential;
@@ -24,6 +27,8 @@ import org.apache.ws.security.validate.Validator;
  */
 public class MuleSecurityManagerValidator implements Validator
 {
+    private static Log logger = LogFactory.getLog(MuleSecurityManagerValidator.class);
+    
     private org.mule.api.security.SecurityManager securityManager;
 
     public Credential validate(Credential credential, RequestData data) throws WSSecurityException
@@ -37,7 +42,19 @@ public class MuleSecurityManagerValidator implements Validator
 
         try
         {
-            securityManager.authenticate(auth);
+          Authentication authentication = securityManager.authenticate(auth);
+
+          SecurityContext secContext = null;
+          try
+          {
+              secContext = securityManager.createSecurityContext(authentication);
+              secContext.setAuthentication(authentication);
+          }
+          catch (UnknownAuthenticationTypeException e)
+          {
+              logger.warn("Could not create security context after having successfully authenticated.", e);
+          }
+          RequestContext.getEvent().getSession().setSecurityContext(secContext);
         }
         catch (org.mule.api.security.SecurityException e)
         {
