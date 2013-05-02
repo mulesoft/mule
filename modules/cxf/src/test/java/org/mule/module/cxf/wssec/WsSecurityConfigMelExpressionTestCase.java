88e962628a479ec232e8153fb2b01eec57979f81
Chris Mordue <cmordue@gmail.com>
2013-05-02 10:56:48 -0700

    MULE-6796: Allow MEL expressions in cxf:property of cxf:ws-config _ WsConfig now will wrap the properties in a map that will evaluate the values based upon the current request context. (cherry picked from commit f578cb0c1100f35064da6befa8707b64a56255ef) (cherry picked from commit 32639ec86459a79858550788b3415056c09e8a8b)

---
+++ modules/cxf/src/test/java/org/mule/module/cxf/wssec/WsSecurityConfigMelExpressionTestCase.java
@@ -0,0 +1,69 @@
+/*
+ * $Id $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.cxf.wssec;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+
+import java.util.Map;
+
+import javax.xml.ws.soap.SOAPFaultException;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+
+public class WsSecurityConfigMelExpressionTestCase extends FunctionalTestCase
+{
+    @Rule
+    public DynamicPort dynamicPort = new DynamicPort("port1");
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/module/cxf/wssec/ws-security-config-mel-expression-config.xml";
+    }
+
+    @Test
+    public void testSuccessfulAuthentication() throws Exception
+    {
+        ClientPasswordCallback.setPassword("secret");
+        MuleMessage request = new DefaultMuleMessage("PasswordText", (Map<String,Object>)null, muleContext);
+        MuleClient client = new MuleClient(muleContext);
+        MuleMessage received = client.send("vm://greetMe", request);
+
+        assertNotNull(received);
+        assertEquals("Hello PasswordText", received.getPayloadAsString());
+
+    }
+
+    @Test
+    public void testFailAuthentication() throws Exception
+    {
+        ClientPasswordCallback.setPassword("secret");
+        MuleMessage request = new DefaultMuleMessage("UnknownPasswordEncoding", (Map<String,Object>)null, muleContext);
+        MuleClient client = new MuleClient(muleContext);
+        MuleMessage received = client.send("vm://greetMe", request);
+
+        assertNotNull(received);
+        assertNotNull(received.getExceptionPayload());
+        assertTrue(received.getExceptionPayload().getException().getCause() instanceof SOAPFaultException);
+        assertTrue(((SOAPFaultException) received.getExceptionPayload().getException().getCause()).getMessage().contains("Security processing failed"));
+    }
+    
+}
