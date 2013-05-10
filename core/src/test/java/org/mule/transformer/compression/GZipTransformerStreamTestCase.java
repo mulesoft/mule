d51d730d484019a0ae688fd2f022aff55f9f45a4
dbakarcic <damian.bakarcic@mulesoft.com>
2013-05-10 14:52:33 -0300

    MULE-6833: GZip transformer failing
    GZipCompressTransformer serializes objects (e.g. Strings) into byte arrays before compressing. Its counterpart, the GZipUncompressTransformer receives a byte array, decompresses it and only deserializes back into an object if the return class is explicitely set on the transformer. Now the GZipUncompressTransformer tries to "guess" if the decompressed byte array was serialized by trying to deserialize it and checking if it failed. If the deserialization was successful it returns the resulting object, otherwise, it returns the decompressed byte array.
    
    MULE-6833: GZip transformer failing
    
    - Added missing license header.
    (cherry picked from commit 99b7507eaddc16847dc9dd8b65c6808b39a0c26b)
    
    Conflicts:
    	core/src/main/java/org/mule/transformer/compression/GZipUncompressTransformer.java
    	core/src/test/java/org/mule/transformer/compression/GZipTransformerStreamTestCase.java

--- core/src/test/java/org/mule/transformer/compression/GZipTransformerStreamTestCase.java
+++ core/src/test/java/org/mule/transformer/compression/GZipTransformerStreamTestCase.java
@@ -10,74 +10,57 @@
 
 package org.mule.transformer.compression;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import org.mule.api.transformer.TransformerException;
-import org.mule.util.IOUtils;
-import org.mule.util.SerializationUtils;
+import org.mule.api.lifecycle.InitialisationException;
+import org.mule.api.transformer.Transformer;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.Arrays;
 
-import org.junit.Test;
-
+/**
+ * Tests {@link GZipCompressTransformer} and its counterpart, the {@link GZipUncompressTransformer} with streams as inputs.
+ */
 public class GZipTransformerStreamTestCase extends GZipTransformerTestCase
 {
-    
-    @Test
-    public void testStreamingCompression() throws TransformerException
+
+    @Override
+    public Object getResultData()
     {
-        GZipCompressTransformer transformer = (GZipCompressTransformer) super.getTransformer();
-        
-        InputStream uncompressedInputStream = new ByteArrayInputStream(SerializationUtils.serialize(TEST_DATA));
+        try
+        {
+            return strat.compressInputStream((InputStream) getTestData());
 
-        // Compress input data.
-        InputStream compressedInputStream = (InputStream) transformer.transform(uncompressedInputStream);
-
-        byte[] compressedBytes = (byte[]) getResultData();
-        assertTrue(Arrays.equals(compressedBytes, IOUtils.toByteArray(compressedInputStream)));
-
-        IOUtils.closeQuietly(uncompressedInputStream);
-        IOUtils.closeQuietly(compressedInputStream);
+        }
+        catch (Exception e)
+        {
+            fail(e.getMessage());
+            return null;
+        }
     }
 
-    @Test
-    public void testStreamingDecompression() throws TransformerException
+    @Override
+    public Object getTestData()
+    {
+        return new ByteArrayInputStream(TEST_DATA.getBytes());
+    }
+
+    @Override
+    public Transformer getRoundTripTransformer()
     {
         GZipUncompressTransformer transformer = new GZipUncompressTransformer();
         transformer.setMuleContext(muleContext);
 
-        InputStream compressedInputStream = new ByteArrayInputStream((byte[]) getResultData());
+        try
+        {
+            transformer.initialise();
+        }
+        catch (InitialisationException e)
+        {
+            fail(e.getMessage());
+        }
 
-        // Decompress the input data.
-        InputStream decompressedInputStream = (InputStream) transformer.transform(compressedInputStream);
-
-        assertEquals(TEST_DATA, SerializationUtils.deserialize(IOUtils.toByteArray(decompressedInputStream)));
-
-        IOUtils.closeQuietly(compressedInputStream);
-        IOUtils.closeQuietly(decompressedInputStream);
-    }
-
-    @Test
-    public void testStreamingCompressionDecompression() throws TransformerException
-    {
-        GZipCompressTransformer compressTransformer = (GZipCompressTransformer) super.getTransformer();
-        GZipUncompressTransformer decompressTransformer = new GZipUncompressTransformer();
-        decompressTransformer.setMuleContext(muleContext);
-
-        InputStream input = new ByteArrayInputStream(SerializationUtils.serialize(TEST_DATA));
-
-        // Compress the input data.
-        InputStream compressedInputStream = (InputStream) compressTransformer.transform(input);
-        // Decompress the input data.
-        InputStream decompressedInputStream = (InputStream) decompressTransformer.transform(compressedInputStream);
-
-        assertEquals(SerializationUtils.deserialize(IOUtils.toByteArray(decompressedInputStream)), TEST_DATA);
-
-        IOUtils.closeQuietly(compressedInputStream);
-        IOUtils.closeQuietly(decompressedInputStream);
+        return transformer;
     }
 }
 
