e0c14eb6dafb26eb74f2d4d351c4fb0dcfd8fc34
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2013-06-07 14:40:58 -0300

    moving FetchAccessTokenMessageProcessor to devkit support

---
+++ modules/devkit-support/src/main/java/org/mule/security/oauth/processor/FetchAccessTokenMessageProcessor.java
@@ -0,0 +1,84 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.security.oauth.processor;
+
+import org.mule.api.MessagingException;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.config.i18n.MessageFactory;
+import org.mule.security.oauth.OAuthAdapter;
+import org.mule.security.oauth.OAuthManager;
+
+public class FetchAccessTokenMessageProcessor implements MessageProcessor
+{
+
+    public String redirectUri;
+    private String accessTokenUrl = null;
+    private OAuthManager<OAuthAdapter> oauthManager;
+
+    public FetchAccessTokenMessageProcessor(OAuthManager<OAuthAdapter> oauthManager)
+    {
+        this.oauthManager = oauthManager;
+    }
+
+    /**
+     * Sets redirectUri
+     * 
+     * @param value Value to set
+     */
+    public void setRedirectUri(String value)
+    {
+        this.redirectUri = value;
+    }
+
+    /**
+     * Sets accessTokenUrl
+     * 
+     * @param value Value to set
+     */
+    public void setAccessTokenUrl(String value)
+    {
+        this.accessTokenUrl = value;
+    }
+
+    /**
+     * Retrieves accessTokenUrl
+     */
+    public String getAccessTokenUrl()
+    {
+        return this.accessTokenUrl;
+    }
+
+    public MuleEvent process(MuleEvent event) throws MuleException
+    {
+        try
+        {
+            OAuthAdapter oauthAdapter = this.oauthManager.createAccessToken(((String) event.getMessage()
+                .getInvocationProperty("_oauthVerifier")));
+            if (oauthAdapter.getAccessTokenUrl() == null)
+            {
+                oauthAdapter.setAccessTokenUrl(this.accessTokenUrl);
+            }
+            oauthAdapter.fetchAccessToken(this.redirectUri);
+            oauthManager.getAccessTokenPoolFactory().passivateObject(oauthAdapter.getAccessTokenId(),
+                oauthAdapter);
+            event.getMessage().setInvocationProperty("OAuthAccessTokenId", oauthAdapter.getAccessTokenId());
+        }
+        catch (Exception e)
+        {
+            throw new MessagingException(MessageFactory.createStaticMessage("Unable to fetch access token"),
+                event, e);
+        }
+        return event;
+    }
+
+}
