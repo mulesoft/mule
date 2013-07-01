1953b71b6aca8d1154c725a804d54838d75e8a49
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2013-07-01 17:07:18 -0300

    improving docs

--- core/src/main/java/org/mule/api/streaming/Consumer.java
+++ core/src/main/java/org/mule/api/streaming/Consumer.java
@@ -17,8 +17,27 @@ import java.util.NoSuchElementException;
 public interface Consumer<T> extends Closeable
 {
 
+    /**
+     * Retrieves the next available item.
+     * 
+     * @return an object of type T if available
+     * @throws NoSuchElementException if no more items are available
+     */
     public T consume() throws NoSuchElementException;
-    
+
+    /**
+     * Returns <code>true</code> if no more items are available. When the resource
+     * has been fully consumed and this method returns <code>true</code>,
+     * implementors of this class are require to invoke the {@link
+     * org.mule.api.Closeable.close()} method before returning in order to release
+     * resources as quickly as possible. Users of this component are still required
+     * to invoke the same close method when they're finish with it. This is so to
+     * account for the case in which a consumer needs to be released before being
+     * fully consumed
+     * 
+     * @return <code>true</code> if no more items are available. <code>false</code>
+     *         otherwise
+     */
     public boolean isConsumed();
 
 }
