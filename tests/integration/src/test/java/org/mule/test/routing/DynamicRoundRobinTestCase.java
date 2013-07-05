a3eb97ee1de01ece148d9ea9c869b6ef54538c41
evangelinamartinezruizmoreno <evangelina.martinez@mulesource.com>
2013-07-05 17:54:08 -0300

    MULE-6918: Adjust code after pull request suggestions. Make it work in cluster and add more tests is still pending.

--- tests/integration/src/test/java/org/mule/test/routing/DynamicRoundRobinTestCase.java
+++ tests/integration/src/test/java/org/mule/test/routing/DynamicRoundRobinTestCase.java
@@ -9,18 +9,17 @@
  */
 package org.mule.test.routing;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.mule.api.MessagingException;
 import org.mule.api.MuleEvent;
 import org.mule.construct.Flow;
-import org.mule.tck.junit4.FunctionalTestCase;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class DynamicRoundRobinTestCase extends FunctionalTestCase
+public class DynamicRoundRobinTestCase extends DynamicRouterTestCase
 {
+    private static final String DYNAMIC_ROUND_ROBIN = "dynamicRoundRobin";
+    private static final String DYNAMIC_ROUND_ROBIN_CUSTOM_ID = "dynamicRoundRobinWithCustomId";
 
     @Override
     protected String getConfigResources()
@@ -28,70 +27,45 @@ public class DynamicRoundRobinTestCase extends FunctionalTestCase
         return "org/mule/test/integration/routing/dynamic-round-robin-config.xml";
     }
 
-    @Before
-    public void clearRoutes()
-    {
-        CustomRouteResolver.routes.clear();
-    }
-
-    @Test(expected = MessagingException.class)
-    public void noRoutes() throws Exception
-    {
-        Flow flow = getTestFlow("dynamicRoundRobin");
-        flow.process(getTestEvent("message"));
-    }
-
     @Test
     public void withRoutes() throws Exception
     {
-        CustomRouteResolver.routes.add(new CustomRouteResolver.AddLetterMessageProcessor("a"));
-        CustomRouteResolver.routes.add(new CustomRouteResolver.AddLetterMessageProcessor("b"));
-        CustomRouteResolver.routes.add(new CustomRouteResolver.AddLetterMessageProcessor("c"));
-        CustomRouteResolver.routes.add(new CustomRouteResolver.AddLetterMessageProcessor("d"));
-        Flow testFlow = getTestFlow("dynamicRoundRobin");
-        runFlowAndAssertResponse(testFlow,"a");
-        runFlowAndAssertResponse(testFlow,"b");
-        runFlowAndAssertResponse(testFlow,"c");
-        runFlowAndAssertResponse(testFlow,"d");
-        runFlowAndAssertResponse(testFlow,"a");
-        runFlowAndAssertResponse(testFlow,"b");
-        runFlowAndAssertResponse(testFlow,"c");
-        runFlowAndAssertResponse(testFlow,"d");
+        CustomRouteResolver.routes.add(new CustomRouteResolver.AddLetterMessageProcessor(LETTER_A));
+        CustomRouteResolver.routes.add(new CustomRouteResolver.AddLetterMessageProcessor(LETTER_B));
+        CustomRouteResolver.routes.add(new CustomRouteResolver.AddLetterMessageProcessor(LETTER_C));
+        CustomRouteResolver.routes.add(new CustomRouteResolver.AddLetterMessageProcessor(LETTER_D));
+        Flow testFlow = getTestFlow(DYNAMIC_ROUND_ROBIN);
+        runFlowAndAssertResponse(testFlow, LETTER_A);
+        runFlowAndAssertResponse(testFlow, LETTER_B);
+        runFlowAndAssertResponse(testFlow, LETTER_C);
+        runFlowAndAssertResponse(testFlow, LETTER_D);
+        runFlowAndAssertResponse(testFlow, LETTER_A);
+        runFlowAndAssertResponse(testFlow, LETTER_B);
+        runFlowAndAssertResponse(testFlow, LETTER_C);
+        runFlowAndAssertResponse(testFlow, LETTER_D);
     }
 
     @Test
     public void withRoutesAndCustomId() throws Exception
     {
-        IdentifiableCustomRouteResolver.routes.add(new IdentifiableCustomRouteResolver.AddLetterMessageProcessor("a"));
-        IdentifiableCustomRouteResolver.routes.add(new IdentifiableCustomRouteResolver.AddLetterMessageProcessor("b"));
-        IdentifiableCustomRouteResolver.routes.add(new IdentifiableCustomRouteResolver.AddLetterMessageProcessor("c"));
-        IdentifiableCustomRouteResolver.routes.add(new IdentifiableCustomRouteResolver.AddLetterMessageProcessor("d"));
-        Flow testFlow = getTestFlow("dynamicRoundRobinWithCustomId");
-        runFlowAndAssertResponse(testFlow, 1, "a");
-        runFlowAndAssertResponse(testFlow, 1, "b");
-        runFlowAndAssertResponse(testFlow, 2, "a");
-        runFlowAndAssertResponse(testFlow, 1, "c");
-        runFlowAndAssertResponse(testFlow, 2, "b");
-        runFlowAndAssertResponse(testFlow, 3, "a");
-        runFlowAndAssertResponse(testFlow, 1, "d");
-        runFlowAndAssertResponse(testFlow, 2, "c");
-    }
-
-    private Flow getTestFlow(String flow) throws Exception
-    {
-        return (Flow) getFlowConstruct(flow);
-    }
-
-    private MuleEvent runFlowAndAssertResponse(Flow flow, Object expectedMessage) throws Exception
-    {
-        MuleEvent event = flow.process(getTestEvent(""));
-        assertThat(event.getMessageAsString(), is(expectedMessage));
-        return event;
+        IdentifiableCustomRouteResolver.routes.add(new IdentifiableCustomRouteResolver.AddLetterMessageProcessor(LETTER_A));
+        IdentifiableCustomRouteResolver.routes.add(new IdentifiableCustomRouteResolver.AddLetterMessageProcessor(LETTER_B));
+        IdentifiableCustomRouteResolver.routes.add(new IdentifiableCustomRouteResolver.AddLetterMessageProcessor(LETTER_C));
+        IdentifiableCustomRouteResolver.routes.add(new IdentifiableCustomRouteResolver.AddLetterMessageProcessor(LETTER_D));
+        Flow testFlow = getTestFlow(DYNAMIC_ROUND_ROBIN_CUSTOM_ID);
+        runFlowAndAssertResponse(testFlow, 1, LETTER_A);
+        runFlowAndAssertResponse(testFlow, 1, LETTER_B);
+        runFlowAndAssertResponse(testFlow, 2, LETTER_A);
+        runFlowAndAssertResponse(testFlow, 1, LETTER_C);
+        runFlowAndAssertResponse(testFlow, 2, LETTER_B);
+        runFlowAndAssertResponse(testFlow, 3, LETTER_A);
+        runFlowAndAssertResponse(testFlow, 1, LETTER_D);
+        runFlowAndAssertResponse(testFlow, 2, LETTER_C);
     }
 
     private MuleEvent runFlowAndAssertResponse(Flow flow, Object flowVar, Object expectedMessage) throws Exception
     {
-        MuleEvent event = getTestEvent("");
+        MuleEvent event = getTestEvent(TEST_MESSAGE);
         event.setFlowVariable("id", flowVar);
         MuleEvent response = flow.process(event);
         assertThat(response.getMessageAsString(), is(expectedMessage));
@@ -99,4 +73,8 @@ public class DynamicRoundRobinTestCase extends FunctionalTestCase
     }
 
 
+    @Override
+    public String getFlowName() {
+        return DYNAMIC_ROUND_ROBIN;
+    }
 }
