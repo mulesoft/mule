3951365f67044e260f2a1d8d0681104a32e0c428
abdu <abdurrahmans@blinkbox.com>
2013-08-02 08:34:12 -0300

    Added test case for MULE-6973, jersey:resources component doesn't register multiple jersey:context-resolver MULE-6972 jersey:resources component doesn't register multiple exception mappers (cherry picked from commit 79a8552) (cherry picked from commit acd4651)

---
+++ modules/jersey/src/test/java/org/mule/module/jersey/MultipleContextResolverTestCase.java
@@ -0,0 +1,65 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.jersey;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.AbstractServiceAndFlowTestCase;
+import org.mule.transport.http.HttpConnector;
+import org.mule.transport.http.HttpConstants;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MultipleContextResolverTestCase extends AbstractServiceAndFlowTestCase
+{
+    public MultipleContextResolverTestCase(ConfigVariant variant, String configResources)
+    {
+        super(variant, configResources);
+    }
+
+    @Parameters
+    public static Collection<Object[]> parameters()
+    {
+        return Arrays.asList(new Object[][]{
+            {ConfigVariant.FLOW, "multiple-context-resolver-conf-flow.xml"},
+        });
+    }      
+    
+
+
+    @Test
+    @Ignore
+    public void testMultipleContextResolver() throws Exception
+    {
+        MuleClient client = new MuleClient(muleContext);
+
+        Map<String, String> props = new HashMap<String, String>();
+        props.put(HttpConnector.HTTP_METHOD_PROPERTY, HttpConstants.METHOD_GET);
+        MuleMessage result = client.send("http://localhost:63081/helloworld/sayHelloWorldWithJson", "", props);
+        assertEquals((Integer)200, result.getInboundProperty(HttpConnector.HTTP_STATUS_PROPERTY, 0));
+        assertEquals(getHelloWorldMessage(), result.getPayloadAsString());
+    }
+
+
+    private String getHelloWorldMessage () {
+        return "{\"message\":\"Hello World \",\"number\":0}";
+    }
+
+}
