1c2716272babb1ee08ac592df16c5b9ed46220ff
Alex Thieme <athieme@athieme.com>
2013-08-21 19:37:50 -0700

    Added config option to set the number of acceptor threads used by Jetty. Acceptor threads are those threads dedicated to accepting incoming connections. Increasing the number of acceptor threads can increase the throughput of a running system.
    Changes were applied to JettyHttpConnector, JettyHttpsConnector and related configuration.
    
    Functional tests are written to verify the configuration options are base down into the connector. Tests verify that acceptor threads are increased when accessing the application defined connector, and when accessing the connector used by the endpoint.

--- transports/jetty/src/test/java/org/mule/transport/servlet/jetty/functional/AbstractJettyAcceptorFunctionalTestCase.java
+++ transports/jetty/src/test/java/org/mule/transport/servlet/jetty/functional/AbstractJettyAcceptorFunctionalTestCase.java
@@ -1,115 +1,58 @@
 /*
+ * $Id$
+ * --------------------------------------------------------------------------------------
  * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
+
 package org.mule.transport.servlet.jetty.functional;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
-import org.mule.api.MuleMessage;
-import org.mule.api.client.MuleClient;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Rule;
 import org.mule.api.endpoint.InboundEndpoint;
 import org.mule.api.transport.Connector;
 import org.mule.construct.Flow;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
-import org.mule.tck.probe.PollingProber;
-import org.mule.tck.probe.Probe;
 import org.mule.transport.servlet.jetty.JettyHttpConnector;
 
-import java.util.Set;
-
-import org.junit.Rule;
-import org.mortbay.thread.QueuedThreadPool;
-
 /**
- * Functional tests that Jetty acceptor threads may be changed (from default of 1).
- * Implementors should define a Mule config containing a single Jetty connector
- * otherwise the number of acceptor threads will be cumulative across all connectors.
+ * Functional tests that Jetty acceptor threads may be changed (from default of 1)
  */
-public abstract class AbstractJettyAcceptorFunctionalTestCase extends FunctionalTestCase
-{
-
-    protected enum Protocol
-    {
-        http, https
-    }
+public abstract class AbstractJettyAcceptorFunctionalTestCase extends FunctionalTestCase {
 
     @Rule
-    public final DynamicPort port1 = new DynamicPort("port1");
+    public DynamicPort port1 = new DynamicPort("port1");
 
-    protected void assertAcceptors(final String connectorName, final String flowName, final int acceptors, final Protocol protocol) throws Exception
-    {
-        assertGlobalConnector(connectorName, acceptors);
-        assertFlowConnector(flowName, acceptors);
-        assertThreads(acceptors);
-        assertRequest(protocol);
+    @Rule
+    public DynamicPort port2 = new DynamicPort("port2");
+
+    @Override
+    protected String getConfigResources() {
+        return null;
     }
 
-    protected void assertFlowConnector(final String flowName, final int acceptors)
-    {
+    void assertAcceptors(final String connectorName, final String flowName, final int acceptors) {
+        // verify the acceptor config is passed down into the application defined connector
+        Connector conn = muleContext.getRegistry().lookupConnector(connectorName);
+        Assert.assertNotNull(conn);
+        Assert.assertThat(conn, CoreMatchers.instanceOf(JettyHttpConnector.class));
+        JettyHttpConnector cnn = (JettyHttpConnector) conn;
+        Assert.assertEquals(acceptors, cnn.getAcceptors());
+
         // verify the acceptor config is passed down into the connector used by the endpoint
         final Flow flow = (Flow) muleContext.getRegistry().lookupFlowConstruct(flowName);
-        assertNotNull(flow);
+        Assert.assertNotNull(flow);
         final InboundEndpoint endpoint = (InboundEndpoint) flow.getMessageSource();
-        assertNotNull(endpoint);
-        final Connector conn = endpoint.getConnector();
-        assertNotNull(conn);
-        assertThat(conn, instanceOf(JettyHttpConnector.class));
-        final JettyHttpConnector cnn = (JettyHttpConnector) conn;
-        assertEquals(acceptors, cnn.getAcceptors());
-    }
-
-    protected void assertGlobalConnector(final String connectorName, final int acceptors)
-    {
-        // verify the acceptor config is passed down into the application defined connector
-        final Connector conn = muleContext.getRegistry().lookupConnector(connectorName);
-        assertNotNull(conn);
-        assertThat(conn, instanceOf(JettyHttpConnector.class));
-        final JettyHttpConnector cnn = (JettyHttpConnector) conn;
-        assertEquals(acceptors, cnn.getAcceptors());
-    }
-
-    protected void assertThreads(final int acceptors)
-    {
-        PollingProber prober = new PollingProber(5000, 50);
-
-        prober.check(new Probe()
-        {
-            int actual;
-
-            public boolean isSatisfied()
-            {
-                final Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
-                actual = 0;
-                for (final Thread th : threadSet)
-                {
-                    if (th.getClass().equals(QueuedThreadPool.PoolThread.class)
-                        && th.getName().contains("Acceptor"))
-                    {
-                        actual = actual + 1;
-                    }
-                }
-
-                return actual == acceptors;
-            }
-
-            public String describeFailure()
-            {
-                return String.format("Expected '%s' acceptor threads but there are '%s'", acceptors, actual);
-            }
-        });
-    }
-
-    protected void assertRequest(final Protocol protocol) throws Exception
-    {
-        final MuleClient client = muleContext.getClient();
-        final MuleMessage message = client.send(String.format("%s://localhost:%s", protocol, port1.getNumber()), TEST_MESSAGE, null);
-        assertEquals("200", message.getInboundProperty("http.status"));
-        assertEquals(TEST_MESSAGE + " received", message.getPayloadAsString());
+        Assert.assertNotNull(endpoint);
+        conn = endpoint.getConnector();
+        Assert.assertNotNull(conn);
+        Assert.assertThat(conn, CoreMatchers.instanceOf(JettyHttpConnector.class));
+        cnn = (JettyHttpConnector) conn;
+        Assert.assertEquals(acceptors, cnn.getAcceptors());
     }
 }
