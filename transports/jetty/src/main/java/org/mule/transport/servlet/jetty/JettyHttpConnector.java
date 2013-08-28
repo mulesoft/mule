df702fec22d7b214ae2d0bb01a9bbb5413c5a779
Alex Thieme <athieme@athieme.com>
2013-08-28 13:48:07 -0700

    apply MuleSoft coding style to my getter/setter in JettyHttpConnector

--- transports/jetty/src/main/java/org/mule/transport/servlet/jetty/JettyHttpConnector.java
+++ transports/jetty/src/main/java/org/mule/transport/servlet/jetty/JettyHttpConnector.java
@@ -411,9 +411,11 @@ public class JettyHttpConnector extends AbstractConnector
 
     /**
      * Get the number of "acceptor" threads Jetty should use
-     * @return  the number of threads
+     *
+     * @return the number of threads
      */
-    public int getAcceptors() {
+    public int getAcceptors()
+    {
         return acceptors;
     }
 
@@ -422,7 +424,8 @@ public class JettyHttpConnector extends AbstractConnector
      *
      * @param acceptors the number of threads
      */
-    public void setAcceptors(final int acceptors) {
+    public void setAcceptors(final int acceptors)
+    {
         this.acceptors = acceptors;
     }
 
