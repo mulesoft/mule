f15cef0fb3830aa61c4e77e257b8d088ba794689
pablo.kraan <pablo.kraan@gmail.com>
2013-09-02 10:24:21 -0300

    MULE-6999: File Transport delays the processing of files when pollingFrequency attribute is uncomfortably narrow
    _ Extracting test utils from AbstractFileFunctionalTestCase to FileTestUtils
    (cherry picked from commit e1cedae)

--- transports/file/src/test/java/org/mule/transport/file/FileEncodingFunctionalTestCase.java
+++ transports/file/src/test/java/org/mule/transport/file/FileEncodingFunctionalTestCase.java
@@ -12,6 +12,9 @@ package org.mule.transport.file;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mule.transport.file.FileTestUtils.createDataFile;
+import static org.mule.transport.file.FileTestUtils.createFolder;
+
 import org.mule.api.MuleMessage;
 import org.mule.module.client.MuleClient;
 
