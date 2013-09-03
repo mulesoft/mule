db0ded3f3e32ee3e4e36b3f1dfd7aa6ae303284d
pablolagreca <pablo.lagreca@mulesoft.com>
2013-09-03 13:15:21 -0300

    Revert "Revert "EE-3372: Adding documentation and a new Producer to allow streaming through queues""
    This reverts commit 10b5a0163c11a2035b033abe421d0698ff9b6d4f.
    
    Conflicts:
    	core/src/main/java/org/mule/streaming/ListableObjectStoreProducer.java

---
+++ core/src/main/java/org/mule/streaming/QueueProducer.java
@@ -0,0 +1,100 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.streaming;
+
+import org.mule.api.MuleException;
+import org.mule.util.queue.Queue;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Implementation of {@link Producer} to stream the contents of a
+ * {@link org.mule.util.queue.Queue} A polling timeout value might be specified,
+ * otherwise the default value of 5000 milliseconds will be assumed
+ */
+public class QueueProducer implements Producer<Serializable>
+{
+
+    private static final Logger logger = LoggerFactory.getLogger(QueueProducer.class);
+    private static final long DEFAULT_TIMEOUT_VALUE = 5000;
+
+    private Queue queue;
+    private int size;
+    private long timeout;
+
+    /**
+     * Creates an instance with 5000 milliseconds as the default polling value
+     * 
+     * @param queue the queue to stream from
+     */
+    public QueueProducer(Queue queue)
+    {
+        this(queue, DEFAULT_TIMEOUT_VALUE);
+    }
+
+    public QueueProducer(Queue queue, long timeout)
+    {
+        if (queue == null)
+        {
+            throw new IllegalArgumentException("Cannot make a producer out of a null queue");
+        }
+        this.queue = queue;
+        this.size = queue.size();
+        this.timeout = timeout;
+    }
+
+    /**
+     * {@inheritDoc} This implementation will poll from the queue once and will
+     * return the obtained item in a single element list. If the producer is closed
+     * or if the queue times out while polling, then an empty list is returned. If
+     * the poll method throws {@link InterruptedException} then an empty list is
+     * returned as well
+     */
+    @Override
+    public List<Serializable> produce()
+    {
+        if (this.queue == null)
+        {
+            return Collections.emptyList();
+        }
+
+        Serializable item = null;
+        try
+        {
+            item = this.queue.poll(this.timeout);
+        }
+        catch (InterruptedException e)
+        {
+            logger.warn("Thread interrupted while polling in producer. Will return an empty list", e);
+        }
+
+        return item != null ? Arrays.asList(item) : Collections.<Serializable> emptyList();
+    }
+
+    @Override
+    public void close() throws MuleException
+    {
+        this.queue = null;
+    }
+
+    @Override
+    public int totalAvailable()
+    {
+        return this.size;
+    }
+
+}
