4079a642b3f81201b90ae119598e9bb4c856d9ad
pablolagreca <pablo.lagreca@mulesoft.com>
2013-09-03 13:17:53 -0300

    Revert "EE-3372: Refactoring so that logic is reusable"
    This reverts commit 469f6767de768f3913ece45e1c6fa1d9edbdb343.

--- core/src/main/java/org/mule/routing/CollectionSplitter.java
+++ core/src/main/java/org/mule/routing/CollectionSplitter.java
@@ -11,9 +11,17 @@
 package org.mule.routing;
 
 import org.mule.api.MuleEvent;
+import org.mule.api.MuleMessage;
+import org.mule.api.MuleMessageCollection;
+import org.mule.config.i18n.CoreMessages;
 import org.mule.routing.outbound.AbstractMessageSequenceSplitter;
-import org.mule.util.collection.EventToMessageSequenceSplittingStrategy;
-import org.mule.util.collection.SplittingStrategy;
+import org.mule.routing.outbound.ArrayMessageSequence;
+import org.mule.routing.outbound.CollectionMessageSequence;
+import org.mule.routing.outbound.IteratorMessageSequence;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 /**
  * Splits a message that has a Collection, Iterable, MessageSequence or Iterator
@@ -25,11 +33,40 @@ import org.mule.util.collection.SplittingStrategy;
  */
 public class CollectionSplitter extends AbstractMessageSequenceSplitter
 {
-    
-    private SplittingStrategy<MuleEvent, MessageSequence<?>> strategy = new EventToMessageSequenceSplittingStrategy();
-    
+    @SuppressWarnings("unchecked")
     protected MessageSequence<?> splitMessageIntoSequence(MuleEvent event)
     {
-        return this.strategy.split(event);
+        MuleMessage msg = event.getMessage();
+        if (msg instanceof MuleMessageCollection)
+        {
+            return new ArrayMessageSequence(((MuleMessageCollection) msg).getMessagesAsArray());
+        }
+        Object payload = msg.getPayload();
+        if (payload instanceof MessageSequence<?>)
+        {
+            return ((MessageSequence<?>) payload);
+        }
+        if (payload instanceof Iterator<?>)
+        {
+            return new IteratorMessageSequence<Object>(((Iterator<Object>) payload));
+        }
+        if (payload instanceof Collection)
+        {
+            return new CollectionMessageSequence(new LinkedList((Collection) payload));
+        }
+        if (payload instanceof Iterable<?>)
+        {
+            return new IteratorMessageSequence<Object>(((Iterable<Object>) payload).iterator());
+        }
+        if (payload instanceof Object[])
+        {
+            return new ArrayMessageSequence((Object[]) payload);
+        }
+        else
+        {
+            throw new IllegalArgumentException(CoreMessages.objectNotOfCorrectType(payload.getClass(),
+                new Class[]{Iterable.class, Iterator.class, MessageSequence.class, Collection.class})
+                .getMessage());
+        }
     }
 }
