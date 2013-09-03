6754cf345c0e13d7493a4154a2588a2c70e0a078
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2013-09-03 15:09:32 -0300

    MULE-7003: Adding Composite producer

---
+++ core/src/test/java/org/mule/streaming/CompositeProducerTestCase.java
@@ -0,0 +1,111 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.streaming;
+
+import org.mule.tck.size.SmallTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@SmallTest
+@RunWith(MockitoJUnitRunner.class)
+public class CompositeProducerTestCase
+{
+
+    private List<String> list1;
+    private List<String> list2;
+    private List<String> list3;
+
+    private List<String> aggregatedList;
+
+    @Mock
+    private Producer<String> producer1;
+
+    @Mock
+    private Producer<String> producer2;
+
+    private CompositeProducer<String> producer;
+
+    @SuppressWarnings("unchecked")
+    @Before
+    public void setUp() throws Exception
+    {
+        this.list1 = Arrays.asList("superman", "batman");
+        this.list2 = Arrays.asList("iron man", "hulk");
+        this.list3 = Arrays.asList("paturuzu", "eternauta");
+
+        this.aggregatedList = new ArrayList<String>();
+        this.aggregatedList.addAll(this.list1);
+        this.aggregatedList.addAll(this.list2);
+        this.aggregatedList.addAll(this.list3);
+
+        Mockito.when(this.producer1.produce())
+            .thenReturn(this.list1)
+            .thenReturn(this.list2)
+            .thenReturn(Collections.<String> emptyList());
+        Mockito.when(this.producer1.totalAvailable()).thenReturn(this.list1.size() + this.list2.size());
+
+        Mockito.when(this.producer2.produce())
+            .thenReturn(this.list3)
+            .thenReturn(Collections.<String> emptyList());
+        Mockito.when(this.producer2.totalAvailable()).thenReturn(this.list3.size());
+
+        this.producer = new CompositeProducer<String>(this.producer1, this.producer2);
+    }
+
+    @Test
+    public void consumeAndClose() throws Exception
+    {
+        List<String> output = new ArrayList<String>();
+        List<String> page = this.producer.produce();
+        output.addAll(page);
+        while (!CollectionUtils.isEmpty(page))
+        {
+            page = this.producer.produce();
+            output.addAll(page);
+        }
+
+        Assert.assertEquals(output.size(), this.aggregatedList.size());
+
+        for (int i = 0; i < this.aggregatedList.size(); i++)
+        {
+            Assert.assertEquals(output.get(i), this.aggregatedList.get(i));
+        }
+
+    }
+
+    @Test
+    public void close() throws Exception
+    {
+        this.producer.close();
+
+        Mockito.verify(this.producer1).close();
+        Mockito.verify(this.producer2).close();
+    }
+
+    @Test
+    public void totalAvailable()
+    {
+        Assert.assertEquals(this.aggregatedList.size(), this.producer.totalAvailable());
+    }
+}
