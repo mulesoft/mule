3bbb85cd844a9718a3ac194930bef925687af0eb
Santiago Vacas <svacas@gmail.com>
2013-09-05 19:22:43 -0300

    MULE-6970: Add json schema support to the json schema validation filter
    refactor using delegates

--- modules/json/src/main/java/org/mule/module/json/transformers/JsonSchemaValidationFilter.java
+++ modules/json/src/main/java/org/mule/module/json/transformers/JsonSchemaValidationFilter.java
@@ -9,200 +9,253 @@
  */
 package org.mule.module.json.transformers;
 
-import org.mule.DefaultMuleMessage;
+import static org.mule.module.xml.filters.SchemaValidationFilter.DEFAULT_SCHEMA_LANGUAGE;
+
 import org.mule.api.MuleContext;
 import org.mule.api.MuleMessage;
-import org.mule.api.context.MuleContextAware;
 import org.mule.api.lifecycle.InitialisationException;
-import org.mule.config.i18n.Message;
-import org.mule.config.i18n.MessageFactory;
-import org.mule.module.json.JsonData;
-import org.mule.module.xml.filters.SchemaValidationFilter;
-import org.mule.util.IOUtils;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import java.util.Map;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.io.Writer;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.validation.Schema;
+import javax.xml.validation.Validator;
 
-import org.eel.kitchen.jsonschema.main.JsonSchema;
-import org.eel.kitchen.jsonschema.main.JsonSchemaFactory;
-import org.eel.kitchen.jsonschema.report.ValidationReport;
-import org.eel.kitchen.jsonschema.util.JsonLoader;
+import org.w3c.dom.ls.LSResourceResolver;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
 
 /**
  * Validate a JSON string against either an XML schema or Json schema depending on the schema location attribute.
- * <p/>
- * Note:
- * Ideally, this would call the Validator using STAX.  Unfortunately,
- * 1. xalan.jar is needed to avoid bugs in the version of Xalan built into the JRE
- * 2. Xalan does not work with STAX
- * 3. Having Xalan in the classpath overrides the default (STAX-compliant) factories for transformations, validators,
- * etc with ones that aren't STAX-compliant
- * <p/>
- * The result is that, while the ideal would be to implement this class by validating a STAXSource, that won't be
- * possible until either we can assume a JRE with bith STAX and a working Xalan fork, or there';s a xalan.jar
- * that supports StAX.
  */
-public class JsonSchemaValidationFilter extends SchemaValidationFilter implements MuleContextAware
+public class JsonSchemaValidationFilter implements JsonSchemaFilter
 {
 
-    protected MuleContext muleContext;
-    protected JsonToXml jToX;
-    protected JsonSchema jsonSchema;
+    protected JsonSchemaFilter delegate;
+    private String schemaLocations;
+    private Schema schemaObject;
+    private LSResourceResolver resourceResolver;
+    private String schemaLanguage = DEFAULT_SCHEMA_LANGUAGE;
+    private MuleContext muleContext;
+    private ErrorHandler errorHandler;
+    private boolean returnResult;
+    private boolean useStaxSource;
+    private XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+    private Map<String, Object> validatorProperties;
+    private Map<String, Boolean> validatorFeatures;
 
     @Override
     public boolean accept(MuleMessage msg)
     {
-        if (isJsonSchema())
-        {
-            return acceptJsonSchema(msg);
-        }
-        return acceptXmlSchema(msg);
-
-    }
-
-    protected boolean acceptXmlSchema(MuleMessage msg)
-    {
-        String jsonString = null;
-
-        try
-        {
-            if (isReturnResult())
-            {
-                TransformerInputs transformerInputs = new TransformerInputs(null, msg.getPayload());
-                Writer jsonWriter = new StringWriter();
-                if (transformerInputs.getInputStream() != null)
-                {
-                    jsonWriter = new StringWriter();
-                    IOUtils.copy(transformerInputs.getInputStream(), jsonWriter, msg.getEncoding());
-                }
-                else
-                {
-                    IOUtils.copy(transformerInputs.getReader(), jsonWriter);
-                }
-                jsonString = jsonWriter.toString();
-                msg.setPayload(jsonString);
-            }
-            String xmlString = (String) jToX.transform(msg.getPayload(), msg.getEncoding());
-            MuleMessage xmlMessage = new DefaultMuleMessage(xmlString, msg, msg.getMuleContext());
-            boolean accepted = super.accept(xmlMessage);
-            if (jsonString != null)
-            {
-                msg.setPayload(jsonString);
-            }
-            return accepted;
-        }
-        catch (Exception ex)
-        {
-            return false;
-        }
-    }
-
-    protected boolean acceptJsonSchema(MuleMessage message)
-    {
-        JsonNode data = null;
-        Object input = message.getPayload();
-        Object output = input;
-
-        try
-        {
-            if (input instanceof String)
-            {
-                data = JsonLoader.fromString((String) input);
-            }
-            else if (input instanceof Reader)
-            {
-                data = JsonLoader.fromReader((Reader) input);
-                output = data.toString();
-            }
-            else if (input instanceof InputStream)
-            {
-                data = JsonLoader.fromReader(new InputStreamReader((InputStream) input));
-                output = data.toString();
-            }
-            else if (input instanceof byte[])
-            {
-                data = JsonLoader.fromReader(new InputStreamReader(new ByteArrayInputStream((byte[]) input)));
-            }
-            else if (input instanceof JsonNode)
-            {
-                data = (JsonNode) input;
-            }
-            else if (input instanceof JsonData)
-            {
-                JsonData jsonData = (JsonData) input;
-                data = JsonLoader.fromReader(new StringReader(jsonData.toString()));
-            }
-            else
-            {
-                logger.warn("Payload type " + input.getClass().getName() + " is not supported");
-                return false;
-            }
-            message.setPayload(output);
-            ValidationReport report = jsonSchema.validate(data);
-            logger.debug("JSON Schema validation report: " + report.getMessages());
-            return report.isSuccess();
-        }
-        catch (Exception e)
-        {
-            logger.info("Unable to validate JSON!", e);
-            return false;
-        }
+        return delegate.accept(msg);
     }
 
     @Override
     public void setMuleContext(MuleContext muleContext)
     {
+        if (delegate != null)
+        {
+            delegate.setMuleContext(muleContext);
+        }
         this.muleContext = muleContext;
     }
 
     @Override
     public void initialise() throws InitialisationException
     {
-        if (isJsonSchema())
+        if (isJsonSchema(schemaLocations))
         {
-            doInitialiseJsonSchema();
+            delegate = new JsonSchemaJsonValidationFilter();
         }
         else
         {
-            super.initialise();
-            jToX = new JsonToXml();
-            jToX.setMuleContext(muleContext);
+            delegate = new JsonSchemaXsdValidationFilter();
         }
+
+        delegate.setSchemaLocations(schemaLocations);
+        delegate.setSchemaObject(schemaObject);
+        delegate.setResourceResolver(resourceResolver);
+        delegate.setSchemaLanguage(schemaLanguage);
+        delegate.setMuleContext(muleContext);
+        delegate.setErrorHandler(errorHandler);
+        delegate.setReturnResult(returnResult);
+        delegate.setUseStaxSource(useStaxSource);
+        delegate.setXMLInputFactory(xmlInputFactory);
+        delegate.setValidatorProperties(validatorProperties);
+        delegate.setValidatorFeatures(validatorFeatures);
+
+        delegate.initialise();
     }
 
-    protected void doInitialiseJsonSchema() throws InitialisationException
+    protected boolean isJsonSchema(String schema)
     {
-        try
-        {
-            InputStream inputStream = IOUtils.getResourceAsStream(getSchemaLocations(), getClass());
-            InputStreamReader reader = new InputStreamReader(inputStream);
-            JsonNode jsonNode = JsonLoader.fromReader(reader);
-            JsonSchemaFactory schemaFactory = JsonSchemaFactory.defaultFactory();
-            jsonSchema = schemaFactory.fromSchema(jsonNode);
-        }
-        catch (Exception e)
-        {
-            Message msg = MessageFactory.createStaticMessage("Unable to load or parse JSON Schema file at: " + getSchemaLocations());
-            throw new InitialisationException(msg, e, this);
-        }
+        return schema.endsWith(".json");
     }
 
     @Override
     public void setSchemaLocations(String schemaLocations)
     {
-        super.setSchemaLocations(schemaLocations);
+        this.schemaLocations = schemaLocations;
+        if (delegate != null)
+        {
+            delegate.setSchemaLocations(schemaLocations);
+        }
     }
 
-    protected boolean isJsonSchema()
+    @Override
+    public String getSchemaLocations()
     {
-        return getSchemaLocations().endsWith(".json");
+        return delegate.getSchemaLocations();
+    }
+
+    @Override
+    public Validator createValidator() throws SAXException
+    {
+        return delegate.createValidator();
+    }
+
+    @Override
+    public String getSchemaLanguage()
+    {
+        return delegate.getSchemaLanguage();
+    }
+
+    @Override
+    public Schema getSchemaObject()
+    {
+        return delegate.getSchemaObject();
+    }
+
+    @Override
+    public void setSchemaObject(Schema schemaObject)
+    {
+        this.schemaObject = schemaObject;
+        if (delegate != null)
+        {
+            delegate.setSchemaObject(schemaObject);
+        }
+    }
+
+    @Override
+    public ErrorHandler getErrorHandler()
+    {
+        return delegate.getErrorHandler();
+    }
+
+    @Override
+    public void setErrorHandler(ErrorHandler errorHandler)
+    {
+        this.errorHandler = errorHandler;
+        if (delegate != null)
+        {
+            delegate.setErrorHandler(errorHandler);
+        }
+    }
+
+    @Override
+    public LSResourceResolver getResourceResolver()
+    {
+        return delegate.getResourceResolver();
+    }
+
+    @Override
+    public void setResourceResolver(LSResourceResolver resourceResolver)
+    {
+        this.resourceResolver = resourceResolver;
+        if (delegate != null)
+        {
+            delegate.setResourceResolver(resourceResolver);
+        }
+    }
+
+    @Override
+    public Map<String, Boolean> getValidatorFeatures()
+    {
+        return delegate.getValidatorFeatures();
+    }
+
+    @Override
+    public void setValidatorFeatures(Map<String, Boolean> validatorFeatures)
+    {
+        this.validatorFeatures = validatorFeatures;
+        if (delegate != null)
+        {
+            delegate.setValidatorFeatures(validatorFeatures);
+        }
+    }
+
+    @Override
+    public Map<String, Object> getValidatorProperties()
+    {
+        return delegate.getValidatorProperties();
+    }
+
+    @Override
+    public void setValidatorProperties(Map<String, Object> validatorProperties)
+    {
+        this.validatorProperties = validatorProperties;
+        if (delegate != null)
+        {
+            delegate.setValidatorProperties(validatorProperties);
+        }
+    }
+
+    @Override
+    public XMLInputFactory getXMLInputFactory()
+    {
+        return delegate.getXMLInputFactory();
+    }
+
+    @Override
+    public void setXMLInputFactory(XMLInputFactory xmlInputFactory)
+    {
+        this.xmlInputFactory = xmlInputFactory;
+        if (delegate != null)
+        {
+            delegate.setXMLInputFactory(xmlInputFactory);
+        }
+    }
+
+    @Override
+    public boolean isUseStaxSource()
+    {
+        return delegate.isUseStaxSource();
+    }
+
+    @Override
+    public void setUseStaxSource(boolean useStaxSource)
+    {
+        this.useStaxSource = useStaxSource;
+        if (delegate != null)
+        {
+            delegate.setUseStaxSource(useStaxSource);
+        }
+    }
+
+    @Override
+    public boolean isReturnResult()
+    {
+        return delegate.isReturnResult();
+    }
+
+    @Override
+    public void setReturnResult(boolean returnResult)
+    {
+        this.returnResult = returnResult;
+        if (delegate != null)
+        {
+            delegate.setReturnResult(returnResult);
+        }
+    }
+
+    @Override
+    public void setSchemaLanguage(String schemaLanguage)
+    {
+        this.schemaLanguage = schemaLanguage;
+        if (delegate != null)
+        {
+            delegate.setSchemaLanguage(schemaLanguage);
+        }
     }
 }
 
