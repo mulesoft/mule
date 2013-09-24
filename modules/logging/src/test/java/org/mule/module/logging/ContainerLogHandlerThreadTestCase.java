659b99c504ee85d87f4ec9ba83160d28e05072d7
pablo.kraan <pablo.kraan@gmail.com>
2013-09-24 19:06:31 -0300

    MULE-6800: Thread leak on Mule redeployments for embedded
    _Disabling those threads do not have to run on embedded mules.
    _Removing old test classes that are duplicated (there are a duplciate class with suffix TestCase instead of Test)
    _Adding dependency to mule-core for tests ONLY in order to reuse the testing framework

---
+++ modules/logging/src/test/java/org/mule/module/logging/ContainerLogHandlerThreadTestCase.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.logging;
+
+import static org.junit.Assert.fail;
+
+import org.mule.tck.probe.thread.ThreadExists;
+
+import org.junit.Test;
+
+public class ContainerLogHandlerThreadTestCase extends AbstractLogHandlerThreadTestCase
+{
+
+    public ContainerLogHandlerThreadTestCase(LoggerFactoryFactory loggerFactory, String logHandlerThreadName)
+    {
+        super(loggerFactory, logHandlerThreadName);
+    }
+
+    @Test
+    public void doesNotStarsLogHandlerThreadOnContainerMode() throws Exception
+    {
+        loggerFactory.create();
+
+        try
+        {
+            prober.check(new ThreadExists(logHandlerThreadName));
+
+            fail("Log handler thread is not supposed be started when Mule is running on container mode");
+        }
+        catch (AssertionError e)
+        {
+            // Expected
+        }
+    }
+}
