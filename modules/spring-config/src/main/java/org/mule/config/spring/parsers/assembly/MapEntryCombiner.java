26cc11f374ad13c4c05f76cb63490271ad036d7c
andres.gregoire <andres.gregoire@mulesoft.com>
2013-09-24 16:24:19 -0300

    MULE-7034: MuleEvent is not serializable when using a JDBC inbound endpoint with a nested query (cherry picked from commit e428bef)

--- modules/spring-config/src/main/java/org/mule/config/spring/parsers/assembly/MapEntryCombiner.java
+++ modules/spring-config/src/main/java/org/mule/config/spring/parsers/assembly/MapEntryCombiner.java
@@ -10,6 +10,7 @@
 
 package org.mule.config.spring.parsers.assembly;
 
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +26,7 @@ import java.util.Set;
  * {@link org.mule.config.spring.parsers.assembly.configuration.PropertyConfiguration#isCollection(String)}
  * so that several entries can be combined.
  */
-public class MapEntryCombiner implements Map
+public class MapEntryCombiner implements Map, Serializable
 {
 
     public static final String KEY = "key";
