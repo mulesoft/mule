8cdaf8a209b1d78627e806233f0a60e4cf1f9d8a
pablo.kraan <pablo.kraan@gmail.com>
2013-09-25 16:14:52 -0300

    MULE-6800: Thread leak on Mule redeployments for embedded
    _Disabling those threads do not have to run on embedded mules.
    _Removing old test classes that are duplicated (there are a duplciate class with suffix TestCase instead of Test)
    _Adding dependency to mule-core for tests ONLY in order to reuse the testing framework
    (cherry picked from commit 659b99c)
    (cherry picked from commit 0f9444c)
    (cherry picked from commit 27ffb75)

---
+++ modules/logging/src/test/java/org/mule/module/logging/StandaloneLogHandlerThreadTestCase.java
@@ -0,0 +1,52 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.logging;
+
+import org.mule.tck.probe.thread.ThreadExists;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class StandaloneLogHandlerThreadTestCase extends AbstractLogHandlerThreadTestCase
+{
+
+    private static String originalSystemProperty;
+
+    public StandaloneLogHandlerThreadTestCase(LoggerFactoryFactory loggerFactory, String logHandlerThreadName)
+    {
+        super(loggerFactory, logHandlerThreadName);
+    }
+
+    @BeforeClass
+    public static void setUpSystemProperty()
+    {
+        originalSystemProperty = System.setProperty(MuleUtils.MULE_HOME, "test");
+    }
+
+    @AfterClass
+    public static void restoreSystemProperty()
+    {
+        if (originalSystemProperty != null)
+        {
+            System.setProperty(MuleUtils.MULE_HOME, originalSystemProperty);
+        }
+
+    }
+
+    @Test
+    public void startsLogHandlerThreadOnStandaloneMode() throws Exception
+    {
+        loggerFactory.create();
+
+        prober.check(new ThreadExists(logHandlerThreadName));
+    }
+}
