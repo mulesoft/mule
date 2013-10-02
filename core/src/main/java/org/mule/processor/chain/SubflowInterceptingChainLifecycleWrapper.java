60fbf0ca34d5eca718b9122d7a6f76c08b8c0989
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2013-10-02 14:47:35 -0300

    MULE-7020: Updating licenses

--- core/src/main/java/org/mule/processor/chain/SubflowInterceptingChainLifecycleWrapper.java
+++ core/src/main/java/org/mule/processor/chain/SubflowInterceptingChainLifecycleWrapper.java
@@ -1,8 +1,5 @@
 /*
- * $Id: SubflowInterceptingChainLifecycleWrapper.java 25035 2012-11-25 16:33:02Z svacas $
- * --------------------------------------------------------------------------------------
  * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
- *
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
--- modules/jboss-transactions/src/test/java/org/mule/module/jboss/transactions/JBossArjunaConfigurationTestCase.java
+++ modules/jboss-transactions/src/test/java/org/mule/module/jboss/transactions/JBossArjunaConfigurationTestCase.java
@@ -1,13 +1,9 @@
 /*
- * $Id:JBossArjunaConfigurationTestCase.java 8215 2012-10-25 15:56:51Z chrismordue $
- * --------------------------------------------------------------------------------------
  * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
- *
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-
 package org.mule.module.jboss.transactions;
 
 import org.mule.api.config.ConfigurationBuilder;
