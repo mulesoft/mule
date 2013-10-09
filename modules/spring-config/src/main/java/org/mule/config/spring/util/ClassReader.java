7f5e8c23486cd69d0d8e89fd80c446e5241dca75
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2013-10-09 18:24:47 -0300

    EE-3430: Applying licenses

--- modules/spring-config/src/main/java/org/mule/config/spring/util/ClassReader.java
+++ modules/spring-config/src/main/java/org/mule/config/spring/util/ClassReader.java
@@ -1,22 +1,9 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+/*
+ * (c) 2003-2014 MuleSoft, Inc. This software is protected under international copyright
+ * law. All use of this software is subject to MuleSoft's Master Subscription Agreement
+ * (or other master license agreement) separately entered into in writing between you and
+ * MuleSoft. If such an agreement is not in place, you may not use the software.
  */
-
 package org.mule.config.spring.util;
 
 import java.io.ByteArrayInputStream;
