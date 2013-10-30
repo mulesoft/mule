8a193b213cb01408c5a0c4dc13e9d55300e46f5b
pablo.kraan <pablo.kraan@gmail.com>
2013-10-30 08:51:51 -0300

    MULE-7050: MuleApplicationClassLoader loadClass() method not synchronized
    _Adding synchronizing FineGrainedControlClassLoader#loadClass(String,boolean)
    _Removing  MuleApplicationClassLoader#loadClass(String, boolean) override
    _Adding test to verify the fix
    _Renaming FineGrainedControlClassLoaderTest to FineGrainedControlClassLoaderTestCase
    (cherry picked from commit a2f0093)
    (cherry picked from commit a72a34d)

--- modules/launcher/src/test/java/org/mule/module/launcher/FineGrainedControlClassLoaderTestCase.java
+++ modules/launcher/src/test/java/org/mule/module/launcher/FineGrainedControlClassLoaderTestCase.java
@@ -6,6 +6,8 @@
  */
 package org.mule.module.launcher;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.mule.tck.junit4.AbstractMuleTestCase;
 import org.mule.tck.size.SmallTest;
 
@@ -15,11 +17,8 @@ import java.util.Set;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 @SmallTest
-public class FineGrainedControlClassLoaderTest extends AbstractMuleTestCase
+public class FineGrainedControlClassLoaderTestCase extends AbstractMuleTestCase
 {
     @Test
     public void isBlockedFQClassName() throws Exception
