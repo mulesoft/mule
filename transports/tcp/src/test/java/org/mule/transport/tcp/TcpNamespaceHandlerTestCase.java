fb8e57900218328d908a014ffe2a9297f344d973
pablo.kraan <pablo.kraan@gmail.com>
2013-11-21 19:12:50 -0300

    MULE-7144: Differentiate TCP connection timeout from responseTimeout
    _TcpSocketFactory: passing connectionTimeout in constructor. When no connectionTimeout is configured defaults to key.getEndpoint().getResponseTimeout() as before.
    _TcpConnector: adding connectionTimeout property
    _TcpConnector: moved TcpSocketFactory instantiation to doInitialise in order to apply the correct connection timeout
    (cherry picked from commit 4d78a87)

--- transports/tcp/src/test/java/org/mule/transport/tcp/TcpNamespaceHandlerTestCase.java
+++ transports/tcp/src/test/java/org/mule/transport/tcp/TcpNamespaceHandlerTestCase.java
@@ -44,6 +44,7 @@ public class TcpNamespaceHandlerTestCase extends FunctionalTestCase
         // this is what we want - i was worried that the client was used as default if the server
         // wasn't set, but that's not the case
         assertEquals(-1, c.getServerSoTimeout());
+        assertEquals(-1, c.getConnectionTimeout());
         assertEquals(3000, c.getClientSoTimeout());
         assertEquals(3000, c.getSocketMaxWait());
         assertTrue(c.isKeepAlive());
@@ -61,6 +62,7 @@ public class TcpNamespaceHandlerTestCase extends FunctionalTestCase
         assertNotNull(c);
         assertEquals(4000, c.getServerSoTimeout());
         assertEquals(3000, c.getClientSoTimeout());
+        assertEquals(2000, c.getConnectionTimeout());
         assertEquals(-1, c.getSocketMaxWait());
         assertTrue(c.isConnected());
         assertTrue(c.isStarted());
