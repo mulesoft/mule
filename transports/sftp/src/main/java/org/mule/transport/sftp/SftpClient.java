5ba7699b033e1dd79dcb8f45c1eb833efd8a5dfb
Sebastian Beltramini <sebastian.beltramini@mulesoft.com>
2013-11-29 11:54:20 -0300

    MULE-7155: Add appending support to the SFTP transport

--- transports/sftp/src/main/java/org/mule/transport/sftp/SftpClient.java
+++ transports/sftp/src/main/java/org/mule/transport/sftp/SftpClient.java
@@ -1,8 +1,5 @@
 /*
- * $Id$
- * --------------------------------------------------------------------------------------
  * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
- *
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
@@ -389,7 +386,7 @@ public class SftpClient
     {
         try
         {
-            
+        
             // Notify sftp put file action
             if (notifier != null)
             {
