71b5c7183f691c80934c4dbcc3f83ab3d56445f3
pablo.kraan <pablo.kraan@gmail.com>
2013-12-06 16:00:59 -0300

    MULE-7169: Add JMS session pooling support
    _ Adding JIRA issue number in TODO

--- transports/jms/src/main/java/org/mule/transport/jms/config/CachingConnectionFactoryFactoryBean.java
+++ transports/jms/src/main/java/org/mule/transport/jms/config/CachingConnectionFactoryFactoryBean.java
@@ -37,7 +37,7 @@ public class CachingConnectionFactoryFactoryBean extends AbstractFactoryBean<Cus
         CustomCachingConnectionFactory cachingConnectionFactory = new CustomCachingConnectionFactory(connectionFactory, username, password);
         cachingConnectionFactory.setCacheProducers(cacheProducers);
         cachingConnectionFactory.setSessionCacheSize(sessionCacheSize);
-        //TODO(pablo.kraan): caching consumer is disabled now as it breaks some tests
+        //TODO(pablo.kraan): Add caching of JMS consumers (MULE-7171)
         cachingConnectionFactory.setCacheConsumers(false);
 
         return cachingConnectionFactory;
