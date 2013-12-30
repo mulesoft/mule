8ac696ca9b1e498374a05ff7ca5f7d92773f88aa
Sebastian Beltramini <sebastian.beltramini@mulesoft.com>
2013-12-30 16:59:13 -0300

    MULE-7194 - Improper handling of UnknownHostException in Outbound TCP (cherry picked from commit 6e0197c)

---
+++ transports/tcp/src/test/java/org/mule/transport/tcp/integration/HostResolutionTestCase.java
@@ -0,0 +1,56 @@
+package org.mule.transport.tcp.integration;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.mule.api.MuleException;
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.transport.NullPayload;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class HostResolutionTestCase extends FunctionalTestCase 
+{
+
+    @Rule
+    public DynamicPort dynamicPort = new DynamicPort("port1");    
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "host-resolution-config.xml";
+    }
+
+    @Test
+    public void testDefaultConfiguration() throws MuleException
+    {
+        final MuleClient client = new MuleClient(muleContext);
+        final MuleMessage message = client.send("vm://defaultConfiguration", "something", null);
+        assertNotNull(message.getExceptionPayload());
+        assertEquals(NullPayload.getInstance(), message.getPayload());
+    }
+
+    @Test
+    public void testFailOnUnresolvedHost_false() throws MuleException
+    {
+        final MuleClient client = new MuleClient(muleContext);
+        final MuleMessage message = client.send("vm://failOnUnresolvedHostFalse", "something", null);
+        assertTrue(message.getExceptionPayload() == null);
+        assertArrayEquals("something else".getBytes(), (byte[])message.getPayload());
+    }
+
+    @Test
+    public void testFailOnUnresolvedHost_true() throws MuleException
+    {
+        final MuleClient client = new MuleClient(muleContext);
+        final MuleMessage message = client.send("vm://failOnUnresolvedHostTrue", "something", null);
+        assertNotNull(message.getExceptionPayload());
+        assertEquals(NullPayload.getInstance(), message.getPayload());
+    }
+
+}
