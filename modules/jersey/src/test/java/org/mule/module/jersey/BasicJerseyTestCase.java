de4db34fae0add79cf52d920408ca8f366d8b8c3
afelisatti <afelisatti@mulesoft.com>
2014-01-31 16:25:39 -0300

    MULE-7269: Review flakys... Changing port in Jersey basic tests to a dynamic one.

--- modules/jersey/src/test/java/org/mule/module/jersey/BasicJerseyTestCase.java
+++ modules/jersey/src/test/java/org/mule/module/jersey/BasicJerseyTestCase.java
@@ -9,9 +9,11 @@ package org.mule.module.jersey;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Rule;
 import org.mule.api.MuleMessage;
 import org.mule.api.client.MuleClient;
 import org.mule.tck.AbstractServiceAndFlowTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
 import org.mule.transport.http.HttpConnector;
 import org.mule.transport.http.HttpConstants;
 
@@ -30,6 +32,14 @@ public class BasicJerseyTestCase extends AbstractServiceAndFlowTestCase
         super(variant, configResources);
     }
 
+    @Rule
+    public DynamicPort port = new DynamicPort("port");
+
+    @Override
+    protected boolean isFailOnTimeout(){
+        return false;
+    }
+
     @Parameters
     public static Collection<Object[]> parameters()
     {
@@ -43,21 +53,21 @@ public class BasicJerseyTestCase extends AbstractServiceAndFlowTestCase
     {
         MuleClient client = muleContext.getClient();
 
-        MuleMessage result = client.send("http://localhost:63081/helloworld", "", null);
+        MuleMessage result = client.send("http://localhost:" + port.getNumber() + "/helloworld", "", null);
         assertEquals((Integer)200, result.getInboundProperty(HttpConnector.HTTP_STATUS_PROPERTY, 0));
         assertEquals("Hello World", result.getPayloadAsString());
 
         // try invalid url
-        result = client.send("http://localhost:63081/hello", "", null);
+        result = client.send("http://localhost:" + port.getNumber() + "/hello", "", null);
         assertEquals((Integer)404, result.getInboundProperty(HttpConnector.HTTP_STATUS_PROPERTY, 0));
 
         Map<String, Object> props = new HashMap<String, Object>();
         props.put(HttpConnector.HTTP_METHOD_PROPERTY, HttpConstants.METHOD_GET);
-        result = client.send("http://localhost:63081/helloworld", "", props);
+        result = client.send("http://localhost:" + port.getNumber() + "/helloworld", "", props);
         assertEquals((Integer)405, result.getInboundProperty(HttpConnector.HTTP_STATUS_PROPERTY, 0));
 
         props.put(HttpConnector.HTTP_METHOD_PROPERTY, HttpConstants.METHOD_DELETE);
-        result = client.send("http://localhost:63081/helloworld", "", props);
+        result = client.send("http://localhost:" + port.getNumber() + "/helloworld", "", props);
         assertEquals("Hello World Delete", result.getPayloadAsString());
         assertEquals((Integer)200, result.getInboundProperty(HttpConnector.HTTP_STATUS_PROPERTY, 0));
     }
@@ -69,21 +79,21 @@ public class BasicJerseyTestCase extends AbstractServiceAndFlowTestCase
 
         Map<String, Object> props = new HashMap<String, Object>();
         props.put(HttpConnector.HTTP_METHOD_PROPERTY, HttpConstants.METHOD_GET);
-        MuleMessage result = client.send("http://localhost:63081/helloworld/sayHelloWithUri/Dan", "", props);
+        MuleMessage result = client.send("http://localhost:" + port.getNumber() + "/helloworld/sayHelloWithUri/Dan", "", props);
         assertEquals((Integer)200, result.getInboundProperty(HttpConnector.HTTP_STATUS_PROPERTY, 0));
         assertEquals("Hello Dan", result.getPayloadAsString());
 
 
-        result = client.send("http://localhost:63081/helloworld/sayHelloWithJson/Dan", "", props);
+        result = client.send("http://localhost:" + port.getNumber() + "/helloworld/sayHelloWithJson/Dan", "", props);
         assertEquals((Integer)200, result.getInboundProperty(HttpConnector.HTTP_STATUS_PROPERTY, 0));
         assertEquals(getJsonHelloBean(), result.getPayloadAsString());
 
-        result = client.send("http://localhost:63081/helloworld/sayHelloWithQuery?name=Dan", "", props);
+        result = client.send("http://localhost:" + port.getNumber() + "/helloworld/sayHelloWithQuery?name=Dan", "", props);
         assertEquals((Integer)200, result.getInboundProperty(HttpConnector.HTTP_STATUS_PROPERTY, 0));
         assertEquals("Hello Dan", result.getPayloadAsString());
 
         props.put("X-Name", "Dan");
-        result = client.send("http://localhost:63081/helloworld/sayHelloWithHeader", "", props);
+        result = client.send("http://localhost:" + port.getNumber() + "/helloworld/sayHelloWithHeader", "", props);
         assertEquals((Integer)201, result.getInboundProperty(HttpConnector.HTTP_STATUS_PROPERTY, 0));
         assertEquals("Hello Dan", result.getPayloadAsString());
         assertEquals("Dan", result.getInboundProperty("X-ResponseName"));
@@ -101,7 +111,7 @@ public class BasicJerseyTestCase extends AbstractServiceAndFlowTestCase
 
         Map<String, Object> props = new HashMap<String, Object>();
         props.put(HttpConnector.HTTP_METHOD_PROPERTY, HttpConstants.METHOD_GET);
-        MuleMessage result = client.send("http://localhost:63081/helloworld/throwException", "", props);
+        MuleMessage result = client.send("http://localhost:" + port.getNumber() + "/helloworld/throwException", "", props);
         assertEquals(expectedErrorCode, result.getInboundProperty(HttpConnector.HTTP_STATUS_PROPERTY, 0));
         assertTrue(result.getPayloadAsString().contains(expectedData));
     }
