75cbaf5f2adf1412db1e2912d53a00a309a63af2
evangelinamartinezruizmoreno <evangelina.martinez@mulesource.com>
2014-01-31 15:36:27 -0300

    MULE-7189: For some reasons previous changes were not picked up on push

--- modules/cxf/src/test/java/org/mule/module/cxf/ProxyRPCBindingTestCase.java
+++ modules/cxf/src/test/java/org/mule/module/cxf/ProxyRPCBindingTestCase.java
@@ -7,55 +7,26 @@
 
 package org.mule.module.cxf;
 
-import static org.junit.Assert.assertEquals;
-
 import org.mule.api.MuleMessage;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.util.IOUtils;
 
-import org.apache.commons.lang.StringUtils;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class ProxyRPCBindingTestCase extends FunctionalTestCase
 {
-    private static final String SAMPLE_REQUEST = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:sam=\"http://example.cxf.module.mule.org/\">\n" +
-                                                 "   <soapenv:Header/>\n" +
-                                                 "   <soapenv:Body>\n" +
-                                                 "      <sam:getAll>\n" +
-                                                 "         <pageSize>10</pageSize>\n" +
-                                                 "         <pageNumber>1</pageNumber>\n" +
-                                                 "      </sam:getAll>\n" +
-                                                 "   </soapenv:Body>\n" +
-                                                 "</soapenv:Envelope>";
-
-    private static final String SAMPLE_RESPONSE = "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
-                                                  "\t<soap:Body>\n" +
-                                                  "\t\t<ns1:getAllResponse xmlns:ns1=\"http://example.cxf.module.mule.org/\">\n" +
-                                                  "\t\t\t<return>\n" +
-                                                  "\t\t\t\t<item>\n" +
-                                                  "\t\t\t\t\t<artType>AUTHOR</artType>\n" +
-                                                  "\t\t\t\t\t<famousWorks>Hamlet</famousWorks>\n" +
-                                                  "\t\t\t\t\t<firstName>William</firstName>\n" +
-                                                  "\t\t\t\t\t<lastName>Shakespeare</lastName>\n" +
-                                                  "\t\t\t\t</item>\n" +
-                                                  "\t\t\t\t<item>\n" +
-                                                  "\t\t\t\t\t<artType>ACTOR</artType>\n" +
-                                                  "\t\t\t\t\t<famousWorks>Mission Impossible</famousWorks>\n" +
-                                                  "\t\t\t\t\t<firstName>Tom</firstName>\n" +
-                                                  "\t\t\t\t\t<lastName>Cruise</lastName>\n" +
-                                                  "\t\t\t\t</item>\n" +
-                                                  "\t\t\t</return>\n" +
-                                                  "\t\t</ns1:getAllResponse>\n" +
-                                                  "\t</soap:Body>\n" +
-                                                  "</soap:Envelope>";
-
-
     @Rule
     public final DynamicPort httpPortProxy = new DynamicPort("port1");
 
     @Rule
     public final DynamicPort httpPortService = new DynamicPort("port2");
+
+    private String getAllRequest;
+    private String getAllResponse;
 
 
     @Override
@@ -64,18 +35,26 @@ public class ProxyRPCBindingTestCase extends FunctionalTestCase
         return "proxy-rpc-binding-conf.xml";
     }
 
+    @Before
+    public void doSetUp() throws Exception
+    {
+        getAllRequest = IOUtils.getResourceAsString("artistregistry-get-all-request.xml", getClass());
+        getAllResponse = IOUtils.getResourceAsString("artistregistry-get-all-response.xml", getClass());
+        XMLUnit.setIgnoreWhitespace(true);
+    }
+
     @Test
     public void proxyRPCBodyPayload() throws Exception
     {
-        MuleMessage response = muleContext.getClient().send("http://localhost:" + httpPortProxy.getNumber() + "/body", SAMPLE_REQUEST, null);
-        assertEquals(StringUtils.deleteWhitespace(SAMPLE_RESPONSE), StringUtils.deleteWhitespace(response.getPayloadAsString()));
+        MuleMessage response = muleContext.getClient().send("http://localhost:" + httpPortProxy.getNumber() + "/body", getAllRequest, null);
+        XMLUnit.compareXML(getAllResponse, response.getPayloadAsString());
     }
 
     @Test
     public void proxyRPCBodyEnvelope() throws Exception
     {
-        MuleMessage response = muleContext.getClient().send("http://localhost:" + httpPortProxy.getNumber() + "/envelope", SAMPLE_REQUEST, null);
-        assertEquals(StringUtils.deleteWhitespace(SAMPLE_RESPONSE), StringUtils.deleteWhitespace(response.getPayloadAsString()));
+        MuleMessage response = muleContext.getClient().send("http://localhost:" + httpPortProxy.getNumber() + "/envelope", getAllRequest, null);
+        XMLUnit.compareXML(getAllResponse, response.getPayloadAsString());
     }
 
 }
