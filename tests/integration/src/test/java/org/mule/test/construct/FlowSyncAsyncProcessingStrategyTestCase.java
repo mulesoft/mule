7052274fe9f283dad0a35043a2e2eef2ffec50df
afelisatti <afelisatti@mulesoft.com>
2014-02-13 16:07:14 -0300

    MULE-7316: Fix flaky. Removed sleep and replaced it for flow listener and polling probe instead.

--- tests/integration/src/test/java/org/mule/test/construct/FlowSyncAsyncProcessingStrategyTestCase.java
+++ tests/integration/src/test/java/org/mule/test/construct/FlowSyncAsyncProcessingStrategyTestCase.java
@@ -6,69 +6,96 @@
  */
 package org.mule.test.construct;
 
-import org.junit.Ignore;
+import org.junit.After;
 import org.mule.api.client.MuleClient;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.listener.FlowExecutionListener;
+import org.mule.tck.probe.PollingProber;
+import org.mule.tck.probe.Probe;
+import org.mule.tck.probe.Prober;
 import org.mule.util.FileUtils;
 
 import java.io.File;
+import java.io.IOException;
 
 import junit.framework.Assert;
 
 import org.junit.Test;
 
-@Ignore("MULE-6926: flaky test")
 public class FlowSyncAsyncProcessingStrategyTestCase extends FunctionalTestCase
 {
     public static final String SLEEP_TIME = "sleepTime";
+    private static final String FILE_PATH = "./test/testfile.txt";
+    private File file;
 
     @Override
     protected String getConfigFile()
     {
         return "org/mule/test/construct/flow-sync-async-processing-strategy-config.xml";
+    }
 
+    @After
+    public void cleanUp()
+    {
+        FileUtils.deleteQuietly(file);
     }
 
     @Test
     public void testSynchProcessingStrategy() throws Exception
     {
-        sendMsgAndWait("vm://testSynch");
-
-        File file = getFileTestWroteTo();
+        sendMessage("vm://testSynch");
+        new FlowExecutionListener("synchFlow", muleContext).waitUntilFlowIsComplete();
+        file = new File(FILE_PATH);
         String str = FileUtils.readFileToString(file);
 
         Assert.assertEquals("Part 1Part 2", str);
-
-        FileUtils.deleteQuietly(file);
     }
 
     @Test
     public void testAsynch() throws Exception
     {
-        sendMsgAndWait("vm://testAsynch");
+        sendMessage("vm://testAsynch");
 
-        File file = getFileTestWroteTo();
-        String str = FileUtils.readFileToString(file);
-
-        Assert.assertEquals("Part 2Part 1", str);
-
-        FileUtils.deleteQuietly(file);
-
+        file = new File(FILE_PATH);
+        Prober prober = new PollingProber(20000, 3000);
+        prober.check(new FileCompleteProbe(file));
     }
 
-    private void sendMsgAndWait(String endpoint) throws Exception
+    private void sendMessage(String endpoint) throws Exception
     {
         MuleClient client = muleContext.getClient();
 
         client.dispatch(endpoint, "Part 1;Part 2", null);
-
-        Thread.sleep(10000);
-
     }
 
-    private File getFileTestWroteTo()
+    private static class FileCompleteProbe implements Probe
     {
-        return new File("./test/testfile.txt");
-    }
+        private final File testFile;
+        private String output;
 
+        public FileCompleteProbe(File file)
+        {
+            testFile = file;
+        }
+
+        public boolean isSatisfied()
+        {
+            if(testFile.exists()) {
+                try {
+                    output = FileUtils.readFileToString(testFile);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return "Part 2Part 1".equals(output);
+            }
+            else {
+                return false;
+            }
+        }
+
+        public String describeFailure()
+        {
+            return "Expected output was 'Part2Part 1' but actual one was: " + output;
+        }
+    }
 }
