f0e7cd72434edc5ef3c458faadf119e9ea8017cc
Alberto Aresca <alberto.aresca@gmail.com>
2014-02-21 12:45:57 -0300

    MULE-7358: org.mule.config.bootstrap.SimpleRegistryBootstrap.registerTransactionFactories can't handle optional factories

--- core/src/test/java/org/mule/config/bootstrap/FakeTransactionFactory.java
+++ core/src/test/java/org/mule/config/bootstrap/FakeTransactionFactory.java
@@ -6,26 +6,24 @@ import org.mule.api.transaction.TransactionException;
 import org.mule.api.transaction.UniversalTransactionFactory;
 
 public final class FakeTransactionFactory implements UniversalTransactionFactory
+{
+
+    @Override
+    public Transaction beginTransaction(MuleContext muleContext) throws TransactionException 
     {
-
-        @Override
-        public Transaction beginTransaction(MuleContext muleContext)
-                throws TransactionException {
-            // TODO Auto-generated method stub
-            return null;
-        }
-
-        @Override
-        public boolean isTransacted() {
-            // TODO Auto-generated method stub
-            return false;
-        }
-
-        @Override
-        public Transaction createUnboundTransaction(MuleContext muleContext)
-                throws TransactionException {
-            // TODO Auto-generated method stub
-            return null;
-        }
-        
+        return null;
     }
+
+    @Override
+    public boolean isTransacted() 
+    {
+        return false;
+    }
+
+    @Override
+    public Transaction createUnboundTransaction(MuleContext muleContext) throws TransactionException 
+    {
+        return null;
+    }
+
+}
