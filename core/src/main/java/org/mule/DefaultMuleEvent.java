fcc40247949b57b4a44f3fb2d0bc1ff47cb2f64e
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2014-03-12 12:06:44 -0300

    Revert "MULE-6889: Concurrent Modification Exception when using the Async Message Proccessor inside a foreach"
    This reverts commit f80948519f58cd158aa5bcad048d4d642370e5d2.

--- core/src/main/java/org/mule/DefaultMuleEvent.java
+++ core/src/main/java/org/mule/DefaultMuleEvent.java
@@ -323,7 +323,7 @@ public class DefaultMuleEvent implements MuleEvent, ThreadSafeAccess, Deserializ
         if (rewriteEvent instanceof DefaultMuleEvent)
         {
             this.processingTime = ((DefaultMuleEvent) rewriteEvent).processingTime;
-            this.flowVariables.putAll(((DefaultMuleEvent) rewriteEvent).flowVariables);
+            this.flowVariables = ((DefaultMuleEvent) rewriteEvent).flowVariables;
         }
         else
         {
