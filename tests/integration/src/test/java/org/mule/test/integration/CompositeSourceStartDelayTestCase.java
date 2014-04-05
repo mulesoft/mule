mulesoft/mule-integration-tests

MULE-7399: Flows can start processing messages before referenced flows are completely started
Broken test: cannot use mule client to exercise an inbound endpoint if mule context is not fully started.

Fix: use HttpClient to exercise an HTTP endpoint.

src/test/java/org/mule/test/integration/CompositeSourceStartDelayTestCase.java
@@ -6,27 +6,31 @@
  */
 package org.mule.test.integration;
 
-import static org.junit.Assert.assertEquals;
-
-import org.mule.api.MuleEvent;
 import org.mule.api.MuleException;
-import org.mule.api.MuleMessage;
-import org.mule.api.client.LocalMuleClient;
-import org.mule.api.lifecycle.Startable;
-import org.mule.api.processor.MessageProcessor;
-import org.mule.api.transformer.TransformerException;
-import org.mule.endpoint.DefaultInboundEndpoint;
+import org.mule.api.endpoint.EndpointBuilder;
+import org.mule.api.endpoint.InboundEndpoint;
+import org.mule.api.lifecycle.StartException;
+import org.mule.endpoint.DefaultEndpointFactory;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
 import org.mule.tck.probe.PollingProber;
 import org.mule.tck.probe.Probe;
-import org.mule.tck.probe.Prober;
-import org.mule.transformer.AbstractTransformer;
+import org.mule.tck.util.endpoint.InboundEndpointWrapper;
 
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class CompositeSourceStartDelayTestCase extends FunctionalTestCase
 {
-    public static volatile boolean awakeMessageSource;
+
+    public static final CountDownLatch startLatch = new CountDownLatch(1);
+
+    @Rule
+    public DynamicPort httpPort = new DynamicPort("httpPort");
 
     public CompositeSourceStartDelayTestCase()
     {
@@ -41,6 +45,21 @@ public class CompositeSourceStartDelayTestCase extends FunctionalTestCase
 
     @Test
     public void testProcessMessageWhenAnSourceIsNotStartedYet() throws Exception
+    {
+        try
+        {
+            asynchronousMuleContextStart();
+
+            PollingProber prober = new PollingProber(RECEIVE_TIMEOUT, 50);
+            prober.check(new ProcessMessageProbe());
+        }
+        finally
+        {
+            startLatch.countDown();
+        }
+    }
+
+    private void asynchronousMuleContextStart()
     {
         Thread thread = new Thread(new Runnable()
         {
@@ -59,68 +78,78 @@ public class CompositeSourceStartDelayTestCase extends FunctionalTestCase
         });
 
         thread.start();
-
-        waitUntilEndpointIsStarted("testInEndpoint");
-
-
-        LocalMuleClient client = muleContext.getClient();
-        MuleMessage response = client.send("vm://testIn", "TEST", null);
-        assertEquals("TEST received", response.getPayloadAsString());
     }
 
-    private void waitUntilEndpointIsStarted(final String endpointName)
+    private class ProcessMessageProbe implements Probe
     {
-        Prober prober = new PollingProber(30000, 50);
-        prober.check(new Probe()
+
+        private final HttpClient httpClient = new HttpClient();
+
+        public boolean isSatisfied()
         {
-            @Override
-            public boolean isSatisfied()
-            {
-                DefaultInboundEndpoint endpoint = (DefaultInboundEndpoint) muleContext.getRegistry().lookupObject(endpointName);
-                return endpoint.getConnector().isStarted();
-            }
+            GetMethod method = new GetMethod("http://localhost:" + httpPort.getValue());
 
-            @Override
-            public String describeFailure()
+            try
             {
-                return "Endpoint was not started";
-            }
-        });
-    }
+                int statusCode = httpClient.executeMethod(method);
+                String response = method.getResponseBodyAsString();
 
-    public static class AwakeSourceMessageProcessor implements MessageProcessor
-    {
-        @Override
-        public MuleEvent process(MuleEvent event) throws MuleException
+                return 200 == statusCode && "/Processed".equals(response);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public String describeFailure()
         {
-            awakeMessageSource = true;
-
-            return event;
+            return "Unable to process message when composite source was not completely started";
         }
     }
 
-    public static class StuckTransformer extends AbstractTransformer implements Startable
+    public static class DelayedStartEndpointFactory extends DefaultEndpointFactory
     {
-        @Override
-        protected Object doTransform(Object src, String enc) throws TransformerException
+
+        public InboundEndpoint getInboundEndpoint(EndpointBuilder builder) throws MuleException
         {
-            return null;
+            InboundEndpoint endpoint = builder.buildInboundEndpoint();
+
+            if (endpoint.getName().equals("sleepingTestIn"))
+            {
+                InboundEndpointWrapper wrappedEndpoint = new DelayedStartInboundEndpointWrapper(endpoint);
+
+                return (InboundEndpoint) registerEndpoint(wrappedEndpoint);
+            }
+            else
+            {
+                return (InboundEndpoint) registerEndpoint(endpoint);
+            }
+        }
+    }
+
+    public static class DelayedStartInboundEndpointWrapper extends InboundEndpointWrapper
+    {
+
+        public DelayedStartInboundEndpointWrapper(InboundEndpoint delegate)
+        {
+            super(delegate);
         }
 
         @Override
         public void start() throws MuleException
         {
-            while (!awakeMessageSource)
+            try
             {
-                try
-                {
-                    Thread.sleep(10);
-                }
-                catch (InterruptedException e)
-                {
-                    // Nothing to do
-                }
+                startLatch.await();
             }
+            catch (InterruptedException e)
+            {
+                Thread.currentThread().interrupt();
+                throw new StartException(e, this);
+            }
+
+            super.start();
         }
     }
 }

mulesoft/mule

(cherry picked from commit 19f7add7e2a9758d40c3dceee125220ab461cd71)

tests/integration/src/test/java/org/mule/test/integration/CompositeSourceStartDelayTestCase.java
@@ -6,28 +6,31 @@
  */
 package org.mule.test.integration;
 
-import static org.junit.Assert.assertEquals;
-
-import org.mule.api.MuleEvent;
 import org.mule.api.MuleException;
-import org.mule.api.MuleMessage;
-import org.mule.api.client.LocalMuleClient;
-import org.mule.api.lifecycle.Startable;
-import org.mule.api.processor.MessageProcessor;
-import org.mule.api.transformer.TransformerException;
-import org.mule.endpoint.DefaultInboundEndpoint;
+import org.mule.api.endpoint.EndpointBuilder;
+import org.mule.api.endpoint.InboundEndpoint;
+import org.mule.api.lifecycle.StartException;
+import org.mule.endpoint.DefaultEndpointFactory;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
 import org.mule.tck.probe.PollingProber;
 import org.mule.tck.probe.Probe;
-import org.mule.tck.probe.Prober;
-import org.mule.transformer.AbstractTransformer;
+import org.mule.tck.util.endpoint.InboundEndpointWrapper;
 
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class CompositeSourceStartDelayTestCase extends FunctionalTestCase
 {
 
-    public static volatile boolean awakeMessageSource;
+    public static final CountDownLatch startLatch = new CountDownLatch(1);
+
+    @Rule
+    public DynamicPort httpPort = new DynamicPort("httpPort");
 
     public CompositeSourceStartDelayTestCase()
     {
@@ -42,6 +45,21 @@ public class CompositeSourceStartDelayTestCase extends FunctionalTestCase
 
     @Test
     public void testProcessMessageWhenAnSourceIsNotStartedYet() throws Exception
+    {
+        try
+        {
+            asynchronousMuleContextStart();
+
+            PollingProber prober = new PollingProber(RECEIVE_TIMEOUT, 50);
+            prober.check(new ProcessMessageProbe());
+        }
+        finally
+        {
+            startLatch.countDown();
+        }
+    }
+
+    private void asynchronousMuleContextStart()
     {
         Thread thread = new Thread(new Runnable()
         {
@@ -60,67 +78,77 @@ public class CompositeSourceStartDelayTestCase extends FunctionalTestCase
         });
 
         thread.start();
-
-        waitUntilEndpointIsStarted("testInEndpoint");
-
-
-        LocalMuleClient client = muleContext.getClient();
-        MuleMessage response = client.send("vm://testIn", "TEST", null);
-        assertEquals("TEST received", response.getPayloadAsString());
     }
 
-    private void waitUntilEndpointIsStarted(final String endpointName)
-    {
-        Prober prober = new PollingProber(30000, 50);
-        prober.check(new Probe()
-        {
-
-            public boolean isSatisfied()
-            {
-                DefaultInboundEndpoint endpoint = (DefaultInboundEndpoint) muleContext.getRegistry().lookupObject(endpointName);
-                return endpoint.getConnector().isStarted();
-            }
-
-            public String describeFailure()
-            {
-                return "Endpoint was not started";
-            }
-        });
-    }
-
-    public static class AwakeSourceMessageProcessor implements MessageProcessor
+    private class ProcessMessageProbe implements Probe
     {
 
-        public MuleEvent process(MuleEvent event) throws MuleException
-        {
-            awakeMessageSource = true;
+        private final HttpClient httpClient = new HttpClient();
 
-            return event;
+        public boolean isSatisfied()
+        {
+            GetMethod method = new GetMethod("http://localhost:" + httpPort.getNumber());
+
+            try
+            {
+                int statusCode = httpClient.executeMethod(method);
+                String response = method.getResponseBodyAsString();
+
+                return 200 == statusCode && "/Processed".equals(response);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public String describeFailure()
+        {
+            return "Unable to process message when composite source was not completely started";
         }
     }
 
-    public static class StuckTransformer extends AbstractTransformer implements Startable
+    public static class DelayedStartEndpointFactory extends DefaultEndpointFactory
     {
 
-        @Override
-        protected Object doTransform(Object src, String enc) throws TransformerException
+        public InboundEndpoint getInboundEndpoint(EndpointBuilder builder) throws MuleException
         {
-            return null;
+            InboundEndpoint endpoint = builder.buildInboundEndpoint();
+
+            if (endpoint.getName().equals("sleepingTestIn"))
+            {
+                InboundEndpointWrapper wrappedEndpoint = new DelayedStartInboundEndpointWrapper(endpoint);
+
+                return (InboundEndpoint) registerEndpoint(wrappedEndpoint);
+            }
+            else
+            {
+                return (InboundEndpoint) registerEndpoint(endpoint);
+            }
+        }
+    }
+
+    public static class DelayedStartInboundEndpointWrapper extends InboundEndpointWrapper
+    {
+
+        public DelayedStartInboundEndpointWrapper(InboundEndpoint delegate)
+        {
+            super(delegate);
         }
 
         public void start() throws MuleException
         {
-            while (!awakeMessageSource)
+            try
             {
-                try
-                {
-                    Thread.sleep(10);
-                }
-                catch (InterruptedException e)
-                {
-                    // Nothing to do
-                }
+                startLatch.await();
             }
+            catch (InterruptedException e)
+            {
+                Thread.currentThread().interrupt();
+                throw new StartException(e, this);
+            }
+
+            super.start();
         }
     }
 }