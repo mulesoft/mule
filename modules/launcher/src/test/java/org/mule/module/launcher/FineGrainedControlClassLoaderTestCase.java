8dfd6463772585f7e5f0c5270b9e8fdca7be6578
pabad <pabad@mulesoft.com>
2014-04-15 21:08:54 -0300

    MULE-7367: rewrote launcher module classloader tests to run as unit tests This change removes the need to use maven shitty plugin, and eliminates a cyclic dependency on requiring artifacts to be installed int he local repo before being testable.
    (cherry picked from commit 765f21f33ec1537878bc8596959237b91dab9703)

--- modules/launcher/src/test/java/org/mule/module/launcher/FineGrainedControlClassLoaderTestCase.java
+++ modules/launcher/src/test/java/org/mule/module/launcher/FineGrainedControlClassLoaderTestCase.java
@@ -6,18 +6,24 @@
  */
 package org.mule.module.launcher;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
 import org.mule.tck.junit4.AbstractMuleTestCase;
 
+import java.lang.reflect.Method;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Test;
+import org.mule.util.ClassUtils;
 
 public class FineGrainedControlClassLoaderTestCase extends AbstractMuleTestCase
 {
+
     @Test
     public void isBlockedFQClassName() throws Exception
     {
@@ -86,5 +92,92 @@ public class FineGrainedControlClassLoaderTestCase extends AbstractMuleTestCase
             overrides);
         assertTrue(classLoader.isOverridden("org.mycompany.MyClass"));
         assertTrue(classLoader.isOverridden("org.mycompany.somepackage.MyClass"));
+    }
+
+    @Test
+    public void parentFirst() throws Exception
+    {
+        URLClassLoader parent = new URLClassLoader(new URL[] { hello() }, Thread.currentThread().getContextClassLoader());
+
+        FineGrainedControlClassLoader ext = new FineGrainedControlClassLoader(new URL[] { bye() }, parent);
+        assertEquals("Hello", callHi(ext));
+    }
+
+    @Test
+    public void childResolvesOverriden() throws Exception
+    {
+        URLClassLoader parent = new URLClassLoader(new URL[] { hello() }, Thread.currentThread().getContextClassLoader());
+
+        Set<String> overrides = new HashSet<String>();
+        overrides.add("mypackage");
+        FineGrainedControlClassLoader ext = new FineGrainedControlClassLoader(new URL[] { bye() }, parent, overrides);
+
+        assertEquals("Bye", callHi(ext));
+    }
+
+    @Test
+    public void parentResolvesMissingOverride() throws Exception
+    {
+        URLClassLoader parent = new URLClassLoader(new URL[] { hello() }, Thread.currentThread().getContextClassLoader());
+
+        Set<String> overrides = new HashSet<String>();
+        overrides.add("mypackage");
+        FineGrainedControlClassLoader ext = new FineGrainedControlClassLoader(new URL[0], parent, overrides);
+        assertEquals("Hello", callHi(ext));
+    }
+
+    @Test(expected = ClassNotFoundException.class)
+    public void blockedParentOverride() throws Exception
+    {
+        URLClassLoader parent = new URLClassLoader(new URL[] { hello() }, Thread.currentThread().getContextClassLoader());
+
+        Set<String> overrides = new HashSet<String>();
+        overrides.add("-mypackage");
+        FineGrainedControlClassLoader ext = new FineGrainedControlClassLoader(new URL[0], parent, overrides);
+        callHi(ext);
+    }
+
+    @Test
+    public void blockedOverrideIsLoadedInChild() throws Exception
+    {
+        URLClassLoader parent = new URLClassLoader(new URL[] { hello() }, Thread.currentThread().getContextClassLoader());
+
+        Set<String> overrides = new HashSet<String>();
+        overrides.add("-mypackage");
+        FineGrainedControlClassLoader ext = new FineGrainedControlClassLoader(new URL[] { bye() }, parent, overrides);
+        assertEquals("Bye", callHi(ext));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void illegalOverride()
+    {
+        Set<String> overrides = new HashSet<String>();
+        overrides.add("org.mule.module.reboot.MuleContainerBootstrap");
+        new FineGrainedControlClassLoader(new URL[0], null, overrides);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void illegalBlock()
+    {
+        Set<String> overrides = new HashSet<String>();
+        overrides.add("-java.util.Collections");
+        new FineGrainedControlClassLoader(new URL[0], null, overrides);
+    }
+
+    private URL hello()
+    {
+        return ClassUtils.getResource("classloader-test-hello.jar", this.getClass());
+    }
+
+    private URL bye()
+    {
+        return ClassUtils.getResource("classloader-test-bye.jar", this.getClass());
+    }
+
+    private String callHi(ClassLoader loader) throws Exception
+    {
+        Class cls = loader.loadClass("mypackage.MyClass");
+        Method method = cls.getMethod("hi");
+        return (String) method.invoke(cls.newInstance());
     }
 }
