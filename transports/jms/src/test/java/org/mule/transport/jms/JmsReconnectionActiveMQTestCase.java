b3c156e641f52941f8ccb17d85bf32413dbc7cd3
andres.gregoire <andres.gregoire@mulesoft.com>
2014-04-29 10:27:01 -0300

    MULE-7534: JMS connector doesn't reconnect to ActiveMQ broker 5.6 when using blocking=true
    Fixed flaky test

--- transports/jms/src/test/java/org/mule/transport/jms/JmsReconnectionActiveMQTestCase.java
+++ transports/jms/src/test/java/org/mule/transport/jms/JmsReconnectionActiveMQTestCase.java
@@ -8,17 +8,22 @@ package org.mule.transport.jms;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import org.mule.api.MuleMessage;
 import org.mule.context.notification.ConnectionNotification;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 import org.mule.tck.listener.ConnectionListener;
+import org.mule.tck.probe.PollingProber;
+import org.mule.tck.probe.Probe;
+import org.mule.tck.probe.Prober;
 
 import javax.jms.Connection;
 import javax.jms.JMSException;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.broker.BrokerService;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -33,15 +38,25 @@ import org.mockito.Mockito;
 public class JmsReconnectionActiveMQTestCase extends FunctionalTestCase
 {
 
+    private static final long PROBER_TIMEOUT = 3000;
+
     @Rule
     public DynamicPort port = new DynamicPort("port");
 
     private BrokerService broker;
+    private Prober prober;
+    private JmsConnector jmsConnector;
 
     @Override
     protected String getConfigFile()
     {
         return "jms-reconnection-activemq-config.xml";
+    }
+
+    @Before
+    public void doSetUp()
+    {
+        prober = new PollingProber(PROBER_TIMEOUT, PollingProber.DEFAULT_POLLING_INTERVAL);
     }
 
     @Override
@@ -75,6 +90,8 @@ public class JmsReconnectionActiveMQTestCase extends FunctionalTestCase
     @Test
     public void reconnectsAfterRestartingActiveMQBroker() throws Exception
     {
+        jmsConnector = (JmsConnector) muleContext.getRegistry().lookupConnector("jmsConnector");
+
         assertMessageRouted();
 
         // Stop the broker, and make the connection factory return invalid connections.
@@ -85,15 +102,28 @@ public class JmsReconnectionActiveMQTestCase extends FunctionalTestCase
         stopBroker();
 
         connectionListener.waitUntilNotificationsAreReceived();
+        assertTrue(jmsConnector.isStopped());
+
 
         // Restart the broker
-        ConnectionListener reconnectionListener = new ConnectionListener(muleContext)
-                .setExpectedAction(ConnectionNotification.CONNECTION_CONNECTED).setNumberOfExecutionsRequired(1);
-
         CustomConnectionFactory.returnInvalidConnections = false;
         startBroker();
 
-        reconnectionListener.waitUntilNotificationsAreReceived();
+        // Wait until jmsConnector is reconnected and started.
+        prober.check(new Probe()
+        {
+            @Override
+            public boolean isSatisfied()
+            {
+                return jmsConnector.isStarted();
+            }
+
+            @Override
+            public String describeFailure()
+            {
+                return "JMS connector did not restart";
+            }
+        });
 
         // Check that reconnection worked
         assertMessageRouted();
