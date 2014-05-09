5a00d34240e7339658ee32e72bee3393ae92db82
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2014-05-09 15:24:51 -0300

    MULE-7593: Merge Scatter-gather throws IllegalStateException when using only one message processor

--- tests/integration/src/test/java/org/mule/test/routing/ScatterGatherRouterTestCase.java
+++ tests/integration/src/test/java/org/mule/test/routing/ScatterGatherRouterTestCase.java
@@ -141,6 +141,41 @@ public class ScatterGatherRouterTestCase extends FunctionalTestCase
     }
 
     @Test
+    public void oneRouteWithRequestResponseInboundEndpoint() throws Exception
+    {
+        muleContext.getClient().send("vm://oneRouteWithRequestResponseInboundEndpoint", getTestEvent("").getMessage());
+        FlowAssert.verify("oneRouteWithRequestResponseInboundEndpoint");
+    }
+
+    @Test
+    public void oneRouteWithOneWayResponseInboundEndpoint() throws Exception
+    {
+        muleContext.getClient().send("vm://oneRouteWithOneWayResponseInboundEndpoint", getTestEvent("").getMessage());
+        FlowAssert.verify("oneRouteWithOneWayResponseInboundEndpoint");
+    }
+
+    @Test
+    public void manyRoutesWithRequestResponseInboundEndpoint() throws Exception
+    {
+        muleContext.getClient().send("vm://manyRoutesWithRequestResponseInboundEndpoint", getTestEvent("").getMessage());
+        FlowAssert.verify("manyRoutesWithRequestResponseInboundEndpoint");
+    }
+
+    @Test
+    public void manyRoutesWithOneWayInboundEndpoint() throws Exception
+    {
+        muleContext.getClient().send("vm://manyRoutesWithOneWayInboundEndpoint", getTestEvent("").getMessage());
+        FlowAssert.verify("manyRoutesWithOneWayInboundEndpoint");
+    }
+
+    @Test
+    public void routesWithForeachAndInboundEndpoint() throws Exception {
+        final String[] payload = new String[] {"apple", "banana", "orange"};
+        muleContext.getClient().send("vm://routesWithForeachAndInboundEndpoint", getTestEvent(Arrays.asList(payload)).getMessage());
+        FlowAssert.verify("routesWithForeachAndInboundEndpoint");
+    }
+
+    @Test
     public void exceptionStrategy() throws Exception
     {
         this.testFlow("exceptionStrategy", getTestEvent(""));
