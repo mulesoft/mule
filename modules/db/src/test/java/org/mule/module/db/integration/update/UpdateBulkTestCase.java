2dc3ef4aeadfdd65eaf137ad635629b467fcaa4f
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2014-05-10 00:41:45 -0300

    MULE-7596: Database bulk operations don't work with objects that are Iterable

--- modules/db/src/test/java/org/mule/module/db/integration/update/UpdateBulkTestCase.java
+++ modules/db/src/test/java/org/mule/module/db/integration/update/UpdateBulkTestCase.java
@@ -19,6 +19,7 @@ import static org.mule.module.db.integration.model.Planet.VENUS;
 import static org.mule.module.db.integration.TestRecordUtil.assertRecords;
 import org.mule.api.MuleMessage;
 import org.mule.api.client.LocalMuleClient;
+import org.mule.api.client.MuleClient;
 import org.mule.module.db.integration.AbstractDbIntegrationTestCase;
 import org.mule.module.db.integration.model.AbstractTestDatabase;
 import org.mule.module.db.integration.model.Field;
@@ -29,6 +30,7 @@ import org.mule.transport.NullPayload;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -37,6 +39,8 @@ import org.junit.runners.Parameterized;
 
 public class UpdateBulkTestCase extends AbstractDbIntegrationTestCase
 {
+
+    private MuleClient client;
 
     public UpdateBulkTestCase(String dataSourceConfigResource, AbstractTestDatabase testDatabase)
     {
@@ -50,27 +54,51 @@ public class UpdateBulkTestCase extends AbstractDbIntegrationTestCase
     }
 
     @Override
+    protected void doSetUp() throws Exception
+    {
+        super.doSetUp();
+        client = muleContext.getClient();
+    }
+
+    @Override
     protected String[] getFlowConfigurationResources()
     {
         return new String[] {"integration/update/update-bulk-config.xml"};
     }
 
     @Test
-    public void updatesInBulkMode() throws Exception
+    public void updatesInBulkModeWithCollection() throws Exception
     {
-        LocalMuleClient client = muleContext.getClient();
-        List<String> planetNames = new ArrayList<String>();
-        planetNames.add(VENUS.getName());
-        planetNames.add(MARS.getName());
-        planetNames.add(EARTH.getName());
-
-        MuleMessage response = client.send("vm://updateBulk", planetNames, null);
-
+        MuleMessage response = client.send("vm://updateBulk", getPlanetNames(), null);
         assertBulkModeResult(response);
     }
 
     @Test
-    public void requiresCollectionPayload() throws Exception
+    public void updatesInBulkModeWithIterator() throws Exception
+    {
+        MuleMessage response = client.send("vm://updateBulk", getPlanetNames().iterator(), null);
+        assertBulkModeResult(response);
+    }
+
+    @Test
+    public void updatesInBulkModeWithIterable() throws Exception
+    {
+        final List<String> planetNames = getPlanetNames();
+        Iterable<String> iterable = new Iterable<String>()
+        {
+            @Override
+            public Iterator<String> iterator()
+            {
+                return planetNames.iterator();
+            }
+        };
+
+        MuleMessage response = client.send("vm://updateBulk", iterable, null);
+        assertBulkModeResult(response);
+    }
+
+    @Test
+    public void requiresSplittableType() throws Exception
     {
         LocalMuleClient client = muleContext.getClient();
         MuleMessage response = client.send("vm://updateBulk", TEST_MESSAGE, null);
@@ -89,5 +117,14 @@ public class UpdateBulkTestCase extends AbstractDbIntegrationTestCase
 
         List<Map<String, String>> result = selectData("select * from PLANET order by ID", getDefaultDataSource());
         assertRecords(result, new Record(new Field("NAME", "Mercury"), new Field("POSITION", 2)), new Record(new Field("NAME", "Mercury"), new Field("POSITION", 3)), new Record(new Field("NAME", "Mercury"), new Field("POSITION", 4)));
+    }
+
+    private List<String> getPlanetNames()
+    {
+        List<String> planetNames = new ArrayList<String>();
+        planetNames.add(VENUS.getName());
+        planetNames.add(MARS.getName());
+        planetNames.add(EARTH.getName());
+        return planetNames;
     }
 }
