38f9c558344e4b1cca298eca82408bc7b84c9f51
pablo.kraan <pablo.kraan@gmail.com>
2014-05-12 11:20:24 -0300

    MULE-7524: Encoded characters not working when using expressions for dynamic endpoints
    _ Reverting previous commits
    (cherry picked from commit 88b6f34)

--- core/src/main/java/org/mule/endpoint/AbstractEndpointBuilder.java
+++ core/src/main/java/org/mule/endpoint/AbstractEndpointBuilder.java
@@ -223,9 +223,8 @@ public abstract class AbstractEndpointBuilder implements EndpointBuilder, Annota
         String uri = uriBuilder.getConstructor();
         if(muleContext.getExpressionManager().isExpression(uri))
         {
-            if (muleContext.getExpressionManager().isValidExpression(uri))
+            if(muleContext.getExpressionManager().isValidExpression(uriBuilder.getConstructor()))
             {
-                uri = uriBuilder.getEncodedConstructor();
                 String dynamicAddress = getDynamicUriFrom(uri);
                 uriBuilder = new URIBuilder(dynamicAddress, muleContext);
 
