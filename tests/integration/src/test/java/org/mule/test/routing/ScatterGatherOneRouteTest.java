2b321d6a426b1818554d3c3ea9b6b72bff2fab31
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2014-05-13 14:23:12 -0300

    MULE-7597: Also enforce the two routes rules when initializing ScatterGather programatically

--- tests/integration/src/test/java/org/mule/test/routing/ScatterGatherOneRouteTest.java
+++ tests/integration/src/test/java/org/mule/test/routing/ScatterGatherOneRouteTest.java
@@ -8,9 +8,16 @@ package org.mule.test.routing;
 
 import org.mule.api.MuleContext;
 import org.mule.api.config.ConfigurationException;
+import org.mule.api.lifecycle.InitialisationException;
+import org.mule.api.processor.MessageProcessor;
 import org.mule.config.spring.SpringXmlConfigurationBuilder;
 import org.mule.context.DefaultMuleContextFactory;
+import org.mule.routing.ScatterGatherRouter;
 import org.mule.tck.junit4.AbstractMuleTestCase;
+
+import com.yourkit.util.Asserts;
+
+import java.util.Collections;
 
 import org.junit.Test;
 
@@ -18,10 +25,27 @@ public class ScatterGatherOneRouteTest extends AbstractMuleTestCase
 {
 
     @Test(expected = ConfigurationException.class)
-    public void oneRoute() throws Exception
+    public void oneRouteOnXml() throws Exception
     {
         MuleContext context = new DefaultMuleContextFactory().createMuleContext();
         SpringXmlConfigurationBuilder builder = new SpringXmlConfigurationBuilder("scatter-gather-one-route-test.xml");
         builder.configure(context);
+    }
+
+    @Test(expected = InitialisationException.class)
+    public void oneRouteProgramatically() throws Exception
+    {
+        ScatterGatherRouter sc = new ScatterGatherRouter();
+        sc.setRoutes(Collections.<MessageProcessor>emptyList());
+
+        try
+        {
+            sc.initialise();
+        }
+        catch (InitialisationException e)
+        {
+            Asserts.assertTrue(e.getCause() instanceof IllegalStateException);
+            throw e;
+        }
     }
 }
