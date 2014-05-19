d2968a73877f3a735fd1ca4cafb28ed5daf7f53b
Victor Romero <victor.romero@mulesoft.com>
2014-05-19 18:52:46 -0300

    MULE-7616 Mule should not print the full message on fatal exception. Unit test.

--- core/src/test/java/org/mule/exception/CatchMessagingExceptionStrategyTestCase.java
+++ core/src/test/java/org/mule/exception/CatchMessagingExceptionStrategyTestCase.java
@@ -18,6 +18,7 @@ import org.mule.api.ExceptionPayload;
 import org.mule.api.MuleContext;
 import org.mule.api.MuleEvent;
 import org.mule.api.MuleException;
+import org.mule.api.DefaultMuleException;
 import org.mule.api.MuleMessage;
 import org.mule.api.processor.MessageProcessor;
 import org.mule.api.transaction.Transaction;
@@ -107,6 +108,22 @@ public class CatchMessagingExceptionStrategyTestCase
         assertThat(exceptionHandlingResult, Is.is(lastEventCreated));
     }
 
+    /**
+     *  On fatal error, the exception strategies are not supposed to use MuleMessage.toString() as it could
+     * potentially log sensible data.
+     */
+    @Test
+    public void testMessageToStringNotCalledOnFailure() throws Exception
+    {
+        MuleEvent lastEventCreated = mock(MuleEvent.class,Answers.RETURNS_DEEP_STUBS.get());
+        catchMessagingExceptionStrategy.setMessageProcessors(asList(createFailingEventMessageProcessor(mock(MuleEvent.class, Answers.RETURNS_DEEP_STUBS.get())), createFailingEventMessageProcessor(lastEventCreated)));
+        catchMessagingExceptionStrategy.initialise();
+
+        when(mockMuleEvent.getMessage().toString()).thenThrow(new RuntimeException("MuleMessage.toString() should not be called"));
+
+        MuleEvent exceptionHandlingResult = exceptionHandlingResult = catchMessagingExceptionStrategy.handleException(mockException, mockMuleEvent);
+    }
+
     private MessageProcessor createChagingEventMessageProcessor(final MuleEvent lastEventCreated)
     {
         return new MessageProcessor()
@@ -119,6 +136,17 @@ public class CatchMessagingExceptionStrategyTestCase
         };
     }
 
+    private MessageProcessor createFailingEventMessageProcessor(final MuleEvent lastEventCreated)
+    {
+        return new MessageProcessor()
+        {
+            @Override
+            public MuleEvent process(MuleEvent event) throws MuleException
+            {
+                throw new DefaultMuleException(mockException);
+            }
+        };
+    }
 
     private MessageProcessor createSetStringMessageProcessor(final String appendText)
     {
