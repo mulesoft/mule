10c1ccf088858de2c3e64065491b42344ba1d169
pablo.kraan <pablo.kraan@gmail.com>
2014-06-30 12:40:02 -0300

    MULE-7703: Add a way to configure default threading profile
    _ Adding system properties to configure the default threading profile
    _ Creating DefaultThreadingProfileConfig to resolve default constant values
    _ Unit test use reflection as there is no other way to check whether or not the system properties were properly resolved

--- core/src/main/java/org/mule/api/config/ThreadingProfile.java
+++ core/src/main/java/org/mule/api/config/ThreadingProfile.java
@@ -41,30 +41,31 @@ import org.apache.commons.collections.map.CaseInsensitiveMap;
  */
 public interface ThreadingProfile extends MuleContextAware
 {
+
     /**
      * Default value for MAX_THREADS_ACTIVE
      */
-    int DEFAULT_MAX_THREADS_ACTIVE = 16;
+    int DEFAULT_MAX_THREADS_ACTIVE = DefaultThreadingProfileConfig.DEFAULT_MAX_THREADS_ACTIVE;
 
     /**
      * Default value for MAX_THREADS_IDLE
      */
-    int DEFAULT_MAX_THREADS_IDLE = 1;
+    int DEFAULT_MAX_THREADS_IDLE = DefaultThreadingProfileConfig.DEFAULT_MAX_THREADS_IDLE;
 
     /**
      * Default value for MAX_BUFFER_SIZE
      */
-    int DEFAULT_MAX_BUFFER_SIZE = 0;
+    int DEFAULT_MAX_BUFFER_SIZE = DefaultThreadingProfileConfig.DEFAULT_MAX_BUFFER_SIZE;
 
     /**
      * Default value for MAX_THREAD_TTL
      */
-    long DEFAULT_MAX_THREAD_TTL = 60000;
+    long DEFAULT_MAX_THREAD_TTL = DefaultThreadingProfileConfig.DEFAULT_MAX_THREAD_TTL;
 
     /**
      * Default value for DEFAULT_THREAD_WAIT_TIMEOUT
      */
-    long DEFAULT_THREAD_WAIT_TIMEOUT = 30000L;
+    long DEFAULT_THREAD_WAIT_TIMEOUT = DefaultThreadingProfileConfig.DEFAULT_THREAD_WAIT_TIMEOUT;
 
     /**
      * Default value for do threading
