1ab855180ed644928fddc82155bf80534bcd07ce
pablo.kraan <pablo.kraan@gmail.com>
2014-07-07 12:27:59 -0300

    MULE-7726: No transacted DB connector requires a connection per operation

--- modules/db/src/test/java/org/mule/module/db/integration/config/DatasourcePoolingLimitTestCase.java
+++ modules/db/src/test/java/org/mule/module/db/integration/config/DatasourcePoolingLimitTestCase.java
@@ -7,8 +7,6 @@
 
 package org.mule.module.db.integration.config;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import org.mule.api.MessagingException;
 import org.mule.api.MuleMessage;
@@ -34,11 +32,19 @@ public class DatasourcePoolingLimitTestCase extends AbstractDatasourcePoolingTes
     @Test
     public void limitsConnections() throws Exception
     {
-        LocalMuleClient client = muleContext.getClient();
+        try
+        {
+            LocalMuleClient client = muleContext.getClient();
 
-        MuleMessage response = client.send("vm://testIn", TEST_MESSAGE, null);
+            client.dispatch("vm://testIn", TEST_MESSAGE, null);
+            client.dispatch("vm://testIn", TEST_MESSAGE, null);
 
-        assertTrue(response.getExceptionPayload().getException() instanceof MessagingException);
-        assertThat(counter, equalTo(1));
+            MuleMessage response = client.request("vm://connectionError", RECEIVE_TIMEOUT);
+            assertTrue(response.getExceptionPayload().getException() instanceof MessagingException);
+        }
+        finally
+        {
+            connectionLatch.countDown();
+        }
     }
 }
