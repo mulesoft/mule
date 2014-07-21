9879300a2010dfcc5d029753aae222e9e663a799
marcosnc <marcosnc@gmail.com>
2014-07-21 16:58:23 -0300

    MULE-7757: Deprecate OGNL module in 3.6.

--- modules/ognl/src/main/java/org/mule/module/ognl/config/OGNLNamespaceHandler.java
+++ modules/ognl/src/main/java/org/mule/module/ognl/config/OGNLNamespaceHandler.java
@@ -11,6 +11,8 @@ import org.mule.config.spring.parsers.generic.ChildDefinitionParser;
 import org.mule.config.spring.parsers.specific.FilterDefinitionParser;
 import org.mule.module.ognl.filters.OGNLFilter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
@@ -21,10 +23,18 @@ import org.w3c.dom.Element;
 public class OGNLNamespaceHandler extends NamespaceHandlerSupport
 {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     public void init()
     {
+        logger.warn(getDeprecationWarning());
         registerBeanDefinitionParser("filter", new FilterDefinitionParser(OGNLFilter.class));
         registerBeanDefinitionParser("expression", new CDATABeanDefinitionParser("expression", String.class));
+    }
+
+    public static String getDeprecationWarning()
+    {
+        return "OGNL module is deprecated and will be removed in Mule 4.0. Use MEL expressions instead.";
     }
 
     private static class CDATABeanDefinitionParser extends
