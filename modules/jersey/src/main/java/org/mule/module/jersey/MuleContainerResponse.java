61a34f77ae991d33d8b58709e12da70d68bf65ef
andres.gregoire <andres.gregoire@mulesoft.com>
2014-08-15 15:45:57 -0300

    MULE-7814: OutOfMemoryError with Jersey/HTTP Streaming

---
+++ modules/jersey/src/main/java/org/mule/module/jersey/MuleContainerResponse.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.jersey;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseWriter;
+import com.sun.jersey.spi.container.WebApplication;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Custom implementation of ContainerResponse that doesn't write the whole response to the stream in the write() method.
+ * The Jersey application processes the request, sets all the data required to build the response in this object, and
+ * calls the write method. This class overrides the write method by only setting the headers and status code in the
+ * MuleMessage. Nothing is written to the output stream until the writeToStream method is called.
+ */
+class MuleContainerResponse extends ContainerResponse
+{
+
+    protected MuleContainerResponse(WebApplication wa, ContainerRequest request, ContainerResponseWriter responseWriter)
+    {
+        super(wa, request, responseWriter);
+    }
+
+    @Override
+    public void write() throws IOException
+    {
+        getContainerResponseWriter().writeStatusAndHeaders(-1, this);
+        return;
+    }
+
+    public void writeToStream(OutputStream out) throws IOException
+    {
+        MuleResponseWriter muleResponseWriter = (MuleResponseWriter) getContainerResponseWriter();
+        muleResponseWriter.setOutputStream(out);
+        super.write();
+    }
+}
---
+++ modules/jersey/src/test/java/org/mule/module/jersey/JerseyStreamingTestCase.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.jersey;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import org.mule.api.MuleMessage;
+import org.mule.api.client.MuleClient;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.transport.http.HttpConnector;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
+import org.junit.Test;
+
+public class JerseyStreamingTestCase extends FunctionalTestCase
+{
+
+    @Override
+    protected String getConfigFile()
+    {
+        return "jersey-streaming-config.xml";
+    }
+
+    @Test
+    public void consumesJerseyStreamingOutputWhenOutputHandlerIsConsumed() throws Exception
+    {
+        MuleClient client = muleContext.getClient();
+
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put(HttpConnector.HTTP_REQUEST_PROPERTY, "/resource/operation");
+        properties.put(HttpConnector.HTTP_CONTEXT_PATH_PROPERTY, "");
+        properties.put(HttpConnector.HTTP_METHOD_PROPERTY, "GET");
+
+        client.dispatch("vm://in", null, properties);
+        MuleMessage response = client.request("vm://out", RECEIVE_TIMEOUT);
+
+        TestResource testResource = muleContext.getRegistry().get("testResourceBean");
+
+        assertThat(testResource.isCalled(), is(true));
+        assertThat(testResource.getTestStreamingOutput().isCalled(), is(false));
+
+        response.getPayloadAsBytes();
+
+        assertThat(testResource.getTestStreamingOutput().isCalled(), is(true));
+    }
+
+    @Path("/resource")
+    public static class TestResource
+    {
+        private volatile boolean called = false;
+        private TestStreamingOutput testStreamingOutput = new TestStreamingOutput();
+
+        @GET
+        @Path("/operation")
+        public Response testOperation() {
+            called = true;
+            return Response.ok(testStreamingOutput).build();
+        }
+
+        public TestStreamingOutput getTestStreamingOutput()
+        {
+            return testStreamingOutput;
+        }
+
+        public boolean isCalled()
+        {
+            return called;
+        }
+    }
+
+
+    public static class TestStreamingOutput implements StreamingOutput
+    {
+        private volatile boolean called = false;
+
+        @Override
+        public void write(OutputStream output) throws IOException, WebApplicationException
+        {
+            called = true;
+            output.write(TEST_MESSAGE.getBytes());
+        }
+
+        public boolean isCalled()
+        {
+            return called;
+        }
+    }
+}
