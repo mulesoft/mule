b32dc4f9d3b629dfd8a84b31064fd7ad1927b7b2
Daniel Feist <dfeist@gmail.com>
2014-08-20 09:14:59 -0300

    MULE-7819 GlobalFunctions lead to memory leaks

--- core/src/main/java/org/mule/el/mvel/MVELExpressionLanguageContext.java
+++ core/src/main/java/org/mule/el/mvel/MVELExpressionLanguageContext.java
@@ -15,6 +15,7 @@ import org.mule.mvel2.ImmutableElementException;
 import org.mule.mvel2.ParserConfiguration;
 import org.mule.mvel2.ParserContext;
 import org.mule.mvel2.ast.FunctionInstance;
+import org.mule.mvel2.compiler.AbstractParser;
 import org.mule.mvel2.integration.VariableResolver;
 
 import java.lang.reflect.Method;
@@ -156,8 +157,16 @@ public class MVELExpressionLanguageContext extends MuleBaseVariableResolverFacto
     @Override
     public void declareFunction(String name, ExpressionLanguageFunction function)
     {
-        addFinalVariable(name, new FunctionInstance(new MVELFunctionAdaptor(name, function,
-            new ParserContext(parserConfiguration))));
+        try
+        {
+            addFinalVariable(name, new FunctionInstance(new MVELFunctionAdaptor(name, function,
+                    new ParserContext(parserConfiguration))));
+        }
+        finally
+        {
+            // Clear AbstractParser.parserContext ThreadLocal once Function has been created.
+            AbstractParser.resetParserContext();
+        }
     }
 
     @Override
