32876b0983a8ded0032da3ff369ad406862c7359
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2014-08-21 12:00:50 -0300

    MULE-7611: Adding test case to reproduce the issue

---
+++ core/src/test/java/org/mule/el/mvel/MVELMapHandlingTestCase.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.el.mvel;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import org.mule.api.MuleEvent;
+import org.mule.api.el.ExpressionLanguage;
+import org.mule.tck.junit4.AbstractMuleContextTestCase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class MVELMapHandlingTestCase extends AbstractMuleContextTestCase
+{
+
+    private static final String KEY = "Name";
+    private static final String VALUE = "MG";
+    private ExpressionLanguage el;
+
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        super.doSetUp();
+        el = muleContext.getExpressionLanguage();
+    }
+
+    @Test
+    public void keyWithNonNullValue() throws Exception
+    {
+        Map<String, String> payload = new HashMap<String, String>();
+        payload.put(KEY, VALUE);
+
+        assertMapKey(payload, KEY, VALUE);
+    }
+
+    @Test
+    public void keyWithNullValue() throws Exception
+    {
+        Map<String, String> payload = new HashMap<String, String>();
+        assertMapKey(payload, KEY, null);
+    }
+
+    @Test
+    public void keyWithNullableValue() throws Exception
+    {
+        Map<String, String> payload = new HashMap<String, String>();
+        payload.put(KEY, VALUE);
+
+        MuleEvent event = getTestEvent(payload);
+
+        assertMapKey(event, KEY, VALUE);
+        payload.remove(KEY);
+        assertMapKey(event, KEY, null);
+    }
+
+    @Test
+    public void nullKeyWhichGetsValueLater() throws Exception
+    {
+        Map<String, String> payload = new HashMap<String, String>();
+
+        MuleEvent event = getTestEvent(payload);
+
+        assertMapKey(event, KEY, null);
+
+        payload.put(KEY, VALUE);
+        assertMapKey(event, KEY, VALUE);
+    }
+
+    private void assertMapKey(Object payload, String key, Object expectedValue) throws Exception
+    {
+        assertMapKey(getTestEvent(payload), key, expectedValue);
+    }
+
+    private void assertMapKey(MuleEvent event, String key, Object expectedValue) throws Exception
+    {
+        runExpressionAndExpect(String.format("#[payload.%s]", key), expectedValue, event);
+        runExpressionAndExpect(String.format("#[payload['%s']]", key), expectedValue, event);
+        runExpressionAndExpect(String.format("#[payload.'%s']", key), expectedValue, event);
+    }
+
+    private void runExpressionAndExpect(String expression, Object expectedValue, MuleEvent event)
+    {
+        Object result = el.evaluate(expression, event);
+        assertThat(String.format("Expression %s returned unexpected value", expression), result, equalTo(expectedValue));
+    }
+}
