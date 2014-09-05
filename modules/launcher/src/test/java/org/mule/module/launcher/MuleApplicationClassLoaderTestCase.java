7f1fa50a674b2223c2a15d6715aba454aff7a5d2
afelisatti <afelisatti@mulesoft.com>
2014-09-05 15:23:12 -0300

    MULE-7840: Fixing broken test.

--- modules/launcher/src/test/java/org/mule/module/launcher/MuleApplicationClassLoaderTestCase.java
+++ modules/launcher/src/test/java/org/mule/module/launcher/MuleApplicationClassLoaderTestCase.java
@@ -54,8 +54,10 @@ public class MuleApplicationClassLoaderTestCase extends AbstractMuleTestCase
     {
         // Create directories structure
         previousMuleHome = System.setProperty(MuleProperties.MULE_HOME_DIRECTORY_PROPERTY, tempMuleHome.getRoot().getAbsolutePath());
-        domainDir = tempMuleHome.newFolder(String.format("domains/%s", DOMAIN_NAME));
-        classesDir = tempMuleHome.newFolder(String.format("apps/%s/classes", APP_NAME));
+        domainDir = new File(tempMuleHome.getRoot(), String.format("domains/%s", DOMAIN_NAME));
+        domainDir.mkdirs();
+        classesDir = new File(tempMuleHome.getRoot(), String.format("apps/%s/classes", APP_NAME));
+        classesDir.mkdirs();
         File libDir = tempMuleHome.newFolder(String.format("apps/%s/lib", APP_NAME));
 
         // Add jar file with resources in lib dir
