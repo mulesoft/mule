715fb6aa3bccf5ecfce6e7d13a6386116fb7f572
Daniel Feist <dfeist@gmail.com>
2014-09-10 19:15:56 +0100

    Merge branch 'mule-3.x' into mule-4.x
    Conflicts:
    core/src/main/java/org/mule/api/MuleEventContext.java
    core/src/main/java/org/mule/api/routing/OutboundRouterCatchAllStrategy.java
    core/src/main/java/org/mule/api/routing/OutboundRouterCollection.java
    core/src/main/java/org/mule/component/simple/PassThroughComponent.java
    core/src/main/java/org/mule/routing/AbstractCatchAllStrategy.java
    core/src/main/java/org/mule/routing/ForwardingCatchAllStrategy.java
    core/src/main/java/org/mule/routing/LoggingCatchAllStrategy.java
    core/src/main/java/org/mule/routing/outbound/ChainingRouter.java
    core/src/main/java/org/mule/routing/outbound/DefaultOutboundRouterCollection.java
    core/src/main/java/org/mule/routing/outbound/EndpointSelector.java
    core/src/main/java/org/mule/routing/outbound/ExceptionBasedRouter.java
    core/src/main/java/org/mule/routing/outbound/ExpressionMessageSplitter.java
    core/src/main/java/org/mule/routing/outbound/ListMessageSplitter.java
    core/src/main/java/org/mule/routing/outbound/MessageChunkingRouter.java
    core/src/main/java/org/mule/routing/outbound/OutboundPassThroughRouter.java
    core/src/main/java/org/mule/routing/outbound/SequenceRouter.java
    core/src/main/java/org/mule/routing/outbound/StaticRecipientList.java
    core/src/main/java/org/mule/routing/outbound/TransformerRouter.java
    core/src/test/java/org/mule/routing/outbound/DefaultOutboundRouterCollectionTestCase.java
    distributions/standalone/src/main/resources/MIGRATION.txt
    modules/client/src/main/java/org/mule/module/client/MuleClient.java
    modules/client/src/main/java/org/mule/module/client/RemoteDispatcher.java
    modules/client/src/main/java/org/mule/module/client/config/RemoteDispatcherAgentDefinitionParser.java
    modules/client/src/main/java/org/mule/module/client/i18n/ClientMessages.java
    modules/client/src/main/java/org/mule/module/client/remoting/RemoteDispatcherAgent.java
    modules/client/src/main/java/org/mule/module/client/remoting/RemoteDispatcherComponent.java
    modules/client/src/main/java/org/mule/module/client/remoting/RemoteDispatcherException.java
    modules/client/src/main/java/org/mule/module/client/remoting/ServerHandshake.java
    modules/client/src/main/java/org/mule/module/client/remoting/UnsupportedWireFormatException.java
    modules/client/src/main/java/org/mule/module/client/remoting/notification/RemoteDispatcherNotification.java
    modules/client/src/main/java/org/mule/module/client/remoting/notification/RemoteDispatcherNotificationListener.java
    modules/scripting/src/main/java/org/mule/module/scripting/builders/ScriptConfigurationBuilder.java
    modules/scripting/src/main/resources/META-INF/mule-scripting.xsd
    modules/spring-extras/src/main/java/org/mule/module/spring/events/ApplicationEventException.java
    modules/spring-extras/src/main/java/org/mule/module/spring/events/AsynchronousEventListener.java
    modules/spring-extras/src/main/java/org/mule/module/spring/events/MuleApplicationEvent.java
    modules/spring-extras/src/main/java/org/mule/module/spring/events/MuleEventListener.java
    modules/spring-extras/src/main/java/org/mule/module/spring/events/MuleEventMulticaster.java
    modules/spring-extras/src/main/java/org/mule/module/spring/events/MuleSubscriptionEventListener.java
    patterns/core/pom.xml
    transports/axis/pom.xml

---
+++ modules/launcher/src/test/java/org/mule/module/launcher/application/DefaultMuleApplicationStatusTestCase.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.launcher.application;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import org.mule.context.notification.MuleContextNotification;
+import org.mule.tck.junit4.AbstractMuleContextTestCase;
+import org.mule.tck.probe.JUnitProbe;
+import org.mule.tck.probe.PollingProber;
+
+import org.junit.Test;
+
+/**
+ * This tests verifies that the {@link org.mule.module.launcher.application.DefaultMuleApplication}
+ * status is set correctly depending on its {@link org.mule.api.MuleContext}'s lifecycle phase
+ */
+public class DefaultMuleApplicationStatusTestCase extends AbstractMuleContextTestCase
+{
+
+    private static final int PROBER_TIMEOUT = 1000;
+    private static final int PROBER_INTERVAL = 100;
+
+    private DefaultMuleApplication application;
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        application = new DefaultMuleApplication(null, null, null);
+        application.setMuleContext(muleContext);
+    }
+
+    @Test
+    public void initialState()
+    {
+        assertStatus(ApplicationStatus.CREATED);
+    }
+
+    @Test
+    public void initialised()
+    {
+        // the context was initialised before we gave it to the application, so we need
+        // to fire the notification again since the listener wasn't there
+        muleContext.fireNotification(new MuleContextNotification(muleContext, MuleContextNotification.CONTEXT_INITIALISED));
+        assertStatus(ApplicationStatus.INITIALISED);
+    }
+
+    @Test
+    public void started() throws Exception
+    {
+        muleContext.start();
+        assertStatus(ApplicationStatus.STARTED);
+    }
+
+    @Test
+    public void stopped() throws Exception
+    {
+        muleContext.start();
+        muleContext.stop();
+        assertStatus(ApplicationStatus.STOPPED);
+    }
+
+    @Test
+    public void destroyed()
+    {
+        muleContext.dispose();
+        assertStatus(ApplicationStatus.DESTROYED);
+    }
+
+    @Test
+    public void deploymentFailedOnInit()
+    {
+        try
+        {
+            application.init();
+            fail("Was expecting init to fail");
+        }
+        catch (Exception e)
+        {
+            assertStatus(ApplicationStatus.DEPLOYMENT_FAILED);
+        }
+    }
+
+    @Test
+    public void deploymentFailedOnStart()
+    {
+        try
+        {
+            application.start();
+            fail("Was expecting start to fail");
+        }
+        catch (Exception e)
+        {
+            assertStatus(ApplicationStatus.DEPLOYMENT_FAILED);
+        }
+    }
+
+    private void assertStatus(final ApplicationStatus status)
+    {
+        PollingProber prober = new PollingProber(PROBER_TIMEOUT, PROBER_INTERVAL);
+        prober.check(new JUnitProbe()
+        {
+            @Override
+            protected boolean test() throws Exception
+            {
+                assertThat(application.getStatus(), is(status));
+                return true;
+            }
+
+            @Override
+            public String describeFailure()
+            {
+                return String.format("Application remained at status %s instead of moving to %s", application.getStatus().name(), status.name());
+            }
+        });
+
+    }
+}
