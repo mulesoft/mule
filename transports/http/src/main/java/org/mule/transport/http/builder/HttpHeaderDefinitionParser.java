f8fc5ffd71c958c45b282848e991d08422fd98ae
andres.gregoire <andres.gregoire@mulesoft.com>
2014-09-10 15:29:38 -0300

    MULE-7845: Basic functionality for new HTTP Connector - Outbound part
    Added mule-module-http as a dependency of the HTTP transport, and changed the schema and namespace handler so that the XML elements of the new module are added to the HTTP namespace.

---
+++ transports/http/src/main/java/org/mule/transport/http/builder/HttpHeaderDefinitionParser.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.transport.http.builder;
+
+import org.mule.config.spring.parsers.collection.ChildMapEntryDefinitionParser;
+import org.mule.config.spring.parsers.delegate.ParentContextDefinitionParser;
+import org.mule.module.http.config.HttpRequestSingleParamDefinitionParser;
+import org.mule.module.http.request.HttpParamType;
+import org.mule.module.http.request.HttpSingleParam;
+
+/**
+ * Custom bean definition parser for the "header" element that works for both the old "header" element in the
+ * HTTP transport (inside the response builder) and the new "header" element in the HTTP module (inside the
+ * request builder).
+ */
+public class HttpHeaderDefinitionParser extends ParentContextDefinitionParser
+{
+    public HttpHeaderDefinitionParser()
+    {
+        super("response-builder", new ChildMapEntryDefinitionParser("headers", "name", "value"));
+        otherwise(new HttpRequestSingleParamDefinitionParser(HttpSingleParam.class, HttpParamType.HEADER));
+    }
+
+}
