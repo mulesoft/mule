005e5f0ff76ee0c0182a38507cf7d335f609fa97
Victor Romero <victor.romero@gmail.com>
2014-09-23 17:03:39 -0300

    MULE-7892 Enable testing mode in functional tests

--- core/src/test/java/org/mule/tck/junit4/AbstractMuleTestCase.java
+++ core/src/test/java/org/mule/tck/junit4/AbstractMuleTestCase.java
@@ -45,6 +45,7 @@ import org.junit.rules.Timeout;
  */
 public abstract class AbstractMuleTestCase
 {
+    public static final String TESTING_MODE_PROPERTY_NAME = "mule.testingMode";
 
     public static final int DEFAULT_TEST_TIMEOUT_SECS = 60;
 
@@ -75,6 +76,8 @@ public abstract class AbstractMuleTestCase
         {
             verbose = true;
         }
+
+        System.setProperty(TESTING_MODE_PROPERTY_NAME, StringUtils.EMPTY);
 
         // register the custom UrlStreamHandlerFactory.
         MuleUrlStreamHandlerFactory.installUrlStreamHandlerFactory();
