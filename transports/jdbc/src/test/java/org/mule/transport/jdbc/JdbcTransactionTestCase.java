ab16f687327cf357e07dc0585bdfd389e9989a57
marcosnc <marcosnc@gmail.com>
2014-10-01 13:30:23 -0300

    MULE-7907: Raise an exception if rollback or commit operations were OK, but close operation failed.
    (cherry picked from commit fa65f1a1ff688ee522b3483cecf846605d8eef86)
    
    The file transports/jdbc/src/test/java/org/mule/transport/jdbc/JdbcTransactionTestCase.java was changed in order to use jUnit 4.9.
    
    (cherry picked from commit b9e23d1df3617bd469c2370bbe4595469f61d5c5)

---
+++ transports/jdbc/src/test/java/org/mule/transport/jdbc/JdbcTransactionTestCase.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.transport.jdbc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mule.api.MuleContext;
+import org.mule.api.transaction.TransactionException;
+import org.mule.tck.junit4.AbstractMuleTestCase;
+import org.mule.tck.size.SmallTest;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+@SmallTest
+public class JdbcTransactionTestCase extends AbstractMuleTestCase
+{
+
+    private SQLException expectedExceptionCause;
+
+    private JdbcTransaction tx;
+    private Connection connection;
+
+    @Before
+    public void initializeMocks() throws SQLException, TransactionException
+    {
+        tx = new JdbcTransaction(mock(MuleContext.class));
+
+        connection = mock(Connection.class);
+
+        when(connection.getAutoCommit()).thenReturn(false);
+
+        tx.bindResource(mock(DataSource.class), connection);
+
+        expectedExceptionCause = new SQLException();
+    }
+
+    @After
+    public void ensureConnectionCloseWasCalled() throws SQLException
+    {
+        verify(connection).close();
+    }
+
+    @Test
+    public void closeConnectionIfErrorOnCommit() throws Exception
+    {
+        doThrow(expectedExceptionCause).when(connection).commit();
+        commitExecutor.execute();
+    }
+
+    @Test
+    public void closeConnectionIfErrorOnRollback() throws Exception
+    {
+        doThrow(expectedExceptionCause).when(connection).rollback();
+        rollbackExecutor.execute();
+    }
+
+    @Test
+    public void closeConnectionIfCommitOk() throws Exception
+    {
+        tx.commit();
+    }
+
+    @Test
+    public void closeConnectionIfRollbackOk() throws Exception
+    {
+        tx.rollback();
+    }
+
+    @Test
+    public void raiseCloseExceptionAfterCommitOk() throws Exception
+    {
+        doThrow(expectedExceptionCause).when(connection).close();
+        commitExecutor.execute();
+    }
+
+    @Test
+    public void raiseCloseExceptionAfterRollbackOk() throws Exception
+    {
+        doThrow(expectedExceptionCause).when(connection).close();
+        rollbackExecutor.execute();
+    }
+
+    @Test
+    public void raiseCommitExceptionIfCommitAndCloseFail() throws Exception
+    {
+        doThrow(expectedExceptionCause).when(connection).commit();
+        doThrow(new SQLException()).when(connection).close();
+        commitExecutor.execute();
+    }
+
+    @Test
+    public void raiseRollbackExceptionIfRollbackAndCloseFail() throws Exception
+    {
+        doThrow(expectedExceptionCause).when(connection).rollback();
+        doThrow(new SQLException()).when(connection).close();
+        rollbackExecutor.execute();
+    }
+
+    private TestExceptionExecutor commitExecutor = new TestExceptionExecutor()
+    {
+        @Override
+        protected void txOperation() throws TransactionException
+        {
+            tx.commit();
+        }
+    };
+
+    private TestExceptionExecutor rollbackExecutor = new TestExceptionExecutor()
+    {
+        @Override
+        protected void txOperation() throws TransactionException
+        {
+            tx.rollback();
+        }
+    };
+
+    private abstract class TestExceptionExecutor
+    {
+        protected abstract void txOperation() throws TransactionException;
+
+        public void execute()
+        {
+            try
+            {
+                txOperation();
+                fail("TransactionException expected.");
+            }
+            catch(TransactionException e)
+            {
+                assertThat((SQLException)e.getCause(), is(expectedExceptionCause));
+            }
+        }
+    }
+}
+
+
