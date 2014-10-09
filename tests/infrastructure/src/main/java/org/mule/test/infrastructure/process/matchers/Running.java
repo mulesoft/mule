7b7596744d01236f1136871c43c4d85c5389b9eb
Alejandro Sequeira <asequeira@gmail.com>
2014-10-09 17:32:54 -0300

    MULE-7780 - JUnit rules and matchers for Mule Standalone server

---
+++ tests/infrastructure/src/main/java/org/mule/test/infrastructure/process/matchers/Running.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.infrastructure.process.matchers;
+
+import org.mule.test.infrastructure.process.MuleProcessController;
+
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class Running extends TypeSafeMatcher<MuleProcessController>
+{
+
+    private boolean isRunning;
+
+    public Running(boolean status)
+    {
+        this.isRunning = status;
+    }
+
+    @Override
+    public boolean matchesSafely(MuleProcessController mule)
+    {
+        try
+        {
+            return isRunning == mule.isRunning();
+        }
+        catch (Error e)
+        {
+            return false;
+        }
+    }
+
+    @Override
+    public void describeTo(Description description)
+    {
+        description.appendText("a Mule Standalone server that is " + (isRunning ? "": "not ") + "running");
+    }
+
+    @Factory
+    public static <T> Matcher<MuleProcessController> isRunning()
+    {
+        return new Running(true);
+    }
+
+    @Factory
+    public static <T> Matcher<MuleProcessController> notRunning()
+    {
+        return new Running(false);
+    }
+
+};
---
+++ tests/infrastructure/src/main/java/org/mule/test/infrastructure/process/rules/MuleStandalone.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.infrastructure.process.rules;
+
+import org.mule.test.infrastructure.process.MuleProcessController;
+
+import org.junit.rules.ExternalResource;
+
+/**
+ * This is a JUnit rule to start and stop Mule ESB during tests. Usage:
+ * <p/>
+ * <pre>
+ * public static class MuleStandaloneIntegrationTests {
+ *  &#064;Rule
+ *  public MuleStandalone standalone = new MuleStandalone(&quot;/path/to/mule/home&quot;);
+ *
+ *  &#064;Test
+ *  public void integrationTest() throws IOException
+ *  {
+ *      MuleProcessController mule = standalone.getMule();
+ *      assertThat(mule.isRunning(), is(true));
+ *  }
+ * }
+ * </pre>
+ */
+public class MuleStandalone extends ExternalResource
+{
+
+    private final MuleProcessController mule;
+    private String[] args;
+
+    public MuleStandalone(String muleHome)
+    {
+        mule = new MuleProcessController(muleHome);
+    }
+
+    public MuleStandalone(String muleHome, String... args)
+    {
+        mule = new MuleProcessController(muleHome);
+        this.args = args;
+    }
+
+    @Override
+    protected void before() throws Throwable
+    {
+        mule.start(args);
+    }
+
+    @Override
+    protected void after()
+    {
+        mule.stop();
+    }
+
+    public MuleProcessController getMule()
+    {
+        return mule;
+    }
+
+
+}
