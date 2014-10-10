e7a4549218a8b2ba3cab795987d28e1173db7ea7
Alejandro Sequeira <asequeira@gmail.com>
2014-10-10 11:56:45 -0300

    MULE-7780 - JUnit rules and matchers for Mule Standalone server
    (cherry picked from commit 7b7596744d01236f1136871c43c4d85c5389b9eb)

---
+++ tests/infrastructure/src/main/java/org/mule/test/infrastructure/process/matchers/Deployment.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.infrastructure.process.matchers;
+
+import org.mule.test.infrastructure.process.MuleProcessController;
+
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class Deployment extends TypeSafeMatcher<MuleProcessController>
+{
+
+    private String applicationName;
+
+    public Deployment(String applicationName)
+    {
+        this.applicationName = applicationName;
+    }
+
+    @Override
+    public boolean matchesSafely(MuleProcessController mule)
+    {
+        return mule.isDeployed(applicationName);
+    }
+
+    @Override
+    public void describeTo(Description description)
+    {
+        description.appendText("a Mule Standalone server that has successfully deployed: " + applicationName);
+    }
+
+    @Factory
+    public static <T> Matcher<MuleProcessController> hasDeployed(String applicationName)
+    {
+        return new Deployment(applicationName);
+    }
+
+};
