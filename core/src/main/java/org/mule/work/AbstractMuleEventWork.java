ea920891f20da4124502f31f9f816e47a99a0344
Victor Romero <victor.romero@gmail.com>
2014-11-06 12:12:59 -0300

    Revert "SE-988 Thread Safety issues in one of the constructors of default mule session."
    This reverts commit 65c84194170ab3befb3759516d7c3aef78376d8d.

--- core/src/main/java/org/mule/work/AbstractMuleEventWork.java
+++ core/src/main/java/org/mule/work/AbstractMuleEventWork.java
@@ -7,7 +7,6 @@
 package org.mule.work;
 
 import org.mule.DefaultMuleEvent;
-import org.mule.DefaultMuleMessage;
 import org.mule.OptimizedRequestContext;
 import org.mule.api.MuleEvent;
 
@@ -34,7 +33,6 @@ public abstract class AbstractMuleEventWork implements Work
     {
         // Set event in RequestContext now we are in new thread (fresh copy already made in constructor)
         OptimizedRequestContext.unsafeSetEvent(event);
-        ((DefaultMuleEvent) event).resetAccessControl();
         doRun();
     }
 
