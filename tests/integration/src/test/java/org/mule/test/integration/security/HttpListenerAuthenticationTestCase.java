2d03d4e705e168536569c497350fc72d439d60b2
afelisatti <afelisatti@mulesoft.com>
2014-12-18 13:05:20 -0300

    MULE-8127: Adding matcher for HTTP response header validation and  other changes asked for in PR.

--- tests/integration/src/test/java/org/mule/test/integration/security/HttpListenerAuthenticationTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/security/HttpListenerAuthenticationTestCase.java
@@ -8,26 +8,26 @@ package org.mule.test.integration.security;
 
 import static org.apache.commons.httpclient.HttpStatus.SC_UNAUTHORIZED;
 import static org.apache.http.HttpStatus.SC_OK;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.mule.module.http.api.HttpHeaders.Names.WWW_AUTHENTICATE;
 import static org.mule.tck.matchers.HttpResponseStatusCodeMatcher.hasStatusCode;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.tck.matchers.HttpResponseHeaderMatcher;
 import org.mule.util.IOUtils;
 
 import java.io.IOException;
 
-import org.apache.http.Header;
-import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -39,6 +39,9 @@ public class HttpListenerAuthenticationTestCase extends FunctionalTestCase
     private static final String VALID_PASSWORD = "password";
     private static final String INVALID_PASSWORD = "invalidPassword";
     private static final String EXPECTED_PAYLOAD = "TestBasicAuthOk";
+    CloseableHttpClient httpClient;
+    CloseableHttpResponse httpResponse;
+
 
     @Rule
     public DynamicPort listenPort = new DynamicPort("port");
@@ -49,38 +52,38 @@ public class HttpListenerAuthenticationTestCase extends FunctionalTestCase
         return "org/mule/test/integration/security/http-listener-authentication-config.xml";
     }
 
+    @After
+    public void tearDown()
+    {
+        IOUtils.closeQuietly(httpResponse);
+        IOUtils.closeQuietly(httpClient);
+    }
+
     @Test
     public void invalidBasicAuthentication() throws Exception
     {
         CredentialsProvider credsProvider = getCredentialsProvider(VALID_USER, INVALID_PASSWORD);
-
-        final HttpResponse httpResponse = getHttpResponse(credsProvider);
+        getHttpResponse(credsProvider);
 
         assertThat(httpResponse, hasStatusCode(SC_UNAUTHORIZED));
-
-        Header authHeader = httpResponse.getFirstHeader(WWW_AUTHENTICATE);
-
-        assertThat(authHeader, notNullValue());
-        assertThat(authHeader.getValue(), is(BASIC_REALM_MULE_REALM));
+        assertThat(httpResponse, HttpResponseHeaderMatcher.header(WWW_AUTHENTICATE, is(BASIC_REALM_MULE_REALM)));
     }
 
     @Test
     public void validBasicAuthentication() throws Exception
     {
         CredentialsProvider credsProvider = getCredentialsProvider(VALID_USER, VALID_PASSWORD);
-        final HttpResponse httpResponse = getHttpResponse(credsProvider);
+        getHttpResponse(credsProvider);
 
         assertThat(httpResponse, hasStatusCode(SC_OK));
         assertThat(IOUtils.toString(httpResponse.getEntity().getContent()), is(EXPECTED_PAYLOAD));
     }
 
-    private HttpResponse getHttpResponse(CredentialsProvider credsProvider) throws IOException
+    private void getHttpResponse(CredentialsProvider credsProvider) throws IOException
     {
-        CloseableHttpClient httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
-
         HttpPost httpPost = new HttpPost(String.format("http://localhost:%s/basic", listenPort.getNumber()));
-
-        return httpClient.execute(httpPost);
+        httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+        httpResponse = httpClient.execute(httpPost);
     }
 
     private CredentialsProvider getCredentialsProvider(String user, String password)
