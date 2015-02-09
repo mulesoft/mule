676e853d207d0307e2a9b61c063a2805a84e11c2
marcosnc <marcosnc@gmail.com>
2015-02-09 17:15:07 -0300

    MULE-8306: Add a LeakCleaner interface in order to allow the artifacts (applications and domains) to perform cleanup tasks, in particular to deregister DB drivers from DriverManager when the artifact is disposed.

--- modules/launcher/src/main/java/org/mule/module/launcher/artifact/AbstractArtifactClassLoader.java
+++ modules/launcher/src/main/java/org/mule/module/launcher/artifact/AbstractArtifactClassLoader.java
@@ -9,7 +9,10 @@ package org.mule.module.launcher.artifact;
 import org.mule.module.launcher.DirectoryResourceLocator;
 import org.mule.module.launcher.FineGrainedControlClassLoader;
 import org.mule.module.launcher.LocalResourceLocator;
+import org.mule.util.IOUtils;
+import org.mule.util.StringUtils;
 
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -24,11 +27,17 @@ import org.apache.commons.logging.LogFactory;
  */
 public abstract class AbstractArtifactClassLoader extends FineGrainedControlClassLoader implements ArtifactClassLoader
 {
+    private static final String DEFAULT_LEAK_CLEANER_CLASS = "DefaultLeakCleaner.class";
+
     protected Log logger = LogFactory.getLog(getClass());
 
     protected List<ShutdownListener> shutdownListeners = new ArrayList<ShutdownListener>();
 
     private LocalResourceLocator localResourceLocator;
+
+    private LeakCleaner leakCleaner;
+
+    private String leakCleanerClassName;
 
     public AbstractArtifactClassLoader(URL[] urls, ClassLoader parent)
     {
@@ -60,7 +69,60 @@ public abstract class AbstractArtifactClassLoader extends FineGrainedControlClas
                 logger.error(e);
             }
         }
+
+        LeakCleaner leakCleaner = getLeakCleanerInstance();
+        if (leakCleaner != null)
+        {
+            leakCleaner.clean();
+        }
+
         super.dispose();
+    }
+
+    public String getLeakCleanerClassName()
+    {
+        return StringUtils.defaultString(leakCleanerClassName, DEFAULT_LEAK_CLEANER_CLASS);
+    }
+
+    public void setLeakCleanerClassName(String leakCleanerClass)
+    {
+        this.leakCleanerClassName = leakCleanerClass;
+        leakCleaner = null;
+    }
+
+    public LeakCleaner getLeakCleanerInstance()
+    {
+        if(leakCleaner==null)
+        {
+            leakCleaner = createLeakCleanerInstance(getLeakCleanerClassName());
+        }
+        return leakCleaner;
+    }
+
+    private LeakCleaner createLeakCleanerInstance(String className)
+    {
+        InputStream classStream = this.getClass().getResourceAsStream(className);
+        if (classStream == null)
+        {
+            classStream = AbstractArtifactClassLoader.class.getResourceAsStream(className);
+        }
+        if (classStream == null)
+        {
+            logger.warn("Could not find leak cleaner class with name: " + className);
+            return null;
+        }
+
+        byte[] classBytes = IOUtils.toByteArray(classStream);
+        Class clazz = this.defineClass(null, classBytes, 0, classBytes.length);
+        try
+        {
+            return (LeakCleaner) clazz.newInstance();
+        }
+        catch (Exception e)
+        {
+            logger.warn("Could not instantiate leak cleaner instance of type: " + className, e);
+        }
+        return null;
     }
 
     public URL findLocalResource(String resourceName)
