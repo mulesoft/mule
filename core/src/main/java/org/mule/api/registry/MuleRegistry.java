2c826037d038074095250d79f6894afc77c31444
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2015-03-05 12:32:04 -0300

    rename initialisingRegistry

--- core/src/main/java/org/mule/api/registry/MuleRegistry.java
+++ core/src/main/java/org/mule/api/registry/MuleRegistry.java
@@ -30,7 +30,7 @@ import java.util.Properties;
  * Adds lookup/register/unregister methods for Mule-specific entities to the standard
  * Registry interface.
  */
-public interface MuleRegistry extends InitialisingRegistry
+public interface MuleRegistry extends LifecycleRegistry
 {
 
     /**
