9463ea65cf100fadf96deb974ed06349c6e135be
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2015-03-06 17:35:19 -0300

    MULE-8303: ClassCastException when setting a MEL expression in the config-ref of a Connector's call

--- modules/devkit-support/src/test/java/org/mule/devkit/processor/DevkitBasedMessageProcessorTest.java
+++ modules/devkit-support/src/test/java/org/mule/devkit/processor/DevkitBasedMessageProcessorTest.java
@@ -7,16 +7,63 @@
 
 package org.mule.devkit.processor;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.mule.api.MuleContext;
 import org.mule.api.MuleEvent;
 import org.mule.api.MuleException;
+import org.mule.common.security.oauth.exception.NotAuthorizedException;
+import org.mule.security.oauth.OnNoTokenPolicy;
+import org.mule.security.oauth.OnNoTokenPolicyAware;
+import org.mule.tck.junit4.AbstractMuleTestCase;
 import org.mule.tck.size.SmallTest;
 
 import org.junit.Test;
 
 @SmallTest
-public class DevkitBasedMessageProcessorTest
+public class DevkitBasedMessageProcessorTest extends AbstractMuleTestCase
 {
+
+    @Test
+    public void handlesNotAuthorizedExceptionWithFixedModule() throws Exception
+    {
+        OnNoTokenPolicyAware policyAware = mock(OnNoTokenPolicyAware.class);
+        when(policyAware.getOnNoTokenPolicy()).thenReturn(OnNoTokenPolicy.STOP_FLOW);
+
+        DevkitBasedMessageProcessor processor = new UnauthorizedProcessor("forbidden");
+        processor.setModuleObject(policyAware);
+
+        MuleEvent event = mock(MuleEvent.class, RETURNS_DEEP_STUBS);
+        MuleEvent responseEvent = processor.process(event);
+
+        assertThat("event should be filtered because STOP_FLOW was used", responseEvent, is(nullValue()));
+    }
+
+    @Test
+    public void handlesNotAuthorizedExceptionWithExpressionModule() throws Exception
+    {
+        final String expression = "#[policyAware]";
+
+        OnNoTokenPolicyAware policyAware = mock(OnNoTokenPolicyAware.class);
+        when(policyAware.getOnNoTokenPolicy()).thenReturn(OnNoTokenPolicy.STOP_FLOW);
+
+        MuleEvent event = mock(MuleEvent.class, RETURNS_DEEP_STUBS);
+        MuleContext muleContext = mock(MuleContext.class, RETURNS_DEEP_STUBS);
+        when(muleContext.getExpressionManager().evaluate(expression, event, true)).thenReturn(policyAware);
+
+        DevkitBasedMessageProcessor processor = new UnauthorizedProcessor("forbidden");
+        processor.setMuleContext(muleContext);
+        processor.setModuleObject(expression);
+
+        MuleEvent responseEvent = processor.process(event);
+
+        assertThat("event should be filtered because STOP_FLOW was used", responseEvent, is(nullValue()));
+    }
 
     @Test
     public void testThatProcessMethodCanBeOverridden() throws MuleException
@@ -26,6 +73,20 @@ public class DevkitBasedMessageProcessorTest
         connectorMessageProcessor.process(null);
 
         assertTrue(connectorMessageProcessor.wasCalled);
+    }
+
+    private class UnauthorizedProcessor extends DevkitBasedMessageProcessor {
+
+        public UnauthorizedProcessor(String operationName)
+        {
+            super(operationName);
+        }
+
+        @Override
+        protected MuleEvent doProcess(MuleEvent event) throws Exception
+        {
+            throw new NotAuthorizedException("unauthorized");
+        }
     }
 
     private static class DevkitMessageProcessorProxy extends DevkitBasedMessageProcessor
