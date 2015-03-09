fad51e812620a1489da5e028e9bb602a173ed00c
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2015-03-09 17:52:43 -0300

    MULE-8370: ObjectAlreadyExistsException when using splitter and until-successful

--- core/src/main/java/org/mule/routing/AsynchronousUntilSuccessfulProcessingStrategy.java
+++ core/src/main/java/org/mule/routing/AsynchronousUntilSuccessfulProcessingStrategy.java
@@ -31,6 +31,7 @@ import org.mule.util.store.QueuePersistenceObjectStore;
 
 import java.io.NotSerializableException;
 import java.io.Serializable;
+import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -52,6 +53,7 @@ import org.apache.commons.logging.LogFactory;
 public class AsynchronousUntilSuccessfulProcessingStrategy extends AbstractUntilSuccessfulProcessingStrategy implements Initialisable, Startable, Stoppable, MessagingExceptionHandlerAware
 {
 
+    private static final Random random = new Random();
     protected transient Log logger = LogFactory.getLog(getClass());
     private MessagingExceptionHandler messagingExceptionHandler;
     private ScheduledExecutorService scheduledPool;
@@ -215,8 +217,11 @@ public class AsynchronousUntilSuccessfulProcessingStrategy extends AbstractUntil
         // the key is built in way to prevent UntilSuccessful workers across a
         // cluster to compete for the same
         // events over a shared object store
-        String key = muleEvent.getFlowConstruct() + "-" + muleEvent.getMuleContext().getClusterId() + "-"
-                     + muleEvent.getId();
+        // it also adds a random trailer to support events which have been
+        // splitted and thus have the same id. Random number was chosen over
+        // UUID for performance reasons
+        String key = String.format("%s-%s-%s-%d", muleEvent.getFlowConstruct(), muleEvent.getMuleContext().getClusterId(), muleEvent.getId(), random.nextInt());
+
         return new QueueKey(QueuePersistenceObjectStore.DEFAULT_QUEUE_STORE, key);
     }
 
