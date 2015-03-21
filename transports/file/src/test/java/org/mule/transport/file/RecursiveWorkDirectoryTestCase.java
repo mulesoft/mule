f7b97f8cd088db39459646ed4b96c59555713433
pablo.kraan <pablo.kraan@gmail.com>
2015-03-20 22:27:06 -0300

    MULE-8420: FileMessageDispatcher return subfolders when no files found
    _ Fixing build

--- transports/file/src/test/java/org/mule/transport/file/RecursiveWorkDirectoryTestCase.java
+++ transports/file/src/test/java/org/mule/transport/file/RecursiveWorkDirectoryTestCase.java
@@ -7,8 +7,8 @@
 
 package org.mule.transport.file;
 
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 import org.mule.api.MuleMessage;
 import org.mule.api.client.MuleClient;
@@ -24,9 +24,8 @@ public class RecursiveWorkDirectoryTestCase extends FunctionalTestCase
     @Rule
     public SystemPropertyTemporaryFolder temporaryFolder = new SystemPropertyTemporaryFolder("temp");
 
-
     @Override
-    protected String getConfigFile()
+    protected String getConfigResources()
     {
         return  "recursive-work-directory-config.xml";
     }
