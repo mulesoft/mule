36e20d5df35afcc43d4845f726d9a596e8b7a55e
pablo.kraan <pablo.kraan@gmail.com>
2015-03-31 09:46:06 -0300

    MULE-8441: Add a way to inject all available core extensions in a core extension

---
+++ core/src/main/java/org/mule/CoreExtensionsAware.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule;
+
+import java.util.List;
+
+/**
+ * Enables injection of all available {@link MuleCoreExtension}.
+ * <p/>
+ * Classes implementing this interface cannot contain methods annotated with
+ * {@link MuleCoreExtensionDependency} as that will create an unresolvable dependency on runtime.
+ */
+public interface CoreExtensionsAware
+{
+
+    void setCoreExtensions(List<MuleCoreExtension> coreExtensions);
+}
