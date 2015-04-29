639f11fc38f95d793e90722efdcbfe14fb3174d3
pablo.kraan <pablo.kraan@gmail.com>
2015-04-29 10:30:47 -0300

    MULE-6926: Fix flaky tests

--- tests/integration/src/test/java/org/mule/test/integration/messaging/meps/InOptionalOutOutOnlyJMSTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/messaging/meps/InOptionalOutOutOnlyJMSTestCase.java
@@ -67,7 +67,6 @@ public class InOptionalOutOutOnlyJMSTestCase extends AbstractServiceAndFlowTestC
     }
 
     @Test
-    @Ignore("MULE-6926: Flaky test")
     public void testExchange() throws Exception
     {
         MuleClient client = muleContext.getClient();
