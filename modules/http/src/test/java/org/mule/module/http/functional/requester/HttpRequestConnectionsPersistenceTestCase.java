ff89948f1b678076a218d94d0a7741e33797393f
afelisatti <afelisatti@mulesoft.com>
2015-05-07 16:58:38 -0300

    MULE-8583: Upgrading async-http-client to 1.9.21.

--- modules/http/src/test/java/org/mule/module/http/functional/requester/HttpRequestConnectionsPersistenceTestCase.java
+++ modules/http/src/test/java/org/mule/module/http/functional/requester/HttpRequestConnectionsPersistenceTestCase.java
@@ -25,7 +25,8 @@ import org.junit.Test;
 
 public class HttpRequestConnectionsPersistenceTestCase extends AbstractHttpRequestTestCase
 {
-
+    private static final int GRIZZLY_IDLE_CHECK_TIMEOUT_MILLIS = 6000;
+    private static final int POLL_DELAY_MILLIS = 200;
     private int remotePort;
 
     @Override
@@ -41,7 +42,7 @@ public class HttpRequestConnectionsPersistenceTestCase extends AbstractHttpReque
         flow.process(getTestEvent(TEST_MESSAGE));
         ensureConnectionIsOpen();
 
-        new PollingProber().check(new JUnitProbe()
+        new PollingProber(GRIZZLY_IDLE_CHECK_TIMEOUT_MILLIS, POLL_DELAY_MILLIS).check(new JUnitProbe()
         {
             @Override
             public boolean test() throws Exception
