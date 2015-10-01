ca74323b9c48624d27c41bbc3424af2c9228991f
pablo.kraan <pablo.kraan@gmail.com>
2015-10-01 12:48:11 -0300

    MULE-8974: DatasourceFactory must dispose disposable datasources

--- modules/db/src/test/java/org/mule/module/db/internal/domain/database/DataSourceFactoryTestCase.java
+++ modules/db/src/test/java/org/mule/module/db/internal/domain/database/DataSourceFactoryTestCase.java
@@ -11,6 +11,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import org.mule.api.MuleContext;
+import org.mule.api.lifecycle.Disposable;
 import org.mule.module.db.internal.domain.connection.DbPoolingProfile;
 import org.mule.tck.junit4.AbstractMuleTestCase;
 import org.mule.tck.size.SmallTest;
@@ -28,6 +29,7 @@ public class DataSourceFactoryTestCase extends AbstractMuleTestCase
 {
 
     private final PooledDataSource dataSource = mock(PooledDataSource.class);
+    private final DisposableDataSource disposableDataSource = mock(DisposableDataSource.class);
     private final DataSourceFactory dataSourceFactory = new DataSourceFactory("test")
     {
         @Override
@@ -39,25 +41,28 @@ public class DataSourceFactoryTestCase extends AbstractMuleTestCase
         @Override
         public DataSource decorateDataSource(DataSource dataSource, DbPoolingProfile poolingProfile, MuleContext muleContext)
         {
-            return dataSource;
+            return disposableDataSource;
         }
     };
+
+    public interface DisposableDataSource extends DataSource, Disposable
+    {
+    }
 
     @Test
     public void destroysPooledDataSource() throws Exception
     {
-
         DataSourceConfig dataSourceConfig = new DataSourceConfig();
         dataSourceConfig.setPoolingProfile(mock(DbPoolingProfile.class));
 
-        doDisposeTest(dataSourceConfig, 1);
+        doCloseTest(dataSourceConfig, 1);
     }
 
     @Test
     public void doesNotDestroySingleDataSource() throws Exception
     {
         DataSourceConfig dataSourceConfig = new DataSourceConfig();
-        doDisposeTest(dataSourceConfig, 0);
+        doCloseTest(dataSourceConfig, 0);
     }
 
     @Test
@@ -65,7 +70,7 @@ public class DataSourceFactoryTestCase extends AbstractMuleTestCase
     {
         DataSourceConfig dataSourceConfig = new DataSourceConfig();
         dataSourceConfig.setUseXaTransactions(true);
-        doDisposeTest(dataSourceConfig, 0);
+        doCloseTest(dataSourceConfig, 0);
     }
 
     @Test
@@ -74,15 +79,65 @@ public class DataSourceFactoryTestCase extends AbstractMuleTestCase
         DataSourceConfig dataSourceConfig = new DataSourceConfig();
         dataSourceConfig.setUseXaTransactions(true);
         dataSourceConfig.setPoolingProfile(mock(DbPoolingProfile.class));
+        doCloseTest(dataSourceConfig, 0);
+    }
+
+    @Test
+    public void doesNotDisposesPooledDataSource() throws Exception
+    {
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setPoolingProfile(mock(DbPoolingProfile.class));
         doDisposeTest(dataSourceConfig, 0);
+    }
+
+    @Test
+    public void doesNotDisposesSingleDataSource() throws Exception
+    {
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        doDisposeTest(dataSourceConfig, 0);
+    }
+
+    @Test
+    public void disposesSingleXaDataSource() throws Exception
+    {
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setUseXaTransactions(true);
+        doDisposeTest(dataSourceConfig, 1);
+    }
+
+    @Test
+    public void disposesPooledXaDataSource() throws Exception
+    {
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setUseXaTransactions(true);
+        dataSourceConfig.setPoolingProfile(mock(DbPoolingProfile.class));
+        doDisposeTest(dataSourceConfig, 1);
+    }
+
+    @Test
+    public void disposesDisposableDatasource() throws Exception
+    {
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setUseXaTransactions(true);
+
+        doDisposeTest(dataSourceConfig, 1);
+    }
+
+    private void doCloseTest(DataSourceConfig dataSourceConfig, int expectedCloseInvocations) throws SQLException
+    {
+        configureFactory(dataSourceConfig);
+        verify(dataSource, times(expectedCloseInvocations)).close(false);
     }
 
     private void doDisposeTest(DataSourceConfig dataSourceConfig, int expectedDisposeInvocations) throws SQLException
     {
+        configureFactory(dataSourceConfig);
+        verify(disposableDataSource, times(expectedDisposeInvocations)).dispose();
+    }
+
+    private void configureFactory(DataSourceConfig dataSourceConfig) throws SQLException
+    {
         dataSourceFactory.create(dataSourceConfig);
-
         dataSourceFactory.dispose();
-
-        verify(dataSource, times(expectedDisposeInvocations)).close(false);
     }
 }
