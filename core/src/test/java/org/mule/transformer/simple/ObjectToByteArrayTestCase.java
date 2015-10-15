0f24e7c04291b54754d0798f3f4694ec1083f260
andres.gregoire <andres.gregoire@mulesoft.com>
2015-10-15 11:39:32 -0300

    MULE-9006: XmlToDomDocument transformer conflicts with ObjectToByteArray transformer
    Changed default priorityWeighting of the SerializableToByteArray transformer (and ObjectToByteArray that extends it) to be consistent with the rest of the core transformers.

---
+++ core/src/test/java/org/mule/transformer/simple/ObjectToByteArrayTestCase.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.transformer.simple;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mule.api.transformer.Converter.DEFAULT_PRIORITY_WEIGHTING;
+import org.mule.tck.junit4.AbstractMuleTestCase;
+import org.mule.tck.size.SmallTest;
+
+import org.junit.Test;
+
+/**
+ * This unit test only tests the default priority of the {@link org.mule.transformer.simple.ObjectToByteArray} transformer.
+ * Actual transformation logic is tested in the {@link org.mule.transformer.simple.ObjectByteArrayTransformersWithObjectsTestCase}
+ * test and its subclasses.
+ */
+@SmallTest
+public class ObjectToByteArrayTestCase extends AbstractMuleTestCase
+{
+
+    @Test
+    public void transformerHasHigherDefaultPriority() throws Exception
+    {
+        ObjectToByteArray transformer = new ObjectToByteArray();
+        assertThat(transformer.getPriorityWeighting(), equalTo(DEFAULT_PRIORITY_WEIGHTING + 1));
+    }
+}
