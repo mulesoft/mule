3cdd53d58eb3f5abcf03fc6185997c85fd417d65
Rodrigo Merino <rodrigo.merino@mulesoft.com>
2015-11-05 17:44:43 -0300

    MULE-9040: Database Connector: "No suitable driver found" on redeploy

--- modules/launcher/src/main/java/org/mule/module/launcher/artifact/DefaultResourceReleaser.java
+++ modules/launcher/src/main/java/org/mule/module/launcher/artifact/DefaultResourceReleaser.java
@@ -6,9 +6,13 @@
  */
 package org.mule.module.launcher.artifact;
 
-import java.lang.management.ManagementFactory;
+import static java.lang.Integer.toHexString;
+import static java.lang.String.format;
+import static java.lang.management.ManagementFactory.getPlatformMBeanServer;
+import static java.sql.DriverManager.deregisterDriver;
+import static java.sql.DriverManager.getDrivers;
+
 import java.sql.Driver;
-import java.sql.DriverManager;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
@@ -32,24 +36,61 @@ public class DefaultResourceReleaser implements ResourceReleaser
 
     private void deregisterJdbcDrivers()
     {
-        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        Enumeration<Driver> drivers = getDrivers();
         while (drivers.hasMoreElements())
         {
             Driver driver = drivers.nextElement();
-            try
-            {
-                logger.debug("Deregistering driver: {}", driver.getClass());
-                DriverManager.deregisterDriver(driver);
 
-                if (isOracleDriver(driver))
+            // Only unregister drivers that were loaded by the classloader that called this releaser.
+            if (isDriverLoadedByThisClassLoader(driver))
+            {
+                doDeregisterDriver(driver);
+            }
+            else
+            {
+                if (logger.isDebugEnabled())
                 {
-                    deregisterOracleDiagnosabilityMBean();
+                    logger.debug(format("Skipping deregister driver %s. It wasn't loaded by the classloader of the artifact being released.", driver.getClass()));
                 }
             }
-            catch (Exception e)
+        }
+    }
+
+    /**
+     * @param driver the JDBC driver to check its {@link ClassLoader} for.
+     * @return {@code true} if the {@link ClassLoader} of the driver is a descendant of the {@link ClassLoader} of this
+     *         releaser, {@code false} otherwise.
+     */
+    private boolean isDriverLoadedByThisClassLoader(Driver driver)
+    {
+        ClassLoader driverClassLoader = driver.getClass().getClassLoader();
+        while (driverClassLoader != null)
+        {
+            if (driverClassLoader.equals(getClass().getClassLoader()))
             {
-                logger.warn(String.format("Can not deregister driver %s. This can cause a memory leak.", driver.getClass()), e);
+                return true;
             }
+            driverClassLoader = driverClassLoader.getParent();
+        }
+
+        return false;
+    }
+
+    private void doDeregisterDriver(Driver driver)
+    {
+        try
+        {
+            logger.debug("Deregistering driver: {}", driver.getClass());
+            deregisterDriver(driver);
+
+            if (isOracleDriver(driver))
+            {
+                deregisterOracleDiagnosabilityMBean();
+            }
+        }
+        catch (Exception e)
+        {
+            logger.warn(format("Can not deregister driver %s. This can cause a memory leak.", driver.getClass()), e);
         }
     }
 
@@ -61,10 +102,10 @@ public class DefaultResourceReleaser implements ResourceReleaser
     private void deregisterOracleDiagnosabilityMBean()
     {
         final ClassLoader cl = this.getClass().getClassLoader();
-        final MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        final MBeanServer mBeanServer = getPlatformMBeanServer();
         final Hashtable<String, String> keys = new Hashtable<String, String>();
         keys.put("type", DIAGNOSABILITY_BEAN_NAME);
-        keys.put("name", cl.getClass().getName() + "@" + Integer.toHexString(cl.hashCode()).toLowerCase());
+        keys.put("name", cl.getClass().getName() + "@" + toHexString(cl.hashCode()).toLowerCase());
 
         try
         {
@@ -74,7 +115,7 @@ public class DefaultResourceReleaser implements ResourceReleaser
         {
             if (logger.isDebugEnabled())
             {
-                logger.debug(String.format("No Oracle's '%s' MBean found.", DIAGNOSABILITY_BEAN_NAME));
+                logger.debug(format("No Oracle's '%s' MBean found.", DIAGNOSABILITY_BEAN_NAME));
             }
         }
         catch (Throwable e)
