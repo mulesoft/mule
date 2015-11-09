145452b5e43c8d8ea4884533e58519b7887713bb
afelisatti <afelisatti@mulesoft.com>
2015-11-09 11:16:16 -0300

    MULE-9044: HTTP Listener returning 500 instead of 400 on invalid Content-Type

--- modules/http/src/test/java/org/mule/module/http/functional/listener/HttpListenerContentTypeTestCase.java
+++ modules/http/src/test/java/org/mule/module/http/functional/listener/HttpListenerContentTypeTestCase.java
@@ -8,15 +8,26 @@
 package org.mule.module.http.functional.listener;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.equalToIgnoringCase;
 import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.mule.module.http.api.HttpConstants.HttpStatus.BAD_REQUEST;
+import static org.mule.module.http.api.HttpHeaders.Names.CONTENT_TYPE;
 import org.mule.api.MuleMessage;
 import org.mule.api.client.LocalMuleClient;
 import org.mule.api.config.MuleProperties;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.util.IOUtils;
 
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.entity.StringEntity;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -38,11 +49,38 @@ public class HttpListenerContentTypeTestCase extends FunctionalTestCase
     public void returnsContentTypeInResponse() throws Exception
     {
         LocalMuleClient client = muleContext.getClient();
-        final String url = String.format("http://localhost:%s/testInput", httpPort.getNumber());
 
-        MuleMessage response = client.send(url, TEST_MESSAGE, null);
+        MuleMessage response = client.send(getUrl(), TEST_MESSAGE, null);
 
         assertContentTypeProperty(response);
+    }
+
+    @Test
+    public void rejectsInvalidContentTypeWithoutBody() throws Exception
+    {
+        Request request = Request.Post(getUrl()).addHeader(CONTENT_TYPE, "application");
+        testRejectContentType(request, "Invalid Content-Type");
+    }
+
+    @Test
+    public void rejectsInvalidContentTypeWithBody() throws Exception
+    {
+        Request request = Request.Post(getUrl()).body(new StringEntity(TEST_MESSAGE, "application", null));
+        testRejectContentType(request, "Could not parse");
+    }
+
+    private void testRejectContentType(Request request, String expectedMessage) throws IOException
+    {
+        HttpResponse response = request.execute().returnResponse();
+        StatusLine statusLine = response.getStatusLine();
+
+        assertThat(IOUtils.toString(response.getEntity().getContent()), containsString(expectedMessage));
+        assertThat(statusLine.getStatusCode(), is(BAD_REQUEST.getStatusCode()));
+    }
+
+    private String getUrl()
+    {
+        return String.format("http://localhost:%s/testInput", httpPort.getValue());
     }
 
     private void assertContentTypeProperty(MuleMessage response)
