05b1294c204e3a7ea3a5221404ae4bf648a4d043
Rodrigo Merino <rodrigo.merino@mulesoft.com>
2015-11-24 14:18:24 -0300

    MULE-9146: AsyncUntilSuccessful uses a fixed thread pool

--- core/src/main/java/org/mule/routing/AsynchronousUntilSuccessfulProcessingStrategy.java
+++ core/src/main/java/org/mule/routing/AsynchronousUntilSuccessfulProcessingStrategy.java
@@ -35,6 +35,7 @@ import org.mule.util.store.QueuePersistenceObjectStore;
 import java.io.Serializable;
 import java.util.Random;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -59,7 +60,8 @@ public class AsynchronousUntilSuccessfulProcessingStrategy extends AbstractUntil
     private static final Random random = new Random();
     protected transient Log logger = LogFactory.getLog(getClass());
     private MessagingExceptionHandler messagingExceptionHandler;
-    private ScheduledExecutorService scheduledPool;
+    private ExecutorService pool;
+    private ScheduledExecutorService scheduledRetriesPool;
 
     @Override
     public void initialise() throws InitialisationException
@@ -77,16 +79,19 @@ public class AsynchronousUntilSuccessfulProcessingStrategy extends AbstractUntil
     {
         final String threadPrefix = String.format("%s%s.%s", ThreadNameHelper.getPrefix(getUntilSuccessfulConfiguration().getMuleContext()),
                                                   getUntilSuccessfulConfiguration().getFlowConstruct().getName(), "until-successful");
-        scheduledPool = getUntilSuccessfulConfiguration().getThreadingProfile().createScheduledPool(threadPrefix);
+        pool = getUntilSuccessfulConfiguration().getThreadingProfile().createPool(threadPrefix);
+        scheduledRetriesPool = getUntilSuccessfulConfiguration().createScheduledRetriesPool(threadPrefix);
+
         scheduleAllPendingEventsForProcessing();
     }
-
 
     @Override
     public void stop()
     {
-        scheduledPool.shutdown();
-        scheduledPool = null;
+        scheduledRetriesPool.shutdown();
+        scheduledRetriesPool = null;
+        pool.shutdown();
+        pool = null;
     }
 
     @Override
@@ -136,12 +141,32 @@ public class AsynchronousUntilSuccessfulProcessingStrategy extends AbstractUntil
         }
     }
 
-    private void scheduleForProcessing(final Serializable eventStoreKey, boolean firstTime) throws Exception
+    private void scheduleForProcessing(final Serializable eventStoreKey, boolean firstTime)
     {
-        this.scheduledPool.schedule(new Callable<Object>()
+        if (firstTime)
+        {
+            submitForProcessing(eventStoreKey);
+        }
+        else
+        {
+            this.scheduledRetriesPool.schedule(new Callable<Object>()
+            {
+                @Override
+                public Object call() throws Exception
+                {
+                    submitForProcessing(eventStoreKey);
+                    return null;
+                }
+            }, getUntilSuccessfulConfiguration().getMillisBetweenRetries(), TimeUnit.MILLISECONDS);
+        }
+    }
+
+    protected void submitForProcessing(final Serializable eventStoreKey)
+    {
+        this.pool.execute(new Runnable()
         {
             @Override
-            public Object call() throws Exception
+            public void run()
             {
                 try
                 {
@@ -151,12 +176,11 @@ public class AsynchronousUntilSuccessfulProcessingStrategy extends AbstractUntil
                 {
                     incrementProcessAttemptCountAndRescheduleOrRemoveFromStore(eventStoreKey, e);
                 }
-                return null;
             }
-        }, firstTime ? 0 : getUntilSuccessfulConfiguration().getMillisBetweenRetries(), TimeUnit.MILLISECONDS);
+        });
     }
 
-    private void incrementProcessAttemptCountAndRescheduleOrRemoveFromStore(final Serializable eventStoreKey, Exception lastException) throws Exception
+    private void incrementProcessAttemptCountAndRescheduleOrRemoveFromStore(final Serializable eventStoreKey, Exception lastException)
     {
         try
         {
