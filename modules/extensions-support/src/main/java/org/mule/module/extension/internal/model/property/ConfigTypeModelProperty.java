a180019c15dd629ff05c731787bd8dc7da8f9c9c
estebanwasinger <estebanwasinger@gmail.com>
2015-12-18 15:11:05 -0300

    MULE-9214: Add missing syntax validators for annotation driven extensions

---
+++ modules/extensions-support/src/main/java/org/mule/module/extension/internal/model/property/ConfigTypeModelProperty.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.extension.internal.model.property;
+
+
+import org.mule.extension.api.introspection.EnrichableModel;
+
+/**
+ * An immutable model property which specifies that the owning {@link EnrichableModel}
+ * requires a configuration of a given {@link #configType}
+ *
+ * @since 4.0
+ */
+public final class ConfigTypeModelProperty
+{
+
+    /**
+     * A unique key that identifies this property type
+     */
+    public static final String KEY = ConfigTypeModelProperty.class.getName();
+
+    private final Class<?> configType;
+
+    /**
+     * Creates a new instance for the given {@code configType}
+     *
+     * @param configType
+     */
+    public ConfigTypeModelProperty(Class<?> configType)
+    {
+        this.configType = configType;
+    }
+
+    /**
+     * @return the {@link {@link #configType }}
+     */
+    public Class<?> getConfigType()
+    {
+        return configType;
+    }
+}
