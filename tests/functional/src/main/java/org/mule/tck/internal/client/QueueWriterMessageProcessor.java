78e4e947bfae23aabcbd539fb550f2e2d6f851d7
pablo.kraan <pablo.kraan@gmail.com>
2015-12-23 11:25:07 -0300

    MULE-9231: Create a queue test connector
    _ Extracting abstract classes for the connector's message processor providers
    _ Updating DefaultLocalMuleClient to manage connectors in request operation
    _ Adding test:queue element to write events to a queue

---
+++ tests/functional/src/main/java/org/mule/tck/internal/client/QueueWriterMessageProcessor.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.tck.internal.client;
+
+import static org.mule.tck.internal.client.TestConnectorConfig.DEFAULT_CONFIG_ID;
+import org.mule.DefaultMuleEvent;
+import org.mule.api.MuleContext;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.api.context.MuleContextAware;
+import org.mule.api.processor.MessageProcessor;
+
+/**
+ * Writes {@link MuleEvent} to a test connector's queue.
+ */
+public class QueueWriterMessageProcessor implements MessageProcessor, MuleContextAware
+{
+
+    private MuleContext muleContext;
+    private String name;
+
+    @Override
+    public MuleEvent process(MuleEvent event) throws MuleException
+    {
+        TestConnectorConfig connectorConfig = muleContext.getRegistry().lookupObject(DEFAULT_CONFIG_ID);
+        connectorConfig.write(name, DefaultMuleEvent.copy(event));
+
+        return event;
+    }
+
+    @Override
+    public void setMuleContext(MuleContext context)
+    {
+        muleContext = context;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
