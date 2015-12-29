25dd949c1d048af9bdadfae7a0643ed1f989dd08
Guillermo Fernandes <guillermo.fernandes@mulesoft.com>
2015-12-29 18:05:14 -0300

    MULE-9244 - Changed HttpResponseBuilder to set the reason phrase if it has been set to the message outbound property

--- modules/http/src/test/java/org/mule/module/http/functional/listener/HttpListenerResponseBuilderTestCase.java
+++ modules/http/src/test/java/org/mule/module/http/functional/listener/HttpListenerResponseBuilderTestCase.java
@@ -40,7 +40,11 @@ public class HttpListenerResponseBuilderTestCase extends FunctionalTestCase
     @Rule
     public SystemProperty statusResponseBuilderPath = new SystemProperty("statusResponseBuilderPath","statusResponseBuilderPath");
     @Rule
-    public SystemProperty statusResponseBuilderOverridePath = new SystemProperty("statusResponseBuilderOverridePath","statusResponseBuilderOverridePath");
+    public SystemProperty statusResponseBuilderOverridePath = new SystemProperty("statusResponseBuilderOverridePath", "statusResponseBuilderOverridePath");
+    @Rule
+    public SystemProperty reasonPhraseResponseBuilderOverridePath = new SystemProperty("reasonPhraseResponseBuilderOverridePath", "reasonPhraseResponseBuilderOverridePath");
+    @Rule
+    public SystemProperty statusReasonPhraseResponseBuilderWontOverridePath = new SystemProperty("statusReasonPhraseResponseBuilderWontOverridePath", "statusReasonPhraseResponseBuilderWontOverridePath");
     @Rule
     public SystemProperty headerResponseBuilderPath = new SystemProperty("headerResponseBuilderPath","headerResponseBuilderPath");
     @Rule
@@ -85,6 +89,20 @@ public class HttpListenerResponseBuilderTestCase extends FunctionalTestCase
     {
         final String url = String.format("http://localhost:%s/%s", listenPort.getNumber(), statusResponseBuilderOverridePath.getValue());
         statusLineResponseBuilderTest(url, 202);
+    }
+
+    @Test
+    public void reasonPhraseResponseBuilderOverrideResponseBuilder() throws Exception
+    {
+        final String url = String.format("http://localhost:%s/%s", listenPort.getNumber(), reasonPhraseResponseBuilderOverridePath.getValue());
+        statusLineResponseBuilderTest(url, 200, "response success!");
+    }
+
+    @Test
+    public void statusReasonPhraseResponseBuilderWontOverrideResponseBuilder() throws Exception
+    {
+        final String url = String.format("http://localhost:%s/%s", listenPort.getNumber(), statusReasonPhraseResponseBuilderWontOverridePath.getValue());
+        statusLineResponseBuilderTest(url, 200, "OK");
     }
 
     @Test
@@ -154,14 +172,19 @@ public class HttpListenerResponseBuilderTestCase extends FunctionalTestCase
     }
 
 
-    private void statusLineResponseBuilderTest(String url, int expectedStatus) throws IOException
+    private void statusLineResponseBuilderTest(String url, int expectedStatus, String expectedReasonPhrase) throws IOException
     {
         final Response response = Request.Get(url).connectTimeout(1000).execute();
         final HttpResponse httpResponse = response.returnResponse();
         assertThat(httpResponse.getAllHeaders().length, is(2));
         assertThat(httpResponse.getFirstHeader(HttpHeaders.Names.CONTENT_LENGTH).getValue(), is("0"));
         assertThat(httpResponse.getStatusLine().getStatusCode(), is(expectedStatus));
-        assertThat(httpResponse.getStatusLine().getReasonPhrase(), is("everything works!"));
+        assertThat(httpResponse.getStatusLine().getReasonPhrase(), is(expectedReasonPhrase));
+    }
+
+    private void statusLineResponseBuilderTest(String url, int expectedStatus) throws IOException
+    {
+        statusLineResponseBuilderTest(url, expectedStatus, "everything works!");
     }
 
     private void emptyResponseBuilderTest(String url) throws IOException
