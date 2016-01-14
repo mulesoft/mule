c465398dfe8ddfdbfa351a1b1a64485a3e68b2b7
Rodrigo Merino <rodrigo.merino@mulesoft.com>
2016-01-14 14:19:45 -0300

    MULE-9323: WS consumer fails when reading secured WSDL

--- modules/ws/src/main/java/org/mule/module/ws/consumer/WSConsumer.java
+++ modules/ws/src/main/java/org/mule/module/ws/consumer/WSConsumer.java
@@ -32,9 +32,12 @@ import org.mule.module.ws.security.WSSecurity;
 import org.mule.processor.AbstractInterceptingMessageProcessor;
 import org.mule.processor.chain.DefaultMessageProcessorChainBuilder;
 import org.mule.transport.http.HttpConnector;
+import org.mule.util.Base64;
 import org.mule.util.IOUtils;
 
+import java.io.IOException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -63,6 +66,7 @@ import org.apache.cxf.message.Message;
 import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.InputSource;
 
 
 public class WSConsumer implements MessageProcessor, Initialisable, MuleContextAware, Disposable
@@ -308,9 +312,16 @@ public class WSConsumer implements MessageProcessor, Initialisable, MuleContextA
         try
         {
             WSDLReader wsdlReader = WSDLFactory.newInstance().newWSDLReader();
-            wsdlDefinition = wsdlReader.readWSDL(url.toString());
+
+            URLConnection urlConnection = url.openConnection();
+            if (url.getUserInfo() != null)
+            {
+                urlConnection.setRequestProperty("Authorization", "Basic " + Base64.encodeBytes(url.getUserInfo().getBytes()));
+            }
+
+            wsdlDefinition = wsdlReader.readWSDL(url.toString(), new InputSource(urlConnection.getInputStream()));
         }
-        catch (WSDLException e)
+        catch (WSDLException | IOException e)
         {
             throw new InitialisationException(e, this);
         }
