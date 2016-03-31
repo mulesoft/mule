275f360f60c6888f6dba2dfcbf6a3913ebb5f278
Daniel Feist <dfeist@gmail.com>
2016-03-31 12:29:55 +0100

    MULE-9533 JMS Outbound endpoint does not respect response timeout

--- transports/jms/src/test/java/org/mule/transport/jms/integration/JmsTemporaryReplyToTestCase.java
+++ transports/jms/src/test/java/org/mule/transport/jms/integration/JmsTemporaryReplyToTestCase.java
@@ -6,13 +6,15 @@
  */
 package org.mule.transport.jms.integration;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertEquals;
-
-import org.mule.api.MuleException;
+import static org.junit.Assert.assertThat;
 import org.mule.api.MuleMessage;
-import org.mule.api.client.MuleClient;
 import org.mule.transport.NullPayload;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 /**
@@ -22,6 +24,9 @@ import org.junit.Test;
  */
 public class JmsTemporaryReplyToTestCase extends AbstractJmsFunctionalTestCase
 {
+
+    private static String ECHO_FLOW_NAME = "EchoFlow";
+
     @Override
     protected String getConfigFile()
     {
@@ -29,36 +34,63 @@ public class JmsTemporaryReplyToTestCase extends AbstractJmsFunctionalTestCase
     }
 
     @Test
-    public void testTemporaryReplyEnabledSync() throws MuleException
+    public void testReplyEnabledSync() throws Exception
     {
-        MuleClient muleClient = muleContext.getClient();
-        MuleMessage response = muleClient.send("vm://in1Sync", TEST_MESSAGE, null);
-        assertEquals(TEST_MESSAGE + " TestService1", response.getPayload());
+        MuleMessage response = runFlow("JMSService1SyncFixed", TEST_MESSAGE).getMessage();
+        assertEchoResponse(response);
     }
 
     @Test
-    public void testTemporaryReplyDisabledSync() throws MuleException
+    public void testReplyEnabledSyncTimeout() throws Exception
     {
-        MuleClient muleClient = muleContext.getClient();
-        MuleMessage response = muleClient.send("vm://in2Sync", TEST_MESSAGE, null);
+        MuleMessage response = runFlow("JMSService1SyncTimeoutFixed", TEST_MESSAGE).getMessage();
+        assertNullPayloadResponse(response);
+    }
+
+    @Test
+    public void testTemporaryReplyEnabledSync() throws Exception
+    {
+        MuleMessage response = runFlow("JMSService1Sync", TEST_MESSAGE).getMessage();
+        assertEchoResponse(response);
+    }
+
+    @Test
+    public void testTemporaryReplyEnabledSyncTimeout() throws Exception
+    {
+        MuleMessage response = runFlow("JMSService1SyncTimeout", TEST_MESSAGE).getMessage();
+        assertNullPayloadResponse(response);
+    }
+
+    @Test
+    public void testTemporaryReplyDisabledSync() throws Exception
+    {
+        // TODO This behaviour appears inconsistent.  NullPayload would be more appropriate.
+        assertThat(runFlow("JMSService2Sync"), is(nullValue()));
+    }
+
+    @Test
+    public void testDisableTemporaryReplyOnTheConnector() throws Exception
+    {
+        MuleMessage response = runFlow("JMSService3", TEST_MESSAGE).getMessage();
         assertEquals(TEST_MESSAGE, response.getPayload());
     }
 
     @Test
-    public void testDisableTemporaryReplyOnTheConnector() throws MuleException
+    public void testExplicitReplyToAsyncSet() throws Exception
     {
-        MuleClient muleClient = muleContext.getClient();
-        MuleMessage response = muleClient.send("vm://in3", TEST_MESSAGE, null);
-
-        assertEquals(NullPayload.getInstance(), response.getPayload());
-    }
-
-    @Test
-    public void testExplicitReplyToAsyncSet() throws MuleException
-    {
-        MuleClient muleClient = muleContext.getClient();
-        MuleMessage response = muleClient.send("vm://in4", TEST_MESSAGE, null);
+        MuleMessage response = runFlow("JMSService4", TEST_MESSAGE).getMessage();
         // We get the original message back, not the result from the remote component
-        assertEquals(TEST_MESSAGE + " TestService1", response.getPayload());
+        assertEchoResponse(response);
     }
+
+    private void assertEchoResponse(MuleMessage response) throws Exception
+    {
+        assertThat(response.getPayloadAsString(), equalTo(TEST_MESSAGE + " " + ECHO_FLOW_NAME));
+    }
+
+    private void assertNullPayloadResponse(MuleMessage response)
+    {
+        assertThat(response.getPayload(), CoreMatchers.<Object>is(NullPayload.getInstance()));
+    }
+
 }
