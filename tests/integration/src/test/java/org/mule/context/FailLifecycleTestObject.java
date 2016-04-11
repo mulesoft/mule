9fdbdd5cb0c32403ae0a993dedf32eb429ca5ccc
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2016-04-11 14:29:37 -0300

    MULE-9399: Mule throws a LifecycleException when disposing an Extensi… (#2553)
    MULE-9399: Mule throws a LifecycleException when disposing an Extension Source

---
+++ tests/integration/src/test/java/org/mule/context/FailLifecycleTestObject.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.context;
+
+import org.mule.api.lifecycle.Disposable;
+import org.mule.api.lifecycle.Initialisable;
+import org.mule.api.lifecycle.InitialisationException;
+
+public class FailLifecycleTestObject implements Initialisable, Disposable
+{
+
+    private static boolean initInvoked = false;
+    private static boolean disposeInvoked = false;
+
+    @Override
+    public void initialise() throws InitialisationException
+    {
+        initInvoked = true;
+        throw new InitialisationException(new RuntimeException(), this);
+    }
+
+    @Override
+    public void dispose()
+    {
+        disposeInvoked = true;
+    }
+
+    public static boolean isInitInvoked()
+    {
+        return initInvoked;
+    }
+
+    public static boolean isDisposeInvoked()
+    {
+        return disposeInvoked;
+    }
+
+    public static void setup()
+    {
+        initInvoked = false;
+        disposeInvoked = false;
+    }
+}
