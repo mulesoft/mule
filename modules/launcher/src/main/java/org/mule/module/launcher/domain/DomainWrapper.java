c175d782f844c6f144f34eeca349832dfb83c206
pablo.kraan <pablo.kraan@gmail.com>
2016-04-12 17:37:28 -0300

    MULE-9436:  Define deployment model for extensions
    _ Reverting changes from previous commit
    _ Removing overridden method that was skipping start on domain artifacts
    _ Defining default domain on FakeMuleServer

--- modules/launcher/src/main/java/org/mule/module/launcher/domain/DomainWrapper.java
+++ modules/launcher/src/main/java/org/mule/module/launcher/domain/DomainWrapper.java
@@ -8,7 +8,6 @@ package org.mule.module.launcher.domain;
 
 import org.mule.api.MuleContext;
 import org.mule.api.config.ConfigurationBuilder;
-import org.mule.module.launcher.DeploymentStartException;
 import org.mule.module.launcher.application.Application;
 import org.mule.module.launcher.artifact.DeployableArtifactWrapper;
 import org.mule.module.launcher.descriptor.DomainDescriptor;
@@ -59,10 +58,4 @@ public class DomainWrapper extends DeployableArtifactWrapper<Domain, DomainDescr
             domainFactory.dispose(this);
         }
     }
-
-    @Override
-    public void start() throws DeploymentStartException
-    {
-    }
-
 }
--- tests/functional/src/main/java/org/mule/functional/junit4/ExtensionFunctionalTestCase.java
+++ tests/functional/src/main/java/org/mule/functional/junit4/ExtensionFunctionalTestCase.java
@@ -49,9 +49,20 @@ import org.apache.commons.io.FileUtils;
  * The value added by this class in comparison to a traditional
  * {@link FunctionalTestCase} is that before creating
  * the {@link MuleContext}, it creates a {@link ExtensionManager}
- * and registers the extensions provided on the test.
+ * and automatically discovers extensions by delegating on
+ * {@link ExtensionManager#discoverExtensions(ClassLoader)}.
  * <p/>
- * Once extensions are registered, a {@link ResourcesGenerator} is used to automatically
+ * By default, standard extension discovery will be
+ * performed by invoking {@link ExtensionManager#discoverExtensions(ClassLoader)}.
+ * Although this behavior suits most use cases, it can be time consuming because of
+ * all the classpath scanning and the overhead of initialising extensions that
+ * are most likely not used in this tests. As the number of extensions available grows,
+ * the problem gets worst. For those cases,  you can override the {@link #getDescribers()}
+ * and specify which describers are to be used to initialise the extensions manager. In that way,
+ * extensions discovery is skipped and you only initialise what you need.
+ * <p/>
+ * Once extensions are discovered and described,
+ * a {@link ResourcesGenerator} is used to automatically
  * generate any backing resources needed (for example, XSD schemas, spring bundles,
  * service registration files, etc).
  * <p/>
@@ -155,7 +166,14 @@ public abstract class ExtensionFunctionalTestCase extends FunctionalTestCase
             }
         }
 
-        loadExtensionsFromDescribers(extensionManager, describers);
+        if (ArrayUtils.isEmpty(describers))
+        {
+            extensionManager.discoverExtensions(getClass().getClassLoader());
+        }
+        else
+        {
+            loadExtensionsFromDescribers(extensionManager, describers);
+        }
 
         ResourcesGenerator generator = new ExtensionsTestInfrastructureResourcesGenerator(serviceRegistry, generatedResourcesDirectory);
 
