6721c6d82b66e9b10e49d99fbb0350d37cf3f7e4
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2016-04-13 18:55:17 -0300

    manifest

--- modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/StudioModelPropertyTestCase.java
+++ modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/StudioModelPropertyTestCase.java
@@ -13,6 +13,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mule.runtime.core.config.MuleManifest.getProductVersion;
 import org.mule.runtime.core.api.registry.ServiceRegistry;
+import org.mule.runtime.core.registry.SpiServiceRegistry;
 import org.mule.runtime.extension.api.introspection.ExtensionFactory;
 import org.mule.runtime.extension.api.introspection.ExtensionModel;
 import org.mule.runtime.extension.api.introspection.declaration.fluent.ExtensionDeclarer;
