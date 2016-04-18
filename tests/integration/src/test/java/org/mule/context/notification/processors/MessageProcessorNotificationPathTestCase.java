0216257125d307f9aaa7e2cecbe55ab6120561f1
dfeist <dfeist@gmail.com>
2016-04-18 12:19:58 +0100

    MULE-9505 Message Processors invocations inside an Until Successful are not notified. Add missing test scenarios. (#2579) (cherry picked from commit c554d4e7b0942542d00966637a5dee893aa3ecb1)

--- tests/integration/src/test/java/org/mule/context/notification/processors/MessageProcessorNotificationPathTestCase.java
+++ tests/integration/src/test/java/org/mule/context/notification/processors/MessageProcessorNotificationPathTestCase.java
@@ -51,8 +51,6 @@ public class MessageProcessorNotificationPathTestCase extends FunctionalTestCase
         testFlowPaths("all2", "/0", "/0/0", "/0/0/0","/0/0/1", "/0/1", "/0/1/0","/0/1/1", "/1");
         testFlowPaths("choice2", "/0", "/0/0", "/0/0/0","/0/0/1", "/0/1", "/0/1/0", "/0/2", "/0/2/0","/0/2/1");
         testFlowPaths("all", "/0", "/0/0", "/0/0/0", "/0/1", "/0/1/0", "/1");
-        testFlowPaths("scatterGather", "/0", "/0/0", "/0/0/0", "/0/1", "/0/1/0", "/0/1/1");
-        testFlowPaths("until-successful", "/0", "/0/0", "/1");
     }
 
     @Test
@@ -66,7 +64,9 @@ public class MessageProcessorNotificationPathTestCase extends FunctionalTestCase
     {
         testFlowPaths("foreach", "/0", "/0/0", "/1");
         testFlowPaths("enricher", "/0", "/0/0", "/1", "/1/0", "/1/0/0", "/1/0/1");
-        testFlowPaths("until-successful", "/0", "/0/0", "/0/0/0", "/0/0/1");
+        testFlowPaths("until-successful", "/0", "/0/0", "/1");
+        testFlowPaths("until-successful-with-processor-chain", "/0", "/0/0", "/0/0/0", "/0/0/1", "/1");
+        testFlowPaths("until-successful-with-enricher", "/0", "/0/0", "/0/0/0", "/1");
         testFlowPaths("async", "/0", "/0/0", "/0/1");
     }
 
