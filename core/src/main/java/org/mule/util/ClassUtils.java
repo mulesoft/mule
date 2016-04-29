52e84b95d6e2cab00767c6bf22c43c08aa9c6793
Roger Butenuth <roger.butenuth@codecentric.de>
2016-04-29 16:41:30 +0200

    MULE-9636: Can't call method in Java-Component when argument is primitive data type (#2616)

--- core/src/main/java/org/mule/util/ClassUtils.java
+++ core/src/main/java/org/mule/util/ClassUtils.java
@@ -857,7 +857,7 @@ public class ClassUtils extends org.apache.commons.lang.ClassUtils
             {
                 return false;
             }
-            if (!c1[i].isAssignableFrom(c2[i]))
+            if (!primitiveToWrapper(c1[i]).isAssignableFrom(primitiveToWrapper(c2[i])))
             {
                 return false;
             }
@@ -865,6 +865,7 @@ public class ClassUtils extends org.apache.commons.lang.ClassUtils
         return true;
     }
 
+   
     public static Class wrapperToPrimitive(Class wrapper)
     {
         return (Class) MapUtils.getObject(wrapperToPrimitiveMap, wrapper, wrapper);
