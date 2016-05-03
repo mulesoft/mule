c0db80dedb2012c726fc8dcd532b9ea889465a4d
Pablo Musumeci <pablo.musumeci@mulesoft.com>
2016-05-03 08:50:43 -0300

    MULE-9271: SDK: POJO fields with and without @Parameter annotation doesn't get validated

--- modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/introspection/validation/ParameterModelValidatorTestCase.java
+++ modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/introspection/validation/ParameterModelValidatorTestCase.java
@@ -9,16 +9,23 @@ package org.mule.runtime.module.extension.internal.introspection.validation;
 import static java.util.Arrays.asList;
 import static org.mockito.Answers.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.when;
+import static org.mule.runtime.module.extension.internal.util.ExtensionsTestUtils.arrayOf;
+import static org.mule.runtime.module.extension.internal.util.ExtensionsTestUtils.dictionaryOf;
+import static org.mule.runtime.module.extension.internal.util.ExtensionsTestUtils.objectTypeBuilder;
 import static org.mule.runtime.module.extension.internal.util.ExtensionsTestUtils.toMetadataType;
+import org.mule.runtime.extension.api.annotation.Parameter;
 import org.mule.runtime.extension.api.introspection.ExtensionModel;
 import org.mule.runtime.extension.api.introspection.operation.OperationModel;
 import org.mule.runtime.extension.api.introspection.parameter.ParameterModel;
 import org.mule.runtime.extension.api.introspection.property.ImportedTypesModelProperty;
 import org.mule.runtime.extension.api.introspection.property.SubTypesModelProperty;
 import org.mule.runtime.module.extension.internal.exception.IllegalParameterModelDefinitionException;
+import org.mule.runtime.module.extension.internal.util.ExtensionsTestUtils;
 import org.mule.tck.junit4.AbstractMuleTestCase;
 import org.mule.tck.size.SmallTest;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.junit.Before;
@@ -75,6 +82,52 @@ public class ParameterModelValidatorTestCase extends AbstractMuleTestCase
     }
 
     @Test(expected = IllegalParameterModelDefinitionException.class)
+    public void invalidParameterDueToReservedName()
+    {
+        when(invalidParameterModel.getType()).thenReturn(toMetadataType(InvalidPojo.class));
+        when(invalidParameterModel.getName()).thenReturn("pojo");
+        when(operationModel.getParameterModels()).thenReturn(asList(invalidParameterModel));
+        validator.validate(extensionModel);
+    }
+
+    @Test(expected = IllegalParameterModelDefinitionException.class)
+    public void invalidParameterCollectionDueToReservedName()
+    {
+        when(invalidParameterModel.getType()).thenReturn(ExtensionsTestUtils.arrayOf(List.class, objectTypeBuilder(InvalidPojo.class)));
+        when(invalidParameterModel.getType()).thenReturn(arrayOf(List.class, objectTypeBuilder(InvalidPojo.class)));
+        when(invalidParameterModel.getName()).thenReturn("pojos");
+        when(operationModel.getParameterModels()).thenReturn(asList(invalidParameterModel));
+        validator.validate(extensionModel);
+    }
+
+    @Test(expected = IllegalParameterModelDefinitionException.class)
+    public void invalidParameterDictionaryDueToReservedName()
+    {
+        when(invalidParameterModel.getType()).thenReturn(dictionaryOf(Map.class, objectTypeBuilder(String.class), objectTypeBuilder(InvalidPojo.class)));
+        when(invalidParameterModel.getName()).thenReturn("pojos");
+        when(operationModel.getParameterModels()).thenReturn(asList(invalidParameterModel));
+        validator.validate(extensionModel);
+    }
+
+    @Test(expected = IllegalParameterModelDefinitionException.class)
+    public void invalidRecursiveParameterDueToReservedName()
+    {
+        when(invalidParameterModel.getType()).thenReturn(toMetadataType(RecursivePojo.class));
+        when(invalidParameterModel.getName()).thenReturn("pojo");
+        when(operationModel.getParameterModels()).thenReturn(asList(invalidParameterModel));
+        validator.validate(extensionModel);
+    }
+
+    @Test(expected = IllegalParameterModelDefinitionException.class)
+    public void invalidNestedParameterDueToReservedName()
+    {
+        when(invalidParameterModel.getType()).thenReturn(toMetadataType(NestedInvalidPojo.class));
+        when(invalidParameterModel.getName()).thenReturn("pojo");
+        when(operationModel.getParameterModels()).thenReturn(asList(invalidParameterModel));
+        validator.validate(extensionModel);
+    }
+
+    @Test(expected = IllegalParameterModelDefinitionException.class)
     public void invalidModelDueToDefaultValueWhenRequired()
     {
         when(invalidParameterModel.getType()).thenReturn(toMetadataType(String.class));
@@ -92,5 +145,34 @@ public class ParameterModelValidatorTestCase extends AbstractMuleTestCase
         when(invalidParameterModel.getName()).thenReturn("url");
         when(operationModel.getParameterModels()).thenReturn(asList(invalidParameterModel));
         validator.validate(extensionModel);
+    }
+
+    private static class InvalidPojo
+    {
+
+        public InvalidPojo()
+        {
+            // needs to be instantiable
+        }
+
+        @Parameter
+        private String name;
+    }
+
+    public static class NestedInvalidPojo
+    {
+
+        @Parameter
+        private InvalidPojo invalidPojo;
+    }
+
+    public static class RecursivePojo
+    {
+
+        @Parameter
+        private RecursivePojo pojo;
+
+        @Parameter
+        private InvalidPojo invalidPojo;
     }
 }
