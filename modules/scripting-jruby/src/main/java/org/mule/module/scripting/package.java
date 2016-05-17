39d12687d55505411b2c3430a18f938a72eacd8b
Rodrigo <elrodro83@users.noreply.github.com>
2016-05-17 16:14:26 -0300

    MULE-9714: Create mule-module-scripting-jruby sources and javadocs (#2673)

---
+++ modules/scripting-jruby/src/main/java/org/mule/module/scripting/package.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+/**
+ * This module exists so that the <b>asm</b> dependency used by <b>jruby</b> can be shaded.
+ */
+package org.mule.module.scripting;
