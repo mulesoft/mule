751daaecf5e443023a7ea0bfd36e8c5853d79836
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2016-05-30 11:31:16 -0300

    MULE-9770: include json representation of ExtensionModel inside extension's jar

--- modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/resources/ExtensionModelResourceFactoryTestCase.java
+++ modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/resources/ExtensionModelResourceFactoryTestCase.java
@@ -4,41 +4,35 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.runtime.module.extension.internal.resources.manifest;
+package org.mule.runtime.module.extension.internal.resources;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mule.runtime.core.config.MuleManifest.getProductVersion;
-import static org.mule.runtime.module.extension.internal.util.ExtensionsTestUtils.compareXML;
+import static org.mule.runtime.module.extension.internal.ExtensionProperties.EXTENSION_MODEL_JSON_FILE_NAME;
 import org.mule.runtime.core.registry.SpiServiceRegistry;
 import org.mule.runtime.extension.api.introspection.ExtensionFactory;
 import org.mule.runtime.extension.api.introspection.ExtensionModel;
 import org.mule.runtime.extension.api.introspection.declaration.DescribingContext;
 import org.mule.runtime.extension.api.introspection.declaration.spi.Describer;
+import org.mule.runtime.extension.api.persistence.ExtensionModelJsonSerializer;
 import org.mule.runtime.extension.api.resources.GeneratedResource;
 import org.mule.runtime.extension.api.resources.spi.GeneratedResourceFactory;
 import org.mule.runtime.module.extension.internal.DefaultDescribingContext;
 import org.mule.runtime.module.extension.internal.introspection.DefaultExtensionFactory;
 import org.mule.runtime.module.extension.internal.introspection.describer.AnnotationsBasedDescriber;
 import org.mule.runtime.module.extension.internal.introspection.version.StaticVersionResolver;
-import org.mule.runtime.module.extension.internal.resources.AbstractGeneratedResourceFactoryTestCase;
 import org.mule.tck.size.SmallTest;
 import org.mule.test.heisenberg.extension.HeisenbergExtension;
 
-import java.io.InputStream;
-import java.util.Optional;
-
-import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 
 @SmallTest
-public class ExtensionManifestGeneratorTestCase extends AbstractGeneratedResourceFactoryTestCase
+public class ExtensionModelResourceFactoryTestCase extends AbstractGeneratedResourceFactoryTestCase
 {
 
-    private ExtensionManifestGenerator generator = new ExtensionManifestGenerator();
-
+    private ExtensionModelResourceFactory resourceFactory = new ExtensionModelResourceFactory();
     private ExtensionModel extensionModel;
 
     @Before
@@ -51,21 +45,20 @@ public class ExtensionManifestGeneratorTestCase extends AbstractGeneratedResourc
         extensionModel = extensionFactory.createFrom(describer.describe(context), context);
     }
 
+
     @Override
     protected Class<? extends GeneratedResourceFactory>[] getResourceFactoryTypes()
     {
-        return new Class[] {ExtensionManifestGenerator.class};
+        return new Class[] {ExtensionModelResourceFactory.class};
     }
 
     @Test
     public void generate() throws Exception
     {
-        InputStream in = getClass().getResourceAsStream("/heisenberg-test-manifest.xml");
-        assertThat(in, is(notNullValue()));
-        String expectedSchema = IOUtils.toString(in);
-        Optional<GeneratedResource> resource = generator.generateResource(extensionModel);
-        assertThat(resource.isPresent(), is(true));
+        GeneratedResource resource = resourceFactory.generateResource(extensionModel).get();
+        assertThat(resource.getPath(), equalTo(EXTENSION_MODEL_JSON_FILE_NAME));
 
-        compareXML(expectedSchema, new String(resource.get().getContent()));
+        ExtensionModel deserialized = new ExtensionModelJsonSerializer().deserialize(new String(resource.getContent()));
+        assertThat(extensionModel.getName(), equalTo(deserialized.getName()));
     }
 }
