c908de2f4d1b28befdca50144d3d029feebd374b
Guillermo Fernandes <gsfernandes@users.noreply.github.com>
2016-06-03 11:45:15 -0300

    MULE-9793 ClassNotFoundException: org.apache.commons.io.input.ReaderInputStream when using file extension (#2743)

--- extensions/validation/src/main/java/org/mule/extension/validation/internal/ImmutableValidationResult.java
+++ extensions/validation/src/main/java/org/mule/extension/validation/internal/ImmutableValidationResult.java
@@ -6,7 +6,7 @@
  */
 package org.mule.extension.validation.internal;
 
-import static org.apache.commons.lang.StringUtils.EMPTY;
+import static org.mule.runtime.core.util.StringUtils.EMPTY;
 import org.mule.runtime.core.config.i18n.Message;
 import org.mule.extension.validation.api.ValidationResult;
 
