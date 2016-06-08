0358153a3b1c1fd2d65205d3fa6e46ec6ea5a759
Guillermo Fernandes <guillermo.fernandes@mulesoft.com>
2016-06-08 15:01:36 -0300

    MULE-9430 Add a way to dynamically configure the exported API on the core artifact - Added missing javax.inject package from Java EE

--- modules/container/src/main/java/org/mule/runtime/container/ContainerClassLoaderFactory.java
+++ modules/container/src/main/java/org/mule/runtime/container/ContainerClassLoaderFactory.java
@@ -74,6 +74,7 @@ public class ContainerClassLoaderFactory
             "javax.servlet",
             "javax.ws",
             "javax.mail",
+            "javax.inject",
             "org.xml.sax", "org.apache.xerces",
             "org.apache.logging.log4j", "org.slf4j", "org.apache.commons.logging", "org.apache.log4j",
             "org.dom4j", "org.w3c.dom",
