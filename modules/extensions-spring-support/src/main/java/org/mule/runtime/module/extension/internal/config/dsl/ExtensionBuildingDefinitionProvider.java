02668598f213f2132ff793354946b8bd1014d741
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2016-06-27 14:32:11 -0700

    MULE-9899: Use new parsing mechanism in extensions framework - Fix br… (#2863)

--- modules/extensions-spring-support/src/main/java/org/mule/runtime/module/extension/internal/config/dsl/ExtensionBuildingDefinitionProvider.java
+++ modules/extensions-spring-support/src/main/java/org/mule/runtime/module/extension/internal/config/dsl/ExtensionBuildingDefinitionProvider.java
@@ -13,8 +13,10 @@ import static org.mule.runtime.config.spring.dsl.api.AttributeDefinition.Builder
 import static org.mule.runtime.config.spring.dsl.api.TypeDefinition.fromType;
 import static org.mule.runtime.core.util.ClassUtils.withContextClassLoader;
 import static org.mule.runtime.module.extension.internal.config.dsl.ExtensionXmlNamespaceInfo.EXTENSION_NAMESPACE;
+import static org.mule.runtime.module.extension.internal.util.IntrospectionUtils.getMetadataType;
 import static org.mule.runtime.module.extension.internal.util.IntrospectionUtils.isInstantiable;
 import static org.mule.runtime.module.extension.internal.util.MuleExtensionUtils.getClassLoader;
+import org.mule.metadata.api.ClassTypeLoader;
 import org.mule.metadata.api.model.ArrayType;
 import org.mule.metadata.api.model.DictionaryType;
 import org.mule.metadata.api.model.MetadataType;
@@ -32,9 +34,11 @@ import org.mule.runtime.extension.api.introspection.RuntimeExtensionModel;
 import org.mule.runtime.extension.api.introspection.config.ConfigurationModel;
 import org.mule.runtime.extension.api.introspection.config.RuntimeConfigurationModel;
 import org.mule.runtime.extension.api.introspection.connection.ConnectionProviderModel;
+import org.mule.runtime.extension.api.introspection.declaration.type.ExtensionsTypeLoaderFactory;
 import org.mule.runtime.extension.api.introspection.operation.OperationModel;
 import org.mule.runtime.extension.api.introspection.operation.RuntimeOperationModel;
 import org.mule.runtime.extension.api.introspection.parameter.ParameterModel;
+import org.mule.runtime.extension.api.introspection.property.ExportModelProperty;
 import org.mule.runtime.extension.api.introspection.property.SubTypesModelProperty;
 import org.mule.runtime.extension.api.introspection.property.XmlModelProperty;
 import org.mule.runtime.extension.api.introspection.source.RuntimeSourceModel;
@@ -54,6 +58,7 @@ import org.mule.runtime.module.extension.internal.config.dsl.source.SourceDefini
 import org.mule.runtime.module.extension.internal.introspection.SubTypesMappingContainer;
 import org.mule.runtime.module.extension.internal.runtime.DynamicConfigPolicy;
 import org.mule.runtime.module.extension.internal.util.IdempotentExtensionWalker;
+import org.mule.runtime.module.extension.internal.util.IntrospectionUtils;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -140,45 +145,47 @@ public class ExtensionBuildingDefinitionProvider implements ComponentBuildingDef
             return;
         }
 
-        final Builder definition = new Builder().withNamespace(xmlModelProperty.getNamespace());
+        final Builder definitionBuilder = new Builder().withNamespace(xmlModelProperty.getNamespace());
         Optional<SubTypesModelProperty> subTypesProperty = extensionModel.getModelProperty(SubTypesModelProperty.class);
         SubTypesMappingContainer typeMapping = new SubTypesMappingContainer(subTypesProperty.isPresent() ? subTypesProperty.get().getSubTypesMapping() : emptyMap());
 
-        withContextClassLoader(getClassLoader(extensionModel), () ->
-                new IdempotentExtensionWalker()
+        withContextClassLoader(getClassLoader(extensionModel), () -> {
+            new IdempotentExtensionWalker()
+            {
+                @Override
+                public void onConfiguration(ConfigurationModel model)
                 {
-                    @Override
-                    public void onConfiguration(ConfigurationModel model)
-                    {
-                        parseWith(new ConfigurationDefinitionParser(definition, (RuntimeConfigurationModel) model, muleContext));
-                    }
+                    parseWith(new ConfigurationDefinitionParser(definitionBuilder, (RuntimeConfigurationModel) model, muleContext));
+                }
 
-                    @Override
-                    public void onOperation(OperationModel model)
-                    {
-                        parseWith(new OperationDefinitionParser(definition, (RuntimeExtensionModel) extensionModel, (RuntimeOperationModel) model, muleContext));
-                    }
+                @Override
+                public void onOperation(OperationModel model)
+                {
+                    parseWith(new OperationDefinitionParser(definitionBuilder, (RuntimeExtensionModel) extensionModel, (RuntimeOperationModel) model, muleContext));
+                }
 
-                    @Override
-                    public void onConnectionProvider(ConnectionProviderModel model)
-                    {
-                        parseWith(new ConnectionProviderDefinitionParser(definition, model, muleContext));
-                    }
+                @Override
+                public void onConnectionProvider(ConnectionProviderModel model)
+                {
+                    parseWith(new ConnectionProviderDefinitionParser(definitionBuilder, model, muleContext));
+                }
 
-                    @Override
-                    public void onSource(SourceModel model)
-                    {
-                        parseWith(new SourceDefinitionParser(definition, (RuntimeExtensionModel) extensionModel, (RuntimeSourceModel) model, muleContext));
-                    }
+                @Override
+                public void onSource(SourceModel model)
+                {
+                    parseWith(new SourceDefinitionParser(definitionBuilder, (RuntimeExtensionModel) extensionModel, (RuntimeSourceModel) model, muleContext));
+                }
 
-                    @Override
-                    public void onParameter(ParameterModel model)
-                    {
-                        typeMapping.getSubTypes(model.getType()).forEach(subtype -> registerTopLevelParameter(subtype, definition));
-                        registerTopLevelParameter(model.getType(), definition);
-                    }
-                }.walk(extensionModel)
-        );
+                @Override
+                public void onParameter(ParameterModel model)
+                {
+                    typeMapping.getSubTypes(model.getType()).forEach(subtype -> registerTopLevelParameter(subtype, definitionBuilder));
+                    registerTopLevelParameter(model.getType(), definitionBuilder);
+                }
+            }.walk(extensionModel);
+
+            registerExportedTypesTopLevelParsers(extensionModel, definitionBuilder);
+        });
     }
 
     private void parseWith(ExtensionDefinitionParser parser)
@@ -223,4 +230,14 @@ public class ExtensionBuildingDefinitionProvider implements ComponentBuildingDef
         });
     }
 
+    private void registerExportedTypesTopLevelParsers(ExtensionModel extensionModel, Builder definitionBuilder)
+    {
+        //TODO MDM-7 replace isInstantiableWithParameters
+        final ClassTypeLoader typeLoader = ExtensionsTypeLoaderFactory.getDefault().createTypeLoader(getClassLoader(extensionModel));
+        extensionModel.getModelProperty(ExportModelProperty.class).map(ExportModelProperty::getExportedClasses)
+                .ifPresent(exportedTypes -> exportedTypes.stream()
+                        .filter(IntrospectionUtils::isInstantiableWithParameters)
+                        .map(type -> getMetadataType(type, typeLoader))
+                        .forEach(exportedType -> registerTopLevelParameter(exportedType, definitionBuilder)));
+    }
 }
