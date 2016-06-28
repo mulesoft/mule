4729901b99aade94a3847dc27fb2ad0c253a160b
Rodrigo Merino <rodrigo.merino@mulesoft.com>
2016-06-28 18:40:25 -0300

    MULE-9958: Make DataType getters return Objects instead of Strings * add support for extensions to receive MeidaType and Charset as parameters

---
+++ core/src/main/java/org/mule/runtime/core/transformer/datatype/StringToMediaTypeTransformer.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.transformer.datatype;
+
+import static org.mule.runtime.core.config.i18n.MessageFactory.createStaticMessage;
+
+import org.mule.runtime.api.metadata.DataType;
+import org.mule.runtime.api.metadata.DataTypeBuilder;
+import org.mule.runtime.api.metadata.MediaType;
+import org.mule.runtime.core.api.transformer.TransformerException;
+import org.mule.runtime.core.transformer.AbstractTransformer;
+
+import java.nio.charset.Charset;
+
+/**
+ * Converts strings to {@link MediaType} instances. See {@link DataTypeBuilder#mediaType(String)}
+ */
+public class StringToMediaTypeTransformer extends AbstractTransformer
+{
+
+    public StringToMediaTypeTransformer()
+    {
+        this.registerSourceType(DataType.STRING);
+        this.setReturnDataType(DataType.builder().type(MediaType.class).build());
+    }
+
+    @Override
+    protected Object doTransform(Object src, Charset enc) throws TransformerException
+    {
+        try
+        {
+            return DataType.builder().mediaType((String) src).build().getMediaType();
+        }
+        catch (Exception e)
+        {
+            throw new TransformerException(createStaticMessage("Exception transforming to MediaType."), e);
+        }
+    }
+
+}
