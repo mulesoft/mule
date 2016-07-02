32335825758c65a2e0f2cff92bd89ae4a5ea7adb
Rodrigo Merino <rodrigo.merino@mulesoft.com>
2016-07-02 16:11:51 -0300

    MULE-9994: Improve overall performance of JUnit tests (#2910)
    * Fix CI 3

--- tests/integration/src/test/java/org/mule/test/tck/ResponseAssertionMessageProcessorTestCase.java
+++ tests/integration/src/test/java/org/mule/test/tck/ResponseAssertionMessageProcessorTestCase.java
@@ -11,13 +11,32 @@ import static org.mockito.Mockito.when;
 
 import org.mule.functional.functional.ResponseAssertionMessageProcessor;
 import org.mule.runtime.core.MessageExchangePattern;
+import org.mule.runtime.core.api.lifecycle.InitialisationException;
 import org.mule.tck.SensingNullMessageProcessor;
 import org.mule.tck.processor.TestNonBlockingProcessor;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ResponseAssertionMessageProcessorTestCase extends AssertionMessageProcessorTestCase
 {
+
+    private TestNonBlockingProcessor testNonBlockingProcessor;
+
+    @Before
+    public void before() throws InitialisationException
+    {
+        testNonBlockingProcessor = new TestNonBlockingProcessor();
+        testNonBlockingProcessor.initialise();
+    }
+
+    @After
+    public void after()
+    {
+        testNonBlockingProcessor.dispose();
+    }
+
     @Override
     protected ResponseAssertionMessageProcessor createAssertionMessageProcessor()
     {
@@ -33,7 +52,7 @@ public class ResponseAssertionMessageProcessorTestCase extends AssertionMessageP
         when(mockEvent.getExchangePattern()).thenReturn(MessageExchangePattern.REQUEST_RESPONSE);
 
         ResponseAssertionMessageProcessor asp = createAssertionMessageProcessor();
-        asp.setListener(new TestNonBlockingProcessor());
+        asp.setListener(testNonBlockingProcessor);
         asp.setFlowConstruct(flowConstruct);
         asp.setExpression(TRUE_EXPRESSION);
         asp.setResponseExpression(TRUE_EXPRESSION);
@@ -55,7 +74,7 @@ public class ResponseAssertionMessageProcessorTestCase extends AssertionMessageP
         when(mockEvent.getExchangePattern()).thenReturn(MessageExchangePattern.REQUEST_RESPONSE);
 
         ResponseAssertionMessageProcessor asp = createAssertionMessageProcessor();
-        asp.setListener(new TestNonBlockingProcessor());
+        asp.setListener(testNonBlockingProcessor);
         asp.setFlowConstruct(flowConstruct);
         asp.setExpression(TRUE_EXPRESSION);
         asp.setResponseExpression(TRUE_EXPRESSION);
