592d5cade4d1b1f97603fb141fe3ffd32750bd5f
Rodrigo Merino <rodrigo.merino@mulesoft.com>
2016-07-14 12:04:02 -0300

    MULE-10091: Properties missing when using jetty-ssl

--- transports/jetty/src/test/java/org/mule/transport/servlet/jetty/JettyHttpFunctionalTestCase.java
+++ transports/jetty/src/test/java/org/mule/transport/servlet/jetty/JettyHttpFunctionalTestCase.java
@@ -6,10 +6,19 @@
  */
 package org.mule.transport.servlet.jetty;
 
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mule.api.config.MuleProperties.MULE_REMOTE_CLIENT_ADDRESS;
+import static org.mule.transport.http.HttpConnector.HTTP_CONTEXT_URI_PROPERTY;
+import static org.mule.transport.http.HttpConnector.HTTP_METHOD_PROPERTY;
+import static org.mule.transport.http.HttpConnector.HTTP_REQUEST_PATH_PROPERTY;
+import static org.mule.transport.http.HttpConnector.HTTP_REQUEST_PROPERTY;
 
+import org.mule.api.MuleEventContext;
 import org.mule.api.MuleMessage;
 import org.mule.api.client.LocalMuleClient;
+import org.mule.api.lifecycle.Callable;
 import org.mule.transport.http.HttpConstants;
 import org.mule.transport.http.functional.HttpFunctionalTestCase;
 
@@ -46,6 +55,22 @@ public class JettyHttpFunctionalTestCase extends HttpFunctionalTestCase
         props.put(HttpConstants.HEADER_CONTENT_TYPE, "text/plain;charset=UTF-8");
         MuleMessage result = client.send("anotherClientEndpoint", TEST_MESSAGE, props);
         assertEquals(TEST_MESSAGE + " Received", result.getPayloadAsString());
+    }
+
+    public static class CheckForProperties implements Callable
+    {
+
+        @Override
+        public Object onCall(MuleEventContext eventContext) throws Exception
+        {
+            assertThat(eventContext.getMessage().getInboundPropertyNames(), hasItem(HTTP_REQUEST_PROPERTY));
+            assertThat(eventContext.getMessage().getInboundPropertyNames(), hasItem(HTTP_REQUEST_PATH_PROPERTY));
+            assertThat(eventContext.getMessage().getInboundPropertyNames(), hasItem(MULE_REMOTE_CLIENT_ADDRESS));
+            assertThat(eventContext.getMessage().getInboundPropertyNames(), hasItem(HTTP_CONTEXT_URI_PROPERTY));
+            assertThat(eventContext.getMessage().getInboundPropertyNames(), hasItem(HTTP_METHOD_PROPERTY));
+            return eventContext.getMessage();
+        }
+
     }
 
 }
