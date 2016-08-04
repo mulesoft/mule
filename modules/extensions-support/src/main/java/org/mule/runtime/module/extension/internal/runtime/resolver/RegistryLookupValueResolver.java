256c804d3abe6b5300dc23edae02ff64f20fa8e5
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2016-08-04 18:36:23 -0300

    i shall have my revenge

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/resolver/RegistryLookupValueResolver.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/resolver/RegistryLookupValueResolver.java
@@ -7,12 +7,12 @@
 package org.mule.runtime.module.extension.internal.runtime.resolver;
 
 import static java.lang.String.format;
+import static org.mule.runtime.core.config.i18n.MessageFactory.createStaticMessage;
 import static org.mule.runtime.core.util.Preconditions.checkArgument;
 import org.mule.runtime.core.api.MuleContext;
 import org.mule.runtime.core.api.MuleEvent;
 import org.mule.runtime.core.api.MuleException;
 import org.mule.runtime.core.api.config.ConfigurationException;
-import org.mule.runtime.core.config.i18n.MessageFactory;
 
 import org.apache.commons.lang.StringUtils;
 
@@ -59,7 +59,7 @@ public class RegistryLookupValueResolver<T> implements ValueResolver<T>
         T value = event.getMuleContext().getRegistry().get(key);
         if (value == null)
         {
-            throw new ConfigurationException(MessageFactory.createStaticMessage(format("Element '%s' is not defined in the Mule Registry", key)));
+            throw new ConfigurationException(createStaticMessage(format("Element '%s' is not defined in the Mule Registry", key)));
         }
 
         return value;
