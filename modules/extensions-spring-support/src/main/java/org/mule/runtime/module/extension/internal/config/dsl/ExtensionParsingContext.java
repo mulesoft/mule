39103c76eb59abd605e2e55dac4cfb78b34ba9b7
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2016-08-12 13:40:25 -0300

    MULE-10243: Subtypes cannot be parsed as top level elements (#3125)

--- modules/extensions-spring-support/src/main/java/org/mule/runtime/module/extension/internal/config/dsl/ExtensionParsingContext.java
+++ modules/extensions-spring-support/src/main/java/org/mule/runtime/module/extension/internal/config/dsl/ExtensionParsingContext.java
@@ -6,12 +6,20 @@
  */
 package org.mule.runtime.module.extension.internal.config.dsl;
 
+import static java.util.Optional.empty;
+import static java.util.Optional.ofNullable;
+import org.mule.metadata.api.annotation.TypeIdAnnotation;
 import org.mule.metadata.api.model.MetadataType;
 import org.mule.metadata.api.model.ObjectType;
 import org.mule.runtime.extension.api.introspection.ExtensionModel;
+import org.mule.runtime.extension.api.util.SubTypesMappingContainer;
+
+import com.google.common.collect.ImmutableList;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Context to be used while registering parsers for an {@link ExtensionModel} definition, to keep track of global data accross all
@@ -22,6 +30,7 @@ import java.util.Map;
 public class ExtensionParsingContext {
 
   private final Map<String, MetadataType> parsedObjectTypes = new HashMap<>();
+  private Optional<SubTypesMappingContainer> subTypesMapping = empty();
 
   /**
    * Register an {@link ObjectType} to indicate it has already being parsed for the given {@code name} and {@code namespace}
@@ -45,7 +54,38 @@ public class ExtensionParsingContext {
     return parsedObjectTypes.containsKey(generateObjectKey(name, namespace));
   }
 
+  /**
+   * Returns a {@link List} with all the declared {@link MetadataType} subtypes
+   * for the indicated {@link MetadataType} {@code type}.
+   * <p>
+   * Lookup will be performed first by {@link TypeIdAnnotation typeId},
+   * defaulting to {@link MetadataType type} comparison if no {@link TypeIdAnnotation typeId} was found
+   *
+   * @param type the {@link MetadataType} for which to retrieve its declared subTypes
+   * @return a {@link List} with all the declared subtypes for the indicated {@link MetadataType}
+   */
+  public List<MetadataType> getSubTypes(MetadataType type) {
+    return subTypesMapping.map(mapping -> mapping.getSubTypes(type)).orElse(ImmutableList.of());
+  }
+
+  /**
+   * Configures the subtype mapping to use
+   *
+   * @param subTypesMapping a {@link SubTypesMappingContainer}
+   */
+  public void setSubTypesMapping(SubTypesMappingContainer subTypesMapping) {
+    this.subTypesMapping = ofNullable(subTypesMapping);
+  }
+
+  /**
+   * @return a {@link List} with all the types which extend another type, in no particular order
+   */
+  public List<MetadataType> getAllSubTypes() {
+    return subTypesMapping.map(SubTypesMappingContainer::getAllSubTypes).orElse(ImmutableList.of());
+  }
+
   private String generateObjectKey(String name, String namespace) {
     return name + namespace;
   }
+
 }
