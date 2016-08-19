db2abd99783fba8e514d7bde04a4370976d027af
Alejandro Pulver <alepulver@gmail.com>
2016-08-19 15:46:27 -0300

    MULE-10306: Add option to disable internal entity expansion in XML (leads to DoS) (#3150)

---
+++ modules/xml/src/test/java/org/mule/transformers/xml/xslt/XsltTransformerBLDisabledTestCase.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.transformers.xml.xslt;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+import static org.mule.module.xml.transformer.AbstractXmlTransformer.EXPAND_INTERNAL_ENTITIES_PROPERTY;
+import org.mule.tck.junit4.rule.SystemProperty;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class XsltTransformerBLDisabledTestCase extends XsltTransformerBLTestCase
+{
+
+    @Rule
+    public final SystemProperty myPropertyHasMyValue = new SystemProperty(EXPAND_INTERNAL_ENTITIES_PROPERTY, "false");
+
+    @Test
+    public void disabled() throws Exception
+    {
+        String input = makeInput();
+        Object payload = input.getBytes();
+        String output = (String) runFlow("flow", payload).getMessage().getPayload();
+        assertThat(output, not(containsString("010101010101010101010101010101010101010101010101")));
+    }
+
+}
