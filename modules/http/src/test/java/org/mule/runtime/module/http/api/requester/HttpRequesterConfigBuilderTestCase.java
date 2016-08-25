46ee03b43c40c24dd123bea55fe3cd947c4e9871
Ana Felisatti <afelisatti@mulesoft.com>
2016-08-25 18:51:07 -0300

    MULE-10357: Reduce test thread leaks (#3185)
    (cherry picked from commit 938b3292809a79e24833325baac4e29d4f11b6d5)

--- modules/http/src/test/java/org/mule/runtime/module/http/api/requester/HttpRequesterConfigBuilderTestCase.java
+++ modules/http/src/test/java/org/mule/runtime/module/http/api/requester/HttpRequesterConfigBuilderTestCase.java
@@ -23,6 +23,7 @@ import org.mule.tck.junit4.AbstractMuleTestCase;
 import org.mule.tck.size.SmallTest;
 import org.mule.runtime.api.tls.TlsContextFactory;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Answers;
@@ -40,88 +41,111 @@ public class HttpRequesterConfigBuilderTestCase extends AbstractMuleTestCase {
   private HttpAuthentication mockAuthentication = mock(HttpAuthentication.class, Answers.RETURNS_DEEP_STUBS.get());
   private ProxyConfig mockProxyConfig = mock(ProxyConfig.class, Answers.RETURNS_DEEP_STUBS.get());
   private HttpRequesterConfigBuilder builder = new HttpRequesterConfigBuilder(mockMuleContext);
+  private HttpRequesterConfig requestConfig;
 
   @Before
   public void setUp() {
     when(mockMuleContext.getRegistry().get(Mockito.anyString())).thenReturn(null);
   }
 
+  @After
+  public void tearDown() throws Exception {
+    if (requestConfig != null) {
+      requestConfig.stop();
+    }
+  }
+
   @Test
   public void responseTimeout() throws Exception {
     int responseTimeout = 100;
-    assertThat(builder.setResponseTimeout(responseTimeout).build().getResponseTimeout(), is(valueOf(responseTimeout)));
+    requestConfig = builder.setResponseTimeout(responseTimeout).build();
+    assertThat(requestConfig.getResponseTimeout(), is(valueOf(responseTimeout)));
   }
 
   @Test
   public void responseTimeoutExpression() throws Exception {
-    assertThat(builder.setResponseTimeoutExpression(AN_EXPRESSION).build().getResponseTimeout(), is(AN_EXPRESSION));
+    requestConfig = builder.setResponseTimeoutExpression(AN_EXPRESSION).build();
+    assertThat(requestConfig.getResponseTimeout(), is(AN_EXPRESSION));
   }
 
   @Test(expected = MuleException.class)
   public void tlsContextWithoutHttps() throws Exception {
-    assertThat(builder.setTlsContext(mockTlsContext).build().getTlsContext(), is(mockTlsContext));
+    requestConfig = builder.setTlsContext(mockTlsContext).build();
+    assertThat(requestConfig.getTlsContext(), is(mockTlsContext));
   }
 
   @Test
   public void tlsContext() throws Exception {
-    assertThat(builder.setProtocol(HTTPS).setTlsContext(mockTlsContext).build().getTlsContext(), is(mockTlsContext));
+    requestConfig = builder.setProtocol(HTTPS).setTlsContext(mockTlsContext).build();
+    assertThat(requestConfig.getTlsContext(), is(mockTlsContext));
   }
 
   @Test
   public void basicAuthentication() throws Exception {
-    assertThat(builder.setAuthentication(mockAuthentication).build().getAuthentication(), is(mockAuthentication));
+    requestConfig = builder.setAuthentication(mockAuthentication).build();
+    assertThat(requestConfig.getAuthentication(), is(mockAuthentication));
   }
 
   @Test
   public void proxy() throws Exception {
     when(mockProxyConfig.getHost()).thenReturn(HOST);
     when(mockProxyConfig.getPort()).thenReturn(PORT);
-    assertThat(builder.setProxyConfig(mockProxyConfig).build().getProxyConfig(), is(mockProxyConfig));
+    requestConfig = builder.setProxyConfig(mockProxyConfig).build();
+    assertThat(requestConfig.getProxyConfig(), is(mockProxyConfig));
   }
 
   @Test
   public void sendBodyMode() throws Exception {
-    assertThat(builder.setSendBodyMode(ALWAYS).build().getSendBodyMode(), is(ALWAYS.name()));
+    requestConfig = builder.setSendBodyMode(ALWAYS).build();
+    assertThat(requestConfig.getSendBodyMode(), is(ALWAYS.name()));
   }
 
   @Test
   public void sendBodyModeExpression() throws Exception {
-    assertThat(builder.setSendBodyModeExpression(AN_EXPRESSION).build().getSendBodyMode(), is(AN_EXPRESSION));
+    requestConfig = builder.setSendBodyModeExpression(AN_EXPRESSION).build();
+    assertThat(requestConfig.getSendBodyMode(), is(AN_EXPRESSION));
   }
 
   @Test
   public void setRequestStreamingMode() throws Exception {
-    assertThat(builder.setRequestStreamingMode(AUTO).build().getRequestStreamingMode(), is(AUTO.name()));
+    requestConfig = builder.setRequestStreamingMode(AUTO).build();
+    assertThat(requestConfig.getRequestStreamingMode(), is(AUTO.name()));
   }
 
   @Test
   public void setRequestStreamingModeExpression() throws Exception {
-    assertThat(builder.setRequestStreamingModeExpression(AN_EXPRESSION).build().getRequestStreamingMode(), is(AN_EXPRESSION));
+    requestConfig = builder.setRequestStreamingModeExpression(AN_EXPRESSION).build();
+    assertThat(requestConfig.getRequestStreamingMode(), is(AN_EXPRESSION));
   }
 
   @Test
   public void parseResponse() throws Exception {
-    assertThat(builder.setParseResponse(true).build().getParseResponse(), is("true"));
+    requestConfig = builder.setParseResponse(true).build();
+    assertThat(requestConfig.getParseResponse(), is("true"));
   }
 
   @Test
   public void parseResponseExpression() throws Exception {
-    assertThat(builder.setParseResponseExpression(AN_EXPRESSION).build().getParseResponse(), is(AN_EXPRESSION));
+    requestConfig = builder.setParseResponseExpression(AN_EXPRESSION).build();
+    assertThat(requestConfig.getParseResponse(), is(AN_EXPRESSION));
   }
 
   @Test
   public void host() throws Exception {
-    assertThat(builder.setHostExpression(HOST).build().getHost(), is(HOST));
+    requestConfig = builder.setHostExpression(HOST).build();
+    assertThat(requestConfig.getHost(), is(HOST));
   }
 
   @Test
   public void port() throws Exception {
-    assertThat(builder.setPort(PORT).build().getPort(), is(valueOf(PORT)));
+    requestConfig = builder.setPort(PORT).build();
+    assertThat(requestConfig.getPort(), is(valueOf(PORT)));
   }
 
   @Test
   public void portExpression() throws Exception {
-    assertThat(builder.setPortExpression(AN_EXPRESSION).build().getPort(), is(valueOf(AN_EXPRESSION)));
+    requestConfig = builder.setPortExpression(AN_EXPRESSION).build();
+    assertThat(requestConfig.getPort(), is(valueOf(AN_EXPRESSION)));
   }
 
 }
