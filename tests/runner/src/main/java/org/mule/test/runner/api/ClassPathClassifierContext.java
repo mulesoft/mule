7724cc52c148872a9b1bce4bddb4f95e2e66cef8
pablo.kraan <pablo.kraan@gmail.com>
2016-09-13 18:04:16 -0300

    MULE-10203: Move to a separate module ArtifactClassLoaderRunner
    _ Fixing distro

--- tests/runner/src/main/java/org/mule/test/runner/api/ClassPathClassifierContext.java
+++ tests/runner/src/main/java/org/mule/test/runner/api/ClassPathClassifierContext.java
@@ -9,7 +9,6 @@ package org.mule.test.runner.api;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static org.mule.runtime.core.util.Preconditions.checkNotNull;
-
 import org.mule.test.runner.utils.RunnerModuleUtils;
 
 import java.io.File;
