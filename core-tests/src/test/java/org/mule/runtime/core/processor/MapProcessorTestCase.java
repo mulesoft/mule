3f2e7120aa0eb69ab4e058997f98ddcf6c0d1316
Daniel Feist <dfeist@gmail.com>
2016-09-16 22:46:32 +0100

    MULE-10450 Introduce new Non-Blocking API (#3296)

---
+++ core-tests/src/test/java/org/mule/runtime/core/processor/MapProcessorTestCase.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.processor;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.mule.runtime.core.api.message.InternalMessage.of;
+import static org.mule.tck.junit4.AbstractMuleContextTestCase.RECEIVE_TIMEOUT;
+import static reactor.core.publisher.Mono.just;
+import org.mule.runtime.core.api.Event;
+import org.mule.runtime.core.api.EventContext;
+import org.mule.runtime.core.api.processor.Processor;
+import org.mule.runtime.core.util.concurrent.Latch;
+import org.mule.tck.junit4.AbstractMuleTestCase;
+import org.mule.tck.size.SmallTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+@SmallTest
+public class MapProcessorTestCase extends AbstractMuleTestCase {
+
+  @Mock
+  private EventContext eventContext;
+
+  @Mock
+  private Event event;
+
+  private RuntimeException exception = new RuntimeException() {};
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  private Processor testProcessor = event -> Event.builder(eventContext).message(of(TEST_PAYLOAD)).build();
+  private Processor testProcessorReturnsNull = event -> Event.builder(eventContext).message(of(null)).build();
+  private Processor testProcessorThrowsException = event -> {
+    throw exception;
+  };
+
+  @Test
+  public void mapBlocking() throws Exception {
+    assertThat(testProcessor.process(event).getMessage().getPayload().getValue(), equalTo(TEST_PAYLOAD));
+  }
+
+  @Test
+  public void mapStreamBlockingGet() {
+    assertThat(just(event).transform(testProcessor).block().getMessage().getPayload().getValue(),
+               equalTo(TEST_PAYLOAD));
+  }
+
+  @Test
+  public void mapStreamSubscribe() throws Exception {
+    Latch latch = new Latch();
+    just(event).transform(testProcessor).subscribe(event -> {
+      assertThat(event.getMessage().getPayload().getValue(), equalTo(TEST_PAYLOAD));
+      latch.countDown();
+    });
+    latch.await(RECEIVE_TIMEOUT, MILLISECONDS);
+  }
+
+  @Test
+  public void mapBlockingNullResult() throws Exception {
+    assertThat(testProcessorReturnsNull.process(event).getMessage().getPayload().getValue(), is(nullValue()));
+  }
+
+  @Test
+  public void mapStreamBlockingGetNullResult() {
+    assertThat(just(event).transform(testProcessorReturnsNull).block().getMessage().getPayload().getValue(), is(nullValue()));
+  }
+
+  @Test
+  public void mapStreamSubscribeNullResult() throws Exception {
+    Latch latch = new Latch();
+    just(event).transform(testProcessorReturnsNull).subscribe(event -> {
+      assertThat(event.getMessage().getPayload().getValue(), is(nullValue()));
+      latch.countDown();
+    });
+    latch.await(RECEIVE_TIMEOUT, MILLISECONDS);
+  }
+
+  @Test
+  public void mapBlockingExceptionThrown() throws Exception {
+    thrown.expect(is(exception));
+    assertThat(testProcessorThrowsException.process(event), is(nullValue()));
+  }
+
+  @Test
+  public void mapStreamBlockingGetExceptionThrown() {
+    thrown.expect(is(exception));
+    assertThat(just(event).transform(testProcessorThrowsException).block(), is(nullValue()));
+  }
+
+  @Test
+  public void mapStreamSubscribeExceptionThrown() throws Exception {
+    Latch latch = new Latch();
+    just(event).transform(testProcessorThrowsException).doOnError(throwable -> {
+      assertThat(throwable, is(exception));
+      latch.countDown();
+    }).subscribe();
+    latch.await(RECEIVE_TIMEOUT, MILLISECONDS);
+  }
+
+}
---
+++ core/src/main/java/org/mule/runtime/core/api/processor/ReactiveProcessor.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.api.processor;
+
+import org.mule.runtime.core.api.Event;
+
+import java.util.function.Function;
+
+import org.reactivestreams.Publisher;
+
+/**
+ * Processes streams of {@link Event}'s using a functional approach based on <a href="http://www.reactive-streams.org/">Reactive
+ * Streams<a/> where {@link Event} processing is defined via stream functions.
+ *
+ * @since 4.0
+ */
+public interface ReactiveProcessor extends Function<Publisher<Event>, Publisher<Event>> {
+
+  /**
+   * In order for Mule to determine the best way to execute different processors based on the chosen {@link ProcessingStrategy} it
+   * needs to know the type of work the message processor will be performing and if it is {@link ProcessingType#BLOCKING},
+   * {@link ProcessingType#CPU} intensive or neither ({@link ProcessingType#CPU_LITE}).
+   *
+   * @return the processing type for this processor.
+   */
+  ProcessingType getProccesingType();
+
+  /**
+   * Defines the type of processing that the processor will be doing.
+   */
+  enum ProcessingType {
+
+    /**
+     * CPU intensive processing such as calculation or transformation.
+     */
+    CPU,
+    /**
+     * Processing which neither blocks nor is CPU intensive such as message passing, filtering and routing etc.
+     */
+    CPU_LITE,
+    /**
+     * Blocking processing such as blocking IO or anything that may block the current thread.
+     */
+    BLOCKING
+  }
+
+}
