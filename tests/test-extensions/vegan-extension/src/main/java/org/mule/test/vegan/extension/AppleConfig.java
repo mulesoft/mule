2d1929b80e108230aa3c0e34769cb2033a1f628b
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2016-09-26 14:37:21 -0300

    MULE-10434: Compilation fails for an extension with operations shared with different config

--- tests/test-extensions/vegan-extension/src/main/java/org/mule/test/vegan/extension/AppleConfig.java
+++ tests/test-extensions/vegan-extension/src/main/java/org/mule/test/vegan/extension/AppleConfig.java
@@ -20,7 +20,7 @@ import java.util.Map;
 import java.util.function.Function;
 
 @Configuration(name = APPLE)
-@Operations({EatAppleOperation.class, SpreadVeganismOperation.class})
+@Operations({EatAppleOperation.class, SpreadVeganismOperation.class, VeganFidelityOperation.class})
 @Sources({HarvestApplesSource.class, PaulMcCartneySource.class})
 @ConnectionProviders(VeganAppleConnectionProvider.class)
 public class AppleConfig {
