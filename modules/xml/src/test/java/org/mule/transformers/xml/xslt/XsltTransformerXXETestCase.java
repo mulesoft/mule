bc16524886018bfeed6661a2c8b5e54b739a1d5d
Alejandro Pulver <alejandro.pulver@mulesoft.com>
2016-10-03 14:28:38 -0300

    MULE-10306: disable entity expansion by default in XML transformers

--- modules/xml/src/test/java/org/mule/transformers/xml/xslt/XsltTransformerXXETestCase.java
+++ modules/xml/src/test/java/org/mule/transformers/xml/xslt/XsltTransformerXXETestCase.java
@@ -7,13 +7,16 @@
 package org.mule.transformers.xml.xslt;
 
 import static junit.framework.Assert.assertTrue;
+import static org.mule.module.xml.transformer.AbstractXmlTransformer.EXPAND_INTERNAL_ENTITIES_PROPERTY;
 
 import org.mule.api.transformer.TransformerMessagingException;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.SystemProperty;
 import org.mule.util.IOUtils;
 
 import java.io.ByteArrayInputStream;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -27,6 +30,10 @@ import org.junit.Test;
  */
 public class XsltTransformerXXETestCase extends FunctionalTestCase
 {
+
+    // enable entity expansion as it's required     to test external entity expansion
+    @Rule
+    public final SystemProperty myPropertyHasMyValue = new SystemProperty(EXPAND_INTERNAL_ENTITIES_PROPERTY, "true");
 
     @Override
     protected String getConfigFile()
