77c0cf890c45bf037a016c43cd7c60b276449cc7
Martin Buchwald <m.e.buchwald@gmail.com>
2016-10-07 12:09:02 -0300

    Added tests for connection failures

---
+++ transports/ftp/src/test/java/org/mule/transport/ftp/FtpReconnectionTestCase.java
@@ -0,0 +1,95 @@
+package org.mule.transport.ftp;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.mule.api.MuleContext;
+import org.mule.exception.AbstractSystemExceptionStrategy;
+import org.mule.exception.DefaultSystemExceptionStrategy;
+import org.mule.routing.filters.WildcardFilter;
+import org.mule.tck.listener.FlowExecutionListener;
+import org.mule.transport.ConnectException;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+/**
+ * Verify that some Connect Exceptions-like are being handled and result in reconnections.
+ */
+public class FtpReconnectionTestCase extends AbstractFtpServerTestCase
+{
+    protected TestSystemExceptionStrategy tryReconnectionStrategy;
+
+    public FtpReconnectionTestCase (ConfigVariant variant, String configResources)
+    {
+        super(variant, configResources);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> parameters()
+    {
+        return Arrays.asList(new Object[][] {
+                {ConfigVariant.FLOW, "ftp-reconnection.xml"}
+        });
+    }
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        super.doSetUp();
+
+        tryReconnectionStrategy = new TestSystemExceptionStrategy(muleContext);
+        muleContext.setExceptionListener(tryReconnectionStrategy);
+
+        createFtpServerDir("lostConnection");
+    }
+
+    @Test
+    public void testLostConnection() throws Exception
+    {
+        try
+        {
+            FlowExecutionListener flowExecutionListener = new FlowExecutionListener("LostConnection", muleContext);
+            Thread.sleep(1000);
+            stopServer();
+            flowExecutionListener.waitUntilFlowIsComplete();
+            fail("Should be reconnecting forerver");
+        }
+        catch (java.lang.AssertionError e)
+        {
+            assertTrue(tryReconnectionStrategy.reconnect);
+        }
+    }
+
+    @Test
+    public void testNoConnection() throws Exception
+    {
+        try
+        {
+            stopServer();
+            FlowExecutionListener flowExecutionListener = new FlowExecutionListener("LostConnection", muleContext);
+            flowExecutionListener.waitUntilFlowIsComplete();
+            fail("Should be reconnecting forerver");
+        }
+        catch (java.lang.AssertionError e)
+        {
+            assertTrue(tryReconnectionStrategy.reconnect);
+        }
+    }
+
+    protected class TestSystemExceptionStrategy extends AbstractSystemExceptionStrategy
+    {
+        public boolean reconnect = false;
+        public TestSystemExceptionStrategy(MuleContext muleContext)
+        {
+            super(muleContext);
+        }
+
+        protected void handleReconnection(ConnectException ex)
+        {
+            reconnect = true;
+        }
+    }
+}
