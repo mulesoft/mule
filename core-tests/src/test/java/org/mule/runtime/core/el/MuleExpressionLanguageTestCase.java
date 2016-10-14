4c7cbcfaf3fa3f932256af025a8c45bd57b6732a
afelisatti <afelisatti@mulesoft.com>
2016-10-14 09:45:41 -0300

    PR comments 2

--- core-tests/src/test/java/org/mule/runtime/core/el/MuleExpressionLanguageTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/el/MuleExpressionLanguageTestCase.java
@@ -6,10 +6,12 @@
  */
 package org.mule.runtime.core.el;
 
+import static java.util.Optional.empty;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
@@ -53,36 +55,72 @@ public class MuleExpressionLanguageTestCase extends AbstractMuleTestCase {
   }
 
   @Test
-  public void eventBindings() throws Exception {
+  public void attributesBinding() throws Exception {
     ExpressionLanguage expressionLanguage = spy(MuleExpressionLanguage.class);
-    MuleEvent event = mock(MuleEvent.class);
-    String var1 = "flowVar1";
-    String var2 = "flowVar2";
-    when(event.getVariableNames()).thenReturn(Sets.newHashSet(var1, var2));
-    TypedValue varValue = mock(TypedValue.class);
-    when(event.getVariable(var1)).thenReturn(varValue);
-    when(event.getVariable(var2)).thenReturn(varValue);
-    Error error = mock(Error.class);
-    Optional opt = Optional.of(error);
-    when(event.getError()).thenReturn(opt);
+    MuleEvent event = getEventWithError(empty());
     Message message = mock(Message.class);
     when(event.getMessage()).thenReturn(message);
     Attributes at = mock(Attributes.class);
     when(message.getAttributes()).thenReturn(at);
+
+    BindingContext context = getBindingContextFromEvaluation(expressionLanguage, event);
+    assertThat(context.lookup(ATTRIBUTES).get().getValue(), is(sameInstance(at)));
+  }
+
+  @Test
+  public void errorBinding() throws Exception {
+    ExpressionLanguage expressionLanguage = spy(MuleExpressionLanguage.class);
+    Error error = mock(Error.class);
+    Optional opt = Optional.of(error);
+    MuleEvent event = getEventWithError(opt);
+
+    BindingContext context = getBindingContextFromEvaluation(expressionLanguage, event);
+    assertThat(context.lookup(ERROR).get().getValue(), is(sameInstance(error)));
+  }
+
+  @Test
+  public void payloadBinding() throws Exception {
+    ExpressionLanguage expressionLanguage = spy(MuleExpressionLanguage.class);
+    MuleEvent event = getEventWithError(empty());
+    Message message = mock(Message.class, RETURNS_DEEP_STUBS);
+    when(event.getMessage()).thenReturn(message);
     TypedValue payload = mock(TypedValue.class);
     when(message.getPayload()).thenReturn(payload);
+
+    BindingContext context = getBindingContextFromEvaluation(expressionLanguage, event);
+    assertThat(context.lookup(PAYLOAD).get(), is(sameInstance(payload)));
+  }
+
+  @Test
+  public void variablesBindings() throws Exception {
+    ExpressionLanguage expressionLanguage = spy(MuleExpressionLanguage.class);
+    MuleEvent event = getEventWithError(empty());
+    String var1 = "var1";
+    String var2 = "var2";
+    when(event.getVariableNames()).thenReturn(Sets.newHashSet(var1, var2));
+    TypedValue varValue = mock(TypedValue.class);
+    when(event.getVariable(var1)).thenReturn(varValue);
+    when(event.getVariable(var2)).thenReturn(varValue);
+
+    BindingContext context = getBindingContextFromEvaluation(expressionLanguage, event);
+    Map<String, TypedValue> vars = (Map<String, TypedValue>) context.lookup(VARIABLES).get().getValue();
+    assertThat(vars.get(var1), is(sameInstance(varValue)));
+    assertThat(vars.get(var2), is(sameInstance(varValue)));
+  }
+
+  private MuleEvent getEventWithError(Optional<Error> error) {
+    MuleEvent event = mock(MuleEvent.class, RETURNS_DEEP_STUBS);
+    when(event.getError()).thenReturn(error);
+    return event;
+  }
+
+  private BindingContext getBindingContextFromEvaluation(ExpressionLanguage expressionLanguage, MuleEvent event) {
     ArgumentCaptor<BindingContext> contextCaptor = ArgumentCaptor.forClass(BindingContext.class);
     doReturn(new DefaultTypedValue("ok", DataType.STRING)).when(expressionLanguage).evaluate(anyString(),
                                                                                              contextCaptor.capture());
 
     expressionLanguage.evaluate("exp", BindingContext.builder().build(), event);
-    BindingContext context = contextCaptor.getValue();
-    assertThat(context.lookup(ATTRIBUTES).get().getValue(), is(sameInstance(at)));
-    assertThat(context.lookup(PAYLOAD).get(), is(sameInstance(payload)));
-    assertThat(context.lookup(ERROR).get().getValue(), is(sameInstance(error)));
-    Map<String, TypedValue> vars = (Map<String, TypedValue>) context.lookup(VARIABLES).get().getValue();
-    assertThat(vars.get(var1), is(sameInstance(varValue)));
-    assertThat(vars.get(var2), is(sameInstance(varValue)));
+    return contextCaptor.getValue();
   }
 
 }
