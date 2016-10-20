782e619d6d48e1a4b68da84b4cc145bccc7383d9
pablo.kraan <pablo.kraan@gmail.com>
2016-10-20 19:04:36 -0300

    MULE-10817: ExpressionMuleEventKeyGenerator does not evaluates composite expressions

--- core/src/test/java/org/mule/keygenerator/ExpressionKeyGeneratorTestCase.java
+++ core/src/test/java/org/mule/keygenerator/ExpressionKeyGeneratorTestCase.java
@@ -6,64 +6,78 @@
  */
 package org.mule.keygenerator;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
 import org.mule.api.MuleContext;
 import org.mule.api.MuleEvent;
-import org.mule.api.MuleMessage;
 import org.mule.api.expression.ExpressionManager;
 import org.mule.tck.junit4.AbstractMuleTestCase;
 
 import java.io.NotSerializableException;
 import java.io.Serializable;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ExpressionKeyGeneratorTestCase extends AbstractMuleTestCase
 {
 
-    private static final String KEY = "KEY";
-    private static final String EXPRESSION = "muleExpression";
+    private static final String RESOLVED_KEY = "KEY";
+    private static final String SINGLE_EXPRESSION = "#[expression]";
 
     private ExpressionMuleEventKeyGenerator keyGenerator;
-    private MuleMessage message;
+    private ExpressionManager expressionManager;
+    private MuleContext muleContext;
     private MuleEvent event;
 
     @Before
     public void setUp() throws Exception
     {
+        keyGenerator = new ExpressionMuleEventKeyGenerator();
         expressionManager = mock(ExpressionManager.class);
+        muleContext = mock(MuleContext.class);
+        when(muleContext.getExpressionManager()).thenReturn(expressionManager);
+        event = mock(MuleEvent.class);
+
         MuleContext context = mock(MuleContext.class);
         when(context.getExpressionManager()).thenReturn(expressionManager);
 
-        message = mock(MuleMessage.class);
-
-        event = mock(MuleEvent.class);
-        when(event.getMessage()).thenReturn(message);
-        when(event.getMuleContext()).thenReturn(context);
-
         keyGenerator = new ExpressionMuleEventKeyGenerator();
-        keyGenerator.setExpression(EXPRESSION);
     }
-
-    private ExpressionManager expressionManager;
 
     @Test
     public void testGeneratesSerializableKey() throws Exception
     {
-        when(expressionManager.evaluate(EXPRESSION, event)).thenReturn(KEY);
+        keyGenerator.setExpression(SINGLE_EXPRESSION);
+        keyGenerator.setMuleContext(muleContext);
+        when(expressionManager.evaluate(SINGLE_EXPRESSION, event)).thenReturn(RESOLVED_KEY);
+
         Serializable key = keyGenerator.generateKey(event);
 
-        assertEquals(KEY, key);
+        assertThat(key, Matchers.<Serializable>equalTo(RESOLVED_KEY));
+    }
+
+    @Test
+    public void resolvesCompositeExpression() throws Exception
+    {
+        keyGenerator.setExpression(SINGLE_EXPRESSION + SINGLE_EXPRESSION);
+        keyGenerator.setMuleContext(muleContext);
+        when(expressionManager.parse(SINGLE_EXPRESSION + SINGLE_EXPRESSION, event)).thenReturn(RESOLVED_KEY);
+
+        Serializable key = keyGenerator.generateKey(event);
+
+        assertThat(key, Matchers.<Serializable>equalTo(RESOLVED_KEY));
     }
 
     @Test(expected = NotSerializableException.class)
     public void testThrowsExceptionOnNonSerializableKey() throws Exception
     {
-        when(expressionManager.evaluate(EXPRESSION, event)).thenReturn(null);
+        keyGenerator.setExpression(SINGLE_EXPRESSION);
+        keyGenerator.setMuleContext(muleContext);
+        when(expressionManager.evaluate(SINGLE_EXPRESSION, event)).thenReturn(null);
+
         keyGenerator.generateKey(event);
     }
 }
