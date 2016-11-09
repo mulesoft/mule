b60e9c34530c471e70b205f563dc2556ab1f5db8
Rodrigo Merino <rodrigo.merino@mulesoft.com>
2016-11-09 09:58:05 -0300

    more review

--- core/src/main/java/org/mule/runtime/core/api/context/notification/SynchronousServerEvent.java
+++ core/src/main/java/org/mule/runtime/core/api/context/notification/SynchronousServerEvent.java
@@ -7,10 +7,9 @@
 package org.mule.runtime.core.api.context.notification;
 
 /**
- * <code>BlockingServerEvent</code> is a marker interface that tells the server event manager to publish this event in the current
- * thread, thus blocking the current thread of execution until all listeners have been processed
+ * Marker interface that tells the server event manager to publish this event in the current thread, thus executing the listeners
+ * synchronously.
  */
-
 public interface SynchronousServerEvent {
   // no methods
 }
