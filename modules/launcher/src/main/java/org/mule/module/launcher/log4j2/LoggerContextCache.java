d4fb80c25962db5a82e87092d18c1484236c9ba5
Pablo Kraan <pablokraan@users.noreply.github.com>
2016-11-16 20:21:53 -0300

    MULE-10985: Parallel deployment causes runtime contention on Logger. (#3599)
    _ Reduning scope of synchronization to reduce contention when logger contexts are already initialized

--- modules/launcher/src/main/java/org/mule/module/launcher/log4j2/LoggerContextCache.java
+++ modules/launcher/src/main/java/org/mule/module/launcher/log4j2/LoggerContextCache.java
@@ -122,7 +122,7 @@ final class LoggerContextCache implements Disposable
         }
     }
 
-    synchronized LoggerContext getLoggerContext(final ClassLoader classLoader)
+    LoggerContext getLoggerContext(final ClassLoader classLoader)
     {
         final LoggerContext ctx;
         try
@@ -155,7 +155,13 @@ final class LoggerContextCache implements Disposable
 
         if (ctx.getState() == LifeCycle.State.INITIALIZED)
         {
-            ctx.start();
+            synchronized (this)
+            {
+                if (ctx.getState() == LifeCycle.State.INITIALIZED)
+                {
+                    ctx.start();
+                }
+            }
         }
 
         return ctx;
