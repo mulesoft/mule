1b6d6319e5cd270613ae1ab1f85e4abf19701c0f
Daniel Feist <dfeist@gmail.com>
2016-11-21 14:27:30 +0000

    Review 3

--- core-tests/src/test/java/org/mule/runtime/core/construct/FlowTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/construct/FlowTestCase.java
@@ -138,7 +138,7 @@ public class FlowTestCase extends AbstractFlowConstructTestCase {
     assertThat(response.getMessageAsString(muleContext), equalTo(TEST_PAYLOAD + "abcdef"));
     assertThat(response.getVariable("thread").getValue(), not(sameInstance(currentThread())));
 
-    assertThat(response.getMessageAsString(muleContext), equalTo(TEST_PAYLOAD + "abcdef"));
+    assertThat(sensingMessageProcessor.event.getMessageAsString(muleContext), equalTo(TEST_PAYLOAD + "abc"));
     assertThat(sensingMessageProcessor.event.getVariable("thread").getValue(), not(sameInstance(currentThread())));
   }
 
