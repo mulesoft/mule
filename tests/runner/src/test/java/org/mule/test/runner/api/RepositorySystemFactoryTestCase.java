f0606ef4f2a5038fded643ac268d91e18e7eba6c
Guillermo Fernandes <guillermo.fernandes@mulesoft.com>
2016-11-29 15:30:24 -0300

    Moved assumption as a function instead of overriding the method from abstract test

--- tests/runner/src/test/java/org/mule/test/runner/api/RepositorySystemFactoryTestCase.java
+++ tests/runner/src/test/java/org/mule/test/runner/api/RepositorySystemFactoryTestCase.java
@@ -13,12 +13,14 @@ import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
 import static org.junit.rules.ExpectedException.none;
 import static org.mockito.Mockito.mock;
 import org.mule.tck.junit4.AbstractMuleTestCase;
 import org.mule.tck.size.SmallTest;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Collections;
@@ -28,6 +30,9 @@ import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.resolution.ArtifactResolutionException;
 import org.eclipse.aether.resolution.ArtifactResult;
 import org.eclipse.aether.transfer.ArtifactNotFoundException;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -37,7 +42,6 @@ import org.junit.rules.TemporaryFolder;
 public class RepositorySystemFactoryTestCase extends AbstractMuleTestCase {
 
   private static final String MAVEN_CENTRAL = "http://central.maven.org/maven2/";
-  private static final String ONLINE_REPOSITORY_TEST_METHOD_NAME = "onlineRepository";
 
   @Rule
   public TemporaryFolder root = new TemporaryFolder();
@@ -46,25 +50,10 @@ public class RepositorySystemFactoryTestCase extends AbstractMuleTestCase {
 
   private Artifact commonsCollectionsArtifact = new DefaultArtifact("commons-collections:commons-collections:jar:3.2");
 
-  @Override
-  protected boolean isDisabledInThisEnvironment(String testMethodName) {
-    if (testMethodName.equals(ONLINE_REPOSITORY_TEST_METHOD_NAME)) {
-      try {
-        HttpURLConnection connection = (HttpURLConnection) new URL(MAVEN_CENTRAL).openConnection();
-        connection.setRequestMethod("HEAD");
-        int responseCode = connection.getResponseCode();
-        if (responseCode != 200) {
-          return true;
-        }
-      } catch (Exception e) {
-        return true;
-      }
-    }
-    return false;
-  }
-
   @Test
   public void onlineRepository() throws Exception {
+    assumeThat(MAVEN_CENTRAL, isReachable());
+
     WorkspaceLocationResolver workspaceLocationResolver = mock(WorkspaceLocationResolver.class);
     File mavenLocalRepositoryLocation = root.newFolder("repository");
     DependencyResolver dependencyResolver = RepositorySystemFactory.newOnlineDependencyResolver(
@@ -93,6 +82,27 @@ public class RepositorySystemFactoryTestCase extends AbstractMuleTestCase {
     expectedException.expect(ArtifactResolutionException.class);
     expectedException.expectCause(instanceOf(ArtifactNotFoundException.class));
     dependencyResolver.resolveArtifact(commonsCollectionsArtifact);
+  }
+
+  private Matcher<String> isReachable() {
+    return new TypeSafeMatcher<String>() {
+
+      @Override
+      public void describeTo(Description description) {
+        description.appendText("Test ignored due to it requires to access internet");
+      }
+
+      @Override
+      protected boolean matchesSafely(String remoteServer) {
+        try {
+          HttpURLConnection connection = (HttpURLConnection) new URL(remoteServer).openConnection();
+          connection.setRequestMethod("HEAD");
+          return connection.getResponseCode() == 200;
+        } catch (IOException e) {
+          return false;
+        }
+      }
+    };
   }
 
 }
