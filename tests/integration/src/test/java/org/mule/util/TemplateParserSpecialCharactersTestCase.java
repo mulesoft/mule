4282f1198994886a7f29de5848b31442d3bc2540
facundov <facundov@epidataconsulting.com>
2016-11-30 12:51:42 -0300

    MULE-11079: Fix: Set Payload doesn't work correctly with special characters (#3665)
    Fix code style in testCase.

--- tests/integration/src/test/java/org/mule/util/TemplateParserSpecialCharactersTestCase.java
+++ tests/integration/src/test/java/org/mule/util/TemplateParserSpecialCharactersTestCase.java
@@ -19,44 +19,39 @@ import org.junit.Before;
 import org.junit.Test;
 
 @SmallTest
-public class TemplateParserSpecialCharactersTestCase extends AbstractMuleTestCase
-{
+public class TemplateParserSpecialCharactersTestCase extends AbstractMuleTestCase {
 
-    private Map<String, Object> properties;
+  private Map<String, Object> properties;
 
-    @Before
-    public void initializeMap()
-    {
-        properties = new HashMap<>();
-        properties.put("prop1", "12345@$6789");
-        properties.put("prop2", "12345@$67$89$10$");
-        properties.put("prop3", "\\12345@\\$67$89$10$12\\");
-    }
+  @Before
+  public void initializeMap() {
+    properties = new HashMap<>();
+    properties.put("prop1", "12345@$6789");
+    properties.put("prop2", "12345@$67$89$10$");
+    properties.put("prop3", "\\12345@\\$67$89$10$12\\");
+  }
 
-    @Test
-    public void testValueWithADollarSign()
-    {
-        TemplateParser templateParser = createAntStyleParser();
-        String template = "${prop1}";
-        String result = templateParser.parse(properties, template);
-        assertEquals("12345@$6789", result);
-    }
+  @Test
+  public void testValueWithADollarSign() {
+    TemplateParser templateParser = createAntStyleParser();
+    String template = "${prop1}";
+    String result = templateParser.parse(properties, template);
+    assertEquals("12345@$6789", result);
+  }
 
-    @Test
-    public void testTemplateWithManyDollarSign()
-    {
-        TemplateParser templateParser = createAntStyleParser();
-        String template = "${prop2}";
-        String result = templateParser.parse(properties, template);
-        assertEquals("12345@$67$89$10$", result);
-    }
+  @Test
+  public void testTemplateWithManyDollarSign() {
+    TemplateParser templateParser = createAntStyleParser();
+    String template = "${prop2}";
+    String result = templateParser.parse(properties, template);
+    assertEquals("12345@$67$89$10$", result);
+  }
 
-    @Test
-    public void testTemplateWithDollarSignsAndBackSlashes()
-    {
-        TemplateParser templateParser = createAntStyleParser();
-        String template = "${prop3}";
-        String result = templateParser.parse(properties, template);
-        assertEquals("\\12345@\\$67$89$10$12\\", result);
-    }
+  @Test
+  public void testTemplateWithDollarSignsAndBackSlashes() {
+    TemplateParser templateParser = createAntStyleParser();
+    String template = "${prop3}";
+    String result = templateParser.parse(properties, template);
+    assertEquals("\\12345@\\$67$89$10$12\\", result);
+  }
 }
