f728d07f0175f0a6d8c0587a6ebd3f01d79b498d
Rodrigo Merino <rodrigo.merino@mulesoft.com>
2016-12-02 10:10:24 -0300

    review of the review

--- core-tests/src/test/java/org/mule/runtime/core/processor/strategy/MultiReactorProcessingStrategyTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/processor/strategy/MultiReactorProcessingStrategyTestCase.java
@@ -46,7 +46,8 @@ public class MultiReactorProcessingStrategyTestCase extends AbstractProcessingSt
 
   @Override
   @Description("When MultiReactorProcessingStrategy is configured, two concurrent requests may be processed by two different "
-      + " cpu light threads.  This is why this strategy is called 'MultiReactor' and not 'Reactor`.")
+      + " cpu light threads.  This is why this strategy is called 'MultiReactor' and not 'Reactor`.  MULE-11132 is needed for "
+      + "true reactor behaviour.")
   public void singleCpuLightConcurrent() throws Exception {
     super.singleCpuLightConcurrent();
     assertThat(threads.size(), equalTo(2));
