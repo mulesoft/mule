7d567ab227a4aee4a4a11d49fadc23f806d33530
fsgonz <fabian.gonzalez@mulesoft.com>
2016-12-27 10:49:27 -0300

    Mule 11282: Fix: QuartzMessageReceiver handles the stop message by default and does not stop current scheduled jobs. (#3810)

---
+++ transports/quartz/src/test/java/org/mule/transport/quartz/QuartzConnectorHotStopRestartTestCase.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.transport.quartz;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+public class QuartzConnectorHotStopRestartTestCase extends FunctionalTestCase
+{
+
+    private static final String QUARTZ_CONNECTOR_NAME = "Quartz";
+
+    @Override
+    protected String getConfigFile()
+    {
+        return "quartz-connector-hot-stop-restart-test.xml";
+    }
+
+    @Test
+    public void testStopRestartConnectorShouldPauseAndRestartJob() throws Exception
+    {
+        QuartzConnector connector = muleContext.getRegistry().lookupObject(QUARTZ_CONNECTOR_NAME);
+        QuartzMessageReceiver receiver = (QuartzMessageReceiver) connector.getReceivers().get("quartz://QuartzJob");
+
+        // Initially the scheduler must be started
+        assertThat(FALSE, equalTo(connector.getQuartzScheduler().isShutdown()));
+        receiver.doStop();
+        // When a stop request is sent, the scheduler should be shutdown
+        assertThat(TRUE, equalTo(connector.getQuartzScheduler().isShutdown()));
+        receiver.doStart();
+        // When a start request is performed, the scheduler should be shutdown
+        assertThat(FALSE, equalTo(connector.getQuartzScheduler().isShutdown()));
+    }
+}
