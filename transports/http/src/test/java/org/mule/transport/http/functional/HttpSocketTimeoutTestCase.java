7e1644f36ee4b1c41ab108802291de7aba0b4575
afelisatti <afelisatti@mulesoft.com>
2016-12-30 12:00:19 -0300

    PR fixes

--- transports/http/src/test/java/org/mule/transport/http/functional/HttpSocketTimeoutTestCase.java
+++ transports/http/src/test/java/org/mule/transport/http/functional/HttpSocketTimeoutTestCase.java
@@ -6,49 +6,95 @@
  */
 package org.mule.transport.http.functional;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.mule.api.MuleEvent;
 import org.mule.api.MuleException;
 import org.mule.api.MuleMessage;
-import org.mule.api.client.MuleClient;
-import org.mule.module.http.internal.request.ResponseValidatorException;
+import org.mule.api.processor.MessageProcessor;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.tck.probe.PollingProber;
+import org.mule.tck.probe.Probe;
+import org.mule.util.concurrent.Latch;
 
 public class HttpSocketTimeoutTestCase extends FunctionalTestCase {
 
     @Rule
     public DynamicPort port = new DynamicPort("port");
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+
+    private static Latch latch;
+    private static boolean timedout;
+    private static final int LATCH_TIMEOUT = 300;
+    private static final int POLL_TIMEOUT = 600;
+    private static final int POLL_DELAY = 150;
 
     @Override
     protected String getConfigFile() {
         return "http-socket-timeout-config.xml";
     }
 
+    @Before
+    public void setUp() {
+        latch = new Latch();
+        timedout = false;
+    }
+
     @Test
-    public void usesSoTimeoutIfAvailable() throws MuleException {
-        MuleClient client = muleContext.getClient();
-        MuleMessage message = client.send(getUrl("timeout"), getTestMuleMessage(), 25000);
+    public void usesSoTimeoutIfAvailable() throws Exception {
+        MuleMessage message = runFlow("timeout", getTestMuleMessage()).getMessage();
         assertThat(message, notNullValue());
         assertThat(message.getPayload(), notNullValue());
     }
 
     @Test
-    public void usesResponseTimeoutByDefault() throws MuleException {
-        MuleClient client = muleContext.getClient();
-        expectedException.expect(ResponseValidatorException.class);
-        client.send(getUrl("noTimeout"), getTestMuleMessage(), 25000);
+    public void usesResponseTimeoutByDefault() throws Exception {
+        runFlow("noTimeout", getTestMuleMessage()).getMessage();
+        new PollingProber(POLL_TIMEOUT, POLL_DELAY).check(new Probe() {
+
+            @Override
+            public boolean isSatisfied()
+            {
+                return timedout;
+            }
+
+            @Override
+            public String describeFailure()
+            {
+                return "HTTP request should have timed out.";
+            }
+        });
     }
 
-    private String getUrl(String path) {
-        return String.format("http://localhost:%s/%s", port.getValue(), path);
+    protected static class WaitFailureProcessor implements MessageProcessor
+    {
+        @Override
+        public MuleEvent process(MuleEvent event) throws MuleException
+        {
+            try
+            {
+                timedout = latch.await(LATCH_TIMEOUT, MILLISECONDS);
+            }
+            catch (InterruptedException e)
+            {
+                //do nothing
+            }
+            return event;
+        }
     }
 
-
+    protected static class ReleaseLatchProcessor implements MessageProcessor
+    {
+        @Override
+        public MuleEvent process(MuleEvent event) throws MuleException
+        {
+            latch.release();
+            return event;
+        }
+    }
 }
