b5a4d5f61b47a96a932f751d51515d1f4290bf83
Alejandro Pulver <alepulver@gmail.com>
2017-01-11 11:40:51 -0300

    MULE-11369: make XMLInputFactory throw an exception again when rejecting internal entities (as other parsers do)

--- modules/xml/src/test/java/org/mule/transformers/jaxb/JaxbSecurityTestCase.java
+++ modules/xml/src/test/java/org/mule/transformers/jaxb/JaxbSecurityTestCase.java
@@ -104,7 +104,7 @@ public class JaxbSecurityTestCase extends FunctionalTestCase
     }
 
     @Test
-    public void expandsEntitiesWhenDisabled() throws Exception
+    public void expandsEntitiesDisabled() throws Exception
     {
         try
         {
