3a328164ee21aeeb921af8ab29f7ac7021ff93e7
afelisatti <afelisatti@mulesoft.com>
2017-01-20 09:47:49 -0300

    format

--- core/src/main/java/org/mule/runtime/core/el/mvel/MessageVariableResolverFactory.java
+++ core/src/main/java/org/mule/runtime/core/el/mvel/MessageVariableResolverFactory.java
@@ -62,8 +62,9 @@ public class MessageVariableResolverFactory extends MuleBaseVariableResolverFact
 
   @Override
   public boolean isTarget(String name) {
-    return MESSAGE.equals(name) || PAYLOAD.equals(name) || ATTRIBUTES.equals(name) || FLOW_VARS.equals(name) || EXCEPTION.equals(name) || ERROR.equals(name)
-        || SESSION_VARS.equals(name) || MVELExpressionLanguageContext.MULE_MESSAGE_INTERNAL_VARIABLE.equals(name)
+    return MESSAGE.equals(name) || PAYLOAD.equals(name) || ATTRIBUTES.equals(name) || FLOW_VARS.equals(name)
+        || EXCEPTION.equals(name) || ERROR.equals(name) || SESSION_VARS.equals(name)
+        || MVELExpressionLanguageContext.MULE_MESSAGE_INTERNAL_VARIABLE.equals(name)
         || PARAM_VARS.equals(name) || PROPERTY_VARS.equals(name); //TODO until MULE-10291 & MULE-10353 are done, we will use flowVars to store the parameter.value and property.value
   }
 
