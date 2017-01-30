bf676af7e33d4a34408b709bc48b9c15cf666ef2
fsgonz <fabian.gonzalez@mulesoft.com>
2017-01-30 09:58:11 -0300

    MULE-11286: Fix: avoid verification of timestamp action matching in request and response of a soap service. #3892 (#4003)
    * MULE-11286: Fix: avoid verification of timestamp action matching in
    request and response of a soap service. #3892
    
    * MULE-11497: Unification of tests. Name of rule system property
    attribute.
    
    * MULE-11286: static imports

---
+++ modules/ws/src/test/java/org/mule/module/ws/functional/WssCheckTimestampInResponseTestCase.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.ws.functional;
+
+import static org.mule.api.config.MuleProperties.MULE_CHECK_TIMESTAMP_IN_WSS_RESPONSE;
+import static java.lang.Boolean.TRUE;
+import static java.lang.Boolean.FALSE;
+import org.mule.tck.junit4.rule.SystemProperty;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class WssCheckTimestampInResponseTestCase extends AbstractWssCheckTimestampInResponseTestCase
+{
+
+    @Parameters
+    public static Collection<Object[]> data()
+    {
+        return Arrays.asList(new Object[][] {
+                                             // TS in response and TS check action requested
+                                             {new SystemProperty(MULE_CHECK_TIMESTAMP_IN_WSS_RESPONSE, TRUE.toString()),
+                                              EXPECTED_RESPONSE, dynamicPortTSInResponseTSCheck, ECHO_REQUEST_WITH_HEADERS},
+                                             // No TS in response and TS check action requested
+                                             {new SystemProperty(MULE_CHECK_TIMESTAMP_IN_WSS_RESPONSE, TRUE.toString()),
+                                              EXPECTED_ERROR_NO_TIMESTAMP_RESPONSE, dynamicPortNoTSInResponseTSCheck, ECHO_REQUEST_WITH_HEADERS},
+                                             // TS in response and no TS check action requested
+                                             {new SystemProperty(MULE_CHECK_TIMESTAMP_IN_WSS_RESPONSE, FALSE.toString()),
+                                              EXPECTED_ERROR_NO_TIMESTAMP_RESPONSE, dynamicPortTSInResponseNoTSCheck, ECHO_REQUEST_WITH_HEADERS},
+                                             // No TS in response and No TS check action requested
+                                             {new SystemProperty(MULE_CHECK_TIMESTAMP_IN_WSS_RESPONSE, FALSE.toString()),
+                                              EXPECTED_RESPONSE, dynamicPortNoTSInResponseNoTSCheck, ECHO_REQUEST_WITH_HEADERS}
+        });
+    }
+
+    @Override
+    protected String getConfigFile()
+    {
+        return "wss-timestamp-in-response-test-case.xml";
+    }
+
+}
