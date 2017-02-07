ba9724077d9f94b2a1d7d451fb0a2c9878df37b4
anosenzo <alejandro.nosenzo@mulesoft.com>
2017-02-06 23:53:33 -0300

    Add update_deps_versions.sh to the scripts folder to update mule maven plugin properties.

---
+++ buildtools/scripts/update_deps_versions.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+updatePropertiesVersion() {
+  VERSION_TO_PROPERTY="$1"
+  POM_PROPERTY_PATH="$2"
+
+  # PROPERTIES argument should be passed as a literal "arrayName[@]" without $ because here using the ! it is double expanded
+  # to obtiain the values and declare again the array.
+  PROPERTIES=("${!3}")
+
+  echo "Updating deps in pom: $POM_PROPERTY_PATH"
+
+  for PROPERTY_NAME in "${PROPERTIES[@]}"
+  do
+
+      perl -0777 -i -pe "s/(<properties>.*<$PROPERTY_NAME)(.*)(\/$PROPERTY_NAME>.*<\/properties>)/\${1}>$VERSION_TO_PROPERTY<\${3}/s" "$POM_PROPERTY_PATH"
+      echo "- Updating property $PROPERTY_NAME version to $VERSION_TO_PROPERTY"
+
+  done
+}
+
+VERSION_TO=$1
+
+# Properties with releaseVersion in the root pom.xml
+propertiesDeps=("mule.module.maven.plugin.version"
+                "mule.extensions.maven.plugin.version"
+                "mule.app.plugins.maven.plugin.version")
+
+updatePropertiesVersion "$VERSION_TO" ../../pom.xml propertiesDeps[@]
+
+
+
