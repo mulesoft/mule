78a780b63b3986d5b1b58583f0ec46636f8c6f49
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-02-23 12:11:57 -0300

    MULE-11459: Introduce Cursor Streams - fix flacky test (#4176)

--- tests/functional/src/main/java/org/mule/functional/junit4/FlowRunner.java
+++ tests/functional/src/main/java/org/mule/functional/junit4/FlowRunner.java
@@ -172,6 +172,7 @@ public class FlowRunner extends FlowConstructRunner<FlowRunner> implements Dispo
       try {
         Event result = flow.process(event);
         event.getContext().success(result);
+        streamingManager.success(event);
         return result;
       } catch (Exception e) {
         event.getContext().error(e);
