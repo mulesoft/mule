67995168e6caec26e089a5502395a84d3b6f218f
Juan Lautaro Fernandez <fernandez.lautaro@gmail.com>
2017-02-22 21:32:24 -0300

    MULE-11502:support for synthetic 'target' parameter in smort connectors (#4172)
    * MULE-11502:support for synthetic 'target' parameter in smort connectors

--- modules/extensions-xml-support/src/test/java/org/mule/test/functional/ModuleSimpleTestCase.java
+++ modules/extensions-xml-support/src/test/java/org/mule/test/functional/ModuleSimpleTestCase.java
@@ -6,10 +6,14 @@
  */
 package org.mule.test.functional;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.nullValue;
-import org.hamcrest.core.Is;
+import static org.hamcrest.core.Is.is;
 import org.junit.Test;
+import org.mule.runtime.api.message.Message;
+import org.mule.runtime.api.metadata.TypedValue;
 import org.mule.runtime.core.api.Event;
 
 public class ModuleSimpleTestCase extends AbstractXmlExtensionMuleArtifactFunctionalTestCase {
@@ -27,50 +31,72 @@ public class ModuleSimpleTestCase extends AbstractXmlExtensionMuleArtifactFuncti
   @Test
   public void testSetPayloadHardcodedFlow() throws Exception {
     Event event = flowRunner("testSetPayloadHardcodedFlow").run();
-    assertThat(event.getMessage().getPayload().getValue(), Is.is("hardcoded value"));
+    assertThat(event.getMessage().getPayload().getValue(), is("hardcoded value"));
   }
 
   @Test
   public void testSetPayloadParamFlow() throws Exception {
     Event muleEvent = flowRunner("testSetPayloadParamFlow").run();
-    assertThat(muleEvent.getMessage().getPayload().getValue(), Is.is("new payload"));
+    assertThat(muleEvent.getMessage().getPayload().getValue(), is("new payload"));
   }
 
   @Test
   public void testSetPayloadParamDefaultFlow() throws Exception {
     Event muleEvent = flowRunner("testSetPayloadParamDefaultFlow").run();
-    assertThat(muleEvent.getMessage().getPayload().getValue(), Is.is("15"));
+    assertThat(muleEvent.getMessage().getPayload().getValue(), is("15"));
   }
 
   @Test
   public void testSetPayloadNoSideEffectFlowVariable() throws Exception {
     Event muleEvent = flowRunner("testSetPayloadNoSideEffectFlowVariable").run();
-    assertThat(muleEvent.getMessage().getPayload().getValue(), Is.is("10"));
-    assertThat(muleEvent.getVariable("testVar").getValue(), Is.is("unchanged value"));
+    assertThat(muleEvent.getMessage().getPayload().getValue(), is("10"));
+    assertThat(muleEvent.getVariable("testVar").getValue(), is("unchanged value"));
   }
 
   @Test
   public void testDoNothingFlow() throws Exception {
     Event muleEvent = flowRunner("testDoNothingFlow").run();
-    assertThat(muleEvent.getMessage().getPayload().getValue(), Is.is("before calling"));
-    assertThat(muleEvent.getVariable("variableBeforeCalling").getValue(), Is.is("value of flowvar before calling"));
+    assertThat(muleEvent.getMessage().getPayload().getValue(), is("before calling"));
+    assertThat(muleEvent.getVariable("variableBeforeCalling").getValue(), is("value of flowvar before calling"));
   }
 
   @Test
   public void testSetPayloadParamValueAppender() throws Exception {
     Event muleEvent = flowRunner("testSetPayloadParamValueAppender").run();
-    assertThat(muleEvent.getMessage().getPayload().getValue(), Is.is("new payload from module"));
+    assertThat(muleEvent.getMessage().getPayload().getValue(), is("new payload from module"));
   }
 
   @Test
   public void testSetPayloadConcatParamsValues() throws Exception {
     Event muleEvent = flowRunner("testSetPayloadConcatParamsValues").run();
-    assertThat(muleEvent.getMessage().getPayload().getValue(), Is.is("105"));
+    assertThat(muleEvent.getMessage().getPayload().getValue(), is("105"));
   }
 
   @Test
   public void testSetPayloadUsingUndefinedParam() throws Exception {
     Event muleEvent = flowRunner("testSetPayloadUsingUndefinedParam").run();
-    assertThat(muleEvent.getMessage().getPayload().getValue(), Is.is(nullValue()));
+    assertThat(muleEvent.getMessage().getPayload().getValue(), is(nullValue()));
+  }
+
+  @Test
+  public void testSetPayloadHardcodedFlowWithTarget() throws Exception {
+    Event event = flowRunner("testSetPayloadHardcodedFlowWithTarget").run();
+    assertThat(event.getMessage().getPayload().getValue(), nullValue());
+    final TypedValue<Object> targetVariable = event.getVariable("target-variable");
+    assertThat(targetVariable, notNullValue());
+    assertThat(targetVariable.getValue(), instanceOf(Message.class));
+    Message targetMessage = (Message) targetVariable.getValue();
+    assertThat(targetMessage.getPayload().getValue(), is("hardcoded value"));
+  }
+
+  @Test
+  public void testSetPayloadHardcodedFlowWithTargetOverridingAnExistingVariable() throws Exception {
+    Event event = flowRunner("testSetPayloadHardcodedFlowWithTargetOverridingAnExistingVariable").run();
+    assertThat(event.getMessage().getPayload().getValue(), nullValue());
+    final TypedValue<Object> targetVariable = event.getVariable("existing-variable");
+    assertThat(targetVariable, notNullValue());
+    assertThat(targetVariable.getValue(), instanceOf(Message.class));
+    Message targetMessage = (Message) targetVariable.getValue();
+    assertThat(targetMessage.getPayload().getValue(), is("hardcoded value"));
   }
 }
