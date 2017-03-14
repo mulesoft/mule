a7e45683be540bb36777934ceb8043f132d6b011
estebanwasinger <estebanwasinger@gmail.com>
2017-03-14 12:00:04 -0300

    MULE-11512: JMS TopicBridgeTestCase: replace consumer with listener to avoid flakyness

--- extensions/jms/src/test/java/org/mule/extensions/jms/test/topic/integration/JmsAbstractTopicBridge.java
+++ extensions/jms/src/test/java/org/mule/extensions/jms/test/topic/integration/JmsAbstractTopicBridge.java
@@ -6,76 +6,45 @@
  */
 package org.mule.extensions.jms.test.topic.integration;
 
-import static java.util.concurrent.Executors.newFixedThreadPool;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.fail;
+import static org.mule.extensions.jms.test.JmsMessageStorage.pollMuleMessage;
+import org.junit.Rule;
+import org.junit.Test;
 import org.mule.extensions.jms.api.message.JmsAttributes;
 import org.mule.extensions.jms.api.message.JmsMessageProperties;
 import org.mule.extensions.jms.test.JmsAbstractTestCase;
-import org.mule.runtime.core.api.message.InternalMessage;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.mule.runtime.api.message.Message;
+import org.mule.tck.junit4.rule.SystemProperty;
 
 public abstract class JmsAbstractTopicBridge extends JmsAbstractTestCase {
 
   private static final String FIRST_MESSAGE = "My First Message";
   private static final String BRIDGED_PREFIX = "bridged_";
-  private static final String BRIDGED_PREFIX_VAR = "bridgePrefix";
-  private static final String BRIDGE_FLOW = "bridge";
   private static final String SEND_PAYLOAD_FLOW = "send-payload";
-  private static final String BRIDGE_RECEIVER_FLOW = "bridge-receiver";
-  private static final String INITIAL_DESTINATION = "initialTopic";
-  private static final String INITIAL_DESTINATION_VAR = "initialDestination";
-  private static final String FINAL_DESTINATION = "finalTopic";
-  private static final String FINAL_DESTINATION_VAR = "finalDestination";
   private static final String PROPERTY_KEY_VAR = "initialProperty";
   private static final String PROPERTY_KEY_VALUE = "INIT_PROPERTY";
   private static final String PROPERTY_VALUE_VAR = "propertyValue";
   private static final String PROPERTY_VALUE_VALUE = "Custom Value";
 
-  protected final String BRIDGE_CONFIG_XML = "operations/jms-topic-bridge.xml";
-  private ExecutorService executor;
+  final String BRIDGE_CONFIG_XML = "operations/jms-topic-bridge.xml";
 
-  @Before
-  public void setup() {
-    this.executor = newFixedThreadPool(2);
-  }
+  @Rule
+  public SystemProperty initialDestination = new SystemProperty("initialDestination", newDestination("iniDest"));
 
-  @After
-  public void cleanup() {
-    if (this.executor != null) {
-      this.executor.shutdown();
-    }
-  }
+  @Rule
+  public SystemProperty finalDestination = new SystemProperty("finalDestination", newDestination("finalDest"));
 
   @Test
-  @Ignore("MULE-11512: replace consumer with listener to avoid flakyness")
   public void bridge() throws Exception {
-
-    //Subscribe to source topic
-    initBridgeListener();
-
-    // Listen for bridged message
-    Future<InternalMessage> bridgeTarget = initBridgeTargetListener();
-
     sendMessage();
-
-    InternalMessage message = bridgeTarget.get();
-
-    assertBridgedMessage(message);
+    assertBridgedMessage(pollMuleMessage());
   }
 
-  protected void assertBridgedMessage(InternalMessage message) {
+  private void assertBridgedMessage(Message message) {
     assertThat(message, not(nullValue()));
     assertThat(message.getPayload(), not(nullValue()));
     assertThat(message.getPayload().getValue(), is(equalTo(BRIDGED_PREFIX + FIRST_MESSAGE)));
@@ -86,32 +55,10 @@ public abstract class JmsAbstractTopicBridge extends JmsAbstractTestCase {
     assertThat(properties.getUserProperties().get(PROPERTY_KEY_VALUE), is(equalTo(PROPERTY_VALUE_VALUE)));
   }
 
-  protected void sendMessage() throws Exception {
+  private void sendMessage() throws Exception {
     flowRunner(SEND_PAYLOAD_FLOW)
-        .withVariable(INITIAL_DESTINATION_VAR, INITIAL_DESTINATION)
         .withVariable(PROPERTY_KEY_VAR, PROPERTY_KEY_VALUE)
         .withVariable(PROPERTY_VALUE_VAR, PROPERTY_VALUE_VALUE)
         .withPayload(FIRST_MESSAGE).run();
-  }
-
-  protected Future<InternalMessage> initBridgeTargetListener() {
-    return executor.submit(
-                           () -> flowRunner(BRIDGE_RECEIVER_FLOW)
-                               .withVariable(FINAL_DESTINATION_VAR, FINAL_DESTINATION)
-                               .run().getMessage());
-  }
-
-  protected void initBridgeListener() {
-    executor.submit(() -> {
-      try {
-        flowRunner(BRIDGE_FLOW)
-            .withVariable(INITIAL_DESTINATION_VAR, INITIAL_DESTINATION)
-            .withVariable(FINAL_DESTINATION_VAR, FINAL_DESTINATION)
-            .withVariable(BRIDGED_PREFIX_VAR, BRIDGED_PREFIX)
-            .run();
-      } catch (Exception e) {
-        fail(e.getMessage());
-      }
-    });
   }
 }
