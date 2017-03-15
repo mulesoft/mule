3f48a1d954ac44d428a0b9a2d69980f433aad475
Alejandro Pulver <alejandro.pulver@mulesoft.com>
2017-03-15 17:18:18 -0300

    Changed parser RuntimeException into MuleRuntimeException

--- modules/spring-config/src/main/java/org/mule/runtime/config/spring/dsl/spring/MapEntryBeanDefinitionCreator.java
+++ modules/spring-config/src/main/java/org/mule/runtime/config/spring/dsl/spring/MapEntryBeanDefinitionCreator.java
@@ -6,14 +6,17 @@
  */
 package org.mule.runtime.config.spring.dsl.spring;
 
+import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
 import static org.mule.runtime.dsl.api.component.DslSimpleType.SIMPLE_TYPE_VALUE_PARAMETER_NAME;
 import static org.mule.runtime.dsl.api.component.DslSimpleType.isSimpleType;
 import static org.springframework.beans.factory.support.BeanDefinitionBuilder.genericBeanDefinition;
+
+import org.mule.runtime.api.exception.MuleRuntimeException;
+import org.mule.runtime.config.spring.dsl.model.ComponentModel;
+import org.mule.runtime.config.spring.dsl.processor.ObjectTypeVisitor;
 import org.mule.runtime.dsl.api.component.ComponentBuildingDefinition;
 import org.mule.runtime.dsl.api.component.MapEntry;
 import org.mule.runtime.dsl.api.component.TypeDefinition.MapEntryType;
-import org.mule.runtime.config.spring.dsl.model.ComponentModel;
-import org.mule.runtime.config.spring.dsl.processor.ObjectTypeVisitor;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -74,7 +77,7 @@ public class MapEntryBeanDefinitionCreator extends BeanDefinitionCreator {
     } else if (componentModel.getParameters().get(ENTRY_TYPE_VALUE_REF_PARAMETER_NAME) != null) {
       value = new RuntimeBeanReference(componentModel.getParameters().get(ENTRY_TYPE_VALUE_REF_PARAMETER_NAME));
     } else {
-      throw new RuntimeException("Unknown bean type");
+      throw new MuleRuntimeException(createStaticMessage("Unknown bean type for map entry"));
     }
 
     AbstractBeanDefinition beanDefinition = genericBeanDefinition(MapEntry.class).addConstructorArgValue(keyBeanDefinition)
