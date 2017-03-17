97598e2601ae6c39657943806508b41931e4637b
Fabian Gonzalez <fabian.gonzalez@mulesof.com>
2017-03-17 15:24:34 -0300

    MULE-11973: code review.

--- modules/pgp/src/test/java/org/mule/module/pgp/PGPWithoutSecretKeyTestCase.java
+++ modules/pgp/src/test/java/org/mule/module/pgp/PGPWithoutSecretKeyTestCase.java
@@ -3,21 +3,17 @@
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
- */ 
+ */
 package org.mule.module.pgp;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.mule.DefaultMuleMessage;
 import org.mule.api.ExceptionPayload;
 import org.mule.api.MuleEvent;
 import org.mule.api.MuleException;
-import org.mule.api.MuleMessage;
-import org.mule.api.client.MuleClient;
 import org.mule.api.processor.MessageProcessor;
 import org.mule.api.security.CryptoFailureException;
 import org.mule.tck.junit4.FunctionalTestCase;
@@ -53,22 +49,14 @@ public class PGPWithoutSecretKeyTestCase extends FunctionalTestCase
     @Test
     public void whenNoSecretKeyFilenameProvidedAndEncryptThenNoError() throws Exception
     {
-        MuleClient client = muleContext.getClient();
-
-        client.send("vm://inEncrypt", new DefaultMuleMessage("plainText", muleContext));
-        MuleMessage message = client.request("vm://outEncrypt", 5000);
-        assertThat(message, is(notNullValue()));
-
+        MuleEvent event = runFlow("pgpEncryptProcessor", TEST_MESSAGE);
+        assertThat(event.getMessage().getExceptionPayload(), is(nullValue()));
     }
 
     @Test
     public void whenNoSecretKeyFilenameProvidedAndDecryptThenFlowError() throws Exception
     {
-        MuleClient client = muleContext.getClient();
-
-        client.send("vm://inDecrypt", new DefaultMuleMessage("encryptedtext", muleContext));
-        MuleMessage message = client.request("vm://outDecrypt", 5000);
-        assertThat(message, is(nullValue()));
+        runFlow("pgpDecryptProcessor", TEST_MESSAGE);
         CryptoFailureException cryptoFailureException =
                 ExceptionUtils.getDeepestOccurenceOfType(exceptionFromFlow, CryptoFailureException.class);
         assertThat(cryptoFailureException.getCause().getMessage(), equalTo(NO_SECRET_KEY_RING_FILENAME_DEFINED));
