39ab07e02bc96f771e91e032c7c482f050d1ef17
Rodrigo <elrodro83@users.noreply.github.com>
2017-03-23 15:05:23 -0300

    MULE-12043: In AbstractXPathExpressionEvaluator, replace the cache (#4348)
    WeakHashMap with a guava cache

--- modules/xml/src/main/java/org/mule/module/xml/expression/AbstractXPathExpressionEvaluator.java
+++ modules/xml/src/main/java/org/mule/module/xml/expression/AbstractXPathExpressionEvaluator.java
@@ -6,6 +6,8 @@
  */
 package org.mule.module.xml.expression;
 
+import static org.mule.api.config.MuleProperties.SYSTEM_PROPERTY_PREFIX;
+
 import org.mule.api.MuleContext;
 import org.mule.api.MuleMessage;
 import org.mule.api.MuleRuntimeException;
@@ -27,12 +29,13 @@ import org.mule.transformer.types.DataTypeFactory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.WeakHashMap;
 
 import org.dom4j.Document;
 import org.jaxen.JaxenException;
 import org.jaxen.XPath;
 import org.w3c.dom.Node;
+
+import com.google.common.cache.CacheBuilder;
 
 /**
  * Provides a base class for XPath property extractors. The XPath engine used is jaxen (http://jaxen.org) which supports
@@ -40,7 +43,10 @@ import org.w3c.dom.Node;
  */
 public abstract class AbstractXPathExpressionEvaluator implements ExpressionEvaluator, Initialisable, Disposable, MuleContextAware
 {
-    private Map<String, XPath> cache = new WeakHashMap<String, XPath>(8);
+    private static final int MAX_CACHE_SIZE = Integer.getInteger(SYSTEM_PROPERTY_PREFIX + "xml.xpath.cacheSize", 256);
+
+    private final Map<String, XPath> cache = CacheBuilder.newBuilder().initialCapacity(8).maximumSize(MAX_CACHE_SIZE)
+            .<String, XPath>build().asMap();
 
     private MuleContext muleContext;
     private NamespaceManager namespaceManager;
