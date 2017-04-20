b22d45e0de5578a662ffa3bf90d76d00742914a9
Ana Felisatti <afelisatti@mulesoft.com>
2017-04-20 16:55:01 -0300

    MULE-12268: Add test to verify the reception of multiple headers in the HTTP request (#4534)

--- modules/http/src/test/java/org/mule/module/http/functional/requester/HttpRequestMultipleValueHeadersTestCase.java
+++ modules/http/src/test/java/org/mule/module/http/functional/requester/HttpRequestMultipleValueHeadersTestCase.java
@@ -6,14 +6,17 @@
  */
 package org.mule.module.http.functional.requester;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.isA;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 import static org.mule.module.http.api.HttpHeaders.Values.CHUNKED;
-
 import org.mule.api.MuleEvent;
 import org.mule.construct.Flow;
+import org.mule.tck.junit4.rule.DynamicPort;
 import org.mule.tck.junit4.rule.SystemProperty;
 import org.mule.util.CaseInsensitiveMapWrapper;
 
@@ -36,11 +39,13 @@ public class HttpRequestMultipleValueHeadersTestCase extends AbstractHttpRequest
     public SystemProperty host = new SystemProperty("host", "localhost");
     @Rule
     public SystemProperty encoding = new SystemProperty("encoding" , CHUNKED);
+    @Rule
+    public DynamicPort port = new DynamicPort("port");
 
     @Override
     protected String getConfigFile()
     {
-        return "http-request-headers-config.xml";
+        return "http-request-multiple-header-config.xml";
     }
 
     public HttpRequestMultipleValueHeadersTestCase()
@@ -66,6 +71,18 @@ public class HttpRequestMultipleValueHeadersTestCase extends AbstractHttpRequest
         assertThat(headers.asMap(), hasKey("multipleProperty"));
         assertThat(headers.asMap().get("multipleProperty"), isA(Iterable.class));
         assertThat(headers.asMap().get("multipleProperty"), contains("1", "2", "3"));
+    }
+
+    @Test
+    public void receivesMultipleHeaders() throws Exception
+    {
+        MuleEvent event = runFlow("inboundProperties");
+
+        Object header = event.getMessage().getInboundProperty("header");
+        assertThat(header, instanceOf(Collection.class));
+        Collection<String> headerValue = (Collection<String>) header;
+        assertThat(headerValue, hasSize(2));
+        assertThat(headerValue, containsInAnyOrder("customValue1", "customValue2"));
     }
 
     private void processEventInFlow(MuleEvent event, String flowName) throws Exception
