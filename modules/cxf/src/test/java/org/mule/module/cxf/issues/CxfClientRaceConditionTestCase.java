500da6c06309ea60c8c1298906fda31f3ecf8c96
Fabian Gonzalez <fabian.gonzalez@mulesof.com>
2017-04-24 11:51:17 -0300

    MULE-12252: CR.

--- modules/cxf/src/test/java/org/mule/module/cxf/issues/CxfClientRaceConditionTestCase.java
+++ modules/cxf/src/test/java/org/mule/module/cxf/issues/CxfClientRaceConditionTestCase.java
@@ -11,9 +11,20 @@ import static javax.jms.DeliveryMode.NON_PERSISTENT;
 import static javax.jms.Session.AUTO_ACKNOWLEDGE;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mule.module.http.api.client.HttpRequestOptionsBuilder.newOptions;
+import static org.mule.transport.http.HttpConnector.HTTP;
 
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleException;
+import org.mule.api.MuleMessage;
+import org.mule.api.client.MuleClient;
+import org.mule.module.http.api.client.HttpRequestOptions;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
 import org.mule.tck.size.SmallTest;
+import org.mule.util.IOUtils;
+
+import java.io.PipedInputStream;
 
 import javax.jms.BytesMessage;
 import javax.jms.Connection;
@@ -26,20 +37,37 @@ import javax.jms.Session;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQBytesMessage;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 @SmallTest
-public class CfxClientRaceConditionTestCase extends FunctionalTestCase
+public class CxfClientRaceConditionTestCase extends FunctionalTestCase
 {
-    private static final String AMDS_CXF_JAX_WS_CLIENT_EXPECTED_MESSAGE =
+    private static final String HTTP_METHOD = "httpTest";
+
+    private static final String HTTP_HOST = "localhost";
+
+    private static final HttpRequestOptions HTTP_REQUEST_OPTIONS = newOptions().method(org.mule.module.http.api.HttpConstants.Methods.POST.name()).disableStatusCodeValidation().build();
+
+    private static final String EXPECTED_JMS_MESSAGE =
             "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\"><soap:Body/></soap:Envelope>";
 
+    private static final String HTTP_REQUEST_BODY =
+            "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:ns=\"http://dummy.org\"><soap:Body/><ns:dummy></ns:dummy></soap:Envelope>";
+
+    private static final String EXPECTED_HTTP_MESSAGE =
+            "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\"><soap:Body><ns:dummy xmlns:ns=\"http://dummy.org\"/></soap:Body></soap:Envelope>";
+
     private static final String INBOUND_TEST_QUEUE = "inboundTest";
-    
+
     private static final String OUTBOUND_TEST_QUEUE = "cxfTest";
-    
-    private static final String ACTIVEMQ_CONNECTION_ADDRESS = "vm://localhost";
-    
+
+    private static final String JMS_CONNECTION_ADDRESS = "vm://localhost";
+
+    @Rule
+    public DynamicPort port1 = new DynamicPort("port1");
+
+
     private int totalMessages;
 
     @Before
@@ -49,14 +77,20 @@ public class CfxClientRaceConditionTestCase extends FunctionalTestCase
     }
 
     @Test
-    public void testRaceCondition() throws Exception
+    public void testRaceConditionHTTP() throws Exception
     {
-        cxfJmsTest(INBOUND_TEST_QUEUE, OUTBOUND_TEST_QUEUE, AMDS_CXF_JAX_WS_CLIENT_EXPECTED_MESSAGE);
+        cxfHTTPTest(HTTP, HTTP_HOST, port1.getNumber(), HTTP_METHOD, HTTP_REQUEST_OPTIONS);
     }
 
-    private void cxfJmsTest(String inboundQueue, String outboundQueue, String expectedMessage) throws Exception
+    @Test
+    public void testRaceConditionJMS() throws Exception
     {
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_CONNECTION_ADDRESS);
+        cxfJMSTest(INBOUND_TEST_QUEUE, OUTBOUND_TEST_QUEUE, EXPECTED_JMS_MESSAGE);
+    }
+
+    private void cxfJMSTest(String inboundQueue, String outboundQueue, String expectedMessage) throws Exception
+    {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(JMS_CONNECTION_ADDRESS);
 
         Connection consumerConnection = connectionFactory.createConnection();
         consumerConnection.start();
@@ -99,10 +133,30 @@ public class CfxClientRaceConditionTestCase extends FunctionalTestCase
         consumerConnection.close();
     }
 
+    private void cxfHTTPTest(String protocol, String host, Integer port, String method, HttpRequestOptions options) throws MuleException
+    {
+        long errorCount = 0;
+
+        for (int i = 0; i < totalMessages; i++)
+        {
+
+            MuleMessage request = new DefaultMuleMessage(HTTP_REQUEST_BODY, muleContext);
+            MuleClient client = muleContext.getClient();
+            MuleMessage message = client.send(protocol + "://" + host + ":" + port + "/" + method, request, options);
+            String response = IOUtils.toString((PipedInputStream) message.getPayload());
+            if (EXPECTED_HTTP_MESSAGE.length() != response.length())
+            {
+                errorCount++;
+            }
+        }
+
+        assertThat(errorCount, equalTo(0L));
+    }
+
     @Override
     protected String getConfigResources()
     {
-        return "issues/cfx-client-race-condition.xml";
+        return "issues/cxf-client-race-condition.xml";
     }
 
 }
