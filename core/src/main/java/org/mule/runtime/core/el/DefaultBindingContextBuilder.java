d7d1181418b96409f6e76f33535d5286cd5c4b6a
Mariano de Achaval <mariano.achaval@mulesoft.com>
2017-04-28 18:11:10 -0300

    MULE-1187 Add the concept of namespace and modules in binding context (#4580)

--- core/src/main/java/org/mule/runtime/core/el/DefaultBindingContextBuilder.java
+++ core/src/main/java/org/mule/runtime/core/el/DefaultBindingContextBuilder.java
@@ -6,29 +6,37 @@
  */
 package org.mule.runtime.core.el;
 
+import static java.util.Collections.unmodifiableList;
 import static java.util.Collections.unmodifiableMap;
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 import org.mule.runtime.api.el.Binding;
 import org.mule.runtime.api.el.BindingContext;
+import org.mule.runtime.api.el.ExpressionModule;
 import org.mule.runtime.api.metadata.TypedValue;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
 
 public class DefaultBindingContextBuilder implements BindingContext.Builder {
 
   private Map<String, TypedValue> bindings;
+  private List<ExpressionModule> modules;
 
   public DefaultBindingContextBuilder() {
     this.bindings = new HashMap<>();
+    this.modules = new ArrayList<>();
   }
 
   public DefaultBindingContextBuilder(BindingContext bindingContext) {
     this.bindings = bindingContext.identifiers().stream().collect(toMap(id -> id, id -> bindingContext.lookup(id).get()));
+    this.modules = new ArrayList<>(bindingContext.modules());
   }
 
   @Override
@@ -40,20 +48,29 @@ public class DefaultBindingContextBuilder implements BindingContext.Builder {
   @Override
   public BindingContext.Builder addAll(BindingContext context) {
     context.identifiers().forEach(id -> bindings.put(id, context.lookup(id).get()));
+    modules.addAll(context.modules());
+    return this;
+  }
+
+  @Override
+  public BindingContext.Builder addModule(ExpressionModule expressionModule) {
+    this.modules.add(expressionModule);
     return this;
   }
 
   @Override
   public BindingContext build() {
-    return new BindingContextImplementation(bindings);
+    return new BindingContextImplementation(bindings, modules);
   }
 
   private class BindingContextImplementation implements BindingContext {
 
     private Map<String, TypedValue> bindings;
+    private List<ExpressionModule> modules;
 
-    private BindingContextImplementation(Map<String, TypedValue> bindings) {
-      this.bindings = unmodifiableMap(bindings);
+    private BindingContextImplementation(Map<String, TypedValue> bindings, List<ExpressionModule> modules) {
+      this.bindings = unmodifiableMap(new HashMap<>(bindings));
+      this.modules = unmodifiableList(new ArrayList<>(modules));
     }
 
     @Override
@@ -69,6 +86,11 @@ public class DefaultBindingContextBuilder implements BindingContext.Builder {
     @Override
     public Optional<TypedValue> lookup(String identifier) {
       return ofNullable(bindings.get(identifier));
+    }
+
+    @Override
+    public Collection<ExpressionModule> modules() {
+      return modules;
     }
   }
 }
