aaa38fa42f30865818b8ac8f0806dabbe09ada31
Guillermo Fernandes <gsfernandes@users.noreply.github.com>
2017-05-02 17:32:32 -0300

    MULE-12356: Fixed dependencies and tests to use and register services correctly (#4611)

--- modules/extensions-spring-support/src/test/java/org/mule/test/module/extension/soap/SoapExtensionArtifactFunctionalTestCase.java
+++ modules/extensions-spring-support/src/test/java/org/mule/test/module/extension/soap/SoapExtensionArtifactFunctionalTestCase.java
@@ -6,27 +6,13 @@
  */
 package org.mule.test.module.extension.soap;
 
-import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.startIfNeeded;
-import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.stopIfNeeded;
 import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
-import org.mule.runtime.api.exception.MuleException;
-import org.mule.runtime.core.api.MuleContext;
-import org.mule.runtime.core.api.config.ConfigurationBuilder;
-import org.mule.runtime.core.api.scheduler.SchedulerService;
-import org.mule.runtime.core.config.builders.AbstractConfigurationBuilder;
-import org.mule.service.http.api.HttpService;
-import org.mule.services.http.impl.service.HttpServiceImplementation;
-import org.mule.services.soap.SoapServiceImplementation;
 import org.mule.services.soap.TestHttpSoapServer;
-import org.mule.services.soap.api.SoapService;
-import org.mule.tck.SimpleUnitTestSupportSchedulerService;
 import org.mule.tck.junit4.rule.DynamicPort;
 import org.mule.tck.junit4.rule.SystemProperty;
 import org.mule.test.module.extension.soap.services.FootballService;
 import org.mule.test.module.extension.soap.services.LaLigaService;
 import org.mule.test.runner.ArtifactClassLoaderRunnerConfig;
-
-import java.util.List;
 
 import org.junit.Rule;
 
@@ -34,10 +20,6 @@ import org.junit.Rule;
 public abstract class SoapExtensionArtifactFunctionalTestCase extends MuleArtifactFunctionalTestCase {
 
   private static final String SOAP_CONFIG = "soap-football-extension-config.xml";
-
-  private final SchedulerService schedulerService = new SimpleUnitTestSupportSchedulerService();
-  private final HttpService httpService = new HttpServiceImplementation(schedulerService);
-  private final SoapService soapService = new SoapServiceImplementation(httpService);
 
   @Rule
   public DynamicPort footballPort = new DynamicPort("footballPort");
@@ -55,24 +37,6 @@ public abstract class SoapExtensionArtifactFunctionalTestCase extends MuleArtifa
   public SystemProperty laLigaProp = new SystemProperty("laLigaAddress", laLigaService.getDefaultAddress());
 
   @Override
-  protected void addBuilders(List<ConfigurationBuilder> builders) {
-    super.addBuilders(builders);
-    try {
-      startIfNeeded(httpService);
-    } catch (MuleException e) {
-      // do nothing
-    }
-    builders.add(new AbstractConfigurationBuilder() {
-
-      @Override
-      protected void doConfigure(MuleContext muleContext) throws Exception {
-        muleContext.getRegistry().registerObject(httpService.getName(), httpService);
-        muleContext.getRegistry().registerObject(soapService.getName(), soapService);
-      }
-    });
-  }
-
-  @Override
   protected void doSetUp() throws Exception {
     footballService.init();
     laLigaService.init();
@@ -83,18 +47,9 @@ public abstract class SoapExtensionArtifactFunctionalTestCase extends MuleArtifa
     return SOAP_CONFIG;
   }
 
-  @Override
-  protected void doTearDown() throws Exception {
-    super.doTearDown();
-    stopIfNeeded(soapService);
-    stopIfNeeded(httpService);
-    stopIfNeeded(schedulerService);
-    footballService.stop();
-    laLigaService.stop();
-  }
-
   String getBodyXml(String tagName, String content) {
     String ns = "http://services.soap.extension.module.test.mule.org/";
     return String.format("<con:%s xmlns:con=\"%s\">%s</con:%s>", tagName, ns, content, tagName);
   }
+
 }
