585f68591ad35c1b084fa43d3ad87807222f117c
Guillermo Fernandes <guillermo.fernandes@mulesoft.com>
2017-05-17 12:54:41 -0300

    MULE-10394: Fixed flaky test

--- modules/deployment-model-impl/src/test/java/org/mule/runtime/module/deployment/impl/internal/artifact/MavenClassLoaderModelLoaderTestCase.java
+++ modules/deployment-model-impl/src/test/java/org/mule/runtime/module/deployment/impl/internal/artifact/MavenClassLoaderModelLoaderTestCase.java
@@ -9,6 +9,7 @@ package org.mule.runtime.module.deployment.impl.internal.artifact;
 
 import static com.google.common.io.Files.createTempDir;
 import static java.util.Collections.emptyMap;
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasProperty;
@@ -17,12 +18,15 @@ import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.junit.Assert.fail;
 import static org.junit.rules.ExpectedException.none;
 import static org.mule.runtime.core.config.bootstrap.ArtifactType.APP;
-import static org.mule.tck.MuleTestUtils.testWithSystemProperty;
+import static org.mule.tck.MuleTestUtils.testWithSystemProperties;
 import org.mule.runtime.globalconfig.api.GlobalConfigLoader;
 import org.mule.tck.junit4.rule.SystemProperty;
 
 import java.io.File;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.eclipse.aether.collection.DependencyCollectionException;
 import org.junit.Before;
@@ -32,12 +36,13 @@ import org.junit.rules.ExpectedException;
 
 public class MavenClassLoaderModelLoaderTestCase {
 
+  public static final String MULE_RUNTIME_CONFIG_MAVEN_REPOSITORY_LOCATION = "muleRuntimeConfig.maven.repositoryLocation";
   private MavenClassLoaderModelLoader mavenClassLoaderModelLoader;
 
   private File artifactFile = getApplicationFolder("apps/single-dependency");
 
   @Rule
-  public SystemProperty repositoryLocation = new SystemProperty("muleRuntimeConfig.maven.repositoryLocation",
+  public SystemProperty repositoryLocation = new SystemProperty(MULE_RUNTIME_CONFIG_MAVEN_REPOSITORY_LOCATION,
                                                                 createTempDir().getAbsolutePath());
 
   @Rule
@@ -63,13 +68,26 @@ public class MavenClassLoaderModelLoaderTestCase {
     } catch (Exception e) {
       // It is should fail
     }
-    testWithSystemProperty("muleRuntimeConfig.maven.repositories.mavenCentral.url", "https://repo.maven.apache.org/maven2/",
-                           () -> {
-                             GlobalConfigLoader.reset();
-                             assertThat(mavenClassLoaderModelLoader.load(artifactFile, emptyMap(), APP).getDependencies(),
-                                        hasItem(hasProperty("descriptor",
-                                                            (hasProperty("artifactId", equalTo("commons-collections"))))));
-                           });
+
+    // Find any System property for muleRuntimeConfig from previous executions...
+    final List<String> muleRuntimeConfig =
+        System.getProperties().stringPropertyNames().stream()
+            .filter(propertyName -> propertyName.startsWith("muleRuntimeConfig") && !propertyName.equals(
+                                                                                                         MULE_RUNTIME_CONFIG_MAVEN_REPOSITORY_LOCATION))
+            .collect(
+                     toList());
+    Map<String, String> properties = new HashMap<>();
+    // It should clear those properties when running the test
+    muleRuntimeConfig.forEach(property -> properties.put(property, null));
+    properties.put("muleRuntimeConfig.maven.repositories.mavenCentral.url", "https://repo.maven.apache.org/maven2/");
+
+    testWithSystemProperties(properties,
+                             () -> {
+                               GlobalConfigLoader.reset();
+                               assertThat(mavenClassLoaderModelLoader.load(artifactFile, emptyMap(), APP).getDependencies(),
+                                          hasItem(hasProperty("descriptor",
+                                                              (hasProperty("artifactId", equalTo("commons-collections"))))));
+                             });
   }
 
   private File getApplicationFolder(String appPath) {
