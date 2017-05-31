b1eb2b2fe0a2d2dd82503695ae94cee03559aad2
Guillermo Fernandes <guillermo.fernandes@mulesoft.com>
2017-05-31 13:52:56 -0300

    MULE-11954: Added support for CraftedExtensionModel on tests

--- tests/runner/src/main/java/org/mule/test/runner/utils/ExtensionLoaderUtils.java
+++ tests/runner/src/main/java/org/mule/test/runner/utils/ExtensionLoaderUtils.java
@@ -7,6 +7,7 @@
 package org.mule.test.runner.utils;
 
 import org.mule.runtime.extension.api.loader.ExtensionModelLoader;
+import org.mule.runtime.module.extension.internal.loader.java.CraftedExtensionModelLoader;
 import org.mule.runtime.module.extension.internal.loader.java.DefaultJavaExtensionModelLoader;
 import org.mule.runtime.module.extension.soap.internal.loader.SoapExtensionModelLoader;
 
@@ -16,6 +17,8 @@ public class ExtensionLoaderUtils {
     switch (id) {
       case SoapExtensionModelLoader.SOAP_LOADER_ID:
         return new SoapExtensionModelLoader();
+      case CraftedExtensionModelLoader.CRAFTED_LOADER_ID:
+        return new CraftedExtensionModelLoader();
       case DefaultJavaExtensionModelLoader.JAVA_LOADER_ID:
         return new DefaultJavaExtensionModelLoader();
       default:
