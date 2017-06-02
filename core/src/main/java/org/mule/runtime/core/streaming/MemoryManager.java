d8613c134789c8dfef4509d8f30a6bf4548b8ff2
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-06-02 21:11:48 +0200

    MULE-12658: Routing scenarios fail on oom due to streaming pool being unbounded (#4916)

---
+++ core/src/main/java/org/mule/runtime/core/streaming/MemoryManager.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.streaming;
+
+/**
+ * Handles the runtime's memory
+ *
+ * @since 4.0
+ */
+public interface MemoryManager {
+
+  /**
+   * Returns the maximum amount of memory that the runtime will attempt to use,
+   * following the same semantics as {@link Runtime#totalMemory()}
+   */
+  long getMaxMemory();
+}
+
+
