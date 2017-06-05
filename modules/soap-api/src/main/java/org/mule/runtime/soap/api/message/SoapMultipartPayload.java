2bd485661d5c1133027a8f91cd5c4fd5972bd86a
Juan Desimoni <juan.desimoni@mulesoft.com>
2017-06-05 18:01:40 -0300

    MULE-12671: WSC is not providing a way to pass transport headers (#4931)

--- modules/soap-api/src/main/java/org/mule/runtime/soap/api/message/SoapMultipartPayload.java
+++ modules/soap-api/src/main/java/org/mule/runtime/soap/api/message/SoapMultipartPayload.java
@@ -6,11 +6,14 @@
  */
 package org.mule.runtime.soap.api.message;
 
+import static org.mule.runtime.core.message.DefaultMultiPartPayload.BODY_ATTRIBUTES;
 import static org.mule.runtime.core.util.collection.Collectors.toImmutableMap;
 import org.mule.runtime.api.message.Message;
+import org.mule.runtime.api.message.MultiPartPayload;
 import org.mule.runtime.core.message.DefaultMultiPartPayload;
 import org.mule.runtime.core.message.PartAttributes;
 
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -19,10 +22,12 @@ import java.util.Map;
  *
  * @since 4.0
  */
-public class SoapMultipartPayload extends DefaultMultiPartPayload {
+public class SoapMultipartPayload implements MultiPartPayload {
+
+  private final DefaultMultiPartPayload multipart;
 
   public SoapMultipartPayload(List<Message> parts) {
-    super(parts);
+    this.multipart = new DefaultMultiPartPayload(parts);
   }
 
   /**
@@ -30,22 +35,42 @@ public class SoapMultipartPayload extends DefaultMultiPartPayload {
    *
    * @return an String with the XML response.
    */
-  public String getBody() {
-    return (String) getPart(BODY_ATTRIBUTES.getName()).getPayload().getValue();
+  public InputStream getBody() {
+    return (InputStream) multipart.getPart(BODY_ATTRIBUTES.getName()).getPayload().getValue();
   }
 
   /**
    * Returns the attachments parts content of the {@link SoapMultipartPayload}.
    *
-   * @return a {@link List} with the attachments.
+   * @return a {@link Map} with the attachments.
    */
-  public Map<String, Object> getAttachments() {
-    return getParts().stream()
+  public Map<String, InputStream> getAttachments() {
+    return multipart.getParts().stream()
         .filter(part -> !getPartName(part).equals(BODY_ATTRIBUTES.getName()))
-        .collect(toImmutableMap(this::getPartName, part -> part.getPayload().getValue()));
+        .collect(toImmutableMap(this::getPartName, part -> (InputStream) part.getPayload().getValue()));
   }
 
   private String getPartName(Message part) {
     return ((PartAttributes) part.getAttributes().getValue()).getName();
+  }
+
+  @Override
+  public List<Message> getParts() {
+    return multipart.getParts();
+  }
+
+  @Override
+  public List<String> getPartNames() {
+    return multipart.getPartNames();
+  }
+
+  @Override
+  public Message getPart(String partName) {
+    return multipart.getPart(partName);
+  }
+
+  @Override
+  public Map<String, Message> getNamedParts() {
+    return multipart.getNamedParts();
   }
 }
