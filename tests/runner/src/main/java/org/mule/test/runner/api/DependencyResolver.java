b898f7b6f63ef4af68f40a888584e64a65860232
Guillermo Fernandes <gsfernandes@users.noreply.github.com>
2017-06-06 16:39:32 -0300

    MULE-12698: Changes on Mule Maven Client API (#4948)

--- tests/runner/src/main/java/org/mule/test/runner/api/DependencyResolver.java
+++ tests/runner/src/main/java/org/mule/test/runner/api/DependencyResolver.java
@@ -70,7 +70,7 @@ public class DependencyResolver {
     this.resolutionContext = new AetherResolutionContext(mavenConfiguration);
     this.repositoryState =
         new AetherRepositoryState(this.resolutionContext.getLocalRepositoryLocation(), workspaceReader, false,
-                                  false, resolutionContext.getAuthenticatorSelector());
+                                  true, resolutionContext.getAuthenticatorSelector(), true);
 
     logger.info(ReflectionToStringBuilder.toString(resolutionContext.getAuthenticatorSelector()));
   }
