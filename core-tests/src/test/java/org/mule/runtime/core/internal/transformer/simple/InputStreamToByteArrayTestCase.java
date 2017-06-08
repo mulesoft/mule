9a6e995e2ea9643d897b87c577ba52f881d1438d
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-06-08 16:21:44 -0300

    MULE-12737: one-liner set-variable returns a different object than declaring output application/java

---
+++ core-tests/src/test/java/org/mule/runtime/core/internal/transformer/simple/InputStreamToByteArrayTestCase.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.internal.transformer.simple;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import org.mule.runtime.api.streaming.bytes.CursorStreamProvider;
+import org.mule.runtime.core.internal.streaming.bytes.InMemoryCursorStreamProvider;
+import org.mule.runtime.core.internal.streaming.bytes.SimpleByteBufferManager;
+import org.mule.runtime.core.streaming.bytes.InMemoryCursorStreamConfig;
+import org.mule.tck.junit4.AbstractMuleTestCase;
+import org.mule.tck.size.SmallTest;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+@SmallTest
+public class InputStreamToByteArrayTestCase extends AbstractMuleTestCase {
+
+  private static final String DONKEY = "donkey";
+
+  private InputStreamToByteArray transformer = new InputStreamToByteArray();
+
+  @Test
+  public void transformInputStream() throws Exception {
+    InputStream inputStream = new ByteArrayInputStream(DONKEY.getBytes());
+    assertThat(transformer.transform(inputStream), equalTo(DONKEY.getBytes()));
+  }
+
+  @Test
+  public void transformCursorStreamProvider() throws Exception {
+    InputStream inputStream = new ByteArrayInputStream(DONKEY.getBytes());
+    CursorStreamProvider provider =
+        new InMemoryCursorStreamProvider(inputStream, InMemoryCursorStreamConfig.getDefault(), new SimpleByteBufferManager());
+    assertThat(transformer.transform(provider), equalTo(DONKEY.getBytes()));
+
+  }
+}
+
