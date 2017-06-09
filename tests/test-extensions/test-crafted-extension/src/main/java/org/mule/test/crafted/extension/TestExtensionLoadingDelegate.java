a5e630e5634e4f288f0fd4097a4df7a1dd94d265
mbuchwald <martin.buchwald@mulesoft.com>
2017-06-09 17:03:23 -0300

    MULE-12491: Validate loading extension models with different loader types (#4987)

---
+++ tests/test-extensions/test-crafted-extension/src/main/java/org/mule/test/crafted/extension/TestExtensionLoadingDelegate.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.crafted.extension;
+
+import static org.mule.runtime.api.meta.Category.COMMUNITY;
+
+import org.mule.runtime.api.meta.MuleVersion;
+import org.mule.runtime.api.meta.model.declaration.fluent.ExtensionDeclarer;
+import org.mule.runtime.extension.api.loader.ExtensionLoadingContext;
+import org.mule.runtime.extension.api.loader.ExtensionLoadingDelegate;
+
+public class TestExtensionLoadingDelegate implements ExtensionLoadingDelegate {
+
+  private static final String EXTENSION_NAME = "crafted-extension";
+
+  @Override
+  public void accept(ExtensionDeclarer extensionDeclarer, ExtensionLoadingContext context) {
+    extensionDeclarer.named(EXTENSION_NAME)
+        .describedAs("Crafted Extension")
+        .onVersion("1.0.0")
+        .withMinMuleVersion(new MuleVersion("4.0"))
+        .withCategory(COMMUNITY)
+        .fromVendor("Mulesoft");
+  }
+}
