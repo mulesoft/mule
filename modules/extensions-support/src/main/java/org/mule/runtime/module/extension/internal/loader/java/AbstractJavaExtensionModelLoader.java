462a2d0a712ba7351508435b0abe323b7bca97c2
JuanDesi <juan.desimoni@mulesoft.com>
2017-06-09 21:40:48 -0300

    MULE-11803: Allow privileged extensions to enrich their ExtensionModel

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/java/AbstractJavaExtensionModelLoader.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/java/AbstractJavaExtensionModelLoader.java
@@ -12,44 +12,10 @@ import org.mule.runtime.extension.api.loader.DeclarationEnricher;
 import org.mule.runtime.extension.api.loader.ExtensionLoadingContext;
 import org.mule.runtime.extension.api.loader.ExtensionModelLoader;
 import org.mule.runtime.extension.api.loader.ExtensionModelValidator;
-<<<<<<< HEAD
-import org.mule.runtime.module.extension.internal.loader.enricher.BooleanParameterDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.ClassLoaderDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.ConfigNameDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.ConnectionDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.DataTypeDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.DisplayDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.DynamicMetadataDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.ErrorsDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.ExtensionDescriptionsEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.ExtensionsErrorsDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.ImportedTypesDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.JavaConfigurationDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.JavaExportedTypesDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.JavaOAuthDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.JavaXmlDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.ParameterLayoutOrderDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.enricher.SubTypesDeclarationEnricher;
-import org.mule.runtime.module.extension.internal.loader.validation.ConfigurationModelValidator;
-import org.mule.runtime.module.extension.internal.loader.validation.ConnectionProviderModelValidator;
-import org.mule.runtime.module.extension.internal.loader.validation.ExportedTypesModelValidator;
-import org.mule.runtime.module.extension.internal.loader.validation.JavaSubtypesModelValidator;
-import org.mule.runtime.module.extension.internal.loader.validation.MetadataComponentModelValidator;
-import org.mule.runtime.module.extension.internal.loader.validation.NullSafeModelValidator;
-import org.mule.runtime.module.extension.internal.loader.validation.OAuthConnectionProviderModelValidator;
-import org.mule.runtime.module.extension.internal.loader.validation.OperationParametersTypeModelValidator;
-import org.mule.runtime.module.extension.internal.loader.validation.OperationReturnTypeModelValidator;
-import org.mule.runtime.module.extension.internal.loader.validation.ParameterGroupModelValidator;
-import org.mule.runtime.module.extension.internal.loader.validation.ParameterTypeModelValidator;
-=======
 import org.mule.runtime.module.extension.internal.loader.enricher.*;
 import org.mule.runtime.module.extension.internal.loader.validation.*;
-<<<<<<< HEAD
->>>>>>> MULE-11803: Allow privileged extensions to enrich their ExtensionModel
-=======
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
->>>>>>> MULE-11803: Allow privileged extensions to enrich their ExtensionModel
 
 import java.util.Collection;
 import java.util.List;
@@ -70,7 +36,6 @@ public class AbstractJavaExtensionModelLoader extends ExtensionModelLoader {
   public static final String TYPE_PROPERTY_NAME = "type";
   public static final String VERSION = "version";
   private static final Logger LOGGER = LoggerFactory.getLogger(AbstractJavaExtensionModelLoader.class);
-
   private final List<ExtensionModelValidator> customValidators = unmodifiableList(asList(
                                                                                          new ConfigurationModelValidator(),
                                                                                          new ConnectionProviderModelValidator(),
@@ -85,15 +50,8 @@ public class AbstractJavaExtensionModelLoader extends ExtensionModelLoader {
                                                                                          new OAuthConnectionProviderModelValidator()));
 
   private final List<DeclarationEnricher> customDeclarationEnrichers = unmodifiableList(asList(
-<<<<<<< HEAD
-<<<<<<< HEAD
                                                                                                new ClassLoaderDeclarationEnricher(),
                                                                                                new JavaXmlDeclarationEnricher(),
-                                                                                               new BooleanParameterDeclarationEnricher(),
-=======
-                                                                                               new ClassLoaderDeclarationEnricher(),
-                                                                                               new JavaXmlDeclarationEnricher(),
->>>>>>> MULE-11803: Allow privileged extensions to enrich their ExtensionModel
                                                                                                new ConfigNameDeclarationEnricher(),
                                                                                                new ConnectionDeclarationEnricher(),
                                                                                                new ErrorsDeclarationEnricher(),
@@ -108,27 +66,6 @@ public class AbstractJavaExtensionModelLoader extends ExtensionModelLoader {
                                                                                                new SubTypesDeclarationEnricher(),
                                                                                                new ExtensionDescriptionsEnricher(),
                                                                                                new ParameterLayoutOrderDeclarationEnricher()));
-<<<<<<< HEAD
-=======
-    new ClassLoaderDeclarationEnricher(),
-    new JavaXmlDeclarationEnricher(),
-    new ConfigNameDeclarationEnricher(),
-    new ConnectionDeclarationEnricher(),
-    new ErrorsDeclarationEnricher(),
-    new ExtensionsErrorsDeclarationEnricher(),
-    new DataTypeDeclarationEnricher(),
-    new DisplayDeclarationEnricher(),
-    new DynamicMetadataDeclarationEnricher(),
-    new ImportedTypesDeclarationEnricher(),
-    new JavaConfigurationDeclarationEnricher(),
-    new JavaExportedTypesDeclarationEnricher(),
-    new JavaOAuthDeclarationEnricher(),
-    new SubTypesDeclarationEnricher(),
-    new ExtensionDescriptionsEnricher(),
-    new ParameterLayoutOrderDeclarationEnricher()));
->>>>>>> MULE-11803: Allow privileged extensions to enrich their ExtensionModel
-=======
->>>>>>> MULE-11803: Allow privileged extensions to enrich their ExtensionModel
 
   private final String id;
   private final BiFunction<Class<?>, String, ModelLoaderDelegate> delegateFactory;
