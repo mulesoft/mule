fb99452d34ada967e2676a12a3f22c92f8f4aa70
pablolagreca <pablolagreca@hotmail.com>
2017-06-29 23:31:36 +0200

    MULE-9825 - Add support for property placeholders in mule configuration (#5127)

--- embedded/embedded-test/src/main/java/org/mule/runtime/module/embedded/EmbeddedTestHelper.java
+++ embedded/embedded-test/src/main/java/org/mule/runtime/module/embedded/EmbeddedTestHelper.java
@@ -7,9 +7,12 @@
 package org.mule.runtime.module.embedded;
 
 import static java.lang.Thread.currentThread;
+import static org.apache.commons.io.FileUtils.deleteDirectory;
 import static org.apache.commons.io.FileUtils.toFile;
-import static org.mule.maven.client.test.MavenTestHelper.createDefaultCommunityMavenConfiguration;
-import static org.mule.maven.client.test.MavenTestHelper.createDefaultEnterpriseMavenConfiguration;
+import static org.mule.maven.client.api.model.RemoteRepository.newRemoteRepositoryBuilder;
+import static org.mule.maven.client.test.MavenTestHelper.createDefaultCommunityMavenConfigurationBuilder;
+import static org.mule.maven.client.test.MavenTestHelper.createDefaultEnterpriseMavenConfigurationBuilder;
+import static org.mule.maven.client.test.MavenTestHelper.getLocalRepositoryFolder;
 import static org.mule.runtime.module.embedded.api.EmbeddedContainer.builder;
 import org.mule.maven.client.api.model.MavenConfiguration;
 import org.mule.runtime.module.embedded.api.ContainerConfiguration;
@@ -17,14 +20,13 @@ import org.mule.runtime.module.embedded.api.EmbeddedContainer;
 import org.mule.runtime.module.embedded.internal.classloading.FilteringClassLoader;
 import org.mule.runtime.module.embedded.internal.classloading.JdkOnlyClassLoaderFactory;
 
-import org.junit.rules.TemporaryFolder;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.function.Consumer;
 
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.model.ZipParameters;
+import org.junit.rules.TemporaryFolder;
 
 /**
  * Helper class for running embedded tests.
@@ -36,13 +38,14 @@ public class EmbeddedTestHelper {
   private final TemporaryFolder temporaryFolder;
   private final boolean enterprise;
   private File containerFolder;
+  private File localRepositoryFolder;
   private EmbeddedContainer container;
 
   public EmbeddedTestHelper(boolean enterprise) {
     try {
       temporaryFolder = new TemporaryFolder();
       temporaryFolder.create();
-      this.containerFolder = temporaryFolder.newFolder();
+      this.localRepositoryFolder = temporaryFolder.newFolder();
       this.enterprise = enterprise;
     } catch (IOException e) {
       throw new RuntimeException(e);
@@ -87,12 +90,17 @@ public class EmbeddedTestHelper {
     test(() -> {
       EmbeddedContainer.EmbeddedContainerBuilder embeddedContainerBuilder;
       try {
-        MavenConfiguration mavenConfiguration =
-            enterprise ? createDefaultEnterpriseMavenConfiguration() : createDefaultCommunityMavenConfiguration();
+        recreateContainerFolder();
+        MavenConfiguration.MavenConfigurationBuilder mavenConfigurationBuilder =
+            enterprise ? createDefaultEnterpriseMavenConfigurationBuilder() : createDefaultCommunityMavenConfigurationBuilder();
         embeddedContainerBuilder = builder()
             .withMuleVersion(System.getProperty("mule.version"))
             .withContainerConfiguration(ContainerConfiguration.builder().withContainerFolder(containerFolder).build())
-            .withMavenConfiguration(mavenConfiguration);
+            .withMavenConfiguration(mavenConfigurationBuilder.withLocalMavenRepositoryLocation(localRepositoryFolder)
+                .withRemoteRepository(newRemoteRepositoryBuilder().withId("local.repo")
+                    .withUrl(getLocalRepositoryFolder().toURI().toURL())
+                    .build())
+                .build());
         embeddedContainerConfigurer.accept(embeddedContainerBuilder);
       } catch (Exception e) {
         throw new RuntimeException(e);
@@ -103,15 +111,14 @@ public class EmbeddedTestHelper {
         container = embeddedContainerBuilder.build();
         container.start();
         test.run();
-        container.stop();
-      } catch (Throwable e) {
+      } finally {
         if (container != null)
           try {
             container.stop();
+            deleteDirectory(containerFolder);
           } catch (Throwable containerStopException) {
             // Never mind
           }
-        throw e;
       }
     });
   }
