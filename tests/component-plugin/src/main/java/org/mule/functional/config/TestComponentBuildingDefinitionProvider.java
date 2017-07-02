3320f61b4ec6b6db84b43ec8885f0ef365dc14b6
afelisatti <afelisatti@mulesoft.com>
2017-07-02 19:03:04 -0300

    Review

--- tests/component-plugin/src/main/java/org/mule/functional/config/TestComponentBuildingDefinitionProvider.java
+++ tests/component-plugin/src/main/java/org/mule/functional/config/TestComponentBuildingDefinitionProvider.java
@@ -7,7 +7,6 @@
 package org.mule.functional.config;
 
 import static org.mule.functional.config.TestXmlNamespaceInfoProvider.TEST_NAMESPACE;
-import static org.mule.runtime.dsl.api.component.AttributeDefinition.Builder.fromChildCollectionConfiguration;
 import static org.mule.runtime.dsl.api.component.AttributeDefinition.Builder.fromChildConfiguration;
 import static org.mule.runtime.dsl.api.component.AttributeDefinition.Builder.fromReferenceObject;
 import static org.mule.runtime.dsl.api.component.AttributeDefinition.Builder.fromSimpleParameter;
@@ -27,12 +26,9 @@ import org.mule.functional.api.component.SkeletonSource;
 import org.mule.functional.api.component.ThrowProcessor;
 import org.mule.functional.client.QueueWriterMessageProcessor;
 import org.mule.runtime.core.api.MuleContext;
-import org.mule.runtime.core.api.processor.Processor;
-import org.mule.runtime.core.processor.simple.SetPayloadMessageProcessor;
 import org.mule.runtime.dsl.api.component.ComponentBuildingDefinition;
 import org.mule.runtime.dsl.api.component.ComponentBuildingDefinitionProvider;
 import org.mule.tck.processor.TestNonBlockingProcessor;
-import org.mule.tck.testmodels.mule.TestExceptionStrategy;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -89,13 +85,6 @@ public class TestComponentBuildingDefinitionProvider implements ComponentBuildin
         .withIdentifier("processor")
         .withTypeDefinition(fromType(FunctionalTestProcessor.class))
         .build());
-
-    componentBuildingDefinitions.add(baseDefinition.copy()
-        .withIdentifier("exception-strategy")
-        .withTypeDefinition(fromType(TestExceptionStrategy.class))
-        .withConstructorParameterDefinition(fromSimpleParameter("class").build())
-        .withSetterParameterDefinition("messageProcessors", fromChildCollectionConfiguration(Processor.class).build())
-        .asPrototype().build());
 
     componentBuildingDefinitions.add(baseDefinition.copy()
         .withIdentifier("throw")
@@ -158,12 +147,6 @@ public class TestComponentBuildingDefinitionProvider implements ComponentBuildin
         .withIdentifier("shared-config")
         .withTypeDefinition(fromType(SharedConfig.class))
         .build());
-
-    componentBuildingDefinitions
-        .add(baseDefinition.copy().withIdentifier("set-payload").withTypeDefinition(fromType(SetPayloadMessageProcessor.class))
-            .withSetterParameterDefinition("value", fromSimpleParameter("value").build())
-            .withSetterParameterDefinition("mimeType", fromSimpleParameter("mimeType").build())
-            .withSetterParameterDefinition("encoding", fromSimpleParameter("encoding").build()).build());
 
     return componentBuildingDefinitions;
   }
