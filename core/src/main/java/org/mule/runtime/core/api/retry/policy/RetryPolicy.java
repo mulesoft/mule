264b5e6592bbda962577f2489008681e6a0702c8
Daniel Feist <dfeist@gmail.com>
2017-07-17 17:40:02 +0100

    MULE-12180 Remove async until successful functionality and make sync … (#5270)

--- core/src/main/java/org/mule/runtime/core/api/retry/policy/RetryPolicy.java
+++ core/src/main/java/org/mule/runtime/core/api/retry/policy/RetryPolicy.java
@@ -7,6 +7,7 @@
 package org.mule.runtime.core.api.retry.policy;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 import org.reactivestreams.Publisher;
@@ -27,20 +28,21 @@ public interface RetryPolicy {
 
 
   /**
-   * Applies the retry policy in a non blocking manner by transforming
-   * the given {@code publisher} into one configured to apply the retry
-   * logic.
+   * Applies the retry policy in a non blocking manner by transforming the given {@code publisher} into one configured to apply
+   * the retry logic.
    *
-   * @param publisher   a publisher with the items which might fail
+   * @param publisher a publisher with the items which might fail
    * @param shouldRetry a predicate which evaluates each item to know if it should be retried or not
    * @param onExhausted an action to perform when the retry action has been exhausted
-   * @param <T>         the generic type of the publisher's content
+   * @param errorFunction function used to map cause exception to exception emitted by retry policy.
+   * @param <T> the generic type of the publisher's content
    * @return a {@link Publisher} configured with the retry policy.
    * @since 4.0
    */
   default <T> Publisher<T> applyPolicy(Publisher<T> publisher,
                                        Predicate<Throwable> shouldRetry,
-                                       Consumer<Throwable> onExhausted) {
+                                       Consumer<Throwable> onExhausted,
+                                       Function<Throwable, Throwable> errorFunction) {
     return publisher;
   }
 
--- core/src/main/java/org/mule/runtime/core/routing/UntilSuccessful.java
+++ core/src/main/java/org/mule/runtime/core/routing/UntilSuccessful.java
@@ -6,25 +6,48 @@
  */
 package org.mule.runtime.core.routing;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
+import static java.lang.Thread.sleep;
+import static java.util.Collections.singletonList;
+import static java.util.Optional.ofNullable;
 import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
-import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.initialiseIfNeeded;
 import static org.mule.runtime.core.api.processor.MessageProcessors.newChain;
-import static org.mule.runtime.core.api.processor.MessageProcessors.newExplicitChain;
-import static org.mule.runtime.dsl.api.component.config.ComponentLocationUtils.getFlowNameFrom;
+import static org.mule.runtime.core.api.processor.MessageProcessors.processToApply;
+import static org.mule.runtime.core.api.processor.MessageProcessors.processToApplyWithChildContext;
+import static org.mule.runtime.core.api.processor.MessageProcessors.processWithChildContext;
+import static org.mule.runtime.core.api.transaction.TransactionCoordination.isTransactionActive;
+import static org.mule.runtime.core.api.util.ExceptionUtils.getMessagingExceptionCause;
+import static org.mule.runtime.core.routing.outbound.AbstractOutboundRouter.DEFAULT_FAILURE_EXPRESSION;
+import static reactor.core.publisher.Flux.empty;
+import static reactor.core.publisher.Flux.error;
+import static reactor.core.publisher.Flux.from;
+import static reactor.core.publisher.Flux.just;
 
 import org.mule.runtime.api.exception.MuleException;
+import org.mule.runtime.api.exception.MuleRuntimeException;
+import org.mule.runtime.api.i18n.I18nMessage;
 import org.mule.runtime.api.lifecycle.InitialisationException;
-import org.mule.runtime.api.lifecycle.Startable;
-import org.mule.runtime.api.lifecycle.Stoppable;
-import org.mule.runtime.api.util.Preconditions;
+import org.mule.runtime.api.scheduler.Scheduler;
 import org.mule.runtime.core.api.Event;
-import org.mule.runtime.core.api.exception.MessagingExceptionHandlerAware;
+import org.mule.runtime.core.api.construct.Pipeline;
+import org.mule.runtime.core.api.exception.MessagingException;
 import org.mule.runtime.core.api.processor.MessageProcessorChain;
 import org.mule.runtime.core.api.processor.Processor;
+import org.mule.runtime.core.api.processor.ReactiveProcessor;
+import org.mule.runtime.core.api.processor.Scope;
+import org.mule.runtime.core.api.retry.policy.RetryPolicyExhaustedException;
+import org.mule.runtime.core.api.retry.policy.SimpleRetryPolicyTemplate;
 import org.mule.runtime.core.api.store.ListableObjectStore;
-import org.mule.runtime.core.internal.exception.MessagingExceptionHandlerToSystemAdapter;
-import org.mule.runtime.core.routing.outbound.AbstractOutboundRouter;
+import org.mule.runtime.core.api.util.ExceptionUtils;
+import org.mule.runtime.core.processor.AbstractMuleObjectOwner;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
 
 /**
  * UntilSuccessful attempts to route a message to the message processor it contains. Routing is considered successful if no
@@ -38,183 +61,151 @@ import org.mule.runtime.core.routing.outbound.AbstractOutboundRouter;
  * for processing. UntilSuccessful is backed by a {@link ListableObjectStore} for storing the events that are pending
  * (re)processing.
  */
-public class UntilSuccessful extends AbstractOutboundRouter implements UntilSuccessfulConfiguration {
+public class UntilSuccessful extends AbstractMuleObjectOwner implements Scope {
 
-  public static final String PROCESS_ATTEMPT_COUNT_PROPERTY_NAME = "process.attempt.count";
-  static final int DEFAULT_PROCESS_ATTEMPT_COUNT_PROPERTY_VALUE = 1;
+  private static final Logger LOGGER = LoggerFactory.getLogger(UntilSuccessful.class);
+
+  private static final String UNTIL_SUCCESSFUL_MSG_PREFIX =
+      "'until-successful' retries exhausted. Last exception message was: %s";
+  private static final String EXPRESSION_FAILED_MSG = "Failure expression positive when processing event: ";
   private static final long DEFAULT_MILLIS_BETWEEN_RETRIES = 60 * 1000;
+  private static final int DEFAULT_RETRIES = 5;
 
-  private ListableObjectStore<Event> objectStore;
-  private int maxRetries = 5;
-  private Long millisBetweenRetries = null;
-  private Long secondsBetweenRetries = null;
+  private int maxRetries = DEFAULT_RETRIES;
+  private Long millisBetweenRetries = DEFAULT_MILLIS_BETWEEN_RETRIES;
   private String failureExpression = DEFAULT_FAILURE_EXPRESSION;
-  private String ackExpression;
-  private String eventKeyPrefix;
-  protected Processor dlqMP;
-  private boolean synchronous = false;
-  private UntilSuccessfulProcessingStrategy untilSuccessfulStrategy;
+  private MessageProcessorChain nestedChain;
+  private Predicate<Event> shouldRetry;
+  private SimpleRetryPolicyTemplate policyTemplate;
+  private Scheduler timer;
 
   @Override
   public void initialise() throws InitialisationException {
-    if (routes.isEmpty()) {
-      throw new InitialisationException(createStaticMessage("One message processor must be configured within UntilSuccessful."),
+    if (nestedChain == null) {
+      throw new InitialisationException(createStaticMessage("One message processor must be configured within 'until-successful'."),
                                         this);
     }
-
-    if (routes.size() > 1) {
-      throw new InitialisationException(createStaticMessage("Only one message processor is allowed within UntilSuccessful."
-          + " Use a Processor Chain to group several message processors into one."), this);
-    }
-
-    setWaitTime();
-
-    if (messagingExceptionHandler == null) {
-      messagingExceptionHandler = new MessagingExceptionHandlerToSystemAdapter(muleContext);
-    }
-
     super.initialise();
+    timer = muleContext.getSchedulerService().cpuLightScheduler();
+    policyTemplate =
+        new SimpleRetryPolicyTemplate(millisBetweenRetries, maxRetries, timer);
+    shouldRetry = event -> (muleContext.getExpressionManager().evaluateBoolean(failureExpression, event,
+                                                                               getLocation(), false, true));
+  }
 
-    if ((ackExpression != null) && (!muleContext.getExpressionManager().isExpression(ackExpression))) {
-      throw new InitialisationException(createStaticMessage("Invalid ackExpression: " + ackExpression), this);
-    }
+  @Override
+  public void dispose() {
+    super.dispose();
+    timer.stop();
+  }
 
-    if (synchronous) {
-      this.untilSuccessfulStrategy = new SynchronousUntilSuccessfulProcessingStrategy();
+  @Override
+  public Event process(Event event) throws MuleException {
+    return processToApply(event, this);
+  }
+
+  @Override
+  public Publisher<Event> apply(Publisher<Event> publisher) {
+    return from(publisher)
+        .flatMap(event -> Mono
+            .from(processWithChildContext(event, scheduleRoute(p -> Mono.from(p)
+                .transform(nestedChain)
+                .doOnNext(result -> {
+                  if (shouldRetry.test(result)) {
+                    throw new FailureExpressionAssertionException(createStaticMessage(EXPRESSION_FAILED_MSG + event));
+                  }
+                })), ofNullable(getLocation())))
+            .transform(p -> policyTemplate.applyPolicy(p, getRetryPredicate(), e -> {
+            }, getThrowableFunction(event))));
+  }
+
+  private Predicate<Throwable> getRetryPredicate() {
+    return e -> e instanceof FailureExpressionAssertionException
+        || (e instanceof MessagingException && shouldRetry.test(((MessagingException) e).getEvent()));
+  }
+
+  private Function<Throwable, Throwable> getThrowableFunction(Event event) {
+    return throwable -> {
+      Throwable cause = getMessagingExceptionCause(throwable);
+      return new MessagingException(event,
+                                    new RetryPolicyExhaustedException(createStaticMessage(UNTIL_SUCCESSFUL_MSG_PREFIX,
+                                                                                          cause.getMessage()),
+                                                                      cause, this),
+                                    this);
+    };
+  }
+
+  private ReactiveProcessor scheduleRoute(ReactiveProcessor route) {
+    if (flowConstruct instanceof Pipeline) {
+      return publisher -> from(publisher).transform(((Pipeline) flowConstruct).getProcessingStrategy().onPipeline(route));
     } else {
-      this.untilSuccessfulStrategy = new AsynchronousUntilSuccessfulProcessingStrategy();
-      ((MessagingExceptionHandlerAware) this.untilSuccessfulStrategy).setMessagingExceptionHandler(messagingExceptionHandler);
-    }
-    this.untilSuccessfulStrategy.setUntilSuccessfulConfiguration(this);
-
-    initialiseIfNeeded(untilSuccessfulStrategy, muleContext);
-    eventKeyPrefix = getFlowNameFrom(getLocation()) + "-" + muleContext.getClusterId() + "-";
-  }
-
-  private void setWaitTime() {
-    boolean hasSeconds = secondsBetweenRetries != null;
-    boolean hasMillis = millisBetweenRetries != null;
-
-    Preconditions
-        .checkArgument(!(hasSeconds && hasMillis),
-                       "Can't specify millisBetweenRetries and secondsBetweenRetries properties at the same time. Please specify only one and remember that secondsBetweenRetries is deprecated.");
-
-    if (hasSeconds) {
-      logger
-          .warn("You're using the secondsBetweenRetries in the until-successful router. That attribute was deprecated in favor of the new millisBetweenRetries."
-              + "Please consider updating your config since the old attribute will be removed in Mule 4");
-
-      setMillisBetweenRetries(SECONDS.toMillis(secondsBetweenRetries));
-    } else if (!hasMillis) {
-      millisBetweenRetries = DEFAULT_MILLIS_BETWEEN_RETRIES;
+      return publisher -> publisher;
     }
   }
 
-  @Override
-  public void start() throws MuleException {
-    super.start();
-    if (untilSuccessfulStrategy instanceof Startable) {
-      ((Startable) untilSuccessfulStrategy).start();
-    }
-  }
-
-  @Override
-  public void stop() throws MuleException {
-    if (untilSuccessfulStrategy instanceof Stoppable) {
-      ((Stoppable) untilSuccessfulStrategy).stop();
-    }
-    super.stop();
-  }
-
-  @Override
-  public boolean isMatch(final Event event, Event.Builder builder) throws MuleException {
-    return true;
-  }
-
-  @Override
-  protected Event route(final Event event) throws MuleException {
-    return untilSuccessfulStrategy.route(event, flowConstruct);
-  }
-
-  @Override
-  public ListableObjectStore<Event> getObjectStore() {
-    return objectStore;
-  }
-
-  public void setObjectStore(final ListableObjectStore<Event> objectStore) {
-    this.objectStore = objectStore;
-  }
-
-  @Override
+  /**
+   * @return the number of retries to process the route when failing. Default value is 5.
+   */
   public int getMaxRetries() {
     return maxRetries;
   }
 
+  /**
+   *
+   * @param maxRetries the number of retries to process the route when failing. Default value is 5.
+   */
   public void setMaxRetries(final int maxRetries) {
     this.maxRetries = maxRetries;
   }
 
   /**
-   * @deprecated use {@link #setMillisBetweenRetries(long)} instead
-   * @param secondsBetweenRetries the number of seconds to wait between retries
+   * @return the number of milliseconds between retries. Default value is 60000.
    */
-  @Deprecated
-  public void setSecondsBetweenRetries(final long secondsBetweenRetries) {
-    this.secondsBetweenRetries = secondsBetweenRetries;
-  }
-
-  @Override
   public long getMillisBetweenRetries() {
     return millisBetweenRetries;
   }
 
+  /**
+   * @param millisBetweenRetries the number of milliseconds between retries. Default value is 60000.
+   */
   public void setMillisBetweenRetries(long millisBetweenRetries) {
     this.millisBetweenRetries = millisBetweenRetries;
   }
 
-  @Override
+  /**
+   * @return Expression to determine if the message was processed successfully or not. Always returns a not null value.
+   */
   public String getFailureExpression() {
     return failureExpression;
   }
 
+  /**
+   * @param failureExpression Expression to determine if the message was processed successfully or not. Always returns a not null
+   *        value.
+   */
   public void setFailureExpression(final String failureExpression) {
     this.failureExpression = failureExpression;
   }
 
-  @Override
-  public String getAckExpression() {
-    return ackExpression;
-  }
-
-  public void setAckExpression(final String ackExpression) {
-    this.ackExpression = ackExpression;
-  }
-
-  public String getEventKeyPrefix() {
-    return eventKeyPrefix;
+  /**
+   * Configure the nested {@link Processor}'s that error handling and transactional behaviour should be applied to.
+   *
+   * @param processors
+   */
+  public void setMessageProcessors(List<Processor> processors) {
+    this.nestedChain = newChain(processors);
   }
 
   @Override
-  public Processor getDlqMP() {
-    return dlqMP;
+  protected List<Object> getOwnedObjects() {
+    return singletonList(nestedChain);
   }
 
-  @Override
-  public Processor getRoute() {
-    final MessageProcessorChain chain = newChain(newExplicitChain(routes.get(0)));
-    chain.setMuleContext(muleContext);
-    chain.setFlowConstruct(flowConstruct);
-    return chain;
-  }
+  private static class FailureExpressionAssertionException extends MuleRuntimeException {
 
-  @Override
-  public AbstractOutboundRouter getRouter() {
-    return this;
+    public FailureExpressionAssertionException(I18nMessage message) {
+      super(message);
+    }
   }
-
-  public void setSynchronous(boolean synchronous) {
-    this.synchronous = synchronous;
-  }
-
 }
 
