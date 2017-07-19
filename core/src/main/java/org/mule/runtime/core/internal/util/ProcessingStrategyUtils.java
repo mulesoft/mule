fa7609a60261ee381265ca541f068574420821e8
luciano.raineri <luciano.raineri@mulesoft.com>
2017-07-19 13:52:12 -0300

    removed processing strategy editro and parsing function from ProcessingStrategyUtils

--- core/src/main/java/org/mule/runtime/core/internal/util/ProcessingStrategyUtils.java
+++ core/src/main/java/org/mule/runtime/core/internal/util/ProcessingStrategyUtils.java
@@ -15,19 +15,7 @@ import org.mule.runtime.core.processor.strategy.BlockingProcessingStrategyFactor
 import org.mule.runtime.core.processor.strategy.TransactionAwareWorkQueueProcessingStrategyFactory;
 
 public class ProcessingStrategyUtils {
-
-  public static String DEFAULT_PROCESSING_STRATEGY = "default";
-  public static String SYNC_PROCESSING_STRATEGY = "synchronous";
-
-  public static ProcessingStrategyFactory parseProcessingStrategy(String processingStrategy) {
-    if (DEFAULT_PROCESSING_STRATEGY.equals(processingStrategy)) {
-      return new TransactionAwareWorkQueueProcessingStrategyFactory();
-    } else if (SYNC_PROCESSING_STRATEGY.equals(processingStrategy)) {
-      return new BlockingProcessingStrategyFactory();
-    }
-    return null;
-  }
-
+  
   /**
    * Determines if the {@link FlowConstruct} based on implementation and processing strategy configured is synchronous.
    *
