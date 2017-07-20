6399a3b2e3b6987f2524e034ef1cba62b33dfa26
luciano.raineri <luciano.raineri@mulesoft.com>
2017-07-20 12:18:21 -0300

    Added some tests to IdempotentMessageValidatorTestCase

--- core-tests/src/test/java/org/mule/runtime/core/routing/IdempotentMessageValidatorTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/routing/IdempotentMessageValidatorTestCase.java
@@ -7,10 +7,13 @@
 package org.mule.runtime.core.routing;
 
 import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.rules.ExpectedException.none;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mule.runtime.api.message.Message.of;
 
 import org.mule.runtime.api.message.Message;
 import org.mule.runtime.core.api.Event;
@@ -21,21 +24,30 @@ import org.mule.runtime.core.internal.routing.IdempotentMessageValidator;
 import org.mule.tck.core.util.store.InMemoryObjectStore;
 import org.mule.tck.junit4.AbstractMuleContextTestCase;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class IdempotentMessageValidatorTestCase extends AbstractMuleContextTestCase {
 
+  private static IdempotentMessageValidator idempotent = new IdempotentMessageValidator();
+
+  @Before
+  public void reset() {
+    //Needs to create a new validator for every test because the idExpression needs to be reset and there is not way of knowing the default from the test
+    idempotent = new IdempotentMessageValidator();
+    idempotent.setStorePrefix("foo");
+    idempotent.setObjectStore(new InMemoryObjectStore<String>());
+    idempotent.setMuleContext(muleContext);
+  }
+
   @Rule
   public ExpectedException expected = none();
 
   @Test
   public void idempotentReceiver() throws Exception {
-    IdempotentMessageValidator idempotent = new IdempotentMessageValidator();
-    idempotent.setMuleContext(muleContext);
-    idempotent.setStorePrefix("foo");
-    idempotent.setObjectStore(new InMemoryObjectStore<String>());
 
     final EventContext contextA = mock(EventContext.class);
     when(contextA.getCorrelationId()).thenReturn("1");
@@ -52,6 +64,57 @@ public class IdempotentMessageValidatorTestCase extends AbstractMuleContextTestC
 
     // This will not process, because the ID is a duplicate
     event = Event.builder(contextB).message(okMessage).build();
+
+    expected.expect(ValidationException.class);
+    processedEvent = idempotent.process(event);
+  }
+
+  @Test
+  public void testIdCheckWithMEL() throws Exception {
+    String melExpression = "#[payload]";
+    final EventContext context = mock(EventContext.class);
+    when(context.getCorrelationId()).thenReturn("1");
+    Message okMessage = of("OK");
+    Event event = Event.builder(context).message(okMessage).build();
+
+    //Set MEL expression to hash value
+    idempotent.setIdExpression(melExpression);
+
+    // This one will process the event on the target endpoint
+    Event processedEvent = idempotent.process(event);
+    assertNotNull(processedEvent);
+    assertEquals(idempotent.getObjectStore().retrieve("OK"),"1");
+
+    // This will not process, because the message is a duplicate
+    okMessage = of("OK");
+    event = Event.builder(context).message(okMessage).build();
+
+    expected.expect(ValidationException.class);
+    processedEvent = idempotent.process(event);
+  }
+
+  @Test
+  public void testIdCheckWithDW() throws Exception {
+    String dwExpression = "%dw 2.0\n" +
+                           "output application/text\n" +
+                           "---\n" +
+                           "payload ++ ' World'";
+    final EventContext context = mock(EventContext.class);
+    when(context.getCorrelationId()).thenReturn("1");
+    Message okMessage = of("Hello");
+    Event event = Event.builder(context).message(okMessage).build();
+
+    //Set DW expression to hash value
+    idempotent.setIdExpression(dwExpression);
+
+    // This one will process the event on the target endpoint
+    Event processedEvent = idempotent.process(event);
+    assertNotNull(processedEvent);
+    assertEquals(idempotent.getObjectStore().retrieve("Hello World"),"1");
+
+    // This will not process, because the message is a duplicate
+    okMessage = of("Hello");
+    event = Event.builder(context).message(okMessage).build();
 
     expected.expect(ValidationException.class);
     processedEvent = idempotent.process(event);
