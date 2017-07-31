b75b2da218c4132a7e5ad79fb94d734c55909183
Facundo Velazquez <facundovelazquezsantillan@gmail.com>
2017-07-31 18:45:20 +0200

    MULE-12183: Move MultipleRequestReplierEvent to its own class to avoid serialization problems. (#5354)

---
+++ core/src/main/java/org/mule/runtime/core/internal/routing/requestreply/MultipleRequestReplierEvent.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.runtime.core.internal.routing.requestreply;
+
+import org.mule.runtime.core.api.Event;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultipleRequestReplierEvent implements Serializable {
+
+  private final List<Event> muleEvents = new ArrayList<>();
+
+  protected synchronized void addEvent(Event event) {
+    muleEvents.add(event);
+  }
+
+  protected synchronized void removeEvent() {
+    muleEvents.remove(0);
+  }
+
+  protected synchronized Event getEvent() {
+    return muleEvents.get(0);
+  }
+}
