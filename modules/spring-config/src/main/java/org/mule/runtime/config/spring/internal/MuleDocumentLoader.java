99d6205b4fc73b9c4f08931c3bf7c2e8573173f0
Alejandro Pulver <alejandro.pulver@mulesoft.com>
2017-08-06 15:07:56 -0300

    MULE-13256: fix test

--- modules/spring-config/src/main/java/org/mule/runtime/config/spring/internal/MuleDocumentLoader.java
+++ modules/spring-config/src/main/java/org/mule/runtime/config/spring/internal/MuleDocumentLoader.java
@@ -19,6 +19,7 @@ import java.io.InputStream;
 import java.util.LinkedHashMap;
 import java.util.Stack;
 
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -26,6 +27,7 @@ import javax.xml.parsers.SAXParserFactory;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.xml.DefaultDocumentLoader;
 import org.springframework.beans.factory.xml.DocumentLoader;
+import org.springframework.util.xml.XmlValidationModeDetector;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.UserDataHandler;
@@ -58,7 +60,48 @@ final public class MuleDocumentLoader implements DocumentLoader {
     }
   };
 
-  private final DocumentLoader defaultLoader = new DefaultDocumentLoader();
+  /**
+   * JAXP attribute used to configure the schema language for validation.
+   */
+  private static final String SCHEMA_LANGUAGE_ATTRIBUTE = "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
+
+  /**
+   * JAXP attribute value indicating the XSD schema language.
+   */
+  private static final String XSD_SCHEMA_LANGUAGE = "http://www.w3.org/2001/XMLSchema";
+
+  private final DocumentLoader defaultLoader = new DefaultDocumentLoader() {
+
+    protected DocumentBuilderFactory createDocumentBuilderFactory(int validationMode, boolean namespaceAware)
+        throws ParserConfigurationException {
+
+      DocumentBuilderFactory factory = XMLSecureFactories.createDefault().getDocumentBuilderFactory();
+      factory.setNamespaceAware(namespaceAware);
+
+      if (validationMode != XmlValidationModeDetector.VALIDATION_NONE) {
+        factory.setValidating(true);
+        if (validationMode == XmlValidationModeDetector.VALIDATION_XSD) {
+          // Enforce namespace aware for XSD...
+          factory.setNamespaceAware(true);
+          try {
+            factory.setAttribute(SCHEMA_LANGUAGE_ATTRIBUTE, XSD_SCHEMA_LANGUAGE);
+          } catch (IllegalArgumentException ex) {
+            ParserConfigurationException pcex = new ParserConfigurationException(
+                                                                                 "Unable to validate using XSD: Your JAXP provider ["
+                                                                                     + factory +
+                                                                                     "] does not support XML Schema. Are you running on Java 1.4 with Apache Crimson? "
+                                                                                     +
+                                                                                     "Upgrade to Apache Xerces (or Java 1.5) for full XSD support.");
+            pcex.initCause(ex);
+            throw pcex;
+          }
+        }
+      }
+
+      return factory;
+    }
+  };
+
   private final XmlMetadataAnnotationsFactory metadataFactory;
 
   public MuleDocumentLoader() {
