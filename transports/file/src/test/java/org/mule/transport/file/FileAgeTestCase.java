6050ee19b3de69f1ba568f27a1266acf06bc0439
facundov <facundov@epidataconsulting.com>
2017-08-07 15:24:56 -0300

    MULE-13280: In File Endpoints, FileAge is not always honored.

--- transports/file/src/test/java/org/mule/transport/file/FileAgeTestCase.java
+++ transports/file/src/test/java/org/mule/transport/file/FileAgeTestCase.java
@@ -9,9 +9,14 @@ package org.mule.transport.file;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static java.lang.Long.parseLong;
+import static org.mule.transport.file.FileTestUtils.createDataFile;
+import static org.mule.util.FileUtils.openDirectory;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.SystemProperty;
 
+import java.io.File;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -19,8 +24,11 @@ import org.junit.Test;
 public class FileAgeTestCase extends FunctionalTestCase
 {
 
-    private static final String CONNECTOR_FILE_AGE= "60000";
-    private static final String ENDPOINT_FILE_AGE = "120000";
+    private static long DELTA_TIME = 1000;
+
+    private static final String CONNECTOR_FILE_AGE= "2000";
+
+    private static final String ENDPOINT_FILE_AGE = "3000";
 
     @Rule
     public SystemProperty fileAgeConnectorSystemProperty = new SystemProperty("fileAgeConnector", CONNECTOR_FILE_AGE);
@@ -28,10 +36,25 @@ public class FileAgeTestCase extends FunctionalTestCase
     @Rule
     public SystemProperty fileAgeEndpointSystemProperty = new SystemProperty("fileAgeEndpoint", ENDPOINT_FILE_AGE);
 
+    private File temporaryDirectory ;
+
+    private File secondTemporaryDirectory ;
+
+    private File thirdTemporaryDirectory ;
+
     @Override
     protected String getConfigFile()
     {
         return "file-age-config.xml";
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+        openDirectory(getFileInsideWorkingDirectory("test").getAbsolutePath());
+        temporaryDirectory = openDirectory(getFileInsideWorkingDirectory("test").getAbsolutePath());
+        secondTemporaryDirectory = openDirectory(getFileInsideWorkingDirectory("test2").getAbsolutePath());
+        thirdTemporaryDirectory = openDirectory(getFileInsideWorkingDirectory("test3").getAbsolutePath());
     }
 
     @Test
@@ -39,6 +62,30 @@ public class FileAgeTestCase extends FunctionalTestCase
     {
         FileConnector fileConnector = muleContext.getRegistry().lookupObject("fileConnector");
         assertThat(fileConnector.getFileAge(), is(parseLong(CONNECTOR_FILE_AGE)));
+    }
+
+    @Test
+    public void fileAgeInheritedFromConnectorIsHonored() throws Exception
+    {
+        File createdFile = createDataFile(temporaryDirectory, TEST_MESSAGE, "UTF-8");
+        Thread.sleep(parseLong(CONNECTOR_FILE_AGE) - DELTA_TIME);
+        assertThat(createdFile.exists(), is(true));
+    }
+
+    @Test
+    public void overrodeFileAgeInEndpointIsHonored() throws Exception
+    {
+        File createdFile = createDataFile(secondTemporaryDirectory, TEST_MESSAGE, "UTF-8");
+        Thread.sleep(parseLong(ENDPOINT_FILE_AGE) - DELTA_TIME);
+        assertThat(createdFile.exists(), is(true));
+    }
+
+    @Test
+    public void fileAgeInEndpointIsHonoredWhenAbsentInReferredConnector() throws Exception
+    {
+        File createdFile = createDataFile(thirdTemporaryDirectory, TEST_MESSAGE, "UTF-8");
+        Thread.sleep(parseLong(ENDPOINT_FILE_AGE) - DELTA_TIME);
+        assertThat(createdFile.exists(), is(true));
     }
 
 }
