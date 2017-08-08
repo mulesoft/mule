64a5dd2d078c3c47f3fb987ee6174757a1602ac4
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-08-08 17:03:31 -0300

    feedback

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/config/ImmutableConfigurationState.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/config/ImmutableConfigurationState.java
@@ -11,21 +11,38 @@ import org.mule.runtime.extension.api.runtime.config.ConfigurationState;
 
 import java.util.Map;
 
+/**
+ * Immutable implementation of {@link ConfigurationState}
+ *
+ * @since 4.0
+ */
 public class ImmutableConfigurationState implements ConfigurationState {
 
   private final Map<String, Object> configParameters;
   private final Map<String, Object> connectionParameters;
 
+  /**
+   * Creates a new instance
+   *
+   * @param configParameters     the config parameters
+   * @param connectionParameters the connection parameters
+   */
   public ImmutableConfigurationState(Map<String, Object> configParameters, Map<String, Object> connectionParameters) {
     this.configParameters = unmodifiableMap(configParameters);
     this.connectionParameters = unmodifiableMap(connectionParameters);
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Map<String, Object> getConfigParameters() {
     return configParameters;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Map<String, Object> getConnectionParameters() {
     return connectionParameters;
