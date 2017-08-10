2275297d87fbd057903bddd4770706edbd5d4d67
luciano.raineri <luciano.raineri@mulesoft.com>
2017-08-10 19:50:27 -0300

    formatter

--- tests/test-extensions/substitution-group-extension/src/main/java/org/mule/test/substitutiongroup/extension/SomeExportedPojo.java
+++ tests/test-extensions/substitution-group-extension/src/main/java/org/mule/test/substitutiongroup/extension/SomeExportedPojo.java
@@ -9,7 +9,8 @@ package org.mule.test.substitutiongroup.extension;
 import org.mule.runtime.extension.api.annotation.dsl.xml.TypeDsl;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
 
-@TypeDsl(substitutionGroup = "heisenberg:global-abstract-weapon", allowTopLevelDefinition = true)
+@TypeDsl(substitutionGroup = "heisenberg:global-abstract-weapon",
+    baseType = "heisenberg:org.mule.test.heisenberg.extension.model.Weapon", allowTopLevelDefinition = true)
 public class SomeExportedPojo {
 
   @Parameter
