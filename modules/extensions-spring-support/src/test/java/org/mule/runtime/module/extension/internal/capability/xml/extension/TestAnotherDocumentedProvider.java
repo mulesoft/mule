537add659145a6085963c115322d93a72162cfd6
JuanDesi <juan.desimoni@mulesoft.com>
2017-08-16 16:37:55 -0300

    MULE-13249: Annotation processor should not reference implementation classes from com.sun

--- modules/extensions-spring-support/src/test/java/org/mule/runtime/module/extension/internal/capability/xml/extension/TestAnotherDocumentedProvider.java
+++ modules/extensions-spring-support/src/test/java/org/mule/runtime/module/extension/internal/capability/xml/extension/TestAnotherDocumentedProvider.java
@@ -4,7 +4,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.runtime.module.extension.internal.capability.xml;
+package org.mule.runtime.module.extension.internal.capability.xml.extension;
 
 import static org.mule.runtime.api.connection.ConnectionValidationResult.success;
 import org.mule.runtime.api.connection.ConnectionException;
