f09a594b18c8d8dea9c6384e4678896b2f1a62c2
pablolagreca <Neruda10>
2017-08-17 15:49:57 -0300

    Revert "MULE-11669 - Define mechanism to identify the http:request of a proxy (#5512)"
    This reverts commit 3a648772d32763624ad937cfb6eb470afc0b9947.

--- modules/policy-api/src/main/java/org/mule/runtime/policy/api/OperationPolicyPointcutParametersFactory.java
+++ modules/policy-api/src/main/java/org/mule/runtime/policy/api/OperationPolicyPointcutParametersFactory.java
@@ -7,7 +7,7 @@
 package org.mule.runtime.policy.api;
 
 import org.mule.runtime.api.component.ComponentIdentifier;
-import org.mule.runtime.api.meta.AnnotatedObject;
+import org.mule.runtime.api.component.location.ComponentLocation;
 
 import java.util.Map;
 
@@ -29,11 +29,11 @@ public interface OperationPolicyPointcutParametersFactory {
   /**
    * Creates an specific {@link PolicyPointcutParameters} for a particular operation identifier by {@code operationIdentifier}.
    *
-   * @param operation the operation where the policy is being applied.
+   * @param componentLocation the component location where the operation is defined.
    * @param operationParameters set of parameters that are going to be used to execute the operation.
    * @return the pointcut parameters.
    */
-  PolicyPointcutParameters createPolicyPointcutParameters(AnnotatedObject operation,
+  PolicyPointcutParameters createPolicyPointcutParameters(ComponentLocation componentLocation,
                                                           Map<String, Object> operationParameters);
 
 }
