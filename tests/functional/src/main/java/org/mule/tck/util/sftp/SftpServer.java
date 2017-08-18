e9a3266c742198d695966a8b5ff1ada28e13a96a
facundov <facundov@epidataconsulting.com>
2017-08-18 10:04:52 -0300

    Fixing SFTPServer class

--- tests/functional/src/main/java/org/mule/tck/util/sftp/SftpServer.java
+++ tests/functional/src/main/java/org/mule/tck/util/sftp/SftpServer.java
@@ -15,10 +15,9 @@ import org.apache.sshd.common.NamedFactory;
 import org.apache.sshd.server.Command;
 import org.apache.sshd.server.SshServer;
 import org.apache.sshd.server.auth.password.PasswordAuthenticator;
-import org.apache.sshd.server.command.ScpCommandFactory;
 import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.server.scp.ScpCommandFactory;
 import org.apache.sshd.server.session.ServerSession;
-import org.apache.sshd.server.shell.ProcessShellFactory;
 import org.apache.sshd.server.subsystem.sftp.SftpSubsystemFactory;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
