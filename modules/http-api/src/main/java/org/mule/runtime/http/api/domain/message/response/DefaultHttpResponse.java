86a26b0d152fb82632aed2941a2a51b36b34f1a1
Rodro <rodrigo.merino@mulesoft.com>
2017-08-23 14:47:12 -0300

    MULE-12321: When an app with OAuth Client Credentials can't connect at deployment it doesn't show the HTTP requester response as in 3.x

--- modules/http-api/src/main/java/org/mule/runtime/http/api/domain/message/response/DefaultHttpResponse.java
+++ modules/http-api/src/main/java/org/mule/runtime/http/api/domain/message/response/DefaultHttpResponse.java
@@ -6,6 +6,8 @@
  */
 package org.mule.runtime.http.api.domain.message.response;
 
+import static java.lang.System.lineSeparator;
+
 import org.mule.runtime.api.util.MultiMap;
 import org.mule.runtime.http.api.domain.entity.HttpEntity;
 import org.mule.runtime.http.api.domain.message.BaseHttpMessage;
@@ -55,6 +57,14 @@ class DefaultHttpResponse extends BaseHttpMessage implements HttpResponse {
   @Override
   public String getReasonPhrase() {
     return this.responseStatus.getReasonPhrase();
+  }
+
+  @Override
+  public String toString() {
+    return "DefaultHttpResponse {" + lineSeparator()
+        + "  responseStatus: " + responseStatus.getStatusCode() + " (" + responseStatus.getReasonPhrase() + ")," + lineSeparator()
+        + "  headers: " + headers.toString() + lineSeparator()
+        + "}";
   }
 
 }
--- modules/oauth-api/src/main/java/org/mule/runtime/oauth/api/exception/TokenNotFoundException.java
+++ modules/oauth-api/src/main/java/org/mule/runtime/oauth/api/exception/TokenNotFoundException.java
@@ -7,9 +7,11 @@
 package org.mule.runtime.oauth.api.exception;
 
 import static java.lang.String.format;
+import static java.lang.System.lineSeparator;
 import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
 
 import org.mule.runtime.api.exception.MuleException;
+import org.mule.runtime.http.api.domain.message.response.HttpResponse;
 
 /**
  * It was not possible to retrieve the access token or the refresh token from the token URL response
@@ -18,9 +20,11 @@ public class TokenNotFoundException extends MuleException {
 
   private static final long serialVersionUID = -4527896867466127563L;
 
-  public TokenNotFoundException(Object tokenUrlResponseBody) {
-    super(createStaticMessage(format("Could not extract access token or refresh token from token URL response: %s",
-                                     tokenUrlResponseBody)));
+  public TokenNotFoundException(String tokenUrl, HttpResponse response, String body) {
+    super(createStaticMessage(format("Could not extract access token or refresh token from token URL '%s'. Response was:"
+        + lineSeparator() + "%s"
+        + lineSeparator() + "%s",
+                                     tokenUrl, response.toString(), body)));
   }
 
 }
