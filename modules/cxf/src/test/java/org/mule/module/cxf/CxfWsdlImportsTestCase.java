30837a60769093b6f1ff77ce4250e4781f1027cf
Facundo Velazquez <facundovelazquezsantillan@gmail.com>
2017-09-02 17:45:57 +0200

    MULE-7794: CXF Proxy is throwing NPE when Schemas are imported in WSDL. (#5624)

---
+++ modules/cxf/src/test/java/org/mule/module/cxf/CxfWsdlImportsTestCase.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.cxf;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.runners.Parameterized.Parameter;
+import static org.junit.runners.Parameterized.Parameters;
+
+import org.mule.api.MuleMessage;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class CxfWsdlImportsTestCase extends FunctionalTestCase
+{
+
+    private static final String VALID_REQUEST_ECHO = "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">"
+                                                     + "<soap:Body> " +
+                                                     "<echo xmlns=\"http://www.echo.org\">" +
+                                                     "<echo>Testing echo</echo>" +
+                                                     "</echo>"
+                                                     + "</soap:Body>"
+                                                     + "</soap:Envelope>";
+
+    private static final String INVALID_REQUEST_ECHO = "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">"
+                                                       + "<soap:Body> " +
+                                                       "<wrongEcho xmlns=\"http://www.echo.org\">" +
+                                                       "  <echo>Testing echo</ech>" +
+                                                       "</wrongEcho>"
+                                                       + "</soap:Body>"
+                                                       + "</soap:Envelope>";
+
+    private static final String VALID_REQUEST_LOG = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:log=\"http://www.log.org\">" +
+                                                    "<soapenv:Body>" +
+                                                    "<log:log>" +
+                                                    "<log:message>Testing Log</log:message>" +
+                                                    "</log:log>" +
+                                                    "</soapenv:Body>" +
+                                                    "</soapenv:Envelope>";
+
+    private static final String INVALID_REQUEST_LOG = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:log=\"http://www.muleumo.org\">" +
+                                                      "<soapenv:Body>" +
+                                                      "<log:wrongLog>" +
+                                                      "<log:message>Testing Log</log:message>" +
+                                                      "</log:wrongLog>" +
+                                                      "</soapenv:Body>" +
+                                                      "</soapenv:Envelope>";
+
+    private static final String ECHO_TEST_ERROR_MESSAGE = "tag name \"wrongEcho\" is not allowed";
+
+    private static final String LOG_TEST_ERROR_MESSAGE = "tag name \"wrongLog\" is not allowed";
+
+    @Rule
+    public final DynamicPort httpPort = new DynamicPort("port1");
+
+    @Parameter
+    public String validPayload;
+
+    @Parameter(1)
+    public String invalidPayload;
+
+    @Parameter(2)
+    public String errorMessage;
+
+    @Parameters
+    public static Collection<Object[]> data()
+    {
+        return Arrays.asList(new Object[][]
+                                     {
+                                             {VALID_REQUEST_ECHO, INVALID_REQUEST_ECHO, ECHO_TEST_ERROR_MESSAGE},
+                                             {VALID_REQUEST_LOG, INVALID_REQUEST_LOG, LOG_TEST_ERROR_MESSAGE}
+                                     });
+    }
+
+
+    @Override
+    protected String getConfigFile()
+    {
+        return "proxy-validation-imports-config.xml";
+    }
+
+    @Test
+    public void testValidRequest() throws Exception
+    {
+        MuleMessage response = muleContext.getClient().send("http://localhost:" + httpPort.getNumber() + "/services/test", getTestMuleMessage(validPayload));
+        assertThat(response.getPayloadAsString(), is(validPayload));
+    }
+
+    @Test
+    public void testInvalidRequest() throws Exception
+    {
+        MuleMessage response = muleContext.getClient().send("http://localhost:" + httpPort.getNumber() + "/services/test", getTestMuleMessage(invalidPayload));
+        assertThat(response.getPayloadAsString(), containsString(errorMessage));
+    }
+
+}
