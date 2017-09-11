22b1b4d5b61484e430c461308b5775958d45cd0b
Daniel Feist <dfeist@gmail.com>
2017-09-11 16:33:00 +0100

    MULE-13403 ProcessorChains are not being synchronised properly. Fix first successful behaviour.

--- core/src/main/java/org/mule/runtime/core/internal/routing/FirstSuccessfulRoutingStrategy.java
+++ core/src/main/java/org/mule/runtime/core/internal/routing/FirstSuccessfulRoutingStrategy.java
@@ -8,6 +8,7 @@ package org.mule.runtime.core.internal.routing;
 
 import static java.util.Optional.empty;
 import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
+import static org.mule.runtime.core.api.processor.MessageProcessors.processToApplyWithChildContext;
 import static org.mule.runtime.core.internal.event.DefaultEventContext.child;
 import static org.mule.runtime.core.api.config.i18n.CoreMessages.cannotCopyStreamPayload;
 import static org.mule.runtime.core.api.config.i18n.CoreMessages.objectIsNull;
@@ -21,6 +22,7 @@ import org.mule.runtime.core.api.DefaultTransformationService;
 import org.mule.runtime.core.api.connector.DispatchException;
 import org.mule.runtime.core.api.event.BaseEvent;
 import org.mule.runtime.core.api.event.BaseEventContext;
+import org.mule.runtime.core.api.processor.MessageProcessors;
 import org.mule.runtime.core.api.processor.Processor;
 import org.mule.runtime.core.api.routing.RoutingException;
 
@@ -83,9 +85,7 @@ public class FirstSuccessfulRoutingStrategy implements RoutingStrategy {
 
     for (Processor mp : messageProcessors) {
       try {
-        returnEvent =
-            processor.processRoute(mp, builder(child(((BaseEventContext) event.getContext()), empty()), event).build());
-
+        returnEvent = processToApplyWithChildContext(event, mp);
         if (returnEvent == null) {
           failed = false;
         } else if (returnEvent.getMessage() == null) {
