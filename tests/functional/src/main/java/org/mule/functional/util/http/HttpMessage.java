14b3fa84c565ec0f08bfb492691edf537828e258
santiagopurucker <santiago.purucker@mulesoft.com>
2017-09-12 19:24:45 -0300

    MULE-13496: Adding uri and query params to HttpMessage test class (#5690)

--- tests/functional/src/main/java/org/mule/functional/util/http/HttpMessage.java
+++ tests/functional/src/main/java/org/mule/functional/util/http/HttpMessage.java
@@ -6,6 +6,7 @@
  */
 package org.mule.functional.util.http;
 
+import org.mule.runtime.api.util.MultiMap;
 import org.mule.runtime.core.api.util.IOUtils;
 
 import com.google.common.collect.ImmutableMultimap;
@@ -17,7 +18,8 @@ import java.util.Set;
 public class HttpMessage {
 
   private byte[] body = new byte[0];
-
+  private String uri;
+  private MultiMap queryParams;
   private Multimap<String, String> headers = ImmutableMultimap.<String, String>builder().build();
 
   public HttpMessage(HttpExchange httpExchange) {
@@ -27,6 +29,8 @@ public class HttpMessage {
     headerNames.stream()
         .forEach(headerName -> headersBuilder.putAll(headerName, httpExchange.getRequestHeaders().get(headerName)));
     this.headers = headersBuilder.build();
+    uri = httpExchange.getRequestURI().getPath();
+    queryParams = queryToMap(httpExchange.getRequestURI().getQuery());
   }
 
   public byte[] getBody() {
@@ -35,5 +39,30 @@ public class HttpMessage {
 
   public Multimap<String, String> getHeaders() {
     return headers;
+  }
+
+
+  public MultiMap<String, String> getQueryParams() {
+    return queryParams;
+  }
+
+  public String getUri() {
+    return uri;
+  }
+
+  private MultiMap<String, String> queryToMap(String query) {
+    MultiMap<String, String> result = new MultiMap();
+    if (query == null) {
+      return result;
+    }
+    for (String param : query.split("&")) {
+      String pair[] = param.split("=");
+      if (pair.length > 1) {
+        result.put(pair[0], pair[1]);
+      } else {
+        result.put(pair[0], "");
+      }
+    }
+    return result;
   }
 }
