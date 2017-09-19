260895cb29a42b40f2fbe682de6d1f479f894c6e
estebanwasinger <estebanwasinger@gmail.com>
2017-09-19 15:11:55 -0300

    MULE-13568: None and Pooling connection providers doesn't use the Plugin ClassLoader to create connections

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/config/DefaultConnectionProviderObjectBuilder.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/config/DefaultConnectionProviderObjectBuilder.java
@@ -7,15 +7,21 @@
 package org.mule.runtime.module.extension.internal.runtime.config;
 
 import static org.mule.runtime.api.meta.model.connection.ConnectionManagementType.POOLING;
+import static org.mule.runtime.core.api.util.ClassUtils.withContextClassLoader;
+import static org.mule.runtime.module.extension.internal.util.MuleExtensionUtils.getClassLoader;
 import static org.mule.runtime.module.extension.internal.util.MuleExtensionUtils.injectRefName;
+
 import org.mule.runtime.api.config.PoolingProfile;
 import org.mule.runtime.api.connection.ConnectionProvider;
 import org.mule.runtime.api.exception.MuleException;
 import org.mule.runtime.api.meta.model.ExtensionModel;
 import org.mule.runtime.api.meta.model.connection.ConnectionManagementType;
 import org.mule.runtime.api.meta.model.connection.ConnectionProviderModel;
-import org.mule.runtime.core.api.MuleContext;
+import org.mule.runtime.api.util.LazyValue;
 import org.mule.runtime.api.util.Pair;
+import org.mule.runtime.api.util.Reference;
+import org.mule.runtime.core.api.MuleContext;
+import org.mule.runtime.core.internal.connection.DefaultConnectionProviderWrapper;
 import org.mule.runtime.core.internal.connection.ErrorTypeHandlerConnectionProviderWrapper;
 import org.mule.runtime.core.internal.connection.PoolingConnectionProviderWrapper;
 import org.mule.runtime.core.internal.connection.ReconnectableConnectionProviderWrapper;
@@ -23,6 +29,12 @@ import org.mule.runtime.core.internal.retry.ReconnectionConfig;
 import org.mule.runtime.module.extension.internal.runtime.objectbuilder.ResolverSetBasedObjectBuilder;
 import org.mule.runtime.module.extension.internal.runtime.resolver.ResolverSet;
 import org.mule.runtime.module.extension.internal.runtime.resolver.ResolverSetResult;
+
+import java.lang.reflect.Method;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
 
 /**
  * Implementation of {@link ResolverSetBasedObjectBuilder} which produces instances of {@link ConnectionProviderModel}
@@ -49,6 +61,7 @@ public class DefaultConnectionProviderObjectBuilder<C> extends ConnectionProvide
 
     provider = applyConnectionManagement(provider);
     provider = applyErrorHandling(provider);
+    provider = getExtensionClassloaderAwareConnectionProvider(provider);
 
     return new Pair<>(provider, result);
   }
@@ -71,6 +84,52 @@ public class DefaultConnectionProviderObjectBuilder<C> extends ConnectionProvide
     } else {
       provider = new ReconnectableConnectionProviderWrapper<>(provider, reconnectionConfig);
     }
+    return provider;
+  }
+
+  /**
+   * Wraps the {@link ConnectionProvider} inside of a dynamic proxy which changes the current {@link ClassLoader} to the
+   * the extension's {@link ClassLoader} when executing any method of this wrapped {@link ConnectionProvider}
+   * <p>
+   * This ensures that every time that the {@link ConnectionProvider} is used, it will work in the correct classloader.
+   * <p>
+   * Although if the {@link ConnectionProvider} is created with the correct classloader and then used with an incorrect
+   * one this may work, due that static class references were loaded correctly, logic loading class in a dynamic way
+   * will fail.
+   *
+   * @param provider The {@link ConnectionProvider} to wrap
+   * @return The wrapped {@link ConnectionProvider}
+   */
+  private ConnectionProvider<C> getExtensionClassloaderAwareConnectionProvider(ConnectionProvider<C> provider) {
+    Enhancer enhancer = new Enhancer();
+    enhancer.setSuperclass(DefaultConnectionProviderWrapper.class);
+    enhancer.setCallback(new MethodInterceptor() {
+
+      LazyValue<ClassLoader> extensionClassLoader = new LazyValue<>(() -> getClassLoader(extensionModel));
+
+      @Override
+      public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        Reference<Object> resultReference = new Reference<>();
+        Reference<Throwable> errorReference = new Reference<>();
+
+        withContextClassLoader(extensionClassLoader.get(), () -> {
+          try {
+            resultReference.set(methodProxy.invokeSuper(o, objects));
+          } catch (Throwable t) {
+            errorReference.set(t);
+          }
+        });
+
+        if (errorReference.get() != null) {
+          throw errorReference.get();
+        } else {
+          return resultReference.get();
+        }
+      }
+    });
+
+    provider = (ConnectionProvider<C>) enhancer.create(new Class[] {ConnectionProvider.class, MuleContext.class},
+                                                       new Object[] {provider, muleContext});
     return provider;
   }
 
