68518cba2b70bfd1e1e294152748916946bac4c3
Juan Lautaro Fernandez <fernandez.lautaro@gmail.com>
2017-09-20 18:46:30 -0300

    MULE-13569: 'java.io.IOException: Pipe closed' when using smart connector FIX TESTS, FIX TESTS (#5742)
    * MULE-13569: process module with child context
    
    MULE-13569: process module with child context, part 2, gracias anita.
    
    MULE-13569: process module with child context, cleaning dead code
    
    * fixes after PLGs review

--- tests/allure/src/main/java/org/mule/test/allure/AllureConstants.java
+++ tests/allure/src/main/java/org/mule/test/allure/AllureConstants.java
@@ -502,4 +502,17 @@ public interface AllureConstants {
 
   }
 
+
+  interface XmlSdk {
+
+    String XML_SDK = "Xml SDK";
+
+    interface Streaming {
+
+      String STREAMING = "Streaming consumption in operations";
+
+    }
+
+  }
+
 }
