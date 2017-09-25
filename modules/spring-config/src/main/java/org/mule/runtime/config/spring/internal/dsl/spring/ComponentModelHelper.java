d0283e1ee79f57123e6a5045f393c14bbdc01138
pablolagreca <Neruda10>
2017-09-25 11:43:55 -0300

    MULE-11902 - Add support for flow paths in batch scope

--- modules/spring-config/src/main/java/org/mule/runtime/config/spring/internal/dsl/spring/ComponentModelHelper.java
+++ modules/spring-config/src/main/java/org/mule/runtime/config/spring/internal/dsl/spring/ComponentModelHelper.java
@@ -14,6 +14,7 @@ import static org.mule.runtime.config.spring.api.dsl.model.ApplicationModel.FLOW
 import org.mule.runtime.api.component.TypedComponentIdentifier;
 import org.mule.runtime.api.component.Component;
 import org.mule.runtime.config.spring.api.dsl.model.ComponentModel;
+import org.mule.runtime.config.spring.internal.dsl.model.ComponentLocationVisitor;
 import org.mule.runtime.config.spring.internal.dsl.model.SpringComponentModel;
 import org.mule.runtime.core.api.processor.InterceptingMessageProcessor;
 import org.mule.runtime.core.api.processor.Processor;
@@ -158,6 +159,8 @@ public class ComponentModelHelper {
   }
 
   public static boolean isRouter(ComponentModel componentModel) {
-    return isOfType(componentModel, Router.class) || isOfType(componentModel, AbstractSelectiveRouter.class);
+    return isOfType(componentModel, Router.class) || isOfType(componentModel, AbstractSelectiveRouter.class)
+        || ComponentLocationVisitor.BATCH_JOB_COMPONENT_IDENTIFIER.equals(componentModel.getIdentifier())
+        || ComponentLocationVisitor.BATCH_PROCESSS_RECORDS_COMPONENT_IDENTIFIER.equals(componentModel.getIdentifier());
   }
 }
