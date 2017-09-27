6bed6d6e525b5232a84cd3d3df3ba9eaa24f2efb
Gustavo Adolfo Rodriguez <gustavo.libreros@mulesoft.com>
2017-09-27 11:01:17 -0500

    MULE-10962: Migrate Basic Tests from JMS transport to JMS Extension
     - Convenient JUnit rule for setting system properties determined by a supplier

---
+++ tests/unit/src/main/java/org/mule/tck/junit4/rule/SystemPropertyLambda.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.tck.junit4.rule;
+
+import java.util.function.Supplier;
+
+/**
+ * Allows using a supplier for resolving the value of the system property to set.
+ */
+public class SystemPropertyLambda extends SystemProperty {
+
+  private Supplier<String> propertySupplier;
+
+  public SystemPropertyLambda(String name, Supplier<String> propertySupplier) {
+    super(name);
+    this.propertySupplier = propertySupplier;
+  }
+
+  @Override
+  public String getValue() {
+    return propertySupplier.get();
+  }
+}
