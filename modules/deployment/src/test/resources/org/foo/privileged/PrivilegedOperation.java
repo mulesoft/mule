1a32fa24e1cd09f0f1bbc0ef91579fc6b9dd2402
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-09-28 13:55:19 -0300

    MULE-13637: String and Binary operations should always have a mime type - fix tests

--- modules/deployment/src/test/resources/org/foo/privileged/PrivilegedOperation.java
+++ modules/deployment/src/test/resources/org/foo/privileged/PrivilegedOperation.java
@@ -7,12 +7,15 @@
 
 package org.foo.hello;
 
+import static org.mule.runtime.extension.api.annotation.param.MediaType.TEXT_PLAIN;
 import org.mule.runtime.extension.api.annotation.param.Config;
+import org.mule.runtime.extension.api.annotation.param.MediaType;
 
 public class PrivilegedOperation {
 
   public PrivilegedOperation() {}
 
+  @MediaType(value = TEXT_PLAIN, strict = false)
   public String printMessage(@Config PrivilegedExtension config) {
     System.out.println("Test privileged extension says: " + config.getMessage());
     return config.getMessage();
