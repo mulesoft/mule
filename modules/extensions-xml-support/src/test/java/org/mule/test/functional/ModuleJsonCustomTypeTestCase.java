33f594f4241e84da7fab8b866335852dc1005701
fernandezlautaro <fernandez.lautaro@gmail.com>
2017-10-04 15:35:43 -0300

    MULE-13664: adding tests showing the datatype is correctly set after calling operations

--- modules/extensions-xml-support/src/test/java/org/mule/test/functional/ModuleJsonCustomTypeTestCase.java
+++ modules/extensions-xml-support/src/test/java/org/mule/test/functional/ModuleJsonCustomTypeTestCase.java
@@ -6,16 +6,24 @@
  */
 package org.mule.test.functional;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.core.Is.is;
-
-import org.mule.runtime.core.api.event.CoreEvent;
-
+import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
+import org.mule.runtime.core.api.event.CoreEvent;
 
 import java.util.Map;
 
 public class ModuleJsonCustomTypeTestCase extends AbstractXmlExtensionMuleArtifactFunctionalTestCase {
+
+  private static final Map<String, Object> EXPECTED_TYPE_1 = ImmutableMap.of("street_type", "Avenue",
+                                                                             "street_name", "calle 7");
+
+  private static final Map<String, Object> EXPECTED_TYPE_2 = ImmutableMap.of("firstName", "Dardo",
+                                                                             "lastName", "Rocha",
+                                                                             "age", 83);
 
   @Override
   protected String getModulePath() {
@@ -29,10 +37,7 @@ public class ModuleJsonCustomTypeTestCase extends AbstractXmlExtensionMuleArtifa
 
   @Test
   public void testSendingJsonType1FromMap() throws Exception {
-    final Map<String, Object> payload = new java.util.HashMap<>();
-    payload.put("street_type", "Avenue");
-    payload.put("street_name", "calle 7");
-    final CoreEvent muleEvent = flowRunner("testIsJsonType1FromPayloadFlow").withPayload(payload).run();
+    final CoreEvent muleEvent = flowRunner("testIsJsonType1FromPayloadFlow").withPayload(EXPECTED_TYPE_1).run();
     assertIsJsonType1(muleEvent);
   }
 
@@ -44,11 +49,7 @@ public class ModuleJsonCustomTypeTestCase extends AbstractXmlExtensionMuleArtifa
 
   @Test
   public void testSendingJsonType2FromMap() throws Exception {
-    final Map<String, Object> payload = new java.util.HashMap<>();
-    payload.put("firstName", "Dardo");
-    payload.put("lastName", "Rocha");
-    payload.put("age", 83);
-    final CoreEvent muleEvent = flowRunner("testIsJsonType2FromPayloadFlow").withPayload(payload).run();
+    final CoreEvent muleEvent = flowRunner("testIsJsonType2FromPayloadFlow").withPayload(EXPECTED_TYPE_2).run();
     assertIsJsonType2(muleEvent);
   }
 
@@ -56,6 +57,24 @@ public class ModuleJsonCustomTypeTestCase extends AbstractXmlExtensionMuleArtifa
   public void testSendingJsonType2FromExpression() throws Exception {
     final CoreEvent muleEvent = flowRunner("testIsJsonType2FromExpressionFlow").run();
     assertIsJsonType2(muleEvent);
+  }
+
+  @Test
+  public void testHardcodedType1Flow() throws Exception {
+    final CoreEvent muleEvent = flowRunner("testHardcodedType1Flow").run();
+    assertThat(muleEvent.getMessage().getPayload().getValue(), instanceOf(Map.class));
+    for (Map.Entry<String, Object> entry : EXPECTED_TYPE_1.entrySet()) {
+      assertThat((Map<String, Object>) muleEvent.getMessage().getPayload().getValue(),
+                 hasEntry(entry.getKey(), entry.getValue()));
+    }
+  }
+
+  @Test
+  public void testHardcodedType1AndExtractFieldsInVarsFlow() throws Exception {
+    final CoreEvent muleEvent = flowRunner("testHardcodedType1AndExtractFieldsInVarsFlow").run();
+    for (Map.Entry<String, Object> entry : EXPECTED_TYPE_1.entrySet()) {
+      assertThat(muleEvent.getVariables().get("extracted-" + entry.getKey()).getValue(), is(entry.getValue()));
+    }
   }
 
   private void assertIsJsonType1(CoreEvent muleEvent) {
@@ -67,5 +86,4 @@ public class ModuleJsonCustomTypeTestCase extends AbstractXmlExtensionMuleArtifa
     assertThat(muleEvent.getMessage().getPayload().getValue(),
                is("{\n  \"firstName\": \"Dardo\",\n  \"lastName\": \"Rocha\",\n  \"age\": 83\n}"));
   }
-
 }
