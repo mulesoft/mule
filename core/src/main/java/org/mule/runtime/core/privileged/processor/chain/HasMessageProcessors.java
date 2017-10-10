46250a9edc46d2c6c984704dca458e7876edf8f9
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-10-10 15:01:23 -0300

    feedback

--- core/src/main/java/org/mule/runtime/core/privileged/processor/chain/HasMessageProcessors.java
+++ core/src/main/java/org/mule/runtime/core/privileged/processor/chain/HasMessageProcessors.java
@@ -10,6 +10,11 @@ import org.mule.runtime.core.api.processor.Processor;
 
 import java.util.List;
 
+/**
+ * Contract interface for a class which can return a list of {@link Processor}
+ *
+ * @since 4.0
+ */
 public interface HasMessageProcessors {
 
   List<Processor> getMessageProcessors();
