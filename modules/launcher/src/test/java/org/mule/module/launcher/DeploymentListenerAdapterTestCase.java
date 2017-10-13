083f6736d14c9ee2ea45d9e9d5f5d646f92278e3
Facundo Velazquez <facundovelazquezsantillan@gmail.com>
2017-10-13 20:48:58 +0200

    MULE-13718: ClusterCoreExtensions are not being loaded as domain deployment listeners. (#5928)

---
+++ modules/launcher/src/test/java/org/mule/module/launcher/DeploymentListenerAdapterTestCase.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.launcher;
+
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mule.config.bootstrap.ArtifactType.ALL;
+import static org.mule.config.bootstrap.ArtifactType.APP;
+import static org.mule.config.bootstrap.ArtifactType.DOMAIN;
+import static org.mule.module.launcher.DeploymentListenerAdapter.UNSUPPORTED_ARTIFACT_TYPE_ERROR;
+
+import org.mule.api.MuleContext;
+import org.mule.config.bootstrap.ArtifactType;
+import org.mule.tck.junit4.AbstractMuleTestCase;
+import org.mule.tck.size.SmallTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@SmallTest
+@RunWith(MockitoJUnitRunner.class)
+public class DeploymentListenerAdapterTestCase extends AbstractMuleTestCase
+{
+
+    private static final String ARTIFACT_NAME = "artifactName";
+
+    @Rule
+    public  ExpectedException expectedException = ExpectedException.none();
+
+    @Mock
+    private ArtifactDeploymentListener listener;
+
+    @Mock
+    private MuleContext context;
+
+
+    @Test
+    public void adapt() throws Exception
+    {
+        DeploymentListener applicationDeploymentListener = new DeploymentListenerAdapter(listener, APP);
+        DeploymentListener domainDeploymentListener = new DeploymentListenerAdapter(listener, DOMAIN);
+        assertDeploymentListenerInvocations(applicationDeploymentListener, APP);
+        assertDeploymentListenerInvocations(domainDeploymentListener, DOMAIN);
+    }
+
+    @Test
+    public void invalidArtifactType()
+    {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(UNSUPPORTED_ARTIFACT_TYPE_ERROR);
+        new DeploymentListenerAdapter(listener, ALL);
+    }
+
+    private void assertDeploymentListenerInvocations(DeploymentListener deploymentListener, ArtifactType artifactType)
+    {
+        notifyDeploymentEvents(deploymentListener);
+        verifyArtifactDeploymentListenerExecutions(artifactType);
+        reset(listener);
+    }
+
+    private void notifyDeploymentEvents(DeploymentListener deploymentListener)
+    {
+        deploymentListener.onDeploymentStart(ARTIFACT_NAME);
+        deploymentListener.onDeploymentFailure(ARTIFACT_NAME, null);
+        deploymentListener.onDeploymentSuccess(ARTIFACT_NAME);
+        deploymentListener.onUndeploymentStart(ARTIFACT_NAME);
+        deploymentListener.onUndeploymentFailure(ARTIFACT_NAME, null);
+        deploymentListener.onUndeploymentSuccess(ARTIFACT_NAME);
+        deploymentListener.onMuleContextCreated(ARTIFACT_NAME, context);
+        deploymentListener.onMuleContextConfigured(ARTIFACT_NAME, context);
+        deploymentListener.onMuleContextInitialised(ARTIFACT_NAME, context);
+    }
+
+    private void verifyArtifactDeploymentListenerExecutions (ArtifactType artifactType)
+    {
+        verify(listener).onDeploymentStart(artifactType, ARTIFACT_NAME);
+        verify(listener).onDeploymentFailure(artifactType, ARTIFACT_NAME, null);
+        verify(listener).onDeploymentSuccess(artifactType, ARTIFACT_NAME);
+        verify(listener).onUndeploymentStart(artifactType, ARTIFACT_NAME);
+        verify(listener).onUndeploymentFailure(artifactType, ARTIFACT_NAME, null);
+        verify(listener).onUndeploymentSuccess(artifactType, ARTIFACT_NAME);
+        verify(listener).onMuleContextCreated(ARTIFACT_NAME, context);
+        verify(listener).onMuleContextConfigured(ARTIFACT_NAME, context);
+        verify(listener).onMuleContextInitialised(ARTIFACT_NAME, context);
+    }
+
+}
