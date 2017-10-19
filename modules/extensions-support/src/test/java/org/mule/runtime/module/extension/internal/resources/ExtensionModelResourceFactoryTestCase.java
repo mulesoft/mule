de66eec8a1630b6040caa4dc74ac2312a2e57f26
Juan Desimoni <juan.desimoni@mulesoft.com>
2017-10-19 17:01:03 -0300

    MULE-13804: extension-model.json still ship in extension binary (#6018)

--- modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/resources/ExtensionModelResourceFactoryTestCase.java
+++ modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/resources/ExtensionModelResourceFactoryTestCase.java
@@ -6,24 +6,29 @@
  */
 package org.mule.runtime.module.extension.internal.resources;
 
+import static java.lang.Thread.currentThread;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mule.runtime.module.extension.api.util.MuleExtensionUtils.loadExtension;
 import static org.mule.runtime.module.extension.internal.ExtensionProperties.EXTENSION_MODEL_JSON_FILE_NAME;
 import org.mule.runtime.api.meta.model.ExtensionModel;
+import org.mule.runtime.core.api.util.IOUtils;
 import org.mule.runtime.extension.api.persistence.ExtensionModelJsonSerializer;
 import org.mule.runtime.extension.api.resources.GeneratedResource;
 import org.mule.runtime.extension.api.resources.spi.GeneratedResourceFactory;
+import org.mule.runtime.module.extension.internal.resources.documentation.ExtensionDocumentationResourceGenerator;
 import org.mule.tck.size.SmallTest;
 import org.mule.test.heisenberg.extension.HeisenbergExtension;
 
+import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.Before;
 import org.junit.Test;
 
 @SmallTest
 public class ExtensionModelResourceFactoryTestCase extends AbstractGeneratedResourceFactoryTestCase {
 
-  private ExtensionModelResourceFactory resourceFactory = new ExtensionModelResourceFactory();
+  private static final String RESOURCE_NAME = "heisenberg-extension-descriptions.xml";
+  private ExtensionDocumentationResourceGenerator resourceFactory = new ExtensionDocumentationResourceGenerator();
   private ExtensionModel extensionModel;
 
   @Before
@@ -33,15 +38,15 @@ public class ExtensionModelResourceFactoryTestCase extends AbstractGeneratedReso
 
   @Override
   protected Class<? extends GeneratedResourceFactory>[] getResourceFactoryTypes() {
-    return new Class[] {ExtensionModelResourceFactory.class};
+    return new Class[] {ExtensionDocumentationResourceGenerator.class};
   }
 
   @Test
   public void generate() throws Exception {
     GeneratedResource resource = resourceFactory.generateResource(extensionModel).get();
-    assertThat(resource.getPath(), equalTo(EXTENSION_MODEL_JSON_FILE_NAME));
-
-    ExtensionModel deserialized = new ExtensionModelJsonSerializer().deserialize(new String(resource.getContent()));
-    assertThat(extensionModel.getName(), equalTo(deserialized.getName()));
+    assertThat(resource.getPath(), equalTo(RESOURCE_NAME));
+    XMLUnit.setIgnoreWhitespace(true);
+    String expected = IOUtils.toString(currentThread().getContextClassLoader().getResource(RESOURCE_NAME).openStream());
+    XMLUnit.compareXML(expected, new String(resource.getContent()));
   }
 }
