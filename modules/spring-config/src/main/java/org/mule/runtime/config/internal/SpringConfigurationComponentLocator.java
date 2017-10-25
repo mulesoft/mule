efe8719fa80e57104711203d9db206fd6cecee69
pablolagreca <Neruda10>
2017-10-25 17:03:47 -0300

    pr feedback

--- modules/spring-config/src/main/java/org/mule/runtime/config/internal/SpringConfigurationComponentLocator.java
+++ modules/spring-config/src/main/java/org/mule/runtime/config/internal/SpringConfigurationComponentLocator.java
@@ -10,7 +10,6 @@ import static java.util.Collections.unmodifiableList;
 import static java.util.Optional.empty;
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
-
 import org.mule.runtime.api.component.Component;
 import org.mule.runtime.api.component.ComponentIdentifier;
 import org.mule.runtime.api.component.location.ComponentLocation;
@@ -47,7 +46,7 @@ public class SpringConfigurationComponentLocator implements ConfigurationCompone
    * @param component the component to be added
    */
   public void addComponent(Component component) {
-    this.componentsMap.putIfAbsent(component.getLocation().getLocation(), component);
+    this.componentsMap.put(component.getLocation().getLocation(), component);
   }
 
   /**
