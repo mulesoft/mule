8b2bce051c87efb1b26fe565b8b32cff8210b043
Rodrigo Merino <elrodro83@users.noreply.github.com>
2017-10-28 18:20:59 +0200

    MULE-13905: Fix flaky error handling unit tests (#6089)

--- core-tests/src/test/java/org/mule/runtime/core/internal/exception/OnCriticalErrorHandlerTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/exception/OnCriticalErrorHandlerTestCase.java
@@ -9,6 +9,7 @@ package org.mule.runtime.core.internal.exception;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
@@ -16,50 +17,61 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mule.runtime.core.internal.exception.DefaultErrorTypeRepository.CRITICAL_ERROR_TYPE;
 import static org.mule.test.allure.AllureConstants.ErrorHandlingFeature.ERROR_HANDLING;
+
 import org.mule.runtime.api.message.Error;
 import org.mule.runtime.api.message.ErrorType;
-import org.mule.runtime.core.api.event.CoreEvent;
-import org.mule.tck.junit4.AbstractMuleTestCase;
+import org.mule.runtime.core.internal.message.InternalEvent;
+import org.mule.tck.junit4.rule.SystemProperty;
 
-import java.util.Optional;
-
-import io.qameta.allure.Feature;
+import org.junit.Before;
 import org.junit.Test;
 
+import io.qameta.allure.Feature;
+
 @Feature(ERROR_HANDLING)
-public class OnCriticalErrorHandlerTestCase extends AbstractMuleTestCase {
+public class OnCriticalErrorHandlerTestCase extends AbstractErrorHandlerTestCase {
 
   private OnCriticalErrorHandler handler = spy(new OnCriticalErrorHandler());
 
+  private Error error;
+
+
+  public OnCriticalErrorHandlerTestCase(SystemProperty verbose) {
+    super(verbose);
+  }
+
+  @Before
+  public void before() throws Exception {
+    super.before();
+
+    error = mock(Error.class, RETURNS_DEEP_STUBS);
+
+    muleEvent = InternalEvent.builder(muleEvent).error(error).build();
+  }
+
   @Test
   public void logsException() {
-    MessagingException messagingException = mock(MessagingException.class);
-    when(messagingException.getDetailedMessage()).thenReturn("Log");
-    handler.apply(messagingException);
-    handler.handleException(messagingException, mock(CoreEvent.class));
+    when(mockException.getDetailedMessage()).thenReturn("Log");
+    when(mockException.getEvent()).thenReturn(muleEvent);
+    handler.apply(mockException);
+    handler.handleException(mockException, muleEvent);
     verify(handler, times(2)).logException(any(MessagingException.class));
   }
 
   @Test
   public void acceptsCritical() {
-    CoreEvent event = mock(CoreEvent.class);
-    Error error = mock(Error.class);
-    when(event.getError()).thenReturn(Optional.of(error));
     when(error.getErrorType()).thenReturn(CRITICAL_ERROR_TYPE);
 
-    assertThat(handler.accept(event), is(true));
+    assertThat(handler.accept(muleEvent), is(true));
   }
 
   @Test
   public void acceptsCriticalChild() {
-    CoreEvent event = mock(CoreEvent.class);
-    Error error = mock(Error.class);
-    when(event.getError()).thenReturn(Optional.of(error));
     ErrorType errorType = mock(ErrorType.class);
     when(error.getErrorType()).thenReturn(errorType);
     when(errorType.getParentErrorType()).thenReturn(CRITICAL_ERROR_TYPE);
 
-    assertThat(handler.accept(event), is(true));
+    assertThat(handler.accept(muleEvent), is(true));
   }
 
 }
