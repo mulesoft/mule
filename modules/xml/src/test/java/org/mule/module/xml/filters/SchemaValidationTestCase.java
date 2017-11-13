fa3742cb3e112b2180595a9d43fcead9c7ebe296
ndinu <ndinu@users.noreply.github.com>
2017-11-13 10:41:11 -0300

    MULE-14030: SAXParseException when trying to import nested xsd (#6175)

--- modules/xml/src/test/java/org/mule/module/xml/filters/SchemaValidationTestCase.java
+++ modules/xml/src/test/java/org/mule/module/xml/filters/SchemaValidationTestCase.java
@@ -40,9 +40,9 @@ public class SchemaValidationTestCase extends AbstractMuleTestCase
     @Rule
     public SystemProperty externalEntities = new SystemProperty(EXTERNAL_ENTITIES_PROPERTY, "true");
 
-    private static final String SIMPLE_SCHEMA = "schema1.xsd";
+    private static final String SIMPLE_SCHEMA = "schema/schema1.xsd";
 
-    private static final String INCLUDE_SCHEMA = "schema-with-include.xsd";
+    private static final String INCLUDE_SCHEMA = "schema/schema-with-include.xsd";
 
     private static final String VALID_XML_FILE = "/validation1.xml";
 
