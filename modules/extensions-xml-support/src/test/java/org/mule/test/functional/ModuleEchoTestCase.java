0bf1ab72b5ddcfdee769e5d0d29c1dd5acbf7b07
fernandezlautaro <fernandez.lautaro@gmail.com>
2017-12-18 15:04:03 -0300

    MULE-14268: adding tests for MULE-14162

---
+++ modules/extensions-xml-support/src/test/java/org/mule/test/functional/ModuleEchoTestCase.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.functional;
+
+
+import static org.hamcrest.Matchers.is;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+import org.mule.runtime.api.metadata.MediaType;
+import org.mule.runtime.core.api.event.CoreEvent;
+
+public class ModuleEchoTestCase extends AbstractXmlExtensionMuleArtifactFunctionalTestCase {
+
+  @Override
+  protected String getModulePath() {
+    return "modules/module-echo.xml";
+  }
+
+  @Override
+  protected String getConfigFile() {
+    return "flows/flows-using-module-echo.xml";
+  }
+
+  @Test
+  public void verifyDataType() throws Exception {
+    CoreEvent muleEvent = flowRunner("test").withMediaType(MediaType.JSON).withPayload("{ \"name\":\"Emiliano\" }").run();
+    MatcherAssert.assertThat(muleEvent.getMessage().getPayload().getValue(), is(MediaType.JSON.toRfcString()));
+  }
+
+}
