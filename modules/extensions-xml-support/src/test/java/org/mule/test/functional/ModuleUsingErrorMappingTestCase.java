b94956d62ff888148b10109ed577db34f70ef9c5
Juan Lautaro Fernandez <fernandez.lautaro@gmail.com>
2017-12-20 13:31:55 -0300

    MULE-14156: fixing lookup for rootest elements when macroexpanding (#6325)

--- modules/extensions-xml-support/src/test/java/org/mule/test/functional/ModuleUsingErrorMappingTestCase.java
+++ modules/extensions-xml-support/src/test/java/org/mule/test/functional/ModuleUsingErrorMappingTestCase.java
@@ -26,6 +26,8 @@ public class ModuleUsingErrorMappingTestCase extends AbstractXmlExtensionMuleArt
   private static final String CONNECT_ERROR_MESSAGE = "Could not connect.";
   private static final String UNMATCHED_ERROR_MESSAGE = "Error.";
   private static final String EXPRESSION_ERROR_MESSAGE = "Bad expression.";
+  private static final String TIMEOUT_ERROR_MESSAGE = "Timeout happened!";
+  private static final String SECURITY_ERROR_MESSAGE = "simple operation called";
 
   @Override
   protected String getModulePath() {
@@ -98,6 +100,31 @@ public class ModuleUsingErrorMappingTestCase extends AbstractXmlExtensionMuleArt
   public void multipleMappingsRequestProxy() throws Exception {
     verifyFailingExpression("multipleMappingsProxy", EXPRESSION_ERROR_MESSAGE);
     verifySuccessExpression("multipleMappingsProxy", CONNECT_ERROR_MESSAGE);
+  }
+
+  @Test
+  @Description("Verifies that a mapped error via global error handler works.")
+  public void globalMappingWithDefaultErrorHandlerTimeout() throws Exception {
+    verifySuccessExpression("globalMappingWithDefaultErrorHandlerTimeout", TIMEOUT_ERROR_MESSAGE);
+  }
+
+
+  @Test
+  @Description("Verifies that a mapped error via global error handler works, where the on continue holds an operation from a smart connector.")
+  public void globalMappingWithDefaultErrorHandlerSecurity() throws Exception {
+    verifySuccessExpression("globalMappingWithDefaultErrorHandlerSecurity", SECURITY_ERROR_MESSAGE);
+  }
+
+  @Test
+  @Description("Verifies that a mapped error via global error handler works using subflows.")
+  public void globalMappingWithDefaultErrorHandlerTimeoutThruSubflow() throws Exception {
+    verifySuccessExpression("globalMappingWithDefaultErrorHandlerTimeoutThruSubflow", TIMEOUT_ERROR_MESSAGE);
+  }
+
+  @Test
+  @Description("Verifies that a mapped error via global error handler works using nested subflows.")
+  public void globalMappingWithDefaultErrorHandlerTimeoutThruSubSubflow() throws Exception {
+    verifySuccessExpression("globalMappingWithDefaultErrorHandlerTimeoutThruSubSubflow", TIMEOUT_ERROR_MESSAGE);
   }
 
   private void verifySuccessExpression(String flowName, String expectedPayload) throws Exception {
