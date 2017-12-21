b95fd7b53873578f709efbb7c8d2263e8d3bcf62
Esteban Wasinger <estebanwasinger@gmail.com>
2017-12-21 14:48:40 -0300

    MULE-14279: API broken by MULE-14171 (#6334)

--- core/src/main/java/org/mule/runtime/core/internal/message/InternalMessage.java
+++ core/src/main/java/org/mule/runtime/core/internal/message/InternalMessage.java
@@ -241,6 +241,78 @@ public interface InternalMessage extends Message, MessageProperties, MessageAtta
 
   interface CollectionBuilder extends Message.CollectionBuilder, Builder {
 
+    @Override
+    CollectionBuilder itemMediaType(MediaType mediaType);
+
+    @Override
+    CollectionBuilder mediaType(MediaType mediaType);
+
+    @Override
+    CollectionBuilder attributes(TypedValue<?> typedValue);
+
+    @Override
+    CollectionBuilder nullAttributesValue();
+
+    @Override
+    CollectionBuilder attributesValue(Object o);
+
+    @Override
+    CollectionBuilder attributesMediaType(MediaType mediaType);
+
+    @Override
+    CollectionBuilder exceptionPayload(ExceptionPayload exceptionPayload);
+
+    @Override
+    CollectionBuilder addInboundProperty(String key, Serializable value);
+
+    @Override
+    CollectionBuilder addInboundProperty(String key, Serializable value, MediaType mediaType);
+
+    @Override
+    CollectionBuilder addInboundProperty(String key, Serializable value, DataType dataType);
+
+    @Override
+    CollectionBuilder addOutboundProperty(String key, Serializable value);
+
+    @Override
+    CollectionBuilder addOutboundProperty(String key, Serializable value, MediaType mediaType);
+
+    @Override
+    CollectionBuilder addOutboundProperty(String key, Serializable value, DataType dataType);
+
+    @Override
+    CollectionBuilder removeInboundProperty(String key);
+
+    @Override
+    CollectionBuilder removeOutboundProperty(String key);
+
+    @Override
+    CollectionBuilder addInboundAttachment(String key, DataHandler value);
+
+    @Override
+    CollectionBuilder addOutboundAttachment(String key, DataHandler value);
+
+    @Override
+    CollectionBuilder removeInboundAttachment(String key);
+
+    @Override
+    CollectionBuilder removeOutboundAttachment(String key);
+
+    @Override
+    CollectionBuilder inboundProperties(Map<String, Serializable> inboundProperties);
+
+    @Override
+    CollectionBuilder outboundProperties(Map<String, Serializable> outboundProperties);
+
+    @Override
+    CollectionBuilder inboundAttachments(Map<String, DataHandler> inboundAttachments);
+
+    @Override
+    CollectionBuilder outboundAttachments(Map<String, DataHandler> outbundAttachments);
+
+    @Override
+    InternalMessage build();
+
   }
 
   interface MapBuilder extends Message.MapBuilder, Builder {
