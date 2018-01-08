e6dddfbc3b6e817d694ee02eba45d3fd766f2603
Daniel Feist <dfeist@gmail.com>
2018-01-08 15:33:28 +0000

    MULE-14394 Notification listener exceptions should not be propagated … (#6402)

--- core/src/main/java/org/mule/runtime/core/api/context/notification/ServerNotificationHandler.java
+++ core/src/main/java/org/mule/runtime/core/api/context/notification/ServerNotificationHandler.java
@@ -11,6 +11,12 @@ import org.mule.runtime.api.notification.NotificationListener;
 
 public interface ServerNotificationHandler {
 
+  /**
+   * Fire the {@link Notification}. Regardless of if a notification is fired synchronously or asynchronously any {@link Throwable}
+   * thrown by the {@link NotificationListener} will not be propagated.
+   * 
+   * @param notification the notification to fire.
+   */
   void fireNotification(Notification notification);
 
   boolean isNotificationDynamic();
--- core/src/main/java/org/mule/runtime/core/internal/context/notification/Sender.java
+++ core/src/main/java/org/mule/runtime/core/internal/context/notification/Sender.java
@@ -6,15 +6,21 @@
  */
 package org.mule.runtime.core.internal.context.notification;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 import org.mule.runtime.core.api.context.notification.ListenerSubscriptionPair;
 import org.mule.runtime.api.notification.Notification;
 import org.mule.runtime.core.api.context.notification.NotifierCallback;
+
+import org.slf4j.Logger;
 
 /**
  * This does the work necessary to deliver events to a particular listener. It is generated for a particular {@link Configuration}
  * and stored in a {@link Policy}.
  */
 public class Sender<N extends Notification> {
+
+  private static final Logger LOGGER = getLogger(Sender.class);
 
   private ListenerSubscriptionPair<N> pair;
 
@@ -26,8 +32,10 @@ public class Sender<N extends Notification> {
     if (pair.getSelector().test(notification)) {
       try {
         notifier.notify(pair.getListener(), notification);
-      } catch (Exception e) {
-        // Exceptions from listeners do not affect the notification processing
+      } catch (Throwable e) {
+        // Exceptions or errors from listeners do not affect the notification processing
+        LOGGER.info("NotificationListener {} was unable to fire notification {} due to an exception: {}.",
+                    pair.getListener(), notification, e);
       }
     }
   }
