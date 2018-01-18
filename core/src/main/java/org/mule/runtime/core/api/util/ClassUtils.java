6ffe8afdf0b8ca79270920e9ca19b1dfeee9db87
Rodro <rodrigo.merino@mulesoft.com>
2018-01-18 10:58:42 -0300

    MULE-14478: Make ParameterGroupObjectBuilder#fieldsCache shared * fix perfromance regression in memoize

--- core/src/main/java/org/mule/runtime/core/api/util/ClassUtils.java
+++ core/src/main/java/org/mule/runtime/core/api/util/ClassUtils.java
@@ -863,14 +863,7 @@ public class ClassUtils {
    */
   public static <I, O> Function<I, O> memoize(Function<I, O> f, Map<I, O> cache) {
     return input -> {
-      // This pre-check is made in order to avoid the synchronized block in the implementation of ConcurrentHashMap
-      // (https://bugs.openjdk.java.net/browse/JDK-8161372)
-      O value = cache.get(input);
-      if (value != null) {
-        return value;
-      } else {
-        return cache.computeIfAbsent(input, f);
-      }
+      return cache.computeIfAbsent(input, f);
     };
   }
 
