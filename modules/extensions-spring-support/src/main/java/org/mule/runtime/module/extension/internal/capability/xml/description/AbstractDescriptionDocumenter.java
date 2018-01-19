8632f3d2dc9b1921e34fca22eb4912292b411247
Alejandro García Marra <alemarra@gmail.com>
2018-01-19 12:05:12 -0300

    Mule 14091: Connector's documentation doesn't document Type's parameters descriptions (#6511)

--- modules/extensions-spring-support/src/main/java/org/mule/runtime/module/extension/internal/capability/xml/description/AbstractDescriptionDocumenter.java
+++ modules/extensions-spring-support/src/main/java/org/mule/runtime/module/extension/internal/capability/xml/description/AbstractDescriptionDocumenter.java
@@ -6,13 +6,19 @@
  */
 package org.mule.runtime.module.extension.internal.capability.xml.description;
 
+import static java.util.Collections.unmodifiableMap;
 import org.mule.runtime.extension.api.annotation.Alias;
 import org.mule.runtime.module.extension.internal.capability.xml.schema.ExtensionAnnotationProcessor;
+import org.mule.runtime.module.extension.internal.util.IntrospectionUtils;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 
 /**
@@ -24,7 +30,7 @@ import javax.lang.model.element.TypeElement;
  * @param <T> the type to document.
  * @since 4.0
  */
-abstract class AbstractDescriptionDocumenter<T> {
+abstract class AbstractDescriptionDocumenter {
 
   protected static final ExtensionAnnotationProcessor processor = new ExtensionAnnotationProcessor();
   protected final TypeElement objectType;
@@ -39,15 +45,6 @@ abstract class AbstractDescriptionDocumenter<T> {
     objectType = processingEnv.getElementUtils().getTypeElement(Object.class.getCanonicalName());
   }
 
-  /**
-   * Sets the descriptions for the provided {@code declaration} using the javadocs in the source code.
-   *
-   * @param declaration   the declaration to document
-   * @param configElement the type element associated to the declaration.
-   */
-  abstract void document(T declaration, TypeElement configElement);
-
-
   String getNameOrAlias(Element element) {
     return getAlias(element).orElse(element.getSimpleName().toString());
   }
@@ -55,6 +52,38 @@ abstract class AbstractDescriptionDocumenter<T> {
   Optional<String> getAlias(Element element) {
     Alias annotation = element.getAnnotation(Alias.class);
     return annotation != null ? Optional.of(annotation.value()) : Optional.empty();
+  }
+
+
+  Map<String, Element> getApiMethods(ProcessingEnvironment processingEnv, List<TypeElement> containerClasses) {
+    Map<String, Element> methods = new HashMap<>();
+
+    for (TypeElement containerElement : containerClasses) {
+
+      TypeElement currentElement = containerElement;
+      while (currentElement != null
+          && !processingEnv.getTypeUtils().isSameType(currentElement.asType(), objectType.asType())) {
+        for (ExecutableElement operation : IntrospectionUtils.getApiMethods(currentElement, processingEnv)) {
+          currentElement.getEnclosedElements().stream()
+              .filter(e -> e.getSimpleName().toString().equals(operation.getSimpleName().toString()))
+              .findFirst()
+              .ifPresent(e -> {
+                String nameOrAlias = getNameOrAlias(operation);
+                if (!methods.containsKey(nameOrAlias) || e.getEnclosingElement().equals(containerElement)) {
+                  // Use the leaf in an overriden method hierarchy
+                  methods.put(nameOrAlias, e);
+                }
+              });
+        }
+        Element superClass = processingEnv.getTypeUtils().asElement(currentElement.getSuperclass());
+        if (superClass instanceof TypeElement) {
+          currentElement = (TypeElement) superClass;
+        } else {
+          currentElement = null;
+        }
+      }
+    }
+    return unmodifiableMap(methods);
   }
 
 }
--- modules/extensions-spring-support/src/main/java/org/mule/runtime/module/extension/internal/capability/xml/description/SourcesDescriptionDocumenter.java
+++ modules/extensions-spring-support/src/main/java/org/mule/runtime/module/extension/internal/capability/xml/description/SourcesDescriptionDocumenter.java
@@ -6,25 +6,28 @@
  */
 package org.mule.runtime.module.extension.internal.capability.xml.description;
 
+import static java.util.Collections.singletonList;
 import static org.mule.runtime.extension.api.util.NameUtils.hyphenize;
-
+import org.mule.runtime.api.meta.model.declaration.fluent.SourceCallbackDeclaration;
 import org.mule.runtime.api.meta.model.declaration.fluent.SourceDeclaration;
 import org.mule.runtime.api.meta.model.declaration.fluent.WithSourcesDeclaration;
 import org.mule.runtime.extension.api.annotation.Sources;
+import org.mule.runtime.module.extension.internal.capability.xml.schema.MethodDocumentation;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
-
-import java.util.List;
-import java.util.Optional;
 
 /**
  * {@link AbstractDescriptionDocumenter} implementation that fills {@link WithSourcesDeclaration}s
  *
  * @since 4.0
  */
-final class SourcesDescriptionDocumenter extends AbstractDescriptionDocumenter<WithSourcesDeclaration<?>> {
+final class SourcesDescriptionDocumenter extends AbstractDescriptionDocumenter {
 
   private final ParameterDescriptionDocumenter parameterDeclarer;
 
@@ -33,23 +36,44 @@ final class SourcesDescriptionDocumenter extends AbstractDescriptionDocumenter<W
     this.parameterDeclarer = new ParameterDescriptionDocumenter(processingEnv);
   }
 
-  void document(WithSourcesDeclaration<?> declaration, TypeElement element) {
+  void document(TypeElement element, WithSourcesDeclaration<?>... containerDeclarations) {
     getSourceClasses(processingEnv, element)
-        .forEach(sourceElement -> findMatchingSource(declaration, sourceElement)
+        .forEach(sourceElement -> findMatchingSource(containerDeclarations, sourceElement)
             .ifPresent(source -> {
               source.setDescription(processor.getJavaDocSummary(processingEnv, sourceElement));
               parameterDeclarer.document(source, sourceElement);
+
+              Map<String, Element> methods = getApiMethods(processingEnv, singletonList(sourceElement));
+              source.getSuccessCallback().ifPresent(cb -> documentCallback(methods, cb));
+              source.getErrorCallback().ifPresent(cb -> documentCallback(methods, cb));
             }));
   }
 
-  private Optional<SourceDeclaration> findMatchingSource(WithSourcesDeclaration<?> declaration, Element element) {
-    return declaration.getMessageSources().stream()
-        .filter(provider -> {
-          String name = provider.getName();
-          String defaultNaming = hyphenize(element.getSimpleName().toString());
-          return name.equals(defaultNaming) || getAlias(element).map(name::equals).orElse(false);
-        })
-        .findAny();
+  private void documentCallback(Map<String, Element> methods, SourceCallbackDeclaration cb) {
+    Element method = methods.get(cb.getName());
+    if (method != null) {
+      MethodDocumentation documentation = processor.getMethodDocumentation(processingEnv, method);
+      parameterDeclarer.document(cb, method, documentation);
+    }
+  }
+
+  private Optional<SourceDeclaration> findMatchingSource(WithSourcesDeclaration<?>[] containerDeclarations, Element element) {
+    for (WithSourcesDeclaration<?> declaration : containerDeclarations) {
+      Optional<SourceDeclaration> sourceDeclaration = declaration.getMessageSources().stream()
+          .filter(source -> {
+            String name = source.getName();
+            String elementName = element.getSimpleName().toString();
+            String defaultNaming = hyphenize(elementName);
+            return name.equals(defaultNaming) || getAlias(element).map(name::equals).orElse(name.equals(elementName));
+          })
+          .findFirst();
+
+      if (sourceDeclaration.isPresent()) {
+        return sourceDeclaration;
+      }
+    }
+
+    return Optional.empty();
   }
 
   private List<TypeElement> getSourceClasses(ProcessingEnvironment processingEnv, Element element) {
