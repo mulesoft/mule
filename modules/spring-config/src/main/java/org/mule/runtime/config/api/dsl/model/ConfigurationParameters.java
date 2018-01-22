5f1239f5eba5142f1a13f3cda57c732dc0e1128f
pablolagreca <Neruda10>
2018-01-22 13:51:09 -0300

    MULE-14523 - Revert MULE-14426 since there is missing functionality (#6524)
    MULE-14523 - Revert MULE-14426 since there is missing functionality
    
    (cherry picked from commit cdfe828c22d6c41b7f8310e83f99984a436e49bb)

---
+++ modules/spring-config/src/main/java/org/mule/runtime/config/api/dsl/model/ConfigurationParameters.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.config.api.dsl.model;
+
+import org.mule.runtime.api.component.ComponentIdentifier;
+import org.mule.runtime.api.util.Pair;
+
+import java.util.List;
+
+/**
+ * Representation of the DSL configuration parameters of a component.
+ * 
+ * @since 4.1
+ */
+public interface ConfigurationParameters {
+
+  /**
+   * Gets the value of a simple parameter as an string.
+   * 
+   * @param parameterName parameter name, same as in the config.
+   * @return the parameter value as an string.
+   */
+  String getStringParameter(String parameterName);
+
+  /**
+   * Gets all the configuration of a complex parameter type with an specific {@link ComponentIdentifier}.
+   * 
+   * @param componentIdentifier the component identifier of the parameter.
+   * @return the complex parameter with the given {@link ComponentIdentifier}
+   */
+  List<ConfigurationParameters> getComplexConfigurationParameter(ComponentIdentifier componentIdentifier);
+
+  /**
+   * Gets all the complex parameters and they related identifier
+   * 
+   * @return all the complex parameters.
+   */
+  List<Pair<ComponentIdentifier, ConfigurationParameters>> getComplexConfigurationParameters();
+
+}
