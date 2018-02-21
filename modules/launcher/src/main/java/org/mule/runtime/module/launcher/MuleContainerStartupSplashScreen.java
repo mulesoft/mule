f154c071c539d5802b966dfaee51decd0b469514
anosenzo <alejandro.nosenzo@mulesoft.com>
2018-02-21 15:05:35 -0300

    Fix PR comments

--- modules/launcher/src/main/java/org/mule/runtime/module/launcher/MuleContainerStartupSplashScreen.java
+++ modules/launcher/src/main/java/org/mule/runtime/module/launcher/MuleContainerStartupSplashScreen.java
@@ -8,6 +8,8 @@ package org.mule.runtime.module.launcher;
 
 import static java.util.Arrays.asList;
 import static java.util.Arrays.sort;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toMap;
 import static org.apache.commons.lang3.StringUtils.defaultString;
 import static org.mule.runtime.container.api.MuleFoldersUtil.getPatchesLibFolder;
 import static org.mule.runtime.container.api.MuleFoldersUtil.getServerPluginsFolder;
@@ -27,8 +29,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class MuleContainerStartupSplashScreen extends SplashScreen {
 
@@ -109,9 +109,7 @@ public class MuleContainerStartupSplashScreen extends SplashScreen {
   }
 
   private void listJavaSystemProperties() {
-    Stream<String> javaPropertiesNamesToList = Stream.of("java.vendor", "java.vm.name", "java.home");
-    Map<String, String> javaProperties =
-        javaPropertiesNamesToList.collect(Collectors.toMap(String::toString, propertyName -> System.getProperty(propertyName)));
-    listItems(javaProperties, "JDK properties:");
+    listItems(stream(new String[] {"java.vendor", "java.vm.name", "java.home"})
+        .collect(toMap(String::toString, propertyName -> System.getProperty(propertyName))), "JDK properties:");
   }
 }
