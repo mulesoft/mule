606c59ccc8916b3cf02b172bca18aca89734ced5
Esteban Wasinger <estebanwasinger@gmail.com>
2018-03-02 11:45:27 -0300

    MULE-14642: SchedulingStrategy and ObjectStore are not declared as imported types (#6613)

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/enricher/PollingSourceDeclarationEnricher.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/enricher/PollingSourceDeclarationEnricher.java
@@ -6,6 +6,7 @@
  */
 package org.mule.runtime.module.extension.internal.loader.enricher;
 
+import static org.mule.metadata.api.utils.MetadataTypeUtils.getTypeId;
 import static org.mule.runtime.api.meta.ExpressionSupport.NOT_SUPPORTED;
 import static org.mule.runtime.api.meta.model.parameter.ParameterGroupModel.DEFAULT_GROUP_NAME;
 import static org.mule.runtime.extension.api.ExtensionConstants.SCHEDULING_STRATEGY_PARAMETER_DESCRIPTION;
@@ -14,13 +15,21 @@ import static org.mule.runtime.extension.api.loader.DeclarationEnricherPhase.STR
 import static org.mule.runtime.internal.dsl.DslConstants.CORE_NAMESPACE;
 import static org.mule.runtime.internal.dsl.DslConstants.CORE_PREFIX;
 import static org.mule.runtime.internal.dsl.DslConstants.SCHEDULING_STRATEGY_ELEMENT_IDENTIFIER;
+
 import org.mule.metadata.api.ClassTypeLoader;
+import org.mule.metadata.api.model.MetadataType;
+import org.mule.metadata.api.model.ObjectType;
+import org.mule.runtime.api.meta.model.ImportedTypeModel;
 import org.mule.runtime.api.meta.model.ParameterDslConfiguration;
+import org.mule.runtime.api.meta.model.declaration.fluent.ExtensionDeclaration;
+import org.mule.runtime.api.meta.model.declaration.fluent.ExtensionDeclarer;
 import org.mule.runtime.api.meta.model.declaration.fluent.ParameterDeclaration;
 import org.mule.runtime.api.meta.model.declaration.fluent.SourceDeclaration;
+import org.mule.runtime.api.util.Reference;
 import org.mule.runtime.core.api.source.scheduler.Scheduler;
 import org.mule.runtime.extension.api.declaration.fluent.util.IdempotentDeclarationWalker;
 import org.mule.runtime.extension.api.declaration.type.ExtensionsTypeLoaderFactory;
+import org.mule.runtime.extension.api.loader.DeclarationEnricher;
 import org.mule.runtime.extension.api.loader.DeclarationEnricherPhase;
 import org.mule.runtime.extension.api.loader.ExtensionLoadingContext;
 import org.mule.runtime.extension.api.property.InfrastructureParameterModelProperty;
@@ -29,18 +38,25 @@ import org.mule.runtime.extension.api.runtime.source.PollingSource;
 
 import javax.xml.namespace.QName;
 
+/**
+ * {@link DeclarationEnricher} for {@link PollingSource polling sources}
+ *
+ * @since 4.1
+ */
 public class PollingSourceDeclarationEnricher extends AbstractAnnotatedDeclarationEnricher {
 
   @Override
   public void enrich(ExtensionLoadingContext extensionLoadingContext) {
     ClassTypeLoader loader = ExtensionsTypeLoaderFactory.getDefault().createTypeLoader();
+    ExtensionDeclarer extensionDeclarer = extensionLoadingContext.getExtensionDeclarer();
+    Reference<Boolean> thereArePollingSources = new Reference<>(false);
 
     new IdempotentDeclarationWalker() {
 
       @Override
       protected void onSource(SourceDeclaration source) {
-        extractImplementingType(source).ifPresent(type -> {
-          if (PollingSource.class.isAssignableFrom(type)) {
+        extractType(source).ifPresent(type -> {
+          if (type.isAssignableTo(PollingSource.class)) {
             ParameterDeclaration parameter = new ParameterDeclaration(SCHEDULING_STRATEGY_PARAMETER_NAME);
             parameter.setDescription(SCHEDULING_STRATEGY_PARAMETER_DESCRIPTION);
             parameter.setRequired(true);
@@ -55,12 +71,28 @@ public class PollingSourceDeclarationEnricher extends AbstractAnnotatedDeclarati
                 .allowTopLevelDefinition(false)
                 .build());
 
+            thereArePollingSources.set(true);
 
             source.getParameterGroup(DEFAULT_GROUP_NAME).addParameter(parameter);
           }
         });
       }
-    }.walk(extensionLoadingContext.getExtensionDeclarer().getDeclaration());
+    }.walk(extensionDeclarer.getDeclaration());
+
+    if (thereArePollingSources.get() && !isSchedulerAlreadyImported(extensionDeclarer.getDeclaration())) {
+      ClassTypeLoader typeLoader = ExtensionsTypeLoaderFactory.getDefault().createTypeLoader();
+      extensionDeclarer.withImportedType(new ImportedTypeModel((ObjectType) typeLoader.load(Scheduler.class)));
+    }
+  }
+
+  private boolean isSchedulerAlreadyImported(ExtensionDeclaration extension) {
+    return extension.getImportedTypes().stream().anyMatch(model -> isScheduler(model.getImportedType()));
+  }
+
+  private boolean isScheduler(MetadataType type) {
+    return getTypeId(type)
+        .filter(typeId -> Scheduler.class.getName().equals(typeId))
+        .isPresent();
   }
 
   @Override
--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/enricher/RedeliveryPolicyDeclarationEnricher.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/enricher/RedeliveryPolicyDeclarationEnricher.java
@@ -6,17 +6,27 @@
  */
 package org.mule.runtime.module.extension.internal.loader.enricher;
 
+import static org.mule.metadata.api.utils.MetadataTypeUtils.getTypeId;
 import static org.mule.runtime.extension.internal.loader.util.InfrastructureParameterBuilder.addRedeliveryPolicy;
+
+import org.mule.metadata.api.ClassTypeLoader;
+import org.mule.metadata.api.model.MetadataType;
+import org.mule.metadata.api.model.ObjectType;
+import org.mule.runtime.api.meta.model.ImportedTypeModel;
+import org.mule.runtime.api.meta.model.declaration.fluent.ExtensionDeclaration;
 import org.mule.runtime.api.meta.model.declaration.fluent.SourceDeclaration;
+import org.mule.runtime.api.store.ObjectStore;
+import org.mule.runtime.api.util.Reference;
 import org.mule.runtime.extension.api.declaration.fluent.util.IdempotentDeclarationWalker;
+import org.mule.runtime.extension.api.declaration.type.ExtensionsTypeLoaderFactory;
 import org.mule.runtime.extension.api.loader.DeclarationEnricher;
 import org.mule.runtime.extension.api.loader.ExtensionLoadingContext;
 
 /**
  * A {@link DeclarationEnricher} which adds the following to all {@link SourceDeclaration}:
- *
+ * <p>
  * <ul>
- *   <li>A Redelivery policy parameter</li>
+ * <li>A Redelivery policy parameter</li>
  * </ul>
  *
  * @since 1.0
@@ -25,12 +35,31 @@ public final class RedeliveryPolicyDeclarationEnricher implements DeclarationEnr
 
   @Override
   public void enrich(ExtensionLoadingContext extensionLoadingContext) {
+    final Reference<Boolean> hasObjectStoreParams = new Reference<>(false);
+
+    ExtensionDeclaration extension = extensionLoadingContext.getExtensionDeclarer().getDeclaration();
     new IdempotentDeclarationWalker() {
 
       @Override
       protected void onSource(SourceDeclaration declaration) {
         addRedeliveryPolicy(declaration);
+        hasObjectStoreParams.set(true);
       }
-    }.walk(extensionLoadingContext.getExtensionDeclarer().getDeclaration());
+    }.walk(extension);
+
+    if (hasObjectStoreParams.get() && !isObjectStoreAlreadyImported(extension)) {
+      ClassTypeLoader typeLoader = ExtensionsTypeLoaderFactory.getDefault().createTypeLoader();
+      extension.getImportedTypes().add(new ImportedTypeModel((ObjectType) typeLoader.load(ObjectStore.class)));
+    }
+  }
+
+  private boolean isObjectStoreAlreadyImported(ExtensionDeclaration extension) {
+    return extension.getImportedTypes().stream().anyMatch(model -> isObjectStore(model.getImportedType()));
+  }
+
+  private boolean isObjectStore(MetadataType type) {
+    return getTypeId(type)
+        .filter(typeId -> ObjectStore.class.getName().equals(typeId))
+        .isPresent();
   }
 }
