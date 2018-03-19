5cc1c5ed68e23ad2d86323da15978b988d1b36f1
Facundo Velazquez <facundovelazquezsantillan@gmail.com>
2018-03-19 15:56:29 -0300

    MULE-14601: Json Schema Validator treats duplicate keys in an inconsistent way depending on the input type. (#6578)

--- modules/json/src/main/java/org/mule/module/json/validation/ValidateJsonSchemaMessageProcessor.java
+++ modules/json/src/main/java/org/mule/module/json/validation/ValidateJsonSchemaMessageProcessor.java
@@ -6,14 +6,21 @@
  */
 package org.mule.module.json.validation;
 
+import static com.fasterxml.jackson.core.JsonParser.Feature.STRICT_DUPLICATE_DETECTION;
+import static java.lang.Boolean.getBoolean;
+import static org.mule.api.config.MuleProperties.SYSTEM_PROPERTY_PREFIX;
 import org.mule.api.MuleEvent;
 import org.mule.api.MuleException;
 import org.mule.api.lifecycle.Initialisable;
 import org.mule.api.lifecycle.InitialisationException;
 import org.mule.api.processor.MessageProcessor;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonParser.Feature;
 
 /**
  * A {@link MessageProcessor} that uses a {@link JsonSchemaValidator}
@@ -24,6 +31,7 @@ import java.util.Map;
 public class ValidateJsonSchemaMessageProcessor implements MessageProcessor, Initialisable
 {
 
+    private boolean allowDuplicateKeys;
     private String schemaLocation;
     private JsonSchemaDereferencing dereferencing = JsonSchemaDereferencing.CANONICAL;
     private Map<String, String> schemaRedirects = new HashMap<>();
@@ -33,10 +41,18 @@ public class ValidateJsonSchemaMessageProcessor implements MessageProcessor, Ini
     @Override
     public void initialise() throws InitialisationException
     {
+        List<Feature> features = new ArrayList<>();
+
+        if (!allowDuplicateKeys)
+        {
+            features.add(STRICT_DUPLICATE_DETECTION);
+        }
+
         validator = JsonSchemaValidator.builder()
                 .setSchemaLocation(schemaLocation)
                 .setDereferencing(dereferencing)
                 .addSchemaRedirects(schemaRedirects)
+                .addFeatures(features)
                 .build();
     }
 
@@ -60,5 +76,15 @@ public class ValidateJsonSchemaMessageProcessor implements MessageProcessor, Ini
     public void setSchemaRedirects(Map<String, String> schemaRedirects)
     {
         this.schemaRedirects = schemaRedirects;
+    }
+
+    public boolean isAllowDuplicateKeys()
+    {
+        return allowDuplicateKeys;
+    }
+
+    public void setAllowDuplicateKeys(boolean allowDuplicateKeys)
+    {
+        this.allowDuplicateKeys = allowDuplicateKeys;
     }
 }
---
+++ modules/json/src/test/java/org/mule/module/json/validation/ValidateSchemaDuplicateKeysTestCase.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.json.validation;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertThat;
+import static org.junit.rules.ExpectedException.none;
+import static org.mule.module.json.validation.JsonSchemaTestUtils.SCHEMA_FSTAB_JSON;
+import static org.mule.module.json.validation.JsonSchemaTestUtils.getFstabWithDuplicateKeys;
+import static org.mule.module.json.validation.ValidateSchemaFunctionalTestCase.SCHEMA_LOCATION;
+
+import org.mule.api.MuleEvent;
+import org.mule.tck.junit4.rule.SystemProperty;
+
+import java.util.Collection;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.internal.matchers.ThrowableMessageMatcher;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class ValidateSchemaDuplicateKeysTestCase extends AbstractValidateSchemaFunctionalTestCase
+{
+    @Rule
+    public SystemProperty schemaLocation = new SystemProperty(SCHEMA_LOCATION, SCHEMA_FSTAB_JSON);
+
+    @Rule
+    public SystemProperty duplicateKeys;
+
+    @Rule
+    public ExpectedException expectedException;
+
+    public ValidateSchemaDuplicateKeysTestCase(String allowDuplicateKeys, ExpectedException expectedException)
+    {
+        this.expectedException = expectedException;
+        duplicateKeys = new SystemProperty("allowDuplicateKeys", allowDuplicateKeys);
+    }
+
+    @Parameters
+    public static Collection<Object[]> data()
+    {
+        return asList(new Object[][] {
+                {"true", none()},
+                {"false", getExpectedExceptionForDuplicateKeys()}
+        });
+    }
+
+    @Override
+    protected String getConfigFile()
+    {
+        return "validate-schema-duplicate-keys-config.xml";
+    }
+
+    @Test
+    public void duplicateKeys() throws Exception
+    {
+        MuleEvent result =  runFlow(VALIDATE_FLOW, getFstabWithDuplicateKeys());
+        assertThat(result.getMessage().getPayloadAsString(), is(getFstabWithDuplicateKeys()));
+    }
+
+    private static ExpectedException getExpectedExceptionForDuplicateKeys()
+    {
+        ExpectedException expectedException = none();
+        expectedException.expectCause(new ThrowableMessageMatcher<>(containsString("Duplicate field")));
+        return expectedException;
+    }
+
+}
