68e86888cb72e8c65fb8e61ccf8f98cd1b51be0c
Ana Felisatti <afelisatti@mulesoft.com>
2018-03-23 15:45:15 -0300

    MULE-13034: Escape all error handlers. (#6680)
    (cherry picked from commit 84a78d19fd4afef9d844f08b8d0865e3b2c73f41)

--- modules/http/src/main/java/org/mule/module/http/internal/listener/NoMethodRequestHandler.java
+++ modules/http/src/main/java/org/mule/module/http/internal/listener/NoMethodRequestHandler.java
@@ -11,11 +11,12 @@ import static org.mule.module.http.api.HttpConstants.HttpStatus.METHOD_NOT_ALLOW
 public class NoMethodRequestHandler extends ErrorRequestHandler
 {
 
+    public static final String METHOD_NOT_ALLOWED_FORMAT = "Method not allowed for endpoint: %s";
     private static NoMethodRequestHandler instance = new NoMethodRequestHandler();
 
     private NoMethodRequestHandler()
     {
-        super(METHOD_NOT_ALLOWED.getStatusCode(), METHOD_NOT_ALLOWED.getReasonPhrase(), "Method not allowed for endpoint: %s");
+        super(METHOD_NOT_ALLOWED.getStatusCode(), METHOD_NOT_ALLOWED.getReasonPhrase(), METHOD_NOT_ALLOWED_FORMAT);
     }
 
     public static NoMethodRequestHandler getInstance()
