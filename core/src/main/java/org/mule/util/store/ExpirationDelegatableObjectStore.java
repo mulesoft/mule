9d7f93a51c91943581ead45c234ffe95f421bb7e
fsgonz <fabian.gonzalez@mulesoft.com>
2018-04-10 12:29:22 -0300

    EE-5938: Delegate Distributed Object Store Expiration to hazelcast. (#6703)

---
+++ core/src/main/java/org/mule/util/store/ExpirationDelegatableObjectStore.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.util.store;
+
+/**
+ * Interface to identify those object stores that manage the expiration due to TTL of records independently from mule
+ * monitoring
+ */
+public interface ExpirationDelegatableObjectStore
+{
+
+    /**
+     * Sets the properties needed for the expiration policy
+     * 
+     * @param entryTTL the entryTTL of the object store record.
+     */
+    void setExpirationPolicyProperties(int entryTTL);
+}
