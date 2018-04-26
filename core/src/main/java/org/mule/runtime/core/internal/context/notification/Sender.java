54b0ae98c15472ea4b58c611847457bb19dce9d7
Rodrigo Merino <elrodro83@users.noreply.github.com>
2018-04-26 11:19:57 -0300

    MULE-14901: 50% performance degradation with empty (#6791)
    MessageProcessorNotificationListener

--- core/src/main/java/org/mule/runtime/core/internal/context/notification/Sender.java
+++ core/src/main/java/org/mule/runtime/core/internal/context/notification/Sender.java
@@ -6,21 +6,16 @@
  */
 package org.mule.runtime.core.internal.context.notification;
 
-import static org.slf4j.LoggerFactory.getLogger;
-
-import org.mule.runtime.core.api.context.notification.ListenerSubscriptionPair;
 import org.mule.runtime.api.notification.Notification;
+import org.mule.runtime.api.notification.NotificationListener;
+import org.mule.runtime.core.api.context.notification.ListenerSubscriptionPair;
 import org.mule.runtime.core.api.context.notification.NotifierCallback;
-
-import org.slf4j.Logger;
 
 /**
  * This does the work necessary to deliver events to a particular listener. It is generated for a particular {@link Configuration}
  * and stored in a {@link Policy}.
  */
 public class Sender<N extends Notification> {
-
-  private static final Logger LOGGER = getLogger(Sender.class);
 
   private ListenerSubscriptionPair<N> pair;
 
@@ -30,15 +25,12 @@ public class Sender<N extends Notification> {
 
   public void dispatch(N notification, NotifierCallback notifier) {
     if (pair.getSelector().test(notification)) {
-      try {
-        notifier.notify(pair.getListener(), notification);
-      } catch (Throwable e) {
-        // Exceptions or errors from listeners do not affect the notification processing
-        LOGGER.info("NotificationListener {} was unable to fire notification {} due to an exception: {}.",
-                    pair.getListener(), notification, e);
-      }
+      notifier.notify(pair.getListener(), notification);
     }
   }
 
+  NotificationListener<N> getListener() {
+    return pair.getListener();
+  }
 }
 
