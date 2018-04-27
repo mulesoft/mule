8c006e8301dc31690318e3d36f70584f6126b3e9
Juan Lautaro Fernandez <fernandez.lautaro@gmail.com>
2018-04-27 15:23:28 -0300

    MULE-14803: forbid the use of any type of configuration properties within smart connectors, but allow file::filename, and global-property (#6804)
    (cherry picked from commit 65d6058)

--- modules/extensions-xml-support/src/main/java/org/mule/runtime/extension/api/loader/xml/XmlExtensionModelLoader.java
+++ modules/extensions-xml-support/src/main/java/org/mule/runtime/extension/api/loader/xml/XmlExtensionModelLoader.java
@@ -17,6 +17,7 @@ import org.mule.runtime.extension.api.loader.ExtensionModelLoader;
 import org.mule.runtime.extension.api.loader.ExtensionModelValidator;
 import org.mule.runtime.extension.internal.loader.XmlExtensionLoaderDelegate;
 import org.mule.runtime.extension.internal.loader.validator.CorrectPrefixesValidator;
+import org.mule.runtime.extension.internal.loader.validator.ForbiddenConfigurationPropertiesValidator;
 import org.mule.runtime.extension.internal.loader.validator.GlobalElementNamesValidator;
 import org.mule.runtime.module.extension.internal.loader.enricher.stereotypes.StereotypesDeclarationEnricher;
 
@@ -34,7 +35,8 @@ public class XmlExtensionModelLoader extends ExtensionModelLoader {
   private final List<DeclarationEnricher> customEnrichers = unmodifiableList(asList(new StereotypesDeclarationEnricher()));
 
   private final List<ExtensionModelValidator> customValidators = unmodifiableList(asList(new CorrectPrefixesValidator(),
-                                                                                         new GlobalElementNamesValidator()));
+                                                                                         new GlobalElementNamesValidator(),
+                                                                                         new ForbiddenConfigurationPropertiesValidator()));
 
   /**
    * Attribute to look for in the parametrized attributes picked up from the descriptor.
