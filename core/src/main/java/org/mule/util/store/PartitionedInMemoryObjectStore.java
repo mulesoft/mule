2bf7ef933877848b247b0b4901ef41bc7590e440
facunlk <facundo.lopez.kaufmann@gmail.com>
2018-05-04 14:07:20 -0500

    MULE-14969: PartitionInMemoryObjectStore is leaking ExpiryInfo objects (#6817)

--- core/src/main/java/org/mule/util/store/PartitionedInMemoryObjectStore.java
+++ core/src/main/java/org/mule/util/store/PartitionedInMemoryObjectStore.java
@@ -7,6 +7,10 @@
 package org.mule.util.store;
 
 import static org.mule.api.store.ObjectStoreManager.UNBOUNDED;
+import org.mule.api.store.ObjectAlreadyExistsException;
+import org.mule.api.store.ObjectDoesNotExistException;
+import org.mule.api.store.ObjectStoreException;
+import org.mule.api.store.PartitionableExpirableObjectStore;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -17,11 +21,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
-
-import org.mule.api.store.ObjectAlreadyExistsException;
-import org.mule.api.store.ObjectDoesNotExistException;
-import org.mule.api.store.ObjectStoreException;
-import org.mule.api.store.PartitionableExpirableObjectStore;
 
 public class PartitionedInMemoryObjectStore<T extends Serializable> extends AbstractPartitionedObjectStore<T>
     implements PartitionableExpirableObjectStore<T>
@@ -103,6 +102,7 @@ public class PartitionedInMemoryObjectStore<T extends Serializable> extends Abst
     public void clear(String partitionName) throws ObjectStoreException
     {
         this.getPartition(partitionName).clear();
+        this.getExpiryInfoPartition(partitionName).clear();
     }
 
     @Override
@@ -151,7 +151,7 @@ public class PartitionedInMemoryObjectStore<T extends Serializable> extends Abst
     @Override
     public void close(String partitionName) throws ObjectStoreException
     {
-        // Nothing to do
+        disposePartition(partitionName);
     }
 
     @Override
