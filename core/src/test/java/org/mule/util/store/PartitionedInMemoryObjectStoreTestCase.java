1b35c83810a101e0e7e3c10fcaa04414706d08a1
facunlk <facundo.lopez.kaufmann@gmail.com>
2018-05-04 14:07:20 -0500

    MULE-14969: PartitionInMemoryObjectStore is leaking ExpiryInfo objects (#6817)
    (cherry picked from commit 2bf7ef933877848b247b0b4901ef41bc7590e440)

--- core/src/test/java/org/mule/util/store/PartitionedInMemoryObjectStoreTestCase.java
+++ core/src/test/java/org/mule/util/store/PartitionedInMemoryObjectStoreTestCase.java
@@ -10,9 +10,16 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mule.tck.probe.PollingProber.DEFAULT_POLLING_INTERVAL;
 import org.mule.api.store.ObjectStoreException;
 import org.mule.tck.junit4.AbstractMuleTestCase;
+import org.mule.tck.probe.PollingProber;
+import org.mule.tck.probe.Probe;
 import org.mule.tck.size.SmallTest;
+
+import java.io.Serializable;
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -26,6 +33,7 @@ public class PartitionedInMemoryObjectStoreTestCase extends AbstractMuleTestCase
     private static final String TEST_KEY1 = "testKey1";
     private static final String TEST_KEY2 = "testKey2";
     private static final String TEST_KEY3 = "testKey3";
+    private static final int GC_TIMEOUT_MILLIS = 10000;
 
     private PartitionedInMemoryObjectStore<String> store;
 
@@ -92,6 +100,66 @@ public class PartitionedInMemoryObjectStoreTestCase extends AbstractMuleTestCase
         assertThat(store.contains(TEST_KEY2, TEST_PARTITION), is(false));
         assertThat(store.retrieve(TEST_KEY3, TEST_PARTITION), equalTo(TEST_VALUE));
     }
+
+    @Test
+    public void removesDataOnClear() throws ObjectStoreException
+    {
+        TrackedKey trackedKey = new TrackedKey();
+        final PhantomReference<TrackedKey> keyRef = trackAndStore(trackedKey);
+
+        store.clear(TEST_PARTITION);
+        assertThat(store.contains(trackedKey, TEST_PARTITION), is(false));
+        trackedKey = null;
+
+        validateClear(keyRef);
+    }
+
+    @Test
+    public void removesDataOnClose() throws ObjectStoreException
+    {
+        TrackedKey trackedKey = new TrackedKey();
+        final PhantomReference<TrackedKey> keyRef = trackAndStore(trackedKey);
+
+        store.close(TEST_PARTITION);
+        assertThat(store.contains(trackedKey, TEST_PARTITION), is(false));
+        trackedKey = null;
+
+        validateClear(keyRef);
+    }
+
+    private PhantomReference<TrackedKey> trackAndStore(TrackedKey trackedKey) throws ObjectStoreException
+    {
+        final PhantomReference<TrackedKey> keyRef = new PhantomReference<>(trackedKey, new ReferenceQueue<>());
+
+        store.store(trackedKey, TEST_VALUE, TEST_PARTITION);
+        assertThat(store.contains(trackedKey, TEST_PARTITION), is(true));
+        return keyRef;
+    }
+
+    private void validateClear(final PhantomReference<TrackedKey> keyRef) throws ObjectStoreException
+    {
+        new PollingProber(GC_TIMEOUT_MILLIS, DEFAULT_POLLING_INTERVAL).check(new Probe() {
+
+            @Override
+            public boolean isSatisfied()
+            {
+                System.gc();
+                return keyRef.isEnqueued();
+            }
+
+            @Override
+            public String describeFailure()
+            {
+                return "A hard reference is being maintained to the entry expiration info.";
+            }
+        });
+    }
+
+    private class TrackedKey implements Serializable
+    {
+    }
+
+
 
 
 }
