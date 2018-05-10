223b75f6841fe5c9ab4cdbb5c1958006bd223cc5
afelisatti <afelisatti@mulesoft.com>
2018-05-10 17:34:52 -0300

    MULE-15004: Using serverTimeout has no effect

--- modules/http/src/test/java/org/mule/module/http/functional/listener/HttpListenerSocketConfigTestCase.java
+++ modules/http/src/test/java/org/mule/module/http/functional/listener/HttpListenerSocketConfigTestCase.java
@@ -7,14 +7,21 @@
 package org.mule.module.http.functional.listener;
 
 
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.util.StringUtils;
 
-import org.apache.http.client.fluent.Request;
-import org.apache.http.client.fluent.Response;
-import org.apache.http.entity.StringEntity;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.Socket;
+
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -23,7 +30,8 @@ public class HttpListenerSocketConfigTestCase extends FunctionalTestCase
 
     @Rule
     public DynamicPort listenPort1 = new DynamicPort("port1");
-
+    @Rule
+    public DynamicPort listenPort2 = new DynamicPort("port2");
 
     @Override
     protected String getConfigFile()
@@ -32,18 +40,58 @@ public class HttpListenerSocketConfigTestCase extends FunctionalTestCase
     }
 
     @Test
-    public void globalServerSocketProperties() throws Exception
+    public void serverTimeoutsTcpConnection() throws Exception
     {
-        // For now, just test that the context is parsed correctly.
-        assertResponse(listenPort1.getNumber(), "global");
+        Socket socket = new Socket("localhost", listenPort1.getNumber());
+        Thread.sleep(1000);
+        sendRequest(socket, "global");
+        assertThat(getResponse(socket), is(nullValue()));
     }
 
-    private void assertResponse(int port, String path) throws Exception
+    @Test
+    public void keepAlivePreventsServerTimeout() throws Exception
     {
-        final String url = String.format("http://localhost:%s/%s", port, path);
-        final Response response = Request.Post(url).body(new StringEntity(TEST_MESSAGE)).connectTimeout(1000).execute();
-        assertThat(response.returnContent().asString(), equalTo(TEST_MESSAGE));
+        Socket socket = new Socket("localhost", listenPort2.getNumber());
+        sendRequest(socket, "timeout");
+        assertThat(getResponse(socket), is(notNullValue()));
+        Thread.sleep(1000);
+        sendRequest(socket, "timeout");
+        assertThat(getResponse(socket), is(notNullValue()));
+        Thread.sleep(3000);
+        sendRequest(socket, "timeout");
+        assertThat(getResponse(socket), is(nullValue()));
+    }
 
+    private void sendRequest(Socket socket, final String path) throws IOException
+    {
+        PrintWriter writer = new PrintWriter(socket.getOutputStream());
+        writer.println("GET /" + path + " HTTP/1.1");
+        writer.println("Host: www.example.com");
+        writer.println("");
+        writer.flush();
+    }
+
+    private String getResponse(Socket socket)
+    {
+        try
+        {
+            StringWriter writer = new StringWriter();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            if (reader != null)
+            {
+                String line;
+                while (!StringUtils.isEmpty(line = reader.readLine()))
+                {
+                    writer.append(line).append("\r\n");
+                }
+            }
+            String response = writer.toString();
+            return response.length() == 0 ? null : response;
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
     }
 
 }
