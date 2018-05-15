93f07a70deff64f82ad87c287b1aefdfc4a81e32
Rodrigo Merino <elrodro83@users.noreply.github.com>
2018-05-15 17:34:01 -0300

    MULE-15035: Allow test runner to dynamically add test resources to the (#6863)
    region classloader

---
+++ tests/runner/src/main/java/org/mule/test/runner/classloader/DelegatingArtifactClassLoader.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.runner.classloader;
+
+import org.mule.runtime.module.artifact.api.classloader.ArtifactClassLoader;
+import org.mule.runtime.module.artifact.api.classloader.ClassLoaderLookupPolicy;
+import org.mule.runtime.module.artifact.api.classloader.ShutdownListener;
+import org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptor;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+
+class DelegatingArtifactClassLoader extends ClassLoader implements ArtifactClassLoader {
+
+  static {
+    registerAsParallelCapable();
+  }
+
+  public DelegatingArtifactClassLoader(ClassLoader parent) {
+    super(parent);
+  }
+
+  @Override
+  public String getArtifactId() {
+    return null;
+  }
+
+  @Override
+  public <T extends ArtifactDescriptor> T getArtifactDescriptor() {
+    return null;
+  }
+
+  @Override
+  public ClassLoader getClassLoader() {
+    return this;
+  }
+
+  @Override
+  public void addShutdownListener(ShutdownListener listener) {
+
+  }
+
+  @Override
+  public ClassLoaderLookupPolicy getClassLoaderLookupPolicy() {
+    return null;
+  }
+
+  @Override
+  public void dispose() {}
+
+  @Override
+  public URL findLocalResource(String resourceName) {
+    return getParent().getResource(resourceName);
+  }
+
+  @Override
+  public URL findResource(String s) {
+    return getParent().getResource(s);
+  }
+
+  @Override
+  public Enumeration<URL> findResources(String name) throws IOException {
+    return getParent().getResources(name);
+  }
+
+  @Override
+  public Class<?> findLocalClass(String name) throws ClassNotFoundException {
+    return getParent().loadClass(name);
+  }
+
+  @Override
+  public Class<?> loadClass(String name) throws ClassNotFoundException {
+    return getParent().loadClass(name);
+  }
+
+  @Override
+  public URL getResource(String name) {
+    return getParent().getResource(name);
+  }
+
+  @Override
+  public InputStream getResourceAsStream(String name) {
+    return getParent().getResourceAsStream(name);
+  }
+
+  @Override
+  public Enumeration<URL> getResources(String name) throws IOException {
+    return getParent().getResources(name);
+  }
+
+}
+
---
+++ tests/runner/src/main/java/org/mule/test/runner/classloader/TestRegionClassLoader.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.runner.classloader;
+
+import org.mule.runtime.module.artifact.api.classloader.ClassLoaderLookupPolicy;
+import org.mule.runtime.module.artifact.api.classloader.DefaultArtifactClassLoaderFilter;
+import org.mule.runtime.module.artifact.api.classloader.RegionClassLoader;
+import org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptor;
+
+import java.util.Set;
+
+/**
+ * {@link RegionClassLoader} specialization that provides a simpler api to add classloaders to the region.
+ *
+ * @since 4.2
+ */
+public final class TestRegionClassLoader extends RegionClassLoader {
+
+  static {
+    registerAsParallelCapable();
+  }
+
+  public TestRegionClassLoader(ClassLoader parent, ClassLoaderLookupPolicy lookupPolicy) {
+    super("Region", new ArtifactDescriptor("Region"), parent, lookupPolicy);
+  }
+
+  /**
+   * Adds a class loader to the region.
+   *
+   * @see RegionClassLoader#addClassLoader(org.mule.runtime.module.artifact.api.classloader.ArtifactClassLoader,
+   *      org.mule.runtime.module.artifact.api.classloader.ArtifactClassLoaderFilter)
+   */
+  public void addContent(ClassLoader parentClassLoader, Set<String> exportedClassPackages,
+                         Set<String> exportedResources) {
+    addClassLoader(new DelegatingArtifactClassLoader(parentClassLoader),
+                   new DefaultArtifactClassLoaderFilter(exportedClassPackages, exportedResources));
+  }
+}
