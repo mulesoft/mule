131af30c112806535e5405e2e852b0987cb69dd5
afelisatti <afelisatti@mulesoft.com>
2018-05-17 22:21:42 -0300

    MULE-15050: Allow server response body to be deferred

--- modules/http-api/src/main/java/org/mule/runtime/http/api/server/async/HttpResponseReadyCallback.java
+++ modules/http-api/src/main/java/org/mule/runtime/http/api/server/async/HttpResponseReadyCallback.java
@@ -8,6 +8,9 @@ package org.mule.runtime.http.api.server.async;
 
 import org.mule.runtime.http.api.domain.message.response.HttpResponse;
 
+import java.io.Writer;
+import java.nio.charset.Charset;
+
 /**
  * Handler for sending an HTTP response asynchronously
  *
@@ -22,5 +25,18 @@ public interface HttpResponseReadyCallback {
    * @param responseStatusCallback callback to be called if there's a failure while sending the response or when it is successfully sent.
    */
   void responseReady(HttpResponse response, ResponseStatusCallback responseStatusCallback);
+
+  /**
+   * Method to send a delayed response to the client. The provided {@link HttpResponse} will be used to send all HTTP metadata
+   * (status code, headers and so on) but its {@link org.mule.runtime.http.api.domain.entity.HttpEntity} will be ignored and the
+   * HTTP body will be written through the returned {@link Writer}. The connection will remain open until the {@link Writer} is
+   * closed.
+   *
+   * @param response HTTP response content
+   * @param responseStatusCallback callback to be called if there's a failure while sending the response or when it is successfully sent.
+   * @param encoding the encoding the {@link Writer} will use
+   * @return an HTTP body {@link Writer}
+   */
+  Writer startResponse(HttpResponse response, ResponseStatusCallback responseStatusCallback, Charset encoding);
 
 }
