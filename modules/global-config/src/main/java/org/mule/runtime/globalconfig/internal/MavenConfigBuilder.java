caead6a6fd2199fb795eeacf3926d7ec4d6ac20d
Guillermo Fernandes <gsfernandes@users.noreply.github.com>
2018-05-31 17:11:03 -0300

    MULE-15094: Add support for security settings on Maven configuration (#6899)
    (cherry picked from commit 694820f5ef26a8d12d0302d640bdc9ca0268bbbe)

--- modules/global-config/src/main/java/org/mule/runtime/globalconfig/internal/MavenConfigBuilder.java
+++ modules/global-config/src/main/java/org/mule/runtime/globalconfig/internal/MavenConfigBuilder.java
@@ -43,6 +43,8 @@ public class MavenConfigBuilder {
           mavenConfig.hasPath("globalSettingsLocation") ? mavenConfig.getString("globalSettingsLocation") : null;
       String userSettingsLocation =
           mavenConfig.hasPath("userSettingsLocation") ? mavenConfig.getString("userSettingsLocation") : null;
+      String settingsSecurityLocation =
+          mavenConfig.hasPath("settingsSecurityLocation") ? mavenConfig.getString("settingsSecurityLocation") : null;
       String repositoryLocation =
           mavenConfig.hasPath("repositoryLocation") ? mavenConfig.getString("repositoryLocation") : null;
       boolean ignoreArtifactDescriptorRepositories =
@@ -55,6 +57,7 @@ public class MavenConfigBuilder {
 
       File globalSettingsFile = findResource(globalSettingsLocation);
       File userSettingsFile = findResource(userSettingsLocation);
+      File settingsSecurityFile = findResource(settingsSecurityLocation);
 
       File repositoryFolder = getRuntimeRepositoryFolder();
       if (repositoryLocation != null) {
@@ -75,6 +78,9 @@ public class MavenConfigBuilder {
       }
       if (userSettingsFile != null) {
         mavenConfigurationBuilder.userSettingsLocation(userSettingsFile);
+      }
+      if (settingsSecurityFile != null) {
+        mavenConfigurationBuilder.settingsSecurityLocation(settingsSecurityFile);
       }
 
       ConfigObject repositories =
