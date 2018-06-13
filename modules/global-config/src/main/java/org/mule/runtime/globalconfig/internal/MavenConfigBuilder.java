9c7e81fd6cd8a9148a393bdb70c2b8b5eaee4e8a
Guillermo Fernandes <guillermo.fernandes@mulesoft.com>
2018-06-13 13:19:46 -0300

    MULE-15161: offLineMode for Maven configuration cannot be set on Mule Runtime

--- modules/global-config/src/main/java/org/mule/runtime/globalconfig/internal/MavenConfigBuilder.java
+++ modules/global-config/src/main/java/org/mule/runtime/globalconfig/internal/MavenConfigBuilder.java
@@ -51,6 +51,10 @@ public class MavenConfigBuilder {
           mavenConfig.hasPath("ignoreArtifactDescriptorRepositories")
               ? mavenConfig.getBoolean("ignoreArtifactDescriptorRepositories") : true;
 
+      boolean offLineMode =
+          mavenConfig.hasPath("offLineMode")
+              ? mavenConfig.getBoolean("offLineMode") : false;
+
       File globalSettingsFile = findResource(globalSettingsLocation);
       File userSettingsFile = findResource(userSettingsLocation);
       File settingsSecurityFile = findResource(settingsSecurityLocation);
@@ -67,7 +71,8 @@ public class MavenConfigBuilder {
       MavenConfiguration.MavenConfigurationBuilder mavenConfigurationBuilder =
           MavenConfiguration.newMavenConfigurationBuilder()
               .localMavenRepositoryLocation(repositoryFolder)
-              .ignoreArtifactDescriptorRepositories(ignoreArtifactDescriptorRepositories);
+              .ignoreArtifactDescriptorRepositories(ignoreArtifactDescriptorRepositories)
+              .offlineMode(offLineMode);
       if (globalSettingsFile != null) {
         mavenConfigurationBuilder.globalSettingsLocation(globalSettingsFile);
       }
