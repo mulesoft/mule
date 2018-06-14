d3513427cbc7727dec962866198125e9c3964192
Martin Buchwald <martin.buchwald@mulesoft.com>
2018-06-14 13:20:19 -0300

    Imports

--- modules/ws/src/main/java/org/mule/module/ws/consumer/WSDLUtils.java
+++ modules/ws/src/main/java/org/mule/module/ws/consumer/WSDLUtils.java
@@ -13,7 +13,13 @@ import org.mule.util.xmlsecurity.XMLSecureFactories;
 
 import java.io.File;
 import java.io.StringWriter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.wsdl.Binding;
 import javax.wsdl.BindingOperation;
