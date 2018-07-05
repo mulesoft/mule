e4cf080fb44141358142f42d23a84efae0975641
fsgonz <fabian.gonzalez@mulesoft.com>
2018-07-05 11:14:40 -0300

    MULE-15296: Missing Interceptor that sets the BindingOperationInfo if operation is one-way. (#6991)

---
+++ modules/cxf/src/test/java/org/mule/module/cxf/ProxyBindingTestCase.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.cxf;
+
+import static org.junit.Assert.assertTrue;
+import static org.mule.module.http.api.client.HttpRequestOptionsBuilder.newOptions;
+
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mule.api.MuleMessage;
+import org.mule.module.http.api.client.HttpRequestOptions;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.util.IOUtils;
+
+public class ProxyBindingTestCase extends FunctionalTestCase
+{
+    @Rule
+    public final DynamicPort httpPortProxy = new DynamicPort("port1");
+
+    @Rule
+    public final DynamicPort httpPortService = new DynamicPort("port2");
+
+    public static final HttpRequestOptions HTTP_REQUEST_OPTIONS = newOptions().method(org.mule.module.http.api.HttpConstants.Methods.POST.name()).build();
+
+    private String getAllRequest;
+    private String addArtistRequest;
+    private String getAllResponse;
+    private String addArtistResponse;
+
+    @Override
+    protected String getConfigFile()
+    {
+        return "proxy-service-one-way-flow.xml";
+    }
+
+    @Before
+    public void doSetUp() throws Exception
+    {
+        getAllRequest = IOUtils.getResourceAsString("artistregistry-get-all-request.xml", getClass());
+        addArtistRequest = IOUtils.getResourceAsString("artistregistry-add-artist-request.xml", getClass());
+        getAllResponse = IOUtils.getResourceAsString("artistregistry-get-all-response.xml", getClass());
+        addArtistResponse = IOUtils.getResourceAsString("artistregistry-add-artist-response.xml", getClass());
+        XMLUnit.setIgnoreWhitespace(true);
+    }
+
+    /**
+     * With this test we verify that the correct operation is used when there are more than one operation in the same
+     * binding.
+     */
+    @Test
+    public void proxyCorrectlyIdentifiesBothOperations() throws Exception
+    {
+        MuleMessage responseGetAll = muleContext.getClient().send("http://localhost:" + httpPortProxy.getNumber() + "/body", getTestMuleMessage(getAllRequest), HTTP_REQUEST_OPTIONS);
+        assertTrue(XMLUnit.compareXML(getAllResponse, responseGetAll.getPayloadAsString()).identical());
+        MuleMessage responseAddArtist = muleContext.getClient().send("http://localhost:" + httpPortProxy.getNumber() + "/body", getTestMuleMessage(addArtistRequest), HTTP_REQUEST_OPTIONS);
+        assertTrue(XMLUnit.compareXML(addArtistResponse, responseAddArtist.getPayloadAsString()).identical());
+    }
+}
