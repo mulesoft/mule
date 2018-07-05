a031c95b9fb12d5a76ccebc9bacfa0803fe66bc7
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2018-07-05 16:43:46 -0300

    MULE-15298: TypedValue length should be calculated lazyly (#6992)

--- core-tests/src/test/java/org/mule/runtime/core/message/TypedValueTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/message/TypedValueTestCase.java
@@ -7,27 +7,32 @@
 package org.mule.runtime.core.message;
 
 import static java.nio.charset.StandardCharsets.UTF_16;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mule.runtime.api.metadata.DataType.OBJECT;
+import static org.mule.runtime.api.metadata.DataType.STRING;
 import static org.mule.runtime.api.metadata.DataType.builder;
 import static org.mule.runtime.api.metadata.TypedValue.of;
-
 import org.mule.runtime.api.metadata.DataType;
 import org.mule.runtime.api.metadata.TypedValue;
 import org.mule.tck.testmodels.fruit.Apple;
 
-import org.junit.Test;
-
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.OptionalLong;
+
+import org.junit.Test;
 
 public class TypedValueTestCase {
 
   private static String STRING_VALUE = "MY_VALUE";
   private static byte[] BYTE_ARRAY_VALUE = "MY_VALUE".getBytes();
   private static InputStream INPUT_STREAM_VALUE = new ByteArrayInputStream(BYTE_ARRAY_VALUE);
+  private static Object OBJECT_VALUE = new Integer(0);
 
   @Test
   public void string() {
@@ -38,7 +43,7 @@ public class TypedValueTestCase {
 
   @Test
   public void stringUTF16() {
-    TypedValue<String> typedValue = new TypedValue<>(STRING_VALUE, builder(DataType.STRING).charset(UTF_16).build());
+    TypedValue<String> typedValue = new TypedValue<>(STRING_VALUE, builder(STRING).charset(UTF_16).build());
     assertThat(typedValue.getByteLength().isPresent(), is(true));
     assertThat(typedValue.getByteLength().getAsLong(), is((long) STRING_VALUE.getBytes(UTF_16).length));
   }
@@ -68,4 +73,38 @@ public class TypedValueTestCase {
     assertThat(of(new Apple()).getByteLength().isPresent(), is(false));
   }
 
+  @Test
+  public void serializeWithoutLength() throws Exception {
+    final TypedValue<Object> typedValue = new TypedValue<>(OBJECT_VALUE, DataType.fromObject(OBJECT_VALUE), OptionalLong.empty());
+    final TypedValue<Object> deserealized = serializationRoundTrip(typedValue);
+    assertThat(deserealized, equalTo(typedValue));
+  }
+
+  @Test
+  public void serializeWithLength() throws Exception {
+    final TypedValue<String> typedValue = new TypedValue<>(STRING_VALUE, STRING);
+    final TypedValue<String> deserealized = serializationRoundTrip(typedValue);
+    assertThat(deserealized, equalTo(typedValue));
+    assertThat(deserealized.getByteLength().getAsLong(), is(new Long(STRING_VALUE.length())));
+  }
+
+  private <T> TypedValue<T> serializationRoundTrip(TypedValue<T> typedValue) throws Exception {
+    return deserealize(serialize(typedValue));
+  }
+
+  private byte[] serialize(Object object) throws Exception {
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    try (ObjectOutputStream out = new ObjectOutputStream(outputStream)) {
+      out.writeObject(object);
+      return outputStream.toByteArray();
+    }
+  }
+
+  private <T> T deserealize(byte[] bytes) throws Exception {
+    ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
+    try (ObjectInputStream in = new ObjectInputStream(inputStream)) {
+      return (T) in.readObject();
+    }
+  }
 }
