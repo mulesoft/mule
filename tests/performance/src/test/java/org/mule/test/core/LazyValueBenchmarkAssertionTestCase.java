6e1e317d2f8db45ad7a27f3575363965a3886f50
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2018-08-07 12:12:54 -0300

    MULE-15509: Reduce contention in LazyValue (#7094)

---
+++ tests/performance/src/test/java/org/mule/test/core/LazyValueBenchmarkAssertionTestCase.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.core;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+import org.mule.AbstractBenchmarkAssertionTestCase;
+import org.mule.LazyValueBenchmark;
+
+import org.junit.Test;
+
+public class LazyValueBenchmarkAssertionTestCase extends AbstractBenchmarkAssertionTestCase {
+
+  @Test
+  public void lazyAccess() {
+    runAndAssertBenchmark(LazyValueBenchmark.class, "lazyAccess", 500, NANOSECONDS, 800);
+  }
+
+  @Test
+  public void notSoLazyAccess() {
+    runAndAssertBenchmark(LazyValueBenchmark.class, "notSoLazyAccess", 500, NANOSECONDS, 800);
+  }
+
+  @Test
+  public void lazyIfComputed() {
+    runAndAssertBenchmark(LazyValueBenchmark.class, "lazyIfComputed", 500, NANOSECONDS, 800);
+  }
+
+  @Test
+  public void notSoLazyIfComputed() {
+    runAndAssertBenchmark(LazyValueBenchmark.class, "notSoLazyIfComputed", 500, NANOSECONDS, 800);
+  }
+}
