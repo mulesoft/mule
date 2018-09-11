916d641738f3a6d97002e9b1656569c186757aca
Ana Felisatti <afelisatti@mulesoft.com>
2018-09-11 15:19:33 -0300

    MULE-15634: Evaluate classifier and type attributes usage (#7197)

--- modules/artifact/src/main/java/org/mule/runtime/module/artifact/api/classloader/RegionClassLoader.java
+++ modules/artifact/src/main/java/org/mule/runtime/module/artifact/api/classloader/RegionClassLoader.java
@@ -11,6 +11,7 @@ import static java.lang.Integer.toHexString;
 import static java.lang.String.format;
 import static java.lang.System.identityHashCode;
 import static java.util.Collections.emptyList;
+import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 import static org.apache.commons.io.FilenameUtils.normalize;
 import static org.apache.commons.lang3.ClassUtils.getPackageName;
@@ -35,11 +36,9 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.function.Supplier;
 import java.util.regex.Matcher;
 
 import org.slf4j.Logger;
@@ -78,6 +77,7 @@ public class RegionClassLoader extends MuleDeployableArtifactClassLoader {
   private final List<RegionMemberClassLoader> registeredClassLoaders = new ArrayList<>();
   private final Map<String, ArtifactClassLoader> packageMapping = new HashMap<>();
   private final Map<String, List<ArtifactClassLoader>> resourceMapping = new HashMap<>();
+  private final Object descriptorMappingLock = new Object();
   private final Map<BundleDescriptor, URLClassLoader> descriptorMapping = new HashMap<>();
   private ArtifactClassLoader ownerClassLoader;
 
@@ -251,39 +251,27 @@ public class RegionClassLoader extends MuleDeployableArtifactClassLoader {
         }
       }
     } else if (name.startsWith(RESOURCE_PREFIX)) {
-      Matcher matcher = GAV_PATTERN.matcher(name);
+      Matcher matcher = GAV_EXTENDED_PATTERN.matcher(name);
       // Check for specific artifact requests
       if (matcher.matches()) {
         String groupId = matcher.group(1);
         String artifactId = matcher.group(2);
         String version = matcher.group(3);
-        String resource = matcher.group(4);
-        LOGGER.debug("Region request for '{}' in group '{}', artifact '{}' and version '{}'.", resource, groupId, artifactId,
-                     version);
+        String classifier = matcher.group(4);
+        String type = matcher.group(5);
+        String resource = matcher.group(6);
+        LOGGER.debug("Region request for '{}' in group '{}', artifact '{}' and version '{}', with classifier '{}' and type '{}'.",
+                     resource, groupId, artifactId, version, classifier, type);
         String normalizedResource = normalize(resource, true);
 
-        // Check whether it's an exported resource from a matching artifact
-        List<ArtifactClassLoader> exportingArtifactClassLoaders = resourceMapping.get(normalizedResource);
-        if (exportingArtifactClassLoaders != null) {
-          for (ArtifactClassLoader artifactClassLoader : exportingArtifactClassLoaders) {
-            BundleDescriptor descriptor = artifactClassLoader.getArtifactDescriptor().getBundleDescriptor();
-            // The descriptor may not be present during some tests
-            if (descriptor != null && isRequestedArtifact(descriptor, groupId, artifactId, version, () -> {
-              LOGGER.warn("Required version '{}' for artifact '{}:{}' not found. Searching in available version '{}'...",
-                          version, descriptor.getGroupId(), descriptor.getArtifactId(), descriptor.getVersion());
-              return true;
-            })) {
-              return artifactClassLoader.findResource(normalizedResource);
-            }
-          }
-        }
-
-        // Check whether it's a resource from an API dependency, since all those should be considered exported
-        if (version != null) {
+        if (API_CLASSIFIERS.contains(classifier) && "zip".equals(type) && !WILDCARD.equals(version)) {
+          // Check whether it's a resource from an API dependency, since all those should be considered exported
           BundleDescriptor requiredDescriptor = new BundleDescriptor.Builder()
               .setGroupId(groupId)
               .setArtifactId(artifactId)
               .setVersion(version)
+              .setClassifier(classifier)
+              .setType(type)
               .build();
           URLClassLoader classLoader = descriptorMapping.get(requiredDescriptor);
           if (classLoader != null) {
@@ -291,18 +279,39 @@ public class RegionClassLoader extends MuleDeployableArtifactClassLoader {
           } else {
             ClassLoaderModel classLoaderModel = this.getArtifactDescriptor().getClassLoaderModel();
             for (BundleDependency dependency : classLoaderModel.getDependencies()) {
-              Optional<String> classifier = dependency.getDescriptor().getClassifier();
-              if (classifier.isPresent() && API_CLASSIFIERS.contains(classifier.get())) {
-                BundleDescriptor descriptor = dependency.getDescriptor();
-                if (isRequestedArtifact(descriptor, groupId, artifactId, version, () -> false)) {
-                  return descriptorMapping.computeIfAbsent(descriptor, (CheckedFunction<BundleDescriptor, URLClassLoader>) d -> {
-                    try {
-                      return new URLClassLoader(new URL[] {dependency.getBundleUri().toURL()});
-                    } catch (MalformedURLException e) {
-                      throw new MuleRuntimeException(e);
+              BundleDescriptor descriptor = dependency.getDescriptor();
+              if (isRequestedArtifact(descriptor, requiredDescriptor, () -> false)) {
+                return descriptorMapping.computeIfAbsent(descriptor, (CheckedFunction<BundleDescriptor, URLClassLoader>) d -> {
+                  // We don't want class loaders in limbo
+                  synchronized (descriptorMappingLock) {
+                    if (descriptorMapping.containsKey(descriptor)) {
+                      return descriptorMapping.get(descriptor);
+                    } else {
+                      try {
+                        return new URLClassLoader(new URL[] {dependency.getBundleUri().toURL()});
+                      } catch (MalformedURLException e) {
+                        throw new MuleRuntimeException(e);
+                      }
                     }
-                  }).findResource(normalizedResource);
-                }
+                  }
+                }).findResource(normalizedResource);
+              }
+            }
+          }
+        } else {
+          // Check whether it's an exported resource from a matching artifact
+          List<ArtifactClassLoader> exportingArtifactClassLoaders = resourceMapping.get(normalizedResource);
+          if (exportingArtifactClassLoaders != null) {
+            for (ArtifactClassLoader artifactClassLoader : exportingArtifactClassLoaders) {
+              BundleDescriptor descriptor = artifactClassLoader.getArtifactDescriptor().getBundleDescriptor();
+              // The descriptor may not be present during some tests
+              if (descriptor != null
+                  && isRequestedArtifact(descriptor, groupId, artifactId, version, ofNullable(classifier), type, () -> {
+                    LOGGER.warn("Required version '{}' for artifact '{}:{}' not found. Searching in available version '{}'...",
+                                version, descriptor.getGroupId(), descriptor.getArtifactId(), descriptor.getVersion());
+                    return true;
+                  })) {
+                return artifactClassLoader.findResource(normalizedResource);
               }
             }
           }
@@ -325,15 +334,6 @@ public class RegionClassLoader extends MuleDeployableArtifactClassLoader {
     }
 
     return null;
-  }
-
-  private boolean isRequestedArtifact(BundleDescriptor descriptor, String groupId, String artifactId, String version,
-                                      Supplier<Boolean> onVersionMismatch) {
-    boolean versionResult = true;
-    if (!descriptor.getVersion().equals(version)) {
-      versionResult = onVersionMismatch.get();
-    }
-    return descriptor.getGroupId().equals(groupId) && descriptor.getArtifactId().equals(artifactId) && versionResult;
   }
 
   @Override
--- modules/artifact/src/test/java/org/mule/runtime/module/artifact/api/classloader/MuleArtifactClassLoaderTestCase.java
+++ modules/artifact/src/test/java/org/mule/runtime/module/artifact/api/classloader/MuleArtifactClassLoaderTestCase.java
@@ -6,17 +6,19 @@
  */
 package org.mule.runtime.module.artifact.api.classloader;
 
+import static org.apache.tika.io.IOUtils.readLines;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.mockito.Mockito.mock;
-import org.mule.runtime.core.api.util.ClassUtils;
+import static org.mule.runtime.core.api.util.ClassUtils.getResource;
+import static org.powermock.api.mockito.PowerMockito.spy;
 import org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptor;
 import org.mule.tck.junit4.AbstractMuleTestCase;
 
-import java.net.MalformedURLException;
+import java.io.IOException;
 import java.net.URL;
 
 import org.junit.Test;
@@ -24,40 +26,83 @@ import org.junit.Test;
 public class MuleArtifactClassLoaderTestCase extends AbstractMuleTestCase {
 
   private static final String API_RESOURCE_NAME = "test-api.raml";
+  private static final String JAR_RESOURCE_NAME = "hello.txt";
+  private static final String API_LINE = "#%RAML 1.0";
+
   private final URL apiLocation;
-  private final URL apiResourceLocation;
+  private final URL apiLibraryLocation;
+  private final URL jarLocation;
+  private final URL testsJarLocation;
 
-  public MuleArtifactClassLoaderTestCase() throws MalformedURLException {
-    apiLocation = ClassUtils.getResource("com/organization/test-artifact/1.0.0/classloader-test-api.zip", this.getClass());
-    apiResourceLocation = new URL("jar:" + apiLocation.toString() + "!/" + API_RESOURCE_NAME);
+  public MuleArtifactClassLoaderTestCase() {
+    apiLocation = spy(getResource("com/organization/test-artifact/1.0.0/test-artifact-1.0.0-raml.zip", this.getClass()));
+    apiLibraryLocation =
+        getResource("com/organization/test-artifact/1.0.0/test-artifact-1.0.0-raml-fragment.zip", this.getClass());
+    jarLocation = getResource("com/organization/test-artifact/1.0.0/test-artifact-1.0.0.jar", this.getClass());
+    testsJarLocation = getResource("com/organization/test-artifact/1.0.0/test-artifact-1.0.0-tests.jar", this.getClass());
   }
 
   @Test
-  public void findsResourceFromSpecificInnerArtifact() {
-    MuleArtifactClassLoader classLoader =
-        new MuleArtifactClassLoader("test", mock(ArtifactDescriptor.class), new URL[] {apiLocation}, null,
-                                    mock(ClassLoaderLookupPolicy.class));
-    URL resource = classLoader.findResource("resource::com.organization:test-artifact:1.0.0:test-api.raml");
-    assertThat(resource, is(notNullValue()));
-    assertThat(resource, is(equalTo(apiResourceLocation)));
+  public void findsResourcesFromSpecificInnerZipArtifacts() throws Exception {
+    MuleArtifactClassLoader classLoader = createClassLoader();
+
+    findAndValidate(classLoader, "resource::com.organization:test-artifact:1.0.0:raml:zip", API_RESOURCE_NAME, apiLocation,
+                    API_LINE);
+    findAndValidate(classLoader, "resource::com.organization:test-artifact:1.0.0:raml-fragment:zip", "reused-fragment.raml",
+                    apiLibraryLocation, "#%RAML 1.0 Library");
   }
 
   @Test
-  public void findsResourceFromSpecificInnerArtifactWithoutVersion() {
-    MuleArtifactClassLoader classLoader =
-        new MuleArtifactClassLoader("test", mock(ArtifactDescriptor.class), new URL[] {apiLocation}, null,
-                                    mock(ClassLoaderLookupPolicy.class));
-    URL resource = classLoader.findResource("resource::com.organization:test-artifact::test-api.raml");
-    assertThat(resource, is(notNullValue()));
-    assertThat(resource, is(equalTo(apiResourceLocation)));
+  public void findsResourcesFromSpecificInnerJarArtifacts() throws Exception {
+    MuleArtifactClassLoader classLoader = createClassLoader();
+
+    findAndValidate(classLoader, "resource::com.organization:test-artifact:1.0.0::jar", JAR_RESOURCE_NAME, jarLocation,
+                    "Hello, world!");
+    findAndValidate(classLoader, "resource::com.organization:test-artifact:1.0.0:tests:jar", JAR_RESOURCE_NAME, testsJarLocation,
+                    "Aloha, world!");
+  }
+
+  @Test
+  public void findsResourceFromSpecificInnerArtifactWithoutVersion() throws Exception {
+    MuleArtifactClassLoader classLoader = createClassLoader();
+    findAndValidate(classLoader, "resource::com.organization:test-artifact:*:raml:zip", API_RESOURCE_NAME, apiLocation, API_LINE);
   }
 
   @Test
   public void cannotFindResourceFromSpecificInnerArtifactWithWrongVersion() {
-    MuleArtifactClassLoader classLoader =
-        new MuleArtifactClassLoader("test", mock(ArtifactDescriptor.class), new URL[] {apiLocation}, null,
-                                    mock(ClassLoaderLookupPolicy.class));
-    assertThat(classLoader.findResource("resource::com.organization:test-artifact:1.3:test-api.raml"), is(nullValue()));
+    MuleArtifactClassLoader classLoader = createClassLoader();
+    assertThat(classLoader.findResource("resource::com.organization:test-artifact:1.3:raml:zip:test-api.raml"), is(nullValue()));
+  }
+
+  @Test
+  public void findsResourceFromSpecificInnerArtifactAfterWildcardRequest() throws Exception {
+    MuleArtifactClassLoader classLoader = createClassLoader();
+    findAndValidate(classLoader, "resource::com.organization:test-artifact:*:raml:zip", API_RESOURCE_NAME, apiLocation, API_LINE);
+    findAndValidate(classLoader, "resource::com.organization:test-artifact:1.0.0:raml:zip", API_RESOURCE_NAME, apiLocation,
+                    API_LINE);
+  }
+
+  @Test
+  public void findsResourceFromSpecificInnerArtifactAfterFullRequest() throws Exception {
+    MuleArtifactClassLoader classLoader = createClassLoader();
+    findAndValidate(classLoader, "resource::com.organization:test-artifact:1.0.0:raml:zip", API_RESOURCE_NAME, apiLocation,
+                    API_LINE);
+    findAndValidate(classLoader, "resource::com.organization:test-artifact:*:raml:zip", API_RESOURCE_NAME, apiLocation, API_LINE);
+  }
+
+  private MuleArtifactClassLoader createClassLoader() {
+    return new MuleArtifactClassLoader("test", mock(ArtifactDescriptor.class),
+                                       new URL[] {apiLocation, apiLibraryLocation, jarLocation, testsJarLocation},
+                                       null, mock(ClassLoaderLookupPolicy.class));
+  }
+
+  private void findAndValidate(MuleArtifactClassLoader classLoader, String request, String resourceName,
+                               URL expectedArtifactLocation, String expectedLine)
+      throws IOException {
+    URL resource = classLoader.findResource(request + ":" + resourceName);
+    assertThat(resource, is(notNullValue()));
+    assertThat(resource, is(equalTo(new URL("jar:" + expectedArtifactLocation.toString() + "!/" + resourceName))));
+    assertThat(readLines(resource.openStream()).get(0), is(expectedLine));
   }
 
 }
