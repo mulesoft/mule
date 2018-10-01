064c0e8a7bae97fbdbbc809eba1160dbd013d1db
fsgonz <fabian.gonzalez@mulesoft.com>
2018-10-01 11:09:09 -0300

    MULE-15763: Add jdoc to underline the importance of setting the correct context in lifecycle methods. (#7275)

--- core/src/main/java/org/mule/api/lifecycle/Stoppable.java
+++ core/src/main/java/org/mule/api/lifecycle/Stoppable.java
@@ -18,6 +18,15 @@ import org.mule.api.MuleException;
 public interface Stoppable
 {
     String PHASE_NAME = "stop";
-    
+
+    /**
+     * Stops a lifecycle component.
+     * <p>
+     * <b>Note</b>: in most cases the correct context has to be set before stopping the component. 
+     * Special attention has to be paid to the classloader from which the component is 
+     * stopped.
+     * 
+     * @throws MuleException exception during the stop process.
+     */
     void stop() throws MuleException;
 }
