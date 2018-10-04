8afd0f80e0414db637887d72c14b831b82b7333c
Pablo Balbi <pablo.l.balbi@gmail.com>
2018-10-04 17:56:59 -0300

    Receiver connection with retry policy test

--- transports/email/src/test/java/org/mule/transport/email/connectors/ImapConnectorTestCase.java
+++ transports/email/src/test/java/org/mule/transport/email/connectors/ImapConnectorTestCase.java
@@ -6,10 +6,29 @@
  */
 package org.mule.transport.email.connectors;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mule.api.context.WorkManager;
+import org.mule.api.endpoint.InboundEndpoint;
+import org.mule.api.retry.RetryCallback;
+import org.mule.api.retry.RetryPolicyTemplate;
 import org.mule.api.transport.Connector;
+import org.mule.retry.policies.NoRetryPolicyTemplate;
 import org.mule.transport.email.ImapConnector;
+import org.mule.transport.email.RetrieveMessageReceiver;
 
 import com.icegreen.greenmail.util.ServerSetup;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 /**
  * Simple tests for pulling from an IMAP server.
@@ -29,6 +48,38 @@ public class ImapConnectorTestCase extends AbstractReceivingMailConnectorTestCas
         connector.setCheckFrequency(POLL_PERIOD_MS);
         connector.setServiceOverrides(newEmailToStringServiceOverrides());
         return connector;
+    }
+
+    @Test
+    public void receiversConnectionIsDoneWithConnectorsRetryPolicy() throws Exception
+    {
+        ImapConnector aConnector = new ImapConnector(muleContext);
+        String imapConnectorUri = "imap://bob:password@localhost:123";
+        InboundEndpoint anEndpoint = getTestInboundEndpoint("in", imapConnectorUri, null, null, null, aConnector);
+        RetrieveMessageReceiver aReceiver = new RetrieveMessageReceiver(aConnector, getTestService(), anEndpoint, 100, true, "aFolder");
+
+        final AtomicBoolean retryPolicyWasEnforced = new AtomicBoolean(false);
+
+        RetryPolicyTemplate probeRetryPolicyTemplate = mock(NoRetryPolicyTemplate.class);
+        when(probeRetryPolicyTemplate.execute(any(RetryCallback.class), any(WorkManager.class)))
+                .thenAnswer(new Answer<Object>()
+        {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable
+            {
+                retryPolicyWasEnforced.set(true);
+                return null;
+            }
+        });
+        aConnector.setRetryPolicyTemplate(probeRetryPolicyTemplate);
+
+        aReceiver.connect();
+
+        assertThat(aReceiver.isConnected(), is(true));
+
+        // Receivers connection was done with retry policy
+        verify(probeRetryPolicyTemplate, times(1)).execute(any(RetryCallback.class), any(WorkManager.class));
+        assertThat(retryPolicyWasEnforced.get(), is(true));
     }
 
 }
