2ea120b4e172a311467e3a7da38adcadc2f09e83
Martín Buchwald <martin.buchwald@mulesoft.com>
2018-10-18 15:27:52 -0300

    MULE-15872: Foreach need to use different correlationId for every iteration (#7336)

--- core/src/main/java/org/mule/routing/outbound/AbstractMessageSequenceSplitter.java
+++ core/src/main/java/org/mule/routing/outbound/AbstractMessageSequenceSplitter.java
@@ -146,7 +146,8 @@ public abstract class AbstractMessageSequenceSplitter extends AbstractIntercepti
                 if ((!correlationSet && (enableCorrelation == CorrelationMode.IF_NOT_SET))
                     || (enableCorrelation == CorrelationMode.ALWAYS))
                 {
-                    message.setCorrelationId(correlationId);
+                    String correlationIdSequence = correlationId + correlationSequence;
+                    message.setCorrelationId(correlationIdSequence);
                 }
 
                 // take correlation group size from the message properties, set by
