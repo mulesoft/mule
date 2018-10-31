b22c28af02172e6a993f12d886f8d8b3f472d8c0
Nicolas Santiago Di Nucci <nico.dinu@gmail.com>
2018-10-31 18:07:43 -0300

    fix tests

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/validation/DeprecatedModelValidator.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/validation/DeprecatedModelValidator.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.runtime.module.extension.internal.loader.validation;
 
 
@@ -30,7 +36,7 @@ public class DeprecatedModelValidator implements ExtensionModelValidator {
 
       @Override
       protected void onParameter(ParameterizedModel owner, ParameterGroupModel groupModel, ParameterModel model) {
-        if (model.isRequired() && false /* model.isDeprecated() */) {
+        if (model.isRequired() && model.isDeprecated()) {
           problemsReporter.addError(new Problem(model,
                                                 String
                                                     .format("Parameter \"%s\" from %s named \"%s\" is required but has been marked as deprecated. Required parameters cannot be deprecated. Use the @Optional annotation to make it optional and add a default value.",
