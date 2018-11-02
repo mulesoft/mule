995f94a306599d429e66b2b264166bcfc186b797
fsgonz <fabian.gonzalez@mulesoft.com>
2018-11-02 13:12:57 -0300

    MULE-15496: Default Trust Manager Algorithm is equal to the Default Keystore Algorithm. (#7084) (#7102)

---
+++ tests/integration/src/test/java/org/mule/test/integration/domain/tls/TlsCustomTruststoreTestCase.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.integration.domain.tls;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mule.api.MuleMessage;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.tck.junit4.rule.SystemProperty;
+
+public class TlsCustomTruststoreTestCase extends FunctionalTestCase
+{
+
+    private static final String RESPONSE = "test";
+
+    @Rule
+    public DynamicPort port = new DynamicPort("port");
+
+    @Rule
+    public DynamicPort portSsl = new DynamicPort("portSsl");
+
+    @Rule
+    public SystemProperty customTrustStoreEnabled = new SystemProperty("javax.net.ssl.trustStore", "src/test/resources/chain-cert-truststore.jks");
+
+    @Override
+    protected String getConfigFile()
+    {
+        return "tls/tls-clustom-truststore-config.xml";
+    }
+
+    @Test
+    public void usingCustomTlsTrustManager() throws Exception
+    {
+        MuleMessage message = runFlow("flow-custom").getMessage();
+        assertThat(RESPONSE, equalTo(message.getPayloadAsString()));
+    }
+
+    @Test
+    public void usingDefaultTlsTrustManager() throws Exception
+    {
+        MuleMessage message = runFlow("flow-default").getMessage();
+        assertThat(RESPONSE, equalTo(message.getPayloadAsString()));
+    }
+}
