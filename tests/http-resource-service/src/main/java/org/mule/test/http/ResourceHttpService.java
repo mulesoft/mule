a448a60b2ebb9e8aa7dbeb2ffb5b8088d2f3da6e
Ana Felisatti <afelisatti@mulesoft.com>
2018-11-02 13:28:25 -0300

    MULE-15850: Remove HttpClient lookup functionality (#7390)

--- tests/http-resource-service/src/main/java/org/mule/test/http/ResourceHttpService.java
+++ tests/http-resource-service/src/main/java/org/mule/test/http/ResourceHttpService.java
@@ -8,7 +8,6 @@ package org.mule.test.http;
 
 import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.http.api.HttpService;
-import org.mule.runtime.http.api.client.ClientNotFoundException;
 import org.mule.runtime.http.api.client.HttpClient;
 import org.mule.runtime.http.api.client.HttpClientConfiguration;
 import org.mule.runtime.http.api.client.HttpClientFactory;
@@ -96,10 +95,6 @@ public class ResourceHttpService implements HttpService {
         };
       }
 
-      @Override
-      public HttpClient lookup(String name) throws ClientNotFoundException {
-        throw new UnsupportedOperationException("not implemented for this test");
-      }
     };
   }
 
