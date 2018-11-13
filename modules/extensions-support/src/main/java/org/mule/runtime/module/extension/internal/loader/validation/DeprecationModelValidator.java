9b86ccff37a862e39790d711fe9e14fd6abecc1a
Nicolas Santiago Di Nucci <nico.dinu@gmail.com>
2018-11-13 15:09:08 -0300

    Adds requested changes

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/validation/DeprecationModelValidator.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/validation/DeprecationModelValidator.java
@@ -7,13 +7,23 @@
 package org.mule.runtime.module.extension.internal.loader.validation;
 
 
+import static java.lang.String.format;
 import static org.mule.runtime.extension.api.util.NameUtils.getComponentModelTypeName;
 import static org.mule.runtime.module.extension.internal.loader.validation.ModelValidationUtils.isCompiletime;
 
+import org.mule.runtime.api.meta.MuleVersion;
+import org.mule.runtime.api.meta.NamedObject;
 import org.mule.runtime.api.meta.model.ExtensionModel;
+import org.mule.runtime.api.meta.model.config.ConfigurationModel;
+import org.mule.runtime.api.meta.model.connection.ConnectionProviderModel;
+import org.mule.runtime.api.meta.model.construct.ConstructModel;
+import org.mule.runtime.api.meta.model.deprecated.DeprecableModel;
+import org.mule.runtime.api.meta.model.function.FunctionModel;
+import org.mule.runtime.api.meta.model.operation.OperationModel;
 import org.mule.runtime.api.meta.model.parameter.ParameterGroupModel;
 import org.mule.runtime.api.meta.model.parameter.ParameterModel;
 import org.mule.runtime.api.meta.model.parameter.ParameterizedModel;
+import org.mule.runtime.api.meta.model.source.SourceModel;
 import org.mule.runtime.api.meta.model.util.IdempotentExtensionWalker;
 import org.mule.runtime.extension.api.loader.ExtensionModelValidator;
 import org.mule.runtime.extension.api.loader.Problem;
@@ -33,22 +43,104 @@ public class DeprecationModelValidator implements ExtensionModelValidator {
     if (!isCompiletime(extensionModel)) {
       return;
     }
+
+    validateDeprecationVersions(extensionModel, problemsReporter);
+
     new IdempotentExtensionWalker() {
 
       @Override
       protected void onParameter(ParameterizedModel owner, ParameterGroupModel groupModel, ParameterModel model) {
+        validateDeprecationVersions(model, problemsReporter);
         if (model.isRequired() && model.isDeprecated()) {
           problemsReporter.addError(new Problem(model,
-                                                String
-                                                    .format("Parameter \"%s\" from %s named \"%s\" is required but has been marked as deprecated. Required parameters cannot be deprecated. Use the @Optional annotation to make it optional and add a default value.",
-                                                            model.getName(), getComponentModelTypeName(owner), owner.getName())));
+                                                format("Parameter \"%s\" from %s named \"%s\" is required but has been marked as deprecated. Required parameters cannot be deprecated. Use the @Optional annotation to make it optional and add a default value.",
+                                                       model.getName(), getComponentModelTypeName(owner), owner.getName())));
         }
       }
 
+      @Override
+      protected void onConnectionProvider(ConnectionProviderModel model) {
+        validateDeprecationVersions(model, problemsReporter);
+      }
 
+      @Override
+      protected void onSource(SourceModel model) {
+        validateDeprecationVersions(model, problemsReporter);
+      }
+
+      @Override
+      protected void onConstruct(ConstructModel model) {
+        validateDeprecationVersions(model, problemsReporter);
+      }
+
+      @Override
+      protected void onOperation(OperationModel model) {
+        validateDeprecationVersions(model, problemsReporter);
+      }
+
+      @Override
+      protected void onFunction(FunctionModel model) {
+        validateDeprecationVersions(model, problemsReporter);
+      }
+
+      @Override
+      protected void onConfiguration(ConfigurationModel model) {
+        validateDeprecationVersions(model, problemsReporter);
+      }
 
     }.walk(extensionModel);
 
+  }
+
+  private void validateDeprecationVersions(DeprecableModel deprecableModel, ProblemsReporter problemsReporter) {
+    deprecableModel.getDeprecationModel().ifPresent(deprecationModel -> {
+      MuleVersion deprecatedSince = null;
+      MuleVersion toRemoveIn = null;
+      try {
+        deprecatedSince = new MuleVersion(deprecationModel.getDeprecatedSince());
+      } catch (IllegalArgumentException e) {
+        reportInvalidVersion(deprecationModel.getDeprecatedSince(), deprecableModel, problemsReporter, "since");
+      }
+      try {
+        toRemoveIn = deprecationModel.getToRemoveIn().map(toRemoveInString -> new MuleVersion(toRemoveInString)).orElse(null);
+      } catch (IllegalArgumentException e) {
+        deprecationModel.getToRemoveIn()
+            .ifPresent(toRemoveInString -> reportInvalidVersion(toRemoveInString, deprecableModel, problemsReporter,
+                                                                "toRemoveIn"));
+      }
+
+      if (deprecatedSince != null && toRemoveIn != null && !deprecatedSince.priorTo(toRemoveIn)) {
+        reportSinceVersionPriorToRemoveVersion(deprecatedSince, toRemoveIn, deprecableModel, problemsReporter);
+      }
+
+    });
+  }
+
+  private void reportInvalidVersion(String versionString, DeprecableModel deprecableModel, ProblemsReporter problemsReporter,
+                                    String versionType) {
+    if (deprecableModel instanceof NamedObject) {
+      problemsReporter.addError(new Problem((NamedObject) deprecableModel,
+                                            format("The %s named %s was deprecated with an invalid '%s' version : '%s' . This version must follow the semver convention",
+                                                   getModelTypeName(deprecableModel),
+                                                   ((NamedObject) deprecableModel).getName(), versionType,
+                                                   versionString)));
+    }
+  }
+
+  private void reportSinceVersionPriorToRemoveVersion(MuleVersion deprecatedSince, MuleVersion toRemoveIn,
+                                                      DeprecableModel deprecableModel, ProblemsReporter problemsReporter) {
+    if (deprecableModel instanceof NamedObject) {
+      problemsReporter
+          .addError(new Problem((NamedObject) deprecableModel,
+                                format("The versions chosen for the deprecation of the %s named %s are invalid, `since`(%s) version must be prior to the `removeTo`(%s) version.",
+                                       getModelTypeName(deprecableModel), ((NamedObject) deprecableModel).getName(),
+                                       deprecatedSince.toString(), toRemoveIn.toString())));
+    }
+  }
+
+  private String getModelTypeName(DeprecableModel deprecableModel) {
+    return deprecableModel instanceof ParameterizedModel ? getComponentModelTypeName((ParameterizedModel) deprecableModel)
+        : "parameter";
   }
 
 }
--- modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/loader/validation/DeprecationModelValidatorTestCase.java
+++ modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/loader/validation/DeprecationModelValidatorTestCase.java
@@ -6,12 +6,15 @@
  */
 package org.mule.runtime.module.extension.internal.loader.validation;
 
-
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
 import static java.util.Optional.ofNullable;
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 
 import org.mule.runtime.api.meta.model.ExtensionModel;
@@ -19,6 +22,7 @@ import org.mule.runtime.api.meta.model.operation.OperationModel;
 import org.mule.runtime.api.meta.model.parameter.ParameterGroupModel;
 import org.mule.runtime.api.meta.model.parameter.ParameterModel;
 import org.mule.runtime.extension.api.loader.ProblemsReporter;
+import org.mule.runtime.extension.api.model.deprecated.ImmutableDeprecationModel;
 import org.mule.runtime.module.extension.internal.loader.java.property.CompileTimeModelProperty;
 import org.mule.tck.junit4.AbstractMuleTestCase;
 import org.mule.tck.size.SmallTest;
@@ -56,15 +60,23 @@ public class DeprecationModelValidatorTestCase extends AbstractMuleTestCase {
     when(extensionModel.getFunctionModels()).thenReturn(emptyList());
     when(extensionModel.getConnectionProviders()).thenReturn(emptyList());
     when(extensionModel.getSourceModels()).thenReturn(emptyList());
+    when(extensionModel.getDeprecationModel()).thenReturn(empty());
+    when(extensionModel.getName()).thenReturn("extensionName");
     when(operationModel.getParameterGroupModels()).thenReturn(asList(parameterGroupModel));
+    when(operationModel.getDeprecationModel()).thenReturn(empty());
+    when(operationModel.getName()).thenReturn("extensionOperation");
     when(parameterGroupModel.getParameterModels()).thenReturn(asList(parameterModel));
-    when(parameterModel.isDeprecated()).thenReturn(true);
+    when(parameterModel.isDeprecated()).thenReturn(false);
+    when(parameterModel.getDeprecationModel()).thenReturn(empty());
+    when(parameterModel.getName()).thenReturn("parameterName");
+
 
     when(extensionModel.getModelProperty(CompileTimeModelProperty.class)).thenReturn(ofNullable(new CompileTimeModelProperty()));
   }
 
   @Test
-  public void validDeprecatedOperation() {
+  public void optionalDeprecatedParameter() {
+    when(parameterModel.isDeprecated()).thenReturn(true);
     ProblemsReporter problemsReporter = new ProblemsReporter(extensionModel);
     when(parameterModel.isRequired()).thenReturn(false);
     validator.validate(extensionModel, problemsReporter);
@@ -72,11 +84,59 @@ public class DeprecationModelValidatorTestCase extends AbstractMuleTestCase {
   }
 
   @Test
-  public void invalidDeprecatedOperation() {
+  public void requiredDeprecatedParameter() {
+    when(parameterModel.isDeprecated()).thenReturn(true);
     ProblemsReporter problemsReporter = new ProblemsReporter(extensionModel);
     when(parameterModel.isRequired()).thenReturn(true);
     validator.validate(extensionModel, problemsReporter);
     assertTrue(problemsReporter.hasErrors());
+  }
+
+  @Test
+  public void deprecatedParameterWithInvalidVersion() {
+    when(parameterModel.isDeprecated()).thenReturn(true);
+    when(parameterModel.isRequired()).thenReturn(false);
+    when(parameterModel.getDeprecationModel())
+        .thenReturn(of(new ImmutableDeprecationModel("This parameter is deprecated", "2.hola", null)));
+    ProblemsReporter problemsReporter = new ProblemsReporter(extensionModel);
+    validator.validate(extensionModel, problemsReporter);
+    assertTrue(problemsReporter.hasErrors());
+    assertThat(problemsReporter.getErrors().get(0).getMessage(),
+               is("The parameter named parameterName was deprecated with an invalid 'since' version : '2.hola' . This version must follow the semver convention"));
+  }
+
+  @Test
+  public void deprecatedOperationWithInvalidVersion() {
+    when(operationModel.isDeprecated()).thenReturn(true);
+    when(operationModel.getDeprecationModel())
+        .thenReturn(of(new ImmutableDeprecationModel("This operation is deprecated", "2.hola", null)));
+    ProblemsReporter problemsReporter = new ProblemsReporter(extensionModel);
+    validator.validate(extensionModel, problemsReporter);
+    assertTrue(problemsReporter.hasErrors());
+    assertThat(problemsReporter.getErrors().get(0).getMessage(),
+               is("The operation named extensionOperation was deprecated with an invalid 'since' version : '2.hola' . This version must follow the semver convention"));
+  }
+
+  @Test
+  public void deprecatedOperationWithValidVersions() {
+    when(operationModel.isDeprecated()).thenReturn(true);
+    when(operationModel.getDeprecationModel())
+        .thenReturn(of(new ImmutableDeprecationModel("This operation is deprecated", "2.2.3", "3.0.0")));
+    ProblemsReporter problemsReporter = new ProblemsReporter(extensionModel);
+    validator.validate(extensionModel, problemsReporter);
+    assertFalse(problemsReporter.hasErrors());
+  }
+
+  @Test
+  public void deprecatedOperationWithToRemoveInPriorToSinceVersion() {
+    when(operationModel.isDeprecated()).thenReturn(true);
+    when(operationModel.getDeprecationModel())
+        .thenReturn(of(new ImmutableDeprecationModel("This operation is deprecated", "2.2.0", "2.0.0")));
+    ProblemsReporter problemsReporter = new ProblemsReporter(extensionModel);
+    validator.validate(extensionModel, problemsReporter);
+    assertTrue(problemsReporter.hasErrors());
+    assertThat(problemsReporter.getErrors().get(0).getMessage(),
+               is("The versions chosen for the deprecation of the operation named extensionOperation are invalid, `since`(2.2.0) version must be prior to the `removeTo`(2.0.0) version."));
   }
 
 }
