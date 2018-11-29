5aa4dad30013d54262a9e4869b523227841c4915
Rodrigo Merino <elrodro83@users.noreply.github.com>
2018-11-29 17:15:05 -0300

    MULE-16101: Miscellaneous performance improvements (#7468)
    (cherry picked from commit 27e0a98d84bfac95456d1faa1b17d70ac428538d)

--- core-tests/src/test/java/org/mule/runtime/core/internal/processor/strategy/TransactionAwareProactorStreamProcessingStrategyTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/processor/strategy/TransactionAwareProactorStreamProcessingStrategyTestCase.java
@@ -45,6 +45,7 @@ public class TransactionAwareProactorStreamProcessingStrategyTestCase extends Pr
                                                                 () -> cpuLight,
                                                                 () -> blocking,
                                                                 () -> cpuIntensive,
+                                                                () -> custom,
                                                                 MAX_VALUE);
   }
 
