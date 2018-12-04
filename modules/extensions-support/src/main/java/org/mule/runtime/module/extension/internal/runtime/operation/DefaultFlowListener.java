f655503e8edaefde71323f1d4ff463aef7df42b5
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2018-12-04 17:24:59 -0300

    MULE-16165: FlowListener#onComplete() should await for root completion (#7473)

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/operation/DefaultFlowListener.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/operation/DefaultFlowListener.java
@@ -7,8 +7,7 @@
 package org.mule.runtime.module.extension.internal.runtime.operation;
 
 import static org.mule.runtime.api.util.Preconditions.checkArgument;
-import static reactor.core.publisher.Mono.from;
-
+import static org.slf4j.LoggerFactory.getLogger;
 import org.mule.runtime.api.message.Message;
 import org.mule.runtime.api.meta.model.ExtensionModel;
 import org.mule.runtime.api.meta.model.operation.OperationModel;
@@ -17,22 +16,21 @@ import org.mule.runtime.core.internal.exception.MessagingException;
 import org.mule.runtime.core.privileged.event.BaseEventContext;
 import org.mule.runtime.extension.api.runtime.operation.FlowListener;
 
-import org.reactivestreams.Publisher;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.function.Consumer;
 
+import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+
 /**
- * Default implementatio of {@link FlowListener}.
+ * Default implementation of {@link FlowListener}.
  * <p>
- * It uses an {@link CoreEvent}'s response {@link Publisher} to suscribe to the event termination and execute the necessary logic.
+ * It uses an {@link CoreEvent}'s response {@link Publisher} to subscribe to the event termination and execute the necessary logic.
  *
  * @since 4.0
  */
 public class DefaultFlowListener implements FlowListener {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(DefaultFlowListener.class);
+  private static final Logger LOGGER = getLogger(DefaultFlowListener.class);
 
   private final ExtensionModel extensionModel;
   private final OperationModel operationModel;
@@ -49,7 +47,9 @@ public class DefaultFlowListener implements FlowListener {
   public DefaultFlowListener(ExtensionModel extensionModel, OperationModel operationModel, CoreEvent event) {
     this.extensionModel = extensionModel;
     this.operationModel = operationModel;
-    ((BaseEventContext) event.getContext()).onResponse((responseEvent, t) -> onTerminate(responseEvent, t));
+    BaseEventContext context = (BaseEventContext) event.getContext();
+    context.onResponse(this::onResponse);
+    context.getRootContext().onTerminated((e, t) -> onTerminated());
   }
 
   /**
@@ -79,32 +79,32 @@ public class DefaultFlowListener implements FlowListener {
     onComplete = handler;
   }
 
-  private void onTerminate(CoreEvent event, Throwable error) {
-    try {
-      if (event != null && successConsumer != null) {
-        try {
-          successConsumer.accept(event.getMessage());
-        } catch (Exception e) {
-          LOGGER.warn("Operation " + operationModel.getName() + " from extension " + extensionModel.getName()
-              + " threw exception while executing the onSuccess FlowListener", e);
-        }
-      } else if (error != null && errorConsumer != null) {
-        Exception exception = error instanceof Exception ? (Exception) error : new MessagingException(event, error);
-        try {
-          errorConsumer.accept(exception);
-        } catch (Exception e) {
-          LOGGER.warn("Operation " + operationModel.getName() + " from extension " + extensionModel.getName()
-              + " threw exception while executing the onError FlowListener", e);
-        }
+  private void onResponse(CoreEvent event, Throwable error) {
+    if (event != null && successConsumer != null) {
+      try {
+        successConsumer.accept(event.getMessage());
+      } catch (Exception e) {
+        LOGGER.warn("Operation " + operationModel.getName() + " from extension " + extensionModel.getName()
+                        + " threw exception while executing the onSuccess FlowListener", e);
       }
-    } finally {
-      if (onComplete != null) {
-        try {
-          onComplete.run();
-        } catch (Exception e) {
-          LOGGER.warn("Operation " + operationModel.getName() + " from extension " + extensionModel.getName()
-              + " threw exception while executing the onComplete FlowListener", e);
-        }
+    } else if (error != null && errorConsumer != null) {
+      Exception exception = error instanceof Exception ? (Exception) error : new MessagingException(event, error);
+      try {
+        errorConsumer.accept(exception);
+      } catch (Exception e) {
+        LOGGER.warn("Operation " + operationModel.getName() + " from extension " + extensionModel.getName()
+                        + " threw exception while executing the onError FlowListener", e);
+      }
+    }
+  }
+
+  private void onTerminated() {
+    if (onComplete != null) {
+      try {
+        onComplete.run();
+      } catch (Exception e) {
+        LOGGER.warn("Operation " + operationModel.getName() + " from extension " + extensionModel.getName()
+                        + " threw exception while executing the onComplete FlowListener", e);
       }
     }
   }
