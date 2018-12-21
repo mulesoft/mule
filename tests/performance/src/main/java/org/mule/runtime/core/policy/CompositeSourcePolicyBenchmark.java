3a127e3325c38c55fd0b239a332791b8635cd489
Rodro <rodrigo.merino@mulesoft.com>
2018-12-21 12:16:48 -0300

    microbenchmark!

---
+++ tests/performance/src/main/java/org/mule/runtime/core/policy/CompositeSourcePolicyBenchmark.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.policy;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyMap;
+import static java.util.Optional.empty;
+import static java.util.concurrent.TimeUnit.MICROSECONDS;
+import static org.mule.runtime.core.api.event.EventContextFactory.create;
+import static reactor.core.publisher.Mono.from;
+
+import org.mule.AbstractBenchmark;
+import org.mule.runtime.api.message.Message;
+import org.mule.runtime.core.api.event.CoreEvent;
+import org.mule.runtime.core.api.exception.NullExceptionHandler;
+import org.mule.runtime.core.api.functional.Either;
+import org.mule.runtime.core.api.policy.Policy;
+import org.mule.runtime.core.api.policy.PolicyChain;
+import org.mule.runtime.core.api.util.func.CheckedFunction;
+import org.mule.runtime.core.internal.policy.CompositeSourcePolicy;
+import org.mule.runtime.core.internal.policy.MessageSourceResponseParametersProcessor;
+import org.mule.runtime.core.internal.policy.SourcePolicy;
+import org.mule.runtime.core.internal.policy.SourcePolicyFailureResult;
+import org.mule.runtime.core.internal.policy.SourcePolicySuccessResult;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Setup;
+import org.reactivestreams.Publisher;
+
+import java.util.Map;
+
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(MICROSECONDS)
+public class CompositeSourcePolicyBenchmark extends AbstractBenchmark {
+
+  private SourcePolicy handler;
+  private MessageSourceResponseParametersProcessor sourceRpp;
+
+  @Setup(Level.Trial)
+  public void setUp() {
+    System.out.println("Trial?");
+    handler = new CompositeSourcePolicy(asList(new Policy(new PolicyChain() {
+
+      @Override
+      public Publisher<CoreEvent> apply(Publisher<CoreEvent> publisher) {
+        return publisher;
+      }
+    }, "")), empty(), (policy, nextProcessor) -> nextProcessor);
+
+    sourceRpp = new MessageSourceResponseParametersProcessor() {
+
+      @Override
+      public CheckedFunction<CoreEvent, Map<String, Object>> getSuccessfulExecutionResponseParametersFunction() {
+        return event -> emptyMap();
+      }
+
+      @Override
+      public CheckedFunction<CoreEvent, Map<String, Object>> getFailedExecutionResponseParametersFunction() {
+        return event -> emptyMap();
+      }
+    };
+  }
+
+  @Benchmark
+  public Either<SourcePolicyFailureResult, SourcePolicySuccessResult> source() {
+    CoreEvent event;
+    Message.Builder messageBuilder = Message.builder().value(PAYLOAD);
+    CoreEvent.Builder eventBuilder =
+        CoreEvent.builder(create("", "", CONNECTOR_LOCATION, NullExceptionHandler.getInstance())).message(messageBuilder.build());
+    event = eventBuilder.build();
+
+    return from(handler.process(event, eventPub -> eventPub, sourceRpp)).block();
+  }
+
+}
