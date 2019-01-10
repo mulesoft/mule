8ae159b4b0ccfeefea0b2321100dd0fb3ebcdc50
Guillermo Fernandes <gsfernandes@users.noreply.github.com>
2019-01-10 09:28:54 -0300

    MULE-16230: VersionFormatArtifactDescriptorValidator fails due to Mave… (#7564)
    ...nBundleDescriptorLoader is not considering the effective pom model to read the version property - Use pom.properties to collect data for bundleDescriptor instead of resolving the effective pom

--- modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/maven/MavenBundleDescriptorLoader.java
+++ modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/maven/MavenBundleDescriptorLoader.java
@@ -7,26 +7,20 @@
 
 package org.mule.runtime.module.deployment.impl.internal.maven;
 
-import static java.util.Optional.empty;
-import static java.util.Optional.of;
-import static org.mule.maven.client.api.MavenClientProvider.discoverProvider;
-import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
 import static org.mule.runtime.core.api.config.bootstrap.ArtifactType.APP;
 import static org.mule.runtime.core.api.config.bootstrap.ArtifactType.DOMAIN;
 import static org.mule.runtime.core.api.config.bootstrap.ArtifactType.PLUGIN;
-import static org.mule.runtime.core.api.config.bootstrap.ArtifactType.POLICY;
+import static org.mule.runtime.deployment.model.api.application.ApplicationDescriptor.MULE_APPLICATION_CLASSIFIER;
+import static org.mule.runtime.deployment.model.api.application.ApplicationDescriptor.MULE_DOMAIN_CLASSIFIER;
 import static org.mule.runtime.deployment.model.api.artifact.ArtifactDescriptorConstants.MULE_LOADER_ID;
 import static org.mule.runtime.deployment.model.api.plugin.ArtifactPluginDescriptor.MULE_PLUGIN_CLASSIFIER;
-import static org.mule.runtime.globalconfig.api.GlobalConfigLoader.getMavenConfig;
 import static org.mule.runtime.module.deployment.impl.internal.maven.AbstractMavenClassLoaderModelLoader.CLASSLOADER_MODEL_JSON_DESCRIPTOR;
 import static org.mule.runtime.module.deployment.impl.internal.maven.AbstractMavenClassLoaderModelLoader.CLASSLOADER_MODEL_JSON_DESCRIPTOR_LOCATION;
 import static org.mule.runtime.module.deployment.impl.internal.maven.MavenUtils.getPomModelFolder;
 import static org.mule.runtime.module.deployment.impl.internal.maven.MavenUtils.getPomModelFromJar;
+import static org.mule.runtime.module.deployment.impl.internal.maven.MavenUtils.getPomPropertiesFolder;
+import static org.mule.runtime.module.deployment.impl.internal.maven.MavenUtils.getPomPropertiesFromJar;
 import static org.mule.tools.api.classloader.ClassLoaderModelJsonSerializer.deserialize;
-import org.mule.maven.client.api.MavenClient;
-import org.mule.maven.client.api.MavenClientProvider;
-import org.mule.maven.client.api.model.MavenConfiguration;
-import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.core.api.config.bootstrap.ArtifactType;
 import org.mule.runtime.deployment.model.api.plugin.ArtifactPluginDescriptor;
 import org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptorCreateException;
@@ -36,7 +30,7 @@ import org.mule.runtime.module.artifact.api.descriptor.InvalidDescriptorLoaderEx
 
 import java.io.File;
 import java.util.Map;
-import java.util.concurrent.locks.StampedLock;
+import java.util.Properties;
 
 import org.apache.maven.model.Model;
 
@@ -48,31 +42,21 @@ public class MavenBundleDescriptorLoader implements BundleDescriptorLoader {
 
   private static final String JAR = "jar";
 
-  private BundleDescriptorLoader bundleDescriptorLoader;
-
-  public MavenBundleDescriptorLoader() {
-    bundleDescriptorLoader = new MuleContainerBundleDescriptorLoader();
-  }
-
-  public MavenBundleDescriptorLoader(MavenClient mavenClient) {
-    bundleDescriptorLoader = new FixedMavenBundleDescriptorLoader(mavenClient);
-  }
-
   @Override
   public String getId() {
     return MULE_LOADER_ID;
   }
 
   /**
-   * Looks for the POM file within the current {@code artifactFile} jar or folder structure (under
-   * {@link org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptor#MULE_ARTIFACT_FOLDER} folder)
-   * to retrieve the artifact locator.
+   * Looks for the POM file within the current {@code pluginFolder} structure (under
+   * {@link ArtifactPluginDescriptor#MULE_ARTIFACT_FOLDER} folder) to retrieve the plugin artifact locator.
    *
    * @param artifactFile {@link File} with the content of the artifact to work with. Non null
    * @param attributes collection of attributes describing the loader. Non null.
    * @param artifactType the type of the artifact of the descriptor to be loaded.
-   * @return a locator of the coordinates of the current artifact
-   * @throws ArtifactDescriptorCreateException if the bundle descriptor cannot be loaded.
+   * @return a locator of the coordinates of the current plugin
+   * @throws ArtifactDescriptorCreateException if the plugin is missing the {@link ArtifactPluginDescriptor#MULE_PLUGIN_POM} or
+   *         there's an issue while reading that file
    */
   @Override
   public BundleDescriptor load(File artifactFile, Map<String, Object> attributes, ArtifactType artifactType)
@@ -94,8 +78,38 @@ public class MavenBundleDescriptorLoader implements BundleDescriptorLoader {
     }
   }
 
-  public BundleDescriptor getBundleDescriptor(File artifactFile, ArtifactType artifactType) {
-    return bundleDescriptorLoader.getBundleDescriptor(artifactFile, artifactType);
+  private BundleDescriptor getBundleDescriptor(File artifactFile, ArtifactType artifactType) {
+    BundleDescriptor.Builder builder = new BundleDescriptor.Builder();
+
+    if (artifactType.equals(APP) || artifactType.equals(DOMAIN)) {
+      Properties pomProperties;
+      if (artifactFile.isDirectory()) {
+        pomProperties = getPomPropertiesFolder(artifactFile);
+      } else {
+        pomProperties = getPomPropertiesFromJar(artifactFile);
+      }
+      return builder.setGroupId(pomProperties.getProperty("groupId"))
+          .setArtifactId(pomProperties.getProperty("artifactId"))
+          .setVersion(pomProperties.getProperty("version"))
+          .setClassifier(artifactType.equals(APP) ? MULE_APPLICATION_CLASSIFIER : MULE_DOMAIN_CLASSIFIER)
+          .build();
+    } else {
+      Model model;
+      if (artifactFile.isDirectory()) {
+        model = getPomModelFolder(artifactFile);
+      } else {
+        model = getPomModelFromJar(artifactFile);
+      }
+
+      return new BundleDescriptor.Builder()
+          .setArtifactId(model.getArtifactId())
+          .setGroupId(model.getGroupId() != null ? model.getGroupId() : model.getParent().getGroupId())
+          .setVersion(model.getVersion() != null ? model.getVersion() : model.getParent().getVersion())
+          .setType(JAR)
+          // Handle manually the packaging for mule plugin as the mule plugin maven plugin defines the packaging as mule-extension
+          .setClassifier(artifactType.equals(PLUGIN) ? MULE_PLUGIN_CLASSIFIER : model.getPackaging())
+          .build();
+    }
   }
 
   private boolean isHeavyPackage(File artifactFile) {
@@ -107,107 +121,6 @@ public class MavenBundleDescriptorLoader implements BundleDescriptorLoader {
       return new File(artifactFile, CLASSLOADER_MODEL_JSON_DESCRIPTOR_LOCATION);
     } else {
       return new File(artifactFile.getParent(), CLASSLOADER_MODEL_JSON_DESCRIPTOR);
-    }
-  }
-
-  interface BundleDescriptorLoader {
-
-    BundleDescriptor getBundleDescriptor(File artifactFile, ArtifactType artifactType);
-
-  }
-
-
-  private class MuleContainerBundleDescriptorLoader implements BundleDescriptorLoader {
-
-    private MavenClientProvider mavenClientProvider;
-    private MavenConfiguration mavenRuntimeConfig;
-
-    private FixedMavenBundleDescriptorLoader fixedMavenBundleDescriptorLoader;
-
-    private StampedLock lock = new StampedLock();
-
-    MuleContainerBundleDescriptorLoader() {
-      mavenClientProvider = discoverProvider(MavenClientProvider.class.getClassLoader());
-      mavenRuntimeConfig = getMavenConfig();
-
-      createFixedMavenBundleDescriptorLoader();
-    }
-
-    private void createFixedMavenBundleDescriptorLoader() {
-      fixedMavenBundleDescriptorLoader =
-          new FixedMavenBundleDescriptorLoader(mavenClientProvider.createMavenClient(mavenRuntimeConfig));
-    }
-
-    @Override
-    public BundleDescriptor getBundleDescriptor(File artifactFile, ArtifactType artifactType) {
-      long stamp = lock.readLock();
-      try {
-        MavenConfiguration updatedMavenConfiguration = getMavenConfig();
-        if (!mavenRuntimeConfig.equals(updatedMavenConfiguration)) {
-          long writeStamp = lock.tryConvertToWriteLock(stamp);
-          if (writeStamp == 0L) {
-            lock.unlockRead(stamp);
-            stamp = lock.writeLock();
-          } else {
-            stamp = writeStamp;
-          }
-          if (!mavenRuntimeConfig.equals(updatedMavenConfiguration)) {
-            mavenRuntimeConfig = updatedMavenConfiguration;
-            createFixedMavenBundleDescriptorLoader();
-          }
-        }
-
-        return fixedMavenBundleDescriptorLoader.getBundleDescriptor(artifactFile, artifactType);
-      } finally {
-        lock.unlock(stamp);
-      }
-    }
-  }
-
-  private class FixedMavenBundleDescriptorLoader implements BundleDescriptorLoader {
-
-    private MavenClient mavenClient;
-    private File temporaryFolder;
-
-    public FixedMavenBundleDescriptorLoader(MavenClient mavenClient) {
-      this.mavenClient = mavenClient;
-
-      File localMavenRepositoryLocation = mavenClient.getMavenConfiguration().getLocalMavenRepositoryLocation();
-      temporaryFolder = new File(localMavenRepositoryLocation, ".mule");
-      if (!temporaryFolder.exists() && !temporaryFolder.mkdirs()) {
-        throw new MuleRuntimeException(createStaticMessage("Cannot create a temporary folder for loading bundle descriptor at %s",
-                                                           temporaryFolder.getAbsolutePath()));
-      }
-    }
-
-    @Override
-    public BundleDescriptor getBundleDescriptor(File artifactFile, ArtifactType artifactType) {
-      Model model;
-
-      // Only user defined artifacts could support properties place holder and require to resolve an effective pom. Services are kind of
-      // mixed packaging where they have a heavy packaging but still pom.xml has to be read to retrieve the bundle descriptor data.
-      if (artifactType.equals(APP) || artifactType.equals(DOMAIN) || artifactType.equals(POLICY) || artifactType.equals(PLUGIN)) {
-        if (artifactFile.isDirectory()) {
-          model = mavenClient.getEffectiveModel(artifactFile, empty());
-        } else {
-          model = mavenClient.getEffectiveModel(artifactFile, of(temporaryFolder));
-        }
-      } else {
-        if (artifactFile.isDirectory()) {
-          model = getPomModelFolder(artifactFile);
-        } else {
-          model = getPomModelFromJar(artifactFile);
-        }
-      }
-
-      return new BundleDescriptor.Builder()
-          .setArtifactId(model.getArtifactId())
-          .setGroupId(model.getGroupId() != null ? model.getGroupId() : model.getParent().getGroupId())
-          .setVersion(model.getVersion() != null ? model.getVersion() : model.getParent().getVersion())
-          .setType(JAR)
-          // Handle manually the packaging for mule plugin as the mule plugin maven plugin defines the packaging as mule-extension
-          .setClassifier(artifactType.equals(PLUGIN) ? MULE_PLUGIN_CLASSIFIER : model.getPackaging())
-          .build();
     }
   }
 }
