6bb7bf9dc48fe7a13c8b3ecdfde2c6cfbc70add7
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2019-02-06 13:15:34 -0300

    format

--- core/src/main/java/org/mule/runtime/core/internal/util/JdkVersionUtils.java
+++ core/src/main/java/org/mule/runtime/core/internal/util/JdkVersionUtils.java
@@ -292,7 +292,7 @@ public class JdkVersionUtils {
     }
     if (!isSupportedJdkVendor()) {
       logger.info("You're executing with a JDK made by a vendor that is not on the recommended list of vendors. Vendor: "
-                      + JAVA_VENDOR + " Please consider changing to a recommended JDK vendor.");
+          + JAVA_VENDOR + " Please consider changing to a recommended JDK vendor.");
     }
   }
 }
