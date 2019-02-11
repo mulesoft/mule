d3b79aaca6ed4eb0f92e97ce81c1188ec018ea4e
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-02-11 11:53:18 -0300

    Missing header

--- modules/http/src/test/java/org/mule/module/http/internal/listener/grizzly/ResponseDeferringCompletionHandlerTestCase.java
+++ modules/http/src/test/java/org/mule/module/http/internal/listener/grizzly/ResponseDeferringCompletionHandlerTestCase.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.module.http.internal.listener.grizzly;
 
 import static org.apache.http.HttpHeaders.TRANSFER_ENCODING;
