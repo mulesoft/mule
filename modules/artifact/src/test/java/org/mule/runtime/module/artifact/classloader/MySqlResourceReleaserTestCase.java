72be2995b4c36f35310956593771926322afa615
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-02-19 14:19:49 -0300

    Refactoring in testcase

--- modules/artifact/src/test/java/org/mule/runtime/module/artifact/classloader/MySqlResourceReleaserTestCase.java
+++ modules/artifact/src/test/java/org/mule/runtime/module/artifact/classloader/MySqlResourceReleaserTestCase.java
@@ -9,11 +9,10 @@ package org.mule.runtime.module.artifact.classloader;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
-import static org.mule.runtime.module.artifact.api.classloader.ParentFirstLookupStrategy.PARENT_FIRST;
-import org.mule.module.artifact.classloader.DefaultResourceReleaser;
+import static org.mule.runtime.module.artifact.api.classloader.ChildFirstLookupStrategy.CHILD_FIRST;
 import org.mule.runtime.core.api.util.ClassUtils;
 import org.mule.runtime.module.artifact.api.classloader.ClassLoaderLookupPolicy;
 import org.mule.runtime.module.artifact.api.classloader.LookupStrategy;
@@ -21,79 +20,82 @@ import org.mule.runtime.module.artifact.api.classloader.MuleArtifactClassLoader;
 import org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptor;
 import org.mule.tck.junit4.AbstractMuleTestCase;
 
-import java.lang.reflect.Field;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Map;
-import java.util.stream.Collectors;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.mockito.stubbing.Answer;
 
 @RunWith(Parameterized.class)
 public class MySqlResourceReleaserTestCase extends AbstractMuleTestCase {
 
-  private final String testingClassname;
+  private static final String MYSQL_RESOURCE_RELEASER_CLASS_LOCATION =
+      "/org/mule/runtime/module/artifact/classloader/MySqlTestResourceReleaser.class";
 
-  public static String foundClassname = "";
+  private final String classnameBeingTested;
 
-  public MySqlResourceReleaserTestCase(String cleanupThreadClassname) {
-    testingClassname = cleanupThreadClassname;
+  // Used to verify the connection cleaner class package found
+  public static String foundClassname;
+  private final String mySqlDriverJarname;
+  private ClassLoaderLookupPolicy testLookupPolicy;
+
+  // Parameterized
+  public MySqlResourceReleaserTestCase(String cleanupThreadClassname, String jarName) {
+    classnameBeingTested = cleanupThreadClassname;
+    mySqlDriverJarname = jarName;
+
+    testLookupPolicy = new ClassLoaderLookupPolicy() {
+
+      @Override
+      public LookupStrategy getClassLookupStrategy(String className) {
+        return CHILD_FIRST;
+      }
+
+      @Override
+      public LookupStrategy getPackageLookupStrategy(String packageName) {
+        return null;
+      }
+
+      @Override
+      public ClassLoaderLookupPolicy extend(Map<String, LookupStrategy> lookupStrategies) {
+        return null;
+      }
+    };
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    foundClassname = "Wrong one";
   }
 
   @Parameterized.Parameters(name = "Testing classname {0}")
-  public static Collection<Object[]> data() throws NoSuchFieldException, IllegalAccessException {
-    Field connectionsCleanerThreadClassnames =
-        DefaultResourceReleaser.class.getDeclaredField("CONNECTION_CLEANUP_THREAD_KNOWN_CLASS_ADDRESES");
-    connectionsCleanerThreadClassnames.setAccessible(true);
-    Collection<String> cleanerThreadClassNames =
-        (Collection<String>) connectionsCleanerThreadClassnames.get(DefaultResourceReleaser.class);
-    return cleanerThreadClassNames.stream()
-        .map(threadClassname -> new Object[] {threadClassname})
-        .collect(Collectors.toCollection(() -> new ArrayList<>()));
+  public static Object[][] data() throws NoSuchFieldException, IllegalAccessException {
+    return new Object[][]{
+            {"com.mysql.jdbc.AbandonedConnectionCleanupThread", "mysql/mysql-driver-v5.jar"},
+            {"com.mysql.cj.jdbc.AbandonedConnectionCleanupThread", "mysql/mysql-driver-v5.jar"}
+    };
   }
-
-  public static final String MYSQL_RESOURCE_RELEASER_CLASS_LOCATION =
-      "/org/mule/runtime/module/artifact/classloader/MySqlTestResourceReleaser.class";
-
-  private String classPackageFound;
 
   @Test
-  public void testMySql5DriverCleanupThreadClassIsFound() throws ClassNotFoundException {
-    ClassLoaderLookupPolicy lookupPolicyMock = mock(ClassLoaderLookupPolicy.class);
-    when(lookupPolicyMock.getClassLookupStrategy(anyString())).thenReturn(mock(LookupStrategy.class));
-
+  public void testMySqlDriverCleanupThreadClassIsFound() throws ClassNotFoundException {
     MuleArtifactClassLoader artifactClassLoader =
-        spy(new MuleArtifactClassLoader("test", mock(ArtifactDescriptor.class), new URL[] {ClassUtils.getResource("mysql/mysql-driver-v5.jar",this.getClass())}, Thread.currentThread().getContextClassLoader(), new ClassLoaderLookupPolicy() {
-
-          @Override
-          public LookupStrategy getClassLookupStrategy(String className) {
-            return PARENT_FIRST;
-          }
-
-          @Override
-          public LookupStrategy getPackageLookupStrategy(String packageName) {
-            return null;
-          }
-
-          @Override
-          public ClassLoaderLookupPolicy extend(Map<String, LookupStrategy> lookupStrategies) {
-            return null;
-          }
-        }));
+        spy(new MuleArtifactClassLoader("test", mock(ArtifactDescriptor.class),
+                                    new URL[] {ClassUtils.getResource(mySqlDriverJarname, this.getClass())},
+                                    Thread.currentThread().getContextClassLoader(), testLookupPolicy));
+    doAnswer((Answer) invocationOnMock -> {
+      String classBeingLoaded = invocationOnMock.getArgument(0);
+        if (classBeingLoaded.equals(classnameBeingTested) || classBeingLoaded.equals(MYSQL_RESOURCE_RELEASER_CLASS_LOCATION)) {
+            return invocationOnMock.callRealMethod();
+        } else {
+          return null;
+        }
+    }).when(artifactClassLoader).loadClass(anyString());
 
     artifactClassLoader.setResourceReleaserClassLocation(MYSQL_RESOURCE_RELEASER_CLASS_LOCATION);
-
-/*    doAnswer((Answer<Class>) invocationOnMock -> {
-      Class driverClass = mock(Class.class);
-      when(driverClass.getCanonicalName()).thenReturn("perro");
-      return driverClass;
-    }).when(artifactClassLoader).loadClass(anyString());*/
-
     artifactClassLoader.dispose();
-    assertThat(foundClassname, is(testingClassname));
+    assertThat(foundClassname, is(classnameBeingTested));
   }
-
 }
