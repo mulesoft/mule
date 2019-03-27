ca1dd5b7a4dc0b72463020f50ecd9da69b74fe19
Euge <emariotti3@users.noreply.github.com>
2019-03-27 10:25:08 -0300

    MULE-15189:All services APIs should be annotated with NoImplement. (#7750)

--- modules/soap-api/src/main/java/org/mule/runtime/soap/api/SoapService.java
+++ modules/soap-api/src/main/java/org/mule/runtime/soap/api/SoapService.java
@@ -9,13 +9,14 @@ package org.mule.runtime.soap.api;
 import org.mule.runtime.api.service.Service;
 import org.mule.runtime.soap.api.client.SoapClient;
 import org.mule.runtime.soap.api.client.SoapClientFactory;
-
+import org.mule.api.annotation.NoImplement;
 
 /**
  * Contract for a service that provides a Soap client factory.
  *
  * @since 4.0
  */
+@NoImplement
 public interface SoapService extends Service {
 
   /**
