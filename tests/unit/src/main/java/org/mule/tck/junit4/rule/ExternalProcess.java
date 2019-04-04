a0fa7f556c75e6c9688b527a81acdb89ca12dce4
Rodro <rodrigo.merino@mulesoft.com>
2019-04-04 18:57:31 -0300

    review

--- tests/unit/src/main/java/org/mule/tck/junit4/rule/ExternalProcess.java
+++ tests/unit/src/main/java/org/mule/tck/junit4/rule/ExternalProcess.java
@@ -22,6 +22,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Predicate;
 
 /**
  * Executes a process that starts a server required by the test.
@@ -32,13 +33,15 @@ public final class ExternalProcess extends ExternalResource {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ExternalProcess.class);
 
+  private final Predicate<String> daemonStartedPredicate;
   private final String[] processCommand;
 
   private Process p;
   private ExecutorService daemonExecutor;
   private Future<?> daemonFuture;
 
-  public ExternalProcess(String... processCommand) {
+  public ExternalProcess(Predicate<String> daemonStartedPredicate, String... processCommand) {
+    this.daemonStartedPredicate = daemonStartedPredicate;
     this.processCommand = processCommand;
   }
 
@@ -46,7 +49,7 @@ public final class ExternalProcess extends ExternalResource {
   protected void before() throws Throwable {
     super.before();
 
-    LOGGER.trace("Starting esternal process: " + asList(processCommand));
+    LOGGER.trace("Starting external process: " + asList(processCommand));
 
     ProcessBuilder pb = new ProcessBuilder(processCommand);
     pb.redirectErrorStream(true);
@@ -63,7 +66,7 @@ public final class ExternalProcess extends ExternalResource {
         while (line != null) {
 
           LOGGER.warn(line);
-          if (line.contains("org.eclipse.jetty.server.ServerConnector: Started ServerConnector")) {
+          if (daemonStartedPredicate.test(line)) {
             Thread.sleep(500);
             started.set(true);
             // return;
