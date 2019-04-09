45da6080c4b7344fafff378752721acdc7ef7d14
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2019-04-09 13:19:53 -0300

    MULE-12159: java11 deployment model fixes

--- modules/deployment-model/src/test/java/org/mule/runtime/deployment/model/internal/domain/MuleSharedDomainClassLoaderTestCase.java
+++ modules/deployment-model/src/test/java/org/mule/runtime/deployment/model/internal/domain/MuleSharedDomainClassLoaderTestCase.java
@@ -13,34 +13,23 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mule.runtime.container.api.MuleFoldersUtil.getDomainFolder;
 import static org.mule.runtime.deployment.model.api.domain.DomainDescriptor.DEFAULT_DOMAIN_NAME;
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
-
 import org.mule.runtime.core.api.config.MuleProperties;
 import org.mule.runtime.core.api.util.FileUtils;
 import org.mule.runtime.module.artifact.api.classloader.ClassLoaderLookupPolicy;
-import org.mule.runtime.module.artifact.api.classloader.MuleArtifactClassLoader;
 import org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptor;
 import org.mule.tck.junit4.AbstractMuleTestCase;
 import org.mule.tck.junit4.rule.SystemPropertyTemporaryFolder;
 import org.mule.tck.size.SmallTest;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.File;
 import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 
+import org.junit.Rule;
+import org.junit.Test;
+
 @SmallTest
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(MuleArtifactClassLoader.class)
-@PowerMockIgnore("javax.management.*")
 public class MuleSharedDomainClassLoaderTestCase extends AbstractMuleTestCase {
 
   public static final String RESOURCE_FILE_NAME = "file.properties";
@@ -49,11 +38,6 @@ public class MuleSharedDomainClassLoaderTestCase extends AbstractMuleTestCase {
   public SystemPropertyTemporaryFolder temporaryFolder =
       new SystemPropertyTemporaryFolder(MuleProperties.MULE_HOME_DIRECTORY_PROPERTY);
   private final ClassLoaderLookupPolicy lookupPolicy = mock(ClassLoaderLookupPolicy.class);
-
-  @Before
-  public void setUp() throws Exception {
-    mockStatic(MuleArtifactClassLoader.class);
-  }
 
   @Test
   public void findResourcesInProvidedUrls() throws Exception {
