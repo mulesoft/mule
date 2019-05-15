d1430e50ea8e0cec44b9c9a85df4acfc50c67f26
Martin Buchwald <martin.buchwald@mulesoft.com>
2019-05-15 12:08:56 -0300

    MULE-16846: DB QueryTimeout not being able to set this value from configuration property

--- modules/db/src/test/java/org/mule/module/db/integration/select/SelectTimeoutTestCase.java
+++ modules/db/src/test/java/org/mule/module/db/integration/select/SelectTimeoutTestCase.java
@@ -7,11 +7,19 @@
 
 package org.mule.module.db.integration.select;
 
+import org.junit.Rule;
+import org.junit.Test;
 import org.mule.module.db.integration.AbstractQueryTimeoutTestCase;
 import org.mule.module.db.integration.model.AbstractTestDatabase;
+import org.mule.tck.junit4.rule.SystemProperty;
 
 public class SelectTimeoutTestCase extends AbstractQueryTimeoutTestCase
 {
+
+    private static final String QUERY_TIMEOUT_PARAMETER_FLOW = "queryTimeoutParameter";
+
+    @Rule
+    public SystemProperty queryTimeoutProperty = new SystemProperty("queryTimeoutParamValue", "1");
 
     public SelectTimeoutTestCase(String dataSourceConfigResource, AbstractTestDatabase testDatabase)
     {
@@ -22,5 +30,11 @@ public class SelectTimeoutTestCase extends AbstractQueryTimeoutTestCase
     protected String[] getFlowConfigurationResources()
     {
         return new String[] {"integration/select/select-timeout-config.xml"};
+    }
+
+    @Test
+    public void timeoutsQueryParameterTest() throws Exception
+    {
+        timeoutsQuery(QUERY_TIMEOUT_PARAMETER_FLOW);
     }
 }
