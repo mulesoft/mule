fbf8930b68f3ba21af5587726b1da1fd1a8ecf1e
Guillermo Fernandes <gsfernandes@users.noreply.github.com>
2019-05-16 10:15:28 -0300

    MULE-16824: LazyInit should initialize the Mule configuration at part… (#7865)
    ... of the component initializations phase
    
    (cherry picked from commit 037fbd27561d27718902c4f7eb1882da6a51d8f6)

--- core/src/main/java/org/mule/runtime/core/api/config/DefaultMuleConfiguration.java
+++ core/src/main/java/org/mule/runtime/core/api/config/DefaultMuleConfiguration.java
@@ -31,10 +31,6 @@ import org.mule.runtime.core.api.util.NetworkUtils;
 import org.mule.runtime.core.api.util.StringUtils;
 import org.mule.runtime.core.api.util.UUID;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.BooleanUtils;
-import org.slf4j.Logger;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.UnknownHostException;
@@ -45,6 +41,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.BooleanUtils;
+import org.slf4j.Logger;
+
 /**
  * Configuration info. which can be set when creating the MuleContext but becomes immutable after starting the MuleContext. TODO
  * MULE-13121 Cleanup MuleConfiguration removing redundant config in Mule 4
@@ -53,6 +53,8 @@ import java.util.Map;
 public class DefaultMuleConfiguration implements MuleConfiguration, MuleContextAware, InternalComponent {
 
   protected static final Logger logger = getLogger(DefaultMuleConfiguration.class);
+
+  private boolean lazyInit;
 
   /**
    * When true, each event will keep trace information of the flows and components it traverses to be shown as part of an
@@ -476,8 +478,17 @@ public class DefaultMuleConfiguration implements MuleConfiguration, MuleContextA
     }
   }
 
+  public boolean isLazyInit() {
+    return lazyInit;
+  }
+
+  public void setLazyInit(boolean lazyInit) {
+    this.lazyInit = lazyInit;
+  }
+
   protected boolean verifyContextNotInitialized() {
-    if (muleContext != null && muleContext.getLifecycleManager().isPhaseComplete(Initialisable.PHASE_NAME)) {
+    // LazyInit needs to be able to change the configuration
+    if (muleContext != null && !isLazyInit() && muleContext.getLifecycleManager().isPhaseComplete(Initialisable.PHASE_NAME)) {
       logger.warn("Cannot modify MuleConfiguration once the MuleContext has been initialized.  Modification will be ignored.");
       return false;
     } else {
@@ -486,7 +497,8 @@ public class DefaultMuleConfiguration implements MuleConfiguration, MuleContextA
   }
 
   protected boolean verifyContextNotStarted() {
-    if (muleContext != null && muleContext.getLifecycleManager().isPhaseComplete(Startable.PHASE_NAME)) {
+    // LazyInit needs to be able to change the configuration
+    if (muleContext != null && !isLazyInit() && muleContext.getLifecycleManager().isPhaseComplete(Startable.PHASE_NAME)) {
       logger.warn("Cannot modify MuleConfiguration once the MuleContext has been started.  Modification will be ignored.");
       return false;
     } else {
