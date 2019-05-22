b9582e0709171fb7c51b9ed660ae4b39447fb572
Martín Buchwald <martin.buchwald@mulesoft.com>
2019-05-22 14:41:57 -0300

    MULE-16878: StringToEnum Transformer should have different names for each extensionModel (#7903)
    (cherry picked from commit fa00f27273ce7b8e21eda2993b884e7b8e38516f)

--- core-tests/src/test/java/org/mule/runtime/core/internal/transformer/simple/StringToEnumTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/transformer/simple/StringToEnumTestCase.java
@@ -6,6 +6,7 @@
  */
 package org.mule.runtime.core.internal.transformer.simple;
 
+import static java.lang.String.format;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -48,6 +49,12 @@ public class StringToEnumTestCase extends AbstractMuleTestCase {
   public void nullClass() {
     expectedException.expect(IllegalArgumentException.class);
     new StringToEnum(null);
+  }
+
+  @Test
+  public void name() {
+    String name = format("StringTo%sTransformer", TestEnum.class.getName());
+    assertThat(transformer.getName(), is(name));
   }
 
 }
