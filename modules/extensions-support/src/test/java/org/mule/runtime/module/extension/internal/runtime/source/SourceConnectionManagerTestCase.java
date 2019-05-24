64bd13dc3394ce067c7a254b8ca8112d98bd9f63
Rodrigo Merino <elrodro83@users.noreply.github.com>
2019-05-24 14:44:39 -0300

    MULE-16896: Bound connections not released on TransactionException (#7914)

--- modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/runtime/source/SourceConnectionManagerTestCase.java
+++ modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/runtime/source/SourceConnectionManagerTestCase.java
@@ -6,24 +6,24 @@
  */
 package org.mule.runtime.module.extension.internal.runtime.source;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Answers.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.when;
+
+import org.mule.runtime.api.connection.ConnectionException;
 import org.mule.runtime.api.connection.ConnectionProvider;
 import org.mule.runtime.core.api.MuleContext;
 import org.mule.runtime.core.api.connector.ConnectionManager;
 import org.mule.runtime.core.internal.connection.DefaultConnectionManager;
 import org.mule.runtime.extension.api.runtime.config.ConfigurationInstance;
 import org.mule.tck.junit4.AbstractMuleTestCase;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Answers.RETURNS_DEEP_STUBS;
-import static org.mockito.Mockito.when;
 
-import org.mule.runtime.api.connection.ConnectionException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SourceConnectionManagerTestCase extends AbstractMuleTestCase {
@@ -64,6 +64,9 @@ public class SourceConnectionManagerTestCase extends AbstractMuleTestCase {
     sourceConnectionManager.release(connectionA);
 
     assertThat(sourceConnectionManager.getConnectionHandler(connectionA).isPresent(), is(false));
+
+    // This must be ignored
+    sourceConnectionManager.release(connectionA);
   }
 
   @Test
@@ -75,5 +78,8 @@ public class SourceConnectionManagerTestCase extends AbstractMuleTestCase {
     sourceConnectionManager.invalidate(connectionA);
 
     assertThat(sourceConnectionManager.getConnectionHandler(connectionA).isPresent(), is(false));
+
+    // This must be ignored
+    sourceConnectionManager.invalidate(connectionA);
   }
 }
