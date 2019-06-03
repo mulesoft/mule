7b45a9906bdf6ceab3433396eca8d5716152afc0
Ana Felisatti <afelisatti@mulesoft.com>
2019-06-03 17:27:15 -0300

    MULE-16949: Correct spelling in internal method (#7950)

--- modules/http/src/main/java/org/mule/module/http/internal/listener/HttpResponseBuilder.java
+++ modules/http/src/main/java/org/mule/module/http/internal/listener/HttpResponseBuilder.java
@@ -305,7 +305,7 @@ public class HttpResponseBuilder extends HttpMessageBuilder implements Initialis
             logger.debug("Content-Length encoding is being used so the 'Transfer-Encoding' header has been removed");
             httpResponseHeaderBuilder.removeHeader(TRANSFER_ENCODING);
         }
-        httpResponseHeaderBuilder.setContentLenght(String.valueOf(contentLength));
+        httpResponseHeaderBuilder.setContentLength(String.valueOf(contentLength));
     }
 
     private void setupChunkedEncoding(HttpResponseHeaderBuilder httpResponseHeaderBuilder)
