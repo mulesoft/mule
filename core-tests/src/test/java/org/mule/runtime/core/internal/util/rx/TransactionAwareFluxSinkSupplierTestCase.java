89f7fcec46c6e591b9a8b022ee98a78b23a9fe20
Martín Buchwald <martin.buchwald@mulesoft.com>
2019-06-03 16:58:22 -0300

    MULE-16936: sinks must be used by only one thread when running a transaction (#7940)

---
+++ core-tests/src/test/java/org/mule/runtime/core/internal/util/rx/TransactionAwareFluxSinkSupplierTestCase.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.internal.util.rx;
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static java.util.Collections.synchronizedSet;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mule.runtime.api.tx.TransactionException;
+import org.mule.runtime.core.api.transaction.Transaction;
+import org.mule.runtime.core.api.transaction.TransactionCoordination;
+import reactor.core.publisher.FluxSink;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class TransactionAwareFluxSinkSupplierTestCase {
+
+  private static final int THREAD_TEST = 10;
+
+  private FluxSinkSupplier mockSupplier = mock(FluxSinkSupplier.class);
+  private FluxSink mockSink = mock(FluxSink.class);
+  private FluxSinkSupplier<FluxSink> txSupplier;
+
+  @Before
+  public void setUp() {
+    when(mockSupplier.get()).thenReturn(mockSink);
+    txSupplier = new TransactionAwareFluxSinkSupplier(() -> mock(FluxSink.class), mockSupplier);
+  }
+
+  @Test
+  public void delegatesWhenNotInTx() {
+    assertThat(txSupplier.get(), is(mockSink));
+    // Assert that it doesn't change between calls
+    assertThat(txSupplier.get(), is(mockSink));
+  }
+
+  @Test
+  public void returnsNewSinkWhenInTx() throws TransactionException {
+    Transaction tx = mock(Transaction.class);
+    try {
+      TransactionCoordination.getInstance().bindTransaction(tx);
+      FluxSink supplied = txSupplier.get();
+      // Assert is another sink
+      assertThat(supplied, is(not(mockSink)));
+      // Assert that supplied for same thread supplies same sink
+      assertThat(txSupplier.get(), is(supplied));
+    } finally {
+      TransactionCoordination.getInstance().unbindTransaction(tx);
+    }
+  }
+
+  @Test
+  public void newSinkPerThread() throws Exception {
+    List<Thread> threads = new ArrayList<>();
+    Set<FluxSink> sinks = synchronizedSet(new HashSet<>());
+    for (int i = 0; i < THREAD_TEST; i++) {
+      Thread thread = new Thread(() -> {
+        Transaction tx = mock(Transaction.class);
+        try {
+          try {
+            TransactionCoordination.getInstance().bindTransaction(tx);
+            FluxSink sink = txSupplier.get();
+            // Assert that supplied for same thread supplies same sink
+            assertThat(txSupplier.get(), is(sink));
+            sinks.add(sink);
+          } finally {
+            TransactionCoordination.getInstance().unbindTransaction(tx);
+          }
+        } catch (TransactionException e) {
+
+        }
+      });
+      threads.add(thread);
+    }
+
+    for (Thread t : threads) {
+      t.start();
+    }
+    for (Thread t : threads) {
+      t.join();
+    }
+
+    // Every thread created has a different sink
+    assertThat(sinks, hasSize(THREAD_TEST));
+  }
+
+}
--- core/src/main/java/org/mule/runtime/core/internal/policy/CompositeOperationPolicy.java
+++ core/src/main/java/org/mule/runtime/core/internal/policy/CompositeOperationPolicy.java
@@ -26,7 +26,9 @@ import org.mule.runtime.core.api.util.concurrent.FunctionalReadWriteLock;
 import org.mule.runtime.core.internal.exception.MessagingException;
 import org.mule.runtime.core.internal.message.InternalEvent;
 import org.mule.runtime.core.internal.rx.FluxSinkRecorder;
+import org.mule.runtime.core.internal.util.rx.FluxSinkSupplier;
 import org.mule.runtime.core.internal.util.rx.RoundRobinFluxSinkSupplier;
+import org.mule.runtime.core.internal.util.rx.TransactionAwareFluxSinkSupplier;
 import org.mule.runtime.core.privileged.event.BaseEventContext;
 
 import com.github.benmanes.caffeine.cache.LoadingCache;
@@ -64,7 +66,7 @@ public class CompositeOperationPolicy
 
   private final OperationPolicyProcessorFactory operationPolicyProcessorFactory;
 
-  private final LoadingCache<String, RoundRobinFluxSinkSupplier<CoreEvent>> policySinks;
+  private final LoadingCache<String, FluxSinkSupplier<CoreEvent>> policySinks;
 
   private final AtomicBoolean disposed;
   private final FunctionalReadWriteLock readWriteLock;
@@ -90,11 +92,15 @@ public class CompositeOperationPolicy
     this.disposed = new AtomicBoolean(false);
     this.readWriteLock = readWriteLock();
     this.policySinks = newBuilder()
-        .removalListener((String key, RoundRobinFluxSinkSupplier<CoreEvent> value, RemovalCause cause) -> {
+        .removalListener((String key, FluxSinkSupplier<CoreEvent> value, RemovalCause cause) -> {
           value.dispose();
         })
-        .build(componentLocation -> new RoundRobinFluxSinkSupplier<>(getRuntime().availableProcessors(),
-                                                                     new OperationWithPoliciesFluxObjectFactory()));
+        .build(componentLocation -> {
+          Supplier<FluxSink<CoreEvent>> factory = new OperationWithPoliciesFluxObjectFactory();
+          return new TransactionAwareFluxSinkSupplier<>(factory,
+                                                        new RoundRobinFluxSinkSupplier<>(getRuntime().availableProcessors(),
+                                                                                         factory));
+        });
   }
 
   private final class OperationWithPoliciesFluxObjectFactory implements Supplier<FluxSink<CoreEvent>> {
