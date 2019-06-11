47be9afc235511ed7a172d731f2858ce59861a0c
Guillermo Fernandes <gsfernandes@users.noreply.github.com>
2019-06-11 16:48:31 -0300

    MULE-16924: Support lightweight deployment with class loader model an… (#7932)
    ...d local repository

--- modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/plugin/PluginMavenClassLoaderModelLoader.java
+++ modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/plugin/PluginMavenClassLoaderModelLoader.java
@@ -6,12 +6,17 @@
  */
 package org.mule.runtime.module.deployment.impl.internal.plugin;
 
+import static java.nio.file.Paths.get;
+import static java.util.Optional.empty;
 import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
 import static org.mule.runtime.core.api.config.bootstrap.ArtifactType.PLUGIN;
 import static org.mule.runtime.deployment.model.api.artifact.ArtifactDescriptorConstants.MULE_LOADER_ID;
 import static org.mule.runtime.deployment.model.api.plugin.ArtifactPluginDescriptor.MULE_PLUGIN_CLASSIFIER;
+import static org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptor.META_INF;
+import static org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptor.MULE_ARTIFACT;
 import static org.mule.runtime.module.artifact.api.descriptor.BundleScope.SYSTEM;
 import static org.mule.tools.api.classloader.ClassLoaderModelJsonSerializer.deserialize;
+
 import org.mule.maven.client.api.MavenClient;
 import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.core.api.config.bootstrap.ArtifactType;
@@ -19,12 +24,14 @@ import org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptor;
 import org.mule.runtime.module.artifact.api.descriptor.BundleDependency;
 import org.mule.runtime.module.artifact.api.descriptor.BundleDescriptor;
 import org.mule.runtime.module.artifact.api.descriptor.ClassLoaderModel;
+import org.mule.runtime.module.artifact.api.descriptor.InvalidDescriptorLoaderException;
 import org.mule.runtime.module.deployment.impl.internal.maven.AbstractMavenClassLoaderModelLoader;
 import org.mule.runtime.module.deployment.impl.internal.maven.ArtifactClassLoaderModelBuilder;
 import org.mule.runtime.module.deployment.impl.internal.maven.HeavyweightClassLoaderModelBuilder;
 import org.mule.runtime.module.deployment.impl.internal.maven.LightweightClassLoaderModelBuilder;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -44,6 +51,31 @@ public class PluginMavenClassLoaderModelLoader extends AbstractMavenClassLoaderM
 
   public PluginMavenClassLoaderModelLoader(MavenClient mavenClient) {
     super(mavenClient);
+  }
+
+  @Override
+  protected ClassLoaderModel createClassLoaderModel(File artifactFile, Map<String, Object> attributes, ArtifactType artifactType)
+      throws InvalidDescriptorLoaderException {
+    if (super.isHeavyPackage(artifactFile, attributes)) {
+      return super.createClassLoaderModel(artifactFile, attributes, artifactType);
+    }
+
+    // If it is a lightweight which uses the local repository a class-loader-model.json may be present in the META-INF/mule-artifact
+    if (attributes instanceof PluginExtendedClassLoaderModelAttributes) {
+      PluginExtendedClassLoaderModelAttributes pluginExtendedClassLoaderModelAttributes =
+          (PluginExtendedClassLoaderModelAttributes) attributes;
+      BundleDescriptor pluginBundleDescriptor =
+          (BundleDescriptor) pluginExtendedClassLoaderModelAttributes.get(BundleDescriptor.class.getName());
+      File rootFolder = pluginExtendedClassLoaderModelAttributes.getDeployableArtifactDescriptor().getRootFolder();
+      Path muleArtifactJson =
+          get(rootFolder.getAbsolutePath(), META_INF, MULE_ARTIFACT, pluginBundleDescriptor.getGroupId(),
+              pluginBundleDescriptor.getArtifactId(), pluginBundleDescriptor.getBaseVersion(),
+              CLASSLOADER_MODEL_JSON_DESCRIPTOR);
+      if (muleArtifactJson.toFile().exists()) {
+        return createHeavyPackageClassLoaderModel(artifactFile, muleArtifactJson.toFile(), attributes, empty());
+      }
+    }
+    return createLightPackageClassLoaderModel(artifactFile, attributes, artifactType);
   }
 
   @Override
