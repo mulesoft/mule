9df77781964df71d1a0f9bb96c3a32c549d8d569
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-06-11 10:07:26 -0300

    Improved explanation about deferred subscription method.

--- core/src/main/java/org/mule/runtime/core/internal/rx/RxUtils.java
+++ core/src/main/java/org/mule/runtime/core/internal/rx/RxUtils.java
@@ -16,20 +16,28 @@ import reactor.core.publisher.Flux;
  */
 public class RxUtils {
 
-  // TODO: Improve this JavaDoc. It's very unclear and redundant.
   /**
-   * Defers the subscription of the <it>deferredSubscriber</it> until <it>subscriptionTrigger</it> subscribes. When that happens,
+   * Defers the subscription of the <it>deferredSubscriber</it> until <it>triggeringSubscriber</it> subscribes. Once that occurs
    * the latter subscription will take place on the same context. For an example of this, look at
    * {@link org.mule.runtime.core.internal.routing.ChoiceRouter}
    *
-   * @param subscriptionTrigger The {@link Flux} whose subscription will trigger the subscription of the
+   * This serves its purpose in some in which the are two Fluxes, A and B, and are related in that in some part of A's reactor
+   * chain, the processed event is published into a sink that belongs to B. Also, suppose that some of A's processors need to be
+   * initialized in order to make the whole assembled chain work. In those cases, one may want to do A's subscription after it has
+   * initialized, and once B has subscribed.
+   * 
+   * A -----> B's Sink -> B -------> downstream chain
+   *
+   * In this method, A corresponds to <it>deferredSubscriber</it>; and B to <it>triggeringSubscriber</it>.
+   *
+   * @param triggeringSubscriber the {@link Flux} whose subscription will trigger the subscription of the
    *        <it>deferredSubscriber</it> {@link Flux}, on the same context as the former one.
-   * @param deferredSubscriber The {@link Flux} that whose subscription will be deferred
-   * @return the subscriptionTrigger {@link Flux}, decorated with the callback that will perform this deferred subscription.
+   * @param deferredSubscriber the {@link Flux} whose subscription will be deferred
+   * @return the triggeringSubscriber {@link Flux}, decorated with the callback that will perform this deferred subscription.
    */
-  public static Flux<CoreEvent> subscribeFluxOnPublisherSubscription(Flux<CoreEvent> subscriptionTrigger,
+  public static Flux<CoreEvent> subscribeFluxOnPublisherSubscription(Flux<CoreEvent> triggeringSubscriber,
                                                                      Flux<CoreEvent> deferredSubscriber) {
-    return subscriptionTrigger
+    return triggeringSubscriber
         .compose(eventPub -> subscriberContext()
             .flatMapMany(ctx -> eventPub.doOnSubscribe(s -> deferredSubscriber.subscriberContext(ctx).subscribe())));
   }
