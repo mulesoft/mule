5c68a1010c20e4a98f39e45a1047bce1549d70a7
Nacho Raik <ignacioraiksg@gmail.com>
2019-06-12 17:11:57 -0300

    MULE-16904: Fix import for 4.1.x

--- core-tests/src/test/java/org/mule/runtime/core/internal/security/DefaultMuleSecurityManagerTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/security/DefaultMuleSecurityManagerTestCase.java
@@ -32,7 +32,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.runners.MockitoJUnitRunner;
 
 
 @SmallTest
