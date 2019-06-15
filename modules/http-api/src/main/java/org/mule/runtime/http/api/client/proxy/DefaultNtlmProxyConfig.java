32ba706dcc7e79692e13383b520bc82bb2fd2a09
Rodrigo Merino <elrodro83@users.noreply.github.com>
2019-06-15 10:37:46 -0300

    MULE-17006: add equals/hashcode to proxy configs (#7978)
    (cherry picked from commit e677f700332e9c39cef9f3daff1e675c1e41e1e8)

--- modules/http-api/src/main/java/org/mule/runtime/http/api/client/proxy/DefaultNtlmProxyConfig.java
+++ modules/http-api/src/main/java/org/mule/runtime/http/api/client/proxy/DefaultNtlmProxyConfig.java
@@ -6,12 +6,16 @@
  */
 package org.mule.runtime.http.api.client.proxy;
 
+import static java.util.Objects.hash;
+
+import java.util.Objects;
+
 /**
  * Basic implementation of a {@link NtlmProxyConfig}. Instances can only be obtained through a {@link NtlmProxyConfigBuilder}.
  */
 class DefaultNtlmProxyConfig extends DefaultProxyConfig implements ProxyConfig.NtlmProxyConfig {
 
-  private String ntlmDomain;
+  private final String ntlmDomain;
 
   DefaultNtlmProxyConfig(String host, int port, String username, String password, String ntlmDomain, String nonProxyHosts) {
     super(host, port, username, password, nonProxyHosts);
@@ -22,4 +26,33 @@ class DefaultNtlmProxyConfig extends DefaultProxyConfig implements ProxyConfig.N
   public String getNtlmDomain() {
     return ntlmDomain;
   }
+
+  @Override
+  public int hashCode() {
+    return hash(super.hashCode(), ntlmDomain);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (!super.equals(obj)) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+
+    DefaultNtlmProxyConfig that = (DefaultNtlmProxyConfig) obj;
+
+    return Objects.equals(this.getHost(), that.getHost())
+        && Objects.equals(this.getPort(), that.getPort())
+        && Objects.equals(this.getPassword(), that.getPassword())
+        && Objects.equals(this.getUsername(), that.getUsername())
+        && Objects.equals(this.getNonProxyHosts(), that.getNonProxyHosts())
+        && Objects.equals(this.ntlmDomain, that.ntlmDomain);
+  }
+
+
 }
