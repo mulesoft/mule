9777cde6ad5d090a85e03c197688268e6e8cf398
SebaElizalde <sebi.elizalde@gmail.com>
2019-06-19 11:24:44 -0300

    Added missing header

--- core-tests/src/test/java/org/mule/runtime/core/api/source/scheduler/RunNowCronSchedulerWrapperTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/api/source/scheduler/RunNowCronSchedulerWrapperTestCase.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.runtime.core.api.source.scheduler;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -26,115 +32,115 @@ import org.junit.Test;
 @SmallTest
 public class RunNowCronSchedulerWrapperTestCase {
 
-    private Integer count;
+  private Integer count;
 
-    @Test
-    public void wrapperRunsTwice() throws Exception {
-        count = 0;
-        CronScheduler cronScheduler = new CronScheduler();
-        cronScheduler.setExpression("0/30 0/1 * 1/1 * ? *");
-        cronScheduler.setTimeZone("GMT");
-        RunNowCronSchedulerWrapper schedulerWrapper = new RunNowCronSchedulerWrapper(cronScheduler);
-        Scheduler executor = new TestScheduler(1, "custom", true);
-        schedulerWrapper.doSchedule(executor, () -> {
-            count++;
-        });
-        executor.shutdown();
-        if (executor.awaitTermination(1000, TimeUnit.MILLISECONDS)) {
-            assertThat(count, equalTo(2));
-        } else {
-            fail("The executor took too long to run the scheduled tasks.");
-        }
+  @Test
+  public void wrapperRunsTwice() throws Exception {
+    count = 0;
+    CronScheduler cronScheduler = new CronScheduler();
+    cronScheduler.setExpression("0/30 0/1 * 1/1 * ? *");
+    cronScheduler.setTimeZone("GMT");
+    RunNowCronSchedulerWrapper schedulerWrapper = new RunNowCronSchedulerWrapper(cronScheduler);
+    Scheduler executor = new TestScheduler(1, "custom", true);
+    schedulerWrapper.doSchedule(executor, () -> {
+      count++;
+    });
+    executor.shutdown();
+    if (executor.awaitTermination(1000, TimeUnit.MILLISECONDS)) {
+      assertThat(count, equalTo(2));
+    } else {
+      fail("The executor took too long to run the scheduled tasks.");
+    }
+  }
+
+  private class TestScheduler extends ScheduledThreadPoolExecutor implements Scheduler {
+
+    private String threadNamePrefix;
+    private ExecutorService executor;
+
+    public TestScheduler(int threads, String threadNamePrefix, boolean reject) {
+      super(1, new NamedThreadFactory(threadNamePrefix + ".tasks"));
+      this.threadNamePrefix = threadNamePrefix;
+      executor = new ThreadPoolExecutor(threads, threads, 0l, TimeUnit.MILLISECONDS,
+                                        new LinkedBlockingQueue(reject ? threads : Integer.MAX_VALUE),
+                                        new NamedThreadFactory(threadNamePrefix));
     }
 
-    private class TestScheduler extends ScheduledThreadPoolExecutor implements Scheduler {
-
-        private String threadNamePrefix;
-        private ExecutorService executor;
-
-        public TestScheduler(int threads, String threadNamePrefix, boolean reject) {
-            super(1, new NamedThreadFactory(threadNamePrefix + ".tasks"));
-            this.threadNamePrefix = threadNamePrefix;
-            executor = new ThreadPoolExecutor(threads, threads, 0l, TimeUnit.MILLISECONDS,
-                    new LinkedBlockingQueue(reject ? threads : Integer.MAX_VALUE),
-                    new NamedThreadFactory(threadNamePrefix));
-        }
-
-        @Override
-        public Future<?> submit(Runnable task) {
-            return executor.submit(task);
-        }
-
-        @Override
-        public Future<?> submit(Callable task) {
-            return executor.submit(task);
-        }
-
-        @Override
-        public void stop() {
-            shutdown();
-            executor.shutdown();
-        }
-
-        @Override
-        public ScheduledFuture<?> scheduleWithCronExpression(Runnable command, String cronExpression) {
-            command.run();
-            return new NullScheduledFuture();
-        }
-
-        @Override
-        public ScheduledFuture<?> scheduleWithCronExpression(Runnable command, String cronExpression, TimeZone timeZone) {
-            command.run();
-            return new NullScheduledFuture();
-        }
-
-        @Override
-        public String getName() {
-            return threadNamePrefix;
-        }
-
+    @Override
+    public Future<?> submit(Runnable task) {
+      return executor.submit(task);
     }
 
-    private class NullScheduledFuture<V> implements ScheduledFuture<V> {
-
-        private NullScheduledFuture() {
-            // Nothing to do
-        }
-
-        @Override
-        public long getDelay(TimeUnit unit) {
-            return 0;
-        }
-
-        @Override
-        public int compareTo(Delayed o) {
-            return 0;
-        }
-
-        @Override
-        public boolean cancel(boolean mayInterruptIfRunning) {
-            return false;
-        }
-
-        @Override
-        public boolean isCancelled() {
-            return false;
-        }
-
-        @Override
-        public boolean isDone() {
-            return false;
-        }
-
-        @Override
-        public V get() throws InterruptedException, ExecutionException {
-            return null;
-        }
-
-        @Override
-        public V get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-            return null;
-        }
+    @Override
+    public Future<?> submit(Callable task) {
+      return executor.submit(task);
     }
+
+    @Override
+    public void stop() {
+      shutdown();
+      executor.shutdown();
+    }
+
+    @Override
+    public ScheduledFuture<?> scheduleWithCronExpression(Runnable command, String cronExpression) {
+      command.run();
+      return new NullScheduledFuture();
+    }
+
+    @Override
+    public ScheduledFuture<?> scheduleWithCronExpression(Runnable command, String cronExpression, TimeZone timeZone) {
+      command.run();
+      return new NullScheduledFuture();
+    }
+
+    @Override
+    public String getName() {
+      return threadNamePrefix;
+    }
+
+  }
+
+  private class NullScheduledFuture<V> implements ScheduledFuture<V> {
+
+    private NullScheduledFuture() {
+      // Nothing to do
+    }
+
+    @Override
+    public long getDelay(TimeUnit unit) {
+      return 0;
+    }
+
+    @Override
+    public int compareTo(Delayed o) {
+      return 0;
+    }
+
+    @Override
+    public boolean cancel(boolean mayInterruptIfRunning) {
+      return false;
+    }
+
+    @Override
+    public boolean isCancelled() {
+      return false;
+    }
+
+    @Override
+    public boolean isDone() {
+      return false;
+    }
+
+    @Override
+    public V get() throws InterruptedException, ExecutionException {
+      return null;
+    }
+
+    @Override
+    public V get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+      return null;
+    }
+  }
 
 }
