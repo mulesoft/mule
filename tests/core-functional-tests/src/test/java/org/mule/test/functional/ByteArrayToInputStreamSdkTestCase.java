4c4f83819a87afda11647bab922f1a065faacbf9
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-06-21 11:13:50 -0300

    Moving test to core-functional-tests module

--- tests/core-functional-tests/src/test/java/org/mule/test/functional/ByteArrayToInputStreamSdkTestCase.java
+++ tests/core-functional-tests/src/test/java/org/mule/test/functional/ByteArrayToInputStreamSdkTestCase.java
@@ -4,7 +4,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.test.policy;
+package org.mule.test.functional;
 
 
 import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
