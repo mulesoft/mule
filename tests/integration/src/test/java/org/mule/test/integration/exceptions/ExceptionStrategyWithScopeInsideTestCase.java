172ad9fc36fb3625c43a454b954954439c7795bf
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-06-27 14:19:33 -0300

    Functional test case with compund scenario

---
+++ tests/integration/src/test/java/org/mule/test/integration/exceptions/ExceptionStrategyWithScopeInsideTestCase.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.integration.exceptions;
+
+import org.mule.tck.junit4.FunctionalTestCase;
+
+import org.junit.Test;
+
+public class ExceptionStrategyWithScopeInsideTestCase extends FunctionalTestCase
+{
+
+    @Override
+    protected String getConfigFile()
+    {
+        return "org/mule/test/integration/exceptions/exception-strategy-with-scopes-config.xml";
+    }
+
+    @Test
+    public void withScatterGatherRouter() throws Exception
+    {
+        runFlowWithPayloadAndExpect("error-handler-with-scatter-gather-router", "hello dog", "hello goat");
+    }
+}
