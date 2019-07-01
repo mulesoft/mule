049b38ad46d500d197f28f765f5a1fb3385296ba
Ezequiel Werner <eze210@gmail.com>
2019-07-01 16:17:05 -0300

    Merging LocalTxQueueTransaction tests

--- core-tests/src/test/java/org/mule/runtime/core/internal/util/queue/LocalTxQueueTransactionTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/util/queue/LocalTxQueueTransactionTestCase.java
@@ -8,36 +8,49 @@ package org.mule.runtime.core.internal.util.queue;
 
 import static java.lang.System.currentTimeMillis;
 import static java.lang.Thread.sleep;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mule.runtime.api.message.Message.of;
+import static org.mule.tck.util.MuleContextUtils.eventBuilder;
+
 import org.mule.runtime.core.api.MuleContext;
 import org.mule.runtime.core.api.config.DefaultMuleConfiguration;
 import org.mule.runtime.core.api.context.MuleContextBuilder;
+import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.runtime.core.api.transaction.xa.ResourceManagerException;
 import org.mule.runtime.core.api.util.queue.DefaultQueueConfiguration;
 import org.mule.runtime.core.api.util.queue.QueueConfiguration;
 import org.mule.runtime.core.internal.util.journal.queue.LocalTxQueueTransactionJournal;
+import org.mule.runtime.core.internal.util.journal.queue.LocalTxQueueTransactionRecoverer;
 import org.mule.tck.junit4.AbstractMuleContextTestCase;
 
-import java.io.Serializable;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-
+import org.apache.commons.lang3.NotImplementedException;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.mockito.stubbing.Answer;
 
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+
 public class LocalTxQueueTransactionTestCase extends AbstractMuleContextTestCase {
 
-  static final String QUEUE_NAME = "inQueue";
+  public static final String QUEUE_NAME = "inQueue";
+  private static final int TIMEOUT = 10;
+
+  private static final long QUEUE_DELAY_MILLIS = 2000;
+  private DelayedQueueStore delayedQueue;
 
   @Rule
   public TemporaryFolder temporaryFolder = new TemporaryFolder();
@@ -45,35 +58,21 @@ public class LocalTxQueueTransactionTestCase extends AbstractMuleContextTestCase
   private LocalTxQueueTransactionJournal txLog;
   private DefaultQueueStore inQueue;
   private PersistentQueueTransactionContext persistentTransactionContext;
+  private LocalTxQueueTransactionRecoverer queueTransactionRecoverer;
   private LocalTxQueueTransactionContext localTxTransactionContext;
-
-  private static final long QUEUE_DELAY_MILLIS = 2000;
-  private DelayedQueueStore delayedQueue;
 
   @Override
   protected void doSetUp() throws Exception {
     ((DefaultMuleConfiguration) muleContext.getConfiguration()).setWorkingDirectory(temporaryFolder.getRoot().getAbsolutePath());
     txLog = new LocalTxQueueTransactionJournal(temporaryFolder.getRoot().getAbsolutePath(), muleContext);
+
     QueueConfiguration queueConfiguration = new DefaultQueueConfiguration(0, true);
     inQueue = new DefaultQueueStore(QUEUE_NAME, muleContext, queueConfiguration);
     delayedQueue = new DelayedQueueStore(QUEUE_NAME, muleContext, queueConfiguration, QUEUE_DELAY_MILLIS);
-    persistentTransactionContext = new PersistentQueueTransactionContext(txLog, createQueueProvider(inQueue));
+
+    persistentTransactionContext = new PersistentQueueTransactionContext(txLog, createRecoverOnlyQueueProvider(inQueue));
+    queueTransactionRecoverer = new LocalTxQueueTransactionRecoverer(txLog, createRecoverOnlyQueueProvider(inQueue));
     localTxTransactionContext = createLocalTxContext(txLog, createQueueProvider(delayedQueue));
-  }
-
-  private QueueProvider createQueueProvider(final DefaultQueueStore queueStore) {
-    return new QueueProvider() {
-
-      @Override
-      public QueueStore getQueue(String queueName) {
-        return queueStore;
-      }
-
-      @Override
-      public RecoverableQueueStore getRecoveryQueue(String queueName) {
-        return queueStore;
-      }
-    };
   }
 
   private LocalTxQueueTransactionContext createLocalTxContext(LocalTxQueueTransactionJournal txLog, QueueProvider provider)
@@ -85,12 +84,128 @@ public class LocalTxQueueTransactionTestCase extends AbstractMuleContextTestCase
     });
     return new LocalTxQueueTransactionContext(txLog, provider, lock);
   }
-
+  
   @Override
   protected void configureMuleContext(MuleContextBuilder contextBuilder) {
     DefaultMuleConfiguration muleConfiguration = new DefaultMuleConfiguration();
     muleConfiguration.setWorkingDirectory(temporaryFolder.getRoot().getAbsolutePath());
     contextBuilder.setMuleConfiguration(muleConfiguration);
+  }
+
+  @Test
+  public void pollAndFailThenRecover() throws Exception {
+    inQueue.offer(testEvent(), 0, TIMEOUT);
+    Serializable value = persistentTransactionContext.poll(inQueue, 100000);
+    assertThat(inQueue.poll(TIMEOUT), nullValue());
+    assertThat(value, notNullValue());
+    txLog.close();
+    txLog = new LocalTxQueueTransactionJournal(temporaryFolder.getRoot().getAbsolutePath(), muleContext);
+
+    queueTransactionRecoverer.recover();
+    CoreEvent muleEvent = (CoreEvent) inQueue.poll(TIMEOUT);
+    assertThat(muleEvent, notNullValue());
+    assertThat(testEvent().getContext().getId(), equalTo(muleEvent.getContext().getId()));
+  }
+
+  @Test
+  public void pollAndFailThenRecoverWithTwoElements() throws Exception {
+    final String MESSAGE_CONTENT_2 = TEST_PAYLOAD + "2";
+    CoreEvent testEvent2 = eventBuilder(muleContext).message(of(MESSAGE_CONTENT_2)).build();
+
+    inQueue.offer(testEvent(), 0, TIMEOUT);
+    inQueue.offer(testEvent2, 0, TIMEOUT);
+    Serializable value = persistentTransactionContext.poll(inQueue, 100000);
+    assertThat(inQueue.getSize(), is(1));
+    assertThat(value, notNullValue());
+    txLog.close();
+    txLog = new LocalTxQueueTransactionJournal(temporaryFolder.getRoot().getAbsolutePath(), muleContext);
+    queueTransactionRecoverer.recover();
+    CoreEvent muleEvent = (CoreEvent) inQueue.poll(TIMEOUT);
+    assertThat(muleEvent, notNullValue());
+    assertThat(muleEvent.getMessage().getPayload().getValue().toString(), is(MESSAGE_CONTENT_2)); // recovered element should be
+                                                                                                  // last
+
+    muleEvent = (CoreEvent) inQueue.poll(TIMEOUT);
+    assertThat(muleEvent, notNullValue());
+    assertThat(muleEvent.getMessage().getPayload().getValue().toString(), is(TEST_PAYLOAD)); // recovered element
+  }
+
+  @Test
+  public void failBetweenLogEntryWriteAndRealPoolThenRecover() throws Exception {
+    inQueue.offer(testEvent(), 0, TIMEOUT);
+    persistentTransactionContext.poll(inQueue, TIMEOUT);
+    txLog.close();
+    txLog = new LocalTxQueueTransactionJournal(temporaryFolder.getRoot().getAbsolutePath(), muleContext);
+    queueTransactionRecoverer.recover();
+    CoreEvent muleEvent = (CoreEvent) inQueue.poll(TIMEOUT);
+    assertThat(muleEvent, notNullValue());
+    assertThat(testEvent().getContext().getId(), equalTo(muleEvent.getContext().getId()));
+    muleEvent = (CoreEvent) inQueue.poll(TIMEOUT);
+    assertThat(muleEvent, nullValue());
+  }
+
+  private QueueProvider createRecoverOnlyQueueProvider(final DefaultQueueStore queue) {
+    return new QueueProvider() {
+
+      @Override
+      public QueueStore getQueue(String queueName) {
+        throw new NotImplementedException("This is test code");
+      }
+
+      @Override
+      public RecoverableQueueStore getRecoveryQueue(String queueName) {
+        return queue;
+      }
+    };
+  }
+
+  private QueueProvider createQueueProvider(final DefaultQueueStore queue) {
+    return new QueueProvider() {
+
+      @Override
+      public QueueStore getQueue(String queueName) {
+        return queue;
+      }
+
+      @Override
+      public RecoverableQueueStore getRecoveryQueue(String queueName) {
+        return queue;
+      }
+    };
+  }
+
+  @Test
+  public void offerAndFailThenRecover() throws Exception {
+    final DefaultQueueStore outQueue = new DefaultQueueStore(QUEUE_NAME, muleContext, new DefaultQueueConfiguration(0, true));
+    persistentTransactionContext = new PersistentQueueTransactionContext(txLog, createRecoverOnlyQueueProvider(outQueue));
+    persistentTransactionContext.offer(outQueue, testEvent(), TIMEOUT);
+    assertThat(outQueue.poll(TIMEOUT), nullValue());
+    txLog.close();
+    txLog = new LocalTxQueueTransactionJournal(temporaryFolder.getRoot().getAbsolutePath(), muleContext);
+    queueTransactionRecoverer = new LocalTxQueueTransactionRecoverer(txLog, createRecoverOnlyQueueProvider(outQueue));
+    queueTransactionRecoverer.recover();
+    Serializable muleEvent = outQueue.poll(TIMEOUT);
+    assertThat(muleEvent, nullValue());
+  }
+
+  @Test
+  public void offerAndFailBetweenRealOfferAndCommitThenRecover() throws Exception {
+    txLog = new TestTransactionLogger(temporaryFolder.getRoot().getAbsolutePath(), muleContext).failDuringLogCommit();
+    final DefaultQueueStore outQueue = new DefaultQueueStore(QUEUE_NAME, muleContext, new DefaultQueueConfiguration(0, true));
+    persistentTransactionContext = new PersistentQueueTransactionContext(txLog, createRecoverOnlyQueueProvider(outQueue));
+    persistentTransactionContext.offer(outQueue, testEvent(), TIMEOUT);
+    try {
+      persistentTransactionContext.doCommit();
+      fail();
+    } catch (ResourceManagerException e) {
+      // expected
+    }
+    txLog.close();
+    txLog = new TestTransactionLogger(temporaryFolder.getRoot().getAbsolutePath(), muleContext);
+    queueTransactionRecoverer = new LocalTxQueueTransactionRecoverer(txLog, createRecoverOnlyQueueProvider(outQueue));
+    queueTransactionRecoverer.recover();
+    Serializable muleEvent = outQueue.poll(TIMEOUT);
+    assertThat(muleEvent, nullValue());
   }
 
   @Test
@@ -177,4 +292,5 @@ public class LocalTxQueueTransactionTestCase extends AbstractMuleContextTestCase
       return super.poll(timeout - delayMillis);
     }
   }
+
 }
