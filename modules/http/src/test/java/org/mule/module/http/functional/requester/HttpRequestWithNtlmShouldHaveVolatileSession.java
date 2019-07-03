0e59e9d137cede93bb1bd21b9e5ce49cb8dbf609
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-07-03 17:54:29 -0300

    Test against windows test machine added

--- modules/http/src/test/java/org/mule/module/http/functional/requester/HttpRequestWithNtlmShouldHaveVolatileSession.java
+++ modules/http/src/test/java/org/mule/module/http/functional/requester/HttpRequestWithNtlmShouldHaveVolatileSession.java
@@ -1,9 +1,12 @@
 package org.mule.module.http.functional.requester;
 
 import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static org.apache.http.HttpStatus.SC_PROXY_AUTHENTICATION_REQUIRED;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isA;
 import static org.mule.module.http.api.HttpHeaders.Names.AUTHORIZATION;
+import static org.mule.module.http.api.HttpHeaders.Names.PROXY_AUTHENTICATE;
+import static org.mule.module.http.api.HttpHeaders.Names.PROXY_AUTHORIZATION;
 import static org.mule.module.http.api.HttpHeaders.Names.WWW_AUTHENTICATE;
 import static org.junit.Assert.assertThat;
 import org.mule.api.MuleEvent;
@@ -16,6 +19,8 @@ import org.junit.Test;
 
 public class HttpRequestWithNtlmShouldHaveVolatileSession extends AbstractNtlmTestCase
 {
+
+    private final String CORRECT_PASSWORD = "perro";
 
     @Override
     protected String getWorkstation()
@@ -47,15 +52,16 @@ public class HttpRequestWithNtlmShouldHaveVolatileSession extends AbstractNtlmTe
     }
 
     @Before
-    public void setupNtlmAuthenticator()
+    public void regenerateTestAuthorizer()
     {
         setupTestAuthorizer(AUTHORIZATION, WWW_AUTHENTICATE, SC_UNAUTHORIZED);
     }
 
-    @Ignore
     @Test(expected = ResponseValidatorException.class)
     public void httpRequestWithNtlmAuthAndDynamicPasswordFieldShouldHaveVolatileSession() throws Exception
     {
-        runFlow("dynamicPasswordShouldUseVolatileNtlmSession");
+        runFlow("dynamicPasswordShouldUseVolatileNtlmSession", CORRECT_PASSWORD);
+        runFlow("dynamicPasswordShouldUseVolatileNtlmSession", "gato");
+        // runFlow("dynamicPasswordShouldUseVolatileNtlmSession", CORRECT_PASSWORD);
     }
 }
