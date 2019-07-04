31ba4fd3483a1703a8b63fdec026bad14353611b
Rodro <rodrigo.merino@mulesoft.com>
2019-07-04 14:12:44 -0300

    Unify duplicated logic to explore jar files

--- modules/artifact/src/main/java/org/mule/runtime/module/artifact/internal/util/JarInfo.java
+++ modules/artifact/src/main/java/org/mule/runtime/module/artifact/internal/util/JarInfo.java
@@ -9,6 +9,9 @@ package org.mule.runtime.module.artifact.internal.util;
 
 import static org.mule.runtime.api.util.Preconditions.checkArgument;
 
+import org.mule.runtime.module.artifact.api.classloader.ExportedService;
+
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -18,25 +21,42 @@ public class JarInfo {
 
   private final Set<String> packages;
   private final Set<String> resources;
+  private final List<ExportedService> services;
 
   /**
    * Creates a new instance corresponding to a given JAR.
    *
    * @param packages Java packages found on the JAR. Non null.
-   * @param resources Java resources found on the JAR. NOn null.
+   * @param resources Java resources found on the JAR. Non null.
+   * @param services SPI services definitions found on the JAR. Non null.
    */
-  public JarInfo(Set<String> packages, Set<String> resources) {
+  public JarInfo(Set<String> packages, Set<String> resources, List<ExportedService> services) {
     checkArgument(packages != null, "Packages cannot be null");
     checkArgument(resources != null, "Resources cannot be null");
+    checkArgument(services != null, "Services cannot be null");
     this.packages = packages;
     this.resources = resources;
+    this.services = services;
   }
 
+  /**
+   * @return Java packages found on the JAR. Non null.
+   */
   public Set<String> getPackages() {
     return packages;
   }
 
+  /**
+   * @return Java resources found on the JAR. Non null.
+   */
   public Set<String> getResources() {
     return resources;
+  }
+
+  /**
+   * @return SPI services definitions found on the JAR. Non null.
+   */
+  public List<ExportedService> getServices() {
+    return services;
   }
 }
