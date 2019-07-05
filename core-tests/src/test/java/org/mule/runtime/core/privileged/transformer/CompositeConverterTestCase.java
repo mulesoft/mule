8845f0b73b2afee087b91c7a8191ed75234a4a5e
Maria Mariotti <mmariotti@mmariotti-ltm.internal.salesforce.com>
2019-07-05 10:13:35 -0300

    MULE-17102: removed not equals hash assertion.

--- core-tests/src/test/java/org/mule/runtime/core/privileged/transformer/CompositeConverterTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/privileged/transformer/CompositeConverterTestCase.java
@@ -203,7 +203,6 @@ public class CompositeConverterTestCase extends AbstractMuleTestCase {
 
     Converter byteArrayToObjectConverter2 = new ByteArrayToObject();
     Converter byteArrayToInputStreamConverter2 = new ByteArrayToInputStream();
-    Converter byteArrayToSerializableConverter = new ByteArrayToSerializable();
 
     int hashCodeConverterA = new CompositeConverter(byteArrayToObjectConverter, byteArrayToInputStreamConverter).hashCode();
 
@@ -211,13 +210,6 @@ public class CompositeConverterTestCase extends AbstractMuleTestCase {
     int hashCodeConverterAnotherClone =
         new CompositeConverter(byteArrayToObjectConverter2, byteArrayToInputStreamConverter2).hashCode();
 
-    int hashCodeConverterReverseClone =
-        new CompositeConverter(byteArrayToInputStreamConverter, byteArrayToObjectConverter).hashCode();
-    int hashCodeConverterNotAClone =
-        new CompositeConverter(byteArrayToInputStreamConverter, byteArrayToSerializableConverter).hashCode();
-
     assertThat(Arrays.asList(hashCodeConverterAClone, hashCodeConverterAnotherClone), everyItem(equalTo(hashCodeConverterA)));
-    assertThat(Arrays.asList(hashCodeConverterReverseClone, hashCodeConverterNotAClone),
-               everyItem(not(equalTo(hashCodeConverterA))));
   }
 }
