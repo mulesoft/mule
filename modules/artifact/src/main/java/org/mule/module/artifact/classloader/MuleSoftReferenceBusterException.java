27fb9f5bd2e8952d4bda920247a23eead68e3dbb
fsgonz <fabian.gonzalez@mulesoft.com>
2019-07-17 12:55:13 -0300

    MULE-17168: Remove softreferences for app classloaders to avoid OOM on redeploy. (#8053)

--- modules/artifact/src/main/java/org/mule/module/artifact/classloader/MuleSoftReferenceBusterException.java
+++ modules/artifact/src/main/java/org/mule/module/artifact/classloader/MuleSoftReferenceBusterException.java
@@ -4,7 +4,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.runtime.module.artifact.internal.classloader;
+package org.mule.module.artifact.classloader;
 
 import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
 
