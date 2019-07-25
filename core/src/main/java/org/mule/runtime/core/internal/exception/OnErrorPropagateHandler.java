7467537188ac87a2474f70fd45c01f642cc3687c
Martin Buchwald <martin.buchwald@mulesoft.com>
2019-07-25 15:34:43 -0300

    Refactor for fix

--- core/src/main/java/org/mule/runtime/core/internal/exception/OnErrorPropagateHandler.java
+++ core/src/main/java/org/mule/runtime/core/internal/exception/OnErrorPropagateHandler.java
@@ -6,22 +6,18 @@
  */
 package org.mule.runtime.core.internal.exception;
 
-import static java.util.regex.Pattern.compile;
 import static reactor.core.publisher.Mono.just;
 
 import org.mule.runtime.api.component.location.Location;
 import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.runtime.core.api.processor.Processor;
-import org.mule.runtime.core.api.transaction.TransactionCoordination;
 import org.mule.runtime.core.privileged.exception.MessageRedeliveredException;
 import org.mule.runtime.core.privileged.exception.TemplateOnErrorHandler;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
-import java.util.regex.Pattern;
 
-import org.mule.runtime.core.privileged.transaction.TransactionAdapter;
 import org.reactivestreams.Publisher;
 
 /**
@@ -30,8 +26,6 @@ import org.reactivestreams.Publisher;
  * @since 4.0
  */
 public class OnErrorPropagateHandler extends TemplateOnErrorHandler {
-
-  private static final Pattern ERROR_HANDLER_LOCATION_PATTERN = compile(".*/.*/.*");
 
   @Override
   public boolean acceptsAll() {
@@ -47,62 +41,6 @@ public class OnErrorPropagateHandler extends TemplateOnErrorHandler {
       }
       return event;
     };
-  }
-
-  private boolean isTransactionInGlobalErrorHandler(TransactionAdapter transaction) {
-    String transactionContainerName = transaction.getComponentLocation().get().getRootContainerName();
-    return flowLocation.isPresent() && transactionContainerName.equals(flowLocation.get().getGlobalName());
-  }
-
-  private boolean isOwnedTransaction() {
-    TransactionAdapter transaction = (TransactionAdapter) TransactionCoordination.getInstance().getTransaction();
-    if (transaction == null || !transaction.getComponentLocation().isPresent()) {
-      return false;
-    }
-
-    if (inDefaultErrorHandler()) {
-      return defaultErrorHandlerOwnsTransaction(transaction);
-    } else if (isTransactionInGlobalErrorHandler((transaction))) {
-      // We are in a GlobalErrorHandler that is defined for the container (Flow or TryScope) that created the tx
-      return true;
-    } else if (flowLocation.isPresent()) {
-      // We are in a Global Error Handler, which is not the one that created the Tx
-      return false;
-    } else {
-      // We are in a simple scenario where the error handler's location ends with "/error-handler/1".
-      // We cannot use the RootContainerLocation, since in case of nested TryScopes (the outer one creating the tx)
-      // the RootContainerLocation will be the same for both, and we don't want the inner TryScope's OnErrorPropagate
-      // to rollback the tx.
-      String errorHandlerLocation = this.getLocation().getLocation();
-      if (!ERROR_HANDLER_LOCATION_PATTERN.matcher(errorHandlerLocation).find()) {
-        return sameRootContainerLocation(transaction);
-      }
-      String transactionLocation = transaction.getComponentLocation().get().getLocation();
-      errorHandlerLocation = errorHandlerLocation.substring(0, errorHandlerLocation.lastIndexOf('/'));
-      errorHandlerLocation = errorHandlerLocation.substring(0, errorHandlerLocation.lastIndexOf('/'));
-      return (sameRootContainerLocation(transaction) && errorHandlerLocation.equals(transactionLocation));
-    }
-  }
-
-  private boolean sameRootContainerLocation(TransactionAdapter transaction) {
-    String transactionContainerName = transaction.getComponentLocation().get().getRootContainerName();
-    return transactionContainerName.equals(this.getRootContainerLocation().getGlobalName());
-  }
-
-  private boolean inDefaultErrorHandler() {
-    return getLocation() == null;
-  }
-
-  private boolean defaultErrorHandlerOwnsTransaction(TransactionAdapter transaction) {
-    String transactionLocation = transaction.getComponentLocation().get().getLocation();
-    if (flowLocation.isPresent()) {
-      // We are in a default error handler for a TryScope, which must have been replicated to match the tx location
-      // to rollback it
-      return transactionLocation.equals(flowLocation.get().toString());
-    } else {
-      // We are in a default error handler of a Flow
-      return sameRootContainerLocation(transaction);
-    }
   }
 
   /**
