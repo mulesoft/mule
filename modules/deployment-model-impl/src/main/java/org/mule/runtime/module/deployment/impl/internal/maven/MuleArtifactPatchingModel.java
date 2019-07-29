69afd5c3c6abb00bec358049aa4c9f6405d83cb6
Pablo La Greca <pablo.lagreca@mulesoft.com>
2019-07-29 19:53:12 -0300

    MULE-17243 - initial support for patching mule plugins for all apps in a runtime - reading descriptor from patch file
    (cherry picked from commit 0c3e2e51111c096058958b9886a985f38fbcecdb)
    (cherry picked from commit 076ff0ceb40de490ab5719968e820325ee965f0e)
    (cherry picked from commit 55831cb913eb9dd62aacf5fdf4a4be4694646808)

--- modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/maven/MuleArtifactPatchingModel.java
+++ modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/maven/MuleArtifactPatchingModel.java
@@ -6,55 +6,65 @@
  */
 package org.mule.runtime.module.deployment.impl.internal.maven;
 
-import static java.util.Collections.emptyList;
-import static org.mule.runtime.module.deployment.impl.internal.maven.AbstractMavenClassLoaderModelLoader.MULE_ARTIFACT_PATCHES_JSON_FILE_NAME;
+import static java.lang.String.format;
+import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
+import static org.mule.runtime.core.internal.util.JarUtils.loadFileContentFrom;
 import static org.mule.runtime.module.deployment.impl.internal.maven.AbstractMavenClassLoaderModelLoader.MULE_ARTIFACT_PATCHES_JSON_LOCATION;
+import static org.mule.runtime.module.deployment.impl.internal.maven.AbstractMavenClassLoaderModelLoader.MULE_ARTIFACT_PATCH_JSON_FILE_NAME;
+import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.module.reboot.api.MuleContainerBootstrapUtils;
+import org.mule.tools.api.classloader.model.ArtifactCoordinates;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class MuleArtifactPatchingModel {
 
-  private static MuleArtifactPatchingModel loadedModel;
+  private static Map<String, MuleArtifactPatchingModel> loadedModelByJAr = new HashMap<>();
 
-  private List<MuleArtifactPatchDescriptor> muleArtifactPatchDescriptors = emptyList();
+  private ArtifactCoordinates artifactCoordinates;
+  private List<String> affectedVersions;
 
-  public List<MuleArtifactPatchDescriptor> getMuleArtifactPatchDescriptors() {
-    return muleArtifactPatchDescriptors;
-  }
-
-  public void setMuleArtifactPatchDescriptors(List<MuleArtifactPatchDescriptor> muleArtifactPatchDescriptors) {
-    this.muleArtifactPatchDescriptors = muleArtifactPatchDescriptors;
-  }
-
-  static synchronized MuleArtifactPatchingModel loadModel() {
-    if (loadedModel == null) {
-      File pluginPatchesDescriptor =
-          new File(MuleContainerBootstrapUtils.getMuleHome(), MULE_ARTIFACT_PATCHES_JSON_LOCATION);
-      loadedModel = new MuleArtifactPatchingModel();
-      if (pluginPatchesDescriptor.exists()) {
-        loadedModel = deserialize(pluginPatchesDescriptor, MULE_ARTIFACT_PATCHES_JSON_FILE_NAME);
-      }
+  static synchronized MuleArtifactPatchingModel loadModel(String jarFile) throws IOException {
+    if (!loadedModelByJAr.containsKey(jarFile)) {
+      File pluginPatchJarFile =
+          new File(new File(MuleContainerBootstrapUtils.getMuleHome(), MULE_ARTIFACT_PATCHES_JSON_LOCATION), jarFile);
+      Optional<byte[]> muleArtifactPatchContent =
+          loadFileContentFrom(pluginPatchJarFile, MULE_ARTIFACT_PATCH_JSON_FILE_NAME);
+      muleArtifactPatchContent.map(bytes -> deserialize(bytes))
+          .orElseThrow(() -> new MuleRuntimeException(createStaticMessage(format("Invalid jar file %s. It does not contains descriptor %s",
+                                                                                 jarFile, MULE_ARTIFACT_PATCH_JSON_FILE_NAME))));
     }
-    return loadedModel;
+    return loadedModelByJAr.get(jarFile);
   }
 
-  private static MuleArtifactPatchingModel deserialize(File classLoaderModelDescriptor, String fileName) {
-    try {
-      Gson gson = (new GsonBuilder()).enableComplexMapKeySerialization().setPrettyPrinting().create();
-      Reader reader = new FileReader(classLoaderModelDescriptor);
-      MuleArtifactPatchingModel classLoaderModel = gson.fromJson(reader, MuleArtifactPatchingModel.class);
-      reader.close();
-      return classLoaderModel;
-    } catch (IOException e) {
-      throw new RuntimeException("Could not create " + fileName, e);
-    }
+  private static MuleArtifactPatchingModel deserialize(byte[] muleArtifactPatchBytes) {
+    Gson gson = (new GsonBuilder()).enableComplexMapKeySerialization().setPrettyPrinting().create();
+    MuleArtifactPatchingModel classLoaderModel =
+        gson.fromJson(new String(muleArtifactPatchBytes), MuleArtifactPatchingModel.class);
+    return classLoaderModel;
+  }
+
+  public ArtifactCoordinates getArtifactCoordinates() {
+    return artifactCoordinates;
+  }
+
+  public void setArtifactCoordinates(ArtifactCoordinates artifactCoordinates) {
+    this.artifactCoordinates = artifactCoordinates;
+  }
+
+  public List<String> getAffectedVersions() {
+    return affectedVersions;
+  }
+
+  public void setAffectedVersions(List<String> affectedVersions) {
+    this.affectedVersions = affectedVersions;
   }
 }
