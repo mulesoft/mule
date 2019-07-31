d995d6c3bf9c37776f5d20b6b9cfe771bd819706
Pablo La Greca <pablo.lagreca@mulesoft.com>
2019-07-31 16:23:51 -0300

    MULE-17243 - initial support for patching mule plugins for all apps in a runtime - reading descriptor from patch file - fix
    (cherry picked from commit 3626b57e53dd0f0ea626e816e6698db303fa9c7f)

--- modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/maven/MuleArtifactPatchingModel.java
+++ modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/maven/MuleArtifactPatchingModel.java
@@ -9,7 +9,7 @@ package org.mule.runtime.module.deployment.impl.internal.maven;
 import static java.lang.String.format;
 import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
 import static org.mule.runtime.core.internal.util.JarUtils.loadFileContentFrom;
-import static org.mule.runtime.module.deployment.impl.internal.maven.AbstractMavenClassLoaderModelLoader.MULE_ARTIFACT_PATCHES_JSON_LOCATION;
+import static org.mule.runtime.module.deployment.impl.internal.maven.AbstractMavenClassLoaderModelLoader.MULE_ARTIFACT_PATCHES_LOCATION;
 import static org.mule.runtime.module.deployment.impl.internal.maven.AbstractMavenClassLoaderModelLoader.MULE_ARTIFACT_PATCH_JSON_FILE_NAME;
 import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.module.reboot.api.MuleContainerBootstrapUtils;
@@ -35,10 +35,14 @@ public class MuleArtifactPatchingModel {
   static synchronized MuleArtifactPatchingModel loadModel(String jarFile) throws IOException {
     if (!loadedModelByJAr.containsKey(jarFile)) {
       File pluginPatchJarFile =
-          new File(new File(MuleContainerBootstrapUtils.getMuleHome(), MULE_ARTIFACT_PATCHES_JSON_LOCATION), jarFile);
+          new File(new File(MuleContainerBootstrapUtils.getMuleHome(), MULE_ARTIFACT_PATCHES_LOCATION), jarFile);
       Optional<byte[]> muleArtifactPatchContent =
           loadFileContentFrom(pluginPatchJarFile, MULE_ARTIFACT_PATCH_JSON_FILE_NAME);
-      muleArtifactPatchContent.map(bytes -> deserialize(bytes))
+      muleArtifactPatchContent.map(bytes -> {
+        MuleArtifactPatchingModel artifactPatchingModel = deserialize(bytes);
+        loadedModelByJAr.put(jarFile, artifactPatchingModel);
+        return artifactPatchingModel;
+      })
           .orElseThrow(() -> new MuleRuntimeException(createStaticMessage(format("Invalid jar file %s. It does not contains descriptor %s",
                                                                                  jarFile, MULE_ARTIFACT_PATCH_JSON_FILE_NAME))));
     }
