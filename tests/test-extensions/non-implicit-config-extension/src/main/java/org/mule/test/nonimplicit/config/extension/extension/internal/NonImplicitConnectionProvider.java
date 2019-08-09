a377c58d3b227f48637f493dde5c814479a7787a
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2019-08-09 18:40:59 -0300

    MULE-17161: Allow SDK to specify that certain configs should never be used in an implicit way

---
+++ tests/test-extensions/non-implicit-config-extension/src/main/java/org/mule/test/nonimplicit/config/extension/extension/internal/NonImplicitConnectionProvider.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.nonimplicit.config.extension.extension.internal;
+
+import org.mule.runtime.api.connection.ConnectionException;
+import org.mule.runtime.api.connection.ConnectionProvider;
+import org.mule.runtime.api.connection.ConnectionValidationResult;
+import org.mule.runtime.extension.api.annotation.param.Optional;
+import org.mule.runtime.extension.api.annotation.param.Parameter;
+import org.mule.runtime.extension.api.annotation.param.ParameterGroup;
+import org.mule.test.nonimplicit.config.extension.extension.api.Counter;
+
+public class NonImplicitConnectionProvider implements ConnectionProvider<Counter> {
+
+  @ParameterGroup(name = "Internal")
+  private PrivateParameterGroup privateGroup;
+
+  @Parameter
+  @Optional(defaultValue = "#[vars.number]")
+  private int number;
+
+  @Override
+  public Counter connect() throws ConnectionException {
+    if (privateGroup == null) {
+      throw new ConnectionException("Internal parameter group was not instantiate with nullsafe");
+    }
+    return new Counter(number);
+  }
+
+  @Override
+  public void disconnect(Counter counter) {
+
+  }
+
+  @Override
+  public ConnectionValidationResult validate(Counter counter) {
+    return ConnectionValidationResult.success();
+  }
+}
