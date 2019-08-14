ea7d3a422a73ff36e79cefcf158f25dfe8ad97ed
Rodro <rodrigo.merino@mulesoft.com>
2019-08-14 15:00:29 -0300

    MULE-17334: Add missing exported/imported types in test extensions

--- tests/test-extensions/parameter-resolver-extension/src/main/java/org/mule/test/parameter/resolver/extension/extension/ParameterResolverExtension.java
+++ tests/test-extensions/parameter-resolver-extension/src/main/java/org/mule/test/parameter/resolver/extension/extension/ParameterResolverExtension.java
@@ -9,9 +9,14 @@ package org.mule.test.parameter.resolver.extension.extension;
 
 import org.mule.runtime.extension.api.annotation.Configurations;
 import org.mule.runtime.extension.api.annotation.Extension;
+import org.mule.runtime.extension.api.annotation.Import;
+import org.mule.test.heisenberg.extension.model.DifferedKnockableDoor;
+import org.mule.test.heisenberg.extension.model.KnockeableDoor;
 
 @Configurations({ParameterResolverConfig.class, NestedWrapperTypesConfig.class})
 @Extension(name = "ParameterResolver")
+@Import(type = KnockeableDoor.class)
+@Import(type = DifferedKnockableDoor.class)
 public class ParameterResolverExtension {
 
 
