b22d65148588821f221004999c6d047b39403a88
Rodrigo Merino <elrodro83@users.noreply.github.com>
2019-08-16 13:45:51 -0300

    MULE-17334: Add missing exported/imported types in test extensions (#8121)

--- modules/extensions-spring-support/src/test/java/org/mule/runtime/module/extension/internal/capability/xml/DefaultExtensionSchemaGeneratorTestCase.java
+++ modules/extensions-spring-support/src/test/java/org/mule/runtime/module/extension/internal/capability/xml/DefaultExtensionSchemaGeneratorTestCase.java
@@ -7,6 +7,7 @@
 package org.mule.runtime.module.extension.internal.capability.xml;
 
 import static com.google.common.collect.ImmutableSet.copyOf;
+import static java.lang.Boolean.getBoolean;
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
 import static java.util.Optional.ofNullable;
@@ -14,6 +15,7 @@ import static java.util.stream.Collectors.toList;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mule.runtime.api.dsl.DslResolvingContext.getDefault;
+import static org.mule.runtime.api.util.MuleSystemProperties.SYSTEM_PROPERTY_PREFIX;
 import static org.mule.runtime.core.api.config.MuleManifest.getProductVersion;
 import static org.mule.runtime.core.api.util.FileUtils.stringToFile;
 import static org.mule.runtime.core.api.util.IOUtils.getResourceAsString;
@@ -23,6 +25,7 @@ import static org.mule.runtime.module.extension.api.loader.AbstractJavaExtension
 import static org.mule.runtime.module.extension.internal.capability.xml.DefaultExtensionSchemaGeneratorTestCase.SchemaGeneratorTestUnit.newTestUnit;
 import static org.mule.runtime.module.extension.internal.resources.BaseExtensionResourcesGeneratorAnnotationProcessor.COMPILATION_MODE;
 import static org.mule.test.module.extension.internal.util.ExtensionsTestUtils.compareXML;
+
 import org.mule.extension.test.extension.reconnection.ReconnectionExtension;
 import org.mule.runtime.api.dsl.DslResolvingContext;
 import org.mule.runtime.api.meta.model.ExtensionModel;
@@ -80,6 +83,9 @@ import org.junit.runners.Parameterized;
 @RunWith(Parameterized.class)
 public class DefaultExtensionSchemaGeneratorTestCase extends AbstractMuleTestCase {
 
+  private static final boolean UPDATE_EXPECTED_FILES_ON_ERROR =
+      getBoolean(SYSTEM_PROPERTY_PREFIX + "extensionSchemas.updateExpectedFilesOnError");
+
   static final Map<String, ExtensionModel> extensionModels = new HashMap<>();
 
   private static ExtensionModelLoader javaLoader = new DefaultJavaExtensionModelLoader();
@@ -91,7 +97,7 @@ public class DefaultExtensionSchemaGeneratorTestCase extends AbstractMuleTestCas
   @Parameterized.Parameter(1)
   public String expectedXSD;
 
-  private ExtensionSchemaGenerator generator = new DefaultExtensionSchemaGenerator();
+  private final ExtensionSchemaGenerator generator = new DefaultExtensionSchemaGenerator();
   private String expectedSchema;
 
 
@@ -152,7 +158,7 @@ public class DefaultExtensionSchemaGeneratorTestCase extends AbstractMuleTestCas
    * @return whether or not the "expected" test files should be updated when comparison fails
    */
   private boolean shouldUpdateExpectedFilesOnError() {
-    return false;
+    return UPDATE_EXPECTED_FILES_ON_ERROR;
   }
 
   @Before
--- tests/test-extensions/typed-value-extension/src/main/java/org/mule/test/typed/value/extension/extension/TypedValueExtension.java
+++ tests/test-extensions/typed-value-extension/src/main/java/org/mule/test/typed/value/extension/extension/TypedValueExtension.java
@@ -9,15 +9,19 @@ package org.mule.test.typed.value.extension.extension;
 
 import org.mule.runtime.api.metadata.TypedValue;
 import org.mule.runtime.extension.api.annotation.Extension;
+import org.mule.runtime.extension.api.annotation.Import;
 import org.mule.runtime.extension.api.annotation.Operations;
 import org.mule.runtime.extension.api.annotation.Sources;
 import org.mule.runtime.extension.api.annotation.param.Optional;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
 import org.mule.test.heisenberg.extension.model.DifferedKnockableDoor;
+import org.mule.test.heisenberg.extension.model.KnockeableDoor;
 
 @Operations(TypedValueParameterOperations.class)
 @Extension(name = "TypedValue")
 @Sources(TypedValueSource.class)
+@Import(type = KnockeableDoor.class)
+@Import(type = DifferedKnockableDoor.class)
 public class TypedValueExtension {
 
   @Parameter
