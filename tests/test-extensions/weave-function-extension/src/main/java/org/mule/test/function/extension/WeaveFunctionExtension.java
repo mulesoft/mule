03bdb881c3a38aa3ed2edc43118591233c782040
Rodrigo Merino <elrodro83@users.noreply.github.com>
2019-08-22 12:08:18 -0300

    MULE-17334: Add missing exported/imported types in test extensions (#8138)
    * Missing one

--- tests/test-extensions/weave-function-extension/src/main/java/org/mule/test/function/extension/WeaveFunctionExtension.java
+++ tests/test-extensions/weave-function-extension/src/main/java/org/mule/test/function/extension/WeaveFunctionExtension.java
@@ -9,8 +9,11 @@ package org.mule.test.function.extension;
 
 import org.mule.runtime.extension.api.annotation.ExpressionFunctions;
 import org.mule.runtime.extension.api.annotation.Extension;
+import org.mule.runtime.extension.api.annotation.Import;
+import org.mule.runtime.extension.api.annotation.ImportedTypes;
 import org.mule.runtime.extension.api.annotation.Operations;
 import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
+import org.mule.test.heisenberg.extension.model.KnockeableDoor;
 
 
 
@@ -18,6 +21,7 @@ import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
 @ExpressionFunctions(GlobalWeaveFunction.class)
 @Operations(WeaveTestUtilsOperations.class)
 @Xml(prefix = "fn")
+@ImportedTypes(@Import(type = KnockeableDoor.class))
 public class WeaveFunctionExtension {
 
 }
