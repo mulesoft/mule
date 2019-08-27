158e54385620655c921b78f13eef7e6c73d4003a
Ezequiel Werner <eze210@gmail.com>
2019-08-27 17:00:47 -0300

    Fixing tests

--- modules/deployment/src/test/java/org/mule/runtime/module/deployment/internal/ApplicationDependingOnDomainDeploymentTestCase.java
+++ modules/deployment/src/test/java/org/mule/runtime/module/deployment/internal/ApplicationDependingOnDomainDeploymentTestCase.java
@@ -6,36 +6,22 @@
  */
 package org.mule.runtime.module.deployment.internal;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.reset;
-import org.mule.runtime.deployment.model.api.domain.Domain;
 import org.mule.runtime.module.deployment.impl.internal.builder.ApplicationFileBuilder;
 import org.mule.runtime.module.deployment.impl.internal.builder.DomainFileBuilder;
-import org.mule.tck.util.CompilerUtils;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Properties;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ApplicationDependingOnDomainDeploymentTestCase extends AbstractDeploymentTestCase {
-
-  private static File pluginForbiddenJavaEchoTestClassFile;
-  private static File pluginForbiddenMuleContainerEchoTestClassFile;
-  private static File pluginForbiddenMuleThirdPartyEchoTestClassFile;
-
 
   private final DomainFileBuilder emptyDomain100FileBuilder =
       new DomainFileBuilder("empty-domain").definedBy("empty-domain-config.xml").withVersion("1.0.0");
   private final DomainFileBuilder emptyDomain101FileBuilder =
       new DomainFileBuilder("empty-domain").definedBy("empty-domain-config.xml").withVersion("1.0.1");
-
 
   private final ApplicationFileBuilder appDependingOnDomain100FileBuilder = new ApplicationFileBuilder("dummy-domain100-app")
       .definedBy("empty-config.xml").dependingOn(emptyDomain100FileBuilder);
@@ -45,37 +31,30 @@ public class ApplicationDependingOnDomainDeploymentTestCase extends AbstractDepl
   private final ApplicationFileBuilder appReferencingDomain101FileBuilder = new ApplicationFileBuilder("dummy-domain101-app-ref")
       .definedBy("empty-config.xml").dependingOn(emptyDomain100FileBuilder)
       .deployedWith("domain", "empty-domain-1.0.1-mule-domain");
-
-  @BeforeClass
-  public static void compileTestClasses() throws Exception {
-    pluginForbiddenJavaEchoTestClassFile =
-        new CompilerUtils.SingleClassCompiler().dependingOn(barUtilsForbiddenJavaJarFile)
-            .compile(getResourceFile("/org/foo/echo/PluginForbiddenJavaEcho.java"));
-    pluginForbiddenMuleContainerEchoTestClassFile =
-        new CompilerUtils.SingleClassCompiler().dependingOn(barUtilsForbiddenMuleContainerJarFile)
-            .compile(getResourceFile("/org/foo/echo/PluginForbiddenMuleContainerEcho.java"));
-    pluginForbiddenMuleThirdPartyEchoTestClassFile =
-        new CompilerUtils.SingleClassCompiler().dependingOn(barUtilsForbiddenMuleThirdPartyJarFile)
-            .compile(getResourceFile("/org/foo/echo/PluginForbiddenMuleThirdPartyEcho.java"));
-  }
-
-  @Before
-  public void resetListeners() {
-    reset(applicationDeploymentListener);
-    reset(domainDeploymentListener);
-  }
+  private final ApplicationFileBuilder appReferencingDomain100FileBuilder = new ApplicationFileBuilder("dummy-domain100-app-ref")
+      .definedBy("empty-config.xml").dependingOn(emptyDomain100FileBuilder)
+      .deployedWith("domain", "empty-domain-1.0.0-mule-domain");
 
   public ApplicationDependingOnDomainDeploymentTestCase(boolean parallelDeployment) {
     super(parallelDeployment);
   }
 
   @Test
-  public void appDeploymentPointingToDomain() throws Exception {
+  public void domainNotFound() throws Exception {
     startDeployment();
 
-    // Fails because domain is not found
+    // By GAV
     addExplodedAppFromBuilder(appDependingOnDomain100FileBuilder, appDependingOnDomain100FileBuilder.getId());
     assertDeploymentFailure(applicationDeploymentListener, appDependingOnDomain100FileBuilder.getId());
+
+    // By name
+    addExplodedAppFromBuilder(appReferencingDomain101FileBuilder, appReferencingDomain101FileBuilder.getId());
+    assertDeploymentFailure(applicationDeploymentListener, appReferencingDomain101FileBuilder.getId());
+  }
+
+  @Test
+  public void referenceToDomainByGAV() throws Exception {
+    startDeployment();
 
     // Add domain 1.0.0
     addExplodedDomainFromBuilder(emptyDomain100FileBuilder, emptyDomain100FileBuilder.getId());
@@ -87,23 +66,42 @@ public class ApplicationDependingOnDomainDeploymentTestCase extends AbstractDepl
   }
 
   @Test
-  public void appDeploymentPointingToCompatibleDomain() throws Exception {
+  public void referenceToCompatibleDomainByGAV() throws Exception {
     startDeployment();
-
-    // Fails because domain is not found
-    addExplodedAppFromBuilder(appDependingOnDomain100FileBuilder, appDependingOnDomain100FileBuilder.getId());
-    assertDeploymentFailure(applicationDeploymentListener, appDependingOnDomain100FileBuilder.getId());
-    reset(applicationDeploymentListener);
 
     // Add domain with version upgraded (1.0.1)
     addExplodedDomainFromBuilder(emptyDomain101FileBuilder, emptyDomain101FileBuilder.getId());
     assertDeploymentSuccess(domainDeploymentListener, emptyDomain101FileBuilder.getId());
-    Domain domainFound = deploymentService.findDomain(emptyDomain101FileBuilder.getId());
-    assertThat(domainFound, is(notNullValue()));
 
     // Add app pointing to 1.0.0 or compatible
     addExplodedAppFromBuilder(appDependingOnDomain100FileBuilder, appDependingOnDomain100FileBuilder.getId());
     assertDeploymentSuccess(applicationDeploymentListener, appDependingOnDomain100FileBuilder.getId());
+  }
+
+  @Test
+  public void referenceDomainByName() throws Exception {
+    startDeployment();
+
+    addExplodedDomainFromBuilder(emptyDomain101FileBuilder, emptyDomain101FileBuilder.getId());
+    assertDeploymentSuccess(domainDeploymentListener, emptyDomain101FileBuilder.getId());
+
+    reset(applicationDeploymentListener);
+
+    addExplodedAppFromBuilder(appReferencingDomain101FileBuilder, appReferencingDomain101FileBuilder.getId());
+    assertDeploymentSuccess(applicationDeploymentListener, appReferencingDomain101FileBuilder.getId());
+  }
+
+  @Test
+  public void failsWhenSpecifiedNameIsNotFoundEvenWhenCompatibleIsPresent() throws Exception {
+    startDeployment();
+
+    // Add domain with version upgraded (1.0.1)
+    addExplodedDomainFromBuilder(emptyDomain101FileBuilder, emptyDomain101FileBuilder.getId());
+    assertDeploymentSuccess(domainDeploymentListener, emptyDomain101FileBuilder.getId());
+
+    // Add app pointing to 1.0.0 by name
+    addExplodedAppFromBuilder(appReferencingDomain100FileBuilder, appReferencingDomain100FileBuilder.getId());
+    assertDeploymentFailure(applicationDeploymentListener, appReferencingDomain100FileBuilder.getId());
   }
 
   @Test
@@ -126,22 +124,6 @@ public class ApplicationDependingOnDomainDeploymentTestCase extends AbstractDepl
     // This application depends on domain 1.0.1, which is not considered compatible with 1.0.1, so this deployment is ok
     addExplodedAppFromBuilder(appDependingOnDomain101FileBuilder, appDependingOnDomain101FileBuilder.getId());
     assertDeploymentSuccess(applicationDeploymentListener, appDependingOnDomain101FileBuilder.getId());
-  }
-
-  @Test
-  public void referenceDomainByName() throws Exception {
-    startDeployment();
-
-    addExplodedAppFromBuilder(appReferencingDomain101FileBuilder, appReferencingDomain101FileBuilder.getId());
-    assertDeploymentFailure(applicationDeploymentListener, appReferencingDomain101FileBuilder.getId());
-
-    addExplodedDomainFromBuilder(emptyDomain101FileBuilder, emptyDomain101FileBuilder.getId());
-    assertDeploymentSuccess(domainDeploymentListener, emptyDomain101FileBuilder.getId());
-
-    reset(applicationDeploymentListener);
-
-    addExplodedAppFromBuilder(appReferencingDomain101FileBuilder, appReferencingDomain101FileBuilder.getId());
-    assertDeploymentSuccess(applicationDeploymentListener, appReferencingDomain101FileBuilder.getId());
   }
 
   @Override
