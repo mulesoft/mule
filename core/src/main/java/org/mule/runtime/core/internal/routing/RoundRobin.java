c91f6420b387fd99289c80f13866efce8de2df6c
Rodro <rodrigo.merino@mulesoft.com>
2019-09-05 16:07:06 -0300

    review

--- core/src/main/java/org/mule/runtime/core/internal/routing/RoundRobin.java
+++ core/src/main/java/org/mule/runtime/core/internal/routing/RoundRobin.java
@@ -6,12 +6,8 @@
  */
 package org.mule.runtime.core.internal.routing;
 
-import static java.util.stream.Collectors.toList;
 import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.initialiseIfNeeded;
-import static org.mule.runtime.core.api.rx.Exceptions.checkedConsumer;
-import static org.mule.runtime.core.internal.util.rx.RxUtils.subscribeFluxOnPublisherSubscription;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.processToApply;
-import static reactor.core.publisher.Flux.from;
 
 import org.mule.runtime.api.component.AbstractComponent;
 import org.mule.runtime.api.exception.MuleException;
@@ -19,7 +15,6 @@ import org.mule.runtime.api.lifecycle.InitialisationException;
 import org.mule.runtime.api.lifecycle.Lifecycle;
 import org.mule.runtime.core.api.MuleContext;
 import org.mule.runtime.core.api.context.MuleContextAware;
-import org.mule.runtime.core.api.el.ExpressionManagerSession;
 import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.runtime.core.api.processor.Processor;
 import org.mule.runtime.core.privileged.processor.Router;
@@ -28,7 +23,6 @@ import org.mule.runtime.core.privileged.routing.RoutingException;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -107,59 +101,6 @@ public class RoundRobin extends AbstractComponent implements Router, Lifecycle, 
   }
 
   /**
-   * Router that generates executable routes for each one configured and handles the logic of deciding which one will be executed
-   * when an event arrives. It also provides access to the routes publishers so they can be merged together.
-   */
-  private class SinkRouter {
-
-    private final Flux<CoreEvent> router;
-    private final List<ExecutableRoute> routes;
-
-    SinkRouter(Publisher<CoreEvent> publisher, List<ProcessorRoute> routes) {
-      this.routes = routes.stream().map(ProcessorRoute::toExecutableRoute).collect(toList());
-      router = from(publisher)
-          .doOnNext(checkedConsumer(this::route))
-          .doOnComplete(() -> this.routes.stream()
-              .forEach(executableRoute -> executableRoute.complete()));
-    }
-
-    /**
-     * Decides which route should execute for an incoming event and executes it, purposely separating those actions so that a
-     * single {@link ExpressionManagerSession} can be used for all routes decision process.
-     *
-     * @param event the incoming event
-     * @throws RoutingException
-     */
-    private void route(CoreEvent event) throws RoutingException {
-      int modulo = getAndIncrementModuloN(routes.size());
-      if (modulo < 0) {
-        throw new CouldNotRouteOutboundMessageException(RoundRobin.this);
-      }
-
-      routes.get(modulo).execute(event);
-    }
-
-    /**
-     * @return the publishers of all routes so they can be merged and subscribed, including a phantom one to guarantee the
-     *         subscription of the router occurs after all routes and with the general context.
-     */
-    private List<Flux<CoreEvent>> collectPublishers() {
-      List<Flux<CoreEvent>> routes = new ArrayList<>();
-      for (Iterator routesIterator = this.routes.iterator(); routesIterator.hasNext();) {
-        ExecutableRoute nextRoute = (ExecutableRoute) routesIterator.next();
-        if (routesIterator.hasNext()) {
-          routes.add(nextRoute.getPublisher());
-        } else {
-          // If it's the last route, this will be trigger for the whole inbound chain of the router to be subscribed.
-          // Since there's always at least one route, the default one, one route will always be decorated.
-          routes.add(subscribeFluxOnPublisherSubscription(nextRoute.getPublisher(), router));
-        }
-      }
-      return routes;
-    }
-  }
-
-  /**
    * Get the index of the processor to use
    */
   private int getAndIncrementModuloN(int modulus) {
@@ -168,5 +109,22 @@ public class RoundRobin extends AbstractComponent implements Router, Lifecycle, 
     }
 
     return index.getAndUpdate(lastIndex -> (lastIndex + 1) % modulus);
+  }
+
+  private class SinkRouter extends AbstractSinkRouter {
+
+    SinkRouter(Publisher<CoreEvent> publisher, List<ProcessorRoute> routes) {
+      super(publisher, routes);
+    }
+
+    @Override
+    protected void route(CoreEvent event) throws RoutingException {
+      int modulo = getAndIncrementModuloN(routes.size());
+      if (modulo < 0) {
+        throw new CouldNotRouteOutboundMessageException(RoundRobin.this);
+      }
+
+      getRoutes().get(modulo).execute(event);
+    }
   }
 }
