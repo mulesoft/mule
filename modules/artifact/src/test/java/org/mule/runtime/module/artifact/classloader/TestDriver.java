4609ff17f7cd493ad5b473bb483ecdd7cd61f02c
Guillermo Fernandes <gsfernandes@users.noreply.github.com>
2019-09-13 11:54:37 -0300

    MULE-17447: ResourceReleaser should be loaded and invoked only if any… (#8216)
    ... of the classes to be released has been loaded
    
    (cherry picked from commit 8fd73b9cb61271220034c0c35788b6450a3505cc)
    (cherry picked from commit 90a69eee6e26d6ba0c405701a858a47db38d3dea)

---
+++ modules/artifact/src/test/java/org/mule/runtime/module/artifact/classloader/TestDriver.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.module.artifact.classloader;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class TestDriver implements Driver {
+
+  @Override
+  public Connection connect(String url, Properties info) throws SQLException {
+    return null;
+  }
+
+  @Override
+  public boolean acceptsURL(String url) throws SQLException {
+    return false;
+  }
+
+  @Override
+  public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+    return new DriverPropertyInfo[0];
+  }
+
+  @Override
+  public int getMajorVersion() {
+    return 0;
+  }
+
+  @Override
+  public int getMinorVersion() {
+    return 0;
+  }
+
+  @Override
+  public boolean jdbcCompliant() {
+    return false;
+  }
+
+  @Override
+  public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+    return null;
+  }
+}
