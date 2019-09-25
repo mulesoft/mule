8560c784314478eaf64906be41faa4b1b4b205c6
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-09-25 15:51:56 -0300

    Missing license and javadoc

--- core/src/main/java/org/mule/runtime/core/internal/exception/RecursiveSubFlowException.java
+++ core/src/main/java/org/mule/runtime/core/internal/exception/RecursiveSubFlowException.java
@@ -1,9 +1,20 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.runtime.core.internal.exception;
 
 import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
 import org.mule.runtime.api.lifecycle.LifecycleException;
 import org.mule.runtime.core.api.processor.Processor;
 
+/**
+ * Exception thrown when a recursion exists between static flow-refs.
+ *
+ * @since 4.3.0
+ */
 public class RecursiveSubFlowException extends LifecycleException {
 
   public RecursiveSubFlowException(String offendingFlowName, Processor flowRefProcessor) {
