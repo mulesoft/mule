8c9eb2defb5b17d843f40282ef2cae0bf4c5a11e
Juli <luzzijulieta@gmail.com>
2019-10-01 16:22:40 -0300

    MULE-17386: CommonSourcePolicy callback invoked twice when event is failed

--- core/src/main/java/org/mule/runtime/core/internal/execution/FlowProcessMediator.java
+++ core/src/main/java/org/mule/runtime/core/internal/execution/FlowProcessMediator.java
@@ -191,18 +191,18 @@ public class FlowProcessMediator implements Initialisable {
       ctx.sourcePolicy.process(ctx.event, ctx.template,
                                new CompletableCallback<Either<SourcePolicyFailureResult, SourcePolicySuccessResult>>() {
 
-                                   @Override
-                                   public void complete(Either<SourcePolicyFailureResult, SourcePolicySuccessResult> value) {
-                                       ctx.result = value;
-                                       dispatchResponse(ctx);
-                                   }
+                                 @Override
+                                 public void complete(Either<SourcePolicyFailureResult, SourcePolicySuccessResult> value) {
+                                   ctx.result = value;
+                                   dispatchResponse(ctx);
+                                 }
 
-                                   @Override
-                                   public void error(Throwable e) {
-                                       ctx.result = left(new SourcePolicyFailureResult(new MessagingException(ctx.event, e),
-                                               () -> emptyMap()));
-                                       dispatchResponse(ctx);
-                                   }
+                                 @Override
+                                 public void error(Throwable e) {
+                                   ctx.result = left(new SourcePolicyFailureResult(new MessagingException(ctx.event, e),
+                                                                                   () -> emptyMap()));
+                                   dispatchResponse(ctx);
+                                 }
                                });
     } catch (Exception e) {
       e = (Exception) Exceptions.unwrap(e);
