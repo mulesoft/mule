63439ad8d0e5838ca02842186102050f1d3d8a5f
Julieta <luzzijulieta@gmail.com>
2019-10-07 10:57:16 -0300

    MULE-17386: CommonSourcePolicy callback invoked twice when event is failed (#8272)
    MULE-17386: CommonSourcePolicy callback invoked twice when event is failed

--- core/src/main/java/org/mule/runtime/core/privileged/processor/MessageProcessors.java
+++ core/src/main/java/org/mule/runtime/core/privileged/processor/MessageProcessors.java
@@ -35,6 +35,7 @@ import org.mule.runtime.core.api.functional.Either;
 import org.mule.runtime.core.api.processor.Processor;
 import org.mule.runtime.core.api.processor.ReactiveProcessor;
 import org.mule.runtime.core.api.processor.strategy.ProcessingStrategy;
+import org.mule.runtime.core.internal.construct.FlowBackPressureException;
 import org.mule.runtime.core.internal.exception.MessagingException;
 import org.mule.runtime.core.internal.rx.FluxSinkRecorder;
 import org.mule.runtime.core.internal.rx.FluxSinkRecorderToReactorSinkAdapter;
@@ -473,7 +474,7 @@ public class MessageProcessors {
                     .map(event -> right(MessagingException.class, event))
 
                     // This Either here is used to propagate errors. If the error is sent directly through the merged with Flux,
-                    // it will be cancelled, ignoring the onErrorcontinue of the parent Flux.
+                    // it will be cancelled, ignoring the onErrorContinue of the parent Flux.
                     .doOnComplete(() -> errorSwitchSinkSinkRef.complete())
                     .mergeWith(create(errorSwitchSinkSinkRef))
 
@@ -491,7 +492,9 @@ public class MessageProcessors {
       try {
         if (throwable != null) {
           final MessagingException error = (MessagingException) throwable;
-          errorSwitchSinkSinkRef.next(left(new MessagingException(toParentContext(error.getEvent()), error)));
+          if (!(throwable.getCause() instanceof FlowBackPressureException)) {
+            errorSwitchSinkSinkRef.next(left(new MessagingException(toParentContext(error.getEvent()), error)));
+          }
         } else if (response == null && completeParentIfEmpty) {
           getParentContext(eventChildCtx).success();
           errorSwitchSinkSinkRef.next();
