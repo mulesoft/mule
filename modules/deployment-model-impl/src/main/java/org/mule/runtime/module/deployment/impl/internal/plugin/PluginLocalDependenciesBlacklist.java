5e596692d5cf425f76c728dab7f321ae4320a37c
Ezequiel Werner <eze210@gmail.com>
2019-10-09 16:08:53 -0300

    Adding ibm ctg connector to blacklist

--- modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/plugin/PluginLocalDependenciesBlacklist.java
+++ modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/plugin/PluginLocalDependenciesBlacklist.java
@@ -16,65 +16,66 @@ import java.util.List;
 
 class PluginLocalDependenciesBlacklist {
 
-    private static List<BundleDescriptor> pluginsBlacklist = singletonList(new BundleDescriptor.Builder().setGroupId("test").setArtifactId("test").setVersion("test").build());
+  private static List<BundleDescriptor> pluginsBlacklist = singletonList(new BundleDescriptor.Builder()
+      .setGroupId("com.mulesoft.connectors").setArtifactId("mule-ibm-ctg-connector").setVersion("2.3.1").build());
 
-    static boolean isBlacklisted(ArtifactPluginDescriptor pluginDescriptor) {
-        BundleDescriptor pluginBundleDescriptor = pluginDescriptor.getBundleDescriptor();
-        for (BundleDescriptor blacklistedPluginDescriptor : pluginsBlacklist) {
-            if (doDescriptorsMatch(blacklistedPluginDescriptor, pluginBundleDescriptor)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean doDescriptorsMatch(BundleDescriptor blacklistedDescriptor, BundleDescriptor pluginBundleDescriptor) {
-        if (!doGroupsMatch(blacklistedDescriptor, pluginBundleDescriptor)) {
-            return false;
-        }
-
-        if (!doArtifactIdsMatch(blacklistedDescriptor, pluginBundleDescriptor)) {
-            return false;
-        }
-
-        return isBlacklistedVersionGreaterOrEqual(blacklistedDescriptor.getVersion(), pluginBundleDescriptor.getVersion());
-    }
-
-    private static boolean isBlacklistedVersionGreaterOrEqual(String blacklistedVersion, String pluginVersion) {
-        Semver blacklistedSemver = new Semver(blacklistedVersion);
-        Semver pluginSemver = new Semver(pluginVersion);
-
-        // Check majors
-        if (blacklistedSemver.getMajor() < pluginSemver.getMajor()) {
-            return false;
-        }
-
-        if (blacklistedSemver.getMajor() > pluginSemver.getMajor()) {
-            return true;
-        }
-
-        // Majors are equals, check minors
-        if (blacklistedSemver.getMinor() < pluginSemver.getMinor()) {
-            return false;
-        }
-
-        if (blacklistedSemver.getMinor() > pluginSemver.getMinor()) {
-            return true;
-        }
-
-        // Majors and minors are equals, check patch versions
-        if (blacklistedSemver.getPatch() < pluginSemver.getPatch()) {
-            return false;
-        }
-
+  static boolean isBlacklisted(ArtifactPluginDescriptor pluginDescriptor) {
+    BundleDescriptor pluginBundleDescriptor = pluginDescriptor.getBundleDescriptor();
+    for (BundleDescriptor blacklistedPluginDescriptor : pluginsBlacklist) {
+      if (doDescriptorsMatch(blacklistedPluginDescriptor, pluginBundleDescriptor)) {
         return true;
+      }
+    }
+    return false;
+  }
+
+  private static boolean doDescriptorsMatch(BundleDescriptor blacklistedDescriptor, BundleDescriptor pluginBundleDescriptor) {
+    if (!doGroupsMatch(blacklistedDescriptor, pluginBundleDescriptor)) {
+      return false;
     }
 
-    private static boolean doGroupsMatch(BundleDescriptor first, BundleDescriptor second) {
-        return first.getGroupId().equals(second.getGroupId());
+    if (!doArtifactIdsMatch(blacklistedDescriptor, pluginBundleDescriptor)) {
+      return false;
     }
 
-    private static boolean doArtifactIdsMatch(BundleDescriptor first, BundleDescriptor second) {
-        return first.getArtifactId().equals(second.getArtifactId());
+    return isBlacklistedVersionGreaterOrEqual(blacklistedDescriptor.getVersion(), pluginBundleDescriptor.getVersion());
+  }
+
+  private static boolean isBlacklistedVersionGreaterOrEqual(String blacklistedVersion, String pluginVersion) {
+    Semver blacklistedSemver = new Semver(blacklistedVersion);
+    Semver pluginSemver = new Semver(pluginVersion);
+
+    // Check majors
+    if (blacklistedSemver.getMajor() < pluginSemver.getMajor()) {
+      return false;
     }
+
+    if (blacklistedSemver.getMajor() > pluginSemver.getMajor()) {
+      return true;
+    }
+
+    // Majors are equals, check minors
+    if (blacklistedSemver.getMinor() < pluginSemver.getMinor()) {
+      return false;
+    }
+
+    if (blacklistedSemver.getMinor() > pluginSemver.getMinor()) {
+      return true;
+    }
+
+    // Majors and minors are equals, check patch versions
+    if (blacklistedSemver.getPatch() < pluginSemver.getPatch()) {
+      return false;
+    }
+
+    return true;
+  }
+
+  private static boolean doGroupsMatch(BundleDescriptor first, BundleDescriptor second) {
+    return first.getGroupId().equals(second.getGroupId());
+  }
+
+  private static boolean doArtifactIdsMatch(BundleDescriptor first, BundleDescriptor second) {
+    return first.getArtifactId().equals(second.getArtifactId());
+  }
 }
