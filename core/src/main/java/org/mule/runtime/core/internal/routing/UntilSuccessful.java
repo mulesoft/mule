094ba22c8c86eaa24a8b3ed7d31ae3f0c41805d3
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-10-10 10:45:37 -0300

    Rewrite with tests working

--- core/src/main/java/org/mule/runtime/core/internal/routing/UntilSuccessful.java
+++ core/src/main/java/org/mule/runtime/core/internal/routing/UntilSuccessful.java
@@ -10,22 +10,26 @@ import static com.github.benmanes.caffeine.cache.Caffeine.newBuilder;
 import static java.util.Collections.singletonList;
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
-import static java.util.Optional.ofNullable;
 import static org.mule.runtime.api.el.BindingContextUtils.NULL_BINDING_CONTEXT;
 import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
+import static org.mule.runtime.core.api.retry.policy.SimpleRetryPolicyTemplate.RETRY_COUNT_FOREVER;
+import static org.mule.runtime.core.api.transaction.TransactionCoordination.isTransactionActive;
 import static org.mule.runtime.core.api.util.ExceptionUtils.getMessagingExceptionCause;
+import static org.mule.runtime.core.privileged.processor.MessageProcessors.applyWithChildContext;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.buildNewChainWithListOfProcessors;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.getProcessingStrategy;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.processToApply;
-import static org.mule.runtime.core.privileged.processor.MessageProcessors.processWithChildContextDontComplete;
+import static org.slf4j.LoggerFactory.getLogger;
+import static reactor.core.Exceptions.propagate;
+import static reactor.core.Exceptions.unwrap;
 import static reactor.core.publisher.Flux.from;
-
-import com.github.benmanes.caffeine.cache.LoadingCache;
+import static reactor.core.scheduler.Schedulers.fromExecutorService;
 import org.mule.runtime.api.exception.MuleException;
 import org.mule.runtime.api.lifecycle.InitialisationException;
 import org.mule.runtime.api.metadata.DataType;
 import org.mule.runtime.api.scheduler.Scheduler;
 import org.mule.runtime.api.scheduler.SchedulerService;
+import org.mule.runtime.api.util.LazyValue;
 import org.mule.runtime.api.util.Pair;
 import org.mule.runtime.core.api.el.ExpressionManagerSession;
 import org.mule.runtime.core.api.el.ExtendedExpressionManager;
@@ -37,24 +41,35 @@ import org.mule.runtime.core.api.retry.policy.RetryPolicyExhaustedException;
 import org.mule.runtime.core.api.retry.policy.RetryPolicyTemplate;
 import org.mule.runtime.core.api.retry.policy.SimpleRetryPolicyTemplate;
 import org.mule.runtime.core.internal.exception.MessagingException;
+import org.mule.runtime.core.internal.util.rx.ConditionalExecutorServiceDecorator;
 import org.mule.runtime.core.privileged.processor.Scope;
 import org.mule.runtime.core.privileged.processor.chain.MessageProcessorChain;
 
+import com.github.benmanes.caffeine.cache.LoadingCache;
+
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 import javax.inject.Inject;
 
 import org.reactivestreams.Publisher;
-import reactor.core.publisher.Mono;
+import org.slf4j.Logger;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.retry.RetryExhaustedException;
 
 /**
  * UntilSuccessful attempts to route a message to the message processor it contains. Routing is considered successful if no
  * exception has been raised and, optionally, if the response matches an expression.
  */
 public class UntilSuccessful extends AbstractMuleObjectOwner implements Scope {
+
+  private final Logger LOGGER = getLogger(UntilSuccessful.class);
 
   private static final String UNTIL_SUCCESSFUL_MSG_PREFIX =
       "'until-successful' retries exhausted. Last exception message was: %s";
@@ -76,6 +91,8 @@ public class UntilSuccessful extends AbstractMuleObjectOwner implements Scope {
       newBuilder().build(p -> new SimpleRetryPolicyTemplate(p.getFirst(), p.getSecond()));
   private Scheduler timer;
   private List<Processor> processors;
+  private int maxRetriesAsInteger;
+  private Duration timeBetweenRetries;
 
   @Override
   public void initialise() throws InitialisationException {
@@ -97,8 +114,10 @@ public class UntilSuccessful extends AbstractMuleObjectOwner implements Scope {
         policyTemplate = of(new NoRetryPolicyTemplate());
       } else if (!expressionManager.isExpression(this.millisBetweenRetries)) {
         long millisBetweenRetries = Long.parseLong(this.millisBetweenRetries);
+        timeBetweenRetries = Duration.ofMillis(millisBetweenRetries);
         policyTemplate = of(new SimpleRetryPolicyTemplate(millisBetweenRetries, maxRetries));
       }
+      maxRetriesAsInteger = maxRetries;
     }
     shouldRetry = event -> event.getError().isPresent();
   }
@@ -125,12 +144,61 @@ public class UntilSuccessful extends AbstractMuleObjectOwner implements Scope {
     return policyTemplate.orElseGet(() -> createRetryPolicyTemplate(event));
   }
 
+
+  public Publisher<CoreEvent> alternativeApply(Publisher<CoreEvent> publisher) {
+    return from(publisher)
+        .flatMap(event -> {
+          AtomicInteger retryCounter = new AtomicInteger(maxRetriesAsInteger + 1);
+          Duration choosenDuration = timeBetweenRetries;
+          Integer maxRetriesEvaluated = 0;
+
+          if (expressionManager.isExpression(maxRetries)) {
+            ExpressionManagerSession session = expressionManager.openSession(getLocation(), event, NULL_BINDING_CONTEXT);
+            Integer maxRetries = (Integer) session.evaluate(this.maxRetries, DataType.NUMBER).getValue();
+            maxRetriesEvaluated = maxRetries;
+            retryCounter.set(maxRetries + 1);
+            if (expressionManager.isExpression(millisBetweenRetries)) {
+              Integer millisBetweenRetries = (Integer) session.evaluate(this.millisBetweenRetries, DataType.NUMBER).getValue();
+              choosenDuration = Duration.ofMillis(millisBetweenRetries);
+            } else {
+              choosenDuration = Duration.ofMillis(Integer.parseInt(millisBetweenRetries));
+            }
+          }
+
+          final LazyValue<Boolean> isTransactional = new LazyValue<>(() -> isTransactionActive());
+          reactor.core.scheduler.Scheduler reactorRetryScheduler =
+              fromExecutorService(new ConditionalExecutorServiceDecorator(timer, s -> isTransactional.get()));
+
+          AtomicReference<FluxSink> sinkReference = new AtomicReference<>();
+          Integer finalMaxRetriesEvaluated = maxRetriesEvaluated;
+          return Flux.<CoreEvent>create(sink -> {
+            sinkReference.set(sink);
+            sink.next(event);
+          })
+              .transform(publisher1 -> applyWithChildContext(publisher1, nestedChain,
+                                                             Optional.of(UntilSuccessful.this.getLocation())))
+              .doOnNext(completedEvent -> sinkReference.get().complete())
+              .onErrorContinue(getRetryPredicate(), (error, failureEvent) -> {
+                int retriesLeft = retryCounter.decrementAndGet();
+                if (retriesLeft > 0) {
+                  // Retry
+                  LOGGER.error("Retrying execution of event, attempt {} of {}.", retriesLeft,
+                               maxRetriesAsInteger != RETRY_COUNT_FOREVER ? finalMaxRetriesEvaluated.toString() : "unlimited");
+                  sinkReference.get().next(event);
+                } else {
+                  // Retries exhausted
+                  LOGGER.error("Retry attempts exhausted. Failing...");
+                  throw propagate(new RetryExhaustedException(error));
+                }
+              })
+              .onErrorMap(RetryExhaustedException.class, re -> getThrowableFunction(event).apply(unwrap(re.getCause())))
+              .delayElements(choosenDuration, reactorRetryScheduler);
+        });
+  }
+
   @Override
   public Publisher<CoreEvent> apply(Publisher<CoreEvent> publisher) {
-    return from(publisher)
-        .flatMap(event -> Mono.from(processWithChildContextDontComplete(event, nestedChain, ofNullable(getLocation())))
-            .transform(p -> fetchPolicyTemplate(event).applyPolicy(p, getRetryPredicate(), e -> {
-            }, getThrowableFunction(event), timer)));
+    return alternativeApply(publisher);
   }
 
   private Predicate<Throwable> getRetryPredicate() {
