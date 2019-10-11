fc1907df38fba4c33874764d1d84d6da433948ee
SebaElizalde <sebi.elizalde@gmail.com>
2019-10-11 18:17:50 -0300

    added fields to drugbatch

--- tests/test-extensions/heisenberg-extension/src/main/java/org/mule/test/heisenberg/extension/model/drugs/DrugBatch.java
+++ tests/test-extensions/heisenberg-extension/src/main/java/org/mule/test/heisenberg/extension/model/drugs/DrugBatch.java
@@ -8,4 +8,30 @@ package org.mule.test.heisenberg.extension.model.drugs;
 
 public class DrugBatch {
 
+  private String drugType;
+  private Integer batchSize;
+
+  public DrugBatch() {}
+
+  public String getDrugType() {
+    return drugType;
+  }
+
+  public DrugBatch(String drugType, Integer batchSize) {
+    this.drugType = drugType;
+    this.batchSize = batchSize;
+  }
+
+  public void setDrugType(String drugType) {
+    this.drugType = drugType;
+  }
+
+  public Integer getBatchSize() {
+    return batchSize;
+  }
+
+  public void setBatchSize(Integer batchSize) {
+    this.batchSize = batchSize;
+  }
+
 }
