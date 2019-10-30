7bc3544034bda0895ae93879aa160ee4ebfc2611
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2019-10-30 17:04:45 -0300

    MULE-17696: Optional ParameterGroups are required to be exported (#8345)

--- modules/extensions-spring-support/src/test/java/org/mule/test/module/extension/classloading/ClassLoadingOnConnectionsTestCase.java
+++ modules/extensions-spring-support/src/test/java/org/mule/test/module/extension/classloading/ClassLoadingOnConnectionsTestCase.java
@@ -8,6 +8,7 @@ package org.mule.test.module.extension.classloading;
 
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
+import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.AllOf.allOf;
@@ -18,28 +19,28 @@ import static org.mule.test.classloading.CLNoneConnectionProvider.CONNECT;
 import static org.mule.test.classloading.CLNoneConnectionProvider.DISCONNECT;
 import static org.mule.test.classloading.CLPoolingConnectionProvider.ON_BORROW;
 import static org.mule.test.classloading.CLPoolingConnectionProvider.ON_RETURN;
+import static org.mule.test.classloading.internal.AllOptionalParameterGroup.ALL_OPTIONAL_PARAMETER_GROUP;
 
 import org.mule.tck.junit4.rule.SystemProperty;
 import org.mule.test.classloading.api.ClassLoadingHelper;
 import org.mule.test.module.extension.AbstractExtensionFunctionalTestCase;
 
-import org.hamcrest.Matcher;
-import org.hamcrest.core.StringContains;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.core.StringContains;
+import org.junit.Rule;
+import org.junit.Test;
 
 public class ClassLoadingOnConnectionsTestCase extends AbstractExtensionFunctionalTestCase {
 
   @Rule
   public SystemProperty disableTestConnectivity = disableAutomaticTestConnectivity();
 
-  @Before
-  public void setUp() {
+  @Override
+  protected void doTearDown() throws Exception {
     ClassLoadingHelper.createdClassLoaders.clear();
   }
 
@@ -51,29 +52,38 @@ public class ClassLoadingOnConnectionsTestCase extends AbstractExtensionFunction
   @Test
   public void noneConnectionProvider() throws Exception {
     flowRunner("none-operation").run();
-    verifyUsedClassLoaders(CONNECT, DISCONNECT);
+    verifyUsedClassLoaders(ALL_OPTIONAL_PARAMETER_GROUP, CONNECT, DISCONNECT);
   }
 
   @Test
   public void cachedConnectionProvider() throws Exception {
     flowRunner("cached-operation").run();
-    verifyUsedClassLoaders(CONNECT);
+    verifyUsedClassLoaders(ALL_OPTIONAL_PARAMETER_GROUP, CONNECT);
   }
 
   @Test
   public void poolingConnectionProvider() throws Exception {
     flowRunner("pooling-operation").run();
-    verifyUsedClassLoaders(CONNECT, ON_BORROW, ON_RETURN);
+    verifyUsedClassLoaders(ALL_OPTIONAL_PARAMETER_GROUP, CONNECT, ON_BORROW, ON_RETURN);
+  }
+
+  @Test
+  public void allOptionalParameterGroup() throws Exception {
+    verifyUsedClassLoaders(ALL_OPTIONAL_PARAMETER_GROUP);
   }
 
   void verifyUsedClassLoaders(String... phasesToExecute) {
     Map<String, ClassLoader> createdClassLoaders = ClassLoadingHelper.createdClassLoaders;
     List<ClassLoader> collect = createdClassLoaders.values().stream().distinct().collect(toList());
-    assertThat(collect.size(), is(1));
-    ClassLoader classLoader = collect.get(0);
-    assertThat(classLoader.toString(),
-               allOf(containsString("classloading-extension"), containsString(".TestRegionClassLoader[Region] @")));
+    collect.forEach(this::assertExtensionClassLoader);
     Set<String> executedPhases = createdClassLoaders.keySet();
     assertThat(executedPhases, is(hasItems(stream(phasesToExecute).map(StringContains::containsString).toArray(Matcher[]::new))));
+  }
+
+  private void assertExtensionClassLoader(ClassLoader classLoader) {
+    assertThat(classLoader.toString(),
+               allOf(containsString("classloading-extension"),
+                     anyOf(containsString(".TestRegionClassLoader[Region] @"),
+                           containsString("MuleArtifactClassLoader"))));
   }
 }
--- tests/test-extensions/classloading-extension/src/main/java/org/mule/test/classloading/CLConfiguration.java
+++ tests/test-extensions/classloading-extension/src/main/java/org/mule/test/classloading/CLConfiguration.java
@@ -8,13 +8,17 @@ package org.mule.test.classloading;
 
 import org.mule.runtime.extension.api.annotation.Configuration;
 import org.mule.runtime.extension.api.annotation.Operations;
+import org.mule.runtime.extension.api.annotation.param.ParameterGroup;
+import org.mule.test.classloading.internal.AllOptionalParameterGroup;
 
 @Configuration(name = "config")
 @Operations(CLOperations.class)
 public class CLConfiguration {
 
-  public CLConfiguration() {
+  @ParameterGroup(name = "allOptional", showInDsl = true)
+  private AllOptionalParameterGroup allOptionals;
 
+  public AllOptionalParameterGroup getAllOptionals() {
+    return allOptionals;
   }
-
 }
