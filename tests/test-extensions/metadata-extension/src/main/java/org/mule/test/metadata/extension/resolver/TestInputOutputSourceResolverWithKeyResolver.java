011a6c41704144456e89f90d47015b060ffc8ba4
Guillermo Fernandes <gsfernandes@users.noreply.github.com>
2019-10-31 16:42:28 -0300

    MULE-17505: Runtime is not starting ConnectionProviders during metada… (#8349)
    ...ta resolution phase
    
    (cherry picked from commit d76ab1a753611f682c347a69e67d16f38b5b3dfe)
    (cherry picked from commit 5bc4a254bc466aca52ac5984e9dcbc79e1d7fdf9)

--- tests/test-extensions/metadata-extension/src/main/java/org/mule/test/metadata/extension/resolver/TestInputOutputSourceResolverWithKeyResolver.java
+++ tests/test-extensions/metadata-extension/src/main/java/org/mule/test/metadata/extension/resolver/TestInputOutputSourceResolverWithKeyResolver.java
@@ -7,7 +7,6 @@
 package org.mule.test.metadata.extension.resolver;
 
 import static org.mule.runtime.api.metadata.MetadataKeyBuilder.newKey;
-import static org.mule.test.metadata.extension.MetadataSource.STARTED;
 
 import org.mule.metadata.api.model.MetadataType;
 import org.mule.runtime.api.connection.ConnectionException;
@@ -17,22 +16,26 @@ import org.mule.runtime.api.metadata.MetadataResolvingException;
 import org.mule.runtime.api.metadata.resolving.InputTypeResolver;
 import org.mule.runtime.api.metadata.resolving.OutputTypeResolver;
 import org.mule.runtime.api.metadata.resolving.TypeKeysResolver;
-
-import com.google.common.collect.ImmutableSet;
+import org.mule.test.metadata.extension.MetadataConnectionProvider;
+import org.mule.test.metadata.extension.MetadataSource;
 
 import java.util.Set;
+
+import com.google.common.collect.ImmutableSet;
 
 public class TestInputOutputSourceResolverWithKeyResolver
     implements TypeKeysResolver, InputTypeResolver<String>, OutputTypeResolver<String> {
 
-  public static final String STARTED_KEY_MASK = "Source Started = [%s]";
+  public static final String STARTED_SOURCE_KEY_MASK = "Source Started = [%s]";
+  public static final String STARTED_CONNECTION_PROVIDER_KEY_MASK = "Connection Provider Started = [%s]";
   public static final String TEST_INPUT_OUTPUT_SOURCE_RESOLVER_WITH_KEY_RESOLVER = "TestInputOutputSourceResolverWithKeyResolver";
 
   @Override
   public Set<MetadataKey> getKeys(MetadataContext context) throws MetadataResolvingException, ConnectionException {
     return ImmutableSet.<MetadataKey>builder()
         .addAll(TestMetadataResolverUtils.getKeys(context))
-        .add(newKey(String.format(STARTED_KEY_MASK, STARTED)).build())
+        .add(newKey(String.format(STARTED_SOURCE_KEY_MASK, MetadataSource.STARTED)).build())
+        .add(newKey(String.format(STARTED_CONNECTION_PROVIDER_KEY_MASK, MetadataConnectionProvider.STARTED)).build())
         .build();
   }
 
