65d1a95c75856db40a39c0f265e1f1bb65451a95
Luciano RaineriMarchina <luciano.raineri@mulesoft.com>
2019-11-07 15:52:05 -0300

    saving

--- modules/spring-config/src/main/java/org/mule/runtime/config/api/dsl/model/metadata/DslElementBasedValueProviderCacheIdGenerator.java
+++ modules/spring-config/src/main/java/org/mule/runtime/config/api/dsl/model/metadata/DslElementBasedValueProviderCacheIdGenerator.java
@@ -31,7 +31,7 @@ import org.mule.runtime.extension.api.property.RequiredForMetadataModelProperty;
 
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.List
 import java.util.Map;
 import java.util.Optional;
 
