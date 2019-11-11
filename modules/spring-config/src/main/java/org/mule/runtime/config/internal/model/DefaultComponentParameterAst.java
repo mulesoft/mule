437a53da35cfc41b15a07f9891c05ead59346a66
Rodro <rodrigo.merino@mulesoft.com>
2019-11-11 13:58:59 -0300

    either, take 2

--- modules/spring-config/src/main/java/org/mule/runtime/config/internal/model/DefaultComponentParameterAst.java
+++ modules/spring-config/src/main/java/org/mule/runtime/config/internal/model/DefaultComponentParameterAst.java
@@ -8,6 +8,8 @@ package org.mule.runtime.config.internal.model;
 
 import static java.util.Optional.ofNullable;
 import static org.apache.commons.lang3.StringUtils.isEmpty;
+import static org.mule.runtime.api.functional.Either.left;
+import static org.mule.runtime.api.functional.Either.right;
 import static org.mule.runtime.api.meta.ExpressionSupport.NOT_SUPPORTED;
 import static org.mule.runtime.module.extension.internal.util.MuleExtensionUtils.isExpression;
 
@@ -17,6 +19,7 @@ import org.mule.metadata.api.model.NumberType;
 import org.mule.metadata.api.model.ObjectFieldType;
 import org.mule.metadata.api.model.StringType;
 import org.mule.metadata.api.visitor.MetadataTypeVisitor;
+import org.mule.runtime.api.functional.Either;
 import org.mule.runtime.api.meta.model.parameter.ParameterModel;
 import org.mule.runtime.ast.api.ComponentAst;
 import org.mule.runtime.ast.api.ComponentMetadataAst;
@@ -54,6 +57,71 @@ public class DefaultComponentParameterAst implements ComponentParameterAst {
 
   @Override
   public <T> Either<String, T> getValue() {
+    if (complexValue != null) {
+      return right((T) complexValue);
+    } else if (isEmpty(rawValue)) {
+      // previous implementations were assuming that an empty string was the same as the param not being present...
+      return right((T) getModel().getDefaultValue());
+    } else {
+      AtomicReference<String> expression = new AtomicReference<>();
+      AtomicReference<T> value = new AtomicReference<>();
+
+      getModel().getType().accept(new MetadataTypeVisitor() {
+
+        @Override
+        public void visitObjectField(ObjectFieldType objectFieldType) {
+          objectFieldType.getValue().accept(this);
+        }
+
+        @Override
+        public void visitBoolean(BooleanType booleanType) {
+          if (isExpression(rawValue)) {
+            defaultVisit(booleanType);
+          } else {
+            value.set((T) Boolean.valueOf(rawValue));
+          }
+        }
+
+        @Override
+        public void visitNumber(NumberType numberType) {
+          if (isExpression(rawValue)) {
+            defaultVisit(numberType);
+          } else {
+            value.set((T) Long.valueOf(rawValue));
+          }
+        }
+
+        @Override
+        public void visitString(StringType stringType) {
+          if (isExpression(rawValue)) {
+            defaultVisit(stringType);
+          } else {
+            value.set((T) rawValue);
+          }
+        }
+
+        @Override
+        protected void defaultVisit(MetadataType metadataType) {
+          if (getModel().getName().equals("config-ref")) {
+            // For config-ref, just return the name of the referenced object
+            value.set((T) rawValue);
+          } else if (!NOT_SUPPORTED.equals(getModel().getExpressionSupport())) {
+            // For complex types that may be the result of an expression, just return the expression
+            expression.set(rawValue);
+          }
+        }
+      });
+
+      if (expression.get() != null) {
+        return left(expression.get());
+      } else {
+        return right(value.get());
+      }
+    }
+  }
+
+  @Override
+  public <T> Either<String, T> getValueEither() {
     if (complexValue != null) {
       return right((T) complexValue);
     } else if (isEmpty(rawValue)) {
