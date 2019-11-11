23e02681807b7e6dd78ac88055349c5d655637c1
Sebastian Elizalde <sebi.elizalde@gmail.com>
2019-11-11 13:48:26 -0300

    MULE-17398: Operations that return a PagingProvider do not populate the type catalog properly (#8355)

--- modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/loader/java/JavaDeclarationDelegateTestCase.java
+++ modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/loader/java/JavaDeclarationDelegateTestCase.java
@@ -472,7 +472,7 @@ public class JavaDeclarationDelegateTestCase extends AbstractJavaExtensionDeclar
     assertThat(extensionDeclaration.getOperations(), hasSize(57));
 
     WithOperationsDeclaration withOperationsDeclaration = extensionDeclaration.getConfigurations().get(0);
-    assertThat(withOperationsDeclaration.getOperations().size(), is(20));
+    assertThat(withOperationsDeclaration.getOperations().size(), is(21));
     assertOperation(withOperationsDeclaration, SAY_MY_NAME_OPERATION, "");
     assertOperation(withOperationsDeclaration, NAME_AS_STREAM, "");
     assertOperation(withOperationsDeclaration, GET_ENEMY_OPERATION, "");
@@ -508,6 +508,7 @@ public class JavaDeclarationDelegateTestCase extends AbstractJavaExtensionDeclar
     assertOperation(extensionDeclaration, "executeRemoteKill", "");
     assertOperation(extensionDeclaration, "callGusFringNonBlocking", "");
     assertOperation(withOperationsDeclaration, "pagedOperationWithExtensionClassLoader", "");
+    assertOperation(withOperationsDeclaration, "getDrugs", "");
 
     OperationDeclaration operation = getOperation(withOperationsDeclaration, SAY_MY_NAME_OPERATION);
     assertThat(operation, is(notNullValue()));
