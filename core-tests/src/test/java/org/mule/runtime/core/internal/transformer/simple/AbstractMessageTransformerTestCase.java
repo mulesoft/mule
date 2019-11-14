42157abb58811bb1c73b35594e7cf207082b603f
Rodrigo Merino <elrodro83@users.noreply.github.com>
2019-11-14 17:41:02 -0300

    MULE-17754: `NoSuchElementException` when using TransformationService (#8390)
    with global errorHandler

--- core-tests/src/test/java/org/mule/runtime/core/internal/transformer/simple/AbstractMessageTransformerTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/transformer/simple/AbstractMessageTransformerTestCase.java
@@ -6,22 +6,30 @@
  */
 package org.mule.runtime.core.internal.transformer.simple;
 
+import static java.nio.charset.Charset.defaultCharset;
+import static java.util.Collections.singletonMap;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mule.runtime.api.component.AbstractComponent.ROOT_CONTAINER_NAME_KEY;
 import static org.mule.runtime.api.metadata.DataType.BYTE_ARRAY;
+
 import org.mule.runtime.api.message.Message;
 import org.mule.runtime.api.metadata.DataType;
 import org.mule.runtime.core.api.MuleContext;
+import org.mule.runtime.core.api.config.DefaultMuleConfiguration;
 import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.runtime.core.api.transformer.AbstractMessageTransformer;
 import org.mule.runtime.core.api.transformer.MessageTransformerException;
+import org.mule.runtime.core.internal.exception.GlobalErrorHandler;
 import org.mule.tck.junit4.AbstractMuleContextTestCase;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -42,6 +50,14 @@ public class AbstractMessageTransformerTestCase extends AbstractMuleContextTestC
     }
   };
 
+  @Override
+  protected Map<String, Object> getStartUpRegistryObjects() {
+    final GlobalErrorHandler errorHandler = new GlobalErrorHandler();
+    errorHandler.setExceptionListeners(new ArrayList<>());
+    errorHandler.setAnnotations(singletonMap(ROOT_CONTAINER_NAME_KEY, "errorHandlerFromConfig"));
+    return singletonMap("errorHandlerFromConfig", errorHandler);
+  }
+
   @Test
   public void reusesDefaultFlowConstruct() throws MessageTransformerException {
     MuleContext muleContextSpy = spy(muleContext);
@@ -54,6 +70,14 @@ public class AbstractMessageTransformerTestCase extends AbstractMuleContextTestC
 
   private void transform() throws MessageTransformerException {
     assertThat(transformer.transform(Message.of(new Object()), Charset.defaultCharset(), null), is(BYTES));
+  }
+
+  @Test
+  public void transfromationWithDefaultErrorHandler() throws MessageTransformerException {
+    ((DefaultMuleConfiguration) muleContext.getConfiguration()).setDefaultErrorHandlerName("errorHandlerFromConfig");
+
+    transformer.setMuleContext(muleContext);
+    transformer.transform(Message.of(new Object()), defaultCharset(), null);
   }
 
 }
