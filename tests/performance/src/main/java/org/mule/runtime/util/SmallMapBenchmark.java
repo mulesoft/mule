0046c40155a524dab0f8047f76a08d800a806e5b
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2019-11-28 19:48:31 -0300

    improved benchmark

--- tests/performance/src/main/java/org/mule/runtime/util/SmallMapBenchmark.java
+++ tests/performance/src/main/java/org/mule/runtime/util/SmallMapBenchmark.java
@@ -10,7 +10,6 @@ import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
 import org.mule.AbstractBenchmark;
 import org.mule.runtime.api.exception.MuleException;
-import org.mule.runtime.api.util.Reference;
 import org.mule.runtime.api.util.collection.FastMap;
 
 import java.util.HashMap;
@@ -23,58 +22,74 @@ import org.openjdk.jmh.annotations.TearDown;
 @OutputTimeUnit(NANOSECONDS)
 public class SmallMapBenchmark extends AbstractBenchmark {
 
+  public static final String IRON_MAN = "IronMan";
+  public static final String CAPTAIN_AMERICA = "Captain America";
+  public static final String BLACK_WIDOW = "Black Widow";
+  public static final String ANT_MAN = "Ant Man";
+  public static final String ANT_MAN1 = "Ant Man";
+  public static final String DR_STRANGE = "Dr. Strange";
+
   @TearDown
   public void teardown() throws MuleException {}
 
   @Benchmark
-  public Object halfHashMap() {
+  public Object hashMapOf3() {
     Map<String, String> map = new HashMap<>();
-    testHalfMap(map);
+    populate3(map);
+
+    map.get(IRON_MAN);
+    map.get(DR_STRANGE);
+
     return map;
   }
 
   @Benchmark
-  public Object halfSmallMap() {
+  public Object smallMapOf3() {
     Map<String, String> map = new FastMap<>();
-    testHalfMap(map);
+    populate3(map);
+
+    map.get(IRON_MAN);
+    map.get(DR_STRANGE);
     return map;
   }
 
-  @Benchmark
+  //@Benchmark
   public Object fullHashMap() {
     Map<String, String> map = new HashMap<>();
-    testFullMap(map);
+    populateFive(map);
     return map;
   }
 
-  @Benchmark
+  //@Benchmark
   public Object fullSmallMap() {
     Map<String, String> map = new FastMap<>();
-    testFullMap(map);
+    populateFive(map);
     return map;
   }
 
-  private void testFullMap(Map<String, String> map) {
-    map.put("IronMan", "Mr. Stark");
-    map.put("Captain America", "Steve Rodgers");
-    map.put("Black Widow", "Natasha Rommanoff");
-    map.put("Ant Man", "Hank Pim");
-    map.put("Ant Man", "Hank Pim");
-    map.put("Dr. Strange", "Dr. Strange");
+  private void populateFive(Map<String, String> map) {
+    map.put(IRON_MAN, "Mr. Stark");
+    map.put(CAPTAIN_AMERICA, "Steve Rodgers");
+    map.put(BLACK_WIDOW, "Natasha Rommanoff");
+    map.put(ANT_MAN, "Hank Pim");
+    map.put(ANT_MAN1, "Hank Pim");
+    map.put(DR_STRANGE, "Dr. Strange");
 
+    map.get("IronMan");
+    map.get("Ant Man");
 
-    Reference<String> r = new Reference<>();
-    map.entrySet().forEach(entry -> r.set(entry.getKey() + entry.getValue()));
+    //Reference<String> r = new Reference<>();
+    //map.entrySet().forEach(entry -> r.set(entry.getKey() + entry.getValue()));
   }
 
-  private void testHalfMap(Map<String, String> map) {
-    map.put("IronMan", "Mr. Stark");
-    map.put("Captain America", "Steve Rodgers");
-    map.put("Black Widow", "Natasha Rommanoff");
-    map.put("Black Widow", "Natasha Rommanoff");
+  private void populate3(Map<String, String> map) {
+    map.put(IRON_MAN, "Mr. Stark");
+    map.put(CAPTAIN_AMERICA, "Steve Rodgers");
+    map.put(BLACK_WIDOW, "Natasha Rommanoff");
+    map.put(BLACK_WIDOW, "Natasha Rommanoff");
 
-    Reference<String> r = new Reference<>();
-    map.entrySet().forEach(entry -> r.set(entry.getKey() + entry.getValue()));
+    //Reference<String> r = new Reference<>();
+    //map.entrySet().forEach(entry -> r.set(entry.getKey() + entry.getValue()));
   }
 
 }
