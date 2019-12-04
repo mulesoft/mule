e5e674c8608f6f7941e85430f9dde581b42e0e2a
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2019-12-04 19:56:29 -0300

    seems to work.. but

--- core/src/main/java/org/mule/runtime/core/internal/streaming/bytes/FileStoreCursorStreamConfig.java
+++ core/src/main/java/org/mule/runtime/core/internal/streaming/bytes/FileStoreCursorStreamConfig.java
@@ -6,8 +6,12 @@
  */
 package org.mule.runtime.core.internal.streaming.bytes;
 
+import static java.lang.Math.min;
+import static org.mule.runtime.api.util.DataUnit.BYTE;
+import static org.mule.runtime.core.internal.streaming.bytes.ByteStreamingConstants.DEFAULT_BUFFER_BUCKET_SIZE;
 import static org.mule.runtime.extension.api.ExtensionConstants.DEFAULT_BYTES_STREAMING_MAX_BUFFER_SIZE;
 import static org.mule.runtime.extension.api.ExtensionConstants.DEFAULT_BYTE_STREAMING_BUFFER_DATA_UNIT;
+
 import org.mule.runtime.api.util.DataSize;
 
 /**
@@ -18,6 +22,8 @@ import org.mule.runtime.api.util.DataSize;
 public final class FileStoreCursorStreamConfig {
 
   private final DataSize maxInMemorySize;
+  private final int bucketSize;
+  private final int bucketsCount;
 
   /**
    * @return A new instance configured with default settings
@@ -32,7 +38,13 @@ public final class FileStoreCursorStreamConfig {
    * @param maxInMemorySize the maximum amount of data to be held in memory
    */
   public FileStoreCursorStreamConfig(DataSize maxInMemorySize) {
-    this.maxInMemorySize = maxInMemorySize;
+    int effectiveMaxInMemorySize = maxInMemorySize.toBytes();
+    bucketSize = min(DEFAULT_BUFFER_BUCKET_SIZE, effectiveMaxInMemorySize);
+    bucketsCount = effectiveMaxInMemorySize / bucketSize;
+    if (effectiveMaxInMemorySize % bucketSize > 0) {
+      effectiveMaxInMemorySize = bucketsCount * bucketSize;
+    }
+    this.maxInMemorySize = new DataSize(effectiveMaxInMemorySize, BYTE);
   }
 
   /**
@@ -40,5 +52,13 @@ public final class FileStoreCursorStreamConfig {
    */
   public DataSize getMaxInMemorySize() {
     return maxInMemorySize;
+  }
+
+  public int getBucketSize() {
+    return bucketSize;
+  }
+
+  public int getBucketsCount() {
+    return bucketsCount;
   }
 }
