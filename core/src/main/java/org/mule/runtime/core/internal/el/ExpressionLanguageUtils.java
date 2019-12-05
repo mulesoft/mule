dcb89c8037e2483f15ee1dd4d88782ec8d78a37f
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2019-12-05 18:42:30 -0300

    compiling core... some red tests

---
+++ core/src/main/java/org/mule/runtime/core/internal/el/ExpressionLanguageUtils.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.internal.el;
+
+import static org.mule.runtime.api.el.BindingContextUtils.NULL_BINDING_CONTEXT;
+import static org.mule.runtime.api.el.BindingContextUtils.addEventBuindingsToBuilder;
+import static org.mule.runtime.core.internal.event.VoidEventFactory.getVoidEvent;
+
+import org.mule.runtime.api.el.BindingContext;
+import org.mule.runtime.api.el.CompiledExpression;
+import org.mule.runtime.api.el.ExpressionLanguage;
+
+public final class ExpressionLanguageUtils {
+
+  private ExpressionLanguageUtils() {}
+
+  private static final BindingContext COMPILATION_BINDING_CONTEXT =
+      addEventBuindingsToBuilder(getVoidEvent(), NULL_BINDING_CONTEXT).build();
+
+
+  public static CompiledExpression compile(String expression, ExpressionLanguage expressionLanguage) {
+    return expressionLanguage.compile(expression, COMPILATION_BINDING_CONTEXT);
+  }
+}
