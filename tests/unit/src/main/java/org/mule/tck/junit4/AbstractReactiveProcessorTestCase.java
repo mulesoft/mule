b6ddd5b8fbb858d3ab37b9e5edcb488d4b33b296
Julieta <luzzijulieta@gmail.com>
2019-12-17 17:36:25 -0300

    Revert "MULE-16528: remove `flatMap` in foreach (#8464)" (#8499)
    This reverts commit fd58a490a473f073a4ba65ee3df2b9c19bb956b7.

--- tests/unit/src/main/java/org/mule/tck/junit4/AbstractReactiveProcessorTestCase.java
+++ tests/unit/src/main/java/org/mule/tck/junit4/AbstractReactiveProcessorTestCase.java
@@ -89,7 +89,7 @@ public abstract class AbstractReactiveProcessorTestCase extends AbstractMuleCont
     return process(processor, event, true);
   }
 
-  public CoreEvent process(Processor processor, CoreEvent event, boolean unwrapMessagingException) throws Exception {
+  protected CoreEvent process(Processor processor, CoreEvent event, boolean unwrapMessagingException) throws Exception {
     setMuleContextIfNeeded(processor, muleContext);
     try {
       switch (mode) {
