f6405ce0f3744cabbba785d78df834cb993465f1
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2019-12-22 19:15:16 -0300

    MULE-17883: ParameterResolvers initialize a new AttributeEvaluator on each execution (#8518)

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/resolver/ExpressionBasedParameterResolver.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/resolver/ExpressionBasedParameterResolver.java
@@ -6,19 +6,13 @@
  */
 package org.mule.runtime.module.extension.internal.runtime.resolver;
 
+import static java.util.Optional.ofNullable;
+
 import org.mule.runtime.api.exception.MuleException;
 import org.mule.runtime.api.exception.MuleRuntimeException;
-import org.mule.runtime.api.lifecycle.Initialisable;
-import org.mule.runtime.api.lifecycle.InitialisationException;
-import org.mule.runtime.api.metadata.DataType;
-import org.mule.runtime.api.transformation.TransformationService;
-import org.mule.runtime.core.api.MuleContext;
-import org.mule.runtime.core.api.el.ExtendedExpressionManager;
 import org.mule.runtime.extension.api.runtime.parameter.ParameterResolver;
 
 import java.util.Optional;
-
-import javax.inject.Inject;
 
 /**
  * {@link ParameterResolver} implementation for the parameters that are resolved from an expression
@@ -26,31 +20,18 @@ import javax.inject.Inject;
  * @param <T> Concrete parameter type to be resolved
  * @since 4.0
  */
-class ExpressionBasedParameterResolver<T> implements ParameterResolver<T>, Initialisable {
+class ExpressionBasedParameterResolver<T> implements ParameterResolver<T> {
 
   private final String expression;
   private final ValueResolvingContext context;
-  private final DataType expectedDataType;
-  private TypeSafeExpressionValueResolver<T> valueResolver;
+  private final TypeSafeExpressionValueResolver<T> valueResolver;
 
-  @Inject
-  private TransformationService transformationService;
-
-  @Inject
-  private ExtendedExpressionManager extendedExpressionManager;
-
-  @Inject
-  private MuleContext muleContext;
-
-  private Class<T> type;
-  private Boolean melDefault;
-  private Boolean melAvailable;
-
-  ExpressionBasedParameterResolver(String expression, Class<T> type, ValueResolvingContext context, DataType expectedDataType) {
+  ExpressionBasedParameterResolver(String expression,
+                                   TypeSafeExpressionValueResolver<T> valueResolver,
+                                   ValueResolvingContext context) {
     this.expression = expression;
-    this.type = type;
+    this.valueResolver = valueResolver;
     this.context = context;
-    this.expectedDataType = expectedDataType;
   }
 
   /**
@@ -70,38 +51,6 @@ class ExpressionBasedParameterResolver<T> implements ParameterResolver<T>, Initi
    */
   @Override
   public Optional<String> getExpression() {
-    return Optional.ofNullable(expression);
-  }
-
-  @Override
-  public void initialise() throws InitialisationException {
-    valueResolver = new TypeSafeExpressionValueResolver<>(expression, type, expectedDataType);
-    valueResolver.setExtendedExpressionManager(extendedExpressionManager);
-    valueResolver.setTransformationService(transformationService);
-    valueResolver.setMuleContext(muleContext);
-    valueResolver.setMelDefault(melDefault);
-    valueResolver.setMelAvailable(melAvailable);
-
-    valueResolver.initialise();
-  }
-
-  public void setTransformationService(TransformationService transformationService) {
-    this.transformationService = transformationService;
-  }
-
-  public void setExtendedExpressionManager(ExtendedExpressionManager extendedExpressionManager) {
-    this.extendedExpressionManager = extendedExpressionManager;
-  }
-
-  public void setMuleContext(MuleContext muleContext) {
-    this.muleContext = muleContext;
-  }
-
-  public void setMelDefault(Boolean melDefault) {
-    this.melDefault = melDefault;
-  }
-
-  public void setMelAvailable(Boolean melAvailable) {
-    this.melAvailable = melAvailable;
+    return ofNullable(expression);
   }
 }
