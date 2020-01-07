340c2ab5bf953af75a00aa1060049ef7e802fb12
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2020-01-07 18:49:02 -0300

    refactoring

--- modules/oauth-api/src/main/java/org/mule/runtime/oauth/api/builder/OAuthPlatformManagedDancerBuilder.java
+++ modules/oauth-api/src/main/java/org/mule/runtime/oauth/api/builder/OAuthPlatformManagedDancerBuilder.java
@@ -8,21 +8,11 @@ package org.mule.runtime.oauth.api.builder;
 
 import org.mule.api.annotation.Experimental;
 import org.mule.api.annotation.NoImplement;
-import org.mule.runtime.api.tls.TlsContextFactory;
-import org.mule.runtime.http.api.client.HttpClient;
-import org.mule.runtime.http.api.client.proxy.ProxyConfig;
 import org.mule.runtime.oauth.api.PlatformManagedOAuthDancer;
 import org.mule.runtime.oauth.api.listener.PlatformManagedOAuthStateListener;
 
-import java.nio.charset.Charset;
-import java.util.function.Function;
-
 /**
  * Builder that allows to configure the attributes for the {@link PlatformManagedOAuthDancer}
- * <p>
- * Because the actual OAuth authorization is not performed by Mule but the Anypoint Platform, some of the methods in the base
- * {@link OAuthDancerBuilder} do not apply and will throw an {@link UnsupportedOperationException}. The the javadocs of all
- * methods defined and overridden in this interface to know which ones are valid.
  * <p>
  * Platform Managed OAuth is an experimental feature. It will only be enabled on selected environments and scenarios.
  * Backwards compatibility is not guaranteed.
@@ -41,6 +31,10 @@ public interface OAuthPlatformManagedDancerBuilder extends OAuthDancerBuilder<Pl
    */
   OAuthPlatformManagedDancerBuilder connectionId(String connectionId);
 
+  OAuthPlatformManagedDancerBuilder organizationId(String organizationId);
+
+  OAuthPlatformManagedDancerBuilder platformUrl(String platformUrl);
+
   /**
    * Adds the {@code listener}. Listeners will be invoked in the same order as they were added
    *
@@ -48,107 +42,4 @@ public interface OAuthPlatformManagedDancerBuilder extends OAuthDancerBuilder<Pl
    * @throws IllegalArgumentException if the {@code listener} is {@code null}
    */
   OAuthPlatformManagedDancerBuilder addListener(PlatformManagedOAuthStateListener listener);
-
-  /**
-   * {@inheritDoc}
-   *
-   * @throws UnsupportedOperationException
-   */
-  @Override
-  default OAuthDancerBuilder tokenUrl(String tokenUrl) {
-    throw new UnsupportedOperationException("This operation does not apply to this builder type");
-  }
-
-  /**
-   * {@inheritDoc}
-   *
-   * @throws UnsupportedOperationException
-   */
-  @Override
-  default OAuthDancerBuilder tokenUrl(HttpClient httpClient, String tokenUrl) {
-    throw new UnsupportedOperationException("This operation does not apply to this builder type");
-  }
-
-  /**
-   * {@inheritDoc}
-   *
-   * @throws UnsupportedOperationException
-   */
-  @Override
-  default OAuthDancerBuilder tokenUrl(String tokenUrl, TlsContextFactory tlsContextFactory) {
-    throw new UnsupportedOperationException("This operation does not apply to this builder type");
-  }
-
-  /**
-   * {@inheritDoc}
-   *
-   * @throws UnsupportedOperationException
-   */
-  @Override
-  default OAuthDancerBuilder<PlatformManagedOAuthDancer> tokenUrl(String tokenUrl, ProxyConfig proxyConfig) {
-    throw new UnsupportedOperationException("This operation does not apply to this builder type");
-  }
-
-  /**
-   * {@inheritDoc}
-   *
-   * @throws UnsupportedOperationException
-   */
-  @Override
-  default OAuthDancerBuilder<PlatformManagedOAuthDancer> tokenUrl(String tokenUrl,
-                                                                  TlsContextFactory tlsContextFactory,
-                                                                  ProxyConfig proxyConfig) {
-    throw new UnsupportedOperationException("This operation does not apply to this builder type");
-  }
-
-  /**
-   * {@inheritDoc}
-   *
-   * @throws UnsupportedOperationException
-   */
-  @Override
-  default OAuthDancerBuilder encoding(Charset encoding) {
-    throw new UnsupportedOperationException("This operation does not apply to this builder type");
-  }
-
-  /**
-   * {@inheritDoc}
-   *
-   * @throws UnsupportedOperationException
-   */
-  @Override
-  default OAuthDancerBuilder clientCredentials(String clientId, String clientSecret) {
-    throw new UnsupportedOperationException("This operation does not apply to this builder type");
-  }
-
-  /**
-   * {@inheritDoc}
-   *
-   * @throws UnsupportedOperationException
-   */
-  @Override
-  default OAuthDancerBuilder<PlatformManagedOAuthDancer> resourceOwnerIdTransformer(
-                                                                                    Function<String, String> resourceOwnerIdTransformer) {
-    throw new UnsupportedOperationException("This operation does not apply to this builder type");
-  }
-
-  /**
-   * {@inheritDoc}
-   *
-   * @throws UnsupportedOperationException
-   */
-  @Override
-  default OAuthDancerBuilder scopes(String scopes) {
-    throw new UnsupportedOperationException("This operation does not apply to this builder type");
-  }
-
-  /**
-   * {@inheritDoc}
-   *
-   * @throws UnsupportedOperationException
-   */
-  @Override
-  default OAuthDancerBuilder withClientCredentialsIn(ClientCredentialsLocation clientCredentialsLocation) {
-    throw new UnsupportedOperationException("This operation does not apply to this builder type");
-  }
 }
