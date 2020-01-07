4f8144a47078ce9f7768a4320a5082f935c0f12b
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2020-01-07 18:53:37 -0300

    MULE-17916: Error on returning a CoreEvent from Soapkit executor (#8551)
    (cherry picked from commit 0134a508786f753b2c42f00b802f341eb56b9d02)
    (cherry picked from commit b143040252773058565e836ba1b551eddd645859)
    (cherry picked from commit 60c7e24a5f96334f0952fb121ee724d7587b3094)

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/operation/AbstractReturnDelegate.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/operation/AbstractReturnDelegate.java
@@ -24,6 +24,7 @@ import static org.mule.runtime.module.extension.internal.util.MuleExtensionUtils
 import org.mule.metadata.api.model.MetadataType;
 import org.mule.runtime.api.component.location.ComponentLocation;
 import org.mule.runtime.api.connection.ConnectionHandler;
+import org.mule.runtime.api.event.Event;
 import org.mule.runtime.api.message.Message;
 import org.mule.runtime.api.meta.model.ComponentModel;
 import org.mule.runtime.api.meta.model.HasOutputModel;
@@ -112,6 +113,10 @@ abstract class AbstractReturnDelegate implements ReturnDelegate {
   }
 
   protected Message toMessage(Object value, ExecutionContextAdapter operationContext) {
+    if (value instanceof Event) {
+      return ((Event) value).getMessage();
+    }
+
     Map<String, Object> params = operationContext.getParameters();
     Optional<MediaType> contextMimeTypeParam = getContextMimeType(params);
     Optional<Charset> contextEncodingParam = getContextEncoding(params);
