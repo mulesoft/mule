4a9fe01e27e440481d3ac09449c306271d62fb0f
Ezequiel Werner <eze210@gmail.com>
2020-01-07 16:24:18 -0300

    MULE-17580: Fix PollingSourceTestCase flaky test (#8550)

--- modules/extensions-spring-support/src/test/java/org/mule/test/module/extension/source/PollingSourceTestCase.java
+++ modules/extensions-spring-support/src/test/java/org/mule/test/module/extension/source/PollingSourceTestCase.java
@@ -13,7 +13,6 @@ import static org.mule.test.petstore.extension.NumberPetAdoptionSource.ALL_NUMBE
 import static org.mule.test.petstore.extension.PetAdoptionSource.ALL_PETS;
 import static org.mule.test.petstore.extension.PetAdoptionSource.FAILED_ADOPTION_COUNT;
 
-import org.junit.Ignore;
 import org.mule.runtime.api.exception.MuleException;
 import org.mule.runtime.api.lifecycle.Startable;
 import org.mule.runtime.core.api.event.CoreEvent;
@@ -53,7 +52,6 @@ public class PollingSourceTestCase extends AbstractExtensionFunctionalTestCase {
   }
 
   @Test
-  @Ignore("MULE-17580")
   public void vanillaPoll() throws Exception {
     startFlow("vanilla");
     assertAllPetsAdopted();
