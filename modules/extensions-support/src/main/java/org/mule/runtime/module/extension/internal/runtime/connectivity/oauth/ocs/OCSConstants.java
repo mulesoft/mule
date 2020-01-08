50c85ad1df1af1d6a515e9a2632a0357e7f90e86
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2020-01-08 12:43:44 -0300

    MULE-17921: Support Platform Managed OAuth tokens

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/connectivity/oauth/ocs/OCSConstants.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/connectivity/oauth/ocs/OCSConstants.java
@@ -1,8 +1,8 @@
 /*
- * (c) 2003-2020 MuleSoft, Inc. This software is protected under international copyright
- * law. All use of this software is subject to MuleSoft's Master Subscription Agreement
- * (or other master license agreement) separately entered into in writing between you and
- * MuleSoft. If such an agreement is not in place, you may not use the software.
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
  */
 package org.mule.runtime.module.extension.internal.runtime.connectivity.oauth.ocs;
 
@@ -56,6 +56,5 @@ public final class OCSConstants {
    */
   public static final String OCS_ORG_ID = "orgId";
 
-  private OCSConstants() {
-  }
+  private OCSConstants() {}
 }
