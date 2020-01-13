2c4befb1d3361fe82abdc59121d950e71210e513
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2020-01-13 19:48:12 -0300

    doodling

---
+++ modules/oauth-api/src/main/java/org/mule/runtime/oauth/api/PlatformManagedConnectionDescriptor.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.oauth.api;
+
+import org.mule.api.annotation.Experimental;
+import org.mule.api.annotation.NoExtend;
+
+import java.util.Map;
+
+@NoExtend
+@Experimental
+public interface PlatformManagedConnectionDescriptor {
+
+  String getId();
+
+  String getUri();
+
+  String getName();
+
+  Map<String, Object> getParameters();
+}
