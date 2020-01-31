5a309924c86071fdf6b80e6cf54455960c858720
Sebastian Elizalde <sebi.elizalde@gmail.com>
2020-01-31 17:33:47 -0300

    MULE-18019: Fix reconnection for Paged Operations on the first call to getPage. (#8619)
    (cherry picked from commit b694f25b86408cc1631bdc95b7d492a7c4fea3e4)

--- tests/test-extensions/reconnection-ext/src/main/java/org/mule/extension/test/extension/reconnection/ReconnectionOperations.java
+++ tests/test-extensions/reconnection-ext/src/main/java/org/mule/extension/test/extension/reconnection/ReconnectionOperations.java
@@ -6,18 +6,31 @@
  */
 package org.mule.extension.test.extension.reconnection;
 
+import static java.util.Collections.singletonList;
+import static java.util.Optional.empty;
 import static org.mule.extension.test.extension.reconnection.ReconnectableConnectionProvider.fail;
+import static org.mule.runtime.extension.api.error.MuleErrors.CONNECTIVITY;
 
 import org.mule.extension.test.extension.reconnection.metadata.RetryPolicyOutputResolver;
+import org.mule.runtime.api.connection.ConnectionException;
 import org.mule.runtime.core.api.retry.policy.RetryPolicyTemplate;
 import org.mule.runtime.extension.api.annotation.metadata.OutputResolver;
 import org.mule.runtime.extension.api.annotation.param.Connection;
+import org.mule.runtime.extension.api.error.MuleErrors;
+import org.mule.runtime.extension.api.exception.ModuleException;
+import org.mule.runtime.extension.api.runtime.streaming.PagingProvider;
+
+import java.util.List;
+import java.util.Optional;
 
 
 /**
  * This class is a container for operations, every public method in this class will be taken as an extension operation.
  */
 public class ReconnectionOperations {
+
+  public static volatile int closePagingProviderCalls = 0;
+  public static volatile int getPageCalls = 0;
 
   /**
    * Example of a simple operation that receives a string parameter and returns a new string message that will be set on the payload.
@@ -30,5 +43,63 @@ public class ReconnectionOperations {
   public RetryPolicyTemplate getRetryPolicyTemplate(@Connection ReconnectableConnection connection,
                                                     RetryPolicyTemplate template) {
     return template;
+  }
+
+  public PagingProvider<ReconnectableConnection, ReconnectableConnection> pagedOperation(Integer failOn,
+                                                                                         MuleErrors withErrorType) {
+    return new PagingProvider<ReconnectableConnection, ReconnectableConnection>() {
+
+      @Override
+      public List<ReconnectableConnection> getPage(ReconnectableConnection connection) {
+        getPageCalls++;
+        if (getPageCalls == failOn) {
+          if (withErrorType.equals(CONNECTIVITY)) {
+            throw new ModuleException(withErrorType, new ConnectionException("Failed to retrieve Page"));
+          }
+          throw new ModuleException(withErrorType, new IllegalArgumentException("An illegal argument was received."));
+        }
+        return singletonList(connection);
+      }
+
+      @Override
+      public Optional<Integer> getTotalResults(ReconnectableConnection connection) {
+        return empty();
+      }
+
+      @Override
+      public void close(ReconnectableConnection connection) {
+        closePagingProviderCalls++;
+      }
+    };
+  }
+
+  public PagingProvider<ReconnectableConnection, ReconnectableConnection> stickyPagedOperation(Integer failOn,
+                                                                                               MuleErrors withErrorType) {
+    return new PagingProvider<ReconnectableConnection, ReconnectableConnection>() {
+
+      @Override
+      public List<ReconnectableConnection> getPage(ReconnectableConnection connection) {
+        getPageCalls++;
+        if (getPageCalls == failOn) {
+          throw new ModuleException(withErrorType, new ConnectionException("Failed to retrieve Page"));
+        }
+        return singletonList(connection);
+      }
+
+      @Override
+      public Optional<Integer> getTotalResults(ReconnectableConnection connection) {
+        return empty();
+      }
+
+      @Override
+      public void close(ReconnectableConnection connection) {
+        closePagingProviderCalls++;
+      }
+
+      @Override
+      public boolean useStickyConnections() {
+        return true;
+      }
+    };
   }
 }
