29a3fcc00c4de97b44f0b5b37fb5fc4bcbc88387
Rodro <rodrigo.merino@mulesoft.com>
2020-02-13 13:38:38 -0300

    MULE-18059: Flow in initial state stopped is started when app is stopped and started

--- core/src/main/java/org/mule/runtime/core/internal/construct/AbstractFlowConstruct.java
+++ core/src/main/java/org/mule/runtime/core/internal/construct/AbstractFlowConstruct.java
@@ -109,7 +109,7 @@ public abstract class AbstractFlowConstruct extends AbstractExecutableComponent 
   @Override
   public final void start() throws MuleException {
     // Check if Initial State is Stopped
-    if (!isStopped() && initialState.equals(INITIAL_STATE_STOPPED)) {
+    if (muleContext.isStarting() && initialState.equals(INITIAL_STATE_STOPPED)) {
       lifecycleManager.fireStartPhase(new EmptyLifecycleCallback<>());
       lifecycleManager.fireStopPhase(new EmptyLifecycleCallback<>());
 
@@ -166,6 +166,7 @@ public abstract class AbstractFlowConstruct extends AbstractExecutableComponent 
     return name;
   }
 
+  @Override
   public FlowExceptionHandler getExceptionListener() {
     return exceptionListener;
   }
