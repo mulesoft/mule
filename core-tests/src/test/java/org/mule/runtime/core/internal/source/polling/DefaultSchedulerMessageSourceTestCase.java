4a91c8786cb21862e12b64f9f52fe73d8cce24d4
Sebastián Zaffarano <szaffarano@mulesoft.com>
2020-02-17 15:43:56 -0300

    MULE-18063: Need a way to stop built-in schedule for CH apps

--- core-tests/src/test/java/org/mule/runtime/core/internal/source/polling/DefaultSchedulerMessageSourceTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/source/polling/DefaultSchedulerMessageSourceTestCase.java
@@ -6,22 +6,38 @@
  */
 package org.mule.runtime.core.internal.source.polling;
 
+import static java.lang.System.setProperty;
 import static java.util.Collections.singletonMap;
 import static java.util.Optional.of;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mule.runtime.api.component.AbstractComponent.LOCATION_KEY;
 import static org.mule.runtime.api.component.location.ConfigurationComponentLocator.REGISTRY_KEY;
+import static org.mule.runtime.api.util.MuleSystemProperties.MULE_DISABLE_SCHEDULERS;
 import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.disposeIfNeeded;
 import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.stopIfNeeded;
 import static org.mule.tck.MuleTestUtils.APPLE_FLOW;
 import static org.mule.tck.MuleTestUtils.createFlowWithSource;
+import static org.mule.test.allure.AllureConstants.SchedulerFeature.SCHEDULER;
 import static org.slf4j.LoggerFactory.getLogger;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.mule.runtime.api.component.location.Location;
 import org.mule.runtime.api.exception.MuleException;
 import org.mule.runtime.api.scheduler.Scheduler;
@@ -38,15 +54,13 @@ import org.mule.tck.SensingNullMessageProcessor;
 import org.mule.tck.junit4.AbstractMuleContextTestCase;
 import org.mule.tck.probe.PollingProber;
 import org.mule.tck.probe.Probe;
-
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import org.slf4j.Logger;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+
+@Feature(SCHEDULER)
 public class DefaultSchedulerMessageSourceTestCase extends AbstractMuleContextTestCase {
 
   private static final Logger LOGGER = getLogger(DefaultSchedulerMessageSourceTestCase.class);
@@ -75,48 +89,51 @@ public class DefaultSchedulerMessageSourceTestCase extends AbstractMuleContextTe
 
   @Test
   public void simplePoll() throws Exception {
-
-    DefaultSchedulerMessageSource schedulerMessageSource = createMessageSource();
-
-    SensingNullMessageProcessor flow = getSensingNullMessageProcessor();
-    schedulerMessageSource.setListener(flow);
-    schedulerMessageSource.setAnnotations(singletonMap(LOCATION_KEY, TEST_CONNECTOR_LOCATION));
-
-    doAnswer(invocationOnMock -> {
-      CoreEvent inputEvent = invocationOnMock.getArgument(0);
-      flow.process(inputEvent);
-      return null;
-    }).when(sourcePolicy).process(any(CoreEvent.class), any(), any());
-
-    schedulerMessageSource.trigger();
-    new PollingProber(RECEIVE_TIMEOUT, 100).check(new Probe() {
+    pollAndAssertWith(p -> new PollingProber(RECEIVE_TIMEOUT, 100).check(new Probe() {
 
       @Override
       public boolean isSatisfied() {
-        return flow.event != null;
+        return p.event != null;
       }
 
       @Override
       public String describeFailure() {
         return "flow event never set by the source flow";
       }
-    });
+    }));
+  }
+
+  @Test
+  @Description("Verifies that no events are processed when the scheduler is disabled")
+  @Issue("MULE-18063")
+  public void simplePollDisabledWithScheduler() throws Exception {
+    String wasDisabled = setProperty(MULE_DISABLE_SCHEDULERS, "true");
+    try {
+      pollAndAssertWith(p -> new PollingProber(RECEIVE_TIMEOUT, 100).check(new Probe() {
+
+        @Override
+        public boolean isSatisfied() {
+          return p.event == null;
+        }
+
+        @Override
+        public String describeFailure() {
+          return "unexpected event when scheduler is disabled";
+        }
+      }));
+    } finally {
+      setProperty(MULE_DISABLE_SCHEDULERS, wasDisabled != null ? wasDisabled : "false");
+    }
   }
 
   @Test
   public void disposeScheduler() throws Exception {
-    SchedulerService schedulerService = muleContext.getSchedulerService();
-    reset(schedulerService);
-
     AtomicReference<Scheduler> pollScheduler = new AtomicReference<>();
 
-    doAnswer(invocation -> {
-      Scheduler scheduler = (Scheduler) invocation.callRealMethod();
-      pollScheduler.set(scheduler);
-      return scheduler;
-    }).when(schedulerService).cpuLightScheduler();
+    SchedulerService schedulerService = mockSchedulerService(pollScheduler);
 
     DefaultSchedulerMessageSource schedulerMessageSource = createMessageSource();
+
     verify(schedulerService).cpuLightScheduler();
 
     schedulerMessageSource.start();
@@ -127,6 +144,35 @@ public class DefaultSchedulerMessageSourceTestCase extends AbstractMuleContextTe
     schedulerMessageSource.dispose();
 
     verify(pollScheduler.get()).stop();
+  }
+
+  @Test
+  @Description("Verifies that a disabled scheduler is neither initialized nor disposed")
+  @Issue("MULE-18063")
+  public void disposeDisabledScheduler() throws Exception {
+
+    String wasDisabled = setProperty(MULE_DISABLE_SCHEDULERS, "true");
+
+    try {
+      AtomicReference<Scheduler> pollScheduler = new AtomicReference<>();
+
+      SchedulerService schedulerService = mockSchedulerService(pollScheduler);
+
+      DefaultSchedulerMessageSource schedulerMessageSource = createMessageSource();
+
+      verify(schedulerService, never()).cpuLightScheduler();
+
+      schedulerMessageSource.start();
+
+      assertThat(pollScheduler.get(), is(nullValue()));
+
+      schedulerMessageSource.stop();
+      schedulerMessageSource.dispose();
+
+      assertThat(pollScheduler.get(), is(nullValue()));
+    } finally {
+      setProperty(MULE_DISABLE_SCHEDULERS, wasDisabled != null ? wasDisabled : "false");
+    }
   }
 
   private DefaultSchedulerMessageSource schedulerMessageSource;
@@ -159,4 +205,37 @@ public class DefaultSchedulerMessageSourceTestCase extends AbstractMuleContextTe
     return factory;
   }
 
+  private void pollAndAssertWith(Consumer<SensingNullMessageProcessor> validate) throws Exception {
+    DefaultSchedulerMessageSource schedulerMessageSource = createMessageSource();
+
+    SensingNullMessageProcessor flow = getSensingNullMessageProcessor();
+    schedulerMessageSource.setListener(flow);
+    schedulerMessageSource.setAnnotations(singletonMap(LOCATION_KEY, TEST_CONNECTOR_LOCATION));
+
+    doAnswer(invocationOnMock -> {
+      CoreEvent inputEvent = invocationOnMock.getArgument(0);
+      flow.process(inputEvent);
+      return null;
+    }).when(sourcePolicy).process(any(CoreEvent.class), any(), any());
+
+    schedulerMessageSource.trigger();
+
+    // let some time before making the assertion to avoid assert before the scheduler start
+    MILLISECONDS.sleep(100);
+
+    validate.accept(flow);
+  }
+
+  public SchedulerService mockSchedulerService(AtomicReference<Scheduler> pollScheduler) throws Exception {
+    SchedulerService schedulerService = muleContext.getSchedulerService();
+    reset(schedulerService);
+
+    doAnswer(invocation -> {
+      Scheduler scheduler = (Scheduler) invocation.callRealMethod();
+      pollScheduler.set(scheduler);
+      return scheduler;
+    }).when(schedulerService).cpuLightScheduler();
+
+    return schedulerService;
+  }
 }
--- core/src/main/java/org/mule/runtime/core/internal/source/scheduler/DefaultSchedulerMessageSource.java
+++ core/src/main/java/org/mule/runtime/core/internal/source/scheduler/DefaultSchedulerMessageSource.java
@@ -6,8 +6,11 @@
  */
 package org.mule.runtime.core.internal.source.scheduler;
 
+import static java.lang.Boolean.parseBoolean;
+import static java.lang.System.getProperty;
 import static java.util.Collections.emptyList;
 import static java.util.Optional.empty;
+import static org.mule.runtime.api.util.MuleSystemProperties.MULE_DISABLE_SCHEDULERS;
 import static org.mule.runtime.core.api.config.i18n.CoreMessages.failedToScheduleWork;
 import static org.mule.runtime.core.api.source.MessageSource.BackPressureStrategy.FAIL;
 import static org.mule.runtime.core.api.util.ClassUtils.withContextClassLoader;
@@ -63,6 +66,7 @@ public class DefaultSchedulerMessageSource extends AbstractComponent
 
   private final PeriodicScheduler scheduler;
   private final boolean disallowConcurrentExecution;
+  private final boolean disabled;
 
   private Scheduler pollingExecutor;
   private ScheduledFuture<?> schedulingJob;
@@ -93,11 +97,12 @@ public class DefaultSchedulerMessageSource extends AbstractComponent
     this.muleContext = muleContext;
     this.scheduler = scheduler;
     this.disallowConcurrentExecution = disallowConcurrentExecution;
+    this.disabled = parseBoolean(getProperty(MULE_DISABLE_SCHEDULERS));
   }
 
   @Override
   public synchronized void start() throws MuleException {
-    if (started) {
+    if (started || disabled) {
       return;
     }
     try {
@@ -126,7 +131,9 @@ public class DefaultSchedulerMessageSource extends AbstractComponent
 
   @Override
   public void trigger() {
-    pollingExecutor.execute(() -> withContextClassLoader(muleContext.getExecutionClassLoader(), () -> poll()));
+    if (!disabled) {
+      pollingExecutor.execute(() -> withContextClassLoader(muleContext.getExecutionClassLoader(), () -> poll()));
+    }
   }
 
   @Override
@@ -200,6 +207,9 @@ public class DefaultSchedulerMessageSource extends AbstractComponent
    */
   @Override
   public void initialise() throws InitialisationException {
+    if (disabled) {
+      return;
+    }
     getFromAnnotatedObject(componentLocator, this)
         .ifPresent(flow -> this.flowConstruct = flow);
 
