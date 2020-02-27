36c6ee5a62b5c6f9b930ac249d2f1faef3b5fc2f
ndinu <ndinu@users.noreply.github.com>
2020-02-27 10:06:42 -0300

    MULE-18094: Pooled Platform Managed Connection Providers fail to be created (#8668)

---
+++ tests/test-extensions/test-oauth-extension/src/main/java/org/mule/test/oauth/WithPooledProviderConfig.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.oauth;
+
+import org.mule.runtime.extension.api.annotation.Configuration;
+import org.mule.runtime.extension.api.annotation.Operations;
+import org.mule.runtime.extension.api.annotation.connectivity.ConnectionProviders;
+
+@ConnectionProviders(TestOAuthPooledProvider.class)
+@Operations(TestOAuthOperations.class)
+@Configuration(name = "pooled-provider")
+public class WithPooledProviderConfig {
+
+}
