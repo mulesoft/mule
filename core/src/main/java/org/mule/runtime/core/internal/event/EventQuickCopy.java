5453af54b441cbeeae92a85eed1db918c32d2a5c
Juli <luzzijulieta@gmail.com>
2020-03-03 17:45:47 -0300

    MULE-16528: remove  in foreach

--- core/src/main/java/org/mule/runtime/core/internal/event/EventQuickCopy.java
+++ core/src/main/java/org/mule/runtime/core/internal/event/EventQuickCopy.java
@@ -46,11 +46,9 @@ public final class EventQuickCopy {
         && ((EventQuickCopyContextDecorator) event).getEvent().getContext() == context) {
       return ((EventQuickCopyContextDecorator) event).getEvent();
     } else {
-      if (event instanceof InternalEvent && context instanceof BaseEventContext) {
-        return new EventQuickCopyContextDecorator((BaseEventContext) context, (InternalEvent) event);
-      } else {
-        return CoreEvent.builder(context, event).build();
-      }
+      return (event instanceof InternalEvent && context instanceof BaseEventContext)
+          ? new EventQuickCopyContextDecorator((BaseEventContext) context, (InternalEvent) event)
+          : CoreEvent.builder(context, event).build();
     }
   }
 
