857ef0b49e36833508ce01914f90fb3d7f8cd10f
Rodro <rodrigo.merino@mulesoft.com>
2020-03-05 16:49:50 -0300

    review

--- core/src/main/java/org/mule/runtime/core/internal/exception/ExceptionRouter.java
+++ core/src/main/java/org/mule/runtime/core/internal/exception/ExceptionRouter.java
@@ -13,14 +13,11 @@ import java.util.function.Consumer;
 
 /**
  * Refer to {@link FlowExceptionHandler#router(java.util.function.Function, Consumer, Consumer)}.
+ * <p>
+ * Also implements {@link Disposable} to allow shutdown of any child objects.
  *
  * @since 4.3
  */
 public interface ExceptionRouter extends Consumer<Exception>, Disposable {
 
-  @Override
-  void accept(Exception t);
-
-  @Override
-  void dispose();
 }
