0626aef6d8f79c307a8f9e1c6fd37ccb2bf9579a
Rodrigo Merino <elrodro83@users.noreply.github.com>
2020-03-06 16:59:19 -0300

    MULE-18155: Dynamic flow-ref must explicitly expect a String on the expressionLanguage (#8716)

--- modules/spring-config/src/main/java/org/mule/runtime/config/internal/factories/FlowRefFactoryBean.java
+++ modules/spring-config/src/main/java/org/mule/runtime/config/internal/factories/FlowRefFactoryBean.java
@@ -11,7 +11,9 @@ import static java.util.Collections.singletonMap;
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
 import static java.util.Optional.ofNullable;
+import static org.mule.runtime.api.el.BindingContextUtils.NULL_BINDING_CONTEXT;
 import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
+import static org.mule.runtime.api.metadata.DataType.STRING;
 import static org.mule.runtime.core.api.config.DefaultMuleConfiguration.isFlowTrace;
 import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.disposeIfNeeded;
 import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.initialiseIfNeeded;
@@ -474,7 +476,7 @@ public class FlowRefFactoryBean extends AbstractComponentFactory<Processor> impl
      * @throws MuleException
      */
     protected Processor resolveTargetFlowOrSubflow(CoreEvent event) throws MuleException {
-      String targetFlowName = (String) expressionManager.evaluate(refName, event, getLocation()).getValue();
+      String targetFlowName = (String) expressionManager.evaluate(refName, STRING, NULL_BINDING_CONTEXT, event, getLocation(), true).getValue();
       try {
         return targetsCache.getUnchecked(targetFlowName);
 
