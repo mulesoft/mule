3346d110ccb09d117f3fb6b4b614e092dd63bd8b
Gabriel Dal Borgo <gabrieldalborgo@gmail.com>
2020-03-11 16:30:19 -0300

    Add system ppropery for platform auth url suffix

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/connectivity/oauth/ocs/PlatformManagedOAuthConfig.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/connectivity/oauth/ocs/PlatformManagedOAuthConfig.java
@@ -11,11 +11,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Collections.emptyMap;
 import static java.util.Optional.empty;
 import static org.mule.runtime.api.util.MultiMap.emptyMultiMap;
-import static org.mule.runtime.extension.internal.ocs.OCSConstants.OCS_CLIENT_ID;
-import static org.mule.runtime.extension.internal.ocs.OCSConstants.OCS_CLIENT_SECRET;
-import static org.mule.runtime.extension.internal.ocs.OCSConstants.OCS_ORG_ID;
-import static org.mule.runtime.extension.internal.ocs.OCSConstants.OCS_PLATFORM_AUTH_URL;
-import static org.mule.runtime.extension.internal.ocs.OCSConstants.OCS_SERVICE_URL;
+import static org.mule.runtime.extension.internal.ocs.OCSConstants.*;
 
 import org.mule.runtime.api.component.ConfigurationProperties;
 import org.mule.runtime.api.meta.model.ExtensionModel;
@@ -32,6 +28,8 @@ import java.nio.charset.Charset;
  * @since 4.3.0
  */
 public class PlatformManagedOAuthConfig extends OAuthConfig<PlatformManagedOAuthGrantType> {
+
+  private static final String DEFAULT_PLATFORM_AUTH_URL_SUFFIX = "/oauth2/token";
 
   private final String connectionUri;
   private final String serviceUrl;
@@ -55,7 +53,10 @@ public class PlatformManagedOAuthConfig extends OAuthConfig<PlatformManagedOAuth
     return new PlatformManagedOAuthConfig(ownerConfigName,
                                           connectionUri,
                                           getProperty(configurationProperties, OCS_SERVICE_URL),
-                                          getProperty(configurationProperties, OCS_PLATFORM_AUTH_URL),
+                                          resolvePlatformAuthUrl(getProperty(configurationProperties, OCS_PLATFORM_AUTH_URL),
+                                                                 getProperty(configurationProperties,
+                                                                             OCS_PLATFORM_AUTH_URL_SUFFIX,
+                                                                             DEFAULT_PLATFORM_AUTH_URL_SUFFIX)),
                                           getProperty(configurationProperties, OCS_CLIENT_ID),
                                           getProperty(configurationProperties, OCS_CLIENT_SECRET),
                                           getProperty(configurationProperties, OCS_ORG_ID),
@@ -69,6 +70,14 @@ public class PlatformManagedOAuthConfig extends OAuthConfig<PlatformManagedOAuth
   private static String getProperty(ConfigurationProperties configurationProperties, String key) {
     return configurationProperties.resolveStringProperty(key)
         .orElseThrow(() -> new IllegalArgumentException(format("OCS property '%s' has not been set", key)));
+  }
+
+  private static String getProperty(ConfigurationProperties configurationProperties, String key, String def) {
+    return configurationProperties.resolveStringProperty(key).orElse(def);
+  }
+
+  private static String resolvePlatformAuthUrl(String url, String suffix) {
+    return sanitizeUrl(url) + suffix;
   }
 
   public PlatformManagedOAuthConfig(String ownerConfigName,
@@ -142,4 +151,13 @@ public class PlatformManagedOAuthConfig extends OAuthConfig<PlatformManagedOAuth
   public ExtensionModel getExtensionModel() {
     return extensionModel;
   }
+
+  private static String sanitizeUrl(String url) {
+    if (url.endsWith("/")) {
+      url = url.substring(0, url.length() - 1);
+    }
+
+    return url;
+  }
+
 }
