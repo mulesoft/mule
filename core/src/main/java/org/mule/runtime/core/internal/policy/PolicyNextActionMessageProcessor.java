d2ab426710fd6cd8b7ab7d6c30feb371890cff77
Federico Balbi <balbifm@gmail.com>
2020-03-12 13:59:47 -0300

    MULE-18162: Enhance policy trace logger (#8719)

--- core/src/main/java/org/mule/runtime/core/internal/policy/PolicyNextActionMessageProcessor.java
+++ core/src/main/java/org/mule/runtime/core/internal/policy/PolicyNextActionMessageProcessor.java
@@ -13,17 +13,14 @@ import static org.mule.runtime.api.notification.PolicyNotification.BEFORE_NEXT;
 import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.initialiseIfNeeded;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.buildNewChainWithListOfProcessors;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.processToApply;
-import static org.slf4j.LoggerFactory.getLogger;
 import static reactor.core.publisher.Flux.from;
 import static reactor.core.publisher.Mono.subscriberContext;
 
 import org.mule.runtime.api.component.AbstractComponent;
 import org.mule.runtime.api.component.location.ComponentLocation;
-import org.mule.runtime.api.event.EventContext;
 import org.mule.runtime.api.exception.MuleException;
 import org.mule.runtime.api.lifecycle.Initialisable;
 import org.mule.runtime.api.lifecycle.InitialisationException;
-import org.mule.runtime.api.message.Message;
 import org.mule.runtime.core.api.MuleContext;
 import org.mule.runtime.core.api.context.notification.FlowStackElement;
 import org.mule.runtime.core.api.context.notification.ServerNotificationHandler;
@@ -40,7 +37,6 @@ import java.util.function.Consumer;
 import javax.inject.Inject;
 
 import org.reactivestreams.Publisher;
-import org.slf4j.Logger;
 
 /**
  * Next-operation message processor implementation.
@@ -54,8 +50,6 @@ public class PolicyNextActionMessageProcessor extends AbstractComponent implemen
 
   private static final String SOURCE_POLICY_PART_IDENTIFIER = "source";
 
-  private static final Logger LOGGER = getLogger(PolicyNextActionMessageProcessor.class);
-
   public static final String POLICY_NEXT_OPERATION = "policy.nextOperation";
   public static final String POLICY_IS_PROPAGATE_MESSAGE_TRANSFORMATIONS = "policy.isPropagateMessageTransformations";
 
@@ -67,6 +61,7 @@ public class PolicyNextActionMessageProcessor extends AbstractComponent implemen
 
   private PolicyNotificationHelper notificationHelper;
   private PolicyEventMapper policyEventMapper;
+  private PolicyTraceLogger policyTraceLogger = new PolicyTraceLogger();
 
   private OnExecuteNextErrorConsumer onExecuteNextErrorConsumer;
 
@@ -136,8 +131,7 @@ public class PolicyNextActionMessageProcessor extends AbstractComponent implemen
   @Override
   public Publisher<CoreEvent> apply(Publisher<CoreEvent> publisher) {
     return from(publisher)
-        .doOnNext(coreEvent -> logExecuteNextEvent("Before execute-next", coreEvent.getContext(),
-                                                   coreEvent.getMessage(), getPolicyId()))
+        .doOnNext(event -> policyTraceLogger.logBeforeExecuteNext(getPolicyId(), event))
         .doOnNext(event -> {
           popBeforeNextFlowFlowStackElement().accept(event);
           notificationHelper.notification(BEFORE_NEXT).accept(event);
@@ -146,10 +140,9 @@ public class PolicyNextActionMessageProcessor extends AbstractComponent implemen
         .doOnNext(coreEvent -> {
           notificationHelper.fireNotification(coreEvent, null, AFTER_NEXT);
           pushAfterNextFlowStackElement().accept(coreEvent);
-          logExecuteNextEvent("After execute-next", coreEvent.getContext(), coreEvent.getMessage(),
-                              getPolicyId());
         })
-        .map(policyEventMapper::fromPolicyNext);
+        .map(policyEventMapper::fromPolicyNext)
+        .doOnNext(event -> policyTraceLogger.logAfterExecuteNext(getPolicyId(), event));
   }
 
   private BaseExceptionHandler policyNextErrorHandler() {
@@ -185,10 +178,4 @@ public class PolicyNextActionMessageProcessor extends AbstractComponent implemen
     return muleContext.getConfiguration().getId();
   }
 
-  private static void logExecuteNextEvent(String startingMessage, EventContext eventContext, Message message, String policyName) {
-    if (LOGGER.isTraceEnabled()) {
-      LOGGER.trace("\nEvent Id: " + eventContext.getCorrelationId() + "\n" + startingMessage + ".\nPolicy: " + policyName
-          + "\n" + message.getAttributes().getValue().toString());
-    }
-  }
 }
