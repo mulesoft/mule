e3d4ad8f9917d6b1b9f29ef231cc48a6adb3fe15
Luciano RaineriMarchina <lucianoRM@users.noreply.github.com>
2020-04-08 23:25:16 -0300

    MULE-18281: ArtifactAstDependencyGraph is not taking configRef parameters into account (#8791)
    (cherry picked from commit 5bbd0435fac49eecca5f24974b03415560adf6b2)
    (cherry picked from commit b143a6fe7de5154df19c75195e44a42a84d05aef)

--- modules/spring-config/src/main/java/org/mule/runtime/config/internal/LazyMuleArtifactContext.java
+++ modules/spring-config/src/main/java/org/mule/runtime/config/internal/LazyMuleArtifactContext.java
@@ -164,8 +164,6 @@ public class LazyMuleArtifactContext extends MuleArtifactContext
     // Changes the component locator in order to allow accessing any component by location even when they are prototype
     this.componentLocator = new SpringConfigurationComponentLocator();
 
-    graph = generateFor(applicationModel);
-
     this.parentComponentModelInitializer = parentComponentModelInitializer;
 
     final CustomizationService customizationService = muleContext.getCustomizationService();
@@ -225,6 +223,8 @@ public class LazyMuleArtifactContext extends MuleArtifactContext
     }
 
     initialize();
+    //Graph should be generated after the initialize() method since the applicationModel will change by macro expanding XmlSdk components.
+    this.graph = generateFor(applicationModel);
   }
 
   @Override
