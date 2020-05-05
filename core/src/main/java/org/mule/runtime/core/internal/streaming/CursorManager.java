c8a211829675513b83506586846925103a1048e9
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2020-05-05 12:09:52 -0300

    MULE-18370: Key collision at ManagedCursorProvider cache (#8842)
    (cherry picked from commit e4fbfe44f2625b01e01946d9db9af1d3243694d0)

--- core/src/main/java/org/mule/runtime/core/internal/streaming/CursorManager.java
+++ core/src/main/java/org/mule/runtime/core/internal/streaming/CursorManager.java
@@ -6,13 +6,15 @@
  */
 package org.mule.runtime.core.internal.streaming;
 
-import static java.lang.System.identityHashCode;
 import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
+import static org.mule.runtime.core.internal.streaming.CursorUtils.unwrap;
 
 import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.api.streaming.Cursor;
 import org.mule.runtime.api.streaming.CursorProvider;
+import org.mule.runtime.api.streaming.bytes.CursorStream;
 import org.mule.runtime.api.streaming.bytes.CursorStreamProvider;
+import org.mule.runtime.api.streaming.object.CursorIterator;
 import org.mule.runtime.api.streaming.object.CursorIteratorProvider;
 import org.mule.runtime.core.internal.streaming.bytes.ManagedCursorStreamProvider;
 import org.mule.runtime.core.internal.streaming.object.ManagedCursorIteratorProvider;
@@ -61,14 +63,17 @@ public class CursorManager {
    * @param ownerContext the root context of the event that created the provider
    * @return a {@link CursorProvider}
    */
-  public CursorProvider manage(CursorProvider provider, BaseEventContext ownerContext) {
+  public CursorProvider manage(final CursorProvider provider, BaseEventContext ownerContext) {
+    CursorProvider innerDelegate = unwrap(provider);
+    IdentifiableCursorProvider identifiable = IdentifiableCursorProviderDecorator.of(provider);
+
     ManagedCursorProvider managedProvider;
-    if (provider instanceof CursorStreamProvider) {
-      managedProvider = new ManagedCursorStreamProvider(provider, statistics);
-    } else if (provider instanceof CursorIteratorProvider) {
-      managedProvider = new ManagedCursorIteratorProvider(provider, statistics);
+    if (innerDelegate instanceof CursorStreamProvider) {
+      managedProvider = new ManagedCursorStreamProvider((IdentifiableCursorProvider<CursorStream>) identifiable, statistics);
+    } else if (innerDelegate instanceof CursorIteratorProvider) {
+      managedProvider = new ManagedCursorIteratorProvider((IdentifiableCursorProvider<CursorIterator>) identifiable, statistics);
     } else {
-      throw new MuleRuntimeException(createStaticMessage("Unknown cursor provider type: " + provider.getClass().getName()));
+      throw new MuleRuntimeException(createStaticMessage("Unknown cursor provider type: " + innerDelegate.getClass().getName()));
     }
 
     return registry.get(ownerContext).addProvider(managedProvider);
@@ -88,19 +93,19 @@ public class CursorManager {
     private final Cache<Integer, WeakReference<ManagedCursorProvider>> providers = Caffeine.newBuilder().build();
 
     private ManagedCursorProvider addProvider(ManagedCursorProvider provider) {
-      final int hash = identityHashCode(provider.getDelegate());
-      ManagedCursorProvider managedProvider = getOrAddManagedProvider(provider, hash);
+      final int id = provider.getId();
+      ManagedCursorProvider managedProvider = getOrAddManagedProvider(provider, id);
 
       // This can happen when a foreach component splits a text document using a stream.
       // Iteration N might try to manage the same root provider that was already managed in iteration N-1, but the
       // managed decorator from that previous iteration has been collected, which causes the weak reference to yield
       // a null value. In which case we simply track it again.
       if (managedProvider == null) {
-        synchronized (provider.getDelegate()) {
-          managedProvider = getOrAddManagedProvider(provider, hash);
+        synchronized (unwrap(provider)) {
+          managedProvider = getOrAddManagedProvider(provider, id);
           if (managedProvider == null) {
-            providers.invalidate(hash);
-            managedProvider = getOrAddManagedProvider(provider, hash);
+            providers.invalidate(id);
+            managedProvider = getOrAddManagedProvider(provider, id);
           }
         }
       }
---
+++ core/src/main/java/org/mule/runtime/core/internal/streaming/IdentifiableCursorProvider.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.internal.streaming;
+
+import org.mule.runtime.api.streaming.Cursor;
+import org.mule.runtime.api.streaming.CursorProvider;
+
+/**
+ * A Cursor provider that contains an ID that can unequivocally identifies it.
+ *
+ * @param <T> the generic {@link Cursor} type as defined in {@link CursorProvider}
+ * @since 4.3.0 - 4.2.3
+ */
+public interface IdentifiableCursorProvider<T extends Cursor> extends CursorProvider<T> {
+
+  /**
+   * @return the provider's id.
+   */
+  int getId();
+}
