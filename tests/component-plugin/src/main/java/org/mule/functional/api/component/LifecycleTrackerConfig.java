82e7041358772e0d906be83b7ea39e030e77cef3
Rodrigo Merino <elrodro83@users.noreply.github.com>
2020-05-19 13:33:25 -0300

    MULE-18417: Connector test failing with java.lang.IllegalStateException (#8868)
    at LazyMuleArtifactContext.initializeComponents

---
+++ tests/component-plugin/src/main/java/org/mule/functional/api/component/LifecycleTrackerConfig.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.functional.api.component;
+
+import org.mule.runtime.api.component.AbstractComponent;
+import org.mule.runtime.api.component.Component;
+import org.mule.runtime.api.exception.MuleException;
+import org.mule.runtime.api.lifecycle.InitialisationException;
+import org.mule.runtime.api.lifecycle.Lifecycle;
+import org.mule.runtime.core.api.MuleContext;
+import org.mule.runtime.core.api.context.MuleContextAware;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LifecycleTrackerConfig extends AbstractComponent implements MuleContextAware, Lifecycle {
+
+  private static List<LifecycleTrackerConfig> configs = new ArrayList<>();
+
+  private final List<Component> nested = new ArrayList<>();
+
+  private final List<String> tracker = new ArrayList<>();
+  private MuleContext muleContext;
+
+
+  public LifecycleTrackerConfig() {
+    configs.add(this);
+  }
+
+  public static List<LifecycleTrackerConfig> getConfigs() {
+    return configs;
+  }
+
+  public List<String> getTracker() {
+    return tracker;
+  }
+
+  public void setProperty(final String value) {
+    getTracker().add("setProperty");
+  }
+
+  public List<Component> getNested() {
+    return new ArrayList<>(nested);
+  }
+
+  public void setNested(List<Component> nested) {
+    this.nested.addAll(nested);
+  }
+
+  @Override
+  public void setMuleContext(final MuleContext context) {
+    if (muleContext == null) {
+      getTracker().add("setMuleContext");
+      this.muleContext = context;
+    }
+  }
+
+  @Override
+  public void initialise() throws InitialisationException {
+    getTracker().add("initialise");
+  }
+
+  @Override
+  public void start() throws MuleException {
+    getTracker().add("start");
+  }
+
+  @Override
+  public void stop() throws MuleException {
+    getTracker().add("stop");
+  }
+
+  @Override
+  public void dispose() {
+    getTracker().add("dispose");
+  }
+
+}
