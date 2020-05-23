1fc594ef9d6b524bdc9905cb21d3aee935e30b11
Rodro <rodrigo.merino@mulesoft.com>
2020-05-23 10:29:05 -0300

    MULE-13618 cleanup

--- modules/spring-config/src/main/java/org/mule/runtime/config/internal/dsl/model/ComponentLocationVisitor.java
+++ modules/spring-config/src/main/java/org/mule/runtime/config/internal/dsl/model/ComponentLocationVisitor.java
@@ -16,22 +16,11 @@ import static org.mule.runtime.api.component.ComponentIdentifier.buildFromString
 import static org.mule.runtime.api.component.TypedComponentIdentifier.builder;
 import static org.mule.runtime.api.component.TypedComponentIdentifier.ComponentType.ROUTE;
 import static org.mule.runtime.api.component.TypedComponentIdentifier.ComponentType.SCOPE;
-import static org.mule.runtime.config.api.dsl.CoreDslConstants.ERROR_HANDLER_IDENTIFIER;
-import static org.mule.runtime.config.api.dsl.CoreDslConstants.FLOW_IDENTIFIER;
-import static org.mule.runtime.config.api.dsl.CoreDslConstants.SUBFLOW_IDENTIFIER;
 import static org.mule.runtime.config.internal.dsl.spring.ComponentModelHelper.isErrorHandler;
 import static org.mule.runtime.config.internal.dsl.spring.ComponentModelHelper.isMessageSource;
 import static org.mule.runtime.config.internal.dsl.spring.ComponentModelHelper.isProcessor;
 import static org.mule.runtime.config.internal.dsl.spring.ComponentModelHelper.isRouter;
 import static org.mule.runtime.config.internal.dsl.spring.ComponentModelHelper.isTemplateOnErrorHandler;
-import static org.mule.runtime.config.internal.model.ApplicationModel.HTTP_PROXY_OPERATION_IDENTIFIER;
-import static org.mule.runtime.config.internal.model.ApplicationModel.HTTP_PROXY_POLICY_IDENTIFIER;
-import static org.mule.runtime.config.internal.model.ApplicationModel.HTTP_PROXY_SOURCE_POLICY_IDENTIFIER;
-import static org.mule.runtime.config.internal.model.ApplicationModel.MUNIT_AFTER_SUITE_IDENTIFIER;
-import static org.mule.runtime.config.internal.model.ApplicationModel.MUNIT_AFTER_TEST_IDENTIFIER;
-import static org.mule.runtime.config.internal.model.ApplicationModel.MUNIT_BEFORE_SUITE_IDENTIFIER;
-import static org.mule.runtime.config.internal.model.ApplicationModel.MUNIT_BEFORE_TEST_IDENTIFIER;
-import static org.mule.runtime.config.internal.model.ApplicationModel.MUNIT_TEST_IDENTIFIER;
 
 import org.mule.runtime.api.component.AbstractComponent;
 import org.mule.runtime.api.component.ComponentIdentifier;
@@ -39,6 +28,7 @@ import org.mule.runtime.api.component.TypedComponentIdentifier;
 import org.mule.runtime.api.meta.model.EnrichableModel;
 import org.mule.runtime.api.meta.model.config.ConfigurationModel;
 import org.mule.runtime.api.meta.model.connection.ConnectionProviderModel;
+import org.mule.runtime.api.meta.model.construct.ConstructModel;
 import org.mule.runtime.api.util.Pair;
 import org.mule.runtime.ast.api.ComponentAst;
 import org.mule.runtime.config.internal.dsl.spring.ComponentModelHelper;
@@ -65,9 +55,6 @@ public class ComponentLocationVisitor implements Consumer<Pair<ComponentAst, Lis
   private static final String PROCESSORS_PART_NAME = "processors";
   private static final String SOURCE_PART_NAME = "source";
   private static final ComponentIdentifier ROUTE_COMPONENT_IDENTIFIER = buildFromStringRepresentation("mule:route");
-  private static final ComponentIdentifier CHOICE_WHEN_COMPONENT_IDENTIFIER = buildFromStringRepresentation("mule:when");
-  private static final ComponentIdentifier CHOICE_OTHERWISE_COMPONENT_IDENTIFIER =
-      buildFromStringRepresentation("mule:otherwise");
   private static final ComponentIdentifier MODULE_BODY_IDENTIFIER =
       ComponentIdentifier.builder().namespace("module").name("body").build();
 
@@ -132,20 +119,8 @@ public class ComponentLocationVisitor implements Consumer<Pair<ComponentAst, Lis
 
       if (locationWithCustomPart.isPresent()) {
         componentLocation = locationWithCustomPart.get();
-      } else if (isHttpProxyPart(componentModel)) {
-        componentLocation =
-            parentComponentLocation.appendLocationPart(componentModel.getIdentifier().getName(), typedComponentIdentifier,
-                                                       componentModel.getMetadata().getFileName(),
-                                                       componentModel.getMetadata().getStartLine(),
-                                                       componentModel.getMetadata().getStartColumn());
       } else if (isRootProcessorScope(parentComponentModel)) {
         componentLocation = processFlowDirectChild(componentModel, hierarchy, parentComponentLocation, typedComponentIdentifier);
-      } else if (isMunitFlowIdentifier(parentComponentModel)) {
-        componentLocation = parentComponentLocation.appendRoutePart()
-            .appendLocationPart(findNonProcessorPath(componentModel, hierarchy), typedComponentIdentifier,
-                                componentModel.getMetadata().getFileName(),
-                                componentModel.getMetadata().getStartLine(),
-                                componentModel.getMetadata().getStartColumn());
       } else if (isErrorHandler(componentModel)) {
         componentLocation = processErrorHandlerComponent(componentModel, parentComponentLocation, typedComponentIdentifier);
       } else if (isTemplateOnErrorHandler(componentModel)) {
@@ -231,29 +206,13 @@ public class ComponentLocationVisitor implements Consumer<Pair<ComponentAst, Lis
   }
 
   private boolean isRoute(ComponentAst componentModel) {
-    return componentModel.getIdentifier().equals(ROUTE_COMPONENT_IDENTIFIER)
-        || componentModel.getIdentifier().equals(CHOICE_WHEN_COMPONENT_IDENTIFIER)
-        || componentModel.getIdentifier().equals(CHOICE_OTHERWISE_COMPONENT_IDENTIFIER)
-        || componentModel.getComponentType().equals(ROUTE);
-  }
-
-  private boolean isHttpProxyPart(ComponentAst componentModel) {
-    return componentModel.getIdentifier().equals(HTTP_PROXY_SOURCE_POLICY_IDENTIFIER)
-        || componentModel.getIdentifier().equals(HTTP_PROXY_OPERATION_IDENTIFIER);
-  }
-
-  private boolean isMunitFlowIdentifier(ComponentAst componentModel) {
-    return componentModel.getIdentifier().equals(MUNIT_TEST_IDENTIFIER);
+    return componentModel.getComponentType().equals(ROUTE);
   }
 
   private boolean isRootProcessorScope(ComponentAst componentModel) {
-    ComponentIdentifier identifier = componentModel.getIdentifier();
-    return identifier.equals(FLOW_IDENTIFIER)
-        || identifier.equals(MUNIT_BEFORE_SUITE_IDENTIFIER)
-        || identifier.equals(SUBFLOW_IDENTIFIER)
-        || identifier.equals(MUNIT_BEFORE_TEST_IDENTIFIER)
-        || identifier.equals(MUNIT_AFTER_SUITE_IDENTIFIER)
-        || identifier.equals(MUNIT_AFTER_TEST_IDENTIFIER);
+    return componentModel.getModel(ConstructModel.class)
+        .map(ConstructModel::allowsTopLevelDeclaration)
+        .orElse(false);
   }
 
   private boolean parentComponentIsRouter(ComponentAst componentModel, List<ComponentAst> hierarchy) {
@@ -340,32 +299,9 @@ public class ComponentLocationVisitor implements Consumer<Pair<ComponentAst, Lis
   }
 
   private boolean existsWithinRootContainer(ComponentAst componentModel, List<ComponentAst> hierarchy) {
-    return existsWithin(hierarchy, FLOW_IDENTIFIER)
-        || existsWithin(hierarchy, MUNIT_TEST_IDENTIFIER)
-        || existsWithin(hierarchy, MUNIT_BEFORE_SUITE_IDENTIFIER)
-        || existsWithin(hierarchy, MUNIT_BEFORE_TEST_IDENTIFIER)
-        || existsWithin(hierarchy, MUNIT_AFTER_SUITE_IDENTIFIER)
-        || existsWithin(hierarchy, MUNIT_AFTER_TEST_IDENTIFIER)
-        || existsWithin(hierarchy, HTTP_PROXY_POLICY_IDENTIFIER)
-        || existsWithinConfig(hierarchy)
-        || existsWithinRootErrorHandler(componentModel, hierarchy)
-        || existsWithinSubflow(hierarchy);
+    return hierarchy.stream()
+        .anyMatch(p -> p.getModel(ConfigurationModel.class).isPresent()
+            || isRootProcessorScope(p));
   }
 
-  private boolean existsWithinConfig(List<ComponentAst> hierarchy) {
-    return hierarchy.stream().anyMatch(p -> p.getModel(ConfigurationModel.class).isPresent());
-  }
-
-  private boolean existsWithinRootErrorHandler(ComponentAst componentAst, List<ComponentAst> hierarchy) {
-    return !hierarchy.isEmpty()
-        && hierarchy.get(0).getIdentifier().equals(ERROR_HANDLER_IDENTIFIER);
-  }
-
-  private boolean existsWithinSubflow(List<ComponentAst> hierarchy) {
-    return existsWithin(hierarchy, SUBFLOW_IDENTIFIER);
-  }
-
-  private boolean existsWithin(List<ComponentAst> hierarchy, ComponentIdentifier componentIdentifier) {
-    return hierarchy.stream().anyMatch(componentModel -> componentModel.getIdentifier().equals(componentIdentifier));
-  }
 }
