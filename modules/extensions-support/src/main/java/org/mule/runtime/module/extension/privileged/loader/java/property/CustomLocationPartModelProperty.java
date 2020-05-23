ac75d54631cd0ce9bfe7ab79298711387e85aab5
Rodro <rodrigo.merino@mulesoft.com>
2020-05-23 10:19:39 -0300

    privileged for use in munit

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/privileged/loader/java/property/CustomLocationPartModelProperty.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/privileged/loader/java/property/CustomLocationPartModelProperty.java
@@ -4,7 +4,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.runtime.module.extension.internal.loader.java.property;
+package org.mule.runtime.module.extension.privileged.loader.java.property;
 
 import org.mule.runtime.api.component.location.ComponentLocation;
 import org.mule.runtime.api.meta.model.ModelProperty;
