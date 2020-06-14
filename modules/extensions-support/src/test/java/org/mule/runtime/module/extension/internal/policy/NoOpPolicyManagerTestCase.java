1b54305031bd8f191acdb176562e7d7421d984fa
Federico Balbi <balbifm@gmail.com>
2020-06-14 14:25:15 -0300

    MULE-18442: Fix NoOpPolicyManager and DefaultPolicyManager returning … (#8913)
    ... different instances of noPolicyOperation

---
+++ modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/policy/NoOpPolicyManagerTestCase.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.module.extension.internal.policy;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mule.runtime.core.internal.policy.DefaultPolicyManager.noPolicyOperation;
+
+import org.mule.runtime.api.component.Component;
+import org.mule.runtime.core.api.event.CoreEvent;
+import org.mule.runtime.core.internal.policy.OperationParametersProcessor;
+import org.mule.runtime.core.internal.policy.OperationPolicy;
+import org.mule.tck.junit4.AbstractMuleTestCase;
+
+import io.qameta.allure.Issue;
+import org.junit.Test;
+
+public class NoOpPolicyManagerTestCase extends AbstractMuleTestCase {
+
+  private NoOpPolicyManager noOpPolicyManager = new NoOpPolicyManager();
+
+  @Test
+  @Issue("MULE-18442")
+  public void noOpPolicyManagerCreatesNoPolicyOperation() {
+    Component operation = mock(Component.class);
+    CoreEvent operationEvent = mock(CoreEvent.class);
+    OperationParametersProcessor operationParameters = mock(OperationParametersProcessor.class);
+
+    OperationPolicy policy = noOpPolicyManager.createOperationPolicy(operation, operationEvent, operationParameters);
+
+    assertThat(policy, is(noPolicyOperation()));
+  }
+}
