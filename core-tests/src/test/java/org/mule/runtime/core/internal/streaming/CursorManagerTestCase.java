313719b8ece7f4d02110faa85576e938858914fd
Mariano Lischetti <lischettim@gmail.com>
2020-06-20 11:34:03 -0300

    MULE-18506: [Rollback] MULE-18370: Key collision at ManagedCursorProvider cache (#8953)
    * MULE-18506: [Rollback] MULE-18370: Key collision at ManagedCursorProvider cache (#8951)
    
    (cherry picked from commit 87341788ed811e550bd2ebfba67438a22449325d)
    
    * MULE-18506
    
    (cherry picked from commit 71708fc4d2be28da30915baff010ae9f327cb31e)

--- core-tests/src/test/java/org/mule/runtime/core/internal/streaming/CursorManagerTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/streaming/CursorManagerTestCase.java
@@ -17,7 +17,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mule.runtime.core.internal.streaming.CursorUtils.unwrap;
 import static org.mule.tck.probe.PollingProber.check;
 
 import org.mule.runtime.api.streaming.CursorProvider;
@@ -37,7 +36,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -50,9 +48,6 @@ public class CursorManagerTestCase extends AbstractMuleTestCase {
 
   @Mock
   private StreamingGhostBuster ghostBuster;
-
-  @Mock
-  private BaseEventContext ctx;
 
   private CursorManager cursorManager;
   private ExecutorService executorService;
@@ -78,7 +73,7 @@ public class CursorManagerTestCase extends AbstractMuleTestCase {
     executorService = Executors.newFixedThreadPool(threadCount);
 
     final CursorProvider cursorProvider = mock(CursorStreamProvider.class);
-    IdentifiableCursorProvider identifiableCursorProvider = IdentifiableCursorProviderDecorator.of(cursorProvider);
+    final BaseEventContext ctx = mock(BaseEventContext.class);
 
     for (int i = 0; i < threadCount; i++) {
       executorService.submit(() -> {
@@ -87,7 +82,7 @@ public class CursorManagerTestCase extends AbstractMuleTestCase {
         } catch (InterruptedException e) {
           // doesn't matter
         }
-        CursorProvider managed = cursorManager.manage(identifiableCursorProvider, ctx);
+        CursorProvider managed = cursorManager.manage(cursorProvider, ctx);
         synchronized (managedProviders) {
           managedProviders.add(managed);
         }
@@ -100,24 +95,9 @@ public class CursorManagerTestCase extends AbstractMuleTestCase {
     assertThat(managedProviders.get(0), is(instanceOf(ManagedCursorProvider.class)));
 
     ManagedCursorProvider managedProvider = (ManagedCursorProvider) managedProviders.get(0);
-    assertThat(unwrap(managedProvider), is(sameInstance(cursorProvider)));
+    assertThat(managedProvider.getDelegate(), is(sameInstance(cursorProvider)));
     assertThat(managedProviders.stream().allMatch(p -> p == managedProvider), is(true));
 
     verify(ghostBuster).track(managedProvider);
-  }
-
-  @Test
-  public void remanageCollectedDecorator() {
-    CursorStreamProvider provider = mock(CursorStreamProvider.class);
-    when(ghostBuster.track(any())).thenReturn(new WeakReference<>(null));
-
-    cursorManager.manage(provider, ctx);
-
-    ArgumentCaptor<ManagedCursorProvider> managedDecoratorCaptor = forClass(ManagedCursorProvider.class);
-    verify(ghostBuster, times(2)).track(managedDecoratorCaptor.capture());
-
-    List<ManagedCursorProvider> captured = managedDecoratorCaptor.getAllValues();
-    assertThat(captured, hasSize(2));
-    assertThat(captured.get(0), is(sameInstance(captured.get(1))));
   }
 }
