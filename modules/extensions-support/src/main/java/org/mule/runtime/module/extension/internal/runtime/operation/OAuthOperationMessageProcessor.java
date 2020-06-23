c9a653207473e8250b85e8496f11af96dfd9bad3
Sebastián Zaffarano <szaffarano@mulesoft.com>
2020-06-23 16:29:32 -0300

    MULE-18375: apply changes in OauthOperationMessageProcessor (#8970)
    (cherry picked from commit 11c64dc7ace20dafcbcf2116f542977d6f6810a8)
    (cherry picked from commit 1c067b98965cf7a7881759a50566610e8bbb3748)

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/operation/OAuthOperationMessageProcessor.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/operation/OAuthOperationMessageProcessor.java
@@ -71,8 +71,9 @@ public class OAuthOperationMessageProcessor extends OperationMessageProcessor {
   }
 
   @Override
-  protected Mono<CoreEvent> doProcess(CoreEvent event, ExecutionContextAdapter<OperationModel> operationContext) {
-    return super.doProcess(event, operationContext)
+  protected Mono<CoreEvent> doProcess(CoreEvent event, ExecutionContextAdapter<OperationModel> operationContext,
+                                      ReturnDelegate returnDelegate) {
+    return super.doProcess(event, operationContext, returnDelegate)
         .onErrorResume(AccessTokenExpiredException.class, e -> {
           OAuthConnectionProviderWrapper connectionProvider = getOAuthConnectionProvider(operationContext);
           if (connectionProvider == null) {
@@ -110,7 +111,7 @@ public class OAuthOperationMessageProcessor extends OperationMessageProcessor {
                                   connectionProvider.getResourceOwnerId(), operationContext.getConfiguration().get().getName()));
             }
 
-            return super.doProcess(event, operationContext);
+            return super.doProcess(event, operationContext, returnDelegate);
           } else {
             return error(e);
           }
