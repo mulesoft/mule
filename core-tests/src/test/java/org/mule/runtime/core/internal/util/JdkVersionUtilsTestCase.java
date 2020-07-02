c0e4a096108512b5542f8242b84dc117b98bd67a
Julieta <luzzijulieta@gmail.com>
2020-07-02 12:22:40 -0300

    MULE-18111: Add information about the last commit included in the distro build (#8816)
    MULE-18111: Add information about the last commit included in the distro build

--- core-tests/src/test/java/org/mule/runtime/core/internal/util/JdkVersionUtilsTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/util/JdkVersionUtilsTestCase.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
 import org.mule.runtime.core.api.config.MuleManifest;
 import org.mule.runtime.core.internal.util.JdkVersionUtils.JdkVersion;
 import org.mule.tck.junit4.AbstractMuleTestCase;
