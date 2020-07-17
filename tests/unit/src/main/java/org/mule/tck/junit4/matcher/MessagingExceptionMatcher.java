efe1b4aef4aa69f1a58c643f25b449ae39b07385
Sebastián Zaffarano <szaffarano@mulesoft.com>
2020-07-17 16:23:29 -0300

    MULE-18549: APIKit 1.3.13 (latest) tests time out with 4.3.0
    (cherry picked from commit b2304c8eeed5746c7ef36d03923852298d7b20ad)

--- tests/unit/src/main/java/org/mule/tck/junit4/matcher/MessagingExceptionMatcher.java
+++ tests/unit/src/main/java/org/mule/tck/junit4/matcher/MessagingExceptionMatcher.java
@@ -6,6 +6,7 @@
  */
 package org.mule.tck.junit4.matcher;
 
+import org.mule.runtime.api.component.Component;
 import org.mule.runtime.core.internal.exception.MessagingException;
 
 import org.hamcrest.Description;
@@ -20,24 +21,36 @@ import org.hamcrest.TypeSafeMatcher;
 public class MessagingExceptionMatcher extends TypeSafeMatcher<MessagingException> {
 
   private EventMatcher eventMatcher;
+  private Matcher<Component> componentMatcher;
 
-  public MessagingExceptionMatcher(EventMatcher eventMatcher) {
+  public MessagingExceptionMatcher(EventMatcher eventMatcher, Matcher<Component> componentMatcher) {
     this.eventMatcher = eventMatcher;
+    this.componentMatcher = componentMatcher;
   }
 
   public static MessagingExceptionMatcher withEventThat(EventMatcher eventMatcher) {
-    return new MessagingExceptionMatcher(eventMatcher);
+    return new MessagingExceptionMatcher(eventMatcher, null);
+  }
+
+  public static MessagingExceptionMatcher withFailingComponent(Matcher<Component> componentMatcher) {
+    return new MessagingExceptionMatcher(null, componentMatcher);
   }
 
   @Override
   protected boolean matchesSafely(MessagingException item) {
-    return eventMatcher.matches(item.getEvent());
+    return (eventMatcher != null ? eventMatcher.matches(item.getEvent()) : true)
+        && (componentMatcher != null ? componentMatcher.matches(item.getFailingComponent()) : true);
   }
 
   @Override
   public void describeTo(Description description) {
     description.appendText("a MessagingException with ");
-    eventMatcher.describeTo(description);
+    if (eventMatcher != null) {
+      eventMatcher.describeTo(description);
+    }
+    if (componentMatcher != null) {
+      componentMatcher.describeTo(description);
+    }
   }
 
 }
