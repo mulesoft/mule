66250b6c84d6f8deee044db315e02a4c4b9b28bf
Euge <emariotti3@users.noreply.github.com>
2020-08-04 17:37:32 -0300

    MULE-18542: Timer threads leak on redeploy when using Oracle Driver. (#9000) (#9163)
    (cherry picked from commit 40bc94b5fa43eca3ddb3205c61e5c6d2c8228dac)

---
+++ modules/deployment/src/test/resources/org/foo/oracle/OracleExtension.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.foo.oracle;
+
+import org.mule.runtime.extension.api.annotation.Extension;
+import org.mule.runtime.extension.api.annotation.Operations;
+import org.mule.runtime.extension.api.annotation.param.Parameter;
+
+import javax.inject.Inject;
+
+/**
+ * Extension for testing purposes
+ */
+@Extension(name = "Oracle")
+@Operations({OracleOperation.class})
+public class OracleExtension {
+
+    @Parameter
+    private String url;
+    @Parameter
+    private String user;
+    @Parameter
+    private String password;
+
+
+    public OracleExtension() {}
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public String getUser() {
+        return this.user;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
