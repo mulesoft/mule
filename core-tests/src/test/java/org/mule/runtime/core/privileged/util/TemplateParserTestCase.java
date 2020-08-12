6d3d3d8f7ca2f992dcfe816d8ad23ed7941c96ca
Martín Buchwald <martin.buchwald@mulesoft.com>
2020-08-12 18:21:02 -0300

    Revert "MULE-18421: MULE-15775 - fix case with contiguos # characters (#8872)"
    This reverts commit 7963d9198ca65d9b1076288fc3348eaf6e91d6c0.

--- core-tests/src/test/java/org/mule/runtime/core/privileged/util/TemplateParserTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/privileged/util/TemplateParserTestCase.java
@@ -440,16 +440,6 @@ public class TemplateParserTestCase extends AbstractMuleTestCase {
   }
 
   @Test
-  public void muleParseConsecutiveSharpsEvaluated() {
-    TemplateParser tp = createMuleStyleParser();
-    final String expectedResult = "##[muleman] # ###[value]";
-    String expression = "#[payload]";
-    assertTrue(tp.isValid(expression));
-    String result = tp.parse(null, expression, token -> expectedResult);
-    assertEquals(expectedResult, result);
-  }
-
-  @Test
   public void muleParseMultiLine() {
     TemplateParser tp = createMuleStyleParser();
     final String expectedResult = "{\n" +
