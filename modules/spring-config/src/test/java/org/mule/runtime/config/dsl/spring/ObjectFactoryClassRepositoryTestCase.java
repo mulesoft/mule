326d82c8fde7ef4166a414088189bc43eb779dd4
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2020-08-24 08:49:05 -0300

    MULE-18390: OutOfMemory while running MUnit tests (#8846) (#9240)
    (cherry picked from commit b843cbd3a2261257248a73e063337262b01697e4)
    (cherry picked from commit ed7ef654f7b344f8898a0471be4ffcc00d505d22)
    (cherry picked from commit 7000ad70849ee42a89b18e07dfa5f95107459220)
    
    Co-authored-by: Luciano RaineriMarchina <lucianoRM@users.noreply.github.com>

--- modules/spring-config/src/test/java/org/mule/runtime/config/dsl/spring/ObjectFactoryClassRepositoryTestCase.java
+++ modules/spring-config/src/test/java/org/mule/runtime/config/dsl/spring/ObjectFactoryClassRepositoryTestCase.java
@@ -9,52 +9,94 @@ package org.mule.runtime.config.dsl.spring;
 import static java.util.Optional.empty;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 import org.mule.runtime.config.internal.dsl.spring.ObjectFactoryClassRepository;
+import org.mule.runtime.core.internal.util.CompositeClassLoader;
 import org.mule.runtime.dsl.api.component.AbstractComponentFactory;
 import org.mule.runtime.dsl.api.component.ComponentBuildingDefinition;
 import org.mule.runtime.dsl.api.component.ObjectFactory;
 
 import java.lang.reflect.InvocationTargetException;
+import java.net.URLClassLoader;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.SmartFactoryBean;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ObjectFactoryClassRepositoryTestCase {
 
-  @Mock
-  private ComponentBuildingDefinition componentBuildingDefinition;
-
   @Test
   public void cacheEnableForCGLib()
       throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
-    Class<ObjectFactory> objectFactoryClass = getObjectFactoryClass();
+    final ComponentBuildingDefinition componentBuildingDefinition = mock(ComponentBuildingDefinition.class);
+    Class<ObjectFactory> objectFactoryClass =
+        getObjectFactoryClass(componentBuildingDefinition, FakeObjectConnectionProviderObjectFactory.class);
 
     // First we create the objectFactory that will create an Enhancer with the callback registered and using internal cache
     ObjectFactory objectFactory = objectFactoryClass.newInstance();
     assertThat(((SmartFactoryBean) objectFactory).isSingleton(), is(true));
     Class firstProxyGenerated = objectFactory.getClass();
 
-    objectFactoryClass = getObjectFactoryClass();
+    objectFactoryClass = getObjectFactoryClass(componentBuildingDefinition, FakeObjectConnectionProviderObjectFactory.class);
     objectFactory = objectFactoryClass.newInstance();
     assertThat(((SmartFactoryBean) objectFactory).isSingleton(), is(true));
 
     assertThat(firstProxyGenerated, sameInstance(objectFactory.getClass()));
   }
 
-  public Class<ObjectFactory> getObjectFactoryClass() {
+  @Test
+  public void compositeClassLoadersAreCorrectlyCached() throws Exception {
+    final ComponentBuildingDefinition componentBuildingDefinition = mock(ComponentBuildingDefinition.class);
+    ClassLoader classLoader1 = new URLClassLoader(((URLClassLoader) this.getClass().getClassLoader()).getURLs(),
+                                                  this.getClass().getClassLoader().getParent());
+    ClassLoader classLoader2 = new URLClassLoader(((URLClassLoader) this.getClass().getClassLoader()).getURLs(),
+                                                  this.getClass().getClassLoader().getParent());
+
+    Class factoryClass1 = classLoader1.loadClass(FakeObjectConnectionProviderObjectFactory.class.getName());
+    Class factoryClass2 = classLoader2.loadClass(FakeObjectConnectionProviderObjectFactory.class.getName());
+    Class otherFactoryClass1 = classLoader1.loadClass(OtherFakeObjectConnectionProviderObjectFactory.class.getName());
+    Class otherFactoryClass2 = classLoader2.loadClass(OtherFakeObjectConnectionProviderObjectFactory.class.getName());
+
+    Class<ObjectFactory> enhancedFactoryClass1 = getObjectFactoryClass(componentBuildingDefinition, factoryClass1);
+    Class<ObjectFactory> enhancedFactoryClass2 = getObjectFactoryClass(componentBuildingDefinition, factoryClass2);
+    Class<ObjectFactory> enhancedOtherFactoryClass1 = getObjectFactoryClass(componentBuildingDefinition, otherFactoryClass1);
+    Class<ObjectFactory> enhancedOtherFactoryClass2 = getObjectFactoryClass(componentBuildingDefinition, otherFactoryClass2);
+
+    assertThat(enhancedFactoryClass1.getClassLoader(), instanceOf(CompositeClassLoader.class));
+    assertThat(enhancedFactoryClass2.getClassLoader(), instanceOf(CompositeClassLoader.class));
+    assertThat(enhancedOtherFactoryClass1.getClassLoader(), instanceOf(CompositeClassLoader.class));
+    assertThat(enhancedOtherFactoryClass2.getClassLoader(), instanceOf(CompositeClassLoader.class));
+
+    assertThat(enhancedFactoryClass1.getClassLoader(), is(sameInstance(enhancedOtherFactoryClass1.getClassLoader())));
+    assertThat(enhancedFactoryClass2.getClassLoader(), is(sameInstance(enhancedOtherFactoryClass2.getClassLoader())));
+    assertThat(enhancedFactoryClass1.getClassLoader(), is(not(sameInstance(enhancedFactoryClass2.getClassLoader()))));
+    assertThat(enhancedOtherFactoryClass1.getClassLoader(), is(not(sameInstance(enhancedOtherFactoryClass2.getClassLoader()))));
+  }
+
+  public Class<ObjectFactory> getObjectFactoryClass(ComponentBuildingDefinition componentBuildingDefinition,
+                                                    Class factoryType) {
     ObjectFactoryClassRepository objectFactoryClassRepository = new ObjectFactoryClassRepository();
     return objectFactoryClassRepository.getObjectFactoryClass(componentBuildingDefinition,
-                                                              FakeObjectConnectionProviderObjectFactory.class,
+                                                              factoryType,
                                                               FakeObject.class, () -> false, empty());
   }
 
 
   public static class FakeObjectConnectionProviderObjectFactory extends AbstractComponentFactory {
+
+    @Override
+    public Object doGetObject() throws Exception {
+      return new FakeObject();
+    }
+
+  }
+
+  public static class OtherFakeObjectConnectionProviderObjectFactory extends AbstractComponentFactory {
 
     @Override
     public Object doGetObject() throws Exception {
