880e58b3b466fef0ce5717c622fd4eb75777b883
Mariano Lischetti <lischettim@gmail.com>
2020-08-27 10:17:36 -0300

    MULE-18573: CursorProvider key collision at CursorProvider cache (#9271)
    (cherry picked from commit a7fa47e0c1c002b34e16e77f4853c51cdc19609a)

---
+++ core-tests/src/test/java/org/mule/runtime/core/internal/streaming/StreamingGhostBusterTestCase.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.internal.streaming;
+
+import static java.lang.System.gc;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.initialiseIfNeeded;
+import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.startIfNeeded;
+import static org.mule.runtime.core.internal.streaming.IdentifiableCursorProviderDecorator.of;
+import static org.mule.tck.probe.PollingProber.DEFAULT_POLLING_INTERVAL;
+import static org.mule.tck.probe.PollingProber.check;
+import static org.mule.test.allure.AllureConstants.StreamingFeature.STREAMING;
+import static org.mule.test.allure.AllureConstants.StreamingFeature.StreamingStory.STREAM_MANAGEMENT;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Story;
+import org.junit.Test;
+import org.mule.runtime.api.exception.MuleException;
+import org.mule.runtime.api.streaming.bytes.CursorStreamProvider;
+import org.mule.runtime.core.internal.streaming.bytes.ManagedCursorStreamProvider;
+import org.mule.tck.junit4.AbstractMuleContextTestCase;
+
+import java.lang.ref.WeakReference;
+
+@Feature(STREAMING)
+@Story(STREAM_MANAGEMENT)
+public class StreamingGhostBusterTestCase extends AbstractMuleContextTestCase {
+
+  private static final int GC_POLLING_TIMEOUT = 10000;
+
+  private StreamingGhostBuster ghostBuster;
+
+  @Override
+  protected void doSetUp() throws Exception {
+    ghostBuster = new StreamingGhostBuster();
+    initialiseIfNeeded(ghostBuster, true, muleContext);
+    startIfNeeded(ghostBuster);
+  }
+
+  @Override
+  protected void doTearDown() throws MuleException {
+    ghostBuster.stop();
+    ghostBuster.dispose();
+  }
+
+  @Test
+  @Issue("MULE-18573")
+  public void releaseResourcesWhenReferenceIsCollected() {
+    MutableStreamingStatistics statistics = mock(MutableStreamingStatistics.class);
+    CursorStreamProvider provider = mock(CursorStreamProvider.class);
+    ManagedCursorStreamProvider managedCursorProvider = new ManagedCursorStreamProvider(of(provider), statistics);
+
+    WeakReference<ManagedCursorProvider> reference = ghostBuster.track(managedCursorProvider);
+
+    // Force GC collection
+    managedCursorProvider = null;
+
+    check(GC_POLLING_TIMEOUT, DEFAULT_POLLING_INTERVAL, () -> {
+      gc();
+      assertThat(reference.get(), is(nullValue()));
+      verify(provider).releaseResources();
+      return true;
+    });
+  }
+
+}
