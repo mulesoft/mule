ce99ca19f4495ed2ea33716fafac17f7ed06983d
Mariano Lischetti <lischettim@gmail.com>
2020-08-27 10:17:36 -0300

    MULE-18573: CursorProvider key collision at CursorProvider cache (#9271)
    (cherry picked from commit a7fa47e0c1c002b34e16e77f4853c51cdc19609a)

---
+++ core-tests/src/test/java/org/mule/runtime/core/internal/streaming/IdentifiableCursorProviderDecoratorTestCase.java
@@ -0,0 +1,209 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.internal.streaming;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
+import static org.mule.runtime.core.internal.streaming.CursorUtils.unwrap;
+import static org.mule.test.allure.AllureConstants.StreamingFeature.STREAMING;
+import static org.mule.test.allure.AllureConstants.StreamingFeature.StreamingStory.STREAM_MANAGEMENT;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Story;
+import org.mule.runtime.api.streaming.bytes.CursorStreamProvider;
+import org.mule.runtime.api.streaming.object.CursorIteratorProvider;
+import org.mule.tck.junit4.AbstractMuleTestCase;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+@Feature(STREAMING)
+@Story(STREAM_MANAGEMENT)
+public class IdentifiableCursorProviderDecoratorTestCase extends AbstractMuleTestCase {
+
+  private static List<Integer> COLLECTED_IDS;
+  private static AtomicInteger FAKE_ID_GENERATOR = new AtomicInteger(2000);
+
+  @BeforeClass
+  public static void beforeClass() {
+    COLLECTED_IDS = new LinkedList<>();
+  }
+
+  @AfterClass
+  public static void afterClass() {
+    try {
+      Set<Integer> uniqueIds = new HashSet<>(COLLECTED_IDS);
+      assertThat(uniqueIds, hasSize(COLLECTED_IDS.size()));
+    } finally {
+      COLLECTED_IDS = null;
+    }
+  }
+
+  @Test
+  @Issue("MULE-18573")
+  public void decorateSimpleCursorStreamProvider() {
+    CursorStreamProvider provider = mock(CursorStreamProvider.class);
+    IdentifiableCursorProviderDecorator decorator = IdentifiableCursorProviderDecorator.of(provider);
+    assertThat(decorator.getDelegate(), is(sameInstance(provider)));
+    assertThat(decorator, is(instanceOf(CursorStreamProvider.class)));
+    getId(decorator);
+  }
+
+  @Test
+  @Issue("MULE-18573")
+  public void decorateSimpleCursorStreamProviderTwice() {
+    CursorStreamProvider provider = mock(CursorStreamProvider.class);
+    IdentifiableCursorProviderDecorator decorator = IdentifiableCursorProviderDecorator.of(provider);
+    assertThat(decorator.getDelegate(), is(sameInstance(provider)));
+    assertThat(decorator, is(instanceOf(CursorStreamProvider.class)));
+
+    IdentifiableCursorProviderDecorator decoratorTwice = IdentifiableCursorProviderDecorator.of(decorator);
+    assertThat(decoratorTwice.getDelegate(), is(sameInstance(provider)));
+    assertThat(decoratorTwice, is(instanceOf(CursorStreamProvider.class)));
+    assertThat(decoratorTwice, is(sameInstance(decorator)));
+
+    getId(decorator);
+  }
+
+  @Test
+  @Issue("MULE-18573")
+  public void decorateSimpleCursorIteratorProvider() {
+    CursorIteratorProvider provider = mock(CursorIteratorProvider.class);
+    IdentifiableCursorProviderDecorator decorator = IdentifiableCursorProviderDecorator.of(provider);
+    assertThat(decorator.getDelegate(), is(sameInstance(provider)));
+    assertThat(decorator, is(instanceOf(CursorIteratorProvider.class)));
+    getId(decorator);
+  }
+
+  @Test
+  @Issue("MULE-18573")
+  public void decorateSimpleCursorIteratorProviderTwice() {
+    CursorIteratorProvider provider = mock(CursorIteratorProvider.class);
+    IdentifiableCursorProviderDecorator decorator = IdentifiableCursorProviderDecorator.of(provider);
+    assertThat(decorator.getDelegate(), is(sameInstance(provider)));
+    assertThat(decorator, is(instanceOf(CursorIteratorProvider.class)));
+
+    IdentifiableCursorProviderDecorator decoratorTwice = IdentifiableCursorProviderDecorator.of(decorator);
+    assertThat(decoratorTwice.getDelegate(), is(sameInstance(provider)));
+    assertThat(decoratorTwice, is(instanceOf(CursorIteratorProvider.class)));
+    assertThat(decoratorTwice, is(sameInstance(decorator)));
+    getId(decorator);
+  }
+
+  @Test
+  @Issue("MULE-18573")
+  public void decorateIdentifiableCursorStreamProvider() {
+    IdentifiableCursorProvider provider =
+        mock(IdentifiableCursorProvider.class, withSettings().extraInterfaces(CursorStreamProvider.class));
+    final int id = FAKE_ID_GENERATOR.incrementAndGet();
+    when(provider.getId()).thenReturn(id);
+
+    IdentifiableCursorProviderDecorator decorator = IdentifiableCursorProviderDecorator.of(provider);
+    assertThat(decorator.getDelegate(), is(sameInstance(provider)));
+    assertThat(decorator, is(instanceOf(CursorStreamProvider.class)));
+    assertThat(getId(provider), is(id));
+  }
+
+  @Test
+  @Issue("MULE-18573")
+  public void decorateIdentifiableCursorIteratorProvider() {
+    IdentifiableCursorProvider provider =
+        mock(IdentifiableCursorProvider.class, withSettings().extraInterfaces(CursorIteratorProvider.class));
+    final int id = FAKE_ID_GENERATOR.incrementAndGet();
+    when(provider.getId()).thenReturn(id);
+
+    IdentifiableCursorProviderDecorator decorator = IdentifiableCursorProviderDecorator.of(provider);
+    assertThat(decorator.getDelegate(), is(sameInstance(provider)));
+    assertThat(decorator, is(instanceOf(CursorIteratorProvider.class)));
+    assertThat(getId(decorator), is(id));
+  }
+
+  @Test
+  @Issue("MULE-18573")
+  public void doubleDecorateCursorStreamProvider() {
+    CursorStreamProvider provider = mock(CursorStreamProvider.class);
+    CursorProviderDecorator firstDecorator =
+        mock(CursorProviderDecorator.class, withSettings().extraInterfaces(CursorStreamProvider.class));
+    when(firstDecorator.getDelegate()).thenReturn(provider);
+
+    IdentifiableCursorProviderDecorator decorator = IdentifiableCursorProviderDecorator.of(firstDecorator);
+    assertThat(decorator.getDelegate(), is(sameInstance(firstDecorator)));
+    assertThat(decorator, is(instanceOf(CursorStreamProvider.class)));
+    getId(decorator);
+  }
+
+  @Test
+  @Issue("MULE-18573")
+  public void doubleDecorateIdentifiableCursorStreamProvider() {
+    CursorStreamProvider provider = mock(CursorStreamProvider.class);
+    CursorProviderDecorator firstDecorator =
+        mock(CursorProviderDecorator.class,
+             withSettings().extraInterfaces(CursorStreamProvider.class, IdentifiableCursorProvider.class));
+    final int id = FAKE_ID_GENERATOR.incrementAndGet();
+    when(firstDecorator.getDelegate()).thenReturn(provider);
+    when(((IdentifiableCursorProvider) firstDecorator).getId()).thenReturn(id);
+
+    IdentifiableCursorProviderDecorator decorator = IdentifiableCursorProviderDecorator.of(firstDecorator);
+    assertThat(unwrap(decorator), is(sameInstance(provider)));
+    assertThat(decorator, is(instanceOf(CursorStreamProvider.class)));
+    assertThat(getId(decorator), is(id));
+  }
+
+  @Test
+  @Issue("MULE-18573")
+  public void doubleDecorateCursorIteratorProvider() {
+    CursorIteratorProvider provider = mock(CursorIteratorProvider.class);
+    CursorProviderDecorator firstDecorator =
+        mock(CursorProviderDecorator.class, withSettings().extraInterfaces(CursorIteratorProvider.class));
+    when(firstDecorator.getDelegate()).thenReturn(provider);
+
+    IdentifiableCursorProviderDecorator decorator = IdentifiableCursorProviderDecorator.of(firstDecorator);
+    assertThat(decorator.getDelegate(), is(sameInstance(firstDecorator)));
+    assertThat(decorator, is(instanceOf(CursorIteratorProvider.class)));
+    getId(decorator);
+  }
+
+  @Test
+  @Issue("MULE-18573")
+  public void doubleDecorateIdentifiableCursorIteratorProvider() {
+    CursorIteratorProvider provider = mock(CursorIteratorProvider.class);
+    CursorProviderDecorator firstDecorator =
+        mock(CursorProviderDecorator.class,
+             withSettings().extraInterfaces(CursorIteratorProvider.class, IdentifiableCursorProvider.class));
+
+    final int id = FAKE_ID_GENERATOR.incrementAndGet();
+    when(firstDecorator.getDelegate()).thenReturn(provider);
+    when(((IdentifiableCursorProvider) firstDecorator).getId()).thenReturn(id);
+
+    IdentifiableCursorProviderDecorator decorator = IdentifiableCursorProviderDecorator.of(firstDecorator);
+    assertThat(unwrap(decorator), is(sameInstance(provider)));
+    assertThat(decorator, is(instanceOf(CursorIteratorProvider.class)));
+    assertThat(getId(decorator), is(id));
+  }
+
+  private int getId(IdentifiableCursorProvider provider) {
+    int id = provider.getId();
+    COLLECTED_IDS.add(id);
+
+    return id;
+  }
+}
+
---
+++ core/src/main/java/org/mule/runtime/core/internal/streaming/IdentifiableCursorProviderDecorator.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.internal.streaming;
+
+import static java.lang.Integer.MIN_VALUE;
+
+import org.mule.runtime.api.streaming.Cursor;
+import org.mule.runtime.api.streaming.CursorProvider;
+import org.mule.runtime.api.streaming.bytes.CursorStream;
+import org.mule.runtime.api.streaming.bytes.CursorStreamProvider;
+import org.mule.runtime.api.streaming.object.CursorIterator;
+import org.mule.runtime.api.streaming.object.CursorIteratorProvider;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * A decorator that turns any {@link CursorProvider} into an {@link IdentifiableCursorProvider}.
+ * <p>
+ * If the decoratee is already an {@link IdentifiableCursorProvider} or ({@link CursorProviderDecorator} of one), then this
+ * decorator will yield the same id as the input identifiable provider. If this is not the case, an ID will be generaetd.
+ * <p>
+ * Instances are to be created through the {@link #of(CursorProvider)} factory method.
+ *
+ * @param <T> the generic {@link Cursor} type as defined in {@link CursorProvider}
+ * @since 4.4, 4.3.1, 4.2.3
+ */
+public abstract class IdentifiableCursorProviderDecorator<T extends Cursor> extends CursorProviderDecorator<T>
+    implements IdentifiableCursorProvider<T> {
+
+  private static final transient AtomicInteger ID_GENERATOR = new AtomicInteger(MIN_VALUE);
+
+  private final int id;
+
+  /**
+   * Creates a new decorator for the given {@code cursorProvider}.
+   * <p>
+   * If the {@code cursorProvider} is already an {@link IdentifiableCursorProvider}, then the returned decorator will yield the
+   * same id as the input {@code cursorProvider}. If {@code cursorProvider} is a {@link CursorProviderDecorator}, then the
+   * {@link CursorProviderDecorator#getDelegate()} will be recursively be tested to find if any delegate in the chain is
+   * identifiable. If such delegate is found, then the same {@code id} will be reused. Otherwise, one will be generated.
+   *
+   * @param cursorProvider the decoratee
+   * @param <T>            the generic {@link Cursor} type
+   * @return a new decorator.
+   */
+  public static <T extends Cursor> IdentifiableCursorProviderDecorator<T> of(CursorProvider<T> cursorProvider) {
+    final CursorProvider<T> root = cursorProvider;
+    if (cursorProvider instanceof IdentifiableCursorProviderDecorator) {
+      return (IdentifiableCursorProviderDecorator<T>) cursorProvider;
+    }
+    Integer id = null;
+    do {
+      if (cursorProvider instanceof IdentifiableCursorProvider) {
+        id = ((IdentifiableCursorProvider<T>) cursorProvider).getId();
+        break;
+      }
+
+      if (cursorProvider instanceof CursorProviderDecorator) {
+        cursorProvider = ((CursorProviderDecorator<T>) cursorProvider).getDelegate();
+      }
+    } while (cursorProvider instanceof CursorProviderDecorator);
+
+    if (id == null) {
+      id = ID_GENERATOR.incrementAndGet();
+    }
+
+    if (cursorProvider instanceof CursorStreamProvider) {
+      return (IdentifiableCursorProviderDecorator<T>) new IdentifiableCursorStreamProviderDecorator(
+                                                                                                    (CursorStreamProvider) root,
+                                                                                                    id);
+    } else {
+      return (IdentifiableCursorProviderDecorator<T>) new IdentifiableCursorIteratorProviderDecorator(
+                                                                                                      (CursorIteratorProvider) root,
+                                                                                                      id);
+    }
+  }
+
+  private IdentifiableCursorProviderDecorator(CursorProvider<T> delegate, int id) {
+    super(delegate);
+    this.id = id;
+  }
+
+  @Override
+  public int getId() {
+    return id;
+  }
+
+  private static class IdentifiableCursorStreamProviderDecorator extends IdentifiableCursorProviderDecorator<CursorStream>
+      implements CursorStreamProvider {
+
+    public IdentifiableCursorStreamProviderDecorator(CursorStreamProvider delegate, int id) {
+      super(delegate, id);
+    }
+  }
+
+
+  private static class IdentifiableCursorIteratorProviderDecorator extends IdentifiableCursorProviderDecorator<CursorIterator>
+      implements CursorIteratorProvider {
+
+    public IdentifiableCursorIteratorProviderDecorator(CursorIteratorProvider delegate, int id) {
+      super(delegate, id);
+    }
+  }
+}
