1b2506a05171488e3433cccccfcd923c46db20c0
jcgrethe <jgrethe@itba.edu.ar>
2020-08-31 17:30:19 -0300

    style

--- core/src/test/java/org/mule/context/notification/FlowTraceManagerReinitializeTestCase.java
+++ core/src/test/java/org/mule/context/notification/FlowTraceManagerReinitializeTestCase.java
@@ -1,11 +1,15 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.context.notification;
 
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.mule.api.MuleContext;
 import org.mule.api.config.MuleConfiguration;
 import org.mule.api.lifecycle.InitialisationException;
@@ -13,6 +17,7 @@ import org.mule.config.DefaultMuleConfiguration;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -36,7 +41,8 @@ public class FlowTraceManagerReinitializeTestCase {
     }
 
     @Before
-    public void before() throws InitialisationException {
+    public void before() throws InitialisationException
+    {
         manager = new MessageProcessingFlowTraceManager();
         MuleContext context = mock(MuleContext.class);
         MuleConfiguration config = mock(MuleConfiguration.class);
@@ -47,8 +53,9 @@ public class FlowTraceManagerReinitializeTestCase {
     }
 
     @Test
-    public void reinitializeDoesntRemoveListeners() throws InitialisationException {
-        manager = Mockito.spy(manager);
+    public void reinitializeDoesntRemoveListeners() throws InitialisationException
+    {
+        manager = spy(manager);
         manager.initialise();
         verify(manager, never()).removeNotificationListeners();
     }
