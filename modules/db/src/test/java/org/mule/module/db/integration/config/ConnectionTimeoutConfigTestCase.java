fd78394aa7514b37a6f31d969be17b6a5177fb7a
Juan Grethe <jgrethe@itba.edu.ar>
2020-08-31 15:02:49 -0300

    MULE-18714: Not able to resolve the property connectionTimeout in database connector #9320

---
+++ modules/db/src/test/java/org/mule/module/db/integration/config/ConnectionTimeoutConfigTestCase.java
@@ -0,0 +1,51 @@
+package org.mule.module.db.integration.config;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.mule.common.TestResult;
+import org.mule.common.Testable;
+import org.mule.module.db.integration.AbstractDbIntegrationTestCase;
+import org.mule.module.db.integration.model.AbstractTestDatabase;
+import org.mule.module.db.integration.model.DerbyTestDatabase;
+import org.mule.tck.junit4.rule.SystemProperty;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mule.common.Result.Status.SUCCESS;
+
+public class ConnectionTimeoutConfigTestCase extends AbstractDbIntegrationTestCase {
+
+    @Rule
+    public SystemProperty connectionTimeout = new SystemProperty("connectionTimeout", "10000");
+
+    public ConnectionTimeoutConfigTestCase(String dataSourceConfigResource, AbstractTestDatabase testDatabase)
+    {
+        super(dataSourceConfigResource, testDatabase);
+    }
+
+    @Parameterized.Parameters
+    public static List<Object[]> parameters()
+    {
+        return Collections.singletonList(new Object[]{"integration/config/connection-timeout-config.xml", new DerbyTestDatabase()});
+    }
+
+    @Override
+    protected String[] getFlowConfigurationResources()
+    {
+        return new String[0];
+    }
+
+    @Test
+    public void testsConnection() throws Exception
+    {
+        Testable testable = muleContext.getRegistry().lookupObject("dbConfig");
+
+        TestResult result = testable.test();
+
+        assertThat(result.getStatus(), equalTo(SUCCESS));
+    }
+}
