90de81f9c1ffcba6b5643a0302d0cd6a2e738011
Rodrigo Merino <elrodro83@users.noreply.github.com>
2020-09-02 09:54:56 -0300

    MULE-18651: Implement the input stream data flow statistics (#9335)
    decorator/factories

---
+++ core/src/main/java/org/mule/runtime/core/internal/management/stats/PayloadStatisticsSet.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.internal.management.stats;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.function.LongConsumer;
+
+import org.apache.commons.collections.set.AbstractSetDecorator;
+
+final class PayloadStatisticsSet<T> extends AbstractSetDecorator {
+
+  private final LongConsumer populator;
+
+  PayloadStatisticsSet(Set<T> decorated, LongConsumer populator) {
+    super(decorated);
+    this.populator = populator;
+  }
+
+  @Override
+  public Iterator iterator() {
+    return new PayloadStatisticsIterator<>(super.iterator(), populator);
+  }
+
+  @Override
+  public Spliterator spliterator() {
+    return Spliterators.spliterator(iterator(), size(), 0);
+  }
+
+  @Override
+  public Object[] toArray() {
+    populator.accept(size());
+    return super.toArray();
+  }
+
+  @Override
+  public Object[] toArray(Object[] object) {
+    populator.accept(size());
+    return super.toArray(object);
+  }
+
+}
