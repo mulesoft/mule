8f5b4c9d1521222d63afd106c081460b353e8ab5
fabian.gonzalez <fabian.gonzalez@192.168.0.28>
2020-09-04 15:29:11 -0300

    Move classes.

--- core/src/main/java/org/mule/runtime/core/internal/management/stats/OutputDecoratorVisitor.java
+++ core/src/main/java/org/mule/runtime/core/internal/management/stats/OutputDecoratorVisitor.java
@@ -4,13 +4,15 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.runtime.core.api.management.stats;
+package org.mule.runtime.core.internal.management.stats;
 
 import java.io.InputStream;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+
+import org.mule.runtime.core.api.management.stats.CursorComponentDecoratorFactory;
 
 /**
  * Visitor that returns a decorator for output statistics.
