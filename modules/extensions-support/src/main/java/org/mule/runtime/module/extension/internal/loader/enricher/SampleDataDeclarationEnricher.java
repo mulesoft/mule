e71fa88e5f379971b4e13d4cbb7cc7541a2f8a0d
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2020-09-04 16:57:47 -0300

    cleanup

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/enricher/SampleDataDeclarationEnricher.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/enricher/SampleDataDeclarationEnricher.java
@@ -8,10 +8,8 @@ package org.mule.runtime.module.extension.internal.loader.enricher;
 
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
-import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 import static org.mule.runtime.module.extension.internal.data.sample.SampleDataUtils.getSampleDataProviderId;
-import static org.mule.runtime.module.extension.internal.util.IntrospectionUtils.getAnnotatedElement;
 import static org.mule.runtime.module.extension.internal.util.IntrospectionUtils.getImplementingName;
 
 import org.mule.runtime.api.meta.model.data.sample.SampleDataProviderModel;
@@ -19,14 +17,12 @@ import org.mule.runtime.api.meta.model.declaration.fluent.ExecutableComponentDec
 import org.mule.runtime.api.meta.model.declaration.fluent.ExtensionDeclaration;
 import org.mule.runtime.api.meta.model.declaration.fluent.OperationDeclaration;
 import org.mule.runtime.api.meta.model.declaration.fluent.ParameterDeclaration;
-import org.mule.runtime.api.meta.model.declaration.fluent.ParameterGroupDeclaration;
 import org.mule.runtime.api.meta.model.declaration.fluent.SourceDeclaration;
 import org.mule.runtime.api.util.Reference;
 import org.mule.runtime.extension.api.annotation.param.Config;
 import org.mule.runtime.extension.api.annotation.param.Connection;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
 import org.mule.runtime.extension.api.annotation.values.OfValues;
-import org.mule.runtime.extension.api.annotation.values.ValuePart;
 import org.mule.runtime.extension.api.declaration.fluent.util.IdempotentDeclarationWalker;
 import org.mule.runtime.extension.api.declaration.type.DefaultExtensionsTypeLoaderFactory;
 import org.mule.runtime.extension.api.loader.DeclarationEnricher;
@@ -36,14 +32,11 @@ import org.mule.runtime.module.extension.api.loader.java.type.ExtensionParameter
 import org.mule.runtime.module.extension.api.loader.java.type.FieldElement;
 import org.mule.runtime.module.extension.internal.loader.java.property.ImplementingMethodModelProperty;
 import org.mule.runtime.module.extension.internal.loader.java.property.ImplementingTypeModelProperty;
-import org.mule.runtime.module.extension.internal.loader.java.property.ParameterGroupModelProperty;
 import org.mule.runtime.module.extension.internal.loader.java.property.SampleDataProviderFactoryModelProperty.SampleDataProviderFactoryModelPropertyBuilder;
 import org.mule.runtime.module.extension.internal.loader.java.type.runtime.ParameterizableTypeWrapper;
 import org.mule.sdk.api.annotation.data.sample.SampleData;
 import org.mule.sdk.api.data.sample.SampleDataProvider;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.HashMap;
@@ -161,53 +154,12 @@ public class SampleDataDeclarationEnricher extends AbstractAnnotatedDeclarationE
     return empty();
   }
 
-  /**
-   * Given a list of {@link ParameterGroupDeclaration}, introspect it and finds all the considered parameters with an associated
-   * {@link ValueProvider}
-   *
-   * @param parameterGroups parameter groups to introspect
-   * @return a Map containing all the {@link ParameterGroupDeclaration} with their correspondent {@link ValueProvider}
-   */
-  private Map<ParameterGroupDeclaration, OfValues> introspectParameterGroups(List<ParameterGroupDeclaration> parameterGroups) {
-    Map<ParameterGroupDeclaration, OfValues> optionResolverEnabledParameters = new HashMap<>();
-
-    parameterGroups
-            .forEach(paramGroup -> paramGroup.getModelProperty(ParameterGroupModelProperty.class)
-                    .ifPresent(modelProperty -> {
-
-                      AnnotatedElement container = modelProperty.getDescriptor().getContainer();
-                      if (container != null) {
-                        OfValues annotation = container.getAnnotation(OfValues.class);
-                        if (annotation != null) {
-                          optionResolverEnabledParameters.put(paramGroup, annotation);
-                        }
-                      }
-                    }));
-
-    return optionResolverEnabledParameters;
-  }
-
-  private <T extends Annotation> Optional<T> getAnnotation(ParameterDeclaration param, Class<T> annotationClass) {
-    return getAnnotatedElement(param)
-            .map(annotatedElement -> ofNullable(annotatedElement.getAnnotation(annotationClass)))
-            .orElse(empty());
-  }
-
   private List<String> getRequiredParametersAliases(List<ExtensionParameter> parameterDeclarations,
                                                     Map<String, String> parameterNames) {
     return parameterDeclarations.stream()
             .filter(ExtensionParameter::isRequired)
             .map(param -> parameterNames.getOrDefault(param.getName(), param.getName()))
             .collect(toList());
-  }
-
-  private Map<ParameterDeclaration, Integer> getParts(ParameterGroupDeclaration paramDeclaration) {
-    Map<ParameterDeclaration, Integer> parts = new HashMap<>();
-
-    paramDeclaration.getParameters().forEach(param -> getAnnotation(param, ValuePart.class)
-            .ifPresent(part -> parts.put(param, part.order())));
-
-    return parts;
   }
 
   private Map<String, String> getContainerParameterNames(List<ParameterDeclaration> allParameters) {
