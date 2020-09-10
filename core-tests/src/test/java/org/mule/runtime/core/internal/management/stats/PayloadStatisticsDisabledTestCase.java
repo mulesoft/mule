3f67b47dbb78dab21d1ea963ac54a901c98a05ac
fabian.gonzalez <fabian.gonzalez@gonzalezf-ltm3.internal.salesforce.com>
2020-09-10 16:12:18 -0300

    MULE-18741: allow stats when components are added after toggling payload stats enabled.

--- core-tests/src/test/java/org/mule/runtime/core/internal/management/stats/PayloadStatisticsDisabledTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/management/stats/PayloadStatisticsDisabledTestCase.java
@@ -6,51 +6,28 @@
  */
 package org.mule.runtime.core.internal.management.stats;
 
+import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
-import static java.util.Optional.of;
-import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.mule.runtime.api.component.ComponentIdentifier.buildFromStringRepresentation;
 import static org.mule.runtime.api.util.MuleSystemProperties.MULE_DISABLE_PAYLOAD_STATISTICS;
 import static org.mule.runtime.api.util.MuleSystemProperties.MULE_ENABLE_STATISTICS;
-import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.initialiseIfNeeded;
-import static org.mule.runtime.dsl.api.component.config.DefaultComponentLocation.from;
 import static org.mule.test.allure.AllureConstants.StreamingFeature.STREAMING;
 import static org.mule.test.allure.AllureConstants.StreamingFeature.StreamingStory.STATISTICS;
-
-import org.mule.runtime.api.component.Component;
-import org.mule.runtime.api.exception.MuleException;
-import org.mule.runtime.api.lifecycle.InitialisationException;
-import org.mule.runtime.api.util.MuleSystemProperties;
-import org.mule.runtime.core.api.management.stats.AllStatistics;
-import org.mule.runtime.core.api.management.stats.PayloadStatistics;
-import org.mule.runtime.core.privileged.registry.RegistrationException;
-import org.mule.runtime.extension.api.runtime.streaming.PagingProvider;
-import org.mule.tck.junit4.AbstractMuleContextTestCase;
-import org.mule.tck.junit4.rule.SystemProperty;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.mule.runtime.api.component.Component;
+import org.mule.runtime.core.api.management.stats.AllStatistics;
+import org.mule.runtime.core.api.management.stats.PayloadStatistics;
+import org.mule.tck.junit4.rule.SystemProperty;
 
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
@@ -67,28 +44,30 @@ public class PayloadStatisticsDisabledTestCase extends AbstractPayloadStatistics
 
   @Test
   public void decorateInputStreamDisabled() throws IOException {
-    final InputStream decorated = new ByteArrayInputStream("Hello World".getBytes(UTF_8));
-    final InputStream decorator = decoratorFactory.componentDecoratorFactory(component1).decorateInput(decorated, CORR_ID);
-
-    assertThat(decorated, sameInstance(decorator));
-
-    final PayloadStatistics statistics = muleContext.getStatistics().getPayloadStatistics(component1.getLocation().getLocation());
-
-    final DefaultFlowConstructStatistics defaultFlowConstrutStatistics =
-        new DefaultFlowConstructStatistics("flowType", "flowName");
     final AllStatistics allStatistics = muleContext.getStatistics();
-    allStatistics.add(defaultFlowConstrutStatistics);
+    // By default the payload stats are disabled by system property
+    verifyStatisticsOnDecoratedStream(component1, 0L, false);
 
+    // Enable stats and verify that they are computed for same component
+    allStatistics.enablePayloadStatistics(true);
+    verifyStatisticsOnDecoratedStream(component1, 1L, true);
 
-    assertThat(statistics.getInputByteCount(), is(0L));
+    // Add a new component to stats and verify that the stats are computed
+    verifyStatisticsOnDecoratedStream(component2, 1L, true);
 
+    // Verify that the stats are not computed when disabled
+    allStatistics.enablePayloadStatistics(false);
+    verifyStatisticsOnDecoratedStream(component2, 1L, false);
+  }
+
+  private void verifyStatisticsOnDecoratedStream(Component component, Long expectedInputByteCount, boolean expectedStatsEnabled)
+      throws IOException {
+    final InputStream decorated = new ByteArrayInputStream("Hello World".getBytes(UTF_8));
+    final InputStream decorator = decoratorFactory.componentDecoratorFactory(component).decorateInput(decorated, CORR_ID);
+    final PayloadStatistics statistics = muleContext.getStatistics().getPayloadStatistics(component.getLocation().getLocation());
     decorator.read();
-
-    verifyNoStatistics(statistics);
-
-    assertThat(allStatistics.getApplicationStatistics().isEnabled(), is(TRUE));
-    assertThat(defaultFlowConstrutStatistics.isEnabled(), is(TRUE));
-    assertThat(statistics.isEnabled(), is(Boolean.FALSE));
+    assertThat(statistics.getInputByteCount(), is(expectedInputByteCount));
+    assertThat(statistics.isEnabled(), is(expectedStatsEnabled));
   }
 
 }
