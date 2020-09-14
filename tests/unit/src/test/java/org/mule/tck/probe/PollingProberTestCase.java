4921bfacb2008d5726ce6e58b8034fd748be9f31
Ezequiel Werner <eze210@gmail.com>
2020-09-14 15:44:09 -0300

    polling prober retries

---
+++ tests/unit/src/test/java/org/mule/tck/probe/PollingProberTestCase.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.tck.probe;
+
+import static java.lang.String.format;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.mule.tck.probe.PollingProber.DEFAULT_TIMEOUT;
+import static org.junit.Assert.assertThat;
+
+import org.mule.tck.junit4.AbstractMuleTestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PollingProberTestCase extends AbstractMuleTestCase {
+
+  private PollingProber prober;
+
+  @Before
+  public void setup() {
+    // Create a prober with a fast polling
+    prober = new PollingProber(DEFAULT_TIMEOUT, 1);
+  }
+
+  @Test
+  public void probeBecomesTrueAfterFiveTries() {
+    prober.check(new ProbeWhichBecomesTrueAfterNTries(5, false));
+  }
+
+  @Test
+  public void probeAssertsTrueAfterFiveTries() {
+    prober.check(new ProbeWhichBecomesTrueAfterNTries(5, true));
+  }
+
+  @Test
+  public void jUnitProbeBecomesTrueAfterFiveTries() {
+    prober.check(new JUnitProbeWhichBecomesTrueAfterNTries(5, false));
+  }
+
+
+  @Test
+  public void jUnitProbeAssertsTrueAfterFiveTries() {
+    prober.check(new JUnitProbeWhichBecomesTrueAfterNTries(5, true));
+  }
+
+  private class ProbeWhichBecomesTrueAfterNTries implements Probe {
+
+    private int currentTries = 0;
+    private int failingTries;
+    private boolean isAssertion;
+
+    private ProbeWhichBecomesTrueAfterNTries(int failingTries, boolean isAssertion) {
+      this.failingTries = failingTries;
+      this.isAssertion = isAssertion;
+    }
+
+    @Override
+    public boolean isSatisfied() {
+      currentTries += 1;
+
+      if (isAssertion) {
+        assertThat(currentTries, greaterThan(failingTries));
+        return true;
+      } else {
+        return currentTries >= failingTries;
+      }
+    }
+
+    @Override
+    public String describeFailure() {
+      return format("You need to call the probe %d times before it becomes true", failingTries);
+    }
+  }
+
+  private class JUnitProbeWhichBecomesTrueAfterNTries extends JUnitProbe {
+
+    private int currentTries = 0;
+    private int failingTries;
+    private boolean isAssertion;
+
+    private JUnitProbeWhichBecomesTrueAfterNTries(int failingTries, boolean isAssertion) {
+      this.failingTries = failingTries;
+      this.isAssertion = isAssertion;
+    }
+
+    @Override
+    public boolean test() {
+      currentTries += 1;
+
+      if (isAssertion) {
+        assertThat(currentTries, greaterThan(failingTries));
+        return true;
+      } else {
+        return currentTries >= failingTries;
+      }
+    }
+
+    @Override
+    public String describeFailure() {
+      return format("You need to call the probe %d times before it becomes true", failingTries);
+    }
+  }
+}
