07e9e61012d78870962a07784dbdc51e8e4cf1b0
Ezequiel Werner <eze210@gmail.com>
2020-09-15 14:01:56 -0300

    MULE-17582: Fix ApplicationDeploymentTestCase#redeploysBrokenExplodedAppAfterStartup flaky test (#9411)
    (cherry picked from commit 6cbd0c79a71cf65d14b682fb7e710910c42b254b)

--- modules/deployment/src/test/java/org/mule/runtime/module/deployment/internal/AbstractDeploymentTestCase.java
+++ modules/deployment/src/test/java/org/mule/runtime/module/deployment/internal/AbstractDeploymentTestCase.java
@@ -1674,15 +1674,22 @@ public abstract class AbstractDeploymentTestCase extends AbstractMuleTestCase {
   }
 
   /**
-   * Updates a file's last modified time to be greater than the original timestamp
+   * Updates a file's last modified time to be greater than the original timestamp.
+   * It locks the deployment lock in order to prevent the directory watcher to redeploy the artifact multiple times.
    *
    * @param timestamp time value in milliseconds of the original file's last modified time
    * @param file      file to update
    */
   protected void updateFileModifiedTime(long timestamp, File file) {
-    do {
-      file.setLastModified(currentTimeMillis());
-    } while (file.lastModified() == timestamp);
+    ReentrantLock deploymentLock = deploymentService.getLock();
+    deploymentLock.lock();
+    try {
+      do {
+        file.setLastModified(currentTimeMillis());
+      } while (file.lastModified() <= timestamp);
+    } finally {
+      deploymentLock.unlock();
+    }
   }
 
   protected void resetUndeployLatch() {
