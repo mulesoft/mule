fd805b11ffa61d692a043a1f58bef23097632e00
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2020-09-15 22:01:03 -0300

    MULE-18742: Getting ConcurrentModificationException error when disposing logs (#9419)
    (cherry picked from commit 7133da5260313fa1f74d0643a7e1f90fe1ad7b6d)

--- modules/launcher/src/test/java/org/mule/runtime/module/launcher/log4j2/MuleLog4jContextFactoryTestCase.java
+++ modules/launcher/src/test/java/org/mule/runtime/module/launcher/log4j2/MuleLog4jContextFactoryTestCase.java
@@ -6,17 +6,31 @@
  */
 package org.mule.runtime.module.launcher.log4j2;
 
+import static java.lang.Runtime.getRuntime;
 import static java.lang.System.getProperty;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mule.test.allure.AllureConstants.Logging.LOGGING;
+import static org.mule.test.allure.AllureConstants.Logging.LoggingStory.CONTEXT_FACTORY;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Story;
+import org.apache.logging.log4j.core.util.Cancellable;
+import org.apache.logging.log4j.core.util.ShutdownCallbackRegistry;
+import org.mule.runtime.api.util.concurrent.Latch;
 import org.mule.runtime.module.launcher.api.log4j2.AsyncLoggerExceptionHandler;
 import org.mule.tck.junit4.AbstractMuleTestCase;
 import org.mule.tck.size.SmallTest;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.logging.log4j.core.config.xml.XmlConfigurationFactory;
 import org.junit.After;
@@ -24,10 +38,15 @@ import org.junit.Before;
 import org.junit.Test;
 
 @SmallTest
+@Feature(LOGGING)
+@Story(CONTEXT_FACTORY)
 public class MuleLog4jContextFactoryTestCase extends AbstractMuleTestCase {
 
   private static final String LOG_CONFIGURATION_FACTORY_PROPERTY = "log4j.configurationFactory";
   private static final String ASYNC_LOGGER_EXCEPTION_HANDLER_PROPERTY = "AsyncLoggerConfig.ExceptionHandler";
+
+  private static final Integer SHUTDOWN_HOOKS_NUMBER = getRuntime().availableProcessors();
+  private static final Integer LATCH_TIMEOUT_MILLIS = 500;
 
   private Map<String, String> originalSystemProperties;
 
@@ -72,5 +91,37 @@ public class MuleLog4jContextFactoryTestCase extends AbstractMuleTestCase {
     MuleLog4jContextFactory factory = new MuleLog4jContextFactory(contextSelector);
     factory.dispose();
     verify(contextSelector).dispose();
+  }
+
+  @Test
+  @Issue("MULE-18742")
+  @Description("If any shutdown callback is cancelled while the log is disposing everything should work")
+  public void cancelWhileDisposing() {
+    ArtifactAwareContextSelector contextSelector = mock(ArtifactAwareContextSelector.class);
+    MuleLog4jContextFactory factory = new MuleLog4jContextFactory(contextSelector);
+    ShutdownCallbackRegistry shutdownCallbackRegistry = factory.getShutdownCallbackRegistry();
+
+    Latch latch = new Latch();
+    AtomicInteger executedHooks = new AtomicInteger();
+
+    Cancellable hookToCancel = shutdownCallbackRegistry.addShutdownCallback(() -> {
+      try {
+        latch.await(LATCH_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+      } catch (InterruptedException e) {
+      }
+      executedHooks.incrementAndGet();
+    });
+    shutdownCallbackRegistry.addShutdownCallback(() -> {
+      hookToCancel.cancel();
+      latch.release();
+      executedHooks.incrementAndGet();
+    });
+    for (int i = 0; i < SHUTDOWN_HOOKS_NUMBER - 2; i++) {
+      shutdownCallbackRegistry.addShutdownCallback(executedHooks::incrementAndGet);
+    }
+
+    assertThat(executedHooks.get(), is(0));
+    factory.dispose();
+    assertThat(executedHooks.get(), is(SHUTDOWN_HOOKS_NUMBER));
   }
 }
