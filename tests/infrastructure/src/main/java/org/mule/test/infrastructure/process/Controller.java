9d027726bcc549e3309e6ce411a3d5912f5ab0dd
Rodrigo Merino <elrodro83@users.noreply.github.com>
2020-09-30 11:48:51 -0300

    MULE-15880: Fix LicenseInstallationTestCase flaky test - Troubleshooting (#9481)

--- tests/infrastructure/src/main/java/org/mule/test/infrastructure/process/Controller.java
+++ tests/infrastructure/src/main/java/org/mule/test/infrastructure/process/Controller.java
@@ -24,9 +24,6 @@ import static org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptor
 import static org.mule.test.infrastructure.process.AbstractOSController.MULE_EE_SERVICE_NAME;
 import static org.mule.test.infrastructure.process.AbstractOSController.MULE_SERVICE_NAME;
 
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.filefilter.IOFileFilter;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -38,6 +35,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.filefilter.IOFileFilter;
 
 public class Controller {
 
@@ -225,11 +225,15 @@ public class Controller {
   }
 
   public void installLicense(String path) {
-    osSpecificController.runSync(null, "--installLicense", path);
+    if (0 != osSpecificController.runSync(null, "--installLicense", path, "-M-client")) {
+      throw new MuleControllerException("Could not install license " + path);
+    }
   }
 
   public void uninstallLicense() {
-    osSpecificController.runSync(null, "-unInstallLicense");
+    if (0 != osSpecificController.runSync(null, "--unInstallLicense", "-M-client")) {
+      throw new MuleControllerException("Could not uninstall license");
+    }
   }
 
   protected boolean isDeployed(String appName) {
