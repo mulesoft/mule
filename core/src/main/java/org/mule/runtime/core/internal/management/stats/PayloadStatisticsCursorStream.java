7d781ecea98eb0a1f7857c691432494339a2d9fb
Rodrigo Merino <elrodro83@users.noreply.github.com>
2020-10-16 19:53:37 -0300

    MULE-18896: PayloadStatistics not calculated for stream passthrough (#9587)

--- core/src/main/java/org/mule/runtime/core/internal/management/stats/PayloadStatisticsCursorStream.java
+++ core/src/main/java/org/mule/runtime/core/internal/management/stats/PayloadStatisticsCursorStream.java
@@ -8,6 +8,7 @@ package org.mule.runtime.core.internal.management.stats;
 
 import org.mule.runtime.api.streaming.CursorProvider;
 import org.mule.runtime.api.streaming.bytes.CursorStream;
+import org.mule.runtime.api.streaming.bytes.CursorStreamProvider;
 
 import java.io.IOException;
 import java.util.function.LongConsumer;
@@ -44,7 +45,28 @@ final class PayloadStatisticsCursorStream extends CursorStream {
 
   @Override
   public CursorProvider getProvider() {
-    return delegate.getProvider();
+    return new CursorStreamProvider() {
+
+      @Override
+      public CursorStream openCursor() {
+        return new PayloadStatisticsCursorStream((CursorStream) delegate.getProvider().openCursor(), populator);
+      }
+
+      @Override
+      public void close() {
+        delegate.getProvider().close();
+      }
+
+      @Override
+      public void releaseResources() {
+        delegate.getProvider().releaseResources();
+      }
+
+      @Override
+      public boolean isClosed() {
+        return delegate.getProvider().isClosed();
+      }
+    };
   }
 
   @Override
@@ -54,6 +76,16 @@ final class PayloadStatisticsCursorStream extends CursorStream {
       populator.accept(1);
     }
 
+    return read;
+  }
+
+  @Override
+  public int read(byte[] b, int off, int len) throws IOException {
+    final int read = delegate.read(b, off, len);
+    // ignore -1 indicating no data read
+    if (read > 0) {
+      populator.accept(read);
+    }
     return read;
   }
 
