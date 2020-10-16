b6984e2c0e21a7b3beae919f83986de0dfe4c87f
Rodrigo Merino <elrodro83@users.noreply.github.com>
2020-10-16 10:08:26 -0300

    MULE-18889: Export META-INF/services/* resources in lightweight deployments (#9558) (#9582)
    * To make it consistent with the `classloader-model.json` that is
    generated by the packager for heavyweight deployments.

--- modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/maven/LightweightClassLoaderModelBuilder.java
+++ modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/maven/LightweightClassLoaderModelBuilder.java
@@ -9,6 +9,7 @@ package org.mule.runtime.module.deployment.impl.internal.maven;
 import static com.vdurmont.semver4j.Semver.SemverType.LOOSE;
 import static java.lang.String.format;
 import static java.lang.System.lineSeparator;
+import static java.util.Collections.singleton;
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
 import static java.util.stream.Collectors.toList;
@@ -48,9 +49,9 @@ import org.apache.maven.model.Plugin;
  */
 public class LightweightClassLoaderModelBuilder extends ArtifactClassLoaderModelBuilder {
 
-  private MavenClient mavenClient;
-  private Set<BundleDependency> nonProvidedDependencies;
-  private Map<Pair<String, String>, Boolean> sharedLibraryAlreadyExported = new HashMap<>();
+  private final MavenClient mavenClient;
+  private final Set<BundleDependency> nonProvidedDependencies;
+  private final Map<Pair<String, String>, Boolean> sharedLibraryAlreadyExported = new HashMap<>();
 
   public LightweightClassLoaderModelBuilder(File artifactFolder, BundleDescriptor artifactBundleDescriptor,
                                             MavenClient mavenClient, Set<BundleDependency> nonProvidedDependencies) {
@@ -118,6 +119,11 @@ public class LightweightClassLoaderModelBuilder extends ArtifactClassLoaderModel
     JarInfo jarInfo = fileJarExplorer.explore(resolvedBundleDependency.getBundleUri());
     this.exportingPackages(jarInfo.getPackages());
     this.exportingResources(jarInfo.getResources());
+
+    jarInfo.getServices()
+        .forEach(service -> this
+            .exportingResources(singleton("META-INF/services/" + service.getServiceInterface())));
+
     resolvedBundleDependency.getTransitiveDependencies()
         .forEach(this::exportBundleDependencyAndTransitiveDependencies);
   }
