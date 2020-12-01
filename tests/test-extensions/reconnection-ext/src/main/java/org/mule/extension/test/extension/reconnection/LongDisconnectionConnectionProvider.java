0ac45f6ccb1bbb6f08635de329abdbeeda992a71
gabrieldalborgo <46535762+gabrieldalborgo@users.noreply.github.com>
2020-12-01 09:54:23 -0300

    MULE-19026: Connection is not properly invalidated during a source reconnection (#9803)

---
+++ tests/test-extensions/reconnection-ext/src/main/java/org/mule/extension/test/extension/reconnection/LongDisconnectionConnectionProvider.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.test.extension.reconnection;
+
+import org.mule.runtime.api.connection.ConnectionException;
+import org.mule.runtime.api.connection.ConnectionValidationResult;
+import org.mule.runtime.api.connection.PoolingConnectionProvider;
+import org.mule.runtime.extension.api.annotation.Alias;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.mule.runtime.api.connection.ConnectionValidationResult.success;
+
+@Alias("long-disconnection")
+public class LongDisconnectionConnectionProvider implements PoolingConnectionProvider<SynchronizableConnection> {
+
+  private static final int DISCONNECTION_TIMEOUT = 3;
+
+  @Override
+  public SynchronizableConnection connect() throws ConnectionException {
+    return new SynchronizableConnection();
+  }
+
+  @Override
+  public void disconnect(SynchronizableConnection synchronizableConnection) {
+    try {
+      boolean result = synchronizableConnection.getDisconnectionLatch()
+          .await(DISCONNECTION_TIMEOUT, SECONDS);
+      synchronizableConnection.setDisconnectionWaitedFullTimeout(!result);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public ConnectionValidationResult validate(SynchronizableConnection synchronizableConnection) {
+    return success();
+  }
+}
