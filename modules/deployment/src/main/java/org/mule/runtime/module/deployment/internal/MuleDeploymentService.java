34da99b134e27e11f8e26a443b93f242cf72ce8b
Fabian Gonzalez <fabian.gonzalez@mulesoft.com>
2020-12-11 12:16:56 -0300

    MULE-18679: CopyOnWriteArrayList for startupListeners in MuleDeploymentService

--- modules/deployment/src/main/java/org/mule/runtime/module/deployment/internal/MuleDeploymentService.java
+++ modules/deployment/src/main/java/org/mule/runtime/module/deployment/internal/MuleDeploymentService.java
@@ -143,6 +143,10 @@ public class MuleDeploymentService implements DeploymentService {
 
     deploymentDirectoryWatcher.start();
 
+    notifyStartupListeners();
+  }
+
+  protected void notifyStartupListeners() {
     for (StartupListener listener : startupListeners) {
       try {
         listener.onAfterStartup();
