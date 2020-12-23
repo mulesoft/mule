857ebbf2bf70a5e217c6472a6eee89ca03ef802f
Mariano Lischetti <lischettim@gmail.com>
2020-12-23 12:12:28 -0300

    MULE-19068: TargetReturnDelegate is not managing cursor stream provider (#9875) (#9879)
    (cherry picked from commit 66b34ccb634c087944fdebd8e237b6e5fd465751)
    (cherry picked from commit 10c80af1edeeb16ef95c607f504c3a64ac5ff8ad)

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/operation/ComponentMessageProcessor.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/operation/ComponentMessageProcessor.java
@@ -457,7 +457,8 @@ public abstract class ComponentMessageProcessor<T extends ComponentModel> extend
 
 
   protected TargetReturnDelegate getTargetReturnDelegate() {
-    return new TargetReturnDelegate(target, targetValue, componentModel, expressionManager, cursorProviderFactory, muleContext);
+    return new TargetReturnDelegate(target, targetValue, componentModel, expressionManager, cursorProviderFactory, muleContext,
+                                    streamingManager);
   }
 
   protected ValueReturnDelegate getValueReturnDelegate() {
