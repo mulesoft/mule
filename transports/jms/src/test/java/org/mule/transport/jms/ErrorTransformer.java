b1a1f4fb529fb76b5f3d06d215b206092a65d0df
pabloperalta <pablojulianperalta@gmail.com>
2020-12-29 17:29:55 -0300

    MULE-19073: Exception in Catch Exception Strategy does not roll back transaction

---
+++ transports/jms/src/test/java/org/mule/transport/jms/ErrorTransformer.java
@@ -0,0 +1,26 @@
+/*
+ * (c) 2003-2019 MuleSoft, Inc. This software is protected under international copyright
+ * law. All use of this software is subject to MuleSoft's Master Subscription Agreement
+ * (or other master license agreement) separately entered into in writing between you and
+ * MuleSoft. If such an agreement is not in place, you may not use the software.
+ */
+package org.mule.transport.jms;
+
+import org.mule.api.MuleMessage;
+import org.mule.api.transformer.TransformerException;
+import org.mule.config.i18n.MessageFactory;
+import org.mule.transformer.AbstractMessageTransformer;
+
+public class ErrorTransformer extends AbstractMessageTransformer {
+
+  public static boolean crash;
+
+  @Override
+  public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
+    if (crash) {
+      throw new TransformerException(MessageFactory.createStaticMessage("ErrorTransformer failed"));
+    } else {
+      return message;
+    }
+  }
+}
