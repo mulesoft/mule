435105acd71b644e6e6daf425e3cc6bf99060ea5
Guillermo Fernandes <gsfernandes@users.noreply.github.com>
2020-12-30 10:41:53 -0300

    fff

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/operation/DefaultExecutionMediator.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/operation/DefaultExecutionMediator.java
@@ -31,6 +31,7 @@ import org.mule.runtime.core.api.retry.policy.RetryPolicyTemplate;
 import org.mule.runtime.core.api.transaction.Transaction;
 import org.mule.runtime.core.api.transaction.TransactionCoordination;
 import org.mule.runtime.core.internal.connection.ConnectionManagerAdapter;
+import org.mule.runtime.core.internal.util.CompositeClassLoader;
 import org.mule.runtime.extension.api.runtime.Interceptable;
 import org.mule.runtime.extension.api.runtime.config.ConfigurationInstance;
 import org.mule.runtime.extension.api.runtime.config.ConfigurationStats;
@@ -131,7 +132,11 @@ public final class DefaultExecutionMediator<T extends ComponentModel> implements
 
       InterceptorsExecutionResult beforeExecutionResult = before(context, interceptors);
       if (beforeExecutionResult.isOk()) {
-        result = from(withContextClassLoader(getClassLoader(context.getExtensionModel()), () -> executor.execute(context)));
+        final Thread currentThread = Thread.currentThread();
+        final ClassLoader currentClassLoader = currentThread.getContextClassLoader();
+        final ClassLoader extensionClassLoader = getClassLoader(context.getExtensionModel());
+        final CompositeClassLoader compositeClassLoader = new CompositeClassLoader(extensionClassLoader, currentClassLoader);
+        result = from(withContextClassLoader(compositeClassLoader, () -> executor.execute(context)));
         executedInterceptors.addAll(interceptors);
       } else {
         result = error(beforeExecutionResult.getThrowable());
