b62759c1e4005df78c19097790cc04c11d7cdf0c
Juan Baca Paunero <wencesj@gmail.com>
2021-01-04 12:47:00 -0300

    More changes requested in PR review

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/enricher/DefaultEncodingDeclarationEnricher.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/enricher/DefaultEncodingDeclarationEnricher.java
@@ -6,25 +6,16 @@
  */
 package org.mule.runtime.module.extension.internal.loader.enricher;
 
-import static java.util.stream.Collectors.toList;
 import static org.apache.commons.collections.CollectionUtils.isEmpty;
-import static org.mule.runtime.extension.api.loader.DeclarationEnricherPhase.POST_STRUCTURE;
 import static org.reflections.ReflectionUtils.getAllFields;
 import static org.reflections.ReflectionUtils.withAnnotation;
+
 import org.mule.runtime.api.meta.model.declaration.fluent.BaseDeclaration;
-import org.mule.runtime.api.meta.model.declaration.fluent.ConfigurationDeclaration;
-import org.mule.runtime.api.meta.model.declaration.fluent.ConnectionProviderDeclaration;
-import org.mule.runtime.api.meta.model.declaration.fluent.SourceDeclaration;
 import org.mule.runtime.extension.api.annotation.param.DefaultEncoding;
-import org.mule.runtime.extension.api.declaration.fluent.util.IdempotentDeclarationWalker;
 import org.mule.runtime.extension.api.exception.IllegalConfigurationModelDefinitionException;
 import org.mule.runtime.extension.api.loader.DeclarationEnricher;
-import org.mule.runtime.extension.api.loader.DeclarationEnricherPhase;
-import org.mule.runtime.extension.api.loader.ExtensionLoadingContext;
 import org.mule.runtime.module.extension.internal.loader.java.property.DefaultEncodingModelProperty;
 import org.mule.runtime.module.extension.internal.loader.java.property.ImplementingTypeModelProperty;
-
-import com.google.common.base.Joiner;
 
 import java.lang.reflect.Field;
 import java.util.Collection;
@@ -39,62 +30,19 @@ import java.util.Collection;
  *
  * @since 4.0
  */
-public final class DefaultEncodingDeclarationEnricher implements DeclarationEnricher {
+public final class DefaultEncodingDeclarationEnricher extends AbstractAnnotatedFieldDeclarationEnricher {
 
   @Override
-  public DeclarationEnricherPhase getExecutionPhase() {
-    return POST_STRUCTURE;
-  }
-
-  @Override
-  public void enrich(ExtensionLoadingContext extensionLoadingContext) {
-    new IdempotentDeclarationWalker() {
-
-      @Override
-      public void onConfiguration(ConfigurationDeclaration declaration) {
-        doEnrich(declaration);
-      }
-
-      @Override
-      protected void onConnectionProvider(ConnectionProviderDeclaration declaration) {
-        doEnrich(declaration);
-      }
-
-      @Override
-      protected void onSource(SourceDeclaration declaration) {
-        doEnrich(declaration);
-      }
-    }.walk(extensionLoadingContext.getExtensionDeclarer().getDeclaration());
-  }
-
-  private void doEnrich(BaseDeclaration declaration) {
-    declaration.getModelProperty(ImplementingTypeModelProperty.class).ifPresent(p -> {
-      ImplementingTypeModelProperty typeProperty = (ImplementingTypeModelProperty) p;
+  protected void doEnrich(BaseDeclaration<?> declaration) {
+    declaration.getModelProperty(ImplementingTypeModelProperty.class).ifPresent(typeProperty -> {
       Collection<Field> fields = getAllFields(typeProperty.getType(), withAnnotation(DefaultEncoding.class));
       if (isEmpty(fields)) {
         return;
       }
 
-      // TODO: MULE-9220 - Add a syntax validator which checks that the annotated parameter is a String
-      if (fields.size() > 1) {
-        throw new IllegalConfigurationModelDefinitionException(String
-            .format("Only one field is allowed to be annotated with @%s, but class '%s' has %d fields "
-                + "with such annotation. Offending fields are: [%s]", DefaultEncoding.class.getSimpleName(),
-                    typeProperty.getType()
-                        .getName(),
-                    fields.size(), Joiner.on(", ").join(fields.stream().map(Field::getName).collect(toList()))));
-      }
+      validate(fields, typeProperty, DefaultEncoding.class, String.class);
 
-      final Field defaultEncodingField = fields.iterator().next();
-      if (!String.class.equals(defaultEncodingField.getType())) {
-        throw new IllegalConfigurationModelDefinitionException(String
-            .format("Class '%s' declares the field '%s' which is annotated with @%s and is of type '%s'. Only "
-                + "fields of type String are allowed to carry such annotation", typeProperty.getType().getName(),
-                    defaultEncodingField.getName(), DefaultEncoding.class.getSimpleName(),
-                    defaultEncodingField.getType().getName()));
-      }
-
-      declaration.addModelProperty(new DefaultEncodingModelProperty(defaultEncodingField));
+      declaration.addModelProperty(new DefaultEncodingModelProperty(fields.iterator().next()));
     });
   }
 }
--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/enricher/RuntimeVersionDeclarationEnricher.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/enricher/RuntimeVersionDeclarationEnricher.java
@@ -6,10 +6,7 @@
  */
 package org.mule.runtime.module.extension.internal.loader.enricher;
 
-import static java.lang.String.format;
-import static java.util.stream.Collectors.toList;
 import static org.apache.commons.collections.CollectionUtils.isEmpty;
-import static org.mule.runtime.extension.api.loader.DeclarationEnricherPhase.POST_STRUCTURE;
 import static org.reflections.ReflectionUtils.getAllFields;
 import static org.reflections.ReflectionUtils.withAnnotation;
 
@@ -21,7 +18,6 @@ import org.mule.runtime.api.meta.model.declaration.fluent.SourceDeclaration;
 import org.mule.runtime.extension.api.declaration.fluent.util.IdempotentDeclarationWalker;
 import org.mule.runtime.extension.api.exception.IllegalConfigurationModelDefinitionException;
 import org.mule.runtime.extension.api.loader.DeclarationEnricher;
-import org.mule.runtime.extension.api.loader.DeclarationEnricherPhase;
 import org.mule.runtime.extension.api.loader.ExtensionLoadingContext;
 import org.mule.runtime.module.extension.internal.loader.java.property.ImplementingTypeModelProperty;
 import org.mule.runtime.module.extension.internal.loader.java.property.RuntimeVersionModelProperty;
@@ -29,8 +25,6 @@ import org.mule.sdk.api.annotation.param.RuntimeVersion;
 
 import java.lang.reflect.Field;
 import java.util.Collection;
-
-import com.google.common.base.Joiner;
 
 /**
  * {@link DeclarationEnricher} which looks at classes with fields annotated with {@link RuntimeVersion}. It validates
@@ -41,62 +35,19 @@ import com.google.common.base.Joiner;
  *
  * @since 4.4
  */
-public class RuntimeVersionDeclarationEnricher implements DeclarationEnricher {
+public class RuntimeVersionDeclarationEnricher extends AbstractAnnotatedFieldDeclarationEnricher {
 
   @Override
-  public DeclarationEnricherPhase getExecutionPhase() {
-    return POST_STRUCTURE;
-  }
-
-  @Override
-  public void enrich(ExtensionLoadingContext extensionLoadingContext) {
-    new IdempotentDeclarationWalker() {
-
-      @Override
-      public void onConfiguration(ConfigurationDeclaration declaration) {
-        doEnrich(declaration);
-      }
-
-      @Override
-      protected void onConnectionProvider(ConnectionProviderDeclaration declaration) {
-        doEnrich(declaration);
-      }
-
-      @Override
-      protected void onSource(SourceDeclaration declaration) {
-        doEnrich(declaration);
-      }
-
-    }.walk(extensionLoadingContext.getExtensionDeclarer().getDeclaration());
-  }
-
-  private void doEnrich(BaseDeclaration declaration) {
-    declaration.getModelProperty(ImplementingTypeModelProperty.class).ifPresent(p -> {
-      ImplementingTypeModelProperty typeProperty = (ImplementingTypeModelProperty) p;
+  protected void doEnrich(BaseDeclaration<?> declaration) {
+    declaration.getModelProperty(ImplementingTypeModelProperty.class).ifPresent(typeProperty -> {
       Collection<Field> fields = getAllFields(typeProperty.getType(), withAnnotation(RuntimeVersion.class));
       if (isEmpty(fields)) {
         return;
       }
 
-      if (fields.size() > 1) {
-        throw new IllegalConfigurationModelDefinitionException(format("Only one field is allowed to be annotated with @%s, but class '%s' has %d fields "
-            + "with such annotation. Offending fields are: [%s]", RuntimeVersion.class.getSimpleName(),
-                                                                      typeProperty.getType().getName(),
-                                                                      fields.size(), Joiner.on(", ").join(fields.stream()
-                                                                          .map(Field::getName).collect(toList()))));
-      }
+      validate(fields, typeProperty, RuntimeVersion.class, MuleVersion.class);
 
-      final Field runtimeVersionField = fields.iterator().next();
-      if (!MuleVersion.class.equals(runtimeVersionField.getType())) {
-        throw new IllegalConfigurationModelDefinitionException(format("Class '%s' declares the field '%s' which is annotated with @%s and is of type '%s'. Only "
-            + "fields of type %s are allowed to carry such annotation", typeProperty.getType().getName(),
-                                                                      runtimeVersionField.getName(),
-                                                                      RuntimeVersion.class.getSimpleName(),
-                                                                      runtimeVersionField.getType().getName(),
-                                                                      MuleVersion.class));
-      }
-
-      declaration.addModelProperty(new RuntimeVersionModelProperty(runtimeVersionField));
+      declaration.addModelProperty(new RuntimeVersionModelProperty(fields.iterator().next()));
     });
   }
 }
