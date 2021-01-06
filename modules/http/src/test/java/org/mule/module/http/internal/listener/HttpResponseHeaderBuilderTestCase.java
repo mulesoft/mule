ec53a4246a2f1c552c7aabfaa1d92219be8106eb
Juan Grethe <jgrethe@itba.edu.ar>
2021-01-06 11:15:28 -0300

    MULE-19107: fix null pointer exception when a header is null (#9923)
    * MULE-19107: fix null pointer exception when a header is null
    
    * test
    
    * format
    
    * change warn
    
    (cherry picked from commit 205168d0eea00830c2700e34eb8ab5dcef19c3ca)

---
+++ modules/http/src/test/java/org/mule/module/http/internal/listener/HttpResponseHeaderBuilderTestCase.java
@@ -0,0 +1,16 @@
+package org.mule.module.http.internal.listener;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class HttpResponseHeaderBuilderTestCase {
+
+    @Test
+    public void testNullHeaderValueIsIgnored()
+    {
+        HttpResponseHeaderBuilder httpResponseHeaderBuilder = new HttpResponseHeaderBuilder();
+        httpResponseHeaderBuilder.addHeader("header", null);
+        assertTrue(httpResponseHeaderBuilder.getHeaderNames().isEmpty());
+    }
+}
