7418b7f33e05926e51b0bbfd1450c9437bd098e9
Martín Buchwald <martin.buchwald@mulesoft.com>
2021-01-13 16:06:27 -0300

    MULE-19076: Log warning when remove variable component tries to remove a non existing variable (#9877)

--- core/src/main/java/org/mule/runtime/core/internal/processor/simple/RemoveFlowVariableProcessor.java
+++ core/src/main/java/org/mule/runtime/core/internal/processor/simple/RemoveFlowVariableProcessor.java
@@ -6,14 +6,18 @@
  */
 package org.mule.runtime.core.internal.processor.simple;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 import org.mule.runtime.core.privileged.event.PrivilegedEvent;
 import org.mule.runtime.core.privileged.processor.simple.AbstractRemoveVariablePropertyProcessor;
+import org.slf4j.Logger;
 
 import java.util.Set;
 
 public class RemoveFlowVariableProcessor extends AbstractRemoveVariablePropertyProcessor {
 
   private static final String FLOW_VAR_NAME = "flow variables";
+  private static final Logger LOGGER = getLogger(RemoveFlowVariableProcessor.class);
 
   @Override
   protected Set<String> getPropertyNames(PrivilegedEvent event) {
@@ -22,7 +26,13 @@ public class RemoveFlowVariableProcessor extends AbstractRemoveVariablePropertyP
 
   @Override
   protected PrivilegedEvent removeProperty(PrivilegedEvent event, String propertyName) {
-    return PrivilegedEvent.builder(event).removeVariable(propertyName).build();
+    if (event.getVariables().containsKey(propertyName)) {
+      return PrivilegedEvent.builder(event).removeVariable(propertyName).build();
+    } else {
+      LOGGER.warn("There is no variable named '%s'. Check the 'variableName' parameter in the 'remove-variable' component at %s",
+                  propertyName, this.getLocation().getLocation());
+      return event;
+    }
   }
 
   @Override
