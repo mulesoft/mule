1410b5d76abc657588911c360ba8bcfbbb9a0a75
Mariano Lischetti <lischettim@gmail.com>
2021-01-29 15:38:25 -0300

    MULE-19143: Use InternalEventContext to track foreach context (#10016)

---
+++ tests/performance/src/test/java/org/mule/test/core/internal/routing/ForeachTestCase.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.core.internal.routing;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+import org.junit.Test;
+import org.mule.AbstractBenchmarkAssertionTestCase;
+import org.mule.runtime.core.internal.routing.ForeachBenchmark;
+
+public class ForeachTestCase extends AbstractBenchmarkAssertionTestCase {
+
+  @Test
+  public void singleForeach() {
+    runAndAssertBenchmark(ForeachBenchmark.class, "singleForeach", 1000, MILLISECONDS);
+  }
+
+  @Test
+  public void nestedForeach() {
+    runAndAssertBenchmark(ForeachBenchmark.class, "nestedForeach", 6000, MILLISECONDS);
+  }
+
+  @Test
+  public void multiplesThreadsUsingSameForeach() {
+    runAndAssertBenchmark(ForeachBenchmark.class, "multiplesThreadsUsingSameForeach", 3000, MILLISECONDS);
+  }
+}
