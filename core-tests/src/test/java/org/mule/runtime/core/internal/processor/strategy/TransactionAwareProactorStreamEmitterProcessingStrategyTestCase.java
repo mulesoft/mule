67c3c3c6a5f9e5551b8ed02275226c3f81e127c7
Martín Buchwald <martin.buchwald@mulesoft.com>
2021-02-09 15:37:09 -0300

    MULE-19209: StreamPerThreadSink is holding a strong reference to thread (#10079)

--- core-tests/src/test/java/org/mule/runtime/core/internal/processor/strategy/TransactionAwareProactorStreamEmitterProcessingStrategyTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/processor/strategy/TransactionAwareProactorStreamEmitterProcessingStrategyTestCase.java
@@ -7,8 +7,10 @@
 package org.mule.runtime.core.internal.processor.strategy;
 
 import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Thread.currentThread;
 import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
@@ -17,16 +19,25 @@ import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.mule.runtime.core.api.transaction.TransactionCoordination.getInstance;
 import static org.mule.runtime.core.internal.processor.strategy.AbstractProcessingStrategyTestCase.Mode.FLOW;
 import static org.mule.runtime.core.internal.processor.strategy.AbstractProcessingStrategyTestCase.Mode.SOURCE;
+import static org.mule.tck.junit4.matcher.Eventually.eventually;
+import static org.mule.tck.util.CollectableReference.collectedByGc;
 import static org.mule.tck.util.MuleContextUtils.getNotificationDispatcher;
 import static org.mule.test.allure.AllureConstants.ProcessingStrategiesFeature.PROCESSING_STRATEGIES;
 import static org.mule.test.allure.AllureConstants.ProcessingStrategiesFeature.ProcessingStrategiesStory.DEFAULT;
 import static reactor.util.concurrent.Queues.XS_BUFFER_SIZE;
 
+import io.qameta.allure.Issue;
+import org.junit.Test;
+import org.mule.runtime.api.exception.MuleException;
+import org.mule.runtime.api.util.concurrent.Latch;
 import org.mule.runtime.core.api.MuleContext;
+import org.mule.runtime.core.api.event.CoreEvent;
+import org.mule.runtime.core.api.processor.Processor;
 import org.mule.runtime.core.api.processor.strategy.ProcessingStrategy;
 import org.mule.runtime.core.internal.processor.strategy.AbstractProcessingStrategyTestCase.TransactionAwareProcessingStrategyTestCase;
 import org.mule.runtime.core.internal.processor.strategy.ProactorStreamEmitterProcessingStrategyFactory.ProactorStreamEmitterProcessingStrategy;
 import org.mule.tck.testmodels.mule.TestTransaction;
+import org.mule.tck.util.CollectableReference;
 
 import java.util.Collection;
 
@@ -111,6 +122,43 @@ public class TransactionAwareProactorStreamEmitterProcessingStrategyTestCase
     assertThat(threads, not(hasItem(startsWith(IO))));
     assertThat(threads, not(hasItem(startsWith(CPU_INTENSIVE))));
     assertThat(threads, not(hasItem(startsWith(CUSTOM))));
+  }
+
+  @Test
+  @Issue("MULE-19209")
+  public void txDoesNotLeakThread() throws Exception {
+    ThreadReferenceCaptor captor = new ThreadReferenceCaptor();
+    flow = flowBuilder.get().processors(captor).build();
+
+    startFlow();
+    // we cannot do a thread.join because we must not set a strong reference
+    Latch latch = new Latch();
+
+    asyncExecutor.submit(() -> {
+      try {
+        getInstance().bindTransaction(new TestTransaction("appName", getNotificationDispatcher(muleContext)));
+        processFlow(testEvent());
+        threads.clear();
+        latch.release();
+      } catch (Exception e) {
+      }
+    });
+
+    latch.await();
+    asyncExecutor.stop();
+
+    assertThat(captor.reference, is(eventually(collectedByGc())));
+  }
+
+  public static class ThreadReferenceCaptor implements Processor {
+
+    public CollectableReference<Thread> reference;
+
+    @Override
+    public CoreEvent process(CoreEvent event) throws MuleException {
+      reference = new CollectableReference<>(currentThread());
+      return event;
+    }
   }
 
 }
