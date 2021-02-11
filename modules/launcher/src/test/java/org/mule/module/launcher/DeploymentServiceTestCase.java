6cb121c086913875e237c0580e82edb7af831ef7
sofiamorseletto <sofimorseletto@gmail.com>
2021-02-11 16:10:09 -0300

    Fix

--- modules/launcher/src/test/java/org/mule/module/launcher/DeploymentServiceTestCase.java
+++ modules/launcher/src/test/java/org/mule/module/launcher/DeploymentServiceTestCase.java
@@ -147,7 +147,6 @@ public class DeploymentServiceTestCase extends AbstractMuleContextTestCase
                 {
                         {false},
                         {true},
-                        {true},
                 });
     }
 
