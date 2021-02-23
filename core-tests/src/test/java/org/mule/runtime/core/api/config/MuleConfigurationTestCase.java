5ec13f6601b217c065ea1ff389711a2d32a77d00
Rodro <rodrigo.merino@mulesoft.com>
2021-02-23 09:07:39 -0300

    MULE-19229: Remove lazyInit concept from MuleConfiguration

---
+++ core-tests/src/test/java/org/mule/runtime/core/api/config/MuleConfigurationTestCase.java
@@ -0,0 +1,195 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.api.config;
+
+import static java.lang.System.clearProperty;
+import static java.lang.System.setProperty;
+import static java.util.Arrays.asList;
+import static java.util.Optional.empty;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mule.runtime.api.util.MuleSystemProperties.SYSTEM_PROPERTY_PREFIX;
+import static org.mule.runtime.core.api.config.bootstrap.ArtifactType.APP;
+
+import org.mule.runtime.api.component.ConfigurationProperties;
+import org.mule.runtime.core.api.MuleContext;
+import org.mule.runtime.core.api.context.DefaultMuleContextFactory;
+import org.mule.runtime.core.api.context.MuleContextBuilder;
+import org.mule.runtime.core.internal.config.builders.DefaultsConfigurationBuilder;
+import org.mule.tck.config.TestServicesConfigurationBuilder;
+import org.mule.tck.junit4.AbstractMuleTestCase;
+import org.mule.tck.size.SmallTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+@SmallTest
+public class MuleConfigurationTestCase extends AbstractMuleTestCase {
+
+  @Rule
+  public TestServicesConfigurationBuilder testServicesConfigurationBuilder = new TestServicesConfigurationBuilder();
+
+  private final ConfigurationProperties mockedConfigurationProperties = mock(ConfigurationProperties.class);
+
+  protected String workingDirectory = "target";
+  private MuleContext muleContext;
+
+  @Before
+  public void setUp() {
+    when(mockedConfigurationProperties.resolveStringProperty(anyString())).thenReturn(empty());
+    testServicesConfigurationBuilder.registerAdditionalService("mockedConfigurationProperties", mockedConfigurationProperties);
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    muleContext.dispose();
+    muleContext = null;
+  }
+
+  /** Test for MULE-3092 */
+  @Test
+  public void testConfigureProgramatically() throws Exception {
+    DefaultMuleConfiguration config = new DefaultMuleConfiguration();
+    config.setDefaultEncoding("UTF-16");
+    config.setDefaultSynchronousEndpoints(true);
+    config.setSystemModelType("direct");
+    config.setDefaultResponseTimeout(30000);
+    config.setDefaultTransactionTimeout(60000);
+    config.setWorkingDirectory(workingDirectory);
+    config.setClientMode(true);
+    config.setId("MY_SERVER");
+    config.setDomainId("MY_DOMAIN");
+    config.setCacheMessageAsBytes(false);
+    config.setEnableStreaming(false);
+    config.setMaxQueueTransactionFilesSize(500);
+    config.setAutoWrapMessageAwareTransform(false);
+
+    MuleContextBuilder contextBuilder = MuleContextBuilder.builder(APP);
+    contextBuilder.setMuleConfiguration(config);
+    muleContext = new DefaultMuleContextFactory()
+        .createMuleContext(asList(testServicesConfigurationBuilder, new DefaultsConfigurationBuilder()), contextBuilder);
+    muleContext.start();
+
+    verifyConfiguration();
+  }
+
+  /** Test for MULE-3092 */
+  @Test
+  public void testConfigureWithSystemProperties() throws Exception {
+    setProperty(SYSTEM_PROPERTY_PREFIX + "encoding", "UTF-16");
+    setProperty(SYSTEM_PROPERTY_PREFIX + "endpoints.synchronous", "true");
+    setProperty(SYSTEM_PROPERTY_PREFIX + "systemModelType", "direct");
+    setProperty(SYSTEM_PROPERTY_PREFIX + "timeout.synchronous", "30000");
+    setProperty(SYSTEM_PROPERTY_PREFIX + "timeout.transaction", "60000");
+    setProperty(SYSTEM_PROPERTY_PREFIX + "remoteSync", "true");
+    setProperty(SYSTEM_PROPERTY_PREFIX + "workingDirectory", workingDirectory);
+    setProperty(SYSTEM_PROPERTY_PREFIX + "clientMode", "true");
+
+    setProperty(SYSTEM_PROPERTY_PREFIX + "serverId", "MY_SERVER");
+    setProperty(SYSTEM_PROPERTY_PREFIX + "domainId", "MY_DOMAIN");
+    setProperty(SYSTEM_PROPERTY_PREFIX + "message.cacheBytes", "false");
+    setProperty(SYSTEM_PROPERTY_PREFIX + "message.cacheOriginal", "false");
+    setProperty(SYSTEM_PROPERTY_PREFIX + "streaming.enable", "false");
+    setProperty(SYSTEM_PROPERTY_PREFIX + "message.assertAccess", "false");
+    setProperty(SYSTEM_PROPERTY_PREFIX + "transform.autoWrap", "false");
+
+    muleContext = new DefaultMuleContextFactory()
+        .createMuleContext(testServicesConfigurationBuilder, new DefaultsConfigurationBuilder());
+    muleContext.start();
+
+    verifyConfiguration();
+
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "encoding");
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "endpoints.synchronous");
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "systemModelType");
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "timeout.synchronous");
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "timeout.transaction");
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "remoteSync");
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "workingDirectory");
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "clientMode");
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "disable.threadsafemessages");
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "serverId");
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "domainId");
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "message.cacheBytes");
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "message.cacheOriginal");
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "streaming.enable");
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "message.assertAccess");
+    clearProperty(SYSTEM_PROPERTY_PREFIX + "transform.autoWrap");
+  }
+
+  /** Test for MULE-3110 */
+  @Test
+  public void testConfigureAfterInitFails() throws Exception {
+    muleContext = new DefaultMuleContextFactory()
+        .createMuleContext(testServicesConfigurationBuilder, new DefaultsConfigurationBuilder());
+
+    DefaultMuleConfiguration mutableConfig = ((DefaultMuleConfiguration) muleContext.getConfiguration());
+
+    // These are OK to change after init but before start
+    mutableConfig.setDefaultSynchronousEndpoints(true);
+    mutableConfig.setSystemModelType("direct");
+    mutableConfig.setClientMode(true);
+
+    // These are not OK to change after init
+    mutableConfig.setDefaultEncoding("UTF-16");
+    mutableConfig.setWorkingDirectory(workingDirectory);
+    mutableConfig.setId("MY_SERVER");
+    mutableConfig.setDomainId("MY_DOMAIN");
+
+    MuleConfiguration config = muleContext.getConfiguration();
+
+    // These are OK to change after init but before start
+    assertEquals("direct", config.getSystemModelType());
+    assertTrue(config.isClientMode());
+
+    // These are not OK to change after init
+    assertFalse("UTF-16".equals(config.getDefaultEncoding()));
+    assertFalse(workingDirectory.equals(config.getWorkingDirectory()));
+    assertFalse("MY_SERVER".equals(config.getId()));
+    assertFalse("MY_DOMAIN".equals(config.getDomainId()));
+  }
+
+  /** Test for MULE-3110 */
+  @Test
+  public void testConfigureAfterStartFails() throws Exception {
+    muleContext = new DefaultMuleContextFactory().createMuleContext(testServicesConfigurationBuilder,
+                                                                    new DefaultsConfigurationBuilder());
+    muleContext.start();
+
+    DefaultMuleConfiguration mutableConfig = ((DefaultMuleConfiguration) muleContext.getConfiguration());
+    mutableConfig.setDefaultSynchronousEndpoints(true);
+    mutableConfig.setSystemModelType("direct");
+    mutableConfig.setClientMode(true);
+
+    MuleConfiguration config = muleContext.getConfiguration();
+    assertFalse("direct".equals(config.getSystemModelType()));
+    assertFalse(config.isClientMode());
+  }
+
+  protected void verifyConfiguration() {
+    MuleConfiguration config = muleContext.getConfiguration();
+    assertEquals("UTF-16", config.getDefaultEncoding());
+    assertEquals("direct", config.getSystemModelType());
+    // on windows this ends up with a c:/ in it
+    assertTrue(config.getWorkingDirectory().indexOf(workingDirectory) != -1);
+    assertTrue(config.isClientMode());
+    assertEquals("MY_SERVER", config.getId());
+    assertEquals("MY_DOMAIN", config.getDomainId());
+    assertFalse(config.isCacheMessageAsBytes());
+    assertFalse(config.isEnableStreaming());
+    assertFalse(config.isAutoWrapMessageAwareTransform());
+    assertThat(config.getMaxQueueTransactionFilesSizeInMegabytes(), is(500));
+  }
+}
