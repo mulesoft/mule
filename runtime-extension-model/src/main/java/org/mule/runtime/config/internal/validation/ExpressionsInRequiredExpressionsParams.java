5973bdbe6577e720fd3d925272f43444c89d46e8
Martín Buchwald <martin.buchwald@mulesoft.com>
2021-02-24 17:14:41 -0300

    test fixing

--- runtime-extension-model/src/main/java/org/mule/runtime/config/internal/validation/ExpressionsInRequiredExpressionsParams.java
+++ runtime-extension-model/src/main/java/org/mule/runtime/config/internal/validation/ExpressionsInRequiredExpressionsParams.java
@@ -9,10 +9,14 @@ package org.mule.runtime.config.internal.validation;
 import static java.lang.String.format;
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
+import static org.mule.runtime.api.component.ComponentIdentifier.builder;
 import static org.mule.runtime.api.meta.ExpressionSupport.REQUIRED;
 import static org.mule.runtime.ast.api.util.ComponentAstPredicatesFactory.currentElemement;
+import static org.mule.runtime.ast.api.util.ComponentAstPredicatesFactory.equalsIdentifier;
 import static org.mule.runtime.ast.api.validation.Validation.Level.ERROR;
+import static org.mule.runtime.internal.dsl.DslConstants.CORE_PREFIX;
 
+import org.mule.runtime.api.component.ComponentIdentifier;
 import org.mule.runtime.api.meta.model.parameter.ParameterizedModel;
 import org.mule.runtime.ast.api.ArtifactAst;
 import org.mule.runtime.ast.api.ComponentAst;
@@ -30,6 +34,10 @@ public class ExpressionsInRequiredExpressionsParams implements Validation {
 
   private static final String DEFAULT_EXPRESSION_PREFIX = "#[";
   private static final String DEFAULT_EXPRESSION_SUFFIX = "]";
+  private static final String CONFIGURATION_NAME = "configuration";
+
+  private static final ComponentIdentifier CONFIGURATION_IDENTIFIER =
+      builder().namespace(CORE_PREFIX).name(CONFIGURATION_NAME).build();
 
   @Override
   public String getName() {
@@ -48,7 +56,11 @@ public class ExpressionsInRequiredExpressionsParams implements Validation {
 
   @Override
   public Predicate<List<ComponentAst>> applicable() {
-    return currentElemement(component -> component.getModel(ParameterizedModel.class).isPresent());
+    return currentElemement(component -> component.getModel(ParameterizedModel.class).isPresent())
+        // According to the extension model, no collections or target-value for foreach, parallel-foreach, etc...
+        // must be defined by an expression, but this was not enforced. This check is needed to avoid breaking on
+        // legacy cases
+        .and(currentElemement(equalsIdentifier(CONFIGURATION_IDENTIFIER)));
   }
 
   @Override
