91bcddc87a56e82669f387801be6322f97fab7c7
sofiamorseletto <sofimorseletto@gmail.com>
2021-02-24 16:28:23 -0300

    MULE-19166: Remove appStatusPreRedeployment property in DomainDeploymentTemplate since it's no longer necessary (#10126)
    * Adding test
    
    * Reformatting curly brackets
    
    * Removing no longer necessary methods
    
    (cherry picked from commit 67ad2f85e1d85c5f55501fe1ecbda5ca6c165e20)

--- modules/launcher/src/main/java/org/mule/module/launcher/DefaultArchiveDeployer.java
+++ modules/launcher/src/main/java/org/mule/module/launcher/DefaultArchiveDeployer.java
@@ -7,19 +7,15 @@
 package org.mule.module.launcher;
 
 import static com.google.common.base.Optional.absent;
-import static java.lang.Boolean.valueOf;
 import static org.apache.commons.lang.StringUtils.removeEndIgnoreCase;
 import static org.mule.module.launcher.DeploymentPropertiesUtils.resolveDeploymentProperties;
 import static org.mule.util.SplashScreen.miniSplash;
 
 import org.mule.config.i18n.CoreMessages;
 import org.mule.config.i18n.MessageFactory;
-import org.mule.module.launcher.application.Application;
-import org.mule.module.launcher.application.ApplicationStatus;
 import org.mule.module.launcher.application.NullDeploymentListener;
 import org.mule.module.launcher.artifact.Artifact;
 import org.mule.module.launcher.artifact.ArtifactFactory;
-import org.mule.module.launcher.domain.Domain;
 import org.mule.module.launcher.util.ObservableList;
 import org.mule.util.CollectionUtils;
 
@@ -283,29 +279,15 @@ public class DefaultArchiveDeployer<T extends Artifact> implements ArchiveDeploy
 
         // check if this artifact is running first, undeployArtifact it then
         T artifact = (T) CollectionUtils.find(artifacts, new BeanPropertyValueEqualsPredicate(ARTIFACT_NAME_PROPERTY, artifactName));
-        Map<Application, ApplicationStatus> appStatusPreRedeployment = new HashMap<>();
         if (artifact != null)
         {
-            if (artifact instanceof Domain){
-                Collection<Application> domainApplications = findApplicationsAssociated((Domain) artifact);
-                for (Application domainApplication : domainApplications)
-                {
-                    appStatusPreRedeployment.put(domainApplication, domainApplication.getStatus());
-                }
-            }
             deploymentTemplate.preRedeploy(artifact);
             undeployArtifact(artifactName);
         }
 
-        T deployedAtifact = deployPackagedArtifact(artifactUrl, deploymentProperties);
-        deploymentTemplate.postRedeploy(deployedAtifact, appStatusPreRedeployment);
-        return deployedAtifact;
-    }
-
-
-    private Collection<Application> findApplicationsAssociated(Domain domain)
-    {
-        return deploymentService.findDomainApplications(domain.getArtifactName());
+        T deployedArtifact = deployPackagedArtifact(artifactUrl, deploymentProperties);
+        deploymentTemplate.postRedeploy(deployedArtifact);
+        return deployedArtifact;
     }
 
     private T deployExplodedApp(String addedApp, Optional<Properties> properties) throws DeploymentException
@@ -576,7 +558,7 @@ public class DefaultArchiveDeployer<T extends Artifact> implements ArchiveDeploy
             trackArtifact(artifact);
 
             deploymentListener.onDeploymentStart(artifact.getArtifactName());
-            deployer.deploy(artifact, shouldStartArtifact(artifact, deploymentProperties.orNull()));
+            deployer.deploy(artifact);
             artifactArchiveInstaller.createAnchorFile(artifact.getArtifactName());
             deploymentListener.onDeploymentSuccess(artifact.getArtifactName());
             artifactZombieMap.remove(artifact.getArtifactName());
@@ -600,16 +582,6 @@ public class DefaultArchiveDeployer<T extends Artifact> implements ArchiveDeploy
                 throw new DeploymentException(MessageFactory.createStaticMessage(msg), t);
             }
         }
-    }
-
-    private boolean shouldStartArtifact(T artifact, Properties deploymentProperties)
-    {
-        if (!(artifact instanceof Application) || deploymentProperties == null)
-        {
-            return true;
-        }
-
-        return valueOf(deploymentProperties.getProperty(START_ARTIFACT_ON_DEPLOYMENT_PROPERTY, "true"));
     }
 
     @Override
