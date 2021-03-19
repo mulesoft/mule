f1fa85daf165ef1099097dc04867f6c3c4c9fec7
Rodrigo Merino <elrodro83@users.noreply.github.com>
2021-03-19 09:38:37 -0300

    MULE-19271: Cannot use properties (`${}`) in `configuration` element (#10194)

--- runtime-extension-model/src/main/java/org/mule/runtime/config/internal/validation/ExpressionsInRequiredExpressionsParams.java
+++ runtime-extension-model/src/main/java/org/mule/runtime/config/internal/validation/ExpressionsInRequiredExpressionsParams.java
@@ -7,21 +7,18 @@
 package org.mule.runtime.config.internal.validation;
 
 import static java.lang.String.format;
-import static java.util.Optional.empty;
-import static java.util.Optional.of;
+import static java.util.Collections.emptyList;
 import static org.mule.runtime.api.component.ComponentIdentifier.builder;
 import static org.mule.runtime.api.meta.ExpressionSupport.REQUIRED;
 import static org.mule.runtime.ast.api.util.ComponentAstPredicatesFactory.currentElemement;
 import static org.mule.runtime.ast.api.util.ComponentAstPredicatesFactory.equalsIdentifier;
 import static org.mule.runtime.ast.api.validation.Validation.Level.ERROR;
-import static org.mule.runtime.ast.api.validation.Validation.Level.WARN;
 import static org.mule.runtime.internal.dsl.DslConstants.CORE_PREFIX;
 
 import org.mule.runtime.api.component.ComponentIdentifier;
 import org.mule.runtime.api.meta.model.parameter.ParameterizedModel;
 import org.mule.runtime.ast.api.ArtifactAst;
 import org.mule.runtime.ast.api.ComponentAst;
-import org.mule.runtime.ast.api.ComponentParameterAst;
 import org.mule.runtime.ast.api.validation.Validation;
 
 import java.util.List;
@@ -66,20 +63,27 @@ public class ExpressionsInRequiredExpressionsParams implements Validation {
 
   @Override
   public Optional<String> validate(ComponentAst component, ArtifactAst artifact) {
-    for (ComponentParameterAst param : component.getParameters()) {
-      if (param.getValue().isRight() && param.getValue().getRight() instanceof String) {
-        final String stringValue = (String) param.getValue().getRight();
+    return component.getModel(ParameterizedModel.class)
+        .map(pmz -> pmz.getAllParameterModels())
+        .orElse(emptyList())
+        .stream()
+        .filter(pm -> REQUIRED.equals(pm.getExpressionSupport()))
+        .map(pm -> component.getParameter(pm.getName()))
+        .filter(param -> {
+          if (param.getValue().isRight() && param.getValue().getRight() instanceof String) {
+            final String stringValue = (String) param.getValue().getRight();
 
-        if (REQUIRED.equals(param.getModel().getExpressionSupport())
-            && (!stringValue.startsWith(DEFAULT_EXPRESSION_PREFIX)
-                || !stringValue.endsWith(DEFAULT_EXPRESSION_SUFFIX))) {
-          return of(format("A static value was given for parameter '%s' but it requires a expression",
-                           param.getModel().getName()));
-        }
-      }
-    }
+            if (!stringValue.startsWith(DEFAULT_EXPRESSION_PREFIX)
+                || !stringValue.endsWith(DEFAULT_EXPRESSION_SUFFIX)) {
+              return true;
+            }
+          }
 
-    return empty();
+          return false;
+        })
+        .map(param -> format("A static value ('%s') was given for parameter '%s' but it requires an expression",
+                             param.getRawValue(), param.getModel().getName()))
+        .findFirst();
   }
 
 }
