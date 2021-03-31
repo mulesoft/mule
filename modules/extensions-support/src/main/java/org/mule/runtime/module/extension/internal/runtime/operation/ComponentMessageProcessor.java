ca04a9700cc09588ed0b6f37b50211c1f408eaa8
Ezequiel Werner <eze210@gmail.com>
2021-03-31 17:31:39 -0300

    fix n test

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/operation/ComponentMessageProcessor.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/operation/ComponentMessageProcessor.java
@@ -37,6 +37,7 @@ import static org.mule.runtime.core.internal.processor.strategy.AbstractProcessi
 import static org.mule.runtime.core.internal.util.rx.ImmediateScheduler.IMMEDIATE_SCHEDULER;
 import static org.mule.runtime.core.internal.util.rx.RxUtils.createRoundRobinFluxSupplier;
 import static org.mule.runtime.core.internal.util.rx.RxUtils.propagateCompletion;
+import static org.mule.runtime.core.privileged.event.PrivilegedEvent.setCurrentEvent;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.WITHIN_PROCESS_TO_APPLY;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.createDefaultProcessingStrategyFactory;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.getProcessingStrategy;
@@ -104,6 +105,7 @@ import org.mule.runtime.core.internal.processor.strategy.OperationInnerProcessor
 import org.mule.runtime.core.internal.rx.FluxSinkRecorder;
 import org.mule.runtime.core.internal.util.rx.FluxSinkSupplier;
 import org.mule.runtime.core.privileged.event.BaseEventContext;
+import org.mule.runtime.core.privileged.event.PrivilegedEvent;
 import org.mule.runtime.core.privileged.exception.ErrorTypeLocator;
 import org.mule.runtime.core.privileged.exception.EventProcessingException;
 import org.mule.runtime.core.privileged.processor.chain.MessageProcessorChain;
@@ -167,6 +169,7 @@ import javax.inject.Inject;
 import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 
+import org.slf4j.MDC;
 import reactor.core.publisher.Flux;
 import reactor.util.context.Context;
 
@@ -783,8 +786,39 @@ public abstract class ComponentMessageProcessor<T extends ComponentModel> extend
 
     ExecutionContextAdapter<T> operationContext = oep.getExecutionContextAdapter();
 
-    executeOperation(operationContext, mapped(callbackSupplier.get(), operationContext,
-                                              isTargetWithPolicies(event) ? valueReturnDelegate : returnDelegate));
+    setCurrentEvent((PrivilegedEvent) event);
+    boolean wasProcessorPathSet = setCurrentLocation();
+    try {
+      executeOperation(operationContext, mapped(callbackSupplier.get(), operationContext,
+                                                isTargetWithPolicies(event) ? valueReturnDelegate : returnDelegate));
+    } finally {
+      unsetCurrentLocation(wasProcessorPathSet);
+    }
+  }
+
+  private boolean setCurrentLocation() {
+    ComponentLocation componentLocation = getLocation();
+    if (componentLocation == null) {
+      return false;
+    }
+
+    String processorPath = componentLocation.getLocation();
+    if (processorPath == null) {
+      return false;
+    }
+
+    if (MDC.get("processorPath") != null) {
+      return false;
+    }
+
+    MDC.put("processorPath", processorPath);
+    return true;
+  }
+
+  private void unsetCurrentLocation(boolean wasProcessorPathSet) {
+    if (wasProcessorPathSet) {
+      MDC.remove("processorPath");
+    }
   }
 
   private void initRetryPolicyResolver() {
--- modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/runtime/operation/OperationMessageProcessorTestCase.java
+++ modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/runtime/operation/OperationMessageProcessorTestCase.java
@@ -56,7 +56,6 @@ import static org.mule.tck.MuleTestUtils.stubComponentExecutor;
 import static org.mule.tck.junit4.matcher.DataTypeMatcher.like;
 import static org.mule.tck.util.MuleContextUtils.registerIntoMockContext;
 import static org.mule.test.module.extension.internal.util.ExtensionsTestUtils.toMetadataType;
-
 import org.mule.metadata.api.model.MetadataType;
 import org.mule.runtime.api.el.DefaultExpressionLanguageFactoryService;
 import org.mule.runtime.api.event.EventContext;
@@ -90,6 +89,7 @@ import org.mule.runtime.core.privileged.processor.chain.MessageProcessorChain;
 import org.mule.runtime.extension.api.declaration.type.DefaultExtensionsTypeLoaderFactory;
 import org.mule.runtime.extension.api.model.ImmutableOutputModel;
 import org.mule.runtime.extension.api.runtime.config.ConfigurationProvider;
+import org.mule.runtime.extension.api.runtime.operation.CompletableComponentExecutor;
 import org.mule.runtime.extension.api.runtime.operation.ExecutionContext;
 import org.mule.runtime.extension.api.runtime.operation.Result;
 import org.mule.runtime.module.extension.api.runtime.privileged.ExecutionContextAdapter;
@@ -101,6 +101,8 @@ import org.mule.runtime.module.extension.internal.runtime.resolver.ValueResolvin
 import org.mule.runtime.module.extension.internal.util.ReflectionCache;
 import org.mule.tck.size.SmallTest;
 import org.mule.weave.v2.el.WeaveDefaultExpressionLanguageFactoryService;
+
+import com.google.common.reflect.TypeToken;
 
 import java.io.InputStream;
 import java.lang.reflect.Type;
@@ -116,8 +118,7 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-
-import com.google.common.reflect.TypeToken;
+import org.slf4j.MDC;
 
 @SmallTest
 public class OperationMessageProcessorTestCase extends AbstractOperationMessageProcessorTestCase {
@@ -493,6 +494,17 @@ public class OperationMessageProcessorTestCase extends AbstractOperationMessageP
   }
 
   @Test
+  public void operationExecutionHasCorrelationIdAndProcessorPathInMDC() throws MuleException {
+    String expectedCorrelationId = event.getCorrelationId();
+    String expectedProcessorPath = messageProcessor.getLocation().getLocation();
+
+    messageProcessor.process(event);
+    Map<String, String> mdc = ((TestOperationMessageProcessor) messageProcessor).getLastOperationExecutionMDC();
+    assertThat(mdc.get("correlationId"), is(expectedCorrelationId));
+    assertThat(mdc.get("processorPath"), is(expectedProcessorPath));
+  }
+
+  @Test
   public void cursorStreamProvidersAreManaged() throws Exception {
     CursorStreamProvider provider = mock(CursorStreamProvider.class);
     final InputStream inputStream = mock(InputStream.class);
@@ -549,6 +561,7 @@ public class OperationMessageProcessorTestCase extends AbstractOperationMessageP
   private static class TestOperationMessageProcessor extends OperationMessageProcessor {
 
 
+    private Map<String, String> lastOperationExecutionMDC;
 
     public TestOperationMessageProcessor(ExtensionModel extensionModel, OperationModel operationModel,
                                          ConfigurationProvider configurationProvider, String target, String targetValue,
@@ -565,6 +578,17 @@ public class OperationMessageProcessorTestCase extends AbstractOperationMessageP
     @Override
     protected boolean honourOperationRetryPolicyOverride() {
       return true;
+    }
+
+    @Override
+    protected void executeOperation(ExecutionContextAdapter<OperationModel> operationContext,
+                                    CompletableComponentExecutor.ExecutorCallback callback) {
+      lastOperationExecutionMDC = MDC.getCopyOfContextMap();
+      super.executeOperation(operationContext, callback);
+    }
+
+    public Map<String, String> getLastOperationExecutionMDC() {
+      return lastOperationExecutionMDC;
     }
   }
 }
