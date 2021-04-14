b04aa515055448ab8eb3b2ad3d7815b2f09879f8
Nicolás Zdanovicz <nzdanovicz@mulesoft.com>
2021-04-14 10:05:03 -0300

    MULE-19327: Added support for conditional closing of the connection f… (#10259)
    * MULE-19327: Added support for conditional closing of the connection and connection factory on stop

--- core/src/main/java/org/mule/api/config/MuleProperties.java
+++ core/src/main/java/org/mule/api/config/MuleProperties.java
@@ -207,6 +207,7 @@ public class MuleProperties
 
     public static final String MULE_JMS_REDELIVERY_DELAY = SYSTEM_PROPERTY_PREFIX + "jms.redeliveryDelay";
     public static final String MULE_JMS_MAX_REDELIVERY_DELAY = SYSTEM_PROPERTY_PREFIX + "jms.maxRedeliveryDelay";
+    public static final String MULE_JMS_CLOSE_CONNECTION_ON_STOP=SYSTEM_PROPERTY_PREFIX + "jms.closeConnectionOnStop";
     public static final String MULE_JMS_INITIAL_REDELIVERY_DELAY = SYSTEM_PROPERTY_PREFIX + "jms.initialRedeliveryDelay";
     public static final String MULE_XML_RESET_CONTROLLER_AFTER_EACH_TRANSFORMATION = SYSTEM_PROPERTY_PREFIX + "xml.resetControllerAfterEachTransformation";
     public static final String DISABLE_ERROR_COUNT_ON_ERROR_NOTIFICATION_DISABLED = SYSTEM_PROPERTY_PREFIX + "disable.error.count.on.error.notifications.disabled";
