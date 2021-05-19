a88980c8558940b5c44426df6d8c42d031d98efd
Juan Grethe <jgrethe@itba.edu.ar>
2021-05-19 18:02:01 -0300

    MULE-19430: change stack for ArrayDeque (#10362)
    * MULE-19430: change stack for ArrayDeque

--- core/src/main/java/org/mule/runtime/core/api/transaction/TransactionCoordination.java
+++ core/src/main/java/org/mule/runtime/core/api/transaction/TransactionCoordination.java
@@ -15,7 +15,8 @@ import org.apache.commons.collections.ArrayStack;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public final class TransactionCoordination {
 
@@ -29,7 +30,7 @@ public final class TransactionCoordination {
    * {@link #bindTransaction(Transaction)}, it may be more consistent to have it as an instance variable.
    */
   private final ThreadLocal<Transaction> transactions = new ThreadLocal<>();
-  private final ThreadLocal<Stack<Transaction>> suspendedTransaction = new ThreadLocal<>();
+  private final ThreadLocal<Deque<Transaction>> suspendedTransaction = new ThreadLocal<>();
   private final ThreadLocal<ArrayStack> isolatedTransactions = new ThreadLocal<>();
 
   /** Lock variable that is used to access {@link #txCounter}. */
@@ -173,7 +174,7 @@ public final class TransactionCoordination {
 
     TransactionCoordination.getInstance().unbindTransaction(tx);
     if (suspendedTransaction.get() == null) {
-      suspendedTransaction.set(new Stack<>());
+      suspendedTransaction.set(new ArrayDeque<>());
     }
     suspendedTransaction.get().push(tx);
   }
