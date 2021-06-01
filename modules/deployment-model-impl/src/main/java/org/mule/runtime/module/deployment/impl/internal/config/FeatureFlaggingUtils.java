33326d6c4c2a52322727f553f4a1febc1afca2a3
Ivan Fritzler <ifritzler@mulesoft.com>
2021-05-31 23:31:25 -0300

    Code review

--- modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/config/FeatureFlaggingUtils.java
+++ modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/config/FeatureFlaggingUtils.java
@@ -7,14 +7,27 @@
 package org.mule.runtime.module.deployment.impl.internal.config;
 
 import org.mule.runtime.api.config.Feature;
+import org.mule.runtime.api.config.FeatureFlaggingService;
+import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.core.api.config.FeatureContext;
 import org.mule.runtime.core.api.config.FeatureFlaggingRegistry;
 import org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptor;
 
-import java.util.Optional;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.slf4j.Logger;
+import reactor.util.annotation.NonNull;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+import static com.google.common.cache.CacheBuilder.newBuilder;
 import static java.lang.Boolean.getBoolean;
 import static java.lang.System.getProperty;
+import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Utility class meant to provide a {@link org.mule.runtime.api.config.FeatureFlaggingService} substitute during the earlier
@@ -24,23 +37,93 @@ import static java.lang.System.getProperty;
  */
 public class FeatureFlaggingUtils {
 
+  private static final Logger LOGGER = getLogger(FeatureFlaggingUtils.class);
+
   private FeatureFlaggingUtils() {}
+
+  private static final LoadingCache<ArtifactDescriptor, FeatureFlaggingService> featureFlags = newBuilder()
+      .weakKeys()
+      .build(new CacheLoader<ArtifactDescriptor, FeatureFlaggingService>() {
+
+        @Override
+        public FeatureFlaggingService load(@NonNull ArtifactDescriptor artifactDescriptor) {
+          return buildFeatureFlaggingService(artifactDescriptor);
+        }
+      });
 
   /**
    * True if a {@link Feature} is enabled, assuming that the given {@link ArtifactDescriptor} provides relevant
    * {@link FeatureContext} metadata.
    *
-   * @param feature            The {@link Feature}
-   * @param artifactDescriptor Relevant {@link ArtifactDescriptor}
+   * @param feature            The {@link Feature}.
+   * @param artifactDescriptor Relevant {@link ArtifactDescriptor}.
    * @return True if the {@link Feature} must be enabled.
    */
   public static boolean isFeatureEnabled(Feature feature, ArtifactDescriptor artifactDescriptor) {
+    try {
+      return featureFlags.get(artifactDescriptor).isEnabled(feature);
+    } catch (ExecutionException e) {
+      throw new MuleRuntimeException(createStaticMessage("Error creating deployment FeatureFlaggingService for artifact [{%s}]",
+                                                         artifactDescriptor.getName()),
+                                     e);
+    }
+  }
+
+  /**
+   * Constructs a {@link FeatureFlaggingService} instance containing all the registered {@link Feature} flags.
+   * 
+   * @param artifactDescriptor The {@link ArtifactDescriptor} whose metadata will be used to create the {@link FeatureContext}.
+   * @return A {@link FeatureFlaggingService}.
+   * @see FeatureFlaggingRegistry
+   */
+  private static FeatureFlaggingService buildFeatureFlaggingService(ArtifactDescriptor artifactDescriptor) {
+    Map<Feature, Boolean> features = new HashMap<>();
+    LOGGER.debug("Configuring feature flags for artifact [{}]", artifactDescriptor.getName());
     FeatureContext featureContext = new FeatureContext(artifactDescriptor.getMinMuleVersion(), artifactDescriptor.getName());
+    FeatureFlaggingRegistry.getInstance().getFeatureFlagConfigurations()
+        .forEach((feature, featureContextPredicate) -> features.put(feature, evaluateFeatureFlag(feature, featureContext)));
+    return new DeploymentFeatureFlaggingService(features);
+  }
+
+  /**
+   * Determines a {@link Feature} flag value.
+   * 
+   * @param feature        The {@link Feature} to evaluate.
+   * @param featureContext Relevant {@link FeatureContext}.
+   * @return True if the {@link Feature} is enabled.
+   */
+  private static boolean evaluateFeatureFlag(Feature feature, FeatureContext featureContext) {
+    boolean enabled;
     Optional<String> systemPropertyName = feature.getOverridingSystemPropertyName();
     if (systemPropertyName.isPresent() && getProperty(systemPropertyName.get()) != null) {
-      return getBoolean(systemPropertyName.get());
+      enabled = getBoolean(systemPropertyName.get());
+      LOGGER.debug("Setting feature {} = {} for artifact [{}] because of System Property '{}'", feature, enabled,
+                   featureContext.getArtifactName(),
+                   systemPropertyName);
     } else {
-      return FeatureFlaggingRegistry.getInstance().getFeatureFlagConfigurations().get(feature).test(featureContext);
+      enabled = FeatureFlaggingRegistry.getInstance().getFeatureFlagConfigurations().get(feature).test(featureContext);
+      LOGGER.debug("Setting feature {} = {} for artifact [{}]", feature, enabled, featureContext.getArtifactName());
+    }
+    return enabled;
+  }
+
+  /**
+   * Internal {@link FeatureFlaggingService} implementation.
+   */
+  private static class DeploymentFeatureFlaggingService implements FeatureFlaggingService {
+
+    private final Map<Feature, Boolean> featureFlags;
+
+    public DeploymentFeatureFlaggingService(Map<Feature, Boolean> featureFlags) {
+      this.featureFlags = featureFlags;
+    }
+
+    @Override
+    public boolean isEnabled(Feature feature) {
+      if (!featureFlags.containsKey(feature)) {
+        throw new MuleRuntimeException(createStaticMessage("Feature %s not registered", feature));
+      }
+      return featureFlags.get(feature);
     }
   }
 }
