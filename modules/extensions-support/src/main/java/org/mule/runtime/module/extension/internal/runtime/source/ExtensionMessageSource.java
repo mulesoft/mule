db895f8153e11470b8bfe18f49a613771fb6d359
ndinu <ndinu@users.noreply.github.com>
2021-07-14 16:29:08 -0300

    MULE-19584: Add Source Lifecycle logging (#10476)

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/source/ExtensionMessageSource.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/source/ExtensionMessageSource.java
@@ -375,6 +375,8 @@ public class ExtensionMessageSource extends ExtensionComponent<SourceModel> impl
   }
 
   private void reallyDoStart() throws MuleException {
+    LOGGER.debug("Message source '{}' on flow '{}' is being started", sourceModel.getName(),
+                 getLocation().getRootContainerName());
     lifecycle(() -> lifecycleManager.fireStartPhase((phase, o) -> {
       startIfNeeded(retryPolicyTemplate);
 
@@ -391,6 +393,8 @@ public class ExtensionMessageSource extends ExtensionComponent<SourceModel> impl
 
   @Override
   public void doStop() throws MuleException {
+    LOGGER.debug("Message source '{}' on flow '{}' is being stopped", sourceModel.getName(),
+                 getLocation().getRootContainerName());
     safeLifecycle(() -> lifecycleManager.fireStopPhase((phase, o) -> {
       synchronized (started) {
         started.set(false);
@@ -621,9 +625,27 @@ public class ExtensionMessageSource extends ExtensionComponent<SourceModel> impl
     flowConstruct = new LazyValue<>(() -> (FlowConstruct) componentLocator.find(getRootContainerLocation()).orElse(null));
     messageProcessContext = createProcessingContext();
     if (shouldRunOnThisNode()) {
+      if (LOGGER.isDebugEnabled()) {
+        boolean isPrimaryPollingInstance = clusterService.isPrimaryPollingInstance();
+        if (primaryNodeOnly) {
+          LOGGER
+              .debug("Message source '{}' on flow '{}' is being initialised, it is running on the primary node and it must only run on this node.",
+                     sourceModel.getName(), getLocation().getRootContainerName());
+        } else {
+          LOGGER
+              .debug("Message source '{}' on flow '{}' is being initialised, there are no cluster restrictions regarding which node must run the source. This node {} the primary node.",
+                     sourceModel.getName(), getLocation().getRootContainerName(), isPrimaryPollingInstance ? "is" : "is not");
+        }
+      }
+
       reallyDoInitialise();
     } else {
+      LOGGER
+          .debug("Message source '{}' on flow '{}' is not being initialised because it should only run on the primary node of the cluster.",
+                 sourceModel.getName(), getLocation().getRootContainerName());
       new PrimaryNodeLifecycleNotificationListener(() -> {
+        LOGGER.debug("Message source '{}' on flow '{}' is being initialised because it became the primary node of the cluster.",
+                     sourceModel.getName(), getLocation().getRootContainerName());
         reallyDoInitialise();
         reallyDoStart();
       }, notificationListenerRegistry).register();
