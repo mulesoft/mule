681947a0cfe080e0c8514762cc58f895fcc8c35f
dabreu <mdabreu@mulesoft.com>
2021-07-14 10:39:20 -0300

    MULE-19581 - Add Polling Sources logging

--- modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/runtime/source/PollingSourceWrapperTestCase.java
+++ modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/runtime/source/PollingSourceWrapperTestCase.java
@@ -7,15 +7,20 @@
 package org.mule.runtime.module.extension.internal.runtime.source;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mule.runtime.api.store.ObjectStoreSettings.DEFAULT_EXPIRATION_INTERVAL;
 import static org.mule.runtime.core.api.util.ClassUtils.setFieldValue;
+
 import org.mule.runtime.api.component.location.ComponentLocation;
 import org.mule.runtime.api.exception.MuleException;
 import org.mule.runtime.api.lock.LockFactory;
@@ -24,10 +29,18 @@ import org.mule.runtime.api.scheduler.SchedulingStrategy;
 import org.mule.runtime.api.store.ObjectStoreManager;
 import org.mule.runtime.api.store.ObjectStoreSettings;
 import org.mule.runtime.core.api.exception.SystemExceptionHandler;
+import org.mule.runtime.core.api.util.UUID;
 import org.mule.runtime.module.extension.internal.runtime.source.poll.PollingSourceWrapper;
+import org.mule.sdk.api.runtime.operation.Result;
+import org.mule.sdk.api.runtime.source.PollContext;
 import org.mule.sdk.api.runtime.source.PollingSource;
 import org.mule.sdk.api.runtime.source.SourceCallback;
 import org.mule.tck.size.SmallTest;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -36,7 +49,10 @@ import org.mockito.Answers;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.slf4j.Logger;
 
 @SmallTest
 @RunWith(MockitoJUnitRunner.class)
@@ -47,6 +63,7 @@ public class PollingSourceWrapperTestCase {
   public static final String EXPECTED_RECENT_IDS_OS = "_pollingSource_myFlow/recently-processed-ids";
   public static final String EXPECTED_IDS_UPDATED_WATERMARK_OS = "_pollingSource_myFlow/ids-on-updated-watermark";
   public static final String EXPECTED_INFLIGHT_IDS_OS = "_pollingSource_myFlow/inflight-ids";
+  private static final String POLL_ITEM_ID = UUID.getUUID().toString();
 
   @Mock(answer = Answers.RETURNS_DEEP_STUBS)
   private LockFactory lockFactoryMock;
@@ -63,15 +80,29 @@ public class PollingSourceWrapperTestCase {
   @Mock
   private SourceCallback callbackMock;
 
+  private PollingSource pollingSource = mock(PollingSource.class);
+  private SchedulingStrategy schedulingStrategy = mock(SchedulingStrategy.class);
+  private Logger logger = mock(Logger.class);
+
   @InjectMocks
   private PollingSourceWrapper<Object, Object> pollingSourceWrapper =
-      new PollingSourceWrapper<Object, Object>(mock(PollingSource.class), mock(SchedulingStrategy.class), Integer.MAX_VALUE,
+      new PollingSourceWrapper<Object, Object>(pollingSource, schedulingStrategy, Integer.MAX_VALUE,
                                                mock(SystemExceptionHandler.class));
 
   @Before
   public void setUp() throws Exception {
     when(componentLocationMock.getRootContainerName()).thenReturn(TEST_FLOW_NAME);
     setComponentLocationMock();
+
+    when(schedulingStrategy.schedule(any(), any())).thenAnswer(new Answer<Void>() {
+
+      @Override
+      public Void answer(InvocationOnMock invocation) throws Throwable {
+        Runnable runnable = (Runnable) invocation.getArgument(1);
+        runnable.run();
+        return null;
+      }
+    });
   }
 
   @Test
@@ -86,6 +117,32 @@ public class PollingSourceWrapperTestCase {
   public void idempotencyStoreGetsCreatedOnStart() throws MuleException {
     pollingSourceWrapper.onStart(callbackMock);
     assertTransientStoreIsCreated(EXPECTED_INFLIGHT_IDS_OS, DEFAULT_EXPIRATION_INTERVAL);
+  }
+
+  @Test
+  public void loggingOnAcceptedItem() throws MuleException, Exception {
+    stubPollItem(Optional.empty());
+    Logger origLogger = setLogger(pollingSourceWrapper, "LOGGER", logger);
+    try {
+      pollingSourceWrapper.onStart(callbackMock);
+      verifyAcceptedItemLogMessage(logger, "");
+    } finally {
+      // restore original logger
+      setLogger(pollingSourceWrapper, "LOGGER", origLogger);
+    }
+  }
+
+  @Test
+  public void loggingOnRejectedItem() throws MuleException, Exception {
+    stubPollItem(Optional.of(POLL_ITEM_ID));
+    Logger origLogger = setLogger(pollingSourceWrapper, "LOGGER", logger);
+    try {
+      pollingSourceWrapper.onStart(callbackMock);
+      verifyRejectedItemLogMessage(logger, POLL_ITEM_ID, PollContext.PollItemStatus.ALREADY_IN_PROCESS);
+    } finally {
+      // restore original logger
+      setLogger(pollingSourceWrapper, "LOGGER", origLogger);
+    }
   }
 
   private void assertPersistentStoreIsCreated(String expectedName, Long expirationInterval) {
@@ -111,4 +168,59 @@ public class PollingSourceWrapperTestCase {
     setFieldValue(pollingSourceWrapper, "componentLocation", componentLocationMock, false);
   }
 
+  private Logger setLogger(Object object, String fieldName, Logger newLogger) throws Exception {
+    Field field = object.getClass().getDeclaredField(fieldName);
+    field.setAccessible(true);
+    Logger oldLogger;
+    try {
+      Field modifiersField = Field.class.getDeclaredField("modifiers");
+      modifiersField.setAccessible(true);
+      modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+      try {
+        oldLogger = (Logger) field.get(null);
+        field.set(null, newLogger);
+      } finally {
+        // undo accessibility changes
+        modifiersField.setInt(field, field.getModifiers());
+        modifiersField.setAccessible(false);
+      }
+    } finally {
+      // undo accessibility changes
+      field.setAccessible(false);
+    }
+    return oldLogger;
+  }
+
+  private void stubPollItem(Optional<String> pollItemId) {
+    doAnswer(new Answer() {
+
+      @Override
+      public Object answer(InvocationOnMock invocation) throws Throwable {
+        PollContext pollContext = (PollContext) invocation.getArgument(0);
+        pollContext
+            .accept(item -> {
+              if (pollItemId.isPresent()) {
+                ((PollContext.PollItem) item).setId(pollItemId.get());
+              }
+              ((PollContext.PollItem) item).setResult(Result.builder().output("test").build());
+            });
+        return null;
+      }
+    }).when(pollingSource).poll(any());
+  }
+
+  private void verifyAcceptedItemLogMessage(Logger logger, String pollItemId) {
+    verifyLogMessages(logger, String.format(PollingSourceWrapper.ACCEPTED_ITEM_MESSAGE, pollItemId));
+  }
+
+  private void verifyRejectedItemLogMessage(Logger logger, String pollItemId, PollContext.PollItemStatus status) {
+    verifyLogMessages(logger, String.format(PollingSourceWrapper.REJECTED_ITEM_MESSAGE, pollItemId, status));
+  }
+
+  private void verifyLogMessages(Logger logger, String... expectedMessages) {
+    ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+    verify(logger, atLeastOnce()).debug(argumentCaptor.capture());
+    List<String> messages = argumentCaptor.getAllValues();
+    assertThat(messages, hasItems(expectedMessages));
+  }
 }
