98aa0310e7cfcfd656bd79b227c5fa8b58c24b99
Nicolas Santiago Di Nucci <nico.dinu@gmail.com>
2021-07-27 16:43:29 -0300

    fix mistake

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/utils/FieldValueProviderNameUtils.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/utils/FieldValueProviderNameUtils.java
@@ -15,7 +15,8 @@ import org.mule.runtime.api.meta.model.parameter.FieldValueProviderModel;
 
 public class FieldValueProviderNameUtils {
 
-  private static String FIELD_VALUE_PROVIDER_NAME_FORMAT = "%s[%s]";
+  private static String FIELD_VALUE_PROVIDER_NAME_FORMAT_SUFFIX_SEPARATOR = "[";
+  private static String FIELD_VALUE_PROVIDER_NAME_FORMAT = "%s" + FIELD_VALUE_PROVIDER_NAME_FORMAT_SUFFIX_SEPARATOR + "%s]";
 
   private FieldValueProviderNameUtils() {};
 
@@ -24,7 +25,8 @@ public class FieldValueProviderNameUtils {
   }
 
   public static String getParameterName(FieldValueProviderModel fieldValueProviderModel) {
-    return fieldValueProviderModel.getProviderName().substring(0, fieldValueProviderModel.getProviderName().indexOf("<"));
+    return fieldValueProviderModel.getProviderName()
+        .substring(0, fieldValueProviderModel.getProviderName().indexOf(FIELD_VALUE_PROVIDER_NAME_FORMAT_SUFFIX_SEPARATOR));
   }
 
   private static String getIdentifierForSelectors(String[] targetSelectors) {
