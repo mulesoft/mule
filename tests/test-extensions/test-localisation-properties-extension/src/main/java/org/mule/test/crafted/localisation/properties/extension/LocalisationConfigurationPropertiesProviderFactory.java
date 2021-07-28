29b9e1b6814ee51e34891870dd0c1daf42d19c3b
Martín Buchwald <martin.buchwald@mulesoft.com>
2021-07-28 13:57:17 -0300

    MULE-19659: Migrate CE tests to use getParameter with group (#10551)
    Co-authored-by: Ezequiel Werner <eze210@gmail.com>

--- tests/test-extensions/test-localisation-properties-extension/src/main/java/org/mule/test/crafted/localisation/properties/extension/LocalisationConfigurationPropertiesProviderFactory.java
+++ tests/test-extensions/test-localisation-properties-extension/src/main/java/org/mule/test/crafted/localisation/properties/extension/LocalisationConfigurationPropertiesProviderFactory.java
@@ -8,6 +8,7 @@ package org.mule.test.crafted.localisation.properties.extension;
 
 import static java.util.Objects.requireNonNull;
 import static org.mule.runtime.api.component.ComponentIdentifier.builder;
+import static org.mule.runtime.api.meta.model.parameter.ParameterGroupModel.DEFAULT_GROUP_NAME;
 import static org.mule.test.crafted.localisation.properties.extension.TestLocalisationPropertiesExtensionLoadingDelegate.EXTENSION_NAME;
 
 import org.mule.runtime.api.component.ComponentIdentifier;
@@ -32,7 +33,7 @@ public class LocalisationConfigurationPropertiesProviderFactory implements Confi
   public LocalisationConfigurationPropertiesProvider createProvider(ComponentAst providerElementDeclaration,
                                                                     UnaryOperator<String> localResolver,
                                                                     ResourceProvider externalResourceProvider) {
-    String file = providerElementDeclaration.getParameter("file").getResolvedRawValue();
+    String file = providerElementDeclaration.getParameter(DEFAULT_GROUP_NAME, "file").getResolvedRawValue();
     requireNonNull(file, "Required attribute 'file' of 'locale-configuration-properties' not found");
 
     String locale = requireNonNull(providerElementDeclaration.getParameter("language", "locale").getResolvedRawValue(),
