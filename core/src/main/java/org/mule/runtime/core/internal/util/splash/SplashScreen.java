d05f524cffbc0f05d517c48c429da626763d0fa4
Martín Buchwald <martin.buchwald@mulesoft.com>
2021-08-02 12:54:25 -0300

    imports

--- core/src/main/java/org/mule/runtime/core/internal/util/splash/SplashScreen.java
+++ core/src/main/java/org/mule/runtime/core/internal/util/splash/SplashScreen.java
@@ -13,7 +13,10 @@ import com.google.common.collect.ImmutableList;
 import org.mule.runtime.core.api.MuleContext;
 import org.mule.runtime.core.api.util.StringMessageUtils;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
