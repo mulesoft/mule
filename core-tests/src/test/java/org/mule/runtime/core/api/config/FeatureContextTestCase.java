b08c24f28435e02f42af6e38454e34882b1f5104
Ivan Fritzler <ifritzler@mulesoft.com>
2021-08-03 15:18:22 -0300

    Test fix

--- core-tests/src/test/java/org/mule/runtime/core/api/config/FeatureContextTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/api/config/FeatureContextTestCase.java
@@ -1,27 +1,28 @@
 package org.mule.runtime.core.api.config;
 
-import org.junit.Assert;
-import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 import org.mule.runtime.api.meta.MuleVersion;
 import org.mule.tck.size.SmallTest;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.hamcrest.core.Is.is;
+import org.junit.Test;
 
 @SmallTest
 public class FeatureContextTestCase {
 
-    private static final List<String> suffixedVersions = Arrays.asList("4.4.0-SNAPSHOT", "4.4.0-rc1", "4.4.0-rc1-SNAPSHOT");
-    private static final MuleVersion nonSuffixedVersion = new MuleVersion("4.4.0");
+  private static final List<String> suffixedVersions = Arrays.asList("4.4.0-SNAPSHOT", "4.4.0-rc1", "4.4.0-rc1-SNAPSHOT");
+  private static final MuleVersion nonSuffixedVersion = new MuleVersion("4.4.0");
 
-    @Test
-    public void testSemverSuffixesMustBeIgnored() {
-        suffixedVersions.forEach(suffixedVersion -> {
-            FeatureContext featureContext = new FeatureContext(new MuleVersion(suffixedVersion), "");
-            Assert.assertThat(featureContext.getArtifactMinMuleVersion().get().equals(nonSuffixedVersion), is(true));
-        });
-    }
+  @Test
+  public void testSemverSuffixesMustBeIgnored() {
+    suffixedVersions.forEach(suffixedVersion -> {
+      FeatureContext featureContext = new FeatureContext(new MuleVersion(suffixedVersion), "");
+      assertThat(featureContext.getArtifactMinMuleVersion().get().sameAs(nonSuffixedVersion), is(true));
+    });
+  }
 
 }
