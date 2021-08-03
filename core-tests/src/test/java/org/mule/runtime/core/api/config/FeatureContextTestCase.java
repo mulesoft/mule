adfc38a9425b19628dd4f27965fdb25e21df9645
Ivan Fritzler <ifritzler@mulesoft.com>
2021-08-03 15:21:43 -0300

    Allure annotations

--- core-tests/src/test/java/org/mule/runtime/core/api/config/FeatureContextTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/api/config/FeatureContextTestCase.java
@@ -2,6 +2,8 @@ package org.mule.runtime.core.api.config;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.mule.test.allure.AllureConstants.DeploymentConfiguration.DEPLOYMENT_CONFIGURATION;
+import static org.mule.test.allure.AllureConstants.DeploymentConfiguration.FeatureFlaggingStory.FEATURE_FLAGGING;
 
 import org.mule.runtime.api.meta.MuleVersion;
 import org.mule.tck.size.SmallTest;
@@ -9,15 +11,20 @@ import org.mule.tck.size.SmallTest;
 import java.util.Arrays;
 import java.util.List;
 
+import io.qameta.allure.Issue;
+import io.qameta.allure.Story;
 import org.junit.Test;
 
 @SmallTest
+@io.qameta.allure.Feature(DEPLOYMENT_CONFIGURATION)
+@Story(FEATURE_FLAGGING)
 public class FeatureContextTestCase {
 
   private static final List<String> suffixedVersions = Arrays.asList("4.4.0-SNAPSHOT", "4.4.0-rc1", "4.4.0-rc1-SNAPSHOT");
   private static final MuleVersion nonSuffixedVersion = new MuleVersion("4.4.0");
 
   @Test
+  @Issue("MULE-19682")
   public void testSemverSuffixesMustBeIgnored() {
     suffixedVersions.forEach(suffixedVersion -> {
       FeatureContext featureContext = new FeatureContext(new MuleVersion(suffixedVersion), "");
