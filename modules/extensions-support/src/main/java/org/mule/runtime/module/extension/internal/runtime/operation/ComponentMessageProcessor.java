9c2275237e456d127e1acc8f6fcd1033132e72b1
Axel Sanguinetti <asanguinetti@mulesoft.com>
2021-08-17 12:12:17 -0300

    MULE-19716: Moving DefaultExecutionMediator's CompositeClassLoader instantiation to initialization phase.

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/operation/ComponentMessageProcessor.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/operation/ComponentMessageProcessor.java
@@ -1142,6 +1142,7 @@ public abstract class ComponentMessageProcessor<T extends ComponentModel> extend
                                         componentModel,
                                         createInterceptorChain(),
                                         errorTypeRepository,
+                                        muleContext.getExecutionClassLoader(),
                                         resultTransformer);
   }
 
