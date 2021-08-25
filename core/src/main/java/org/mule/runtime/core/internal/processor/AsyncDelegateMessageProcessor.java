e9d72b129bb490f3d372b5e5f5772caa61ca0b45
Fabian Gonzalez <fabian.gonzalez@mulesoft.com>
2021-08-24 22:22:56 -0300

    MULE-19735: Make async delegation initialise processing strategy (#10701)

--- core/src/main/java/org/mule/runtime/core/internal/processor/AsyncDelegateMessageProcessor.java
+++ core/src/main/java/org/mule/runtime/core/internal/processor/AsyncDelegateMessageProcessor.java
@@ -149,6 +149,7 @@ public class AsyncDelegateMessageProcessor extends AbstractMessageProcessorOwner
     backpressureHandler = new QueueBackpressureHandler(schedulerService, () -> muleContext.getSchedulerBaseConfig(),
                                                        this::dispatchEvent, name != null ? name : getLocation().getLocation());
 
+    initialiseIfNeeded(processingStrategy, muleContext);
     super.initialise();
   }
 
@@ -200,6 +201,7 @@ public class AsyncDelegateMessageProcessor extends AbstractMessageProcessorOwner
   public void dispose() {
     super.dispose();
     disposeIfNeeded(delegate, logger);
+    disposeIfNeeded(processingStrategy, logger);
   }
 
   @Override
