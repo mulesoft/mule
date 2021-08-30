5621c3be79817c4c1b219711a399d84fcc1c5972
sofiamorseletto <sofimorseletto@gmail.com>
2021-08-30 12:19:57 -0300

    Moving flow stopping procedure

---
+++ modules/deployment/src/test/resources/org/foo/EchoErrorTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.foo;
+
+import org.mule.runtime.module.deployment.api.EventCallback;
+
+public class EchoErrorTest implements EventCallback {
+
+  public void eventReceived(String payload) throws Exception {
+    throw new AssertionError("Test error", new Exception());
+  }
+
+  public String echo(String data) {
+    throw new AssertionError("Test error", new Exception());
+  }
+}
