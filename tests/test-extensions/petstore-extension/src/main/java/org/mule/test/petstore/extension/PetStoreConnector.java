ad24fbf5be81e29779b4c16bd88a038dc2e0c247
Rodrigo Merino <elrodro83@users.noreply.github.com>
2021-09-07 18:16:44 -0300

    MULE-19757: Cannot add a parameter of type Scheduler to a pojo (#10750) (#10751)

--- tests/test-extensions/petstore-extension/src/main/java/org/mule/test/petstore/extension/PetStoreConnector.java
+++ tests/test-extensions/petstore-extension/src/main/java/org/mule/test/petstore/extension/PetStoreConnector.java
@@ -30,8 +30,8 @@ import java.util.concurrent.atomic.AtomicInteger;
     PooledPetStoreConnectionProviderWithValidConnection.class})
 @ErrorTypes(PetstoreErrorTypeDefinition.class)
 @Sources({PetStoreSource.class, FailingPetStoreSource.class, SentientSource.class, PetAdoptionSource.class,
-    PetStoreStreamSource.class, PartialPetAdoptionSource.class, ConnectedPetAdoptionSource.class, PetFailingPollingSource.class,
-    PetFilterPollingSource.class})
+    PetAdoptionSchedulerInParamSource.class, PetStoreStreamSource.class, PartialPetAdoptionSource.class,
+    ConnectedPetAdoptionSource.class, PetFailingPollingSource.class, PetFilterPollingSource.class})
 @org.mule.sdk.api.annotation.Sources({NumberPetAdoptionSource.class, WatermarkingPetAdoptionSource.class,
     PetStoreListSource.class, PetAdoptionLimitingSource.class})
 @Xml(namespace = "http://www.mulesoft.org/schema/mule/petstore", prefix = "petstore")
