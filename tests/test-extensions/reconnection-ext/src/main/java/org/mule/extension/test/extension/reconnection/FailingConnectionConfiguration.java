87890c477635fad4e02dd8d6b771d029eaf4ea05
Mariana DAbreu <dabreu@users.noreply.github.com>
2021-09-17 12:19:05 -0300

    MULE-19678 - When a ConnectionException is thrown during source start, invalidate the associated connection if present (#10781)

---
+++ tests/test-extensions/reconnection-ext/src/main/java/org/mule/extension/test/extension/reconnection/FailingConnectionConfiguration.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.test.extension.reconnection;
+
+import org.mule.runtime.extension.api.annotation.Configuration;
+import org.mule.runtime.extension.api.annotation.Sources;
+import org.mule.runtime.extension.api.annotation.connectivity.ConnectionProviders;
+
+@ConnectionProviders(FailingConnectionConnectionProvider.class)
+@Sources(FailingConnectionSource.class)
+@Configuration(name = "failing-connection-config")
+public class FailingConnectionConfiguration {
+}
