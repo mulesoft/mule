7313b2bf9596043a095170e269c57c5037c53705
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2021-09-21 18:45:15 +0200

    self review

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/parser/SemanticTermsParser.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/parser/SemanticTermsParser.java
@@ -8,7 +8,15 @@ package org.mule.runtime.module.extension.internal.loader.parser;
 
 import java.util.Set;
 
+/**
+ * General contract for a model parser capable of reading semantic terms
+ *
+ * @since 4.5.0
+ */
 public interface SemanticTermsParser {
 
+  /**
+   * @return a {@link Set} with the model's semantic terms
+   */
   Set<String> getSemanticTerms();
 }
