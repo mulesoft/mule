0e5b86c57cd9c70d1190b824d7d214a688405d83
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2021-10-06 18:34:43 +0200

    config ref stereotypes

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/java/StereotypeModelLoaderDelegate.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/loader/java/StereotypeModelLoaderDelegate.java
@@ -6,14 +6,11 @@
  */
 package org.mule.runtime.module.extension.internal.loader.java;
 
-import static java.lang.String.format;
-import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toList;
 import static org.mule.runtime.api.meta.model.stereotype.StereotypeModelBuilder.newStereotype;
 import static org.mule.runtime.extension.api.stereotype.MuleStereotypes.PROCESSOR;
 import static org.mule.runtime.extension.api.stereotype.MuleStereotypes.SOURCE;
-import static org.mule.runtime.module.extension.internal.loader.java.MuleExtensionAnnotationParser.mapReduceExtensionAnnotation;
-import static org.mule.runtime.module.extension.internal.loader.utils.JavaModelLoaderUtils.isProcessorChain;
-import static org.mule.runtime.module.extension.internal.loader.utils.JavaModelLoaderUtils.isRoute;
+import static org.mule.runtime.internal.dsl.DslConstants.CONFIG_ATTRIBUTE_NAME;
 
 import org.mule.metadata.api.ClassTypeLoader;
 import org.mule.runtime.api.dsl.DslResolvingContext;
@@ -26,28 +23,21 @@ import org.mule.runtime.api.meta.model.declaration.fluent.ExtensionDeclaration;
 import org.mule.runtime.api.meta.model.declaration.fluent.ExtensionDeclarer;
 import org.mule.runtime.api.meta.model.declaration.fluent.HasModelProperties;
 import org.mule.runtime.api.meta.model.declaration.fluent.HasStereotypeDeclarer;
-import org.mule.runtime.api.meta.model.declaration.fluent.NamedDeclaration;
-import org.mule.runtime.api.meta.model.declaration.fluent.NestableElementDeclaration;
-import org.mule.runtime.api.meta.model.declaration.fluent.NestedChainDeclaration;
 import org.mule.runtime.api.meta.model.declaration.fluent.NestedComponentDeclarer;
-import org.mule.runtime.api.meta.model.declaration.fluent.NestedRouteDeclaration;
-import org.mule.runtime.api.meta.model.declaration.fluent.WithAllowedStereotypesDeclaration;
+import org.mule.runtime.api.meta.model.declaration.fluent.StereotypedDeclaration;
 import org.mule.runtime.api.meta.model.stereotype.StereotypeModel;
-import org.mule.runtime.extension.api.annotation.param.stereotype.AllowedStereotypes;
 import org.mule.runtime.extension.api.declaration.type.DefaultExtensionsTypeLoaderFactory;
 import org.mule.runtime.extension.api.loader.ExtensionLoadingContext;
-import org.mule.runtime.module.extension.api.loader.java.type.ExtensionParameter;
-import org.mule.runtime.module.extension.api.loader.java.type.MethodElement;
 import org.mule.runtime.module.extension.internal.loader.parser.AllowedStereotypesModelParser;
 import org.mule.runtime.module.extension.internal.loader.parser.StereotypeModelFactory;
 import org.mule.runtime.module.extension.internal.loader.parser.StereotypeModelParser;
-import org.mule.runtime.module.extension.internal.loader.parser.StereotypeModelParser.ParsedStereotype;
 import org.mule.runtime.module.extension.internal.loader.parser.java.stereotypes.CustomStereotypeModelProperty;
 import org.mule.runtime.module.extension.internal.loader.parser.java.stereotypes.DefaultStereotypeModelFactory;
 import org.mule.sdk.api.stereotype.MuleStereotypes;
-import org.mule.sdk.api.stereotype.StereotypeDefinition;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -60,8 +50,7 @@ public class StereotypeModelLoaderDelegate {
 
   private String namespace;
 
-  private final Map<StereotypeDefinition, StereotypeModel> stereotypesCache = new HashMap<>();
-  private final Multimap<ComponentDeclaration, ConfigurationDeclaration> componentConfigs = LinkedListMultimap.create();
+  private final Map<ComponentDeclaration, List<StereotypeModel>> componentsConfigStereotypes = new HashMap<>();
   private final StereotypeModel sourceParent;
   private final StereotypeModel processorParent;
   private final ClassTypeLoader typeLoader;
@@ -105,7 +94,7 @@ public class StereotypeModelLoaderDelegate {
                             ComponentDeclarer declarer,
                             Optional<Supplier<StereotypeModel>> fallback) {
     doAddStereotypes(parser, declarer, fallback);
-
+    addConfigRefStereoTypesIfNeeded((ComponentDeclaration<?>) declarer.getDeclaration());
   }
 
   public void addAllowedStereotypes(AllowedStereotypesModelParser parser,
@@ -115,6 +104,16 @@ public class StereotypeModelLoaderDelegate {
       declarer.withAllowedStereotypes(MuleStereotypes.PROCESSOR);
     } else {
       allowedStereotypes.forEach(declarer::withAllowedStereotypes);
+    }
+  }
+
+  private void addConfigRefStereoTypesIfNeeded(ComponentDeclaration<?> declaration) {
+    List<StereotypeModel> configStereotypes = componentsConfigStereotypes.get(declaration);
+    if (configStereotypes != null && !configStereotypes.isEmpty()) {
+      declaration.getAllParameters().stream()
+          .filter(p -> CONFIG_ATTRIBUTE_NAME.equals(p.getName()))
+          .findAny()
+          .ifPresent(configRef -> configRef.setAllowedStereotypeModels(configStereotypes));
     }
   }
 
@@ -138,57 +137,17 @@ public class StereotypeModelLoaderDelegate {
     return stereotypeModel;
   }
 
-  private void addAllowedStereotypes(String namespace, ComponentDeclaration<?> declaration, MethodElement methodElement) {
-
-    Map<String, NestableElementDeclaration<?>> nested = declaration.getNestedComponents().stream()
-        .collect(toMap(NamedDeclaration::getName, n -> n));
-
-    methodElement.getParameters().stream()
-        .filter(p -> nested.containsKey(p.getAlias()))
-        .forEach(parameter -> {
-          if (isProcessorChain(parameter)) {
-            addAllowedStereotypes(namespace, parameter, (WithAllowedStereotypesDeclaration) nested.get(parameter.getAlias()));
-
-          } else if (isRoute(parameter)) {
-            NestedRouteDeclaration route = (NestedRouteDeclaration) nested.get(parameter.getAlias());
-            Optional<AllowedStereotypes> allowedStereotypes = parameter.getType().getAnnotation(AllowedStereotypes.class);
-            allowedStereotypes.ifPresent(processorsStereotypes -> {
-              NestableElementDeclaration processorsChain = route.getNestedComponents().stream()
-                  .filter(routeChild -> routeChild instanceof NestedChainDeclaration)
-                  .findFirst()
-                  .orElseThrow(() -> new IllegalStateException("Missing Chain component in Route declaration"));
-
-              addAllowedStereotypes((WithAllowedStereotypesDeclaration) processorsChain,
-                  processorsStereotypes.value(), namespace);
-            });
-          }
-        });
-  }
-
-
-  private void addAllowedStereotypes(String namespace, ExtensionParameter parameter,
-                                     WithAllowedStereotypesDeclaration declaration) {
-    Optional<AllowedStereotypes> allowedStereotypes = parameter.getAnnotation(AllowedStereotypes.class);
-
-    if (allowedStereotypes.isPresent()) {
-      addAllowedStereotypes(declaration, allowedStereotypes.get().value(), namespace);
-    } else {
-      declaration.addAllowedStereotype(PROCESSOR);
-    }
-  }
-
-
-  private void addAllowedStereotypes(WithAllowedStereotypesDeclaration declaration,
-                                     Class<? extends org.mule.runtime.extension.api.stereotype.StereotypeDefinition>[] stereotypes, String namespace) {
-    for (Class<? extends org.mule.runtime.extension.api.stereotype.StereotypeDefinition> definition : stereotypes) {
-      declaration.addAllowedStereotype(createCustomStereotype(definition, namespace, stereotypesCache));
-    }
-  }
-
   private void populateComponentConfigsMap(ConfigurationDeclaration config) {
-    config.getConstructs().forEach(construct -> componentConfigs.put(construct, config));
-    config.getMessageSources().forEach(source -> componentConfigs.put(source, config));
-    config.getOperations().forEach(operation -> componentConfigs.put(operation, config));
+    StereotypeModel configStereotype = config.getStereotype();
+    if (configStereotype != null) {
+      config.getConstructs().forEach(construct -> addComponentConfigStereotype(construct, configStereotype));
+      config.getMessageSources().forEach(source -> addComponentConfigStereotype(source, configStereotype));
+      config.getOperations().forEach(operation -> addComponentConfigStereotype(operation, configStereotype));
+    }
+  }
+
+  private void addComponentConfigStereotype(ComponentDeclaration declaration, StereotypeModel configStereotype) {
+    componentsConfigStereotypes.computeIfAbsent(declaration, key -> new LinkedList<>()).add(configStereotype);
   }
 
   private String getStereotypePrefix(ExtensionDeclarer extensionDeclarer) {
@@ -197,26 +156,5 @@ public class StereotypeModelLoaderDelegate {
 
   public void setNamespace(String namespace) {
     this.namespace = namespace.toUpperCase();
-  }
-
-  private static class DefaultParsedStereotype implements ParsedStereotype {
-
-    private final Optional<StereotypeModel> stereotype;
-    private final boolean validator;
-
-    public DefaultParsedStereotype(Optional<StereotypeModel> stereotype, boolean validator) {
-      this.stereotype = stereotype;
-      this.validator = validator;
-    }
-
-    @Override
-    public Optional<StereotypeModel> getStereotypeModel() {
-      return stereotype;
-    }
-
-    @Override
-    public boolean isValidator() {
-      return validator;
-    }
   }
 }
