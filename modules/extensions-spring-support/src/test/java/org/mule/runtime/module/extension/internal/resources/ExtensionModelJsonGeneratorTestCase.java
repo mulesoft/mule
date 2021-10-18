13777921d0bdc852a241b7486c80a3a89ecae8e9
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2021-10-18 19:10:12 +0200

    processing types, but not enough

--- modules/extensions-spring-support/src/test/java/org/mule/runtime/module/extension/internal/resources/ExtensionModelJsonGeneratorTestCase.java
+++ modules/extensions-spring-support/src/test/java/org/mule/runtime/module/extension/internal/resources/ExtensionModelJsonGeneratorTestCase.java
@@ -13,10 +13,27 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mule.runtime.api.util.MuleSystemProperties.SYSTEM_PROPERTY_PREFIX;
 import static org.mule.runtime.module.extension.internal.FileGenerationParameterizedExtensionModelTestCase.ResourceExtensionUnitTest.newUnitTest;
 
+import org.mule.extension.test.extension.reconnection.ReconnectionExtension;
 import org.mule.runtime.api.meta.model.ExtensionModel;
 import org.mule.runtime.extension.api.persistence.ExtensionModelJsonSerializer;
 import org.mule.runtime.module.extension.internal.FileGenerationParameterizedExtensionModelTestCase;
 import org.mule.tck.size.SmallTest;
+import org.mule.test.data.sample.extension.SampleDataExtension;
+import org.mule.test.function.extension.WeaveFunctionExtension;
+import org.mule.test.heisenberg.extension.HeisenbergExtension;
+import org.mule.test.implicit.config.extension.extension.api.ImplicitConfigExtension;
+import org.mule.test.marvel.MarvelExtension;
+import org.mule.test.metadata.extension.MetadataExtension;
+import org.mule.test.nonimplicit.config.extension.extension.api.NonImplicitConfigExtension;
+import org.mule.test.oauth.TestOAuthExtension;
+import org.mule.test.petstore.extension.PetStoreConnector;
+import org.mule.test.ram.RickAndMortyExtension;
+import org.mule.test.semantic.extension.SemanticTermsExtension;
+import org.mule.test.substitutiongroup.extension.SubstitutionGroupExtension;
+import org.mule.test.subtypes.extension.SubTypesMappingConnector;
+import org.mule.test.transactional.TransactionalExtension;
+import org.mule.test.typed.value.extension.extension.TypedValueExtension;
+import org.mule.test.values.extension.ValuesExtension;
 import org.mule.test.vegan.extension.VeganExtension;
 
 import java.util.Collection;
@@ -37,26 +54,24 @@ public class ExtensionModelJsonGeneratorTestCase extends FileGenerationParameter
   @Parameterized.Parameters(name = "{1}")
   public static Collection<Object[]> data() {
     List<ResourceExtensionUnitTest> extensions;
-    extensions = asList(
-                        newUnitTest(JAVA_LOADER, VeganExtension.class, "vegan.json")// ,
-    // newUnitTest(JAVA_LOADER, PetStoreConnector.class, "petstore.json"),
-    // newUnitTest(JAVA_LOADER, MetadataExtension.class, "metadata.json"),
-    // newUnitTest(JAVA_LOADER, HeisenbergExtension.class, "heisenberg.json"),
-    // newUnitTest(JAVA_LOADER, SubstitutionGroupExtension.class, "substitutiongroup.json"),
-    // newUnitTest(JAVA_LOADER, TransactionalExtension.class, "tx-ext.json"),
-    // newUnitTest(JAVA_LOADER, SubTypesMappingConnector.class, "subtypes.json"),
-    // newUnitTest(JAVA_LOADER, MarvelExtension.class, "marvel.json"),
-    // newUnitTest(SOAP_LOADER, RickAndMortyExtension.class, "ram.json"),
-    // newUnitTest(JAVA_LOADER, TypedValueExtension.class, "typed-value.json"),
-    // newUnitTest(JAVA_LOADER, TestOAuthExtension.class, "test-oauth.json"),
-    // newUnitTest(JAVA_LOADER, WeaveFunctionExtension.class, "test-fn.json"),
-    // newUnitTest(JAVA_LOADER, ValuesExtension.class, "values.json"),
-    // newUnitTest(JAVA_LOADER, SampleDataExtension.class, "sample-data.json"),
-    // newUnitTest(JAVA_LOADER, ImplicitConfigExtension.class, "implicit-config.json"),
-    // newUnitTest(JAVA_LOADER, NonImplicitConfigExtension.class, "non-implicit-config.json"),
-    // newUnitTest(JAVA_LOADER, SemanticTermsExtension.class, "semantic-terms-extension.json"),
-    // newUnitTest(JAVA_LOADER, ReconnectionExtension.class, "reconnection-extension.json")
-    );
+    extensions = asList(newUnitTest(JAVA_LOADER, VeganExtension.class, "vegan.json"),
+                        newUnitTest(JAVA_LOADER, PetStoreConnector.class, "petstore.json"),
+                        newUnitTest(JAVA_LOADER, MetadataExtension.class, "metadata.json"),
+                        newUnitTest(JAVA_LOADER, HeisenbergExtension.class, "heisenberg.json"),
+                        newUnitTest(JAVA_LOADER, SubstitutionGroupExtension.class, "substitutiongroup.json"),
+                        newUnitTest(JAVA_LOADER, TransactionalExtension.class, "tx-ext.json"),
+                        newUnitTest(JAVA_LOADER, SubTypesMappingConnector.class, "subtypes.json"),
+                        newUnitTest(JAVA_LOADER, MarvelExtension.class, "marvel.json"),
+                        newUnitTest(SOAP_LOADER, RickAndMortyExtension.class, "ram.json"),
+                        newUnitTest(JAVA_LOADER, TypedValueExtension.class, "typed-value.json"),
+                        newUnitTest(JAVA_LOADER, TestOAuthExtension.class, "test-oauth.json"),
+                        newUnitTest(JAVA_LOADER, WeaveFunctionExtension.class, "test-fn.json"),
+                        newUnitTest(JAVA_LOADER, ValuesExtension.class, "values.json"),
+                        newUnitTest(JAVA_LOADER, SampleDataExtension.class, "sample-data.json"),
+                        newUnitTest(JAVA_LOADER, ImplicitConfigExtension.class, "implicit-config.json"),
+                        newUnitTest(JAVA_LOADER, NonImplicitConfigExtension.class, "non-implicit-config.json"),
+                        newUnitTest(JAVA_LOADER, SemanticTermsExtension.class, "semantic-terms-extension.json"),
+                        newUnitTest(JAVA_LOADER, ReconnectionExtension.class, "reconnection-extension.json"));
 
     return createExtensionModels(extensions);
   }
