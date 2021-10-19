af4119ff8cd1fb2f42445dfd54fd030f1b071d41
Mariana DAbreu <dabreu@users.noreply.github.com>
2021-10-19 16:02:44 -0300

    MULE-19853 - Support using inline string array parameter on operations defined with XML SDK (#10893)

--- modules/extensions-xml-support/src/main/java/org/mule/runtime/extension/internal/config/dsl/XmlExtensionBuildingDefinitionProvider.java
+++ modules/extensions-xml-support/src/main/java/org/mule/runtime/extension/internal/config/dsl/XmlExtensionBuildingDefinitionProvider.java
@@ -7,6 +7,7 @@
 package org.mule.runtime.extension.internal.config.dsl;
 
 import static java.util.Collections.emptySet;
+import static org.mule.metadata.java.api.utils.JavaTypeUtils.getType;
 import static org.mule.runtime.api.meta.model.parameter.ParameterRole.BEHAVIOUR;
 import static org.mule.runtime.api.util.Preconditions.checkState;
 import static org.mule.runtime.config.internal.dsl.model.extension.xml.MacroExpansionModuleModel.DEFAULT_GLOBAL_ELEMENTS;
@@ -20,6 +21,10 @@ import static org.mule.runtime.dsl.api.component.AttributeDefinition.Builder.fro
 import static org.mule.runtime.dsl.api.component.KeyAttributeDefinitionPair.newBuilder;
 import static org.mule.runtime.dsl.api.component.TypeDefinition.fromType;
 
+import org.mule.metadata.api.model.ArrayType;
+import org.mule.metadata.api.model.MetadataType;
+import org.mule.metadata.api.visitor.BasicTypeMetadataVisitor;
+import org.mule.metadata.api.visitor.MetadataTypeVisitor;
 import org.mule.runtime.api.artifact.Registry;
 import org.mule.runtime.api.dsl.DslResolvingContext;
 import org.mule.runtime.api.meta.model.ExtensionModel;
@@ -38,15 +43,18 @@ import org.mule.runtime.dsl.api.component.ComponentBuildingDefinition;
 import org.mule.runtime.dsl.api.component.ComponentBuildingDefinition.Builder;
 import org.mule.runtime.dsl.api.component.ComponentBuildingDefinitionProvider;
 import org.mule.runtime.dsl.api.component.KeyAttributeDefinitionPair;
+import org.mule.runtime.extension.api.dsl.syntax.DslElementSyntax;
 import org.mule.runtime.extension.api.dsl.syntax.resolver.DslSyntaxResolver;
 import org.mule.runtime.extension.api.property.XmlExtensionModelProperty;
 import org.mule.runtime.extension.api.runtime.config.ConfigurationProvider;
+import org.mule.runtime.extension.api.util.ExtensionMetadataTypeUtils;
 import org.mule.runtime.extension.internal.factories.ModuleOperationMessageProcessorFactoryBean;
 import org.mule.runtime.module.extension.internal.config.ExtensionBuildingDefinitionProvider;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -172,20 +180,8 @@ public class XmlExtensionBuildingDefinitionProvider implements ExtensionBuilding
 
     final List<ParameterModel> allParameterModels = operationModel.getAllParameterModels();
     for (ParameterModel parameterModel : allParameterModels) {
-      if (parameterModel.getDslConfiguration().allowsInlineDefinition() &&
-          parameterModel.getRole() != BEHAVIOUR) {
-        paramsDefinitions.add(newBuilder()
-            .withKey(parameterModel.getName())
-            .withAttributeDefinition(fromChildConfiguration(String.class)
-                .withIdentifier(dslSyntaxResolver.resolve(parameterModel)
-                    .getElementName())
-                .withDefaultValue(parameterModel.getDefaultValue()).build())
-            .build());
-
-        definitions.add(baseDefinition
-            .withIdentifier(dslSyntaxResolver.resolve(parameterModel).getElementName())
-            .withTypeDefinition(fromType(String.class))
-            .build());
+      if (parameterModel.getDslConfiguration().allowsInlineDefinition()) {
+        processInlineParameterDefinition(baseDefinition, dslSyntaxResolver, paramsDefinitions, parameterModel);
       } else {
         paramsDefinitions.add(newBuilder()
             .withKey(parameterModel.getName())
@@ -197,6 +193,66 @@ public class XmlExtensionBuildingDefinitionProvider implements ExtensionBuilding
       }
     }
     return paramsDefinitions;
+  }
+
+  private void processInlineParameterDefinition(Builder baseDefinition, DslSyntaxResolver dslSyntaxResolver,
+                                                List<KeyAttributeDefinitionPair> paramsDefinitions,
+                                                ParameterModel parameterModel) {
+    Class type = isBehaviour(parameterModel) ? getType(parameterModel.getType()) : String.class;
+
+    // Adds the inline parameter definition including its child configuration
+    paramsDefinitions.add(newBuilder()
+        .withKey(parameterModel.getName())
+        .withAttributeDefinition(fromChildConfiguration(type)
+            .withIdentifier(dslSyntaxResolver.resolve(parameterModel).getElementName())
+            .withDefaultValue(parameterModel.getDefaultValue()).build())
+        .build());
+
+    // Adds the parameter definition parser
+    definitions.add(baseDefinition
+        .withIdentifier(dslSyntaxResolver.resolve(parameterModel).getElementName())
+        .withTypeDefinition(fromType(type)).build());
+
+    if (isBehaviour(parameterModel)) {
+      paramsDefinitions.add(newBuilder()
+          .withKey(parameterModel.getName())
+          .withAttributeDefinition(fromSimpleParameter(dslSyntaxResolver.resolve(parameterModel)
+              .getAttributeName())
+                  .withDefaultValue(parameterModel.getDefaultValue())
+                  .build())
+          .build());
+
+      // For behaviour inline parameters, also add the definition parser associated to its type
+      registerComponentBuildingDefinitionType(baseDefinition, parameterModel, dslSyntaxResolver.resolve(parameterModel));
+    }
+  }
+
+  private boolean isBehaviour(ParameterModel parameterModel) {
+    return parameterModel.getRole() == BEHAVIOUR;
+  }
+
+  private void registerComponentBuildingDefinitionType(Builder baseDefinition, ParameterModel parameterModel,
+                                                       DslElementSyntax parameterDsl) {
+    parameterModel.getType().accept(new MetadataTypeVisitor() {
+
+      @Override
+      public void visitArrayType(ArrayType arrayType) {
+        Optional<DslElementSyntax> itemDsl = parameterDsl.getGeneric(arrayType.getType());
+        if (parameterDsl.supportsChildDeclaration() && itemDsl.isPresent()) {
+          arrayType.getType().accept(new BasicTypeMetadataVisitor() {
+
+            @Override
+            protected void visitBasicType(MetadataType metadataType) {
+              definitions.add(baseDefinition
+                  .withIdentifier(itemDsl.get().getElementName())
+                  .withNamespace(itemDsl.get().getPrefix())
+                  .withTypeDefinition(fromType(ExtensionMetadataTypeUtils.getType(metadataType).orElse(Object.class)))
+                  .build());
+            }
+          });
+        }
+      }
+    });
   }
 
   @Override
