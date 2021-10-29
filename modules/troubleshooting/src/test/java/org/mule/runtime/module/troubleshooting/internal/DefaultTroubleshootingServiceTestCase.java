b868bd576689f93abe0176363b2e868826800fda
Ezequiel Werner <eze210@gmail.com>
2021-10-29 17:06:54 -0300

    lic

--- modules/troubleshooting/src/test/java/org/mule/runtime/module/troubleshooting/internal/DefaultTroubleshootingServiceTestCase.java
+++ modules/troubleshooting/src/test/java/org/mule/runtime/module/troubleshooting/internal/DefaultTroubleshootingServiceTestCase.java
@@ -1,9 +1,13 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.runtime.module.troubleshooting.internal;
 
-import static com.google.gson.JsonParser.parseString;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mule.runtime.module.troubleshooting.internal.DefaultTroubleshootingServiceTestCase.TestTroubleshootingOperation.REQUIRED_ARGUMENT_NAME;
@@ -13,7 +17,6 @@ import static org.mule.runtime.module.troubleshooting.internal.TroubleshootingTe
 import static org.mule.runtime.module.troubleshooting.internal.TroubleshootingTestUtils.mockFlowStackEntry;
 import static org.mule.runtime.module.troubleshooting.internal.operations.EventDumpOperation.EVENT_DUMP_OPERATION_NAME;
 
-import com.google.gson.JsonElement;
 import org.junit.Before;
 import org.junit.Test;
 import org.mule.runtime.core.api.event.EventContextService.FlowStackEntry;
