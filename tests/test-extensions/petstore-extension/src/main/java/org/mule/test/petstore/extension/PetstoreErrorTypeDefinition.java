d7da4768a9840b48de4e0bc8cd925c9ccd738aad
Mariana DAbreu <dabreu@users.noreply.github.com>
2021-11-02 18:53:01 -0300

    MULE-19895: Incorrect connectivity error type returned when ConnectionException is thrown (#10937)

--- tests/test-extensions/petstore-extension/src/main/java/org/mule/test/petstore/extension/PetstoreErrorTypeDefinition.java
+++ tests/test-extensions/petstore-extension/src/main/java/org/mule/test/petstore/extension/PetstoreErrorTypeDefinition.java
@@ -7,7 +7,23 @@
 package org.mule.test.petstore.extension;
 
 import org.mule.runtime.extension.api.error.ErrorTypeDefinition;
+import org.mule.runtime.extension.api.error.MuleErrors;
+
+import java.util.Optional;
 
 public enum PetstoreErrorTypeDefinition implements ErrorTypeDefinition<PetstoreErrorTypeDefinition> {
-  PET_ERROR
+
+  CONNECTIVITY(MuleErrors.CONNECTIVITY), PET_STORE_CONNECTIVITY(CONNECTIVITY), PET_ERROR;
+
+  private ErrorTypeDefinition<? extends Enum<?>> parentError;
+
+  private PetstoreErrorTypeDefinition(ErrorTypeDefinition<? extends Enum<?>> parentError) {
+    this.parentError = parentError;
+  }
+
+  private PetstoreErrorTypeDefinition() {}
+
+  public Optional<ErrorTypeDefinition<? extends Enum<?>>> getParent() {
+    return Optional.ofNullable(this.parentError);
+  }
 }
