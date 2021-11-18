e3329d3a8286c18609d478ea0655a44c674cb264
Rodrigo Merino <elrodro83@users.noreply.github.com>
2021-11-18 10:56:56 -0300

    MULE-19936: ConfigurationPropertiesProviderFactoryUtils not properly handling children/complex params (#10998)

---
+++ tests/test-extensions/test-config-properties-extension-deprecated-complex/src/main/java/org/mule/test/complex/config/properties/deprecated/extension/connection/SomePojo.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.complex.config.properties.deprecated.extension.connection;
+
+public class SomePojo {
+
+  private String textValue;
+
+  public String getTextValue() {
+    return textValue;
+  }
+
+  public void setTextValue(String textValue) {
+    this.textValue = textValue;
+  }
+}
