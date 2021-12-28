cdbbe01853fae311febbf8410c7c20ae6293d53c
rbourbonnavarro <rodrigobourbon44@gmail.com>
2021-12-28 17:29:40 -0300

    MULE-20012: Custom object serializer mishandled when lazy init is run multiple times (#11120) (#11123)
    (cherry picked from commit 45c7afb6600807e81c5e0852ae0d2d799813b42e)

--- tests/allure/src/main/java/org/mule/test/allure/AllureConstants.java
+++ tests/allure/src/main/java/org/mule/test/allure/AllureConstants.java
@@ -889,4 +889,9 @@ public interface AllureConstants {
       String OBJECT_PROPERTIES = "Object properties";
     }
   }
+
+  interface ObjectSerializer {
+
+    String CUSTOM_OBJECT_SERIALIZER = "Custom object serializer";
+  }
 }
