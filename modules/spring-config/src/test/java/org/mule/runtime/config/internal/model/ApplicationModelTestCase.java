8d942d7bd0156a53a1d40e00091507051f45ac85
jcgrethe <jgrethe@itba.edu.ar>
2022-01-06 11:09:09 -0300

    change assert to list

--- modules/spring-config/src/test/java/org/mule/runtime/config/internal/model/ApplicationModelTestCase.java
+++ modules/spring-config/src/test/java/org/mule/runtime/config/internal/model/ApplicationModelTestCase.java
@@ -12,14 +12,14 @@ import static org.junit.Assert.assertThat;
 import io.qameta.allure.Issue;
 import org.junit.Test;
 
-import java.util.Collection;
+import java.util.List;
 
 public class ApplicationModelTestCase {
 
   @Test
   @Issue("MULE-20020")
   public void testPrepareAstMustReturnsACollection() {
-    assertThat(ApplicationModelAstPostProcessor.AST_POST_PROCESSORS.get(), instanceOf(Collection.class));
+    assertThat(ApplicationModelAstPostProcessor.AST_POST_PROCESSORS.get(), instanceOf(List.class));
   }
 
 }
