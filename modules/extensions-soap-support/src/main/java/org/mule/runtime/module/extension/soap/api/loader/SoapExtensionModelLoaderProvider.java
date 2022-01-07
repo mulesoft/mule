8744a98a62b49baa0674b6d9dc5f0d89ab4d8c00
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2022-01-07 15:13:29 +0100

    support soap

---
+++ modules/extensions-soap-support/src/main/java/org/mule/runtime/module/extension/soap/api/loader/SoapExtensionModelLoaderProvider.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.module.extension.soap.api.loader;
+
+import static java.util.Collections.singleton;
+
+import org.mule.runtime.extension.api.loader.ExtensionModelLoader;
+import org.mule.runtime.extension.api.loader.ExtensionModelLoaderProvider;
+import org.mule.runtime.module.extension.soap.internal.loader.SoapExtensionModelLoader;
+
+import java.util.Set;
+
+public class SoapExtensionModelLoaderProvider implements ExtensionModelLoaderProvider {
+
+  @Override
+  public Set<ExtensionModelLoader> getExtensionModelLoaders() {
+    return singleton(new SoapExtensionModelLoader());
+  }
+}
