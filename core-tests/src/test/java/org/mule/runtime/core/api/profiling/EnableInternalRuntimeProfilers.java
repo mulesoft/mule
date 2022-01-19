061976a5c2bcadf57b071674a1cafb7e5dc7b133
Fabian Gonzalez <fabian.gonzalez@mulesoft.com>
2022-01-19 17:03:43 -0300

    MULE-20053: Add Test Rule to enable Data Consumers as Internal Runtime Consumers

--- core-tests/src/test/java/org/mule/runtime/core/api/profiling/EnableInternalRuntimeProfilers.java
+++ core-tests/src/test/java/org/mule/runtime/core/api/profiling/EnableInternalRuntimeProfilers.java
@@ -25,7 +25,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Enables the {@link ProfilingDataConsumer}'s as internal runtime profilers so that they will be enabled by default without using
+ * Enables the {@link ProfilingDataConsumer}'s as internal runtime profilers so that they will consume profiling data by default without using
  * the feature toggling mechanism.
  *
  * The {@link ProfilingDataConsumer}'s are disabled after the test.
