0c3654166351f4abe74e40ee57b8d527f15a7e19
Rodrigo Merino <elrodro83@users.noreply.github.com>
2022-01-20 16:01:07 -0300

    MULE-20039: Move internal classes of spring-config module to meaningful (#11254)
    subpackages

---
+++ modules/spring-config/src/main/java/org/mule/runtime/config/internal/model/ComponentModelInitializer.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.config.internal.model;
+
+import org.mule.runtime.ast.api.ComponentAst;
+
+import java.util.function.Predicate;
+
+/**
+ * Internal class to be used to initialize {@link ComponentAst} lazily.
+ *
+ * @since 4.1
+ */
+public interface ComponentModelInitializer {
+
+  /**
+   * Initializes the {@link <ComponentAst> componentModels} that match for the predicate.
+   *
+   * @param componentModelPredicate a {@link Predicate} for {@link ComponentAst} to be initialized.
+   * @param applyStartPhase         boolean indicating if the Start phase should be applied to the created components
+   */
+  void initializeComponents(Predicate<ComponentAst> componentModelPredicate, boolean applyStartPhase);
+
+}
