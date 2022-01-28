3806f65b62e2d019446eb259e1498f5a94a4fb99
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2022-01-28 12:25:39 +0100

    green tests

--- modules/extensions-mule-support/src/test/java/org/mule/runtime/module/extension/mule/internal/loader/ApplicationAsExtensionModelTestCase.java
+++ modules/extensions-mule-support/src/test/java/org/mule/runtime/module/extension/mule/internal/loader/ApplicationAsExtensionModelTestCase.java
@@ -6,10 +6,27 @@
  */
 package org.mule.runtime.module.extension.mule.internal.loader;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
 import static org.mule.runtime.api.util.Preconditions.checkArgument;
 
 import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
+import org.mule.runtime.api.meta.NamedObject;
 import org.mule.runtime.api.meta.model.ExtensionModel;
+import org.mule.runtime.api.meta.model.config.ConfigurationModel;
+import org.mule.runtime.api.meta.model.connection.ConnectionProviderModel;
+import org.mule.runtime.api.meta.model.connection.HasConnectionProviderModels;
+import org.mule.runtime.api.meta.model.construct.ConstructModel;
+import org.mule.runtime.api.meta.model.construct.HasConstructModels;
+import org.mule.runtime.api.meta.model.function.FunctionModel;
+import org.mule.runtime.api.meta.model.function.HasFunctionModels;
+import org.mule.runtime.api.meta.model.operation.HasOperationModels;
+import org.mule.runtime.api.meta.model.operation.OperationModel;
+import org.mule.runtime.api.meta.model.source.HasSourceModels;
+import org.mule.runtime.api.meta.model.source.SourceModel;
+import org.mule.runtime.api.meta.model.util.ExtensionWalker;
 import org.mule.runtime.core.api.extension.ExtensionManager;
 import org.mule.runtime.core.api.util.IOUtils;
 import org.mule.runtime.extension.api.persistence.ExtensionModelJsonSerializer;
@@ -37,10 +54,56 @@ public class ApplicationAsExtensionModelTestCase extends MuleArtifactFunctionalT
 
   @Test
   public void loadApplicationExtensionModel() throws Exception {
-    ExtensionModel extensionModel = extensionManager.getExtension(muleContext.getConfiguration().getId()).get();
+    ExtensionModel extensionModel = getAppExtensionModel();
     String json = serializer.serialize(extensionModel);
     String expected = getResource("/models/app-as-mule-extension.json");
     JSONAssert.assertEquals(expected, json, true);
+  }
+
+  @Test
+  public void flowIsIgnored() throws Exception {
+    ExtensionModel extensionModel = getAppExtensionModel();
+
+    new ExtensionWalker() {
+
+      @Override
+      protected void onOperation(HasOperationModels owner, OperationModel model) {
+        assertNotFlow(model);
+      }
+
+      @Override
+      protected void onSource(HasSourceModels owner, SourceModel model) {
+        assertNotFlow(model);
+      }
+
+      @Override
+      protected void onConstruct(HasConstructModels owner, ConstructModel model) {
+        assertNotFlow(model);
+      }
+
+      @Override
+      protected void onConfiguration(ConfigurationModel model) {
+        assertNotFlow(model);
+      }
+
+      @Override
+      protected void onConnectionProvider(HasConnectionProviderModels owner, ConnectionProviderModel model) {
+        assertNotFlow(model);
+      }
+
+      @Override
+      protected void onFunction(HasFunctionModels owner, FunctionModel model) {
+        assertNotFlow(model);
+      }
+    }.walk(extensionModel);
+  }
+
+  private void assertNotFlow(NamedObject named) {
+    assertThat(named.getName(), is(not(equalTo("superfluousFlow"))));
+  }
+
+  private ExtensionModel getAppExtensionModel() {
+    return extensionManager.getExtension(muleContext.getConfiguration().getId()).get();
   }
 
   private String getResource(String path) {
