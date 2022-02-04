992f4dfca83fb859dcb80ed2b0e743d783391de6
Ezequiel Werner <eze210@gmail.com>
2022-02-04 09:24:12 -0300

    st

---
+++ modules/http2-api/src/main/java/org/mule/runtime/http2/api/domain/Http2Interaction.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.http2.api.domain;
+
+import org.mule.runtime.http2.api.server.Http2ResponseSender;
+
+public class Http2Interaction {
+
+  private Http2ResponseSender responseSender;
+  private Http2Response response;
+
+  public void setResponseSender(Http2ResponseSender responseSender) {
+    this.responseSender = responseSender;
+  }
+
+  public void setResponse(Http2Response response) {
+    this.response = response;
+  }
+
+  public void sendResponse() {
+    this.responseSender.sendResponse(response);
+  }
+}
