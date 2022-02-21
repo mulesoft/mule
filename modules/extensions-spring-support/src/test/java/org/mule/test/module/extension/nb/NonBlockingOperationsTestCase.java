090f80065162894fb94975246fb01aa79521fcd8
rbourbonnavarro <rodrigobourbon44@gmail.com>
2022-02-21 17:13:12 -0300

    MULE-19537: restoring assertion in test.

--- modules/extensions-spring-support/src/test/java/org/mule/test/module/extension/nb/NonBlockingOperationsTestCase.java
+++ modules/extensions-spring-support/src/test/java/org/mule/test/module/extension/nb/NonBlockingOperationsTestCase.java
@@ -81,6 +81,7 @@ public class NonBlockingOperationsTestCase extends AbstractExtensionFunctionalTe
   @Test
   public void nonBlockingConnectedOperation() throws Exception {
     fireMissileAndAssert("fireMissile");
+    assertCapturedThreadsNameMatch(startsWith("SimpleUnitTestSupportScheduler."));
   }
 
   @Test
@@ -152,6 +153,8 @@ public class NonBlockingOperationsTestCase extends AbstractExtensionFunctionalTe
     IronMan ironMan = getIronMan("ironMan");
     final String payload = "take me to the avengers tower";
     Event event = flowRunner("computeFlightPlan").withPayload(payload).run();
+    // TODO: MULE-18119 - uncomment the following line when this issue is fixed
+    // assertCapturedThreadsNameMatch(startsWith("SimpleUnitTestSupportScheduler."));
     assertThat(event.getMessage().getPayload().getValue().toString(), equalTo(payload));
     probe(1000, 1000, () -> FLIGHT_PLAN.equals(ironMan.getFlightPlan()));
   }
