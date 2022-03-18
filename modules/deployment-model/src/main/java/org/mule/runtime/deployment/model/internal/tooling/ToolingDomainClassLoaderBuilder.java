518f238a4c1ec1e017a870b3c57dfedc7e8d69f2
elrodro83 <rodrigo.merino@mulesoft.com>
2022-03-18 17:41:14 -0300

    fix

--- modules/deployment-model/src/main/java/org/mule/runtime/deployment/model/internal/tooling/ToolingDomainClassLoaderBuilder.java
+++ modules/deployment-model/src/main/java/org/mule/runtime/deployment/model/internal/tooling/ToolingDomainClassLoaderBuilder.java
@@ -6,22 +6,24 @@
  */
 package org.mule.runtime.deployment.model.internal.tooling;
 
-import static java.lang.String.format;
-import static org.apache.commons.lang3.StringUtils.isEmpty;
 import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
 import static org.mule.runtime.api.util.Preconditions.checkArgument;
 import static org.mule.runtime.deployment.model.internal.tooling.ToolingRegionClassLoader.newToolingRegionClassLoader;
 
+import static java.lang.String.format;
+
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
 import org.mule.runtime.deployment.model.api.DeploymentException;
 import org.mule.runtime.deployment.model.api.builder.RegionPluginClassLoadersFactory;
 import org.mule.runtime.deployment.model.api.domain.Domain;
-import org.mule.runtime.deployment.model.api.domain.DomainDescriptor;
 import org.mule.runtime.deployment.model.internal.AbstractArtifactClassLoaderBuilder;
 import org.mule.runtime.module.artifact.api.classloader.ArtifactClassLoader;
 import org.mule.runtime.module.artifact.api.classloader.ClassLoaderLookupPolicy;
 import org.mule.runtime.module.artifact.api.classloader.DeployableArtifactClassLoaderFactory;
 import org.mule.runtime.module.artifact.api.classloader.RegionClassLoader;
 import org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptor;
+import org.mule.runtime.module.artifact.api.descriptor.DomainDescriptor;
 
 import java.io.IOException;
 
