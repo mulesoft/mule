be930c9b520361d9986c2214ad2ebed6a474b92a
Juan Baca Paunero <Bardammu@users.noreply.github.com>
2022-03-22 17:49:49 -0300

    W-10619908: Runtime should dispatch ExceptionNotification when ExtensionMessageSource fails to start (#11357) (#11380)

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/source/ExtensionMessageSource.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/source/ExtensionMessageSource.java
@@ -48,6 +48,7 @@ import org.mule.runtime.core.api.SingleResourceTransactionFactoryManager;
 import org.mule.runtime.core.api.construct.FlowConstruct;
 import org.mule.runtime.core.api.el.ExpressionManager;
 import org.mule.runtime.core.api.event.CoreEvent;
+import org.mule.runtime.core.api.exception.SystemExceptionHandler;
 import org.mule.runtime.core.api.extension.ExtensionManager;
 import org.mule.runtime.core.api.lifecycle.LifecycleState;
 import org.mule.runtime.core.api.lifecycle.LifecycleStateEnabled;
@@ -94,13 +95,12 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import javax.inject.Inject;
 
 import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
-import java.util.function.Supplier;
-
 import reactor.core.publisher.Mono;
 
 /**
@@ -210,9 +210,15 @@ public class ExtensionMessageSource extends ExtensionComponent<SourceModel> impl
   private void startSource(boolean restarting, RestartContext restartContext) throws MuleException {
     Runnable onSuccess;
     Consumer<Throwable> onFailure;
+    SystemExceptionHandler systemExceptionHandler = muleContext.getExceptionListener();
     if (retryPolicyTemplate.isAsync()) {
       onSuccess = this::onReconnectionSuccessful;
-      onFailure = this::onReconnectionFailed;
+      onFailure = (t) -> {
+        RetryPolicyExhaustedException exception = t instanceof RetryPolicyExhaustedException ? (RetryPolicyExhaustedException) t
+            : new RetryPolicyExhaustedException(t, ExtensionMessageSource.this);
+        systemExceptionHandler.handleException(exception);
+        onReconnectionFailed(exception);
+      };
     } else {
       onSuccess = () -> {
       };
@@ -239,11 +245,17 @@ public class ExtensionMessageSource extends ExtensionComponent<SourceModel> impl
         this.onReconnectionSuccessful();
       }
     } catch (ExecutionException exception) {
-      throw new RetryPolicyExhaustedException(exception.getCause(), ExtensionMessageSource.this);
+      RetryPolicyExhaustedException retryPolicyExhaustedException =
+          new RetryPolicyExhaustedException(exception.getCause(), ExtensionMessageSource.this);
+      systemExceptionHandler.handleException(retryPolicyExhaustedException);
+      throw retryPolicyExhaustedException;
     } catch (InterruptedException e) {
-      throw new MuleRuntimeException(createStaticMessage(format("Found exception starting source '%s' on flow '%s'",
-                                                                sourceModel.getName(), getLocation().getRootContainerName())),
-                                     e);
+      MuleRuntimeException muleRuntimeException =
+          new MuleRuntimeException(createStaticMessage(format("Found exception starting source '%s' on flow '%s'",
+                                                              sourceModel.getName(), getLocation().getRootContainerName())),
+                                   e);
+      systemExceptionHandler.handleException(muleRuntimeException);
+      throw muleRuntimeException;
     }
   }
 
