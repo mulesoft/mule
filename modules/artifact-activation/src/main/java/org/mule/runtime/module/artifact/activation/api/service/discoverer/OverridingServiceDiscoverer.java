0bd9d8b779cd61ae5f58f6f4be65e6c5bff800fd
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2022-03-29 16:11:12 +0200

    progress

---
+++ modules/artifact-activation/src/main/java/org/mule/runtime/module/artifact/activation/api/service/discoverer/OverridingServiceDiscoverer.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.module.artifact.activation.api.service.discoverer;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+
+import org.mule.runtime.api.service.Service;
+import org.mule.runtime.module.artifact.activation.api.service.ServiceAssembly;
+import org.mule.runtime.module.artifact.activation.api.service.ServiceProviderDiscoverer;
+import org.mule.runtime.module.artifact.activation.api.service.ServiceResolutionError;
+import org.mule.runtime.module.artifact.activation.internal.service.discoverer.DefaultServiceDiscoverer;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class OverridingServiceDiscoverer extends DefaultServiceDiscoverer {
+
+  private final Map<String, Service> overrides;
+
+  public OverridingServiceDiscoverer(ServiceProviderDiscoverer serviceProviderDiscoverer, List<Service> overrides) {
+    super(serviceProviderDiscoverer);
+    this.overrides = overrides.stream().collect(toMap(Service::getName, identity()));
+  }
+
+  @Override
+  public List<Service> discoverServices() throws ServiceResolutionError {
+    List<Service> services = new LinkedList<>(super.discoverServices());
+    services.addAll(overrides.values());
+
+    return services;
+  }
+
+  @Override
+  protected List<ServiceAssembly> discoverAssemblies() throws ServiceResolutionError {
+    return super.discoverAssemblies().stream()
+        .filter(assembly -> !overrides.containsKey(assembly.getName()))
+        .collect(toList());
+  }
+}
