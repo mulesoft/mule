b59753b87c534e91e99aed44b67836525b5db126
Mariano Rean <mrean@mulesoft.com>
2022-03-30 10:46:36 -0300

    cr update

--- core/src/main/java/org/mule/runtime/core/internal/exception/LocalErrorHandler.java
+++ core/src/main/java/org/mule/runtime/core/internal/exception/LocalErrorHandler.java
@@ -1,12 +1,26 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.runtime.core.internal.exception;
 
 import org.mule.runtime.api.exception.MuleException;
 
 public class LocalErrorHandler extends ErrorHandler {
 
-    @Override
-    public void start() throws MuleException {}
+  @Override
+  public void start() throws MuleException {
+    if (logger.isDebugEnabled()) {
+      logger.debug("In start of local error handler: {}", this);
+    }
+  }
 
-    @Override
-    public void stop() throws MuleException {}
+  @Override
+  public void stop() throws MuleException {
+    if (logger.isDebugEnabled()) {
+      logger.debug("In stop of local error handler: {}", this);
+    }
+  }
 }
