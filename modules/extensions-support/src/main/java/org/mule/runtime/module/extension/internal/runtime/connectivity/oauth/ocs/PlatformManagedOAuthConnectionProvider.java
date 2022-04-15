31157f8a090b0283c479628964ae64aa43b8cc34
ndinu <ndinu@users.noreply.github.com>
2022-04-15 19:12:56 -0300

    W-10867511: MuleException parsing complex parameters sent by OCS (#11418)

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/connectivity/oauth/ocs/PlatformManagedOAuthConnectionProvider.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/connectivity/oauth/ocs/PlatformManagedOAuthConnectionProvider.java
@@ -22,7 +22,7 @@ import static org.mule.runtime.core.internal.util.FunctionalUtils.safely;
 import static org.mule.runtime.module.extension.internal.runtime.connectivity.oauth.ExtensionsOAuthUtils.getCallbackValuesExtractors;
 import static org.mule.runtime.module.extension.internal.runtime.connectivity.oauth.ExtensionsOAuthUtils.updateOAuthParameters;
 import static org.mule.runtime.module.extension.internal.runtime.connectivity.oauth.ExtensionsOAuthUtils.validateOAuthConnection;
-import static org.mule.runtime.module.extension.internal.runtime.resolver.ParametersResolver.fromValues;
+import static org.mule.runtime.module.extension.internal.runtime.resolver.ResolverSetUtils.getResolverSetFromStaticValues;
 import static org.mule.runtime.module.extension.internal.util.MuleExtensionUtils.getClassLoader;
 import static org.mule.runtime.module.extension.internal.util.MuleExtensionUtils.getImplementingType;
 import static org.slf4j.LoggerFactory.getLogger;
@@ -37,6 +37,7 @@ import org.mule.runtime.api.exception.DefaultMuleException;
 import org.mule.runtime.api.exception.MuleException;
 import org.mule.runtime.api.lifecycle.InitialisationException;
 import org.mule.runtime.api.meta.model.connection.ConnectionManagementType;
+import org.mule.runtime.api.meta.model.parameter.ParameterizedModel;
 import org.mule.runtime.api.util.Pair;
 import org.mule.runtime.api.util.Reference;
 import org.mule.runtime.core.api.MuleContext;
@@ -61,7 +62,6 @@ import org.mule.runtime.module.extension.internal.runtime.config.ConnectionProvi
 import org.mule.runtime.module.extension.internal.runtime.config.DefaultConnectionProviderObjectBuilder;
 import org.mule.runtime.module.extension.internal.runtime.connectivity.oauth.ExtensionsOAuthUtils;
 import org.mule.runtime.module.extension.internal.runtime.connectivity.oauth.OAuthConnectionProviderWrapper;
-import org.mule.runtime.module.extension.internal.runtime.resolver.ParametersResolver;
 import org.mule.runtime.module.extension.internal.runtime.resolver.ResolverSet;
 import org.mule.runtime.module.extension.internal.runtime.resolver.ValueResolvingContext;
 import org.mule.runtime.module.extension.internal.util.FieldSetter;
@@ -195,19 +195,12 @@ public class PlatformManagedOAuthConnectionProvider<C>
   }
 
   private ConnectionProvider<C> createDelegate(PlatformManagedConnectionDescriptor descriptor) throws MuleException {
-    ParametersResolver resolver = fromValues(descriptor.getParameters(),
-                                             muleContext,
-                                             false,
-                                             new ReflectionCache(),
-                                             expressionManager,
-                                             this.toString());
     Class<?> connectionProviderDelegateClass =
         getImplementingType(oauthConfig.getDelegateConnectionProviderModel())
             .orElseThrow(() -> new IllegalStateException("Delegate connection provider must have an implementing type."));
 
     return (ConnectionProvider<C>) withContextClassLoader(getClassLoader(oauthConfig.getExtensionModel()), () -> {
-      ResolverSet delegateResolverSet =
-          resolver.getParametersAsResolverSet(oauthConfig.getDelegateConnectionProviderModel(), muleContext);
+      ResolverSet delegateResolverSet = getResolverSetFromParameterValues(descriptor.getParameters());
       ConnectionProviderObjectBuilder builder =
           new DefaultConnectionProviderObjectBuilder<>(connectionProviderDelegateClass,
                                                        oauthConfig.getDelegateConnectionProviderModel(),
@@ -233,6 +226,22 @@ public class PlatformManagedOAuthConnectionProvider<C>
         }
       }
     }, MuleException.class, e -> e);
+  }
+
+  private ResolverSet getResolverSetFromParameterValues(Map<String, Object> parameters) throws MuleException {
+    return getResolverSetForParameterizedModel(oauthConfig.getDelegateConnectionProviderModel(), parameters);
+  }
+
+  private ResolverSet getResolverSetForParameterizedModel(ParameterizedModel parameterizedModel,
+                                                          Map<String, Object> parameters)
+      throws MuleException {
+    return getResolverSetFromStaticValues(parameterizedModel,
+                                          parameters,
+                                          muleContext,
+                                          false,
+                                          new ReflectionCache(),
+                                          expressionManager,
+                                          this.toString());
   }
 
   private PlatformManagedConnectionDescriptor fetchConnectionDescriptor() throws MuleException {
