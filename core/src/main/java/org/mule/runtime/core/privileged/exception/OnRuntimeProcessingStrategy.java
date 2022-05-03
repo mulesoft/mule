50c659af12769a20a33b8db57d88e2179bd4f666
Mariano Rean <mrean@mulesoft.com>
2022-05-03 15:53:23 -0300

    use the parent flow to obtain ps

--- core/src/main/java/org/mule/runtime/core/privileged/exception/OnRuntimeProcessingStrategy.java
+++ core/src/main/java/org/mule/runtime/core/privileged/exception/OnRuntimeProcessingStrategy.java
@@ -40,7 +40,7 @@ public class OnRuntimeProcessingStrategy implements ProcessingStrategy {
   public ReactiveProcessor onProcessor(ReactiveProcessor processor) {
     return publisher -> Flux.from(publisher)
         .flatMap(e -> {
-          String location = e.getContext().getOriginatingLocation().getRootContainerName();
+          String location = e.getFlowCallStack().getElements().get(0).getFlowName();
           return Mono.just(e).transform(getProcessingStrategy(location).map(ps -> ps.onProcessor(processor))
               .orElse(getProcessor(processor, location)));
         });
