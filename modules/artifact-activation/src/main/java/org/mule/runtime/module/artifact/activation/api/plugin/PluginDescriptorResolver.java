84a232527417ae5edb09f24e3f2f0cf8da819c4a
rbourbonnavarro <rodrigobourbon44@gmail.com>
2022-05-04 12:26:56 -0300

    Fixing compilation.

--- modules/artifact-activation/src/main/java/org/mule/runtime/module/artifact/activation/api/plugin/PluginDescriptorResolver.java
+++ modules/artifact-activation/src/main/java/org/mule/runtime/module/artifact/activation/api/plugin/PluginDescriptorResolver.java
@@ -38,8 +38,9 @@ public interface PluginDescriptorResolver {
    * The function must return {@link Optional#empty()} if the plugin represented by the {@link BundleDescriptor} is not a
    * dependency of the artifact for {@code ownerArtifactClassLoader}.
    *
-   * @param artifactPluginDescriptors plugin descriptors from which to retrieve the one matching the given {@code bundleDescriptor}.
-   * @param bundleDescriptor the bundle descriptor of the plugin to get the artifact descriptor for.
+   * @param artifactPluginDescriptors plugin descriptors from which to retrieve the one matching the given
+   *                                  {@code bundleDescriptor}.
+   * @param bundleDescriptor          the bundle descriptor of the plugin to get the artifact descriptor for.
    * @return optionally returns an {@link ArtifactPluginDescriptor} corresponding to the given {@link BundleDescriptor}.
    */
   Optional<ArtifactPluginDescriptor> resolve(Set<ArtifactPluginDescriptor> artifactPluginDescriptors,
