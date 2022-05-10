94a299207ce6fb962276bfbf1962705640d59f96
Mariano Rean <mrean@mulesoft.com>
2022-05-10 16:02:39 -0300

    Revert "W-10818447: Verify that ErrorHandlerFactoryBean does not generate dup… (#11394) (#11407)"
    This reverts commit eef7a7d3ab876410abc0eef5f31b2077414329b7.

--- core/src/main/java/org/mule/runtime/core/internal/exception/GlobalErrorHandler.java
+++ core/src/main/java/org/mule/runtime/core/internal/exception/GlobalErrorHandler.java
@@ -6,50 +6,28 @@
  */
 package org.mule.runtime.core.internal.exception;
 
-import static org.mule.runtime.api.util.MuleSystemProperties.REVERT_SIGLETON_ERROR_HANDLER_PROPERTY;
-
-import static java.lang.Boolean.getBoolean;
-
 import org.mule.runtime.api.component.location.Location;
-import org.mule.runtime.api.exception.MuleException;
 import org.mule.runtime.core.api.event.CoreEvent;
-
+import org.mule.runtime.core.privileged.exception.MessagingExceptionHandlerAcceptor;
+import org.mule.runtime.core.privileged.exception.TemplateOnErrorHandler;
 import org.reactivestreams.Publisher;
 
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 public class GlobalErrorHandler extends ErrorHandler {
-
-  private static final boolean IS_PROTOTYPE = getBoolean(REVERT_SIGLETON_ERROR_HANDLER_PROPERTY);
-
-  // We need to keep a reference to one of the local error handlers to be able to stop its inner processors.
-  // This is a temporary solution and won't be necessary after W-10674245.
-  // TODO: W-10674245 remove this
-  private ErrorHandler local;
 
   @Override
   public Publisher<CoreEvent> apply(Exception exception) {
     throw new IllegalStateException("GlobalErrorHandlers should be used only as template for local ErrorHandlers");
   }
 
-  @Override
-  public void stop() throws MuleException {
-    if (!IS_PROTOTYPE) {
-      ((LocalErrorHandler) local).stopParent();
-    }
-  }
-
   public ErrorHandler createLocalErrorHandler(Location flowLocation) {
-    ErrorHandler local;
-    if (IS_PROTOTYPE) {
-      local = new ErrorHandler();
-    } else {
-      local = new LocalErrorHandler();
-    }
+    ErrorHandler local = new ErrorHandler();
     local.setName(this.name);
     local.setExceptionListeners(this.getExceptionListeners());
     local.setExceptionListenersLocation(flowLocation);
-    if (this.local == null) {
-      this.local = local;
-    }
     return local;
   }
 }
