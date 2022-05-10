ec0d47786d8e11eaffaf086098b825d4786ecff0
Martín Buchwald <martin.buchwald@mulesoft.com>
2022-05-10 16:34:21 -0300

    review

--- core/src/main/java/org/mule/runtime/core/internal/profiling/consumer/TransactionLoggerDataConsumer.java
+++ core/src/main/java/org/mule/runtime/core/internal/profiling/consumer/TransactionLoggerDataConsumer.java
@@ -14,13 +14,13 @@ import static org.mule.runtime.core.internal.profiling.consumer.ComponentProfili
 import static org.slf4j.LoggerFactory.getLogger;
 import static com.google.common.collect.ImmutableSet.of;
 
-import java.util.Set;
-import java.util.function.Predicate;
-import com.google.gson.Gson;
 import org.mule.runtime.api.profiling.ProfilingDataConsumer;
 import org.mule.runtime.api.profiling.type.ProfilingEventType;
 import org.mule.runtime.api.profiling.type.context.TransactionProfilingEventContext;
 import org.mule.runtime.core.internal.profiling.consumer.annotations.RuntimeInternalProfilingDataConsumer;
+import java.util.Set;
+import java.util.function.Predicate;
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 
 /**
