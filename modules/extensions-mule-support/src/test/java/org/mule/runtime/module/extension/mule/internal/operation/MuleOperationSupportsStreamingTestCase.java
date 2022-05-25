2e418e7ed6dbfb6a6fbed033ea69e41df24c95c9
Martín Buchwald <martin.buchwald@mulesoft.com>
2022-05-25 11:51:02 +0200

    More tests

--- modules/extensions-mule-support/src/test/java/org/mule/runtime/module/extension/mule/internal/operation/MuleOperationSupportsStreamingTestCase.java
+++ modules/extensions-mule-support/src/test/java/org/mule/runtime/module/extension/mule/internal/operation/MuleOperationSupportsStreamingTestCase.java
@@ -6,15 +6,20 @@
  */
 package org.mule.runtime.module.extension.mule.internal.operation;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.is;
 import static org.mule.runtime.extension.api.ExtensionConstants.STREAMING_STRATEGY_PARAMETER_NAME;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
 import org.mule.runtime.api.meta.model.operation.OperationModel;
+import org.mule.runtime.api.streaming.object.CursorIteratorProvider;
+import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.runtime.core.api.extension.ExtensionManager;
+import org.mule.runtime.core.api.streaming.iterator.ConsumerStreamingIterator;
 
 import org.junit.Test;
+
 import javax.inject.Inject;
 import java.util.List;
 
@@ -43,6 +48,50 @@ public class MuleOperationSupportsStreamingTestCase extends MuleArtifactFunction
   @Test
   public void withNonRepeatableStreaming() {
     assertForOpeartion("withNonRepeatableStreaming", true);
+  }
+
+  @Test
+  public void withRepeatableStreamingUsed() {
+    assertForOpeartion("withRepeatableStreamingUsed", true);
+  }
+
+  @Test
+  public void withNonRepeatableStreamingConsumed() {
+    assertForOpeartion("withNonRepeatableStreamingConsumed", true);
+  }
+
+  @Test
+  public void repeatableStreamingOperationExecution() throws Exception {
+    CoreEvent resultEvent = flowRunner("flowRepeatable").run();
+    Object stream = resultEvent.getMessage().getPayload().getValue();
+    assertThat(stream, is(instanceOf(CursorIteratorProvider.class)));
+  }
+
+  @Test
+  public void nonRepeatableStreamingOperationExecution() throws Exception {
+    CoreEvent resultEvent = flowRunner("flowNonRepeatable").keepStreamsOpen().run();
+    Object stream = resultEvent.getMessage().getPayload().getValue();
+    assertThat(stream, is(instanceOf(ConsumerStreamingIterator.class)));
+
+    ConsumerStreamingIterator streamingIterator = (ConsumerStreamingIterator) stream;
+    assertThat(streamingIterator.hasNext(), is(true));
+  }
+
+  @Test
+  public void repeatableStreamingUsedOperationExecution() throws Exception {
+    CoreEvent resultEvent = flowRunner("flowRepeatableUsed").run();
+    Object stream = resultEvent.getMessage().getPayload().getValue();
+    assertThat(stream, is(instanceOf(CursorIteratorProvider.class)));
+  }
+
+  @Test
+  public void consumedNonRepeatableStreamingOperationExecution() throws Exception {
+    CoreEvent resultEvent = flowRunner("flowNonRepeatableConsumed").keepStreamsOpen().run();
+    Object stream = resultEvent.getMessage().getPayload().getValue();
+    assertThat(stream, is(instanceOf(ConsumerStreamingIterator.class)));
+
+    ConsumerStreamingIterator streamingIterator = (ConsumerStreamingIterator) stream;
+    assertThat(streamingIterator.hasNext(), is(false));
   }
 
   private void assertForOpeartion(String operation, boolean expectedSupportStreaming) {
