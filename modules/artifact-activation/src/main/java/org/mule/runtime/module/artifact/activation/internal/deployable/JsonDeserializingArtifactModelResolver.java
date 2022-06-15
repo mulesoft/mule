86ea912c09f40a9bc3e3a6edab289b8b40c95529
rbourbonnavarro <rodrigobourbon44@gmail.com>
2022-06-15 14:48:12 -0300

    Removing `TODO`.

--- modules/artifact-activation/src/main/java/org/mule/runtime/module/artifact/activation/internal/deployable/JsonDeserializingArtifactModelResolver.java
+++ modules/artifact-activation/src/main/java/org/mule/runtime/module/artifact/activation/internal/deployable/JsonDeserializingArtifactModelResolver.java
@@ -45,8 +45,6 @@ public final class JsonDeserializingArtifactModelResolver<M extends MuleDeployab
 
   @Override
   public M resolve(File artifactLocation) {
-    // TODO W-11203071 - the model needs to be completed, when the app is packaged every field is present in the output
-    // mule-artifact.json, but here we don't have that
     final File artifactJsonFile = new File(artifactLocation, MULE_ARTIFACT_JSON_DESCRIPTOR);
     if (!artifactJsonFile.exists()) {
       throw new ArtifactActivationException(createStaticMessage("Couldn't find model file " + artifactJsonFile));
