6232f925e1010117d3285a99a6f37ee2b2b74281
GuidoGianfrate <61558996+GuidoGianfrate@users.noreply.github.com>
2022-07-05 14:22:59 -0300

    W-10583584: Leak in JMS (#11568)
     W-10583584: Leak in JMS / ActiveMQResourceReleaser

---
+++ modules/artifact/src/main/java/org/mule/module/artifact/classloader/ThreadCommonMethodsUtil.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.artifact.classloader;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.mule.runtime.module.artifact.api.classloader.ArtifactClassLoader;
+import org.mule.runtime.module.artifact.api.classloader.MuleArtifactClassLoader;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+import org.slf4j.Logger;
+
+
+/**
+ * This class contain common methods used by ActiveMQResourceReleaser and JdbcResourceReleaser Used to prevent leaks related to
+ * threads
+ */
+public class ThreadCommonMethodsUtil {
+
+  private static final Logger logger = getLogger(ThreadCommonMethodsUtil.class);
+  public static final String COMPOSITE_CLASS_LOADER_CLASS_NAME = "CompositeClassLoader";
+
+  public static boolean isThreadLoadedByDisposedDomain(String undeployedArtifactId, ClassLoader threadContextClassLoader) {
+    try {
+
+      Class threadContextClassLoaderClass = threadContextClassLoader.getClass();
+      if (!threadContextClassLoaderClass.getSimpleName().equals(COMPOSITE_CLASS_LOADER_CLASS_NAME)) {
+        return false;
+      }
+
+
+      Method getDelegateClassLoadersMethod = threadContextClassLoaderClass.getMethod("getDelegates");
+      List<ClassLoader> classLoaderList = (List<ClassLoader>) getDelegateClassLoadersMethod.invoke(threadContextClassLoader);
+
+
+      for (ClassLoader classLoaderDelegate : classLoaderList) {
+        if (classLoaderDelegate instanceof ArtifactClassLoader) {
+          ArtifactClassLoader artifactClassLoader = (ArtifactClassLoader) classLoaderDelegate;
+          if (artifactClassLoader.getArtifactId().contains(undeployedArtifactId)) {
+            return true;
+          }
+        }
+      }
+
+    } catch (Exception e) {
+      logger.warn("Exception occurred while attempting to compare {} and {} artifactId.", threadContextClassLoader,
+                  threadContextClassLoader.getClass().getClassLoader());
+    }
+    return false;
+  }
+
+  public static boolean isThreadLoadedByDisposedApplication(String undeployedArtifactId, ClassLoader threadContextClassLoader) {
+    try {
+      if (!(threadContextClassLoader instanceof MuleArtifactClassLoader)) {
+        return false;
+      }
+      String threadClassLoaderArtifactId = ((MuleArtifactClassLoader) threadContextClassLoader).getArtifactId();
+      return threadClassLoaderArtifactId != null && threadClassLoaderArtifactId.equals(undeployedArtifactId);
+    } catch (Exception e) {
+      logger.warn("Exception occurred while attempting to compare {} and {} artifact id.", threadContextClassLoader,
+                  threadContextClassLoader.getClass().getClassLoader());
+    }
+    return false;
+  }
+
+  public static void clearReferencesStopTimerThread(Thread thread)
+      throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+    // Need to get references to:
+    // in Sun/Oracle JDK:
+    // - newTasksMayBeScheduled field (in java.util.TimerThread)
+    // - queue field
+    // - queue.clear()
+    // in IBM JDK, Apache Harmony:
+    // - cancel() method (in java.util.Timer$TimerImpl)
+    try {
+      Field newTasksMayBeScheduledField =
+          thread.getClass().getDeclaredField("newTasksMayBeScheduled");
+      newTasksMayBeScheduledField.setAccessible(true);
+      Field queueField = thread.getClass().getDeclaredField("queue");
+      queueField.setAccessible(true);
+      Object queue = queueField.get(thread);
+      Method clearMethod = queue.getClass().getDeclaredMethod("clear");
+      clearMethod.setAccessible(true);
+      synchronized (queue) {
+        newTasksMayBeScheduledField.setBoolean(thread, false);
+        clearMethod.invoke(queue);
+        // In case queue was already empty. Should only be one
+        // thread waiting but use notifyAll() to be safe.
+        queue.notifyAll();
+      }
+    } catch (NoSuchFieldException noSuchFieldEx) {
+      logger.warn("Unable to clear timer references using 'clear' method. Attempting to use 'cancel' method.");
+      Method cancelMethod = thread.getClass().getDeclaredMethod("cancel");
+      cancelMethod.setAccessible(true);
+      cancelMethod.invoke(thread);
+    }
+  }
+}
---
+++ modules/artifact/src/test/java/org/mule/runtime/module/artifact/classloader/ActiveMQResourceReleaserTestCase.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.module.artifact.classloader;
+
+import static org.mule.maven.client.api.MavenClientProvider.discoverProvider;
+import static org.mule.runtime.core.internal.util.CompositeClassLoader.from;
+import static org.mule.test.allure.AllureConstants.LeakPrevention.LEAK_PREVENTION;
+import static org.mule.maven.client.api.model.MavenConfiguration.newMavenConfigurationBuilder;
+import static org.mule.runtime.module.artifact.api.classloader.ChildFirstLookupStrategy.CHILD_FIRST;
+import static org.mule.test.allure.AllureConstants.LeakPrevention.LeakPreventionMetaspace.METASPACE_LEAK_PREVENTION_ON_REDEPLOY;
+import static java.lang.Thread.enumerate;
+import static java.lang.Thread.activeCount;
+import static java.lang.Thread.currentThread;
+import static org.mockito.Mockito.mock;
+import static org.apache.commons.io.FileUtils.toFile;
+import static org.junit.Assert.assertFalse;
+import static org.awaitility.Awaitility.await;
+
+import org.mule.maven.client.api.MavenClient;
+import org.mule.maven.client.api.MavenClientProvider;
+import org.mule.maven.client.api.model.BundleDependency;
+import org.mule.maven.client.api.model.BundleDescriptor;
+import org.mule.maven.client.api.model.MavenConfiguration;
+import org.mule.runtime.core.internal.util.CompositeClassLoader;
+import org.mule.runtime.module.artifact.api.classloader.ClassLoaderLookupPolicy;
+import org.mule.runtime.module.artifact.api.classloader.LookupStrategy;
+import org.mule.runtime.module.artifact.api.classloader.MuleArtifactClassLoader;
+import org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptor;
+import org.mule.tck.junit4.AbstractMuleTestCase;
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@Feature(LEAK_PREVENTION)
+@RunWith(Parameterized.class)
+@Story(METASPACE_LEAK_PREVENTION_ON_REDEPLOY)
+public class ActiveMQResourceReleaserTestCase extends AbstractMuleTestCase {
+
+  private final static String DRIVER_ARTIFACT_ID = "activemq-all";
+  private final static String DRIVER_GROUP_ID = "org.apache.activemq";
+  static final String DRIVER_CLASS_NAME = "org.apache.activemq.ActiveMQConnectionFactory";
+  private static final String ACTIVEMQ_DRIVER_TIMER_THREAD_NAME = "ActiveMQ InactivityMonitor ReadCheckTimer";
+  private final static String ACTIVEMQ_URL_CONFIG =
+      "failover:(tcp://192.168.1.111:61616)?jms.useAsyncSend=true&initialReconnectDelay=1000&maxReconnectAttempts=-1";
+
+  String driverVersion;
+  private final ClassLoaderLookupPolicy testLookupPolicy;
+  MuleArtifactClassLoader artifactClassLoader = null;
+
+  public ActiveMQResourceReleaserTestCase(String driverVersion) {
+    this.driverVersion = driverVersion;
+    this.testLookupPolicy = new ClassLoaderLookupPolicy() {
+
+      @Override
+      public LookupStrategy getClassLookupStrategy(String className) {
+        return CHILD_FIRST;
+      }
+
+      @Override
+      public LookupStrategy getPackageLookupStrategy(String packageName) {
+        return null;
+      }
+
+      @Override
+      public ClassLoaderLookupPolicy extend(Map<String, LookupStrategy> lookupStrategies) {
+        return null;
+      }
+
+      @Override
+      public ClassLoaderLookupPolicy extend(Stream<String> packages, LookupStrategy lookupStrategy) {
+        return null;
+      }
+
+      @Override
+      public ClassLoaderLookupPolicy extend(Map<String, LookupStrategy> lookupStrategies, boolean overwrite) {
+        return null;
+      }
+
+      @Override
+      public ClassLoaderLookupPolicy extend(Stream<String> packages, LookupStrategy lookupStrategy, boolean overwrite) {
+        return null;
+      }
+    };
+  }
+
+  @Parameterized.Parameters(name = "Testing Driver {0}")
+  public static String[] data() throws NoSuchFieldException, IllegalAccessException {
+    return new String[] {
+        "5.16.4"
+    };
+  }
+
+  @Before
+  public void setup() throws Exception {
+
+    URL settingsUrl = getClass().getClassLoader().getResource("custom-settings.xml");
+    final MavenClientProvider mavenClientProvider = discoverProvider(this.getClass().getClassLoader());
+
+    final Supplier<File> localMavenRepository =
+        mavenClientProvider.getLocalRepositorySuppliers().environmentMavenRepositorySupplier();
+
+    final MavenConfiguration.MavenConfigurationBuilder mavenConfigurationBuilder =
+        newMavenConfigurationBuilder().globalSettingsLocation(toFile(settingsUrl));
+
+    MavenClient mavenClient = mavenClientProvider
+        .createMavenClient(mavenConfigurationBuilder.localMavenRepositoryLocation(localMavenRepository.get()).build());
+
+    BundleDescriptor bundleDescriptor = new BundleDescriptor.Builder().setGroupId(DRIVER_GROUP_ID)
+        .setArtifactId(DRIVER_ARTIFACT_ID).setVersion(driverVersion).build();
+
+    BundleDependency dependency = mavenClient.resolveBundleDescriptor(bundleDescriptor);
+    artifactClassLoader =
+        new MuleArtifactClassLoader("test", mock(ArtifactDescriptor.class),
+                                    new URL[] {dependency.getBundleUri().toURL()}, currentThread().getContextClassLoader(),
+                                    testLookupPolicy);
+
+    CompositeClassLoader classLoader = from(artifactClassLoader);
+    currentThread().setContextClassLoader(classLoader);
+  }
+
+  @Test
+  public void checkIfActiveMQResourceReleaserInterruptAbstractInactivityMonitorThread() throws ClassNotFoundException,
+      NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
+    Class<?> activeMqFactoryClass = Class.forName(DRIVER_CLASS_NAME, true, artifactClassLoader);
+    Object activeMpFactoryObject = activeMqFactoryClass.getDeclaredConstructor(String.class).newInstance(ACTIVEMQ_URL_CONFIG);
+
+    Object conObj = activeMqFactoryClass.getMethod("createConnection").invoke(activeMpFactoryObject);
+    Class<?> connection = conObj.getClass();
+
+    Method startActiveMQConn = connection.getMethod("start");
+    ActiveMQResourceReleaserThreadUtil runnable = new ActiveMQResourceReleaserThreadUtil(startActiveMQConn, conObj);
+    Thread activeMQConnectionThread = new Thread(runnable, "ActiveMQConnectionThread");
+    activeMQConnectionThread.start();
+
+    await().atMost(1, TimeUnit.SECONDS).until(listOfThreadsContainInactivityMonitorThread());
+    artifactClassLoader.dispose();
+    assertFalse(getNameListOfActiveThreads().contains(ACTIVEMQ_DRIVER_TIMER_THREAD_NAME));
+  }
+
+  private Callable<Boolean> listOfThreadsContainInactivityMonitorThread() {
+    return new Callable<Boolean>() {
+
+      public Boolean call() {
+        return getNameListOfActiveThreads().contains(ACTIVEMQ_DRIVER_TIMER_THREAD_NAME);
+      }
+    };
+  }
+
+  private List<String> getNameListOfActiveThreads() {
+    Thread[] threads = new Thread[activeCount()];
+    enumerate(threads);
+    List<String> activeThreadNames = new ArrayList<>();
+    for (Thread thread : threads) {
+      Optional<String> actualName = Optional.ofNullable(thread.getName());
+      actualName.ifPresent(activeThreadNames::add);
+    }
+    return activeThreadNames;
+  }
+}
