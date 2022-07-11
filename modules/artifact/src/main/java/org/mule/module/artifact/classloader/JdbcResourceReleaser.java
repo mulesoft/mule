ec2da6d5ed8c5432bb6e26cfa59fb27ecc216297
GuidoGianfrate <61558996+GuidoGianfrate@users.noreply.github.com>
2022-07-11 16:53:12 -0300

    W-10583584: Leak in JMS

--- modules/artifact/src/main/java/org/mule/module/artifact/classloader/JdbcResourceReleaser.java
+++ modules/artifact/src/main/java/org/mule/module/artifact/classloader/JdbcResourceReleaser.java
@@ -6,19 +6,18 @@
  */
 package org.mule.module.artifact.classloader;
 
-import static org.mule.module.artifact.classloader.ThreadCommonMethodsUtil.clearReferencesStopTimerThread;
-import static org.mule.module.artifact.classloader.ThreadCommonMethodsUtil.isThreadLoadedByDisposedApplication;
-import static org.mule.module.artifact.classloader.ThreadCommonMethodsUtil.isThreadLoadedByDisposedDomain;
 import static java.lang.Integer.toHexString;
 import static java.lang.String.format;
 import static java.lang.Thread.activeCount;
 import static java.lang.Thread.enumerate;
 import static java.lang.Boolean.getBoolean;
+
 import static java.lang.management.ManagementFactory.getPlatformMBeanServer;
 import static java.sql.DriverManager.deregisterDriver;
 import static java.sql.DriverManager.getDrivers;
 
 import org.mule.runtime.module.artifact.api.classloader.ArtifactClassLoader;
+import org.mule.runtime.module.artifact.api.classloader.MuleArtifactClassLoader;
 import org.mule.runtime.module.artifact.api.classloader.ResourceReleaser;
 
 import java.util.regex.Matcher;
@@ -59,6 +58,7 @@ public class JdbcResourceReleaser implements ResourceReleaser {
 
   public static final String DIAGNOSABILITY_BEAN_NAME = "diagnosability";
   public static final String ORACLE_DRIVER_TIMER_THREAD_CLASS_NAME = "TimerThread";
+  public static final String COMPOSITE_CLASS_LOADER_CLASS_NAME = "CompositeClassLoader";
   public static final Pattern ORACLE_DRIVER_TIMER_THREAD_PATTERN = Pattern.compile("^Timer-\\d+");
   private static final Logger logger = LoggerFactory.getLogger(JdbcResourceReleaser.class);
   private static final List<String> CONNECTION_CLEANUP_THREAD_KNOWN_CLASS_ADDRESES =
@@ -332,5 +332,80 @@ public class JdbcResourceReleaser implements ResourceReleaser {
         && oracleTimerThreadNameMatcher.matches()
         && (isThreadLoadedByDisposedApplication(artifactId, thread.getContextClassLoader())
             || isThreadLoadedByDisposedDomain(artifactId, thread.getContextClassLoader()));
+  }
+
+  private boolean isThreadLoadedByDisposedDomain(String undeployedArtifactId, ClassLoader threadContextClassLoader) {
+    try {
+      Class threadContextClassLoaderClass = threadContextClassLoader.getClass();
+      if (!threadContextClassLoaderClass.getSimpleName().equals(COMPOSITE_CLASS_LOADER_CLASS_NAME)) {
+        return false;
+      }
+
+      Method getDelegateClassLoadersMethod = threadContextClassLoaderClass.getMethod("getDelegates");
+      List<ClassLoader> classLoaderList = (List<ClassLoader>) getDelegateClassLoadersMethod.invoke(threadContextClassLoader);
+
+      for (ClassLoader classLoaderDelegate : classLoaderList) {
+        if (classLoaderDelegate instanceof ArtifactClassLoader) {
+          ArtifactClassLoader artifactClassLoader = (ArtifactClassLoader) classLoaderDelegate;
+          if (artifactClassLoader.getArtifactId().contains(undeployedArtifactId)) {
+            return true;
+          }
+        }
+      }
+
+    } catch (Exception e) {
+      logger.warn("Exception occurred while attempting to compare {} and {} artifactId.", threadContextClassLoader,
+                  this.getClass().getClassLoader());
+    }
+
+    return false;
+  }
+
+  private boolean isThreadLoadedByDisposedApplication(String undeployedArtifactId, ClassLoader threadContextClassLoader) {
+    try {
+      if (!(threadContextClassLoader instanceof MuleArtifactClassLoader)) {
+        return false;
+      }
+      String threadClassLoaderArtifactId = ((MuleArtifactClassLoader) threadContextClassLoader).getArtifactId();
+      return threadClassLoaderArtifactId != null && threadClassLoaderArtifactId.equals(undeployedArtifactId);
+    } catch (Exception e) {
+      logger.warn("Exception occurred while attempting to compare {} and {} artifact id.", threadContextClassLoader,
+                  this.getClass().getClassLoader());
+    }
+
+    return false;
+  }
+
+  private void clearReferencesStopTimerThread(Thread thread)
+      throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+    // Need to get references to:
+    // in Sun/Oracle JDK:
+    // - newTasksMayBeScheduled field (in java.util.TimerThread)
+    // - queue field
+    // - queue.clear()
+    // in IBM JDK, Apache Harmony:
+    // - cancel() method (in java.util.Timer$TimerImpl)
+    try {
+      Field newTasksMayBeScheduledField =
+          thread.getClass().getDeclaredField("newTasksMayBeScheduled");
+      newTasksMayBeScheduledField.setAccessible(true);
+      Field queueField = thread.getClass().getDeclaredField("queue");
+      queueField.setAccessible(true);
+      Object queue = queueField.get(thread);
+      Method clearMethod = queue.getClass().getDeclaredMethod("clear");
+      clearMethod.setAccessible(true);
+      synchronized (queue) {
+        newTasksMayBeScheduledField.setBoolean(thread, false);
+        clearMethod.invoke(queue);
+        // In case queue was already empty. Should only be one
+        // thread waiting but use notifyAll() to be safe.
+        queue.notifyAll();
+      }
+    } catch (NoSuchFieldException noSuchFieldEx) {
+      logger.warn("Unable to clear timer references using 'clear' method. Attempting to use 'cancel' method.");
+      Method cancelMethod = thread.getClass().getDeclaredMethod("cancel");
+      cancelMethod.setAccessible(true);
+      cancelMethod.invoke(thread);
+    }
   }
 }
