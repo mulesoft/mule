2ca8bb523aec17c693f4e380d5c4e12aca6d52e3
Ezequiel Werner <eze210@gmail.com>
2022-07-12 15:10:22 -0300

    test

---
+++ modules/extensions-mule-support/src/test/java/org/mule/runtime/module/extension/mule/internal/operation/MuleOperationErrorHandlingTestCase.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.module.extension.mule.internal.operation;
+
+import static org.mule.tck.junit4.matcher.ErrorTypeMatcher.errorType;
+import static org.mule.test.allure.AllureConstants.ReuseFeature.REUSE;
+import static org.mule.test.allure.AllureConstants.ReuseFeature.ReuseStory.ERROR_HANDLING;
+import static org.mule.test.allure.AllureConstants.ReuseFeature.ReuseStory.OPERATIONS;
+import static org.junit.rules.ExpectedException.none;
+
+import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Stories;
+import io.qameta.allure.Story;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+@Feature(REUSE)
+@Stories({@Story(OPERATIONS), @Story(ERROR_HANDLING)})
+public class MuleOperationErrorHandlingTestCase extends MuleArtifactFunctionalTestCase {
+
+  @Rule
+  public ExpectedException expected = none();
+
+  @Override
+  protected String getConfigFile() {
+    return "mule-error-handling-operations-config.xml";
+  }
+
+  @Test
+  public void errorWithinAppWithNamespaceThis() throws Exception {
+    flowRunner("raiseErrorWithinThisNamespace").runExpectingException(errorType("THIS", "CUSTOM"));
+  }
+
+  @Test
+  public void errorWithinAppWithNamespaceOther() throws Exception {
+    flowRunner("raiseErrorWithinOtherNamespace").runExpectingException(errorType("OTHER", "CUSTOM"));
+  }
+
+  @Test
+  public void divisionByZero() throws Exception {
+    flowRunner("divisionByZeroFlow").runExpectingException(errorType("MULE", "EXPRESSION"));
+  }
+
+  @Test
+  public void heisenbergCureCancer() throws Exception {
+    flowRunner("heisenbergCureCancerFlow").runExpectingException(errorType("HEISENBERG", "HEALTH"));
+  }
+}
