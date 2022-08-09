2b5aa99f1112904a81b0da429396636050aeaffd
rbourbonnavarro <rodrigobourbon44@gmail.com>
2022-08-09 12:45:24 -0300

    W-11202321: Descriptors generation API: add log config file to deployable descriptors (#11658)

---
+++ modules/artifact-activation/src/test/java/org/mule/runtime/module/artifact/activation/internal/descriptor/DeployableDescriptorsWithCustomLogConfig.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.module.artifact.activation.internal.descriptor;
+
+import static org.mule.runtime.core.api.config.MuleProperties.MULE_HOME_DIRECTORY_PROPERTY;
+import static org.mule.test.allure.AllureConstants.ClassloadingIsolationFeature.ClassloadingIsolationStory.ARTIFACT_DESCRIPTORS_WITH_CUSTOM_LOG_CONFIG;
+
+import static java.util.Arrays.asList;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import org.mule.runtime.api.util.Pair;
+import org.mule.runtime.module.artifact.api.descriptor.DeployableArtifactDescriptor;
+import org.mule.tck.junit4.rule.SystemProperty;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+
+import io.qameta.allure.Story;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@Story(ARTIFACT_DESCRIPTORS_WITH_CUSTOM_LOG_CONFIG)
+@RunWith(Parameterized.class)
+public class DeployableDescriptorsWithCustomLogConfig extends AbstractDeployableArtifactDescriptorFactoryTestCase {
+
+  private final String deployableProjectFolder;
+  private final Boolean isApplication;
+
+  @Rule
+  public final SystemProperty muleHomeSystemProperty;
+
+  @Parameterized.Parameters(name = "Deployable project folder: {0}, Custom log config file path: {1}")
+  public static Collection<Object[]> deployableArtifactDescriptors() {
+    return asList(new Object[][] {
+        {"apps/with-custom-log-config", "some-path"},
+        {"domains/with-custom-log-config", "some-path"},
+        {"apps/with-custom-log-config", null},
+        {"domains/with-custom-log-config", null}});
+  }
+
+  public DeployableDescriptorsWithCustomLogConfig(String deployableProjectFolder, String customLogConfigFilePath) {
+    this.deployableProjectFolder = deployableProjectFolder;
+    isApplication = deployableProjectFolder.startsWith("apps");
+
+    muleHomeSystemProperty =
+        new SystemProperty(MULE_HOME_DIRECTORY_PROPERTY, customLogConfigFilePath);
+  }
+
+  @Test
+  public void createDeployableDescriptorWithCustomLogConfig() throws URISyntaxException {
+    DeployableArtifactDescriptor deployableArtifactDescriptor = getDeployableArtifactDescriptor();
+
+    String base = muleHomeSystemProperty.getValue() != null ? muleHomeSystemProperty.getValue() : ".";
+    Path customLogConfig = Paths.get(base, "custom-log4j2.xml");
+    assertThat(deployableArtifactDescriptor.getLogConfigFile().toPath(), is(customLogConfig));
+  }
+
+  private DeployableArtifactDescriptor getDeployableArtifactDescriptor() throws URISyntaxException {
+    if (isApplication) {
+      return createApplicationDescriptor(deployableProjectFolder);
+    } else {
+      return createDomainDescriptor(deployableProjectFolder);
+    }
+  }
+
+}
--- tests/allure/src/main/java/org/mule/test/allure/AllureConstants.java
+++ tests/allure/src/main/java/org/mule/test/allure/AllureConstants.java
@@ -123,6 +123,7 @@ public interface AllureConstants {
 
       String ARTIFACT_DESCRIPTORS = "Artifact descriptors";
       String PATCHED_ARTIFACT_DESCRIPTORS = "Artifact descriptors with patched plugins";
+      String ARTIFACT_DESCRIPTORS_WITH_CUSTOM_LOG_CONFIG = "Artifact descriptors with a custom logging configuration";
       String CLASSLOADER_MODEL = "ClassLoader model";
       String CLASSLOADER_GENERATION = "ClassLoader generation";
     }
