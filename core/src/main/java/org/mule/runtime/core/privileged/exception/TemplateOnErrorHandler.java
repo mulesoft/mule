f81738f685fba8c4abcb09364113bc286d4eeda1
Mariano Rean <mrean@mulesoft.com>
2022-08-18 18:22:03 -0300

    W-11503016: Reuse reactor chains for global error handlers to use the… (#11650)
    (cherry picked from commit 255a790e2a565d71113c181b30dce2b3aff25aff)

--- core/src/main/java/org/mule/runtime/core/privileged/exception/TemplateOnErrorHandler.java
+++ core/src/main/java/org/mule/runtime/core/privileged/exception/TemplateOnErrorHandler.java
@@ -16,10 +16,14 @@ import static org.mule.runtime.api.util.MuleSystemProperties.REUSE_GLOBAL_ERROR_
 import static org.mule.runtime.core.api.config.MuleDeploymentProperties.MULE_LAZY_INIT_DEPLOYMENT_PROPERTY;
 import static org.mule.runtime.core.api.exception.WildcardErrorTypeMatcher.WILDCARD_TOKEN;
 import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.disposeIfNeeded;
+import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.initialiseIfNeeded;
+import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.startIfNeeded;
+import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.stopIfNeeded;
 import static org.mule.runtime.core.api.rx.Exceptions.unwrap;
 import static org.mule.runtime.core.internal.component.ComponentAnnotations.updateRootContainerName;
 import static org.mule.runtime.core.internal.exception.ErrorHandlerContextManager.addContext;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.buildNewChainWithListOfProcessors;
+import static org.mule.runtime.core.privileged.processor.MessageProcessors.getDefaultProcessingStrategyFactory;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.getProcessingStrategy;
 
 import static java.lang.Boolean.FALSE;
@@ -46,6 +50,7 @@ import org.mule.runtime.api.component.location.ComponentLocation;
 import org.mule.runtime.api.component.location.ConfigurationComponentLocator;
 import org.mule.runtime.api.component.location.Location;
 import org.mule.runtime.api.exception.ErrorTypeRepository;
+import org.mule.runtime.api.exception.MuleException;
 import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.api.lifecycle.Disposable;
 import org.mule.runtime.api.lifecycle.InitialisationException;
@@ -124,6 +129,8 @@ public abstract class TemplateOnErrorHandler extends AbstractExceptionListener
   private String errorHandlerLocation;
   private boolean isLocalErrorHandlerLocation;
   private ComponentLocation location;
+
+  private Optional<ProcessingStrategy> ownedProcessingStrategy;
 
   private Function<Function<Publisher<CoreEvent>, Publisher<CoreEvent>>, FluxSink<CoreEvent>> fluxFactory;
 
@@ -292,12 +299,35 @@ public abstract class TemplateOnErrorHandler extends AbstractExceptionListener
   }
 
   @Override
+  public void start() throws MuleException {
+    if (fromGlobalErrorHandler && reuseGlobalErrorHandler()) {
+      if (ownedProcessingStrategy.isPresent()) {
+        startIfNeeded(ownedProcessingStrategy);
+      }
+    }
+    super.start();
+  }
+
+  @Override
+  public void stop() throws MuleException {
+    if (fromGlobalErrorHandler && reuseGlobalErrorHandler()) {
+      if (ownedProcessingStrategy.isPresent()) {
+        stopIfNeeded(ownedProcessingStrategy);
+      }
+    }
+    super.stop();
+  }
+
+  @Override
   protected void doInitialise() throws InitialisationException {
     super.doInitialise();
     this.location = this.getLocation();
     Optional<ProcessingStrategy> processingStrategy;
     if (fromGlobalErrorHandler && reuseGlobalErrorHandler()) {
-      processingStrategy = getProcessingStrategyFromGlobalErrorHandler(locator);
+      processingStrategy =
+          ofNullable(getDefaultProcessingStrategyFactory(muleContext).create(muleContext, getLocation().getRootContainerName()));
+      initialiseIfNeeded(processingStrategy);
+      ownedProcessingStrategy = processingStrategy;
     } else if (flowLocation.isPresent()) {
       Location location = globalLocation(flowLocation.get());
       processingStrategy = getProcessingStrategy(locator, location);
@@ -337,6 +367,9 @@ public abstract class TemplateOnErrorHandler extends AbstractExceptionListener
 
   @Override
   public void dispose() {
+    if (fromGlobalErrorHandler && reuseGlobalErrorHandler()) {
+      ownedProcessingStrategy.ifPresent(processingStrategy -> disposeIfNeeded(processingStrategy, LOGGER));
+    }
     disposeIfNeeded(fluxFactory, LOGGER);
     super.dispose();
   }
