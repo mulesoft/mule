1414798d39bd5fa48a10611ddb40f3f9ff83b27b
Ezequiel Werner <eze210@gmail.com>
2022-09-01 18:35:31 -0300

    fmt

--- core/src/main/java/org/mule/runtime/core/api/lifecycle/DisposedObjectsTracker.java
+++ core/src/main/java/org/mule/runtime/core/api/lifecycle/DisposedObjectsTracker.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.runtime.core.api.lifecycle;
 
 import static java.lang.System.nanoTime;
@@ -13,64 +19,66 @@ import org.slf4j.LoggerFactory;
 
 public class DisposedObjectsTracker {
 
-    private static final long POSSIBLE_LEAK_TIMEOUT = 15000000000L;
+  private static final long POSSIBLE_LEAK_TIMEOUT = 15000000000L;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(DisposedObjectsTracker.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(DisposedObjectsTracker.class);
 
-    private Set<LeakTrackReference> disposedObjects;
+  private Set<LeakTrackReference> disposedObjects;
 
-    public DisposedObjectsTracker() {
-        disposedObjects = new HashSet<>();
+  public DisposedObjectsTracker() {
+    disposedObjects = new HashSet<>();
 
-        Thread maintainerThread = new Thread(this::maintain);
-        maintainerThread.start();
-        maintainerThread.setDaemon(true);
+    Thread maintainerThread = new Thread(this::maintain);
+    maintainerThread.start();
+    maintainerThread.setDaemon(true);
+  }
+
+  public synchronized void markThatShouldBeCollected(Object object) {
+    if (object == null) {
+      return;
+    }
+    disposedObjects.add(new LeakTrackReference(object));
+  }
+
+  synchronized void maintain() {
+    Set<LeakTrackReference> notYetCollected = new HashSet<>(disposedObjects.size());
+    for (LeakTrackReference ref : disposedObjects) {
+      if (!ref.isCollected()) {
+        notYetCollected.add(ref);
+      }
+      if (ref.isPossibleLeak()) {
+        LOGGER.warn("Possible leak of object {}. It was disposed, but it's still in memory after {} nanoseconds", ref.get(),
+                    POSSIBLE_LEAK_TIMEOUT);
+      }
+    }
+  }
+
+  private static class LeakTrackReference<T> {
+
+    private final WeakReference<T> weakReference;
+    private final PhantomReference<T> phantomReference;
+    private final long disposeTimestamp;
+
+    public LeakTrackReference(T object) {
+      disposeTimestamp = nanoTime();
+      weakReference = new WeakReference<>(object);
+      phantomReference = new PhantomReference<>(object, new ReferenceQueue<>());
     }
 
-    public synchronized void markThatShouldBeCollected(Object object) {
-        if (object == null) {
-            return;
-        }
-        disposedObjects.add(new LeakTrackReference(object));
+    boolean isCollected() {
+      return phantomReference.isEnqueued();
     }
 
-    synchronized void maintain() {
-        Set<LeakTrackReference> notYetCollected = new HashSet<>(disposedObjects.size());
-        for (LeakTrackReference ref : disposedObjects) {
-            if (!ref.isCollected()) {
-                notYetCollected.add(ref);
-            }
-            if (ref.isPossibleLeak()) {
-                LOGGER.warn("Possible leak of object {}. It was disposed, but it's still in memory after {} nanoseconds", ref.get(), POSSIBLE_LEAK_TIMEOUT);
-            }
-        }
+    boolean isPossibleLeak() {
+      return !isCollected() && timeoutElapsed();
     }
 
-    private static class LeakTrackReference<T> {
-        private final WeakReference<T> weakReference;
-        private final PhantomReference<T> phantomReference;
-        private final long disposeTimestamp;
-
-        public LeakTrackReference(T object) {
-            disposeTimestamp = nanoTime();
-            weakReference = new WeakReference<>(object);
-            phantomReference = new PhantomReference<>(object, new ReferenceQueue<>());
-        }
-
-        boolean isCollected() {
-            return phantomReference.isEnqueued();
-        }
-
-        boolean isPossibleLeak() {
-            return !isCollected() && timeoutElapsed();
-        }
-
-        private boolean timeoutElapsed() {
-            return nanoTime() > disposeTimestamp + POSSIBLE_LEAK_TIMEOUT;
-        }
-
-        public Object get() {
-            return weakReference.get();
-        }
+    private boolean timeoutElapsed() {
+      return nanoTime() > disposeTimestamp + POSSIBLE_LEAK_TIMEOUT;
     }
+
+    public Object get() {
+      return weakReference.get();
+    }
+  }
 }
