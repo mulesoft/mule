2c4c6f6778c5c0570ca6ec93cc68f17bd765e85f
Mariano Rean <mrean@mulesoft.com>
2022-09-06 16:41:03 -0300

    add flag only in java 9 or greater

--- tests/unit/src/main/java/org/mule/tck/util/CompilerUtils.java
+++ tests/unit/src/main/java/org/mule/tck/util/CompilerUtils.java
@@ -450,9 +450,12 @@ public class CompilerUtils {
 
       options.addAll(processProperties);
 
-      // This is necessary to avoid compiling issues with java 9 features. It doesn't lower coverage because we are testing what
-      // happens when deploying.
-      options.addAll(Arrays.asList("--release", "8"));
+
+      if (!System.getProperty("java.version").startsWith("1.")) {
+        // This is necessary to avoid compiling issues with java 9 features. It doesn't lower coverage because we are testing what
+        // happens when deploying.
+        options.addAll(Arrays.asList("--release", "8"));
+      }
 
       return options;
     }
