0f0a1963b6cb7b2131fe1cd9583163f3b9184c7b
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2022-09-06 17:58:34 +0200

    fix api problem

---
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/client/operation/EventedOperationsParameterDecorator.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.module.extension.internal.runtime.client.operation;
+
+import org.mule.runtime.api.event.Event;
+import org.mule.runtime.extension.api.client.OperationParameters;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class EventedOperationsParameterDecorator implements OperationParameters {
+
+  private final OperationParameters delegate;
+  private final Event contextEvent;
+
+  public EventedOperationsParameterDecorator(OperationParameters delegate, Event contextEvent) {
+    this.delegate = delegate;
+    this.contextEvent = contextEvent;
+  }
+
+  public Event getContextEvent() {
+    return contextEvent;
+  }
+
+  @Override
+  public Optional<String> getConfigName() {
+    return delegate.getConfigName();
+  }
+
+  @Override
+  public Map<String, Object> get() {
+    return delegate.get();
+  }
+}
