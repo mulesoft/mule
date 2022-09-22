5895351cf4972724570c14585cfb82bb5bb24f3e
Ayelén López Bizón <ayelen.lopez@mulesoft.com>
2022-09-22 12:41:13 -0300

    W-10619792: [MULE-19874] - First Successful + Raise Error hangs runtime (#11785)

--- modules/core-components/src/main/java/org/mule/runtime/core/internal/routing/FirstSuccessfulRouter.java
+++ modules/core-components/src/main/java/org/mule/runtime/core/internal/routing/FirstSuccessfulRouter.java
@@ -189,7 +189,6 @@ class FirstSuccessfulRouter {
             }
           }
           // Scope execution was successful
-          inflightEvents.decrementAndGet();
           Stack<CoreEvent> nextEventContainer = nextExecutionContextResolver.getCurrentContextFromEvent(successfulEvent);
           nextEventContainer.pop();
           downstreamRecorder
