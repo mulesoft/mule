b9de02101acd9b9e697f92e00caab53880095c41
Rodrigo Merino <elrodro83@users.noreply.github.com>
2022-09-23 12:12:48 -0300

    W-11798980: Separate triggerFlows from apikitFlows statistics (#11797)

--- core/src/main/java/org/mule/runtime/core/internal/construct/AbstractPipeline.java
+++ core/src/main/java/org/mule/runtime/core/internal/construct/AbstractPipeline.java
@@ -132,6 +132,7 @@ public abstract class AbstractPipeline extends AbstractFlowConstruct implements 
   private final int maxConcurrency;
   private final DefaultFlowsSummaryStatistics flowsSummaryStatistics;
   private final boolean triggerFlow;
+  private final boolean apikitFlow;
   private final ComponentInitialStateManager componentInitialStateManager;
   private final BackPressureStrategySelector backpressureStrategySelector;
   private final ErrorType FLOW_BACKPRESSURE_ERROR_TYPE;
@@ -158,7 +159,8 @@ public abstract class AbstractPipeline extends AbstractFlowConstruct implements 
     this.processors = unmodifiableList(processors);
     this.maxConcurrency = maxConcurrency != null ? maxConcurrency : DEFAULT_MAX_CONCURRENCY;
     this.flowsSummaryStatistics = flowsSummaryStatistics;
-    this.triggerFlow = source != null || isApiKitFlow(getName());
+    this.triggerFlow = source != null;
+    this.apikitFlow = isApiKitFlow(getName());
 
     this.processingStrategyFactory = processingStrategyFactory.orElseGet(() -> defaultProcessingStrategy());
     if (this.processingStrategyFactory instanceof AsyncProcessingStrategyFactory) {
@@ -271,11 +273,9 @@ public abstract class AbstractPipeline extends AbstractFlowConstruct implements 
 
     doInitialiseProcessingStrategy();
 
-    if (triggerFlow) {
-      flowsSummaryStatistics.incrementDeclaredTriggerFlow();
-    } else {
-      flowsSummaryStatistics.incrementDeclaredPrivateFlow();
-    }
+    updateFlowsSummaryStatistics(DefaultFlowsSummaryStatistics::incrementDeclaredTriggerFlow,
+                                 DefaultFlowsSummaryStatistics::incrementDeclaredApikitFlow,
+                                 DefaultFlowsSummaryStatistics::incrementDeclaredPrivateFlow);
   }
 
   @Override
@@ -555,11 +555,9 @@ public abstract class AbstractPipeline extends AbstractFlowConstruct implements 
       }
     }
 
-    if (triggerFlow) {
-      flowsSummaryStatistics.incrementActiveTriggerFlow();
-    } else {
-      flowsSummaryStatistics.incrementActivePrivateFlow();
-    }
+    updateFlowsSummaryStatistics(DefaultFlowsSummaryStatistics::incrementActiveTriggerFlow,
+                                 DefaultFlowsSummaryStatistics::incrementActiveApikitFlow,
+                                 DefaultFlowsSummaryStatistics::incrementActivePrivateFlow);
   }
 
   private void stopOnFailure(Exception e) throws MuleException {
@@ -602,11 +600,9 @@ public abstract class AbstractPipeline extends AbstractFlowConstruct implements 
 
   @Override
   protected void doStop() throws MuleException {
-    if (triggerFlow) {
-      flowsSummaryStatistics.decrementActiveTriggerFlow();
-    } else {
-      flowsSummaryStatistics.decrementActivePrivateFlow();
-    }
+    updateFlowsSummaryStatistics(DefaultFlowsSummaryStatistics::decrementActiveTriggerFlow,
+                                 DefaultFlowsSummaryStatistics::decrementActiveApikitFlow,
+                                 DefaultFlowsSummaryStatistics::decrementActivePrivateFlow);
 
     if (source != null) {
       stopSafely(() -> {
@@ -637,11 +633,9 @@ public abstract class AbstractPipeline extends AbstractFlowConstruct implements 
 
   @Override
   protected void doDispose() {
-    if (triggerFlow) {
-      flowsSummaryStatistics.decrementDeclaredTriggerFlow();
-    } else {
-      flowsSummaryStatistics.decrementDeclaredPrivateFlow();
-    }
+    updateFlowsSummaryStatistics(DefaultFlowsSummaryStatistics::decrementDeclaredTriggerFlow,
+                                 DefaultFlowsSummaryStatistics::decrementDeclaredApikitFlow,
+                                 DefaultFlowsSummaryStatistics::decrementDeclaredPrivateFlow);
 
     if (errorRouterForSourceResponseError != null) {
       synchronized (this) {
@@ -659,6 +653,18 @@ public abstract class AbstractPipeline extends AbstractFlowConstruct implements 
     disposeIfDisposable(source);
     disposeIfDisposable(processingStrategy);
     super.doDispose();
+  }
+
+  private void updateFlowsSummaryStatistics(Consumer<DefaultFlowsSummaryStatistics> triggerFlowsUpdater,
+                                            Consumer<DefaultFlowsSummaryStatistics> apikitflowsUpdater,
+                                            Consumer<DefaultFlowsSummaryStatistics> privateFlowsUpdater) {
+    if (triggerFlow) {
+      triggerFlowsUpdater.accept(flowsSummaryStatistics);
+    } else if (apikitFlow) {
+      apikitflowsUpdater.accept(flowsSummaryStatistics);
+    } else {
+      privateFlowsUpdater.accept(flowsSummaryStatistics);
+    }
   }
 
   protected Sink getSink() {
