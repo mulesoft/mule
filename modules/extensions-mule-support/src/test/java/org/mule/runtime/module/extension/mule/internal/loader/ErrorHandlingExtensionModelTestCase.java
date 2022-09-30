f0e8a3385bfe0d7dbdc280a95f9c3d2080ed7feb
Ezequiel Werner <eze210@gmail.com>
2022-09-30 19:34:15 -0300

    W-11835623: Fix broken test (#11829)

--- modules/extensions-mule-support/src/test/java/org/mule/runtime/module/extension/mule/internal/loader/ErrorHandlingExtensionModelTestCase.java
+++ modules/extensions-mule-support/src/test/java/org/mule/runtime/module/extension/mule/internal/loader/ErrorHandlingExtensionModelTestCase.java
@@ -89,7 +89,7 @@ public class ErrorHandlingExtensionModelTestCase extends MuleArtifactFunctionalT
     assertThat(raisedErrors,
                containsInAnyOrder("THIS:CONNECTIVITY", "MULE:ANY", "MULE:RETRY_EXHAUSTED", "THIS:RETRY_EXHAUSTED",
                                   "MULE:CONNECTIVITY", "HEISENBERG:HEALTH", "HEISENBERG:OAUTH2", "THIS:CUSTOM", "THIS:HEALTH",
-                                  "THIS:UNIQUE", "MY:MAPPED", "MY:MAPPEDCONNECTIVITY"));
+                                  "THIS:UNIQUE", "MY:MAPPED", "MY:MAPPEDCONNECTIVITY", "THIS:FOURTH"));
   }
 
   @Test
