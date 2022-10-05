330ca48894b639c83c54cbc279b896f516419575
sofiamorseletto <sofimorseletto@gmail.com>
2022-10-05 16:35:07 -0300

    Adding jdoc

--- core/src/main/java/org/mule/runtime/core/internal/profiling/tracing/event/NamedSpanBasedOnComponentIdentifierAndAttemptSpanCustomizationInfo.java
+++ core/src/main/java/org/mule/runtime/core/internal/profiling/tracing/event/NamedSpanBasedOnComponentIdentifierAndAttemptSpanCustomizationInfo.java
@@ -8,7 +8,15 @@ package org.mule.runtime.core.internal.profiling.tracing.event;
 
 import org.mule.runtime.api.component.Component;
 import org.mule.runtime.core.api.event.CoreEvent;
+import org.mule.runtime.core.privileged.profiling.tracing.SpanCustomizationInfo;
 
+/**
+ * A {@link SpanCustomizationInfo} that sets the span name based on the {@link org.mule.runtime.api.component.ComponentIdentifier}
+ * of the {@link Component} that belongs to the current span and adds a suffix which indicates it represents an attempt and, if it
+ * corresponds, the number of the attempt
+ *
+ * @since 4.5.0
+ */
 public class NamedSpanBasedOnComponentIdentifierAndAttemptSpanCustomizationInfo
     extends NamedSpanBasedOnComponentIdentifierAloneSpanCustomizationInfo {
 
