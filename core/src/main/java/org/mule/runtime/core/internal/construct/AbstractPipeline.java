45a74b42adefe12dc695dd6cca1718caf95ea515
Rodrigo Merino <elrodro83@users.noreply.github.com>
2022-10-05 13:19:05 -0300

    W-11835711: Cherry pick W-11798994, W-11798980, W-11493854 into 4.2.2 (#11825)

--- core/src/main/java/org/mule/runtime/core/internal/construct/AbstractPipeline.java
+++ core/src/main/java/org/mule/runtime/core/internal/construct/AbstractPipeline.java
@@ -21,8 +21,10 @@ import static org.mule.runtime.core.api.construct.BackPressureReason.REQUIRED_SC
 import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.initialiseIfNeeded;
 import static org.mule.runtime.core.api.processor.strategy.AsyncProcessingStrategyFactory.DEFAULT_MAX_CONCURRENCY;
 import static org.mule.runtime.core.api.source.MessageSource.BackPressureStrategy.WAIT;
+import static org.mule.runtime.core.internal.management.stats.DefaultFlowsSummaryStatistics.isApiKitFlow;
 import static org.mule.runtime.core.internal.construct.FlowBackPressureException.createFlowBackPressureException;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.processToApply;
+
 import static reactor.core.Exceptions.propagate;
 import static reactor.core.publisher.Flux.from;
 
@@ -54,6 +56,7 @@ import org.mule.runtime.core.api.source.MessageSource;
 import org.mule.runtime.core.api.util.func.CheckedRunnable;
 import org.mule.runtime.core.internal.context.MuleContextWithRegistry;
 import org.mule.runtime.core.internal.exception.MessagingException;
+import org.mule.runtime.core.internal.management.stats.DefaultFlowsSummaryStatistics;
 import org.mule.runtime.core.internal.message.ErrorBuilder;
 import org.mule.runtime.core.internal.processor.strategy.DirectProcessingStrategyFactory;
 import org.mule.runtime.core.privileged.event.BaseEventContext;
@@ -96,6 +99,9 @@ public abstract class AbstractPipeline extends AbstractFlowConstruct implements 
   private Sink sink;
   private Map<BackPressureReason, FlowBackPressureException> backPressureExceptions;
   private final int maxConcurrency;
+  private final DefaultFlowsSummaryStatistics flowsSummaryStatistics;
+  private final boolean triggerFlow;
+  private final boolean apikitFlow;
   private final ComponentInitialStateManager componentInitialStateManager;
   private final BackPressureStrategySelector backpressureStrategySelector;
   private final ErrorType FLOW_BACKPRESSURE_ERROR_TYPE;
@@ -103,7 +109,8 @@ public abstract class AbstractPipeline extends AbstractFlowConstruct implements 
   public AbstractPipeline(String name, MuleContext muleContext, MessageSource source, List<Processor> processors,
                           Optional<FlowExceptionHandler> exceptionListener,
                           Optional<ProcessingStrategyFactory> processingStrategyFactory, String initialState,
-                          Integer maxConcurrency, FlowConstructStatistics flowConstructStatistics,
+                          Integer maxConcurrency,
+                          DefaultFlowsSummaryStatistics flowsSummaryStatistics, FlowConstructStatistics flowConstructStatistics,
                           ComponentInitialStateManager componentInitialStateManager) {
     super(name, muleContext, exceptionListener, initialState, flowConstructStatistics);
 
@@ -117,6 +124,9 @@ public abstract class AbstractPipeline extends AbstractFlowConstruct implements 
     this.componentInitialStateManager = componentInitialStateManager;
     this.processors = unmodifiableList(processors);
     this.maxConcurrency = maxConcurrency != null ? maxConcurrency : DEFAULT_MAX_CONCURRENCY;
+    this.flowsSummaryStatistics = flowsSummaryStatistics;
+    this.triggerFlow = source != null;
+    this.apikitFlow = isApiKitFlow(getName());
 
     this.processingStrategyFactory = processingStrategyFactory.orElseGet(() -> defaultProcessingStrategy());
     if (this.processingStrategyFactory instanceof AsyncProcessingStrategyFactory) {
@@ -232,6 +242,10 @@ public abstract class AbstractPipeline extends AbstractFlowConstruct implements 
 
     initialiseIfNeeded(source, muleContext);
     initialiseIfNeeded(pipeline, muleContext);
+
+    updateFlowsSummaryStatistics(DefaultFlowsSummaryStatistics::incrementDeclaredTriggerFlow,
+                                 DefaultFlowsSummaryStatistics::incrementDeclaredApikitFlow,
+                                 DefaultFlowsSummaryStatistics::incrementDeclaredPrivateFlow);
   }
 
   /*
@@ -402,6 +416,10 @@ public abstract class AbstractPipeline extends AbstractFlowConstruct implements 
         stopOnFailure(e);
       }
     }
+
+    updateFlowsSummaryStatistics(DefaultFlowsSummaryStatistics::incrementActiveTriggerFlow,
+                                 DefaultFlowsSummaryStatistics::incrementActiveApikitFlow,
+                                 DefaultFlowsSummaryStatistics::incrementActivePrivateFlow);
   }
 
   private void stopOnFailure(Exception e) throws MuleException {
@@ -438,6 +456,10 @@ public abstract class AbstractPipeline extends AbstractFlowConstruct implements 
 
   @Override
   protected void doStop() throws MuleException {
+    updateFlowsSummaryStatistics(DefaultFlowsSummaryStatistics::decrementActiveTriggerFlow,
+                                 DefaultFlowsSummaryStatistics::decrementActiveApikitFlow,
+                                 DefaultFlowsSummaryStatistics::decrementActivePrivateFlow);
+
     stopSafely(() -> stopIfStoppable(source));
     canProcessMessage = false;
 
@@ -455,9 +477,25 @@ public abstract class AbstractPipeline extends AbstractFlowConstruct implements 
 
   @Override
   protected void doDispose() {
+    updateFlowsSummaryStatistics(DefaultFlowsSummaryStatistics::decrementDeclaredTriggerFlow,
+                                 DefaultFlowsSummaryStatistics::decrementDeclaredApikitFlow,
+                                 DefaultFlowsSummaryStatistics::decrementDeclaredPrivateFlow);
+
     disposeIfDisposable(pipeline);
     disposeIfDisposable(source);
     super.doDispose();
+  }
+
+  private void updateFlowsSummaryStatistics(Consumer<DefaultFlowsSummaryStatistics> triggerFlowsUpdater,
+                                            Consumer<DefaultFlowsSummaryStatistics> apikitflowsUpdater,
+                                            Consumer<DefaultFlowsSummaryStatistics> privateFlowsUpdater) {
+    if (triggerFlow) {
+      triggerFlowsUpdater.accept(flowsSummaryStatistics);
+    } else if (apikitFlow) {
+      apikitflowsUpdater.accept(flowsSummaryStatistics);
+    } else {
+      privateFlowsUpdater.accept(flowsSummaryStatistics);
+    }
   }
 
   protected Sink getSink() {
