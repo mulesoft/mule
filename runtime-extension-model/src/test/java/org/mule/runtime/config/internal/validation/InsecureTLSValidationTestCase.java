cf08d7d4dba92da10a9cd2e0eeeee41447cb2442
Mariano Rean <mrean@mulesoft.com>
2022-10-06 16:05:45 -0300

    W-10822938: Inconsistent behaviour when using insecure=true attribute of trust-store config (#11832) (#11820)
    (cherry picked from commit 79a72a4133adb5058cc7d996fcdfa070231d51e0)

---
+++ runtime-extension-model/src/test/java/org/mule/runtime/config/internal/validation/InsecureTLSValidationTestCase.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.config.internal.validation;
+
+import static org.mule.runtime.ast.api.validation.Validation.Level.WARN;
+import static org.mule.test.allure.AllureConstants.MuleDsl.MULE_DSL;
+import static org.mule.test.allure.AllureConstants.MuleDsl.DslValidationStory.DSL_VALIDATION_STORY;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.mule.runtime.ast.api.validation.Validation;
+import org.mule.runtime.ast.api.validation.ValidationResultItem;
+
+import java.util.Optional;
+
+import org.junit.Test;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+
+@Feature(MULE_DSL)
+@Story(DSL_VALIDATION_STORY)
+public class InsecureTLSValidationTestCase extends AbstractCoreValidationTestCase {
+
+  @Override
+  protected Validation getValidation() {
+    return new InsecureTLSValidation();
+  }
+
+  @Test
+  public void TLSInsecureValidation() {
+    final Optional<ValidationResultItem> msg = runValidation("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+        "<mule xmlns=\"http://www.mulesoft.org/schema/mule/core\"\n" +
+        "      xmlns:tls=\"http://www.mulesoft.org/schema/mule/tls\"\n" +
+        "      xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+        "      xsi:schemaLocation=\"http://www.mulesoft.org/schema/mule/core http://www.mulesoft.org/schema/mule/core/current/mule.xsd\n"
+        +
+        "                           http://www.mulesoft.org/schema/mule/tls http://www.mulesoft.org/schema/mule/tls/current/mule-tls.xsd\">\n"
+        +
+        "\n" +
+        "    <tls:context name=\"TLS_Context\">\n" +
+        "        <tls:trust-store path=\"${truststore.path}\" password=\"${truststore.password}\" type=\"${truststore.type}\" insecure=\"true\"/>\n"
+        +
+        "        <tls:key-store type=\"${keystore.type}\" path=\"${keystore.path}\" alias=\"${keystore.alias}\" keyPassword=\"${keystore.keyPassword}\" password=\"${keystore.password}\" />\n"
+        +
+        "     </tls:context>\n" +
+        "    \n" +
+        "</mule>");
+
+    assertThat(msg.get().getValidation().getLevel(), is(WARN));
+    assertThat(msg.get().getMessage(),
+               containsString("Setting insecure to true renders connections vulnerable to attack. Use it only for prototyping or testing. Never use it in production environments."));
+  }
+}
