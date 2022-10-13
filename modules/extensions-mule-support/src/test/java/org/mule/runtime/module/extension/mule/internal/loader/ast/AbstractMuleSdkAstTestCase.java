ff7741f7b0cd71a61c2d0fee1827ff5d739dddce
Axel Sanguinetti <asanguinetti@mulesoft.com>
2022-10-13 14:51:45 -0300

    Feedback: using an ordered implementation of Set in the tests to avoid any possible source of flakyness.

--- modules/extensions-mule-support/src/test/java/org/mule/runtime/module/extension/mule/internal/loader/ast/AbstractMuleSdkAstTestCase.java
+++ modules/extensions-mule-support/src/test/java/org/mule/runtime/module/extension/mule/internal/loader/ast/AbstractMuleSdkAstTestCase.java
@@ -28,7 +28,7 @@ import org.mule.tck.junit4.AbstractMuleTestCase;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -51,7 +51,7 @@ public abstract class AbstractMuleSdkAstTestCase extends AbstractMuleTestCase {
 
   @BeforeClass
   public static void beforeClass() throws Exception {
-    runtimeExtensionModels = new HashSet<>();
+    runtimeExtensionModels = new LinkedHashSet<>();
     Collection<RuntimeExtensionModelProvider> runtimeExtensionModelProviders = new SpiServiceRegistry()
         .lookupProviders(RuntimeExtensionModelProvider.class, currentThread().getContextClassLoader());
     for (RuntimeExtensionModelProvider runtimeExtensionModelProvider : runtimeExtensionModelProviders) {
