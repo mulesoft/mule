10084b48d5a716f83782994cd005a5c396f81c3a
Ezequiel Werner <eze210@gmail.com>
2022-10-25 18:27:05 -0300

    fmt

--- core/src/main/java/org/mule/runtime/core/internal/lock/MuleLockFactory.java
+++ core/src/main/java/org/mule/runtime/core/internal/lock/MuleLockFactory.java
@@ -30,8 +30,8 @@ public class MuleLockFactory implements LockFactory, Initialisable, Disposable {
   private SchedulerService schedulerService;
 
   // TODO (W-11958289): This MuleContext is injected just to obtain the MuleConfiguration, so the MuleConfiguration
-  //  should be injected instead. However, when injecting that class here it makes the test
-  //  LazyInitConfigurationComponentLocatorTestCase fail because the MuleLockFactory is instantiated twice.
+  // should be injected instead. However, when injecting that class here it makes the test
+  // LazyInitConfigurationComponentLocatorTestCase fail because the MuleLockFactory is instantiated twice.
   @Inject
   private MuleContext muleContext;
 
