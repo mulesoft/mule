5cfae3c5961b9f7634c27797981b8ad240e8ac75
Ezequiel Werner <eze210@gmail.com>
2022-10-27 16:11:09 -0300

    W-11929632: Fixing race conditions in InstanceLockGroup (#11895) (#11907)

--- core-tests/src/test/java/org/mule/runtime/core/internal/lock/InstanceLockGroupTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/lock/InstanceLockGroupTestCase.java
@@ -6,21 +6,33 @@
  */
 package org.mule.runtime.core.internal.lock;
 
-
+import static java.lang.Thread.State.TIMED_WAITING;
+import static java.lang.Thread.currentThread;
 import static java.util.Collections.unmodifiableMap;
+import static java.util.concurrent.Executors.newSingleThreadExecutor;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.verify;
+
 import org.mule.runtime.api.store.ObjectStoreException;
 import org.mule.runtime.api.store.TemplateObjectStore;
 import org.mule.runtime.api.util.concurrent.Latch;
 import org.mule.tck.junit4.AbstractMuleTestCase;
+import org.mule.tck.probe.JUnitLambdaProbe;
+import org.mule.tck.probe.PollingProber;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
+import io.qameta.allure.Issue;
 import org.junit.Test;
 import org.mockito.Answers;
 import org.mockito.Mockito;
@@ -30,10 +42,12 @@ public class InstanceLockGroupTestCase extends AbstractMuleTestCase {
 
   public static final int THREAD_COUNT = 100;
   public static final int ITERATIONS_PER_THREAD = 100;
+  private static final ExecutorService executor = newSingleThreadExecutor();
   private Latch threadStartLatch = new Latch();
   private String sharedKeyA = "A";
   private String sharedKeyB = "B";
-  private InstanceLockGroup instanceLockGroup = new InstanceLockGroup(new SingleServerLockProvider());
+  private TestLockProviderWrapper lockProvider = new TestLockProviderWrapper(new SingleServerLockProvider());
+  private InstanceLockGroup instanceLockGroup = new InstanceLockGroup(lockProvider);
   private InMemoryObjectStore objectStore = new InMemoryObjectStore();
   private LockProvider mockLockProvider;
 
@@ -57,6 +71,84 @@ public class InstanceLockGroupTestCase extends AbstractMuleTestCase {
     lockUnlockThenDestroy(5);
   }
 
+  @Test
+  @Issue("W-11929632")
+  public void disposeDoesNotLoseReferenceToTakenLocks() {
+    String testLockId = "TestLockId";
+    instanceLockGroup.lock(testLockId);
+    verify(lockProvider.getSpiedLock(testLockId)).lock();
+
+    // Spawn a task that disposes the lock group and get a reference to the thread that is doing such dispose.
+    AtomicReference<Thread> threadExecutingTheDispose = disposeAsynchronouslyAndGetThreadReference();
+
+    // Wait for the disposer thread to be in state TIMED_WAITING.
+    waitForThreadToBeInStateTimedWaiting(threadExecutingTheDispose);
+
+    instanceLockGroup.unlock(testLockId);
+    verify(lockProvider.getSpiedLock(testLockId)).unlock();
+  }
+
+  private static void waitForThreadToBeInStateTimedWaiting(AtomicReference<Thread> threadExecutingTheDispose) {
+    new PollingProber().check(new JUnitLambdaProbe(() -> {
+      assertThat(threadExecutingTheDispose.get(), is(notNullValue()));
+      assertThat(threadExecutingTheDispose.get().getState(), is(TIMED_WAITING));
+      return true;
+    }));
+  }
+
+  private AtomicReference<Thread> disposeAsynchronouslyAndGetThreadReference() {
+    AtomicReference<Thread> threadExecutingTheDispose = new AtomicReference<>();
+    executor.submit(() -> {
+      threadExecutingTheDispose.set(currentThread());
+      instanceLockGroup.dispose();
+    });
+    return threadExecutingTheDispose;
+  }
+
+  @Test
+  @Issue("W-11929632")
+  public void whenTryLockIsInterruptedTheLockGroupDoesNotGenerateALockEntry() {
+    lockProvider.makeLocksRaiseExceptions();
+
+    try {
+      String testLockId = "TestLockId";
+      instanceLockGroup.tryLock(testLockId, 5L, SECONDS);
+      fail("tryLock should have thrown an InterruptedException");
+    } catch (InterruptedException e) {
+      assertThat(instanceLockGroup.size(), is(0));
+    }
+  }
+
+  @Test
+  @Issue("W-11929632")
+  public void whenLockInterruptiblyIsInterruptedTheLockGroupDoesNotGenerateALockEntry() {
+    lockProvider.makeLocksRaiseExceptions();
+
+    try {
+      String testLockId = "TestLockId";
+      instanceLockGroup.lockInterruptibly(testLockId);
+      fail("tryLock should have thrown an InterruptedException");
+    } catch (InterruptedException e) {
+      assertThat(instanceLockGroup.size(), is(0));
+    }
+  }
+
+  @Test
+  @Issue("W-11929632")
+  public void whenUnlockRaisesIllegalMonitorStateExceptionTheLockGroupDoesNotReleaseTheEntry() {
+    String testLockId = "TestLockId";
+    instanceLockGroup.lock(testLockId);
+
+    lockProvider.makeLocksRaiseExceptions();
+
+    try {
+      instanceLockGroup.unlock(testLockId);
+      fail("unlock should have thrown a IllegalMonitorStateException");
+    } catch (IllegalMonitorStateException e) {
+      assertThat(instanceLockGroup.size(), is(1));
+    }
+  }
+
   private void lockUnlockThenDestroy(int lockTimes) {
     mockLockProvider = Mockito.mock(LockProvider.class, Answers.RETURNS_DEEP_STUBS.get());
     InstanceLockGroup instanceLockGroup = new InstanceLockGroup(mockLockProvider);
@@ -66,8 +158,6 @@ public class InstanceLockGroupTestCase extends AbstractMuleTestCase {
     instanceLockGroup.unlock("lockId");
     Mockito.verify(mockLockProvider, VerificationModeFactory.times(1)).createLock("lockId");
   }
-
-
 
   private void testHighConcurrency(boolean useTryLock) throws InterruptedException, ObjectStoreException {
     List<Thread> threads = new ArrayList<Thread>(THREAD_COUNT * 2);
@@ -185,5 +275,4 @@ public class InstanceLockGroupTestCase extends AbstractMuleTestCase {
       return unmodifiableMap(store);
     }
   }
-
 }
