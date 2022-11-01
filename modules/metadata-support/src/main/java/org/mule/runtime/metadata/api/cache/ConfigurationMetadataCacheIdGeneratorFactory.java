f0fc924ecae7f0168fc9656a21cff14fc1c900a2
Martín Buchwald <martin.buchwald@mulesoft.com>
2022-11-01 17:09:57 -0300

    javadocs

--- modules/metadata-support/src/main/java/org/mule/runtime/metadata/api/cache/ConfigurationMetadataCacheIdGeneratorFactory.java
+++ modules/metadata-support/src/main/java/org/mule/runtime/metadata/api/cache/ConfigurationMetadataCacheIdGeneratorFactory.java
@@ -8,8 +8,16 @@ package org.mule.runtime.metadata.api.cache;
 
 import org.mule.runtime.metadata.internal.cache.AstConfigurationMetadataCacheIdGenerator;
 
+/**
+ * A Factory of {@link ConfigurationMetadataCacheIdGenerator}
+ *
+ * @since 4.5
+ */
 public class ConfigurationMetadataCacheIdGeneratorFactory {
 
+  /**
+   * @return a new instance of an implementation of {@link ConfigurationMetadataCacheIdGenerator}
+   */
   public ConfigurationMetadataCacheIdGenerator create() {
     return new AstConfigurationMetadataCacheIdGenerator();
   }
