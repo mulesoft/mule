1cb55436886d718f1d85a11af56d06393697f867
Martín Buchwald <martin.buchwald@mulesoft.com>
2022-11-02 11:14:54 -0300

    self review

--- modules/metadata-support/src/main/java/org/mule/runtime/metadata/internal/cache/AstConfigurationMetadataCacheIdGenerator.java
+++ modules/metadata-support/src/main/java/org/mule/runtime/metadata/internal/cache/AstConfigurationMetadataCacheIdGenerator.java
@@ -15,13 +15,12 @@ import static org.mule.runtime.metadata.internal.cache.ComponentBasedIdHelper.so
 import static org.mule.runtime.metadata.internal.cache.ComponentBasedIdHelper.getModelNameAst;
 import static java.util.stream.Collectors.toList;
 import static java.util.Optional.empty;
-import static java.util.Optional.of;
 import static java.util.Optional.ofNullable;
 
 import org.mule.runtime.api.meta.model.ComponentModel;
 import org.mule.runtime.api.meta.model.parameter.ParameterizedModel;
+import org.mule.runtime.api.util.LazyValue;
 import org.mule.runtime.ast.api.ComponentAst;
-import org.mule.runtime.ast.api.ComponentParameterAst;
 import org.mule.runtime.metadata.api.cache.ConfigurationMetadataCacheIdGenerator;
 import org.mule.runtime.metadata.api.cache.MetadataCacheId;
 
@@ -34,30 +33,29 @@ import java.util.stream.Stream;
 
 public class AstConfigurationMetadataCacheIdGenerator implements ConfigurationMetadataCacheIdGenerator {
 
-  private final Map<String, MetadataCacheId> configIds = new HashMap<>();
-  private final Map<String, MetadataCacheId> configAsGlobalsIds = new HashMap<>();
+  private final Map<String, LazyValue<MetadataCacheId>> configIds = new HashMap<>();
+  private final Map<String, LazyValue<MetadataCacheId>> configAsGlobalsIds = new HashMap<>();
 
   @Override
   public Optional<MetadataCacheId> getConfigMetadataCacheId(String configName, boolean asGlobalElement) {
     if (isBlank(configName)) {
       return empty();
     }
-    if (asGlobalElement) {
-      return ofNullable(configAsGlobalsIds.getOrDefault(configName, null));
-    } else {
-      return ofNullable(configIds.getOrDefault(configName, null));
-    }
+
+    Map<String, LazyValue<MetadataCacheId>> ids = asGlobalElement ? configAsGlobalsIds : configIds;
+
+    MetadataCacheId result = ids.containsKey(configName) ? ids.get(configName).get() : null;
+    return ofNullable(result);
   }
 
   @Override
   public void addConfiguration(ComponentAst configAst) {
-    resolveGlobalElement(configAst)
-        .ifPresent(metadataCacheId -> configAsGlobalsIds.put(configAst.getLocation().getRootContainerName(), metadataCacheId));
-    doResolve(configAst)
-        .ifPresent(metadataCacheId -> configIds.put(configAst.getLocation().getRootContainerName(), metadataCacheId));
+    String configLocation = configAst.getLocation().getRootContainerName();
+    configAsGlobalsIds.put(configLocation, new LazyValue<>(() -> resolveGlobalElement(configAst)));
+    configIds.put(configLocation, new LazyValue<>(() -> doResolve(configAst)));
   }
 
-  private Optional<MetadataCacheId> doResolve(ComponentAst elementModel) {
+  private MetadataCacheId doResolve(ComponentAst elementModel) {
     List<MetadataCacheId> keyParts = new ArrayList<>();
 
     keyParts.add(resolveDslTagId(elementModel));
@@ -65,33 +63,34 @@ public class AstConfigurationMetadataCacheIdGenerator implements ConfigurationMe
     elementModel.getModel(ComponentModel.class)
         .map(model -> resolveMetadataKeyParts(elementModel, model, true,
                                               configName -> getConfigMetadataCacheId(configName, false)))
-        .orElseGet(() -> resolveGlobalElement(elementModel))
+        .orElseGet(() -> ofNullable(resolveGlobalElement(elementModel)))
         .ifPresent(keyParts::add);
 
-    return of(new MetadataCacheId(keyParts, sourceElementName(elementModel)));
+    return new MetadataCacheId(keyParts, sourceElementName(elementModel));
   }
 
-  private Optional<MetadataCacheId> resolveGlobalElement(ComponentAst elementModel) {
+  private MetadataCacheId resolveGlobalElement(ComponentAst elementModel) {
     List<String> parameterNamesRequiredForMetadata = parameterNamesRequiredForMetadataCacheId(elementModel);
 
-    List<MetadataCacheId> parts = Stream.concat(elementModel.directChildrenStream()
-        .map(this::doResolve)
-        .filter(Optional::isPresent)
-        .map(Optional::get), elementModel.getModel(ParameterizedModel.class)
-            .map(pmz -> elementModel.getParameters()
-                .stream()
-                .filter(p -> p.getValue().getValue().isPresent())
-                .filter(p -> parameterNamesRequiredForMetadata
-                    .contains((p.getModel()).getName()))
-                .map(p -> resolveKeyFromSimpleValue(elementModel, p, configName -> getConfigMetadataCacheId(configName, false))))
-            .orElse(Stream.empty()))
+    List<MetadataCacheId> parts = Stream.concat(
+                                                elementModel.directChildrenStream().map(this::doResolve),
+                                                elementModel.getModel(ParameterizedModel.class).map(pmz -> elementModel
+                                                    .getParameters()
+                                                    .stream()
+                                                    .filter(p -> p.getValue().getValue().isPresent())
+                                                    .filter(p -> parameterNamesRequiredForMetadata
+                                                        .contains((p.getModel()).getName()))
+                                                    .map(p -> resolveKeyFromSimpleValue(elementModel, p,
+                                                                                        configName -> getConfigMetadataCacheId(configName,
+                                                                                                                               false))))
+                                                    .orElse(Stream.empty()))
         .collect(toList());
 
     if (parts.isEmpty()) {
-      return empty();
+      return null;
     }
 
-    return of(new MetadataCacheId(parts, getModelNameAst(elementModel).orElse(null)));
+    return new MetadataCacheId(parts, getModelNameAst(elementModel).orElse(null));
   }
 
 }
