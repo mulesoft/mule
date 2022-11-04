7419c288f8e8d5d52678fc9c5493fbf646d41796
jcgrethe <jgrethe@itba.edu.ar>
2022-11-04 16:30:25 -0300

    fix tests

--- modules/artifact-activation/src/main/java/org/mule/runtime/module/artifact/activation/internal/maven/AbstractMavenDeployableProjectModelBuilder.java
+++ modules/artifact-activation/src/main/java/org/mule/runtime/module/artifact/activation/internal/maven/AbstractMavenDeployableProjectModelBuilder.java
@@ -38,6 +38,7 @@ import org.mule.maven.client.api.SettingsSupplierFactory;
 import org.mule.maven.client.api.model.BundleDependency;
 import org.mule.maven.client.api.model.MavenConfiguration;
 import org.mule.maven.client.internal.AetherMavenClient;
+import org.mule.runtime.container.api.MuleFoldersUtil;
 import org.mule.runtime.module.artifact.activation.api.deployable.DeployableProjectModel;
 import org.mule.runtime.module.artifact.activation.internal.deployable.AbstractDeployableProjectModelBuilder;
 import org.mule.runtime.module.artifact.activation.internal.deployable.DeployablePluginsDependenciesResolver;
@@ -90,11 +91,14 @@ public abstract class AbstractMavenDeployableProjectModelBuilder extends Abstrac
   protected static MavenConfiguration getDefaultMavenConfiguration() {
     final MavenClientProvider mavenClientProvider =
         discoverProvider(MavenDeployableProjectModelBuilder.class.getClassLoader());
-    final Supplier<File> localMavenRepository =
-        mavenClientProvider.getLocalRepositorySuppliers().environmentMavenRepositorySupplier();
+
+
+    File localRepository = new File(MuleFoldersUtil.getMuleBaseFolder(), "repository");
+
+    final Supplier<File> localMavenRepository = localRepository.exists() ? () -> localRepository
+        : mavenClientProvider.getLocalRepositorySuppliers().environmentMavenRepositorySupplier();
 
     final SettingsSupplierFactory settingsSupplierFactory = mavenClientProvider.getSettingsSupplierFactory();
-
     final Optional<File> globalSettings = settingsSupplierFactory.environmentGlobalSettingsSupplier();
     final Optional<File> userSettings = settingsSupplierFactory.environmentUserSettingsSupplier();
     final Optional<File> settingsSecurity = settingsSupplierFactory.environmentSettingsSecuritySupplier();
