d9415545a9c82cce8c0426f61ffb8ce27830b804
Martín Buchwald <martin.buchwald@mulesoft.com>
2022-11-11 13:40:34 -0300

    Fix + review

--- modules/properties-config/src/main/java/org/mule/runtime/config/internal/model/properties/PropertiesResolverUtils.java
+++ modules/properties-config/src/main/java/org/mule/runtime/config/internal/model/properties/PropertiesResolverUtils.java
@@ -102,6 +102,14 @@ public class PropertiesResolverUtils {
 
   }
 
+  /**
+   * Creates the {@link PropertiesResolverConfigurationProperties} with the entire correct hierarchy for an isolated
+   * environment. This includes neither System Properties nor Environment Properties.
+   * @param artifactAst the Artifact AST of the application to calculate the properties resolvers.
+   * @param parentConfigurationProperties The Configuration Properties from a parent configuration, such as a Domain.
+   * @param deploymentProperties a {@link Map} of deployment properties
+   * @param externalResourceProvider a {@link ResourceProvider} to use to retrieve files to be used for properties' resolution
+   */
   public static PropertiesResolverConfigurationProperties createIsolatedConfigurationAttributeResolver(ArtifactAst artifactAst,
                                                                                                        Optional<ConfigurationProperties> parentConfigurationProperties,
                                                                                                        Map<String, String> deploymentProperties,
