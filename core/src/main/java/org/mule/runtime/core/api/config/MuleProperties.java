73d3dce1dace4b78dee62bd987953e9b62ec8056
ndinu <ndinu@users.noreply.github.com>
2022-11-25 15:23:37 -0300

    W-12074281: Change ApplicationTypeLoader (#11978)

--- core/src/main/java/org/mule/runtime/core/api/config/MuleProperties.java
+++ core/src/main/java/org/mule/runtime/core/api/config/MuleProperties.java
@@ -243,6 +243,7 @@ public class MuleProperties {
   public static final String COMPATIBILITY_PLUGIN_INSTALLED = "_compatibilityPluginInstalled";
   public static final String MULE_PROFILING_SERVICE_KEY = "_muleProfilingService";
   public static final String SERVER_NOTIFICATION_MANAGER = "_serverNotificationManager";
+  public static final String OBJECT_ARTIFACT_TYPE_LOADER = "_artifactTypeLoader";
 
   // Not currently used as these need to be instance variables of the MuleContext.
   public static final String OBJECT_NOTIFICATION_MANAGER = "_muleNotificationManager";
