fd15ceabbdbe6de74f1479f37e1961e28b98cc22
Axel Sanguinetti <asanguinetti@mulesoft.com>
2022-11-29 10:48:23 -0300

    Revert "W-10822938: Inconsistent behaviour when using insecure=true attribute of trust-store config (#11836)"
    This reverts commit ceb8ffaeb9208212eca2e8e4016734a51c0e57e0.

--- modules/tls/src/test/java/org/mule/test/module/tls/DefaultTlsContextFactoryTestCase.java
+++ modules/tls/src/test/java/org/mule/test/module/tls/DefaultTlsContextFactoryTestCase.java
@@ -30,7 +30,7 @@ import org.mule.runtime.api.tls.TlsContextFactory;
 import org.mule.runtime.core.api.util.ClassUtils;
 import org.mule.runtime.core.api.util.StringUtils;
 import org.mule.runtime.module.tls.internal.DefaultTlsContextFactory;
-import org.mule.tck.junit4.AbstractMuleContextTestCase;
+import org.mule.tck.junit4.AbstractMuleTestCase;
 
 import java.io.File;
 import java.io.IOException;
@@ -47,7 +47,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-public class DefaultTlsContextFactoryTestCase extends AbstractMuleContextTestCase {
+public class DefaultTlsContextFactoryTestCase extends AbstractMuleTestCase {
 
   @Rule
   public ExpectedException expectedException = ExpectedException.none();
@@ -111,9 +111,8 @@ public class DefaultTlsContextFactoryTestCase extends AbstractMuleContextTestCas
   }
 
   @Test
-  public void insecureTrustStoreShouldNotBeConfigured() throws IOException, InitialisationException {
+  public void insecureTrustStoreShouldNotBeConfigured() throws IOException {
     DefaultTlsContextFactory tlsContextFactory = new DefaultTlsContextFactory(emptyMap());
-    initialiseIfNeeded(tlsContextFactory, muleContext);
     tlsContextFactory.setTrustStorePath("trustStore");
     tlsContextFactory.setTrustStoreInsecure(true);
     assertFalse(tlsContextFactory.isTrustStoreConfigured());
