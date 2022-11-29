afb789d8216177a13e9f505817ee672b38ba4c69
Juan Grethe <jgrethe@salesforce.com>
2022-11-29 19:32:06 -0300

    W-11086488: migrate mule-extensions-maven-plugin to the new Descripto… (#11997)
    * W-11086488: migrate mule-extensions-maven-plugin to the new Descriptor API

--- modules/artifact-activation/src/main/java/org/mule/runtime/module/artifact/activation/internal/maven/AbstractMavenDeployableProjectModelBuilder.java
+++ modules/artifact-activation/src/main/java/org/mule/runtime/module/artifact/activation/internal/maven/AbstractMavenDeployableProjectModelBuilder.java
@@ -135,7 +135,7 @@ public abstract class AbstractMavenDeployableProjectModelBuilder extends Abstrac
     AetherMavenClient aetherMavenClient = new AetherMavenClient(mavenConfiguration);
     List<String> activeProfiles = mavenConfiguration.getActiveProfiles().orElse(emptyList());
 
-    resolveDeployableDependencies(aetherMavenClient, pom, pomModel, activeProfiles, deployableArtifactCoordinates);
+    resolveDeployableDependencies(aetherMavenClient, pom, pomModel, activeProfiles);
 
     resolveDeployablePluginsData(deployableMavenBundleDependencies);
 
@@ -201,19 +201,18 @@ public abstract class AbstractMavenDeployableProjectModelBuilder extends Abstrac
   /**
    * Resolves the dependencies of the deployable in the various forms needed to obtain the {@link DeployableProjectModel}.
    *
-   * @param aetherMavenClient             the configured {@link AetherMavenClient}.
-   * @param pom                           POM file.
-   * @param pomModel                      parsed POM model.
-   * @param activeProfiles                active Maven profiles.
-   * @param deployableArtifactCoordinates artifact coordinates of the deployable.
+   * @param aetherMavenClient the configured {@link AetherMavenClient}.
+   * @param pom               POM file.
+   * @param pomModel          parsed POM model.
+   * @param activeProfiles    active Maven profiles.
    */
   private void resolveDeployableDependencies(AetherMavenClient aetherMavenClient, File pom, Model pomModel,
-                                             List<String> activeProfiles, ArtifactCoordinates deployableArtifactCoordinates) {
+                                             List<String> activeProfiles) {
     DeployableDependencyResolver deployableDependencyResolver = new DeployableDependencyResolver(aetherMavenClient);
 
     // Resolve the Maven bundle dependencies
     deployableMavenBundleDependencies =
-        deployableDependencyResolver.resolveDeployableDependencies(pom, isIncludeTestDependencies(), empty());
+        deployableDependencyResolver.resolveDeployableDependencies(pom, isIncludeTestDependencies(), getMavenReactorResolver());
 
     // MTF/MUnit declares the mule-plugin being tested as system scope, therefore its transitive dependencies
     // will not be included in the dependency graph of the deployable artifact and need to be resolved separately
@@ -240,6 +239,17 @@ public abstract class AbstractMavenDeployableProjectModelBuilder extends Abstrac
                     && bd.getDescriptor().getArtifactId().equals(artifact.getArtifactCoordinates().getArtifactId())))
             .map(org.mule.runtime.module.artifact.api.descriptor.BundleDependency::getDescriptor)
             .collect(toSet());
+  }
+
+  /**
+   * Get the {@link MavenReactorResolver} configured. If it is configured the {@link DeployableDependencyResolver} will look up
+   * the dependencies also in this repository. If {@link Optional#empty()} it will look up in the repositories configured in the
+   * system.
+   *
+   * @return an {@link Optional} {@link MavenReactorResolver}.
+   */
+  protected Optional<MavenReactorResolver> getMavenReactorResolver() {
+    return empty();
   }
 
   private List<BundleDependency> resolveSystemScopeDependencies(AetherMavenClient aetherMavenClient,
@@ -327,8 +337,7 @@ public abstract class AbstractMavenDeployableProjectModelBuilder extends Abstrac
 
     AdditionalPluginDependenciesResolver additionalPluginDependenciesResolver =
         new AdditionalPluginDependenciesResolver(aetherMavenClient,
-                                                 initialAdditionalPluginDependencies,
-                                                 new File("temp"));
+                                                 initialAdditionalPluginDependencies);
 
     additionalPluginDependencies = toPluginDependencies(additionalPluginDependenciesResolver
         .resolveDependencies(deployableMavenBundleDependencies, pluginsDependencies));
