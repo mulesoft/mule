f831040b2b332c89426e04707d7f1741610215d7
Mariano Rean <mrean@mulesoft.com>
2022-11-30 11:56:53 -0300

    formatter

--- core/src/main/java/org/mule/runtime/core/internal/context/DefaultMuleContext.java
+++ core/src/main/java/org/mule/runtime/core/internal/context/DefaultMuleContext.java
@@ -1327,7 +1327,7 @@ public class DefaultMuleContext implements MuleContextWithRegistry, PrivilegedMu
     FeatureFlaggingRegistry featureFlaggingRegistry = FeatureFlaggingRegistry.getInstance();
     featureFlaggingRegistry.registerFeatureFlag(RETHROW_EXCEPTIONS_IN_IDEMPOTENT_MESSAGE_VALIDATOR,
                                                 featureContext -> featureContext.getArtifactMinMuleVersion()
-                                                  .filter(muleVersion -> muleVersion.atLeast("4.5.0")).isPresent());
+                                                    .filter(muleVersion -> muleVersion.atLeast("4.5.0")).isPresent());
   }
 
 }
