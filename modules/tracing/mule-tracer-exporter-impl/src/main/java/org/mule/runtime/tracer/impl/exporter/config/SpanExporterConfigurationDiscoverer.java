ccb606da9b791193d6c363f9ee554141f099e03b
Fabian Gonzalez <fabian.gonzalez@mulesoft.com>
2022-12-05 11:31:19 -0300

    Review

--- modules/tracing/mule-tracer-exporter-impl/src/main/java/org/mule/runtime/tracer/impl/exporter/config/SpanExporterConfigurationDiscoverer.java
+++ modules/tracing/mule-tracer-exporter-impl/src/main/java/org/mule/runtime/tracer/impl/exporter/config/SpanExporterConfigurationDiscoverer.java
@@ -7,8 +7,12 @@
 
 package org.mule.runtime.tracer.impl.exporter.config;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 import org.mule.runtime.core.api.registry.SpiServiceRegistry;
 import org.mule.runtime.tracer.exporter.api.config.SpanExporterConfiguration;
+
+import org.slf4j.Logger;
 
 /**
  * Discoverer for {@link SpanExporterConfiguration}'s through spi.
@@ -16,6 +20,8 @@ import org.mule.runtime.tracer.exporter.api.config.SpanExporterConfiguration;
  * @since 4.5.0
  */
 public class SpanExporterConfigurationDiscoverer {
+
+  private static final Logger LOGGER = getLogger(SpanExporterConfigurationDiscoverer.class);
 
   private SpanExporterConfigurationDiscoverer() {}
 
@@ -29,12 +35,14 @@ public class SpanExporterConfigurationDiscoverer {
                           SpanExporterConfiguration.class.getClassLoader());
 
       if (discoveredSpanExporterConfiguration == null) {
+        LOGGER.warn("No configuration for optel export was found. Using system properties");
         return SYSTEM_PROPERTIES_SPAN_EXPORTER_CONFIGURATION;
       }
     } catch (IllegalStateException e) {
-      return SYSTEM_PROPERTIES_SPAN_EXPORTER_CONFIGURATION;
+      LOGGER.warn("An exception was raised while trying to find the exporter configuration", e);
     }
 
+    LOGGER.warn("No configuration for optel export was found. Using system properties");
     return SYSTEM_PROPERTIES_SPAN_EXPORTER_CONFIGURATION;
   }
 }
