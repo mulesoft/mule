82af0285d36f607705c21e9b072b3582dfe120e3
Ivan Fritzler <ifritzler@mulesoft.com>
2022-12-07 14:02:16 -0300

    Self review

--- modules/tracing/mule-tracer-exporter-impl/src/main/java/org/mule/runtime/tracer/impl/exporter/config/SpanExporterConfigUtils.java
+++ modules/tracing/mule-tracer-exporter-impl/src/main/java/org/mule/runtime/tracer/impl/exporter/config/SpanExporterConfigUtils.java
@@ -37,6 +37,11 @@ public class SpanExporterConfigUtils {
 
   private static class RetryPolicyBuilder {
 
+    private static final String DEFAULT_BACKOFF_MULTIPLIER = "1.5";
+    private static final String DEFAULT_INITIAL_BACKOFF = "1";
+    private static final String DEFAULT_MAXIMUM_BACKOFF = "5";
+    private static final String DEFAULT_MAX_ATTEMPTS = "5";
+
     private final SpanExporterConfiguration spanExporterConfiguration;
 
     public RetryPolicyBuilder(SpanExporterConfiguration spanExporterConfiguration) {
@@ -46,12 +51,12 @@ public class SpanExporterConfigUtils {
     public RetryPolicy build() {
       return RetryPolicy.builder()
           .setBackoffMultiplier(parseDouble(spanExporterConfiguration.getValue(MULE_OPEN_TELEMETRY_EXPORTER_BACKOFF_MULTIPLIER,
-                                                                               "1.5")))
+                  DEFAULT_BACKOFF_MULTIPLIER)))
           .setInitialBackoff(ofSeconds(parseLong(spanExporterConfiguration.getValue(MULE_OPEN_TELEMETRY_EXPORTER_INITIAL_BACKOFF,
-                                                                                    "1"))))
-          .setMaxBackoff(ofSeconds(parseLong(spanExporterConfiguration.getValue(MULE_OPEN_TELEMETRY_EXPORTER_MAX_BACKOFF, "5"))))
+                  DEFAULT_INITIAL_BACKOFF))))
+          .setMaxBackoff(ofSeconds(parseLong(spanExporterConfiguration.getValue(MULE_OPEN_TELEMETRY_EXPORTER_MAX_BACKOFF, DEFAULT_MAXIMUM_BACKOFF))))
           .setMaxAttempts(Integer
-              .parseInt(spanExporterConfiguration.getValue(MULE_OPEN_TELEMETRY_EXPORTER_BACKOFF_MAX_ATTEMPTS, "5")))
+              .parseInt(spanExporterConfiguration.getValue(MULE_OPEN_TELEMETRY_EXPORTER_BACKOFF_MAX_ATTEMPTS, DEFAULT_MAX_ATTEMPTS)))
           .build();
     }
   }
