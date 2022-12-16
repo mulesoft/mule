8ba9689d1edacbd81005bd8b96d4346a1c1e48f1
Rodrigo Merino <elrodro83@users.noreply.github.com>
2022-12-16 14:31:40 -0300

    W-12226023: Allow to waive expression required parameters on design time when the value is a property (#12064)

---
+++ runtime-extension-model/src/main/java/org/mule/runtime/config/internal/validation/ExpressionsInRequiredExpressionsParamsNonPropertyValue.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.config.internal.validation;
+
+import static org.mule.runtime.api.component.ComponentIdentifier.builder;
+import static org.mule.runtime.api.meta.ExpressionSupport.REQUIRED;
+import static org.mule.runtime.ast.api.util.ComponentAstPredicatesFactory.currentElemement;
+import static org.mule.runtime.ast.api.util.MuleAstUtils.hasPropertyPlaceholder;
+import static org.mule.runtime.ast.api.validation.Validation.Level.WARN;
+import static org.mule.runtime.ast.api.validation.ValidationResultItem.create;
+import static org.mule.runtime.extension.api.util.ExtensionModelUtils.getGroupAndParametersPairs;
+import static org.mule.runtime.internal.dsl.DslConstants.CORE_PREFIX;
+
+import static java.lang.String.format;
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
+
+import org.mule.runtime.api.component.ComponentIdentifier;
+import org.mule.runtime.api.meta.model.parameter.ParameterizedModel;
+import org.mule.runtime.ast.api.ArtifactAst;
+import org.mule.runtime.ast.api.ComponentAst;
+import org.mule.runtime.ast.api.validation.Validation;
+import org.mule.runtime.ast.api.validation.ValidationResultItem;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Expressions are fixed for parameters that require expressions.
+ * 
+ * @since 4.5
+ */
+public class ExpressionsInRequiredExpressionsParamsNonPropertyValue implements Validation {
+
+  private static final String CONFIGURATION_NAME = "configuration";
+
+  protected static final ComponentIdentifier CONFIGURATION_IDENTIFIER =
+      builder().namespace(CORE_PREFIX).name(CONFIGURATION_NAME).build();
+
+  private final boolean enabled;
+
+  public ExpressionsInRequiredExpressionsParamsNonPropertyValue(boolean enabled) {
+    this.enabled = enabled;
+  }
+
+  @Override
+  public String getName() {
+    return "Expression in expressionsRequired params are fixed";
+  }
+
+  @Override
+  public String getDescription() {
+    return "Expressions are fixed for parameters that require expressions.";
+  }
+
+  @Override
+  public Level getLevel() {
+    return WARN;
+  }
+
+  @Override
+  public Predicate<List<ComponentAst>> applicable() {
+    if (enabled) {
+      return currentElemement(component -> component.getModel(ParameterizedModel.class)
+          .map(pmz -> pmz.getAllParameterModels())
+          .orElse(emptyList())
+          .stream()
+          .filter(pm -> REQUIRED.equals(pm.getExpressionSupport()))
+          .findAny()
+          .isPresent());
+    } else {
+      return c -> false;
+    }
+  }
+
+  @Override
+  public List<ValidationResultItem> validateMany(ComponentAst component, ArtifactAst artifact) {
+    return component.getModel(ParameterizedModel.class)
+        .map(pmz -> getGroupAndParametersPairs(pmz)
+            .filter(gnp -> REQUIRED.equals(gnp.getSecond().getExpressionSupport()))
+            .map(gnp -> component.getParameter(gnp.getFirst().getName(), gnp.getSecond().getName()))
+            .collect(toList()))
+        .orElse(emptyList())
+        .stream()
+        .filter(param -> hasPropertyPlaceholder(param.getRawValue()))
+        .map(param -> create(component, param, this,
+                             format("Parameter '%s' has value '%s' which is resolved with a property and may cause the artifact to have different behavior on different environments.",
+                                    param.getModel().getName(), param.getRawValue())))
+        .collect(toList());
+  }
+}
