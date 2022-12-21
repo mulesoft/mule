f91ad94a5b4e0e4e028b55aa16afc83f4d96e5b7
Mariano Rean <mrean@mulesoft.com>
2022-12-21 15:56:39 -0300

    fix build

--- modules/tls/src/main/java/org/mule/runtime/module/tls/internal/DefaultTlsContextFactory.java
+++ modules/tls/src/main/java/org/mule/runtime/module/tls/internal/DefaultTlsContextFactory.java
@@ -12,7 +12,7 @@ import static org.mule.runtime.api.config.MuleRuntimeFeature.HONOUR_INSECURE_TLS
 import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
 
 import org.mule.runtime.api.component.AbstractComponent;
-import org.mule.runtime.api.config.FeatureFlaggingService;
+import org.mule.runtime.core.api.config.FeatureFlaggingService;
 import org.mule.runtime.api.lifecycle.CreateException;
 import org.mule.runtime.api.lifecycle.Initialisable;
 import org.mule.runtime.api.lifecycle.InitialisationException;
