3669d9b9128dd9647ad96d98a60ae749e7e1b8ab
Mariano Rean <mrean@mulesoft.com>
2022-12-22 13:20:36 -0300

    W-10822938: Reverting the revert and injecting the DefaultTlsContextFactory when created. (#11949) (#12080)
    (cherry picked from commit e3a17ddd926ebd605f91243025bc23f8e8b33877)

--- runtime-extension-model/src/main/java/org/mule/runtime/config/internal/validation/CoreValidationsProvider.java
+++ runtime-extension-model/src/main/java/org/mule/runtime/config/internal/validation/CoreValidationsProvider.java
@@ -39,7 +39,8 @@ public class CoreValidationsProvider implements ValidationsProvider {
                   new ExpressionsInRequiredExpressionsParams(),
                   new NoExpressionsInNoExpressionsSupportedParams(),
                   new PollingSourceHasSchedulingStrategy(),
-                  new ScatterGatherRoutes()
+                  new ScatterGatherRoutes(),
+                  new InsecureTLSValidation()
     // Commented out because this causes failures because of a lying extension model for munit, in the 'ignore' parameter
     // new NoExpressionsInNoExpressionsSupportedParams()
     // validate expressions!
