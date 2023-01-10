ab1b7f13be26ee0c5ec302993feb28a09ec4f8df
Fabian Gonzalez <fabian.gonzalez@mulesoft.com>
2023-01-10 00:19:04 -0300

    W-12350399: Fix flaky test CommonBehaviourTransientRegistryTestCase (#12137)

--- core-tests/src/test/java/org/mule/runtime/core/internal/registry/CommonBehaviourTransientRegistryTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/registry/CommonBehaviourTransientRegistryTestCase.java
@@ -7,6 +7,7 @@
 package org.mule.runtime.core.internal.registry;
 
 import org.mule.runtime.core.api.MuleContext;
+import org.mule.runtime.core.internal.context.DefaultMuleContext;
 import org.mule.runtime.core.internal.context.MuleContextWithRegistry;
 import org.mule.runtime.core.internal.lifecycle.MuleLifecycleInterceptor;
 import org.mule.runtime.core.privileged.PrivilegedMuleContext;
@@ -21,7 +22,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 public class CommonBehaviourTransientRegistryTestCase extends AbstractRegistryTestCase {
 
   @Mock(answer = Answers.RETURNS_DEEP_STUBS, extraInterfaces = {MuleContextWithRegistry.class, PrivilegedMuleContext.class})
-  private MuleContext muleContext;
+  private DefaultMuleContext muleContext;
 
   @Override
   public Registry getRegistry() {
