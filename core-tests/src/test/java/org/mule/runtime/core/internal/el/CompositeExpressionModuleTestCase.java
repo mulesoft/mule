f220d94830eb96490464bc147c9ab2934abe9730
Ezequiel Werner <eze210@gmail.com>
2023-01-20 14:07:01 -0300

    fmt

--- core-tests/src/test/java/org/mule/runtime/core/internal/el/CompositeExpressionModuleTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/el/CompositeExpressionModuleTestCase.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.runtime.core.internal.el;
 
 import static org.hamcrest.Matchers.contains;
@@ -24,47 +30,47 @@ import org.junit.rules.ExpectedException;
 @Issue("W-12412432")
 public class CompositeExpressionModuleTestCase extends AbstractMuleTestCase {
 
-    @Rule
-    public ExpectedException expectedException = none();
+  @Rule
+  public ExpectedException expectedException = none();
 
-    @Test
-    public void namespaceOfTheCompositeIsTheNamespaceOfBothSingle() {
-        ModuleNamespace namespace = new ModuleNamespace("Namespace");
-        ExpressionModule m1 = new DefaultExpressionModuleBuilder(namespace).build();
-        ExpressionModule m2 = new DefaultExpressionModuleBuilder(namespace).build();
-        ExpressionModule m3 = new DefaultExpressionModuleBuilder(namespace).build();
-        ExpressionModule expressionModule = new CompositeExpressionModule(m1, m2, m3);
-        assertThat(expressionModule.namespace(), is(namespace));
-    }
+  @Test
+  public void namespaceOfTheCompositeIsTheNamespaceOfBothSingle() {
+    ModuleNamespace namespace = new ModuleNamespace("Namespace");
+    ExpressionModule m1 = new DefaultExpressionModuleBuilder(namespace).build();
+    ExpressionModule m2 = new DefaultExpressionModuleBuilder(namespace).build();
+    ExpressionModule m3 = new DefaultExpressionModuleBuilder(namespace).build();
+    ExpressionModule expressionModule = new CompositeExpressionModule(m1, m2, m3);
+    assertThat(expressionModule.namespace(), is(namespace));
+  }
 
-    @Test
-    public void canNotBuildCompositeIfNamespaceDiffers() {
-        ModuleNamespace namespace1 = new ModuleNamespace("Namespace1");
-        ModuleNamespace namespace2 = new ModuleNamespace("Namespace2");
-        ExpressionModule m1 = new DefaultExpressionModuleBuilder(namespace1).build();
-        ExpressionModule m2 = new DefaultExpressionModuleBuilder(namespace2).build();
+  @Test
+  public void canNotBuildCompositeIfNamespaceDiffers() {
+    ModuleNamespace namespace1 = new ModuleNamespace("Namespace1");
+    ModuleNamespace namespace2 = new ModuleNamespace("Namespace2");
+    ExpressionModule m1 = new DefaultExpressionModuleBuilder(namespace1).build();
+    ExpressionModule m2 = new DefaultExpressionModuleBuilder(namespace2).build();
 
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Expected unique namespace but found multiple [Namespace1, Namespace2]");
-        new CompositeExpressionModule(m1, m2);
-    }
+    expectedException.expect(IllegalArgumentException.class);
+    expectedException.expectMessage("Expected unique namespace but found multiple [Namespace1, Namespace2]");
+    new CompositeExpressionModule(m1, m2);
+  }
 
-    @Test
-    public void modulesWithDifferentBindingAreMerged() {
-        MetadataType someType = BaseTypeBuilder.create(MetadataFormat.JAVA).stringType().build();
-        TypedValue<FunctionExecutor> someFunction = new TypedValue<>(mock(FunctionExecutor.class), mock(DataType.class));
-        ModuleNamespace namespace = new ModuleNamespace("Namespace");
+  @Test
+  public void modulesWithDifferentBindingAreMerged() {
+    MetadataType someType = BaseTypeBuilder.create(MetadataFormat.JAVA).stringType().build();
+    TypedValue<FunctionExecutor> someFunction = new TypedValue<>(mock(FunctionExecutor.class), mock(DataType.class));
+    ModuleNamespace namespace = new ModuleNamespace("Namespace");
 
-        ExpressionModule moduleWithType = new DefaultExpressionModuleBuilder(namespace)
-                .addType(someType)
-                .build();
+    ExpressionModule moduleWithType = new DefaultExpressionModuleBuilder(namespace)
+        .addType(someType)
+        .build();
 
-        ExpressionModule moduleWithFunction = new DefaultExpressionModuleBuilder(namespace)
-                .addBinding("someFunction", someFunction)
-                .build();
+    ExpressionModule moduleWithFunction = new DefaultExpressionModuleBuilder(namespace)
+        .addBinding("someFunction", someFunction)
+        .build();
 
-        ExpressionModule merged = new CompositeExpressionModule(moduleWithFunction, moduleWithType);
-        assertThat(merged.declaredTypes(), contains(someType));
-        assertThat(merged.lookup("someFunction").get(), is(someFunction));
-    }
+    ExpressionModule merged = new CompositeExpressionModule(moduleWithFunction, moduleWithType);
+    assertThat(merged.declaredTypes(), contains(someType));
+    assertThat(merged.lookup("someFunction").get(), is(someFunction));
+  }
 }
