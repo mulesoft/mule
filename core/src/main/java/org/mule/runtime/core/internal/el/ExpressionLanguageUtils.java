a929ddad9abd479a70b85ef5e4086c566b69a14e
Ezequiel Werner <eze210@gmail.com>
2023-02-22 10:41:22 -0300

    test

--- core/src/main/java/org/mule/runtime/core/internal/el/ExpressionLanguageUtils.java
+++ core/src/main/java/org/mule/runtime/core/internal/el/ExpressionLanguageUtils.java
@@ -6,6 +6,7 @@
  */
 package org.mule.runtime.core.internal.el;
 
+import static java.lang.Boolean.getBoolean;
 import static java.lang.String.format;
 import static org.mule.runtime.api.el.BindingContextUtils.NULL_BINDING_CONTEXT;
 import static org.mule.runtime.api.el.BindingContextUtils.PAYLOAD;
@@ -25,6 +26,10 @@ import org.mule.runtime.api.el.CompiledExpression;
 import org.mule.runtime.api.el.ExpressionExecutionException;
 import org.mule.runtime.api.el.ExpressionLanguage;
 import org.mule.runtime.api.event.Event;
+import org.mule.runtime.api.util.Pair;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Utilities for using {@link ExpressionLanguage} instances
@@ -32,6 +37,10 @@ import org.mule.runtime.api.event.Event;
  * @since 4.3.0
  */
 public final class ExpressionLanguageUtils {
+
+  private static final boolean USE_PRECOMPILED_EXPRESSIONS = getBoolean("mule.experimental.usePrecompiledExpressions");
+  private static final Map<Pair<String, ExpressionLanguage>, CompiledExpression> alreadyCompiledExpressions =
+      new ConcurrentHashMap<>();
 
   private static final BindingContext COMPILATION_BINDING_CONTEXT =
       // just add the flow binding so that scripts referencing it are compiled fine
@@ -55,6 +64,15 @@ public final class ExpressionLanguageUtils {
    * @return a {@link CompiledExpression}
    */
   public static CompiledExpression compile(String expression, ExpressionLanguage expressionLanguage) {
+    if (USE_PRECOMPILED_EXPRESSIONS) {
+      return alreadyCompiledExpressions.computeIfAbsent(new Pair<>(expression, expressionLanguage),
+                                                        pair -> doCompile(pair.getFirst(), pair.getSecond()));
+    } else {
+      return doCompile(expression, expressionLanguage);
+    }
+  }
+
+  private static CompiledExpression doCompile(String expression, ExpressionLanguage expressionLanguage) {
     return expressionLanguage.compile(expression, COMPILATION_BINDING_CONTEXT);
   }
 
