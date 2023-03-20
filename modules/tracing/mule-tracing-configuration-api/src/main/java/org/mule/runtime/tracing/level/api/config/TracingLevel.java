b2e8ac0856f3a91d551815e2060779b44ec417ac
sofiamorseletto <sofimorseletto@gmail.com>
2023-03-20 12:38:04 -0300

    Fixing doc

--- modules/tracing/mule-tracing-configuration-api/src/main/java/org/mule/runtime/tracing/level/api/config/TracingLevel.java
+++ modules/tracing/mule-tracing-configuration-api/src/main/java/org/mule/runtime/tracing/level/api/config/TracingLevel.java
@@ -8,15 +8,21 @@
 package org.mule.runtime.tracing.level.api.config;
 
 /**
- * Set of hierarchical levels that group and define different types of generation of traces:
- * 
- * Overview: Generates only traces for the Flows and the Inbound and Outbound endpoints of the app. Monitoring: Same behavior as
- * Overview, plus, traces for each 'box' or step of an application. Debug: Same behavior as Monitoring, plus, additional
- * information inside the connectors and components
+ * Set of hierarchical levels that group and define different types of generation of traces.
  *
  * @since 4.6.0
  */
 public enum TracingLevel {
-  OVERVIEW, MONITORING, DEBUG
-
+  /**
+   * Overview: Generates only traces for the Flows and the Inbound and Outbound endpoints of the app.
+   */
+  OVERVIEW,
+  /**
+   * Monitoring: Same behavior as Overview, plus, traces for each 'box' or step of an application.
+   */
+  MONITORING,
+  /**
+   * Debug: Same behavior as Monitoring, plus, additional information inside the connectors and components.
+   */
+  DEBUG
 }
