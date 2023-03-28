b4de6dae6db9ea61ef8217067fb034a9b4389b0f
Fabian Gonzalez <fabian.gonzalez@mulesoft.com>
2023-03-28 10:32:11 -0300

    Review

--- modules/tracing/mule-tracer-customization-impl/src/main/java/org/mule/runtime/tracer/configuration/internal/info/ExecutionInitialSpanInfo.java
+++ modules/tracing/mule-tracer-customization-impl/src/main/java/org/mule/runtime/tracer/configuration/internal/info/ExecutionInitialSpanInfo.java
@@ -39,17 +39,21 @@ public class ExecutionInitialSpanInfo implements InitialSpanInfo {
   private final boolean isPolicySpan;
   private final boolean rootSpan;
   private final String location;
+  private final String apiId;
+  private int initialAttributesCount = INITIAL_ATTRIBUTES_BASE_COUNT;
 
-  public ExecutionInitialSpanInfo(Component component, InitialExportInfoProvider initialExportInfoProvider) {
-    this(component, initialExportInfoProvider, null, "");
+  public static final String API_ID_ATTRIBUTE_KEY = "api.id";
+
+  public ExecutionInitialSpanInfo(Component component, String apiId, InitialExportInfoProvider initialExportInfoProvider) {
+    this(component, apiId, initialExportInfoProvider, null, "");
   }
 
-  public ExecutionInitialSpanInfo(Component component, String overriddenName,
+  public ExecutionInitialSpanInfo(Component component, String apiId, String overriddenName,
                                   InitialExportInfoProvider initialExportInfoProvider) {
-    this(component, initialExportInfoProvider, overriddenName, "");
+    this(component, apiId, initialExportInfoProvider, overriddenName, "");
   }
 
-  public ExecutionInitialSpanInfo(Component component, InitialExportInfoProvider initialExportInfoProvider,
+  public ExecutionInitialSpanInfo(Component component, String apiId, InitialExportInfoProvider initialExportInfoProvider,
                                   String overriddenName, String spanNameSuffix) {
     initialExportInfo = initialExportInfoProvider.getInitialExportInfo(component);
     if (overriddenName == null) {
@@ -60,28 +64,41 @@ public class ExecutionInitialSpanInfo implements InitialSpanInfo {
     this.isPolicySpan = isComponentOfName(component, EXECUTE_NEXT) || component instanceof PolicyChain;
     this.rootSpan = isComponentOfName(component, FLOW);
     this.location = getLocationAsString(component.getLocation());
+    this.apiId = apiId;
+    if (apiId != null) {
+      this.initialAttributesCount = INITIAL_ATTRIBUTES_BASE_COUNT + 1;
+    }
   }
 
-  public ExecutionInitialSpanInfo(String name, InitialExportInfoProvider initialExportInfoProvider) {
-    this(name, initialExportInfoProvider, "");
+  public ExecutionInitialSpanInfo(String name, String apiId, InitialExportInfoProvider initialExportInfoProvider) {
+    this(name, apiId, initialExportInfoProvider, "");
   }
 
-  public ExecutionInitialSpanInfo(String name, InitialExportInfoProvider initialExportInfoProvider, String spanNameSuffix) {
+  public ExecutionInitialSpanInfo(String name, String apiId, InitialExportInfoProvider initialExportInfoProvider,
+                                  String spanNameSuffix) {
     initialExportInfo = initialExportInfoProvider.getInitialExportInfo(name + stripToEmpty(spanNameSuffix));
     this.name = name;
     this.isPolicySpan = false;
     this.rootSpan = false;
     this.location = NO_LOCATION;
+    this.apiId = apiId;
+    if (apiId != null) {
+      this.initialAttributesCount = INITIAL_ATTRIBUTES_BASE_COUNT + 1;
+    }
+
   }
 
   @Override
   public void forEachAttribute(BiConsumer<String, String> biConsumer) {
     biConsumer.accept(LOCATION_KEY, location);
+    if (apiId != null) {
+      biConsumer.accept(API_ID_ATTRIBUTE_KEY, apiId);
+    }
   }
 
   @Override
   public int getInitialAttributesCount() {
-    return INITIAL_ATTRIBUTES_BASE_COUNT;
+    return initialAttributesCount;
   }
 
   @Override
--- modules/tracing/mule-tracer-customization-impl/src/main/java/org/mule/runtime/tracer/configuration/internal/provider/DefaultInitialSpanInfoProvider.java
+++ modules/tracing/mule-tracer-customization-impl/src/main/java/org/mule/runtime/tracer/configuration/internal/provider/DefaultInitialSpanInfoProvider.java
@@ -7,39 +7,71 @@
 package org.mule.runtime.tracer.configuration.internal.provider;
 
 import org.mule.runtime.api.component.Component;
+import org.mule.runtime.api.component.ConfigurationProperties;
+import org.mule.runtime.api.lifecycle.Initialisable;
+import org.mule.runtime.api.lifecycle.InitialisationException;
+import org.mule.runtime.core.api.MuleContext;
+import org.mule.runtime.core.api.config.bootstrap.ArtifactType;
 import org.mule.runtime.tracer.api.span.info.InitialSpanInfo;
 import org.mule.runtime.tracer.configuration.api.InitialSpanInfoProvider;
 import org.mule.runtime.tracer.configuration.internal.export.MonitoringInitialExportInfoProvider;
 import org.mule.runtime.tracer.configuration.internal.export.InitialExportInfoProvider;
 import org.mule.runtime.tracer.configuration.internal.info.ExecutionInitialSpanInfo;
 
+import javax.inject.Inject;
+
+import static org.mule.runtime.core.api.config.bootstrap.ArtifactType.POLICY;
+
 /**
  * Default implementation of {@link InitialSpanInfoProvider}
  *
  * @since 4.6.0
  */
-public class DefaultInitialSpanInfoProvider implements InitialSpanInfoProvider {
+public class DefaultInitialSpanInfoProvider implements InitialSpanInfoProvider, Initialisable {
+
+  public static final String API_ID_CONFIGURATION_PROPERTIES_KEY = "apiId";
+
+  @Inject
+  MuleContext muleContext;
+
+  @Inject
+  ConfigurationProperties configurationProperties;
 
   // TODO: User Story B - Implementation of Monitoring, Troubleshooting, App Level (W-12658074)
   private final InitialExportInfoProvider initialExportInfo = new MonitoringInitialExportInfoProvider();
+  private String artifactTypeStringValue;
+  private String artifactId;
+  private String apiId;
 
   @Override
   public InitialSpanInfo getInitialSpanInfo(Component component) {
-    return new ExecutionInitialSpanInfo(component, initialExportInfo);
+    return new ExecutionInitialSpanInfo(component, apiId, initialExportInfo);
   }
 
   @Override
   public InitialSpanInfo getInitialSpanInfo(Component component, String suffix) {
-    return new ExecutionInitialSpanInfo(component, initialExportInfo, null, suffix);
+    return new ExecutionInitialSpanInfo(component, apiId, initialExportInfo, null, suffix);
   }
 
   @Override
   public InitialSpanInfo getInitialSpanInfo(String name) {
-    return new ExecutionInitialSpanInfo(name, initialExportInfo);
+    return new ExecutionInitialSpanInfo(name, apiId, initialExportInfo);
   }
 
   @Override
   public InitialSpanInfo getInitialSpanInfo(Component component, String overriddenName, String suffix) {
-    return new ExecutionInitialSpanInfo(component, overriddenName, initialExportInfo);
+    return new ExecutionInitialSpanInfo(component, apiId, overriddenName, initialExportInfo);
+  }
+
+  @Override
+  public void initialise() throws InitialisationException {
+    this.artifactId = muleContext.getConfiguration().getId();
+    ArtifactType artifactType = muleContext.getArtifactType();
+    this.artifactTypeStringValue = artifactType.getAsString();
+
+    if (artifactType.equals(POLICY)) {
+      apiId = configurationProperties.resolveStringProperty(API_ID_CONFIGURATION_PROPERTIES_KEY).orElse(null);
+    }
+
   }
 }
