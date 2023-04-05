3e2b31ee59d7a56df71bef516552f43370ed73c4
Mariano Rean <mrean@mulesoft.com>
2023-04-05 11:01:27 -0300

    formatter

--- tests/infrastructure/src/main/java/org/mule/test/infrastructure/process/MuleProcessController.java
+++ tests/infrastructure/src/main/java/org/mule/test/infrastructure/process/MuleProcessController.java
@@ -164,8 +164,8 @@ public class MuleProcessController {
 
 
   /**
-   * Set the environment variables with the corresponding value before running the tests.
-   * This should be called with null after running the tests.
+   * Set the environment variables with the corresponding value before running the tests. This should be called with null after
+   * running the tests.
    *
    * @param testEnvVars map of environment variables and their values
    */
