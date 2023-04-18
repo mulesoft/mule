805c1399c5816ed13c0c4a51b9b0a627a07918d2
rbourbonnavarro <rodrigobourbon44@gmail.com>
2023-04-18 17:58:27 -0300

    Adding tests.

---
+++ modules/tooling-support/src/test/java/org/mule/runtime/module/tooling/internal/config/DefaultDeclarationSessionBuilderTestCase.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.module.tooling.internal.config;
+
+import org.mule.runtime.module.deployment.impl.internal.application.DefaultApplicationFactory;
+import org.mule.runtime.module.tooling.api.ArtifactAgnosticServiceBuilder;
+import org.mule.runtime.module.tooling.internal.AbstractArtifactAgnosticServiceBuilderTestCase;
+
+public class DefaultDeclarationSessionBuilderTestCase extends AbstractArtifactAgnosticServiceBuilderTestCase {
+
+  @Override
+  protected ArtifactAgnosticServiceBuilder getArtifactAgnosticServiceBuilder(DefaultApplicationFactory applicationFactory) {
+    return new DefaultDeclarationSessionBuilder(applicationFactory);
+  }
+
+}
