69755e6433278869c5b2a8fb65d2497cc89ed90a
Mariano Rean <mrean@mulesoft.com>
2023-04-20 16:32:42 -0300

    fix tests

---
+++ modules/spring-config/src/main/java/org/mule/runtime/config/api/dsl/model/properties/DefaultConfigurationPropertiesProviderFactory.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.config.api.dsl.model.properties;
+
+import static org.mule.runtime.api.component.ComponentIdentifier.builder;
+import static org.mule.runtime.internal.dsl.DslConstants.CORE_PREFIX;
+
+import org.mule.runtime.api.component.ComponentIdentifier;
+import org.mule.runtime.api.util.Preconditions;
+import org.mule.runtime.config.api.dsl.model.ConfigurationParameters;
+import org.mule.runtime.properties.api.ConfigurationPropertiesProvider;
+import org.mule.runtime.properties.api.DefaultConfigurationPropertiesProvider;
+import org.mule.runtime.properties.api.ResourceProvider;
+
+/**
+ * Builds the provider for the configuration-properties element.
+ *
+ * @since 4.1
+ * @deprecated since 4.4, use the SDK instead of registering parsers manually.
+ */
+@Deprecated
+public final class DefaultConfigurationPropertiesProviderFactory implements ConfigurationPropertiesProviderFactory {
+
+  public static final String CONFIGURATION_PROPERTIES_ELEMENT = "configuration-properties";
+  public static final ComponentIdentifier CONFIGURATION_PROPERTIES =
+      builder().namespace(CORE_PREFIX).name(CONFIGURATION_PROPERTIES_ELEMENT).build();
+
+  @Override
+  public ComponentIdentifier getSupportedComponentIdentifier() {
+    return CONFIGURATION_PROPERTIES;
+  }
+
+  @Override
+  public ConfigurationPropertiesProvider createProvider(ConfigurationParameters parameters,
+                                                        ResourceProvider externalResourceProvider) {
+
+    String file = parameters.getStringParameter("file");
+    Preconditions.checkArgument(file != null, "Required attribute 'file' of 'configuration-properties' not found");
+    String encoding = parameters.getStringParameter("encoding");
+    return new DefaultConfigurationPropertiesProvider(file, encoding, externalResourceProvider);
+  }
+}
