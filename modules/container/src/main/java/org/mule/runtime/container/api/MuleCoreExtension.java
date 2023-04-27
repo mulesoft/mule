58ab2ab0c36082b991d3c7066e149a8b834d8087
Martín Buchwald <martin.buchwald@mulesoft.com>
2023-04-27 11:51:54 -0300

    W-13067163: Implemente Priority for MuleCoreExtension for loading/executing extensions with different priority

--- modules/container/src/main/java/org/mule/runtime/container/api/MuleCoreExtension.java
+++ modules/container/src/main/java/org/mule/runtime/container/api/MuleCoreExtension.java
@@ -19,4 +19,13 @@ public interface MuleCoreExtension extends Lifecycle, NamedObject {
    * @param containerClassLoader container classloader which provides access to Mule API only. Non null
    */
   void setContainerClassLoader(ArtifactClassLoader containerClassLoader);
+
+  /**
+   * @return the priority (as an int), for the {@link MuleCoreExtension} to be loaded and executed.
+   * Override this method when wanting to ensure that an Extension is loaded before or after the rest.
+   * The lesser this number, the sooner it would be loaded. 
+   */
+  default int priority() {
+    return 1;
+  }
 }
