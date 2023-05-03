1fb03882f572b4437852ad65d099bb6ea7e7c954
Martín Buchwald <martin.buchwald@mulesoft.com>
2023-05-03 16:27:53 -0300

    remove things from test

--- modules/spring-config/src/test/java/org/mule/runtime/config/dsl/spring/ObjectFactoryClassRepositoryTestCase.java
+++ modules/spring-config/src/test/java/org/mule/runtime/config/dsl/spring/ObjectFactoryClassRepositoryTestCase.java
@@ -11,7 +11,6 @@ import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
 import static org.mockito.junit.MockitoJUnit.rule;
-import static org.mule.runtime.api.util.MuleSystemProperties.ENABLE_BYTE_BUDDY_OBJECT_CREATION_PROPERTY;
 
 import io.qameta.allure.Issue;
 
@@ -31,10 +30,7 @@ public class ObjectFactoryClassRepositoryTestCase {
 
   @Rule
   public MockitoRule rule = rule();
-
-  @Rule
-  public SystemProperty enableByteBuddy = new SystemProperty(ENABLE_BYTE_BUDDY_OBJECT_CREATION_PROPERTY, "true");
-
+  
   @Test
   public void testSetters() throws Exception {
 
