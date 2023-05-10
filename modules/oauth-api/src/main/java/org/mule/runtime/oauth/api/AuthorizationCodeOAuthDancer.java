fd4937c79f10c16e1e97d72a7eb4cece45e6cad7
elrodro83 <rmerino@salesforce.com>
2023-05-10 18:12:54 -0300

    deprecation notes

--- modules/oauth-api/src/main/java/org/mule/runtime/oauth/api/AuthorizationCodeOAuthDancer.java
+++ modules/oauth-api/src/main/java/org/mule/runtime/oauth/api/AuthorizationCodeOAuthDancer.java
@@ -6,6 +6,11 @@
  */
 package org.mule.runtime.oauth.api;
 
+/**
+ * Provides compatibility with version 1.x of the mule-oauth-client, which is a transitive api of the service api.
+ * 
+ * @deprecated since 1.5, use {@link org.mule.oauth.client.api.AuthorizationCodeOAuthDancer} from {@code mule-oauth-client 2.x}.
+ */
 @Deprecated
 public interface AuthorizationCodeOAuthDancer extends org.mule.oauth.client.api.AuthorizationCodeOAuthDancer {
 
