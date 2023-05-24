90169a60e3e6166f06cd868b525c8e1e55d5081a
sofiamorseletto <sofimorseletto@gmail.com>
2023-05-24 12:29:33 -0300

    W-13065409: User Story E - Metrics Configuration Discovery Module (#12487)
    * W-13065409: User Story E - Metrics Configuration Discovery Module
    
    * Remove unused imports

---
+++ modules/metrics/mule-metrics-exporter-configuration-api/src/main/java/org/mule/runtime/metrics/exporter/config/api/MeterExporterConfiguration.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.runtime.metrics.exporter.config.api;
+
+/**
+ * Configuration to export metrics.
+ * 
+ * @since 4.5.0
+ */
+public interface MeterExporterConfiguration {
+
+  /**
+   * @param key the key of a configuration parameter
+   *
+   * @return the value associated to the {@param key}
+   */
+  String getStringValue(String key);
+
+  default String getStringValue(String key, String defaultValue) {
+    String value = getStringValue(key);
+
+    if (value == null) {
+      value = defaultValue;
+    }
+
+    return value;
+  }
+}
---
+++ modules/metrics/mule-metrics-exporter-configuration-impl/src/main/java/org/mule/runtime/metrics/exporter/config/impl/FileMeterExporterConfiguration.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.runtime.metrics.exporter.config.impl;
+
+import static org.mule.runtime.core.api.util.PropertiesUtils.loadProperties;
+import static org.mule.runtime.metrics.exporter.config.api.OpenTelemetryMeterExporterConfigurationProperties.MULE_OPEN_TELEMETRY_METER_EXPORTER_CA_FILE_LOCATION;
+import static org.mule.runtime.metrics.exporter.config.api.OpenTelemetryMeterExporterConfigurationProperties.MULE_OPEN_TELEMETRY_METER_EXPORTER_KEY_FILE_LOCATION;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.mule.runtime.api.exception.MuleRuntimeException;
+import org.mule.runtime.config.internal.dsl.model.ClassLoaderResourceProvider;
+import org.mule.runtime.container.api.MuleFoldersUtil;
+import org.mule.runtime.core.api.MuleContext;
+import org.mule.runtime.metrics.exporter.config.api.MeterExporterConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+
+/**
+ * A {@link MeterExporterConfiguration} based on a file in the conf folder.
+ *
+ * @since 4.5.0
+ */
+public class FileMeterExporterConfiguration implements MeterExporterConfiguration {
+
+  private static final Logger LOGGER = getLogger(FileMeterExporterConfiguration.class);
+  private final String CONFIGURATION_PATH = getConfFolder() + FileSystems.getDefault().getSeparator();
+  private static final String CONFIGURATION_FILE_NAME = "meter-exporter.conf";
+
+  private final MuleContext muleContext;
+  private final Properties properties;
+
+  public FileMeterExporterConfiguration(MuleContext muleContext) {
+    this.muleContext = muleContext;
+    this.properties = getMeterExporterProperties();
+  }
+
+  @Override
+  public String getStringValue(String key) {
+    String value = properties.getProperty(key);
+
+    if (value != null && isAValueCorrespondingToAPath(key)) {
+      // Obtain absolute path and return it if possible.
+      String absolutePath = getAbsolutePath(value);
+
+      if (absolutePath != null) {
+        return absolutePath;
+      }
+      // Otherwise, return the non-resolved value so that the error message makes sense.
+    }
+
+    return value;
+  }
+
+  private boolean isAValueCorrespondingToAPath(String key) {
+    return key.equals(MULE_OPEN_TELEMETRY_METER_EXPORTER_CA_FILE_LOCATION) ||
+        key.equals(MULE_OPEN_TELEMETRY_METER_EXPORTER_KEY_FILE_LOCATION);
+  }
+
+  private String getAbsolutePath(String value) {
+    Path path = Paths.get(value);
+
+    try {
+      if (!path.isAbsolute()) {
+        URL url = getExecutionClassLoader(muleContext).getResource(value);
+
+        if (url != null) {
+          return new File(url.toURI()).getAbsolutePath();
+        }
+      }
+    } catch (URISyntaxException e) {
+      return value;
+    }
+
+    return null;
+  }
+
+  private Properties getMeterExporterProperties() {
+    ClassLoaderResourceProvider resourceProvider = new ClassLoaderResourceProvider(getExecutionClassLoader(muleContext));
+    try {
+      InputStream is = resourceProvider
+          .getResourceAsStream(CONFIGURATION_PATH + getPropertiesFileName());
+      return loadProperties(is);
+    } catch (MuleRuntimeException | IOException e) {
+      LOGGER.info("No meter exporter configuration found in the conf directory.");
+    }
+    return new Properties();
+  }
+
+  protected ClassLoader getExecutionClassLoader(MuleContext muleContext) {
+    return muleContext.getExecutionClassLoader();
+  }
+
+  protected String getConfFolder() {
+    return MuleFoldersUtil.getConfFolder().getAbsolutePath();
+  }
+
+  protected String getPropertiesFileName() {
+    return CONFIGURATION_FILE_NAME;
+  }
+}
