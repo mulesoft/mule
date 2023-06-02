f2efaf6c7f95affae0dd2f30c2e7d527bd0272c5
Ramesh Pachanady <rpachanady@mulesoft.com>
2023-06-01 18:52:21 -0700

    W-12354025: Save progress

--- modules/scripting/src/test/java/org/mule/module/scripting/SRubyTestCase.java
+++ modules/scripting/src/test/java/org/mule/module/scripting/SRubyTestCase.java
@@ -6,15 +6,23 @@
  */
 package org.mule.module.scripting;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
+
 import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.SystemProperty;
+import org.mule.util.MuleUrlStreamHandlerFactory;
+import org.mule.util.StringUtils;
+import org.mule.util.SystemUtils;
 
 import static org.junit.Assert.assertEquals;
 
-public class JRubyTestCase extends FunctionalTestCase
+import java.util.Map;
+
+public class SRubyTestCase extends FunctionalTestCase
 {
 
     @Override
@@ -27,13 +35,21 @@ public class JRubyTestCase extends FunctionalTestCase
     public SystemProperty jRubyGlobalVar =
             new SystemProperty("org.jruby.embed.localvariable.behavior", "global");
 
+    @Rule
+    public TestRule globalTimeout = createTestTimeoutRule();
 
     private final String key = "message";
     private final String value = "myMessage";
 
+    @Before
+    public void setup() {
+        setDisposeContextPerClass(false);
+    }
+
     @Test
     public void testJRuby() throws Exception
     {
+        System.out.println("system org.jruby.embed.localvariable.behavior:-" + System.getProperty("org.jruby.embed.localvariable.behavior"));
         MuleEvent event = getTestEvent(key);
         event.setFlowVariable(key, value);
 
