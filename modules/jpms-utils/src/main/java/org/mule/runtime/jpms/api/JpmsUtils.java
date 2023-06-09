f57f9ff85d4927c9fd9d9b4d6e0fa1c476b88f1a
Rodrigo Merino <elrodro83@users.noreply.github.com>
2023-06-09 20:12:12 -0300

    Revert "W-13565514: Not all java.se modules are accessible to artifacts deployed (#12545)" (#12547)
    This reverts commit 5927ed48cf28b3be022f5aacf21a7f360fd9e76e.

--- modules/jpms-utils/src/main/java/org/mule/runtime/jpms/api/JpmsUtils.java
+++ modules/jpms-utils/src/main/java/org/mule/runtime/jpms/api/JpmsUtils.java
@@ -6,8 +6,6 @@
  */
 package org.mule.runtime.jpms.api;
 
-import java.util.Set;
-
 /**
  * No-op implementation of JpmsUtils to use when running on JVM 8.
  * 
@@ -17,10 +15,6 @@ public final class JpmsUtils {
 
   private JpmsUtils() {
     // Nothing to do
-  }
-
-  public static void exploreJdkModules(Set<String> packages) {
-    // nothing to do
   }
 
   public static void validateNoBootModuleLayerTweaking() {
