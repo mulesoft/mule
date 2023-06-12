8a03b5b3f13427d548ba644a96408939539577c8
Rodrigo Merino <elrodro83@users.noreply.github.com>
2023-06-12 12:18:44 -0300

    W-13565514: Not all java.se modules are accessible to artifacts deployed (#12550)

--- modules/jpms-utils/src/main/java/org/mule/runtime/jpms/api/JpmsUtils.java
+++ modules/jpms-utils/src/main/java/org/mule/runtime/jpms/api/JpmsUtils.java
@@ -6,6 +6,8 @@
  */
 package org.mule.runtime.jpms.api;
 
+import java.util.Set;
+
 /**
  * No-op implementation of JpmsUtils to use when running on JVM 8.
  * 
@@ -15,6 +17,10 @@ public final class JpmsUtils {
 
   private JpmsUtils() {
     // Nothing to do
+  }
+
+  public static void exploreJdkModules(Set<String> packages) {
+    // nothing to do
   }
 
   public static void validateNoBootModuleLayerTweaking() {
