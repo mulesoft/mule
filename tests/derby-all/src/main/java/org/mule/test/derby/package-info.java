7bc222ebcde1e84aac792c18accb8422247843f8
elrodro83 <rmerino@salesforce.com>
2023-06-21 09:08:48 -0300

    Fix automodule name for test-jars

---
+++ tests/derby-all/src/main/java/org/mule/test/derby/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+/**
+ * Have something in the base jar so its creation is not skipped.
+ */
+package org.mule.test.derby;
