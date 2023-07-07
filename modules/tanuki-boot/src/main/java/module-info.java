3796ec390e63d025cb7182c40593f248696f6a4a
Axel Sanguinetti <asanguinetti@mulesoft.com>
2023-07-07 18:58:47 -0300

    Feedback: Renaming to tanuki-boot.

--- modules/tanuki-boot/src/main/java/module-info.java
+++ modules/tanuki-boot/src/main/java/module-info.java
@@ -13,7 +13,7 @@
  */
 module org.mule.boot.tanuki {
 
-  exports org.mule.runtime.module.tanuki.internal to org.mule.boot, com.mulesoft.mule.boot;
+  exports org.mule.runtime.module.boot.tanuki.internal to org.mule.boot, com.mulesoft.mule.boot;
 
   // This requirement is static because at runtime it may be changed to the EE version (which contains the same packages)
   requires static org.mule.boot;
