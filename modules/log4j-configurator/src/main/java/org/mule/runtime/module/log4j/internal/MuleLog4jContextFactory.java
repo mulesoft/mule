d8e577c1a991d709fcf88421dae5f49001a8cf46
Ezequiel Werner <eze210@gmail.com>
2023-07-11 00:27:51 -0300

    W-12411993: [NoTanuki] Extract org.mule.runtime.module.launcher.log4j2 to its own jar (#12619)

---
+++ modules/log4j-configurator/src/main/java/org/mule/runtime/module/log4j/internal/MuleLog4jContextFactory.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.module.log4j.internal;
+
+import static org.mule.runtime.api.util.MuleSystemProperties.MULE_LOG_SEPARATION_DISABLED;
+
+import static java.lang.System.getProperty;
+import static java.lang.System.setProperty;
+import static java.util.concurrent.Executors.newCachedThreadPool;
+
+import org.mule.runtime.api.lifecycle.Disposable;
+import org.mule.runtime.module.log4j.api.AsyncLoggerExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import com.lmax.disruptor.ExceptionHandler;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.core.config.ConfigurationFactory;
+import org.apache.logging.log4j.core.config.xml.XmlConfigurationFactory;
+import org.apache.logging.log4j.core.impl.Log4jContextFactory;
+import org.apache.logging.log4j.core.selector.ContextSelector;
+import org.apache.logging.log4j.core.util.Cancellable;
+import org.apache.logging.log4j.core.util.ShutdownCallbackRegistry;
+import org.apache.logging.log4j.spi.LoggerContextFactory;
+
+/**
+ * Implementation of {@link LoggerContextFactory} which acts as the bootstrap for mule's logging mechanism.
+ * <p/>
+ * It forces {@link ArtifactAwareContextSelector} as the only selector, {@link XmlConfigurationFactory} as the only available
+ * {@link ConfigurationFactory}, and sets {@link AsyncLoggerExceptionHandler} as the {@link ExceptionHandler} for failing async
+ * loggers.
+ * <p/>
+ * Other than that, it's pretty much a copy paste of {@link Log4jContextFactory}, due to that classes' lack of extensibility.
+ * <p/>
+ * By forcing {@link XmlConfigurationFactory} as the only available {@link ConfigurationFactory} we're disabling log4j2's ability
+ * to take json and yaml configurations. This is so because those configuration factories depend on versions of the jackson
+ * libraries which would cause conflict with the ones in mule. TODO: Upgrade the jackson libraries bundled with mule so that this
+ * restriction can be lifted off
+ * <p/>
+ * This class also implements {@link ShutdownCallbackRegistry} to avoid default behaviour which is
+ *
+ * @since 4.5
+ */
+public class MuleLog4jContextFactory extends Log4jContextFactory implements Disposable, ShutdownCallbackRegistry {
+
+  public static final boolean LOG_SEPARATION_ENABLED = getProperty(MULE_LOG_SEPARATION_DISABLED) == null;
+
+  private static final String LOG_CONFIGURATION_FACTORY_PROPERTY = "log4j.configurationFactory";
+  private static final String DEFAULT_LOG_CONFIGURATION_FACTORY = XmlConfigurationFactory.class.getName();
+  private static final String ASYNC_LOGGER_EXCEPTION_HANDLER_PROPERTY = "AsyncLoggerConfig.ExceptionHandler";
+  private static final String DEFAULT_ASYNC_LOGGER_EXCEPTION_HANDLER = AsyncLoggerExceptionHandler.class.getName();
+
+  /**
+   * Initializes using a {@link ArtifactAwareContextSelector}
+   * <p>
+   * Log4j tries to instantiate this class using a default constructor.
+   */
+  public MuleLog4jContextFactory() {
+    this(LOG_SEPARATION_ENABLED);
+  }
+
+  /**
+   * Initializes using a {@link ArtifactAwareContextSelector}
+   */
+  public MuleLog4jContextFactory(boolean logSeparationEnabled) {
+    this(logSeparationEnabled ? new ArtifactAwareContextSelector() : new SimpleContextSelector());
+  }
+
+  /**
+   * Initializes using {@code contextSelector}
+   *
+   * @param contextSelector a {@link ContextSelector}
+   */
+  public MuleLog4jContextFactory(ContextSelector contextSelector) {
+    super(contextSelector, new MuleShutdownCallbackRegistry());
+    initialise();
+  }
+
+  protected void initialise() {
+    setupConfigurationFactory();
+    setupAsyncLoggerExceptionHandler();
+  }
+
+  private void setupConfigurationFactory() {
+    setProperty(LOG_CONFIGURATION_FACTORY_PROPERTY, DEFAULT_LOG_CONFIGURATION_FACTORY);
+  }
+
+  private void setupAsyncLoggerExceptionHandler() {
+    String handler = System.getProperty(ASYNC_LOGGER_EXCEPTION_HANDLER_PROPERTY);
+    if (StringUtils.isBlank(handler)) {
+      setProperty(ASYNC_LOGGER_EXCEPTION_HANDLER_PROPERTY, DEFAULT_ASYNC_LOGGER_EXCEPTION_HANDLER);
+    }
+  }
+
+  @Override
+  public void dispose() {
+    ContextSelector selector = getSelector();
+    if (selector instanceof Disposable) {
+      ((Disposable) selector).dispose();
+    }
+    MuleShutdownCallbackRegistry shutdownCallbackRegistry = (MuleShutdownCallbackRegistry) getShutdownCallbackRegistry();
+    shutdownCallbackRegistry.dispose();
+  }
+
+  private static class MuleShutdownCallbackRegistry implements ShutdownCallbackRegistry, Disposable {
+
+    private final ExecutorService executorService =
+        newCachedThreadPool(runnable -> new Thread(runnable, "[MuleRuntime].log4j.shudownhook"));
+
+    private final List<Runnable> hooks = new ArrayList<>();
+
+    @Override
+    public Cancellable addShutdownCallback(Runnable callback) {
+      hooks.add(callback);
+      return new Cancellable() {
+
+        @Override
+        public void cancel() {
+          hooks.remove(callback);
+        }
+
+        @Override
+        public void run() {
+          callback.run();
+        }
+      };
+    }
+
+    @Override
+    public void dispose() {
+      for (Runnable hook : new ArrayList<>(hooks)) {
+        executorService.submit(hook);
+      }
+      try {
+        executorService.awaitTermination(1000, TimeUnit.MILLISECONDS);
+      } catch (InterruptedException e) {
+        // Nothing to do, not even log since we are shutting down the logger
+      }
+      executorService.shutdownNow();
+    }
+  }
+
+}
---
+++ modules/log4j-configurator/src/main/java/org/mule/runtime/module/log4j/internal/MuleLoggerContext.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.module.log4j.internal;
+
+import org.mule.runtime.module.artifact.api.classloader.ArtifactClassLoader;
+import org.mule.runtime.module.artifact.api.classloader.RegionClassLoader;
+import org.mule.runtime.module.artifact.api.descriptor.ApplicationDescriptor;
+import org.mule.runtime.module.artifact.api.descriptor.DeployableArtifactDescriptor;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.selector.ContextSelector;
+import org.apache.logging.log4j.message.MessageFactory;
+
+/**
+ * Subclass of {@link LoggerContext} which adds some information about the mule artifact being logged.
+ * <p/>
+ * The most important function of this class though is to override the {@link #reconfigure()} method to to its inherited purpose
+ * plus invoking {@link LoggerContextConfigurer#configure(MuleLoggerContext)}.
+ * <p/>
+ * The {@link LoggerContextConfigurer} needs to be invoked here so that it's invoked each time the configuration is reloaded.
+ * <p/>
+ * This class must not hold any reference to a {@link ClassLoader} since otherwise {@link Logger} instances held on static fields
+ * will make that class loader GC unreachable
+ *
+ * @since 4.5
+ */
+class MuleLoggerContext extends LoggerContext {
+
+  private final LoggerContextConfigurer loggerContextConfigurer = new LoggerContextConfigurer();
+
+  private final URI configFile;
+  private final boolean standalone;
+  private final boolean logSeparationEnabled;
+  private ContextSelector contextSelector;
+  private final boolean artifactClassloader;
+  private final boolean applicationClassloader;
+  private final String artifactName;
+  private final int ownerClassLoaderHash;
+
+  private DeployableArtifactDescriptor artifactDescriptor;
+
+  MuleLoggerContext(String name, ContextSelector contextSelector, boolean standalone, boolean logSeparationEnabled) {
+    this(name, null, null, contextSelector, standalone, logSeparationEnabled);
+  }
+
+  MuleLoggerContext(String name,
+                    URI configLocn,
+                    ClassLoader ownerClassLoader,
+                    ContextSelector contextSelector,
+                    boolean standalone,
+                    boolean logSeparationEnabled) {
+    super(name, null, configLocn);
+    configFile = configLocn;
+    this.contextSelector = contextSelector;
+    this.standalone = standalone;
+    this.logSeparationEnabled = logSeparationEnabled;
+    ownerClassLoaderHash =
+        ownerClassLoader != null ? ownerClassLoader.hashCode() : getClass().getClassLoader().getSystemClassLoader().hashCode();
+
+    if (ownerClassLoader instanceof ArtifactClassLoader) {
+      artifactClassloader = true;
+      artifactName = getArtifactName((ArtifactClassLoader) ownerClassLoader);
+      artifactDescriptor = getArtifactDescriptor((ArtifactClassLoader) ownerClassLoader);
+      applicationClassloader = ownerClassLoader instanceof RegionClassLoader && ((RegionClassLoader) ownerClassLoader)
+          .getOwnerClassLoader().getArtifactDescriptor() instanceof ApplicationDescriptor;
+    } else {
+      artifactClassloader = false;
+      applicationClassloader = false;
+      artifactName = null;
+    }
+  }
+
+  private DeployableArtifactDescriptor getArtifactDescriptor(ArtifactClassLoader ownerClassLoader) {
+    if (!(ownerClassLoader.getArtifactDescriptor() instanceof DeployableArtifactDescriptor)) {
+      throw new IllegalArgumentException("Artifact should be a deployable, i.e. an application or domain");
+    }
+
+    return ownerClassLoader.getArtifactDescriptor();
+  }
+
+  private String getArtifactName(ArtifactClassLoader ownerClassLoader) {
+    return ownerClassLoader.getArtifactDescriptor().getName();
+  }
+
+  @Override
+  public synchronized void reconfigure() {
+    loggerContextConfigurer.configure(this);
+    if (loggerContextConfigurer.shouldConfigureContext(this)) {
+      super.reconfigure();
+    }
+  }
+
+  @Override
+  public void updateLoggers(Configuration config) {
+    loggerContextConfigurer.update(this);
+    super.updateLoggers(config);
+  }
+
+  /**
+   * Override to return a {@link DispatchingLogger} instead of a simple logger {@inheritDoc}
+   *
+   * @return a {@link DispatchingLogger}
+   */
+  @Override
+  protected Logger newInstance(LoggerContext ctx, final String name, final MessageFactory messageFactory) {
+    Logger logger = super.newInstance(ctx, name, messageFactory);
+    if (artifactClassloader || applicationClassloader || !logSeparationEnabled) {
+      return logger;
+    }
+
+    return new DispatchingLogger(logger, ownerClassLoaderHash, this, contextSelector, messageFactory) {
+
+      // force the name due to log4j2's cyclic constructor dependencies
+      // aren't a friend of the wrapper pattern
+      @Override
+      public String getName() {
+        return name;
+      }
+    };
+  }
+
+  protected URI getConfigFile() {
+    return configFile;
+  }
+
+  protected boolean isStandalone() {
+    return standalone;
+  }
+
+  protected boolean isArtifactClassloader() {
+    return artifactClassloader;
+  }
+
+  protected boolean isApplicationClassloader() {
+    return applicationClassloader;
+  }
+
+  protected DeployableArtifactDescriptor getArtifactDescriptor() {
+    return artifactDescriptor;
+  }
+
+  protected String getArtifactName() {
+    return artifactName;
+  }
+
+  @Override
+  public void stop() {
+    super.stop();
+    // Clean up reference to avoid class loader leaks
+    this.artifactDescriptor = null;
+    this.contextSelector = null;
+  }
+
+  @Override
+  public boolean stop(long timeout, TimeUnit timeUnit) {
+    boolean result = super.stop(timeout, timeUnit);
+    // Clean up reference to avoid class loader leaks
+    this.artifactDescriptor = null;
+    this.contextSelector = null;
+    return result;
+  }
+}
