b783671c5a38c228ccd4be85671f929d4445f25f
elrodro83 <rmerino@salesforce.com>
2023-07-25 14:28:21 -0300

    W-13823388: boot module not exporting utils used by agent plugin

--- modules/reboot/src/main/java/module-info.java
+++ modules/reboot/src/main/java/module-info.java
@@ -9,6 +9,8 @@
  */
 module org.mule.boot {
 
+  exports org.mule.runtime.module.reboot.api;
+
   // TODO W-13151134: export only to modules that require it once org.mule.runtime.launcher is modularized
   // exports org.mule.runtime.module.reboot.internal to org.mule.boot.tanuki,org.mule.runtime.launcher,com.mulesoft.mule.runtime.plugin;
   exports org.mule.runtime.module.reboot.internal;
