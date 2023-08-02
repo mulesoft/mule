1b78a76c42a8b2589d70c9d3e206d0bb2080b23f
Fabian Gonzalez <fabian.gonzalez@mulesoft.com>
2023-08-02 11:34:12 -0300

    Revert "Review"
    This reverts commit ea43edfac07d523d2296213ad2576d2a44771988.

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/resolver/CorrelationInfoArgumentResolver.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/resolver/CorrelationInfoArgumentResolver.java
@@ -4,14 +4,12 @@
 package org.mule.runtime.module.extension.internal.runtime.resolver;
 
 import static org.mule.runtime.core.internal.profiling.NoopCoreEventTracer.getNoopCoreEventTracer;
-import static org.mule.runtime.module.extension.internal.runtime.client.NullComponent.NULL_COMPONENT;
 
 import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.runtime.extension.api.runtime.operation.ExecutionContext;
 import org.mule.runtime.extension.api.runtime.parameter.CorrelationInfo;
 import org.mule.runtime.module.extension.api.runtime.privileged.ExecutionContextAdapter;
 import org.mule.runtime.module.extension.internal.runtime.parameter.ImmutableCorrelationInfo;
-import org.mule.runtime.tracer.api.EventTracer;
 
 /**
  * {@link ArgumentResolver} that yields instances of {@link CorrelationInfo}
@@ -20,30 +18,15 @@ import org.mule.runtime.tracer.api.EventTracer;
  */
 public class CorrelationInfoArgumentResolver implements ArgumentResolver<CorrelationInfo> {
 
-  private final EventTracer<CoreEvent> coreEventEventTracer;
-
-  public CorrelationInfoArgumentResolver() {
-    this.coreEventEventTracer = getNoopCoreEventTracer();
-  }
-
-  public CorrelationInfoArgumentResolver(EventTracer<CoreEvent> coreEventEventTracer) {
-    this.coreEventEventTracer = coreEventEventTracer;
-  }
-
   @Override
   public CorrelationInfo resolve(ExecutionContext executionContext) {
     CoreEvent event = ((ExecutionContextAdapter) executionContext).getEvent();
 
-    if (((ExecutionContextAdapter<?>) executionContext).getComponent().equals(NULL_COMPONENT)) {
-      // TODO: W-13837896
-      // If this was executed from a null component don't allow to execute any tracing
-      // from the connector. This is especially used in the cases where an extension in an
-      // operation invokes another extension through an ExtensionsClient.
-      return new ImmutableCorrelationInfo(event.getContext().getId(), true, event.getCorrelationId(),
-                                          event.getItemSequenceInfo().orElse(null), event, getNoopCoreEventTracer());
-    }
-
+    // TODO: W-13837896
+    // If this was executed from a null component don't allow to execute any tracing
+    // from the connector. This is especially used in the cases where an extension in an
+    // operation invokes another extension through an ExtensionsClient.
     return new ImmutableCorrelationInfo(event.getContext().getId(), true, event.getCorrelationId(),
-                                        event.getItemSequenceInfo().orElse(null), event, coreEventEventTracer);
+                                        event.getItemSequenceInfo().orElse(null), event, getNoopCoreEventTracer());
   }
 }
