46a2c84ce763cc26292a45dce13a547d2259b06b
elrodro83 <rmerino@salesforce.com>
2023-08-04 15:13:27 -0300

    W-13888827: Isolate service dependencies in their own module layer

--- modules/jpms-utils/src/main/java11/org/mule/runtime/jpms/api/JpmsUtils.java
+++ modules/jpms-utils/src/main/java11/org/mule/runtime/jpms/api/JpmsUtils.java
@@ -5,13 +5,34 @@ package org.mule.runtime.jpms.api;
 
 import static java.lang.Boolean.getBoolean;
 import static java.lang.ModuleLayer.boot;
+import static java.lang.ModuleLayer.defineModulesWithOneLoader;
 import static java.lang.StackWalker.Option.RETAIN_CLASS_REFERENCE;
+import static java.lang.module.Configuration.resolve;
+import static java.lang.module.ModuleFinder.ofSystem;
+import static java.nio.file.Paths.get;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.partitioningBy;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
+import java.lang.ModuleLayer.Controller;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
+import java.lang.module.Configuration;
+import java.lang.module.ModuleFinder;
+import java.lang.module.ModuleReference;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Stream;
 
 /**
  * Utilities related to how Mule uses the Java Module system.
@@ -97,13 +118,159 @@ public final class JpmsUtils {
 
           // Original intention is to only expose standard java modules...
           return moduleName.startsWith("java.")
-              // ... but because we need to keep compatibility with Java 8, older verion of some libraries use internal jdk
+              // ... but because we need to keep compatibility with Java 8, older versions of some libraries use internal JDK
               // modules packages:
               // * caffeine 2.x still uses sun.misc.Unsafe. Ref: https://github.com/ben-manes/caffeine/issues/273
               // * obgenesis SunReflectionFactoryHelper, used by Mockito
               || moduleName.startsWith("jdk.");
         })
         .forEach(module -> packages.addAll(module.getPackages()));
+  }
+
+  /**
+   * Creates a {@link ModuleLayer} for the given {@code modulePathEntries} and with the given {@code parent}.
+   * <p>
+   * Note: By definition, automatic modules have transitive readability on ALL other modules on the same layer and the parents.
+   * This may cause a situation where a layer that is supposed to be isolated will instead be able to read all the modules in the
+   * parent layers. To prevent this, the {@code automaticModulesInTheirOwnLayer} parameter must be passes as {@code true}.
+   * 
+   * @param modulePathEntries               the URLs from which to find the modules
+   * @param parent                          the parent class loader for delegation
+   * @param parentLayer                     a layer of modules that will be visible from the newly created {@link ModuleLayer}.
+   * @param automaticModulesInTheirOwnLayer whether an additional {@link ModuleLayer} having only the {@code boot} layer as parent
+   *                                        will be created for the automatic modules.
+   * @return a new {@link ModuleLayer}.
+   */
+  public static ModuleLayer createModuleLayer(URL[] modulePathEntries, ClassLoader parent, Optional<ModuleLayer> parentLayer,
+                                              boolean automaticModulesInTheirOwnLayer,
+                                              boolean filterBootModules) {
+    final Set<String> bootModules;
+    if (filterBootModules) {
+      bootModules = boot().modules().stream()
+          .map(m -> m.getName())
+          .collect(toSet());
+    } else {
+      bootModules = emptySet();
+    }
+
+    Path[] paths = Stream.of(modulePathEntries)
+        .map(url -> {
+          try {
+            return get(url.toURI());
+          } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+          }
+        })
+        .toArray(size -> new Path[size]);
+
+    ModuleFinder modulesFinder = ModuleFinder.of(paths);
+
+    Map<Boolean, List<ModuleReference>> modulesByAutomatic = modulesFinder
+        .findAll()
+        .stream()
+        .filter(moduleRef -> !bootModules.contains(moduleRef.descriptor().name()))
+        .collect(partitioningBy(moduleRef -> isolateInOrphanLayer(moduleRef, parentLayer)));
+    System.out.println(" >> Found modules: " + modulesByAutomatic);
+
+    ModuleLayer resolvedParentLayer = parentLayer.orElse(boot());
+    System.out
+        .println(" >> Resolved parent ModuleLayer " + (parentLayer.isEmpty() ? "(boot)" : "") + ": '" + resolvedParentLayer
+            + "'");
+
+    Controller controller;
+    if (automaticModulesInTheirOwnLayer) {
+      // put all automatic modules in their own layer, having only boot layer as parent...
+      Path[] automaticModulesPaths = modulesByAutomatic.get(true)
+          .stream()
+          .map(ModuleReference::location)
+          .filter(Optional::isPresent)
+          .map(Optional::get)
+          .map(uri -> get(uri))
+          .toArray(size -> new Path[size]);
+      ModuleFinder automaticModulesFinder = ModuleFinder.of(automaticModulesPaths);
+      Configuration automaticModulesConfiguration = boot().configuration()
+          .resolve(automaticModulesFinder, ofSystem(), modulesByAutomatic.get(true)
+              .stream()
+              .map(moduleRef -> moduleRef.descriptor().name())
+              .collect(toList()));
+      Controller automaticModulesController = defineModulesWithOneLoader(automaticModulesConfiguration,
+                                                                         singletonList(boot()),
+                                                                         parentLayer.map(layer -> layer.findLoader(layer.modules()
+                                                                             .iterator().next().getName())).orElse(parent));
+
+      // ... the put the rest of the modules on a new layer with the automatic modules one as parent.
+      Path[] notAutomaticModulesPaths = modulesByAutomatic.get(false)
+          .stream()
+          .map(ModuleReference::location)
+          .filter(Optional::isPresent)
+          .map(Optional::get)
+          .map(uri -> get(uri))
+          .toArray(size -> new Path[size]);
+      ModuleFinder notAutomaticModulesFinder = ModuleFinder.of(notAutomaticModulesPaths);
+      Configuration configuration = resolve(notAutomaticModulesFinder,
+                                            asList(automaticModulesController.layer().configuration(),
+                                                   resolvedParentLayer.configuration()),
+                                            ofSystem(),
+                                            modulesByAutomatic.get(false).stream()
+                                                .map(moduleRef -> moduleRef.descriptor().name())
+                                                .collect(toList()));
+      controller = defineModulesWithOneLoader(configuration,
+                                              asList(automaticModulesController.layer(), resolvedParentLayer),
+                                              parentLayer.map(layer -> layer.findLoader(layer.modules()
+                                                  .iterator().next().getName())).orElse(parent));
+
+    } else {
+      Path[] filteredModulesPaths = modulesByAutomatic.values().stream()
+          .flatMap(Collection::stream)
+          .map(ModuleReference::location)
+          .filter(Optional::isPresent)
+          .map(Optional::get)
+          .map(uri -> get(uri))
+          .toArray(size -> new Path[size]);
+      ModuleFinder filteredModulesFinder = ModuleFinder.of(filteredModulesPaths);
+
+      Configuration configuration = resolvedParentLayer.configuration()
+          .resolve(filteredModulesFinder, ofSystem(), modulesByAutomatic.values().stream()
+              .flatMap(Collection::stream)
+              .map(modueRef -> modueRef.descriptor().name())
+              .collect(toList()));
+      controller = defineModulesWithOneLoader(configuration,
+                                              singletonList(resolvedParentLayer),
+                                              parentLayer.map(layer -> layer.findLoader(layer.modules()
+                                                  .iterator().next().getName())).orElse(parent));
+    }
+
+
+    return controller.layer();
+  }
+
+  private static final Set<String> SERVICE_MODULE_NAME_PREFIXES =
+      new HashSet<>(asList("org.mule.service.", "com.mulesoft.mule.service."));
+
+  /**
+   * 
+   * @param moduleRef
+   * @param containerLayer
+   * @return {@code true} the the referenced module is automatic or does not read any module from the container.
+   */
+  private static boolean isolateInOrphanLayer(ModuleReference moduleRef, Optional<ModuleLayer> containerLayer) {
+    if (moduleRef.descriptor().isAutomatic()
+        // TODO W-13761983 remove this condition
+        && SERVICE_MODULE_NAME_PREFIXES.stream()
+            .noneMatch(moduleRef.descriptor().name()::startsWith)) {
+      return true;
+    }
+
+    final Set<String> containerModuleNames = containerLayer
+        .map(layer -> layer.modules().stream()
+            .map(Module::getName)
+            .collect(toSet()))
+        .orElse(emptySet());
+
+    return containerLayer
+        .map(layer -> moduleRef.descriptor().requires().stream()
+            .noneMatch(req -> containerModuleNames.contains(req.name())))
+        .orElse(false);
   }
 
   /**
@@ -115,8 +282,9 @@ public final class JpmsUtils {
    */
   public static void openToModule(ModuleLayer layer, String moduleName, String bootModuleName, List<String> packages) {
     // Make sure only allowed users within the Mule Runtime use this
-    if (!StackWalker.getInstance(RETAIN_CLASS_REFERENCE).getCallerClass().getName()
-        .equals("org.mule.runtime.module.service.api.artifact.ServiceModuleLayerFactory")) {
+    final String callerClassName = StackWalker.getInstance(RETAIN_CLASS_REFERENCE).getCallerClass().getName();
+    if (!(callerClassName.equals("org.mule.runtime.module.service.api.artifact.ServiceModuleLayerFactory")
+        || callerClassName.equals("org.mule.runtime.jpms.api.JpmsUtils"))) {
       throw new UnsupportedOperationException("This is for internal use only.");
     }
 
