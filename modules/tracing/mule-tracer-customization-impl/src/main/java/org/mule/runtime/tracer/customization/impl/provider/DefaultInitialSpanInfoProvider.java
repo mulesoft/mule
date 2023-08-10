c55b72da823cbe8ec3dbc134e152719ce65452bd
Fabian Gonzalez <fabian.gonzalez@mulesoft.com>
2023-08-10 10:59:05 -0300

    Changed method name to not confuse with a cache

--- modules/tracing/mule-tracer-customization-impl/src/main/java/org/mule/runtime/tracer/customization/impl/provider/DefaultInitialSpanInfoProvider.java
+++ modules/tracing/mule-tracer-customization-impl/src/main/java/org/mule/runtime/tracer/customization/impl/provider/DefaultInitialSpanInfoProvider.java
@@ -26,6 +26,7 @@ import javax.inject.Inject;
  */
 public class DefaultInitialSpanInfoProvider implements InitialSpanInfoProvider {
 
+  // TODO: in the future, this can be exposed in the discovered service (new feature) to show what is being traced and how.
   private final Map<InitialSpanInfoIdentifier, ExecutionInitialSpanInfo> componentInitialSpanInfos = new ConcurrentHashMap<>();
   public static final String API_ID_CONFIGURATION_PROPERTIES_KEY = "apiId";
 
@@ -105,11 +106,11 @@ public class DefaultInitialSpanInfoProvider implements InitialSpanInfoProvider {
   }
 
   /**
-   * @param initialSpanInfo the {@link InitialSpanInfo} to verify if it is cached.
-   *
-   * @return whether it is cached.
+   * @param initialSpanInfo the {@link InitialSpanInfo} to verify if it is dynamically configured, it may be affected by a change
+   *                        in the configuration of tracing.
+   * @return whether it is dynamically configurable.
    */
-  public boolean isCached(InitialSpanInfo initialSpanInfo) {
+  public boolean isDynamicallyConfigurable(InitialSpanInfo initialSpanInfo) {
     return componentInitialSpanInfos.containsValue(initialSpanInfo);
   }
 }
