be34f43b7d078a900afafd863738bf2f59ed6e17
Fabian Gonzalez <fabian.gonzalez@mulesoft.com>
2023-08-11 13:59:37 -0300

    Fix test

--- runtime-extension-model/src/test/java/org/mule/runtime/config/internal/validation/CachingArtifactAstGraphDependencyProviderTestCase.java
+++ runtime-extension-model/src/test/java/org/mule/runtime/config/internal/validation/CachingArtifactAstGraphDependencyProviderTestCase.java
@@ -3,24 +3,23 @@
  */
 package org.mule.runtime.config.internal.validation;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.sameInstance;
 import static org.mule.test.allure.AllureConstants.MuleDsl.DslValidationStory.DSL_VALIDATION_STORY;
 import static org.mule.test.allure.AllureConstants.MuleDsl.MULE_DSL;
 
 import static java.util.stream.Stream.empty;
 
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import io.qameta.allure.Issue;
-import org.junit.Test;
 import org.mule.runtime.ast.api.ArtifactAst;
 import org.mule.runtime.config.internal.validation.ast.CachingArtifactAstGraphDependencyProvider;
 
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
-import org.junit.Assert;
+import io.qameta.allure.Issue;
+import org.junit.Test;
 
 @Feature(MULE_DSL)
 @Story(DSL_VALIDATION_STORY)
@@ -32,8 +31,10 @@ public class CachingArtifactAstGraphDependencyProviderTestCase {
     CachingArtifactAstGraphDependencyProvider cachingArtifactAstGraphDependencyProvider =
         new CachingArtifactAstGraphDependencyProvider();
     ArtifactAst artifactAst = mock(ArtifactAst.class);
-    when(artifactAst.recursiveStream()).thenReturn(empty());
-    Assert.assertThat(cachingArtifactAstGraphDependencyProvider.get(artifactAst),
-                      sameInstance(cachingArtifactAstGraphDependencyProvider.get(artifactAst)));
+    // The double return is done to avoid the consumption of the stream in the first
+    // invocation.
+    when(artifactAst.recursiveStream()).thenReturn(empty()).thenReturn(empty());
+    assertThat(cachingArtifactAstGraphDependencyProvider.get(artifactAst),
+               sameInstance(cachingArtifactAstGraphDependencyProvider.get(artifactAst)));
   }
 }
