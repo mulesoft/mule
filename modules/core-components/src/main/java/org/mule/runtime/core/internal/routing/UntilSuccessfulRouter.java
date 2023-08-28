51e73855fb4f04e2c44e1a8714dbc8f3137cb98c
Fabian Gonzalez <fabian.gonzalez@mulesoft.com>
2023-08-28 10:43:53 -0300

    W-14011209: Race condition results in a NPE due to sharing of Map for… (#12727)

--- modules/core-components/src/main/java/org/mule/runtime/core/internal/routing/UntilSuccessfulRouter.java
+++ modules/core-components/src/main/java/org/mule/runtime/core/internal/routing/UntilSuccessfulRouter.java
@@ -8,6 +8,7 @@ import static org.mule.runtime.api.functional.Either.left;
 import static org.mule.runtime.api.functional.Either.right;
 import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
 import static org.mule.runtime.api.metadata.DataType.NUMBER;
+import static org.mule.runtime.api.util.collection.SmallMap.copy;
 import static org.mule.runtime.core.api.retry.policy.SimpleRetryPolicyTemplate.RETRY_COUNT_FOREVER;
 import static org.mule.runtime.core.api.rx.Exceptions.propagateWrappingFatal;
 import static org.mule.runtime.core.api.transaction.TransactionCoordination.isTransactionActive;
@@ -180,8 +181,15 @@ class UntilSuccessfulRouter {
     return (error, offendingEvent) -> {
       MessagingException messagingError = (MessagingException) error;
       RetryContext ctx = getRetryContextForEvent(messagingError.getEvent());
-      int retriesLeft =
-          ctx.retryCount.getAndDecrement();
+      int retriesLeft = 0;
+
+      // This is defensive not to get blocked in case a race condition happens.
+      if (ctx != null) {
+        retriesLeft = ctx.retryCount.getAndDecrement();
+      } else {
+        LOGGER
+            .error("The RetryContext was not found. This is probably a race condition. No further attempts for the until successful will be done.");
+      }
 
       if (retriesLeft > 0) {
         LOGGER.error("Retrying execution of event, attempt {} of {}.", ctx.getAttemptNumber(),
@@ -193,7 +201,15 @@ class UntilSuccessfulRouter {
       } else { // Retries exhausted
         // Current context already pooped. No need to re-insert it
         LOGGER.error("Retry attempts exhausted. Failing...");
-        Throwable resolvedError = getThrowableFunction(ctx.event).apply(error);
+        Throwable resolvedError;
+
+        // This is defensive not to get blocked in case a race condition happens.
+        if (ctx != null) {
+          resolvedError = getThrowableFunction(ctx.event).apply(error);
+        } else {
+          resolvedError = getThrowableFunction(messagingError.getEvent()).apply(error);
+        }
+
         // Delete current context from event
         eventWithCurrentContextDeleted(messagingError.getEvent());
         downstreamRecorder.next(left(resolvedError, CoreEvent.class));
@@ -265,8 +281,8 @@ class UntilSuccessfulRouter {
   private CoreEvent eventWithCurrentContext(CoreEvent event, RetryContext ctx) {
     // Requires: The ctx that corresponds to this router execution is not in the stack, or there's no stack yet
     // Assures: The ctx that corresponds to this router execution is the one on top of the stack
-
-    Map<String, RetryContext> retryCtxContainer = retryContextResolver.getCurrentContextFromEvent(event);
+    // The retryContextContainer should be copied before adding a new element to avoid race conditions (W-14011209)
+    Map<String, RetryContext> retryCtxContainer = copy(retryContextResolver.getCurrentContextFromEvent(event));
     retryCtxContainer.put(event.getContext().getId(), ctx);
 
     return retryContextResolver.eventWithContext(event, retryCtxContainer);
