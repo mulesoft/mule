75e902860529244fa00e7de13c27b4c275a4f5dd
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2023-09-01 15:08:45 +0200

    W-12276645: reduce computational complexity on ComponentParameterization

--- modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/runtime/resolver/ComponentParameterizationUtilsTestCase.java
+++ modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/runtime/resolver/ComponentParameterizationUtilsTestCase.java
@@ -6,14 +6,17 @@
  */
 package org.mule.runtime.module.extension.internal.runtime.resolver;
 
+import static org.mule.runtime.module.extension.internal.runtime.resolver.ComponentParameterizationUtils.createComponentParameterization;
+
+import static java.lang.String.format;
 import static java.util.Arrays.asList;
 import static java.util.Optional.of;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.rules.ExpectedException.none;
+import static org.mockito.Mock.Strictness.LENIENT;
 import static org.mockito.Mockito.when;
-
-import static org.mule.runtime.module.extension.internal.runtime.resolver.ComponentParameterizationUtils.createComponentParameterization;
 
 import org.mule.runtime.api.meta.model.parameter.ParameterGroupModel;
 import org.mule.runtime.api.meta.model.parameter.ParameterModel;
@@ -37,6 +40,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 public class ComponentParameterizationUtilsTestCase {
 
   private static final String PARAMETER_NAME = "parameterName";
+  private static final String COMPONENT_NAME = "mockModel";
   private static final String ANOTHER_PARAMETER_NAME = "anotherParameterName";
 
   private static final String PARAMETER_GROUP_NAME = "parameterName";
@@ -45,7 +49,7 @@ public class ComponentParameterizationUtilsTestCase {
   private static final String PARAMETER_VALUE = "parameterValue";
   private static final String ANOTHER_PARAMETER_VALUE = "anotherParameterValue";
 
-  @Mock
+  @Mock(strictness = LENIENT)
   private ParameterizedModel parameterizedModelMock;
 
   @Mock
@@ -69,9 +73,12 @@ public class ComponentParameterizationUtilsTestCase {
     when(anotherParameterModelMock.getName()).thenReturn(ANOTHER_PARAMETER_NAME);
     when(parameterGroupModelMock.getName()).thenReturn(PARAMETER_GROUP_NAME);
     when(anotherParameterGroupModelMock.getName()).thenReturn(ANOTHER_PARAMETER_GROUP_NAME);
+
+    when(parameterizedModelMock.getName()).thenReturn(COMPONENT_NAME);
     when(parameterizedModelMock.getParameterGroupModels())
         .thenReturn(asList(parameterGroupModelMock, anotherParameterGroupModelMock));
     when(parameterGroupModelMock.getParameter(PARAMETER_NAME)).thenReturn(of(parameterModelMock));
+
     when(anotherParameterGroupModelMock.getParameter(ANOTHER_PARAMETER_NAME)).thenReturn(of(anotherParameterModelMock));
   }
 
@@ -89,7 +96,7 @@ public class ComponentParameterizationUtilsTestCase {
   @Test
   public void parameterizedModelWithDuplicatedParameterName() {
     expectedException.expect(IllegalArgumentException.class);
-    expectedException.expectMessage("Parameter exists in more than one group");
+    expectedException.expectMessage(format("Parameter '%s' exists in more than one group for component '%s'"));
     when(anotherParameterGroupModelMock.getParameter(PARAMETER_NAME)).thenReturn(of(anotherParameterModelMock));
     Map<String, Object> parameters = new HashMap<>();
     parameters.put(PARAMETER_NAME, PARAMETER_VALUE);
