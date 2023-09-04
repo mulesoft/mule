2d2adc98aab6da8cd575a250abff57d9f25383c2
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2023-09-04 13:54:08 -0300

    W-12276645: misc perf improvements (#12761)

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/client/operation/OperationKey.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/client/operation/OperationKey.java
@@ -3,22 +3,15 @@
  */
 package org.mule.runtime.module.extension.internal.runtime.client.operation;
 
-import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
-
 import static java.lang.String.format;
 import static java.util.Objects.hash;
-import static java.util.Optional.of;
 
-import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.api.meta.model.ExtensionModel;
 import org.mule.runtime.api.meta.model.operation.OperationModel;
-import org.mule.runtime.core.api.event.CoreEvent;
-import org.mule.runtime.core.api.extension.ExtensionManager;
 import org.mule.runtime.extension.api.client.ExtensionsClient;
 import org.mule.runtime.extension.api.runtime.config.ConfigurationProvider;
 
 import java.util.Objects;
-import java.util.Optional;
 
 /**
  * Identifies an extension operation and its referenced {@link ConfigurationProvider} to be executed in the context of a
@@ -31,51 +24,14 @@ public class OperationKey {
   private final String configName;
   private final ExtensionModel extensionModel;
   private final OperationModel operationModel;
-  private final ExtensionManager extensionManager;
-
-  private Optional<ConfigurationProvider> configurationProvider;
 
   public OperationKey(ExtensionModel extensionModel,
                       OperationModel operationModel,
-                      String configName,
-                      ExtensionManager extensionManager) {
+                      String configName) {
     this.extensionModel = extensionModel;
     this.operationModel = operationModel;
     this.configName = configName;
-    this.extensionManager = extensionManager;
   }
-
-  public Optional<ConfigurationProvider> getConfigurationProvider(CoreEvent contextEvent) {
-    if (configurationProvider != null) {
-      return configurationProvider;
-    }
-
-    synchronized (this) {
-      if (configurationProvider != null) {
-        return configurationProvider;
-      }
-
-      if (configName != null) {
-        configurationProvider = of(extensionManager.getConfigurationProvider(configName)
-            .map(configurationProvider -> {
-              if (configurationProvider.getExtensionModel() != extensionModel) {
-                throw new IllegalArgumentException(format(
-                                                          "A config of the '%s' extension was expected but one from '%s' was parameterized instead",
-                                                          extensionModel.getName(),
-                                                          configurationProvider.getExtensionModel().getName()));
-              }
-              return configurationProvider;
-            })
-            .orElseThrow(() -> new MuleRuntimeException(
-                                                        createStaticMessage("No configuration [" + configName + "] found"))));
-      } else {
-        configurationProvider = extensionManager.getConfigurationProvider(extensionModel, operationModel, contextEvent);
-      }
-    }
-
-    return configurationProvider;
-  }
-
 
   public ExtensionModel getExtensionModel() {
     return extensionModel;
@@ -83,6 +39,10 @@ public class OperationKey {
 
   public OperationModel getOperationModel() {
     return operationModel;
+  }
+
+  public String getConfigName() {
+    return configName;
   }
 
   @Override
