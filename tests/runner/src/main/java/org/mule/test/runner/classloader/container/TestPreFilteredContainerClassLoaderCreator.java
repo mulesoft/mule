f75f3ec920a2b8c5da5c9de149db49aaa9a6bbd3
elrodro83 <rmerino@salesforce.com>
2023-09-05 14:43:33 -0300

    reflection adapter

--- tests/runner/src/main/java/org/mule/test/runner/classloader/container/TestPreFilteredContainerClassLoaderCreator.java
+++ tests/runner/src/main/java/org/mule/test/runner/classloader/container/TestPreFilteredContainerClassLoaderCreator.java
@@ -9,16 +9,22 @@ package org.mule.test.runner.classloader.container;
 import static org.mule.runtime.container.internal.ContainerClassLoaderCreatorUtils.getLookupPolicy;
 import static org.mule.runtime.core.api.util.ClassUtils.withContextClassLoader;
 
+import static java.util.stream.Collectors.toList;
+
+import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.container.api.ModuleRepository;
 import org.mule.runtime.container.internal.ClasspathModuleDiscoverer;
 import org.mule.runtime.container.internal.ContainerModuleDiscoverer;
 import org.mule.runtime.container.internal.DefaultModuleRepository;
+import org.mule.runtime.container.internal.ModuleDiscoverer;
 import org.mule.runtime.container.internal.PreFilteredContainerClassLoaderCreator;
 import org.mule.runtime.jpms.api.MuleContainerModule;
 import org.mule.runtime.module.artifact.api.classloader.ArtifactClassLoader;
 import org.mule.runtime.module.artifact.api.classloader.MuleArtifactClassLoader;
 import org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptor;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.List;
 import java.util.Set;
@@ -41,8 +47,7 @@ public class TestPreFilteredContainerClassLoaderCreator implements PreFilteredCo
     this.containerSystemClassloader = containerSystemClassloader;
     this.bootPackages = bootPackages;
 
-    final ContainerModuleDiscoverer moduleDiscoverer = new ContainerModuleDiscoverer(containerSystemClassloader);
-    moduleDiscoverer.addModuleDiscoverer(new ClasspathModuleDiscoverer(containerSystemClassloader, TEST_MODULE_PROPERTIES));
+    ModuleDiscoverer moduleDiscoverer = createContainerModuleDiscoverer(containerSystemClassloader);
     this.testContainerModuleRepository =
         new DefaultModuleRepository(new TestModuleDiscoverer(extraPrivilegedArtifacts,
                                                              moduleDiscoverer));
@@ -72,4 +77,136 @@ public class TestPreFilteredContainerClassLoaderCreator implements PreFilteredCo
                                                        getBootPackages()));
   }
 
+  private static ReflectionAdapterModuleDiscoverer createContainerModuleDiscoverer(ClassLoader containerSystemClassloader) {
+    try {
+      final Class<?> clsContainerModuleDiscoverer =
+          containerSystemClassloader.loadClass(ContainerModuleDiscoverer.class.getName());
+
+      final Object moduleDiscoverer = clsContainerModuleDiscoverer
+          .getConstructor(ClassLoader.class)
+          .newInstance(containerSystemClassloader);
+
+      final Class<?> clsClasspathModuleDiscoverer =
+          containerSystemClassloader.loadClass(ClasspathModuleDiscoverer.class.getName());
+      final Object classpathModuleDiscoverer = clsClasspathModuleDiscoverer
+          .getConstructor(ClassLoader.class, String.class)
+          .newInstance(containerSystemClassloader, TEST_MODULE_PROPERTIES);
+
+      final Class<?> clsModuleDiscoverer =
+          containerSystemClassloader.loadClass(ModuleDiscoverer.class.getName());
+      clsContainerModuleDiscoverer.getDeclaredMethod("addModuleDiscoverer", clsModuleDiscoverer)
+          .invoke(moduleDiscoverer, classpathModuleDiscoverer);
+
+      return new ReflectionAdapterModuleDiscoverer(containerSystemClassloader, moduleDiscoverer);
+    } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+        | NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+      throw new MuleRuntimeException(e);
+    }
+  }
+
+  private static final class ReflectionAdapterModuleDiscoverer implements ModuleDiscoverer {
+
+    private final ClassLoader containerSystemClassloader;
+    private final Object moduleDiscoverer;
+    private final Method methodDiscover;
+
+    public ReflectionAdapterModuleDiscoverer(ClassLoader containerSystemClassloader, Object moduleDiscoverer) {
+      this.containerSystemClassloader = containerSystemClassloader;
+      this.moduleDiscoverer = moduleDiscoverer;
+
+      try {
+        methodDiscover = containerSystemClassloader.loadClass(ModuleDiscoverer.class.getName())
+            .getDeclaredMethod("discover");
+      } catch (NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+        throw new MuleRuntimeException(e);
+      }
+    }
+
+    @Override
+    public List<MuleContainerModule> discover() {
+      try {
+        return ((List<?>) methodDiscover.invoke(moduleDiscoverer))
+            .stream()
+            .map(module -> new ReflectionAdapterMuleContainerModule(containerSystemClassloader, module))
+            .collect(toList());
+      } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+        throw new MuleRuntimeException(e);
+      }
+
+    }
+
+  }
+
+  public static final class ReflectionAdapterMuleContainerModule implements MuleContainerModule {
+
+    private final Object adaptedModule;
+
+    private final Method getName;
+    private final Method getExportedPackages;
+    private final Method getExportedPaths;
+    private final Method getPrivilegedExportedPackages;
+    private final Method getPrivilegedArtifacts;
+
+    public ReflectionAdapterMuleContainerModule(ClassLoader containerSystemClassloader, Object adaptedModule) {
+      this.adaptedModule = adaptedModule;
+
+      try {
+        final Class<?> clsMuleContainerModule = containerSystemClassloader.loadClass(MuleContainerModule.class.getName());
+
+        getName = clsMuleContainerModule.getMethod("getName");
+        getExportedPackages = clsMuleContainerModule.getMethod("getExportedPackages");
+        getExportedPaths = clsMuleContainerModule.getMethod("getExportedPaths");
+        getPrivilegedExportedPackages = clsMuleContainerModule.getMethod("getPrivilegedExportedPackages");
+        getPrivilegedArtifacts = clsMuleContainerModule.getMethod("getPrivilegedArtifacts");
+      } catch (NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+        throw new MuleRuntimeException(e);
+      }
+    }
+
+    @Override
+    public String getName() {
+      try {
+        return (String) getName.invoke(adaptedModule);
+      } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+        throw new MuleRuntimeException(e);
+      }
+    }
+
+    @Override
+    public Set<String> getExportedPackages() {
+      try {
+        return (Set<String>) getExportedPackages.invoke(adaptedModule);
+      } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+        throw new MuleRuntimeException(e);
+      }
+    }
+
+    @Override
+    public Set<String> getExportedPaths() {
+      try {
+        return (Set<String>) getExportedPaths.invoke(adaptedModule);
+      } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+        throw new MuleRuntimeException(e);
+      }
+    }
+
+    @Override
+    public Set<String> getPrivilegedExportedPackages() {
+      try {
+        return (Set<String>) getPrivilegedExportedPackages.invoke(adaptedModule);
+      } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+        throw new MuleRuntimeException(e);
+      }
+    }
+
+    @Override
+    public Set<String> getPrivilegedArtifacts() {
+      try {
+        return (Set<String>) getPrivilegedArtifacts.invoke(adaptedModule);
+      } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+        throw new MuleRuntimeException(e);
+      }
+    }
+
+  }
 }
