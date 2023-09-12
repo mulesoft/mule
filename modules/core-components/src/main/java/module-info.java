9de83e90936ed48e5f47b1c89c6456db1da7c5dc
Martín Buchwald <martin.buchwald@mulesoft.com>
2023-09-12 10:54:44 -0300

    Modularization for core-components

---
+++ modules/core-components/src/main/java/module-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+/**
+ * Core components implementatios for the Mule Runtime
+ *
+ * @moduleGraph
+ * @since 4.6
+ */
+module org.mule.runtime.core.components {
+
+    requires org.mule.runtime.api;
+    requires org.mule.runtime.core;
+    requires org.mule.runtime.extension.model;
+    requires org.mule.runtime.tracer.api;
+    requires org.mule.runtime.tracer.customization.api;
+
+    requires jakarta.activation;
+    requires java.xml;
+    requires org.apache.log4j;
+}
