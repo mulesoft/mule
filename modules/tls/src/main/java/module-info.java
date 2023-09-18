eb43c5a817e277ca04d5561d3dfe6e781f710e9b
Martín Buchwald <martin.buchwald@mulesoft.com>
2023-09-18 16:26:56 -0300

    W-14124345: Modularize TLS module (#12801)

---
+++ modules/tls/src/main/java/module-info.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+/**
+ * A Mule module for connectivity using TLS/SSL authentication.
+ *
+ * @moduleGraph
+ * @since 4.5
+ */
+module org.mule.runtime.tls {
+
+  requires org.mule.runtime.api;
+  requires org.mule.runtime.container;
+  requires org.mule.runtime.core;
+  requires org.mule.runtime.dsl.api;
+  requires org.mule.runtime.extension.model;
+
+  // QName
+  requires java.xml;
+  requires com.google.common;
+
+  exports org.mule.runtime.module.tls.api;
+
+  exports org.mule.runtime.module.tls.internal.config to
+      org.mule.runtime.spring.config,
+      spring.beans;
+
+  provides org.mule.runtime.api.tls.AbstractTlsContextFactoryBuilderFactory with
+      org.mule.runtime.module.tls.api.DefaultTlsContextFactoryBuilderFactory;
+
+  provides org.mule.runtime.core.api.extension.provider.RuntimeExtensionModelProvider with
+      org.mule.runtime.module.tls.api.extension.TlsRuntimeExtensionModelProvider;
+
+  provides org.mule.runtime.dsl.api.component.ComponentBuildingDefinitionProvider with
+      org.mule.runtime.module.tls.internal.config.TlsComponentBuildingDefinitionProvider;
+
+  provides org.mule.runtime.dsl.api.xml.XmlNamespaceInfoProvider with
+      org.mule.runtime.module.tls.internal.config.TlsXmlNamespaceInfoProvider;
+
+}
