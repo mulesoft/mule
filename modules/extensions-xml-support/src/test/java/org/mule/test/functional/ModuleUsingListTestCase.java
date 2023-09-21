75cdf9ca645dc205e9284fde78579ec5449018e8
Ayelén López Bizón <ayelen.lopez@mulesoft.com>
2023-09-21 15:58:41 -0300

    W-13681772: XML SDK fails to replace a complex parameter with a prope… (#12794)
    (cherry picked from commit 320449ad6e5431a7e306b1378191a7837eca550c)

---
+++ modules/extensions-xml-support/src/test/java/org/mule/test/functional/ModuleUsingListTestCase.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.functional;
+
+import static org.mule.test.allure.AllureConstants.XmlSdk.XML_SDK;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
+import org.mule.runtime.core.api.event.CoreEvent;
+
+import java.util.Collection;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import org.junit.Test;
+
+@Feature(XML_SDK)
+@Issue("W-13681772")
+public class ModuleUsingListTestCase extends AbstractCeXmlExtensionMuleArtifactFunctionalTestCase {
+
+  @Override
+  protected String getConfigFile() {
+    return "flows/flow-with-module-using-list.xml";
+  }
+
+  @Override
+  protected String[] getModulePaths() {
+    return new String[] {"modules/module-using-list.xml"};
+  }
+
+  @Test
+  public void testPetstoreWithList() throws Exception {
+    CoreEvent event = flowRunner("testPetstoreWithList").run();
+    Collection<String> pets = (Collection<String>) event.getMessage().getPayload().getValue();
+    assertThat(pets, containsInAnyOrder("Dog", "Cat"));
+  }
+}
