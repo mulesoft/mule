0792ecb789575608be84b9ebdb6e65ca03ed4430
Martín Buchwald <martin.buchwald@mulesoft.com>
2023-09-22 14:59:06 -0300

    wip

---
+++ modules/repository/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+/**
+ * Mule repository module. Provides access to external dependencies required by the runtime.
+ *
+ * @moduleGraph
+ * @since 4.6
+ */
+module org.mule.runtime.repository {
+
+    exports org.mule.runtime.module.repository.api;
+
+}
