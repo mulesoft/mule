634c21972a69db8395365a7126c63e9c494d8a44
Martín Buchwald <martin.buchwald@mulesoft.com>
2023-09-22 13:58:00 -0300

    W-14122558: moving ExtensionResourcesGeneratorAnnotationProcessor to API to solve module layer issues

--- modules/extensions-spring-support/src/main/java/org/mule/runtime/module/extension/api/resources/ExtensionResourcesGeneratorAnnotationProcessor.java
+++ modules/extensions-spring-support/src/main/java/org/mule/runtime/module/extension/api/resources/ExtensionResourcesGeneratorAnnotationProcessor.java
@@ -4,13 +4,15 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.runtime.module.extension.internal.resources;
+package org.mule.runtime.module.extension.api.resources;
 
 import static javax.lang.model.SourceVersion.RELEASE_8;
-import static org.mule.runtime.module.extension.internal.resources.BaseExtensionResourcesGeneratorAnnotationProcessor.EXTENSION_VERSION;
+import static org.mule.runtime.module.extension.api.resources.BaseExtensionResourcesGeneratorAnnotationProcessor.EXTENSION_VERSION;
 
 import org.mule.runtime.extension.api.loader.ExtensionModelLoadingRequest;
 import org.mule.runtime.extension.api.loader.ExtensionModelLoader;
+import org.mule.runtime.module.extension.api.resources.BaseExtensionResourcesGeneratorAnnotationProcessor;
+import org.mule.runtime.module.extension.api.resources.ClassExtensionResourcesGeneratorAnnotationProcessor;
 import org.mule.runtime.module.extension.internal.capability.xml.description.DescriptionDeclarationEnricher;
 import org.mule.runtime.module.extension.internal.resources.validator.ExportedPackagesValidator;
 
@@ -23,12 +25,10 @@ import javax.annotation.processing.SupportedSourceVersion;
  * {@link BaseExtensionResourcesGeneratorAnnotationProcessor} implementation for default java based extensions.
  *
  * @since 3.7.0
- * @deprecated Use org.mule.runtime.module.extension.api.ExtensionResourcesGeneratorAnnotationProcessor instead.
  */
 @SupportedAnnotationTypes(value = {"org.mule.runtime.extension.api.annotation.Extension"})
 @SupportedSourceVersion(RELEASE_8)
 @SupportedOptions(EXTENSION_VERSION)
-@Deprecated
 public class ExtensionResourcesGeneratorAnnotationProcessor extends ClassExtensionResourcesGeneratorAnnotationProcessor {
 
   @Override
