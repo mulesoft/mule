32ddd697682a55dd186d0b3751b7a509af4f9ffb
elrodro83 <rmerino@salesforce.com>
2023-09-22 08:59:30 -0300

    W-14154476: Add a fallback to use a classloader for service dependencies that cannot be loaded as modules

--- modules/service/src/main/java11/org/mule/runtime/module/service/api/artifact/ServiceModuleLayerFactory.java
+++ modules/service/src/main/java11/org/mule/runtime/module/service/api/artifact/ServiceModuleLayerFactory.java
@@ -11,22 +11,29 @@ import static org.mule.runtime.jpms.api.JpmsUtils.createModuleLayer;
 import static org.mule.runtime.jpms.api.JpmsUtils.openToModule;
 
 import static java.lang.Boolean.getBoolean;
-import static java.lang.System.getProperty;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
-import static java.util.Optional.of;
 import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.partitioningBy;
 
+import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.container.api.MuleContainerClassLoaderWrapper;
 import org.mule.runtime.module.artifact.api.classloader.ArtifactClassLoader;
 import org.mule.runtime.module.artifact.api.classloader.ClassLoaderLookupPolicy;
 import org.mule.runtime.module.artifact.api.classloader.MuleArtifactClassLoader;
 import org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptor;
 
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Paths;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,10 +45,15 @@ class ServiceModuleLayerFactory extends ServiceClassLoaderFactory {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ServiceModuleLayerFactory.class);
 
+  private static final Set<String> KNOWN_JPMS_INVALID_LIBS =
+      new HashSet<>(asList("parboiled_2.12",
+                           "scala-parser-combinators_2.12"));
+
   private static final Set<String> SERVICE_MODULE_NAME_PREFIXES =
       new HashSet<>(asList("org.mule.service.",
                            "com.mulesoft.mule.service.",
                            "com.mulesoft.anypoint.gw.service."));
+
   private static final String SCHEDULER_SERVICE_MODULE_NAME = "org.mule.service.scheduler";
 
   private static final String CLASSLOADER_SERVICE_JPMS_MODULE_LAYER_DATAWEAVE =
@@ -66,26 +78,45 @@ class ServiceModuleLayerFactory extends ServiceClassLoaderFactory {
   @Deprecated
   public ArtifactClassLoader create(String artifactId, ServiceDescriptor descriptor, ClassLoader parent,
                                     ClassLoaderLookupPolicy lookupPolicy) {
-    // TODO TD-0144818 remove this special case
-    if (artifactId.equals("service/DataWeave service")
-        && !getBoolean(getProperty(CLASSLOADER_SERVICE_JPMS_MODULE_LAYER_DATAWEAVE, "false"))) {
-      return new MuleArtifactClassLoader(artifactId, descriptor, descriptor.getClassLoaderConfiguration().getUrls(), parent,
-                                         lookupPolicy);
+    URL[] classLoaderConfigurationUrls = descriptor.getClassLoaderConfiguration().getUrls();
+
+    if (artifactId.equals("service/DataWeave service")) {
+      // TODO TD-0144818 remove this special case
+      if (!getBoolean(CLASSLOADER_SERVICE_JPMS_MODULE_LAYER_DATAWEAVE)) {
+        return new MuleArtifactClassLoader(artifactId, descriptor, descriptor.getClassLoaderConfiguration().getUrls(), parent,
+                                           lookupPolicy);
+      }
+
+      List<URL> serviceUrls = asList(classLoaderConfigurationUrls);
+
+      final Map<Boolean, List<URL>> serviceUrlsByJpmsValidity = Stream.of(classLoaderConfigurationUrls)
+          .collect(partitioningBy(url -> {
+            final String fileName;
+            try {
+              fileName = Paths.get(url.toURI()).getFileName().toString();
+            } catch (URISyntaxException e) {
+              throw new MuleRuntimeException(e);
+            }
+
+            return KNOWN_JPMS_INVALID_LIBS.stream().noneMatch(fileName::contains);
+          }));
+
+      parent = new URLClassLoader(serviceUrlsByJpmsValidity.get(false).toArray(URL[]::new), parent);
+      classLoaderConfigurationUrls = serviceUrlsByJpmsValidity.get(true).toArray(URL[]::new);
     }
 
     LOGGER.debug(" >> Creating ModuleLayer for service: '" + artifactId + "'...");
-    ModuleLayer artifactLayer = createModuleLayer(descriptor.getClassLoaderConfiguration().getUrls(), parent,
+    ModuleLayer artifactLayer = createModuleLayer(classLoaderConfigurationUrls, parent,
                                                   parentLayer, true, true);
 
-    String serviceModuleName = artifactLayer.modules()
+    final Module serviceModule = artifactLayer.modules()
         .stream()
         .filter(module -> SERVICE_MODULE_NAME_PREFIXES.stream().anyMatch(module.getName()::startsWith))
         .findAny()
-        // TODO TD-0144818 TD-0144819 TD-0144821 TD-0144822 TD-0144823 temporarily until all services are properly modularized,
-        // This should fail if services do not have the proper module name
-        .orElse(artifactLayer.modules().iterator().next())
-        .getName();
+        .orElseThrow(() -> new NoSuchElementException("No module with an expected prefix (" + SERVICE_MODULE_NAME_PREFIXES
+            + ") for '" + artifactId + "'"));
 
+    String serviceModuleName = serviceModule.getName();
     if (serviceModuleName.equals(SCHEDULER_SERVICE_MODULE_NAME)) {
       openToModule(artifactLayer,
                    serviceModuleName,
