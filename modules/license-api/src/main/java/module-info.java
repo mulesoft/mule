256f8f86a86681472a817e52e83992fd66ddf100
Martín Buchwald <martin.buchwald@mulesoft.com>
2023-09-22 14:32:57 -0300

    W-14145973: Modularize license-api module

---
+++ modules/license-api/src/main/java/module-info.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+/**
+ * This module provides an API for license verification.
+ *
+ * @moduleGraph
+ * @since 4.6
+ */
+module org.mule.runtime.license.api {
+
+  exports org.mule.runtime.module.license.api;
+  exports org.mule.runtime.module.license.api.exception;
+
+  uses org.mule.runtime.module.license.api.LicenseValidator;
+
+  provides org.mule.runtime.module.license.api.LicenseValidator with
+      org.mule.runtime.module.license.internal.DefaultLicenseValidator;
+
+}
