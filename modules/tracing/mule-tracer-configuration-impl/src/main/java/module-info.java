baf62f37c854a1933cac48bd6e6a687010296760
rbourbonnavarro <rodrigobourbon44@gmail.com>
2023-09-27 14:21:27 -0300

    W-14146047: Modularize `mule-tracer-configuration-api` and `mule-tracer-configuration-impl` modules (#12844)

---
+++ modules/tracing/mule-tracer-configuration-impl/src/main/java/module-info.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+/**
+ * Implementation to configure tracing generation.
+ *
+ * @moduleGraph
+ * @since 4.6
+ */
+module org.mule.runtime.tracer.configuration.impl {
+
+  requires org.mule.runtime.api;
+  requires org.mule.runtime.container;
+  requires org.mule.runtime.core;
+  requires org.mule.runtime.properties.config;
+  requires org.mule.runtime.tracer.common;
+  requires org.mule.runtime.tracer.configuration.api;
+  requires org.mule.runtime.tracer.exporter.configuration.api;
+
+  requires com.fasterxml.jackson.databind;
+  requires com.fasterxml.jackson.dataformat.yaml;
+  requires java.inject;
+  requires org.slf4j;
+
+  exports org.mule.runtime.tracing.level.impl.config to
+      org.mule.runtime.spring.config,
+      spring.beans;
+
+}
