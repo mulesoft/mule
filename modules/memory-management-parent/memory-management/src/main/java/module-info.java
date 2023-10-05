4dfb24ba7445ca8015b07df8c866c4de64119475
Martín Buchwald <martin.buchwald@mulesoft.com>
2023-10-05 15:09:18 -0300

    W-14145992: Modularize memory-management module (#12885)

---
+++ modules/memory-management-parent/memory-management/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+/**
+ * This module performs memory management for the runtime.
+ *
+ * @moduleGraph
+ * @since 4.6
+ */
+module org.mule.runtime.memory.management {
+
+  requires org.mule.runtime.api;
+  requires org.mule.runtime.profiling.api;
+
+  exports org.mule.runtime.internal.memory.management to
+      org.mule.runtime.spring.config;
+}
