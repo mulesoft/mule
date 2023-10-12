03fd22f7168849415248828cd7d3ffc289240c99
Rodrigo Merino <elrodro83@users.noreply.github.com>
2023-10-12 16:57:50 -0300

    W-13829786: Avoid reading mule-module.properties for modules with module-info (#12901)

---
+++ modules/container/src/main/java11/org/mule/runtime/container/internal/JpmsModuleLayerModuleDiscoverer.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.container.internal;
+
+import static org.mule.runtime.api.util.MuleSystemProperties.classloaderContainerJpmsModuleLayer;
+
+import static java.lang.module.ModuleDescriptor.Requires.Modifier.TRANSITIVE;
+import static java.util.Collections.emptySet;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Stream.concat;
+
+import org.mule.api.annotation.jpms.PrivilegedApi;
+import org.mule.runtime.jpms.api.MuleContainerModule;
+
+import java.io.InputStream;
+import java.lang.module.ModuleDescriptor.Exports;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Stream;
+
+/**
+ * Discovers {@link MuleContainerModule} from java module descriptors.
+ * 
+ * @since 4.6
+ */
+public class JpmsModuleLayerModuleDiscoverer implements ModuleDiscoverer {
+
+  private final ClasspathModuleDiscoverer fallbackClasspathModuleDiscoverer;
+
+  public JpmsModuleLayerModuleDiscoverer(ClasspathModuleDiscoverer fallbackClasspathModuleDiscoverer) {
+    this.fallbackClasspathModuleDiscoverer = fallbackClasspathModuleDiscoverer;
+  }
+
+  @Override
+  public List<MuleContainerModule> discover() {
+    if (!classloaderContainerJpmsModuleLayer()) {
+      return fallbackClasspathModuleDiscoverer.discover();
+    }
+
+    return this.getClass().getModule().getLayer().modules()
+        .stream()
+        .map(jpmsModule -> {
+          if (jpmsModule.getDescriptor().isAutomatic()) {
+            // fallback
+            try (InputStream stream =
+                jpmsModule.getResourceAsStream("/" + fallbackClasspathModuleDiscoverer.getModulePropertiesFileName())) {
+              final Properties moduleProperties = new Properties();
+              if (stream != null) {
+                moduleProperties.load(stream);
+              } else {
+                moduleProperties.put("module.name", jpmsModule.getName());
+              }
+              return fallbackClasspathModuleDiscoverer.createModule(moduleProperties);
+            } catch (Exception e) {
+              throw new RuntimeException("Cannot create fallback muleContainer module for '" + jpmsModule.getName() + "'", e);
+            }
+          } else {
+            return new JpmsMuleContainerModule(jpmsModule);
+          }
+        })
+        .collect(toList());
+  }
+
+
+  public class JpmsMuleContainerModule implements MuleContainerModule {
+
+    private final Module jpmsModule;
+
+    private final Set<String> exportedPackages;
+    private final Set<String> privilegedExportedPackages;
+    private final Set<String> privilegedArtifacts;
+
+    public JpmsMuleContainerModule(Module jpmsModule) {
+      this.jpmsModule = jpmsModule;
+
+      this.exportedPackages = concat(
+                                     // direct exports
+                                     jpmsModule.getDescriptor().exports().stream()
+                                         .filter(export -> export.targets().isEmpty())
+                                         .map(Exports::source),
+                                     // transitiveExportedPackages
+                                     jpmsModule.getDescriptor().requires()
+                                         .stream()
+                                         .filter(required -> required.modifiers().contains(TRANSITIVE))
+                                         .flatMap(required -> {
+                                           final Module requiredTransitiveModule =
+                                               this.getClass().getModule().getLayer().findModule(required.name()).get();
+
+                                           if (requiredTransitiveModule.getDescriptor().isAutomatic()) {
+                                             return requiredTransitiveModule.getPackages().stream();
+                                           } else {
+                                             return requiredTransitiveModule.getDescriptor().exports().stream()
+                                                 .filter(export -> export.targets().isEmpty())
+                                                 .map(Exports::source);
+                                           }
+                                         }))
+                                             .collect(toSet());
+
+      if (jpmsModule.isAnnotationPresent(PrivilegedApi.class)) {
+        final PrivilegedApi privilegedApiAnnotation = jpmsModule.getAnnotation(PrivilegedApi.class);
+        this.privilegedExportedPackages = Stream.of(privilegedApiAnnotation.privilegedPackages())
+            .collect(toSet());
+        this.privilegedArtifacts = Stream.of(privilegedApiAnnotation.privilegedArtifactIds())
+            .collect(toSet());
+      } else {
+        this.privilegedExportedPackages = emptySet();
+        this.privilegedArtifacts = emptySet();
+      }
+    }
+
+    @Override
+    public String getName() {
+      return jpmsModule.getName();
+    }
+
+    @Override
+    public Set<String> getExportedPackages() {
+      return this.exportedPackages;
+    }
+
+    @Override
+    public Set<String> getExportedPaths() {
+      return emptySet();
+    }
+
+    @Override
+    public Set<String> getPrivilegedExportedPackages() {
+      return this.privilegedExportedPackages;
+    }
+
+    @Override
+    public Set<String> getPrivilegedArtifacts() {
+      return this.privilegedArtifacts;
+    }
+
+  }
+
+}
