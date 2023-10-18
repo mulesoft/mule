08b79230a74f0ef2fb2948d810fb99b34493f01f
Alejandro Curci <acurci@mulesoft.com>
2023-10-18 06:43:52 -0300

    W-13038823: Refactor for reducing cognitive complexity

--- modules/artifact/src/main/java/org/mule/module/artifact/classloader/GroovyResourceReleaser.java
+++ modules/artifact/src/main/java/org/mule/module/artifact/classloader/GroovyResourceReleaser.java
@@ -13,6 +13,7 @@ import static org.mule.runtime.core.api.util.ClassUtils.loadClass;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import org.mule.runtime.module.artifact.api.classloader.ResourceReleaser;
+import org.mule.sdk.api.artifact.lifecycle.ArtifactLifecycleListener;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -27,13 +28,17 @@ import org.slf4j.Logger;
  * A utility class to release all resources associated to Groovy Dependency on un-deployment to prevent classloader leaks.
  *
  * @since 4.4.0
+ * @deprecated Since 4.5.0, this releaser has been deprecated in favor of an {@link ArtifactLifecycleListener} in the extensions
+ *             that are using the IBM MQ driver. We still keep it to support legacy extensions.
  */
+@Deprecated
 public class GroovyResourceReleaser implements ResourceReleaser {
 
   private final ClassLoader classLoader;
   private static final Logger LOGGER = getLogger(GroovyResourceReleaser.class);
   private static final String GROOVY_CLASS_INFO = "org.codehaus.groovy.reflection.ClassInfo";
   private static final String GROOVY_INVOKER_HELPER = "org.codehaus.groovy.runtime.InvokerHelper";
+
   private static final String GROOVY_SCRIPT_ENGINE_FACTORY = "org.codehaus.groovy.jsr223.GroovyScriptEngineFactory";
   private static final String LOGGER_ABSTRACT_MANAGER = "org.apache.logging.log4j.core.appender.AbstractManager";
   private static final String LOGGER_STREAM_MANAGER = "org.apache.logging.log4j.core.appender.OutputStreamManager";
@@ -56,40 +61,42 @@ public class GroovyResourceReleaser implements ResourceReleaser {
 
   private void unregisterInvokerHelper() {
     try {
-      Class classInfoClass = this.classLoader.loadClass(GROOVY_CLASS_INFO);
+      Class<?> classInfoClass = this.classLoader.loadClass(GROOVY_CLASS_INFO);
       Method getAllClassInfoMethod = classInfoClass.getMethod("getAllClassInfo");
       Method getTheClassMethod = classInfoClass.getMethod("getTheClass");
-      Class invokerHelperClass = this.classLoader.loadClass(GROOVY_INVOKER_HELPER);
+      Class<?> invokerHelperClass = this.classLoader.loadClass(GROOVY_INVOKER_HELPER);
       Method removeClassMethod = invokerHelperClass.getMethod("removeClass", Class.class);
       Object classes = getAllClassInfoMethod.invoke(null);
-      if (classes != null && classes instanceof Collection) {
-        for (Object classInfo : ((Collection) classes)) {
-          Object clazz = null;
-          try {
-            clazz = getTheClassMethod.invoke(classInfo);
-            removeClassMethod.invoke(null, clazz);
-          } catch (IllegalAccessException | InvocationTargetException | ClassCastException e) {
-            String className = clazz instanceof Class ? ((Class) clazz).getName() : "Unknown";
-            LOGGER.warn("Could not remove the {} class from the Groovy's InvokerHelper", className, e);
-          }
-        }
+      if (classes instanceof Collection) {
+        unregisterInvokerHelperRemoveClassMethod(removeClassMethod, getTheClassMethod, (Collection) classes);
       }
     } catch (IllegalAccessException | InvocationTargetException | ClassNotFoundException | NoSuchMethodException e) {
       LOGGER.warn("Error trying to remove the Groovy's InvokerHelper classes", e);
     }
   }
 
+  private void unregisterInvokerHelperRemoveClassMethod(Method removeClassMethod, Method getTheClassMethod, Collection<?> classes) {
+    for (Object classInfo : classes) {
+      Object clazz = null;
+      try {
+        clazz = getTheClassMethod.invoke(classInfo);
+        removeClassMethod.invoke(null, clazz);
+      } catch (IllegalAccessException | InvocationTargetException | ClassCastException e) {
+        String className = clazz instanceof Class ? ((Class) clazz).getName() : "Unknown";
+        LOGGER.warn("Could not remove the {} class from the Groovy's InvokerHelper", className, e);
+      }
+    }
+  }
+
   private void cleanSpisEngines() {
     try {
       Class<?> abstractManager = loadClass(LOGGER_ABSTRACT_MANAGER, this.classLoader);
-      HashMap hashMap = getStaticFieldValue(abstractManager, "MAP", true);
-      Iterator it = hashMap.values().iterator();
+      HashMap<?, ?> hashMap = getStaticFieldValue(abstractManager, "MAP", true);
       Class<?> streamManagerClass = loadClass(LOGGER_STREAM_MANAGER, this.classLoader);
-      Object rfmInstance = null;
-      while (it.hasNext()) {
-        Object o = it.next();
-        if (streamManagerClass.isInstance(o)) {
-          rfmInstance = o;
+      Object rfmInstance;
+      for (Object manager : hashMap.values()) {
+        if (streamManagerClass.isInstance(manager)) {
+          rfmInstance = manager;
           Object layout = getFieldValue(rfmInstance, "layout", true);
           Object configuration = getFieldValue(layout, "configuration", true);
 
@@ -97,16 +104,7 @@ public class GroovyResourceReleaser implements ResourceReleaser {
           Method getScriptManagerMethod = configurationClass.getMethod("getScriptManager");
           Object scriptManager = getScriptManagerMethod.invoke(configuration);
           if (scriptManager != null) {
-            Object manager = getFieldValue(scriptManager, "manager", true);
-            HashSet engineSpis = getFieldValue(manager, "engineSpis", true);
-            Class groovy = loadClass(GROOVY_SCRIPT_ENGINE_FACTORY, this.classLoader);
-            Iterator engineSpisIterator = engineSpis.iterator();
-            while (engineSpisIterator.hasNext()) {
-              Object i = engineSpisIterator.next();
-              if (groovy.isInstance(i) && i.getClass().getClassLoader().equals(groovy.getClassLoader())) {
-                engineSpis.remove(i);
-              }
-            }
+            cleanGroovyEngines(scriptManager);
           }
         }
       }
@@ -114,5 +112,22 @@ public class GroovyResourceReleaser implements ResourceReleaser {
         | IllegalAccessException e) {
       LOGGER.warn("Error trying to unregister the Groovy's Scripting Engine", e);
     }
+  }
+
+  private void cleanGroovyEngines(Object scriptManager) throws IllegalAccessException, NoSuchFieldException, ClassNotFoundException {
+    Object manager = getFieldValue(scriptManager, "manager", true);
+    HashSet<?> engineSpis = getFieldValue(manager, "engineSpis", true);
+    Class<?> groovy = loadClass(GROOVY_SCRIPT_ENGINE_FACTORY, this.classLoader);
+    Iterator<?> engineSpisIterator = engineSpis.iterator();
+    while (engineSpisIterator.hasNext()) {
+      Object engine = engineSpisIterator.next();
+      if (isGroovyScriptEngine(groovy, engine)) {
+        engineSpisIterator.remove();
+      }
+    }
+  }
+
+  private boolean isGroovyScriptEngine(Class<?> groovy, Object engine) {
+    return groovy.isInstance(engine) && engine.getClass().getClassLoader().equals(groovy.getClassLoader());
   }
 }
