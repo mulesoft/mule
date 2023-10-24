96735f5d710c87c12999a8215d361c1961908080
elrodro83 <rmerino@salesforce.com>
2023-10-24 15:21:01 -0300

    W-14357577: Provide an API for the GW to obtain a PolicyTemplateDescriptorFactory

--- modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/artifact/DefaultArtifactDescriptorFactoryProvider.java
+++ modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/artifact/DefaultArtifactDescriptorFactoryProvider.java
@@ -8,8 +8,10 @@ package org.mule.runtime.module.deployment.impl.internal.artifact;
 
 import org.mule.runtime.api.deployment.meta.MuleApplicationModel;
 import org.mule.runtime.api.deployment.meta.MulePluginModel;
+import org.mule.runtime.api.deployment.meta.MulePolicyModel;
 import org.mule.runtime.deployment.model.api.artifact.ArtifactDescriptorFactoryProvider;
 import org.mule.runtime.deployment.model.api.plugin.resolver.PluginDependenciesResolver;
+import org.mule.runtime.deployment.model.api.policy.PolicyTemplateDescriptor;
 import org.mule.runtime.module.artifact.api.descriptor.AbstractArtifactDescriptorFactory;
 import org.mule.runtime.module.artifact.api.descriptor.ApplicationDescriptor;
 import org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptorFactory;
@@ -20,6 +22,7 @@ import org.mule.runtime.module.deployment.impl.internal.application.ApplicationD
 import org.mule.runtime.module.deployment.impl.internal.plugin.ArtifactPluginDescriptorFactory;
 import org.mule.runtime.module.deployment.impl.internal.plugin.ArtifactPluginDescriptorLoader;
 import org.mule.runtime.module.deployment.impl.internal.plugin.BundlePluginDependenciesResolver;
+import org.mule.runtime.module.deployment.impl.internal.policy.PolicyTemplateDescriptorFactory;
 
 /**
  * Implementation of {@link ArtifactDescriptorFactoryProvider} discoverable through SPI.
@@ -46,5 +49,10 @@ public class DefaultArtifactDescriptorFactoryProvider implements ArtifactDescrip
   public AbstractArtifactDescriptorFactory<MulePluginModel, ArtifactPluginDescriptor> createArtifactPluginDescriptorFactory(DescriptorLoaderRepository descriptorLoaderRepository,
                                                                                                                             ArtifactDescriptorValidatorBuilder artifactDescriptorValidatorBuilder) {
     return new ArtifactPluginDescriptorFactory(descriptorLoaderRepository, artifactDescriptorValidatorBuilder);
+  }
+
+  @Override
+  public AbstractArtifactDescriptorFactory<MulePolicyModel, PolicyTemplateDescriptor> createPolicyTemplateDescriptorFactory() {
+    return new PolicyTemplateDescriptorFactory();
   }
 }
