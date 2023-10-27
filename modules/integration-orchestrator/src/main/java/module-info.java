8ac5d552e1cac73e362f43e7effe3537fdcdad94
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2023-10-27 18:58:01 +0200

    rebase fixes

---
+++ modules/integration-orchestrator/src/main/java/module-info.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+/**
+ * Mule service that allows to create HTTP servers and clients.
+ *
+ * @moduleGraph
+ * @since 4.5
+ */
+module org.mule.runtime.integration.orchestrator {
+
+  requires transitive org.mule.runtime.api;
+  requires org.mule.runtime.core;
+  requires org.mule.runtime.artifact;
+  requires org.mule.runtime.artifact.ast.serialization;
+  requires org.mule.runtime.deployment.model.impl;
+  requires org.mule.runtime.deployment;
+
+  requires semver4j;
+
+
+  
+}
