06a2b60f29809ac06ad91d2fad497d88b9e6cef1
Axel Sanguinetti <asanguinetti@mulesoft.com>
2023-12-26 21:25:45 -0300

    W-14658876: Migrating DeploymentDirectoryWatcher to a Scheduler from the SchedulerService. (#13114)

--- modules/deployment/src/test/java/org/mule/runtime/module/deployment/test/internal/MuleDeploymentServiceTestCase.java
+++ modules/deployment/src/test/java/org/mule/runtime/module/deployment/test/internal/MuleDeploymentServiceTestCase.java
@@ -6,6 +6,19 @@
  */
 package org.mule.runtime.module.deployment.test.internal;
 
+import static java.lang.Thread.MIN_PRIORITY;
+import static java.util.Optional.of;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.mule.runtime.api.scheduler.Scheduler;
+import org.mule.runtime.api.scheduler.SchedulerConfig;
 import org.mule.runtime.api.scheduler.SchedulerService;
 import org.mule.runtime.module.deployment.impl.internal.application.DefaultApplicationFactory;
 import org.mule.runtime.module.deployment.impl.internal.domain.DefaultDomainFactory;
@@ -15,10 +28,13 @@ import org.mule.tck.junit4.AbstractMuleTestCase;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
@@ -27,6 +43,9 @@ public class MuleDeploymentServiceTestCase extends AbstractMuleTestCase {
 
   @Rule
   public MockitoRule rule = MockitoJUnit.rule();
+
+  @Rule
+  public TemporaryFolder artifactsDir = new TemporaryFolder();
 
   private static final int NUMBER_OF_LISTENER_SUBSCRIBERS = 0;
 
@@ -37,10 +56,12 @@ public class MuleDeploymentServiceTestCase extends AbstractMuleTestCase {
   DefaultApplicationFactory applicationFactory;
 
   @Mock
-  Supplier<SchedulerService> schedulerServiceSupplier;
+  SchedulerService schedulerService;
 
   @Mock
   DeploymentExecutor deploymentExecutor;
+
+  Supplier<SchedulerService> schedulerServiceSupplier = () -> schedulerService;
 
   @Test
   public void startupListenersAddDoesNotResultInConcurrentException() throws Exception {
@@ -61,6 +82,26 @@ public class MuleDeploymentServiceTestCase extends AbstractMuleTestCase {
     for (Thread listenerSubscriber : listenersSubscribers) {
       listenerSubscriber.join();
     }
+  }
+
+  @Test
+  public void deploymentMonitorUsesSchedulerServiceWithMinPriority() {
+    // Minimal mocking so MuleDeploymentService#start succeeds
+    Scheduler mockScheduler = mock(Scheduler.class);
+    when(schedulerService.customScheduler(any())).thenReturn(mockScheduler);
+    when(domainFactory.getArtifactDir()).thenReturn(artifactsDir.getRoot());
+    when(applicationFactory.getArtifactDir()).thenReturn(artifactsDir.getRoot());
+
+    MuleDeploymentService deploymentService =
+        new MuleDeploymentService(domainFactory, applicationFactory, schedulerServiceSupplier, deploymentExecutor);
+
+    // This triggers the periodic scheduling of the DeploymentDirectoryWatcher
+    deploymentService.start();
+    deploymentService.stop();
+
+    ArgumentCaptor<SchedulerConfig> schedulerConfigCaptor = forClass(SchedulerConfig.class);
+    verify(schedulerService).customScheduler(schedulerConfigCaptor.capture());
+    assertThat(schedulerConfigCaptor.getValue().getPriority(), is(of(MIN_PRIORITY)));
   }
 
   private static class ListenerSubscriber implements Runnable {
