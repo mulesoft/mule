97aa552e9171427d4e46259f7620d7ca4c19b3a3
Martín Buchwald <martin.buchwald@mulesoft.com>
2024-01-17 17:27:35 -0300

    W-14608096: Add Transaction Error Mapping when it's not available by the runtime

--- core/src/main/java/org/mule/runtime/core/internal/exception/ErrorTypeLocatorFactory.java
+++ core/src/main/java/org/mule/runtime/core/internal/exception/ErrorTypeLocatorFactory.java
@@ -6,6 +6,9 @@
  */
 package org.mule.runtime.core.internal.exception;
 
+import static org.mule.runtime.api.meta.model.error.ErrorModelBuilder.newError;
+import static org.mule.runtime.ast.internal.error.ErrorTypeBuilder.builder;
+import static org.mule.runtime.core.api.error.Errors.ComponentIdentifiers.Handleable.ANY;
 import static org.mule.runtime.core.api.error.Errors.ComponentIdentifiers.Handleable.CLIENT_SECURITY;
 import static org.mule.runtime.core.api.error.Errors.ComponentIdentifiers.Handleable.COMPOSITE_ROUTING;
 import static org.mule.runtime.core.api.error.Errors.ComponentIdentifiers.Handleable.CONNECTIVITY;
@@ -91,9 +94,16 @@ public class ErrorTypeLocatorFactory {
             .addExceptionMapping(StreamingBufferSizeExceededException.class,
                                  errorTypeRepository.lookupErrorType(STREAM_MAXIMUM_SIZE_EXCEEDED).get())
             .addExceptionMapping(MuleFatalException.class, errorTypeRepository.getErrorType(FATAL).get())
-            .addExceptionMapping(TransactionException.class, errorTypeRepository.lookupErrorType(TRANSACTION).get())
+            .addExceptionMapping(TransactionException.class,
+                                 errorTypeRepository.lookupErrorType(TRANSACTION)
+                                     .orElseGet(ErrorTypeLocatorFactory::createTransactionError))
             .build())
         .defaultError(unknown)
         .build();
+  }
+
+  private static ErrorType createTransactionError() {
+    ErrorType anyError = builder().namespace(ANY.getNamespace()).identifier(ANY.getName()).build();
+    return builder().namespace(TRANSACTION.getNamespace()).identifier(TRANSACTION.getName()).parentErrorType(anyError).build();
   }
 }
