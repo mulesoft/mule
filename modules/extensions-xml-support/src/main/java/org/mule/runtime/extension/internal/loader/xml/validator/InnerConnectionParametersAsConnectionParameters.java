898537761b36f47b12474f7df2c39714f97d2737
elrodro83 <rmerino@salesforce.com>
2024-02-05 13:14:42 -0300

    Revert "cleanup"
    This reverts commit 7cb1d3637bfaa54a214f7c0b61c9a5ad01523eb3.

---
+++ modules/extensions-xml-support/src/main/java/org/mule/runtime/extension/internal/loader/xml/validator/InnerConnectionParametersAsConnectionParameters.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.extension.internal.loader.xml.validator;
+
+import org.mule.runtime.api.meta.model.ExtensionModel;
+import org.mule.runtime.extension.api.loader.ExtensionModelValidator;
+import org.mule.runtime.extension.api.loader.ProblemsReporter;
+
+public class InnerConnectionParametersAsConnectionParameters implements ExtensionModelValidator {
+
+  @Override
+  public void validate(ExtensionModel extensionModel, ProblemsReporter problemsReporter) {
+    // TODO fill me
+  }
+}
