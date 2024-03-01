0854ca88b2c2fa36fb79df05fed0d25a9c667890
Ezequiel Werner <eze210@gmail.com>
2024-03-01 15:56:37 -0300

    Fix and test

--- modules/deployment/src/main/java/org/mule/runtime/module/deployment/internal/DefaultArchiveDeployer.java
+++ modules/deployment/src/main/java/org/mule/runtime/module/deployment/internal/DefaultArchiveDeployer.java
@@ -493,8 +493,7 @@ public class DefaultArchiveDeployer<D extends DeployableArtifactDescriptor, T ex
                                       artifact.getArtifactName())));
         logger.error(t.getMessage(), t);
       } else {
-        logger.info(miniSplash(format("Failed to deploy artifact '%s', %s", artifact.getArtifactName(),
-                                      t.getCause().getMessage())));
+        logger.info(miniSplash(format("Failed to deploy artifact '%s', %s", artifact.getArtifactName(), getCauseMessage(t))));
         logger.error(t.getMessage(), t);
       }
 
@@ -506,6 +505,15 @@ public class DefaultArchiveDeployer<D extends DeployableArtifactDescriptor, T ex
       } else {
         throw new DeploymentException(createStaticMessage("Failed to deploy artifact: " + artifact.getArtifactName()), t);
       }
+    }
+  }
+
+  private static String getCauseMessage(Throwable t) {
+    Throwable cause = t.getCause();
+    if (cause != null) {
+      return cause.getMessage();
+    } else {
+      return null;
     }
   }
 
