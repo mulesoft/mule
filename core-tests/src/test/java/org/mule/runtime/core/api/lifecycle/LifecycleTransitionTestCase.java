62b4ce3f7e940932a343c85662e26c11202fbee5
Rodrigo Merino <elrodro83@users.noreply.github.com>
2024-03-06 14:36:23 -0300

    W-15183178: Encapsulate privileged lifecycle/execution packages (#13281)

--- core-tests/src/test/java/org/mule/runtime/core/api/lifecycle/LifecycleTransitionTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/api/lifecycle/LifecycleTransitionTestCase.java
@@ -16,8 +16,8 @@ import org.mule.runtime.api.lifecycle.Lifecycle;
 import org.mule.runtime.api.lifecycle.LifecycleException;
 import org.mule.runtime.api.lifecycle.Startable;
 import org.mule.runtime.api.lifecycle.Stoppable;
+import org.mule.runtime.core.internal.lifecycle.AbstractLifecycleManager;
 import org.mule.runtime.core.internal.lifecycle.phases.NotInLifecyclePhase;
-import org.mule.runtime.core.privileged.lifecycle.AbstractLifecycleManager;
 import org.mule.tck.junit4.AbstractMuleTestCase;
 
 import org.junit.Test;
