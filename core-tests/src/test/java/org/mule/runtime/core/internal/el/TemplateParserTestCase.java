f2739554d56d31a4d048fb2ffad18781051f95e1
Akshay Sonvane <akshay.sonvane93@gmail.com>
2024-03-14 16:50:24 -0700

    W-15141905: Fix template parser validation

--- core-tests/src/test/java/org/mule/runtime/core/internal/el/TemplateParserTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/el/TemplateParserTestCase.java
@@ -25,6 +25,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import io.qameta.allure.Issue;
 import org.junit.Test;
 
 @SmallTest
@@ -316,6 +317,23 @@ public class TemplateParserTestCase extends AbstractMuleTestCase {
     assertTrue(tp.isValid("#['']"));
     assertFalse(tp.isValid("#[']"));
     assertFalse(tp.isValid("#[']'"));
+  }
+
+  @Test
+  @Issue("W-15141905")
+  public void muleParserShouldValidateEscapedCharacters() {
+    TemplateParser tp = createMuleStyleParser();
+
+    assertTrue(tp.isValid("#[\"\\test\\\"]"));
+    assertTrue(tp.isValid("#['\\test\\']"));
+    assertTrue(tp.isValid("#[\"\\test\\\"\"]"));
+    assertTrue(tp.isValid("#['\\test\\'']"));
+    assertTrue(tp.isValid("#[\\\"\\test\\\\\"]"));
+    assertTrue(tp.isValid("#[\\'\\test\\\\']"));
+    assertTrue(tp.isValid("#[abc\\\"def]"));
+    assertTrue(tp.isValid("#[abc\\'def]"));
+    assertTrue(tp.isValid("\\\\#["));
+    assertTrue(tp.isValid("\\\\#['"));
   }
 
   @Test
