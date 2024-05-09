c15f27d515aad7f0b0656ab98921cba127fc2626
Axel Sanguinetti <asanguinetti@mulesoft.com>
2024-05-09 17:54:18 -0300

    W-15542604: Opening stats packages to kryo.shaded for serialization (#13459)
    (cherry picked from commit 8fb37a5d19bf19f5ff8d4be42db7f8d91628d2d4)

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -567,6 +567,8 @@ module org.mule.runtime.core {
       spring.core;
   opens org.mule.runtime.core.api.context.notification to
       kryo.shaded;
+  opens org.mule.runtime.core.api.management.stats to
+      kryo.shaded;
   opens org.mule.runtime.core.api.processor to
       spring.core;
   opens org.mule.runtime.core.api.retry.policy to
@@ -618,6 +620,8 @@ module org.mule.runtime.core {
   opens org.mule.runtime.core.internal.lock to
       org.mule.runtime.core.components,
       spring.core;
+  opens org.mule.runtime.core.internal.management.stats to
+      kryo.shaded;
   opens org.mule.runtime.core.internal.message to
       kryo.shaded;
   opens org.mule.runtime.core.internal.policy to
---
+++ tests/unit/src/main/java/org/mule/tck/core/serialization/IsNotSerializableTestObjectWithNameMatcher.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.tck.core.serialization;
+
+import static java.lang.String.format;
+
+import static org.hamcrest.core.Is.is;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+/**
+ * Verifies that a {@link NotSerializableTestObject} has the expected name.
+ *
+ * @since 4.6.4
+ */
+public class IsNotSerializableTestObjectWithNameMatcher extends TypeSafeMatcher<NotSerializableTestObject> {
+
+  private final Matcher<String> expectedNameMatcher;
+
+  private IsNotSerializableTestObjectWithNameMatcher(Matcher<String> expectedValueMatcher) {
+    this.expectedNameMatcher = expectedValueMatcher;
+  }
+
+  @Override
+  protected boolean matchesSafely(NotSerializableTestObject item) {
+    return expectedNameMatcher.matches(item.getName());
+  }
+
+  @Override
+  public void describeTo(Description description) {
+    description.appendText(format("a %s with name ", NotSerializableTestObject.class.getName()));
+    description.appendDescriptionOf(expectedNameMatcher);
+  }
+
+  @Override
+  protected void describeMismatchSafely(NotSerializableTestObject item, Description mismatchDescription) {
+    mismatchDescription.appendText(format("got a %s with a name that ", item.getClass().getName()));
+    expectedNameMatcher.describeMismatch(item.getName(), mismatchDescription);
+  }
+
+  public static IsNotSerializableTestObjectWithNameMatcher aNotSerializableTestObjectWithName(Matcher<String> nameMatcher) {
+    return new IsNotSerializableTestObjectWithNameMatcher(nameMatcher);
+  }
+
+  public static IsNotSerializableTestObjectWithNameMatcher aNotSerializableTestObjectWithName(String name) {
+    return new IsNotSerializableTestObjectWithNameMatcher(is(name));
+  }
+}
