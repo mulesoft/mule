c6bb16343a4354c62f285fb4527661c790c8d9bb
Axel Sanguinetti <asanguinetti@mulesoft.com>
2024-05-17 10:25:05 -0300

    Fix package according to this branch.

--- core/src/main/java/org/mule/runtime/core/privileged/util/TemplateParserToken.java
+++ core/src/main/java/org/mule/runtime/core/privileged/util/TemplateParserToken.java
@@ -4,7 +4,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.runtime.core.internal.el;
+package org.mule.runtime.core.privileged.util;
 
 import static java.lang.Integer.MAX_VALUE;
 import static java.lang.String.format;
