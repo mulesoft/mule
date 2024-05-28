c0d8a2fe8cadcb0a33a91bf28df7aacd3aa748e0
Ivan Fritzler <ifritzler@salesforce.com>
2024-05-28 11:33:30 -0300

    WIP

--- modules/metrics/mule-metrics-exporter-impl/src/main/java/org/mule/runtime/metrics/exporter/impl/OpenTelemetryMeterExporter.java
+++ modules/metrics/mule-metrics-exporter-impl/src/main/java/org/mule/runtime/metrics/exporter/impl/OpenTelemetryMeterExporter.java
@@ -25,7 +25,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
-import java.util.Objects;
 
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.metrics.LongCounterBuilder;
