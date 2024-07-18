1c01999f9daa37c50a6fd835065c13f411f479f0
Mariano Rean <mrean@mulesoft.com>
2024-07-18 10:43:30 -0300

    W-15750334: Flows with stopped initial state cannot maintain its status when restarting the application (#13631)
    (cherry picked from commit 22560c3e68700aa543fb369a02db20c9614c783e)

--- core/src/main/java/org/mule/runtime/core/internal/context/DefaultMuleContext.java
+++ core/src/main/java/org/mule/runtime/core/internal/context/DefaultMuleContext.java
@@ -30,6 +30,7 @@ import static org.mule.runtime.api.config.MuleRuntimeFeature.FOREACH_ROUTER_REJE
 import static org.mule.runtime.api.config.MuleRuntimeFeature.HANDLE_SPLITTER_EXCEPTION;
 import static org.mule.runtime.api.config.MuleRuntimeFeature.HONOUR_ERROR_MAPPINGS_WHEN_POLICY_APPLIED_ON_OPERATION;
 import static org.mule.runtime.api.config.MuleRuntimeFeature.HONOUR_INSECURE_TLS_CONFIGURATION;
+import static org.mule.runtime.api.config.MuleRuntimeFeature.HONOUR_PERSISTED_FLOW_STATE;
 import static org.mule.runtime.api.config.MuleRuntimeFeature.HONOUR_RESERVED_PROPERTIES;
 import static org.mule.runtime.api.config.MuleRuntimeFeature.MULE_PRINT_DETAILED_COMPOSITE_EXCEPTION_LOG;
 import static org.mule.runtime.api.config.MuleRuntimeFeature.PARALLEL_FOREACH_FLATTEN_MESSAGE;
@@ -359,6 +360,7 @@ public class DefaultMuleContext implements MuleContextWithRegistry, PrivilegedMu
       configureErrorAndRollbackTxWhenTimeout();
       configureDisableXmlSdkImplicitConfigurationCreation();
       configureEnableXmlSdkReset();
+      configureHonourPersistedFlowState();
     }
   }
 
@@ -1586,6 +1588,12 @@ public class DefaultMuleContext implements MuleContextWithRegistry, PrivilegedMu
   private static void configureEnableXmlSdkReset() {
     FeatureFlaggingRegistry featureFlaggingRegistry = FeatureFlaggingRegistry.getInstance();
     featureFlaggingRegistry.registerFeatureFlag(ENABLE_XML_SDK_MDC_RESET, minMuleVersion("4.8.0"));
+  }
+
+  private static void configureHonourPersistedFlowState() {
+    FeatureFlaggingRegistry featureFlaggingRegistry = FeatureFlaggingRegistry.getInstance();
+    featureFlaggingRegistry.registerFeatureFlag(HONOUR_PERSISTED_FLOW_STATE,
+                                                minMuleVersion("4.8.0"));
   }
 
   private static Predicate<FeatureContext> minMuleVersion(String version) {
