31df2fa91f0d9070d0230216db707fa5327efc94
fsgonz <fabian.gonzalez@salesforce.com>
2024-08-15 12:07:49 -0300

    W-16450677: Allow a bytebuffer provider with the same name to be retrieve and only dispose it only when there are no components using it

--- modules/memory-management-parent/memory-management-tests/src/test/java/org/mule/runtime/internal/memory/management/DefaultMemoryManagementServiceTestCase.java
+++ modules/memory-management-parent/memory-management-tests/src/test/java/org/mule/runtime/internal/memory/management/DefaultMemoryManagementServiceTestCase.java
@@ -8,25 +8,31 @@ package org.mule.runtime.internal.memory.management;
 
 import static org.mule.runtime.api.memory.provider.type.ByteBufferType.DIRECT;
 import static org.mule.runtime.api.memory.provider.type.ByteBufferType.HEAP;
-import static org.mule.runtime.internal.memory.management.DefaultMemoryManagementService.DUPLICATE_BYTE_BUFFER_PROVIDER_NAME;
 import static org.mule.runtime.internal.memory.management.DefaultMemoryManagementService.getInstance;
 import static org.mule.test.allure.AllureConstants.MemoryManagement.MEMORY_MANAGEMENT;
 import static org.mule.test.allure.AllureConstants.MemoryManagement.MemoryManagementServiceStory.DEFAULT_MEMORY_MANAGEMENT_SERVICE;
 
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
-import static java.lang.String.format;
 
 import static org.junit.Assert.assertThat;
 import static org.junit.rules.ExpectedException.none;
 import static org.mockito.junit.MockitoJUnit.rule;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.mule.runtime.api.memory.provider.ByteBufferPoolConfiguration;
 import org.mule.runtime.api.memory.provider.ByteBufferProvider;
 import org.mule.tck.junit4.AbstractMuleTestCase;
 
 import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
@@ -37,6 +43,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.mockito.junit.MockitoRule;
+import org.slf4j.Logger;
 
 @Feature(MEMORY_MANAGEMENT)
 @Story(DEFAULT_MEMORY_MANAGEMENT_SERVICE)
@@ -76,15 +83,6 @@ public class DefaultMemoryManagementServiceTestCase extends AbstractMuleTestCase
   }
 
   @Test
-  @Description("When a bytebuffer provider with the same name is returned, an exception is raised.")
-  public void byteBufferProvidersWithTheSameName() throws Exception {
-    expectedException.expect(IllegalArgumentException.class);
-    expectedException.expectMessage(format(DUPLICATE_BYTE_BUFFER_PROVIDER_NAME, TEST_BYTE_BUFFER));
-    memoryManagementService.getByteBufferProvider(TEST_BYTE_BUFFER, HEAP);
-    memoryManagementService.getByteBufferProvider(TEST_BYTE_BUFFER, DIRECT);
-  }
-
-  @Test
   @Description("When a bytebuffer provider with a null name is returned, an exception is raised.")
   public void byteBufferProvidersWithNullName() throws Exception {
     expectedException.expect(IllegalArgumentException.class);
@@ -106,5 +104,95 @@ public class DefaultMemoryManagementServiceTestCase extends AbstractMuleTestCase
     String notPresentByteBuffer = "notPresentByteBuffer";
     memoryManagementService.disposeByteBufferProvider(notPresentByteBuffer);
     memoryManagementService.disposeByteBufferProvider(notPresentByteBuffer);
+  }
+
+  @Test
+  @Description("When the provider count usage is 0, the bytebuffer provider is disposed.")
+  public void testDisposeByteBufferProviderDisposesWhenCountIsZero() {
+    Map<String, ByteBufferProvider<ByteBuffer>> byteBufferProviders = new HashMap<>();
+    Map<String, Long> byteBufferProvidersUsageCount = new HashMap<>();
+    memoryManagementService = new DefaultMemoryManagementService(byteBufferProviders, byteBufferProvidersUsageCount);
+
+    String providerName = "testProvider";
+
+    ByteBufferProvider<ByteBuffer> mockProvider = mock(ByteBufferProvider.class);
+    byteBufferProviders.put(providerName, mockProvider);
+    byteBufferProvidersUsageCount.put(providerName, 1L);
+
+    memoryManagementService.disposeByteBufferProvider(providerName);
+
+    // Verify that the provider is disposed and removed when the count reaches zero
+    verify(mockProvider, times(1)).dispose();
+    assertThat(byteBufferProviders, not(hasKey(providerName)));
+    assertThat(byteBufferProvidersUsageCount, not(hasKey(providerName)));
+  }
+
+  @Test
+  @Description("When the provider count usage is not 0, the bytebuffer provider is not disposed.")
+  public void testDisposeByteBufferProviderDoesNotDisposeWhenCountIsGreaterThanZero() {
+    Map<String, ByteBufferProvider<ByteBuffer>> byteBufferProviders = new HashMap<>();
+    Map<String, Long> byteBufferProvidersUsageCount = new HashMap<>();
+    memoryManagementService = new DefaultMemoryManagementService(byteBufferProviders, byteBufferProvidersUsageCount);
+
+    String providerName = "testProvider";
+
+    ByteBufferProvider<ByteBuffer> mockProvider = mock(ByteBufferProvider.class);
+    byteBufferProviders.put(providerName, mockProvider);
+    byteBufferProvidersUsageCount.put(providerName, 2L);
+
+    memoryManagementService.disposeByteBufferProvider(providerName);
+
+    // Verify that the provider is not disposed and count is decremented
+    verify(mockProvider, never()).dispose();
+    assertThat(byteBufferProviders, hasKey(providerName));
+    assertThat(byteBufferProvidersUsageCount.get(providerName), equalTo(1L));
+  }
+
+
+  @Test
+  @Description("When provider not present, a warning is logged")
+  public void testDisposeByteBufferProviderLogsWarningWhenProviderNotPresent() {
+    String providerName = "nonExistentProvider";
+
+    Logger mockLogger = mock(Logger.class);
+    DefaultMemoryManagementService.LOGGER = mockLogger; // Inject mock logger
+
+    memoryManagementService.disposeByteBufferProvider(providerName);
+
+    // Verify that a warning is logged when the provider is not present
+    verify(mockLogger, times(1)).warn("Unable to dispose not present ByteBufferProvider '{}'", providerName);
+  }
+
+  @Test
+  @Description("Tests that byte buffer provider is only disposed when there is no more components using it.")
+  public void testGetAndDisposeByteBufferProvider() {
+    Map<String, ByteBufferProvider<ByteBuffer>> byteBufferProviders = new HashMap<>();
+    Map<String, Long> byteBufferProvidersUsageCount = new HashMap<>();
+
+    memoryManagementService = new DefaultMemoryManagementService(byteBufferProviders, byteBufferProvidersUsageCount);
+
+    String providerName = "testProvider";
+
+    // Get the provider twice
+    memoryManagementService.getByteBufferProvider(providerName, HEAP);
+    memoryManagementService.getByteBufferProvider(providerName, HEAP);
+
+    // Verify that the provider is in the map and usage count is 2
+    assertThat(byteBufferProviders, hasKey(providerName));
+    assertThat(byteBufferProvidersUsageCount.get(providerName), equalTo(2L));
+
+    // Dispose the provider once
+    memoryManagementService.disposeByteBufferProvider(providerName);
+
+    // Verify that the count is decremented
+    assertThat(byteBufferProviders, hasKey(providerName));
+    assertThat(byteBufferProvidersUsageCount.get(providerName), equalTo(1L));
+
+    // Dispose the provider again
+    memoryManagementService.disposeByteBufferProvider(providerName);
+
+    // Verify that the provider is removed from the map
+    assertThat(byteBufferProviders, not(hasKey(providerName)));
+    assertThat(byteBufferProvidersUsageCount, not(hasKey(providerName)));
   }
 }
