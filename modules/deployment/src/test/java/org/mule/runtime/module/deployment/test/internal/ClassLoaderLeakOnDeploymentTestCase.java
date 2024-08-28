7541de51a8d1509fadfa7dfdeedb2e85e7936b5f
Ivan Fritzler <58004191+IvanAndresFritzler@users.noreply.github.com>
2024-08-28 14:53:18 -0300

    W-16226692: Application undeploy is leaking MuleApplicationClassloader for Java 17. (#13776)
    (cherry picked from commit 767f2ae8c54c41c3f891c01d73096ca0acdcf7db)

--- modules/deployment/src/test/java/org/mule/runtime/module/deployment/test/internal/ClassLoaderLeakOnDeploymentTestCase.java
+++ modules/deployment/src/test/java/org/mule/runtime/module/deployment/test/internal/ClassLoaderLeakOnDeploymentTestCase.java
@@ -6,19 +6,27 @@
  */
 package org.mule.runtime.module.deployment.test.internal;
 
-import static java.util.Arrays.asList;
+import static org.mule.runtime.module.deployment.test.internal.TestArtifactsCatalog.bridgeMethodExtensionPlugin;
+import static org.mule.runtime.module.deployment.test.internal.TestArtifactsCatalog.helloExtensionV1Plugin;
 import static org.mule.test.allure.AllureConstants.LeakPrevention.LEAK_PREVENTION;
 import static org.mule.test.allure.AllureConstants.LeakPrevention.LeakPreventionMetaspace.METASPACE_LEAK_PREVENTION_ON_REDEPLOY;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptySet;
+import static java.util.Set.of;
+
+import org.mule.runtime.module.deployment.impl.internal.builder.ArtifactPluginFileBuilder;
+
 import java.util.List;
+import java.util.Set;
+import java.util.function.Supplier;
 
 import io.qameta.allure.Issue;
 import io.qameta.allure.Issues;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 /**
  * Contains tests for leak prevention on the deployment process.
@@ -29,22 +37,32 @@ import io.qameta.allure.Story;
 @Issues({@Issue("W-13160893"), @Issue("MULE-17311")})
 public class ClassLoaderLeakOnDeploymentTestCase extends ClassLoaderLeakTestCase {
 
+  public static final Supplier<Set<ArtifactPluginFileBuilder>> BRIDGE_METHOD_PLUGIN = () -> of(bridgeMethodExtensionPlugin);
+  public static final Supplier<Set<ArtifactPluginFileBuilder>> HELLO_V1_PLUGIN = () -> of(helloExtensionV1Plugin);
+  public static final Supplier<Set<ArtifactPluginFileBuilder>> NO_PLUGINS = () -> emptySet();
+
   @Parameterized.Parameters(name = "Parallel: {0}, AppName: {1}, Use Plugin: {2}")
   public static List<Object[]> parameters() {
     return asList(new Object[][] {
         {false, "empty-config-1.0.0-mule-application",
-            "empty-config", false},
+            "empty-config", NO_PLUGINS},
         {true, "empty-config-1.0.0-mule-application",
-            "empty-config", false},
+            "empty-config", NO_PLUGINS},
         {false, "appWithExtensionPlugin-1.0.0-mule-application",
-            "app-with-extension-plugin-config", true},
+            "app-with-extension-plugin-config", HELLO_V1_PLUGIN},
         {true, "appWithExtensionPlugin-1.0.0-mule-application",
-            "app-with-extension-plugin-config", true}
+            "app-with-extension-plugin-config", HELLO_V1_PLUGIN},
+        {false, "appWithExtensionPlugin-1.0.0-mule-application",
+            "app-with-bridge-extension-plugin-config",
+            BRIDGE_METHOD_PLUGIN},
+        {true, "appWithExtensionPlugin-1.0.0-mule-application",
+            "app-with-bridge-extension-plugin-config",
+            BRIDGE_METHOD_PLUGIN}
     });
   }
 
   public ClassLoaderLeakOnDeploymentTestCase(boolean parallellDeployment, String appName, String xmlFile,
-                                             boolean useEchoPluginInApp) {
-    super(parallellDeployment, appName, xmlFile, useEchoPluginInApp);
+                                             Supplier<Set<ArtifactPluginFileBuilder>> applicationPlugins) {
+    super(parallellDeployment, appName, xmlFile, applicationPlugins);
   }
 }
