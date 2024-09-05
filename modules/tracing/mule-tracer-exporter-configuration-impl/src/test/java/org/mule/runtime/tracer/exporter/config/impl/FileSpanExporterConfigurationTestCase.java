e4b4bd02410c3fecfb27d7028a6b68a0040438a9
Leandro Ezequiel Barrios <lbarrios@mulesoft.com>
2024-09-05 13:53:21 -0300

    Ignored test

--- modules/tracing/mule-tracer-exporter-configuration-impl/src/test/java/org/mule/runtime/tracer/exporter/config/impl/FileSpanExporterConfigurationTestCase.java
+++ modules/tracing/mule-tracer-exporter-configuration-impl/src/test/java/org/mule/runtime/tracer/exporter/config/impl/FileSpanExporterConfigurationTestCase.java
@@ -43,6 +43,7 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -182,7 +183,7 @@ public class FileSpanExporterConfigurationTestCase {
     assertThat(keyFileLocationPath.isAbsolute(), is(TRUE));
   }
 
-  @Test
+  @Test @Ignore("To be fixed in W-16676258")
   public void configurationFileChanged() throws Exception {
     File file = createTempFile("tracing", "test");
     Path testFile = get(file.getPath());
