42fad2ab8010e168222e88ef16f1d65c5280b804
Rodrigo Merino <elrodro83@users.noreply.github.com>
2024-09-18 14:40:08 -0300

    W-16778624: Update byteBuddy to v1.15.1 (#13851)

--- core-tests/src/test/java/org/mule/runtime/core/internal/transformer/type/DataTypeBuilderTestCase.java
+++ core-tests/src/test/java/org/mule/runtime/core/internal/transformer/type/DataTypeBuilderTestCase.java
@@ -30,7 +30,7 @@ import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import net.bytebuddy.implementation.bind.annotation.*;
 import org.mule.runtime.api.el.BindingContext;
@@ -412,7 +412,7 @@ public class DataTypeBuilderTestCase extends AbstractMuleTestCase {
 
   }
 
-  protected static class TestInterceptor {
+  public static class TestInterceptor {
 
     @RuntimeType
     public Object intercept(@This Object obj, @Origin Method method, @AllArguments Object[] args,
