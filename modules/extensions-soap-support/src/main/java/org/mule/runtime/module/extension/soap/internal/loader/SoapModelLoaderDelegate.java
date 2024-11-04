017cea8ab61c6aea33936c8fbbc02b44bbe99cf7
elrodro83 <rmerino@salesforce.com>
2024-11-04 10:24:05 -0300

    W-17129224: Remove mule framework switches from mule runtime code

--- modules/extensions-soap-support/src/main/java/org/mule/runtime/module/extension/soap/internal/loader/SoapModelLoaderDelegate.java
+++ modules/extensions-soap-support/src/main/java/org/mule/runtime/module/extension/soap/internal/loader/SoapModelLoaderDelegate.java
@@ -6,13 +6,14 @@
  */
 package org.mule.runtime.module.extension.soap.internal.loader;
 
-import static java.util.stream.Collectors.toList;
 import static org.mule.runtime.extension.api.annotation.Extension.DEFAULT_CONFIG_DESCRIPTION;
 import static org.mule.runtime.extension.api.annotation.Extension.DEFAULT_CONFIG_NAME;
-import static org.mule.runtime.module.extension.internal.loader.utils.ExtensionNamespaceUtils.getExtensionsNamespace;
 import static org.mule.runtime.module.extension.internal.loader.parser.java.MuleExtensionAnnotationParser.getExtensionInfo;
+import static org.mule.runtime.module.extension.internal.loader.utils.ExtensionNamespaceUtils.getExtensionsNamespace;
 import static org.mule.runtime.module.extension.internal.loader.utils.ModelLoaderUtils.getXmlDslModel;
 import static org.mule.runtime.module.extension.soap.internal.loader.SoapExtensionTypeFactory.getSoapExtensionType;
+
+import static java.util.stream.Collectors.toList;
 
 import org.mule.metadata.api.ClassTypeLoader;
 import org.mule.metadata.api.model.MetadataType;
@@ -24,9 +25,9 @@ import org.mule.runtime.api.meta.model.error.ErrorModel;
 import org.mule.runtime.extension.api.declaration.type.DefaultExtensionsTypeLoaderFactory;
 import org.mule.runtime.extension.api.loader.ExtensionLoadingContext;
 import org.mule.runtime.extension.api.soap.MessageDispatcherProvider;
-import org.mule.runtime.module.extension.internal.loader.delegate.ModelLoaderDelegate;
 import org.mule.runtime.module.extension.api.loader.java.type.ExtensionElement;
 import org.mule.runtime.module.extension.internal.error.ErrorsModelFactory;
+import org.mule.runtime.module.extension.internal.loader.delegate.ModelLoaderDelegate;
 import org.mule.runtime.module.extension.internal.loader.delegate.StereotypeModelLoaderDelegate;
 import org.mule.runtime.module.extension.internal.loader.java.TypeAwareConfigurationFactory;
 import org.mule.runtime.module.extension.internal.loader.java.info.ExtensionInfo;
@@ -115,7 +116,7 @@ public final class SoapModelLoaderDelegate implements ModelLoaderDelegate {
                                                   Set<ErrorModel> soapErrors) {
     // TODO - MULE-14311 - Make loader work in compile time
     Class<?> clazz = extension.getDeclaringClass().get();
-    TypeAwareConfigurationFactory configurationFactory = new TypeAwareConfigurationFactory(clazz, clazz.getClassLoader());
+    TypeAwareConfigurationFactory configurationFactory = new TypeAwareConfigurationFactory(clazz, clazz.getClassLoader(), true);
 
     ConfigurationDeclarer configDeclarer = declarer.withConfig(DEFAULT_CONFIG_NAME)
         .describedAs(DEFAULT_CONFIG_DESCRIPTION)
