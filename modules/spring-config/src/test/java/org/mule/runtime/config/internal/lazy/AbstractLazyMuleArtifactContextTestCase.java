2209b900174f012cab0f05aa443c553e1f57aa14
fsgonz <fabian.gonzalez@salesforce.com>
2024-11-25 18:58:35 -0300

    W-16639192: Fix tests LazyMuleArtifactContextBeanFactoryTestCase and LazyComponentInitializerAdapterTestCase

--- modules/spring-config/src/test/java/org/mule/runtime/config/internal/lazy/AbstractLazyMuleArtifactContextTestCase.java
+++ modules/spring-config/src/test/java/org/mule/runtime/config/internal/lazy/AbstractLazyMuleArtifactContextTestCase.java
@@ -25,11 +25,7 @@ import static java.util.Collections.emptySet;
 import static java.util.Optional.empty;
 
 import static org.junit.rules.ExpectedException.none;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -52,6 +48,7 @@ import org.mule.runtime.config.internal.context.BaseConfigurationComponentLocato
 import org.mule.runtime.config.internal.context.ObjectProviderAwareBeanFactory;
 import org.mule.runtime.config.internal.context.lazy.LazyMuleArtifactContext;
 import org.mule.runtime.core.api.MuleContext;
+import org.mule.runtime.core.api.config.DefaultMuleConfiguration;
 import org.mule.runtime.core.api.extension.ExtensionManager;
 import org.mule.runtime.core.api.processor.Processor;
 import org.mule.runtime.core.internal.config.InternalCustomizationService;
@@ -62,22 +59,30 @@ import org.mule.runtime.core.internal.registry.DefaultRegistry;
 import org.mule.runtime.core.privileged.processor.chain.DefaultMessageProcessorChainBuilder;
 import org.mule.runtime.extension.api.model.ImmutableExtensionModel;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import com.google.common.collect.ImmutableSet;
-
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.rules.ExpectedException;
-
-import org.mockito.Mock;
-
 import io.qameta.allure.Feature;
 import io.qameta.allure.Features;
 import io.qameta.allure.Story;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mock;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanDefinitionStoreException;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.DependencyDescriptor;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.core.ResolvableType;
 
 @Features({@Feature(LAZY_INITIALIZATION), @Feature(CONFIGURATION_COMPONENT_LOCATOR)})
 @Story(COMPONENT_LIFE_CYCLE)
@@ -102,7 +107,7 @@ public abstract class AbstractLazyMuleArtifactContextTestCase extends AbstractDs
   @Mock
   private LockFactory lockFactory;
 
-  protected DefaultListableBeanFactory beanFactory;
+  protected TestObjectProviderAwareBeanFactory beanFactory;
 
   @Before
   public void setup() throws Exception {
@@ -112,6 +117,7 @@ public abstract class AbstractLazyMuleArtifactContextTestCase extends AbstractDs
         .add(mockExtension)
         .build();
 
+    when(muleContext.getConfiguration()).thenReturn(new DefaultMuleConfiguration());
     when(extensionManager.getExtensions()).thenReturn(extensions);
     when(muleContext.getExecutionClassLoader()).thenReturn(currentThread().getContextClassLoader());
     when(muleContext.getExtensionManager()).thenReturn(extensionManager);
@@ -134,17 +140,8 @@ public abstract class AbstractLazyMuleArtifactContextTestCase extends AbstractDs
 
   protected abstract ArtifactDeclaration getArtifactDeclaration();
 
-  protected DefaultListableBeanFactory doCreateBeanFactoryMock() {
-    return mock(ObjectProviderAwareBeanFactory.class);
-  }
-
-  private DefaultListableBeanFactory createBeanFactoryMock() {
-    DefaultListableBeanFactory beanFactory = doCreateBeanFactoryMock();
-    doReturn("mutex").when(beanFactory).getSingletonMutex();
-    doReturn(new String[0]).when(beanFactory).getBeanNamesForType(any(Class.class), anyBoolean(), anyBoolean());
-    doNothing().when(beanFactory).setSerializationId(any(String.class));
-
-    return beanFactory;
+  private TestObjectProviderAwareBeanFactory createBeanFactoryMock(MuleContext muleContext) {
+    return new TestObjectProviderAwareBeanFactory(muleContext);
   }
 
   @Override
@@ -190,7 +187,7 @@ public abstract class AbstractLazyMuleArtifactContextTestCase extends AbstractDs
 
           @Override
           protected DefaultListableBeanFactory createBeanFactory() {
-            beanFactory = createBeanFactoryMock();
+            beanFactory = createBeanFactoryMock(muleContext);
             return beanFactory;
           }
         };
@@ -203,5 +200,178 @@ public abstract class AbstractLazyMuleArtifactContextTestCase extends AbstractDs
 
   private Set<ExtensionModel> getExtensions(ExtensionManager extensionManager) {
     return extensionManager == null ? emptySet() : extensionManager.getExtensions();
+  }
+
+  /**
+   * A {@link ObjectProviderAwareBeanFactory} to test lazy configuration.
+   */
+  protected static class TestObjectProviderAwareBeanFactory extends ObjectProviderAwareBeanFactory {
+
+    private final MuleContext muleContext;
+    private boolean beanFactoryMustThrow;
+
+    private final Map<String, BeanDefinition> registeredBeans = new HashMap<>();
+
+    private final Map<String, BeanDefinition> registeredBeansByType = new HashMap<>();
+
+    public TestObjectProviderAwareBeanFactory(MuleContext muleContext) {
+      super(mock(ListableBeanFactory.class));
+      this.muleContext = muleContext;
+    }
+
+    @Override
+    public String[] getBeanNamesForType(Class<?> type, boolean includeNonSingletons, boolean allowEagerInit) {
+      return new String[0];
+    }
+
+    @Override
+    public void registerBeanDefinition(String beanName, BeanDefinition beanDefinition) throws BeanDefinitionStoreException {
+      registeredBeans.put(beanName, beanDefinition);
+      registeredBeansByType.put(beanDefinition.getBeanClassName(), beanDefinition);
+      super.registerBeanDefinition(beanName, beanDefinition);
+    }
+
+    @Override
+    public <T> T getBean(Class<T> requiredType, Object... args) throws BeansException {
+      return getBean(requiredType);
+    }
+
+    @Override
+    public <T> T getBean(Class<T> requiredType) throws BeansException {
+      if (beanFactoryMustThrow) {
+        throw new NoSuchBeanDefinitionException(requiredType);
+      }
+
+      if (registeredBeans.containsKey(requiredType)) {
+        return mock(requiredType);
+      } else {
+        return null;
+      }
+    }
+
+    @Override
+    public <T> ObjectProvider<T> getBeanProvider(Class<T> requiredType) {
+      return getBeanProvider(requiredType, false);
+    }
+
+    @Override
+    public <T> ObjectProvider<T> getBeanProvider(Class<T> requiredType, boolean allowEagerInit) {
+      if (beanFactoryMustThrow) {
+        throw new NoSuchBeanDefinitionException(requiredType);
+      } else {
+        T bean = mock(requiredType);
+        return new ObjectProvider<T>() {
+
+          @Override
+          public T getObject() throws BeansException {
+            return bean;
+          }
+        };
+      }
+    }
+
+    @Override
+    public <T> ObjectProvider<T> getBeanProvider(ResolvableType requiredType) {
+      if (beanFactoryMustThrow) {
+        throw new NoSuchBeanDefinitionException(requiredType);
+      } else {
+        T bean = null;
+        try {
+          bean = (T) mock(Class.forName(requiredType.getType().getTypeName()));
+        } catch (ClassNotFoundException e) {
+          throw new RuntimeException(e);
+        }
+        T finalBean = bean;
+        return new ObjectProvider<T>() {
+
+          @Override
+          public T getObject() throws BeansException {
+            return finalBean;
+          }
+        };
+      }
+    }
+
+    @Override
+    public <T> ObjectProvider<T> getBeanProvider(ResolvableType requiredType, boolean allowEagerInit) {
+      return getBeanProvider(requiredType, false);
+    }
+
+    @Override
+    public Object getBean(String name, Object... args) throws BeansException {
+      return getBean(name);
+    }
+
+    @Override
+    public <T> T getBean(String name, Class<T> requiredType) throws BeansException {
+      return (T) registeredBeans.get(name);
+    }
+
+    @Override
+    public Object getBean(String name) throws BeansException {
+      if (beanFactoryMustThrow) {
+        if (registeredBeans.containsKey(name)) {
+          return registeredBeans.get(name);
+        } else {
+          throw new NoSuchBeanDefinitionException(name);
+        }
+      }
+
+      if (name.equals("_muleContext")) {
+        return muleContext;
+      }
+
+      if (registeredBeans.containsKey(name)) {
+        try {
+          return mock(Class.forName(registeredBeans.get(name).getBeanClassName()));
+        } catch (ClassNotFoundException e) {
+          throw new RuntimeException(e);
+        }
+      } else {
+        return null;
+      }
+    }
+
+    @Override
+    public void setSerializationId(String serializationId) {
+      // Nothing to do.
+    }
+
+    @Override
+    protected Map<String, Object> findAutowireCandidates(String beanName, Class<?> requiredType,
+                                                         DependencyDescriptor descriptor) {
+      if (requiredType.equals(MuleContext.class)) {
+        return Collections.singletonMap("_muleContext", muleContext);
+      }
+
+      if (requiredType.equals(org.mule.runtime.api.artifact.Registry.class)) {
+        return Collections.singletonMap("_registry", new DefaultRegistry(muleContext));
+      }
+
+      return Collections.singletonMap(beanName, mock(requiredType));
+    }
+
+    public void setBeanFactoryMustThrow(boolean beanFactoryMustThrow) {
+      this.beanFactoryMustThrow = beanFactoryMustThrow;
+    }
+
+    public boolean isRegisteredBeanDefiniion(String name) {
+      return registeredBeans.containsKey(name);
+    }
+
+    @Override
+    public boolean containsBean(String name) {
+      return registeredBeans.containsKey(name);
+    }
+
+    @Override
+    public boolean isSingleton(String name) throws NoSuchBeanDefinitionException {
+      return false;
+    }
+
+    @Override
+    public String[] getAliases(String name) {
+      return null;
+    }
   }
 }
