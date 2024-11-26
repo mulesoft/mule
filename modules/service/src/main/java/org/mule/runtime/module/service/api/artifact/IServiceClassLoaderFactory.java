5e8c4e1b177fc04f9972c372b775c189edd835d8
Leandro Ezequiel Barrios <lbarrios@mulesoft.com>
2024-11-26 13:45:24 -0300

    ...

---
+++ modules/service/src/main/java/org/mule/runtime/module/service/api/artifact/IServiceClassLoaderFactory.java
@@ -0,0 +1,6 @@
+package org.mule.runtime.module.service.api.artifact;
+
+import org.mule.runtime.container.api.ContainerDependantArtifactClassLoaderFactory;
+
+public interface IServiceClassLoaderFactory extends ContainerDependantArtifactClassLoaderFactory<ServiceDescriptor> {
+}
