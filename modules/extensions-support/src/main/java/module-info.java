8b8d660259d2beebd891bcec50f0906beb9708b1
Rodrigo Merino <elrodro83@users.noreply.github.com>
2024-11-29 10:02:48 -0300

    W-17331120: Reuse generated dynamic classes `withAnnotations` if available (#14034)
    * Push the utils to add annotations down to extensions-support from core

--- modules/extensions-support/src/main/java/module-info.java
+++ modules/extensions-support/src/main/java/module-info.java
@@ -97,6 +97,9 @@ module org.mule.runtime.extensions.support {
   exports org.mule.runtime.module.extension.api.runtime.privileged to
       org.mule.runtime.extensions.spring.support;
 
+  // for ByteBuddy dynamically generated classes
+  exports org.mule.runtime.module.extension.privileged.component;
+
   exports org.mule.runtime.module.extension.internal to
       org.mule.runtime.extensions.spring.support,
       org.mule.runtime.tooling.support,
