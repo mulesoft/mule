75bf875a2c20d37c9b10d6b97e4a82404b5e882f
Axel Sanguinetti <asanguinetti@mulesoft.com>
2025-01-15 10:15:58 -0300

    Moving configuration to profile in mule pom

--- tests/functional/src/main/java/org/mule/functional/junit4/ArtifactAstXmlParserConfigurationBuilder.java
+++ tests/functional/src/main/java/org/mule/functional/junit4/ArtifactAstXmlParserConfigurationBuilder.java
@@ -14,8 +14,7 @@ import static org.mule.runtime.config.api.ArtifactContextFactory.createArtifactC
 import static org.mule.runtime.config.api.dsl.ArtifactDeclarationUtils.toArtifactast;
 import static org.mule.runtime.module.artifact.activation.api.ast.ArtifactAstUtils.parseAndBuildAppExtensionModel;
 
-import static java.lang.Boolean.parseBoolean;
-import static java.lang.System.getProperty;
+import static java.lang.Boolean.getBoolean;
 import static java.util.Arrays.asList;
 import static java.util.Objects.requireNonNull;
 import static java.util.Optional.ofNullable;
@@ -185,7 +184,7 @@ public class ArtifactAstXmlParserConfigurationBuilder extends AbstractConfigurat
                                                        muleContext.getConfiguration(),
                                                        expressionLanguageMetadataService);
 
-      if (parseBoolean(getProperty(SERIALIZE_DESERIALIZE_AST_PROPERTY, "true"))) {
+      if (getBoolean(SERIALIZE_DESERIALIZE_AST_PROPERTY)) {
         return serializeAndDeserialize(ast);
       } else {
         return ast;
