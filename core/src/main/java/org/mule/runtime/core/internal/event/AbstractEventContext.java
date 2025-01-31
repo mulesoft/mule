fce00831f088c97ac192242f73d98912c3931a7c
Anand Karandikar <anandk10.github@gmail.com>
2025-01-31 14:16:16 +0530

    Make AbstractEventContext public

--- core/src/main/java/org/mule/runtime/core/internal/event/AbstractEventContext.java
+++ core/src/main/java/org/mule/runtime/core/internal/event/AbstractEventContext.java
@@ -52,7 +52,7 @@ import reactor.core.publisher.MonoSink;
  *
  * @since 4.0
  */
-abstract class AbstractEventContext implements SpanContextAware, BaseEventContext {
+public abstract class AbstractEventContext implements SpanContextAware, BaseEventContext {
 
   private static final int STATE_READY = 0;
   private static final int STATE_RESPONSE_RECEIVED = 1;
