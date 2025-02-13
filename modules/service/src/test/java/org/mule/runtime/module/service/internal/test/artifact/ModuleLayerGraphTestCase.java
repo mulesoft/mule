2bcfc09df82ea90f9c4c3062db6ddd8ecc9c8355
Martín Buchwald <martin.buchwald@mulesoft.com>
2025-02-13 18:20:17 -0300

    Progress

--- modules/service/src/test/java/org/mule/runtime/module/service/internal/test/artifact/ModuleLayerGraphTestCase.java
+++ modules/service/src/test/java/org/mule/runtime/module/service/internal/test/artifact/ModuleLayerGraphTestCase.java
@@ -6,6 +6,7 @@
  */
 package org.mule.runtime.module.service.internal.test.artifact;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.mule.runtime.module.service.internal.artifact.ModuleLayerGraph.setModuleLayerId;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,6 +16,16 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ModuleLayerGraphTestCase {
+
+  private static final String GRAPH_REPR = "--------------\n" +
+      "| testLayer  |\n" +
+      "--------------\n" +
+      "       |\n" +
+      "       |\n" +
+      "       V\n" +
+      "--------------\n" +
+      "|testParentLayer|\n" +
+      "--------------\n";
 
   private ModuleLayerGraph graph;
 
@@ -28,15 +39,33 @@ public class ModuleLayerGraphTestCase {
 
   @Test
   public void graphString() {
-    assertThat(graph.graphString(), is("--------------\n" +
-        "| testLayer  |\n" +
-        "--------------\n" +
-        "       |\n" +
-        "       |\n" +
-        "       V\n" +
-        "--------------\n" +
-        "|testParentLayer|\n" +
-        "--------------\n"));
+    assertThat(graph.graphString(), is(GRAPH_REPR));
+  }
+
+  @Test
+  public void moduleLayerModules() {
+    String info = graph.moduleLayerModules();
+    // We don't assert for the exact string because it may come in different order, and depending on the modifications of
+    // dependencies of this module, then this test may start failing even if we sort those dependencies' modules.
+    assertThat(info, containsString("org.mule.runtime.metadata.model.api"));
+    assertThat(info, containsString("org.mule.runtime.jpms.utils"));
+    assertThat(info, containsString("org.mule.runtime.metrics.api"));
+    assertThat(info, containsString("org.mule.runtime.tracer.api"));
+    assertThat(info, containsString("org.mule.runtime.manifest"));
+    assertThat(info, containsString("org.mule.runtime.dsl.api"));
+    assertThat(info, containsString("org.mule.runtime.jar.handling.utils"));
+    assertThat(info, containsString("org.mule.runtime.container"));
+    assertThat(info, containsString("org.mule.runtime.metadata.model.message"));
+    assertThat(info, containsString("org.mule.runtime.profiling.api"));
+    assertThat(info, containsString("org.mule.runtime.tracer.customization.api"));
+    assertThat(info, containsString("org.mule.runtime.tracer.exporter.configuration.api"));
+    assertThat(info, containsString("org.mule.runtime.policy.api"));
+    assertThat(info, containsString("org.mule.runtime.policy.api"));
+    assertThat(info, containsString("org.mule.runtime.core"));
+    assertThat(info, containsString("org.mule.runtime.metadata.model.java"));
+    assertThat(info, containsString("org.mule.runtime.artifact.declaration"));
+    assertThat(info, containsString("org.mule.runtime.errors"));
+    assertThat(info, containsString("org.mule.sdk.api"));
   }
 
 }
