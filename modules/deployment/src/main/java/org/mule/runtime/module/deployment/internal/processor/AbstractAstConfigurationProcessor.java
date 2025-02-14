58a32e6cf7e39773bcb86bff45cffc91bbacb137
fsgonz <fabian.gonzalez@salesforce.com>
2025-02-14 09:47:02 -0300

    efactor

--- modules/deployment/src/main/java/org/mule/runtime/module/deployment/internal/processor/AbstractAstConfigurationProcessor.java
+++ modules/deployment/src/main/java/org/mule/runtime/module/deployment/internal/processor/AbstractAstConfigurationProcessor.java
@@ -55,9 +55,6 @@ abstract class AbstractAstConfigurationProcessor implements ArtifactConfiguratio
 
     configurationBuilder.configure(artifactContextConfiguration.getMuleContext());
 
-    artifactAst.postConfigurationEnrichment();
-
-    // This has to be done after enriching models
     return configurationBuilder.createArtifactContext();
   }
 
