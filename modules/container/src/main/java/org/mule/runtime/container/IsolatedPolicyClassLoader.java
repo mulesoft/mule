8616f6684a793b6d74a2c02993130c792315797f
Hyeran <hyeran.park@mulesoft.com>
2025-02-18 12:38:49 -0800

    use filtering container classloader as parent of IsolatedPolicyClassLoader

--- modules/container/src/main/java/org/mule/runtime/container/IsolatedPolicyClassLoader.java
+++ modules/container/src/main/java/org/mule/runtime/container/IsolatedPolicyClassLoader.java
@@ -6,52 +6,39 @@
  */
 package org.mule.runtime.container;
 
-import org.mule.runtime.container.internal.MuleClassLoaderLookupPolicy;
-import org.mule.runtime.module.artifact.api.classloader.ChildFirstLookupStrategy;
-import org.mule.runtime.module.artifact.api.classloader.ClassLoaderLookupPolicy;
-import org.mule.runtime.module.artifact.api.classloader.DelegateOnlyLookupStrategy;
-import org.mule.runtime.module.artifact.api.classloader.LookupStrategy;
+import org.mule.runtime.container.internal.FilteringContainerClassLoader;
 import org.mule.runtime.module.artifact.api.classloader.MuleDeployableArtifactClassLoader;
-import org.mule.runtime.module.artifact.api.classloader.ParentFirstLookupStrategy;
 import org.mule.runtime.module.artifact.api.descriptor.DeployableArtifactDescriptor;
 
 import java.net.URL;
-import java.util.Collections;
 
 public class IsolatedPolicyClassLoader extends MuleDeployableArtifactClassLoader {
 
-  private static final IsolatedPolicyClassLoader INSTANCE = new IsolatedPolicyClassLoader(
-                                                                                          "isolated-policy-classloader",
-                                                                                          new DeployableArtifactDescriptor("isolated-policy-descriptor"),
-                                                                                          null,
-                                                                                          createIsolatedPolicyLookupPolicy());
+  private static volatile IsolatedPolicyClassLoader INSTANCE;
+
 
   private IsolatedPolicyClassLoader(String artifactId,
                                     DeployableArtifactDescriptor artifactDescriptor,
-                                    ClassLoader parent,
-                                    ClassLoaderLookupPolicy lookupPolicy) {
-    super(artifactId, artifactDescriptor, new URL[0], parent, lookupPolicy);
+                                    FilteringContainerClassLoader containerClassLoader) {
+    super(artifactId, artifactDescriptor, new URL[0], containerClassLoader, containerClassLoader.getClassLoaderLookupPolicy());
   }
 
-  public static IsolatedPolicyClassLoader getInstance() {
+  public static IsolatedPolicyClassLoader getInstance(FilteringContainerClassLoader containerClassLoader) {
     if (INSTANCE == null) {
-      // todo: fill this
+      synchronized (IsolatedPolicyClassLoader.class) {
+        if (INSTANCE == null) {
+          if (containerClassLoader == null) {
+            throw new IllegalStateException("Container Classloader is not set yet, cannot create IsolatedPolicyClassLoader Instance");
+          }
+          INSTANCE = new IsolatedPolicyClassLoader(
+                                                   "isolated-policy-classloader",
+                                                   new DeployableArtifactDescriptor("isolated-policy-descriptor"),
+                                                   containerClassLoader);
+        }
+      }
     }
     return INSTANCE;
   }
 
-  private static ClassLoaderLookupPolicy createIsolatedPolicyLookupPolicy() {
-    return new MuleClassLoaderLookupPolicy(Collections.emptyMap(), Collections.emptySet()) {
-
-      @Override
-      public LookupStrategy getClassLookupStrategy(String className) {
-        if (className.startsWith("java.")) {
-          return new DelegateOnlyLookupStrategy(ClassLoader.getSystemClassLoader());
-        } else {
-          return ChildFirstLookupStrategy.CHILD_FIRST;
-        }
-      }
-    };
-  }
 
 }
