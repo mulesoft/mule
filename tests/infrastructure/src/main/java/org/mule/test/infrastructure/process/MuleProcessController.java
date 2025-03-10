dce3c455c269d3fd8aae11c6a479fe5f26b4367f
Mariano Rean <mrean@mulesoft.com>
2025-03-10 11:51:55 -0300

    W-17340620: Add a new command to verify a given license file (#14144) (#14263)
    (cherry picked from commit 6449a88bee87abe12c6711d847a90c24bc00f40d)

--- tests/infrastructure/src/main/java/org/mule/test/infrastructure/process/MuleProcessController.java
+++ tests/infrastructure/src/main/java/org/mule/test/infrastructure/process/MuleProcessController.java
@@ -129,6 +129,10 @@ public class MuleProcessController {
     getController().uninstallLicense();
   }
 
+  public void verifyLicenseDetails(String path) {
+    getController().verifyLicenseDetails(path);
+  }
+
   public void addLibrary(File jar) {
     getController().addLibrary(jar);
   }
