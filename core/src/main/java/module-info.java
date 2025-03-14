d030169534e22ddd52f3e8fd48ddfb1f7cd93c44
d4nielyan9 <chengchang.yang@mulesoft.com>
2025-03-14 13:45:28 -0700

    remove

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -596,8 +596,7 @@ module org.mule.runtime.core {
   opens org.mule.runtime.core.internal.management.stats to
       kryo.shaded;
   opens org.mule.runtime.core.internal.message to
-      com.mulesoft.mule.runtime.kryo,
-      kryo.shaded;
+      com.mulesoft.mule.runtime.kryo;
   opens org.mule.runtime.core.internal.policy to
       org.mule.runtime.deployment,
       spring.core;
