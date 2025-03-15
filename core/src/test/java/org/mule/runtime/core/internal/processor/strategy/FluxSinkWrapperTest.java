28e40a5d2612b0edf7687b493c19d8fd3a3abfaa
chris-ebert <cebert@salesforce.com>
2025-03-14 18:44:13 -0700

    Added more tests. Refactored a tiny bit to make code more testable

---
+++ core/src/test/java/org/mule/runtime/core/internal/processor/strategy/FluxSinkWrapperTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.internal.processor.strategy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mule.runtime.core.api.event.CoreEvent;
+import org.mule.runtime.core.internal.processor.strategy.reactor.builder.ParameterMockingTestUtil;
+import reactor.core.publisher.FluxSink;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class FluxSinkWrapperTest {
+
+  private FluxSinkWrapper sink;
+  @Mock
+  private FluxSink<CoreEvent> delegate;
+
+  @BeforeEach
+  void setUp() {
+    sink = new FluxSinkWrapper(delegate);
+  }
+
+  @ParameterizedTest(name = "[{index}]{0}")
+  @MethodSource("fluxSinkMethods")
+  void fluxSinkDelegates(String testName, Method m) throws InvocationTargetException, IllegalAccessException {
+    Object[] values = ParameterMockingTestUtil.getParameterValues(m, CoreEvent.class);
+    m.invoke(sink, values);
+    m.invoke(verify(delegate), values);
+  }
+
+  static List<Arguments> fluxSinkMethods() {
+    return Arrays.stream(FluxSink.class.getDeclaredMethods())
+        .filter(m -> m.getName().matches("^[a-z].*"))
+        // Filter out contextView - it's a default inherited from the interface
+        .filter(m -> !m.getName().equals("contextView"))
+        .map(m -> Arguments.of(m.getName(), m))
+        .toList();
+  }
+
+  @Test
+  void contextView() {
+    sink.contextView();
+    verify(delegate).currentContext();
+  }
+}
--- core/src/test/java/org/mule/runtime/core/internal/processor/strategy/reactor/builder/ParameterMockingTestUtil.java
+++ core/src/test/java/org/mule/runtime/core/internal/processor/strategy/reactor/builder/ParameterMockingTestUtil.java
@@ -7,14 +7,17 @@
 package org.mule.runtime.core.internal.processor.strategy.reactor.builder;
 
 import org.mockito.Mockito;
+import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.runtime.core.api.processor.ReactiveProcessor;
 import org.mule.runtime.core.internal.profiling.InternalProfilingService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import javax.sound.sampled.Control;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.TypeVariable;
 import java.util.Arrays;
 import java.util.Optional;
 
@@ -23,13 +26,22 @@ import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+/**
+ * Collect some logic to create mock parameters for method invocations in tests. This is intended for tests that are exercising
+ * simple methods (such as on builders) that don't need much behaviour, just the objects so the method can be invoked.
+ *
+ */
 public class ParameterMockingTestUtil {
 
-  static Object getValue(Parameter parameter) {
-    if (parameter.getType().isAssignableFrom(Optional.class)) {
-      Class<?> parameterizedType =
-          (Class<?>) ((ParameterizedType) parameter.getParameterizedType()).getActualTypeArguments()[0];
+  static Object getValue(Parameter parameter, Class<?> typeClass) {
+    if (Optional.class.isAssignableFrom(parameter.getType())) {
+      Class<?> parameterizedType = parameter.getParameterizedType() instanceof TypeVariable ? typeClass
+          : (Class<?>) ((ParameterizedType) parameter.getParameterizedType()).getActualTypeArguments()[0];
       return Optional.of(Mockito.mock(parameterizedType));
+    } else if (parameter.getParameterizedType() instanceof TypeVariable<?>) {
+      // the parameter is a generic type variable... so return the type we're using for generics.
+      // If we wind up with multiples we'll have to be more clever
+      return mock(typeClass);
     } else if (ReactiveProcessor.class.isAssignableFrom(parameter.getType())) {
       ReactiveProcessor value = (ReactiveProcessor) mock(parameter.getType());
       when(value.apply(any())).thenAnswer(inv -> inv.getArgument(0));
@@ -37,6 +49,7 @@ public class ParameterMockingTestUtil {
     } else if (InternalProfilingService.class.isAssignableFrom(parameter.getType())) {
       InternalProfilingService value = (InternalProfilingService) mock(parameter.getType());
       lenient().when(value.enrichWithProfilingEventFlux(any(), any(), any())).thenAnswer(inv -> inv.getArgument(0));
+      lenient().when(value.enrichWithProfilingEventMono(any(), any(), any())).thenAnswer(inv -> inv.getArgument(0));
       lenient().when(value.setCurrentExecutionContext((Mono<?>) any(), any())).thenAnswer(inv -> inv.getArgument(0));
       lenient().when(value.setCurrentExecutionContext((Flux<?>) any(), any())).thenAnswer(inv -> inv.getArgument(0));
       return value;
@@ -47,7 +60,11 @@ public class ParameterMockingTestUtil {
     }
   }
 
-  static Object[] getParameterValues(Method m) {
-    return Arrays.stream(m.getParameters()).map(ParameterMockingTestUtil::getValue).toArray();
+  public static Object[] getParameterValues(Method m) {
+    return getParameterValues(m, Object.class);
+  }
+
+  public static Object[] getParameterValues(Method m, final Class<?> typeClass) {
+    return Arrays.stream(m.getParameters()).map(p -> getValue(p, typeClass)).toArray();
   }
 }
