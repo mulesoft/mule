72505c4f6aaab4d4d05a39c290d2112c77bc6080
chris-ebert <cebert@salesforce.com>
2025-03-17 16:59:16 -0700

    Hooray. Coverage over 80%

---
+++ core/src/test/java/org/mule/runtime/core/internal/processor/strategy/AbstrctCachedThreadReactorSinkProviderTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.core.internal.processor.strategy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mule.runtime.core.api.event.CoreEvent;
+import reactor.core.publisher.FluxSink;
+
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
+
+@ExtendWith(MockitoExtension.class)
+class AbstrctCachedThreadReactorSinkProviderTest {
+
+  private AbstractCachedThreadReactorSinkProvider provider;
+  @Mock
+  private CoreEvent event;
+  @Mock
+  private FluxSink<CoreEvent> sink;
+
+  @Test
+  void getSink_indexEnabled() {
+    provider = mock(AbstractCachedThreadReactorSinkProvider.class,
+                    withSettings().useConstructor(true).defaultAnswer(CALLS_REAL_METHODS));
+    // Mock the abstract method so we get something back...
+    when(provider.createSink()).thenReturn(sink);
+
+    final FluxSink<CoreEvent> result = provider.getSink();
+    result.next(event);
+    result.complete();
+  }
+
+  @Test
+  void getSink_indexDisabled() {
+    provider = mock(AbstractCachedThreadReactorSinkProvider.class,
+                    withSettings().useConstructor(false).defaultAnswer(CALLS_REAL_METHODS));
+    // Mock the abstract method so we get something back...
+    when(provider.createSink()).thenReturn(sink);
+
+    final FluxSink<CoreEvent> result = provider.getSink();
+    result.next(event);
+    result.complete();
+  }
+}
