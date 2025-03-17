3416e5ba08640904c7621cc51e32026a4a8247bc
Ezequiel Werner <eze210@gmail.com>
2025-03-17 17:01:11 -0300

    jdoc

--- modules/http-api/src/main/java/org/mule/runtime/http/api/sse/client/SseSourceConfigBuilder.java
+++ modules/http-api/src/main/java/org/mule/runtime/http/api/sse/client/SseSourceConfigBuilder.java
@@ -8,6 +8,8 @@ package org.mule.runtime.http.api.sse.client;
 
 import static org.mule.runtime.http.api.sse.client.SseRetryConfig.defaultConfig;
 
+import static java.util.Objects.requireNonNull;
+
 import org.mule.api.annotation.Experimental;
 import org.mule.runtime.http.api.client.HttpRequestOptions;
 import org.mule.runtime.http.api.domain.message.request.HttpRequestBuilder;
@@ -15,7 +17,8 @@ import org.mule.runtime.http.api.domain.message.request.HttpRequestBuilder;
 import java.util.function.Consumer;
 
 /**
- * Builder for instances of {@link SseSourceConfig}.
+ * Builder for instances of {@link SseSourceConfig}. The url is required, so you have to specify it in the
+ * constructor of the builder, and the rest of the parameters have their own with* method.
  * <p>
  * This API is EXPERIMENTAL. Do not use it until it is stable.
  *
@@ -31,24 +34,44 @@ public class SseSourceConfigBuilder {
   private HttpRequestOptions requestOptions = HttpRequestOptions.builder().build();
 
   public SseSourceConfigBuilder(String url) {
-    this.url = url;
+    this.url = requireNonNull(url);
   }
 
+  /**
+   * Configures the {@link SseRetryConfig}.
+   * @param retryConfig the retry config.
+   * @return this same builder.
+   */
   public SseSourceConfigBuilder withRetryConfig(SseRetryConfig retryConfig) {
     this.retryConfig = retryConfig;
     return this;
   }
 
+  /**
+   * Configures a callback to customize the initiator request builder.
+   * @param requestCustomizer the callback.
+   * @return this same builder.
+   */
   public SseSourceConfigBuilder withRequestCustomizer(Consumer<HttpRequestBuilder> requestCustomizer) {
     this.requestCustomizer = requestCustomizer;
     return this;
   }
 
+  /**
+   * Configures the {@link HttpRequestOptions} of the initiator request.
+   * @param requestOptions the request options.
+   * @return this same builder.
+   */
   public SseSourceConfigBuilder withRequestOptions(HttpRequestOptions requestOptions) {
     this.requestOptions = requestOptions;
     return this;
   }
 
+  /**
+   * Builds an instance of {@link SseSourceConfig} with the specified parameters.
+   * This method is intended to be called only once.
+   * @return the new instance of {@link SseSourceConfig}.
+   */
   public SseSourceConfig build() {
     return new SseSourceConfig(url, retryConfig, requestCustomizer, requestOptions);
   }
