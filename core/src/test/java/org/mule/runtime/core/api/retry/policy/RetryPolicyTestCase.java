f47f9cafed127ef02cda2daa77c12bf3b897a63d
chris-ebert <cebert@salesforce.com>
2025-03-21 14:02:39 -0700

    Adding RetryPolicy tests

--- core/src/test/java/org/mule/runtime/core/api/retry/policy/RetryPolicyTestCase.java
+++ core/src/test/java/org/mule/runtime/core/api/retry/policy/RetryPolicyTestCase.java
@@ -8,28 +8,75 @@ package org.mule.runtime.core.api.retry.policy;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mule.runtime.api.scheduler.Scheduler;
+import org.mule.runtime.core.api.event.CoreEvent;
+import org.reactivestreams.Publisher;
+import reactor.test.subscriber.TestSubscriber;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.withSettings;
 
+@ExtendWith(MockitoExtension.class)
 class RetryPolicyTestCase {
 
-    @BeforeEach
-    void setUp() {
-    }
+  private RetryPolicy policy;
+  @Mock
+  private Publisher<CoreEvent> publisher;
+  @Mock
+  private Consumer<Throwable> onExhausted;
+  @Mock
+  private Function<Throwable, Throwable> errorFunction;
+  @Mock
+  private Scheduler retryScheduler;
+  @Mock
+  private Supplier<CompletableFuture<PolicyStatus>> futureSupplier;
+  @Mock
+  private Consumer<Throwable> onRetry;
 
-    @Test
-    void applyPolicy() {
-    }
+  @BeforeEach
+  void setUp() {
+    policy = mock(RetryPolicy.class, withSettings().defaultAnswer(CALLS_REAL_METHODS));
+  }
 
-    @Test
-    void testApplyPolicy() {
-    }
+  @Test
+  void testApplyPolicy() throws ExecutionException, InterruptedException {
+    final CompletableFuture<PolicyStatus> future = policy.applyPolicy(
+                                                                      futureSupplier,
+                                                                      t -> true,
+                                                                      onRetry,
+                                                                      onExhausted,
+                                                                      errorFunction,
+                                                                      retryScheduler);
+    final PolicyStatus result = future.get();
+    assertThat(result, is(notNullValue()));
+  }
 
-    @Test
-    void testApplyPolicy1() {
-    }
+  @Test
+  void testApplyPolicy1() {
+    final Publisher<CoreEvent> result = policy.applyPolicy(publisher, t -> true, onExhausted, errorFunction);
+    result.subscribe(TestSubscriber.create());
 
-    @Test
-    void testApplyPolicy2() {
-    }
+    assertThat(result, is(notNullValue()));
+  }
+
+  @Test
+  void testApplyPolicy2() {
+    final Publisher<CoreEvent> result = policy.applyPolicy(publiser, t -> true, onExhausted, errorFunction, retryScheduler);
+    result.subscribe(TestSubscriber.create());
+    assertThat(result, is(notNullValue()));
+  }
 }
