3f157224aa10ca7ec234dbbf5ad9f9aad9918e17
Ezequiel Werner <eze210@gmail.com>
2025-04-01 21:16:40 -0300

    Make SSE API to be flexible enough to implement Streamable HTTP transport

--- modules/http-api/src/main/java/org/mule/runtime/http/api/sse/server/SseRequestContext.java
+++ modules/http-api/src/main/java/org/mule/runtime/http/api/sse/server/SseRequestContext.java
@@ -9,6 +9,7 @@ package org.mule.runtime.http.api.sse.server;
 import org.mule.api.annotation.Experimental;
 import org.mule.api.annotation.NoImplement;
 import org.mule.runtime.http.api.domain.message.request.HttpRequest;
+import org.mule.runtime.http.api.domain.message.response.HttpResponse;
 import org.mule.runtime.http.api.server.HttpServer;
 
 import java.util.concurrent.CompletableFuture;
@@ -49,4 +50,15 @@ public interface SseRequestContext {
    *         the response failed to be sent.
    */
   CompletableFuture<Void> reject(int statusCode, String reasonPhrase);
+
+  /**
+   * Allows an SSE server to send a custom response instead of the standard one. If this response is sent, the event stream will
+   * not be sent.
+   * 
+   * @param response the custom response that overrides the SSE mechanism.
+   * @return a future that will be completed when the response was successfully sent. It can also be completed exceptionally if
+   *         the response failed to be sent.
+   * @since 4.10.0, 4.9.4
+   */
+  CompletableFuture<Void> customResponse(HttpResponse response);
 }
