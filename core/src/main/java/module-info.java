713db21e7a9619a65832f3328723b3480e3b0a1e
Daniel Yang <97512987+d4nielyan9@users.noreply.github.com>
2025-04-23 09:57:25 -0700

    W-17703164: Ensure that parameters field is not serialized in InternalEventImplem (#14334)
    * Ensure that parameters field is not serialized in InternalEventImplementation
    
    (cherry picked from commit e02663b7195a5743e5ddeaf66bebd5c80d1e4ccc)

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -175,6 +175,9 @@ module org.mule.runtime.core {
   exports org.mule.runtime.core.privileged.el;
   // for MuleFwk, MUnit, MTF, ApiGateway, Tracing and Validation
   exports org.mule.runtime.core.privileged.event;
+  // for Kryo
+  exports org.mule.runtime.core.privileged.event.context to
+      com.mulesoft.mule.runtime.kryo;
   // for MuleFwk, MUnit, ApiKit, DataWeave and Validation
   exports org.mule.runtime.core.privileged.exception;
   // for test-components
@@ -584,6 +587,7 @@ module org.mule.runtime.core {
   opens org.mule.runtime.core.internal.execution to
       spring.core;
   opens org.mule.runtime.core.internal.event to
+      com.mulesoft.mule.runtime.kryo,
       kryo.shaded;
   opens org.mule.runtime.core.internal.lock to
       org.mule.runtime.core.components,
