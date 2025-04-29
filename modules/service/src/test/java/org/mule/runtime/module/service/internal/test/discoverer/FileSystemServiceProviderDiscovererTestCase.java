d2e3b1e87c79aa1038be56b82d87a643eb0f274a
Ezequiel Werner <eze210@gmail.com>
2025-04-28 23:13:05 -0300

    W-17750750: Fix MuleApplicationClassLoader leak on service lazy initialization (#14349) (#14364)
    (cherry picked from commit 1268ddd1f46cbdcf5504928ee67bd52092cf793f)

--- modules/service/src/test/java/org/mule/runtime/module/service/internal/test/discoverer/FileSystemServiceProviderDiscovererTestCase.java
+++ modules/service/src/test/java/org/mule/runtime/module/service/internal/test/discoverer/FileSystemServiceProviderDiscovererTestCase.java
@@ -9,15 +9,20 @@ package org.mule.runtime.module.service.internal.test.discoverer;
 import static org.mule.runtime.container.api.MuleFoldersUtil.getServicesFolder;
 import static org.mule.runtime.core.api.config.MuleProperties.MULE_HOME_DIRECTORY_PROPERTY;
 import static org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptorValidatorBuilder.builder;
+import static org.mule.tck.junit4.matcher.Eventually.eventually;
+import static org.mule.tck.util.CollectableReference.collectedByGc;
 import static org.mule.test.allure.AllureConstants.ServicesFeature.SERVICES;
 import static org.mule.test.allure.AllureConstants.ServicesFeature.ServicesStory.SERVICE_PROVIDER_DISCOVERER;
 
+import static java.security.AccessController.doPrivileged;
+import static java.security.AccessController.getContext;
+
 import static org.apache.commons.io.FileUtils.moveDirectory;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.any;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
@@ -30,6 +35,7 @@ import org.mule.runtime.api.service.ServiceDefinition;
 import org.mule.runtime.api.service.ServiceProvider;
 import org.mule.runtime.module.artifact.api.classloader.ArtifactClassLoader;
 import org.mule.runtime.module.artifact.api.classloader.ClassLoaderLookupPolicy;
+import org.mule.runtime.module.artifact.api.classloader.MuleArtifactClassLoader;
 import org.mule.runtime.module.artifact.api.descriptor.ArtifactDescriptorValidator;
 import org.mule.runtime.module.artifact.api.descriptor.BundleDescriptor;
 import org.mule.runtime.module.artifact.api.descriptor.BundleDescriptorLoader;
@@ -43,20 +49,27 @@ import org.mule.runtime.module.service.internal.discoverer.FileSystemServiceProv
 import org.mule.runtime.module.service.internal.test.discoverer.ServiceRegistryTestCase.FooService;
 import org.mule.tck.junit4.AbstractMuleTestCase;
 import org.mule.tck.junit4.rule.SystemPropertyTemporaryFolder;
+import org.mule.tck.util.CollectableReference;
 
 import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.security.AccessControlContext;
+import java.security.CodeSource;
+import java.security.PermissionCollection;
+import java.security.Principal;
+import java.security.PrivilegedAction;
+import java.security.ProtectionDomain;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Story;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mockito;
-
-import io.qameta.allure.Feature;
-import io.qameta.allure.Story;
 
 @Feature(SERVICES)
 @Story(SERVICE_PROVIDER_DISCOVERER)
@@ -82,7 +95,7 @@ public class FileSystemServiceProviderDiscovererTestCase extends AbstractMuleTes
 
     BundleDescriptorLoader bundleDescriptorLoaderMock = mock(BundleDescriptorLoader.class);
     when(bundleDescriptorLoaderMock.supportsArtifactType(ArtifactType.SERVICE)).thenReturn(true);
-    when(bundleDescriptorLoaderMock.load(Mockito.any(File.class), Mockito.any(Map.class), eq(ArtifactType.SERVICE)))
+    when(bundleDescriptorLoaderMock.load(any(File.class), any(Map.class), eq(ArtifactType.SERVICE)))
         .thenReturn(new BundleDescriptor.Builder()
             .setGroupId("mockGroupId")
             .setArtifactId("mockArtifactId")
@@ -92,13 +105,13 @@ public class FileSystemServiceProviderDiscovererTestCase extends AbstractMuleTes
             .build());
 
 
-    when(descriptorLoaderRepository.get(anyString(), ArgumentMatchers.any(), argThat(equalTo(BundleDescriptorLoader.class))))
+    when(descriptorLoaderRepository.get(anyString(), any(), argThat(equalTo(BundleDescriptorLoader.class))))
         .thenReturn(bundleDescriptorLoaderMock);
-    when(descriptorLoaderRepository.get(anyString(), ArgumentMatchers.any(),
+    when(descriptorLoaderRepository.get(anyString(), any(),
                                         argThat(equalTo(ClassLoaderConfigurationLoader.class))))
                                             .thenReturn(mock(ClassLoaderConfigurationLoader.class));
 
-    doNothing().when(artifactDescriptorValidator).validate(ArgumentMatchers.any());
+    doNothing().when(artifactDescriptorValidator).validate(any());
   }
 
   @Test
@@ -117,10 +130,10 @@ public class FileSystemServiceProviderDiscovererTestCase extends AbstractMuleTes
     installService("barService", BarServiceProvider.class);
 
     ArtifactClassLoader serviceClassLoader = mock(ArtifactClassLoader.class);
-    when(serviceClassLoaderFactory.create(argThat(any(String.class)),
-                                          argThat(any(ServiceDescriptor.class)), argThat(any(ClassLoader.class)), argThat(any(
-                                                                                                                              ClassLoaderLookupPolicy.class))))
-                                                                                                                                  .thenReturn(serviceClassLoader);
+    when(serviceClassLoaderFactory.create(anyString(),
+                                          any(ServiceDescriptor.class), any(ClassLoader.class), any(
+                                                                                                    ClassLoaderLookupPolicy.class)))
+                                                                                                        .thenReturn(serviceClassLoader);
     final FileSystemServiceProviderDiscoverer serviceProviderDiscoverer =
         new FileSystemServiceProviderDiscoverer(containerClassLoader, serviceClassLoaderFactory, descriptorLoaderRepository,
                                                 builder());
@@ -136,6 +149,53 @@ public class FileSystemServiceProviderDiscovererTestCase extends AbstractMuleTes
 
     assertThat(FooServiceProvider.INVOKED, is(true));
     assertThat(BarServiceProvider.INVOKED, is(true));
+  }
+
+  @Test
+  @Issue("W-17750750")
+  public void serviceClassLoaderCreationDoesNotLeakCallerClassLoader() throws Exception {
+    CollectableReference<ClassLoader> callerClassLoaderReference = new CollectableReference<>(new URLClassLoader(new URL[0]));
+    installService("fooService", FooServiceProvider.class);
+
+    // Some resources created with the service classloader (like the Threads) will retain a reference to the AccessControlContext,
+    // so we're mimicking that behavior by holding this reference, which will be set when the IServiceClassLoaderFactory#create
+    // method is called.
+    AtomicReference<AccessControlContext> accOnServiceClassLoaderCreation = new AtomicReference<>();
+    when(serviceClassLoaderFactory.create(anyString(),
+                                          any(ServiceDescriptor.class), any(ClassLoader.class),
+                                          any(ClassLoaderLookupPolicy.class)))
+                                              .thenAnswer(invocation -> {
+                                                // Just intercepting this call to save a strong reference to the ACC.
+                                                accOnServiceClassLoaderCreation.set(getContext());
+                                                return mock(MuleArtifactClassLoader.class);
+                                              });
+
+    // Now we discover the service lazily
+    final FileSystemServiceProviderDiscoverer serviceProviderDiscoverer =
+        new FileSystemServiceProviderDiscoverer(containerClassLoader, serviceClassLoaderFactory, descriptorLoaderRepository,
+                                                builder());
+    final List<ServiceAssembly> assemblies = serviceProviderDiscoverer.discover();
+    assertThat("Should only have the foo service", assemblies.size(), equalTo(1));
+    ServiceAssembly fooServiceAssembly = assemblies.get(0);
+
+    // And we trigger the actual service classloader creation with the caller ACC in the stack.
+    doPrivileged((PrivilegedAction<Void>) () -> {
+      fooServiceAssembly.getClassLoader();
+      return null;
+    }, mockAccessControlContext(callerClassLoaderReference.get()));
+
+    // We do have a strong reference to the AccessControlContext at accOnServiceClassLoaderCreation, but we
+    // should not be retaining the caller classloader reference anywhere.
+    assertThat(callerClassLoaderReference, is(eventually(collectedByGc())));
+  }
+
+  private static AccessControlContext mockAccessControlContext(ClassLoader classLoader) {
+    return new AccessControlContext(new ProtectionDomain[] {
+        new ProtectionDomain(mock(CodeSource.class),
+                             mock(PermissionCollection.class),
+                             classLoader,
+                             new Principal[0])
+    });
   }
 
   private void installService(String serviceName, Class<? extends ServiceProvider> providerClass) throws Exception {
