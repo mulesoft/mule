44d258f42277e9ae30c98e19749fae4d5c263f05
Axel Sanguinetti <asanguinetti@salesforce.com>
2025-05-08 21:57:29 -0300

    Refactor tests

--- runtime-extension-model/src/test/java/org/mule/runtime/core/extension/ParseTemplateUsingLegacyDefaultTargetValueTestCase.java
+++ runtime-extension-model/src/test/java/org/mule/runtime/core/extension/ParseTemplateUsingLegacyDefaultTargetValueTestCase.java
@@ -4,7 +4,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.runtime.core.api.extension.provider;
+package org.mule.runtime.core.extension;
 
 import static org.mule.runtime.api.dsl.DslResolvingContext.nullDslResolvingContext;
 import static org.mule.runtime.api.meta.model.parameter.ParameterGroupModel.OUTPUT;
@@ -15,10 +15,8 @@ import static org.mule.runtime.extension.api.loader.ExtensionModelLoadingRequest
 import static org.mule.test.allure.AllureConstants.MuleDsl.DslValidationStory.DSL_VALIDATION_STORY;
 import static org.mule.test.allure.AllureConstants.MuleDsl.MULE_DSL;
 
-import static java.lang.Boolean.parseBoolean;
 import static java.lang.System.clearProperty;
 import static java.lang.System.setProperty;
-import static java.util.Arrays.asList;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -26,11 +24,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import org.mule.runtime.api.meta.model.ExtensionModel;
 import org.mule.runtime.api.meta.model.operation.OperationModel;
 import org.mule.runtime.api.meta.model.parameter.ParameterModel;
+import org.mule.runtime.core.api.extension.provider.MuleExtensionModelDeclarer;
+import org.mule.runtime.core.api.extension.provider.MuleExtensionModelProvider;
 import org.mule.runtime.extension.api.loader.ExtensionLoadingContext;
 import org.mule.runtime.extension.api.loader.ExtensionModelLoader;
 import org.mule.runtime.extension.api.loader.ExtensionModelLoadingRequest;
-
-import java.util.Collection;
 
 import io.qameta.allure.Feature;
 import io.qameta.allure.Issue;
@@ -38,35 +36,17 @@ import io.qameta.allure.Story;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
 @Feature(MULE_DSL)
 @Story(DSL_VALIDATION_STORY)
-@RunWith(Parameterized.class)
-public class MuleExtensionModelDeclarerTestCase {
+public class ParseTemplateUsingLegacyDefaultTargetValueTestCase {
 
   private String isParseTemplateUseLegacyDefaultTargetValueOriginalValue;
-  private final String isParseTemplateUseLegacyDefaultTargetValue;
-
-  @Parameterized.Parameters(
-      name = "Use parseTemplate legacy default target value: {0}")
-  public static Collection<Object[]> data() {
-    return asList(new Object[][] {
-        {"true"},
-        {"false"}
-    });
-  }
-
-  public MuleExtensionModelDeclarerTestCase(String isParseTemplateUseLegacyDefaultTargetValue) {
-    this.isParseTemplateUseLegacyDefaultTargetValue = isParseTemplateUseLegacyDefaultTargetValue;
-  }
 
   // TODO W-13974116: Remove circular dependency when adding org.mule.tests:mule-tests-unit to this module
   @Before
   public void setUp() {
-    isParseTemplateUseLegacyDefaultTargetValueOriginalValue =
-        setProperty(PARSE_TEMPLATE_USE_LEGACY_DEFAULT_TARGET_VALUE, isParseTemplateUseLegacyDefaultTargetValue);
+    isParseTemplateUseLegacyDefaultTargetValueOriginalValue = setProperty(PARSE_TEMPLATE_USE_LEGACY_DEFAULT_TARGET_VALUE, "true");
   }
 
   @After
@@ -83,8 +63,8 @@ public class MuleExtensionModelDeclarerTestCase {
   public void whenIsParseTemplateUseLegacyDefaultTargetValueTheTargetValueIsMessage() {
     // We want to assert over the ExtensionModel and not the ExtensionDeclarer, because the enrichers may be doing overrides that
     // we don't expect
-    // We also can't use MuleExtensionModelProvider#getExtensionModel because that one caches the result in the class and we need
-    // to recreate it with and without the property
+    // We also can't use MuleExtensionModelProvider#getExtensionModel because that one caches the result in the class and that
+    // will cause interference between test cases
     ExtensionModel muleExtensionModel = new TestExtensionModelLoader()
         .loadExtensionModel(new MuleExtensionModelDeclarer(createMockedFactory()).createExtensionModel(), loadingRequest());
     OperationModel parseTemplateOperation = muleExtensionModel.getOperationModel("parseTemplate").get();
@@ -98,11 +78,7 @@ public class MuleExtensionModelDeclarerTestCase {
         .flatMap(pgm -> pgm.getParameter(TARGET_VALUE_PARAMETER_NAME))
         .get();
 
-    if (parseBoolean(isParseTemplateUseLegacyDefaultTargetValue)) {
-      assertThat(targetValueParam.getDefaultValue(), equalTo("#[message]"));
-    } else {
-      assertThat(targetValueParam.getDefaultValue(), equalTo("#[payload]"));
-    }
+    assertThat(targetValueParam.getDefaultValue(), equalTo("#[message]"));
   }
 
   private static ExtensionModelLoadingRequest loadingRequest() {
