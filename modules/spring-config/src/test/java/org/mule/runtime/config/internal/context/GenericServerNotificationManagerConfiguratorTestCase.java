b4c0751f1a9d4aad827c689ce8974e4270bfa009
Rodrigo Merino <elrodro83@users.noreply.github.com>
2025-05-12 18:23:58 -0300

    W-18476347: Make the shutdown timeout configurable (#14372)

--- modules/spring-config/src/test/java/org/mule/runtime/config/internal/context/GenericServerNotificationManagerConfiguratorTestCase.java
+++ modules/spring-config/src/test/java/org/mule/runtime/config/internal/context/GenericServerNotificationManagerConfiguratorTestCase.java
@@ -8,12 +8,10 @@ package org.mule.runtime.config.internal.context;
 
 import static org.mule.runtime.core.api.config.MuleProperties.OBJECT_NOTIFICATION_MANAGER;
 
-import static java.util.Collections.emptyMap;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 
-import org.mule.runtime.config.internal.SpringXmlConfigurationBuilder;
+import org.mule.runtime.config.utils.AppParserConfigurationBuilder;
 import org.mule.runtime.core.api.config.ConfigurationBuilder;
 import org.mule.runtime.core.internal.context.MuleContextWithRegistry;
 import org.mule.tck.junit4.AbstractMuleContextTestCase;
@@ -24,11 +22,12 @@ public class GenericServerNotificationManagerConfiguratorTestCase extends Abstra
 
   @Override
   protected ConfigurationBuilder getBuilder() throws Exception {
-    return new SpringXmlConfigurationBuilder(new String[] {"./generic-server-notification-manager-test.xml"}, emptyMap());
+    return new AppParserConfigurationBuilder(new String[] {"./generic-server-notification-manager-test.xml"});
   }
 
   @Test
   public void testRegistryHasAGenericServerNotificationManagerIfNoDynamicConfigIsPresent() {
     assertThat(((MuleContextWithRegistry) muleContext).getRegistry().lookupObject(OBJECT_NOTIFICATION_MANAGER), notNullValue());
   }
+
 }
