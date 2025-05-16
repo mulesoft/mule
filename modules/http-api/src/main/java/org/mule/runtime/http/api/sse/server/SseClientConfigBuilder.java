a9b2df50643465027a116a0ab60fd3510cfabb45
Ezequiel Werner <eze210@gmail.com>
2025-05-16 11:30:28 -0300

    Adding a configuration with client id and remote address

---
+++ modules/http-api/src/main/java/org/mule/runtime/http/api/sse/server/SseClientConfigBuilder.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.http.api.sse.server;
+
+import java.net.InetSocketAddress;
+
+public class SseClientConfigBuilder {
+
+  private String clientId;
+  private InetSocketAddress remoteAddress;
+
+  public SseClientConfig build() {
+    return new SseClientConfig(clientId, remoteAddress);
+  }
+
+  public SseClientConfigBuilder withClientId(String clientId) {
+    this.clientId = clientId;
+    return this;
+  }
+
+  public SseClientConfigBuilder withRemoteAddress(InetSocketAddress remoteAddress) {
+    this.remoteAddress = remoteAddress;
+    return this;
+  }
+}
