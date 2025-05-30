c03cb5330294193d290cb473348d2b7f05c6ae88
Axel Sanguinetti <asanguinetti@salesforce.com>
2025-05-30 14:57:33 -0300

    Tests with kit configs in parent artifact and static import

--- modules/spring-config/src/test/java/org/mule/runtime/config/internal/context/FlowClassifierFactoryTestCase.java
+++ modules/spring-config/src/test/java/org/mule/runtime/config/internal/context/FlowClassifierFactoryTestCase.java
@@ -16,6 +16,7 @@ import static org.mule.runtime.core.api.management.stats.FlowClassifier.FlowType
 import static java.util.Collections.singletonList;
 import static java.util.Collections.emptyList;
 import static java.util.Arrays.asList;
+import static java.util.Optional.of;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -30,25 +31,36 @@ import org.mule.runtime.ast.api.ComponentParameterAst;
 import org.mule.runtime.core.api.management.stats.FlowClassifier;
 import org.mule.tck.junit4.AbstractMuleTestCase;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Optional;
-
 public class FlowClassifierFactoryTestCase extends AbstractMuleTestCase {
 
+  private static final String APIKIT_EXTENSION_NAME = "APIKit";
+  private static final String SOAPKIT_EXTENSION_NAME = "APIKit for SOAP";
+
   private ArtifactAst artifactAst;
+  private ArtifactAst parentArtifactAst;
   private ComponentAst apiKitConfig;
   private ComponentAst soapKitConfig;
+  private ComponentAst parentApiKitConfig;
+  private ComponentAst parentSoapKitConfig;
 
   @Before
   public void setUp() {
     artifactAst = mock(ArtifactAst.class);
+    parentArtifactAst = mock(ArtifactAst.class);
     apiKitConfig = mock(ComponentAst.class);
     soapKitConfig = mock(ComponentAst.class);
+    parentApiKitConfig = mock(ComponentAst.class);
+    parentSoapKitConfig = mock(ComponentAst.class);
 
-    setupConfigMock(apiKitConfig, "api-config", "APIKit");
-    setupConfigMock(soapKitConfig, "soapkit-config", "APIKit for SOAP");
+    setupConfigMock(apiKitConfig, "api-config", APIKIT_EXTENSION_NAME);
+    setupConfigMock(soapKitConfig, "soapkit-config", SOAPKIT_EXTENSION_NAME);
+    setupConfigMock(parentApiKitConfig, "parent-api-config", APIKIT_EXTENSION_NAME);
+    setupConfigMock(parentSoapKitConfig, "parent-soapkit-config", SOAPKIT_EXTENSION_NAME);
 
     // Setup APIKit config with flow mappings
     ComponentAst flowMapping = mock(ComponentAst.class);
@@ -58,35 +70,46 @@ public class FlowClassifierFactoryTestCase extends AbstractMuleTestCase {
     ComponentParameterAst flowMappingsParameter = mock(ComponentParameterAst.class);
     when(flowMappingsParameter.getValue()).thenReturn(right(singletonList(flowMapping)));
     when(apiKitConfig.getParameter(DEFAULT_GROUP_NAME, "flowMappings")).thenReturn(flowMappingsParameter);
+
+    // Setup parent APIKit config with flow mappings
+    ComponentAst parentFlowMapping = mock(ComponentAst.class);
+    ComponentParameterAst parentFlowRefParameter = mock(ComponentParameterAst.class);
+    when(parentFlowRefParameter.getValue()).thenReturn(right("parent-mapped-flow"));
+    when(parentFlowMapping.getParameter("FlowMapping", "flow-ref")).thenReturn(parentFlowRefParameter);
+    ComponentParameterAst parentFlowMappingsParameter = mock(ComponentParameterAst.class);
+    when(parentFlowMappingsParameter.getValue()).thenReturn(right(singletonList(parentFlowMapping)));
+    when(parentApiKitConfig.getParameter(DEFAULT_GROUP_NAME, "flowMappings")).thenReturn(parentFlowMappingsParameter);
   }
 
-  private FlowClassifier createClassifier(java.util.List<ComponentAst> components) {
+  private FlowClassifier createClassifier(List<ComponentAst> components, List<ComponentAst> parentComponents) {
     when(artifactAst.topLevelComponentsStream()).thenAnswer(invocation -> components.stream());
+    when(artifactAst.getParent()).thenReturn(of(parentArtifactAst));
+    when(parentArtifactAst.topLevelComponentsStream()).thenAnswer(invocation -> parentComponents.stream());
     return new FlowClassifierFactory(artifactAst).create();
   }
 
   @Test
   public void testGenericFlow() {
-    FlowClassifier classifier = createClassifier(emptyList());
+    FlowClassifier classifier = createClassifier(emptyList(), emptyList());
     assertThat("generic-flow", classifier.getFlowType("generic-flow"), is(GENERIC));
   }
 
   @Test
   public void testApiKitFlow() {
-    FlowClassifier classifier = createClassifier(singletonList(apiKitConfig));
+    FlowClassifier classifier = createClassifier(singletonList(apiKitConfig), emptyList());
     assertThat("mapped-flow", classifier.getFlowType("mapped-flow"), is(APIKIT));
     assertThat("flow:api-config", classifier.getFlowType("flow:api-config"), is(APIKIT));
   }
 
   @Test
   public void testSoapKitFlow() {
-    FlowClassifier classifier = createClassifier(singletonList(soapKitConfig));
+    FlowClassifier classifier = createClassifier(singletonList(soapKitConfig), emptyList());
     assertThat("flow:\\soapkit-config", classifier.getFlowType("flow:\\soapkit-config"), is(SOAPKIT));
   }
 
   @Test
   public void testMultipleConfigs() {
-    FlowClassifier classifier = createClassifier(asList(apiKitConfig, soapKitConfig));
+    FlowClassifier classifier = createClassifier(asList(apiKitConfig, soapKitConfig), emptyList());
     assertThat("mapped-flow", classifier.getFlowType("mapped-flow"), is(APIKIT));
     assertThat("flow:api-config", classifier.getFlowType("flow:api-config"), is(APIKIT));
     assertThat("flow:\\soapkit-config", classifier.getFlowType("flow:\\soapkit-config"), is(SOAPKIT));
@@ -95,31 +118,66 @@ public class FlowClassifierFactoryTestCase extends AbstractMuleTestCase {
 
   @Test
   public void testApiKitFlowWithNonMatchingConfig() {
-    FlowClassifier classifier = createClassifier(singletonList(apiKitConfig));
+    FlowClassifier classifier = createClassifier(singletonList(apiKitConfig), emptyList());
     assertThat("flow:non-matching-config", classifier.getFlowType("flow:non-matching-config"), is(GENERIC));
   }
 
   @Test
   public void testSoapKitFlowWithNonMatchingConfig() {
-    FlowClassifier classifier = createClassifier(singletonList(soapKitConfig));
+    FlowClassifier classifier = createClassifier(singletonList(soapKitConfig), emptyList());
     assertThat("flow:\\non-matching-config", classifier.getFlowType("flow:\\non-matching-config"), is(GENERIC));
   }
 
   @Test
   public void testMultipleConfigsWithNonMatchingFlows() {
-    FlowClassifier classifier = createClassifier(asList(apiKitConfig, soapKitConfig));
+    FlowClassifier classifier = createClassifier(asList(apiKitConfig, soapKitConfig), emptyList());
     assertThat("flow:non-matching-api-config", classifier.getFlowType("flow:non-matching-api-config"), is(GENERIC));
     assertThat("flow:\\non-matching-soap-config", classifier.getFlowType("flow:\\non-matching-soap-config"), is(GENERIC));
   }
 
+  @Test
+  public void testApiKitFlowFromParent() {
+    FlowClassifier classifier = createClassifier(emptyList(), singletonList(parentApiKitConfig));
+    assertThat("parent-mapped-flow", classifier.getFlowType("parent-mapped-flow"), is(APIKIT));
+    assertThat("flow:parent-api-config", classifier.getFlowType("flow:parent-api-config"), is(APIKIT));
+  }
+
+  @Test
+  public void testSoapKitFlowFromParent() {
+    FlowClassifier classifier = createClassifier(emptyList(), singletonList(parentSoapKitConfig));
+    assertThat("flow:\\parent-soapkit-config", classifier.getFlowType("flow:\\parent-soapkit-config"), is(SOAPKIT));
+  }
+
+  @Test
+  public void testMultipleConfigsFromParent() {
+    FlowClassifier classifier = createClassifier(emptyList(), asList(parentApiKitConfig, parentSoapKitConfig));
+    assertThat("parent-mapped-flow", classifier.getFlowType("parent-mapped-flow"), is(APIKIT));
+    assertThat("flow:parent-api-config", classifier.getFlowType("flow:parent-api-config"), is(APIKIT));
+    assertThat("flow:\\parent-soapkit-config", classifier.getFlowType("flow:\\parent-soapkit-config"), is(SOAPKIT));
+    assertThat("generic-flow", classifier.getFlowType("generic-flow"), is(GENERIC));
+  }
+
+  @Test
+  public void testConfigsFromBothMainAndParent() {
+    FlowClassifier classifier = createClassifier(asList(apiKitConfig, soapKitConfig),
+                                                 asList(parentApiKitConfig, parentSoapKitConfig));
+    assertThat("mapped-flow", classifier.getFlowType("mapped-flow"), is(APIKIT));
+    assertThat("flow:api-config", classifier.getFlowType("flow:api-config"), is(APIKIT));
+    assertThat("flow:\\soapkit-config", classifier.getFlowType("flow:\\soapkit-config"), is(SOAPKIT));
+    assertThat("parent-mapped-flow", classifier.getFlowType("parent-mapped-flow"), is(APIKIT));
+    assertThat("flow:parent-api-config", classifier.getFlowType("flow:parent-api-config"), is(APIKIT));
+    assertThat("flow:\\parent-soapkit-config", classifier.getFlowType("flow:\\parent-soapkit-config"), is(SOAPKIT));
+    assertThat("generic-flow", classifier.getFlowType("generic-flow"), is(GENERIC));
+  }
+
   private void setupConfigMock(ComponentAst config, String configId, String extensionName) {
     when(config.getComponentType()).thenReturn(CONFIG);
-    when(config.getComponentId()).thenReturn(Optional.of(configId));
+    when(config.getComponentId()).thenReturn(of(configId));
     ExtensionModel extensionModel = mock(ExtensionModel.class);
     when(extensionModel.getName()).thenReturn(extensionName);
     when(config.getExtensionModel()).thenReturn(extensionModel);
     ConfigurationModel configModel = mock(ConfigurationModel.class);
     when(configModel.getName()).thenReturn("config");
-    when(config.getModel(ConfigurationModel.class)).thenReturn(Optional.of(configModel));
+    when(config.getModel(ConfigurationModel.class)).thenReturn(of(configModel));
   }
 }
