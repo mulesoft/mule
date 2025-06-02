52babd53f03c48e4e63ec9084d23da7b6ffd77ef
Rodrigo Merino <elrodro83@users.noreply.github.com>
2025-06-02 09:25:40 -0300

    W-18346941: Refactor troubleshooting operations to use a writer (#14346) (#14437)

--- modules/troubleshooting/src/main/java/org/mule/runtime/module/troubleshooting/api/TroubleshootingOperationCallback.java
+++ modules/troubleshooting/src/main/java/org/mule/runtime/module/troubleshooting/api/TroubleshootingOperationCallback.java
@@ -8,25 +8,28 @@ package org.mule.runtime.module.troubleshooting.api;
 
 import org.mule.api.annotation.Experimental;
 
-import java.io.Serializable;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Map;
 
 /**
  * Internal interface to the troubleshooting operation callbacks.
- * <p>
- * <b>Important:</b> Return externalizable objects to avoid marshalling errors when using it in an external tool.
  *
- * @since 4.5
+ * @since 4.5, refactored in 4.10
  */
 @Experimental
 public interface TroubleshootingOperationCallback {
 
   /**
-   * The operation logic. The troubleshooting operations should be able to be triggered from an external client, so the
-   * implementation of each operation callback should return an object that can be marshaled and un-marshaled.
+   * The operation logic.
+   * <p>
+   * The troubleshooting operations should be able to be triggered from an external client.
+   * <p>
+   * The implementation of each operation callback should write a human-readable result.
    *
-   * @param arguments A dictionary with the argument names and values.
-   * @return a {@link Serializable} object, which is the operation result.
+   * @param arguments    A dictionary with the argument names and values.
+   * @param resultWriter A writer to write the result to.
+   * @throws IOException when an error occurs writing to {@code resultWriter}.
    */
-  Serializable execute(Map<String, String> arguments);
+  void execute(Map<String, String> arguments, Writer resultWriter) throws IOException;
 }
