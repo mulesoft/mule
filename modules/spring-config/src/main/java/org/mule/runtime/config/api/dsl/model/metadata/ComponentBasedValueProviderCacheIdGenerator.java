940d2d168d88d52ab4a8162ced00a83c5c79fae3
Rodrigo E. Merino <rmerino@salesforce.com>
2025-06-05 22:33:01 -0300

    deprecation for tooling

---
+++ modules/spring-config/src/main/java/org/mule/runtime/config/api/dsl/model/metadata/ComponentBasedValueProviderCacheIdGenerator.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.config.api.dsl.model.metadata;
+
+import static org.mule.runtime.api.util.Preconditions.checkArgument;
+
+import org.mule.runtime.api.dsl.DslResolvingContext;
+import org.mule.runtime.ast.api.ComponentAst;
+import org.mule.runtime.config.api.dsl.model.DslElementModelFactory;
+import org.mule.runtime.core.internal.value.cache.ValueProviderCacheId;
+import org.mule.runtime.core.internal.value.cache.ValueProviderCacheIdGenerator;
+import org.mule.runtime.metadata.api.locator.ComponentLocator;
+import org.mule.runtime.metadata.internal.cache.DslElementBasedValueProviderCacheIdGenerator;
+
+import java.util.Optional;
+
+@Deprecated(since = "4.10", forRemoval = true)
+public class ComponentBasedValueProviderCacheIdGenerator implements ValueProviderCacheIdGenerator<ComponentAst> {
+
+  private final DslElementModelFactory elementModelFactory;
+  private final DslElementBasedValueProviderCacheIdGenerator delegate;
+
+  public ComponentBasedValueProviderCacheIdGenerator(DslResolvingContext context,
+                                                     ComponentLocator<ComponentAst> locator) {
+    this.elementModelFactory = DslElementModelFactory.getDefault(context);
+    this.delegate = new DslElementBasedValueProviderCacheIdGenerator(location -> locator.get(location)
+        .map(c -> elementModelFactory.create(c)
+            .orElse(null)));
+  }
+
+  @Override
+  public Optional<ValueProviderCacheId> getIdForResolvedValues(ComponentAst containerComponent, String parameterName) {
+    checkArgument(containerComponent != null, "Cannot generate a Cache Key for a 'null' component");
+    return elementModelFactory.create(containerComponent).flatMap(e -> delegate.getIdForResolvedValues(e, parameterName));
+  }
+
+  @Override
+  public Optional<ValueProviderCacheId> getIdForResolvedValues(ComponentAst containerComponent, String parameterName,
+                                                               String targetPath) {
+    checkArgument(containerComponent != null, "Cannot generate a Cache Key for a 'null' component");
+    return elementModelFactory.create(containerComponent)
+        .flatMap(e -> delegate.getIdForResolvedValues(e, parameterName, targetPath));
+  }
+}
---
+++ modules/spring-config/src/main/java/org/mule/runtime/config/api/dsl/model/metadata/ModelBasedMetadataCacheIdGeneratorFactory.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.config.api.dsl.model.metadata;
+
+import org.mule.runtime.api.dsl.DslResolvingContext;
+import org.mule.runtime.ast.api.ComponentAst;
+import org.mule.runtime.metadata.api.cache.MetadataCacheIdGenerator;
+import org.mule.runtime.metadata.api.cache.MetadataCacheIdGeneratorFactory;
+import org.mule.runtime.metadata.api.locator.ComponentLocator;
+import org.mule.runtime.metadata.internal.cache.ComponentAstBasedMetadataCacheIdGenerator;
+
+
+/**
+ * A {@link ComponentAst} based implementation of a {@link MetadataCacheIdGeneratorFactory}
+ *
+ * @since 4.1.4, 4.2.0
+ */
+@Deprecated(since = "4.10", forRemoval = true)
+public class ModelBasedMetadataCacheIdGeneratorFactory implements MetadataCacheIdGeneratorFactory<ComponentAst> {
+
+  @Override
+  public MetadataCacheIdGenerator<ComponentAst> create(DslResolvingContext context,
+                                                       ComponentLocator<ComponentAst> locator) {
+    return new ComponentAstBasedMetadataCacheIdGenerator(locator);
+  }
+}
