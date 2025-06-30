081f33078ccc5801a9c75b9707cab673c3f3a883
Leandro Ezequiel Barrios <lbarrios@mulesoft.com>
2025-06-30 14:12:00 -0300

    Replaced hardcoded '\n' with System.lineSeparator()

--- modules/service/src/test/java/org/mule/runtime/module/service/internal/test/manager/ServiceSplashScreenTestCase.java
+++ modules/service/src/test/java/org/mule/runtime/module/service/internal/test/manager/ServiceSplashScreenTestCase.java
@@ -6,24 +6,31 @@
  */
 package org.mule.runtime.module.service.internal.test.manager;
 
+import static java.lang.System.lineSeparator;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.junit.Test;
 import org.mule.runtime.module.service.internal.manager.ServiceSplashScreen;
+
+import org.junit.Test;
 
 public class ServiceSplashScreenTestCase {
 
   @Test
   public void splashScreen() {
-    ServiceSplashScreen serviceSplashScreen = new ServiceSplashScreen("Test Case", "one\ntwo\nthree");
-    assertThat(serviceSplashScreen.toString(), is("\n" +
-        "**********************************************************************\n" +
-        "* Started Test Case                                                  *\n" +
-        "*                                                                    *\n" +
-        "* one                                                                *\n" +
-        "* two                                                                *\n" +
-        "* three                                                              *\n" +
+    // When: the splash screen is created with a title and a message
+    String message = "one" + lineSeparator() + "two" + lineSeparator() + "three";
+    ServiceSplashScreen serviceSplashScreen = new ServiceSplashScreen("Test Case", message);
+
+    // Then: the splash screen is formatted correctly
+    assertThat(serviceSplashScreen.toString(), is(lineSeparator() +
+        "**********************************************************************" + lineSeparator() +
+        "* Started Test Case                                                  *" + lineSeparator() +
+        "*                                                                    *" + lineSeparator() +
+        "* one                                                                *" + lineSeparator() +
+        "* two                                                                *" + lineSeparator() +
+        "* three                                                              *" + lineSeparator() +
         "**********************************************************************"));
   }
 
